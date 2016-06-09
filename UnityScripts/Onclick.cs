4aea93dd2151a420fef3f34f03f324f932167fc9
ZHOU XIAOCHI <zhou0201@e.ntu.edu.sg>
2016-06-09 16:00:50 +0800

    dev-unityscript: Added C# script Database_servlet.cs for accessing remote owl database; Added remote query features to Onclick.cs.

--- UnityScripts/Onclick.cs
+++ UnityScripts/Onclick.cs
@@ -5,20 +5,40 @@ using System.Text;
 using System.Xml;
 using System.IO;
 using System.Collections;
+using System.Net.Sockets;
+using System.Net;
 
 public class Onclick : MonoBehaviour
 {
     public Text text;
     public TextAsset BiodieselPlant;
     public GameObject panel;
-    List<Dictionary<string, string>> levels = new List<Dictionary<string, string>>();
-    Dictionary<string, string> obj;
+    private static byte[] result = new byte[1024];
+    static Socket clientSocket;
+    static IPAddress ip;
+    //  List<Dictionary<string, string>> levels = new List<Dictionary<string, string>>();
+    //  Dictionary<string, string> obj;
 
 
     // Use this for initialization
     void Start()
     {
-      //  panel.SetActive(false);
+        //  panel.SetActive(false);
+        //
+        ip = IPAddress.Parse("10.25.188.58");
+        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            clientSocket.Connect(new IPEndPoint(ip, 8885)); //configure the ip address and the port number
+            Debug.Log("Connected");
+
+        }
+        catch
+        {
+
+            return;
+        }
+
     }
 
     // Update is called once per frame
@@ -26,14 +46,37 @@ public class Onclick : MonoBehaviour
     {
 
     }
+
+
+
+
     void OnMouseDown()
     {
         // this object was clicked - do something
         //  Destroy(this.gameObject);
-        getXML();
-      
-    }
+        // getXML();
 
+       
+   
+        try
+        {
+            string sendMessage = "substance:molecularFormula";
+            clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+            Debug.Log("Message Sent");
+           
+        }
+        catch
+        {
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+
+        int receiveLength = clientSocket.Receive(result);
+        Debug.Log(Encoding.ASCII.GetString(result, 0, receiveLength));
+        panel.SetActive(true);
+        text.text = Encoding.ASCII.GetString(result, 0, receiveLength);
+    }
+    /*
     void getXML()
     {
        
@@ -50,6 +93,6 @@ public class Onclick : MonoBehaviour
 
         
         }
-
+        */
 
 }  
