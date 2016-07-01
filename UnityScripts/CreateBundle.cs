8c62a3fa3746eff15cd9cdbe7d8e3f6d78179cbf
ZHOU XIAOCHI <zhou0201@e.ntu.edu.sg>
2016-07-01 17:10:41 +0800

    Added C# Scripts CreateBundle.cs for creating unity asset bundle and LoadBundle.cs for downloading and displaying unity assets bundle

---
+++ UnityScripts/CreateBundle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+public class CreateBundle : MonoBehaviour
+{
+    [MenuItem("CreateBundle/Build Asset Bundles")]
+    static void BuildABs()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/ABs", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
