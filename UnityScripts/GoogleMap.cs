0a1aa36540250b7036ae05e77015c548ea100d34
ZHOU XIAOCHI <zhou0201@e.ntu.edu.sg>
2016-07-11 15:04:39 +0800

    Added C# Scripts GoogleMap.cs for controlling the google map layer and RTS_Camera.cs for controlling the view.

---
+++ UnityScripts/GoogleMap.cs
@@ -0,0 +1,331 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Net.Sockets;
+using System.Net;
+
+public class GoogleMap : MonoBehaviour
+{
+    public enum MapType
+    {
+        RoadMap,
+        Satellite,
+        Terrain,
+        Hybrid
+    }
+     
+    public GoogleMapLocation centerLocation;
+    public GoogleMapLocation markerLocation1;
+    public GoogleMapLocation markerLocation2;
+    public GoogleMapLocation markerLocation3;
+    public int zoom = 16;
+    public MapType mapType;
+    public int size = 4096;
+    public bool doubleResolution = true;
+    public int multipler = 1000000;
+    public GameObject flag;
+    public GameObject marker1;
+    public GameObject marker2;
+    public GameObject marker3;
+    public GameObject structure;
+    public GameObject plant;
+    // Use this for initialization
+
+
+         private static byte[] result = new byte[1024];
+    static Socket clientSocket;
+    static IPAddress ip;
+
+    
+    IEnumerator Start()
+    {
+        GameObject flag = GameObject.Find("Flag");
+      
+
+        Debug.Log(flag.transform.position.y);
+        /*
+
+        ip = IPAddress.Parse("10.25.188.58");
+        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            clientSocket.Connect(new IPEndPoint(ip, 8885)); //configure the ip address and the port number
+            Debug.Log("Connected");
+        }
+        catch
+        {
+        }
+        try
+        {
+            string sendMessage = "geometry";
+            clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+        }
+        catch
+        {
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+        int receiveLength = clientSocket.Receive(result);
+        string url = Encoding.ASCII.GetString(result, 0, receiveLength);
+        Debug.Log("received" + url);
+        */
+
+        WWW www = WWW.LoadFromCacheOrDownload("http://jparksimulator.com/biodieselplant", 1);
+
+        yield return www;
+        AssetBundle bundle = www.assetBundle;
+        AssetBundleRequest request = bundle.LoadAssetAsync("Structure.fbx", typeof(GameObject));
+        yield return request;
+        GameObject obj_1 = Instantiate(request.asset) as GameObject;
+        AssetBundleRequest request_2 = bundle.LoadAssetAsync("Plant.FBX", typeof(GameObject));
+        yield return request_2;
+        GameObject obj_2 = Instantiate(request_2.asset) as GameObject;
+
+        bundle.Unload(false);
+        www.Dispose();
+
+        GameObject[] obj = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+
+        foreach (GameObject gameobject in obj)
+        {
+            if (gameobject.name.Contains("E-")|| gameobject.name.Contains("P-")||gameobject.name.Contains("R-")||gameobject.name.Contains("T-")|| gameobject.name.Contains("V-"))
+            {
+                gameobject.AddComponent<BoxCollider>();
+                BoxCollider boxcollider = gameobject.GetComponent<BoxCollider>();
+                boxcollider.isTrigger = true;
+                gameobject.AddComponent<onClick>();
+            }
+
+
+
+        }
+
+        
+
+        centerLocation.latitude = 1.2581851f;
+        centerLocation.longitude = 103.677982f;
+        markerLocation1.latitude = 1.2392201f;
+        markerLocation1.longitude = 103.6874937f;
+        markerLocation2.latitude = 1.2383376f;
+        markerLocation2.longitude = 103.6885246f;
+        markerLocation3.latitude = 1.3040654f;
+        markerLocation3.longitude = 103.773342f;
+        StartCoroutine(LoadMap());
+        Text text = GameObject.Find("Text").GetComponent<Text>();
+        marker1 = GameObject.Find("Marker1");
+        marker2 = GameObject.Find("Marker2");
+        marker3 = GameObject.Find("Marker3");
+
+
+        float x1, y1;
+        float x2, y2;
+        float x3, y3;
+
+        x1 = -(markerLocation1.longitude - centerLocation.longitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+        y1 = -(markerLocation1.latitude - centerLocation.latitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+
+        x2 = -(markerLocation2.longitude - centerLocation.longitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+        y2 = -(markerLocation2.latitude - centerLocation.latitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+
+        x3 = -(markerLocation3.longitude - centerLocation.longitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+        y3 = -(markerLocation3.latitude - centerLocation.latitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+
+     //   marker1.transform.position = new Vector3(x1, 0, y1);
+      //  marker2.transform.position = new Vector3(x2, 0, y2);
+      //  marker3.transform.position = new Vector3(x3, 0, y3);
+
+
+
+
+        plant = GameObject.Find("Plant(Clone)");
+        structure = GameObject.Find("Structure(Clone)");
+        plant.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+        plant.transform.position = plant.transform.position * 0.25f;
+        plant.transform.Rotate(new Vector3(0, 43.02f, 0));
+        structure.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+        structure.transform.position = structure.transform.position * 0.25f;
+        structure.transform.rotation = new Quaternion(0, 0, 0,0);
+        structure.transform.Rotate(new Vector3(270.019f, 43.02f, 0));
+        structure.transform.localPosition = structure.transform.localPosition + new Vector3(-46.173f, 0, 43.104f);
+        structure.transform.localPosition = structure.transform.localPosition + new Vector3(-376f, 0, -523f);
+        plant.transform.localPosition = plant.transform.localPosition + new Vector3(-376f, 0, -523f);
+
+
+
+         GameObject plant2 =     (GameObject)Instantiate(plant,      new Vector3(-571, 0, -364),plant.transform.rotation);
+         GameObject structure2 = (GameObject)Instantiate(structure,  new Vector3(-645.7f, 67.6f, -385.7f), structure.transform.rotation);
+       
+         GameObject plant3 = (GameObject)Instantiate(plant2,         new Vector3(-731.2f, 0, -197.9f), plant2.transform.rotation);
+         GameObject structure3= (GameObject)Instantiate(structure2,  new Vector3(-811, 66.3f, -224.2f), structure2.transform.rotation);
+
+
+    }
+
+
+    //  void Start()
+    //   {
+
+
+    //  }
+     // Update is called once per frame
+    void Update()
+    {
+
+
+          
+
+
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                float x, y; float abs_x, abs_y;
+                x = hit.point.x; abs_x = Mathf.Abs(x);
+                y = hit.point.z; abs_y = Mathf.Abs(y);
+                float direction_x = x / abs_x;
+                float direction_y = y / abs_y;
+
+
+                GameObject[] obj_2 = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+                ArrayList models_2 = new ArrayList();
+
+                foreach (GameObject gameobject in obj_2)
+                {
+                    if (gameobject.name.Contains("Clone")|| gameobject.name.Contains("Mark"))
+                    {
+                        models_2.Add(gameobject);
+                    }
+                }
+
+                if (abs_x >= 40000 || abs_y >= 40000)
+                {
+
+                    if (abs_x >= 40000)
+                    {
+                    centerLocation.longitude += (direction_x * - 0.0001f * multipler);
+                        foreach (GameObject model in models_2)
+                        {
+                            model.transform.position = model.transform.position + new Vector3(-direction_x * multipler * 725 * (Mathf.Pow(2,zoom - 16)) ,0, 0);
+                        }
+                    }
+                     
+                    if (abs_y >= 40000)
+                    { 
+                    centerLocation.latitude += (direction_y *  - 0.0001f * multipler);
+                        foreach (GameObject model in models_2)
+                        {
+                            model.transform.position = model.transform.position + new Vector3(0, 0, - direction_y * multipler * 725 * (Mathf.Pow(2, (zoom - 16))));
+                        }
+                    }
+
+
+                    StartCoroutine(LoadMap());
+                }
+
+
+
+
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+
+         GameObject[] obj = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+         ArrayList models = new ArrayList();
+         ArrayList scales = new ArrayList();
+
+            foreach (GameObject gameobject in obj)
+        {
+            if (gameobject.name.Contains("Clone")|| gameobject.name.Contains("Mark"))
+            {
+                models.Add(gameobject);
+                scales.Add(gameobject.transform.localScale);
+            }
+        }
+
+            GameObject reference_obj  ;
+            reference_obj = models[1] as GameObject;
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                zoom = zoom - 1;
+                StartCoroutine(LoadMap());
+                foreach (GameObject model in models)
+                {
+                    model.transform.localScale = model.transform.localScale * 0.5f ;
+                    model.transform.position = model.transform.position * 0.5f;
+                }
+             
+                Debug.Log("Scrolled backward and zoom is " + zoom);
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") > 0 )
+            {
+                zoom = zoom + 1;
+                StartCoroutine(LoadMap());
+                foreach (GameObject model in models)
+                {
+                    model.transform.localScale = model.transform.localScale * 2;
+                    model.transform.position = model.transform.position * 2;
+                }
+                Debug.Log("Scrolled forward and zoom is " + zoom);
+            }
+
+            float x1, y1;
+            float x2, y2;
+            float x3, y3;
+
+            x1 = -(markerLocation1.longitude - centerLocation.longitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+            y1 = -(markerLocation1.latitude - centerLocation.latitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+
+            x2 = -(markerLocation2.longitude - centerLocation.longitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+            y2 = -(markerLocation2.latitude - centerLocation.latitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+
+            x3 = -(markerLocation3.longitude - centerLocation.longitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+            y3 = -(markerLocation3.latitude - centerLocation.latitude) * 725 * (Mathf.Pow(2, (zoom - 16))) * 10000;
+
+        //    marker1.transform.position = new Vector3(x1, 0, y1);
+         //   marker2.transform.position = new Vector3(x2, 0, y2);
+          //  marker3.transform.position = new Vector3(x3, 0, y3);
+
+
+        }
+        // duplicate the biodisel plants and move them to the corresponding location
+
+
+    }
+
+    IEnumerator LoadMap()
+    {
+        var url = "http://maps.googleapis.com/maps/api/staticmap";
+        var qs = "";
+
+        if (centerLocation.address != "")
+            qs += "center=" + WWW.UnEscapeURL(centerLocation.address);
+        else
+        {
+            qs += "center=" + WWW.UnEscapeURL(string.Format("{0},{1}", centerLocation.latitude, centerLocation.longitude));
+        }
+
+        qs += "&zoom=" + zoom.ToString();
+
+        qs += "&size=" + WWW.UnEscapeURL(string.Format("{0}x{0}", size));
+        qs += "&scale=" + (doubleResolution ? "2" : "1");
+        qs += "&maptype=" + mapType.ToString().ToLower();
+        var usingSensor = false;
+#if UNITY_IPHONE
+		usingSensor = Input.location.isEnabledByUser && Input.location.status == LocationServiceStatus.Running;
+#endif
+        qs += "&sensor=" + (usingSensor ? "true" : "false");
+
+
+        var req = new WWW(url + "?" + qs + "&key=AIzaSyCUhcopsyWu4QpNhrS85ETo0XtLaSLUrbk");
+        yield return req;
+        GetComponent<Renderer>().material.mainTexture = req.texture;
+    }
+
+ 
+}
---
+++ UnityScripts/RTS_Camera.cs
@@ -0,0 +1,426 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RTS_Cam
+{
+    [RequireComponent(typeof(Camera))]
+    [AddComponentMenu("RTS Camera")]
+    public class RTS_Camera : MonoBehaviour
+    {
+
+        #region Foldouts
+
+#if UNITY_EDITOR
+
+        public int lastTab = 0;
+
+        public bool movementSettingsFoldout;
+        public bool zoomingSettingsFoldout;
+        public bool rotationSettingsFoldout;
+        public bool heightSettingsFoldout;
+        public bool mapLimitSettingsFoldout;
+        public bool targetingSettingsFoldout;
+        public bool inputSettingsFoldout;
+
+#endif
+
+        #endregion
+
+        private Transform m_Transform; //camera tranform
+        public bool useFixedUpdate = false; //use FixedUpdate() or Update()
+
+        #region Movement
+
+        public float keyboardMovementSpeed = 5f; //speed with keyboard movement
+        public float screenEdgeMovementSpeed = 3f; //spee with screen edge movement
+        public float followingSpeed = 5f; //speed when following a target
+        public float rotationSped = 3f;
+        public float panningSpeed = 10f;
+        public float mouseRotationSpeed = 10f;
+
+        #endregion
+
+        #region Height
+
+        public bool autoHeight = true;
+        public LayerMask groundMask = -1; //layermask of ground or other objects that affect height
+
+        public float maxHeight = 10f; //maximal height
+        public float minHeight = 15f; //minimnal height
+        public float heightDampening = 5f; 
+        public float keyboardZoomingSensitivity = 2f;
+        public float scrollWheelZoomingSensitivity = 25f;
+
+        private float zoomPos = 0; //value in range (0, 1) used as t in Matf.Lerp
+
+        #endregion
+
+        #region MapLimits
+
+        public bool limitMap = true;
+        public float limitX = 50f; //x limit of map
+        public float limitY = 50f; //z limit of map
+
+        #endregion
+
+        #region Targeting
+
+        public Transform targetFollow; //target to follow
+        public Vector3 targetOffset;
+
+        /// <summary>
+        /// are we following target
+        /// </summary>
+        public bool FollowingTarget
+        {
+            get
+            {
+                return targetFollow != null;
+            }
+        }
+
+        #endregion
+
+        #region Input
+
+        public bool useScreenEdgeInput = true;
+        public float screenEdgeBorder = 25f;
+
+        public bool useKeyboardInput = true;
+        public string horizontalAxis = "Horizontal";
+        public string verticalAxis = "Vertical";
+
+        public bool usePanning = true;
+        public KeyCode panningKey = KeyCode.Mouse2;
+
+        public bool useKeyboardZooming = true;
+        public KeyCode zoomInKey = KeyCode.E;
+        public KeyCode zoomOutKey = KeyCode.Q;
+
+        public bool useScrollwheelZooming = true;
+        public string zoomingAxis = "Mouse ScrollWheel";
+
+        public bool useKeyboardRotation = true;
+        public KeyCode rotateRightKey = KeyCode.X;
+        public KeyCode rotateLeftKey = KeyCode.Z;
+
+        public bool useMouseRotation = true;
+        public KeyCode mouseRotationKey = KeyCode.Mouse1;
+
+        private Vector2 KeyboardInput
+        {
+            get { return useKeyboardInput ? new Vector2(Input.GetAxis(horizontalAxis), Input.GetAxis(verticalAxis)) : Vector2.zero; }
+        }
+
+        private Vector2 MouseInput
+        {
+            get { return Input.mousePosition; }
+        }
+
+        private float ScrollWheel
+        {
+            get { return Input.GetAxis(zoomingAxis); }
+        }
+
+        private Vector2 MouseAxis
+        {
+            get { return new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")); }
+        }
+
+        private int ZoomDirection
+        {
+            get
+            {
+                bool zoomIn = Input.GetKey(zoomInKey);
+                bool zoomOut = Input.GetKey(zoomOutKey);
+                if (zoomIn && zoomOut)
+                    return 0;
+                else if (!zoomIn && zoomOut)
+                    return 1;
+                else if (zoomIn && !zoomOut)
+                    return -1;
+                else 
+                    return 0;
+            }
+        }
+
+        private int RotationDirection
+        {
+            get
+            {
+                bool rotateRight = Input.GetKey(rotateRightKey);
+                bool rotateLeft = Input.GetKey(rotateLeftKey);
+                if(rotateLeft && rotateRight)
+                    return 0;
+                else if(rotateLeft && !rotateRight)
+                    return -1;
+                else if(!rotateLeft && rotateRight)
+                    return 1;
+                else 
+                    return 0;
+            }
+        }
+
+        #endregion
+
+        #region Unity_Methods
+
+        private void Start()
+        {
+            Camera camera = new Camera();
+            m_Transform = transform;
+        }
+
+        private void Update()
+        {
+            if (!useFixedUpdate)
+                CameraUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            if (useFixedUpdate)
+                CameraUpdate();
+        }
+
+        #endregion
+
+        #region RTSCamera_Methods
+
+        /// <summary>
+        /// update camera movement and rotation
+        /// </summary>
+        private void CameraUpdate()
+        {
+            if (FollowingTarget)
+                FollowTarget();
+            else
+                Move();
+
+            HeightCalculation();
+            Rotation();
+            LimitPosition();
+        }
+
+        /// <summary>
+        /// move camera with keyboard or with screen edge
+        /// </summary>
+        private void Move()
+        {
+
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                // Get movement of the finger since last frame
+                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+                // Move object across XY plane
+                m_Transform.Translate(-touchDeltaPosition.x * 10, -touchDeltaPosition.y * 10, 0);
+            }
+
+            if (Input.touchCount == 2)
+            {
+                Touch touchZero = Input.GetTouch(0);
+                Touch touchOne = Input.GetTouch(1);
+                Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+                float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+                float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+              
+
+                if (GetComponent<Camera>().orthographic)
+                {
+                    GetComponent<Camera>().orthographicSize += deltaMagnitudeDiff * 10;
+                    GetComponent<Camera>().orthographicSize = Mathf.Max(GetComponent<Camera>().orthographicSize, 0.1f);
+                }
+                else
+                {
+                    GetComponent<Camera>().fieldOfView += deltaMagnitudeDiff * 10;
+                    GetComponent<Camera>().fieldOfView = Mathf.Clamp(GetComponent<Camera>().fieldOfView, 0.1f, 179.9f);
+                }
+            }
+
+            if (useKeyboardInput)
+            {
+                Vector3 desiredMove = new Vector3(KeyboardInput.x, 0, KeyboardInput.y);
+
+                desiredMove *= keyboardMovementSpeed;
+                desiredMove *= Time.deltaTime;
+                desiredMove = Quaternion.Euler(new Vector3(0f, transform.eulerAngles.y, 0f)) * desiredMove;
+                desiredMove = m_Transform.InverseTransformDirection(desiredMove);
+
+                m_Transform.Translate(desiredMove, Space.Self);
+            }
+
+            if (useScreenEdgeInput)
+            {
+                Vector3 desiredMove = new Vector3();
+
+                Rect leftRect = new Rect(0, 0, screenEdgeBorder, Screen.height);
+                Rect rightRect = new Rect(Screen.width - screenEdgeBorder, 0, screenEdgeBorder, Screen.height);
+                Rect upRect = new Rect(0, Screen.height - screenEdgeBorder, Screen.width, screenEdgeBorder);
+                Rect downRect = new Rect(0, 0, Screen.width, screenEdgeBorder);
+
+                desiredMove.x = leftRect.Contains(MouseInput) ? -1 : rightRect.Contains(MouseInput) ? 1 : 0;
+                desiredMove.z = upRect.Contains(MouseInput) ? 1 : downRect.Contains(MouseInput) ? -1 : 0;
+
+                desiredMove *= screenEdgeMovementSpeed;
+                desiredMove *= Time.deltaTime;
+                desiredMove = Quaternion.Euler(new Vector3(0f, transform.eulerAngles.y, 0f)) * desiredMove;
+                desiredMove = m_Transform.InverseTransformDirection(desiredMove);
+
+                m_Transform.Translate(desiredMove, Space.Self);
+            }       
+        
+            if(usePanning && (Input.GetKey(panningKey)) && MouseAxis != Vector2.zero)
+            {
+                Vector3 desiredMove = new Vector3(-MouseAxis.x, 0, -MouseAxis.y);
+
+                desiredMove *= panningSpeed;
+                desiredMove *= Time.deltaTime;
+                desiredMove = Quaternion.Euler(new Vector3(0f, transform.eulerAngles.y, 0f)) * desiredMove;
+                desiredMove = m_Transform.InverseTransformDirection(desiredMove);
+
+                m_Transform.Translate(desiredMove, Space.Self);
+            }
+        }
+
+        /// <summary>
+        /// calcualte height
+        /// </summary>
+        private void HeightCalculation()
+        {
+            float distanceToGround = DistanceToGround();
+            if(useScrollwheelZooming && !Input.GetKey(KeyCode.LeftAlt))
+                zoomPos += ScrollWheel * Time.deltaTime * scrollWheelZoomingSensitivity;
+            if (useKeyboardZooming)
+                zoomPos += ZoomDirection * Time.deltaTime * keyboardZoomingSensitivity;
+
+            zoomPos = Mathf.Clamp01(zoomPos);
+
+            float targetHeight = Mathf.Lerp(minHeight, maxHeight, zoomPos);
+            float difference = 0; 
+
+            if(distanceToGround != targetHeight)
+                difference = targetHeight - distanceToGround;
+
+                m_Transform.position = Vector3.Lerp(m_Transform.position, 
+                new Vector3(m_Transform.position.x, targetHeight + difference, m_Transform.position.z), Time.deltaTime * heightDampening);
+        }
+
+        /// <summary>
+        /// rotate camera
+        /// </summary>
+        private void Rotation()
+        {
+
+        
+            Vector2 touchDeltaPosition = new Vector2(0,0);
+
+            if (Input.touchCount > 0)
+            {
+                touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            }
+
+ 
+
+            if (useKeyboardRotation)
+                transform.Rotate(Vector3.up, RotationDirection * Time.deltaTime * rotationSped, Space.World);
+
+
+
+            if (Input.GetMouseButton(1))
+            {
+                if (Mathf.Abs(MouseAxis.x) >= Mathf.Abs(MouseAxis.y))
+                {
+                    m_Transform.Rotate(Vector3.up, -MouseAxis.x * Time.deltaTime * mouseRotationSpeed, Space.World);
+                 }
+                else
+                {
+                    m_Transform.Rotate(Vector3.left, -MouseAxis.y * Time.deltaTime * mouseRotationSpeed, Space.Self);
+ 
+                }
+            }
+
+
+            if (Input.touchCount > 2)
+            {
+                if (Mathf.Abs(touchDeltaPosition.x) >= Mathf.Abs(touchDeltaPosition.y))
+                {
+                    m_Transform.Rotate(Vector3.up, -touchDeltaPosition.x * Time.deltaTime * 5, Space.World);
+                }
+                else
+                {
+                    m_Transform.Rotate(Vector3.left, -touchDeltaPosition.y * Time.deltaTime * mouseRotationSpeed, Space.Self);
+                }
+
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+
+        /// <summary>
+        /// follow targetif target != null
+        /// </summary>
+        private void FollowTarget()
+        {
+            Vector3 targetPos = new Vector3(targetFollow.position.x, m_Transform.position.y, targetFollow.position.z) + targetOffset;
+            m_Transform.position = Vector3.MoveTowards(m_Transform.position, targetPos, Time.deltaTime * followingSpeed);
+        }
+
+        /// <summary>
+        /// limit camera position
+        /// </summary>
+        private void LimitPosition()
+        {
+            if (!limitMap)
+                return;
+                
+            m_Transform.position = new Vector3(Mathf.Clamp(m_Transform.position.x, -limitX, limitX),
+                m_Transform.position.y,
+                Mathf.Clamp(m_Transform.position.z, -limitY, limitY));
+        }
+
+        /// <summary>
+        /// set the target
+        /// </summary>
+        /// <param name="target"></param>
+        public void SetTarget(Transform target)
+        {
+            targetFollow = target;
+        }
+
+        /// <summary>
+        /// reset the target (target is set to null)
+        /// </summary>
+        public void ResetTarget()
+        {
+            targetFollow = null;
+        }
+
+        /// <summary>
+        /// calculate distance to ground
+        /// </summary>
+        /// <returns></returns>
+        private float DistanceToGround()
+        {
+            Ray ray = new Ray(m_Transform.position, Vector3.down);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, groundMask.value))
+                return (hit.point - m_Transform.position).magnitude;
+
+            return 0f;
+        }
+
+        #endregion
+    }
+}
