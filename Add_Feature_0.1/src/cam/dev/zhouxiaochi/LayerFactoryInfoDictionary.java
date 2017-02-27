ae94426d08d0e61c4ea38a6a16c3808e63a9d7ef
masterzhouxiaochi <zhou0201@e.ntu.edu.sg>
2017-02-27 17:46:31 +0800

    Modified App, added writeToLayer function

--- Add_Feature_0.1/src/cam/dev/zhouxiaochi/LayerFactoryInfoDictionary.java
+++ Add_Feature_0.1/src/cam/dev/zhouxiaochi/LayerFactoryInfoDictionary.java
@@ -19,12 +19,12 @@ public class LayerFactoryInfoDictionary {
 		dictionary.put("TransmissionLines", new LayerFactoryInfo("TransmissionLines",  "kml/UHT Lines (230kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^UHT-\\d+$"));
 		dictionary.put("EHTLines", new LayerFactoryInfo("EHTLines",  "kml/EHT Lines.kml", FeatureServiceUpdater.LayerType.POLYLINE, "^EHT-\\d+$"));
 		dictionary.put("HTLines", new LayerFactoryInfo("HTLines",  "kml/HT Lines.kml", FeatureServiceUpdater.LayerType.POLYLINE, "^HT-\\d+$"));
-		dictionary.put("TLPlant(22kV-11kV)", new LayerFactoryInfo("TLPlant(22kV-11kV)",  "kml/TLPlant(22kV-11kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^PHT-[5|6|7|8]$"));
-		dictionary.put("TLPlant(22kV-3.4kV)", new LayerFactoryInfo("TLPlant(22kV-3.4kV)", "kml/TLPlant(22kV-3.4kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^PHT-[9|10|11|12|13|14|15|16]$"));
-		dictionary.put("TLPlant(3.4kV-3kV)", new LayerFactoryInfo("TLPlant(3.4kV-3kV)",  "kml/TLPlant(3.4kV-3kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^PLT-[1|2|3|4|5|6|7|8]$"));
-		dictionary.put("TLPlant(3kV-0.4kV)", new LayerFactoryInfo("TLPlant(3kV-0.4kV)",  "kml/TLPlant(3kV-0.4kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^PLT-[9|10|11|12|13|14|15|16|17|18|19]"));
-		dictionary.put("TLPlant(main-22kV)", new LayerFactoryInfo("TLPlant(main-22kV)",  "kml/TLPlant(main-22kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^PHT-[1|2|3|4]$"));
-		dictionary.put("JurongTransportationSystem", new LayerFactoryInfo("JurongTransportationSystem",  "kml/Public Roads.kml", FeatureServiceUpdater.LayerType.POLYGON, "^\\w+[Road|Avenue|Drive|Highway|Place|view]\\w*$"));
+		dictionary.put("TLPlant(22kV-11kV)", new LayerFactoryInfo("TLPlant(22kV-11kV)",  "kml/TLPlant(22kV-11kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^PHT-[5678]$"));
+		dictionary.put("TLPlant(22kV-3.4kV)", new LayerFactoryInfo("TLPlant(22kV-3.4kV)", "kml/TLPlant(22kV-3.4kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^PHT-(9|10|11|12|13|14|15|16)$"));
+		dictionary.put("TLPlant(3.4kV-3kV)", new LayerFactoryInfo("TLPlant(3.4kV-3kV)",  "kml/TLPlant(3.4kV-3kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^PLT-[12345678]$"));
+		dictionary.put("TLPlant(3kV-0.4kV)", new LayerFactoryInfo("TLPlant(3kV-0.4kV)",  "kml/TLPlant(3kV-0.4kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^PLT-(9|10|11|12|13|14|15|16|17|18|19)"));
+		dictionary.put("TLPlant(main-22kV)", new LayerFactoryInfo("TLPlant(main-22kV)",  "kml/TLPlant(main-22kV).kml", FeatureServiceUpdater.LayerType.POLYLINE, "^PHT-[1234]$"));
+		dictionary.put("JurongTransportationSystem", new LayerFactoryInfo("JurongTransportationSystem",  "kml/Public Roads.kml", FeatureServiceUpdater.LayerType.POLYGON, "^\\w+(Crescent|Road|Avenue|Drive|Highway|Place|View|Rise|Lane)\\d*$"));
 
 		
 		
