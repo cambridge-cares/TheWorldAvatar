1480443ffa20d72e24d72344cfd02fc448e4bb94
Claudius <mortalcay@gmail.com>
2017-06-05 16:47:17 +0800

    Modified bms node url

--- web/CO2WEB/routes/bmsNodeTemp.js
+++ web/CO2WEB/routes/bmsNodeTemp.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 var  request = require("request");
 
-let url = "http://www.bms.com/JPS_KB_CARES_Lab_Node/FH-01.owl";
+let url = "http://www.careslab.com/JPS_KB_CARES_Lab_Node/FH-01.owl";
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
