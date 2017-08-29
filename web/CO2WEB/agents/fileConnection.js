3d9a2543b571a54b49957dbcf19ce8d4bbd7fce6
shaocong <szhang012@e.ntu.edu.sg>
2017-08-29 17:45:15 +0800

    Modified growl visual behavior: single click go the service url, dblclick to owl file

--- web/CO2WEB/agents/fileConnection.js
+++ web/CO2WEB/agents/fileConnection.js
@@ -38,31 +38,33 @@ let request = require('request');
 
 
  ****/
-
+//commented out lazy innitiation
 //var connections = [];
 
 /**
- *
+ * main function, async
  * @param options  topnode(topnode address on disk), [showServiceOnly(bool) } showImport(bool)]
  * @param callback  fn(err, results)
  */
 function readConnections(options, callback) {
 
+    /*settings***********************/
     let showServiceOnly = options.showServiceOnly || false;
     let showImport = options.showImport&&!showServiceOnly || false; // if showServiceOnly is chosen, will not show Import
-    let fileLocation = options.topnode;
-    if(!fileLocation){
+    let showServiceUrl = options.showServiceUrl  || false;
+    let topnodeLocation = options.topnode;
+    if(!topnodeLocation){
         callback(new Error("top node not specified"));
         return;
     }
 
 
+    //commented out lazy initiation
     // if (connections && connections.length > 0) {
     //      callback(null, connections);
     // } else {
 
-
-
+    /*start from a root node recursively read connections from child files*******/
     startFromRoot2GetConns(function (err, connections) {
 
         if (err) {
@@ -73,20 +75,24 @@ function readConnections(options, callback) {
 
     });
 
-
     //  }
 
 
+    /***
+     *start from a root node recursively read connections from child files
+     * @param callback(err result)
+     */
     function startFromRoot2GetConns(callback) {
 
-        //read root file on disk
-        fs.readFile(fileLocation, function (err, file) {
+        /*read top node**/
+        fs.readFile(topnodeLocation, function (err, file) {
             if (err) {
                 console.log("errReadingFile");
                 callback(err);
                 return;
             }
 
+            /*retreive child info from file and recursively read through children**/
             loopChildrenRecur(file, 0, callback);
 
         })
@@ -98,17 +104,10 @@ function readConnections(options, callback) {
 
          //Get every tab defined in ontology tab
               let imports =  root.find('//owl:Ontology//owl:imports', {owl:'http://www.w3.org/2002/07/owl#', rdf:"http://www.w3.org/1999/02/22-rdf-syntax-ns#resource"});
-              console.log(imports);
-
-
+           //   console.log(imports);
                 let results = [];
               if(imports ) {
                   results = imports.map(function (item) {
-
-                      console.log(item.toString())
-
-                      console.log("resource:!!!!!!" + item.attr("resource").value());
-
                       return item.attr("resource").value();
                   });
 
@@ -120,45 +119,68 @@ function readConnections(options, callback) {
         }
     }
 	
-	function getUrl(root){
+	function getUri(root){
+        /**
 		            for (var ns of root.namespaces()) {
-                //console.log(ns.prefix()+ ":"+ns.href());
-                //get base from xml
                 if (ns.prefix() === null) {//if prefix not null [ self namespace is defined with null prefix]
-                    console.log("prefix:" + ns.prefix());
-
                   return ns.href();
                 }
             }
-			
-			return null;
+			***/
+        let uri = root.find('//owl:Ontology', {owl:'http://www.w3.org/2002/07/owl#'});
+        if(!uri || uri.length < 1){
+            return null;
+        }
+
+        return uri[0].attr("about").value();
+
 	}
 
 
+
+    /**
+     * A link to the service page is defined in the service owl. this function retrieves it
+     * @root  xml root of the file
+     */
+	function getServiceUrl(root) {
+
+	    let urls  = root.find("//Service:hasUrl", {"Service" :"http://www.theworldavatar.com/Service.owl#"});
+
+         if(!urls || urls.length < 1){
+
+             return null;
+         }
+        console.log("found service url in func: " +urls[0].text().trim())
+
+	    return  urls[0].text().trim();
+    }
+
+
+    /**
+     * Find all children defined in an owl file
+     * @param root
+     * @returns {Array}
+     */
     function getChildren(root) {
 		if(!root){
-			
 			return [];
 		}
         var children = [];
         var namespaceOb = {};//construct namespaceOb for find in root with nested namespace
-
         namespaceOb['owl'] = "http://www.w3.org/2002/07/owl#";
         namespaceOb['Eco-industrialPark'] = "http://www.theworldavatar.com/OntoEIP/Eco-industrialPark.owl#";
         namespaceOb['system'] = "http://www.theworldavatar.com/OntoCAPE/OntoCAPE/upper_level/system.owl#";
 
-        console.log(util.inspect(namespaceOb));
-
         //find all node with hasIRI property
         var uris = root.find("//Eco-industrialPark:hasIRI", namespaceOb);
-        console.log("found node Eco-industrialPark:hasIRI:" + uris.length);
+      //  console.log("found node Eco-industrialPark:hasIRI:" + uris.length);
         for (let curi of uris) {
            // console.log(curi.name());
             children.push(curi.text().trim());//push to targets list
         }
         //find all node with SYSTEM:hasIRI property
         let urisS = root.find("//system:hasIRI", namespaceOb);
-        console.log("found node system:hasIRI:"+urisS.length);
+        //console.log("found node system:hasIRI:"+urisS.length);
         for(let curi of urisS){
             //    console.log(curi.name());
             children.push(curi.text().trim());//push to targets list
@@ -175,8 +197,8 @@ function readConnections(options, callback) {
         let y =  root.find("//owl:NamedIndividual[contains(@rdf:about, 'ValueOf_y_')]", {owl:'http://www.w3.org/2002/07/owl#', rdf:"http://www.w3.org/1999/02/22-rdf-syntax-ns#"});
 
         if(x.length > 0 && y.length > 0) {
-            console.log("#########################findcoordis:" + x[0].text().trim());
-            console.log("converted coordi: " +  util.inspect(convertCoordinate(x[0].text().trim(), y[0].text().trim(), false)));
+           // console.log("#########################findcoordis:" + x[0].text().trim());
+           // console.log("converted coordi: " +  util.inspect(convertCoordinate(x[0].text().trim(), y[0].text().trim(), false)));
             return convertCoordinate(x[0].text().trim(), y[0].text().trim(), false);
         } else {
             return null;
@@ -219,7 +241,7 @@ function readConnections(options, callback) {
         namespaceOb['Eco-industrialPark'] = "http://www.theworldavatar.com/OntoEIP/Eco-industrialPark.owl#";
 
         var uris = root.find("//owl:NamedIndividual[rdf:type[contains(@rdf:resource,'http://www.theworldavatar.com/Service.owl')]]", namespaceOb);
-        console.log("found Service node :"+uris.length);
+      //  console.log("found Service node :"+uris.length);
 
         for(let curi of uris){
             //    console.log(curi.name());
@@ -239,12 +261,11 @@ function readConnections(options, callback) {
 			      try{
                let xmlDoc = libxmljs.parseXml(file);
                 root = xmlDoc.root();
-               var myUri = (root.attrs() && root.attrs().length > 0) ? root.attrs()[0].value() : null;
-               
-			   if(myUri === null){
-			    myUri = getUrl(root);
-			   }
-               console.log("myURI" + myUri);
+              // var myUri = (root.attrs() && root.attrs().length > 0) ? root.attrs()[0].value() : null;
+
+			    var myUri = getUri(root);
+
+          //     console.log("myURI" + myUri);
                //TODO: request for remote  VS search with name on current?
                //TODO: xml parse the file => get targets list == childList => request on each child file
 
@@ -253,23 +274,29 @@ function readConnections(options, callback) {
 				  }
                let children = getChildren(root);
            //get my links
-           let myLinks = [];
-           let geoCoords = [];
+           let connectionParent = [],geoCoordsParent = [],serviceUrlsParent = [];
            var imports;
 
            //get this geoCoord, push it on result links
            var coord = getGeoCoord(root);
            if(coord) {
-               geoCoords.push({url : myUri, coord: coord});
+               geoCoordsParent.push({url : myUri, coord: coord});
            }
 
-           console.log(util.inspect(geoCoords));
+           if(showServiceUrl){
+               let serviceUrltmp = getServiceUrl(root);
+               if(serviceUrltmp){
+                   console.log("found service url: " +serviceUrltmp)
+                   serviceUrlsParent.push({url : myUri, serviceUrl: serviceUrltmp})
+               }
+
+           }
            if (showImport) {
                try {
                     imports = getXMLImports(root);
                    for (let imported of imports) {
-                       console.log("import iri:" + imported);
-                       myLinks.push({source: myUri, target: imported,level:null})
+                    //   console.log("import iri:" + imported);
+                       connectionParent.push({source: myUri, target: imported,level:null})
                       // children.push(imported);
                    }
                    ;
@@ -281,28 +308,24 @@ function readConnections(options, callback) {
            }
 
            if (children.length < 1) { // no children is found, including devices and services and call callback
-               console.log(myUri + " is a leaf node return");
-
-               callback(null, {connections: myLinks, geoCoords: geoCoords});
+             //  console.log(myUri + " is a leaf node return");
+               callback(null, {connections: connectionParent, geoCoords: geoCoordsParent, serviceUrls: serviceUrlsParent});
                return;
            }//else
-
-
-
 
                //push all children into link array
 
                if(showServiceOnly) { //pack only services into results
                    var services = getServices(root);
                    for (let service of services) {
-                       console.log("service iri: " + service);
-                       myLinks.push({source: myUri, target: service});
+                     //  console.log("service iri: " + service);
+                       connectionParent.push({source: myUri, target: service});
                    }
 
                }  else {
                    for (let target of children) {
-                       console.log("child iri: " + target);
-                       myLinks.push({source: myUri, target: target, level: level});
+                     //  console.log("child iri: " + target);
+                       connectionParent.push({source: myUri, target: target, level: level});
                    }
                }
            if (showImport) {
@@ -311,39 +334,34 @@ function readConnections(options, callback) {
                //request on each child, execute loopChild on it, then concate the result
                async.concat(children, requestChild, function (err, results) {
                    if (err) {
-                       console.log("concat err");
+                      // console.log("concat err");
                        console.log(err.message);
                        callback(err);
                        return;
                    }
                    console.log("concat results");
 
+                   let connectionsAll = connectionParent;
+                   let geoCoordsAll = geoCoordsParent;
+                   let serviceUrlAll = serviceUrlsParent;
 
-                   let connectionsAll = myLinks;
-                   let geoCoordsAll = geoCoords;
-
-                   console.log("!!!!!!!!!!!!!!!!!!!!!" + results.length);
-                   //console.log(geoCoords)
+                   //console.log(geoCoordsParent)
                    for (let result of results){
-                       console.log(util.inspect(result));
-
-                       let connectionThis = result.connections;
-                       //console.log(connectionThis);
-                     //  connectionThis = connectionThis.clean(null);
-                       let geoCoordsThis = result.geoCoords;
-                       console.log(util.inspect(geoCoordsThis));
-
+                       let connectionChild = result.connections;
+                       let geoCoordsChild = result.geoCoords;
+                       let serviceUrlChild  =result.serviceUrls;
                        //  geoCoordsThis = geoCoordsThis.clean(null);
-                       connectionsAll = connectionsAll.concat(connectionThis);
-                       geoCoordsAll=   geoCoordsAll.concat(geoCoordsThis);
+                       connectionsAll = connectionsAll.concat(connectionChild);
+                       geoCoordsAll=   geoCoordsAll.concat(geoCoordsChild);
+                       serviceUrlAll = serviceUrlAll.concat(serviceUrlChild);
                    }
+                   //console.log("********************concated");
+                  // console.log(util.inspect(connectionsAll));
+                  // console.log(util.inspect(geoCoordsAll));
+                   console.log("All service urls data:")
+                   console.log(JSON.stringify(serviceUrlAll))
 
-
-                   console.log("********************concated");
-                   console.log(util.inspect(connectionsAll));
-                   console.log(util.inspect(geoCoordsAll));
-
-                   callback(null, {connections:connectionsAll, geoCoords:geoCoordsAll});
+                   callback(null, {connections:connectionsAll, geoCoords:geoCoordsAll, serviceUrls:serviceUrlAll});
 
                });
 
@@ -360,7 +378,7 @@ function readConnections(options, callback) {
                    }
 
                    if (response.statusCode === 200) {//request success
-                       console.log("req: " + iri);
+                       //console.log("req: " + iri);
 
                        loopChildrenRecur(body, level+1, callback);
                        return;
