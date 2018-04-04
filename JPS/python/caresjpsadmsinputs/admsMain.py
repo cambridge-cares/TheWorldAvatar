d656a3af0b33a48b6eee36a82994c9e479a86c2f
zhou0201 <zhou0201@e.ntu.edu.sg>
2018-04-04 14:57:40 +0800

    Added JPS folder which currently contain AMDS projects

---
+++ JPS/python/caresjpsadmsinputs/admsMain.py
@@ -0,0 +1,42 @@
+from admsInputDataRetriever import admsInputDataRetriever
+from admsAplWriter import admsAplWriter
+#sampleBuilding past:"C://Users/Shaocong/WORK/admsInput/samplebuildingkb.owl"
+#sampleBuilding new:"C://Users/Shaocong/WORK/admsInput/107_buildings.owl"
+
+import json
+import sys
+import config
+
+'''
+test = admsInputDataRetriever("http://www.theworldavatar.com/TankID_1574.owl#TankID_1574", config.bldTopnode, {'xmin':84400, 'xmax':84600, 'ymin':451000, 'ymax':451300}, ["http://www.theworldavatar.com/OntoCAPE/OntoCAPE/material/substance/substance.owl#CarbonDioxide"], config.bdnLimit = 2, filterSrc = False)
+
+
+result = test.get()
+
+
+for key, value in result.items():
+    print(key+":")
+    print (value) 
+
+
+writer = admsAplWriter(result, 'test.apl')
+
+writer.write()
+'''
+
+if __name__ == "__main__":
+    print('top:')
+    print(config.bldTopnode)
+    print(sys.argv[2])
+    test = admsInputDataRetriever(str(sys.argv[1]), bldTopnode = config.bldTopnode, json.loads(sys.argv[2]), ["http://www.theworldavatar.com/OntoCAPE/OntoCAPE/material/substance/substance.owl#CarbonDioxide"], bdnLimit = config.bdnLimit, filterSrc = False  )
+    result = test.get()
+
+
+    for key, value in result.items():
+        print(key+":")
+        print (value) 
+
+
+    writer = admsAplWriter(result, 'test.apl')
+
+    writer.write()
---
+++ JPS/src/uk/ac/cam/cares/jps/adms/ADMSStarter.java
@@ -0,0 +1,86 @@
+package uk.ac.cam.cares.jps.adms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.tribes.util.Arrays;
+
+/**
+ * Servlet implementation class AMDSStarter
+ */
+@WebServlet("/AMDSStarter")
+public class ADMSStarter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ADMSStarter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String res = startADMS();
+		response.getWriter().write(res);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	
+	public String startADMS()
+	{
+		// ServletContext context = getServletContext();
+		String fullPath ="/eclipse-workspace/JPS/python/caresjpsadmsinputs";// Such path is in the folder where your tomcat for this project is installed 
+		System.out.println("full path" + fullPath);
+		String[] cmd = {fullPath + "/startADMS.sh"}; //// Hardcoded 
+
+		Runtime rt = Runtime.getRuntime();
+		Process pr = null;
+		
+		System.out.println("cmd : " + Arrays.toString(cmd));
+		try {
+			pr = rt.exec(cmd);
+			System.out.print("Executing");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// retrieve output from python script
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+		String line = "";
+
+		try {
+			while((line = bfr.readLine()) != null) {
+			// display each output line form python script
+			System.out.println(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return line;
+	}
+	 
+
+}
