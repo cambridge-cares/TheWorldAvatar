6cca77c7d5f5d722b7777dc7d69dc47fa685e515
weiern <weiern.cares.c4t@gmail.com>
2018-04-05 16:29:25 +0800

    committed PythonHelper.java class

---
+++ JPS/src/uk/ac/cam/cares/jps/util/PythonHelper.java
@@ -0,0 +1,28 @@
+package uk.ac.cam.cares.jps.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import uk.ac.cam.cares.jps.config.AgentLocator;
+
+public class PythonHelper {
+
+	/**
+	 * @param pythonScriptName
+	 *            (including package name followed by script name and .py extension,
+	 *            e.g. caresjpsadmsinputs/ADMSGeoJsonGetter.py)
+	 * @return
+	 * @throws IOException
+	 */
+	public static String callPython(String pythonScriptName, String parameter) throws IOException {
+		String path = AgentLocator.getPathToPythonScript(pythonScriptName);
+
+		String[] cmd = { "python", path, parameter };
+
+		Process p = Runtime.getRuntime().exec(cmd);
+
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		return stdInput.readLine();
+	}
+}
