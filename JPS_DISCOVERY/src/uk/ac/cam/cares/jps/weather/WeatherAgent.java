f0f4a33a3e675ddc0ab4d39d851fc287e1d96cb5
Kevin <lkaditya1@e.ntu.edu.sg>
2018-05-08 14:53:29 +0800

    make the function to calculate the angle of wind direction from text value

--- JPS_DISCOVERY/src/uk/ac/cam/cares/jps/weather/WeatherAgent.java
+++ JPS_DISCOVERY/src/uk/ac/cam/cares/jps/weather/WeatherAgent.java
@@ -17,14 +17,12 @@ import org.slf4j.LoggerFactory;
 
 import uk.ac.cam.cares.jps.discovery.api.AbstractAgentServiceDescription;
 import uk.ac.cam.cares.jps.discovery.api.Agent;
-import uk.ac.cam.cares.jps.discovery.api.AgentServiceDescription;
 import uk.ac.cam.cares.jps.discovery.api.AgentRequest;
 import uk.ac.cam.cares.jps.discovery.api.AgentResponse;
 import uk.ac.cam.cares.jps.discovery.api.Parameter;
 import uk.ac.cam.cares.jps.discovery.api.TypeString;
 import uk.ac.cam.cares.jps.discovery.factory.DiscoveryFactory;
 import uk.ac.cam.cares.jps.discovery.test.DescriptionFactory;
-import uk.ac.cam.cares.jps.discovery.test.WeatherAgentTwo;
 import uk.ac.cam.cares.jps.discovery.util.ISerializer;
 import uk.ac.cam.cares.jps.discovery.util.JPSBaseServlet;
 
@@ -33,10 +31,10 @@ public class WeatherAgent extends JPSBaseServlet {
 	
 	private static final long serialVersionUID = -4199209974912271432L;
 	
-	Logger logger = LoggerFactory.getLogger(WeatherAgentTwo.class);
+	Logger logger = LoggerFactory.getLogger(WeatherAgent.class);
 	private ISerializer serializer = DiscoveryFactory.getSerializer();
 	
-	//private OWLSerializer serializer2= new OWLSerializer();
+
 	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
@@ -76,8 +74,9 @@ public class WeatherAgent extends JPSBaseServlet {
         String cloudcovercalculate = String.valueOf(Math.round(Float.valueOf(cloudcover)/100*8));
         String windvalue= selected2.split("<li class=\"wind\"><strong>")[1].split("</strong></li>")[0].split(" ")[0];
         String direction=selected2.split("<li class=\\\"wind\">")[1].split("</li>")[0].split(" ")[3];
-       
-
+    
+        direction = convertWindDirectiontoAngle(direction);
+        
         logger.info("temperature value= "+tempvalue);
         logger.info("wind value= "+windvalue);
         logger.info("wind direction from= "+direction);
@@ -92,9 +91,9 @@ public class WeatherAgent extends JPSBaseServlet {
 		Parameter param2 = agentResponse.getOutputParameters().get(1);
 		param2.setValue(new TypeString("5"));
 		Parameter param3 = agentResponse.getOutputParameters().get(2);
-		param3.setValue(new TypeString("14"));
+		param3.setValue(new TypeString(windvalue));
 		Parameter param4 = agentResponse.getOutputParameters().get(3);
-		param4.setValue(new TypeString("90"));
+		param4.setValue(new TypeString(direction));
 		
 		//convert from the serialized object to string for the response
 		String serializedAgentResponse = serializer.convertToString(agentResponse);
@@ -109,6 +108,17 @@ public class WeatherAgent extends JPSBaseServlet {
 
 		
 		logger.info("WeatherAgent exit");
+	}
+
+	private String convertWindDirectiontoAngle(String direction) {
+		String windirkey[]= {"N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"};
+		for (int x = 0; x < windirkey.length; x++) {
+			if (direction.equals(windirkey[x])) {
+				Double angle = x * 22.5;
+				direction = String.valueOf(angle);
+			}
+		}
+		return direction;
 	}
 	
 	public Agent getAgent() {
