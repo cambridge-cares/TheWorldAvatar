db772a86e75036e9e5a3c355166483c85c0d1724
weiern <weiern.cares.c4t@gmail.com>
2018-05-09 12:25:41 +0800

    fixed error in PythonHelper.java

--- JPS/src/uk/ac/cam/cares/jps/util/PythonHelper.java
+++ JPS/src/uk/ac/cam/cares/jps/util/PythonHelper.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import uk.ac.cam.cares.jps.adms.PythonException;
+import uk.ac.cam.cares.jps.util.PythonException;
 import uk.ac.cam.cares.jps.config.AgentLocator;
 
 public class PythonHelper {
