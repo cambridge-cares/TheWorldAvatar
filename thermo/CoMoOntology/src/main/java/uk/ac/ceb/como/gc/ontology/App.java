649d037b5d3a68ea6ae8f7c7985431269d106e8e
nk510 <nk510@ceb-s7-10-wdo.ceb.private.cam.ac.uk>
2018-05-25 10:55:49 +0100

    Test App class commit and push.

---
+++ thermo/CoMoOntology/src/main/java/uk/ac/ceb/como/gc/ontology/App.java
@@ -0,0 +1,13 @@
+package uk.ac.ceb.como.gc.ontology;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
