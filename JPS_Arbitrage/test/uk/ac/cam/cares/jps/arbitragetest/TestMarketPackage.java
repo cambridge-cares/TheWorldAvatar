a0d89710e060ac5ab74826fe6d79711b2ca27e85
weiern <weiern.cares.c4t@gmail.com>
2018-06-27 14:41:00 +0800

    refactored testDownloadingAndSavingExchangeRatesInTheKnowledgeBase refactored testRetrievingUtilityPricesByProvidingTheirLocationsAndCPOAndFAMEMarketPricesFromTheKnowledgeBase refactored testRetrievingUtilityPricesByProvidingTheirLocationsAndHNGAndZCEMarketPricesFromTheKnowledgeBase

--- JPS_Arbitrage/test/uk/ac/cam/cares/jps/arbitragetest/TestMarketPackage.java
+++ JPS_Arbitrage/test/uk/ac/cam/cares/jps/arbitragetest/TestMarketPackage.java
@@ -20,13 +20,6 @@ public class TestMarketPackage extends TestCase {
 	private static Logger logger = LoggerFactory
 			.getLogger(TestMarketPackage.class);
 	
-	class HeaderValues {
-		String[] arrayHeader;
-		String[] arrayMonths;
-		String[] arrayDatetime;
-		String[] arrayPrices;
-	}
-	
 	/**
 	 * this function calls DataDownload to call
 	 * DataDownload.Downloading_market_data and check that
@@ -96,11 +89,18 @@ public class TestMarketPackage extends TestCase {
 			ClientProtocolException, IOException {
 
 		String path = "/JPS_Arbitrage/downloadingAndSavingExchangeRatesInTheKnowledgeBase";
-		String actual = AgentCaller.executeGet(path,
-				"whatver", "whatever2");
+		String actual = AgentCaller.executeGet(path, "whatver", "whatever2");
 		logger.info(actual);
-		assertTrue(actual.contains("USD"));
-
+		
+		Gson g = new Gson();
+		Gson objGson = new GsonBuilder().setPrettyPrinting().create();
+		Type listType = new TypeToken<Map<String, String>>(){}.getType();
+		
+		Map<String, String> mapHeadersRates = objGson.fromJson(g.fromJson(actual, String.class), listType);
+		String[] headers = mapHeadersRates.keySet().toArray(new String[0]);
+		
+		assertTrue(headers[0].contains("USD"));
+		assertTrue(headers[1].contains("USD"));
 	}
 
 	/**
@@ -119,12 +119,26 @@ public class TestMarketPackage extends TestCase {
 		String path = "/JPS_Arbitrage/retrievingUtilityPricesByProvidingTheirLocationsAndCPOAndFAMEMarketPricesFromTheKnowledgeBase";
 		String key = "individuals";
 		String value = "V_Price_CoolingWater_001,V_Price_Storage_Biodiesel_001,V_Price_Storage_CrudePalmOil_001,V_Costs_Storage_CrudePalmOil_001,V_Price_Transport_Malaysia-SG_CrudePalmOil_001,V_Price_Electricity_001,V_USD_to_SGD,V_Price_ProcessWater_001,V_Price_HighPressureSteam_001,V_USD_to_CNY,V_Price_MediumPressureSteam_001,V_Price_LowPressureSteam_001,V_Price_Transport_SEA-SC_Biodiesel_001,V_Price_FuelGas_001";
-		String actual = AgentCaller.executeGet(path, key,
-				value);
+		String actual = AgentCaller.executeGet(path, key, value);
 		logger.info(actual);
-		assertTrue(actual.contains(
-				"JAN 2021,FEB 2021,MAR 2021,APR 2021"));
-
+				
+		Gson g = new Gson();
+		
+		Gson objGson = new GsonBuilder().setPrettyPrinting().create();
+		Type listTypeStringString = new TypeToken<Map<String, String>>(){}.getType();
+		Type listTypeStringArray = new TypeToken<Map<String, String[]>>(){}.getType();
+				
+		String[] utilitiesMarketdataArray = g.fromJson(g.fromJson(actual, String.class), String[].class);
+		
+		Map<String, String> utilities = objGson.fromJson(utilitiesMarketdataArray[0], listTypeStringString);
+		Map<String, String[]> marketdataCpo = objGson.fromJson(utilitiesMarketdataArray[1], listTypeStringArray);
+		Map<String, String[]> marketdataFame = objGson.fromJson(utilitiesMarketdataArray[2], listTypeStringArray);
+		
+		assertTrue(utilities.containsKey("V_Price_Storage_Biodiesel_001"));
+		assertNotNull(utilities.get("V_Price_Storage_Biodiesel_001"));
+				
+		assertEquals(marketdataCpo.get("arrayHeader")[0], "CPO");
+		assertEquals(marketdataFame.get("arrayHeader")[0], "FAME");
 	}
 
 	/**
@@ -143,10 +157,26 @@ public class TestMarketPackage extends TestCase {
 		String path = "/JPS_Arbitrage/retrievingUtilityPricesByProvidingTheirLocationsAndHNGAndZCEMarketPricesFromTheKnowledgeBase";
 		String key = "individuals";
 		String value = "V_Price_Storage_NaturalGas_001,V_Price_CoolingWater_001,V_Price_Storage_Methanol_001,V_Price_Electricity_001,V_Price_Transport_SG-SC_Methanol_001,V_USD_to_SGD,V_Price_ProcessWater_001,V_Price_Transport_USGC-NEA_NaturalGas_001,V_Price_HighPressureSteam_001,V_USD_to_CNY,V_Price_MediumPressureSteam_001,V_Price_LowPressureSteam_001,V_Price_FuelGas_001";
-		String actual = AgentCaller.executeGet(path, key,
-				value);
+		String actual = AgentCaller.executeGet(path, key, value);
 		logger.info(actual);
-		assertTrue(actual.contains("MA902,MA903,MA904"));
+		
+		Gson g = new Gson();
+		
+		Gson objGson = new GsonBuilder().setPrettyPrinting().create();
+		Type listTypeStringString = new TypeToken<Map<String, String>>(){}.getType();
+		Type listTypeStringArray = new TypeToken<Map<String, String[]>>(){}.getType();
+				
+		String[] utilitiesMarketdataArray = g.fromJson(g.fromJson(actual, String.class), String[].class);
+		
+		Map<String, String> utilities = objGson.fromJson(utilitiesMarketdataArray[0], listTypeStringString);
+		Map<String, String[]> marketdataNg = objGson.fromJson(utilitiesMarketdataArray[1], listTypeStringArray);
+		Map<String, String[]> marketdataMeoh = objGson.fromJson(utilitiesMarketdataArray[2], listTypeStringArray);
+		
+		assertTrue(utilities.containsKey("V_Price_Storage_Methanol_001"));
+		assertNotNull(utilities.get("V_Price_Storage_Methanol_001"));
+				
+		assertEquals(marketdataNg.get("arrayHeader")[0], "NG");
+		assertEquals(marketdataMeoh.get("arrayHeader")[0], "MeOH");
 
 	}
 
