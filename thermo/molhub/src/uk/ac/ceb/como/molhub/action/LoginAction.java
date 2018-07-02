b2d040e555faaad411d71786e3c533feff028f84
nk510 <nk510@cam.ac.uk>
2018-07-02 17:26:51 +0100

    Login action

---
+++ thermo/molhub/src/uk/ac/ceb/como/molhub/action/LoginAction.java
@@ -0,0 +1,41 @@
+package uk.ac.ceb.como.molhub.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+	private String password;
+
+	public String execute() {
+
+		if (this.username.equals("admin") && this.password.equals("admin")) {
+			return "success";
+		} else {
+			addActionError(getText("error.login"));
+			return "error";
+		}
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
