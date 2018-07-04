f9fb594ee57c22a290eca51536288fbcef540fec
Daniel Nurkowski <dln22.como@gmail.com>
2018-07-04 14:34:52 +0100

    master: Changed the script behaviour if no command line arguments are provided. Now the help text is displayed.

--- Agents/STDCThermoAgent/Source/thermoDriver.py
+++ Agents/STDCThermoAgent/Source/thermoDriver.py
@@ -37,51 +37,53 @@ def main(argv):
             "tcsv=", "href="])
     except getopt.GetoptError:
         usage()
-    xml_file = ''
-    json_file = ''
-    out_datfile=''
-    out_csvfile=''
-    tfit_range=[]
-    tcsv_range=[]
-    use_file = -1
-    href=[]
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage()
-        elif opt in ("-x", "--xml_file"):
-            xml_file = arg
-            if use_file < 0:  use_file = 1
-        elif opt in ("-j", "--json_file"):
-            json_file = arg
-            if use_file < 0:  use_file = 2
-        elif opt in ("-d", "--out_datfile"):
-            out_datfile = arg
-        elif opt in ("-c", "--out_csvfile"):
-            out_csvfile = arg
-        elif opt in ("--tfit"):
-            tfit_range = [float(a) for a in arg.split(',')]
-        elif opt in ("--tcsv"):
-            tcsv_range = [float(a) for a in arg.split(',')]
-        elif opt in ("--href"):
-            href = [float(a) for a in arg.split(',')]
-        else:
-            print("Unhandled option")
-            usage()
-    if use_file ==1: 
-        inp_file = xml_file
-    elif use_file ==2:
-        inp_file = json_file
+    if not opts:
+        usage()
     else:
-        inp_file = ''
+        xml_file = ''
+        json_file = ''
+        out_datfile=''
+        out_csvfile=''
+        tfit_range=[]
+        tcsv_range=[]
+        use_file = -1
+        href=[]
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):
+                usage()
+            elif opt in ("-x", "--xml_file"):
+                xml_file = arg
+                if use_file < 0:  use_file = 1
+            elif opt in ("-j", "--json_file"):
+                json_file = arg
+                if use_file < 0:  use_file = 2
+            elif opt in ("-d", "--out_datfile"):
+                out_datfile = arg
+            elif opt in ("-c", "--out_csvfile"):
+                out_csvfile = arg
+            elif opt in ("--tfit"):
+                tfit_range = [float(a) for a in arg.split(',')]
+            elif opt in ("--tcsv"):
+                tcsv_range = [float(a) for a in arg.split(',')]
+            elif opt in ("--href"):
+                href = [float(a) for a in arg.split(',')]
+            else:
+                print("Unhandled option")
+                usage()
+        if use_file ==1: 
+            inp_file = xml_file
+        elif use_file ==2:
+            inp_file = json_file
+        else:
+            inp_file = ''
 
-    if len(inp_file) == 0: #or 
-        # exists
-        printErrorCode(0,**{'file':inp_file})
-    elif os.path.isfile(inp_file) == False:
-        printErrorCode(1, **{'file':inp_file})
+        if len(inp_file) == 0: #or 
+            # exists
+            printErrorCode(0,**{'file':inp_file})
+        elif os.path.isfile(inp_file) == False:
+            printErrorCode(1, **{'file':inp_file})
 
-    
-    chs.RunThermoScriptFromCmd(use_file,inp_file,out_datfile,out_csvfile,tfit_range,tcsv_range,href)
+        chs.RunThermoScriptFromCmd(use_file,inp_file,out_datfile,out_csvfile,tfit_range,tcsv_range,href)
 
 if __name__ == "__main__":
    try:
