2620d5a1dce6b46020191a7fc4dc6653c1f3b386
nk510 <nk510@cam.ac.uk>
2018-07-10 16:22:27 +0100

    Parsing basis set.

--- thermo/CoMoOntology/src/main/java/uk/ac/cam/ceb/como/jaxb/parser/g09/ParsingBasisSet.java
+++ thermo/CoMoOntology/src/main/java/uk/ac/cam/ceb/como/jaxb/parser/g09/ParsingBasisSet.java
@@ -4,13 +4,23 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
-public class ParsingBasisSet {	
+import org.xmlcml.cml.element.CMLParameter;
 
-public static String getBasisSetString(File f) throws IOException {
-		
-		String basisSetString= "";	
-		
+import uk.ac.cam.ceb.como.compchem.CompChem;
+import uk.ac.cam.ceb.como.io.chem.file.jaxb.Parameter;
+import uk.ac.cam.ceb.como.io.chem.file.jaxb.Scalar;
+import uk.ac.cam.ceb.como.io.chem.file.parser.compchem.CompChemParser;
+import uk.ac.cam.ceb.como.io.chem.file.parser.g09.FrequencyParser;
+import uk.ac.cam.ceb.como.thermo.calculator.rotation.internal.util.IRCompChemWrapper;
+
+public class ParsingBasisSet {
+
+	public static String getBasisSetString(File f) throws IOException {
+
+		String basisSetString = "";
+
 		/**
 		 * 
 		 * @author nk510
@@ -25,40 +35,94 @@ public static String getBasisSetString(File f) throws IOException {
 		for (String line; (line = br.readLine()) != null;) {
 
 			if (line.contains("#p ")) {
-				
+
 				/**
 				 * 
 				 * @author nk510
 				 *         <p>
-				 *         Returns substring that starts with "/" string and ends with first appearing " " character. 
+				 *         Returns substring that starts with "/" string and ends with first
+				 *         appearing " " character.
 				 *         </p>
 				 * 
 				 */
-				
+
 				/**
-				 * Split string that starts with '#p ' on two substrings before and after '/'.
+				 * Split string that starts with '#p ' on two substrings before and after
+				 * charatcter '/'.
 				 */
-				String splitOne[]=line.split("/");				
-				line=  splitOne[1];				
-				
+
+				String splitOne[] = line.split("/");
+				line = splitOne[1];
+
 				/**
-				 * Split second substring into two strings before white space and after white space.
+				 * Split second substring into two strings before white space and after white
+				 * space.
 				 */
-				
-				String splitTwo[] = line.split(" ");				
+
+				String splitTwo[] = line.split(" ");
 
 				basisSetString = basisSetString + splitTwo[0];
-				
+
 				System.out.println(basisSetString);
-				
+
 				break;
 			}
 		}
 
 		br.close();
-	
-	return basisSetString;
-	
-     }
- 
+
+		return basisSetString;
+
+	}
+
+	public static Parameter getBasisSetParameter(File f, int numberOfAtoms) throws Exception {
+
+		Parameter parameter = new Parameter();
+		Scalar scalar = new Scalar();
+
+		if (numberOfAtoms > 1) {
+
+			FrequencyParser parser = new FrequencyParser();
+			parser.set(f);
+			parser.parse();
+
+			CompChem cc = (CompChem) parser.get();
+
+			CompChemParser ccp = new CompChemParser();
+
+			ccp.parse(cc);
+
+			IRCompChemWrapper irccw = new IRCompChemWrapper(cc);
+
+			List<CMLParameter> cmlP = irccw.getParameters();
+
+			for (CMLParameter p : cmlP) {
+
+				/**
+				 *
+				 * Finds basis set when attribute dictRef has value that is equal to 'cc:basis'
+				 *
+				 */
+				if (p.getDictRef().equals("cc:basis")) {
+
+					parameter.setDictRef(p.getDictRef());
+					scalar.setValue(p.getValue());
+					scalar.setDataType("xsd:string");
+
+					parameter.getScalarOrArrayOrMatrix().add(scalar);
+				}
+			}
+
+		} else {
+			
+			parameter.setDictRef("cc:basis");
+			scalar.setValue(getBasisSetString(f));
+			scalar.setDataType("xsd:string");
+
+			parameter.getScalarOrArrayOrMatrix().add(scalar);
+		}
+
+		return parameter;
+	}
+
 }
