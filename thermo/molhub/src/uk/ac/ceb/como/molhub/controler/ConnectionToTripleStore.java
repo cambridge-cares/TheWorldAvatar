d5ab44724ac069af16d9bf968f393f29372c6ba4
nk510 <nk510@cam.ac.uk>
2018-07-30 17:52:05 +0100

    Established connection with triple store.

---
+++ thermo/molhub/src/uk/ac/ceb/como/molhub/controler/ConnectionToTripleStore.java
@@ -0,0 +1,26 @@
+package uk.ac.ceb.como.molhub.controler;
+
+import org.eclipse.rdf4j.repository.Repository;
+
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.http.HTTPRepository;
+
+public class ConnectionToTripleStore {
+	
+	/**
+	 * 
+	 * @param serverUrl remote sparql endpoint.
+	 * @param graphName name of graph in rdf4j triple store where ontologies are saved.
+	 * @return instance of class <p>org.eclipse.rdf4j.repository.RepositoryConnection</p>.
+	 */
+	public static RepositoryConnection getRepositoryConnection(String serverUrl, String graphName) {
+		
+		Repository  repository = new HTTPRepository(serverUrl, graphName);
+		
+		repository.initialize();
+		
+		RepositoryConnection repositoryConnection = repository.getConnection();
+		
+		return repositoryConnection;
+	}
+}
