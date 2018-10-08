f1357dd59174225b06ec1b9a8c83a6473b83b3c8
Claudius <mortalcay@gmail.com>
2018-10-08 10:49:30 +0800

    add the readme files for some agents and update the old iri involved to the new one

--- web/CO2WEB/public/javascripts/PPCO2.js
+++ web/CO2WEB/public/javascripts/PPCO2.js
@@ -1,6 +1,41 @@
 
 $(document).ready(function () {
 
+
+
+
+
+    const toggleDisplay = elemId => {
+        let x = document.getElementById(elemId);
+        if (x.style.display !== 'block') {
+            x.style.display = 'block';
+        } else {
+            x.style.display = 'none';
+        }
+    };
+
+    $("#readme-button").click(function() {
+        toggleDisplay("readme-text");
+    });
+
+    document.addEventListener("click", function(evt) {
+        var readmeButtonElement = document.getElementById('readme-button'),
+            readmeTextElement = document.getElementById('readme-text'),
+            targetElement = evt.target;  // clicked element
+
+        if (targetElement == readmeButtonElement || targetElement == readmeTextElement) {
+            return; //readme-button or readme-text is clicked. do nothing.
+        }
+
+        if(readmeTextElement.style.display === 'block') {
+            readmeTextElement.style.display = 'none';
+        }
+    });
+
+
+
+
+
     let curPath = window.location.href;
     let countrySelected;
     let lastPercentInput;
