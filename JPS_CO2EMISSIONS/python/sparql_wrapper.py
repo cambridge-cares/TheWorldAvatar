9b3d381e098be8e6afc242c0706e3c0013ae16ba
Kevin <lkaditya1@e.ntu.edu.sg>
2018-11-13 17:22:54 +0800

    change to use the claudius directory

--- JPS_CO2EMISSIONS/python/sparql_wrapper.py
+++ JPS_CO2EMISSIONS/python/sparql_wrapper.py
@@ -1,14 +1,16 @@
 from SPARQLWrapper import SPARQLWrapper, JSON, POST
 
 def sparqlQueryRead(queryString):
-    sparql = SPARQLWrapper("http://localhost:8081/fuseki/test/sparql")
+#     sparql = SPARQLWrapper("http://localhost:8081/fuseki/test/sparql")
+    sparql = SPARQLWrapper("http://www.theworldavatar.com:80/damecoolquestion/worldpowerplantsng/sparql")
     sparql.setQuery(queryString)
     sparql.setReturnFormat(JSON)
 
     return sparql.query().convert()
 
 def sparqlQueryWrite(queryString):
-    sparql = SPARQLWrapper("http://localhost:8081/fuseki/test/update")
+#    sparql = SPARQLWrapper("http://theworldavatar.com/damecoolquestion/worldpowerplantsng/update")
+    sparql = SPARQLWrapper("http://www.theworldavatar.com:80/damecoolquestion/worldpowerplantsng/update")
     sparql.setQuery(queryString)
     sparql.setMethod(POST)
     return sparql.query()
