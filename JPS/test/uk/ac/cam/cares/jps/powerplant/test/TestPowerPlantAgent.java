5c417f0f0d3bec3fcbe14c9b9fbf43334b6c3ca1
Kevin <lkaditya1@e.ntu.edu.sg>
2018-11-19 09:49:04 +0800

    add power plant agent test

---
+++ JPS/test/uk/ac/cam/cares/jps/powerplant/test/TestPowerPlantAgent.java
@@ -0,0 +1,39 @@
+package uk.ac.cam.cares.jps.powerplant.test;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import junit.framework.TestCase;
+import uk.ac.cam.cares.jps.base.discovery.AgentCaller;
+
+public class TestPowerPlantAgent extends TestCase {
+	
+	
+	private String getContextPathForJPSco2emissionefactor() {
+		return "/JPS/powerplant/calculateemission";
+	}
+	
+	public void testCallAgent () throws JSONException {
+
+	// EIP --> one parameter
+	JSONObject dataSet = new JSONObject();
+	try {
+		dataSet.put("reactionmechanism", "marinov") ;
+		dataSet.put("plant", "http://www.theworldavatar.com/kb/deu/berlin/powerplants/Heizkraftwerk_Mitte.owl#Plant-002") ;
+	} 
+	catch (JSONException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	String json = dataSet.toString();
+	String resultjson = AgentCaller.executeGet(getContextPathForJPSco2emissionefactor(), "query", json);
+
+
+	
+	//double emission = new JSONObject(resultjson).getJSONObject("hasEmission").getJSONObject("hasValue").getDouble("numericalValue"); //changed later
+	String emission = new JSONObject(resultjson).getString("waste");
+	assertEquals("http://www.theworldavatar.com/kb/deu/berlin/powerplants/Heizkraftwerk_Mitte.owl#WasteStream-001", emission);
+	
+	}
+}
