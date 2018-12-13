3ff0ee0cf0c9d6b56909e2a86675965b1495613a
Kevin <lkaditya1@e.ntu.edu.sg>
2018-12-13 10:18:44 +0800

    change the class on chemical reaction based on ontochem

--- JPS_COMPOSITION/test/uk/ac/cam/cares/jps/agents/test/TestAgentDescriptions.java
+++ JPS_COMPOSITION/test/uk/ac/cam/cares/jps/agents/test/TestAgentDescriptions.java
@@ -102,7 +102,7 @@ public class TestAgentDescriptions extends TestCase {
 	private Service createDescrForAgentPowerPlant() {
 		return new ServiceBuilder().operation(null, JPS + "/PowerPlant")
 			.input("http://www.theworldavatar.com/ontology/ontocape/chemical_process_system/CPS_realization/plant.owl#Plant", "plant")
-			.input("http://www.theworldavatar.com/ontology/ontocape/material/substance/reaction_mechanism.owl#ChemicalReaction", "reactionmechanism")
+			.input("https://como.cheng.cam.ac.uk/kb/ontochem.owl#ReactionMechanism", "reactionmechanism")
 			.output("http://www.theworldavatar.com/ontology/ontocape/chemical_process_system/CPS_function/process.owl#NonReusableWasteProduct", "waste")
 			.build();
 	}
@@ -163,7 +163,7 @@ public class TestAgentDescriptions extends TestCase {
 				.operation(null, null)
 				.input("http://www.theworldavatar.com/ontology/ontocitygml/OntoCityGML.owl#EnvelopeType", "region")
 				.input("http://www.theworldavatar.com/ontology/ontocape/chemical_process_system/CPS_realization/plant.owl#Plant", "plant")
-				.input("http://www.theworldavatar.com/ontology/ontocape/material/substance/reaction_mechanism.owl#ChemicalReaction", "reactionmechanism")
+				.input("https://como.cheng.cam.ac.uk/kb/ontochem.owl#ReactionMechanism", "reactionmechanism")
 				.output("https://www.w3.org/ns/csvw#Table", "dispersiongrid")
 				.output("http://www.theworldavatar.com/ontology/ontocitygml/OntoCityGML.owl#BuildingType", true, "buildings", true)
 				.build();
