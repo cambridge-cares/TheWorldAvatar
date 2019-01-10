4ca032f0b1ea32a358153f705bfdcc103b09c47a
Kevin <lkaditya1@e.ntu.edu.sg>
2019-01-10 09:23:29 +0800

    change the ship iri

--- JPS_SHIP/src/uk/ac/cam/cares/jps/ship/ShipQueryAgent.java
+++ JPS_SHIP/src/uk/ac/cam/cares/jps/ship/ShipQueryAgent.java
@@ -1,16 +1,17 @@
 package uk.ac.cam.cares.jps.ship;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 import uk.ac.cam.cares.jps.base.exception.PythonException;
 import uk.ac.cam.cares.jps.base.util.PythonHelper;
-
-import com.google.gson.Gson;
 
 /**
  * Servlet implementation class ShipQueryAgent
@@ -23,16 +24,16 @@ public class ShipQueryAgent extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 
 		String[] arrayOfShipIRIs = { 
-			"http://www.theworldavatar.com/kb/sgp/Ship-1.owl#Ship-1",
-	        "http://www.theworldavatar.com/kb/sgp/Ship-2.owl#Ship-2",
-	        "http://www.theworldavatar.com/kb/sgp/Ship-3.owl#Ship-3",
-	        "http://www.theworldavatar.com/kb/sgp/Ship-4.owl#Ship-4",
-	        "http://www.theworldavatar.com/kb/sgp/Ship-5.owl#Ship-5",
-	        "http://www.theworldavatar.com/kb/sgp/Ship-6.owl#Ship-6",
-	        "http://www.theworldavatar.com/kb/sgp/Ship-7.owl#Ship-7",
-	        "http://www.theworldavatar.com/kb/sgp/Ship-8.owl#Ship-8",
-	        "http://www.theworldavatar.com/kb/sgp/Ship-9.owl#Ship-9",
-	        "http://www.theworldavatar.com/kb/sgp/Ship-10.owl#Ship-10"
+			"http://www.theworldavatar.com/kb/ships/Ship-1.owl#Ship-1",
+	        "http://www.theworldavatar.com/kb/ships/Ship-2.owl#Ship-2",
+	        "http://www.theworldavatar.com/kb/ships/Ship-3.owl#Ship-3",
+	        "http://www.theworldavatar.com/kb/ships/Ship-4.owl#Ship-4",
+	        "http://www.theworldavatar.com/kb/ships/Ship-5.owl#Ship-5",
+	        "http://www.theworldavatar.com/kb/ships/Ship-6.owl#Ship-6",
+	        "http://www.theworldavatar.com/kb/ships/Ship-7.owl#Ship-7",
+	        "http://www.theworldavatar.com/kb/ships/Ship-8.owl#Ship-8",
+	        "http://www.theworldavatar.com/kb/ships/Ship-9.owl#Ship-9",
+	        "http://www.theworldavatar.com/kb/ships/Ship-10.owl#Ship-10"
 		};
 		
 		Gson g = new Gson();
