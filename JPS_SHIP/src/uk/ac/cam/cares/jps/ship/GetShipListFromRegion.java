eaff9502f41ae317f953d5278decb87ebbebead7
weiern <weiern.cares.c4t@gmail.com>
2019-01-25 11:44:51 +0800

    commented out CityToWeather

--- JPS_SHIP/src/uk/ac/cam/cares/jps/ship/GetShipListFromRegion.java
+++ JPS_SHIP/src/uk/ac/cam/cares/jps/ship/GetShipListFromRegion.java
@@ -30,47 +30,47 @@ public class GetShipListFromRegion extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("application/json");
-		String[] arrayOfShipIRIs = { 
-				"http://www.theworldavatar.com/kb/ships/Ship-1.owl#Ship-1",
-		        "http://www.theworldavatar.com/kb/ships/Ship-2.owl#Ship-2",
-		        "http://www.theworldavatar.com/kb/ships/Ship-3.owl#Ship-3",
-		        "http://www.theworldavatar.com/kb/ships/Ship-4.owl#Ship-4",
-		        "http://www.theworldavatar.com/kb/ships/Ship-5.owl#Ship-5",
-		        "http://www.theworldavatar.com/kb/ships/Ship-6.owl#Ship-6",
-		        "http://www.theworldavatar.com/kb/ships/Ship-7.owl#Ship-7",
-		        "http://www.theworldavatar.com/kb/ships/Ship-8.owl#Ship-8",
-		        "http://www.theworldavatar.com/kb/ships/Ship-9.owl#Ship-9",
-		        "http://www.theworldavatar.com/kb/ships/Ship-10.owl#Ship-10"
-		};
-		
-		JSONObject result = new JSONObject();
-		try {
-			result.put("shipIRIs", arrayOfShipIRIs);
-			res.getWriter().write(result.toString());
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		
-//		String shipEp = "http://172.25.182.41/damecoolquestion/ships-persistent/sparql";
-//		String connectType= "endpoint";
-//		int shipNum = 25;
-//       //get parameter range		
-//		String[] pparams = new String[7];
-//		pparams[0] = shipEp;
-//		pparams[1] = connectType;
-//		pparams[2] = ""+shipNum;
-//		pparams[3] = req.getParameter("xmin");
-//		pparams[4]  = req.getParameter("xmax");
-//		pparams[5]  = req.getParameter("ymin");
-//		pparams[6]  = req.getParameter("ymax");
-//
-//		String paramStr = String.join(" ", pparams);
+//		String[] arrayOfShipIRIs = { 
+//				"http://www.theworldavatar.com/kb/ships/Ship-1.owl#Ship-1",
+//		        "http://www.theworldavatar.com/kb/ships/Ship-2.owl#Ship-2",
+//		        "http://www.theworldavatar.com/kb/ships/Ship-3.owl#Ship-3",
+//		        "http://www.theworldavatar.com/kb/ships/Ship-4.owl#Ship-4",
+//		        "http://www.theworldavatar.com/kb/ships/Ship-5.owl#Ship-5",
+//		        "http://www.theworldavatar.com/kb/ships/Ship-6.owl#Ship-6",
+//		        "http://www.theworldavatar.com/kb/ships/Ship-7.owl#Ship-7",
+//		        "http://www.theworldavatar.com/kb/ships/Ship-8.owl#Ship-8",
+//		        "http://www.theworldavatar.com/kb/ships/Ship-9.owl#Ship-9",
+//		        "http://www.theworldavatar.com/kb/ships/Ship-10.owl#Ship-10"
+//		};
 //		
-//		String shipListStr = PythonHelper.callPython("caresjpsship/shipRegionQuery.py", paramStr
-//				, this);
-//		
-//		System.out.println(shipListStr);
-//		res.getWriter().write(convertFromUTF8(shipListStr));
+//		JSONObject result = new JSONObject();
+//		try {
+//			result.put("shipIRIs", arrayOfShipIRIs);
+//			res.getWriter().write(result.toString());
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//		}
+		
+		String shipEp = "http://172.25.182.41/damecoolquestion/ships-persistent/sparql";
+		String connectType= "endpoint";
+		int shipNum = 25;
+       //get parameter range		
+		String[] pparams = new String[7];
+		pparams[0] = shipEp;
+		pparams[1] = connectType;
+		pparams[2] = ""+shipNum;
+		pparams[3] = req.getParameter("xmin");
+		pparams[4]  = req.getParameter("xmax");
+		pparams[5]  = req.getParameter("ymin");
+		pparams[6]  = req.getParameter("ymax");
+
+		String paramStr = String.join(" ", pparams);
+		
+		String shipListStr = PythonHelper.callPython("caresjpsship/shipRegionQuery.py", paramStr
+				, this);
+		
+		System.out.println(shipListStr);
+		res.getWriter().write(convertFromUTF8(shipListStr));
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
