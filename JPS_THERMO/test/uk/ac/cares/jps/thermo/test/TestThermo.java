ffb33153267f786597433e84b712b0f05f93531b
nk510 <nk510@cam.ac.uk>
2019-03-06 18:02:55 +0800

    Updated: - Converts json into OWL file - Stores OWL file into RDF4J triple store.

--- JPS_THERMO/test/uk/ac/cares/jps/thermo/test/TestThermo.java
+++ JPS_THERMO/test/uk/ac/cares/jps/thermo/test/TestThermo.java
@@ -1,29 +1,13 @@
 package uk.ac.cares.jps.thermo.test;
-import java.io.IOException;
-import java.util.ArrayList;
-
 import org.json.JSONObject;
 import org.junit.Test;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import com.cmclinnovations.ontochem.controller.OntoKin;
-import com.cmclinnovations.ontochem.controller.OntoKinFactory;
-import com.cmclinnovations.ontochem.model.configuration.AppConfigOperationControl;
-import com.cmclinnovations.ontochem.model.configuration.SpringConfiguration;
-import com.cmclinnovations.ontochem.model.exception.OntoException;
 
 import junit.framework.TestCase;
 import uk.ac.cam.cares.jps.base.discovery.AgentCaller;
 import uk.ac.cam.cares.jps.base.query.QueryBroker;
 
 public class TestThermo extends TestCase {
-	
-	 private ApplicationContext applicationContext;
-	 private AppConfigOperationControl opCtrl;
-
-	 
+ 
 	
 	public void testReadFile() {
 		
@@ -47,35 +31,4 @@ public class TestThermo extends TestCase {
 		
 	}
 	
-	@Test
-	public void convertJsonIntoOwl() throws IOException {
-		
-		/**
-		 * @author NK
-		 * This source code (method) is implemented by Dr M.S.F. Farazi <msff2@cam.ac.uk>.
-		 */
-		
-		if (applicationContext == null) {
-			applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-		}
-		if (opCtrl == null) {
-			opCtrl = applicationContext.getBean(AppConfigOperationControl.class);
-		}
-		
-		ArrayList<String> sourceFiles = new ArrayList<String>();
-
-		sourceFiles.add("C:\\apache-tomcat-8.5.35\\webapps\\ROOT\\kb\\fba9f419-c117-348a-862f-f377f58a3b07\\fba9f419-c117-348a-862f-f377f58a3b07_updated_nasa.json");
-		
-		String owlFileDir = "C:\\apache-tomcat-8.5.35\\webapps\\ROOT\\kb\\fba9f419-c117-348a-862f-f377f58a3b07";
-
-		if (sourceFiles != null) {
-			OntoKin ontoKin = OntoKinFactory.getOntoKin(opCtrl.getOpReadCompChem(), opCtrl.getOpWriteOwl(), sourceFiles, owlFileDir);
-			try {
-				ontoKin.convert();
-			} catch (OWLOntologyCreationException | OntoException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	   }	
 }
