f9bf19c51dae12ec0b07e787b6c2415903895cf9
nk510 <nk510@cam.ac.uk>
2019-03-06 17:48:18 +0800

    Upload ontology to RDF4J triple store

---
+++ JPS_THERMO/src/uk/ac/cam/cares/jps/thermo/manager/UploadOntology.java
@@ -0,0 +1,107 @@
+package uk.ac.cam.cares.jps.thermo.manager;
+
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.log4j.Logger;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.http.HTTPRepository;
+import org.eclipse.rdf4j.rio.RDFFormat;
+
+public class UploadOntology {
+
+	/** The Constant logger. */
+	final static Logger logger = Logger.getLogger(UploadOntology.class.getName());	
+
+	
+	public void uploadOntoKin(String owlFilePath, String serverUrl, String ontologyUri) {
+		
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+
+		Thread threadTask = new Thread(new Runnable() {
+
+			final File owlFile = new File (owlFilePath);
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+				/**
+				 * @author nk510 Gets the repository connection.
+				 * @param serverUrl
+				 *            remote ontokin sparql endpoint.
+				 * 
+				 */
+
+				Repository repository = new HTTPRepository(serverUrl);
+
+				repository.initialize();
+
+				RepositoryConnection connection = repository.getConnection();
+
+				try {
+
+					/**
+					 * @author nk510
+					 *         <p>
+					 * 		Begins a new transaction. Requires commit() or rollback() to be
+					 *         called to end of the transaction.
+					 *         </p>
+					 */
+
+					connection.begin();
+
+					try {
+
+						/**
+						 * @author nk510
+						 *         <p>
+						 * 		Each generated owl file will be stored in RDF4J triple store.
+						 *         </p>
+						 */
+						
+						connection.add(owlFile, ontologyUri, RDFFormat.RDFXML);
+
+						connection.commit();
+
+					} catch (RepositoryException e) {
+
+						/**
+						 * @author nk510
+						 *         <p>
+						 *         If something is wrong during the transaction, it will return a
+						 *         message about it.
+						 *         </p>
+						 * 
+						 */
+
+						logger.info("RepositoryException: " + e.getMessage());
+
+						connection.rollback();
+					}
+
+				} catch (Exception e) {
+
+					logger.info(e.getStackTrace());
+
+				}
+
+				connection.close();
+
+				repository.shutDown();
+				
+			}
+			
+		});
+		
+		executor.submit(threadTask);
+
+		ExecutorManager em = new ExecutorManager();
+
+		em.shutdownExecutorService(executor);
+		
+	}
+}
