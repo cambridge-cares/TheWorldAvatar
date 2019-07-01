dd3d4c1232f17892efe73f3d4bc4ad19a77c7a8c
Kevin Aditya <lkaditya1@e.ntu.edu.sg>
2019-07-01 11:06:06 +0800

    implement to query directly on the server of hku for weather

--- JPS_SHIP/src/uk/ac/cam/cares/jps/ship/HKUWeatherRetriever.java
+++ JPS_SHIP/src/uk/ac/cam/cares/jps/ship/HKUWeatherRetriever.java
@@ -1,16 +1,30 @@
 package uk.ac.cam.cares.jps.ship;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.ac.cam.cares.jps.base.annotate.MetaDataAnnotator;
 import uk.ac.cam.cares.jps.base.discovery.AgentCaller;
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.query.QueryBroker;
 import uk.ac.cam.cares.jps.base.scenario.BucketHelper;
 import uk.ac.cam.cares.jps.base.scenario.JPSHttpServlet;
@@ -49,15 +63,55 @@ public class HKUWeatherRetriever extends JPSHttpServlet {
 	}
 	
 	
-	public String requestlatestdata() {
+	public String requestlatestdata() throws JSONException, UnsupportedEncodingException {
 		// it will see from the postgresql which is the latest and give the latest weather data recorded
 		//right now just try to call the current weather data first
 	System.out.println("go to weather agent");
 		JSONObject input = new JSONObject();
 		input.put("city", "http://dbpedia.org/resource/Hong_Kong");
-		String result = AgentCaller.executeGetWithJsonParameter("/JPS_COMPOSITION/CityToWeather",input.toString());
-		
+//		String result = AgentCaller.executeGetWithJsonParameter("/JPS_COMPOSITION/CityToWeather",input.toString());
+		String result=queryFromHKUServer(getTimeInXsdTimeStampFormat(System.currentTimeMillis()-3600*1000)/*"2019-07-01T02:00:00"*/,getTimeInXsdTimeStampFormat(System.currentTimeMillis()));
 	
 		return result;
+	}
+	
+	public static String getTimeInXsdTimeStampFormat(long millis) { 
+		Date date = new Date(millis);
+	    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+		format.setTimeZone(TimeZone.getTimeZone("GMT+0:00"));
+	    String timestamp = format.format(date);
+	    return timestamp.replace(" ", "T");
+	}
+	
+	public static String executeGet(URIBuilder builder) {
+		try {
+			URI uri = builder.build();
+			HttpGet request = new HttpGet(uri);
+			request.setHeader(HttpHeaders.ACCEPT, "application/json");
+			HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+			if (httpResponse.getStatusLine().getStatusCode() != 200) {
+				throw new JPSRuntimeException("HTTP response with error = " + httpResponse.getStatusLine());
+			}
+			return EntityUtils.toString(httpResponse.getEntity());
+		} catch (Exception e) {
+			throw new JPSRuntimeException(e.getMessage(), e);
+		} 
+	}
+	
+	public static String queryFromHKUServer(String from, String to) throws JSONException, UnsupportedEncodingException {
+
+		String myHost = "147.8.183.135" ;
+		int myPort = 10086;
+		String myPath = "/getairhistory";
+				
+		URIBuilder builder;
+		builder = new URIBuilder().setScheme("http").setHost(myHost).setPort(myPort)
+				.setPath(myPath)
+				.setParameter("from", from/*"2019-06-16T20:00:00"*/)
+				.setParameter("to", to/*"2019-06-16T21:00:00"*/);
+		System.out.println("builder= "+builder);
+		String result = executeGet(builder);
+		return result;
+		
 	}
 }
