c5707934065947d6da01a5fc460fbedddd728c78
achadzynski <arkadiusz.chadzynski@cares.cam.ac.uk>
2019-07-16 17:25:40 +0800

    Rartially removed duplicated code off the APL file generator.

--- JPS/python/caresjpsadmsinputs/admsTest.py
+++ JPS/python/caresjpsadmsinputs/admsTest.py
@@ -1,6 +1,6 @@
 import sys
 import json
-import config
+from config import Constants
 from collections import namedtuple
 from admsAplWriter import admsAplWriter
 from admsInputDataRetrieverNew import admsInputDataRetriever
@@ -8,55 +8,63 @@ from caresjpsutil import PythonLogger
 
 pythonLogger = PythonLogger('admsTest.py')
 
-try:
-	pythonLogger.postInfoToLogServer('start')
-		
-	buildingdata = json.loads(sys.argv[1].replace("'",'"'))
-	
-	BDN = namedtuple('BDN', ['BldNumBuildings','BldName','BldType','BldX','BldY','BldHeight', 'BldLength', 'BldWidth', 'BldAngle'])
-	BDN.BldName = buildingdata['BldName']
-	BDN.BldNumBuildings = len(BDN.BldName)
-	BDN.BldType = buildingdata['BldType']
-	BDN.BldX = buildingdata['BldX']
-	BDN.BldY = buildingdata['BldY']
-	BDN.BldHeight = buildingdata['BldHeight']
-	BDN.BldLength = buildingdata['BldLength']
-	BDN.BldWidth = buildingdata['BldWidth']
-	BDN.BldAngle = buildingdata['BldAngle']
-	
-	coordinates = str(sys.argv[2]).replace("'",'"');
-	coordinates = json.loads(coordinates)
-	
-	xmax = coordinates['uppercorner']['upperx']
-	ymax = coordinates['uppercorner']['uppery']
-	
-	xmin = coordinates['lowercorner']['lowerx']
-	ymin = coordinates['lowercorner']['lowery']
-	
-	coordinates['xmin'] = xmin
-	coordinates['ymin'] = ymin
-	coordinates['xmax'] = xmax
-	coordinates['ymax'] = ymax
 
-	
-	pythonLogger.postInfoToLogServer('coordinates=' + str(coordinates))
-	
-	plant = str(sys.argv[3])
-	# workingDir = str(sys.argv[4]).replace('/','//')
-	workingDir = str(sys.argv[4])
-	
-	pythonLogger.postInfoToLogServer('workingDir=' + workingDir)
+def get_bdn(buildingdata):
+    bdn = namedtuple(Constants.BLD_BDN,
+                     [Constants.BLD_NUM, Constants.BLD_NAME, Constants.BLD_TYPE, Constants.BLD_X,
+                      Constants.BLD_Y, Constants.BLD_HEIGHT, Constants.BLD_LRNGTH, Constants.BLD_WIDTH,
+                      Constants.BLD_ANGLE])
+    bdn.BldName = buildingdata[Constants.BLD_NAME]
+    bdn.BldNumBuildings = len(bdn.BldName)
+    bdn.BldType = buildingdata[Constants.BLD_TYPE]
+    bdn.BldX = buildingdata[Constants.BLD_X]
+    bdn.BldY = buildingdata[Constants.BLD_Y]
+    bdn.BldHeight = buildingdata[Constants.BLD_HEIGHT]
+    bdn.BldLength = buildingdata[Constants.BLD_LRNGTH]
+    bdn.BldWidth = buildingdata[Constants.BLD_WIDTH]
+    bdn.BldAngle = buildingdata[Constants.BLD_ANGLE]
 
-	test = admsInputDataRetriever(plant,config.bldTopnode, coordinates,  ["CO2"   ,"CO" ,  "NO2" ,  "HC" ,  "NOx"], 2, config.bdnLimit,False, BDN)
-	result = test.get()
-	
-	pythonLogger.postInfoToLogServer('calling admsAplWirter ...')
-	result['Bdn'] = BDN
-	result['CoordiSys'] = sys.argv[5][5:]
-	writer = admsAplWriter(result, workingDir + '/test.apl')
-	writer.write()
+    return bdn
 
-	pythonLogger.postInfoToLogServer('end')
 
-except Exception as e:
-	pythonLogger.postErrorToLogServer(e)
+def get_coordinates(coordinates):
+    coordinates = json.loads(coordinates)
+    xmax = coordinates[Constants.COORD_MAX_CORNER][Constants.COORD_MAX_X]
+    ymax = coordinates[Constants.COORD_MAX_CORNER][Constants.COORD_MAX_Y]
+    xmin = coordinates[Constants.COORD_MIN_CORNER][Constants.COORD_MIN_X]
+    ymin = coordinates[Constants.COORD_MIN_CORNER][Constants.COORD_MIN_Y]
+    coordinates[Constants.KEY_MIN_X] = xmin
+    coordinates[Constants.KEY_MIN_Y] = ymin
+    coordinates[Constants.KEY_MAX_X] = xmax
+    coordinates[Constants.KEY_MAX_Y] = ymax
+
+    return coordinates
+
+
+def main():
+    try:
+        bdn_data = json.loads(sys.argv[1].replace("'", '"'))
+        coor_data = str(sys.argv[2]).replace("'", '"')
+        plant = str(sys.argv[3])
+        working_dir = str(sys.argv[4])
+        coord_sys = sys.argv[5][5:]
+
+        BDN = get_bdn(bdn_data)
+        coords = get_coordinates(coor_data)
+        pollutants = [Constants.POL_CO2, Constants.POL_CO, Constants.POL_NO2, Constants.POL_HC, Constants.POL_NOX]
+
+        test = admsInputDataRetriever(plant, Constants.BLD_TOPNODE, coords, pollutants, 2, Constants.BLD_LIMIT, False, BDN)
+        result = test.get()
+
+        result['Bdn'] = BDN
+        result['CoordiSys'] = coord_sys
+
+        writer = admsAplWriter(result, working_dir + '/test.apl')
+        writer.write()
+
+    except Exception as e:
+        pythonLogger.postErrorToLogServer(e)
+
+if __name__ == "__main__":
+    main()
+
--- JPS/python/caresjpsadmsinputs/admsTestShip.py
+++ JPS/python/caresjpsadmsinputs/admsTestShip.py
@@ -1,123 +1,106 @@
 import datetime
 import json
 import sys
-from collections import namedtuple
 from caresjpsutil import PythonLogger
 from pyproj import Proj, transform
-import config
+import admsTest
 from admsAplWriterShip import admsAplWriter
 from admsInputDataRetrieverChimney import admsInputDataRetriever
+from config import Constants
 
 pythonLogger = PythonLogger('admsTest.py')
-sourceCRS = Proj(init='epsg:4326')
-targetCRS = Proj(init=sys.argv[5][:4].lower() + sys.argv[5][4:])
 
-try:
 
-    buildingdata = json.loads(sys.argv[1].replace("'", '"'))
-    BDN = namedtuple('BDN',
-                     ['BldNumBuildings', 'BldName', 'BldType', 'BldX', 'BldY', 'BldHeight', 'BldLength', 'BldWidth',
-                      'BldAngle'])
-    BDN.BldName = buildingdata['BldName']
-    BDN.BldNumBuildings = len(BDN.BldName)
-    BDN.BldType = buildingdata['BldType']
-    BDN.BldX = buildingdata['BldX']
-    BDN.BldY = buildingdata['BldY']
-    BDN.BldHeight = buildingdata['BldHeight']
-    BDN.BldLength = buildingdata['BldLength']
-    BDN.BldWidth = buildingdata['BldWidth']
-    BDN.BldAngle = buildingdata['BldAngle']
+def main():
+    try:
+        sourceCRS = Proj(init='epsg:4326')
+        targetCRS = Proj(init=sys.argv[5][:4].lower() + sys.argv[5][4:])
+        bdn_data = json.loads(sys.argv[1].replace("'", '"'))
+        coor_data = str(sys.argv[2]).replace("'", '"')
+        ships = json.loads(sys.argv[3])
+        working_dir = str(sys.argv[4])
+        coord_sys = sys.argv[5][5:]
+        precipitation = float(str(sys.argv[6]))
+        chimney_iri = str(sys.argv[7])
 
-    coordinates = str(sys.argv[2]).replace("'", '"')
-    coordinates = json.loads(coordinates)
+        BDN = admsTest.get_bdn(bdn_data)
+        coords = admsTest.get_coordinates(coor_data)
+        pollutants = [Constants.POL_CO2, Constants.POL_CO, Constants.POL_NO2, Constants.POL_HC, Constants.POL_NOX,
+                      Constants.POL_PART_001, Constants.POL_PART_SO2, Constants.POL_PART_O3]
 
-    xmax = coordinates['uppercorner']['upperx']
-    ymax = coordinates['uppercorner']['uppery']
-    xmin = coordinates['lowercorner']['lowerx']
-    ymin = coordinates['lowercorner']['lowery']
+        ship_coordinates_list = []
+        chimney_iri_list = []
 
-    coordinates['xmin'] = xmin
-    coordinates['ymin'] = ymin
-    coordinates['xmax'] = xmax
-    coordinates['ymax'] = ymax
+        for ship in ships:
+            x_coordinate_value = float(ship['lon'])
+            y_coordinate_value = float(ship['lat'])
+            ship_coordinates_list.append(list(transform(sourceCRS, targetCRS, x_coordinate_value, y_coordinate_value)))
+            chimney_iri_list.append(chimney_iri)
 
-    latitudemid = (float(ymin) + float(ymax)) / 2
-    longitudemid = (float(xmin) + float(xmax)) / 2
-    xmid, ymid = transform(targetCRS, sourceCRS, longitudemid, latitudemid)
+        test = admsInputDataRetriever(chimney_iri_list, Constants.BLD_TOPNODE, coords, pollutants, 2,
+                                      Constants.BLD_LIMIT,
+                                      False, BDN, targetCRS)
+        result = test.get()
 
-    ships = json.loads(sys.argv[3])
-    chimney_iri = str(sys.argv[7])
+        pythonLogger.postInfoToLogServer('calling admsAplWriter ...')
+        result['Bdn'] = BDN
+        result['CoordiSys'] = coord_sys
 
-    ship_coordinates_list = []
-    chimney_iri_list = []
+        latitudemid = (float(coords[Constants.KEY_MIN_Y]) + float(coords[Constants.KEY_MAX_Y])) / 2
+        longitudemid = (float(coords[Constants.KEY_MIN_X]) + float(coords[Constants.KEY_MAX_X])) / 2
+        xmid, ymid = transform(targetCRS, sourceCRS, longitudemid, latitudemid)
 
-    for ship in ships:
-        x_coordinate_value = float(ship['lon'])
-        y_coordinate_value = float(ship['lat'])
-        ship_coordinates_list.append(list(transform(sourceCRS, targetCRS, x_coordinate_value, y_coordinate_value)))
-        chimney_iri_list.append(chimney_iri)
+        result['Met'] = working_dir + '/test.met'
+        result['Lat'] = ymid
+        result['Bkg'] = working_dir + '/testbackgrnd.bgd'
 
-    workingDir = str(sys.argv[4])
+        if "2326" in sys.argv[5][5:]:
+            result['terrindicator'] = "1"
 
-    pythonLogger.postInfoToLogServer('workingDir=' + workingDir)
+        else:
+            result['terrindicator'] = "0"
 
-    test = admsInputDataRetriever(chimney_iri_list, config.bldTopnode, coordinates,
-                                  ["CO2", "CO", "NO2", "HC", "NOx", "Particulate001", "SO2", "O3"], 2, config.bdnLimit,
-                                  False, BDN, targetCRS)
-    result = test.get()
+        result['chemindicator'] = "1"
+        result['wetindicator'] = "1"
+        now = datetime.datetime.now()
+        hournow = now.hour + 1
+        if not (6 <= hournow <= 18):
+            result['night'] = "1"
+            result['dirnight'] = "C:\JPS_DATA\working_dir\JPS\ADMS\chemistrynight.AAI"
 
-    pythonLogger.postInfoToLogServer('calling admsAplWriter ...')
-    result['Bdn'] = BDN
-    result['CoordiSys'] = sys.argv[5][5:]
+        else:
+            result['night'] = "0"
+            result['dirnight'] = ""
 
-    result['Met'] = workingDir + '/test.met'
-    result['Lat'] = ymid
-    result['Bkg'] = workingDir + '/testbackgrnd.bgd'
+        annualprecipitation = precipitation * 365 * 24
+        if annualprecipitation < 103:
+            so2washout = 0.000001 / 500 * annualprecipitation
+        else:
+            so2washout = 0.0000019 + annualprecipitation * 0.0000000008
 
-    if "2326" in sys.argv[5][5:]:
-        result['terrindicator'] = "1"
+        if precipitation < 0.5:
+            pm10washout = 0.0016
+        elif precipitation > 4:
+            pm10washout = 0.0072
+        else:
+            pm10washout = 0.00363
 
-    else:
-        result['terrindicator'] = "0"
+        result['so2washout'] = so2washout
+        result['pm10washout'] = pm10washout
 
-    result['chemindicator'] = "1"
-    result['wetindicator'] = "1"
-    now = datetime.datetime.now()
-    hournow = now.hour + 1
-    if not (6 <= hournow <= 18):
-        result['night'] = "1"
-        result['dirnight'] = "C:\JPS_DATA\workingdir\JPS\ADMS\chemistrynight.AAI"
+        for idx in range(len(ship_coordinates_list)):
+            result['Src'][idx].setCoordinates(ship_coordinates_list[idx])
 
-    else:
-        result['night'] = "0"
-        result['dirnight'] = ""
+            result['Src'][idx].SrcName = "Chimney-{0}".format(idx + 1)
 
-    precipitation = float(str(sys.argv[6]))
-    annualprecipitation = precipitation * 365 * 24
-    if annualprecipitation < 103:
-        so2washout = 0.000001 / 500 * annualprecipitation
-    else:
-        so2washout = 0.0000019 + annualprecipitation * 0.0000000008
+        writer = admsAplWriter(result, working_dir + '/test.apl')
+        writer.write()
 
-    if precipitation < 0.5:
-        pm10washout = 0.0016
-    elif precipitation > 4:
-        pm10washout = 0.0072
-    else:
-        pm10washout = 0.00363
+        pythonLogger.postInfoToLogServer('end')
 
-    result['so2washout'] = so2washout
-    result['pm10washout'] = pm10washout
+    except Exception as e:
+        pythonLogger.postErrorToLogServer(e)
 
-    for idx in range(len(ship_coordinates_list)):
-        result['Src'][idx].setCoordinates(ship_coordinates_list[idx])
 
-        result['Src'][idx].SrcName = "Chimney-{0}".format(idx + 1)
-
-    writer = admsAplWriter(result, workingDir + '/test.apl')
-    writer.write()
-
-    pythonLogger.postInfoToLogServer('end')
-
-except Exception as e:
-    pythonLogger.postErrorToLogServer(e)
+if __name__ == "__main__":
+    main()
