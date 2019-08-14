fc4ac25dd6885e0c0e346ea9429ef726b8647ea1
Kevin Aditya <lkaditya1@e.ntu.edu.sg>
2019-08-14 14:13:22 +0800

    fix the processor to read chimney list and ammend the loop of ship agent

--- JPS/python/caresjpsadmsinputs/adms_processor.py
+++ JPS/python/caresjpsadmsinputs/adms_processor.py
@@ -145,7 +145,7 @@ class AdmsProcessor(object):
         if self.entity_type == Constants.ENTITY_TYPE_SHIP:
             self.targetCRS = Proj(init=args[6][:4].lower() + args[6][4:])
             self.precipitation = float(str(args[7]))
-            self.chimney_iri = json.loads(args[8])
+            self.chimney_iri = json.loads(args[8].replace("'", '"'))
 
     def get_ship_coordinates(self):
         ship_coordinates_list = []
