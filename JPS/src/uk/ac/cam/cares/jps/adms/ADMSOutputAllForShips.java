121e79c67983040696e5e46da1d7abc515fc71a1
Kevin Aditya <lkaditya1@e.ntu.edu.sg>
2019-08-16 18:20:18 +0800

    add new testing and postpocessing for visualization

--- JPS/src/uk/ac/cam/cares/jps/adms/ADMSOutputAllForShips.java
+++ JPS/src/uk/ac/cam/cares/jps/adms/ADMSOutputAllForShips.java
@@ -4,6 +4,7 @@ package uk.ac.cam.cares.jps.adms;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,22 +51,99 @@ public class ADMSOutputAllForShips extends HttpServlet {
 		
 		
 		int startcontentindex=7;
-		int sizeofpol = findHowManyPol(simulationResult, startcontentindex);  //number of polluttant (e.g:CO2,CO,NO2,..etc) with ozone and so2
-		logger.info("number of pollutant= "+sizeofpol);
+		int numpol = findHowManyPol(simulationResult, startcontentindex);  //number of polluttant (e.g:CO2,CO,NO2,..etc) with ozone and so2
+		logger.info("number of pollutant= "+numpol);
 		
-		int heightamount=(simulationResult.get(0).length-startcontentindex)/sizeofpol;//height variation level amount (e.g:0m,10m,20m,30m) currently 4
+		int heightamount=(simulationResult.get(0).length-startcontentindex)/numpol;//height variation level amount (e.g:0m,10m,20m,30m) currently 4
 		
 		
 //		TODO IS IT NEEDED IN THE FUTURE TO CHANGE THE GST PHYSICALLY????
-//		ArrayList<String[]> copier=new ArrayList<String[]>();
-//		String[]headerold=simulationResult.get(0);
-//		int size=headerold.length;
-//		String[]header=new String[size];
-//		for(int w=0;w<size;w++) {
-//			header[w]=headerold[w];
-//		}
-//		copier.add(simulationResult.get(0));
+		ArrayList<String[]> copier=new ArrayList<String[]>();
+		int totalline=simulationResult.size();
+		//hardcoded = 14,15,16 (numpol=9)
+
+
+
+		int pollostmerging = 1;
+		int newarrsize = simulationResult.get(0).length - (heightamount * pollostmerging);// 43
+
+		String[] newheader = new String[newarrsize];
+		for (int line = 0; line < startcontentindex; line++) {
+			newheader[line] = simulationResult.get(0)[line];
+		}
+		int counter1 = 0;
+		for (int heightindex = 0; heightindex < heightamount; heightindex++) { // loop 4x based on height level
+
+			for (int line1 = startcontentindex; line1 < 14; line1++) { // (col 7-13 which is normal)
+				newheader[line1 + numpol * heightindex] = simulationResult.get(0)[line1 + numpol * heightindex
+						- counter1];
+				int a = (line1 + numpol * heightindex) - counter1;
+				System.out.println("index= " + a);
+				System.out.println("a" + simulationResult.get(0)[line1 + numpol * heightindex - counter1]);
+			}
+
+			String headernamepm25 = simulationResult.get(0)[14 + numpol * heightindex];
+			newheader[14 + numpol * heightindex] = headernamepm25.replace(headernamepm25.split("\\|")[2], "PM2.5");
+			int b = 14 + numpol * heightindex;
+			System.out.println("indexb= " + b);
+			System.out.println("b" + headernamepm25.replace(headernamepm25.split("\\|")[2], "PM2.5"));
+
+			String headernamepm10 = simulationResult.get(0)[16 + numpol * heightindex];
+			newheader[15 + numpol * heightindex] = headernamepm10.replace(headernamepm10.split("\\|")[2], "PM10");
+			int c = 15 + numpol * heightindex;
+			System.out.println("indexc= " + c);
+			System.out.println("c" + headernamepm10.replace(headernamepm10.split("\\|")[2], "PM10"));
+			System.out.println("-----------------------------");
+
+			counter1 -= pollostmerging; // how many pol lost after merging (shifted in index array)
+
+		}
+		copier.add(newheader);
+
+		for (int t = 1; t < totalline; t++) { // row
+			String[] newcontent = new String[newarrsize];
+			for (int line = 0; line < startcontentindex; line++) {
+				newcontent[line] = simulationResult.get(t)[line];
+			}
+			int counter = 0;
+			for (int heightindex = 0; heightindex < heightamount; heightindex++) { // loop 4x based on height level
+
+				double pm25 = Double.valueOf(simulationResult.get(t)[14 + numpol * heightindex - counter])
+						+ Double.valueOf(simulationResult.get(t)[15 + numpol * heightindex - counter]);
+				newcontent[14 + numpol * heightindex] = "" + pm25;
+
+				double pm10 = Double.valueOf(simulationResult.get(t)[16 + numpol * heightindex - counter]) + pm25;
+				newcontent[15 + numpol * heightindex] = "" + pm10;
+
+				for (int line1 = startcontentindex; line1 < 14; line1++) { // (col 7-13 which is normal)
+					newcontent[line1 + numpol * heightindex] = simulationResult.get(t)[line1 + numpol * heightindex
+							- counter];
+				}
+
+				counter -= pollostmerging; // how many pol lost after merging
+
+			}
+			copier.add(newcontent);
+		}
 		
+		JSONArray a = new JSONArray();
+		for (int z = 0; z < 4; z++) {
+			JSONArray h = new JSONArray();
+			for (int y = 7; y < 16; y++) { // index0-index 9 for 1 pollutant
+				JSONArray pol = new JSONArray();
+				for (int x = 1; x < copier.size(); x++) { // 1-the last line
+					pol.put(copier.get(x)[y + 9*z]);
+				}
+				h.put(pol);
+			}
+			a.put(h);
+		}
+
+		
+		
+		
+		new QueryBroker().put(folder +"/testmod.levels.gst", MatrixConverter.fromArraytoCsv(copier));
+//============================================================================================		
 		
 		
 		
@@ -76,7 +155,7 @@ public class ADMSOutputAllForShips extends HttpServlet {
 		args.add("gstReader.py"); 
 		args.add(outputFile);
 		args.add(""+heightamount); 
-		args.add(""+sizeofpol);
+		args.add(""+numpol);
 		args.add("");
 		
 		String result = CommandHelper.executeCommands(targetFolder, args);
@@ -92,11 +171,25 @@ public class ADMSOutputAllForShips extends HttpServlet {
 			
 		while (Double.valueOf(simulationResult.get(0)[sizeofpol2].split("Z=")[1].split("m")[0])
 				- Double.valueOf(simulationResult.get(0)[startcontentindex].split("Z=")[1].split("m")[0]) == 0.0) {	
+			
+			String name=simulationResult.get(0)[sizeofpol2].split("\\|")[2];
+			//System.out.println(name);
 			sizeofpol2++;
-			listofpol.add(simulationResult.get(0)[sizeofpol2].split("\\|")[2]);
+			if (name.contains("PM10")) {
+				listofpol.add("PM10");
+			}
+			else if (name.contains("PM2.5")) {
+				listofpol.add("PM2.5");
+			}
+			else {
+				listofpol.add(name);
+			}
 		}
+	       List<String> newList = listofpol.stream().distinct().collect(Collectors.toList()); //remove duplication
+		
 	
-		int sizeofpol=(sizeofpol2-startcontentindex);
+		//int sizeofpol=(sizeofpol2-startcontentindex);
+		int sizeofpol=newList.size();
 		return sizeofpol;
 	}
 }
--- JPS/test/uk/ac/cam/cares/jps/config/test/TestADMSHelper.java
+++ JPS/test/uk/ac/cam/cares/jps/config/test/TestADMSHelper.java
@@ -1,7 +1,11 @@
 package uk.ac.cam.cares.jps.config.test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import org.json.JSONArray;
 
 import com.google.gson.Gson;
 
@@ -39,6 +43,7 @@ public class TestADMSHelper extends TestCase {
 		
 		
 		String csv = new QueryBroker().readFile("D:/JPS-git/JParkSimulator-git/JPS_SHIP/workingdir/test.levels.gst");
+		//String csv = new QueryBroker().readFile("D:/JPS-git/JParkSimulator-git/JPS/workingdir/ADMS/test.levels.gst");
 		List<String[]> simulationResult = MatrixConverter.fromCsvToArray(csv);
 		int startcontentindex=7;
 		int sizeofpol = new ADMSOutputAllForShips().findHowManyPol(simulationResult, startcontentindex);
@@ -47,6 +52,121 @@ public class TestADMSHelper extends TestCase {
 		assertEquals(4,heightamount);
 		assertEquals(7,sizeofpol);
 		
+	}
+	
+	public void testheader() {
+		
+		ArrayList<String[]> copier=new ArrayList<String[]>();
+		String csv = new QueryBroker().readFile("D:/JPS-git/JParkSimulator-git/JPS/workingdir/ADMS/test.levels.gst");
+		List<String[]> simulationResult = MatrixConverter.fromCsvToArray(csv);
+		
+				
+	int totalline=simulationResult.size();
+	//hardcoded = 14,15,16 (numpol=9)
+	int heightamount=4;
+	int numpol=9;
+	int startcontentindex=7;
+	int pollostmerging=1;
+	int newarrsize=simulationResult.get(0).length-(heightamount*pollostmerging);//43
+	
+		String[] newheader = new String[newarrsize];
+		for (int line = 0; line < startcontentindex; line++) {
+			newheader[line] = simulationResult.get(0)[line];
+		}
+		int counter1 = 0;
+		for (int heightindex = 0; heightindex < heightamount; heightindex++) { // loop 4x based on height level
+			
+			for (int line1 = startcontentindex; line1 < 14; line1++) { // (col 7-13 which is normal)
+				newheader[line1 + numpol * heightindex] = simulationResult.get(0)[line1 + numpol * heightindex
+						- counter1];
+				int a=(line1 + numpol * heightindex)- counter1;
+				System.out.println("index= "+a);
+				System.out.println("a"+simulationResult.get(0)[line1 + numpol * heightindex-counter1]);
+			}
+
+			String headernamepm25=simulationResult.get(0)[14 + numpol * heightindex];
+			newheader[14 + numpol * heightindex] = headernamepm25.replace(headernamepm25.split("\\|")[2],"PM2.5");
+			int b=14 + numpol * heightindex;
+			System.out.println("indexb= "+b);
+			System.out.println("b"+headernamepm25.replace(headernamepm25.split("\\|")[2],"PM2.5"));
+			
+			String headernamepm10=simulationResult.get(0)[16 + numpol * heightindex];
+			newheader[15 + numpol * heightindex] = headernamepm10.replace(headernamepm10.split("\\|")[2],"PM10");
+			int c=15 + numpol * heightindex;
+			System.out.println("indexc= "+c);
+			System.out.println("c"+headernamepm10.replace(headernamepm10.split("\\|")[2],"PM10"));
+			System.out.println("-----------------------------");
+
+			counter1 -= pollostmerging; // how many pol lost after merging
+
+		}
+		copier.add(newheader);
+
+		for (int t = 1; t < totalline; t++) { // row
+			// System.out.println("totalline= "+t);
+			String[] newcontent = new String[newarrsize];
+			for (int line = 0; line < startcontentindex; line++) {
+				newcontent[line] = simulationResult.get(t)[line];
+			}
+			int counter = 0;
+			for (int heightindex = 0; heightindex < heightamount; heightindex++) { // loop 4x based on height level
+
+				double pm25 = Double.valueOf(simulationResult.get(t)[14 + numpol * heightindex- counter])
+						+ Double.valueOf(simulationResult.get(t)[15 + numpol * heightindex- counter]);
+				newcontent[14 + numpol * heightindex] = "" + pm25;
+
+				double pm10 = Double.valueOf(simulationResult.get(t)[16 + numpol * heightindex- counter]) + pm25;
+				newcontent[15 + numpol * heightindex] = "" + pm10;
+
+				for (int line1 = startcontentindex; line1 < 14; line1++) { // (col 7-13 which is normal)
+					newcontent[line1 + numpol * heightindex] = simulationResult.get(t)[line1 + numpol * heightindex
+							- counter];
+				}
+
+				counter -= pollostmerging; // how many pol lost after merging
+
+			}
+			copier.add(newcontent);
+		}
+
+		QueryBroker broker = new QueryBroker();
+		String baseUrl = QueryBroker.getLocalDataPath() + "/JPS";
+		System.out.println(baseUrl);
+		
+		JSONArray a = new JSONArray();
+		for (int z = 0; z < 4; z++) {
+			JSONArray h = new JSONArray();
+			for (int y = 7; y < 16; y++) { // index0-index 9 for 1 pollutant
+				JSONArray pol = new JSONArray();
+				for (int x = 1; x < copier.size(); x++) { // 1-the las line
+					pol.put(copier.get(x)[y + 9*z]);
+				}
+				h.put(pol);
+			}
+			a.put(h);
+		}
+		System.out.println(a.getJSONArray(0).getJSONArray(8).toString());
+		System.out.println(a.getJSONArray(0).getJSONArray(8).length());
+		//broker.put(baseUrl + "/newresult.csv", MatrixConverter.fromArraytoCsv(copier));
+		
+		
+		
+//	double  pm251=Double.valueOf(simulationResult.get(t)[14])+Double.valueOf(simulationResult.get(t)[15]);
+//	newcontent[14]=""+pm251;
+//	double  pm101=Double.valueOf(simulationResult.get(t)[16])+pm251;
+//	newcontent[15]=""+pm101;
+//	
+//	double  pm252=Double.valueOf(simulationResult.get(t)[23])+Double.valueOf(simulationResult.get(t)[24]);
+//	newcontent[23]=""+pm251;
+//	double  pm102=Double.valueOf(simulationResult.get(t)[25])+pm251;
+//	newcontent[24]=""+pm101;
+//	
+//	double  pm253=Double.valueOf(simulationResult.get(t)[32])+Double.valueOf(simulationResult.get(t)[33]);
+//	newcontent[32]=""+pm251;
+//	double  pm103=Double.valueOf(simulationResult.get(t)[34])+pm251;
+//	newcontent[33]=""+pm101;
+	
+	
 	}
 
 }
