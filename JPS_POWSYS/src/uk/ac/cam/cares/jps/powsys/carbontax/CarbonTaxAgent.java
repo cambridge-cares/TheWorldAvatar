63ababc6ea5d26ffc578faaad98dff453d4e73c9
Kevin Aditya <lkaditya1@e.ntu.edu.sg>
2019-09-24 14:41:40 +0800

    put rough gams run code and prepare func to create csv

--- JPS_POWSYS/src/uk/ac/cam/cares/jps/powsys/carbontax/CarbonTaxAgent.java
+++ JPS_POWSYS/src/uk/ac/cam/cares/jps/powsys/carbontax/CarbonTaxAgent.java
@@ -1,7 +1,10 @@
 package uk.ac.cam.cares.jps.powsys.carbontax;
 
+import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,7 +17,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import uk.ac.cam.cares.jps.base.discovery.AgentCaller;
+import uk.ac.cam.cares.jps.base.query.JenaResultSetFormatter;
+import uk.ac.cam.cares.jps.base.query.QueryBroker;
 import uk.ac.cam.cares.jps.base.scenario.JPSHttpServlet;
+import uk.ac.cam.cares.jps.base.util.CommandHelper;
+import uk.ac.cam.cares.jps.base.util.MatrixConverter;
+import uk.ac.cam.cares.jps.powsys.nuclear.IriMapper;
 
 @WebServlet(urlPatterns = { "/optimizeforcarbontax" })
 public class CarbonTaxAgent extends JPSHttpServlet {
@@ -44,4 +52,86 @@ public class CarbonTaxAgent extends JPSHttpServlet {
 		
 		AgentCaller.printToResponse(result, response);
 	}
+	
+	public void runGAMS() throws IOException, InterruptedException {
+        System.out.println("Start");
+        System.out.println("separator= "+File.separator);
+        String executablelocation ="C:/GAMS/win64/26.1/gams.exe";
+        //String folderlocation ="D:/Users/KADIT01/Documents/gamsdir/projdir/";
+        String folderlocation ="C:/JPS_DATA/workingdir/JPS_POWSYS/";
+        String[] cmdArray = new String[5];
+        
+        cmdArray[0] = executablelocation;
+        cmdArray[1] = folderlocation + "final.gms";
+        cmdArray[2] = "WDIR="+folderlocation;
+        cmdArray[3] = "SCRDIR="+folderlocation;
+        cmdArray[4] = "LO=2";
+//      cmdArray[2] = "WDIR="+folderlocation + "TMP";
+//      cmdArray[3] = "SCRDIR="+folderlocation + "TMP";
+        
+        String cmdArrayinstring=cmdArray[0]+" "+cmdArray[1]+","+cmdArray[2]+","+cmdArray[3]+" "+cmdArray[4];
+        
+		//System.out.println(cmdArrayinstring);
+        //Process p = Runtime.getRuntime().exec(cmdArray);
+		   //p.waitFor();
+		String startbatCommand ="C:/JPS_DATA/workingdir/JPS_POWSYS/gamsexecute.bat";
+		
+		ArrayList<String> groupcommand= new ArrayList<String>();
+		groupcommand.add("start");
+		groupcommand.add("C:/JPS_DATA/workingdir/JPS_POWSYS/gamsexecute.bat");
+		
+		CommandHelper.executeSingleCommand(folderlocation,startbatCommand);
+//		CommandHelper.executeCommands(folderlocation, groupcommand);   
+        System.out.println("Done");
+	}
+	
+	public void prepareCSVGeneratorParameter(String ENiri, String baseUrl) {		
+
+		String genInfo = "PREFIX j1:<http://www.theworldavatar.com/ontology/ontopowsys/PowSysRealization.owl#> "
+				+ "PREFIX j2:<http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#> "
+				+ "PREFIX j3:<http://www.theworldavatar.com/ontology/ontopowsys/model/PowerSystemModel.owl#> "
+				+ "PREFIX j4:<http://www.theworldavatar.com/ontology/meta_model/topology/topology.owl#> "
+				+ "PREFIX j5:<http://www.theworldavatar.com/ontology/ontocape/model/mathematical_model.owl#> "
+				+ "PREFIX j6:<http://www.theworldavatar.com/ontology/ontocape/chemical_process_system/CPS_behavior/behavior.owl#> "
+				+ "PREFIX j7:<http://www.theworldavatar.com/ontology/ontocape/supporting_concepts/space_and_time/space_and_time_extended.owl#> "
+				+ "PREFIX j8:<http://www.theworldavatar.com/ontology/ontocape/material/phase_system/phase_system.owl#> "
+				+ "SELECT ?entity ?plant ?Pmaxvalue "
+
+				+ "WHERE {?entity  a  j1:PowerGenerator  ."
+				+ "?entity   j2:isSubsystemOf ?plant ." //plant
+				+ "?entity   j2:isModeledBy ?model ."
+				+ "?model   j5:hasModelVariable ?pmax ." 
+				+ "?pmax  a  j3:PMax  ." 
+				+ "?pmax  j2:hasValue ?vpmax ."
+				+ "?vpmax   j2:numericalValue ?Pmaxvalue ." // pmax
+
+				+ "}";
+   
+		QueryBroker broker = new QueryBroker();
+		
+    	String result = broker.queryFile(ENiri, genInfo);
+    	String[] keys = {"entity", "Pmaxvalue", "plant"};
+    	List<String[]> resultList = JenaResultSetFormatter.convertToListofStringArrays(result, keys);
+    	
+		//logger.info("number of queried lot entities = " + resultList.size());
+    	
+    	IriMapper mapper= new IriMapper();
+		for (int i=0; i<resultList.size(); i++) {
+			String[] current = resultList.get(i);
+			String id = "s"+(i+1);
+			mapper.add(current[0], id, "lot");
+			current[0]=id;
+		}
+    
+		String csv = mapper.serialize();
+	    broker.put(baseUrl + "/mappingforlot.csv", csv);
+	    
+	    String[] header = {"id","ys","xs","as","dcs"};
+	    resultList.add(0, header);
+	    String s = MatrixConverter.fromArraytoCsv(resultList);
+	    broker.put(baseUrl + "/inputlandlots.csv", s);
+	    
+	    logger.info("landlots input ok"); 
+	}
+	
 }
