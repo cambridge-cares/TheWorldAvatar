e00757dcad0c0cfbb2eadeaf9ee566c1084e2017
msff2 <msff2@cam.ac.uk>
2019-11-18 07:27:34 +0000

    dev-nist-data-agent: Modified species HTML reader and parser methods.

--- thermo/como-nistwebbook/src/main/java/org/cam/ceb/como/nist/webbook/parser/NISTParser.java
+++ thermo/como-nistwebbook/src/main/java/org/cam/ceb/como/nist/webbook/parser/NISTParser.java
@@ -19,6 +19,7 @@ import java.util.logging.Logger;
 public abstract class NISTParser {
 
     protected String path = "";
+    protected String title = new String();
     protected StringList body = new StringList();
     protected Object data = new Object();
 
@@ -26,8 +27,27 @@ public abstract class NISTParser {
         this.path = path;
     }
 
-    public void parse() {
-        body = null;
+    public void parse(){
+    	parseTitle();
+    	parseBody();
+    }
+    
+    /**
+     * Parses title to extract the name of the current species.
+     */
+    private void parseTitle() {
+    	title = null;
+        try {
+            title = extractTitle();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(NISTGasPhaseThermoChemReader.class.getName()).log(Level.SEVERE, "The file " + path + "does not exist.", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(NISTGasPhaseThermoChemReader.class.getName()).log(Level.SEVERE, "The file " + path + "could not be read.", ex);
+        }
+    }
+    
+    private void parseBody() {
+    	body = null;
         try {
             body = extractBody();
         } catch (FileNotFoundException ex) {
@@ -39,9 +59,19 @@ public abstract class NISTParser {
             parseSection(body);
         }
     }
-    
-    public abstract Object get();
 
+    public abstract Object get();
+    /**
+     * Extracts the title of the current HTML file.
+     * 
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public String extractTitle() throws FileNotFoundException, IOException {
+        return NISTHTMLReaderHelper.extractHTMLTitle(new File(path));
+    }
+    
     public StringList extractBody() throws FileNotFoundException, IOException {
         return NISTHTMLReaderHelper.extractHTMLBody(new File(path));
     }
