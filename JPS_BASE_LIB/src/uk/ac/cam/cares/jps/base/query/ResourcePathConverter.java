55340e584175dbb42ee46b04cc54333231ac1b43
long <laura.ong@cares.cam.ac.uk>
2020-01-31 09:40:18 +0800

    Revert "attempted convert fix as suggested by Andreas. OPF run with new results, but carbon emission didn't change. Wait later."
    This reverts commit 2ca92dd13d3d6ca8ad39ecdeba480dc80245e189.

--- JPS_BASE_LIB/src/uk/ac/cam/cares/jps/base/query/ResourcePathConverter.java
+++ JPS_BASE_LIB/src/uk/ac/cam/cares/jps/base/query/ResourcePathConverter.java
@@ -2,6 +2,7 @@ package uk.ac.cam.cares.jps.base.query;
 
 import java.net.URI;
 
+import uk.ac.cam.cares.jps.base.config.AgentLocator;
 import uk.ac.cam.cares.jps.base.config.KeyValueManager;
 import uk.ac.cam.cares.jps.base.discovery.AgentCaller;
 import uk.ac.cam.cares.jps.base.log.JPSBaseLogger;
@@ -22,7 +23,7 @@ public class ResourcePathConverter {
 		//TODO-AE SC URGENT 20191021 CHANGE BACK this will work on claudius but not anymore locally --> configurable solution?
 		// maybe change back not necessary any more, because of the solution below
 		
-		if (true) {			
+		if (!AgentLocator.isJPSRunningForTest()) {			
 			return path;
 		}
 	
