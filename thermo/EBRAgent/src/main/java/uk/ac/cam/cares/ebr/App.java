8a5a0afeb2ac5c122353ff1de3ef21d95a0ad507
msff2 <msff2@cam.ac.uk>
2020-02-07 00:48:24 +0000

    dev-refine-thermochemistry-nenad: Created the EBRAgent project.

---
+++ thermo/EBRAgent/src/main/java/uk/ac/cam/cares/ebr/App.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.cares.ebr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
