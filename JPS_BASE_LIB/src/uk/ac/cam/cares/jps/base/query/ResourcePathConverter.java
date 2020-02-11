505dc1f9df2836033d20d087bfcaf1567e03973a
Kevin Aditya <lkaditya1@e.ntu.edu.sg>
2020-02-11 09:48:37 +0800

    change the settings of resourcepathconverter to be in claudius settings and change local setting of properties

--- JPS_BASE_LIB/src/uk/ac/cam/cares/jps/base/query/ResourcePathConverter.java
+++ JPS_BASE_LIB/src/uk/ac/cam/cares/jps/base/query/ResourcePathConverter.java
@@ -23,8 +23,7 @@ public class ResourcePathConverter {
 		//TODO-AE SC URGENT 20191021 CHANGE BACK this will work on claudius but not anymore locally --> configurable solution?
 		// maybe change back not necessary any more, because of the solution below
 		
-		if ((path.startsWith("C:")) || (!AgentLocator.isJPSRunningForTest())) {
-						JPSBaseLogger.info(getInstance(),path);
+		if (!AgentLocator.isJPSRunningForTest()) {
 				return path;
 		}
 	
