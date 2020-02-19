b2ba51c8650693938c6b6583366bddd3ad470092
nk510 <nk510@cam.ac.uk>
2020-02-19 18:35:06 +0800

    develop: Included modified test data (csv files) and pom files.

--- thermo/CoMoNISTWebbook/src/main/java/uk/ac/cam/ceb/como/nist/info/NISTSpeciesId.java
+++ thermo/CoMoNISTWebbook/src/main/java/uk/ac/cam/ceb/como/nist/info/NISTSpeciesId.java
@@ -16,13 +16,40 @@ public class NISTSpeciesId {
     private String name = "";
     private String formula = "";
     private String CASRegNr = "";
+    
+    /**
+     * 
+     * @author NK510 (caresssd@hermes.cam.ac.uk)
+     * NISTSpeciesId is extended with identifier, bond and geometry as species attributes (properties)
+     * 
+     */
 
+    private String identifier="";
+    private String bond="";
+    private String geometry="";
+    
     public NISTSpeciesId(String name, String formula, String CASRegNr) {
         this.name = name;
         this.formula = formula;
         this.CASRegNr = CASRegNr;
     }
 
+
+    /**
+     * @author NK510 (caresssd@hermes.cam.ac.uk)
+     * 
+     * @param identifier identifier created during generating species owl file 
+     * @param CASRegNr the CAS registry number
+     * @param bond the bond
+     * @param geometry the geometry
+     */
+    public NISTSpeciesId(String identifier, String CASRegNr, String bond, String geometry) {
+        this.identifier=identifier;
+        this.CASRegNr = CASRegNr;
+        this.bond=bond;
+        this.geometry=geometry;
+    }
+    
     public String getName() {
         return name;
     }
@@ -34,4 +61,32 @@ public class NISTSpeciesId {
     public String getCASRegNr() {
         return CASRegNr;
     }
+
+    
+    /**
+     * @author NK510 (caresssd@hermes.cam.ac.uk)
+     * @return the identifier
+     */
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+
+	/**
+	 * @author NK510 (caresssd@hermes.cam.ac.uk)
+	 * @return the bond
+	 */
+	public String getBond() {
+		return bond;
+	}
+
+
+	/**
+	 * @author NK510 (caresssd@hermes.cam.ac.uk)
+	 * @return the geometry
+	 */
+	public String getGeometry() {
+		return geometry;
+	}
 }
