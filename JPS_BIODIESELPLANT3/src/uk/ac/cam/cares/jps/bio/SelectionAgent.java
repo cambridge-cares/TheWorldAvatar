527c9fb876516aed2c2d9cec5d462c3ab8a35908
Kevin Aditya <lkaditya1@e.ntu.edu.sg>
2020-03-02 16:23:11 +0800

    add new agent structure to seek the model based on the equipment component provided

---
+++ JPS_BIODIESELPLANT3/src/uk/ac/cam/cares/jps/bio/SelectionAgent.java
@@ -0,0 +1,64 @@
+package uk.ac.cam.cares.jps.bio;
+
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.query.ResultSet;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.cam.cares.jps.base.query.JenaHelper;
+import uk.ac.cam.cares.jps.base.query.JenaResultSetFormatter;
+import uk.ac.cam.cares.jps.base.query.QueryBroker;
+import uk.ac.cam.cares.jps.base.scenario.JPSHttpServlet;
+
+
+
+@WebServlet("/DoModelSelection")
+public class SelectionAgent extends JPSHttpServlet {
+	
+    @Override
+    protected void setLogger() {
+        logger = LoggerFactory.getLogger(SelectionAgent.class);
+    }
+    Logger logger = LoggerFactory.getLogger(SelectionAgent.class);
+    
+    
+    @Override
+   	protected JSONObject processRequestParameters(JSONObject requestParams, HttpServletRequest request) {
+		String topnode="http://www.theworldavatar.com/kb/sgp/jurongisland/JurongIsland.owl#JurongIsland";
+		
+    	String compIRI=requestParams.getString("componentIRI");
+    	OntModel model=readModelGreedy(topnode);
+    	String plantlistInfo = "PREFIX j1:<http://www.theworldavatar.com/ontology/ontoeip/ecoindustrialpark/EcoIndustrialPark.owl#> "
+				+ "PREFIX j2:<http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#> "
+				+ "SELECT ?plant ?model "
+				+ "WHERE {?plant  a  j2:CompositeSystem  ."
+				+ "?plant j2:isModeledBy ?model ."
+				+ "?plant   j2:hasSubsystem <"+compIRI+"> ." 
+				+ "}";
+    	
+    	ResultSet resultSet = JenaHelper.query(model, plantlistInfo);
+		String result = JenaResultSetFormatter.convertToJSONW3CStandard(resultSet);
+        String[] keysplant = JenaResultSetFormatter.getKeys(result);
+        List<String[]> resultList = JenaResultSetFormatter.convertToListofStringArrays(result, keysplant);
+    	
+    	
+    	return requestParams;	
+    }
+    
+    
+	public static OntModel readModelGreedy(String iriofEIP) {
+		String plantlistInfo = "PREFIX j1:<http://www.theworldavatar.com/ontology/ontoeip/ecoindustrialpark/EcoIndustrialPark.owl#> "
+				+ "PREFIX j2:<http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#> "
+				+ "SELECT ?component "
+				+ "WHERE {?entity  a  j1:Eco-industrialPark  ." + "?entity   j2:hasSubsystem ?component ." + "}";
+
+		QueryBroker broker = new QueryBroker();
+		return broker.readModelGreedy(iriofEIP, plantlistInfo);
+	}
+}
