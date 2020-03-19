5586a0fec5f27e30ccb9e1caabf3ea8fc3996c59
lkaditya <lkaditya1@e.ntu.edu.sg>
2020-03-19 18:38:29 +0800

    put new test for the selection agent and make the agent itself working

--- JPS_BIODIESELPLANT3/test/uk/ac/cam/cares/bio/test/Test_DoSimulation.java
+++ JPS_BIODIESELPLANT3/test/uk/ac/cam/cares/bio/test/Test_DoSimulation.java
@@ -151,53 +151,5 @@ public class Test_DoSimulation extends TestCase{
 		   new DoSimulation().placeinOWLFiles(jo, "http://theworldavatar.com/kb/sgp/jurongisland/biodieselplant3/BiodieselPlant3.owl");
 	   }
 	   
-		public static OntModel readModelGreedy(String iriofEIP) {
-			String plantlistInfo = "PREFIX j1:<http://www.theworldavatar.com/ontology/ontoeip/ecoindustrialpark/EcoIndustrialPark.owl#> "
-					+ "PREFIX j2:<http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#> "
-					+ "SELECT ?component "
-					+ "WHERE {?entity  a  j2:CompositeSystem  ." + "?entity   j2:hasSubsystem ?component ." + "}";
 
-			QueryBroker broker = new QueryBroker();
-			return broker.readModelGreedy(iriofEIP, plantlistInfo);
-		}
-		
-		
-		
-	   public void testQuery() {
-		   String compIRI="http://www.jparksimulator.com/kb/sgp/jurongisland/biodieselplant3/R-301.owl#R-301";
-	    	String componentlistInfo = "PREFIX j1:<http://www.theworldavatar.com/ontology/ontoeip/ecoindustrialpark/EcoIndustrialPark.owl#> "
-					+ "PREFIX j2:<http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#> "
-					+ "SELECT ?plant "
-					+ "WHERE {"
-					+ "?place j2:hasSubsystem ?plant ."
-					+ "?place  a  j1:Eco-industrialPark ."
-					+ "}";
-		   String topnode="http://www.theworldavatar.com/kb/TheWorld.owl#TheWorld";
-	    	String plantInfo = "PREFIX j1:<http://www.theworldavatar.com/ontology/ontoeip/ecoindustrialpark/EcoIndustrialPark.owl#> "
-					+ "PREFIX j2:<http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#> "
-					+ "SELECT ?plant ?path "
-					+ "WHERE {"
-					+ "?plant a  j2:CompositeSystem ."
-					+ "?plant j2:isModeledBy ?model ."
-					+ "?model j2:hasURLPath ?path ."
-					+ "?plant   j2:hasSubsystem <"+compIRI+"> ." 
-					+ "}";
-		   
-		   OntModel model=readModelGreedy(topnode);
-		   ResultSet resultSet = JenaHelper.query(model, componentlistInfo);
-			String result = JenaResultSetFormatter.convertToJSONW3CStandard(resultSet);
-	        String[] keysplant = JenaResultSetFormatter.getKeys(result);
-	        List<String[]> resultList = JenaResultSetFormatter.convertToListofStringArrays(result, keysplant);
-	        System.out.println("what is resultList size= "+resultList.size());
-	        for(int d=0;d<resultList.size();d++) {
-	        	//OntModel model2=readModelGreedy(resultList.get(d)[0]);
-				String result2 = new QueryBroker().queryFile(resultList.get(d)[0], plantInfo);
-				String[] keysplant2 = JenaResultSetFormatter.getKeys(result2);
-				List<String[]> resultList2 = JenaResultSetFormatter.convertToListofStringArrays(result2, keysplant2);
-		        if(resultList2.size()>0) {
-		        System.out.println("what is the plant= "+resultList2.get(0)[0]);
-		        System.out.println("what is the path= "+resultList2.get(0)[1]);
-		        }
-	        }
-	   }
 }
