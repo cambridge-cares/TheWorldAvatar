a07da2557dbdcc77f011fd56dd8dd2e0e12c73b5
Feroz Farazi <msff2@cam.ac.uk>
2020-03-20 19:45:12 +0000

    slurm-job-agent: Updated JUnit Test.

--- thermo/DFTAgent/src/test/java/com/cmclinnovations/jps/agent/quantum/calculation/DFTAgentJobRequestTest.java
+++ thermo/DFTAgent/src/test/java/com/cmclinnovations/jps/agent/quantum/calculation/DFTAgentJobRequestTest.java
@@ -5,6 +5,9 @@ import java.io.UnsupportedEncodingException;
 
 import org.junit.Test;
 
+import com.cmclinnovations.slurm.job.SlurmJobException;
+import com.cmclinnovations.slurm.job.Status;
+
 import org.junit.Assert;
 
 public class DFTAgentJobRequestTest {
@@ -14,13 +17,15 @@ public class DFTAgentJobRequestTest {
 			String query = "{\"job\":{\"levelOfTheory\":\"B3LYP/6-31G(d)\",\"keyword\": \"Opt\",\"algorithmChoice\": \"Freq\"},\"speciesIRI\": \"http://www.theworldavatar.com/kb/ontospecies/00b7e248-ae24-35bf-b7a0-b470b923ddf6.owl#00b7e248-ae24-35bf-b7a0-b470b923ddf6\"}";
 			DFTAgent dftAgent = new DFTAgent();
 			String messageActual = dftAgent.setUpJob(query);
-			Assert.assertEquals(messageActual, Jobs.JOB_SETUP_SUCCESS_MSG.getName());
+			Assert.assertEquals(messageActual, Status.JOB_SETUP_SUCCESS_MSG.getName());
 			System.out.println(messageActual);
 		} catch (DFTAgentException e) {
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SlurmJobException e) {
 			e.printStackTrace();
 		}
 	}
