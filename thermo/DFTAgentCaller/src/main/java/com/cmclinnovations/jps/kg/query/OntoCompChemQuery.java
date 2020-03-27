8a6d247c2ac8d7f07d5e1ac5a298f0f8b2452eb8
Feroz Farazi <msff2@cam.ac.uk>
2020-03-27 16:41:53 +0000

    dev-run-dft-calculations: Extended the OntoSpciesQuery class by adding a constructor and the queryOntoSpciesKG method.

--- thermo/DFTAgentCaller/src/main/java/com/cmclinnovations/jps/kg/query/OntoCompChemQuery.java
+++ thermo/DFTAgentCaller/src/main/java/com/cmclinnovations/jps/kg/query/OntoCompChemQuery.java
@@ -1,11 +1,13 @@
 package com.cmclinnovations.jps.kg.query;
 
+import com.cmclinnovations.jps.agent.caller.AgentCaller;
+
 /**
  * Queries the JPS OntoCompChem knowledge graph.
  * 
  * @author msff2
  *
  */
-public class OntoCompChemQuery {
+public class OntoCompChemQuery extends AgentCaller{
 
 }
--- thermo/DFTAgentCaller/src/main/java/com/cmclinnovations/jps/kg/query/OntoSpeciesQuery.java
+++ thermo/DFTAgentCaller/src/main/java/com/cmclinnovations/jps/kg/query/OntoSpeciesQuery.java
@@ -1,11 +1,42 @@
 package com.cmclinnovations.jps.kg.query;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.cmclinnovations.jps.agent.caller.AgentCaller;
+import com.cmclinnovations.jps.agent.caller.DFTAgentCallerException;
+import com.cmclinnovations.jps.agent.caller.Utils;
+import com.cmclinnovations.jps.agent.caller.configuration.AgentCallerConfiguration;
+import com.cmclinnovations.jps.agent.caller.configuration.DFTAgentCallerProperty;
+
 /**
  * Queries the JPS OntoSpecies knowledge graph.
  * 
  * @author msff2
  *
  */
-public class OntoSpeciesQuery {
-
+public class OntoSpeciesQuery extends AgentCaller{
+	/**
+	 * The default constructor of this class.
+	 */
+	public OntoSpeciesQuery(){
+        if (applicationContext == null) {
+			applicationContext = new AnnotationConfigApplicationContext(AgentCallerConfiguration.class);
+		}
+		if (dftAgentCallerProperty == null) {
+			dftAgentCallerProperty = applicationContext.getBean(DFTAgentCallerProperty.class);
+		}
+	}
+	
+	public HashSet<String> queryOntoSpciesKG() throws DFTAgentCallerException{
+		List<String> endpoints = Utils.getEndpoints(dftAgentCallerProperty.getEndpointOntoSpecies());
+		if(endpoints == null){
+			throw new DFTAgentCallerException("DFTAgentCaller: endpoints are not correctly formatted.");
+		}
+		return null;
+	}
 }
