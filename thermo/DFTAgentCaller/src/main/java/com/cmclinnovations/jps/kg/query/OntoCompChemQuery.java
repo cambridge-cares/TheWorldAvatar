eb9475b1f50ae7feb768e446355ac21417b6a5ce
Feroz Farazi <msff2@cam.ac.uk>
2020-03-27 15:14:49 +0000

    dev-run-dft-calculations: Created the OntoCompChem and OntoSpecies query classes.

---
+++ thermo/DFTAgentCaller/src/main/java/com/cmclinnovations/jps/kg/query/OntoCompChemQuery.java
@@ -0,0 +1,11 @@
+package com.cmclinnovations.jps.kg.query;
+
+/**
+ * Queries the JPS OntoCompChem knowledge graph.
+ * 
+ * @author msff2
+ *
+ */
+public class OntoCompChemQuery {
+
+}
---
+++ thermo/DFTAgentCaller/src/main/java/com/cmclinnovations/jps/kg/query/OntoSpeciesQuery.java
@@ -0,0 +1,11 @@
+package com.cmclinnovations.jps.kg.query;
+
+/**
+ * Queries the JPS OntoSpecies knowledge graph.
+ * 
+ * @author msff2
+ *
+ */
+public class OntoSpeciesQuery {
+
+}
