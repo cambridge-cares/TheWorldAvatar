9d62fc9ce94ad43df25e80f94e94dd4098f59e33
Feroz Farazi <msff2@cam.ac.uk>
2020-03-30 18:23:25 +0100

    dev-run-dft-calculations: Added methods for reading properties from the dft-agent-caller.properties file.

--- thermo/DFTAgentCaller/src/main/java/com/cmclinnovations/jps/agent/caller/configuration/DFTAgentCallerProperty.java
+++ thermo/DFTAgentCaller/src/main/java/com/cmclinnovations/jps/agent/caller/configuration/DFTAgentCallerProperty.java
@@ -13,11 +13,67 @@ public class DFTAgentCallerProperty {
 	@Value("${sparql.endpoint.ontocompchem}")
 	private String endpointOntoCompChem;
 
+	@Value("${job.property.label}")
+	private String jobPropertyLabel;
+
+	@Value("${job.levelOfTheory.property.label}")
+	private String jobLevelOfTheoryPropertyLabel;
+
+	@Value("${job.levelOfTheory}")
+	private String jobLevelOfTheory;
+
+	@Value("${job.keyword.property.label}")
+	private String jobKeywordPropertyLabel;
+
+	@Value("${job.keyword}")
+	private String jobKeyword;
+
+	@Value("${job.algorithmChoice.property.label}")
+	private String jobAlgorithmChoicePropertyLabel;
+
+	@Value("${job.algorithmChoice}")
+	private String jobAlgorithmChoice;
+
+	@Value("${job.speciesIRI.property.label}")
+	private String jobSpeciesIRIPropertyLabel;
+	
 	public String getEndpointOntoSpecies() {
 		return endpointOntoSpecies;
 	}
 
 	public String getEndpointOntoCompChem() {
 		return endpointOntoCompChem;
+	}
+
+	public String getJobPropertyLabel() {
+		return jobPropertyLabel;
+	}
+
+	public String getJobLevelOfTheoryPropertyLabel() {
+		return jobLevelOfTheoryPropertyLabel;
+	}
+
+	public String getJobLevelOfTheory() {
+		return jobLevelOfTheory;
+	}
+
+	public String getJobKeywordPropertyLabel() {
+		return jobKeywordPropertyLabel;
+	}
+
+	public String getJobKeyword() {
+		return jobKeyword;
+	}
+
+	public String getJobAlgorithmChoicePropertyLabel() {
+		return jobAlgorithmChoicePropertyLabel;
+	}
+
+	public String getJobAlgorithmChoice() {
+		return jobAlgorithmChoice;
+	}
+
+	public String getJobSpeciesIRIPropertyLabel() {
+		return jobSpeciesIRIPropertyLabel;
 	}
 }
