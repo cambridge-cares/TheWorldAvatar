954872b6ae4742773469fec04c00b282fecca36e
Feroz Farazi <msff2@cam.ac.uk>
2020-03-30 14:06:40 +0100

    dev-run-dft-calculations: Created the unique species query methods in the OntoCompChemQuery class.

--- thermo/DFTAgentCaller/src/main/java/com/cmclinnovations/jps/kg/query/OntoCompChemQuery.java
+++ thermo/DFTAgentCaller/src/main/java/com/cmclinnovations/jps/kg/query/OntoCompChemQuery.java
@@ -1,6 +1,15 @@
 package com.cmclinnovations.jps.kg.query;
 
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.cmclinnovations.jps.agent.caller.AgentCaller;
+import com.cmclinnovations.jps.agent.caller.DFTAgentCallerException;
+import com.cmclinnovations.jps.agent.caller.Utils;
+import com.cmclinnovations.jps.agent.caller.configuration.AgentCallerConfiguration;
+import com.cmclinnovations.jps.agent.caller.configuration.DFTAgentCallerProperty;
 
 /**
  * Queries the JPS OntoCompChem knowledge graph.
@@ -9,5 +18,31 @@ import com.cmclinnovations.jps.agent.caller.AgentCaller;
  *
  */
 public class OntoCompChemQuery extends AgentCaller{
-
+ public OntoCompChemQuery(){
+	 if(applicationContext == null){
+		 applicationContext = new AnnotationConfigApplicationContext(AgentCallerConfiguration.class);
+	 }
+	 if(dftAgentCallerProperty == null){
+		 dftAgentCallerProperty = applicationContext.getBean(DFTAgentCallerProperty.class);
+	 }
+ }
+ 
+ public Set<String> queryOntoCompChemKG() throws DFTAgentCallerException, Exception{
+	 List<String> endpoints = Utils.getEndpoints(dftAgentCallerProperty.getEndpointOntoCompChem());
+	 if(endpoints == null){
+		 throw new DFTAgentCallerException("DFTAgentCaller: ontocompcehm endpoints are not formatted correctly.");
+	 }
+	 KnowledgeGraphQuery kgQuery = new KnowledgeGraphQuery(endpoints, formSpeciesCalculationQuery());
+	 return kgQuery.performQuery();
+ }
+ 
+ private String formSpeciesCalculationQuery(){
+	 String query = "PREFIX ontocompchem: <http://www.theworldavatar.com/ontology/ontocompchem/ontocompchem.owl#>\n"
+			 + "SELECT ?uniqueSpecies \n"
+			 + "WHERE { \n"
+			 + "?calculation ontocompchem:hasUniqueSpecies ?uniqueSpecies . \n"
+			 + "}";
+	 return query;
+ }
+ 
 }
