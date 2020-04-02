d4e3b10362b6a93e1ae0fdf77c686daf76174fc8
Feroz Farazi <msff2@cam.ac.uk>
2020-04-02 21:19:37 +0100

    add-estimated-eof: Created the enthalpy of formation upload class.

---
+++ thermo/EoFUpload/src/main/java/com/cmclinnovations/jps/species/eof/EoFUpload.java
@@ -0,0 +1,18 @@
+package com.cmclinnovations.jps.species.eof;
+
+/**
+ * EBR Agent estimates the enthalpy of formation of a set of given target<br>
+ * species. This class uploads the estimated enthalpy of formation of those<br>
+ * species to the OntoSpcies knowledge graph. 
+ * 
+ * @author msff2
+ *
+ */
+public class EoFUpload {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
