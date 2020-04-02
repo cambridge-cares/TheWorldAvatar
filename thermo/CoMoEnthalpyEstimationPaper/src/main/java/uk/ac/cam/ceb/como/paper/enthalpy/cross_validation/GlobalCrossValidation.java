0bcbecbf2cb0f7ad3ee93cc0e55fddd80a301c80
nk510 <nk510@cam.ac.uk>
2020-04-02 16:30:05 +0800

    ebr-agent-nenad: Reads json as input. Unzipped input file (G09, log,csv) specified in json as file name. Runs global cross validation algorithm and zipped folder that contains the generated output files of the algorithm. Tested on Cares HPC machine.

--- thermo/CoMoEnthalpyEstimationPaper/src/main/java/uk/ac/cam/ceb/como/paper/enthalpy/cross_validation/GlobalCrossValidation.java
+++ thermo/CoMoEnthalpyEstimationPaper/src/main/java/uk/ac/cam/ceb/como/paper/enthalpy/cross_validation/GlobalCrossValidation.java
@@ -17,8 +17,8 @@ public class GlobalCrossValidation {
 	public static void main(String[] args) throws JSONException, Exception {
 	// TODO Auto-generated method stub
 		
-//		String jText = new String(Files.readAllBytes(Paths.get(new File(args[0]).getAbsolutePath())), StandardCharsets.UTF_8);		
-//		JSONObject jObject = new JSONObject(jText);	        
+		String jText = new String(Files.readAllBytes(Paths.get(new File(args[0]).getAbsolutePath())), StandardCharsets.UTF_8);		
+		JSONObject jObject = new JSONObject(jText);	        
 //      String folderName = new FolderUtils().generateUniqueFolderName(jObject.get("destRList").toString());
         
 		/**
@@ -26,10 +26,12 @@ public class GlobalCrossValidation {
          * Find input.zip and unzip it before running global cross validation. File path is given in josn file "input.zip".
          *  
          */
-//      LeaveOneOutCrossValidationAlgorithm leaveOneOutCrossValidationAlgorithm = new LeaveOneOutCrossValidationAlgorithm();
-//      leaveOneOutCrossValidationAlgorithm.runGlobalCrossValidation(jObject.get("srcCompoundsRef").toString()+"/", jObject.get("srcRefPool").toString(), jObject.get("destRList").toString(), EvaluationUtils.getCtrRuns(jObject.get("ctrRuns").toString()), EvaluationUtils.getCtrRuns(jObject.get("ctrRes").toString()), EvaluationUtils.getCtrRuns(jObject.get("ctrRadicals").toString()),EvaluationUtils.getReactionType(jObject.get("reactionType").toString()), jObject.get("tempFolder").toString()+"/");
+	  FileUtils.getUnzipFolder(jObject.getString("inputZipFile"));
+	  
+      LeaveOneOutCrossValidationAlgorithm leaveOneOutCrossValidationAlgorithm = new LeaveOneOutCrossValidationAlgorithm();
+      leaveOneOutCrossValidationAlgorithm.runGlobalCrossValidation(jObject.get("srcCompoundsRef").toString()+"/", jObject.get("srcRefPool").toString(), jObject.get("destRList").toString(), EvaluationUtils.getCtrRuns(jObject.get("ctrRuns").toString()), EvaluationUtils.getCtrRuns(jObject.get("ctrRes").toString()), EvaluationUtils.getCtrRuns(jObject.get("ctrRadicals").toString()),EvaluationUtils.getReactionType(jObject.get("reactionType").toString()), jObject.get("tempFolder").toString()+"/");
 
-		FileUtils.getUnzipFolder("C:/Users/NK/Documents/philipp/180-pb556/input.zip");
-		
+//		FileUtils.getUnzipFolder("C:/Users/NK/Documents/philipp/180-pb556/input.zip");
+
 	}
 }
