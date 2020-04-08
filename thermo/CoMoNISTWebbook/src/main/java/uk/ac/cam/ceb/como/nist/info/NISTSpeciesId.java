b870619d83be7fe60f80aa06c6a5eb6a39f4e6a0
nk510 <nk510@cam.ac.uk>
2020-04-08 21:08:36 +0800

    ebr-agent-nenad: modified the code that stores data about species from NIST.

--- thermo/CoMoNISTWebbook/src/main/java/uk/ac/cam/ceb/como/nist/info/NISTSpeciesId.java
+++ thermo/CoMoNISTWebbook/src/main/java/uk/ac/cam/ceb/como/nist/info/NISTSpeciesId.java
@@ -9,78 +9,107 @@ package uk.ac.cam.ceb.como.nist.info;
  * @author pb556
  */
 public class NISTSpeciesId {
-    // species name
-    // species formula
-    // CAS registry number (if known)
+	// species name
+	// species formula
+	// CAS registry number (if known)
 
-    private String name = "";
-    private String formula = "";
-    private String CASRegNr = "";
-    
-    /**
-     * 
-     * @author NK510 (caresssd@hermes.cam.ac.uk)
-     * NISTSpeciesId is extended with identifier, bond, geometry, scf energy, zero point energy as species attributes (properties)
-     * 
-     */
+	private String name = "";
+	private String formula = "";
+	private String CASRegNr = "";
 
-    private String identifier="";
-    private String bond="";
-    private String geometry="";
-    
-    private String scfEnergy="";
-    private String zeroPointEnergy="";
-    
-    private String standardEnthalpyOfFormation="";
-    
-    
-    public NISTSpeciesId(String name, String formula, String CASRegNr) {
-        this.name = name;
-        this.formula = formula;
-        this.CASRegNr = CASRegNr;
-    }
+	/**
+	 * 
+	 * @author NK510 (caresssd@hermes.cam.ac.uk) NISTSpeciesId is extended with
+	 *         identifier, bond, geometry, scf energy, zero point energy as species
+	 *         attributes (properties)
+	 * 
+	 */
 
+	private String identifier = "";
+	private String bond = "";
+	private String geometry = "";
 
-    /**
-     * @author NK510 (caresssd@hermes.cam.ac.uk)
-     * 
-     * @param identifier identifier created during generating species owl file 
-     * @param CASRegNr the CAS registry number
-     * @param bond the bond
-     * @param geometry the geometry
-     */
-    public NISTSpeciesId(String identifier, String CASRegNr, String bond, String geometry, String standardEnthalpyOfFormation, String scfEnergy, String zeroPointEnergy) {
-        this.identifier=identifier;
-        this.CASRegNr = CASRegNr;
-        this.bond=bond;
-        this.geometry=geometry;
-        this.scfEnergy=scfEnergy;
-        this.zeroPointEnergy=zeroPointEnergy;
-        this.standardEnthalpyOfFormation=standardEnthalpyOfFormation;
-    }
-    
-    public String getName() {
-        return name;
-    }
+	private String scfEnergy = "";
+	private String zeroPointEnergy = "";
 
-    public String getFormula() {
-        return formula;
-    }
+	private String standardEnthalpyOfFormation = "";
 
-    public String getCASRegNr() {
-        return CASRegNr;
-    }
+	public NISTSpeciesId(String CASRegNr) {
+		this.CASRegNr = CASRegNr;
+	}
 
-    
-    /**
-     * @author NK510 (caresssd@hermes.cam.ac.uk)
-     * @return the identifier
-     */
+	
+	public NISTSpeciesId(String name, String formula, String CASRegNr) {
+		this.name = name;
+		this.formula = formula;
+		this.CASRegNr = CASRegNr;
+	}
+
+	/**
+	 * @author NK510 (caresssd@hermes.cam.ac.uk)
+	 * 
+	/**
+	 * @param identifier the identifier
+	 * @param CASRegNr the cas registry id
+	 * @param bond the bond
+	 * @param geometry  the geometry
+	 * @param standardEnthalpyOfFormation  the standard enthalpy of formation
+	 * @param scfEnergy the scf energy
+	 * @param zeroPointEnergy the zero point energy
+	 */
+	public NISTSpeciesId(String identifier, String CASRegNr, String bond, String geometry,
+			String standardEnthalpyOfFormation, String scfEnergy, String zeroPointEnergy) {
+		this.identifier = identifier;
+		this.CASRegNr = CASRegNr;
+		this.bond = bond;
+		this.geometry = geometry;
+		this.scfEnergy = scfEnergy;
+		this.zeroPointEnergy = zeroPointEnergy;
+		this.standardEnthalpyOfFormation = standardEnthalpyOfFormation;
+	}
+
+	/**
+	 * @author NK510 (caresssd@hermes.cam.ac.uk)
+	 * 	
+	/**
+	 * @param CASRegNr the cas registry id
+	 * @param bond the bond
+	 * @param geometry the geometry
+	 * @param standardEnthalpyOfFormation the standard enthalpy of formation
+	 * @param scfEnergy the scf energy
+	 * @param zeroPointEnergy the zero point energy
+	 */
+	public NISTSpeciesId(String CASRegNr, String bond, String geometry, String standardEnthalpyOfFormation,
+			String scfEnergy, String zeroPointEnergy) {
+
+		this.CASRegNr = CASRegNr;
+		this.bond = bond;
+		this.geometry = geometry;
+		this.scfEnergy = scfEnergy;
+		this.zeroPointEnergy = zeroPointEnergy;
+		this.standardEnthalpyOfFormation = standardEnthalpyOfFormation;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getFormula() {
+		return formula;
+	}
+
+	public String getCASRegNr() {
+		return CASRegNr;
+	}
+
+	/**
+	 * @author NK510 (caresssd@hermes.cam.ac.uk)
+	 * @return the identifier
+	 */
 
 	public String getIdentifier() {
 		return identifier;
 	}
-
 
 	/**
 	 * @author NK510 (caresssd@hermes.cam.ac.uk)
@@ -90,7 +119,6 @@ public class NISTSpeciesId {
 		return bond;
 	}
 
-
 	/**
 	 * @author NK510 (caresssd@hermes.cam.ac.uk)
 	 * @return the geometry
@@ -98,7 +126,6 @@ public class NISTSpeciesId {
 	public String getGeometry() {
 		return geometry;
 	}
-
 
 	/**
 	 * @author NK510 (caresssd@hermes.cam.ac.uk)
@@ -108,7 +135,6 @@ public class NISTSpeciesId {
 		return scfEnergy;
 	}
 
-
 	/**
 	 * @author NK510 (caresssd@hermes.cam.ac.uk)
 	 * @param scfEnergy
@@ -116,7 +142,6 @@ public class NISTSpeciesId {
 	public void setScfEnergy(String scfEnergy) {
 		this.scfEnergy = scfEnergy;
 	}
-
 
 	/**
 	 * @author NK510 (caresssd@hermes.cam.ac.uk)
@@ -126,7 +151,6 @@ public class NISTSpeciesId {
 		return zeroPointEnergy;
 	}
 
-
 	/**
 	 * @author NK510 (caresssd@hermes.cam.ac.uk)
 	 * @param zeroPointEnergy
@@ -135,11 +159,9 @@ public class NISTSpeciesId {
 		this.zeroPointEnergy = zeroPointEnergy;
 	}
 
-
 	public String getStandardEnthalpyOfFormation() {
 		return standardEnthalpyOfFormation;
 	}
-
 
 	public void setStandardEnthalpyOfFormation(String standardEnthalpyOfFormation) {
 		this.standardEnthalpyOfFormation = standardEnthalpyOfFormation;
