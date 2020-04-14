a0a682673a8eab1c0122bd802c282b9a6445308a
Feroz Farazi <msff2@cam.ac.uk>
2020-04-14 23:16:40 +0100

    citychem: Created CityChem class and extended the pom file.

---
+++ JPS_CITY_CHEM/src/main/java/uk/ac/cam/cares/jps/episode/CityChem.java
@@ -0,0 +1,25 @@
+package uk.ac.cam.cares.jps.episode;
+
+public class CityChem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * Default constructor.
+	 */
+	public CityChem(){
+		
+	}
+	
+	public void install(){
+		
+	}
+	
+	private void installZlib(){
+		
+	}
+	
+
+}
