d9e6d183abecdd77b07f0cd85f343b77a5672a4f
Feroz Farazi <msff2@cam.ac.uk>
2020-04-24 13:52:00 +0100

    ebr-agent-x-validation-or-estimation: Decided name for the common class that will call either the cross-validation code or eof calculation code.

---
+++ thermo/CoMoEnthalpyEstimationPaper/src/main/java/uk/ac/ca/ceb/como/paper/ebr/EBRApproach.java
@@ -0,0 +1,17 @@
+package uk.ac.ca.ceb.como.paper.ebr;
+
+/**
+ * This class reads user requirements from a JSON input file to decide<br>
+ * whether to perform cross-validation or the enthalpy of formation calculation.
+ * 
+ * @author msff2
+ *
+ */
+public class EBRApproach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
