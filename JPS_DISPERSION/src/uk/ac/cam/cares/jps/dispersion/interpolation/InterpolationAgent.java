0a8e58f641be089563038862bb5bd3e1a3c9693c
Laura Ong <laura.ong@cares.cam.ac.uk>
2020-05-04 16:39:31 +0800

    Added notifyWatcher and url

--- JPS_DISPERSION/src/uk/ac/cam/cares/jps/dispersion/interpolation/InterpolationAgent.java
+++ JPS_DISPERSION/src/uk/ac/cam/cares/jps/dispersion/interpolation/InterpolationAgent.java
@@ -1,9 +1,152 @@
 package uk.ac.cam.cares.jps.dispersion.interpolation;
 
-public class InterpolationAgent {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.jena.ontology.OntModel;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.cam.cares.jps.base.config.AgentLocator;
+import uk.ac.cam.cares.jps.base.config.KeyValueMap;
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.query.QueryBroker;
+import uk.ac.cam.cares.jps.base.scenario.JPSHttpServlet;
+@WebServlet({"/InterpolationAgent/startSimulation", "/InterpolationAgent/continueSimulation"})
+public class InterpolationAgent  extends JPSHttpServlet {
+	public String SIM_START_PATH = "/InterpolationAgent/startSimulation";
+	public String SIM_PROCESS_PATH = "/InterpolationAgent/startSimulation";
+	
+	private static final long serialVersionUID = 1L;
+	public static final String KEY_WATCH = "watch";
+	public static final String KEY_CALLBACK_URL = "callback";
+    protected void setLogger() {
+        logger = LoggerFactory.getLogger(InterpolationAgent.class);
+    }
+    /**
+     *  create logger to log changes attached to WasteToEnergyAgent class. 
+     */
+    protected Logger logger = LoggerFactory.getLogger(InterpolationAgent.class);
+		
 	public InterpolationAgent() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	/** main function. Reads the values in and copies the templates back. 
+	 * 
+	 */
+	@Override
+	protected JSONObject processRequestParameters(JSONObject requestParams, HttpServletRequest request) {
+		String path = request.getServletPath();
+		String baseUrl= requestParams.getString("folder");
+		String wasteIRI=requestParams.getString("wastenetwork");
+		//copy over data from iri to base folder
+		//copy over data 
+		copyTemplate(baseUrl, "Interpolation_gas_updated.m");
+		//modify matlab to read 
+		if (SIM_START_PATH.equals(path)) {
+			try {
+				createBat(baseUrl);
+				runModel(baseUrl);
+	            notifyWatcher(requestParams, baseUrl+"/number of units (onsite).csv",
+	                    request.getRequestURL().toString().replace(SIM_START_PATH, SIM_PROCESS_PATH));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		 }else if (SIM_PROCESS_PATH.equals(path)) {
+			 try {
+				 //update respective OWL FILES
+			 }catch (Exception e) {
+				e.printStackTrace();
+			}			 
+		 }
+		return requestParams;
+	}
+	/** notifies the watcher to return with the callback. 
+	 * 
+	 * @param agentArgs JSONObject
+	 * @param filePath String
+	 * @param callbackIRI String
+	 */
+    private void notifyWatcher(JSONObject agentArgs, String filePath, String callbackIRI) {
+        agentArgs.put(KEY_WATCH, filePath);
+        agentArgs.put(KEY_CALLBACK_URL, callbackIRI);
+        execute(KeyValueMap.getInstance().get("url.jps_aws"), agentArgs.toString(), HttpPost.METHOD_NAME);
+    }
+	/** copies over the files in the working directory over to scenario folder. 
+	 * 
+	 * @param newdir
+	 * @param filename
+	 */
+	private void copyTemplate(String newdir, String filename) { //in this case for SphereDist.m; Main.m; D2R.m
+		File file = new File(AgentLocator.getCurrentJpsAppDirectory(this) + "/workingdir/"+filename);
+		
+		String destinationUrl = newdir + "/"+filename;
+		new QueryBroker().putLocal(destinationUrl, file);
+	}
+	/** create the batch file in the mentioned folder. 
+	 * 
+	 * @param baseUrl
+	 * @throws Exception
+	 */
+	public void createBat(String baseUrl) throws Exception {
+		String loc = baseUrl + "\\Main.m";
+		String bat = "setlocal" + "\n" + "cd /d %~dp0" + "\n" + "matlab -nosplash -noFigureWindows -r \"try; run('"
+				+ loc + "'); catch; end; quit\"";
+		new QueryBroker().putLocal(baseUrl + "/runm.bat", bat);
+	}
+	/** runs the batch file. 
+	 * 
+	 * @param baseUrl
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public void runModel(String baseUrl) throws IOException, InterruptedException {
+		String startbatCommand =baseUrl+"/runm.bat";
+		String result= executeSingleCommand(baseUrl,startbatCommand);
+		logger.info("final after calling: "+result);
+	}
+	/** executes the process. Called by runModel. 
+	 * 
+	 * @param targetFolder
+	 * @param command
+	 * @return
+	 * @throws InterruptedException
+	 */
+	private String executeSingleCommand(String targetFolder , String command) throws InterruptedException 
+	{  
+	 
+		logger.info("In folder: " + targetFolder + " Executed: " + command);
+		Runtime rt = Runtime.getRuntime();
+		Process pr = null;
+		try {
+			pr = rt.exec(command, null, new File(targetFolder)); // IMPORTANT: By specifying targetFolder, all the cmds will be executed within such folder.
+			pr.waitFor();
+		} catch (IOException e) {
+			throw new JPSRuntimeException(e.getMessage(), e);
+		}
+		
+				 
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+		String line = "";
+		String resultString = "";
+		try {
+			
+			while((line = bfr.readLine()) != null) {
+				resultString += line;
 
+			}
+		} catch (IOException e) {
+			throw new JPSRuntimeException(e.getMessage(), e);
+		}
+		return resultString; 
+	}
 }
