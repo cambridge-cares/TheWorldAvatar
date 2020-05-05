62197f26ac65f5efece6f86db89e3937462bc627
Feroz Farazi <msff2@cam.ac.uk>
2020-05-05 14:50:29 +0100

    ontocompchem-upload-extension-nenad: Created a JUnit test to upload a calculation without any OntoSpecies entry.

--- thermo/OntoCompChemUpload/src/test/java/com/cmclinnovations/jps/upload/test/CompChemUploadTest2.java
+++ thermo/OntoCompChemUpload/src/test/java/com/cmclinnovations/jps/upload/test/CompChemUploadTest2.java
@@ -5,24 +5,22 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.cmclinnovations.jps.upload.CompChemUpload;
-
 /**
- * This unit test uploads a calculation with a link established to the<br>
- * corresponding OntoSpcies entry. 
+ * This JUnit test uploads a calculation without establishing a link to the<br>
+ * corresponding OntoSpecies entry.
  * 
  * @author msff2
  *
  */
-public class CompChemUploadTest1 {
+public class CompChemUploadTest2 {
 
 	@Test
-	public void test(){
+	public void test() {
 		try{
 		CompChemUpload compChemUpload = new CompChemUpload();
 		compChemUpload.setCalculationFileName("login-skylake.hpc.cam.ac.uk_113420049902577.log");
 		System.out.println(getClass().getClassLoader().getResource("login-skylake.hpc.cam.ac.uk_113420049902577.log").getPath());
 		compChemUpload.setCalculationFilePath(getClass().getClassLoader().getResource("login-skylake.hpc.cam.ac.uk_113420049902577.log").getPath());
-		compChemUpload.setOntoSpeciesIRI("http://www.theworldavatar.com/kb/ontospecies/3ad49265-0d58-3827-ba0f-18201693f82b.owl#3ad49265-0d58-3827-ba0f-18201693f82b");
 		compChemUpload.upload();
 		}catch(Exception e){
 			fail(e.getMessage());
