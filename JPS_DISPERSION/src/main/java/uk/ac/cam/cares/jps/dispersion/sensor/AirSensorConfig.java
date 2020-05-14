bca2e6fca1bc7a9b145eb66115e40ec49e9cd537
Feroz Farazi <msff2@cam.ac.uk>
2020-05-14 04:54:52 +0100

    virtual_sensor_merging: Moved files to src/main/java.

--- JPS_DISPERSION/src/main/java/uk/ac/cam/cares/jps/dispersion/sensor/AirSensorConfig.java
+++ JPS_DISPERSION/src/main/java/uk/ac/cam/cares/jps/dispersion/sensor/AirSensorConfig.java
@@ -1,45 +1,3 @@
-<<<<<<< HEAD:JPS_DISPERSION/src/main/java/uk/ac/cam/cares/jps/dispersion/sensor/AirSensorConfig.java
-package uk.ac.cam.cares.jps.dispersion.sensor;
-
-public class AirSensorConfig {
-private int numberOfDataSlot=169; //not 1month
-private String sensorXLocation="103.859";
-private String sensorYLocation="1.27993";
-private String sensorZLocation="2";
-
-public int getNumberOfDataSlot() {
-	return numberOfDataSlot;
-}
-
-public void setNumberOfDataSlot(int numberOfDataSlot) {
-	this.numberOfDataSlot = numberOfDataSlot;
-}
-
-public String getSensorYLocation() {
-	return sensorYLocation;
-}
-
-public void setSensorYLocation(String sensorYLocation) {
-	this.sensorYLocation = sensorYLocation;
-}
-
-public String getSensorXLocation() {
-	return sensorXLocation;
-}
-
-public void setSensorXLocation(String sensorXLocation) {
-	this.sensorXLocation = sensorXLocation;
-}
-
-public String getSensorZLocation() {
-	return sensorZLocation;
-}
-
-public void setSensorZLocation(String sensorZLocation) {
-	this.sensorZLocation = sensorZLocation;
-}
-}
-=======
 package uk.ac.cam.cares.jps.dispersion.sensor;
 
 public class AirSensorConfig {
@@ -80,4 +38,3 @@ public void setSensorZLocation(String sensorZLocation) {
 	this.sensorZLocation = sensorZLocation;
 }
 }
->>>>>>> c4013b6c58790c38177c62add14e48cfeb449296:JPS_DISPERSION/src/uk/ac/cam/cares/jps/dispersion/sensor/AirSensorConfig.java
