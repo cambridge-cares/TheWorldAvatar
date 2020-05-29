be83603c81bf2f083023ebf352a0700fee293f5f
Feroz Farazi <msff2@cam.ac.uk>
2020-05-29 11:17:33 +0100

    develop: Added files which were deleted before.

---
+++ thermo/CoMoIOChemistry/src/test/java/uk/ac/cam/ceb/como/thermo/partition_function/rotation/internal/hindered/potential/discretisation/TopDescriptionGeneration.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.cam.ceb.como.thermo.partition_function.rotation.internal.hindered.potential.discretisation;
+
+/**
+ * Generation of a descriptor for the tops defined by a torsional bond.
+ * 
+ * @author pb556
+ */
+public class TopDescriptionGeneration {
+    
+//    @Test
+//    public void descriptorTest() throws Exception {
+//        String src = "W:\\Data\\TTIP\\hr\\cml\\freq-hr-fine-h-species-0029-radical-1.cml";
+//        // extraction from the cml/g09 file
+//        CMLHRFileParser parser = new CMLHRFileParser(new File(src));
+//        parser.parse();
+//        CompChem cc = parser.getCompChem();
+//        IRCompChemWrapper ccw = new IRCompChemWrapper(cc);
+//        RotationalModes rotModes = ccw.getRotationalModes(true);
+//        SMILESBuilder builder = new SMILESBuilder();
+//        for (RotationalMode m : rotModes) {
+//            try {
+//                System.out.println(builder.build(m.getTop().asCMLMolecule()));
+//            } catch (Exception e) {
+//                System.out.println();
+//            }
+//        }
+//    }
+}
