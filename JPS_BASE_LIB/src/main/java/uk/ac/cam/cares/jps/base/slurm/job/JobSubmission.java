c305170c37e5c304c97465e6e319969dc24d7116
Feroz Farazi <msff2@cam.ac.uk>
2020-06-07 23:17:14 +0100

    fix-slurm-script-line-ending: Modified the logic not to run more than one job at a time if the property file gap between two jobs is greater than zero.

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/slurm/job/JobSubmission.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/slurm/job/JobSubmission.java
@@ -511,11 +511,10 @@ public class JobSubmission{
 							}
 						} else if(Utils.isJobNotStarted(jobFolder) && !jobsRunning.contains(jobFolder.getName())){
 							if(jobsRunning.size()<Property.MAX_NUMBER_OF_JOBS.getValue()){
-								if(jobsRunning.size()>=1){
-									TimeUnit.MINUTES.sleep(slurmJobProperty.getDelayBetweenConsecutiveJobs());
+								if(!(slurmJobProperty.getDelayBetweenConsecutiveJobs()> 0 && jobsRunning.size()>=1)){
+									runNotStartedJobs(jobFolder);
+									jobsRunning.add(jobFolder.getName());
 								}
-								runNotStartedJobs(jobFolder);
-								jobsRunning.add(jobFolder.getName());
 							}else{
 								break;
 							}
