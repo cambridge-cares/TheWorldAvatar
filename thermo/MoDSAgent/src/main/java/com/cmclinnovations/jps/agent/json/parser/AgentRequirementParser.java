abb8a0e3cd97b2f77dbf3d4f0338a3ce28a93ca1
Jiaru Bai <jb2197@cam.ac.uk>
2020-06-29 00:49:00 +0100

    dev-pode-auto: created parser for parsing agent requirements.

---
+++ thermo/MoDSAgent/src/main/java/com/cmclinnovations/jps/agent/json/parser/AgentRequirementParser.java
@@ -0,0 +1,13 @@
+package com.cmclinnovations.jps.agent.json.parser;
+
+import com.jayway.jsonpath.JsonPath;
+
+public class AgentRequirementParser {
+	public static String getNumberOfCores(String jsonString) {
+		return JsonPath.read(jsonString, "$.ontoagent.core.value");
+	}
+	
+	public static String getRAMSize(String jsonString) {
+		return JsonPath.read(jsonString, "$.ontoagent.memReq.value");
+	}
+}
