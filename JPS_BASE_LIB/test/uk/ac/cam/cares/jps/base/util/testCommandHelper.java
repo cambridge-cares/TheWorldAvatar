18c8d31cf3835bfb85391e2c21f3c976bb1ffa64
adevanand <adevanand@users.noreply.github.com>
2020-07-02 12:45:52 +0530

    Windows compatible

--- JPS_BASE_LIB/test/uk/ac/cam/cares/jps/base/util/testCommandHelper.java
+++ JPS_BASE_LIB/test/uk/ac/cam/cares/jps/base/util/testCommandHelper.java
@@ -1,58 +1,64 @@
 package uk.ac.cam.cares.jps.base.util;
 
-import org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import uk.ac.cam.cares.jps.base.util.CommandHelper;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+
+import java.io.File;
 import java.util.ArrayList;
 import static org.mockito.Mockito.*;
-
 
 class testCommandHelper {
 
 	CommandHelper commandHelper;
-	
+
 	@BeforeEach
-	public void init(){
-			commandHelper= new CommandHelper();
-		}
+	public void init() {
+		commandHelper = new CommandHelper();
+	}
 
 	@Test
-		
-		public void testExecuteSingleCommand() {
+
+	public void testExecuteSingleCommand() {
 		String expected = "test.txttest2";
-		String actual = commandHelper.executeSingleCommand(System.getProperty("user.dir")+"/test_sample_dir", "ls");
-	    assertThrows(JPSRuntimeException.class, () ->commandHelper.executeSingleCommand("sd", "sdsds"),"Unknown exception thrown, expected JPS runtime exception");
-	    //doReturn("foo").when(spy).executeSingleCommand(System.getProperty("user.dir")+"/test_sample_dir", "ls");
-		assertEquals(expected,actual,"The command was not executed correctly");
-		
+		String actual = null;
+		if (commandHelper.isWindows()) {
+			actual = commandHelper.executeSingleCommand(System.getProperty("user.dir") + "/test_sample_dir", "dir");
+		} else if (commandHelper.isMac()) {
+			actual = commandHelper.executeSingleCommand(System.getProperty("user.dir") + "/test_sample_dir", "ls");
 		}
-	
+		assertThrows(JPSRuntimeException.class, () -> commandHelper.executeSingleCommand("sd", "sdsds"),
+				"Unknown exception thrown, expected JPS runtime exception");
+		assertEquals(expected, actual, "The command was not executed correctly");
+
+	}
+
 	@Test
 	public void testexecuteCommands() {
 		ArrayList<String> cmds = new ArrayList<String>();
-		cmds.add("ls");
-	  // cmds.add("cd test2");  
-	  // cmds.add("ls");  
-	String expected = "test.txttest2";
-	String actual = commandHelper.executeCommands(System.getProperty("user.dir")+"/test_sample_dir", cmds);
-    assertThrows(JPSRuntimeException.class, () ->commandHelper.executeCommands("sd", cmds),"Unknown exception thrown, expected JPS runtime exception");
-	assertEquals(expected,actual,"The command was not executed correctly");
-	
+		if (commandHelper.isWindows()) {
+			cmds.add("dir");
+		} else if (commandHelper.isMac()) {
+			cmds.add("ls");
+		}
+		String expected = "test.txttest2";
+		String actual = commandHelper.executeCommands(System.getProperty("user.dir") + "/test_sample_dir", cmds);
+		assertThrows(JPSRuntimeException.class, () -> commandHelper.executeCommands("sd", cmds),
+				"Unknown exception thrown, expected JPS runtime exception");
+		assertEquals(expected, actual, "The command was not executed correctly");
+
 	}
-	
+
 	@Test
 	public void testexecuteAsyncSingleCommand() {
-	String expected = "";
-	String actual = commandHelper.executeAsyncSingleCommand(System.getProperty("user.dir")+"/test_sample_dir", "test.txt");
-    assertThrows(JPSRuntimeException.class, () ->commandHelper.executeAsyncSingleCommand("sd", "sdsds"),"Unknown exception thrown, expected JPS runtime exception");
-	assertEquals(expected,actual,"The command was not executed correctly");
-	
-	}
-	
+		String expected = "";
+		String actual = commandHelper.executeAsyncSingleCommand(System.getProperty("user.dir") + "/test_sample_dir",
+				"test.txt");
+		assertThrows(JPSRuntimeException.class, () -> commandHelper.executeAsyncSingleCommand("sd", "sdsds"),
+				"Unknown exception thrown, expected JPS runtime exception");
+		assertEquals(expected, actual, "The command was not executed correctly");
+
 	}
 
-
+}
