806ff8edc3f9fbf666d289f71248aa97feb84516
Feroz Farazi <msff2@cam.ac.uk>
2020-07-06 20:01:58 +0100

    blaze-graph-crud-operations: Added a JUnit class for the KnowledgeBaseClient class.

---
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/KnowledgeBaseClientTest.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.cares.jps.base.query.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import uk.ac.cam.cares.jps.base.discovery.MediaType;
+import uk.ac.cam.cares.jps.base.query.KnowledgeBaseClient;
+import uk.ac.cam.cares.jps.base.query.SparqlOverHttpService.RDFStoreType;
+
+public class KnowledgeBaseClientTest {
+
+}
