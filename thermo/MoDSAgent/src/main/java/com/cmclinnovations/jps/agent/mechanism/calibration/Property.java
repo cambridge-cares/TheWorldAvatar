6e437bb26414236a14c0d20937021fe1414c2c82
Jiaru Bai <jb2197@cam.ac.uk>
2020-07-15 00:26:38 +0100

    mods-agent-cantera: updated list of important Property of MoDS agent.

--- thermo/MoDSAgent/src/main/java/com/cmclinnovations/jps/agent/mechanism/calibration/Property.java
+++ thermo/MoDSAgent/src/main/java/com/cmclinnovations/jps/agent/mechanism/calibration/Property.java
@@ -36,10 +36,11 @@ public enum Property {
 	PREFIX_RAM("ram"),
 	MODS_AGENT_IRI("<http://www.theworldavatar.com/kb/agents/Service__DFT.owl#Service>"), // double check
 	MODEL_KINETICS("kineticsSRM"),
-	MODEL_KINETICS_PATH("/home/jb2197/Codes_kinetics/mods-backend/Applications/MoDS/bin/runKineticsSRM.sh"),
+	MODEL_KINETICS_EXE("/home/jb2197/Codes_kinetics/mods-backend/Applications/MoDS/bin/runKineticsSRM.sh"),
 	MODEL_KINETICS_OUTPUT("OutputCase00001Cyc0001Info.csv"),
 	MODEL_CANTERA("canteraLFS"),
-	MODEL_CANTERA_PATH("/home/jb2197/Codes_kinetics/cantera/cantera_LFS.py"),
+	MODEL_CANTERA_EXE("python"),
+	MODEL_CANTERA_ARGS("/home/jb2197/Codes_kinetics/cantera/cantera_LFS.py"),
 	MODEL_CANTERA_OUTPUT("OutputCase00001Lfs0001Info.csv");
 	
 	private String propertyName;
