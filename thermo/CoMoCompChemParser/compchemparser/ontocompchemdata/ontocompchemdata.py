ef63ae849173b76f72474d62ad47f9395ab7d681
Daniel Nurkowski <danieln@cmclinnovations.com>
2020-07-18 20:47:28 +0100

    On 88-dev-modify-gaussian-parser-read: dln22

--- thermo/CoMoCompChemParser/compchemparser/ontocompchemdata/ontocompchemdata.py
+++ thermo/CoMoCompChemParser/compchemparser/ontocompchemdata/ontocompchemdata.py
@@ -1,25 +1,29 @@
-class OntoCompChemdData:
+class OntoCompChemData:
 
     def __init__(self):
         self.log = ''
+        self.parser = None
         self.data = {}
 
-    def getData(self, logFile, parser, postprocessor):
+    def setParser(self, parser):
+        self.parser = parser
+
+    def getData(self, logFile):
         """ 
            parser         - Gaussian or Molpro
            postprocessor  - Arkane at the moment
            
         """
         self.log = logFile
-        self.data = {}        
+        self.data = {}    
 
-        parser.parseLog(self.log, self.data)
+        self.parser.parseLog(self.log)
 
-        self.checkParsedData()
+        #self.checkParsedData()
 
-        postprocessor.run(self.log, self.data)
+        #postprocessor.run(self.log, self.data)
 
-        self.checkPostProcessedData()
+        #self.checkPostProcessedData()
 
         #1 load arkane templates
         #2 create input files (streams if possible)
