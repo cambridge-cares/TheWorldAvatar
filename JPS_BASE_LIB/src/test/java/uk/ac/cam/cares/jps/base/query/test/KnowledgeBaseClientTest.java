c32c016c3b56d17aab035bc679480272a2069277
Feroz Farazi <msff2@cam.ac.uk>
2020-07-20 22:05:11 +0100

    blaze-graph-crud-operations: Developed code to convert Blazegraph output into JSON.

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/KnowledgeBaseClientTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/KnowledgeBaseClientTest.java
@@ -11,24 +11,25 @@ import uk.ac.cam.cares.jps.base.query.SparqlOverHttpService.RDFStoreType;
 public class KnowledgeBaseClientTest {
 	@Test
 	public void uploadSingleOntologyTest() throws Exception {
-//		 KnowledgeBaseClient.uploadOntology("http://localhost:8080/blazegraph", "ontokin", "C:/Users/msff2/Documents/c4e-ProciPaper/ACSOmega/OpenData/ABF.owl");
+//		 new KnowledgeBaseClient().uploadOntology("http://localhost:8080/blazegraph", "ontokin", "C:/Users/msff2/Documents/c4e-ProciPaper/ACSOmega/OpenData/ABF.owl");
 	}
 	
 	@Test
 	public void uploadOntologiesTest() throws Exception {
-//		KnowledgeBaseClient.uploadOntologies("http://localhost:8080/blazegraph", "ontokin", "C:/Users/msff2/Documents/c4e-ProciPaper/ACSOmega/OpenDataTest");
+//		new KnowledgeBaseClient().uploadOntologies("http://localhost:8080/blazegraph", "ontokin", "C:/Users/msff2/Documents/c4e-ProciPaper/ACSOmega/OpenDataTest");
 	}
 	
 	@Test
 	public void queryNumberOfMechanisms() throws Exception{
-//		String result = KnowledgeBaseClient.query("http://localhost:8080/blazegraph", "ontokin", RDFStoreType.BLAZEGRAPH, formMechanismCountCountQuery());
+//		String result = new KnowledgeBaseClient().query("http://localhost:8080/blazegraph", "ontokin", RDFStoreType.BLAZEGRAPH, formMechanismCountCountQuery());
 //		System.out.println(result);
 	}
 	
 	@Test
 	public void queryAllMechanismIRIs() throws Exception{
-//		String result = KnowledgeBaseClient.query("http://localhost:8080/blazegraph", "ontokin", RDFStoreType.BLAZEGRAPH, formMechanismIRIsQuery());
-//		System.out.println(result);
+		KnowledgeBaseClient kbClient = new KnowledgeBaseClient();
+		String result = kbClient.query("http://localhost:8080/blazegraph", "ontokin", RDFStoreType.BLAZEGRAPH, formMechanismIRIsQuery());
+		System.out.println(result);
 	}
 	
 	/**
@@ -55,11 +56,11 @@ public class KnowledgeBaseClientTest {
 	private static String formMechanismIRIsQuery(){
 		String query = "PREFIX ontokin: <http://www.theworldavatar.com/kb/ontokin/ontokin.owl#>\n";
 			query = query.concat("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n");
-			query = query.concat("SELECT ?x \n");
+			query = query.concat("SELECT ?x ?y ?z \n");
 			query = query.concat("WHERE\n");
 			query = query.concat("{\n");
-			query = query.concat("?x rdf:type ontokin:ReactionMechanism .\n");
-			query = query.concat("}\n");
+			query = query.concat("?x ?y ?z .\n");
+			query = query.concat("} LIMIT 10\n");
 			return query;
 	}
 }
