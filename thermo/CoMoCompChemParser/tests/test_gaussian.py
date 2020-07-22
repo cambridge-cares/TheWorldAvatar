099a68e952071b544beaf74220970aecc8e3b5de
Daniel Nurkowski <danieln@cmclinnovations.com>
2020-07-22 23:55:44 +0100

    88-dev-modify-gaussian-parser-read: Commiting somehow missed changes for the last commit.

--- thermo/CoMoCompChemParser/tests/test_gaussian.py
+++ thermo/CoMoCompChemParser/tests/test_gaussian.py
@@ -1,42 +1,52 @@
 import unittest
 from compchemparser.parsers.ccgaussian_parser import CcGaussianParser
 import json
+import os
 
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 parser = CcGaussianParser()
+
+Test_suite1 = {'g1': ['co2_g1_g09.log'],
+               'g2': ['co2_g2_g09.log', 'co2_g2mp2_g09.log'],
+               'linked': ['co2_linked_g09.log'],
+               'cas': ['co2_cas_g09.log', 'co2_casmp2.log'],
+               'dft': ['TEOS_ub971_6311+gdp.log'],
+}
+
+
 
 class TestGaussianParser(unittest.TestCase):
 
-    def test_g1(self):
-        test_path = './tests/gaussian/g1/'
-        test_name = 'Gaussian G1 method'
-        filename = 'co2_g1_g09.log'
-        json_ref_name = filename.replace('.log','.json')
+    def test_suite1(self):
+        test_path = os.path.join(THIS_DIR, 'gaussian')
 
-        print("Test " + test_name+", File: "+filename)
-        json_data = parser.parse(test_path+filename)[0]
+        for method, logs in Test_suite1.items():
+            for log_file in logs:
+                test_name = 'Gaussian '+method
+                file_path = os.path.join(test_path, method, log_file)
+                print("Test " + test_name+", File: "+log_file)
 
-        #with open(test_path+json_ref_name, 'w') as outfile:
-        #    json.dump(json_data, outfile)
-        
-        with open(test_path+json_ref_name) as ref_file:
-            ref_data = json.load(ref_file)
-        self.assertEqual(json_data, ref_data)
+                test_data_list = parser.parse(file_path)
+                for i, test_data in enumerate(test_data_list):
+                    test_data = json.loads(test_data)
 
-    def test_cas(self):
-        test_path = './tests/gaussian/cas/'
-        test_name = 'Gaussian CASSCF method'
-        filename = 'co2_cas_g09.log'
-        json_ref_name = filename.replace('.log','.json')
-
-        print("Test " + test_name+", File: "+filename)
-        json_data = parser.parse(test_path+filename)[0]
-
-        #with open(test_path+json_ref_name, 'w') as outfile:
-        #    json.dump(json_data, outfile)
-        
-        with open(test_path+json_ref_name) as ref_file:
-            ref_data = json.load(ref_file)
-        self.assertEqual(json_data, ref_data)
+                    if len(test_data_list) > 1:
+                        ref_name = log_file.replace('.log','#'+str(i+1)+'.json')
+                    else:
+                        ref_name = log_file.replace('.log','.json')
+                    ref_path = os.path.join(test_path, method, ref_name)
+                    # uncomment to generate ref json file
+                    #---------------------------------------------------
+                    with open(ref_path, 'w') as outfile:
+                        json.dump(test_data, outfile, indent = 4)
+                    #---------------------------------------------------
+                    
+                    with open(ref_path) as ref_file:
+                        ref_data = json.load(ref_file)
+                    
+                    for key in test_data.keys():
+                        self.assertEqual(key in ref_data.keys(), True)
+                        self.assertEqual(test_data[key], ref_data[key])
 
 if __name__ == '__main__':
     unittest.main()
