839ed091888ed5cebdfe7e3621cc6b65720f7740
Xiaochi Zhou <xz378@ad.cam.ac.uk>
2020-08-04 12:31:39 +0100

    Enhanced attribute parsing mechanism: - VBN IN NN - NN IN - VB/ MD VB

--- JPS_Chatbot/Chatbot 2.0 Data preparation/wiki_corpus/create_nlu_data_wiki.py
+++ JPS_Chatbot/Chatbot 2.0 Data preparation/wiki_corpus/create_nlu_data_wiki.py
@@ -46,33 +46,62 @@ def parse_attribute(p_label, c_or_i_label, type):
     pool_1 = ['', 'what', 'how', 'when', 'where', 'who']
     pool_2 = ['what is', 'what are', 'show me', 'show', 'give', 'give me', 'list', '']
     WH_word = random.choices(pool_1, weights=(5, 70, 10, 5, 5, 5), k=1)[0] + ' '
-    print('------------------')
+
+    ###################################################################################################
+    print(text)
+    print(tag_chain)
+
     # if 'VBN' in tags or 'V' in tags: # e.g. what is xxx used for
-    if tag_chain == 'VBN IN' or tag_chain == 'VBN IN IN':
+    if tag_chain == 'VBN IN' or tag_chain == 'VBN IN IN' or tag_chain == 'RB VBN IN' or tag_chain == 'RB VBZ IN':
         temp = WH_word + random.choices(
             ['can [%s](%s) be [%s](attribute)', 'is [%s](%s) [%s](attribute)', 'are [%s](%s) [%s](attribute)'],
             weights=(10, 45, 45), k=1)[0]
-        print(text)
-        print(tag_chain)
         question_template = temp % (c_or_i_label, type, p_label.strip())
-        print(question_template)
     elif tag_chain == 'VBN' or tag_chain == 'VBN IN NN IN':  # e.g. coined, invented, awarded , VBN IN NN IN named in honor of
         temp = WH_word + random.choices(
             ['can [%s](%s) be [%s](attribute)', 'is [%s](%s) [%s](attribute)', 'are [%s](%s) [%s](attribute)', ],
             weights=(10, 45, 45), k=1)[0]
-        print(text)
-        print(tag_chain)
         question_template = temp % (c_or_i_label, type, p_label.strip())
-        print(question_template)
+    elif tag_chain == 'VB NN' or tag_chain == 'VBZ NN' or tag_chain == 'VBZ NNS' or tag_chain == 'VB NNS':
+        # e.g. has color  what color does xxx have
+        verb = text[0]
+        noun = text[1]
+        question_template = 'what [%s](attribute) does [%s](%s) %s' % (
+            noun.strip(), c_or_i_label.strip(), type, verb.strip())
+
+    elif tag_chain == 'MD VB':
+        question_template = 'what does [%s](%s) [%s](attribute)' % (c_or_i_label, type, p_label)
+
+    elif tag_chain == 'VBN IN NN' or tag_chain == 'VBN IN NNS' or tag_chain== 'VBN IN NNP':
+        # e.g. described at webpage -> which webpage is xxx described at / at what webpage is xxx described ...
+        verb = text[0]
+        prep = text[1]
+        noun = text[2]
+        wh_word = random.choice(['which', 'what'])
+        opt_1 = '%s [%s](attribute) is %s(%s) [%s %s](attribute)' % (wh_word, noun, c_or_i_label, type, verb, prep)
+        opt_2 = '[%s](attribute) %s [%s](attribute) is %s(%s) [%s](attribute) ' % (prep, wh_word, noun, c_or_i_label, type, verb)
+        question_template = random.choice([opt_1, opt_2])
+
+
+    elif tag_chain == 'NN IN':
+        # e.g. activator of -> xxx is the activator of what / what is xxx an activator of
+        article = random.choice(['the', 'a'])
+        opt_1 = '[%s](%s) is %s [%s](attribute) what' % (c_or_i_label, type,article, p_label)
+        opt_2 = 'what is [%s](%s) %s [%s](attribute)' % (c_or_i_label, type, article, p_label)
+        question_template = random.choice([opt_1, opt_2])
+
+
+
+    # TODO: make type 4 question: xxx is the activator of which xxx , which xxx is yyy an activator of ?
+
+
     else:  # the general_case, we category anything else into this question type, what is the a(attribute) of b (class/instance)
-        print(text)
-        print(tag_chain)
+
         WH_word = random.choices(pool_2, weights=(12.5, 12.5, 12.5, 12.5, 12.5, 12.5, 12.5, 12.5,), k=1)[0] + ' ' + \
                   random.choices(['the', ''], weights=(90, 10))[0] + ' '
         question_template = WH_word + random.choices(
             ['[%s](attribute) of [%s](%s)' % (p_label.strip(), c_or_i_label, type),
              '[%s](%s)\'s [%s](attribute)' % (c_or_i_label, type, p_label.strip())], weights=(80, 20), k=1)[0]
-        print(question_template)
 
     # if tag_chain in structure_dictionary:
     # structure_dictionary[tag_chain].append(question_template)
@@ -83,7 +112,8 @@ def parse_attribute(p_label, c_or_i_label, type):
     # f.close()
     #
     #
-
+    print(question_template)
+    print('==============================\n')
     return question_template
 
 
