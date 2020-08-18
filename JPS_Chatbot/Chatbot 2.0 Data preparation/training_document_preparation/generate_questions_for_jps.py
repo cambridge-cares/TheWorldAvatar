8a9c71ad10b8b623daacf1baef2c48f45469f7ca
X.Zhou <zhou0201@cam.ac.uk>
2020-08-18 21:57:43 +0100

    Tried new owl library owlready2 for reasoning. Unsuccessful.

---
+++ JPS_Chatbot/Chatbot 2.0 Data preparation/training_document_preparation/generate_questions_for_jps.py
@@ -0,0 +1,15 @@
+# Created by Xiaochi Zhou (xz378)
+
+# To experimentally use ontology inference and reasoning to extend the range of question generation
+
+# A new python Owlready2, which presumably
+#   1. DFT questions , ontocompchem
+# OntoCompChem will be the focus of this project
+
+from owlready2 import *
+
+onto = get_ontology("http://www.theworldavatar.com/ontology/ontocompchem/ontocompchem.owl")
+
+
+
+
