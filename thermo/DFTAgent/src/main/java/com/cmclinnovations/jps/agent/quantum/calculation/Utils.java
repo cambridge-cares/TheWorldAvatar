50a3dd53c6fcd1e09c3fc3fa958d2a91e070f1f3
Feroz Farazi <msff2@cam.ac.uk>
2020-08-19 12:10:49 +0100

    extend-jps-agent-in-dft-agent: Removed unused packages from Utils.java.

--- thermo/DFTAgent/src/main/java/com/cmclinnovations/jps/agent/quantum/calculation/Utils.java
+++ thermo/DFTAgent/src/main/java/com/cmclinnovations/jps/agent/quantum/calculation/Utils.java
@@ -7,16 +7,8 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
