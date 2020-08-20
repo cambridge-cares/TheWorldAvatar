2d2afde75ca283a5c2e71ee9aed018909abe0d35
Laura Ong <laura.ong@cares.cam.ac.uk>
2020-08-20 18:35:16 +0800

    Revert "Merge branch '731-dev-read-the-value-of-absdirroottest' into develop"
    This reverts commit 9639c5f7b73aa2eca73c70414649588edb6f0ea7, reversing
    changes made to a81e1c7e4de27c83d57299fb2517699e36edb7a4.

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/config/KeyValueMap.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/config/KeyValueMap.java
@@ -1,8 +1,6 @@
 package uk.ac.cam.cares.jps.base.config;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
@@ -43,18 +41,12 @@ public class KeyValueMap {
 	private void init() {
 		
 		String path = AgentLocator.getJPSBaseDirectory();
-		String libpath = path.replace("JPS_BASE","JPS_BASE_LIB" );
-		System.out.println("LibPath " + libpath);
+		
 		boolean runningForTest = AgentLocator.isJPSRunningForTest();
 		JPSBaseLogger.info(this, "Tomcat is running for test = " + runningForTest);
 		try {
 			loadProperties(path + "/conf/jps.properties");
-			loadProperties(libpath + "/src/main/resources/config.properties");
-		}catch (FileNotFoundException exc) {
-			JPSBaseLogger.error(this, exc);
-			throw new JPSRuntimeException(exc.getMessage(), exc);
-		}
-		catch (IOException exc) {
+		} catch (IOException exc) {
 			JPSBaseLogger.error(this, exc);
 			throw new JPSRuntimeException(exc.getMessage(), exc);
 		}
@@ -70,12 +62,7 @@ public class KeyValueMap {
 	}
 	
 	private void loadProperties(String propertyFile) throws IOException {
-	    //check if the file exists on the file system in string
-		File tmpDir = new File(propertyFile);
-		boolean fileExists = tmpDir.exists();
-		if (!fileExists) {
-			throw new FileNotFoundException(propertyFile + " is not found. ");
-		}
+	    
 		JPSBaseLogger.info(this, "loading key-value pairs from " + propertyFile);
 		
 		FileInputStream inputStream = new FileInputStream(propertyFile);
