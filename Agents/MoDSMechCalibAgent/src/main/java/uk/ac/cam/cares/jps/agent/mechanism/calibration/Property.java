daac2af52bf0c2e72eff9b7edf4031d625707135
Laura Ong <laura.ong@cares.cam.ac.uk>
2020-08-25 11:48:32 +0800

    Merged commit

--- Agents/MoDSMechCalibAgent/src/main/java/uk/ac/cam/cares/jps/agent/mechanism/calibration/Property.java
+++ Agents/MoDSMechCalibAgent/src/main/java/uk/ac/cam/cares/jps/agent/mechanism/calibration/Property.java
@@ -44,14 +44,7 @@ public enum Property {
 	MODEL_CANTERA_SCRIPT("runCanteraLFS.py"),
 	MODEL_CANTERA_MULTI_OPT("../../../runCanteraLFS.py -t multi -d"),
 	MODEL_CANTERA_MIX_AVERAGE_OPT("../../../runCanteraLFS.py -t mix -d"),
-	MODEL_CANTERA_OUTPUT("OutputCase00001Lfs0001Info.csv"),
-	JOB_SETUP_MECHANISM_IRI_MISSING("The mechanism IRI is not provided."),
-	JOB_SETUP_EXPERIMENT_IRI_MISSING("The experiment IRI is provided."),
-	JOB_SETUP_REACTION_IRI_MISSING("The reaction IRI list is provided.");
-	
-	public static final String JOB_REQUEST_PATH = "/job/request";
-    public static final String JOB_STATISTICS_PATH = "/job/statistics";
-    public static final String JOB_SHOW_STATISTICS_PATH = "/job/show/statistics";
+	MODEL_CANTERA_OUTPUT("OutputCase00001Lfs0001Info.csv");
 	
 	private String propertyName;
 	private int value;
