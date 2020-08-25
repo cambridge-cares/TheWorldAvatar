839f3d88b67c412558d4603f43a05607a3fbd793
Jiaru Bai <jb2197@cam.ac.uk>
2020-08-25 10:33:43 +0100

    45-dev-create-coordination-agent: extended the Property listing for AutoMechCalibAgent.

--- Agents/AutoMechCalibAgent/src/main/java/uk/ac/cam/cares/jps/agent/mechanism/coordination/Property.java
+++ Agents/AutoMechCalibAgent/src/main/java/uk/ac/cam/cares/jps/agent/mechanism/coordination/Property.java
@@ -46,9 +46,14 @@ public enum Property {
 	MODEL_CANTERA_MIX_AVERAGE_OPT("../../../runCanteraLFS.py -t mix -d"),
 	MODEL_CANTERA_OUTPUT("OutputCase00001Lfs0001Info.csv"),
 	JOB_SETUP_MECHANISM_IRI_MISSING("The mechanism IRI is not provided."),
-	JOB_SETUP_EXPERIMENT_IRI_MISSING("The experiment IRI is provided."),
-	JOB_SETUP_REACTION_IRI_MISSING("The reaction IRI list is provided.");
+	JOB_SETUP_EXPERIMENT_IRI_MISSING("The experiment IRI is not provided."),
+	JOB_SETUP_REACTION_IRI_MISSING("The reaction IRI list is not provided."),
+	JOB_SETUP_REACTION_IRI_MISPLACED("The reaction IRI list is misplaced"),
+	JOB_SETUP_RELATIVE_PERTURBATION_MISSING("The relative perturbation is provided.");
 	
+	public static final String JOB_COORDINATION_PATH = "/AutoMechCalibAgent/coordination";
+	public static final String AGENT_SENS_ANA_PATH = "/MoDSSensAnaAgent";
+	public static final String AGENT_MECH_CALIB_PATH = "/MoDSMechCalibAgent";
 	public static final String JOB_REQUEST_PATH = "/job/request";
     public static final String JOB_STATISTICS_PATH = "/job/statistics";
     public static final String JOB_SHOW_STATISTICS_PATH = "/job/show/statistics";
