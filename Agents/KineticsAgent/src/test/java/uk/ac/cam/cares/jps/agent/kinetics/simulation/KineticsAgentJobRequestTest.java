2f06bbd5fb0af54db02b4b04e7727d49f6f9a1a7
Feroz Farazi <msff2@cam.ac.uk>
2020-08-26 19:17:38 +0100

    dev-kinetics-agent: Put the JSON input for KineticsAgent.

--- Agents/KineticsAgent/src/test/java/uk/ac/cam/cares/jps/agent/kinetics/simulation/KineticsAgentJobRequestTest.java
+++ Agents/KineticsAgent/src/test/java/uk/ac/cam/cares/jps/agent/kinetics/simulation/KineticsAgentJobRequestTest.java
@@ -14,7 +14,22 @@ public class KineticsAgentJobRequestTest {
 	@Test
 	public void testJobRequest() {
 		try {
-			String query = "{\"job\":{\"levelOfTheory\":\"B3LYP/6-31G(d)\",\"keyword\": \"Opt\",\"algorithmChoice\": \"Freq\"},\"speciesIRI\": \"http://www.theworldavatar.com/kb/ontospecies/00b7e248-ae24-35bf-b7a0-b470b923ddf6.owl#00b7e248-ae24-35bf-b7a0-b470b923ddf6\"}";
+			String query = "{\"Inputs\":{\"$INP_CROSS_SECTION_VALUE\":\"2\","
+					+ "\"$INP_CROSS_SECTION_UNIT\":\"m\","
+					+ "\"$INP_TEMPERATURE_PROFILE_DEPVAR_UNIT\":\"K\","
+					+ "\"$INP_TEMPERATURE_PROFILE_INDVAR_UNIT\":\"s\","
+					+ "\"$INP_TEMPERATURE_PROFILE_VALUES\":\"0, 500 \n 0.01, 550 \n 1, 1000\","
+					+ "\"$INP_MASS_FLOW_RATE_VALUE\":\"0.0005\","
+					+ "\"$INP_MASS_FLOW_RATE_UNIT\":\"kg/s\","
+					+ "\"$INP_PRESSURE_VALUE\":\"1.0\","
+					+ "\"$INP_PRESSURE_UNIT\":\"atm\","
+					+ "\"$INP_MIX_COMP_UNIT\":\"mole fraction\","
+					+ "\"$INP_MIX_COMP_C6H6_FRACTION\":\"0.03\","
+					+ "\"$INP_MIX_COMP_C2H2_FRACTION\":\"0.03\","
+					+ "\"$INP_MIX_COMP_N2_FRACTION\":\"0.94\","
+					+ "\"$INP_COAG_MULT\":\"0.237137371\","
+					+ "\"$INP_ExhCADs\":\"141.0 \n 197.0 \n 253.0 \n 309.0 \n 365.0\"},"
+					+ "\"Case\": \"src\", \"Outputs\":[\"$OUT_MEAN_PART_DIAMETER\", \"$OUT_PART_NUMBER\"]}";
 			KineticsAgent kineticsAgent = new KineticsAgent();
 			String messageActual = kineticsAgent.setUpJob(query).toString();
 			Assert.assertEquals(messageActual, Status.JOB_SETUP_SUCCESS_MSG.getName());
