60a2ae07c0e0bd7ab71046ab65ca17cdb90951e3
Jiaru Bai <jb2197@cam.ac.uk>
2020-08-28 16:17:20 +0100

    66-dev-update-the-structure-of-all-mods-related-agents-to-follow-dft-agent: task 3 - implement agent specific properties in the agent property file and AgentProperty java class.

--- Agents/MoDSSensAnaAgent/src/main/java/uk/ac/cam/cares/jps/agent/configuration/MoDSSensAnaAgentProperty.java
+++ Agents/MoDSSensAnaAgent/src/main/java/uk/ac/cam/cares/jps/agent/configuration/MoDSSensAnaAgentProperty.java
@@ -1,10 +1,11 @@
 package uk.ac.cam.cares.jps.agent.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 /**
- * This class reads all inputs from the mods-agent.properties file, that is<br>
+ * This class reads all inputs from the modssensana-agent.properties file, that is<br>
  * to say it reads:
  * - multiple comma separated endpoints for ontochemexp knowledge graph
  * - multiple comma separated endpoints for ontokin knowledge graph
@@ -15,5 +16,172 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @PropertySource("classpath:modssensana-agent.properties")
 public class MoDSSensAnaAgentProperty {
+	@Value("${hpc.server.login.user.name}")
+	private String hpcServerLoginUserName;
+
+	@Value("${hpc.server.login.user.password}")
+	private String hpcServerLoginUserPassword;
+
+	@Value("${agent.class}")
+	private String agentClass;
+	
+	@Value("${agent.class}")
+	private String agentWorkspacePrefix;
+
+	@Value("${agent.completed.job.space.prefix}")
+	private String agentCompletedJobsSpacePrefix;
+
+	@Value("${agent.failed.job.space.prefix}")
+	private String agentFailedJobsSpacePrefix;
+	
+	@Value("${hpc.address}")
+	private String hpcAddress;
+
+	@Value("${input.file.name}")
+	private String inputFileName;
+
+	@Value("${input.file.extension}")
+	private String inputFileExtension;
+	
+	@Value("${output.file.name}")
+	private String outputFileName;
+
+	@Value("${output.file.extension}")
+	private String outputFileExtension;
+	
+	@Value("${executable.file}")
+	private String executableFile;
+	
+	@Value("${chk.point.file.extension}")
+	private String checkPointFileExtension;
+
+	@Value("${json.input.file.name}")
+	private String jsonInputFileName;
+	
+	@Value("${json.file.extension}")
+	private String jsonFileExtension;
+
+	@Value("${slurm.script.file.name}")
+	private String slurmScriptFileName;
+
+	@Value("${job.preprint.directive}")
+	private String jobPreprintDirective;
+
+	@Value("${rdf4j.server.url}")
+	private String rdf4jServerURL;
+
+	@Value("${rdf4j.repository.ontospecies}")
+	private String rdf4jRepositoryOntoSpecies;
+	
+	@Value("${rdf4j.repository.ontokin}")
+	private String rdf4jRepositoryOntoKin;
+	
+	@Value("${rdf4j.repository.ontochemexp}")
+	private String rdf4jRepositoryOntoChemExp;
+	
+	@Value("${max.number.of.hpc.jobs}")
+	private int maxNumberOfHPCJobs;
+
+	@Value("${agent.initial.delay.to.start}")
+	private int agentInitialDelayToStartJobMonitoring;
+	
+	@Value("${agent.periodic.action.interval}")
+	private int agentPeriodicActionInterval;
+
+	public String getHpcServerLoginUserName() {
+		return hpcServerLoginUserName;
+	}
+
+	public String getHpcServerLoginUserPassword() {
+		return hpcServerLoginUserPassword;
+	}
+
+	public String getAgentClass() {
+		return agentClass;
+	}
+
+	public String getAgentWorkspacePrefix() {
+		return agentWorkspacePrefix;
+	}
+
+	public String getAgentCompletedJobsSpacePrefix() {
+		return agentCompletedJobsSpacePrefix;
+	}
+
+	public String getAgentFailedJobsSpacePrefix() {
+		return agentFailedJobsSpacePrefix;
+	}
+
+	public String getHpcAddress() {
+		return hpcAddress;
+	}
+
+	public String getInputFileName() {
+		return inputFileName;
+	}
+
+	public String getInputFileExtension() {
+		return inputFileExtension;
+	}
+
+	public String getOutputFileName() {
+		return outputFileName;
+	}
+
+	public String getOutputFileExtension() {
+		return outputFileExtension;
+	}
+
+	public String getExecutableFile() {
+		return executableFile;
+	}
+
+	public String getCheckPointFileExtension() {
+		return checkPointFileExtension;
+	}
+
+	public String getJsonInputFileName() {
+		return jsonInputFileName;
+	}
+
+	public String getJsonFileExtension() {
+		return jsonFileExtension;
+	}
+
+	public String getSlurmScriptFileName() {
+		return slurmScriptFileName;
+	}
+
+	public String getJobPreprintDirective() {
+		return jobPreprintDirective;
+	}
+
+	public String getRdf4jServerURL() {
+		return rdf4jServerURL;
+	}
+
+	public String getRdf4jRepositoryOntoSpecies() {
+		return rdf4jRepositoryOntoSpecies;
+	}
+
+	public String getRdf4jRepositoryOntoKin() {
+		return rdf4jRepositoryOntoKin;
+	}
+
+	public String getRdf4jRepositoryOntoChemExp() {
+		return rdf4jRepositoryOntoChemExp;
+	}
+
+	public int getMaxNumberOfHPCJobs() {
+		return maxNumberOfHPCJobs;
+	}
+
+	public int getAgentInitialDelayToStartJobMonitoring() {
+		return agentInitialDelayToStartJobMonitoring;
+	}
+
+	public int getAgentPeriodicActionInterval() {
+		return agentPeriodicActionInterval;
+	}
 	
 }
