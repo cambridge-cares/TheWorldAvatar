8664ed8b189f63ebc3972a458b95b5e1d78eee5b
Jiaru Bai <jb2197@cam.ac.uk>
2020-09-08 19:17:40 +0100

    79-dev-change-number-of-nodes-and-maximum-jobs-for-each-agent: the number of nodes is set to be 3, and the number of maximum jobs is set to be 5.

--- Agents/MoDSSensAnaAgent/src/main/resources/Slurm.sh
+++ Agents/MoDSSensAnaAgent/src/main/resources/Slurm.sh
@@ -8,7 +8,7 @@
 #SBATCH -A COMO-SL2-CPU
 #SBATCH --mem=64000
 #SBATCH --time=96:00:00
-#SBATCH --nodes=10
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=16
 #SBATCH --output slurm.%u.%j.%N.stdout.txt   # (%u,%j,%N)=(user, job allocation #, node)  
 #SBATCH --error slurm.%u.%j.%N.errout.txt    #
