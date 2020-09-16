edda441755bf47cb7dce6a1f0849a82b065ba365
Feroz Farazi <msff2@cam.ac.uk>
2020-09-17 00:28:04 +0100

    implement-jena-jdbc-drivers: Implemented the methods prepareQuery() and performQuery().

--- PerformanceEvaluation/src/main/java/uk/ac/cam/cares/jps/performance/evaluation/kb/client/EndpointEvaluation.java
+++ PerformanceEvaluation/src/main/java/uk/ac/cam/cares/jps/performance/evaluation/kb/client/EndpointEvaluation.java
@@ -50,6 +50,74 @@ public class EndpointEvaluation {
 	public static Map<String, Map<String, String>> performanceMetrices = new HashMap<String, Map<String, String>>(); 
 
 	public static final String QUERY_FILE_NAME = "queries.json";  
+	/**
+	 * This is the main method of the class developed as the starting point<p>
+	 * of the End Point Evaluation tool.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException{
+		// Instantiates the EndpointEvaluation class to call different<p>
+		// methods of the class.
+		EndpointEvaluation epEval = new EndpointEvaluation();
+		// Calls the init() method
+		epEval.init();
+		epEval.prepareQuery();
+		epEval.performQuery();
+		epEval.compare();
+	}
+	
+	/**
+	 * Prepares the group of queries for each end point.
+	 * 
+	 * @throws IOException
+	 */
+	private void prepareQuery() throws IOException{
+		// Reads the end point URLs from the property file.
+		String[] endpoints = endpointProperty.getEndpointURLs().split(",");
+		Map<String, String> executionTimes;
+		for(String endpoint:endpoints){
+			executionTimes = new HashMap<String, String>();
+			performanceMetrices.put(endpoint, executionTimes);
+		}
+		System.out.println("path:"+getClass().getClassLoader().getResource("endpoint.properties").getPath());
+		InputStream inputStream = new FileInputStream(getClass().getClassLoader().getResource(QUERY_FILE_NAME).getPath());
+		String jsonString = FileUtil.inputStreamToString(inputStream);
+		for (String endpoint : performanceMetrices.keySet()) {
+			List<String> queries = readJsonValue(jsonString);
+			for (String query : queries) {
+				performanceMetrices.get(endpoint).put(query, null);
+			}
+		}
+	}
+	/**
+	 * Performs all queries against each end point and records the execution<p>
+	 * time in the hash map called performanceMetrices.
+	 */
+	private void performQuery(){
+		KnowledgeBaseClient kbClient = new KnowledgeBaseClient();
+		Map<String, String> queryVsTime;
+		for(String endpoint:performanceMetrices.keySet()){
+			kbClient.setQueryEndpoint(endpoint);
+			for(String query:performanceMetrices.get(endpoint).keySet()){
+				kbClient.setQuery(query);
+				try{
+					long start = System.currentTimeMillis();
+					JSONArray result = kbClient.executeQuery();
+					long time = System.currentTimeMillis() - start;
+					queryVsTime = performanceMetrices.get(endpoint);
+					queryVsTime.replace(query, null, time+"");
+					if(result==null){
+						System.out.println("No result was returned from:"+endpoint);
+					}else{
+						System.out.println("Result from:"+endpoint+": "+result.toString());
+					}
+				}catch(SQLException e){
+					log.severe(e.getMessage());
+				}
+			}
+		}
+	}
 	
 	/**
 	 * Compares the query performance of remote knowledge repository end points. 
