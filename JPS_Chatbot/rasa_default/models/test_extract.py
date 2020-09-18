9d841059c710b44206fcc0b893e5e85cca68defb
X.Zhou <zhou0201@cam.ac.uk>
2020-09-18 21:06:28 +0100

    Deleted non-essential files: the first version of the NLU model. Its function is covered by rasa_jps.

--- JPS_Chatbot/rasa_default/models/test_extract.py
+++
@@ -1,9 +0,0 @@
-import tarfile
-import os
-path = './'
-files = os.listdir(path)
-paths = [os.path.join(path, basename) for basename in files]
-file_name = max(paths, key=os.path.getctime)
-print(file_name)
-tf = tarfile.open(file_name)
-tf.extractall()
