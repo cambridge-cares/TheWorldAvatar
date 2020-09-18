967596ec2d8ced13f6687e86431f39fd45ea8f3c
Laura Ong <laura.ong@cares.cam.ac.uk>
2020-09-18 16:49:47 +0800

    add commons validator to MEN and Men Agent validateInput method

--- JPS_MEN/src/uk/ac/cam/cares/jps/men/MenAgent.java
+++ JPS_MEN/src/uk/ac/cam/cares/jps/men/MenAgent.java
@@ -11,10 +11,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.validator.routines.DoubleValidator;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
-import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Resource;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,7 +43,11 @@ public class MenAgent extends JPSHttpServlet {
 		
 		logger.info("MENAgent start");
 		
-		JSONObject jo = AgentCaller.readJsonParameter(req);			
+		JSONObject jo = AgentCaller.readJsonParameter(req);	
+		boolean validInputs = validateInput(jo);
+		if (validInputs == false) {
+			AgentCaller.printToResponse("Error caused by problematic inputs", resp);
+		}
 		String transportationModes = jo.optString("transportationmodes", "http://www.jparksimulator.com/kb/sgp/jurongisland/MaterialTransportMode.owl");
 		
 		if (jo.has("chemicalplants")) {
@@ -51,18 +57,6 @@ public class MenAgent extends JPSHttpServlet {
 			cpirilist.addAll(Arrays.asList(arr));
 		} else {
 			String ecoindustrialpark = jo.getString("ecoindustrialpark");
-			
-//			String chemicalplantInfo = "PREFIX cp:<http://www.theworldavatar.com/ontology/ontoeip/ecoindustrialpark/EcoIndustrialPark.owl#> " 
-//					+ "PREFIX j2:<http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#> "
-//					+ "SELECT ?iri "
-//					+ "WHERE {?entity  a  cp:Eco-industrialPark  ." 
-//					+ "?entity   j2:hasSubsystem ?cpl ."
-//					+ "?cpl  a  cp:ChemicalPlant  ."
-//					+ "?cpl   cp:hasConceptualModel ?cm ."
-//					+ "?cm   cp:hasIRI ?iri ." 
-//					+ "}"
-//					+ "ORDER BY ?product DESC(?added)";
-			
 			String chemicalplantInfo = "PREFIX cp:<http://www.theworldavatar.com/ontology/ontoeip/ecoindustrialpark/EcoIndustrialPark.owl#> " 
 					+ "PREFIX j2:<http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#> "
 					+ "SELECT ?iri "
@@ -75,12 +69,7 @@ public class MenAgent extends JPSHttpServlet {
 			
 			for (; rs_plant.hasNext();) {			
 				QuerySolution qs_p = rs_plant.nextSolution();
-	
-				//Literal cpiri = qs_p.getLiteral("iri");
-				
 				Resource cpiri = qs_p.getResource("iri");
-				
-				//String irilist = cpiri.getString();
 				String irilist = cpiri.toString();
 				
 				irilist = irilist.replace(" ", "");
@@ -88,8 +77,6 @@ public class MenAgent extends JPSHttpServlet {
 				irilist = irilist.replace("\r", "");
 				irilist = irilist.replace("\t", "");
 				
-				
-	
 				cpirilist.add(irilist);
 			}
 		}
@@ -124,4 +111,56 @@ public class MenAgent extends JPSHttpServlet {
 		cpirilist.clear();
 		logger.info("MENAgent exit");
 	}
+	public boolean validateInput(JSONObject args) {
+        boolean transport_opt,chemical_opt,bool_opt,impf_opt, comp_opt; 
+        transport_opt= chemical_opt =bool_opt= impf_opt = comp_opt= true; //set default value to true
+        DoubleValidator doubleValidator = new DoubleValidator();
+        UrlValidator urlValidator = new UrlValidator();
+    	try {
+        if (args.has("transportationmodes")) {//string of url representing transport
+        	transport_opt = urlValidator.isValid(args.get("transportationmodes").toString() );
+        	
+        }
+        if (args.has("chemicalplants")) {//optional value
+        	String plantLists = args.get("chemicalplants").toString();
+        	String[] arr = plantLists.split(",");
+        	//test all members of array
+        	for (int i = 0; i< arr.length; i++) {
+        		if (urlValidator.isValid(arr[i]) == false) {
+        			chemical_opt = false;
+        		}
+        	}
+			
+        	
+        }
+        if (args.has("internationalmarketpricefactor")) {
+        	impf_opt = doubleValidator.isValid(args.get("internationalmarketpricefactor").toString());
+        	
+        }
+        if (args.has("internationalmarketlowestpriceapplied")) {
+        	//There isn't a method to check if a value is boolean. Only if it's true or false
+        	Object checkBool = args.get("internationalmarketlowestpriceapplied");
+        	bool_opt = "true".equals(checkBool) || "false".equals(checkBool);
+        }
+       //these are the compulsory data; thus they're in the true category
+        	boolean carbBool=  doubleValidator.isValid(args.get("carbontax").toString());
+        	boolean intBool =  doubleValidator.isValid(args.get("interestfactor").toString());
+        	boolean annuBool = doubleValidator.isValid(args.get("annualcostfactor").toString());
+        	comp_opt = carbBool && intBool &&annuBool;
+    	} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	 if (transport_opt &&bool_opt&&impf_opt&& comp_opt== true) {
+    		 System.out.println("All outputs valid");
+    		 return  true;
+     	}else {
+     		System.out.println("Invalid value input: Please try again ");
+     		return false;
+     	}
+    	
+}
 }
