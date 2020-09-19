96a8f972bee8fe2b93cf73f8ffa04bf884beddf6
X.Zhou <zhou0201@cam.ac.uk>
2020-09-19 20:17:45 +0100

    restructured class import in chatbot_interface

--- JPS_Chatbot/UI/source/rasa_jps/chatbot_interface.py
+++ JPS_Chatbot/UI/source/rasa_jps/chatbot_interface.py
@@ -4,11 +4,11 @@ import re
 import tarfile
 from pprint import pprint
 
-from JPS_query_constructor import JPS_query_constructor
-from jps_fallback_classifier import JPSQuestionClassifier
-from topic_classifier import TopicClassifier
+from .JPS_query_constructor import JPS_query_constructor
+from .jps_fallback_classifier import JPSQuestionClassifier
+from .topic_classifier import TopicClassifier
 #from rasa_nlu.model import Interpreter
-from wiki_fallback_classifier import WikiQuestionTypeClassifier
+from .wiki_fallback_classifier import WikiQuestionTypeClassifier
 import json
 import sys
 from SPARQLWrapper import SPARQLWrapper, JSON
@@ -19,11 +19,11 @@ class Chatbot:
 
     def __init__(self):
         warnings.filterwarnings("ignore")
-        self.tc = TopicClassifier()
+        # self.tc = TopicClassifier()
         self.jps_classifier = JPSQuestionClassifier()
         self.jps_query_constructor = JPS_query_constructor()
 
-        self.wiki_classifier = WikiQuestionTypeClassifier()
+        # self.wiki_classifier = WikiQuestionTypeClassifier()
 
         # self.nlu_model_directory = '../rasa_default/models'
         # self.extract_nlu_model()  # extract trained model
@@ -52,31 +52,40 @@ class Chatbot:
 
     def analyse_questions(self, question):
         question = self.simple_replace(question)
-        topics = self.tc.classify_topic(question)
+        # topics = self.tc.classify_topic(question)
         answer = 'failed'
-        mode = 'jps'
+        # mode = 'jps'
         # mode = 'wiki'
-        print('topics:', topics)
-        try:
-            result = self.wiki_classifier.interpreter.parse(question)
-            sparql_query = self.wiki_classifier.fill_Sparql(result)
-            print('------------- intent result ----------------')
-            pprint(result)
-            print('------------- query constructed ------------')
-            print(sparql_query)
-            print('--------------query result -----------------')
-            answer = self.wiki_classifier.fire_query(sparql_query)
-            print(answer)
-            print('result ---> ', json.loads(answer)['results']['bindings'])
-            print('--------------------------------------------')
-            if len(json.loads(answer)['results']['bindings']) == 0:
-                raise Exception()
-        except:
-            result = self.jps_classifier.interpret(question)
-            answer = self.jps_query_constructor.construct_query(result)
-            print('===================== processed classification ===================')
-            pprint(answer)
-            print('==================================================================')
+         # print('topics:', topics)
+        # try:
+        #     result = self.wiki_classifier.interpreter.parse(question)
+        #     sparql_query = self.wiki_classifier.fill_Sparql(result)
+        #     print('------------- intent result ----------------')
+        #     pprint(result)
+        #     print('------------- query constructed ------------')
+        #     print(sparql_query)
+        #     print('--------------query result -----------------')
+        #     answer = self.wiki_classifier.fire_query(sparql_query)
+        #     print(answer)
+        #     print('result ---> ', json.loads(answer)['results']['bindings'])
+        #     print('--------------------------------------------')
+        #     if len(json.loads(answer)['results']['bindings']) == 0:
+        #         raise Exception()
+        # except:
+        result = self.jps_classifier.interpret(question)
+        answer = self.jps_query_constructor.construct_query(result)
+        print('===================== processed classification ===================')
+        pprint(answer)
+        if type(answer) == tuple:
+            answer = answer[0]
+        print('=========== the answer by jps ===========')
+        print(json.loads(answer))
+        print(len(json.loads(answer)))
+
+        if len(json.loads(answer)) == 0:
+            return None
+        print('==================================================================')
+
             # TODO: update the UI to show results returned from JPS endpoints
             # TODO: based on the result returned by the jps classifier, construct the query ...
 
