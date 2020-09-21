16c027edc302f6dde414d21d6b697931818fc05e
Casper Lindberg <casper.lindberg@cares.cam.ac.uk>
2020-09-21 16:00:24 +0800

    716-junit-test-for-command-helper-class-in-jps-base-lib: invoke static methods in CommandHelper using class name

--- JPS_BASE_LIB/test/uk/ac/cam/cares/jps/base/util/testCommandHelper.java
+++ JPS_BASE_LIB/test/uk/ac/cam/cares/jps/base/util/testCommandHelper.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+
 import java.util.ArrayList;
 
 class testCommandHelper {
@@ -19,13 +20,13 @@ class testCommandHelper {
 	public void testExecuteSingleCommand() {
 		String expected = "hello.battest.txttest2test2.txt";
 		String actual = null;
-		if (commandHelper.isWindows()) {
-			actual = commandHelper.executeSingleCommand(System.getProperty("user.dir") + "/test_sample_dir/", "fc test.txt test2.txt");
+		if (CommandHelper.isWindows()) {
+			actual = CommandHelper.executeSingleCommand(System.getProperty("user.dir") + "/test_sample_dir/", "fc test.txt test2.txt");
 			expected = "Comparing files test.txt and TEST2.TXT***** test.txtDfds***** TEST2.TXTDfdscopy*****";
-		} else if (commandHelper.isMac()) {
-			actual = commandHelper.executeSingleCommand(System.getProperty("user.dir") + "/test_sample_dir", "ls");
+		} else if (CommandHelper.isMac()) {
+			actual = CommandHelper.executeSingleCommand(System.getProperty("user.dir") + "/test_sample_dir", "ls");
 		}
-		assertThrows(JPSRuntimeException.class, () -> commandHelper.executeSingleCommand("sd", "sdsds"),
+		assertThrows(JPSRuntimeException.class, () -> CommandHelper.executeSingleCommand("sd", "sdsds"),
 				"Unknown exception thrown, expected JPS runtime exception");
 		assertEquals(expected, actual, "The command was not executed correctly");
 	}
@@ -34,20 +35,20 @@ class testCommandHelper {
 	public void testexecuteCommands() {
 		ArrayList<String> cmds = new ArrayList<String>();
 		String expected = null;
-		if (commandHelper.isMac()) {
+		if (CommandHelper.isMac()) {
 			cmds.add("diff");
 			cmds.add("test.txt");
 			cmds.add("test2.txt");
 			expected = "1c1< Dfds\\ No newline at end of file---> Dfdscopy\\ No newline at end of file";
-		} else if (commandHelper.isWindows()) {
+		} else if (CommandHelper.isWindows()) {
 			cmds.add("fc");
 			cmds.add("test.txt");
 			cmds.add("test2.txt");
 			expected = "Comparing files test.txt and TEST2.TXT***** test.txtDfds***** TEST2.TXTDfdscopy*****";
 		}
 
-		String actual = commandHelper.executeCommands(System.getProperty("user.dir") + "/test_sample_dir/", cmds);
-		assertThrows(JPSRuntimeException.class, () -> commandHelper.executeCommands("sd", cmds),
+		String actual = CommandHelper.executeCommands(System.getProperty("user.dir") + "/test_sample_dir/", cmds);
+		assertThrows(JPSRuntimeException.class, () -> CommandHelper.executeCommands("sd", cmds),
 				"Unknown exception thrown, expected JPS runtime exception");
 		assertEquals(expected, actual, "The command was not executed correctly");
 	}
@@ -55,12 +56,11 @@ class testCommandHelper {
 	@Test
 	public void testexecuteAsyncSingleCommand() {
 		String expected = "";
-		String actual = commandHelper.executeAsyncSingleCommand(System.getProperty("user.dir") + "/test_sample_dir",
+		String actual = CommandHelper.executeAsyncSingleCommand(System.getProperty("user.dir") + "/test_sample_dir/",
 				"hello.bat");
-		assertThrows(JPSRuntimeException.class, () -> commandHelper.executeAsyncSingleCommand("sd", "sdsds"),
+		assertThrows(JPSRuntimeException.class, () -> CommandHelper.executeAsyncSingleCommand("sd", "sdsds"),
 				"Unknown exception thrown, expected JPS runtime exception");
 		assertEquals(expected, actual, "The command was not executed correctly");
 
 	}
-
 }
