1f57b067002be019e4e718bef78f78264740d527
Owen Parry <oparry@cmclinnovations.com>
2020-09-23 12:02:54 +0100

    72-dev-create-digital-twin-uk-visualisation: Renamed functions that define configuration options for development/deployment at CoMo.

--- web/UKDigitalTwin/config.js
+++ web/UKDigitalTwin/config.js
@@ -12,20 +12,19 @@ var path = require('path')
 var config = {};
 
 config.baseUri = "http://www.theworldavatar.com"
-
 config.crebase = "http://www.theworldavatar.com/damecoolquestion/ontochem"
 config.ontokinbase = "http://www.theworldavatar.com/damecoolquestion/ontokin2"
-
 config.localRDF4j = "http://127.0.0.1:8080/rdf4j-server/repositories/PowerPlant"
 
-configDevelop();
+configDevelopCoMo();
 
-// configDeploy();
+// configDeployCoMo();
 
 //configDeployCMCL();
 
 config.agentShowcaseNode = path.join(config.root , "kb/subsetWorld.owl");
 config.worldNode = path.join(config.root , "kb/TheWorld.owl");
+config.ppFolder = path.join(config.root, "kb/powerplants");
 config.ppNode = path.join(config.root , "kb/powerplants/WorldPowerPlants.owl");
 config.jurongNode = path.join(config.root ,"kb/sgp/jurongisland/JurongIsland.owl");
 config.b3Node = path.join(config.root , "kb/sgp/jurongisland/biodieselplant3/BiodieselPlant3.owl");
@@ -52,37 +51,37 @@ config.heatWasteNode = path.join(config.root, "wasteheatnetwork.owl")
 
 function configDeployCMCL() {
     config.root = "NOT SET"
-
     config.logDir = "/home/user/logs"
-    config.port = 3001
-    config.ppFolder = path.join(config.root, "kb/powerplants")
+    config.port = 3001;
     config.registerUrl = "http://localhost:";
     config.RDF4j = "https://kg.cmclinnovations.com/rdf4j-server/repositories/UKPowerPlant"
 }
 
-function configDevelop() {
-    // config.root = path.join(__dirname ,  "testFiles") ; // own folder for testing
-    config.port =3000
-//    config.root = path.normalize("C:/Users/LONG01/TOMCAT/webapps/ROOT");
-    config.root = path.normalize("C:/Users/KADIT01/TOMCAT/webapps/ROOT");
-    config.registerUrl = "http://localhost:";
-    config.ppFolder = path.join(config.root, "kb/powerplants")
-    //"http://www.thewordavatar.com:82/change";
-    config.logDir = "C:/jps/temp/logs/"
+function configCoMoCommon() {
+    config.logDir = "C:/jps/temp/logs/";
     config.RDF4j = "https://como.ceb.cam.ac.uk/rdf4j-server/repositories/UKPowerPlant"
 }
 
-function configDeploy() {
+function configDevelopCoMo() {
+    // config.root = path.join(__dirname ,  "testFiles") ; // own folder for testing
+    config.port = 3000;
+//    config.root = path.normalize("C:/Users/LONG01/TOMCAT/webapps/ROOT");
+    config.root = path.normalize("C:/Users/KADIT01/TOMCAT/webapps/ROOT");
+    config.registerUrl = "http://localhost:";
+    //"http://www.thewordavatar.com:82/change";
+
+    configCoMoCommon();
+}
+
+function configDeployCoMo() {
      config.root = path.normalize("C:/TOMCAT/webapps/ROOT");
     //config.root = path.normalize("C:/Users/LONG01/TOMCAT/webapps/ROOT");
     //config.root2 = path.normalize("C:/TOMCAT/webapps/ROOT/kb/sgp/semakauisland");
     config.port = 82;//port for deploy
     config.registerUrl = "http://10.25.188.104";
     config.changeUrl = "http://www.theworldavatar.com:82";
-    config.ppFolder = path.join(config.root , "kb/powerplants");
-    //config.ppFolder = path.normalize("C:/TOMCAT/webapps/ROOT/kb/powerplants");
-    config.logDir = "C:/jps/temp/logs/"
-    config.RDF4j = "https://como.ceb.cam.ac.uk/rdf4j-server/repositories/UKPowerPlant"
+
+    configCoMoCommon();
 }
 
 module.exports = config;
