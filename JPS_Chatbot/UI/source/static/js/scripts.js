c80909da2805fc982f960296a7f9b55d593a7705
X.Zhou <zhou0201@cam.ac.uk>
2020-09-24 15:52:07 +0100

    changed the script address for deployment

--- JPS_Chatbot/UI/source/static/js/scripts.js
+++ JPS_Chatbot/UI/source/static/js/scripts.js
@@ -94,7 +94,7 @@
 
     address = 'http://127.0.0.1:5000/'
     cmcl_address = 'https://kg.cmclinnovations.com/'
-    // address = cmcl_address
+    address = cmcl_address
 
     $('#google_result_box').hide()
     $.get(address + "query?question=" + msg, function( data ) {
@@ -119,7 +119,7 @@ function process_json_result(result){
 
      address = 'http://127.0.0.1:5000/'
     cmcl_address = 'https://kg.cmclinnovations.com/'
-    // address = cmcl_address
+    address = cmcl_address
     query_wolfram_alpha(address, msg);
     query_google(address, msg);
     return null
