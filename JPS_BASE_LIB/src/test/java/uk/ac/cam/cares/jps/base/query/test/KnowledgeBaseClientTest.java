8c44be906bbb24087553ca9f8d5124b894bc15a1
Feroz Farazi <msff2@cam.ac.uk>
2020-10-02 19:22:41 +0100

    implement-jena-jdbc-drivers: Added a unit test for setting up an endpoint for performing query.

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/KnowledgeBaseClientTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/KnowledgeBaseClientTest.java
@@ -2,36 +2,79 @@ package uk.ac.cam.cares.jps.base.query.test;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.anyString;
 
-import uk.ac.cam.cares.jps.base.discovery.MediaType;
+
 import uk.ac.cam.cares.jps.base.query.KnowledgeBaseClient;
-import uk.ac.cam.cares.jps.base.query.SparqlOverHttpService.RDFStoreType;
 
+/**
+ * This class covers both unit tests and regression tests on KnowledgeBaseClient,<p>
+ * which is designed to perform query and update operations on virtually any<p>
+ * SPARQL Endpoints.
+ * 
+ * @author Feroz Farazi (msff2@cam.ac.uk)
+ *
+ */
 public class KnowledgeBaseClientTest {
+
+	String queryEndpoint = "http://localhost:8080/blazegraph/namespace/ontokin/sparql";
+	String updateEndpoint = "http://localhost:8080/blazegraph/namespace/ontokin/sparql";
+
+	/**
+	 * Verifies if the KnowledgeBaseClient constructor that is designed to<p>
+	 * set the query endpoint (URL) assigns the value to the corresponding<p>
+	 * member variable. 
+	 * 
+	 * @throws SQLException
+	 */
 	@Test
-	public void uploadSingleOntologyTest() throws Exception {
-//		 new KnowledgeBaseClient().uploadOntology("http://localhost:8080/blazegraph", "ontokin", "C:/Users/msff2/Documents/c4e-ProciPaper/ACSOmega/OpenData/ABF.owl");
+	public void queryEndpointSetupTest() throws SQLException{
+		KnowledgeBaseClient kbClient = new KnowledgeBaseClient(queryEndpoint);
+		assertNotNull(kbClient.getQueryEndpoint());
+		assertEquals(queryEndpoint, kbClient.getQueryEndpoint());
 	}
 	
-	@Test
-	public void uploadOntologiesTest() throws Exception {
-//		new KnowledgeBaseClient().uploadOntologies("http://localhost:8080/blazegraph", "ontokin", "C:/Users/msff2/Documents/c4e-ProciPaper/ACSOmega/OpenDataTest");
-	}
-	
-	@Test
-	public void queryNumberOfMechanisms() throws Exception{
-//		String result = new KnowledgeBaseClient().query("http://localhost:8080/blazegraph", "ontokin", RDFStoreType.BLAZEGRAPH, formMechanismCountCountQuery());
-//		System.out.println(result);
-	}
-	
-	@Test
-	public void queryAllMechanismIRIs() throws Exception{
+//	@Test
+//	public void performMechanismCountQueryTest() throws SQLException{
+//		KnowledgeBaseClient kbClient = mock(KnowledgeBaseClient.class);
+//		JSONArray jsonArray = new JSONArray();
+//		JSONObject jsonObject = new JSONObject();
+//		jsonObject.put("count", "1");
+//		jsonArray.put(jsonObject);
+//		kbClient.setQueryEndpoint(anyString());
+//		when(kbClient.setQuery(formMechanismCountCountQuery())).thenReturn(jsonArray.toString());
+//		kbClient.setQueryEndpoint(queryEndpoint);
+//		kbClient.setQuery(formMechanismCountCountQuery());
+//	}
+//
+//	@Test
+//	public void performQueryTest2() throws SQLException{
 //		KnowledgeBaseClient kbClient = new KnowledgeBaseClient();
-//		String result = kbClient.query("http://localhost:8080/blazegraph", "ontokin", RDFStoreType.BLAZEGRAPH, formMechanismIRIsQuery());
-//		System.out.println(result);
-	}
-	
+//		kbClient.setQueryEndpoint(queryEndpoint);
+//		kbClient.setQuery(formMechanismIRIsQuery());
+//		JSONArray result = kbClient.executeQuery();
+//		System.out.println(result.toString());
+//		assertNotNull(result.toString());
+//	}
+//	
+//	@Test
+//	public void updateQuery() throws SQLException{
+//		KnowledgeBaseClient kbClient = new KnowledgeBaseClient();
+//		kbClient.setQueryEndpoint(queryEndpoint);
+//		kbClient.setUpdateEndpoint(updateEndpoint);
+//		kbClient.setQuery(formInsertQuery());
+//		System.out.println("kbClient.executeUpdate():"+kbClient.executeUpdate());
+//		
+//	}
+
 	/**
 	 * A SPARQL query to count the total number of mechanisms in a repository.
 	 * 
@@ -61,6 +104,31 @@ public class KnowledgeBaseClientTest {
 			query = query.concat("{\n");
 			query = query.concat("?x ?y ?z .\n");
 			query = query.concat("} LIMIT 10\n");
+			return query;
+	}
+	
+	private static String formInsertQuery(){
+		String query = "PREFIX ontokin: <http://www.theworldavatar.com/kb/ontokin/ontokin.owl#>\n";
+			query = query.concat("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n");
+			query = query.concat("INSERT DATA { <http://www.theworldavatar.com/kb/ontokin/POLIMI_H2CO_1412.owl#ArrheniusCoefficient_182161099217501> ontokin:hasTemperatureExponent \"-0.6\" }");
+			return query;
+	}
+	
+	private static String formTempExponentQuery(){
+		String query = "PREFIX ontokin: <http://www.theworldavatar.com/kb/ontokin/ontokin.owl#>\n";
+			query = query.concat("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n");
+			query = query.concat("SELECT ?tempExponent\n");
+			query = query.concat("WHERE\n");
+			query = query.concat("{\n");
+			query = query.concat("<http://www.theworldavatar.com/kb/ontokin/POLIMI_H2CO_1412.owl#ArrheniusCoefficient_182161099217501> ontokin:hasTemperatureExponent ?tempExponent .\n");
+			query = query.concat("}");
+			return query;
+	}
+	
+	private static String formDeleteQuery(){
+		String query = "PREFIX ontokin: <http://www.theworldavatar.com/kb/ontokin/ontokin.owl#>\n";
+			query = query.concat("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n");
+			query = query.concat("INSERT DATA { <http://www.theworldavatar.com/kb/ontokin/POLIMI_H2CO_1412.owl#ArrheniusCoefficient_182161099217501> ontokin:hasTemperatureExponent \"-0.6\" }");
 			return query;
 	}
 }
