1b659172bf105ea53e10d85c4c4b3343d2f8fe50
Casper Lindberg <casper.lindberg@cares.cam.ac.uk>
2020-10-08 17:23:01 +0800

    716-junit-test-for-command-helper-class-in-jps-base-lib: move testCommandHelper to /src/test/java/ and delete unused/unfinished versions of CRSTransformer and FileUtil tests

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/util/test/testCommandHelper.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/util/test/testCommandHelper.java
@@ -1,10 +1,11 @@
-package uk.ac.cam.cares.jps.base.util;
+package uk.ac.cam.cares.jps.base.util.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.util.CommandHelper;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
