e5bf23b501dcbb4b61378487fa8bf1dcc3ff01ba
Kok Foong Lee <kflee@cmclinnovations.com>
2020-10-23 09:57:09 +0100

    87-dev-region-episode-cleanup: implemented a method to obtain the time zone of the simulated scope

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/region/Scope.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/region/Scope.java
@@ -1,7 +1,13 @@
 package uk.ac.cam.cares.jps.base.region;
 
+import java.time.Instant;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
 import org.json.JSONObject;
 
+import uk.ac.cam.cares.jps.base.discovery.AgentCaller;
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.util.CRSTransformer;
 
 public class Scope {
@@ -55,12 +61,59 @@ public class Scope {
         // determine whether it's north or south of the equator
         String NS = null; 
         if (centre[1]>0) {
-        	NS = "N";
+            NS = "N";
         }
         else {
-        	NS = "S";
+            NS = "S";
         }
         String UTMZone = String.valueOf(zoneNumber) + NS;
         return UTMZone;
+    }
+
+    public int getTimeZone() {
+        /**
+         * Obtain GMT time zone with coordinates of scope centre through Google API by default
+         */
+        int timeZone = 0;
+        try {
+            // obtain x y coordinates of the centre
+            double [] centre = getScopeCentre();
+
+            // convert coordinates to latitude longitude
+            if (!this.sourceCRS.equals(CRSTransformer.EPSG_4326)) {
+                centre = CRSTransformer.transform(this.sourceCRS, CRSTransformer.EPSG_4326, centre);
+            }
+
+            // convert from s to hour
+            timeZone = getTimeZoneFromGoogle(centre)/3600; 
+        } catch (JPSRuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("WARNING: Google Map time zone API failure. Setting time zone to 0.");
+            timeZone = 0;
+        }
+        return timeZone;
+    }
+
+    private int getTimeZoneFromGoogle(double[] centre) {
+        /** 
+         * sends request to google API with centre of scope
+         */
+        String latlon = String.valueOf(centre[1]) + "," + String.valueOf(centre[0]);
+        URIBuilder builder = new URIBuilder().setScheme("https").setHost("maps.googleapis.com")
+                .setPath("/maps/api/timezone/json");
+        builder.setParameter("location", latlon);
+        builder.setParameter("key", "AIzaSyBgm3-eMQauJ_dW4Cq66Hg9aP50jpp24rA");
+        builder.setParameter("timestamp", String.valueOf(Instant.now().getEpochSecond()));
+        int result;
+
+        try {
+            HttpGet request = new HttpGet(builder.build());
+            String apiresult = AgentCaller.executeGet(request);
+            JSONObject jo = new JSONObject(apiresult);
+            result = (int) jo.get("rawOffset"); // GMT in seconds
+        } catch (Exception e) {
+            throw new JPSRuntimeException(e.getMessage(), e);
+        }
+        return result;
     }
 }
--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/region/test/ScopeTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/region/test/ScopeTest.java
@@ -25,4 +25,18 @@ public class ScopeTest {
         assertEquals(sc1.getUTMzone(),"48N");
         assertEquals(sc2.getUTMzone(),"50N");
     }
+
+    @Test
+    public void testGetTimeZone() {
+        JSONObject jo1 = new JSONObject();
+        Region.putRegion(jo1, 1);
+        Scope sc1 = new Scope(jo1.getJSONObject(Region.keyRegion)); // Singapore scope
+
+        JSONObject jo2 = new JSONObject();
+        Region.putRegion(jo2, 3);
+        Scope sc2 = new Scope(jo2.getJSONObject(Region.keyRegion)); // Hong Kong scope
+
+        assertEquals(sc1.getTimeZone(),8);
+        assertEquals(sc2.getTimeZone(),8);
+    }
 }
