63d363b44534b0b09c8c9088fcd1edc3ae732efd
tomrsavage <tom.savage@hotmail.com>
2020-10-30 14:27:31 +0000

    updated robustness of mapping tool, added example of connection matrix

--- GasGrid/PopulatingTools/Mapping and Location Tools/map_tool.py
+++ GasGrid/PopulatingTools/Mapping and Location Tools/map_tool.py
@@ -2,48 +2,67 @@ import googlemaps
 import numpy as np
 import gmplot
 import pandas as pd 
+from location_search import loc_search
+
+def map_creation():
+
+    api_key ='AIzaSyAqfIR6zaWjhNXODG8vycZpA4olcoMD3w0'
+
+    gmaps = googlemaps.Client(key=api_key)
+    gmap = gmplot.GoogleMapPlotter(54.842595,-2.5051270,6)
+    gmap.apikey = api_key
+
+    try:
+        locations = pd.read_excel(r'PopulatingTools\Mapping and Location Tools\location.xlsx')
+    except:
+        print('LOCATION.XLSX FILE NOT FOUND, CREATING FROM LIST.CSV')
+        loc_search()
+        locations = pd.read_excel(r'PopulatingTools\Mapping and Location Tools\location.xlsx')
+
+    locations = locations.to_numpy()[:,1:]
+    titles = locations[0,:]
+    i = 0
+    while True:
+        try:
+            if titles[i] != titles[i]:
+                titles = np.delete(titles,i)
+                i -= 1
+            i += 1
+        except:
+            break
+    locations = locations[1:,:]
+    c = ['k','b','r','g','orange','w','purple']
+
+    location_index = np.arange(0,len(locations[0,:]),len(titles))
+    overall_location = np.array([[0,0]])
+    for j in location_index:
+
+        class_location = np.array([[0,0]])
+
+        for i in range(len(locations[:,1+j])):
+            loc = locations[i,j+1:j+3]
+            if loc[0] != loc[0]:
+                break
+            class_location = np.append(class_location,[loc],axis=0)
+        class_location = class_location[1:,:]
+
+        gmap.scatter(class_location[:,0],class_location[:,1],s=2000,c=c[int(j/3)]\
+            ,marker=False,title=titles[int(j/3)])
+
+        overall_location = np.append(overall_location,class_location,axis=0)
+    overall_location = overall_location[1:,:]
 
 
 
-gmaps = googlemaps.Client(key='AIzaSyAqfIR6zaWjhNXODG8vycZpA4olcoMD3w0')
-gmap = gmplot.GoogleMapPlotter(54.842595,-2.5051270,6)
-gmap.apikey = 'AIzaSyAqfIR6zaWjhNXODG8vycZpA4olcoMD3w0'
+    connection_matrix = np.random.randint(0,2,(len(overall_location),len(overall_location)))
 
+    for i in range(len(connection_matrix)):
+        for j in range(i,len(connection_matrix)):
+            if connection_matrix[i,j] == 1:
+                u = np.random.uniform()
+                if u < 0.005:
+                    gmap.plot([overall_location[i,0],overall_location[j,0]],[overall_location[i,1],overall_location[j,1]])
+    gmap.draw('PopulatingTools\Mapping and Location Tools\map.html')
+    return 
 
-
-locations = pd.read_excel(r'C:\Users\trs53\Dropbox\Documents\Cambridge\PopulatingTools\location.xlsx')
-locations = locations.to_numpy()[1:,1:]
-intakes = locations[:,1:3]
-compressors = locations[:,4:6]
-offtakes = locations[:,7:]
-
-for i in range(len(intakes)):
-    intakes[i,0] = float(intakes[i,0])
-    intakes[i,1] = float(intakes[i,1])
-    if intakes[i,0] != intakes[i,0]:
-        intakes = intakes[:i,:]
-        break
-
-for i in range(len(offtakes)):
-    offtakes[i,0] = float(offtakes[i,0])
-    offtakes[i,1] = float(offtakes[i,1])
-    if offtakes[i,0] != offtakes[i,0]:
-        offtakes = offtakes[:i,:]
-        break
-
-for i in range(len(compressors)):
-    compressors[i,0] = float(compressors[i,0])
-    compressors[i,1] = float(compressors[i,1])
-    if compressors[i,0] != compressors[i,0]:
-        compressors = compressors[:i,:]
-        break
-
-
-gmap.scatter(offtakes[:,0],offtakes[:,1],c='w',marker=False,s=200)
-gmap.scatter(compressors[:,0],compressors[:,1],c='k',marker=False,s=200)
-gmap.scatter(intakes[:,0],intakes[:,1],c='r',marker=False,s=200)
-
-gmap.scatter(offtakes[:,0],offtakes[:,1],c='w')
-gmap.scatter(compressors[:,0],compressors[:,1],c='k')
-gmap.scatter(intakes[:,0],intakes[:,1],c='r')
-gmap.draw('map.html')
+map_creation()
