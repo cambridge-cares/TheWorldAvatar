dc18fe255abcb304d6e1e3ff09901946ffea76d1
Casper Lindberg <casper.lindberg@cares.cam.ac.uk>
2020-10-30 16:38:39 +0800

    795-dev-create-filebasedkbclient-working-with-the-current-one: move JDBC drivers from KBClient to subclass RemoteKnowledgeBaseClient and make KBClient abstract
    getInstance calls in JPSBaseLogger replaced with .class

---
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/interfaces/KnowledgeBaseClientInterface.java
@@ -0,0 +1,21 @@
+package uk.ac.cam.cares.jps.base.interfaces;
+
+import java.sql.SQLException;
+
+import org.json.JSONArray;
+
+public interface KnowledgeBaseClientInterface {
+
+	public JSONArray executeQuery(String query) throws SQLException;	
+	
+	public JSONArray executeQuery() throws SQLException;
+	
+	public String execute() throws SQLException;
+	
+	public String execute(String sparql) throws SQLException;
+	
+	public int executeUpdate() throws SQLException;
+	
+	public int executeUpdate(String update) throws SQLException;
+	
+}
