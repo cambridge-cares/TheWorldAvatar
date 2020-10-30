189885bd4f71e2a5fbeec859d3ab93353cb403b3
Casper Lindberg <casper.lindberg@cares.cam.ac.uk>
2020-10-30 17:39:31 +0800

    795-dev-create-filebasedkbclient-working-with-the-current-one: extend FileBasedKnowledgeBaseClientRDFConnection from KnowledgeBaseClient

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/query/FileBasedKnowledgeBaseClientRDFConnection.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/query/FileBasedKnowledgeBaseClientRDFConnection.java
@@ -1,42 +1,81 @@
 package uk.ac.cam.cares.jps.base.query;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.query.TxnType;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdfconnection.RDFConnection;
 import org.apache.jena.rdfconnection.RDFConnectionFactory;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class FileBasedKnowledgeBaseClientRDFConnection {
+import uk.ac.cam.cares.jps.base.interfaces.KnowledgeBaseClientInterface;
 
-	private String filePath;
+public class FileBasedKnowledgeBaseClientRDFConnection extends KnowledgeBaseClient implements KnowledgeBaseClientInterface{
+
 	private Dataset dataset;
 	private RDFConnection conn;
+	private String filePath;
+	
+	//TODO: check connection active before performing process; default constructor and initialise post-construction
 	
 	// constructor
 	public FileBasedKnowledgeBaseClientRDFConnection(String filePath) {
 		
 		this.filePath = filePath;
+		init();
+	}
+		
+	// constructor
+	public FileBasedKnowledgeBaseClientRDFConnection(String filePath, String sparql) {
+		
+		super(sparql); 
+		this.filePath = filePath;
+		init();
+	}
+	
+	// initialise connection
+	private void init() {
+		
 		dataset = DatasetFactory.create();
 		conn = RDFConnectionFactory.connect(dataset);
-		conn.load(this.filePath);
+		conn.load(filePath);
+	}
+	
+	//
+	public void load(String filePath) {
+		this.filePath = filePath;
+		init();
 	}
 	
 	// close connection
-	public void close() {
+	public void close() throws IOException {	//cannot be closed unless in base class
+		
+		writeToFile();
 		conn.close();
 	}
 	
-	// Sparql Update
-	public void executeUpdate(String update) {
+	public int executeUpdate() {
+		return executeUpdate(this.query);
+	}
 	
+	public int executeUpdate(String update) {
+		
 		conn.begin( TxnType.WRITE );
 		try {
 			conn.update( update );
@@ -44,11 +83,19 @@ public class FileBasedKnowledgeBaseClientRDFConnection {
 		} finally {
 			conn.end();
 		}
+		return 0; //return a useful integer?
 	}
 
-	//Sparql Select query
-	public ResultSet executeQuery(String sparql) {
+	public JSONArray executeQuery(String sparql) {
+		ResultSet results = perfromExecuteQuery(sparql);
+		return convert(results);
+	}	
+	
+	public JSONArray executeQuery() {
+		return executeQuery(this.query);
+	}
 			
+	private ResultSet perfromExecuteQuery(String sparql) {
 		conn.begin( TxnType.READ );	
 		try {
 			QueryExecution queryExec = conn.query(sparql);
@@ -59,7 +106,25 @@ public class FileBasedKnowledgeBaseClientRDFConnection {
 		}
 	}
 	
+	private JSONArray convert(ResultSet resultSet) {
+				
+		JSONArray json = new JSONArray();
+		
+		while (resultSet.hasNext()) {
+			QuerySolution qs = resultSet.next();
+			JSONObject obj = new JSONObject();
+			Iterator<String> it = qs.varNames(); 
+			while(it.hasNext()) {
+				String var = it.next(); 
+				obj.put(var, qs.get(var));
+			}
+			json.put(obj);
+		}
+		return json;
+	}
+
 	// write to same file
+	// put this at the end of update or in close?
 	public void writeToFile() throws IOException {
 		
 		writeToFile(this.filePath);
@@ -71,8 +136,8 @@ public class FileBasedKnowledgeBaseClientRDFConnection {
 		try (OutputStream out = new FileOutputStream(filePath)){
 			
 			RDFDataMgr.write(out, dataset.getDefaultModel(), Lang.RDFXML); // check getdefaultmodel
+			//RDFDataMgr.write(out, dataset.getDefaultModel(), Lang.NQ); // check getdefaultmodel
 			out.flush();
 		}
 	}
-	
 }
