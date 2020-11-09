2a0dcb0146dc14593cbd60615e7031d7f9089b25
Casper Lindberg <casper.lindberg@cares.cam.ac.uk>
2020-11-09 18:38:12 +0800

    758-create-a-tool-for-knowledge-graph-for-renaming-tbox-and-abox: junit tests for buildSparqlUpdate methods in RenameTool

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/tools/test/RenameToolTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/tools/test/RenameToolTest.java
@@ -1,137 +1,198 @@
 package uk.ac.cam.cares.jps.base.tools.test;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 
+import org.apache.jena.update.UpdateRequest;
 import org.apache.jena.sparql.lang.sparql_11.ParseException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import uk.ac.cam.cares.jps.base.query.KnowledgeBaseClient;
 import uk.ac.cam.cares.jps.base.tools.RenameTool;
 
 public class RenameToolTest {
 
-	
-	//additional tests for private methods to build queries 
-	
-	@SuppressWarnings("unused")
-	@Test
-	public void testReplaceRdf4j() throws SQLException, ParseException {
-	
-		String dataSetURL = "http://localhost:8080/rdf4j-server/repositories/species/statements"; 
-		
-		String target = null;
-		String replacement = null;
-		
-		if (false) {
-			target = "http://www.w3.org/2008/05/skos#altLabel";
-			replacement = "http://www.example.com/Test";
-		}else {
-			target = "http://www.example.com/Test";
-			replacement = "http://www.w3.org/2008/05/skos#altLabel";
-		}
-		
-		KnowledgeBaseClient kbClient = null;
-		RenameTool.renameURI(kbClient, target, replacement, null);
-		
-		// test using query
-		fail("Not yet implemented");
-	}
-	
-	@SuppressWarnings("unused")
-	@Test
-	public void testReplaceRdf4jString() throws SQLException, ParseException {
-	
-		String dataSetURL = "http://localhost:8080/rdf4j-server/repositories/species/statements"; 
-		
-		String target = null;
-		String replacement = null;
-		
-		if (false) {
-			target = "05/skos#altLabel";
-			replacement = "/Test";
-		}else {
-			target = "/Test";
-			replacement = "05/skos#altLabel";
-		}
-		
-		KnowledgeBaseClient kbClient = null;
-		RenameTool.renameURI(kbClient, target, replacement, null);
-		
-		// test using query
-		fail("Not yet implemented");
-	}
-	
-	@SuppressWarnings("unused")
-	@Test
-	public void testReplaceBlazegraph() throws SQLException, ParseException {
-	
-		String dataSetURL = "http://localhost:8080/blazegraph/namespace/species/update"; 
-		
-		String target = null;
-		String replacement = null;
-		
-		if (false) {
-			target = "http://www.w3.org/2008/05/skos#altLabel";
-			replacement = "http://www.example.com/Test";
-		}else {
-			target = "http://www.example.com/Test";
-			replacement = "http://www.w3.org/2008/05/skos#altLabel";
-		}
-		
-		KnowledgeBaseClient kbClient = null;
-		RenameTool.renameURI(kbClient, target, replacement, null);
+	private RenameTool renameTool;
 
-		// test using query
-		fail("Not yet implemented");	
+	// temporary folder for testing
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+	
+	@Test
+	public void testRenameURI() {
+		
+		fail("Test not created");
 	}
 	
-	@SuppressWarnings("unused")
 	@Test
-	public void testReplaceFuseki() throws SQLException, ParseException {
+	public void testRenameURIFragment() throws URISyntaxException, SQLException, ParseException {
+		
+		fail("Test not created");
+		
+		String testFilePath = Paths.get(this.getClass().getResource("/ToolsTest/species.owl").toURI()).toFile().getPath();
+		String tempFolderPath = tempFolder.getRoot().toString();
+		
+		String target = "/test/target";
+		String replacement = "/test/replacement";
+			
+		KnowledgeBaseClient kbClient = new KnowledgeBaseClient();
+		//TODO setup filebasedkbclient
+		
+		RenameTool.renameURIFragment(kbClient, target, replacement);
+		
+	}
 	
-		String dataSetURL = "http://localhost:8080/fuseki/species/update";
-		
-		String target = null;
-		String replacement = null;
-		
-		if (false) {
-			target = "http://www.w3.org/2008/05/skos#altLabel";
-			replacement =  "http://www.example.com/Test";
-		}else {
-			target = "http://www.example.com/Test";
-			replacement = "http://www.w3.org/2008/05/skos#altLabel";
-		}
-		
-		KnowledgeBaseClient kbClient = null;
-		RenameTool.renameURI(kbClient, target, replacement, null);
-		
-		// test using query
-		fail("Not yet implemented");
-	}	
-	
-	@SuppressWarnings("unused")
+	// Test sparql builder. Update to replace URI.
 	@Test
-	public void testReplaceLocal() throws SQLException, ParseException {
+	public void testBuildSparqlUpdateURI() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		
+		String expected = "DELETE {\n" +
+		"  GRAPH <http://example.com/test> {\n" +
+		"    ?s ?p ?o .\n" +
+		"  }\n" +
+		"}\n" +
+		"INSERT {\n" +
+		"  GRAPH <http://example.com/test> {\n" +
+		"    ?newS ?newP ?newO .\n" +
+		"  }\n" +
+		"}\n"+ 
+		"WHERE\n" +
+		"  { GRAPH <http://example.com/test>\n" +
+		"      { ?s  ?p  ?o\n" +
+		"        BIND(<http://example.com/test/target> AS ?targetURI)\n" +
+		"        BIND(<http://example.com/test/replacement> AS ?replacementURI)\n" +
+		"        FILTER ( ( ?s = ?targetURI ) || ( ( ?p = ?targetURI ) || ( ?o = ?targetURI ) ) )\n" +
+		"        BIND(if(( ?s = ?targetURI ), ?replacementURI, ?s) AS ?newS)\n" +
+		"        BIND(if(( ?p = ?targetURI ), ?replacementURI, ?p) AS ?newP)\n" +
+		"        BIND(if(( ?o = ?targetURI ), ?replacementURI, ?o) AS ?newO)\n" +
+		"      }\n" +
+		"  }\n";
+		
+		// access private member
+		renameTool = new RenameTool();
+		assertNotNull(renameTool.getClass().getDeclaredMethod("buildSparqlUpdateURI", String.class, String.class, String.class));
+		Method buildSparqlUpdateURI = renameTool.getClass().getDeclaredMethod("buildSparqlUpdateURI", String.class, String.class, String.class);
+		buildSparqlUpdateURI.setAccessible(true);
+		
+		UpdateRequest sparql = (UpdateRequest) buildSparqlUpdateURI.invoke(renameTool, "http://example.com/test/target", "http://example.com/test/replacement", "http://example.com/test");
+		
+		String strSparql = sparql.toString();
+		
+		assertEquals(expected, strSparql);
+	}
 	
-		String dataSetURL = "file:\\\\\\C:\\Users\\CLIN01\\Documents\\Codes\\JParkSimulator4-git\\JPS_BASE_LIB\\src\\test\\resources\\species.owl"; 
+	// Test sparql builder. Update to replace URI in default graph.
+	@Test
+	public void testBuildSparqlUpdateURIDefaultGraph() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		
-		String target = null;
-		String replacement = null;
+		String expected = "DELETE {\n" +
+		"  ?s ?p ?o .\n" +
+		"}\n" +
+		"INSERT {\n" +
+		"  ?newS ?newP ?newO .\n" +
+		"}\n"+ 
+		"WHERE\n" +
+		"  { ?s  ?p  ?o\n" +
+		"    BIND(<http://example.com/test/target> AS ?targetURI)\n" +
+		"    BIND(<http://example.com/test/replacement> AS ?replacementURI)\n" +
+		"    FILTER ( ( ?s = ?targetURI ) || ( ( ?p = ?targetURI ) || ( ?o = ?targetURI ) ) )\n" +
+		"    BIND(if(( ?s = ?targetURI ), ?replacementURI, ?s) AS ?newS)\n" +
+		"    BIND(if(( ?p = ?targetURI ), ?replacementURI, ?p) AS ?newP)\n" +
+		"    BIND(if(( ?o = ?targetURI ), ?replacementURI, ?o) AS ?newO)\n" +
+		"  }\n";
 		
-		if (false) {
-			target = "http://www.w3.org/2008/05/skos#altLabel";
-			replacement =  "http://www.example.com/Test";
-		}else {
-			target = "http://www.example.com/Test";
-			replacement = "http://www.w3.org/2008/05/skos#altLabel";
-		}
+		// access private member
+		renameTool = new RenameTool();
+		assertNotNull(renameTool.getClass().getDeclaredMethod("buildSparqlUpdateURI", String.class, String.class, String.class));
+		Method buildSparqlUpdateURI = renameTool.getClass().getDeclaredMethod("buildSparqlUpdateURI", String.class, String.class, String.class);
+		buildSparqlUpdateURI.setAccessible(true);
 		
-		KnowledgeBaseClient kbClient = null;
-		RenameTool.renameURI(kbClient, target, replacement, null);
-		 
-		// test using query
-		fail("Not yet implemented");
+		UpdateRequest sparql = (UpdateRequest) buildSparqlUpdateURI.invoke(renameTool, "http://example.com/test/target", "http://example.com/test/replacement", null);
+		
+		String strSparql = sparql.toString();
+		
+		assertEquals(expected, strSparql);
+	}
+	
+	// Test sparql builder. Update to replace URI fragment.
+	@Test
+	public void testBuildSparqlUpdateString() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		
+		String expected = "DELETE {\n" +
+		"  GRAPH <http://example.com/test> {\n" +
+		"    ?s ?p ?o .\n" +
+		"  }\n" +
+		"}\n" +
+		"INSERT {\n" +
+		"  GRAPH <http://example.com/test> {\n" +
+		"    ?newS ?newP ?newO .\n" +
+		"  }\n" +
+		"}\n"+ 
+		"WHERE\n" +
+		"  { GRAPH <http://example.com/test>\n" +
+		"      { ?s  ?p  ?o\n" +
+		"        BIND(regex(str(?s), \"/test/target\") AS ?matchS)\n" +
+		"        BIND(regex(str(?p), \"/test/target\") AS ?matchP)\n" +
+		"        BIND(regex(str(?o), \"/test/target\") AS ?matchO)\n" +
+		"        FILTER ( ?matchS || ( ?matchP || ?matchO ) )\n" +
+		"        BIND(if(?matchS, uri(replace(str(?s), \"/test/target\", \"/test/replacement\")), ?s) AS ?newS)\n" +
+		"        BIND(if(?matchP, uri(replace(str(?p), \"/test/target\", \"/test/replacement\")), ?p) AS ?newP)\n" +
+		"        BIND(if(?matchO, uri(replace(str(?o), \"/test/target\", \"/test/replacement\")), ?o) AS ?newO)\n" +
+		"      }\n" +
+		"  }\n";
+		
+		// access private member
+		renameTool = new RenameTool();
+		assertNotNull(renameTool.getClass().getDeclaredMethod("buildSparqlUpdateString", String.class, String.class, String.class));
+		Method buildSparqlUpdateString = renameTool.getClass().getDeclaredMethod("buildSparqlUpdateString", String.class, String.class, String.class);
+		buildSparqlUpdateString.setAccessible(true);
+		
+		UpdateRequest sparql = (UpdateRequest) buildSparqlUpdateString.invoke(renameTool, "/test/target", "/test/replacement", "http://example.com/test");
+		
+		String strSparql = sparql.toString();
+		
+		assertEquals(expected, strSparql);
+	}
+	
+	// Test sparql builder. Update to replace URI fragment.
+	@Test
+	public void testBuildSparqlUpdateStringDefaultGraph() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		
+		String expected = "DELETE {\n" +
+		"  ?s ?p ?o .\n" +
+		"}\n" +
+		"INSERT {\n" +
+		"  ?newS ?newP ?newO .\n" +
+		"}\n"+ 
+		"WHERE\n" +
+		"  { ?s  ?p  ?o\n" +
+		"    BIND(regex(str(?s), \"/test/target\") AS ?matchS)\n" +
+		"    BIND(regex(str(?p), \"/test/target\") AS ?matchP)\n" +
+		"    BIND(regex(str(?o), \"/test/target\") AS ?matchO)\n" +
+		"    FILTER ( ?matchS || ( ?matchP || ?matchO ) )\n" +
+		"    BIND(if(?matchS, uri(replace(str(?s), \"/test/target\", \"/test/replacement\")), ?s) AS ?newS)\n" +
+		"    BIND(if(?matchP, uri(replace(str(?p), \"/test/target\", \"/test/replacement\")), ?p) AS ?newP)\n" +
+		"    BIND(if(?matchO, uri(replace(str(?o), \"/test/target\", \"/test/replacement\")), ?o) AS ?newO)\n" +
+		"  }\n";
+		
+		// access private member
+		renameTool = new RenameTool();
+		assertNotNull(renameTool.getClass().getDeclaredMethod("buildSparqlUpdateString", String.class, String.class, String.class));
+		Method buildSparqlUpdateString = renameTool.getClass().getDeclaredMethod("buildSparqlUpdateString", String.class, String.class, String.class);
+		buildSparqlUpdateString.setAccessible(true);
+		
+		UpdateRequest sparql = (UpdateRequest) buildSparqlUpdateString.invoke(renameTool, "/test/target", "/test/replacement", null);
+		
+		String strSparql = sparql.toString();
+		
+		assertEquals(expected, strSparql);
 	}
 }
