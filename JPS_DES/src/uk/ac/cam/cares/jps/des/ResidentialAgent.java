d3688daec78999aa831306b2b0aedfdfe052d6eb
long <laura.ong@cares.cam.ac.uk>
2020-11-26 09:58:33 +0800

    remove lines related to abandoned Querybuilder

--- JPS_DES/src/uk/ac/cam/cares/jps/des/ResidentialAgent.java
+++ JPS_DES/src/uk/ac/cam/cares/jps/des/ResidentialAgent.java
@@ -30,40 +30,10 @@ public class ResidentialAgent extends JPSHttpServlet {
 	public static String Pmax="Pmax.csv";
 	public static String unwill="unwill.csv";
 	public static String schedule="ApplianceScheduleLoad1.csv";
+	
 	public void extractResidentialData(String iriofnetworkdistrict, String baseUrl) {
 		OntModel model = DESAgentNew.readModelGreedy(iriofnetworkdistrict);
-//		String j2Prefix = "http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#";
-//		String j4Prefix = "http://www.theworldavatar.com/ontology/ontopowsys/OntoPowSys.owl#";
-//		String j5Prefix = "http://www.theworldavatar.com/ontology/ontocape/chemical_process_system/CPS_realization/process_control_equipment/measuring_instrument.owl#";
-//		String j6Prefix = "http://www.theworldavatar.com/ontology/ontopowsys/PowSysRealization.owl#";
-//		String entitySub = "?entity";
-//		String a = "a";
-//		try {
-//			WhereBuilder aBuild = new WhereBuilder()
-//					.addPrefix("j6", j6Prefix)
-//					.addWhere(entitySub, a, "j6:Building")
-//					.addWhere("?entity", "j4:isComprisedOf", "?user");
-//			
-//			SelectBuilder sb1 = new SelectBuilder()
-//					.addPrefix("j2", j2Prefix)
-//					.addPrefix("j4", j4Prefix)
-//					.addVar("?entity (COUNT(?entity) AS ?group)")
-//					.addVar("?propval")
-//					.addVar("?user")
-//					.addWhere(aBuild)
-//					.addWhere(entitySub, "j2:hasProperty", "?prop")
-//					.addWhere("?prop", "j2:hasValue", "?vprop")
-//					.addFilter("regex(STR(?user),\"001\")")
-//					.addGroupBy("?entity")
-//					.addGroupBy("?propval")
-//					.addGroupBy("?user");
-//			
-//			SelectBuilder sb2 = new SelectBuilder()
-//					.addPrefix("j4", "http://www.theworldavatar.com/ontology/ontopowsys/OntoPowSys.owl#")
-//					.addVar("?entity (COUNT(?entity) AS ?group)")
-//					.addWhere(aBuild)
-//					.addGroupBy("?entity");
-				
+		//extracts 
 		String groupInfo = "PREFIX j2:<http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#> "
 				+ "PREFIX j4:<http://www.theworldavatar.com/ontology/ontopowsys/OntoPowSys.owl#> "
 				+ "PREFIX j5:<http://www.theworldavatar.com/ontology/ontocape/chemical_process_system/CPS_realization/process_control_equipment/measuring_instrument.owl#> "
@@ -82,7 +52,6 @@ public class ResidentialAgent extends JPSHttpServlet {
 		String result = JenaResultSetFormatter.convertToJSONW3CStandard(resultSet);
 		String[] keys = JenaResultSetFormatter.getKeys(result);
 		List<String[]> resultList = JenaResultSetFormatter.convertToListofStringArrays(result, keys);
-		System.out.println("sizeofresult=" + resultList.size());
 		int size = resultList.size();
 		List<String> iriofgroupuser = new ArrayList<String>();
 		List<String[]> csvofbcap = new ArrayList<String[]>();
@@ -116,17 +85,14 @@ public class ResidentialAgent extends JPSHttpServlet {
 		}
 
 		broker.putLocal(baseUrl + "/" + bcap, bcapcsv);
-		// csvofpmax.add(0, arr0);
 		String pmaxcsv = MatrixConverter.fromArraytoCsv(csvofpmax);
 		System.out.println(pmaxcsv);
 		broker.putLocal(baseUrl + "/" + Pmax, pmaxcsv);
 
-		// csvofpmin.add(0, arr0);
 		String pmincsv = MatrixConverter.fromArraytoCsv(csvofpmin);
 		System.out.println(pmincsv);
 		broker.putLocal(baseUrl + "/" + Pmin, pmincsv);
 
-		// csvofw.add(0, arr0);
 		String wcsv = MatrixConverter.fromArraytoCsv(csvofw);
 		System.out.println(wcsv);
 		broker.putLocal(baseUrl + "/" + unwill, wcsv);
@@ -135,10 +101,6 @@ public class ResidentialAgent extends JPSHttpServlet {
 		System.out.println(schedulecsv);
 		broker.putLocal(baseUrl + "/" + schedule, schedulecsv);
 		
-//		}	catch (org.apache.jena.sparql.lang.sparql_11.ParseException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
 	}
 	protected void readUserforAppSche(List<String> lst, int h, String iriOfTypeUser) {
 		//per equipment, per user, extract high, low and actual value 
@@ -171,15 +133,11 @@ public class ResidentialAgent extends JPSHttpServlet {
 		String resultx = JenaResultSetFormatter.convertToJSONW3CStandard(resultSetx);
 		String[] keysx = JenaResultSetFormatter.getKeys(resultx);
 		List<String[]> resultListx = JenaResultSetFormatter.convertToListofStringArrays(resultx, keysx);
-		System.out.println("sizeofresult="+resultListx.size()); //264 rows, appliance then by hour
 		
 
 		List<String>groupPmax=new ArrayList<String>();
-		groupPmax.add(iriOfTypeUser);
 		List<String>groupPmin=new ArrayList<String>();
-		groupPmin.add(iriOfTypeUser);
 		List<String>groupw=new ArrayList<String>();
-		groupw.add(iriOfTypeUser);
 		List<String>groupschedule=new ArrayList<String>();
 		groupschedule.add(iriOfTypeUser);
 		//Set to ensure no repeats
@@ -187,7 +145,6 @@ public class ResidentialAgent extends JPSHttpServlet {
 		groupschedule.add(lst.get(0));
 		for(int d=0;d<resultListx.size();d++) {
 				if(resultListx.get(d)[5].contentEquals("1")) {
-					//System.out.println("equipment= "+resultListx.get(d)[0]);
 					groupPmax.add(resultListx.get(d)[1]);
 					groupPmin.add(resultListx.get(d)[2]);
 					groupw.add(resultListx.get(d)[3]);
@@ -205,7 +162,6 @@ public class ResidentialAgent extends JPSHttpServlet {
 					countr = 1;
 					if (Integer.parseInt(resultListx.get(d)[5]) < 24) {
 						groupschedule.add(iriOfTypeUser);
-//						groupschedule.add("t"+Integer.toString(Integer.parseInt(resultListx.get(d)[5])+1));
 						groupschedule.add(lst.get(Integer.parseInt(resultListx.get(d)[5])));
 					}
 				}				
---
+++ JPS_DES/test/uk/ac/cam/cares/des/test/Test_AgentsNew.java
@@ -0,0 +1,7 @@
+package uk.ac.cam.cares.des.test;
+
+import junit.framework.TestCase;
+
+public class Test_AgentsNew extends TestCase{
+
+}
