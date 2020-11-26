1b5f0356f9c1a7138950a55da81727ca11cb07e2
Laura Ong <laura.ong@cares.cam.ac.uk>
2020-11-26 15:52:08 +0800

    add testMethod for Residential Agent

--- JPS_DES/test/uk/ac/cam/cares/des/test/Test_AgentsNew.java
+++ JPS_DES/test/uk/ac/cam/cares/des/test/Test_AgentsNew.java
@@ -1,7 +1,11 @@
 package uk.ac.cam.cares.des.test;
 
 import junit.framework.TestCase;
+import uk.ac.cam.cares.jps.des.ResidentialAgent;
 
 public class Test_AgentsNew extends TestCase{
-
+	public void testResidentialAgent() {
+		String iriofnetworkdistrict =  "http://www.theworldavatar.com/kb/sgp/singapore/District-001.owl#District-001";
+		new ResidentialAgent().extractResidentialData(iriofnetworkdistrict,"whatever");
+	}
 }
