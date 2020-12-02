92f39600067c9c7b3a23241b8c55ab63530e7b2b
Aravind <aravind@u.nus.edu>
2020-12-02 18:24:30 +0800

    'removed_war_file'

---
+++ JPS_DIGITAL_TWIN/src/main/test/uk/ac/cam/cares/jps/agent/gPROMSAgentTest.java
@@ -0,0 +1,10 @@
+package uk.ac.cam.cares.jps.agent;
+
+import java.net.URI;
+import org.json.JSONObject;
+import junit.framework.TestCase;
+import uk.ac.cam.cares.jps.base.discovery.AgentCaller;
+
+public class gPROMSAgentTest {
+
+}
