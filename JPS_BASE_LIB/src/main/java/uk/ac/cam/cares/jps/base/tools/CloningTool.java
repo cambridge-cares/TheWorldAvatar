f8b8ac015f5299c79124698c43b88983bc23e9c9
Casper Lindberg <casper.lindberg@cares.cam.ac.uk>
2020-12-03 17:55:32 +0800

    814-kb-cloning-tool: create CloningTool class

---
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/tools/CloningTool.java
@@ -0,0 +1,16 @@
+package uk.ac.cam.cares.jps.base.tools;
+
+import uk.ac.cam.cares.jps.base.query.KnowledgeBaseClient;
+
+public class CloningTool {
+
+	public static void clone(KnowledgeBaseClient sourceKB, KnowledgeBaseClient targetKB) {
+		
+		clone(sourceKB, null, targetKB, null);
+	}
+	
+	public static void clone(KnowledgeBaseClient sourceKB, String sourceGraph, KnowledgeBaseClient targetKB, String targetGraph) {
+	
+		
+	}
+}
