0dd8d9475f4404769dd1f5d33b0eba65e675ef35
Feroz Farazi <msff2@cam.ac.uk>
2020-12-04 00:48:55 +0000

    829-develop-an-abox-creation-tool-using-python: Defined the main entry point in the ConvertLandUseCode.py module.

--- PlantOwl/python/power_plant/ConvertLandUseCode.py
+++ PlantOwl/python/power_plant/ConvertLandUseCode.py
@@ -48,3 +48,49 @@ def convert_lucode():
                   process_data(row)
                else:
                    break
+
+if __name__ == '__main__':
+    convert_lucode()
+
+# """Creates an instance of Graph"""
+# g = Graph()
+# """Assigns the IRI of the OntoLandUse ontology"""
+# land_use_code_iri = "http://www.theworldavatar.com/ontology/ontolanduse/OntoLandUse.owl#"
+# """Binds the IRI with the landusecode literal in the graph"""
+# g.bind("landusecode", land_use_code_iri)
+# """Creates the namespace of lucode AC01"""
+# instance_iri_lucode_namespace = Namespace("http://www.theworldavatar.com/kb/ontolanduse/AC01.owl#")
+# """Assigns the IRI of lucode AC01"""
+# instance_iri_lucode = instance_iri_lucode_namespace.LandUseCode_AC01
+# """Assigns the name of lucode"""
+# instance_name = "AC01"
+# """Creates the namespace of the OntoLandUse ontology"""
+# namespace_onto_land_use = Namespace("http://www.theworldavatar.com/ontology/ontolanduse/OntoLandUse.owl#")
+# g = aboxgen.create_instance(g, namespace_onto_land_use.LandUseCode, instance_iri_lucode, instance_name)
+# instance_iri_admindiv = "http://www.theworldavatar.com/kb/ontolanduse/England.owl#AdministrativeDivision_England"
+# instance_iri_admindiv_namespace = Namespace("http://www.theworldavatar.com/kb/ontolanduse/England.owl#")
+# instance_name = "England"
+# g = aboxgen.create_instance(g, namespace_onto_land_use.AdministrativeDivision, instance_iri_admindiv, instance_name)
+# g = aboxgen.link_instance(g, namespace_onto_land_use.appliedIn, instance_iri_lucode,
+#                           instance_iri_admindiv_namespace.AdministrativeDivision_England)
+# comment = "test comment for the land use code"
+# g = aboxgen.link_data(g, RDFS.comment, instance_iri_lucode_namespace.LandUseCode_AC01, comment)
+#
+#
+# g.bind("foaf", FOAF)
+#
+# bob = URIRef("http://example.org/people#Bob")
+# linda = BNode()  # a GUID is generated
+#
+# name = Literal('Bob')  # passing a string
+# age = Literal(24)  # passing a python int
+# height = Literal(76.5)  # passing a python float
+#
+# g.add((bob, RDF.type, FOAF.Person))
+# g.add((bob, FOAF.name, name))
+# g.add((bob, FOAF.knows, linda))
+# g.add((linda, RDF.type, FOAF.Person))
+# g.add((linda, FOAF.name, Literal("Linda")))
+#
+# file_name = 'test.owl'
+# g.serialize(destination=file_name, format="application/rdf+xml")
