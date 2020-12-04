e9b03d229c5d62c62c4fbd121dc6354bc497dc07
Feroz Farazi <msff2@cam.ac.uk>
2020-12-04 00:41:48 +0000

    829-develop-an-abox-creation-tool-using-python: Declared the ABox excel template column header as constants.

---
+++ PlantOwl/python/power_plant/ConvertLandUseCode.py
@@ -0,0 +1,17 @@
+##########################################
+# Author: Feroz Farazi (msff2@cam.ac.uk) #
+# Date: 04 Dec 2020                      #
+##########################################
+from rdflib import Graph, FOAF, URIRef, BNode, Literal
+from rdflib.namespace import RDF, RDFS, Namespace
+import ABoxGeneration as aboxgen
+from tkinter import Tk  # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+import csv
+
+"""Declared column headers as constants"""
+COLUMN_1 = 'Source'
+COLUMN_2 = 'Type'
+COLUMN_3 = 'Target'
+COLUMN_4 = 'Relation'
+COLUMN_5 = 'Value'
