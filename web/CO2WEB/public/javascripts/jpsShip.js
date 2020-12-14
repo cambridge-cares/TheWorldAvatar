77aec3c371ad8e85e33e648e1780447b49398691
Kok Foong Lee <kflee@cmclinnovations.com>
2020-12-14 15:56:23 +0000

    88-dev-instantiate-weather-stations-and-vs: Consolidate function to create new sensors into a single function

--- web/CO2WEB/public/javascripts/jpsShip.js
+++ web/CO2WEB/public/javascripts/jpsShip.js
@@ -31,12 +31,18 @@ $("#location").on("change", () => {
         map.setZoom(10);
          map.set('maxZoom', 11);
         getRelevantFolder(arrUrl[2], "Singapore");
+        if (testing) {
+            setTimeout(() => initCreateNewSensor(map), 500)
+        }
 
     }else if (mlocation === "Hong Kong") {
         center = new google.maps.LatLng(22.28911086466781,114.1491155592187);
         map.setZoom(10);
          map.set('maxZoom', 11);
         getRelevantFolder(arrUrl[2], "Hong_Kong");
+        if (testing) {
+            setTimeout(() => initCreateNewSensor(map), 500)
+        }
     }
     // var legend = document.createElement("legend");
     // legend.innerHTML = "<h3>Legend</h3>"; 
@@ -87,37 +93,39 @@ function initMap() {
 
     map.controls[google.maps.ControlPosition.LEFT_TOP].push(legend);
     if (testing) {
-        let infoWindow = new google.maps.InfoWindow({
-            content: "Right click on the map to create virtual sensors!",
-            position: center,
-        });
-        infoWindow.open(map);
-        // Configure the click listener.
-        map.addListener("rightclick", (mapsMouseEvent) => {
-            // Close the current InfoWindow.
-            infoWindow.close();
-            // Create a new InfoWindow.
-            infoWindow = new google.maps.InfoWindow({
-                position: mapsMouseEvent.latLng,
-            });
-            var createSensorURL = prefix + "/JPS_DISPERSION/CreateNewSensor";
-            var lat = mapsMouseEvent.latLng.lat;
-            var lng = mapsMouseEvent.latLng.lng;
-            $.get(createSensorURL, { lat, lng }).done(function () {
-                infoWindow.setContent("Sensor created at " +
-                    JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2))
-            });
-            infoWindow.open(map);
-        });
-
-        setTimeout(() => {
-            var bounds = map.getBounds();
-            initStations(bounds);
-        }, 500)
+        setTimeout(() => initCreateNewSensor(map), 500)
     }
 }
 
-function initStations(bounds) {
+function initCreateNewSensor(map) {
+    // initialise the listener to create new stations
+    var bounds = map.getBounds();
+    let infoWindow = new google.maps.InfoWindow({
+        content: "Right click on the map to create virtual sensors!",
+        position: map.getCenter()
+    });
+    infoWindow.open(map);
+    // Configure the click listener.
+    map.addListener("rightclick", (mapsMouseEvent) => {
+        // Close the current InfoWindow.
+        infoWindow.close();
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng,
+        });
+        var createSensorURL = prefix + "/JPS_DISPERSION/CreateNewSensor";
+        var lat = mapsMouseEvent.latLng.lat;
+        var lng = mapsMouseEvent.latLng.lng;
+        $.get(createSensorURL, { lat, lng }).done(function () {
+            infoWindow.setContent("Sensor created at " +
+                JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2))
+        });
+        infoWindow.open(map);
+    });
+    renderSensorsWithinBounds(bounds);
+}
+
+function renderSensorsWithinBounds(bounds) {
     var getSensors = prefix + "/JPS_DISPERSION/GetSensorsWithinBounds";
     var upperx = bounds.getNorthEast().lng;
     var uppery = bounds.getNorthEast().lat;
@@ -157,9 +165,11 @@ function getRelevantFolder(typeOfEmission, city){
             if (city == "Hong_Kong"){
                 city = "hongkong";
             }
-            querySensor(city, function (sensorData) {
-                renderSensorStations(sensorData);
-            });
+            if (!testing) {
+                querySensor(city, function (sensorData) {
+                    renderSensorStations(sensorData);
+                });
+            }
         })
         let agentInformation = prefix + "/JPS/ADMSOutputAllForShips";//"/info"
         console.log(agentInformation);
