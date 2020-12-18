28557a8c6339646cac7bc69689b1a3f66b61b81e
Feroz Farazi <msff2@cam.ac.uk>
2020-12-18 23:04:19 +0000

    844-create-hexagons-with-links-to-lucode: Created the Envelope class.

---
+++ PlantOwl/python/power_plant/Envelope.py
@@ -0,0 +1,7 @@
+##########################################
+# Author: Feroz Farazi (msff2@cam.ac.uk) #
+# Date: 18 Dec 2020                      #
+##########################################
+class Envelope():
+    srsName = ''
+    srsDimension = ''
