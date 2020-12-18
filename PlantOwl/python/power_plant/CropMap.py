3bd2ff382b3774a5a996c5be8330e4435569aee6
Feroz Farazi <msff2@cam.ac.uk>
2020-12-18 23:03:51 +0000

    844-create-hexagons-with-links-to-lucode: Created the CropMap class.

---
+++ PlantOwl/python/power_plant/CropMap.py
@@ -0,0 +1,11 @@
+##########################################
+# Author: Feroz Farazi (msff2@cam.ac.uk) #
+# Date: 18 Dec 2020                      #
+##########################################
+class CropMap():
+    objectID = ''
+    cromeID = ''
+    luCode = ''
+    refDate = ''
+    shapeLength = ''
+    shapeArea = ''
