548473599dddc3b7ced1856bc30a2e39c1f7f87a
Feroz Farazi <msff2@cam.ac.uk>
2021-01-06 11:48:30 +0000

    844-create-hexagons-with-links-to-lucode: Added the linear ring and post list properties and the reader methods.

--- PlantOwl/python/power_plant/GMLParserPropReader.py
+++ PlantOwl/python/power_plant/GMLParserPropReader.py
@@ -10,5 +10,13 @@ config.read('./conf/GMLParser.properties')
 def getNOfMapsInAnAboxFile():
     return config.get('ABOX', 'number.of.maps.in.an.abox.file')
 
+def getClassLinearRing():
+    return config.get('TBOX', 'class.linear.ring')
+
+def getPropertyPosList():
+    return config.get('TBOX', 'property.pos.list')
+
 if __name__ == '__main__':
     print(getNOfMapsInAnAboxFile())
+    print(getClassLinearRing())
+    print(getPropertyPosList())
