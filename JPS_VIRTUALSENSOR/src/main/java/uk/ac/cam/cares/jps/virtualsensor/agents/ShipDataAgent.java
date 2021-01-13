9351a2d8877808cc4cb5dfbde5732145032182d4
Kok Foong Lee <kflee@cmclinnovations.com>
2021-01-13 13:21:44 +0000

    125-create-plymouth-use-case: Initial commit of JPS_VIRTUALSENSOR

---
+++ JPS_VIRTUALSENSOR/src/main/java/uk/ac/cam/cares/jps/virtualsensor/agents/ShipDataAgent.java
@@ -0,0 +1,46 @@
+package uk.ac.cam.cares.jps.virtualsensor.agents;
+
+import javax.servlet.annotation.WebServlet;
+import javax.ws.rs.BadRequestException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import uk.ac.cam.cares.jps.base.agent.JPSAgent;
+import uk.ac.cam.cares.jps.base.region.Scope;
+import uk.ac.cam.cares.jps.virtualsensor.sparql.ShipSparql;
+
+@WebServlet(urlPatterns = {"/ShipDataAgent"})
+public class ShipDataAgent extends JPSAgent {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private static final String keyCollection = "collection";
+    private static final String keyItems = "items";
+    
+    @Override
+    public JSONObject processRequestParameters(JSONObject requestParams) {
+    	JSONObject response = new JSONObject();
+    	if (validateInput(requestParams)) {
+	        Scope sc = new Scope(requestParams);
+	        JSONArray result = ShipSparql.queryShipWithinScope(sc);
+	        JSONObject items = new JSONObject();
+	        items.put(keyItems, result);
+	        response.put(keyCollection, items);
+    	}
+        return response;
+    }
+
+    @Override
+    public boolean validateInput(JSONObject requestParams) {
+    	boolean valid = false;
+    	try {
+    		new Scope(requestParams);
+    		valid = true;
+    	} catch (Exception e) {
+    		throw new BadRequestException();
+    	}
+    	return valid;
+    }
+}
