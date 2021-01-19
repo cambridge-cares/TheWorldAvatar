9ae7d380e4852a575932b7e2b2eb57ef0c476ea1
Russell Foo <iruss1337@gmail.com>
2021-01-19 23:03:44 +0800

    Fixed typo in pyvenv.cmd

--- JPS_SHIP/python/ADMS-speed-load-map/pyvenv.cmd
+++ JPS_SHIP/python/ADMS-speed-load-map/pyvenv.cmd
@@ -7,7 +7,7 @@
 (
        python -m pip install pipenv
        python -m pipenv --rm
-       mkrdir .venv
+       mkdir .venv
        python -m pipenv install --python 3.6 --ignore-pipfile
 ) & rem ^
  Executing on Windows.
