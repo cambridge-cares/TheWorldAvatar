82b028f16057171c0c3972f3ed12c3ae6012d120
Feroz Farazi <msff2@cam.ac.uk>
2021-01-20 15:58:00 +0000

    844-create-hexagons-with-links-to-lucode: Linked to a GUI for selecting the input file and added the centre point of hexagons to the RDF representation.

--- PlantOwl/python/power_plant/GMLParser.py
+++ PlantOwl/python/power_plant/GMLParser.py
@@ -67,15 +67,15 @@ def get_crop_map(context):
     file_counter = 0
     global g
     for event, elem in context:
-        print(elem)
+        #print(elem)
         for map in elem:
-            print(get_tag_name(map.tag))
+            #print(get_tag_name(map.tag))
             cropMap = CropMap()
             cropMap.name = get_tag_name(map.tag)
             cropMap.id = map.attrib[URL_ID]
-            print('cropMap.id',cropMap.id)
+            #print('cropMap.id',cropMap.id)
             for attribute in map:
-                print('attribute.text', attribute.text)
+                #print('attribute.text', attribute.text)
                 if attribute.text == None or attribute.text == '':
                     continue
                 if get_tag_name(attribute.tag.lower()) ==  OBJECT_ID.lower():
@@ -84,42 +84,48 @@ def get_crop_map(context):
                                       URIRef(gmlpropread.getABoxIRI()
                                              + rdfizer.SLASH + rdfizer.format_iri(cropMap.id)),
                                       cropMap.objectID)
-                    print('OBJECT_ID', attribute.text)
+                    #print('OBJECT_ID', attribute.text)
                 if get_tag_name(attribute.tag.lower()) ==  CROME_ID.lower():
                     cropMap.cromeID = attribute.text
                     aboxgen.link_data(g, URIRef(gmlpropread.getCromeIDVocabulary()),
                                       URIRef(gmlpropread.getABoxIRI()
                                              + rdfizer.SLASH + rdfizer.format_iri(cropMap.id)),
                                       cropMap.cromeID)
-                    print('cromeid', attribute.text)
+                    if getcentre_point_from_crome_id(cropMap.cromeID) != None:
+                        aboxgen.link_data(g, URIRef(gmlpropread.getCentrePoint()),
+                                      URIRef(gmlpropread.getABoxIRI()
+                                             + rdfizer.SLASH + rdfizer.format_iri(cropMap.id)),
+                                      getcentre_point_from_crome_id(cropMap.cromeID))
+
+                    #print('cromeid', attribute.text)
                 if get_tag_name(attribute.tag.lower()) ==  LUCODE.lower():
                     cropMap.luCode = attribute.text
                     aboxgen.link_data(g, URIRef(gmlpropread.getLucodeVocabulary()),
                                       URIRef(gmlpropread.getABoxIRI()
                                              + rdfizer.SLASH + rdfizer.format_iri(cropMap.id)),
                                       cropMap.luCode)
-                    print('lucode', attribute.text)
+                    #print('lucode', attribute.text)
                 if get_tag_name(attribute.tag.lower()) ==  REF_DATE.lower():
                     cropMap.refDate = attribute.text
                     aboxgen.link_data(g, URIRef(gmlpropread.getRefDateVocabulary()),
                                       URIRef(gmlpropread.getABoxIRI()
                                              + rdfizer.SLASH + rdfizer.format_iri(cropMap.id)),
                                       cropMap.refDate)
-                    print('refdate', attribute.text)
+                    #print('refdate', attribute.text)
                 if get_tag_name(attribute.tag.lower()) ==  SHAPE_LENGTH.lower():
                     cropMap.shapeLength = attribute.text
                     aboxgen.link_data(g, URIRef(gmlpropread.getShapeLengthVocabulary()),
                                       URIRef(gmlpropread.getABoxIRI()
                                              + rdfizer.SLASH + rdfizer.format_iri(cropMap.id)),
                                       cropMap.shapeLength)
-                    print('Shape_Length', attribute.text)
+                    #print('Shape_Length', attribute.text)
                 if get_tag_name(attribute.tag.lower()) ==  SHAPE_AREA.lower():
                     cropMap.shapeArea = attribute.text
                     aboxgen.link_data(g, URIRef(gmlpropread.getShapeAreaVocabulary()),
                                       URIRef(gmlpropread.getABoxIRI()
                                              + rdfizer.SLASH + rdfizer.format_iri(cropMap.id)),
                                       cropMap.shapeArea)
-                    print('Shape_Area', attribute.text)
+                    #print('Shape_Area', attribute.text)
                 if get_tag_name(attribute.tag.lower()) == SURFACE_PROPERTY.lower():
                     for surface in attribute:
                         for pathces in surface:
@@ -136,7 +142,7 @@ def get_crop_map(context):
                                                               URIRef(gmlpropread.getABoxIRI()
                                                                      + rdfizer.SLASH + rdfizer.format_iri(cropMap.id)),
                                                               posList.text.replace(" ", "#"), URIRef(gmlpropread.getDataTypePolygonalPoints()))
-                                            print(cropMap.polygon)
+                                            #print(cropMap.polygon)
             """Adds data and metadata to the envelope"""
             if map_counter == 0:
                 aboxgen.create_instance(g,
@@ -160,7 +166,7 @@ def get_crop_map(context):
                                         URIRef(gmlpropread.getABoxIRI()+ rdfizer.SLASH
                                         + ENVELOPE_INSTANCE_PREFIX + rdfizer.format_iri(cropMap.name)),
                                         envelope.upperCorner.replace(' ', '#'))
-            """Link each feature to the envelope"""
+            """Links each feature to the envelope"""
             aboxgen.link_instance(g, URIRef(gmlpropread.getBoundedBy()),
                                   URIRef(gmlpropread.getABoxIRI() + rdfizer.SLASH
                                   + rdfizer.format_iri(cropMap.id)),
@@ -171,10 +177,19 @@ def get_crop_map(context):
             save_into_disk(g, file_counter)
             file_counter += 1
             g = Graph()
-            print('Total number of files:', file_counter)
+            print('Total number of feature members processed:', map_counter)
     if map_counter % int(gmlpropread.getNOfMapsInAnAboxFile()) != 0:
         save_into_disk(g, file_counter)
-        print('Total number of maps:', map_counter)
+        print('Total number of feature members processed:', map_counter)
+
+"""Extracts the centre point from the CROME ID"""
+def getcentre_point_from_crome_id(cromeID):
+    coordinates = cromeID[3:]
+    if len(coordinates) % 2 == 0:
+        latitude = coordinates[:int(len(coordinates)/2)+1]
+        longitude = coordinates[int(len(coordinates)/2):]
+        return latitude+'#'+longitude
+    return None
 
 """Saves a graph into the disk/file system"""
 def save_into_disk(g, file_counter):
@@ -198,33 +213,12 @@ def get_tag_name(url):
 
 """Parses a standard GML file consisting of an Envelope and a set of feature members"""
 def parse_gml(file_name):
+    print('Parsing in progress...')
     context = get_context(file_name, URL_ENVELOPE)
-    envelope = get_envelope(context)
-    if envelope.srsName != None or envelope.srsName != '':
-        print(envelope.srsName)
-    if envelope.srsDimension !=None or envelope.srsDimension != '':
-        print(envelope.srsDimension)
+    get_envelope(context)
     context = get_context(file_name, URL_FEATURE_MEMBER)
     get_crop_map(context)
 
-# for event, elem in context:
-#     srsName = elem.attrib['srsName']
-#     print('Id', srsName)
-#     for c in elem:
-#         print('c',c)
-#         print("c.attrib['k']", c.attrib['k'])
-#         if c.attrib['k'] == 'created_by' or c.attrib['k'] == 'source':  # We don't want such tags to keep in our DB.
-#             continue
-#
-#         key = c.attrib['k']  # These are basically the tags inside the nodes having key and values
-#         val = c.attrib['v']
-    # You can do more filtering here if you want specific keys or values. Like if you want only 'atms' then filter the val with 'atm' using conditions.
-
-    # Store the information in file or db or wherever you wanna use it.
-
-    # elem.clear()
-
 """This block of code is the access point to this Python module"""
 if __name__ == '__main__':
-    file_name = 'Crop_Map_of_England_2018_Cambridgeshire_snippet.gml'
-    parse_gml(file_name)
+    parse_gml(rdfizer.select_file())
