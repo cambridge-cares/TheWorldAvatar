a2f6072e0db803d97d94ca1774b701bf308727b8
Kok Foong Lee <kflee@cmclinnovations.com>
2021-01-22 15:38:03 +0000

    dev-virtualsensor: Build python virtual environment for virtual sensor

---
+++ JPS_VIRTUALSENSOR/python_venv/pyvenv.cmd
@@ -0,0 +1,17 @@
+:;( #
+  :;  python -m pip install pipenv #
+  :;  python -m pipenv --rm #
+  :;  mkdir .venv # 
+  :;  python -m pipenv install --python 3.6 -r requirements_unix.txt  #
+  :;  mkdir ~/Sensor_venv
+  :;  cp .venv ~/Sensor_venv -r
+:; );<<'Executing on Unix-like OS'
+(
+	   python -m pip install pipenv
+       python -m pipenv --rm
+	   mkdir .venv
+       python -m pipenv install --python 3.6 --ignore-pipfile -r requirements_windows.txt
+	   mkdir "%systemdrive%%homepath%\Sensor_venv"
+	   xcopy ".venv" "%systemdrive%%homepath%\Sensor_venv" /s /Y >NUL
+) & rem ^
+ Executing on Windows.
