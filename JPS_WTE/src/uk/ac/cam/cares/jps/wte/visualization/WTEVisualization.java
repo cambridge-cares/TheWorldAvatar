f3c826eeed5e135eeaabb8ab86263ca59c6d23e0
Laura Ong <laura.ong@cares.cam.ac.uk>
2021-02-04 16:40:51 +0800

    Fix WTE with JPSAgent

--- JPS_WTE/src/uk/ac/cam/cares/jps/wte/visualization/WTEVisualization.java
+++ JPS_WTE/src/uk/ac/cam/cares/jps/wte/visualization/WTEVisualization.java
@@ -15,8 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
-import uk.ac.cam.cares.jps.base.discovery.AgentCaller;
-import uk.ac.cam.cares.jps.base.scenario.JPSHttpServlet;
 import uk.ac.cam.cares.jps.base.util.InputValidator;
 import uk.ac.cam.cares.jps.wte.FCQuerySource;
 import uk.ac.cam.cares.jps.wte.WastetoEnergyAgent;
@@ -55,7 +53,7 @@ public class WTEVisualization extends JPSAgent{
 		System.gc();
 		return responseParams;
 	}
-//	@Override
+	@Override
     public boolean validateInput(JSONObject requestParams) throws BadRequestException {
         if (requestParams.isEmpty()) {
             throw new BadRequestException();
