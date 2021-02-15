dc36027d0ad35f67f8d97c2726748ab1ffcf9697
Feroz Farazi <msff2@cam.ac.uk>
2021-02-15 17:56:08 +0000

    844-create-hexagons-with-links-to-lucode: Created two functions: initial_lat() and marc().

--- PlantOwl/python/power_plant/CoordinateConversion.py
+++ PlantOwl/python/power_plant/CoordinateConversion.py
@@ -6,6 +6,7 @@
 """This module is defined to convert coordinates from a Coordinate Reference System (CRS)
 into another. Currently, it supports the following features:
 - EPSG:27700 to WGS84"""
+from math import sin, cos
 
 """Coordinate conversion constants are defined here to convert from EPSG:27700 to WGS84"""
 """Semi-major axis, a"""
@@ -24,3 +25,21 @@ DecimalPHI0 = 49.00000000
 DecimalLAM0 = -2.00000000
 """Mathematical Constant, Pi"""
 Pi = 3.14159265358979
+
+
+def initial_lat(northing, aF0, RadPHI0, n, bF0):
+    PHI1 = ((northing - N0) / aF0) + DecimalPHI0
+    M = marc(bF0, n, PHI1)
+    PHI2 = ((northing - N0 - M) / aF0) + PHI1
+    """Calculate the final value of initial latitude"""
+    while abs(northing - N0 - M) > 0.00001:
+        PHI2 = ((northing - N0 - M) / aF0) + PHI1
+        M = marc(bF0, n, RadPHI0, PHI2)
+        PHI1 = PHI2
+    return PHI2
+
+def marc(bF0, n, RadPHI0, PHI1):
+    return bF0 * (((1 + n + ((5 / 4) * (n ^ 2)) + ((5 / 4) * (n ^ 3))) * (PHI1 - RadPHI0))
+                  - (((3 * n) + (3 * (n ^ 2)) + ((21 / 8) * (n ^ 3))) * (sin(PHI1 - RadPHI0)) * (cos(PHI1 + RadPHI0)))
+                  + ((((15 / 8) * (n ^ 2)) + ((15 / 8) * (n ^ 3))) * (sin(2 * (PHI1 - RadPHI0))) * (cos(2 * (PHI1 + RadPHI0))))
+                  - (((35 / 24) * (n ^ 3)) * (sin(3 * (PHI1 - RadPHI0))) * (cos(3 * (PHI1 + RadPHI0)))))
