13f21f649167b21865d771488a2d7b1cb78eff60
Feroz Farazi <msff2@cam.ac.uk>
2021-02-19 10:46:54 +0000

    832-implement-kg-router: Added logic in the method getKnowledgeBaseClient() to return either the instance of RemoteKBClient or FileBasedKBClient.

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/query/KGRouter.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/query/KGRouter.java
@@ -1,5 +1,7 @@
 package uk.ac.cam.cares.jps.base.query;
 
+import java.io.File;
+
 public class KGRouter{
 	public static final String HTTP="http://";
 	public static final String HTTPS="https://";
@@ -8,36 +10,55 @@ public class KGRouter{
 	static KGRouter kgRouter = null;
 	
 	/**
-	 * Based on a target resource provided as the input, it returns the<br>
+	 * Based on a target resource IRI or path provided as the input, it returns the<br>
 	 * corresponding KnowledgeBaseClient. It supports two types of resources:<br>
-	 * a) repositories/namespaces and b) ontology files. Some examples of these<br>
+	 * a) a repository/namespace and b) an ontology file. Some examples of these<br>
 	 * resources are provided below:<br>
-	 * a) some example repositories/namespaces are:<br>
+	 * a) Example repositories/namespaces are:<br>
 	 *    - http://ontokin
 	 *    - http://ontospecies
 	 *    - http://ontocompchem
-	 * b) some example ontology files are:<br>
+	 * b) Example ontology files are:<br>
 	 *    - C://path/to/an/abox.owl (On Windows)
 	 *    - /home/path/to/an/abox.owl (On Linux)
 	 * 
-	 * @param targetResource
+	 * @param targetResourceIRIOrPath the IRI of an RDF/OWL repository/namespace or the path to an RDF/OWL file. 
+	 * @param isQueryOperation true/false
+	 * @param isUpdateOperation true/false
 	 * @return
 	 */
-	public static KnowledgeBaseClient getKnowledgeBaseClient(String targetResource, boolean isQueryOperation, boolean isUpdateOperation){
+	public static KnowledgeBaseClient getKnowledgeBaseClient(String targetResourceIRIOrPath, boolean isQueryOperation, boolean isUpdateOperation){
 		String queryIRI = null;
 		String updateIRI = null;
-		if(targetResource!=null && (targetResource.trim().startsWith(HTTP))){
-			if(kgRouter==null){
-				kgRouter = new KGRouter();
-			}
-			if(isQueryOperation){
-				queryIRI = kgRouter.getQueryIRI(KGROUTER_ENDPOINT, targetResource.replace(HTTP, EMPTY));
-			}
-			if(isUpdateOperation){
-				updateIRI = kgRouter.getUpdateIRI(KGROUTER_ENDPOINT, targetResource.replace(HTTP, EMPTY));
+		KnowledgeBaseClient kbClient = null;
+		if (targetResourceIRIOrPath != null && !targetResourceIRIOrPath.isEmpty()) {
+			if (targetResourceIRIOrPath.trim().startsWith(HTTP)) {
+				if (kgRouter == null) {
+					kgRouter = new KGRouter();
+				}
+				if (isQueryOperation) {
+					queryIRI = kgRouter.getQueryIRI(KGROUTER_ENDPOINT, targetResourceIRIOrPath.replace(HTTP, EMPTY));
+				}
+				if (isUpdateOperation) {
+					updateIRI = kgRouter.getUpdateIRI(KGROUTER_ENDPOINT, targetResourceIRIOrPath.replace(HTTP, EMPTY));
+				}
+				if (queryIRI != null && !queryIRI.isEmpty()) {
+					kbClient = new RemoteKnowledgeBaseClient(queryIRI);
+				}
+				if (updateIRI != null && !updateIRI.isEmpty()) {
+					if (kbClient == null) {
+						kbClient = new RemoteKnowledgeBaseClient();
+					}
+					kbClient.setUpdateEndpoint(updateIRI);
+				}
+			}else{
+				File file = new File(targetResourceIRIOrPath);
+				if(file.exists()){
+					kbClient = new FileBasedKnowledgeBaseClient(targetResourceIRIOrPath);
+				}
 			}
 		}
-		return null;
+		return kbClient;
 	}
 	
 	/**
