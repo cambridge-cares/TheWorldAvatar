4e31a09e67e1fb5468c1b58283eb770d27b1cb0d
Kok Foong Lee <kflee@cmclinnovations.com>
2021-02-23 11:56:08 +0000

    91-move-chimney-to-triplestore: Initialisation of ships now includes chimney waste stream info

---
+++ JPS_VIRTUALSENSOR/src/main/java/uk/ac/cam/cares/jps/virtualsensor/configuration/SparqlAuthentication.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.cares.jps.virtualsensor.configuration;
+
+public class SparqlAuthentication {
+    private static String user = "bg_user";
+    private static String password = "123456";
+
+	public static String getUser() {
+		return user;
+	}
+	public static String getPassword() {
+		return password;
+	}
+}
