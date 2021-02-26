5d208f33541f30f31a99c26a057dba16fe961e75
Daniel Nurkowski <danieln@cmclinnovations.com>
2021-02-26 16:25:37 +0000

    924-python-java-wrapper: Added wrapper tests.

--- JPS_BASE_LIB/python_wrapper/py4jps/tests/test_wrapper.py
+++ JPS_BASE_LIB/python_wrapper/py4jps/tests/test_wrapper.py
@@ -1,15 +1,29 @@
 import unittest
+from py4jps.main import Gateway
+from os import path
 
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 # placeholder class for any unit tests
 class TestWrapper(unittest.TestCase):
 
     def test_1(self):
-        pass
+        gateway = Gateway()
+        gateway.start()
+        module1_view = gateway.createModuleView()
+        gateway.importJava(module1_view,'uk.ac.cam.cares.jps.base.util.*')
 
-    def test_2(self):
-        pass
+        FileUtil = module1_view.FileUtil
+
+        file_str = FileUtil.readFileLocally(path.join(path.dirname(__file__),'test_file1.txt'))
+        self.assertEqual("test file1", file_str)
+        gateway.shutdown()
+
+def runTests():
+    suite = unittest.TestSuite()
+    suite.addTest(TestWrapper('test_1'))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
 
 if __name__ == '__main__':
-    unittest.main()
+    runTests()
