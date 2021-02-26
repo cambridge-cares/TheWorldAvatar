f14fe113cc11d4c7b128c379bdd6221697295625
Daniel Nurkowski <danieln@cmclinnovations.com>
2021-02-26 16:57:50 +0000

    924-python-java-wrapper: Added exclude "test" in the setup.py.

--- JPS_BASE_LIB/python_wrapper/setup.py
+++ JPS_BASE_LIB/python_wrapper/setup.py
@@ -9,6 +9,6 @@ setup(
     license='',
     python_requires='>=3.5, <3.8',
     long_description=open('README.md').read(),
-    packages=find_packages(),
+    packages=find_packages(exclude=('tests')),
     include_package_data=True
 )
