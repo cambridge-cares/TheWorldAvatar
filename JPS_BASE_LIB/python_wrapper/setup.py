a3f0a60fafd84dc3a2020d4ae396117e29d6febd
Daniel Nurkowski <danieln@cmclinnovations.com>
2021-03-02 12:48:43 +0000

    924-python-java-wrapper: Added py4j dependency to the setup.py file.

--- JPS_BASE_LIB/python_wrapper/setup.py
+++ JPS_BASE_LIB/python_wrapper/setup.py
@@ -10,5 +10,6 @@ setup(
     python_requires='>=3.5, <3.8',
     long_description=open('README.md').read(),
     packages=find_packages(exclude=('tests')),
+    install_requires= ['py4j==0.10.9.1'],
     include_package_data=True
 )
