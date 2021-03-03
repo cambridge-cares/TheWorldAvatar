94d68376ded3035923b93a07097165dd9d7970e8
Casper Lindberg <casper.lindberg@cares.cam.ac.uk>
2021-03-03 15:25:25 +0800

    814-kb-cloning-tool: allow a customized construct query to be passed to the CloningTool as an argument

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/tools/CloningTool.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/tools/CloningTool.java
@@ -7,10 +7,13 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.sparql.core.Var;
 import org.apache.jena.update.UpdateRequest;
 
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.query.KnowledgeBaseClient;
 
 /**
- * This tool clones a graph from one repository to another.
+ * This tool clones triples from a source KnowledgeBaseClient to a graph in a  
+ * target KnowledgeBaseClient. Either a graph url (named or null) in the source 
+ * KnowledgeBaseClient or a sparql construct query is specified.  
  * 
  * @author Casper Lindberg
  *
@@ -18,38 +21,58 @@ import uk.ac.cam.cares.jps.base.query.KnowledgeBaseClient;
 public class CloningTool {
 	
 	/**
-	 * Clone all triples from source repository to target repository. Note: context is lost!
-	 * @param sourceKB
+	 * Clone triples from the source knowledge base that match the supplied sparql query  
+	 * pattern to a graph (named or default) in the target knowledge base. 
+	 * @param sourceKB 
+	 * @param constructQuery
 	 * @param targetKB
+	 * @param targetGraph url (null for default graph)
 	 */
-	/* // It is better that the graph is explicitly specified.  
-	public static void clone(KnowledgeBaseClient sourceKB, KnowledgeBaseClient targetKB) {
+	public static void clone(KnowledgeBaseClient sourceKB, Query constructQuery, KnowledgeBaseClient targetKB, String targetGraph) {
 		
-		clone(sourceKB, null, targetKB, null);
+		if(!constructQuery.isConstructType()) {
+			throw new JPSRuntimeException("CloningTool: query must be CONSTRUCT type!");
+		}
+		
+		performClone(sourceKB, constructQuery, targetKB, targetGraph);
 	}
-	*/
 	
 	/**
-	 * Clone a named graph from the source knowledge base to a named graph in the target knowledge base.
+	 * Clone a named graph from the source knowledge base to a graph (named or default) 
+	 * in the target knowledge base.
 	 * @param sourceKB
-	 * @param sourceGraph
+	 * @param sourceGraph url (null for default graph)
 	 * @param targetKB
-	 * @param targetGraph
+	 * @param targetGraph url (null for default graph)
 	 */
 	public static void clone(KnowledgeBaseClient sourceKB, String sourceGraph, KnowledgeBaseClient targetKB, String targetGraph) {
 	
-		//Get model using construct query
-		Query construct = buildSparqlConstruct(sourceGraph);
-		Model results = sourceKB.queryConstruct(construct);
+		//Get default construct query
+		Query constructQuery = buildSparqlConstruct(sourceGraph);
+		
+		performClone(sourceKB, constructQuery, targetKB, targetGraph);
+	}
+	
+	/**
+	 * Perform clone using sparql construct query and update.
+	 * @param sourceKB 
+	 * @param constructQuery
+	 * @param targetKB
+	 * @param targetGraph url
+	 */
+	private static void performClone(KnowledgeBaseClient sourceKB, Query constructQuery, KnowledgeBaseClient targetKB, String targetGraph) {
+		
+		//Get model from source
+		Model triples = sourceKB.queryConstruct(constructQuery);
 		
 		//Update target
-		UpdateRequest update = buildSparqlUpdate(targetGraph, results);
+		UpdateRequest update = buildSparqlUpdate(targetGraph, triples);
 		targetKB.executeUpdate(update);
 	}
 	
 	/**
-	 * Build sparql construct query to get triples.
-	 * @param graph/context (optional)
+	 * Build a sparql construct query to get triples.
+	 * @param graph/context url (can be null)
 	 * @return construct query
 	 */
 	private static Query buildSparqlConstruct(String graph) {
@@ -75,24 +98,23 @@ public class CloningTool {
 	}
 	
 	/**
-	 * Build sparql update to insert triples.
-	 * @param graph
-	 * @param results
-	 * @return updaterequest
+	 * Build a sparql update to insert triples into a graph (named or default).
+	 * @param graph url (can be null)
+	 * @param triples
+	 * @return UpdateRequest
 	 */
-	private static UpdateRequest buildSparqlUpdate(String graph, Model results) {
+	private static UpdateRequest buildSparqlUpdate(String graph, Model triples) {
 		
-		// Build update
 		UpdateBuilder builder = new UpdateBuilder();
 				
 		// Add insert
 		if (graph == null) {
 			//Default graph
-			builder.addInsert(results);
+			builder.addInsert(triples);
 		}else {	
 			//Named graph
 			String graphURI = "<" + graph + ">";
-			builder.addInsert(graphURI, results);	
+			builder.addInsert(graphURI, triples);	
 		}
 	
 		return builder.buildRequest();
