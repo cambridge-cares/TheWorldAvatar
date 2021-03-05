c70bc1e960ec139742ae5db8a5a71ff5a0187a34
Russell Foo <iruss1337@gmail.com>
2021-03-05 09:33:34 +0800

    Completed FangXu's unit tests with proper package names. Commiting them via this branch. Will delete the previous branch 842-dev-unit-tests-for-jpsbaselib.

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/region/test/RegionTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/region/test/RegionTest.java
@@ -1,20 +1,87 @@
 package uk.ac.cam.cares.jps.base.region.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import org.json.JSONObject;
+
+import uk.ac.cam.cares.jps.base.region.Region;
+import uk.ac.cam.cares.jps.base.util.CRSTransformer;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
+public class RegionTest {
+	
+	
+	@Test
+	public void testPutRegion() {
+		
+		JSONObject json = new JSONObject();
+		
+		// test Singapore-ADMS
+		Region.putRegion(json, 1);
+		assertEquals(json.get("region").toString(), 
+				"{\"uppercorner\":{\"uppery\":\"143305.896\","
+								+ "\"upperx\":\"11564077.989\"},"
+				+ "\"srsname\":\"EPSG:3857\","
+				+ "\"lowercorner\":{\"lowery\":\"140107.739\","
+								+ "\"lowerx\":\"11560879.832\"}}");
+		
+		// test Singapore-Episode
+		Region.putRegion(json, 2);
+		assertEquals(json.get("region").toString(), 
+				"{\"uppercorner\":{\"uppery\":\"151860.32\","
+								+ "\"upperx\":\"11572101.89\"},"
+				+ "\"srsname\":\"EPSG:3857\","
+				+ "\"lowercorner\":{\"lowery\":\"131707.739\","
+								+ "\"lowerx\":\"11552101.832\"}}");
+		
+		// test Hong Kong-ADMS
+		Region.putRegion(json, 3);
+		assertEquals(json.get("region").toString(), 
+				"{\"uppercorner\":{\"uppery\":\"2550426.72\","
+								+ "\"upperx\":\"12711879.81\"},"
+				+ "\"srsname\":\"EPSG:3857\","
+				+ "\"lowercorner\":{\"lowery\":\"2545200.172\","
+								+ "\"lowerx\":\"12706653.262\"}}");
+		
+		// test Hong Kong-Episode
+		Region.putRegion(json, 4);
+		assertEquals(json.get("region").toString(), 
+				"{\"uppercorner\":{\"uppery\":\"2562555.26\","
+								+ "\"upperx\":\"12720578.56\"},"
+				+ "\"srsname\":\"EPSG:3857\","
+				+ "\"lowercorner\":{\"lowery\":\"2534900.06\","
+								+ "\"lowerx\":\"12694101.21\"}}");
+	}
 
-import junit.framework.TestCase;
-import uk.ac.cam.cares.jps.base.region.Region;
-import uk.ac.cam.cares.jps.base.util.CRSTransformer;
+	@Test
+	public void testPutRegionAndStation() {
+		
+		JSONObject json = new JSONObject();
+		
+		// test Singapore-ADMS
+		Region.putRegionAndStation(json, 1);
+		assertEquals(json.get("airStationIRI"), "http://www.theworldavatar.com/kb/sgp/singapore/AirQualityStation-001.owl#AirQualityStation-001");		
+		
+		// test Singapore-Episode
+		Region.putRegionAndStation(json, 2);
+		assertEquals(json.get("airStationIRI"), "http://www.theworldavatar.com/kb/sgp/singapore/AirQualityStation-002.owl#AirQualityStation-002");
+		
+		// test Hong Kong-ADMS
+		Region.putRegionAndStation(json, 3);
+		assertEquals(json.get("airStationIRI"), "http://www.theworldavatar.com/kb/hkg/hongkong/AirQualityStation-001.owl#AirQualityStation-001");
+		
+		// test Hong Kong-Episode
+		Region.putRegionAndStation(json, 4);
+		assertEquals(json.get("airStationIRI"), "http://www.theworldavatar.com/kb/hkg/hongkong/AirQualityStation-002.owl#AirQualityStation-002");
+		
+	}
 
-public class RegionTest extends TestCase {
-    /** 
-     * This test ensures that the method getTargetCRSName gives the correct CRS for the right combinations of cities and models
-     */
     @Test
     public void testGetTargetCRSName() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         // agent IRI
@@ -51,4 +118,5 @@ public class RegionTest extends TestCase {
         assertEquals(getSRTM.invoke(reg, Region.SINGAPORE_IRI),srtm_sg);
         assertEquals(getSRTM.invoke(reg, Region.HONG_KONG_IRI),srtm_hk);
     }
+
 }
