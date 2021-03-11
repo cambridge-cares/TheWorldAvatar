3108b444de8563a741306f4c45d8b1be207ab8aa
Feroz Farazi <msff2@cam.ac.uk>
2021-03-11 19:11:19 +0000

    840-develop-an-agent-to-resolve-iris-in-aboxes: Created a method called createABox() to bundle all retrieved data and object properties in an ABox.

--- Agents/IRIResolutionAgent/src/main/java/uk/ac/cam/cares/jps/agent/iri/resolution/IRIResolutionAgent.java
+++ Agents/IRIResolutionAgent/src/main/java/uk/ac/cam/cares/jps/agent/iri/resolution/IRIResolutionAgent.java
@@ -8,15 +8,24 @@ import org.apache.jena.arq.querybuilder.SelectBuilder;
 import org.apache.jena.arq.querybuilder.WhereBuilder;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cmclinnovations.ontology.model.aboxes.ABoxManagement;
+import com.cmclinnovations.ontology.model.aboxes.IABoxManagement;
+import com.cmclinnovations.ontology.model.exception.ABoxManagementException;
 
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.query.KGRouter;
 import uk.ac.cam.cares.jps.base.query.KnowledgeBaseClient;
-import uk.ac.cam.cares.jps.base.query.RemoteKnowledgeBaseClient;
 
 /**
  * IRI Resolution Agent is developed to ensure the resolvability of all IRIs<br>
@@ -49,6 +58,11 @@ public class IRIResolutionAgent extends JPSAgent{
 	public static final String GT = ">";
 	public static final String ONTO_PREFIX = "onto";
 	
+	public static final String LOCALHOST_8080 = "http://localhost:8080";
+	public static final String ACTUAL_HOST = "http://www.theworldavatar.com";
+	
+	private IABoxManagement aBoxManager; 
+	
 	/**
 	 * Receives a short IRI among two types of IRIs and resolves it by querying<br>
 	 * the JPS knowledge graph stored in a polymorphic knowledge store. 
@@ -59,10 +73,10 @@ public class IRIResolutionAgent extends JPSAgent{
 	@RequestMapping(SHORT_JPS_IRI)
 	@ResponseBody
 	public String resolveShortIRI(HttpServletRequest request) {
-		String repositoryOrNamespace = retrieveRepositoryOrNamespace(request.getRequestURI());
-		System.out.println("Requested IRI: "+request.getRequestURI());
-		return repositoryOrNamespace + " " + request.getRequestURL();
-	}
+		// If any user runs the IRI resolver from a localhost, the URL will<br>
+		// contain the localhost address, which will be replaced with the<br>
+		// actual address indicated by the ACTUAL_HOST constant.  
+		return getResourceTarget(request.getRequestURL().toString().replaceAll(LOCALHOST_8080, ACTUAL_HOST));	}
 	
 	/**
 	 * Receives a long IRI among two types of IRIs and resolves it by querying<br>
@@ -74,9 +88,10 @@ public class IRIResolutionAgent extends JPSAgent{
 	@RequestMapping(MEDIUM_JPS_IRI)
 	@ResponseBody
 	public String resolveMediumIRI(HttpServletRequest request){
-		String repositoryOrNamespace = retrieveRepositoryOrNamespace(request.getRequestURI());
-		System.out.println("Requested IRI: "+request.getRequestURI());
-		return repositoryOrNamespace + " " + request.getRequestURL().toString();
+		// If any user runs the IRI resolver from a localhost, the URL will<br>
+		// contain the localhost address, which will be replaced with the<br>
+		// actual address indicated by the ACTUAL_HOST constant.  
+		return getResourceTarget(request.getRequestURL().toString().replaceAll(LOCALHOST_8080, ACTUAL_HOST));
 	}
 
 	/**
@@ -89,10 +104,10 @@ public class IRIResolutionAgent extends JPSAgent{
 	@RequestMapping(LONG_JPS_IRI)
 	@ResponseBody
 	public String resolveLongIRI(HttpServletRequest request){
-		String repositoryOrNamespace = retrieveRepositoryOrNamespace(request.getRequestURI());
-		System.out.println("Requested IRI: "+request.getRequestURI());
-		return repositoryOrNamespace + " " + request.getRequestURL().toString();
-	}
+		// If any user runs the IRI resolver from a localhost, the URL will<br>
+		// contain the localhost address, which will be replaced with the<br>
+		// actual address indicated by the ACTUAL_HOST constant.  
+		return getResourceTarget(request.getRequestURL().toString().replaceAll(LOCALHOST_8080, ACTUAL_HOST));	}
 
 	/**
 	 * Extracts the name of repository or namespace from the current IRI<br>
@@ -103,8 +118,8 @@ public class IRIResolutionAgent extends JPSAgent{
 	 */
 	public String retrieveRepositoryOrNamespace(String iri){
 		String[] tokens = iri.split("/");
-		if(tokens.length>1 && tokens[0].length()>5){
-			return tokens[0];
+		if(tokens.length>1 && tokens[0].length()<5){
+			return tokens[2];
 		}else{
 			return null;
 		}
@@ -118,16 +133,16 @@ public class IRIResolutionAgent extends JPSAgent{
 	 * @return
 	 * @throws Exception
 	 */
-	private String getResourceTarget(String IRI){
+	private String getResourceTarget(String IRI) {
 		KnowledgeBaseClient kbClient = KGRouter.getKnowledgeBaseClient(KGRouter.HTTP_KB_PREFIX.concat(getKBName(IRI)), true, false);
 		String json = kbClient.execute(formIRIResolutionQuery(IRI));
+		System.out.println(json);
 		JSONArray jsonArray = new JSONArray(json);
-		for (int i = 0; i<jsonArray.length(); i++){
-			JSONObject obj = jsonArray.getJSONObject(i);
-//			if(obj.getString(LABEL).equals(targetResourceName)){
-//				System.out.println(obj.get(QUERY_ENDPOINT));
-//				return obj.getString(QUERY_ENDPOINT);
-//			}
+		try {
+			return createABox(IRI, jsonArray);
+		} catch (OWLOntologyCreationException | OWLOntologyStorageException | ABoxManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return null;
 	}
@@ -181,6 +196,31 @@ public class IRIResolutionAgent extends JPSAgent{
 	}
 	
 	/**
+	 * Creates an ABox when all object and data properties passed to this<br>
+	 * method in the jsonArray parameter.
+	 * 
+	 * @param aBoxIRI
+	 * @param jsonArray
+	 * @return
+	 * @throws OWLOntologyCreationException
+	 * @throws OWLOntologyStorageException
+	 * @throws ABoxManagementException
+	 */
+	private String createABox(String aBoxIRI, JSONArray jsonArray) throws OWLOntologyCreationException, OWLOntologyStorageException, ABoxManagementException {
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology = manager.createOntology(IRI.create(aBoxIRI));
+		if(aBoxManager == null){
+			aBoxManager = new  ABoxManagement();
+		}
+		java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();    
+		System.setOut(new java.io.PrintStream(out));  
+		manager.saveOntology(ontology, System.out);
+		System.err.println(out.toString());
+		String ontologyInString = out.toString().replaceAll(" rdf:datatype=\"http://www.w3.org/2001/XMLSchema#string\"", "");
+		return ontologyInString;
+	}
+	
+	/**
 	 * Receives and processes HTTP requests that match with the URL patterns<br>
 	 * listed in the annotations of this class.
 	 * 
@@ -212,9 +252,5 @@ public class IRIResolutionAgent extends JPSAgent{
             throw new BadRequestException();
         }
         return true;
-    }
-    
-    public static void main(String[] args){
-    	new IRIResolutionAgent().getResourceTarget("http://www.theworldavatar.com/kb/ontokin/POLIMI_H2CO_1412.owl/ArrheniusCoefficient_182161099217501");
     }
 }
