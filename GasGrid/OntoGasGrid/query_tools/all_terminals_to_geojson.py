2112fb6ccc294f003f063636197251a6e1d59287
Tom Savage <trs53@cam.ac.uk>
2021-03-12 17:43:02 +0000

    dev-ontology-gas-grid-trs53: better documentation and flexibility for geoJSON file production

--- GasGrid/OntoGasGrid/query_tools/all_terminals_to_geojson.py
+++ GasGrid/OntoGasGrid/query_tools/all_terminals_to_geojson.py
@@ -5,84 +5,99 @@ import time
 import numpy as np 
 import pandas as pd
 
-'''
-First query location, name, and order of all PipeConnections
---------------------------------------------------------
-'''
-
-queryString = """PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX ns1:     <http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#>
-PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX gasgrid: <http://www.theworldavatar.com/ontology/ontogasgrid/gas_network_system.owl#>
-PREFIX loc:     <http://www.bigdata.com/rdf/geospatial/literals/v1#>
-PREFIX geo:     <http://www.bigdata.com/rdf/geospatial#>
-PREFIX comp:	<http://www.theworldavatar.com/ontology/ontogasgrid/gas_network_components.owl#>
-
-SELECT ?location ?label
-WHERE
-{
-?term rdf:type comp:GasTerminal.
-?term rdfs:label ?label.
-?term loc:lat-lon ?location.
-}"""
- 
-sparql = SPARQLWrapper("http://www.theworldavatar.com/blazegraph/namespace/ontogasgrid/sparql")
-sparql.setReturnFormat(JSON) # return in JSON form
-sparql.setQuery(queryString) # setting query
-start = time.time()
-print('Querying...')
-ret = sparql.queryAndConvert() # performing query and returning to JSON
-end = time.time()
-print('Finished in ',np.round(end-start,2),' seconds')
-# extracting data from JSON 
-ret = ret['results']['bindings']
-num_ret = len(ret)
-ret_array = np.zeros((num_ret,3),dtype='object')
-header = ['lat','lon','name']
-for i in tqdm(range(num_ret)):
-    lat,lon = ret[i]['location']['value'].split('#')
-    ret_array[i,:] = [lat,lon,ret[i]['label']['value']]
-ret = pd.DataFrame(ret_array,columns=header).values
 
 
+def query_to_geoJSON(class_namespace,class_name,class_label,endpoint):
+  '''
+  DESCRIPTION: 
+  Produces a .geoJSON file by querying a triple-store for a specific class
+  with the attribute lat-lon from the namespace http://www.bigdata.com/rdf/geospatial/literals/v1#
+  If you encode your locations using alternative methods you will have to change this.
+  The .geoJSON file produced encodes single point locations and can include their attributes.
 
-# start of geoJSON file 
-geojson_file = """
-{
-  "type": "FeatureCollection",
-  "features": ["""
+  INPUTS:
+  class_URI:      The URI of the class you want to produce a geoJSON file of (with namespace)
+  class_label:    The name of the class you are querying, used to name the final geoJSON file
 
-for i in range(num_ret):
-    feature = """{
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#7e7e7e",
-        "marker-size": "medium",
-        "marker-symbol": "",
-        "name": "%s"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          %s,
-          %s
-        ]
-      }
-    },"""%(ret[i,2],ret[i,1],ret[i,0])
-    geojson_file += '\n'+feature
+  OUTPUTS:
+  A file of the form class_label.geoJSON in the working directory. 
+  '''
+  # defining the query string given the class URI
+  queryString = """PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX loc:     <http://www.bigdata.com/rdf/geospatial/literals/v1#>
+  PREFIX ns:   <%s>
 
-# removing last comma as is last line
-geojson_file = geojson_file[:-1]
-# finishing file end 
-end_geojson = """
-  ]
-}
-"""
-geojson_file += end_geojson
-# saving as geoJSON
-geojson_written = open('terminals.geojson','w')
-geojson_written.write(geojson_file)
-geojson_written.close() 
+  SELECT ?location ?label
+  WHERE
+  {
+  ?term rdf:type ns:%s.
+  ?term rdfs:label ?label.
+  ?term loc:lat-lon ?location.
+  }"""%(class_namespace,class_name)
 
-            
-    
+ # performing SPARQL query  
+  sparql = SPARQLWrapper(endpoint)
+  sparql.setReturnFormat(JSON) 
+  sparql.setQuery(queryString) 
+  start = time.time()
+  print('Querying...')
+  ret = sparql.queryAndConvert() 
+  end = time.time()
+  # parsing JSON SPARQL results into an array
+  print('Finished in ',np.round(end-start,2),' seconds')
+  ret = ret['results']['bindings']
+  num_ret = len(ret)
+  # assigning memory to results array 
+  ret_array = np.zeros((num_ret,3),dtype='object')
+  header = ['lat','lon','name']
+  # iterating over results and allocating properties from query 
+  for i in tqdm(range(num_ret)):
+      lat,lon = ret[i]['location']['value'].split('#')
+      ret_array[i,:] = [lat,lon,ret[i]['label']['value']]
+  ret = ret_array 
+  # allocating start of .geoJSON file 
+  geojson_file = """
+  {
+    "type": "FeatureCollection",
+    "features": ["""
+  # iterating over features (rows in results array)
+  for i in range(num_ret):
+      # creating point feature 
+      feature = """{
+        "type": "Feature",
+        "properties": {
+          "name": "%s"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            %s,
+            %s
+          ]
+        }
+      },"""%(ret[i,2],ret[i,1],ret[i,0])
+      # adding new line 
+      geojson_file += '\n'+feature
+
+  # removing last comma as is last line
+  geojson_file = geojson_file[:-1]
+  # finishing file end 
+  end_geojson = """
+    ]
+  }
+  """
+  geojson_file += end_geojson
+  # saving as geoJSON
+  geojson_written = open(class_label+'.geojson','w')
+  geojson_written.write(geojson_file)
+  geojson_written.close() 
+  return 
+
+
+# querying terminals from ontogasgrid
+endpoint = "http://www.theworldavatar.com/blazegraph/namespace/ontogasgrid/sparql"
+class_namespace = 'http://www.theworldavatar.com/ontology/ontogasgrid/gas_network_components.owl#' 
+class_name = 'GasTerminal'
+class_label = 'terminals'
+query_to_geoJSON(class_namespace,class_name,class_label,endpoint)
