fd0c161f19cc0afd5c862c86df2bbfddccb8f9b6
Laura Ong <laura.ong@cares.cam.ac.uk>
2021-03-12 15:31:57 +0800

    JPS_SCENARIOS: added processRequestParameters to KBA. Need to check how to use KGRouter to query if we're kicking out the call agents as well.

--- JPS_SCENARIOS/src/uk/ac/cam/cares/jps/scenario/kb/KnowledgeBaseAgent.java
+++ JPS_SCENARIOS/src/uk/ac/cam/cares/jps/scenario/kb/KnowledgeBaseAgent.java
@@ -5,11 +5,15 @@ import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.BadRequestException;
 
 import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +23,7 @@ import uk.ac.cam.cares.jps.base.config.JPSConstants;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.http.Http;
 import uk.ac.cam.cares.jps.base.query.KnowledgeBaseClient;
+import uk.ac.cam.cares.jps.base.util.InputValidator;
 import uk.ac.cam.cares.jps.base.util.MiscUtil;
 
 @WebServlet(urlPatterns = {"/kb/*", "/data/*", "/dataset/*"})
@@ -190,5 +195,76 @@ public class KnowledgeBaseAgent extends JPSAgent {
 			b.append(", sparql (short)=" + sparql);
 			logger.info(b.toString());
 		}
+	}
+
+	@Override
+	public JSONObject processRequestParameters(JSONObject requestParams) {
+		JSONObject result = processRequestParameters(requestParams,null);
+		return result;
+	}
+	@Override
+	public JSONObject processRequestParameters(JSONObject requestParams, HttpServletRequest request) {
+		System.out.println("JSON PARAMS" + requestParams.toString());
+		if (!validateInput(requestParams)) {
+			throw new JSONException("KnowledgeBaseAgent: Input parameters not found.\n");
+		}
+		String body = MiscUtil.optNullKey(requestParams, JPSConstants.CONTENT);
+		String requestUrl = MiscUtil.optNullKey(requestParams, JPSConstants.REQUESTURL);
+		String path = MiscUtil.optNullKey(requestParams, JPSConstants.PATH);
+		String contentType = MiscUtil.optNullKey(requestParams, JPSConstants.CONTENTTYPE);
+		String paramResourceUrl= MiscUtil.optNullKey(requestParams,JPSConstants.SCENARIO_RESOURCE);
+	    String sparql = MiscUtil.optNullKey(requestParams, JPSConstants.QUERY_SPARQL_UPDATE);
+	    String method = MiscUtil.optNullKey(requestParams, JPSConstants.METHOD);
+		
+	    String paramDatasetUrl = MiscUtil.optNullKey(requestParams, JPSConstants.SCENARIO_DATASET);
+		String datasetUrl = KnowledgeBaseManager.getDatasetUrl(requestUrl);
+		KnowledgeBaseAbstract kb = KnowledgeBaseManager.getKnowledgeBase(datasetUrl);
+		String resourceUrl = getResourceUrl(datasetUrl, requestUrl, paramResourceUrl);
+	
+		JSONObject result = new JSONObject();
+		logInputParams(method, requestUrl, path, paramDatasetUrl, paramResourceUrl, contentType, sparql, false);
+		switch (method) {
+			case HttpGet.METHOD_NAME:
+				sparql = MiscUtil.optNullKey(requestParams, JPSConstants.QUERY_SPARQL_QUERY);
+	            String accept = MiscUtil.optNullKey(requestParams, JPSConstants.HEADERS);
+				String qres = "";
+				if (sparql == null) {
+					qres = kb.get(resourceUrl, accept);
+				} else {
+					qres = kb.query(resourceUrl, sparql);
+				}
+				result.put("result", qres);
+			    break;
+		  case HttpPost.METHOD_NAME:
+				if (sparql == null) {
+					throw new JPSRuntimeException("parameter " + JPSConstants.QUERY_SPARQL_UPDATE + " is missing");
+				}
+				kb.update(resourceUrl, sparql);
+		    break;
+		  case HttpPut.METHOD_NAME:
+				if (sparql != null) {
+					throw new JPSRuntimeException("parameter " + JPSConstants.QUERY_SPARQL_UPDATE + " is not allowed");
+				}    			
+				kb.put(resourceUrl, body, contentType);
+		  
+			}		
+	    return result;
+	    }
+	@Override
+	public boolean validateInput(JSONObject requestParams) throws BadRequestException {
+	    if (requestParams.isEmpty()) {
+	        throw new BadRequestException();
+	    }
+	    try {
+	        boolean q = InputValidator.checkURLpattern(requestParams.getString(JPSConstants.REQUESTURL));
+	        String method = MiscUtil.optNullKey(requestParams,JPSConstants.METHOD);
+	        if (method == null) {
+	        	return false;
+	        }
+	        return q;
+	    }catch (JSONException ex) {
+	    	ex.printStackTrace();
+	    	return false;
+	    }
 	}
 }
