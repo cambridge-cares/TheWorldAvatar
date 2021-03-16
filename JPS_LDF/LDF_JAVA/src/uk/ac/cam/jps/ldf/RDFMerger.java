1837c92ae3359ab9a8bbd4064de11f70a73a57f6
X.Zhou <xz378@cam.ac.uk>
2021-03-16 19:00:48 +0000

    Added RDFMerger.java for merging multiple owl files into one for later compression to HDT

---
+++ JPS_LDF/LDF_JAVA/src/uk/ac/cam/jps/ldf/RDFMerger.java
@@ -0,0 +1,73 @@
+package uk.ac.cam.jps.ldf;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+public class RDFMerger {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+		// To merge RDF files into one single file (n3/ttl) so that the files can be later converted to HDT files
+		
+		
+			// read the files in the folder, select the owl files 
+		
+			File folder = new File("D:\\ontokin_files\\merged_files");
+			File[] listOfFiles = folder.listFiles();
+			ArrayList<String> filenames = new ArrayList<String>();
+			
+			// ======================= filter out all the owl files ============================
+			for (File file : listOfFiles) {
+			    if (file.isFile()) {
+			    	if (file.getName().endsWith(".ttl")) {
+			    		System.out.println(file.getName());
+			    		filenames.add(file.getName()); // make a list of all filenames
+			    	}
+			    }
+			}
+			
+			int counter = 1; // use the counter to merge every 10 files 
+			
+			int block_counter = 0; // use the counter as the name for the block 
+			
+			System.out.println("============================== Start merging ==============================");
+			Model blockModel = ModelFactory.createDefaultModel();
+			for (String filename : filenames) {				
+				// ========================= read the owl file ==============================
+				InputStream inputstream = new FileInputStream("D:\\ontokin_files\\merged_files\\" + filename);
+			 	final Model rdffile = ModelFactory.createDefaultModel();
+				rdffile.read(inputstream, null, "Turtle") ;				
+				// ==========================================================================
+
+				System.out.println("Processing " + filename);
+				blockModel = blockModel.add(rdffile); // put the new file into the graph ... 
+				counter ++ ;
+				if ((counter == filenames.size() + 1) || (counter% 9999 == 0)) {
+					block_counter++;
+					System.out.println("The last file of this block " + filename); // the last file of a block, make the merge and serialize the file ...  
+					FileOutputStream blockfile = new FileOutputStream("D:\\ontokin_files\\merged_files\\final\\final.ttl"); // write the new block file ... 
+					blockModel.write(blockfile, "Turtle");
+					
+					blockModel = ModelFactory.createDefaultModel(); // empty the block model 
+
+				}
+				}
+
+ 
+		
+		
+		
+	}
+
+}
