c0b841be2dc07193702e196728f743a424cd2ad9
Feroz Farazi <msff2@cam.ac.uk>
2021-03-18 23:25:15 +0000

    959-extend-the-entityrdfizerpy-code-to-enable-the-selection-of-multiple-csv-files: Created reader and setter functions for EntityRDFizer to read values from the PropertyReader.py file and let users set the values required for their task and modified getter functions to return the recently set values to the callers.

--- PlantOwl/python/power_plant/PropertyReader.py
+++ PlantOwl/python/power_plant/PropertyReader.py
@@ -7,38 +7,78 @@ import configparser
 config = configparser.RawConfigParser()
 config.read('./conf/EntityRDFizer.properties')
 
+"""Created variables to be used globally to maintain the values
+read from a property file and to update the variables if users set
+new values via setter functions"""
 tboxIRI=''
 aboxIRI=''
 aboxFileName=''
 aboxFileExtension=''
 
 def getTBoxIRI():
-    return config.get('IRISection', 'kb.tbox.iri')
+    return tboxIRI
+
+def setTBoxIRI(iri):
+    global tboxIRI
+    tboxIRI = iri
+
+def readTBoxIRI():
+    global tboxIRI
+    tboxIRI = config.get('IRISection', 'kb.tbox.iri')
+    return tboxIRI
 
 def getABoxIRI():
-    return config.get('IRISection', 'kb.abox.iri')
+    return aboxIRI
+
+def setABoxIRI(iri):
+    global aboxIRI
+    aboxIRI=iri
+
+def readABoxIRI():
+    global aboxIRI
+    aboxIRI = config.get('IRISection', 'kb.abox.iri')
+    return aboxIRI
 
 def getABoxFileName():
+    return aboxFileName
     return config.get('FileSection', 'kb.abox.file.name')
 
+def setABoxFileName(fileName):
+    global aboxFileName
+    aboxFileName = fileName
+
+def readABoxFileName():
+    global aboxFileName
+    aboxFileName = config.get('FileSection', 'kb.abox.file.name')
+    return aboxFileName
+
 def getABoxFileExtension():
-    return config.get('FileSection', 'kb.abox.file.extension')
+    return aboxFileExtension
 
-def setTBoxIRI(tboxIRI):
-    global.tboxIRI=tboxIRI
+def setABoxFileExtension(fileExtension):
+    global aboxFileExtension
+    aboxFileExtension = fileExtension
 
-def setABoxIRI(aboxIRI):
-    global.aboxIRI=aboxIRI
-
-def setABoxFileName(aboxFileName):
-    global.aboxFileName=aboxFileNAme
-
-def setABoxFileExtension():
-    return config.get('FileSection', 'kb.abox.file.extension')
+def readABoxFileExtension():
+    global aboxFileExtension
+    aboxFileExtension = config.get('FileSection', 'kb.abox.file.extension')
+    return aboxFileExtension
 
 if __name__ == '__main__':
+    """Shows the default values available in the property file"""
+    print(readTBoxIRI())
+    print(readABoxIRI())
+    print(readABoxFileName())
+    print(readABoxFileExtension())
+    """Sets new values to update the ones read from the property file"""
+    setTBoxIRI("http://a/test/tbox/iri")
+    setABoxIRI("http://a/test/abox/iri")
+    setABoxFileName("a-test-a-box-file-name")
+    setABoxFileExtension("a-test-a-box-file-extension")
+    """Shows the new values set via setter functions above"""
     print(getTBoxIRI())
     print(getABoxIRI())
     print(getABoxFileName())
     print(getABoxFileExtension())
+
 
