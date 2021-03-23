a9a2de5cfc68420566649658fbf4182cc2f2a3da
Feroz Farazi <msff2@cam.ac.uk>
2021-03-23 23:03:20 +0000

    964-refine-the-bioenergy-project: Refined the BioEnergyCalculation.py file to change the workflow used for calculating bioenergy in the surroundings of a power plant.

---
+++ PlantOwl/python/power_plant/Elean_Power_Station_Use_Case/BioEnergyCalculation.py
@@ -0,0 +1,148 @@
+#############################################################################################
+# Author: Zach and Dave                                                                     #
+# Date: 23 March 2021                                                                       #
+# Acknowledgement: we are thankful to Feroz Farazi (msff2@cam.ac.uk) for reviewing the code #
+#############################################################################################
+"""Here we demonstrate how to query information from a triple store and put it in a variable"""
+
+import SparqlQuery as sq
+import PowerPlant as pp
+import Queries as q
+import Lucode as l
+import numpy as np
+
+"""Sets the URL of the triple store deployed on Blazegraph to query test"""
+onto_crop_map_gml_endpoint = "http://www.theworldavatar.com/blazegraph/namespace/test/sparql"
+"""Sets the URL of the triple store deployed on Blazegraph to query test"""
+onto_crop_energy_endpoint = "http://www.theworldavatar.com/blazegraph/namespace/ontocropenergy/sparql"
+"""Sets the URL of the triple store deployed on CoMo to query UK power plants"""
+uk_power_plant_endpoint = "https://como.ceb.cam.ac.uk/rdf4j-server/repositories/UKPowerPlant"
+"""The name of power plant"""
+power_plant_name = "Ely UK"
+
+"""The name of power plant is extracted from the IRI as it is not codified as a separate attribute"""
+def extract_name(iri):
+    if "#" in iri:
+        tokensHashSeparated = iri.split("#")
+        if len(tokensHashSeparated) > 1:
+            if ".owl" in tokensHashSeparated[0]:
+                tokensHashSeparated[0] = tokensHashSeparated[0].replace(".owl", "")
+            if "/" in tokensHashSeparated[0]:
+                tokensSlashSepated = tokensHashSeparated[0].split("/")
+                length = len(tokensSlashSepated)
+                name = tokensSlashSepated[length - 1]
+                if "_" in name:
+                    return name.replace("_", " ")
+
+"""Retrieves the details of the power plant required for the use case"""
+def get_power_plant_data(endpoint, query):
+    """Sets the SPARQL query to retrieve IRIs of the UK power plant of interest"""
+    results = sq.query_endpoint(endpoint, query)
+    countPowerPlant = 0
+
+    print ('Retrieves and assigns the name, capacity, latitude, longitude and fuel type of all power plants:')
+    for result in results["results"]["bindings"]:
+        countPowerPlant = countPowerPlant+ 1
+        print('Plant %d:' %countPowerPlant)
+        """Puts the IRI of power plant in a variable"""
+        power_plant_iri = result["powerPlantIRI"]['value']
+        """Creates an instance of the PowerPlant class to store the name, capacity, latitude, longitude and fuel type"""
+        power_plant = pp.PowerPlant()
+        """Extracts and assigns the name of power plant to the instance"""
+        power_plant.name = extract_name(power_plant_iri)
+        """Queries the latitude and longitude of power plant"""
+        coordinates = sq.query_endpoint(endpoint, q.get_power_plant_coordinates(power_plant_iri))
+        for coordinate in coordinates["results"]["bindings"]:
+            """Assigns the latitude of power plant to the class instance"""
+            power_plant.latitude = coordinate["latitude"]['value']
+            """Assigns the longitude of power plant to the class instance"""
+            power_plant.longitude = coordinate["longitude"]['value']
+        """Queries the fuel type of power plant"""
+        fuel_types = sq.query_endpoint(endpoint, q.get_fuel_type(power_plant_iri))
+        for fuel_type in fuel_types["results"]["bindings"]:
+            """Assigns the fuel type of power plant to the class instance"""
+            power_plant.fuel_type = fuel_type["fuel"]['value']
+        """Queries the capacity of power plant"""
+        capacity = sq.query_endpoint(endpoint, q.get_capacity(power_plant_iri))
+        for c in capacity["results"]["bindings"]:
+            """Assigns the capacity of power plant to the class instance"""
+            power_plant.capacity = c["capacity"]['value']
+            """Assigns the units of capacity of power plant to the class instance"""
+            power_plant.capacity_units = c["capacity_units"]['value']
+        """Prints the name, capacity, latitude, longitude and fuel type of power plant so that you can see"""
+        print(' Name: ', power_plant.name)
+        print(' Capacity: ', power_plant.capacity, ' Units: ', power_plant.capacity_units)
+        print(' Latitude: ', power_plant.latitude)
+        print(' Longitude: ', power_plant.longitude)
+        print(' Fuel type: ', power_plant.fuel_type)
+    return power_plant
+
+"""Generates a map between LUCODE IRIs and crop map IRIs"""
+def get_lucode_crop_map(onto_crop_energy_endpoint):
+    lucode_crop_map = dict()
+    crops = sq.query_endpoint(onto_crop_energy_endpoint, q.get_lucode_label())
+    for c in crops["results"]["bindings"]:
+        """Puts the IRI of the crop in a variable"""
+        crop_iri = c["Crop"]['value']
+        """Assigns the lucode of crop to the class instance"""
+        lucode_iri = c["Lucode"]['value']
+        lucode = lucode_iri[-4:]
+        """Creates the map between IRIs"""
+        lucode_crop_map[lucode_iri] = crop_iri
+        print('Crop: ', crop_iri)
+        print(' LucodeIRI: ', lucode_iri)
+        print(' Lucode: ', lucode)
+    return lucode_crop_map
+
+"""Calculates different parameters of crops, including energy and power, produced within a geographical region"""
+def calculate_crop_parameters(lucode_crop_map, onto_crop_map_gml_endpoint, onto_crop_energy_endpoint, power_plant):
+    P = []
+    """Combines the desired coordinates into a usable form for the geospatial query"""
+    latlon = '"' + str(power_plant.latitude) + '#' + str(power_plant.longitude) + '"'
+    """Counts the number of polygons within a circle centred at the coordinates of a power plant"""
+    results = sq.query_endpoint(onto_crop_map_gml_endpoint, q.get_lucode_count(latlon))
+    for result in results["results"]["bindings"]:
+        """Creates an instance of the Crop class to store the name, lucode, yeild and energy"""
+        crop = l.Crop()
+        """Assigns the number of instances of crop to the class instance"""
+        if result["Lucode"]['value'] in lucode_crop_map.keys():
+            crop.crop_number = result["LucodeTotal"]['value']
+            lucode_iri = result["Lucode"]['value']
+            print(lucode_crop_map.get(lucode_iri))
+            results_yield = sq.query_endpoint(onto_crop_energy_endpoint, q.get_crop_yield(lucode_crop_map.get(lucode_iri)))
+            for result in results_yield["results"]["bindings"]:
+                """Assigns the yield of crop to the class instance"""
+                crop.crop_yield = result["Yield"]['value']
+            print(lucode_crop_map.get(lucode_iri))
+            results_energy = sq.query_endpoint(onto_crop_energy_endpoint, q.get_crop_energy(lucode_crop_map.get(lucode_iri)))
+            for result in results_energy["results"]["bindings"]:
+                """Assigns the energy of crop to the class instance"""
+                crop.crop_energy = result["HHV"]['value']
+
+            print(' Quantity: ', crop.crop_number)
+            print(' Yield: ', crop.crop_yield)
+            print(' Energy: ', crop.crop_energy)
+
+            power = float(crop.crop_number) * 0.4156872 * float(crop.crop_yield) * 1000 * float(
+                crop.crop_energy) * 1000 / (365.25 * 24 * 3600)
+            print(' Power: ', power)
+            P.append(power)
+
+    print(P)
+    available_power = np.sum(P)
+    output_power = float(available_power) * 0.325
+    print('Total Available Power: ', available_power, ' kW')
+    print('Output Power: ', output_power, 'kW')
+
+"""If the name of a power plant is provided, it extracts the IRI of the power plant, finds the geospatial 
+location of it and calculates the bioenergy available in its surroundings"""
+def execute_energy_use_case(power_plant_name):
+    """Replaces spaces with underscores to form an IRI from the name"""
+    power_plant_name = power_plant_name.replace(" ", "_")
+    power_plant = get_power_plant_data(uk_power_plant_endpoint, q.get_use_case_power_plant_iri(power_plant_name.replace(" ", "_")))
+    lucode_crop_map = get_lucode_crop_map(onto_crop_energy_endpoint)
+    calculate_crop_parameters(lucode_crop_map, onto_crop_map_gml_endpoint, onto_crop_energy_endpoint, power_plant)
+
+"""The following block of code runs first if this module is executed"""
+if __name__ == '__main__':
+    execute_energy_use_case(power_plant_name)
