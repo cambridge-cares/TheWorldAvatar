07806b081f6462945ac3c82a2bbdd8591279b157
Laura Ong <laura.ong@cares.cam.ac.uk>
2021-03-26 11:45:30 +0800

    944-migrate-kb-agent-code-to-jps-agent: JPS_SCENARIOS: add testBaseQueryDirect, testBaseUpdateDirect to TestKnowledgeBA,

--- JPS_SCENARIOS/src/uk/ac/cam/cares/jps/scenario/kg/KnowledgeBaseAgentNew.java
+++ JPS_SCENARIOS/src/uk/ac/cam/cares/jps/scenario/kg/KnowledgeBaseAgentNew.java
@@ -1,9 +1,14 @@
 package uk.ac.cam.cares.jps.scenario.kg;
 
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BadRequestException;
 
+import org.apache.jena.arq.querybuilder.UpdateBuilder;
+import org.apache.jena.arq.querybuilder.WhereBuilder;
+import org.apache.jena.sparql.lang.sparql_11.ParseException;
+import org.apache.jena.update.UpdateRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -37,14 +42,23 @@ public class KnowledgeBaseAgentNew extends JPSAgent{
 	public JSONObject main(JSONObject requestParams) {
 		boolean isUpdateOperation  = false;
 		boolean  isQueryOperation=false;
+		JSONObject JSONresult = new JSONObject();
 		String sparqlquery = MiscUtil.optNullKey(requestParams, JPSConstants.QUERY_SPARQL_QUERY);
 		String sparqlupdate = MiscUtil.optNullKey(requestParams,  JPSConstants.QUERY_SPARQL_UPDATE);
 		if (sparqlquery != null) isQueryOperation = true;
 		else if (sparqlupdate != null) isUpdateOperation = true;
 		String targetResourceIRIOrPath = requestParams.getString(JPSConstants.SCENARIO_RESOURCE);
 		KnowledgeBaseClientInterface kbClient = KGRouter.getKnowledgeBaseClient(targetResourceIRIOrPath, isQueryOperation,isUpdateOperation);
-		String result = kbClient.execute(sparqlquery);
-		return new JSONObject().put("result",result);
+		if (isQueryOperation) { 
+			String result = kbClient.execute(sparqlquery);
+			JSONresult.put("result",result);
+			}
+		else if (isUpdateOperation) {
+			//perform update
+			kbClient.setQuery(sparqlupdate);
+			kbClient.executeUpdate();
+		}
+		return JSONresult; 
 	}
 	@Override
 	public boolean validateInput(JSONObject requestParams) throws BadRequestException {
@@ -62,5 +76,32 @@ public class KnowledgeBaseAgentNew extends JPSAgent{
 	    	ex.printStackTrace();
 	    	return false;
 	    }
+	}
+	/**
+	 * Returns the test Sparql update.
+	 * 
+	 * @return UpdateRequest
+	 * @throws ParseException
+	 */
+	private static UpdateRequest getUpdateRequest() throws ParseException {
+		
+		//DELETE {?s ?p ?o} 
+		//INSERT {?s ?p \"TEST\" } 
+		//WHERE {?s ?p ?o.
+		//		 FILTER(?s = <http://www.theworldavatar.com/kb/species/species.owl#species_1> && ?p = <http://www.w3.org/2008/05/skos#altLabel>)}
+		
+		WhereBuilder where = new WhereBuilder()
+				.addWhere("?s", "?p", "?o")
+				.addFilter("?s = <http://www.theworldavatar.com/kb/species/species.owl#species_1> && ?p = <http://www.w3.org/2008/05/skos#altLabel>");
+				
+		// Build update
+		UpdateBuilder builder = new UpdateBuilder();
+				
+		// Add where 
+		builder.addInsert("?s", "?p", "TEST")
+			.addDelete("?s", "?p", "?o")
+			.addWhere(where);
+		
+		return builder.buildRequest();
 	}
 }
--- JPS_SCENARIOS/test/uk/ac/cam/cares/jps/scenario/kg/test/TestKnowledgeBA.java
+++ JPS_SCENARIOS/test/uk/ac/cam/cares/jps/scenario/kg/test/TestKnowledgeBA.java
@@ -1,14 +1,60 @@
 package uk.ac.cam.cares.jps.scenario.kg.test;
 
-import org.apache.jena.arq.querybuilder.SelectBuilder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.apache.jena.arq.querybuilder.UpdateBuilder;
+import org.apache.jena.arq.querybuilder.WhereBuilder;
+import org.apache.jena.sparql.lang.sparql_11.ParseException;
+import org.apache.jena.update.UpdateRequest;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import junit.framework.TestCase;
+import uk.ac.cam.cares.jps.base.config.AgentLocator;
 import uk.ac.cam.cares.jps.base.config.JPSConstants;
-import uk.ac.cam.cares.jps.base.discovery.AgentCaller;
 import uk.ac.cam.cares.jps.scenario.kg.KnowledgeBaseAgentNew;
 
-public class TestKnowledgeBA  extends TestCase {
+public class TestKnowledgeBA   {
+
+
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+	private String filePath;
+	private String filePathNQ;
+	private String filePathNQ2;
+	private String filePathOWL;
+	private String queryString = "SELECT ?o WHERE {<http://www.theworldavatar.com/kb/species/species.owl#species_1> <http://www.w3.org/2008/05/skos#altLabel> ?o.}";
+
+	
+	@Before
+	public void setUp() throws URISyntaxException, IOException {
+		// Test rdf file
+		String filePathDir = AgentLocator.getCurrentJpsAppDirectory(this) + "/testres" ;
+		
+		Path testResourcePath = Paths.get(filePathDir+"/testRDF.rdf");
+		Path tempFilePath = Paths.get(tempFolder.getRoot().toString() + "/testRDF.rdf");		
+		Files.copy(testResourcePath, tempFilePath, StandardCopyOption.REPLACE_EXISTING);
+		filePath = tempFilePath.toString();
+		
+		// Test owl file
+		Path testResourcePathOWL = Paths.get(filePathDir+"/testOWL.owl");
+		Path tempFilePathOWL = Paths.get(tempFolder.getRoot().toString() + "/testOWL.owl");
+		Files.copy(testResourcePathOWL, tempFilePathOWL, StandardCopyOption.REPLACE_EXISTING);
+		filePathOWL = tempFilePathOWL.toString();
+	}
+	@Test
 	 public void testNewKBAgent() {
 	        KnowledgeBaseAgentNew jpsa = null;
 	        try {
@@ -17,21 +63,66 @@ public class TestKnowledgeBA  extends TestCase {
 	            assertNotNull(jpsa);
 	        }
 	    }
-	public void testAgentRouter() {
-		String gennodeInfo = "PREFIX j1:<http://www.theworldavatar.com/ontology/ontopowsys/PowSysRealization.owl#> "
-            + "PREFIX j2:<http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#> "
-            + "SELECT ?entity "
-            + "WHERE {?entity  a  j1:PowerGenerator  ."
-            + "FILTER EXISTS {?entity j2:isSubsystemOf ?plant } " //filtering gen 001 as it is slackbus
-            + "}";
-
+	 /** Test Sparql query with String. Should return result as String.
+	  */
+	@Test
+	public void testBaseQueryDirect() {
 		JSONObject jo = new JSONObject()
-				.put(JPSConstants.SCENARIO_RESOURCE, "http://www.jparksimulator.com/kb/sgp/jurongisland/jurongislandpowernetwork/JurongIslandPowerNetwork.owl#JurongIsland_PowerNetwork")
-				.put(JPSConstants.QUERY_SPARQL_QUERY,gennodeInfo  );
+				.put(JPSConstants.SCENARIO_RESOURCE, filePath)
+				.put(JPSConstants.QUERY_SPARQL_QUERY,queryString );
 //		AgentCaller.executeGetWithJsonParameter("jps/kb/scenarioFolder", jo.toString());
 
         KnowledgeBaseAgentNew jpsa = new KnowledgeBaseAgentNew();
+        JSONObject result = jpsa.main(jo);		
+		JSONArray ja = new JSONArray(result.getString("result")); 
+		jo = ja.getJSONObject(0); 
+		assertEquals("OH",jo.get("o").toString());
+	}
+	@Test
+	public void testBaseUpdateDirect() throws ParseException {
+		
+		testBaseQueryDirect();
+		String testUpdate = getUpdateRequest().toString();
+		KnowledgeBaseAgentNew jpsa = new KnowledgeBaseAgentNew();
+		 JSONObject jo = new JSONObject()
+		.put(JPSConstants.SCENARIO_RESOURCE,  filePath)
+		.put(JPSConstants.QUERY_SPARQL_UPDATE , testUpdate );
         jpsa.main(jo);
+        String queryString = "SELECT ?o WHERE {<http://www.theworldavatar.com/kb/species/species.owl#species_1> <http://www.w3.org/2008/05/skos#altLabel> ?o.}";
+        jo = new JSONObject()
+        		.put(JPSConstants.SCENARIO_RESOURCE,  filePath)
+        		.put(JPSConstants.QUERY_SPARQL_QUERY,queryString );
+        JSONObject result = jpsa.main(jo);
+        JSONArray ja = new JSONArray(result.getString("result")); 
+		jo = ja.getJSONObject(0); 
+		assertEquals("TEST",jo.get("o").toString());
+	}
+	/**
+	 * Returns the test Sparql update.
+	 * 
+	 * @return UpdateRequest
+	 * @throws ParseException
+	 */
+	private static UpdateRequest getUpdateRequest() throws ParseException {
+		
+		//DELETE {?s ?p ?o} 
+		//INSERT {?s ?p \"TEST\" } 
+		//WHERE {?s ?p ?o.
+		//		 FILTER(?s = <http://www.theworldavatar.com/kb/species/species.owl#species_1> && ?p = <http://www.w3.org/2008/05/skos#altLabel>)}
+		
+		WhereBuilder where = new WhereBuilder()
+				.addWhere("?s", "?p", "?o")
+				.addFilter("?s = <http://www.theworldavatar.com/kb/species/species.owl#species_1> && ?p = <http://www.w3.org/2008/05/skos#altLabel>");
+				
+		// Build update
+		UpdateBuilder builder = new UpdateBuilder();
+				
+		// Add where 
+		builder.addInsert("?s", "?p", "TEST")
+			.addDelete("?s", "?p", "?o")
+			.addWhere(where);
+		
+		return builder.buildRequest();
 	}
 	 
 }
