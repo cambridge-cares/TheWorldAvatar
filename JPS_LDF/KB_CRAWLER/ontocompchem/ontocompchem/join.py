586503dcfec147abefae0976666fb3c2c6d3bd67
X.Zhou <xz378@cam.ac.uk>
2021-04-06 14:15:53 +0100

    added crawler for ontocompchem

--- JPS_LDF/KB_CRAWLER/ontocompchem/ontocompchem/join.py
+++ JPS_LDF/KB_CRAWLER/ontocompchem/ontocompchem/join.py
@@ -1,13 +1,12 @@
 import json 
 
-with open('root_owls') as f:
-	root_owls = json.loads(f.read())
+ 
 
 with open('owls_in_directory') as f:
 	owls_in_directory = json.loads(f.read())
     
     
-all_owl_files = ['http://www.theworldavatar.com/kb/ontocompchem/' + x for x in list(set(root_owls + owls_in_directory))]
+all_owl_files = ['http://www.theworldavatar.com/kb/ontocompchem/' + x for x in list(set(owls_in_directory))]
 
 with open('all_owl', 'w') as f:
     for file in all_owl_files:
