f4597bc25d80a7d9e8fdb9ca19a2e3b03e82f6e0
X.Zhou <xz378@cam.ac.uk>
2021-04-11 18:55:45 +0100

    integrated ldf server for OntoCompChem

--- JPS_Chatbot/UI/source/rasa_jps/JPS_query_constructor_test.py
+++ JPS_Chatbot/UI/source/rasa_jps/JPS_query_constructor_test.py
@@ -47,44 +47,85 @@ WHERE  {
   
 '''
 
+def test_3000():
+    q3 = '''
+    PREFIX ontokin: <http://www.theworldavatar.com/kb/ontokin/ontokin.owl#>
+    PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    SELECT DISTINCT ?label ?POLARIZABILITY ?Unit
+    {
 
-q3 = '''
-PREFIX ontokin: <http://www.theworldavatar.com/kb/ontokin/ontokin.owl#>
-PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-SELECT DISTINCT ?label ?POLARIZABILITY ?Unit
-{
+             ?TransportModel ontokin:hasPolarizability ?POLARIZABILITY .
+         ?Species ontokin:hasTransportModel ?TransportModel .
+         ?Species rdfs:label ?label .
+             ?Species rdfs:label "C2H2O2" .
 
-         ?TransportModel ontokin:hasPolarizability ?POLARIZABILITY .
-     ?Species ontokin:hasTransportModel ?TransportModel .
-     ?Species rdfs:label ?label .
-         ?Species rdfs:label "C2H2O2" .
+              OPTIONAL{
+                    ?TransportModel ontokin:hasPolarizabilityUnits ?Unit .
+         }
 
-          OPTIONAL{
-                ?TransportModel ontokin:hasPolarizabilityUnits ?Unit .
-     }
+    }  LIMIT 1
+    '''
+    p1 = "OH"
+    p2 = "H"
+    r1 = "O"
+    r2 = "H2"
 
-}  LIMIT 1
-'''
-p1 = "OH"
-p2 = "H"
-r1 = "O"
-r2 = "H2"
+    # q2 = q2 % (p1, p1, p2, p2)
+    products = [p1, p2]
+    reactants = [r2]
 
-#q2 = q2 % (p1, p1, p2, p2)
-products = [p1, p2]
-reactants = [r2]
+    start_time = time.time()
 
-start_time = time.time()
+    url = "http://localhost:3000/query?"
+    values = {"query": q3, "products": json.dumps(products), "reactants": json.dumps(reactants)}
+    full_url = url + urllib.parse.urlencode(values)
 
-url = "http://localhost:3000/query?"
-values = {"query": q3, "products": json.dumps(products), "reactants": json.dumps(reactants)}
-full_url = url + urllib.parse.urlencode(values)
+    print(full_url)
 
-print(full_url)
+    req = urllib.request.Request(full_url)
+    response = urllib.request.urlopen(req).read()
+    print(response)
+    end_time = time.time()
+    print(round(end_time - start_time, 2), 'seconds')
+    # response = urllib.request.urlopen(req).read()
 
-req = urllib.request.Request(full_url)
-response = urllib.request.urlopen(req).read()
-print(response)
-end_time = time.time()
-print(round(end_time - start_time,2 ), 'seconds')
-# response = urllib.request.urlopen(req).read()
+    q_ontocompchem = '''
+    PREFIX compchemkb: <https://como.cheng.cam.ac.uk/kb/compchem.owl#>
+    PREFIX gc: <http://purl.org/gc/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX ontocompchem:<http://www.theworldavatar.com/ontology/ontocompchem/ontocompchem.owl#>
+    SELECT DISTINCT ?name ?SpinMultiplicity    
+    WHERE  {
+       ?Molecule  ontocompchem:hasSpinMultiplicity ?SpinMultiplicity .
+       ?GeometryOptimization    gc:hasMolecule    ?Molecule .
+       ?g_calculation  gc:isCalculationOn    ?GeometryOptimization .
+
+     { 
+    	SELECT DISTINCT ?g_calculation ?name
+    	WHERE {
+
+    	   ?g_calculation ontocompchem:hasInitialization ?initialization .
+    	   ?initialization gc:hasMoleculeProperty ?molecule_property .	
+       	   ?molecule_property gc:hasName ?name .
+
+    	   ?molecule_property gc:hasName "C8H14" .
+
+    	} LIMIT 1 
+     } 
+    } LIMIT 1
+    '''
+
+    start_time = time.time()
+    url = "http://localhost:3000/ontocompchem/query?"
+    values = {"query": q_ontocompchem}
+    full_url = url + urllib.parse.urlencode(values)
+    req = urllib.request.Request(full_url)
+    response = urllib.request.urlopen(req).read()
+    print(response)
+    end_time = time.time()
+    print(round(end_time - start_time, 2), 'seconds')
+
+
+for i in range(100):
+    test_3000()
