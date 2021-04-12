6c3f9a566faf5845b9fefdf24fff4323f2291023
Laura Ong <laura.ong@cares.cam.ac.uk>
2021-04-12 11:14:51 +0800

    944-migrate-kb-agent-code-to-jps-agent: JPS_SCENARIOS: modified ScenarioModifier.java after ESS trial run

--- JPS_SCENARIOS/src/uk/ac/cam/cares/jps/scenario/ScenarioModifier.java
+++ JPS_SCENARIOS/src/uk/ac/cam/cares/jps/scenario/ScenarioModifier.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.BadRequestException;
 
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -33,29 +34,13 @@ public class ScenarioModifier extends JPSAgent{
 	 */
 	private static final long serialVersionUID = 1L;
 	private static Logger logger = LoggerFactory.getLogger(ScenarioModifier.class);
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void dxxoGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		JSONObject jo = AgentCaller.readJsonParameter(request);
-		logger.debug(jo.toString());
-		String path = request.getPathInfo();
-		logger.debug("called for path=" + path);
-		
-		String[] parts = ScenarioHelper.dividePath(path);
-		String scenarioName = parts[0]; //name of scenario: base, testNuclear
-		String newscenarioName = scenarioName + UUID.randomUUID().toString();//give random uuid to distinguish
-		path.replace(scenarioName, newscenarioName);//replace string
-		path.replaceFirst("scenariomod", "scenario");//substitute string to call on path. 
-		logger.info("new path: " + path);
-		// Think that only time new scenario is called would be if there is a call. 
-		String result = AgentCaller.executeGetWithJsonParameter("jps/scenario/"+newscenarioName+"/call", jo.toString()); //get a String result
-		// the created scenario url / name is part of the response body
-		//such that the client can use the scenario in future
-		//F, why 
-		AgentCaller.printToResponse(result, response);
-	}
+
+    @Override
+    public JSONObject processRequestParameters(JSONObject requestParams, HttpServletRequest request) {
+    	processRequestParameters(requestParams);
+    	return requestParams;
+    }
+
 	@Override
     public JSONObject processRequestParameters(JSONObject requestParams) {
     	validateInput(requestParams);
