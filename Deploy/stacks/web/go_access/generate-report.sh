ccb7c80bdee90b0688b2fc0e5fb10a26f30a9419
Michael Hillman <the.michael.hillman@gmail.com>
2021-04-16 15:45:49 +0100

    dev-website-views: GoAccess now installed correctly with Docker container

--- Deploy/stacks/web/go_access/generate-report.sh
+++ Deploy/stacks/web/go_access/generate-report.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Combine all daily logs into a single one
-cat /var/log/reverse_proxy/*.log > /var/log/combined.log
+cat /var/log/reverse_proxy/*.log > /var/log/goaccess/combined.log
 
 # Run GoAccess to produce static HTML file
-goaccess /var/log/combined.log -o /var/www/html/index.html --log-format=COMBINED
+goaccess /var/log/goaccess/combined.log -o /var/log/goaccess/index.html --log-format=COMBINED
---
+++ Deploy/stacks/web/go_access/start-cron.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Start cron in the background
+cron
+echo "Cron daemon has been started."
