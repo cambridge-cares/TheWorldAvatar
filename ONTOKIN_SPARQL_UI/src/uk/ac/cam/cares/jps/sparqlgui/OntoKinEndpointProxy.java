04df8c993a52eae7b34dbb8dad8ba89b4116e290
Feroz Farazi <msff2@cam.ac.uk>
2021-04-19 14:24:59 +0100

    157-modify-existing-codes-that-refer-to-the-rdf-version-of-ontokin-to-blazegraph: Modified and refined the OntoKinEndpointProxy class to call KG Router for querying the JPS knowledge graph.

--- ONTOKIN_SPARQL_UI/src/uk/ac/cam/cares/jps/sparqlgui/OntoKinEndpointProxy.java
+++ ONTOKIN_SPARQL_UI/src/uk/ac/cam/cares/jps/sparqlgui/OntoKinEndpointProxy.java
@@ -17,27 +17,36 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.interfaces.KnowledgeBaseClientInterface;
+import uk.ac.cam.cares.jps.base.query.KGRouter;
 
 /**
- * Servlet implementation class SPARQLEndpointProxy
+ * Servlet implementation class OntoKinEndpointProxy
  */
 @WebServlet("/OntoKinEndpointProxy")
 public class OntoKinEndpointProxy extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private static final String ONTOKIN_KB = "http://kb/ontokin";
     public OntoKinEndpointProxy() {
         super();
     }
-
+    
+    /**
+     * Reads data from the JPS knowledge graph.
+     * 
+     * @param request
+     * @param response
+     */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String queryString = request.getParameter("queryString");
 		String queryResult = null;
 		try {
-			queryResult = queryFromRDF4JServer(queryString);
+			queryResult = queryKnoweldgeGraph(ONTOKIN_KB, queryString);
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
@@ -45,6 +54,12 @@ public class OntoKinEndpointProxy extends HttpServlet {
 		response.getWriter().write(queryResult);
 	}
 
+	/**
+	 * Updates and inserts data into the JPS knowledge graph.
+	 * 
+	 * @param request
+	 * @param response
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
@@ -64,57 +79,48 @@ public class OntoKinEndpointProxy extends HttpServlet {
 		} 
 	}
 	
-	public static String queryFromRDF4JServer(String mechanismquery) throws JSONException, UnsupportedEncodingException {
-
-//		String myHost = "www.theworldavatar.com" ;
-//		int myPort = 80;
-		String myHost = "localhost" ;
-		int myPort = 8080;
-		String myPath = "/ontokinendpoint/query";
-				
-		URIBuilder builder;
-		builder = new URIBuilder().setScheme("http").setHost(myHost).setPort(myPort)
-				.setPath(myPath)
-//				.setUserInfo("feroz", "password")
-				.setParameter("query", mechanismquery)
-				.setParameter("output", "json");
-		
-		String result = executeGet(builder);
-		try{
-		FileWriter fw = new FileWriter("D:/hostport1.txt");
-		fw.write("builder:"+builder.toString()+"\n");
-		fw.write("query result:"+result);
-		fw.close();
-		}catch(IOException e){
-			
+	/**
+	 * Performs a query against the JPS knowledge graph.
+	 * 
+	 * @param KB_IRI permanent IRI of a JPS knowledge base, e.g. for the OntoKin knowledge base it is http://kb/ontokin.
+	 * @param mechanismquery
+	 * @return
+	 * @throws JSONException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static String queryKnoweldgeGraph(String KB_IRI, String mechanismquery) throws JSONException, UnsupportedEncodingException {
+		JSONArray result = null;
+		System.out.println("Query:"+mechanismquery);
+		KnowledgeBaseClientInterface kbClientInterface = KGRouter.getKnowledgeBaseClient(KB_IRI, true, false);
+		result = kbClientInterface.executeQuery(mechanismquery);
+		if(result==null){
+			return null;
 		}
-		return result;
-		
+		return result.toString();
 	}
 	
-//	public static String formRateConstantComparisonQuery(){
-//		String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n";
-//		queryString = queryString.concat("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n");
-//		queryString = queryString.concat("PREFIX ontochem: <http://www.theworldavatar.com/ontology/ontokin/OntoKin.owl#> \n");
-//		queryString = queryString.concat("SELECT distinct ?mechanism1 ?mechanism2 ?reaction1 ?reaction2 ?activationEnergy1 ?activationEnergy2 ?preExpFactor1 ?preExpFactor2 \n");
-//		queryString = queryString.concat("WHERE { \n");
-//		queryString = queryString.concat("    ?reaction1 ontochem:hasEquation \"O + C3H4 [=] CH3 + HCCO\" . \n");
-//		queryString = queryString.concat("    ?reaction1 ontochem:belongsToPhase ?phase1 . \n");
-//		queryString = queryString.concat("    ?phase1 ontochem:containedIn ?mechanism1 . \n");
-//		queryString = queryString.concat("    ?reaction1 ontochem:hasArrheniusCoefficient ?arrheniusRateCoefficients1 . \n");
-//		queryString = queryString.concat("    ?arrheniusRateCoefficients1 ontochem:hasActivationEnergy ?activationEnergy1 . \n");
-//		queryString = queryString.concat("    ?arrheniusRateCoefficients1 ontochem:hasPreExponentialFactor ?preExpFactor1 . \n");
-//		queryString = queryString.concat("    ?arrheniusRateCoefficients1 ontochem:hasTemperatureExponent ?tempExponent1 . \n");
-//		queryString = queryString.concat("    ?reaction2 ontochem:hasEquation \"O + C3H4 [=] CH3 + HCCO\" . \n");
-//		queryString = queryString.concat("    ?reaction2 ontochem:belongsToPhase ?phase2 . \n");
-//		queryString = queryString.concat("    ?phase2 ontochem:containedIn ?mechanism2 . \n");
-//		queryString = queryString.concat("    ?reaction2 ontochem:hasArrheniusCoefficient ?arrheniusRateCoefficients2 . \n");
-//		queryString = queryString.concat("    ?arrheniusRateCoefficients2 ontochem:hasActivationEnergy ?activationEnergy2 . \n");
-//		queryString = queryString.concat("    ?arrheniusRateCoefficients2 ontochem:hasPreExponentialFactor ?preExpFactor2 . \n");
-//		queryString = queryString.concat("    ?arrheniusRateCoefficients2 ontochem:hasTemperatureExponent ?tempExponent2 . \n");
-//		queryString = queryString.concat("    FILTER ((?activationEnergy1 != ?activationEnergy2) && (?preExpFactor1 != ?preExpFactor2) && (?tempExponent1 != ?tempExponent2)) \n");
-//		queryString = queryString.concat("	}");
-//		return queryString;
-//	}
-
+	public static String formRateConstantComparisonQuery(){
+		String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n";
+		queryString = queryString.concat("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n");
+		queryString = queryString.concat("PREFIX ontochem: <http://www.theworldavatar.com/ontology/ontokin/OntoKin.owl#> \n");
+		queryString = queryString.concat("SELECT distinct ?mechanism1 ?mechanism2 ?reaction1 ?reaction2 ?activationEnergy1 ?activationEnergy2 ?preExpFactor1 ?preExpFactor2 \n");
+		queryString = queryString.concat("WHERE { \n");
+		queryString = queryString.concat("    ?reaction1 ontochem:hasEquation \"O + C3H4 [=] CH3 + HCCO\" . \n");
+		queryString = queryString.concat("    ?reaction1 ontochem:belongsToPhase ?phase1 . \n");
+		queryString = queryString.concat("    ?phase1 ontochem:containedIn ?mechanism1 . \n");
+		queryString = queryString.concat("    ?reaction1 ontochem:hasArrheniusCoefficient ?arrheniusRateCoefficients1 . \n");
+		queryString = queryString.concat("    ?arrheniusRateCoefficients1 ontochem:hasActivationEnergy ?activationEnergy1 . \n");
+		queryString = queryString.concat("    ?arrheniusRateCoefficients1 ontochem:hasPreExponentialFactor ?preExpFactor1 . \n");
+		queryString = queryString.concat("    ?arrheniusRateCoefficients1 ontochem:hasTemperatureExponent ?tempExponent1 . \n");
+		queryString = queryString.concat("    ?reaction2 ontochem:hasEquation \"O + C3H4 [=] CH3 + HCCO\" . \n");
+		queryString = queryString.concat("    ?reaction2 ontochem:belongsToPhase ?phase2 . \n");
+		queryString = queryString.concat("    ?phase2 ontochem:containedIn ?mechanism2 . \n");
+		queryString = queryString.concat("    ?reaction2 ontochem:hasArrheniusCoefficient ?arrheniusRateCoefficients2 . \n");
+		queryString = queryString.concat("    ?arrheniusRateCoefficients2 ontochem:hasActivationEnergy ?activationEnergy2 . \n");
+		queryString = queryString.concat("    ?arrheniusRateCoefficients2 ontochem:hasPreExponentialFactor ?preExpFactor2 . \n");
+		queryString = queryString.concat("    ?arrheniusRateCoefficients2 ontochem:hasTemperatureExponent ?tempExponent2 . \n");
+		queryString = queryString.concat("    FILTER ((?activationEnergy1 != ?activationEnergy2) && (?preExpFactor1 != ?preExpFactor2) && (?tempExponent1 != ?tempExponent2)) \n");
+		queryString = queryString.concat("	}");
+		return queryString;
+	}
 }
