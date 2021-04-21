e2cf7790205536c47326e7751c06ea871d361543
X.Zhou <xz378@cam.ac.uk>
2021-04-21 14:32:32 +0100

    updated instance information parsing mechanism

--- JPS_Chatbot/UI/data_preparation/Wiki_basic_info/instance_info/parse_result.py
+++ JPS_Chatbot/UI/data_preparation/Wiki_basic_info/instance_info/parse_result.py
@@ -2,7 +2,8 @@ from os import listdir
 from os.path import isfile, join
 import json
 
-
+#################
+#
 # parse each file separately
 # retrieve label and alt labels and formula 
 # 
@@ -29,7 +30,12 @@ def parse_file(filename):
                 main_label = label 
             if 'class' in item:
                 _class = item['class']['value']
-                # print(_class)
+                if _class not in classes:
+                    if 'classLabel' in item:
+                        class_label = item['classLabel']['value']
+                        classes.append(_class)
+                        distinct_classes.append({'class': _class, 'label': class_label})
+                
             if 'formula' in item:
                 formula = item['formula']['value']
                 subscript = str.maketrans("₀₁₂₃₄₅₆₇₈₉","0123456789")
@@ -38,14 +44,21 @@ def parse_file(filename):
                 altLabel_list = item['altLabel_list']['value'].split('$')
                 alts = altLabel_list
      
-    print('the main label is ', main_label)
-    print('the formula is ', formula)
-    print('the alternative labels are', alts)
-    print('=================================')
+    #print('the main label is ', main_label)
+    #print('the formula is ', formula)
+    #print('the alternative labels are', alts)
+    #print('=================================')
     
-    
+classes = []
+distinct_classes = []
 mypath = '.'
+
+
+
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.startswith('Q')] 
+print('There are in total', len(onlyfiles), 'instances')
+x = input('Press any key to continue')
+
 # iterate through all the files, their names begin with Q
 
 
@@ -53,3 +66,5 @@ onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.startsw
 
 for f in onlyfiles:
    parse_file(f)
+   
+print('the classes are ', classes)
