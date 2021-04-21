82eca0c2df9c62544db862b887d01ac7ead8e201
X.Zhou <xz378@cam.ac.uk>
2021-04-21 14:31:42 +0100

    updated property and label query mechanism query properties now produces instance-property mapping

--- JPS_Chatbot/UI/data_preparation/Wiki_basic_info/query_properties.py
+++ JPS_Chatbot/UI/data_preparation/Wiki_basic_info/query_properties.py
@@ -1,6 +1,6 @@
 from util.SPARQL_Query_Wiki import SPARQL_Query_for_Wiki
-import json, re, time
-
+import json, re, time, random
+    
 SPARQL_template = '''
 #All properties with descriptions and aliases and types
 SELECT ?item ?itemLabel  ?itemAltLabel WHERE {
@@ -17,37 +17,102 @@ property_names = []
 list_of_properties = []
 counter = 0
 property_record = []
+instance_property_mapping = {}
 
+errors = []
+
+# during the upgrade, the properties should be connected to instances 
+# a dictionary {'Qxxxxx': label, alt_labels,  [p1: label, alt_labels, p2, p3, p4]}
+# later merge this dictionary with the instance-class dictionary
+# which will serve as the ultimate corpus for trainning the NLU 
+
+
+# get all the properties connected to the first 2000 high-value instances 
+# then randomly choose another 3000 instances
+# create the instance-property mapping 
+
+ 
+ 
+def get_property(instance):
+    id = re.search(r'Q[0-9]+', instance)[0]
+    SPARQL_query = SPARQL_template % id 
+    try:
+        single_list_property = []
+        results = query_wiki.get_results(SPARQL_query)
+        bindings = results['results']['bindings']
+        for b in bindings:
+            item = b['item']['value']
+            if item in property_record: # it already exists 
+                # then do nothing, it is repeated 
+                pass
+            else:
+                print('item', item) # item is the URI of the property, itemLabel to be its label, and 
+                property_record.append(item) # it is new, add it to the list 
+                list_of_properties.append(b)
+                tmp = {}
+                if 'item' in b:
+                    tmp['uri']= b['item']['value']
+            
+                if 'itemLabel' in b:
+                    tmp['label']= b['itemLabel']['value']
+                
+                if 'itemAltLabel' in b:
+                    altlabel = b['itemAltLabel']['value']
+                    tmp['alt_label'] = altlabel.split(',')
+                
+                single_list_property.append(tmp)
+                print('collected', len(list_of_properties))
+                
+        if id not in instance_property_mapping:
+            instance_property_mapping[id] = {'properties': single_list_property}
+    except:
+        errors.append(id)
+        with open('property_log', 'w') as f:
+            f.write(json.dumps(errors) + '\n')
+            f.close()
+        pass        
+    time.sleep(0.5)
+        
+
+random_counter = 0
 with open('WIKI_URI_LIST') as f:
     instances = json.loads(f.read())
     print('number of instances', len(instances))
-    for instance in instances:
-        counter = counter + 1
-        if counter == 500:
-            with open('distinct_properties', 'w') as f:
-                f.write(json.dumps(list_of_properties))
-                f.close()
-                
-            exit()
-        print('iterated', counter, 'out of', len(instances))
-        id = re.search(r'Q[0-9]+', instance)[0]
-        SPARQL_query = SPARQL_template % id 
-        try:
-            results = query_wiki.get_results(SPARQL_query)
-            bindings = results['results']['bindings']
-            for b in bindings:
-                item = b['item']['value']
-                if item in property_record: # it already exists 
-                    # then do nothing, it is repeated 
-                    pass
-                else:
-                    print('item', item)
-                    property_record.append(item) # it is new, add it to the list 
-                    list_of_properties.append(b)
-                    print('collected', len(list_of_properties))
-        except:
-            pass        
-        time.sleep(1)
+    
+random_instance = random.sample(instances[2000:], 3000)
+ 
+# get the properties of the first 
+for instance in instances:
+    counter = counter + 1
+    print('iterated', counter, 'out of 2000')
+    get_property(instance)
+             
+
+
+with open('instance_property_mapping_first_2000', 'w') as f:
+    f.write(json.dumps(instance_property_mapping))
+    f.close()
+
+with open('distinct_properties', 'w') as f:
+    f.write(json.dumps(list_of_properties))
+    f.close()        
+             
+             
+instance_property_mapping = {}
+for instance in random_instance:
+    random_counter = random_counter + 1
+    print('iterated', random_counter, 'out of 3000')
+
+    get_property(instance)
+            
+with open('instance_property_mapping_random_3000', 'w') as f:
+    f.write(json.dumps(instance_property_mapping))
+    f.close()
+            
+        
+
+##################################################
+# randomly choose another 
     
     
 
--- JPS_Chatbot/UI/data_preparation/Wiki_basic_info/query_species_classes_and_labels.py
+++ JPS_Chatbot/UI/data_preparation/Wiki_basic_info/query_species_classes_and_labels.py
@@ -1,5 +1,8 @@
 from util.SPARQL_Query_Wiki import SPARQL_Query_for_Wiki
 import json, re, time
+from os import listdir
+from os.path import isfile, join
+
 
 SPARQL_template = '''
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
@@ -20,50 +23,61 @@ query_wiki = SPARQL_Query_for_Wiki()
 
 distinct_classes = []
 
-with open('query_log', 'w') as f:
-    f.write(str(time.time()) + '\n')
-    f.close()
+# with open('query_log', 'w') as f:
+    # f.write(str(time.time()) + '\n')
+    # f.close()
 
-counter = 0
+
+mypath = './instance_info'
+existing_files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.startswith('Q')]
+
+
+
+counter = len(existing_files)
 
 start_time = time.time()
 with open('WIKI_URI_LIST') as f:
     instances = json.loads(f.read())
     print('number of instances', len(instances))
     for instance in instances:
-        counter = counter + 1
-        print('iterated', counter, 'out of', len(instances))
-        print('already took', round(time.time() - start_time, 2))
         id = re.search(r'Q[0-9]+', instance)[0]
-        SPARQL_query = SPARQL_template % (id, id)
-        try:
-            results = query_wiki.get_results(SPARQL_query)
-            with open('instance_info/%s' % id, 'w') as f:
-                f.write(json.dumps(results, indent=4))
-                f.close()
-            bindings = results['results']['bindings']
-            for b in bindings:
-                if 'class' in b:
-                    class_uri = b['class']['value']
-                    if 'classLabel' in b:
-                        class_label = b['classLabel']['value']
-                
-                        if class_uri in distinct_classes: # it already exists 
-                            # then do nothing, it is repeated 
-                            pass
-                        else:
-                            tmp = {class_uri: class_label}
-                            distinct_classes.append(tmp)
-                            with open('distinct_classes', 'w') as f:
-                                f.write(json.dumps(distinct_classes))
-                                f.close()
-                     
-        except:
-            with open('query_log', 'a') as f:
-                f.write(instance + '\n')
-                f.close()
-            pass        
-        time.sleep(1)
+        if id in existing_files:
+            pass
+        
+        else:
+    
+            counter = counter + 1
+            print('iterated', counter, 'out of', len(instances))
+            print('already took', round(time.time() - start_time, 2))
+            SPARQL_query = SPARQL_template % (id, id)
+            try:
+                results = query_wiki.get_results(SPARQL_query)
+                with open('instance_info/%s' % id, 'w') as f:
+                    f.write(json.dumps(results, indent=4))
+                    f.close()
+                bindings = results['results']['bindings']
+                for b in bindings:
+                    if 'class' in b:
+                        class_uri = b['class']['value']
+                        if 'classLabel' in b:
+                            class_label = b['classLabel']['value']
+                    
+                            if class_uri in distinct_classes: # it already exists 
+                                # then do nothing, it is repeated 
+                                pass
+                            else:
+                                tmp = {class_uri: class_label}
+                                distinct_classes.append(tmp)
+                                with open('distinct_classes', 'w') as f:
+                                    f.write(json.dumps(distinct_classes))
+                                    f.close()
+                         
+            except:
+                with open('query_log', 'a') as f:
+                    f.write(instance + '\n')
+                    f.close()
+                pass        
+            time.sleep(0.1)
     
 
 
