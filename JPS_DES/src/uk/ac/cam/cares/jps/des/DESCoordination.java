12a9b37ac1777d29b97c5865d048f809f7b821c8
long <laura.ong@cares.cam.ac.uk>
2021-04-29 11:20:37 +0800

    990-hotfix-laura-handover: JPS_DES: remove hardcoded string from DES

--- JPS_DES/src/uk/ac/cam/cares/jps/des/DESCoordination.java
+++ JPS_DES/src/uk/ac/cam/cares/jps/des/DESCoordination.java
@@ -14,16 +14,17 @@ import uk.ac.cam.cares.jps.base.scenario.BucketHelper;
 public class DESCoordination extends JPSAgent{
 
 	private static final long serialVersionUID = 1L;
+	private static String TWA_Singapore = "http://www.theworldavatar.com/kb/sgp/singapore";
 	@Override
 	public JSONObject processRequestParameters(JSONObject requestParams) {
 		
  	        String scenarioUrl = BucketHelper.getScenarioUrl();
  	        String usecaseUrl = BucketHelper.getUsecaseUrl();
  	        logger.info("DES scenarioUrl = " + scenarioUrl + ", usecaseUrl = " + usecaseUrl);
- 	        requestParams.put("electricalnetwork", "http://www.theworldavatar.com/kb/sgp/singapore/singaporeelectricalnetwork/SingaporeElectricalNetwork.owl#SingaporeElectricalNetwork");
- 	        requestParams.put("district", "http://www.theworldavatar.com/kb/sgp/singapore/District-001.owl#District-001");
- 	        requestParams.put("temperatureforecast", "http://www.theworldavatar.com/kb/sgp/singapore/SGTemperatureForecast-001.owl#SGTemperatureForecast-001");
- 	        requestParams.put("irradiationforecast", "http://www.theworldavatar.com/kb/sgp/singapore/SGSolarIrradiationForecast-001.owl#SGSolarIrradiationForecast-001");
+ 	        requestParams.put("electricalnetwork", TWA_Singapore +"/singaporeelectricalnetwork/SingaporeElectricalNetwork.owl#SingaporeElectricalNetwork");
+ 	        requestParams.put("district", TWA_Singapore +"/District-001.owl#District-001");
+ 	        requestParams.put("temperatureforecast", TWA_Singapore +"/SGTemperatureForecast-001.owl#SGTemperatureForecast-001");
+ 	        requestParams.put("irradiationforecast", TWA_Singapore +"/SGSolarIrradiationForecast-001.owl#SGSolarIrradiationForecast-001");
  	        requestParams.put("cityIRI", "http://dbpedia.org/page/Singapore");
  	        requestParams.put("baseUrl",  QueryBroker.getLocalDataPath()+"/JPS_DES");
  	        AgentCaller.executeGetWithJsonParameter("JPS_DES/GetForecastData", requestParams.toString());
