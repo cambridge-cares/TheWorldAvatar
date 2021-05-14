1577cf31ff5bd292fe5c2fdd02c04ba81254ab58
Sebastian Mosbach <sm453@cam.ac.uk>
2021-05-14 08:25:11 +0100

    develop: Reverted "Add files via upload". This reverts commit 05039044fe403a289e126dc82c8071b2ba753bba.

--- JPS_ONTOMATCH/python/createVenv.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set op=%CD%
-FOR /F "tokens=*" %%a IN ('where python') do (SET VAR=%%a)
-cd %var:~0,-11%
-pip install virtualenv
-pip install virtualenvwrapper-win
-cd %op%
-python -m venv jps_ontomatch
-.\jps_ontomatch\Scripts\activate & pip install -r requirements.txt
--- JPS_ONTOMATCH/python/createVenv.sh
+++
@@ -1,4 +0,0 @@
-mkdir jpsontomatch
-#no need to copy again
-python -m pip install pipenv
-python -m pipinstall --python 3.6 -r requirements.txt
