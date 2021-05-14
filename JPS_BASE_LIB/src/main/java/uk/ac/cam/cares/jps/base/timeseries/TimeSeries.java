23c2e6b1f175fe43b2b382a4a28c0e686443a7c6
Kok Foong Lee <kflee@cmclinnovations.com>
2021-05-14 17:55:36 +0100

    134-dev-timeseries: allow option to specify any generic type

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/timeseries/TimeSeries.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/timeseries/TimeSeries.java
@@ -5,18 +5,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class TimeSeries {
-    private final List<Long> time;
-    private final List<Double> values;
+    private final List<Object> time;
+    private final List<List<Object>> values;
 
-    public TimeSeries(List<Long> time, List<Double> values) {
+    public TimeSeries(List<Object> time, List<List<Object>> values) {
     	this.time = new ArrayList<>(time);
-    	this.values= new ArrayList<>(values);
+    	
+    	this.values= new ArrayList<>();
+    	
+    	for (int i=0; i<values.size(); i++) {
+    		this.values.add(new ArrayList<>(values.get(i)));
+    	}
     }
 
-	public List<Long> getTime() {
+	public List<Object> getTime() {
 		return Collections.unmodifiableList(time);
 	}
-	public List<Double> getValues() {
+	public List<List<Object>> getValues() {
 		return Collections.unmodifiableList(values);
 	}
 }
