2d758d4165f77e530544a5553e8404d1341d68c7
Michael Hillman <mdhillman@cmclinnovations.com>
2021-05-18 12:21:39 +0100

    dev-website-views: Nginx logs now rotated daily, parsed, and appended to monthly CSV files.

--- Deploy/stacks/web/go_access/generate-report.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Combine all daily logs into a single one
-cat /var/log/reverse_proxy/*.log > /var/log/goaccess/combined.log
-
-# Run GoAccess to produce static HTML file
-goaccess /var/log/goaccess/combined.log -o /var/log/goaccess/index.html --log-format=COMBINED
