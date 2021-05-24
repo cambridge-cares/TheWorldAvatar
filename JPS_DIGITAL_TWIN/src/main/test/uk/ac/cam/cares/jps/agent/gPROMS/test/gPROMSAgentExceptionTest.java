5e48b865352a1b6c646bfd7be23937d1d9e6a5df
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-05-24 17:50:39 +0800

    Remove unused import from gPROMSAgentExceptionTest.java.

--- JPS_DIGITAL_TWIN/src/main/test/uk/ac/cam/cares/jps/agent/gPROMS/test/gPROMSAgentExceptionTest.java
+++ JPS_DIGITAL_TWIN/src/main/test/uk/ac/cam/cares/jps/agent/gPROMS/test/gPROMSAgentExceptionTest.java
@@ -2,7 +2,6 @@ package uk.ac.cam.cares.jps.agent.gPROMS.test;
 
 import junit.framework.TestCase;
 import org.junit.Before;
-import org.junit.Test;
 import uk.ac.cam.cares.jps.agent.gPROMS.gPROMSAgentException;
 
 public class gPROMSAgentExceptionTest extends TestCase {
