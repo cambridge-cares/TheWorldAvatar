7d35e054786a6114e124b7a04f9996f17c68e459
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-05-24 15:25:56 +0800

    Add test for gPROMSAgentException.

---
+++ JPS_DIGITAL_TWIN/src/main/test/uk/ac/cam/cares/jps/agent/gPROMS/test/gPROMSAgentExceptionTest.java
@@ -0,0 +1,34 @@
+package uk.ac.cam.cares.jps.agent.gPROMS.test;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.cam.cares.jps.agent.gPROMS.gPROMSAgentException;
+
+public class gPROMSAgentExceptionTest extends TestCase {
+
+    private String errorMessage;
+
+    @Before
+    public void createErrorMessage() {
+        errorMessage = "This is a gPROMSAgentException";
+    }
+
+    public void testThrowGPROMSAgentExceptionWithMessage() {
+        try {
+            throw new gPROMSAgentException(errorMessage);
+        } catch (gPROMSAgentException exception) {
+            assertEquals(errorMessage, exception.getMessage());
+        }
+    }
+
+    public void testThrowGPROMSAgentExceptionWithMessageAndCause() {
+        try {
+            throw new gPROMSAgentException(errorMessage, null);
+        } catch (gPROMSAgentException exception) {
+            assertEquals(errorMessage, exception.getMessage());
+            assertNull(exception.getCause());
+        }
+    }
+
+}
