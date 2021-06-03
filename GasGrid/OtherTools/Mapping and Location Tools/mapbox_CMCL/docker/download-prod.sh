4c7513b663222055a75e9bba3665ce1146ea095f
Michael Hillman <mdhillman@cmclinnovations.com>
2021-06-03 17:13:28 +0100

    dev-gasgrid: Started script to query terminal flow data from KG and dump to JSON

--- GasGrid/OtherTools/Mapping and Location Tools/mapbox_CMCL/docker/download-prod.sh
+++ GasGrid/OtherTools/Mapping and Location Tools/mapbox_CMCL/docker/download-prod.sh
@@ -2,9 +2,10 @@
 
 # This script downloads the GeoJSON files from the PRODUCTION 
 # version of the Gas Grid Agent running at CMCL.
+mkdir -p /var/www/html/geoJSON_assets
 cd /var/www/html/geoJSON_assets
 
-wget --quiet http://kg.cmclinnovations.com/digital-twin/gas-grid-agent/terminals.geojson
-wget --quiet http://kg.cmclinnovations.com/digital-twin/gas-grid-agent/offtakes.geojson
-wget --quiet http://kg.cmclinnovations.com/digital-twin/gas-grid-agent/pipe_network.geojson
+wget --quiet -O terminals.geojson http://kg.cmclinnovations.com/digital-twin/gas-grid-agent/terminals.geojson
+wget --quiet -O offtakes.geojson http://kg.cmclinnovations.com/digital-twin/gas-grid-agent/offtakes.geojson
+wget --quiet -O pipe_network.geojson http://kg.cmclinnovations.com/digital-twin/gas-grid-agent/pipe_network.geojson
 echo Production files downloaded at `date` >> /var/log/gas-grid/downloads.log
--- GasGrid/OtherTools/Mapping and Location Tools/mapbox_CMCL/docker/start-up.sh
+++ GasGrid/OtherTools/Mapping and Location Tools/mapbox_CMCL/docker/start-up.sh
@@ -4,15 +4,12 @@
 # run whenever the Docker container is started.
 echo "Running start-up.sh script..."
 
+# Start the Apache web server
+/usr/local/apache2/bin/httpd -k start
+
 # Load the cron-jobs file into crontab
 echo "Registering cron jobs..."
 crontab /usr/local/cron-jobs
 
-# Start the Apache web server
-#service apache2 start
-
 # Run the download script at start-up (in addition to via cron)
 /usr/local/download.sh
-
-# Keep the container running
-tail -f /dev/null
