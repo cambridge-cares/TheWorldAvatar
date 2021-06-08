9a7dd2d260c8d6bcb381fb5c4674cbe1f8d91514
Arkadiusz Chadzynski <arkadiusz.chadzynski@cares.cam.ac.uk>
2021-06-08 14:14:48 +0800

    Changed access to fields and methods. Added functionality to watch for any file in a directory. Amended unit tests.

--- AsynchronousWatcherService/src/test/java/CreateFileWatcherTest.java
+++ AsynchronousWatcherService/src/test/java/CreateFileWatcherTest.java
@@ -1,5 +1,6 @@
 import junit.framework.TestCase;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
+import uk.ac.cam.cares.jps.aws.AsynchronousWatcherService;
 import uk.ac.cam.cares.jps.aws.CreateFileWatcher;
 import uk.ac.cam.cares.jps.aws.Watcher;
 import uk.ac.cam.cares.jps.aws.WatcherCallback;
@@ -7,6 +8,8 @@ import uk.ac.cam.cares.jps.aws.WatcherCallback;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CreateFileWatcherTest extends TestCase {
@@ -14,7 +17,7 @@ public class CreateFileWatcherTest extends TestCase {
     public void testNewCreateFileWatcher() {
         CreateFileWatcher cfw = null;
         try {
-            cfw = new CreateFileWatcher(new File("./tmp/test.watch"), 1);
+            cfw = new CreateFileWatcher(new File(System.getProperty("java.io.tmpdir") + "/test/test.watch"), 1);
         } finally {
             assertNotNull(cfw);
             assertEquals(cfw.getClass(), CreateFileWatcher.class);
@@ -25,13 +28,14 @@ public class CreateFileWatcherTest extends TestCase {
     }
 
     public void testNewCreateFileWatcherFields() throws NoSuchFieldException, IllegalAccessException {
-        File fileIn = new File("./tmp/test.watch");
+        File fileIn = new File(System.getProperty("java.io.tmpdir") + "/test/test.watch");
         CreateFileWatcher cfw = new CreateFileWatcher(fileIn, 1);
         Field file = null;
         Field timeout = null;
         Field stop = null;
         Field callback = null;
-        assertEquals(4, cfw.getClass().getDeclaredFields().length);
+        Field anyFile = null;
+        assertEquals(5, cfw.getClass().getDeclaredFields().length);
         try {
             file = cfw.getClass().getDeclaredField("file");
             file.setAccessible(true);
@@ -41,16 +45,19 @@ public class CreateFileWatcherTest extends TestCase {
             stop.setAccessible(true);
             callback = cfw.getClass().getDeclaredField("callback");
             callback.setAccessible(true);
+            anyFile = cfw.getClass().getDeclaredField("anyFile");
+            anyFile.setAccessible(true);
         }   finally {
             assertEquals(file.get(cfw), fileIn);
             assertEquals(timeout.get(cfw), 1);
             assertEquals(false, ((AtomicBoolean) stop.get(cfw)).get());
             assertNull(callback.get(cfw));
+            assertFalse((Boolean) anyFile.get(cfw));
         }
     }
 
     public void testNewCreateFileWatcherMethods() throws NoSuchFieldException, IllegalAccessException, IOException {
-        File fileIn = new File("./tmp/test.watch");
+        File fileIn = new File(System.getProperty("java.io.tmpdir") + "/test/test.watch");
         new File(fileIn.getParent()).mkdirs();
         CreateFileWatcher cfw = new CreateFileWatcher(fileIn, 1000);
         WatcherCallback cb = () -> {assertTrue(true);};
@@ -113,8 +120,8 @@ public class CreateFileWatcherTest extends TestCase {
     }
 
     public void testNewCreateFileWatcherRunMethodTimedout() throws IOException {
-        File fileIn = new File("./tmp/test.watch");
-        File fileOut = new File("./tmp/test.callback");
+        File fileIn = new File(System.getProperty("java.io.tmpdir") + "/test/test.watch");
+        File fileOut = new File(System.getProperty("java.io.tmpdir") + "/test/test.callback");
         new File(fileIn.getParent()).mkdirs();
         CreateFileWatcher cfw = new CreateFileWatcher(fileIn, 100);
         WatcherCallback cb = () -> {
@@ -142,5 +149,30 @@ public class CreateFileWatcherTest extends TestCase {
         }
     }
 
+    public void testNewCreateFileWatcherGetCallbackMethod() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        CreateFileWatcher aws = new CreateFileWatcher(new File(System.getProperty("java.io.tmpdir") + "/test/test.watch"), 1);
+
+        assertNotNull(aws.getClass().getDeclaredMethod("getCallback", String.class, String.class));
+        Method getCallback = aws.getClass().getDeclaredMethod("getCallback", String.class, String.class);
+        getCallback.setAccessible(true);
+        String url = "";
+        String json = "";
+
+        Object result = getCallback.invoke(aws, url, json);
+        assertEquals(result.getClass().getInterfaces()[0], WatcherCallback.class);
+    }
+
+    public void testNewCreateFileWatcherSetAnyFileMethod() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+        CreateFileWatcher aws = new CreateFileWatcher(new File(System.getProperty("java.io.tmpdir") + "/test/test.watch"), 1);
+
+        assertNotNull(aws.getClass().getDeclaredMethod("setWatchAnyFile", boolean.class));
+        Method setWatchAnyFile = aws.getClass().getDeclaredMethod("setWatchAnyFile", boolean.class);
+        Field anyFile = aws.getClass().getDeclaredField("anyFile");
+        anyFile.setAccessible(true);
+        assertFalse((Boolean) anyFile.get(aws));
+        setWatchAnyFile.setAccessible(true);
+        setWatchAnyFile.invoke(aws, true);
+        assertTrue((Boolean) anyFile.get(aws));
+    }
 
 }
