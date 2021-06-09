6ee3216cf716af5349a777fba748d3c4994e42a4
Michael Hillman <mdhillman@cmclinnovations.com>
2021-06-09 17:31:14 +0100

    dev-new-powersystem-vis: Typo in docker-compose.yml

--- web/UKDigitalTwin/mapbox-vis/docker/start-up.sh
+++ web/UKDigitalTwin/mapbox-vis/docker/start-up.sh
@@ -4,10 +4,10 @@
 # run whenever the Docker container is started.
 echo "Running start-up.sh script..."
 
+# Start the Apache web server
+service apache2 start
+
 # Run the JSON_Generator script
 export PYTHONPATH=/usr/local/lib/python3.7/site-packages
 python3 -m pip install -r /requirements.txt
 python3 /var/www/html/JSON_Generator.py 
-
-# Start the Apache web server
-service apache2 start
