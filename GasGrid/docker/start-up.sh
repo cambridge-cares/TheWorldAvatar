f9ce8287e24c9b525b214e596e508e07a9b0e211
Michael Hillman <mdhillman@cmclinnovations.com>
2021-06-10 10:45:09 +0100

    dev-gasgrid: Reordered start up proceedure for Gas Grid Agent image.

--- GasGrid/docker/start-up.sh
+++ GasGrid/docker/start-up.sh
@@ -9,15 +9,23 @@ echo "Running start-up.sh script..."
 echo "Launching the terminal-update.py script in a new process..."
 python /app/input/terminal-update.py -continuous > /var/log/gas-grid/update.log 2>&1 &
 
-# Run the 'run-outputs.sh' script on boot
-echo "Running 'run-outputs.sh' script..."
-/app/output/run-outputs.sh
+# Start the Apache web server
+service apache2 start
 
 # Load the cron-jobs file into crontab
 echo "Registering cron jobs..."
 crontab /app/cron-jobs
 
+# Start cron service
+echo "Starting cron..."
+service cron start
+
+# Run the 'run-outputs.sh' script on boot
+echo "Running 'run-outputs.sh' script (may take up to 10 minutes)..."
+/app/output/run-outputs.sh
+
 # Start the Apache web server
+echo "Starting Apache..."
 service apache2 start
 
 # Keep the container running
