45b0b06bad868ca70e32904b657beb56481a7a65
Kok Foong Lee <kflee@cmclinnovations.com>
2021-06-25 14:27:29 +0100

    134-dev-timeseries: tidied up TimeSeriesClientInterface

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/interfaces/TimeSeriesClientInterface.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/interfaces/TimeSeriesClientInterface.java
@@ -1,20 +1,43 @@
 package uk.ac.cam.cares.jps.base.interfaces;
 
+import java.util.List;
+
 import uk.ac.cam.cares.jps.base.timeseries.TimeSeries;
 
-public interface TimeSeriesClientInterface {
-	/** 
-	 * sets the KnowledgeBaseClient so that the TimeSeriesClient knows where to execute queries/updates
-	 * @param kbClient
+public interface TimeSeriesClientInterface<T> {
+	/**
+	 * appends given time series data to your database
+	 * @param ts
 	 */
-//	void setKBClient(KnowledgeBaseClientInterface kbClient);
-//	
-//	/**
-//	 * Instantiates time series IRI in your endpoint
-//	 * @param TimeSeries
-//	 * @param timeUnit
-//	 */
-//	void init(TimeSeries<?, ?> ts);
-//
-//	void addData(TimeSeries<?, ?> ts);
+	void addTimeSeries(TimeSeries<T> ts);
+	
+	/**
+	 * returns the entire time series
+	 * @param dataIRI
+	 * @return
+	 */
+	TimeSeries<T> getTimeSeries(List<String> dataIRI);
+	
+	/**
+	 * returns time series within the given bounds
+	 * @param dataIRI
+	 * @param lowerbound
+	 * @param upperbound
+	 * @return
+	 */
+	TimeSeries<T> getTimeSeriesWithinBounds(List<String> dataIRI, T lowerbound, T upperbound);
+	
+	/**
+	 * deletes data between the given bounds
+	 * @param dataIRI
+	 * @param lowerBound
+	 * @param upperBound
+	 */
+	void deleteRows(String dataIRI, T lowerBound, T upperBound);
+	
+	/**
+	 * deletes the entire time series data
+	 * @param dataIRI
+	 */
+	void deleteTimeSeries(String dataIRI);
 }
