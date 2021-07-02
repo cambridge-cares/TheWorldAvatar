ab817498c30f07d4ce5b69b07efa6fd6f6fbe140
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-07-02 15:04:25 +0800

    Change unit test after Junit4 and fixed getNewJobFolderName test using a mockup timestamp.

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/slurm/job/test/SlurmJobTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/slurm/job/test/SlurmJobTest.java
@@ -1,22 +1,32 @@
 package uk.ac.cam.cares.jps.base.slurm.job.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import uk.ac.cam.cares.jps.base.slurm.job.SlurmJob;
 import uk.ac.cam.cares.jps.base.slurm.job.Utils;
-import junit.framework.TestCase;
 
-public class SlurmJobTest extends TestCase{
+public class SlurmJobTest {
 	
-	SlurmJob Test = new SlurmJob();
+	SlurmJob test = new SlurmJob();
 	
 	//Test setJobFolderName() and getJobFolderName()
-	public void testsetJobFolderName() {
-		Test.setJobFolderName("Example_One");
-		assertEquals("Example_One",Test.getJobFolderName());
+	@Test
+	public void testSetJobFolderName() {
+		test.setJobFolderName("Example_One");
+		Assert.assertEquals("Example_One", test.getJobFolderName());
 	}
+
 	//Test getNewJobFolderName()
-	public void testgetNewJobFolderName() {
-		
-		assertEquals("Example_Two_".concat("" + Utils.getTimeStamp()),Test.getNewJobFolderName("Example_Two"));
+	@Test
+	public void testGetNewJobFolderName() {
+		String folderName = "Example_Two_1234567";
+		try (MockedStatic<Utils> theMock = Mockito.mockStatic(Utils.class)) {
+			theMock.when(Utils::getTimeStamp).thenReturn(1234567L);
+			Assert.assertEquals(folderName, test.getNewJobFolderName("Example_Two"));
+		}
+		//Assert.assertEquals("Example_Two_".concat("" + Utils.getTimeStamp()), test.getNewJobFolderName("Example_Two"));
 	}
 }
 
