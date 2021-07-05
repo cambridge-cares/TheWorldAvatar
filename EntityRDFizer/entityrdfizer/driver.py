419cf39b5bf239392bed5a73224fae4bbba21c7c
dln22 <danieln@cmclinnovations.com>
2021-07-05 14:33:32 +0100

    dev-scanparse: Updated the rdfizer usage string and readme.

--- EntityRDFizer/entityrdfizer/driver.py
+++ EntityRDFizer/entityrdfizer/driver.py
@@ -4,10 +4,10 @@ import errno
 import glob
 from entityrdfizer import convertDir, convertFile
 
-__doc__ = """entrdifizer
+__doc__ = """csv2rdf
 
 Usage:
-    entrdifizer <fileOrDirPath> [--outDir=<OUT_DIR>]
+    csv2rdf <csvFileOrDirPath> [--outDir=<OUT_DIR>]
 
 Options:
 --outDir=<OUT_DIR>   Output directory path
@@ -19,13 +19,13 @@ def main():
     except DocoptExit:
         raise DocoptExit('Error: parser called with wrong arguments.')
 
-    if os.path.isfile(args['<fileOrDirPath>']):
-        convertFile(args['<fileOrDirPath>'],args['--outDir'])
-    elif os.path.isdir(args['<fileOrDirPath>']):
-        convertDir(args['<fileOrDirPath>'],args['--outDir'])
+    if os.path.isfile(args['<csvFileOrDirPath>']):
+        convertFile(args['<csvFileOrDirPath>'],args['--outDir'])
+    elif os.path.isdir(args['<csvFileOrDirPath>']):
+        convertDir(args['<csvFileOrDirPath>'],args['--outDir'])
     else:
         raise FileNotFoundError(
-            errno.ENOENT, os.strerror(errno.ENOENT), args['<fileOrDirPath>'])
+            errno.ENOENT, os.strerror(errno.ENOENT), args['<csvFileOrDirPath>'])
 
 if __name__ == '__main__':
     main()
