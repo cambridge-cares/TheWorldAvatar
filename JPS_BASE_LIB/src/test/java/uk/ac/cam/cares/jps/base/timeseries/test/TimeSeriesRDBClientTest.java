23b6ee1de4d4674da26b3dc725c3535875e0278b
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-07-13 09:33:06 +0800

    Add unit test for private fields and set method of TimeSeriesRDBClient.java

---
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/timeseries/test/TimeSeriesRDBClientTest.java
@@ -0,0 +1,120 @@
+package uk.ac.cam.cares.jps.base.timeseries.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import uk.ac.cam.cares.jps.base.interfaces.KnowledgeBaseClientInterface;
+import uk.ac.cam.cares.jps.base.query.RemoteKnowledgeBaseClient;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesRDBClient;
+
+import java.lang.reflect.Field;
+import java.time.Instant;
+
+public class TimeSeriesRDBClientTest {
+
+    @Test
+    public void testConstructor() throws NoSuchFieldException, IllegalAccessException {
+        TimeSeriesRDBClient client = new TimeSeriesRDBClient(Instant.class);
+        // Retrieve the value of the private field 'timeColumn' of the client to check its value
+        Field timeColumnField = client.getClass().getDeclaredField("timeColumn");
+        timeColumnField.setAccessible(true);
+        org.jooq.Field<String> timeColumn = (org.jooq.Field<String>) timeColumnField.get(client);
+        Assert.assertEquals("time", timeColumn.getName());
+        Assert.assertEquals(Instant.class, timeColumn.getType());
+    }
+
+    @Test
+    public void testPrivateDatabaseRelatedFields() throws NoSuchFieldException, IllegalAccessException {
+        TimeSeriesRDBClient client = new TimeSeriesRDBClient(Instant.class);
+        // Retrieve the value of the private field 'dbTableName' of the client to check its value
+        Field tableNameField = client.getClass().getDeclaredField("dbTableName");
+        tableNameField.setAccessible(true);
+        String tableName = (String) tableNameField.get(client);
+        Assert.assertEquals("dbTable", tableName);
+        // Retrieve the value of the private field 'dataIRIcolumn' of the client to check its value
+        Field dataIRIcolumnField = client.getClass().getDeclaredField("dataIRIcolumn");
+        dataIRIcolumnField.setAccessible(true);
+        org.jooq.Field<String> dataIRIcolumn = (org.jooq.Field<String>) dataIRIcolumnField.get(client);
+        Assert.assertEquals("dataIRI", dataIRIcolumn.getName());
+        Assert.assertEquals(String.class, dataIRIcolumn.getType());
+        // Retrieve the value of the private field 'tsIRIcolumn' of the client to check its value
+        Field tsIRIcolumnField = client.getClass().getDeclaredField("tsIRIcolumn");
+        tsIRIcolumnField.setAccessible(true);
+        org.jooq.Field<String> tsIRIcolumn = (org.jooq.Field<String>) tsIRIcolumnField.get(client);
+        Assert.assertEquals("timeseriesIRI", tsIRIcolumn.getName());
+        Assert.assertEquals(String.class, tsIRIcolumn.getType());
+        // Retrieve the value of the private field 'tsTableNameColumn' of the client to check its value
+        Field tsTableNameColumnField = client.getClass().getDeclaredField("tsTableNameColumn");
+        tsTableNameColumnField.setAccessible(true);
+        org.jooq.Field<String> tsTableNameColumn = (org.jooq.Field<String>) tsTableNameColumnField.get(client);
+        Assert.assertEquals("tableName", tsTableNameColumn.getName());
+        Assert.assertEquals(String.class, tsTableNameColumn.getType());
+        // Retrieve the value of the private field 'columnNameColumn' of the client to check its value
+        Field columnNameColumnField = client.getClass().getDeclaredField("columnNameColumn");
+        columnNameColumnField.setAccessible(true);
+        org.jooq.Field<String> columnNameColumn = (org.jooq.Field<String>) columnNameColumnField.get(client);
+        Assert.assertEquals("columnName", columnNameColumn.getName());
+        Assert.assertEquals(String.class, columnNameColumn.getType());
+    }
+
+    @Test
+    public void testSetKBClient() throws NoSuchFieldException, IllegalAccessException {
+        TimeSeriesRDBClient client = new TimeSeriesRDBClient(Instant.class);
+        // Retrieve the value of the private field 'kbClient' of the client to check its value
+        Field kbClientField = client.getClass().getDeclaredField("kbClient");
+        kbClientField.setAccessible(true);
+
+        Assert.assertNull(kbClientField.get(client));
+        KnowledgeBaseClientInterface kbClient = new RemoteKnowledgeBaseClient();
+        client.setKBClient(kbClient);
+        Assert.assertSame(kbClient, kbClientField.get(client));
+    }
+
+    @Test
+    public void testSetTimeUnit() throws NoSuchFieldException, IllegalAccessException {
+        TimeSeriesRDBClient client = new TimeSeriesRDBClient(Instant.class);
+        // Retrieve the value of the private field 'timeUnit' of the client to check its value
+        Field timeUnitField = client.getClass().getDeclaredField("timeUnit");
+        timeUnitField.setAccessible(true);
+
+        Assert.assertNull(timeUnitField.get(client));
+        client.setTimeUnit("s");
+        Assert.assertEquals("s", timeUnitField.get(client));
+    }
+
+    @Test
+    public void testSetRdbURL() throws NoSuchFieldException, IllegalAccessException {
+        TimeSeriesRDBClient client = new TimeSeriesRDBClient(Instant.class);
+        // Retrieve the value of the private field 'rdbURL' of the client to check its value
+        Field rdbURLField = client.getClass().getDeclaredField("rdbURL");
+        rdbURLField.setAccessible(true);
+
+        Assert.assertNull(rdbURLField.get(client));
+        client.setRdbURL("http://localhost:5342");
+        Assert.assertEquals("http://localhost:5342", rdbURLField.get(client));
+    }
+
+    @Test
+    public void testSetRdbUser() throws NoSuchFieldException, IllegalAccessException {
+        TimeSeriesRDBClient client = new TimeSeriesRDBClient(Instant.class);
+        // Retrieve the value of the private field 'rdbUser' of the client to check its value
+        Field rdbUserField = client.getClass().getDeclaredField("rdbUser");
+        rdbUserField.setAccessible(true);
+
+        Assert.assertNull(rdbUserField.get(client));
+        client.setRdbUser("postgres");
+        Assert.assertEquals("postgres", rdbUserField.get(client));
+    }
+
+    @Test
+    public void testSetRdbPassword() throws NoSuchFieldException, IllegalAccessException {
+        TimeSeriesRDBClient client = new TimeSeriesRDBClient(Instant.class);
+        // Retrieve the value of the private field 'rdbPassword' of the client to check its value
+        Field rdbPasswordField = client.getClass().getDeclaredField("rdbPassword");
+        rdbPasswordField.setAccessible(true);
+
+        Assert.assertNull(rdbPasswordField.get(client));
+        client.setRdbPassword("password");
+        Assert.assertEquals("password", rdbPasswordField.get(client));
+    }
+
+}
