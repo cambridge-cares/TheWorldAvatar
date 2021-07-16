b5db828a21570d343677c6c814db7ad318978947
sganguly-cares <srishti.ganguly@cares.cam.ac.uk>
2021-07-16 16:41:41 +0800

    Updated Unit test for JPSContext.java

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/scenario/test/JPSContextTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/scenario/test/JPSContextTest.java
@@ -8,121 +8,109 @@ import static org.junit.Assert.*;
 
 
 public class JPSContextTest{
-	
-	JSONObject jo = new JSONObject();
-	String key = "testkey";
-	String value = "testvalue";
-	
+
 	// Test get(JSONObject jo, String key) and put(JSONObject jo, String key, String value)
 	@Test
 	public void getTest(){
+		JSONObject jo = new JSONObject();
+		String key = "testkey";
+		String value = "testvalue";
+		// Default
 		assertNull(JPSContext.get(jo, key));
+		// Custom values
 		JPSContext.put(jo, key, value);
-		assertEquals("testvalue", JPSContext.get(jo, key)) ;
+		assertEquals("testvalue", JPSContext.get(jo, key));
 	}
 	
+	// Test getJpsContext()
 	@Test
 	public void getJpsContextTest() {
-		assertNull(JPSContext.getJpsContext());		
+		assertTrue(JPSContext.getJpsContext().length()==0);
+		
 	}
 	
+	// Test createJpsContext()
 	@Test
 	public void createJpsContextTest() {
 		assertTrue(JPSContext.createJpsContext().length()==0);
 	}
 	
-//	@Test
-//	public void putScenarioUrlTest() {
-//		//put(jo, JPSConstants.SCENARIO_URL, value); JSONObject jo, String value
-//		//Test null 
-//		JSONObject jo = null;
-//		String value = "testvalue";
-//		String key = "testkey";
-//	}
-	
+		
+	// Test putScenarioUrl(JSONObject jo, String value) and getScenarioUrl(JSONObject jo)
 	@Test
 	public void getScenarioUrlTest() {
+		JSONObject jo = new JSONObject();
+		String value = "testvalue";
 		assertNull(JPSContext.getScenarioUrl(jo));
+		JPSContext.putScenarioUrl(jo, value);
+		assertEquals("testvalue", JPSContext.getScenarioUrl(jo));
 	}
 	
-	@Test
-	public void putUsecaseUrlTest() {
-		//JSONObject jo, String value
-		//TODO
-	}
-	
+	// Test putUsecaseUrl((JSONObject jo, String value) and getUsecaseUrl(JSONObject jo)
 	@Test
 	public void getUsecaseUrlTest() {
+		JSONObject jo = new JSONObject();
+		String value = "testvalue";
 		assertNull(JPSContext.getUsecaseUrl(jo));
+		JPSContext.putUsecaseUrl(jo, value);
+		assertEquals("testvalue", JPSContext.getUsecaseUrl(jo));
 	}
 	
-	@Test
-	public void putSimulationTimeTest() {
-		//TODO
-	}
-	
+	// Test putSimulationTime(JSONObject jo, String value) and getSimulationTime(JSONObject jo)
 	@Test
 	public void getSimulationTimeTest() {
+		JSONObject jo = new JSONObject();
+		String value = "testvalue";
 		assertNull(JPSContext.getSimulationTime(jo));
+		JPSContext.putSimulationTime(jo, value);
+		assertEquals("testvalue", JPSContext.getSimulationTime(jo));
+		
 	}
-	
-	@Test
-	public void putTest2() {
-		//TODO
-	}
-	
+
+	// Test put(String key, String value); get(String key) and remove(String key)
 	@Test
 	public void getTest2() {
+		String key = "testkey";
+		String value = "testvalue";
+		assertNull(JPSContext.get(key));
+		JPSContext.put(key, value);
+		assertEquals("testvalue", JPSContext.get(key));
+		JPSContext.remove(key);
 		assertNull(JPSContext.get(key));
 	}
-	
-	@Test
-	public void removeTest() {
-		//TODO
-	}
-	
-	@Test
-	public void putScenarioUrlTest2() {
-		//TODO
-	}
-	
+
+	// Test putScenarioUrl(String value); getScenarioUrl() and removeScenarioUrl()
 	@Test
 	public void getScenarioUrlTest2() {
+		String value = "testvalue";
+		assertNull(JPSContext.getScenarioUrl());
+		JPSContext.putScenarioUrl(value);
+		assertEquals("testvalue", JPSContext.getScenarioUrl());
+		JPSContext.removeScenarioUrl();
 		assertNull(JPSContext.getScenarioUrl());
 	}
 	
-	@Test
-	public void removeScenarioUrlTest() {
-		//TODO
-	}
-	
-	@Test
-	public void putUsecaseUrlTest2() {
-		//TODO
-	}
-	
+	// Test putUsecaseUrl(String value); getUsecaseUrl() and removeUsecaseUrl()
 	@Test
 	public void getUsecaseUrlTest2() {
+		String value = "testvalue";
 		assertNull(JPSContext.getUsecaseUrl());
-	}
-	
-	@Test
-	public void removeUsecaseUrlTest() {
-		//TODO
-	}
-	
-	@Test
-	public void putSimulationTimeTest2() {
-		//TODO
-	}
-	
-	@Test
-	public void getSimulationTimeTest2() {
-		assertNull(JPSContext.getSimulationTime());
-	}
-	
-	@Test 
-	public void removeSimulationTimeTest() {
+		JPSContext.putUsecaseUrl(value);
+		assertEquals("testvalue", JPSContext.getUsecaseUrl());
+		JPSContext.removeUsecaseUrl();
+		assertNull(JPSContext.getUsecaseUrl());
 		
 	}
+	
+	// Test putSimulationTime(String value); getSimulationTime() and removeSimulationTime()
+	@Test
+	public void getSimulationTimeTest2() {
+		String value = "testvalue";
+		assertNull(JPSContext.getSimulationTime());
+		JPSContext.putSimulationTime(value);
+		assertEquals("testvalue", JPSContext.getSimulationTime());
+		JPSContext.removeSimulationTime();
+		assertNull(JPSContext.getSimulationTime());
+	}
+
 }
