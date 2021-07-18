a2455e97fec025949bab5ad7368cc6d1eda7915b
Markus Hofmeister <mh807@cam.ac.uk>
2021-07-18 19:47:12 +0200

    dev-1112: included unit tests for Sparql client

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/timeseries/test/TimeSeriesSparqlTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/timeseries/test/TimeSeriesSparqlTest.java
@@ -1,52 +1,185 @@
 package uk.ac.cam.cares.jps.base.timeseries.test;
 
-import org.jooq.CreateTableColumnStep;
-import org.jooq.DSLContext;
+import static org.eclipse.rdf4j.sparqlbuilder.rdf.Rdf.iri;
+import static org.eclipse.rdf4j.sparqlbuilder.rdf.Rdf.literalOf;
+
+import java.lang.reflect.Field;
+import java.sql.DriverManager;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.rdf4j.sparqlbuilder.constraint.Expressions;
+import org.eclipse.rdf4j.sparqlbuilder.core.Assignment;
+import org.eclipse.rdf4j.sparqlbuilder.core.Prefix;
+import org.eclipse.rdf4j.sparqlbuilder.core.SparqlBuilder;
+import org.eclipse.rdf4j.sparqlbuilder.core.Variable;
+import org.eclipse.rdf4j.sparqlbuilder.core.query.ModifyQuery;
+import org.eclipse.rdf4j.sparqlbuilder.core.query.Queries;
+import org.eclipse.rdf4j.sparqlbuilder.core.query.SelectQuery;
+import org.eclipse.rdf4j.sparqlbuilder.core.query.DeleteDataQuery;
+import org.eclipse.rdf4j.sparqlbuilder.graphpattern.GraphPattern;
+import org.eclipse.rdf4j.sparqlbuilder.graphpattern.GraphPatterns;
+import org.eclipse.rdf4j.sparqlbuilder.graphpattern.SubSelect;
+import org.eclipse.rdf4j.sparqlbuilder.graphpattern.TriplePattern;
+import org.eclipse.rdf4j.sparqlbuilder.rdf.Iri;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
+import org.json.JSONArray;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Ignore;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.postgresql.util.PSQLException;
+
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.interfaces.KnowledgeBaseClientInterface;
 import uk.ac.cam.cares.jps.base.query.RemoteKnowledgeBaseClient;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesRDBClient;
 import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesSparql;
 
-import java.lang.reflect.Field;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.time.Instant;
-import java.util.ArrayList;
 
 public class TimeSeriesSparqlTest {
 	
-//	// Create mocks
-//    private DSLContext context = Mockito.mock(DSLContext.class, Mockito.RETURNS_DEEP_STUBS);
-//
-//    @Test
-//    public void testConstructor() throws IllegalArgumentException, IllegalAccessException {
-//    	TimeSeriesSparql client = new TimeSeriesSparql();
-//
-//        // Test for correct field name and class
-//        Assert.assertEquals("time", timeColumn.getName());
-//        Assert.assertEquals(Instant.class, timeColumn.getType());
-//    }
-//    
-//    @Test
-//    public void testSetKBClient() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-//        
-//    	RemoteKnowledgeBaseClient kbClient = new RemoteKnowledgeBaseClient();
-//    	TimeSeriesRDBClient<Instant> client = new TimeSeriesRDBClient<>(Instant.class);
-//        // Retrieve the value of the private field 'kbClient' of the client to check its value
-//        Field kbClientField = client.getClass().getDeclaredField("kbClient");
-//        kbClientField.setAccessible(true);
-//
-//        Assert.assertNull(kbClientField.get(client));
-//        KnowledgeBaseClientInterface kbClient = new RemoteKnowledgeBaseClient();
-//        //client.setKBClient(kbClient);
-//        Assert.assertSame(kbClient, kbClientField.get(client));
-//    }
+	// Create mocks
+    private ModifyQuery modify = Mockito.mock(ModifyQuery.class);
+	private RemoteKnowledgeBaseClient mockClient = Mockito.mock(RemoteKnowledgeBaseClient.class);
+
+    @Test
+    public void testConstructor() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    	RemoteKnowledgeBaseClient kbClient = new RemoteKnowledgeBaseClient();    
+    	TimeSeriesSparql client = new TimeSeriesSparql(kbClient);
+    	// Retrieve the value of the private field 'kbClient' of the client
+        Field kbc = client.getClass().getDeclaredField("kbClient");
+        kbc.setAccessible(true);
+        RemoteKnowledgeBaseClient kbcl = (RemoteKnowledgeBaseClient) kbc.get(client);
+        // Test whether kbClients are the same 
+    	Assert.assertSame(kbcl, kbClient);
+    }
+    
+    @Test
+    public void testSetKbClient() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    	RemoteKnowledgeBaseClient kbClient1 = new RemoteKnowledgeBaseClient();
+    	RemoteKnowledgeBaseClient kbClient2 = new RemoteKnowledgeBaseClient();
+    	TimeSeriesSparql client = new TimeSeriesSparql(kbClient1);
+    	// Retrieve the value of the private field 'kbClient' of the client
+        Field kbc = client.getClass().getDeclaredField("kbClient");
+        kbc.setAccessible(true);
+        // Test whether kbClients are the same 
+    	Assert.assertSame(kbClient1, (RemoteKnowledgeBaseClient) kbc.get(client));
+    	client.setKBClient(kbClient2);
+    	Assert.assertNotSame(kbClient1, (RemoteKnowledgeBaseClient) kbc.get(client));
+    	Assert.assertSame(kbClient2, (RemoteKnowledgeBaseClient) kbc.get(client));
+    }
+    
+    @Test
+    public void testNamespaces() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    	RemoteKnowledgeBaseClient kbClient = new RemoteKnowledgeBaseClient();
+    	TimeSeriesSparql client = new TimeSeriesSparql(kbClient);
+    	// Retrieve the value of the private field 'ns_ontology' of the client
+        Field ns_onto = client.getClass().getDeclaredField("ns_ontology");
+        ns_onto.setAccessible(true);
+        String onto = (String) ns_onto.get(client);
+        Assert.assertEquals("http://www.theworldavatar.com/ontology/ontotimeseries/OntoTimeSeries.owl#", onto);
+    	// Retrieve the value of the private field 'ns_kb' of the client
+        Field ns_kb = client.getClass().getDeclaredField("ns_kb");
+        ns_kb.setAccessible(true);
+        String kb = (String) ns_kb.get(client);
+        Assert.assertEquals("http://www.theworldavatar.com/kb/ontotimeseries/", kb);        
+    }
+    
+    @Test
+    public void testPrefixes() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    	RemoteKnowledgeBaseClient kbClient = new RemoteKnowledgeBaseClient();
+    	TimeSeriesSparql client = new TimeSeriesSparql(kbClient);
+    	// Retrieve the value of the private field 'prefix_ontology' of the client
+        Field p_onto = client.getClass().getDeclaredField("prefix_ontology");
+        p_onto.setAccessible(true);
+        Prefix onto = (Prefix) p_onto.get(client);
+        Assert.assertEquals("PREFIX ts: <http://www.theworldavatar.com/ontology/ontotimeseries/OntoTimeSeries.owl#>", 
+        					onto.getQueryString());
+    	// Retrieve the value of the private field 'prefix_kb' of the client
+        Field p_kb = client.getClass().getDeclaredField("prefix_kb");
+        p_kb.setAccessible(true);
+        Prefix kb = (Prefix) p_kb.get(client);
+        Assert.assertEquals("PREFIX kb: <http://www.theworldavatar.com/kb/ontotimeseries/>", 
+        					kb.getQueryString());      
+    }
+    
+    @Test
+    public void testIRIs() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    	RemoteKnowledgeBaseClient kbClient = new RemoteKnowledgeBaseClient();
+    	TimeSeriesSparql client = new TimeSeriesSparql(kbClient);
+    	// Retrieve the value of the private field 'TimeSeries' of the client
+        Field timeseries = client.getClass().getDeclaredField("TimeSeries");
+        timeseries.setAccessible(true);
+        Iri ts = (Iri) timeseries.get(client);
+        Assert.assertEquals("ts:TimeSeries", ts.getQueryString());
+    	// Retrieve the value of the private field 'hasTimeSeries' of the client
+        Field hasTimeSeries = client.getClass().getDeclaredField("hasTimeSeries");
+        hasTimeSeries.setAccessible(true);
+        Iri has_ts = (Iri) hasTimeSeries.get(client);
+        Assert.assertEquals("ts:hasTimeSeries", has_ts.getQueryString());
+    	// Retrieve the value of the private field 'hasRDB' of the client
+        Field hasRDB = client.getClass().getDeclaredField("hasRDB");
+        hasRDB.setAccessible(true);
+        Iri rdb = (Iri) hasRDB.get(client);
+        Assert.assertEquals("ts:hasRDB", rdb.getQueryString());
+    	// Retrieve the value of the private field 'TimeSeries' of the client
+        Field hasTimeUnit = client.getClass().getDeclaredField("hasTimeUnit");
+        hasTimeUnit.setAccessible(true);
+        Iri unit = (Iri) hasTimeUnit.get(client);
+        Assert.assertEquals("ts:hasTimeUnit", unit.getQueryString());
+    }
+    
+    @Test
+    public void testInitTSExceptions() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    	RemoteKnowledgeBaseClient kbClient = new RemoteKnowledgeBaseClient();
+    	TimeSeriesSparql client = new TimeSeriesSparql(kbClient);
+    	// Test exception when neither tsIRI nor uuid is provided
+    	try {
+    		client.initTS(null, null, null, null, null);
+    	} catch (JPSRuntimeException e) {
+        	Assert.assertEquals("TimeSeriesSparql: Either the timeseries IRI OR the uuid shall be provided", 
+        						e.getMessage());
+    	}
+    	// Test exception for incorrect tsIRI format
+    	try {
+    		client.initTS("tsIRI", null, null, null, null);
+    	} catch (JPSRuntimeException e) {
+        	Assert.assertEquals("TimeSeriesSparql: Time series IRI needs to start with http://", 
+        						e.getMessage());
+    	}
+    	// Test that no exception is thrown for correct tsIRI
+    	try (MockedStatic<Queries> query = Mockito.mockStatic(Queries.class)) {
+    		// Mocks the behaviour of the Queries when new time series get initialised
+        	query.when(() -> Queries.MODIFY()).thenThrow(new JPSRuntimeException("No exceptions occured"));
+        	try {
+        		client.initTS("http://tsIRI", null, null, null, null);
+        	} catch (JPSRuntimeException e) {
+            	Assert.assertEquals("No exceptions occured", e.getMessage());
+        	}
+    	}
+    }
+    
+    @Test
+    public void testInitTS() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    	// Initialise test data
+    	String tsIRI = "http://tsIRI1";
+    	List<String> dataIRI = new ArrayList<>();
+    	dataIRI.add("http://data1"); dataIRI.add("http://data2"); dataIRI.add("http://data3"); 
+    	String dburl = "postgres";
+    	String timeunit = "s";
+    	
+    	TimeSeriesSparql client = new TimeSeriesSparql(mockClient);
+    	// Test whether modify query is called correct number of times (6 inserts for given sample data)
+    	try (MockedStatic<Queries> query = Mockito.mockStatic(Queries.class)) {
+    		// Mocks the behaviour of the Queries when new time series get initialised
+    		query.when(() -> Queries.MODIFY()).thenReturn(modify);
+    		Mockito.when(mockClient.executeUpdate(Mockito.anyString())).thenReturn(1);
+    		client.initTS(tsIRI, null, dataIRI, dburl, timeunit);
+    		//System.out.println(Mockito.mockingDetails(modify).printInvocations());
+    		Mockito.verify(modify, Mockito.times(6)).insert(Mockito.any());
+    	}
+    }
 }
