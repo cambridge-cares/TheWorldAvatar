597f127009a169efe22caee68d1cd54b3a197af4
dln22 <danieln@cmclinnovations.com>
2021-07-20 14:33:14 +0100

    dev-scanparse: Fixed fileExists function and added new dirExists and getFilesWithExtensions functions to chemutils/chemutils/ioutils/ioutils.py module.

--- thermo/chemutils/chemutils/ioutils/ioutils.py
+++ thermo/chemutils/chemutils/ioutils/ioutils.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 
 def readFile(path):
     with open (os.path.abspath(path), "r") as myfile:
@@ -10,7 +11,10 @@ def writeFile(path,data):
         myfile.write(data)
 
 def fileExists(path):
-    return os.path.exists(os.path.abspath(path))
+    return os.path.isfile(os.path.abspath(path))
+
+def dirExists(path):
+    return os.path.isdir(os.path.abspath(path))
 
 def verbosityBasedOutput(output, silent, outFile):
     if outFile is not None: writeFile(outFile, output)
@@ -20,3 +24,15 @@ def removeBlankTrailingLines(fileContentStr):
     fileContentStr = fileContentStr.split('\n')
     while not fileContentStr[-1].strip() and len(fileContentStr)>0: del fileContentStr[-1]
     return '\n'.join(fileContentStr)
+
+def getFilesWithExtensions(fileOrDir, fileExtList):
+    files = []
+    if fileExists(fileOrDir):
+        fileExt = os.path.splitext(fileOrDir)[1]
+        if fileExt in fileExtList: files = [fileOrDir]
+    elif dirExists(fileOrDir):
+        for fileExt in fileExtList:
+            files.extend([p.resolve() for p in list(pathlib.Path(fileOrDir).glob('*'+fileExt))])
+    else:
+        raise FileNotFoundError('Error: File or directory: "'+fileOrDir+'" does not exists.')
+    return files
