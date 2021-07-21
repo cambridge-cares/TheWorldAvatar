df9851c0027e443be742883f006404933d27425a
dln22 <danieln@cmclinnovations.com>
2021-07-21 10:37:01 +0100

    dev-scanparse: Added compchemparser as chemaboxwriters dependency.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/install_script_conda.sh
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/install_script_conda.sh
@@ -126,6 +126,9 @@ function install_local_dependencies {
     echo ""
     echo ""
 	$SPATH/../../../EntityRDFizer/install_script_conda.sh -i -s -n $VENV_NAME $DEV_INSTALL
+    echo ""
+    echo ""
+	$SPATH/../../../thermo/CoMoCompChemParser/install_script_conda.sh -i -s -n $VENV_NAME $DEV_INSTALL
 
     if [ $? -eq 0 ]; then
     	echo ""
