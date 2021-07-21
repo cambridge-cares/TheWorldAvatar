f2c2af028fa19b78c9c314fa4284523d2d64f5c1
sganguly-cares <srishti.ganguly@cares.cam.ac.uk>
2021-07-21 16:23:27 +0800

    Updated unit test for BucketHelper.java

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/scenario/test/BucketHelperTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/scenario/test/BucketHelperTest.java
@@ -1,19 +1,19 @@
 package uk.ac.cam.cares.jps.base.scenario.test;
 
 import uk.ac.cam.cares.jps.base.scenario.BucketHelper;
+import uk.ac.cam.cares.jps.base.scenario.ScenarioHelper;
 import uk.ac.cam.cares.jps.base.config.KeyValueManager;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.query.ResourcePathConverter;
 
 import org.junit.Test;
 import org.junit.function.ThrowingRunnable;
-import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
-import org.junit.Rule;
-
 public class BucketHelperTest{
 	
+	// Test getScenarioUrl 
 	@Test
 	public void TestgetScenarioUrl(){
 		
@@ -26,20 +26,28 @@ public class BucketHelperTest{
 		assertEquals(serveraddress + "/jps/scenario/base", BucketHelper.getScenarioUrl());
 	}
 	
+	
+	// Test getScenarioName
 	@Test
 	public void TestgetScenarioName() {
 		String TestscenarioUrl = "http://localhost:8080/jps/scenario/testscenario";
 		assertEquals("testscenario", BucketHelper.getScenarioName(TestscenarioUrl));
 	}
 	
+	
+	// Test getIriPrefix
 	@Test
 	public void TestgetIriPrefix() {
 		String Test_getIriPrefix = BucketHelper.getIriPrefix();
+		
+		// Break output into substring, exclude random UUID
 		int idx = Test_getIriPrefix.indexOf("/kb");
 		String substringToTest = Test_getIriPrefix.substring(0,idx+"/kb".length());
 		assertEquals("http://localhost:8080/jps/kb", substringToTest);
 	}
 	
+	
+	// Test getUsecaseUrl
 	@Test
 	public void TestgetUsecaseUrl() {
 		String TestscenarioUrl = "http://localhost:8080/jps/scenario/testscenario";
@@ -57,6 +65,8 @@ public class BucketHelperTest{
 		assertEquals("http://localhost:8080/jps/scenario/testscenario/kb/", substringToTest);
 	}
 	
+	
+	// Test getUsecaseUrlForData
 	@Test
 	public void TestgetUsecaseUrlForData() {
 		String Test_usecaseUrlforData = BucketHelper.getUsecaseUrlForData();
@@ -66,15 +76,17 @@ public class BucketHelperTest{
 	}
 	
 	
+	// Test getKbScenarioUrl
 	@Test(expected = JPSRuntimeException.class)
-	public void TestgetKbScenarioUrl() {
+	public void TestgetKbScenarioUrl() throws JPSRuntimeException {
 		
-		// Test if exception is thrown and its message
+		// Test if exception is thrown 
 		Exception exception = assertThrows(JPSRuntimeException.class, new ThrowingRunnable() {
 			public void run() throws Throwable {
 				BucketHelper.getKbScenarioUrl();
 			}
 		});
+		// Test if exception message is as expected
 		String expectedMessage = "can't create a scenario kb url for the base scenario";
 	    String actualMessage = exception.getMessage();
 	    assertTrue(actualMessage.contains(expectedMessage));
@@ -83,6 +95,8 @@ public class BucketHelperTest{
 		assertEquals("http://localhost:8080/jps/scenario/base/kb", BucketHelper.getKbScenarioUrl());
 	}
 	
+	
+	// Test isScenarioUrl
 	@Test
 	public void TestisScenarioUrl() {
 		// case True
@@ -93,6 +107,8 @@ public class BucketHelperTest{
 		assertFalse(BucketHelper.isScenarioUrl(testurl));
 	}
 	
+	
+	// Test isBaseScenario
 	@Test
 	public void TestisBaseScenario() {
 		String testurl = null;
@@ -105,34 +121,70 @@ public class BucketHelperTest{
 		assertTrue(BucketHelper.isBaseScenario(testurl));
 	}
 	
+	
+	// Test getLocalDataPath
 	@Test
 	public void TestgetLocalDataPath() {
-		//TODO
+		String test_localDataPath = BucketHelper.getLocalDataPath();
+		String scenario_workingdir = ScenarioHelper.getScenarioWorkingDir();
+		int idx = test_localDataPath.indexOf("/data/");
+		String substringToTest = test_localDataPath.substring(0,idx+"/data/".length());
+		assertEquals(scenario_workingdir + "/base/localhost_8080/data/", substringToTest);
 	}
 	
+	
+	// Test getLocalDataPathWithoutThreadContext
 	@Test
 	public void TestgetLocalDataPathWithoutThreadContext() {
-		//TODO
+		String test_LocalDataPathWithoutThreadContext = BucketHelper.getLocalDataPathWithoutThreadContext();
+		String scenario_workingdir = ScenarioHelper.getScenarioWorkingDir();
+		int idx = test_LocalDataPathWithoutThreadContext.indexOf("/data/");
+		String substringToTest = test_LocalDataPathWithoutThreadContext.substring(0,idx+"/data/".length());
+		assertEquals(scenario_workingdir + "/base/localhost_8080/data/", substringToTest);
 	}
 	
+	
+	// Test getLocalPath(String url)
 	@Test
-	public void TestgetLocalPath123() {
-		//TODO
+	public void TestgetLocalPath_singleinput() {
+		String test_url = "http://localhost:8080/jps/scenario/base";
+		String scenario_workingdir = ScenarioHelper.getScenarioWorkingDir();
+		assertEquals(scenario_workingdir + "/base", BucketHelper.getLocalPath(test_url));
 	}
 	
 	
-	
-//	@Test(expected = JPSRuntimeException.class)
-//	public void TestgetLocalPath() {
-//		String testurl = "http://localhost:8080/jps/scenario/base";
-//		String dataseturl = "test";
-//		//BucketHelper.getLocalPath(testurl, dataseturl).throwJPSRuntimeException();
-//	}
-	
-	
-//	@Test
-//	public void TestmapHost() {
-//		URI testuri = URI.create("http://java.sun.com/j2se/1.3/");
-//		assertEquals("here", BucketHelper.mapHost(testuri));
-//	}
+	// Test getLocalPath(String url, String datasetUrl)
+	@Test
+	public void TestgetLocalPath() {
+		
+		// Exception not thrown case
+	    String test_url = "http://localhost:8080/jps/scenario/base";
+	    String test_dataseturl = "http://localhost:8080/jps/dataset/testdata";
+	    String workingdir = ScenarioHelper.getJpsWorkingDir();
+	    assertEquals(workingdir + "/JPS_SCENARIO/dataset/testdata/localhost_8080/jps/scenario/base", BucketHelper.getLocalPath(test_url, test_dataseturl));
+		
+		// Test if exception is thrown 
+		Exception exception = assertThrows(JPSRuntimeException.class, new ThrowingRunnable() {
+			public void run() throws Throwable {
+				BucketHelper.getLocalPath("http://localhost:8080/jps/scenario/base", "test");
+			}
+		});
+		
+		// Test exception message is as expected
+		String expectedMessage = "unknown datasetUrl=test, url=http://localhost:8080/jps/scenario/base"; 
+	    String actualMessage = exception.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
+	    
+	    // Test various if statements
+	    test_dataseturl = null;
+	    String scenario_workingdir = ScenarioHelper.getScenarioWorkingDir();
+	    assertEquals(scenario_workingdir + "/base", BucketHelper.getLocalPath(test_url, test_dataseturl));
+	    
+	    test_url = "http://localhost:8080/data/jps/scenario/base";
+	    assertEquals(ResourcePathConverter.convertToLocalPath(test_url), BucketHelper.getLocalPath(test_url, test_dataseturl));
+
+	    test_url = "http://localhost:8080/jps/testscenario";
+	    String scenarioBucket = ScenarioHelper.getScenarioBucket("base");
+	    assertEquals(scenarioBucket + "/localhost_8080/testscenario", BucketHelper.getLocalPath(test_url, test_dataseturl));		
+	}
 }
