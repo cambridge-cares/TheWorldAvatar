d1e3e2acc03053551161ac7067e19cd39b8132c8
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-07-23 09:57:57 +0800

    1081-dev-add-unit-test-for-resourcepathconverter: Put static moking in try block. Remove unused temporary folder rule and imports.

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/ResourcePathConverterTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/ResourcePathConverterTest.java
@@ -1,26 +1,17 @@
 package uk.ac.cam.cares.jps.base.query.test;
 
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import uk.ac.cam.cares.jps.base.config.AgentLocator;
 import uk.ac.cam.cares.jps.base.config.KeyValueManager;
-import uk.ac.cam.cares.jps.base.listener.BaseOntologyModelManager;
 import uk.ac.cam.cares.jps.base.query.ResourcePathConverter;
 import uk.ac.cam.cares.jps.base.scenario.JPSContext;
 
-import java.io.IOException;
-
-import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anyString;
 
 public class ResourcePathConverterTest {
-
-    @Rule
-    public TemporaryFolder folder= new TemporaryFolder();
 
     @Test
     public void testGetInstance() {
@@ -29,35 +20,35 @@ public class ResourcePathConverterTest {
 
     @Test
     public void testConvert() {
-        String testPath1 = "http://www.test.com/test";
+        String testPath1 = "http://www.theworldavatar.com/ontology/test";
         String testPath2 = "http://www.theworldavatar.com/scenariourl";
         String testPath3 = "http://www.jparksimulator.com/test";
         String address = KeyValueManager.getServerAddress();
-        MockedStatic<AgentLocator> mockA = Mockito.mockStatic(AgentLocator.class);
 
-        mockA.when(AgentLocator::isJPSRunningForTest).thenReturn(false);
-        Assert.assertEquals("http://www.test.com/test", ResourcePathConverter.convert(testPath1));
+        try (MockedStatic<AgentLocator> mockA = Mockito.mockStatic(AgentLocator.class)) {
+            mockA.when(AgentLocator::isJPSRunningForTest).thenReturn(false);
+            Assert.assertEquals(testPath1, ResourcePathConverter.convert(testPath1));
+        }
 
-        mockA.when(AgentLocator::isJPSRunningForTest).thenReturn(true);
-        Assert.assertEquals("http://www.test.com/test", ResourcePathConverter.convert(testPath1));
-        Assert.assertEquals(address+"/scenariourl", ResourcePathConverter.convert(testPath2));
-        Assert.assertEquals(address+"/test", ResourcePathConverter.convert(testPath3));
+        try (MockedStatic<AgentLocator> mockA = Mockito.mockStatic(AgentLocator.class);
+             MockedStatic<JPSContext> mockJPS = Mockito.mockStatic(JPSContext.class)) {
+            mockA.when(AgentLocator::isJPSRunningForTest).thenReturn(true);
+            Assert.assertEquals(testPath1, ResourcePathConverter.convert(testPath1));
+            Assert.assertEquals(address+"/scenariourl", ResourcePathConverter.convert(testPath2));
+            Assert.assertEquals(address+"/test", ResourcePathConverter.convert(testPath3));
 
-        MockedStatic<JPSContext> mockJPS = Mockito.mockStatic(JPSContext.class);
-        mockJPS.when(() -> JPSContext.getScenarioUrl()).thenReturn("http://localhost:8080/scenariourl");
-        Assert.assertEquals("http://www.theworldavatar.com/scenariourl", ResourcePathConverter.convert(testPath2));
-
-        mockA.close();
+            mockJPS.when(JPSContext::getScenarioUrl).thenReturn("http://localhost:8080/scenariourl");
+            Assert.assertEquals(testPath2, ResourcePathConverter.convert(testPath2));
+        }
     }
 
     @Test
     public void testConvertToLocalPath() {
         String testPath1 = "http://www.theworldavatar.com/Ontology";
 
-        MockedStatic<KeyValueManager> mockKey = Mockito.mockStatic(KeyValueManager.class);
-        mockKey.when(() -> KeyValueManager.get(anyString())).thenReturn("http://localhost:8080/test");
-
-        Assert.assertEquals("http://localhost:8080/test/Ontology", ResourcePathConverter.convertToLocalPath(testPath1));
-        mockKey.close();
+        try (MockedStatic<KeyValueManager> mockKey = Mockito.mockStatic(KeyValueManager.class)) {
+            mockKey.when(() -> KeyValueManager.get(anyString())).thenReturn("http://localhost:8080/test");
+            Assert.assertEquals("http://localhost:8080/test/Ontology", ResourcePathConverter.convertToLocalPath(testPath1));
+        }
     }
 }
