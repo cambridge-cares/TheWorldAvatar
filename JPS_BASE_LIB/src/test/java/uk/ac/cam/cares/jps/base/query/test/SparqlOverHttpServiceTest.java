90c3741aa52d03a12fe5d70570ca4a47458e3677
yjytt59 <yjytt59@gmail.com>
2021-07-26 23:57:52 +0800

    mock HttpClient(1082-dev-add-unit-test-for-sparqloverhttpservice)

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/SparqlOverHttpServiceTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/SparqlOverHttpServiceTest.java
@@ -1,5 +1,14 @@
 package uk.ac.cam.cares.jps.base.query.test;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.CDL;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -7,15 +16,18 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
 import uk.ac.cam.cares.jps.base.query.SparqlOverHttpService;
 
 
 public class SparqlOverHttpServiceTest {
     private SparqlOverHttpService testS;
-    String sparql = "SELECT ?o WHERE {<http://www.theworldavatar.com/kb/species/species.owl#species_1> <http://www.w3.org/2008/05/skos#altLabel> ?o.}";
+    private String sparql = "SELECT ?o WHERE {<http://www.theworldavatar.com/kb/species/species.owl#species_1> <http://www.w3.org/2008/05/skos#altLabel> ?o.}";
     private String queryUrl = "http://test.com/sparql/query";
     private String updateUrl = "http://test.com/sparql/update";
+
 
     @Test
     public void testToString() {
@@ -30,6 +42,23 @@ public class SparqlOverHttpServiceTest {
 
     @Test
     public void testExecutePost() throws Exception{
+//        HttpResponse expected = Mockito.mock(HttpResponse.class);
+        HttpResponse expected;
+        HttpClient httpClient = Mockito.mock(HttpClient.class);
+        HttpPost request = new HttpPost(updateUrl);
+        request.setEntity(new StringEntity(formInsertQuery()));
+
+//        Mockito.when(statusLine.getStatusCode()).thenReturn(200);
+
+//        Mockito.when(request.setEntity(Mockito.any(HttpEntity.class))).thenAnswer(
+//                new Answer() {
+//                    public Object answer(InvocationOnMock invocation) {
+//                        Object[] args = invocation.getArguments();
+//                        Object mock = invocation.getMock();
+//                        request.setHeader();
+//                        return request;
+//                    }
+//                });
 
         try (MockedConstruction<RemoteStoreClient> mocked = Mockito.mockConstruction(RemoteStoreClient.class,
                 (mock, context) -> {
@@ -41,15 +70,16 @@ public class SparqlOverHttpServiceTest {
             Assert.assertEquals("1", testS.executePost(formInsertQuery()));;
         }
 
-        try{
-            testS = new SparqlOverHttpService(SparqlOverHttpService.RDFStoreType.FUSEKI, queryUrl, updateUrl);
-            Assert.assertTrue(testS.executePost(formInsertQuery()).contains("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">"));
+        testS = new SparqlOverHttpService(SparqlOverHttpService.RDFStoreType.FUSEKI, queryUrl, updateUrl);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/sparql-update");
+        expected = HttpClientBuilder.create().build().execute(request);
+        expected.setStatusCode(200);
+        Mockito.when(httpClient.execute(request)).thenReturn(expected);
+        Assert.assertTrue(testS.executePost(formInsertQuery()).contains(expected.getEntity().toString()));
 
-            testS = new SparqlOverHttpService(SparqlOverHttpService.RDFStoreType.RDF4J, queryUrl, updateUrl);
-            Assert.assertTrue(testS.executePost(formInsertQuery()).contains("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">"));
-        }catch (Exception e){
-            Assert.assertTrue(e.getMessage().contains("HTTP response with error = "));
-        }
+        testS = new SparqlOverHttpService(SparqlOverHttpService.RDFStoreType.RDF4J, queryUrl, updateUrl);
+        Assert.assertTrue(testS.executePost(formInsertQuery()).contains(expected.getEntity().toString()));
+
 
 
     }
