5a6cc11286a29f8e4d78fffc495854b0409a4749
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-07-29 16:30:49 +0800

    1133-dev-overarching-timeseries-client: Simplify initTimeSeries to only try fail safe when any database error occurs, and provide useful exception messages.

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/timeseries/TimeSeriesClient.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/timeseries/TimeSeriesClient.java
@@ -22,8 +22,8 @@ import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 
 public class TimeSeriesClient<T> {
 	// Associated RDB and RDF/SPARQL clients
-	private TimeSeriesRDBClient<T> rdbClient;
-	private TimeSeriesSparql rdfClient;
+	private final TimeSeriesRDBClient<T> rdbClient;
+	private final TimeSeriesSparql rdfClient;
 	// Exception prefix
 	private final String exceptionPrefix = this.getClass().toString() + ": ";
 	
@@ -97,66 +97,36 @@ public class TimeSeriesClient<T> {
      * @param dataIRIs: List of dataIRIs as Strings
      * @param dataClass: List of data classes for each dataIRI
      * @param timeUnit: time unit as (full) IRI
-     * @return Returns "Success" if initialisation was successful in both the triple store and the relational database
-     * 		   <br>Returns "No changes: &lt;error description&gt;" in case there was an error; however, prior state could be maintained/recovered
-     * 		   <br>Returns "Potentially inconsistent state: &lt;time series IRI&gt;" in case there was an error and NOT all changes could be reverted  
-     * 		   <br>(i.e. there is an inconsistent state between triple store and database and subsequent cleanup for tsIRI is required)
      */
-    public String initTimeSeries(List<String> dataIRIs, List<Class<?>> dataClass, String timeUnit) {
-    	
-    	// Initialise boolean flags to indicate success/failure of potentially necessary deletion (in case any errors occur)
-    	boolean rdb_deletion = true;
-    	boolean rdf_deletion = true;
- 	
-    	// Create random time series IRI in the format: <ClassName>_<UUID>
-    	String tsIRI = rdfClient.ns_kb + "Timeseries_" + UUID.randomUUID().toString();
+    public void initTimeSeries(List<String> dataIRIs, List<Class<?>> dataClass, String timeUnit) {
+
+    	// Create random time series IRI in the format: <Namespace><ClassName>_<UUID>
+    	String tsIRI = TimeSeriesSparql.ns_kb + "Timeseries_" + UUID.randomUUID();
     	
     	// Step1: Initialise time series in knowledge base
     	// In case any exception occurs, nothing will be created in kb, since JPSRuntimeException will be thrown before 
     	// interacting with triple store and SPARQL query is either executed fully or not at all (no partial execution possible)
    		try {
    			rdfClient.initTS(tsIRI, dataIRIs, rdbClient.getRdbURL(), timeUnit);
-   		} catch (Exception e_RdfCreate) {
-   			e_RdfCreate.printStackTrace();
-   			return "No change: " + e_RdfCreate.getMessage();   			
-   		}
+		}
+		catch (JPSRuntimeException e_RdfCreate) {
+			throw new JPSRuntimeException(exceptionPrefix + "timeseries was not created!", e_RdfCreate);
+		}
     	
     	// Step2: Try to initialise time series in relational database
     	try {
     		rdbClient.initTimeSeriesTable(dataIRIs, dataClass, tsIRI);
     	} catch (JPSRuntimeException e_RdbCreate) {
-    		// For RDB exceptions thrown after interaction with relational database started, try to revert 
-    		// whatever might have been instantiated in relational database
-			// nk591: I think it is unnecessary to check database problems that could occur between database queries
-			//        Furthermore, rdbClient.deleteTimeSeriesTable(dataIRIs.get(0)); will also result in an error if nothing
-			//        was created in which case the returned message is misleading.
-//    		if (e_RdbCreate.getMessage().contains("Error while executing SQL command")) {
-//       			try {
-//        			// Try to delete potentially created time series table and corresponding entries in central lookup table
-//    				rdbClient.deleteTimeSeriesTable(dataIRIs.get(0));
-//    			} catch(Exception e_RdbDelete) {
-//    				// RDB deletion potentially unsuccessful
-//   					rdb_deletion = false;
-//    			}
-//    		}
-    		
-    		// For ALL RDB exceptions, try to revert previous knowledge base instantiation
+    		// For exceptions thrown when creating initialising RDB elements relational database started,
+			// try to revert previous knowledge base instantiation
     		try {
     			rdfClient.removeTimeSeries(tsIRI);
     		} catch (Exception e_RdfDelete) {
-    			// RDF deletion potentially unsuccessful
-    			rdf_deletion = false;
+    			throw new JPSRuntimeException(exceptionPrefix + "inconsistent state created when initialising time series " + tsIRI +
+						" , as database related instantiation failed but KG triples could not be deleted.");
     		}
-    		
-    		// Return messages depending on exceptions occurred
-    		if (rdb_deletion && rdf_deletion) {
-    			return "No change: " + e_RdbCreate.getCause().getMessage();
-    		} else {
-    			return "Potentially inconsistent state: " + tsIRI;
-    		}    		
-    	}    	
-    	// Return success in case no exceptions occurred
-    	return "Success";
+    		throw new JPSRuntimeException(exceptionPrefix + "timeseries was not created!", e_RdbCreate);
+    	}
     }
     
     /**
