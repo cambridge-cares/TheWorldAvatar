81a5feacddb1eb676d0cf01d1141a3b4a1a4ea0e
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-07-29 13:54:58 +0800

    1071-dev-add-unit-test-for-jpsbaselogger: Remove the mocking of the LoggerFactory and instead change the JPSBaseLogger private field for the test (resetted afterwards).

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/log/JPSBaseLoggerTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/log/JPSBaseLoggerTest.java
@@ -1,54 +1,78 @@
 package uk.ac.cam.cares.jps.base.log;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.*;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.junit.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import uk.ac.cam.cares.jps.base.agent.JPSAgent;
+
+import java.lang.reflect.Field;
 
 
 public class JPSBaseLoggerTest {
 
     @Mock
     public static Logger logger = Mockito.mock(Logger.class);
-    private JPSBaseLogger mockJPS;
+
+    public static Logger actualLogger;
+    private String object;
+    private JPSAgent sender;
 
     @Before
-    public void construct() {
-        try (MockedStatic<LoggerFactory> integerMock = Mockito.mockStatic(LoggerFactory.class)) {
-            integerMock.when(() -> LoggerFactory.getLogger(JPSBaseLogger.class)).thenReturn(logger);
-            mockJPS = new JPSBaseLogger();
-        }
+    public void setUp() {
+        object = "object";
+        sender = new JPSAgent();
+    }
+
+    @BeforeClass
+    public static void setLoggerToMock() throws NoSuchFieldException, IllegalAccessException {
+        Field loggerField = JPSBaseLogger.class.getDeclaredField("logger");
+        loggerField.setAccessible(true);
+        actualLogger = (Logger) loggerField.get(null);
+        loggerField.set(null, logger);
+    }
+
+    @AfterClass
+    public static void resetLogger() throws NoSuchFieldException, IllegalAccessException {
+        Field loggerField = JPSBaseLogger.class.getDeclaredField("logger");
+        loggerField.setAccessible(true);
+        loggerField.set(null, actualLogger);
+        loggerField.setAccessible(false);
     }
 
     @Test
     public void testDebug() {
-        mockJPS.debug(JPSBaseLogger.class, "test debug");
-        Mockito.verify(logger).debug("Class test debug");
+        JPSBaseLogger.debug(object, "test debug");
+        JPSBaseLogger.debug(sender, "test debug");
+        Mockito.verify(logger).debug("String test debug");
+        Mockito.verify(logger).debug("JPSAgent test debug");
     }
 
     @Test
     public void testInfo() {
-        mockJPS.info(JPSBaseLogger.class, "test info");
-        Mockito.verify(logger).info("Class test info");
-
+        JPSBaseLogger.info(object, "test info");
+        JPSBaseLogger.info(sender, "test info");
+        Mockito.verify(logger).info("String test info");
+        Mockito.verify(logger).info("JPSAgent test info");
     }
-
 
     @Test
     public void testErrorException() {
         Exception e = new Exception("test exception");
         StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
         e.setStackTrace(stackTraceElements);
-        mockJPS.error(JPSBaseLogger.class, e);
-        Mockito.verify(logger).error(ArgumentMatchers.any(String.class));
+        JPSBaseLogger.error(object, e);
+        JPSBaseLogger.error(sender, e);
+        Mockito.verify(logger).error("String test exception\n" + ExceptionUtils.getStackTrace(e));
+        Mockito.verify(logger).error("JPSAgent test exception\n" + ExceptionUtils.getStackTrace(e));
     }
 
     @Test
     public void testError() {
-        mockJPS.error(JPSBaseLogger.class, "test error");
-        Mockito.verify(logger).error("Class test error");
+        JPSBaseLogger.error(object, "test error");
+        JPSBaseLogger.error(sender, "test error");
+        Mockito.verify(logger).error("String test error");
+        Mockito.verify(logger).error("JPSAgent test error");
     }
 }
