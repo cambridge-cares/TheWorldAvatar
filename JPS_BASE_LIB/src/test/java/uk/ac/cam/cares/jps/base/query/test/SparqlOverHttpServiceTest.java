1cb59c69c2769d4ca6ffc6dba93f1cb9b0353dd8
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-07-29 10:49:59 +0800

    1082-dev-add-unit-test-for-sparqloverhttpservice: Refactor and add test cases in testExecutePost for blazegraph and fuseki type.

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/SparqlOverHttpServiceTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/query/test/SparqlOverHttpServiceTest.java
@@ -1,49 +1,31 @@
 package uk.ac.cam.cares.jps.base.query.test;
 
-import com.sun.jndi.toolkit.url.Uri;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
+import org.apache.http.*;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.message.BasicStatusLine;
 import org.apache.http.util.EntityUtils;
 import org.json.CDL;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
-import uk.ac.cam.cares.jps.base.config.AgentLocator;
 import uk.ac.cam.cares.jps.base.discovery.AgentCaller;
 import uk.ac.cam.cares.jps.base.discovery.MediaType;
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.interfaces.StoreClientInterface;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
-import uk.ac.cam.cares.jps.base.query.ResourcePathConverter;
 import uk.ac.cam.cares.jps.base.query.SparqlOverHttpService;
-import uk.ac.cam.cares.jps.base.scenario.JPSContext;
 
-import java.net.HttpURLConnection;
+import java.lang.reflect.Field;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
-
-import static java.util.Objects.requireNonNull;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 
 
 public class SparqlOverHttpServiceTest {
@@ -65,34 +47,50 @@ public class SparqlOverHttpServiceTest {
 
     @Test
     public void testExecutePost() throws Exception{
-        HttpResponse mockResp = Mockito.mock(HttpResponse.class, RETURNS_DEEP_STUBS);
 
+        // Test with RDFStoreType Blazegraph
         try (MockedConstruction<RemoteStoreClient> mocked = Mockito.mockConstruction(RemoteStoreClient.class,
                 (mock, context) -> {
-                    // further stubbings ...
                     Mockito.when(mock.executeUpdate()).thenReturn(1);
                 })) {
-
+            // Make the kbClient accessible
+            Field kbClientField = SparqlOverHttpService.class.getDeclaredField("kbClient");
+            kbClientField.setAccessible(true);
             testS = new SparqlOverHttpService(SparqlOverHttpService.RDFStoreType.BLAZEGRAPH, queryUrl, updateUrl);
-            Assert.assertEquals("1", testS.executePost(formInsertQuery()));;
+            // Verify that the kbClient is not set
+            Assert.assertNull(kbClientField.get(testS));
+            // Verify output of the executePost
+            Assert.assertEquals("1", testS.executePost(formInsertQuery()));
+            // kbClient should now be set with a mock and the setUpdateEndpoint should have been invoked
+            Mockito.verify((StoreClientInterface) kbClientField.get(testS)).setUpdateEndpoint(updateUrl);
+            // The setQuery should have been invoked as well with the provided query
+            Mockito.verify((StoreClientInterface) kbClientField.get(testS)).setQuery(formInsertQuery());
         }
+
+        // Test with RDFStoreType Fuseki
 
         testS = new SparqlOverHttpService(SparqlOverHttpService.RDFStoreType.FUSEKI, queryUrl, updateUrl);
-
-        HttpResponse expected;
-        HttpPost request = new HttpPost(updateUrl);
-        request.setEntity(new StringEntity(formInsertQuery()));
-        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/sparql-update");
-        expected = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build().execute(request);
-
-        try (MockedStatic<HttpClientBuilder> httpClientB = Mockito.mockStatic(HttpClientBuilder.class,RETURNS_DEEP_STUBS)) {
-            httpClientB.when(() -> HttpClientBuilder.create().build().execute(request)).thenReturn(expected);
-
+        // Set-up dummy post response
+        CloseableHttpResponse dummyResponse = Mockito.mock(CloseableHttpResponse.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(dummyResponse.getEntity()).thenReturn(new StringEntity("test response"));
+        Mockito.when(dummyResponse.getStatusLine().getStatusCode()).thenReturn(204, 500);
+        try (MockedStatic<HttpClientBuilder> httpClientB = Mockito.mockStatic(HttpClientBuilder.class, Mockito.RETURNS_DEEP_STUBS)) {
+            httpClientB.when(() -> HttpClientBuilder.create().build().execute(Mockito.any(HttpPost.class))).thenReturn(dummyResponse);
+            Assert.assertEquals("test response", testS.executePost(formInsertQuery()));
+            // Run the method again, this time there should be an exception as the status code is 500
+            StatusLine dummyStatus = new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 500, "error");
+            Mockito.when(dummyResponse.getStatusLine()).thenReturn(dummyStatus);
+            Mockito.when(dummyResponse.getAllHeaders()).thenReturn(new Header[] {});
+            try {
+                testS.executePost(formInsertQuery());
+                Assert.fail();
+            }
+            catch (JPSRuntimeException e) {
+                Assert.assertEquals("HTTP response with error = " + dummyStatus + "\n" + "test response", e.getMessage());
+            }
         }
-        Assert.assertEquals(testS.executePost(formInsertQuery()), EntityUtils.toString(expected.getEntity()));
 
-        testS = new SparqlOverHttpService(SparqlOverHttpService.RDFStoreType.RDF4J, queryUrl, updateUrl);
-        Assert.assertTrue(testS.executePost(formInsertQuery()).contains(expected.getEntity().toString()));
+        // Test with RDFStoreType RDF4J
 
     }
 
