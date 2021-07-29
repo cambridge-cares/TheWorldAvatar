6d71c43723325a8b59e7b2cc64c07de5fc9f33f8
yjytt59 <yjytt59@gmail.com>
2021-07-29 11:32:17 +0800

    initial mockedStatic (1071-dev-add-unit-test-for-jpsbaselogger)

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/log/JPSBaseLoggerTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/log/JPSBaseLoggerTest.java
@@ -3,39 +3,52 @@ package uk.ac.cam.cares.jps.base.log;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class JPSBaseLoggerTest {
+
+    @Mock
+    public static Logger logger = Mockito.mock(Logger.class);
+    private JPSBaseLogger mockJPS;
+
+    @Before
+    public void construct() {
+        try (MockedStatic<LoggerFactory> integerMock = Mockito.mockStatic(LoggerFactory.class)) {
+            integerMock.when(() -> LoggerFactory.getLogger(JPSBaseLogger.class)).thenReturn(logger);
+            mockJPS = new JPSBaseLogger();
+        }
+    }
+
     @Test
-    public void testLogger() {
+    public void testDebug() {
+        mockJPS.debug(JPSBaseLogger.class, "test debug");
+        Mockito.verify(logger).debug("Class test debug");
+    }
+
+    @Test
+    public void testInfo() {
+        mockJPS.info(JPSBaseLogger.class, "test info");
+        Mockito.verify(logger).info("Class test info");
+
+    }
+
+
+    @Test
+    public void testErrorException() {
         Exception e = new Exception("test exception");
         StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
         e.setStackTrace(stackTraceElements);
-        try (MockedStatic<LoggerFactory> integerMock = Mockito.mockStatic(LoggerFactory.class)) {
-            final Logger logger = Mockito.mock(Logger.class);
-            integerMock.when(() -> LoggerFactory.getLogger(JPSBaseLogger.class)).thenReturn(logger);
+        mockJPS.error(JPSBaseLogger.class, e);
+        Mockito.verify(logger).error(ArgumentMatchers.any(String.class));
+    }
 
-            new JPSBaseLogger().error(JPSBaseLogger.class, e);
-            Mockito.verify(logger).error(ArgumentMatchers.any(String.class));
-
-            new JPSBaseLogger().error(JPSBaseLogger.class, "testInfo");
-            Mockito.verify(logger).error("Class testInfo");
-
-            new JPSBaseLogger().info(JPSBaseLogger.class, "testInfo");
-            Mockito.verify(logger).info("Class testInfo");
-
-            new JPSBaseLogger().debug(JPSBaseLogger.class, "testInfo");
-            Mockito.verify(logger).debug("Class testInfo");
-
-            Mockito.verifyNoMoreInteractions(logger);
-        }
-
+    @Test
+    public void testError() {
+        mockJPS.error(JPSBaseLogger.class, "test error");
+        Mockito.verify(logger).error("Class test error");
     }
 }
