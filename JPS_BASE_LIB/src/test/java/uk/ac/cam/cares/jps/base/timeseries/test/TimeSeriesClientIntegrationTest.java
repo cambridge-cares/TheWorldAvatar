6fd1cb3fb40f05007ebc27587986bb691ab1f14d
Markus Hofmeister <mh807@cam.ac.uk>
2021-07-30 14:26:56 +0200

    1133-dev-overarching-timeseries-client: Simplify initialisation of test data

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/timeseries/test/TimeSeriesClientIntegrationTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/timeseries/test/TimeSeriesClientIntegrationTest.java
@@ -37,16 +37,8 @@ public class TimeSeriesClientIntegrationTest {
 	private static TimeSeriesClient<Instant> tsClient;
 	
 	// Time series test data
-	private static List<String> dataIRI_1, dataIRI_3;
-	private static List<Class<?>> dataClass_1, dataClass_3;
-	private static List<Instant> timeList_1;
-	private static List<Instant> timeList_2;
-	private static List<Double> data1_1;
-	private static List<String> data2_1;
-	private static List<Integer> data3_1;
-	private static TimeSeries<Instant> ts1, ts2, ts3;	
-	private static List<List<?>> dataToAdd_1;
-	private static List<List<?>> dataToAdd_2;
+	private static List<String> dataIRI_1, dataIRI_2;
+	private static List<Class<?>> dataClass_1, dataClass_2;
 	private static String timeUnit;
 
 	// Will create two Docker containers for Blazegraph and postgreSQL
@@ -61,7 +53,7 @@ public class TimeSeriesClientIntegrationTest {
 	private PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:13.3");
 	
 	@BeforeClass
-	// Initialise 3 test time series data sets
+	// Initialise 2 test time series data sets
 	public static void initialiseData() {
 		// Initialise time unit for all test data series
 		timeUnit = "http://s";
@@ -73,62 +65,14 @@ public class TimeSeriesClientIntegrationTest {
 		// Specify type of data for each column (most data will be in doubles, but one can specify different data types)
 		dataClass_1 = new ArrayList<>();
 		dataClass_1.add(Double.class); dataClass_1.add(String.class); dataClass_1.add(Integer.class);
-    	// Create data to add (as a TimeSeries object)
-    	timeList_1 = new ArrayList<>();
-    	data1_1 = new ArrayList<>();
-    	data2_1 = new ArrayList<>();
-    	data3_1 = new ArrayList<>();
-    	
-    	for (int i = 0; i < 10; i++) {
-   			timeList_1.add(Instant.now().plusSeconds(i));
-    		data1_1.add((double) i);
-    		data2_1.add(String.valueOf(i));
-    		data3_1.add(i);
-    	}
-    	dataToAdd_1 = new ArrayList<>();
-    	dataToAdd_1.add(data1_1); dataToAdd_1.add(data2_1); dataToAdd_1.add(data3_1);
-    	// Constructor for the TimeSeries object takes in the time column, dataIRIs, and the corresponding values in lists
-    	ts1 = new TimeSeries<>(timeList_1, dataIRI_1, dataToAdd_1);
 		/* 
-		 * Initialise 2nd time series with same associated data series
+		 * Initialise 2nd time series with only one associated data series
 		 */
-    	// Create data to add (as a TimeSeries object)
-    	timeList_2 = new ArrayList<>();
-		List<Double> data1_2 = new ArrayList<>();
-		List<String> data2_2 = new ArrayList<>();
-		List<Integer> data3_2 = new ArrayList<>();
-    	
-    	for (int i = 0; i < 10; i++) {
-    		// Add additional 10 s to ensure no overlap between time lists
-   			timeList_2.add(Instant.now().plusSeconds(10+i));
-    		data1_2.add((double) (10 + i));
-    		data2_2.add(String.valueOf(10+i));
-    		data3_2.add(10 + i);
-    	}
-    	dataToAdd_2  = new ArrayList<>();
-    	dataToAdd_2.add(data1_2); dataToAdd_2.add(data2_2); dataToAdd_2.add(data3_2);
-    	// Constructor for the TimeSeries object takes in the time column, dataIRIs, and the corresponding values in lists
-    	ts2 = new TimeSeries<>(timeList_2, dataIRI_1, dataToAdd_2);
-		/* 
-		 * Initialise 3rd time series with only one associated data series
-		 */
-	    dataIRI_3 = new ArrayList<>();
-		dataIRI_3.add("http://data4");
+	    dataIRI_2 = new ArrayList<>();
+	    dataIRI_2.add("http://data4");
 		// Specify type of data for each column (most data will be in doubles, but one can specify different data types)
-		dataClass_3 = new ArrayList<>();
-		dataClass_3.add(Double.class);
-    	// Create data to add (as a TimeSeries object)
-		List<Instant> timeList_3 = new ArrayList<>();
-		List<Double> data1_3 = new ArrayList<>();
-
-    	for (int i = 0; i < 10; i++) {
-   			timeList_3.add(Instant.now().plusSeconds(i));
-    		data1_3.add((double) i);
-    	}
-		List<List<?>> dataToAdd_3 = new ArrayList<>();
-    	dataToAdd_3.add(data1_3);
-    	// Constructor for the TimeSeries object takes in the time column, dataIRIs, and the corresponding values in lists
-    	ts3 = new TimeSeries<>(timeList_3, dataIRI_3, dataToAdd_3);
+		dataClass_2 = new ArrayList<>();
+		dataClass_2.add(Double.class);
 	}
 	
 	@Before
@@ -166,8 +110,7 @@ public class TimeSeriesClientIntegrationTest {
 	    tsClient.setRDBClient(postgres.getJdbcUrl(), postgres.getUsername(), postgres.getPassword());	    
 	}
 		
-	@Test
-	 
+	@Test	 
 	public void testInitTimeSeriesWithoutExceptions() {
 		
 		// Retrieve RDB and RDF/SPARQL clients
@@ -195,8 +138,7 @@ public class TimeSeriesClientIntegrationTest {
 		Assert.assertEquals(kb, db);
 	}
 	
-	@Test
-	 
+	@Test	 
 	public void testInitTimeSeriesWithKGInitException() {
 	
 		// Interrupt triple store connection
@@ -211,8 +153,7 @@ public class TimeSeriesClientIntegrationTest {
 		}		
 	}
 	
-	@Test
-	 
+	@Test	 
 	public void testInitTimeSeriesWithUnavailableRDB()  {
 	
 		// Interrupt database connection
@@ -227,8 +168,7 @@ public class TimeSeriesClientIntegrationTest {
 		}		
 	}
 	
-	@Test
-	 
+	@Test	 
 	public void testInitTimeSeriesWithUnavailableRDBAndKGRevertException() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		
 		// Retrieve the value of the private field 'rdfClient' of the time series client
@@ -255,8 +195,7 @@ public class TimeSeriesClientIntegrationTest {
 		}		
 	}
 	
-	@Test
-	 
+	@Test	 
 	public void testDeleteTimeSeriesWithoutExceptions() {
 		
 		// Retrieve RDB and RDF/SPARQL clients
@@ -265,13 +204,13 @@ public class TimeSeriesClientIntegrationTest {
 		
 		// Initialise time series (3 dataIRIs, 1 tsIRI) in knowledge base and database		
 		tsClient.initTimeSeries(dataIRI_1, dataClass_1, timeUnit);
-		tsClient.initTimeSeries(dataIRI_3, dataClass_3, timeUnit);
+		tsClient.initTimeSeries(dataIRI_2, dataClass_2, timeUnit);
 		
 		// Verify correct instantiation in both kb and database
 		Assert.assertEquals(2, rdfClient.countTS());
 		TimeSeries<Instant> ts1 = rdbClient.getTimeSeries(dataIRI_1);
 		Assert.assertEquals(3, ts1.getDataIRIs().size());
-		TimeSeries<Instant> ts2 = rdbClient.getTimeSeries(dataIRI_3);
+		TimeSeries<Instant> ts2 = rdbClient.getTimeSeries(dataIRI_2);
 		Assert.assertEquals(1, ts2.getDataIRIs().size());
 		
 		// Delete 1st time series - verify deletion and that 2nd time series is still unaltered
@@ -285,24 +224,23 @@ public class TimeSeriesClientIntegrationTest {
 		} catch (Exception e) {
 			Assert.assertTrue(e.getMessage().contains("does not have a time series instance (i.e. tsIRI)"));
 		}
-		TimeSeries<Instant> ts3 = rdbClient.getTimeSeries(dataIRI_3);
+		TimeSeries<Instant> ts3 = rdbClient.getTimeSeries(dataIRI_2);
 		Assert.assertEquals(ts2.getDataIRIs(), ts3.getDataIRIs());
 		
 		// Delete 2nd time series - verify deletion and that nothing remains in KG and database
-		tsIRI = rdfClient.getTimeSeries(dataIRI_3.get(0));
+		tsIRI = rdfClient.getTimeSeries(dataIRI_2.get(0));
 		tsClient.deleteTimeSeries(tsIRI);
 		Assert.assertEquals(0, rdfClient.countTS());
-		Assert.assertNull(rdfClient.getTimeSeries(dataIRI_3.get(0)));
+		Assert.assertNull(rdfClient.getTimeSeries(dataIRI_2.get(0)));
 		try {
-			ts2 = rdbClient.getTimeSeries(dataIRI_3);
+			ts2 = rdbClient.getTimeSeries(dataIRI_2);
 			Assert.fail();
 		} catch (Exception e) {
 			Assert.assertTrue(e.getMessage().contains("does not have a time series instance (i.e. tsIRI)"));
 		}		
 	}
 	
-	@Test
-	 
+	@Test	 
 	public void testDeleteTimeSeriesWithUnavailableRDB() {
 		
 		// Initialise time series (3 dataIRIs, 1 tsIRI) in knowledge base and database		
@@ -333,8 +271,7 @@ public class TimeSeriesClientIntegrationTest {
 		Assert.assertEquals(kb, db);		
 	}
 	
-	@Test
-	 
+	@Test	 
 	public void testDeleteTimeSeriesWithUnavailableKG() {
 		
 		// Initialise time series (3 dataIRIs, 1 tsIRI) in knowledge base and database		
@@ -355,8 +292,7 @@ public class TimeSeriesClientIntegrationTest {
 		}		
 	}
 	
-	@Test
-	 
+	@Test	 
 	public void testDeleteTimeSeriesWithKGDeleteException() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
 		
 		// Initialise time series (3 dataIRIs, 1 tsIRI) in knowledge base and database		
@@ -396,7 +332,6 @@ public class TimeSeriesClientIntegrationTest {
 	}
 	
 	@Test
-	@Ignore
 	public void testDeleteTimeSeriesWithKGRevertException() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
 		
 		// Initialise time series (3 dataIRIs, 1 tsIRI) in knowledge base and database		
