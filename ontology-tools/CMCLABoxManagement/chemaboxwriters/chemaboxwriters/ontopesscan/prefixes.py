782d4977321d955c97e960dda0e9d3baea493de9
Angiras Menon <angirasmenon@gmail.com>
2021-07-30 11:42:19 +0100

    am2145: dev-scanparse. CSV handler added for OntoPESSCan ABox Writer with help of dln22.

---
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontopesscan/prefixes.py
@@ -0,0 +1,8 @@
+spec_pref = 'http://www.theworldavatar.com/kb/ontospecies/'
+pes_pref = 'http://www.theworldavatar.com/kb/ontopesscan/'
+gain_pref = 'http://purl.org/gc/' 
+unit_pref = 'http://data.nasa.gov/qudt/owl/'
+
+onto_spec = 'http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl'
+onto_comp = 'http://www.theworldavatar.com/ontology/ontocompchem'
+onto_pes =  'http://www.theworldavatar.com/ontology/OntoPESScan.owl'
