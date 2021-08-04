a6d989f47311d98f8f114463bcd223de9f6af828
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-08-04 16:35:08 +0800

    1160-dev-aqmesh-input-agent: Add class for connections directly with the API (only access token method implemented).

---
+++ Agents/AQMeshInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/aqmesh/AQMeshAPIConnector.java
@@ -0,0 +1,124 @@
+package uk.ac.cam.cares.jps.agent.aqmesh;
+
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeries;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class AQMeshAPIConnector {
+
+    private String username;
+    private String password;
+    private String token;
+    private static final String API_URL = "https://api.aqmeshdata.net/api";
+    private static final String AUTHENTICATE_URL = API_URL +"/Authenticate";
+
+    public AQMeshAPIConnector(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public AQMeshAPIConnector(String filepath) throws IOException {
+        loadAPIConfigs(filepath);
+    }
+
+    public void connect() {
+        try {
+            token = getAccessToken();
+        } catch (IOException e) {
+            throw  new JPSRuntimeException("Access token for AQMesh API could not be retrieved!", e);
+        }
+        System.out.println("Token received from AQMesh API. It will be valid for 120 minutes.");
+    }
+
+    private String getAccessToken() throws IOException {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpPost postRequest = new HttpPost(AUTHENTICATE_URL);
+            JSONObject body = new JSONObject();
+            body.put("username", username);
+            body.put("password", password);
+            postRequest.setEntity(new StringEntity(body.toString(), ContentType.APPLICATION_JSON));
+
+            try (CloseableHttpResponse response = httpclient.execute(postRequest)) {
+                int status = response.getStatusLine().getStatusCode();
+                switch (status) {
+                    case 400:
+                    case 401:
+                        throw new HttpResponseException(status, "Invalid username or password.");
+                    case 200:
+                        JSONObject responseBody = new JSONObject(EntityUtils.toString(response.getEntity()));
+                        return responseBody.get("token").toString();
+                    default:
+                        throw new HttpResponseException(status, "Could not retrieve token.");
+                }
+            }
+        }
+    }
+
+    public TimeSeries<LocalDateTime> getParticleReadings() {
+
+        JSONArray particleReadings = retrieveReadings();
+        List<LocalDateTime> times = new ArrayList<>();
+        List<List<?>> values = new ArrayList<>();
+        List<String> dataIRI = new ArrayList<>();
+
+        return new TimeSeries<LocalDateTime>(times, dataIRI, values);
+    }
+
+    public TimeSeries<LocalDateTime> getGasReadings() {
+
+        JSONArray particleReadings = retrieveReadings();
+        List<LocalDateTime> times = new ArrayList<>();
+        List<List<?>> values = new ArrayList<>();
+        List<String> dataIRI = new ArrayList<>();
+
+        return new TimeSeries<LocalDateTime>(times, dataIRI, values);
+    }
+
+    private JSONArray retrieveReadings() {
+        return new JSONArray();
+    }
+
+    private void loadAPIConfigs(String filepath) throws IOException {
+        // Check whether properties file exists at specified location
+        File file = new File(filepath);
+        if (!file.exists()) {
+            throw new FileNotFoundException("No properties file found at specified filepath: " + filepath);
+        }
+        // Read username and password for AQMesh API from properties file
+        // Try-with-resource to ensure closure of input stream
+        try (InputStream input = new FileInputStream(file)) {
+
+            // Load properties file from specified path
+            Properties prop = new Properties();
+            prop.load(input);
+
+            // Get username and password from properties file
+            if (prop.containsKey("aqmesh.username")) {
+                this.username = prop.getProperty("aqmesh.username");
+            } else {
+                throw new IOException("Properties file is missing \"aqmesh.username=<aqmesh_username>\"");
+            }
+            if (prop.containsKey("aqmesh.password")) {
+                this.password = prop.getProperty("aqmesh.password");
+            } else {
+                throw new IOException("Properties file is missing \"aqmesh.password=<aqmesh.password>\"");
+            }
+        }
+    }
+
+}
--- Agents/AQMeshInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/aqmesh/AQMeshInputAgent.java
+++ Agents/AQMeshInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/aqmesh/AQMeshInputAgent.java
@@ -1,53 +1,65 @@
 package uk.ac.cam.cares.jps.agent.aqmesh;
 
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeries;
 import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.time.Instant;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.Properties;
 
 public class AQMeshInputAgent {
 
-    private static final String propertiesFile = "aqmesh_inputAgent.properties";
-    private static TimeSeriesClient<Instant> tsClient;
+    private TimeSeriesClient<LocalDateTime> tsClient;
 
-    public static void main(String[] args) throws IOException {
+    public AQMeshInputAgent(String propertiesFile) throws IOException {
         // Initialize time series client
-        tsClient = new TimeSeriesClient<Instant>(Instant.class, propertiesFile);
-
-        // Check whether time series already exists
-        if (!timeSeriesExists()) {
-            initializeTimeSeries();
-        }
-
-        String token = getAccessToken();
-        updateTimeSeries(token);
-        
+        tsClient = new TimeSeriesClient<>(LocalDateTime.class, propertiesFile);
     }
 
-    private static void updateTimeSeries(String token) {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 0) {
+            throw new JPSRuntimeException("No properties file provided as command line argument.");
+        }
+        String propertiesFile = args[0];
+
+        AQMeshInputAgent agent = new AQMeshInputAgent(propertiesFile);
+        // Check whether time series already exists
+        if (!agent.timeSeriesExists()) {
+            agent.initializeTimeSeries();
+        }
+
+        AQMeshAPIConnector connector = new AQMeshAPIConnector(propertiesFile);
+        connector.connect();
+        agent.updateTimeSeries(connector);
+    }
+
+    private void updateTimeSeries(AQMeshAPIConnector connector) {
         try {
-            updateParticleReadings(token);
-            updateGasReadings(token);
+            updateParticleReadings(connector);
+            updateGasReadings(connector);
         }
         catch (Exception e) {
         }
     }
 
-    private static void updateGasReadings(String token) {
+    private void updateGasReadings(AQMeshAPIConnector connector) {
+        TimeSeries<LocalDateTime> gasReadings = connector.getGasReadings();
     }
 
-    private static void updateParticleReadings(String token) {
+    private void updateParticleReadings(AQMeshAPIConnector connector) {
+        TimeSeries<LocalDateTime> particleReadings = connector.getParticleReadings();
     }
 
-    private static String getAccessToken() {
-        return "token";
+    private boolean timeSeriesExists() {
+        return true;
     }
 
-    private static boolean timeSeriesExists() {
-        return false;
-    }
-
-    private static void initializeTimeSeries() {
+    private void initializeTimeSeries() {
     }
 
 }
