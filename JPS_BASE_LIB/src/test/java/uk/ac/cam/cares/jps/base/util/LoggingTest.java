c094aef3fb740088c2fc54c6cfd0e2b311949bf6
Michael Hillman <mdhillman@cmclinnovations.com>
2021-08-09 18:03:38 +0100

    dev-jps-logging: Updated JPS Base Library to download Log4J2 config when running unit tests.

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/util/LoggingTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/util/LoggingTest.java
@@ -1,4 +1,4 @@
-package uk.ac.cam.cares.jps.agent.email;
+package uk.ac.cam.cares.jps.base.util;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,9 +7,10 @@ import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.ConsoleAppender;
 import org.apache.logging.log4j.core.appender.RollingFileAppender;
 import org.apache.logging.log4j.core.config.Configuration;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import uk.ac.cam.cares.jps.base.util.SysStreamHandler;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Test logging functionality.
@@ -29,24 +30,24 @@ public class LoggingTest {
     @Test
     public void checkLogging() {
         LoggerContext loggerContext = (LoggerContext) LogManager.getContext();
-        Assertions.assertNotNull(loggerContext, "Current LoggerContext is null.");
-        
+        assertNotNull(loggerContext, "Current LoggerContext is null.");
+
         Configuration loggerConfig = loggerContext.getConfiguration();
-        Assertions.assertNotNull(loggerConfig, "Current configuration is null.");
+        assertNotNull(loggerConfig, "Current configuration is null.");
 
         // Check for file appender
         Appender fileAppender = loggerConfig.getAppender("RollingFile");
-        Assertions.assertNotNull(fileAppender, "Expected to find an appender named 'RollingFile'.");
-        Assertions.assertTrue(fileAppender instanceof RollingFileAppender, "Expecting 'RollingFile' appender to be an instance of RollingFileAppender class.");
+        assertNotNull(fileAppender, "Expected to find an appender named 'RollingFile'.");
+        assertTrue(fileAppender instanceof RollingFileAppender, "Expecting 'RollingFile' appender to be an instance of RollingFileAppender class.");
 
         // Check for console appender
         Appender consoleAppender = loggerConfig.getAppender("Console");
-        Assertions.assertNotNull(consoleAppender, "Expected to find an appender named 'Console'.");
-        Assertions.assertTrue(consoleAppender instanceof ConsoleAppender, "Expecting 'Console' appender to be an instance of ConsoleAppender class.");
+        assertNotNull(consoleAppender, "Expected to find an appender named 'Console'.");
+        assertTrue(consoleAppender instanceof ConsoleAppender, "Expecting 'Console' appender to be an instance of ConsoleAppender class.");
 
         // Redirect the system streams
         SysStreamHandler.redirectToLoggers();
-        
+
         // Output some logging statements
         LOGGER.debug("This is a DEBUG level test message, from the LoggingTest class.");
         LOGGER.info("This is an INFO level test message, from the LoggingTest class.");
