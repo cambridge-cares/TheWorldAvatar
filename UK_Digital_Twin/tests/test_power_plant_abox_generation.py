bdfbef1fae75689bcc8685f1425ab3c5e706054f
Feroz Farazi <msff2@cam.ac.uk>
2021-08-10 15:40:42 +0100

    99-UK-digital-twin-powsys: Implemented a feature to compare statements in two RDF files to understand if the files have the same content.

--- UK_Digital_Twin/tests/test_power_plant_abox_generation.py
+++ UK_Digital_Twin/tests/test_power_plant_abox_generation.py
@@ -1,6 +1,9 @@
 import unittest
 import os
+from os import listdir
+from os.path import isfile, join
 from rdflib import Graph
+from UK_Power_Plant_Generator.powerPlantABoxGeneration import addUKPowerPlantTriples
 
 path_to_generated_rdf = '../resources/test_generated_rdf'
 path_to_reference_rdf = '../resources/test_reference_rdf'
@@ -23,6 +26,18 @@ class TestUKDigitalTwin(unittest.TestCase):
        """
        abs_path_generated_rdf = os.path.abspath(path_to_generated_rdf)
        abs_path_reference_rdf = os.path.abspath(path_to_reference_rdf)
+       """
+       Calls the function that generates RDF representations of power plants
+       """
+       addUKPowerPlantTriples('default', test_input_folder_name, abs_path_generated_rdf)
+       owl_files = [f for f in listdir(path_to_generated_rdf) if isfile(join(path_to_generated_rdf, f))]
+       for an_owl_file in owl_files:
+          generated_statements = self.read_rdf_model_statements(abs_path_generated_rdf+os.sep+an_owl_file)
+          reference_statements = self.read_rdf_model_statements(abs_path_reference_rdf+os.sep+an_owl_file)
+          print('Comparing the content of the following two files:\n', abs_path_generated_rdf+os.sep+an_owl_file, '\n', abs_path_reference_rdf+os.sep+an_owl_file)
+          self.assertEqual(generated_statements, reference_statements)
+          print('The files have the same content.')
+          print('\n')
 
     """
     Reads an RDF model to return all the statements codified it
