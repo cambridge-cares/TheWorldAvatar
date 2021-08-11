be15258b8f96137a9900682ccaf7d8d8da6af9c6
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-08-11 16:34:52 +0800

    1160-dev-aqmesh-input-agent: Add private method to read JSON key to IRI mappings from files in a provided folder and use that in the constructor of AQMeshInputAgent.

--- Agents/AQMeshInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/aqmesh/AQMeshInputAgent.java
+++ Agents/AQMeshInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/aqmesh/AQMeshInputAgent.java
@@ -1,12 +1,20 @@
 package uk.ac.cam.cares.jps.agent.aqmesh;
 
 import org.json.JSONArray;
+import uk.ac.cam.cares.jps.agent.utils.JSONKeyToIRIMapper;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.timeseries.TimeSeries;
 import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesSparql;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * Class to retrieve data from the AQMesh API and storing it with connection to The World Avatar (Knowledge Base).
@@ -16,10 +24,25 @@ public class AQMeshInputAgent {
 
     private TimeSeriesClient<LocalDateTime> tsClient;
     private AQMeshAPIConnector connector;
+    private List<JSONKeyToIRIMapper> mappings;
+    private static final String generatedIRIPrefix = TimeSeriesSparql.ns_kb + "aqmesh";
 
+    /**
+     * Standard constructor which reads in JSON key to IRI mappings from the config folder
+     * defined in the provided properties file.
+     * @param propertiesFile The properties file from which to read the path of the mapping folder.
+     */
     public AQMeshInputAgent(String propertiesFile) throws IOException {
-        // Initialize time series client
-        tsClient = new TimeSeriesClient<>(LocalDateTime.class, propertiesFile);
+        // Set the mapping between JSON keys and IRIs
+        try (InputStream input = new FileInputStream(propertiesFile)) {
+            // Load properties file from specified path
+            Properties prop = new Properties();
+            prop.load(input);
+            // Read the mappings folder from the properties file
+            String mappingFolder = prop.getProperty("aqmesh.mappingfolder");
+            // Read the JSON key to IRI mappings from
+            readMappings(mappingFolder);
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,6 +63,11 @@ public class AQMeshInputAgent {
         agent.setAPIConnector(connector);
     }
 
+
+    public void setTsClient(TimeSeriesClient tsClient) {
+        this.tsClient = tsClient;
+    }
+
     public void setAPIConnector(AQMeshAPIConnector connector) {
         this.connector = connector;
     }
@@ -52,6 +80,31 @@ public class AQMeshInputAgent {
         catch (Exception e) {
         }
     }
+
+    /**
+     * Reads the JSON key to IRI mappings from files in the provided folder.
+     * @param mappingFolder The path to the folder in which the mapping files are located.
+     */
+    private void readMappings(String mappingFolder) throws IOException {
+        mappings = new ArrayList<>();
+        File folder = new File(mappingFolder);
+        File[] mappingFiles = folder.listFiles();
+        // Make sure the folder exists and contains files
+        if (mappingFiles == null) {
+            throw new IOException("Folder does not exist: " + mappingFolder);
+        }
+        if (mappingFiles.length == 0) {
+            throw new IOException("No files in the folder: " + mappingFolder);
+        }
+        // Create a mapper for each file
+        else {
+            for (File mappingFile: mappingFiles) {
+                JSONKeyToIRIMapper mapper = new JSONKeyToIRIMapper(AQMeshInputAgent.generatedIRIPrefix, mappingFile.getAbsolutePath());
+                mappings.add(mapper);
+            }
+        }
+    }
+
 
     private void updateGasReadings() {
         JSONArray gasReadings = connector.getGasReadings();
--- Agents/AQMeshInputAgent/src/test/java/uk/ac/cam/cares/jps/agent/aqmesh/AQMeshInputAgentTest.java
+++ Agents/AQMeshInputAgent/src/test/java/uk/ac/cam/cares/jps/agent/aqmesh/AQMeshInputAgentTest.java
@@ -1,4 +1,93 @@
 package uk.ac.cam.cares.jps.agent.aqmesh;
 
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import uk.ac.cam.cares.jps.agent.utils.JSONKeyToIRIMapper;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AQMeshInputAgentTest {
+
+    // Temporary folder to place a properties file (same file for all potential tests)
+    @ClassRule
+    public static TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void testConstructor() throws IOException, NoSuchFieldException, IllegalAccessException {
+        // Filepath for the properties file
+        String propertiesFile = Paths.get(folder.getRoot().toString(), "aqmesh.properties").toString();
+        // Create a property file with a mapping folder that does not exist
+        String folderName = "no_valid_folder";
+        writePropertyFile(propertiesFile, Collections.singletonList("aqmesh.mappingfolder=" + folderName));
+        // Run constructor that should give an exception
+        try {
+            new AQMeshInputAgent(propertiesFile);
+            Assert.fail();
+        }
+        catch (IOException e) {
+            Assert.assertTrue(e.getMessage().contains("Folder does not exist:"));
+            Assert.assertTrue(e.getMessage().contains(folderName));
+        }
+
+        // Create an empty folder
+        folderName = "mappings";
+        File mappingFolder = folder.newFolder(folderName);
+        // Create a property file with the empty folder
+        folderName = mappingFolder.getCanonicalPath().replace("\\","/");
+        writePropertyFile(propertiesFile, Collections.singletonList("aqmesh.mappingfolder=" + folderName));
+        // Run constructor that should give an exception
+        try {
+            new AQMeshInputAgent(propertiesFile);
+            Assert.fail();
+        }
+        catch (IOException e) {
+            Assert.assertTrue(e.getMessage().contains("No files in the folder:"));
+            Assert.assertTrue(e.getMessage().contains(folderName));
+        }
+
+        // Add mapping files into the empty folder
+        // All IRIs set
+        String firstMappingFile = Paths.get(mappingFolder.getAbsolutePath(), "firstMapping.properties").toString();
+        String[] keys = {"key1", "key2" ,"key3"};
+        ArrayList<String> mappings = new ArrayList<>();
+        for (String key: keys) {
+            mappings.add(key + "=example:prefix/api_" + key);
+        }
+        writePropertyFile(firstMappingFile, mappings);
+        // No IRIs set
+        String secondMappingFile = Paths.get(mappingFolder.getAbsolutePath(), "secondMapping.properties").toString();
+        mappings = new ArrayList<>();
+        for (String key: keys) {
+            mappings.add(key + "=");
+        }
+        writePropertyFile(secondMappingFile, mappings);
+        // Create agent
+        AQMeshInputAgent agent = new AQMeshInputAgent(propertiesFile);
+        // Assert that the mappings were set
+        Field mappingField = AQMeshInputAgent.class.getDeclaredField("mappings");
+        mappingField.setAccessible(true);
+        ArrayList<JSONKeyToIRIMapper> mappingList = (ArrayList<JSONKeyToIRIMapper>) mappingField.get(agent);
+        Assert.assertEquals(2, mappingList.size());
+    }
+
+    private void writePropertyFile(String filepath, List<String> properties) throws IOException {
+        // Overwrite potentially existing properties file
+        FileWriter writer = new FileWriter(filepath, false);
+        // Populate file
+        for (String s : properties) {
+            writer.write(s + "\n");
+        }
+        // Close the file and return the file
+        writer.close();
+    }
+
 }
