d2cc96e00c010d3d05c23a52b36faa6d35bc55c5
Wanni Xie <wx243@cam.ac.uk>
2021-08-17 16:51:47 +0100

    99-UK-digital-twin-powsys: updated the web-vis of UK digital twin.

--- web/UKDigitalTwin/mapbox-vis/JSON_Generator.py
+++ web/UKDigitalTwin/mapbox-vis/JSON_Generator.py
@@ -8,123 +8,8 @@ import pandas as pd
 import matplotlib.cm 
 import matplotlib.colors
 
-def gen_fuel_col(gen_fuel):
-  #returns a colour code for each generator fuel type.
-  #https://htmlcolorcodes.com/
-  map_fuel_dict = {
-        "BiomassGeneration": "#1E8449",
-        "Biomass": "#1E8449",
-        "WindGeneration": "#13bef2",
-        "Wind": "#13bef2",
-        "HydroGeneration": "#1F618D",
-        "Hydro": "#1F618D",
-        "SolarGeneration": "#ffd21f",
-        "Solar": "#ffd21f",
-        "CoalGeneration": "#99A3A4",
-        "Coal": "#99A3A4",
-        "OilGeneration": "#1B2631",
-        "Oil": "#1B2631",
-        "NaturalGasGeneration": "#eb8500",
-        "NaturalGas": "#eb8500",
-        "NuclearGeneration": "#ed2400",
-        "Nuclear": "#ed2400",
-        "PumpHydroStorage": "#1F618D",
-        "PumpHydro": "#1F618D",
-        "SourGasGeneration": "#eb8500",
-        "SourGas": "#eb8500",
-        "Waste_anaerobicdigestion": "#1E8449",
-        "Waste_municipalsolidwaste": "#bc1fff",
-        "Waste": "#873600"
-    }
-  return map_fuel_dict[gen_fuel]
-
-def check_GPS_char(c):
-  #Checks to provided character is acceptable in GPS format
-  acceptable = "-.0123456789"
-  if c in acceptable:
-    return True
-  return False
-
-def GPS_special_chars(coordinate):
-  #Checks that if there's a '-' it's only in the first position.
-  #Checks that there is only one '.' decimal point.
-  acceptable = ".0123456789"
-  tick = 0
-  point = 0
-  for i in coordinate[1]:
-    if (tick > 0) and not (i in acceptable):
-      coordinate[1] = coordinate[1][:tick] + coordinate[1][tick+1:]
-      tick -= 1
-      print("Warning: " + coordinate[0] + " contains (automatically removed) misplaced '-' in longitude.")
-    if i == '.':
-      if point > 0:
-        coordinate[1] = coordinate[1][:tick] + coordinate[1][tick+1:]
-        tick -= 1
-        print("Warning: " + coordinate[0] + " contains (automatically removed) additional '.' in longitude.")
-      point += 1
-    tick += 1
-  tick = 0
-  point = 0
-  for i in coordinate[2]:
-    if (tick > 0) and not (i in acceptable):
-      coordinate[2] = coordinate[2][:tick] + coordinate[2][tick+1:]
-      tick -= 1
-      print("Warning: " + coordinate[0] + " contains (automatically removed) misplaced '-' in latitude.")
-    if i == '.':
-      if point > 0:
-        coordinate[2] = coordinate[2][:tick] + coordinate[2][tick+1:]
-        tick -= 1
-        print("Warning: " + coordinate[0] + " contains (automatically removed) additional '.' in latitude.")
-      point += 1
-    tick += 1
-  return coordinate
-
-def check_GPS(ret):
-  #Checks the GPS coordinates are of the correct format.
-  #the 'ret' input array should have the first three columns as:
-  #0: The name.
-  #1: The longitude.
-  #2: The latitude.
-  #3+: (Optional) Doesn't matter, it just uses the first three columns, if you have more that's fine. 
-  #Note that the 'automatic replacement' is not in the knowledge graph, just on the output side.
-  for i in ret:
-    #Check the longitude and latitude don't have unexpected characters and have at least some permitted characters. 
-    for j in i[1]:
-      if not check_GPS_char(j):
-        print("Warning: " + i[0] + " contains (automatically removed) longitude (GPS) incompatible character: " + j)
-        i[1] = i[1].replace(j, '')
-    if len(i[1]) == 0:
-      print("Warning: " + i[0] + " contains no valid longitude, 0.0000000 substituted")
-      i[1] = "0.0000000"
-    for j in i[2]:
-      if not check_GPS_char(j):
-        print("Warning: " + i[0] + " contains (automatically removed) latitude (GPS) incompatible character: " + j)
-        i[2] = i[2].replace(j, '')
-    if len(i[2]) == 0:
-      print("Warning: " + i[0] + " contains no valid latitude, 0.0000000 substituted")
-      i[2] = "0.0000000"
-    #Check the longitude and latitude have special characters ('-' and '.') used correctly.
-    i = GPS_special_chars(i)
-    #Check the longitude and latitude are now valid numbers.
-    try:
-        float(i[1])
-    except ValueError:
-        print("Warning: " + i[0] + " contains longitude that is not a number, 0.0000000 substituted")
-        i[1] = "0.0000000"
-    try:
-        float(i[2])
-    except ValueError:
-        print("Warning: " + i[0] + " contains latitude that is not a number, 0.0000000 substituted")
-        i[2] = "0.0000000"
-    #Check the longitude and latitude are in the valid range (-180 to 180 for longitude, -90 to 90 for latitude).
-    if (float(i[1]) < -180.0) or (float(i[1]) > 180.0):
-      print("Warning: " + i[0] + " contains longitude that is out of the valid -180 to 180 range, 0.0000000 substituted")
-      i[1] = "0.0000000"
-    if (float(i[2]) < -90.0) or (float(i[2]) > 90.0):
-      print("Warning: " + i[0] + " contains latitude that is out of the valid -90 to 90 range, 0.0000000 substituted")
-      i[2] = "0.0000000"
-  print("GPS check completed. If there was a 'Warning' in the output above then the 'automatic replacement / substitution' mentioned does not effect the knowledge graph, just the created JSON file. ")
-  return ret
+from GPSLocationChecker import check_GPS_char, GPS_special_chars, check_GPS
+from colourLayers import gen_fuel_col
 
 def query_to_geoJSON(endpoint, class_label):
   '''
@@ -207,7 +92,7 @@ def query_to_geoJSON(endpoint, class_label):
             ?PowerGenerator ontoecape_technical_system:realizes/ontoeip_powerplant:consumesPrimaryFuel ?Primary_Fuel_type .
             ?PowerGenerator ontoecape_technical_system:realizes/ontoeip_powerplant:usesGenerationTechnology ?GenerationTechnology .
   
-        } LIMIT 1200
+        } LIMIT 2
  """
 
   query_sdg = """
@@ -229,8 +114,7 @@ def query_to_geoJSON(endpoint, class_label):
   # sparql_pow= SPARQLWrapper('https://como.ceb.cam.ac.uk/rdf4j-server/repositories/UKPowerPlantKG')
   sparql_pow.setReturnFormat(JSON) 
   
-  # TODO: query string passing 
-  sparql_pow.setQuery(query_pow) 
+  # TODO: query string passing  
   sparql_pow.setQuery(query_pow_new)
   
   start = time.time()
@@ -239,8 +123,12 @@ def query_to_geoJSON(endpoint, class_label):
   end = time.time()
   # parsing JSON SPARQL results into an array
   print('Finished in ',np.round(end-start,2),' seconds')
-  ret = ret['results']['bindings']
-  #print(ret) #Unformatted
+  # print("The query return type is: ", type(ret))
+  # print(list(ret.keys())) #Unformatted
+  # print(ret["head"])  
+  
+  ret = ret['results']['bindings'] # extract the elements of the original dict
+  print("The query return is ", ret) #Unformatted
   num_ret = len(ret)
   # assigning memory to results array 
   ret_array = np.zeros((num_ret,5),dtype='object')
