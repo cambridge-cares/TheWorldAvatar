6e92cb89ae94ac0d822bed6230fb41404f17b479
Niklas Kasenburg <niklas.kasenburg@cares.cam.ac.uk>
2021-08-18 10:48:39 +0800

    1160-dev-aqmesh-input-agent: Add loghing to launcher class.

--- Agents/AQMeshInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/aqmesh/AQMeshInputAgentLauncher.java
+++ Agents/AQMeshInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/aqmesh/AQMeshInputAgentLauncher.java
@@ -7,9 +7,25 @@ import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
 import java.io.IOException;
 import java.time.OffsetDateTime;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class AQMeshInputAgentLauncher {
 
-    // TODO: Proper logging for each phase of the main script
+    /**
+     * Logger for reporting info/errors.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(AQMeshInputAgentLauncher.class);
+    // Logging / error messages
+    private static final String ARGUMENT_MISMATCH_MSG = "Need three properties files in the following order: 1) input agent 2) time series client 3) API connector.";
+    private static final String AGENT_ERROR_MSG = "The AQMesh input agent could not be constructed!";
+    private static final String TSCLIENT_ERROR_MSG = "Could not construct the time series client needed by the input agent!";
+    private static final String CONNECTOR_ERROR_MSG = "Could not construct the AQMesh API connector needed to interact with the API!";
+    private static final String GET_READINGS_ERROR_MSG = "One or both readings could not be retrieved, this might have created a mismatch" +
+            " in the pointers if one readings was successful and needs to be fixed!";
+    private static final String ONE_READING_EMPTY_ERROR_MSG = "One of the readings (gas or particle) is empty, that means there is " +
+            "a mismatch in the pointer for each readings. This should be fixed (and might require a clean up of the database)!";
+
     // TODO: Use proper argument parsing
     /**
      * Main method that runs through all steps to update the data received from the AQMesh API.
@@ -21,24 +37,30 @@ public class AQMeshInputAgentLauncher {
 
         // Ensure that there are three properties files
         if (args.length != 3) {
-            throw new JPSRuntimeException("Need three properties files in the following order: 1) input agent 2) time series client 3) API connector.");
+            LOGGER.error(ARGUMENT_MISMATCH_MSG);
+            throw new JPSRuntimeException(ARGUMENT_MISMATCH_MSG);
         }
+        LOGGER.debug("Launcher called with the following files: " + String.join(" ", args));
 
         // Create the agent
         AQMeshInputAgent agent;
         try {
             agent = new AQMeshInputAgent(args[0]);
         } catch (IOException e) {
-            throw new JPSRuntimeException("The AQMesh input agent could not be constructed!", e);
+            LOGGER.error(AGENT_ERROR_MSG, e);
+            throw new JPSRuntimeException(AGENT_ERROR_MSG, e);
         }
+        LOGGER.info("Input agent object initialized.");
 
         // Create and set the time series client
         try {
             TimeSeriesClient<OffsetDateTime> tsClient = new TimeSeriesClient<>(OffsetDateTime.class, args[1]);
             agent.setTsClient(tsClient);
         } catch (IOException | JPSRuntimeException e) {
-            throw new JPSRuntimeException("Could not construct the time series client needed by the input agent!", e);
+            LOGGER.error(TSCLIENT_ERROR_MSG, e);
+            throw new JPSRuntimeException(TSCLIENT_ERROR_MSG, e);
         }
+        LOGGER.info("Time series client object initialized.");
 
         // Initialize time series'
         agent.initializeTimeSeriesIfNotExist();
@@ -48,9 +70,12 @@ public class AQMeshInputAgentLauncher {
         try {
             connector = new AQMeshAPIConnector(args[2]);
         } catch (IOException e) {
-            throw new JPSRuntimeException("Could not construct the AQMesh API connector needed to interact with the API!", e);
+            LOGGER.error(CONNECTOR_ERROR_MSG, e);
+            throw new JPSRuntimeException(CONNECTOR_ERROR_MSG, e);
         }
+        LOGGER.info("API connector object initialized.");
         connector.connect();
+        LOGGER.info("Connected to AQMesh API.");
 
         // Retrieve readings
         JSONArray particleReadings;
@@ -60,27 +85,30 @@ public class AQMeshInputAgentLauncher {
             gasReadings = connector.getGasReadings();
         }
         catch (Exception e) {
-            throw new JPSRuntimeException("One or both readings could not be retrieved, this might have created a mismatch" +
-                    " in the pointers if one readings was successful and needs to be fixed!", e);
+            LOGGER.error(GET_READINGS_ERROR_MSG, e);
+            throw new JPSRuntimeException(GET_READINGS_ERROR_MSG, e);
         }
+        LOGGER.info(String.format("Retrieved %d particle readings and %d gas readings.",
+                particleReadings.length(), gasReadings.length()));
 
         // If both readings are not empty there is new data
         if(!particleReadings.isEmpty() && !gasReadings.isEmpty()) {
             // Update the data
             agent.updateData(particleReadings, gasReadings);
+            LOGGER.info("Data updated with new readings from API.");
         }
         // If both are empty no new readings are available
         else if(particleReadings.isEmpty() && gasReadings.isEmpty()) {
-            // TODO: should be logged as info
-            System.out.println("No new readings available.");
+            LOGGER.info("No new readings are available.");
         }
         // One reading is empty and the other is not. This is likely due to asynchronous access to the readings, which
         // sets the pointers for each reading separately (should not happen when only using the agent unless there is an API error).
-        // Data should be added to keep the database consistent. The pointer should be reset and
-        // probably manual clean up in the database is required.
+        // The pointer should be reset and probably manual clean up in the database is required.
+        // Note: This is normally not a problem, but since the AQMeshInputAgent requires that all JSON keys are present in the combined
+        // readings, having one reading empty will result in an error when calling the updateData method.
         else {
-            throw new JPSRuntimeException("One of the readings (gas or particle) is empty, that means there is " +
-                    "a mismatch in the pointer for each readings. This should be fixed (and might require a clean up of the database)!");
+            LOGGER.error(ONE_READING_EMPTY_ERROR_MSG);
+            throw new JPSRuntimeException(ONE_READING_EMPTY_ERROR_MSG);
         }
     }
 
