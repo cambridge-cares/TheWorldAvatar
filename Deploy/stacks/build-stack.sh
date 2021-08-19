1cc31b5080579be4179351b2423505cf64c549ca
Owen Parry <oparry@cmclinnovations.com>
2021-08-19 11:40:17 +0100

    dev-docker_stack_improvements: Added initial versions of new wrapper scripts for building and deploying stacks.

---
+++ Deploy/stacks/build-stack.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+# Wrapper script for docker-compose that builds the requested stack in one of three
+# modes (dev/test/prod).
+#
+# Each stack directory contains a base configuration file and, optionally, mode-specific configuration files.
+# That is:
+#   docker-compose.build.yml
+#   docker-compose.build.dev.yml (optional)
+#   docker-compose.build.test.yml (optional)
+#   docker-compose.build.prod.yml (optional)
+
+
+# Show a usage statement if too few arguments were supplied
+#  (use 'lt' rather than 'ne' to allow additional arguments to docker-compose build)
+if [ "$#" -lt 2 ]; then
+  echo "============================================================================="
+  echo " Usage:"
+  echo "  $0 [stack_name] [mode] <--push> <additional_args>"
+  echo ""
+  echo "      stack_name : the stack to build (agent/db/web)"
+  echo "            mode : configuration mode name (dev/test/prod)"
+  echo "          --push : including this flag will push images to the registry once built"
+  echo " additional_args : remaining arguments are passed on to 'docker-compose build'"
+  echo ""
+  echo "e.g. To build the web stack in dev mode, rebuilding each layer from scratch rather than using the cache:"
+  echo "  $0 web dev --no-cache"
+  echo "============================================================================="
+  exit 1
+fi
+
+default_build_args="--progress auto"
+
+# Load common helper functions
+if [ -e ./common_funcs.sh ]; then
+  . ./common_funcs.sh
+else
+  echo "Unable to load bash helper functions, make sure you're running this script in Deploy/stacks/"
+  exit 1
+fi
+
+# Assign input args to variables and pop/shift them from the arg array
+process="build"
+stack="$1"
+mode="$2"
+shift;shift
+
+# Process remaining args. Avoiding using getopts here in a vain attempt to keep things shell-agnostic
+additional_build_args=""
+additional_push_args=""
+push_enabled=$FALSE
+while test $# -gt 0; do
+  case "$1" in
+    --push)
+      push_enabled=$TRUE
+      shift
+      ;;
+    -*)
+      additional_build_args="$additional_build_args $1"
+      shift
+      ;;
+    *)
+      # Assume all remaining non-flag args are service name - pass on to 'build' and 'push'
+      additional_push_args="$additional_push_args $1"
+      additional_build_args="$additional_build_args $1"
+      shift
+      ;;
+  esac
+done
+
+# Validate args
+if ! $(is_valid_stack $stack); then echo "$0: '$stack' is not a valid stack" && exit 2; fi
+if ! $(is_valid_mode $mode); then echo "$0: '$mode' is not a valid mode" && exit 3; fi
+
+# Print preamble and cd to stack directory
+init_stack_script $stack "Building the $stack stack in $mode mode\n\n"
+
+# Get yml filenames
+yml_fnames=$(get_yml_fnames $mode $process)
+if [ "$?" -ne 0 ]; then echo "$yml_fnames" ; exit "$?"; fi
+yml_fname_args=$(echo $yml_fnames |sed -e 's/ / -f /' -e 's/^/-f /')
+
+# Write environment variables to file so that docker-compose can pick them up
+env_filename="env.txt"
+write_env_file $env_filename
+
+# Assemble arguments for docker-compose
+compose_opts="$yml_fname_args --env-file $env_filename"
+
+# Run docker-compose build, appending any additional args passed to this script
+cmd="docker-compose $compose_opts build $default_build_args $additional_build_args"
+echo "Running $cmd in ./$stack ..."
+$cmd
+build_exit_code=$?
+exit_on_error $build_exit_code "\n'docker-compose build' failed"
+
+# Run docker-compose push if requested
+if [ $push_enabled -eq $TRUE ]; then
+  echo "Pushing images to registry"
+  cmd="docker-compose $compose_opts push $additional_push_args"
+  echo "Running $cmd in ./$stack ..."
+  $cmd
+  push_exit_code=$?
+  exit_on_error $push_exit_code "\n'docker-compose push' failed"
+fi
+
+# print success message and exit
+exit_with_msg 0 "\nFinished building $stack stack in $mode mode"
---
+++ Deploy/stacks/deploy-stack.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+
+# Wrapper script for docker-compose that deploys the requested stack in one of three
+# modes (dev/test/prod).
+#
+# Each stack directory contains a base configuration file and, optionally, mode-specific configuration files.
+# That is:
+#   docker-compose.deploy.yml
+#   docker-compose.deploy.dev.yml (optional)
+#   docker-compose.deploy.test.yml (optional)
+#   docker-compose.deploy.prod.yml (optional)
+
+
+# Show a usage statement if too few arguments were supplied (use 'lt' rather than 'ne' to allow additional arguments to docker-compose up)
+if [ "$#" -lt 2 ]; then
+  echo "============================================================================="
+  echo " Usage:"
+  echo "  $0 [stack_name] [mode] <--force-pull service_list> <additional_args>"
+  echo ""
+  echo "                stack_name : the stack to deploy (agent/db/web)"
+  echo "                      mode : configuration mode name (dev/test/prod)"
+  echo " --force-pull service_list : pull images from the registry, even if they exist locally."
+  echo "                             'service_list' is a comma-separated list of service names"
+  echo "           additional_args : remaining arguments are passed on to 'docker-compose up'"
+  echo ""
+  echo "e.g. To deploy the web stack in dev mode:"
+  echo "  $0 web dev"
+  echo "============================================================================="
+  exit 1
+fi
+
+
+# Default options for docker-compose up
+default_up_args="-d --no-build"
+
+# Assign first two input args to variables and pop/shift them from the arg array
+process="deploy"
+stack="$1"
+mode="$2"
+shift;shift
+
+# Process remaining args. Avoiding using getopts here in a vain attempt to keep things shell-agnostic
+additional_up_args=""
+services_to_force_pull=""
+while test $# -gt 0; do
+  case "$1" in
+    --force-pull)
+      shift
+      services_to_force_pull=$1
+      shift
+      ;;
+    *)
+      additional_up_args="$additional_up_args $1"
+      shift
+      ;;
+  esac
+done
+
+# Load common helper functions
+if [ -e ./common_funcs.sh ]; then
+  . ./common_funcs.sh
+else
+  echo "Unable to load bash helper functions, make sure you're running this script in Deploy/stacks/"
+  exit 1
+fi
+
+# Validate args
+if ! $(is_valid_stack $stack); then echo "$0: '$stack' is not a valid stack" && exit 2; fi
+if ! $(is_valid_mode $mode); then echo "$0: '$mode' is not a valid mode" && exit 3; fi
+
+
+# Print preamble and cd to stack directory
+init_stack_script $stack "Deploying the $stack stack in $mode mode\n\n"
+
+# Get yml filenames
+yml_fnames=$(get_yml_fnames $mode $process)
+if [ "$?" -ne 0 ]; then echo "$yml_fnames" ; exit "$?"; fi
+yml_fname_args=$(echo $yml_fnames |sed -e 's/ / -f /' -e 's/^/-f /')
+
+# Write environment variables to file so that docker-compose can pick them up
+env_filename="env.txt"
+write_env_file $env_filename
+
+# Assemble arguments for docker-compose
+compose_opts="$yml_fname_args --env-file $env_filename"
+
+# Examine secret files defined in the config files and set missing values where necessary
+set_missing_secrets $yml_fnames
+
+# For images that exist locally, but need to be updated from the registry, need to run docker pull explicitly
+if [ ! -z "$services_to_force_pull" ]; then
+  echo "Overriding local image(s) with registry version..."
+  cmd="docker-compose $compose_opts pull $services_to_force_pull"
+  echo "Running $cmd in ./$stack ..."
+  $cmd
+  pull_return_code=$?
+  exit_on_error "$pull_return_code" "\n'docker-compose pull' failed"
+  printf "Done\n\n"
+fi
+
+# Run docker-compose up, passing on any additional args that were supplied to this script
+echo "Deploying stack"
+cmd="docker-compose $compose_opts up $default_up_args $additional_up_args"
+echo "Running $cmd in ./$stack ..."
+$cmd
+up_exit_code=$?
+exit_on_error $up_exit_code "\n'docker-compose up' failed"
+
+# print success message and exit
+exit_with_msg 0 "\n$stack stack deployed in $mode mode"
