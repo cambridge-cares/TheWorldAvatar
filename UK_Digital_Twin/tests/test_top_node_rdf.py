3e14d5a47fc241b117acc60338b0a1088b73f3d3
Feroz Farazi <msff2@cam.ac.uk>
2021-08-24 18:46:45 +0100

    99-UK-digital-twin-powsys: Created the test_top_node_rdf.py module.

---
+++ UK_Digital_Twin/tests/test_top_node_rdf.py
@@ -0,0 +1,8 @@
+"""Path to the generated top node represented using RDF"""
+path_to_generated_top_node_rdf = '../resources/test_generated_top_node_rdf'
+"""Path to the reference electricity consumption data represented using RDF"""
+path_to_reference_top_node_rdf = '../resources/test_reference_top_node_rdf'
+
+"""
+The goal of this class is to test the semantic representation of the top node of the UK power system.
+"""
