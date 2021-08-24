33e4e689977196f125ec7a2037d8630262f3a70c
Feroz Farazi <msff2@cam.ac.uk>
2021-08-24 18:49:11 +0100

    99-UK-digital-twin-powsys: Developed code for comparing a generated file with a reference file.

--- UK_Digital_Twin/tests/test_top_node_rdf.py
+++ UK_Digital_Twin/tests/test_top_node_rdf.py
@@ -1,5 +1,10 @@
 import unittest
 import os
+from os import listdir
+from os.path import isfile, join
+
+from Top_Node_Generator.topnodeABoxGeneration import generateTopNodeGraph
+from tests.test_power_plant_rdf import TestPowerPlantInfoRDF
 
 """Path to the generated top node represented using RDF"""
 path_to_generated_top_node_rdf = '../resources/test_generated_top_node_rdf'
@@ -19,6 +24,20 @@ class TestTopNodeRDF(unittest.TestCase):
         """
         abs_path_generated_top_node_rdf = os.path.abspath(path_to_generated_top_node_rdf)
         abs_path_reference_top_node_rdf = os.path.abspath(path_to_reference_top_node_rdf)
+        """
+        Calls the function that generates RDF representations of the top node
+        """
+        generateTopNodeGraph('default', False, abs_path_generated_top_node_rdf, True)
+        owl_files = [f for f in listdir(path_to_generated_top_node_rdf) if isfile(join(path_to_generated_top_node_rdf, f))]
+        testPowerPlantInfoRDF = TestPowerPlantInfoRDF()
+        for an_owl_file in owl_files:
+            generated_statements = testPowerPlantInfoRDF.read_rdf_model_statements(abs_path_generated_top_node_rdf + os.sep + an_owl_file)
+            reference_statements = testPowerPlantInfoRDF.read_rdf_model_statements(abs_path_reference_top_node_rdf + os.sep + an_owl_file)
+            print('Comparing the content of the following two files:\n', abs_path_generated_top_node_rdf + os.sep + an_owl_file,
+                  '\n', abs_path_reference_top_node_rdf + os.sep + an_owl_file)
+            self.assertEqual(generated_statements, reference_statements)
+            print('The files have the same content.')
+            print('\n')
 
 if __name__ == '__main__':
     """
