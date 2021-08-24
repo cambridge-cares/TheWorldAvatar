ba2356198666287d7156bc63065b298ed95c49b0
Feroz Farazi <msff2@cam.ac.uk>
2021-08-24 18:48:12 +0100

    99-UK-digital-twin-powsys: Created the test_compare_top_node_axioms() function.

--- UK_Digital_Twin/tests/test_top_node_rdf.py
+++ UK_Digital_Twin/tests/test_top_node_rdf.py
@@ -1,3 +1,6 @@
+import unittest
+import os
+
 """Path to the generated top node represented using RDF"""
 path_to_generated_top_node_rdf = '../resources/test_generated_top_node_rdf'
 """Path to the reference electricity consumption data represented using RDF"""
@@ -6,3 +9,19 @@ path_to_reference_top_node_rdf = '../resources/test_reference_top_node_rdf'
 """
 The goal of this class is to test the semantic representation of the top node of the UK power system.
 """
+class TestTopNodeRDF(unittest.TestCase):
+    """
+    Test that it can generate the same axioms provided in the reference OWL file.
+    """
+    def test_compare_top_node_axioms(self):
+        """
+        Gets absolute path from a relative path
+        """
+        abs_path_generated_top_node_rdf = os.path.abspath(path_to_generated_top_node_rdf)
+        abs_path_reference_top_node_rdf = os.path.abspath(path_to_reference_top_node_rdf)
+
+if __name__ == '__main__':
+    """
+    Allows to run the test case from the command-line interface
+    """
+    # unittest.main()
