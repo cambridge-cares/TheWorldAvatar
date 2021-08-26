85e3b38d587af4a379c3768a3dec89ffd05a26c0
dln22 <danieln@cmclinnovations.com>
2021-08-26 16:49:57 +0100

    develop: Fixed regex that works out number of atoms from the chemformula.

--- Agents/STDCThermoAgent/stdc/utils/utils.py
+++ Agents/STDCThermoAgent/stdc/utils/utils.py
@@ -14,10 +14,10 @@ FREQUENCY_UNITS = ['1/S','HZ','KHZ','GHZ','MHZ']
 # -----------------------------
 def getInertiaTensor(aElMolWt,aXYZ):
     # ===================================
-    # helper functions to populate 
+    # helper functions to populate
     # inertia tensor
     #
-    # diagonal elements    
+    # diagonal elements
     def getDiagMoments(a,b,m):
         MolWt = sum(m)
         sum1 = 0.0
@@ -67,11 +67,11 @@ def getInertiaTensor(aElMolWt,aXYZ):
     IT = [[Ixx,Ixy,Ixz],[Iyx,Iyy,Iyz],[Izx,Izy,Izz]]
     return IT
 
-# Get moments of Inertia 
+# Get moments of Inertia
 # of a molecule
 #---------------------------------
 def getMomentsOfInertia(aElMolWt,aXYZ,aGeomType):
-    InertiaMom = []    
+    InertiaMom = []
     #construct the mass and XYZ np arrays
     if len(aElMolWt)>0 and len(aXYZ)>0:
         IT = getInertiaTensor(aElMolWt,aXYZ)
@@ -82,7 +82,7 @@ def getMomentsOfInertia(aElMolWt,aXYZ,aGeomType):
 # Entropy of a species from NASA polynomials
 #--------------------------
 def getEntropy(alow,ahigh,Trange,T):
-    S = 0.0        
+    S = 0.0
     if T>0.0:
         Tmid = Trange[1]
         Ta=[]
@@ -163,9 +163,10 @@ def getGibbsEnergy(alow,ahigh,Trange,T):
     G = H - T*S
     return G
 
-def chemFormulaToAtomsCounts(chemFormula, atomCounts={}, multiplier=1.0):    
-    atomCounts, chemFormula = _funcGroupsAtomsCounts(chemFormula.upper(), atomCounts)
-    atomCounts = _chemFormulaToAtomsCounts(chemFormula, atomCounts, multiplier)
+def chemFormulaToAtomsCounts(chemFormula):
+    atomCounts={}
+    atomCounts, chemFormula = _funcGroupsAtomsCounts(chemFormula, atomCounts)
+    atomCounts = _chemFormulaToAtomsCounts(chemFormula, atomCounts, multiplier=1.0)
 
     return atomCounts
 
@@ -175,7 +176,7 @@ def _funcGroupsAtomsCounts(chemFormula, atomCounts):
     funcGroupsMatch = re.findall(funcGroupRegex,chemFormula)
     if funcGroupsMatch:
         for funcGroup in sorted(funcGroupsMatch,reverse=True,key=len):
-            chemFormula = chemFormula.replace(funcGroup,'')        
+            chemFormula = chemFormula.replace(funcGroup,'')
             countMatch = re.search('\)(\d+)$',funcGroup)
             if countMatch:
                 count = countMatch.groups()[0]
@@ -189,26 +190,34 @@ def _funcGroupsAtomsCounts(chemFormula, atomCounts):
             else:
                 funcGroupCounts[funcGroup] += int(count)
 
-        for funcGroup, funcGroupCount in funcGroupCounts.items():            
+        for funcGroup, funcGroupCount in funcGroupCounts.items():
             atomCounts = _chemFormulaToAtomsCounts(funcGroup, atomCounts, funcGroupCount)
     return atomCounts, chemFormula
 
 def _chemFormulaToAtomsCounts(chemFormula, atomCounts, multiplier):
-    Elements =['H','HE','LI','BE','B','C','N','O','F','NE','NA','MG','AL',
-               'SI','P','S','CL','AR','K','CA','SC','TI','V','CR','MN',
-               'FE','CO','NI','CU','ZN','GA','GE','AS','SE','BR','KR',
-               'RB','SR','Y','ZR','NB','MO','TC','RU','RH','PD','AG','CD',
-               'IN','SN','SB','TE','IN','XE','CS','BA','LA','CE','PR',
-               'ND','PM','SM','EU','GD','TB','DY','HO','ER','TM','YB',
-               'LU','HF','TA','W','RE','OS','IR','PT','AU','HG','TL',
-               'PB','BI','PO','AT','RN','FR','RA','AC','TH','PA','U',
-               'NP','PU','AM','CM','BK','CF','ES','FM','MD','NO','LR',
-               'RF','DB','SG','BH','HS','MT','DS','RG','CN','NH','FL','MC']
+    Elements =[
+    'H', 'He',
+    'Li','Be','B','C','N','O','F','Ne',
+    'Na','Mg','Al', 'Si','P','S','Cl','Ar',
+    'K','Ca',
+        'Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu','Zn',
+        'Ga','Ge','As','Se','Br','Kr',
+    'Rb','Sr',
+        'Y','Zr','Nb','Mo','Tc','Ru','Rh','Pd','Ag','Cd',
+        'In','Sn','Sb','Te','I','Xe',
+    'Cs','Ba',
+        'La','Ce','Pr','Nd','Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu',
+        'Hf','Ta','W','Re','Os','Ir','Pt','Au','Hg',
+        'Tl','Pb','Bi','Po','At','Rn',
+    'Fr','Ra',
+        'Ac','Th','Pa','U','Np','Pu','Am','Cm','Bk','Cf','Es','Fm','Md','No','Lr',
+        'Rf','Db','Sg','Bh','Hs','Mt','Ds','Rg','Cn',
+        'Nh','Fl','Mc','Lv','Ts','Og']
 
-    atomCountsRegex=f'(['+Elements[0]
+    atomCountsRegex=f'('+Elements[0]+'\d*'
     for el in Elements[1:]:
-        atomCountsRegex += f'|'+ el
-    atomCountsRegex += f']\d*)'
+        atomCountsRegex += f'|'+ el + '\d*'
+    atomCountsRegex += f')'
 
     atomCountMatch = re.findall(atomCountsRegex,chemFormula)
     if atomCountMatch:
