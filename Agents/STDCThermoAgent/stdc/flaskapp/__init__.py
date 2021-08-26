b7e0488cd94683b86910191e6d533f4a5e312414
dln22 <danieln@cmclinnovations.com>
2021-08-26 23:22:46 +0100

    develop: Seprated the flaskapp into home and calculator blueprints.

--- Agents/STDCThermoAgent/stdc/flaskapp/__init__.py
+++ Agents/STDCThermoAgent/stdc/flaskapp/__init__.py
@@ -12,39 +12,13 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.update(test_config)
 
+    with app.app_context():
+        # Import parts of our application
+        import stdc.flaskapp.home.routes as home
+        import stdc.flaskapp.calculator.routes as calculator
 
-    # Show an instructional message at the app root
-    @app.route('/')
-    def default():
-        msg  = "To see the result of an API call, enter a URL of the form:<BR>"
-        msg += "&nbsp&nbsp /api/thermoagent/calculate?ontocompchem_IRI=oc_IRI&ontospecies_IRI=os_IRI"
-        return msg
-
-    # Define a route for API requests
-    @app.route('/api/thermoagent/calculate', methods=['GET'])
-    def api():
-        # Check arguments (query parameters)
-        print(request.args)
-        inputs= {}
-        ontocompchem_IRI = request.args['ontocompchem_IRI']
-        ontospecies_IRI = request.args['ontospecies_IRI']
-        oc_inputs = get_ontocompchem_data(ontocompchem_IRI, ontospecies_IRI)
-        os_inputs = get_ontospecies_data(ontospecies_IRI)
-        inputs = {**oc_inputs,**os_inputs}
-
-        if 'temperature' in request.args:
-            inputs['temperature'] = request.args['temperature']
-        if 'pressure' in request.args:
-            inputs['pressure'] = request.args['pressure']
-        print(inputs)
-
-        try:
-            # Run the model
-            ThermoData = runThermoCalculator(inputs)
-            print(ThermoData)
-            return jsonify({"result": ThermoData})
-        except Exception as ex:
-            print(ex)
-            return jsonify({"status": '500', 'errormsg': 'Invalid request'})
+        # Register Blueprints
+        app.register_blueprint(home.home_bp)
+        app.register_blueprint(calculator.calculator_bp)
 
     return app
