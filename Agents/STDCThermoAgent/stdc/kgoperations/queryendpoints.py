66e3046164fffd1e3621e658bec3e0ea7305b653
dln22 <danieln@cmclinnovations.com>
2021-08-26 16:53:41 +0100

    develop: Added kgoperations module dealing with the KG.

---
+++ Agents/STDCThermoAgent/stdc/kgoperations/queryendpoints.py
@@ -0,0 +1,4 @@
+SPARQL_ENDPOINTS = {
+    'ontocompchem': 'http://theworldavatar.com/blazegraph/namespace/ontocompchem/sparql',
+    'ontospecies': 'http://theworldavatar.com/blazegraph/namespace/ontospecies/sparql'
+}
---
+++ Agents/STDCThermoAgent/stdc/kgoperations/querykg.py
@@ -0,0 +1,12 @@
+from stdc.kgoperations.javagateway import jpsBaseLibGW
+import json
+
+
+jpsBaseLib_view = jpsBaseLibGW.createModuleView()
+jpsBaseLibGW.importPackages(jpsBaseLib_view,"uk.ac.cam.cares.jps.base.query.*")
+
+def querykg(sparqlEndPoint=None, queryStr=None):
+
+    StoreClient = jpsBaseLib_view.RemoteStoreClient(sparqlEndPoint)
+    response = json.loads(str(StoreClient.executeQuery(queryStr)))
+    return response
