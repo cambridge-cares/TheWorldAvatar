5d1c62044f343b35be8c96b14814e71e19087ef0
Wanni Xie <wx243@cam.ac.uk>
2021-08-26 17:43:55 +0100

    99-UK-digital-twin-powsys: added new query function for visualising the grid branches.

--- web/UKDigitalTwin/test_interactive_map/queryStrings.py
+++ web/UKDigitalTwin/test_interactive_map/queryStrings.py
@@ -252,168 +252,6 @@ def queryUKElectricityConsumptionAndAssociatedGEOInfo(electricity_consumption_en
   print(No_geoInfoAreas)
   return ret_array 
 
-# def queryGridModeltForVisualisation_Bus_alt(topoEndpoint, busModelEndpoint):
-    
-#   queryVar_1 = ["?EquipmentConnection_EBus", "?numericalValue_x", "?numericalValue_y"] 
-      
-#   queryVar_2 = ["?Bus_num", "?Bus_type", "?para_Gs", "?para_Bs", "?para_area", "?para_basekV", \
-#                 "?para_zone", "?para_Vmax", "?para_Vmin", "?input_Pd", "?input_Gd", "?input_Vm", "?input_Va"] 
-      
-#   selectClause_1 = " ".join(queryVar_1)
-#   selectClause_2 = " ".join(queryVar_2)
-  
-#   queryBusModelGPS = """
-#     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-#     PREFIX ontopowsys_PowSysFunction: <http://www.theworldavatar.com/ontology/ontopowsys/PowSysFunction.owl#>
-#     PREFIX ontocape_upper_level_system: <http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#>
-#     PREFIX ontoecape_technical_system: <http://www.theworldavatar.com/ontology/ontocape/upper_level/technical_system.owl#>
-#     PREFIX ontopowsys_PowSysRealization: <http://www.theworldavatar.com/ontology/ontopowsys/PowSysRealization.owl#>
-#     PREFIX mathematical_model: <http://www.theworldavatar.com/ontology/ontocape/model/mathematical_model.owl#>
-#     PREFIX ontopowsys_PowerSystemModel: <http://www.theworldavatar.com/ontology/ontopowsys/model/PowerSystemModel.owl#>
-#     PREFIX space_and_time_extended: <http://www.theworldavatar.com/ontology/ontocape/supporting_concepts/space_and_time/space_and_time_extended.owl#>
-#     SELECT DISTINCT %s
-#     WHERE
-#     {
-#     ?EquipmentConnection_EBus rdf:type ontopowsys_PowSysFunction:PowerEquipmentConnection .   
-#     ?EquipmentConnection_EBus space_and_time_extended:hasGISCoordinateSystem ?CoordinateSystem_Bus .    
-#     ?CoordinateSystem_Bus  space_and_time_extended:hasProjectedCoordinate_x ?x_coordinate_Bus .
-#     ?CoordinateSystem_Bus  space_and_time_extended:hasProjectedCoordinate_y ?y_coordinate_Bus .
-#     ?x_coordinate_Bus  ontocape_upper_level_system:hasValue ?GPS_x_coordinate_Bus .
-#     ?y_coordinate_Bus  ontocape_upper_level_system:hasValue ?GPS_y_coordinate_Bus . 
-#     ?GPS_x_coordinate_Bus  ontocape_upper_level_system:numericalValue %s .
-#     ?GPS_y_coordinate_Bus  ontocape_upper_level_system:numericalValue %s .
- 
-#     }
-#     """ % (selectClause_1, queryVar_1[1], queryVar_1[2])
-   
-    
-#   queryBusModel = """
-#     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-#     PREFIX ontopowsys_PowSysFunction: <http://www.theworldavatar.com/ontology/ontopowsys/PowSysFunction.owl#>
-#     PREFIX ontocape_upper_level_system: <http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#>
-#     PREFIX ontoecape_technical_system: <http://www.theworldavatar.com/ontology/ontocape/upper_level/technical_system.owl#>
-#     PREFIX ontopowsys_PowSysRealization: <http://www.theworldavatar.com/ontology/ontopowsys/PowSysRealization.owl#>
-#     PREFIX mathematical_model: <http://www.theworldavatar.com/ontology/ontocape/model/mathematical_model.owl#>
-#     PREFIX ontopowsys_PowerSystemModel: <http://www.theworldavatar.com/ontology/ontopowsys/model/PowerSystemModel.owl#>
-#     PREFIX space_and_time_extended: <http://www.theworldavatar.com/ontology/ontocape/supporting_concepts/space_and_time/space_and_time_extended.owl#>
-#     SELECT DISTINCT %s
-#     WHERE
-#     {
-#     ?Model_EBus a mathematical_model:Submodel . 
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?Bus_Number . 
-#     ?Bus_Number rdf:type ontopowsys_PowerSystemModel:BusNumber . 
-#     ?Bus_Number rdf:type mathematical_model:Parameter . 
-#     ?Bus_Number ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?Type . 
-#     ?Type rdf:type ontopowsys_PowerSystemModel:BusType . 
-#     ?Type rdf:type mathematical_model:Parameter . 
-#     ?Type ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?Gsvar . 
-#     ?Gsvar rdf:type ontopowsys_PowerSystemModel:Gs . 
-#     ?Gsvar rdf:type mathematical_model:Parameter . 
-#     ?Gsvar ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?Bsvar . 
-#     ?Bsvar rdf:type ontopowsys_PowerSystemModel:Bs . 
-#     ?Bsvar rdf:type mathematical_model:Parameter . 
-#     ?Bsvar ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?areavar . 
-#     ?areavar rdf:type ontopowsys_PowerSystemModel:Area . 
-#     ?areavar rdf:type mathematical_model:Parameter . 
-#     ?areavar ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?BKV . 
-#     ?BKV rdf:type ontopowsys_PowerSystemModel:baseKV . 
-#     ?BKV rdf:type mathematical_model:Parameter . 
-#     ?BKV ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?zvar . 
-#     ?zvar rdf:type ontopowsys_PowerSystemModel:Zone . 
-#     ?zvar rdf:type mathematical_model:Parameter . 
-#     ?zvar ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?vmaxvar . 
-#     ?vmaxvar rdf:type ontopowsys_PowerSystemModel:VmMax . 
-#     ?vmaxvar rdf:type mathematical_model:Parameter . 
-#     ?vmaxvar ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?vminvar . 
-#     ?vminvar rdf:type ontopowsys_PowerSystemModel:VmMin . 
-#     ?vminvar rdf:type mathematical_model:Parameter . 
-#     ?vminvar ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-     
-#     ?Model_EBus mathematical_model:hasModelVariable ?Pd . 
-#     ?Pd rdf:type ontopowsys_PowerSystemModel:PdBus . 
-#     ?Pd rdf:type mathematical_model:InputVariable . 
-#     ?Pd ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?Gd . 
-#     ?Gd rdf:type ontopowsys_PowerSystemModel:GdBus . 
-#     ?Gd rdf:type mathematical_model:InputVariable . 
-#     ?Gd ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?VM . 
-#     ?VM rdf:type ontopowsys_PowerSystemModel:Vm . 
-#     ?VM rdf:type mathematical_model:InputVariable . 
-#     ?VM ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     ?Model_EBus mathematical_model:hasModelVariable ?VA . 
-#     ?VA rdf:type ontopowsys_PowerSystemModel:Va . 
-#     ?VA rdf:type mathematical_model:InputVariable . 
-#     ?VA ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
-    
-#     }
-#     """% (selectClause_2, queryVar_2[0], queryVar_2[1], queryVar_2[2], queryVar_2[3], queryVar_2[4], queryVar_2[5], queryVar_2[6], queryVar_2[7], \
-#         queryVar_2[8], queryVar_2[9], queryVar_2[10], queryVar_2[11], queryVar_2[12])
-  
-#   # print(queryBusModelInput)
-#   # query the Bus model parameters and its GPS location
-#   start = time.time()
-#   print('Federated Querying the Bus Model Parameters...')
-#   res_para = json.loads(performFederatedQuery(queryBusModelParameter_federated, topoEndpoint, busModelEndpoint))
-#   end = time.time()  
-#   print('Finished querying the Bus patameter in ',np.round(end-start,2),' seconds') 
-#   for r in res_para:
-#       for key in r.keys():
-#           r[key] = (r[key].split('\"^^')[0]).replace('\"','')
-           
-#   qres_para = [[ int(r['Bus_num']), float(r['numericalValue_x']), float(r['numericalValue_y']), int(r['Bus_type']), float(r['para_Gs']), float(r['para_Bs']), int(r['para_area']), \
-#                 float(r['para_basekV']), int(r['para_zone']), float(r['para_Vmax']), float(r['para_Vmin'])] for r in res_para ]
-
-#   for q in qres_para: 
-#       if q[3] == 1:
-#          q[3] = "PQ Bus"
-#       elif q[3] == 2:  
-#          q[3] = "PV Bus"
-#       elif q[3] == 3:  
-#          q[3] = "Slack Bus" 
-  
-#   # query the Bus model input variables
-#   start = time.time()
-#   print('Querying the Bus Model Input Variables...')
-#   # the performQuery interface needs the label of the endpoint as the function argument which should be registered in advanced in the Blazegraph look-up table
-#   res_input = json.loads(performQuery("ukpowergridmodel", queryBusModelInput)) 
-#   end = time.time()  
-#   print('Finished querying the Bus patameter in ',np.round(end-start,2),' seconds') 
-  
-#   for r in res_input:
-#      for key in r.keys():
-#          r[key] = (r[key].split('\"^^')[0]).replace('\"','')
-#   qres_input = [[ int(r['Bus_num']), float(r['input_Pd']), float(r['input_Gd']), float(r['input_Vm']), float(r['input_Va'])] for r in res_input ]
-         
-#   if len(res_input) == len(res_para) and len(res_input) != 0:
-#       return qres_para, qres_input
-#   elif len(res_para) == 0:
-#       print("The federated query of the bus model parameter is failed.")
-#       return None
-#   elif len(res_input) == 0:
-#       print("The query of the bus model varibles is failed.")
-#       return None
-
 """This function is used for query the bus model parameters and input variables from the grid model endpoint and query the location of the bus from the grid topology endpoint"""
 # topoEndpoint = "https://como.ceb.cam.ac.uk/rdf4j-server/repositories/UKPowerGridTopology"
 # busModelEndpoint = "https://como.ceb.cam.ac.uk/rdf4j-server/repositories/UKPowerGridModel"
@@ -589,6 +427,159 @@ def queryGridModeltForVisualisation_Bus(topoEndpoint, busModelEndpoint):
       print("The query of the bus model varibles is failed.")
       return None
 
+    
+"""This function is used for query the branch model parameters and input variables, also there connectivity relationship with buses"""
+# branchModelEndpoint = "https://como.ceb.cam.ac.uk/rdf4j-server/repositories/UKPowerGridModel", lable: "ukpowergridmodel"
+def queryGridModeltForVisualisation_Branch(branchModelEndpoint):
+    
+  queryVar = ["?ELine", "?From_Bus", "?To_Bus", "?para_R", "?para_X", "?para_B", "?para_RateA", "?para_RateB", "?para_RateC", "?para_RatioCoefficient", \
+                "?para_Angle", "?para_Status", "?para_AngleMax", "?para_AngleMin"] 
+      
+  selectClause = " ".join(queryVar)
+  
+  queryBuranchModelParameter = """
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX ontopowsys_PowSysFunction: <http://www.theworldavatar.com/ontology/ontopowsys/PowSysFunction.owl#>
+    PREFIX ontocape_upper_level_system: <http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#>
+    PREFIX ontoecape_technical_system: <http://www.theworldavatar.com/ontology/ontocape/upper_level/technical_system.owl#>
+    PREFIX ontopowsys_PowSysRealization: <http://www.theworldavatar.com/ontology/ontopowsys/PowSysRealization.owl#>
+    PREFIX mathematical_model: <http://www.theworldavatar.com/ontology/ontocape/model/mathematical_model.owl#>
+    PREFIX ontopowsys_PowerSystemModel: <http://www.theworldavatar.com/ontology/ontopowsys/model/PowerSystemModel.owl#>
+    PREFIX space_and_time_extended: <http://www.theworldavatar.com/ontology/ontocape/supporting_concepts/space_and_time/space_and_time_extended.owl#>
+    SELECT DISTINCT %s
+    WHERE
+    {
+    %s ontocape_upper_level_system:isModeledBy ?Model_Eline . 
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?FromBusNumber . 
+    ?FromBusNumber rdf:type ontopowsys_PowerSystemModel:BusFrom . 
+    ?FromBusNumber rdf:type mathematical_model:Parameter . 
+    ?FromBusNumber ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?ToBusNumber . 
+    ?ToBusNumber rdf:type ontopowsys_PowerSystemModel:BusTo . 
+    ?ToBusNumber rdf:type mathematical_model:Parameter . 
+    ?ToBusNumber ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?res . 
+    ?res rdf:type ontopowsys_PowerSystemModel:R . 
+    ?res rdf:type mathematical_model:Parameter . 
+    ?res ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?rea . 
+    ?rea rdf:type ontopowsys_PowerSystemModel:X . 
+    ?rea rdf:type mathematical_model:Parameter . 
+    ?rea ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?sus . 
+    ?sus rdf:type ontopowsys_PowerSystemModel:B . 
+    ?sus rdf:type mathematical_model:Parameter . 
+    ?sus ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?ratea . 
+    ?ratea rdf:type ontopowsys_PowerSystemModel:RateA . 
+    ?ratea rdf:type mathematical_model:Parameter . 
+    ?ratea ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?rateb . 
+    ?rateb rdf:type ontopowsys_PowerSystemModel:RateB . 
+    ?rateb rdf:type mathematical_model:Parameter . 
+    ?rateb ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?ratec . 
+    ?ratec rdf:type ontopowsys_PowerSystemModel:RateC . 
+    ?ratec rdf:type mathematical_model:Parameter . 
+    ?ratec ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?ratio . 
+    ?ratio rdf:type ontopowsys_PowerSystemModel:RatioCoefficient . 
+    ?ratio rdf:type mathematical_model:Parameter . 
+    ?ratio ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+
+    ?Model_Eline mathematical_model:hasModelVariable ?ang . 
+    ?ang rdf:type ontopowsys_PowerSystemModel:Angle . 
+    ?ang rdf:type mathematical_model:Parameter . 
+    ?ang ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?stat . 
+    ?stat rdf:type ontopowsys_PowerSystemModel:BranchStatus . 
+    ?stat rdf:type mathematical_model:Parameter . 
+    ?stat ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?angmin . 
+    ?angmin rdf:type ontopowsys_PowerSystemModel:AngleMin . 
+    ?angmin rdf:type mathematical_model:Parameter . 
+    ?angmin ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?angmax . 
+    ?angmax rdf:type ontopowsys_PowerSystemModel:AngleMax . 
+    ?angmax rdf:type mathematical_model:Parameter . 
+    ?angmax ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue %s .
+    
+    }
+    """ % (selectClause, queryVar[0], queryVar[1], queryVar[2], queryVar[3], queryVar[4], queryVar[5], queryVar[6], queryVar[7], \
+        queryVar[8], queryVar[9], queryVar[10], queryVar[11], queryVar[12], queryVar[13])
+  
+  # print(queryBuranchModelParameter)
+  # query the Bus model parameters and its GPS location
+  
+  test = """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX ontopowsys_PowSysFunction: <http://www.theworldavatar.com/ontology/ontopowsys/PowSysFunction.owl#>
+    PREFIX ontocape_upper_level_system: <http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#>
+    PREFIX ontoecape_technical_system: <http://www.theworldavatar.com/ontology/ontocape/upper_level/technical_system.owl#>
+    PREFIX ontopowsys_PowSysRealization: <http://www.theworldavatar.com/ontology/ontopowsys/PowSysRealization.owl#>
+    PREFIX mathematical_model: <http://www.theworldavatar.com/ontology/ontocape/model/mathematical_model.owl#>
+    PREFIX ontopowsys_PowerSystemModel: <http://www.theworldavatar.com/ontology/ontopowsys/model/PowerSystemModel.owl#>
+    PREFIX space_and_time_extended: <http://www.theworldavatar.com/ontology/ontocape/supporting_concepts/space_and_time/space_and_time_extended.owl#>
+    SELECT DISTINCT ?ELine ?From_Bus ?To_Bus ?para_R ?para_X ?para_B
+    WHERE
+    {
+    ?ELine ontocape_upper_level_system:isModeledBy ?Model_Eline . 
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?FromBusNumber . 
+    ?FromBusNumber rdf:type ontopowsys_PowerSystemModel:BusFrom . 
+    ?FromBusNumber rdf:type mathematical_model:Parameter . 
+    ?FromBusNumber ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue ?From_Bus .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?ToBusNumber . 
+    ?ToBusNumber rdf:type ontopowsys_PowerSystemModel:BusTo . 
+    ?ToBusNumber rdf:type mathematical_model:Parameter . 
+    ?ToBusNumber ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue ?To_Bus .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?res . 
+    ?res rdf:type ontopowsys_PowerSystemModel:R . 
+    ?res rdf:type mathematical_model:Parameter . 
+    ?res ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue ?para_R .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?rea . 
+    ?rea rdf:type ontopowsys_PowerSystemModel:X . 
+    ?rea rdf:type mathematical_model:Parameter . 
+    ?rea ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue ?para_X .
+    
+    ?Model_Eline mathematical_model:hasModelVariable ?sus . 
+    ?sus rdf:type ontopowsys_PowerSystemModel:B . 
+    ?sus rdf:type mathematical_model:Parameter . 
+    ?sus ontocape_upper_level_system:hasValue/ontocape_upper_level_system:numericalValue ?para_B .
+    
+    
+    }
+    """
+  start = time.time()
+  print('Querying the Branch Model Parameters (model input)...')
+  # res_para = json.loads(performQuery(branchModelEndpoint, queryBuranchModelParameter))
+  res_para = json.loads(performQuery(branchModelEndpoint, test))
+  end = time.time()  
+  print('Finished querying in ',np.round(end-start,2),' seconds')
+  
+  print(res_para)
+  for r in res_para:
+      for key in r.keys():
+          r[key] = (r[key].split('\"^^')[0]).replace('\"','')
+           
+  qres_para = [[ int(r['Bus_num']), float(r['numericalValue_x']), float(r['numericalValue_y']), int(r['Bus_type']), float(r['para_Gs']), float(r['para_Bs']), int(r['para_area']), \
+                float(r['para_basekV']), int(r['para_zone']), float(r['para_Vmax']), float(r['para_Vmin'])] for r in res_para ]
+  return qres_para
+
 def queryUKSDGIndicatorForVisualisation():
 
   query_sdg = """
@@ -614,7 +605,8 @@ if __name__ == '__main__':
     busModelEndpoint = "https://como.ceb.cam.ac.uk/rdf4j-server/repositories/UKPowerGridModel"
     # res = queryUKElectricityConsumptionAndAssociatedGEOInfo(electricity_consumption_RDF4j_Endpoint, ONS_json, False)
     
-    res = queryGridModeltForVisualisation_Bus(topoEndpoint, busModelEndpoint)
+    # res = queryGridModeltForVisualisation_Bus(topoEndpoint, busModelEndpoint)
+    res  = queryGridModeltForVisualisation_Branch("ukpowergridmodel")
     # for r in res:
     #     print(r)
     print(res)
