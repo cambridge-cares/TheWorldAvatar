fb396d83209835f4f372e637985f695f99b80ec3
Angiras Menon <angirasmenon@gmail.com>
2021-09-02 14:39:52 +0100

    dev-scanparse: am2145. Updated prefixes and ABox writer for OntoMOPs.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontomops/prefixes.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontomops/prefixes.py
@@ -1,8 +1,15 @@
 onto_spec = 'http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl'
-onto_kb = 'http://www.theworldavatar.com/kb/ontospecies/'
+onto_mops = 'http://www.theworldavatar.com/ontology/ontomops/OntoMOPS.owl'
+
+
+rdf_pref = 'http://www.w3.org/2000/01/rdf-schema'
 gain_pref = 'http://purl.org/gc/'
 kin_pref = 'http://www.theworldavatar.com/ontology/ontokin/OntoKin.owl'
 table_pref = 'http://www.daml.org/2003/01/periodictable/PeriodicTable.owl'
+spec_pref = 'http://www.theworldavatar.com/kb/ontospecies/'
 unit_pref = 'http://data.nasa.gov/qudt/owl/'
+mops_pref = 'http://www.theworldavatar.com/kb/ontomops/'
+uom_pref = 'http://www.ontology-of-units-of-measure.org/resource/om-2/'
+
 species_entry_prefix= 'Species_'
 omops_entry_prefix= 'Mops_'
