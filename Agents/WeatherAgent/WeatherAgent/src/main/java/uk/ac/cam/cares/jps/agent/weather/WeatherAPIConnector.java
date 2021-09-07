668f05f1d9ca7688ba7ff6b4f981feaa19bff8c7
Kok Foong Lee <kflee@cmclinnovations.com>
2021-09-07 16:40:22 +0100

    135-dev-weather-input: core functions to create, query, and update weather data

---
+++ Agents/WeatherAgent/WeatherAgent/src/main/java/uk/ac/cam/cares/jps/agent/weather/WeatherAPIConnector.java
@@ -0,0 +1,67 @@
+package uk.ac.cam.cares.jps.agent.weather;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.eclipse.rdf4j.sparqlbuilder.rdf.Iri;
+
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+
+class WeatherAPIConnector {
+	
+	/**
+	 * obtain current weather data given the set of coordinates (EPSG:4326)
+	 * @param xy
+	 * @return
+	 */
+	static Map<Iri,Double> getWeatherDataFromOpenWeather(double lat, double lon) {
+    	Config.initProperties();
+		URIBuilder builder = new URIBuilder().setScheme("http").setHost("api.openweathermap.org")
+                .setPath("/data/2.5/weather");
+		builder.setParameter("lat", String.valueOf(lat));
+		builder.setParameter("lon", String.valueOf(lon));
+		builder.setParameter("units", "metric");
+		builder.setParameter("appid", Config.apikey);
+		
+		try {
+            HttpGet request = new HttpGet(builder.build());
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            CloseableHttpResponse response = httpclient.execute(request);
+            JSONObject apiresult = new JSONObject(EntityUtils.toString(response.getEntity()));
+            
+            // units are set according to mcwind file for Episode
+            double precipitation = 0.0; // in mm/h
+			if (apiresult.has("rain")) {
+				JSONObject rain = apiresult.getJSONObject("rain");
+				try {
+					precipitation = rain.getDouble("1h");
+				} catch (Exception e) {
+					precipitation = rain.getDouble("3h")/3.0;
+				}
+			}
+
+			// collect result into a Map			
+			Map<Iri,Double> resultMap = new HashMap<>();
+			resultMap.put(WeatherQueryClient.OutsideAirPrecipitation, precipitation);
+			resultMap.put(WeatherQueryClient.OutsideAirPressure, apiresult.getJSONObject("main").getDouble("pressure"));
+			resultMap.put(WeatherQueryClient.OutsideAirCloudCover,apiresult.getJSONObject("clouds").getDouble("all")/100);
+			resultMap.put(WeatherQueryClient.OutsideWindSpeed, apiresult.getJSONObject("wind").getDouble("speed"));
+			if (apiresult.getJSONObject("wind").has("deg")) {
+				resultMap.put(WeatherQueryClient.OutsideWindDirection, apiresult.getJSONObject("wind").getDouble("deg"));
+			}
+			resultMap.put(WeatherQueryClient.OutsideAirRelativeHumidity, apiresult.getJSONObject("main").getDouble("humidity"));
+			resultMap.put(WeatherQueryClient.OutsideAirTemperature, apiresult.getJSONObject("main").getDouble("temp"));
+			
+            return resultMap;
+        } catch (Exception e) {
+            throw new JPSRuntimeException(e.getMessage(), e);
+        }
+	}
+}
---
+++ Agents/WeatherAgent/WeatherAgent/src/main/java/uk/ac/cam/cares/jps/agent/weather/WeatherQueryClient.java
@@ -0,0 +1,343 @@
+package uk.ac.cam.cares.jps.agent.weather;
+
+import static org.eclipse.rdf4j.sparqlbuilder.rdf.Rdf.iri;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.eclipse.rdf4j.sparqlbuilder.core.Prefix;
+import org.eclipse.rdf4j.sparqlbuilder.core.SparqlBuilder;
+import org.eclipse.rdf4j.sparqlbuilder.core.Variable;
+import org.eclipse.rdf4j.sparqlbuilder.core.query.ModifyQuery;
+import org.eclipse.rdf4j.sparqlbuilder.core.query.Queries;
+import org.eclipse.rdf4j.sparqlbuilder.core.query.SelectQuery;
+import org.eclipse.rdf4j.sparqlbuilder.graphpattern.GraphPattern;
+import org.eclipse.rdf4j.sparqlbuilder.graphpattern.GraphPatterns;
+import org.eclipse.rdf4j.sparqlbuilder.graphpattern.TriplePattern;
+import org.eclipse.rdf4j.sparqlbuilder.rdf.Iri;
+import org.eclipse.rdf4j.sparqlbuilder.rdf.Rdf;
+import org.eclipse.rdf4j.sparqlbuilder.rdf.RdfLiteral.StringLiteral;
+
+import uk.ac.cam.cares.jps.base.derivation.DerivationClient;
+import uk.ac.cam.cares.jps.base.interfaces.StoreClientInterface;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeries;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesSparql;
+
+class WeatherQueryClient {
+	// prefix
+	private static Prefix p_geoliteral = SparqlBuilder.prefix(iri("http://www.bigdata.com/rdf/geospatial/literals/v1#"));
+	private static String ontostation = "https://github.com/cambridge-cares/TheWorldAvatar/blob/develop/JPS_Ontology/ontology/ontostation/OntoStation.owl#";
+    private static Prefix p_station = SparqlBuilder.prefix("station",iri(ontostation));
+    private static Prefix p_ontosensor = SparqlBuilder.prefix("sensor",iri("http://www.theworldavatar.com/ontology/ontosensor/OntoSensor.owl#"));
+    private static Prefix p_system = SparqlBuilder.prefix("system",iri("http://www.theworldavatar.com/ontology/ontocape/upper_level/system.owl#"));
+    private static Prefix p_derived_SI_unit = SparqlBuilder.prefix("derived_SI_unit",iri("http://www.theworldavatar.com/ontology/ontocape/supporting_concepts/SI_unit/derived_SI_units.owl#"));
+    private static Prefix p_time = SparqlBuilder.prefix("time", iri("http://www.w3.org/2006/time#"));
+    
+    // classes
+    private static Iri WeatherStation = p_station.iri("WeatherStation");
+    private static Iri latlon = p_geoliteral.iri("lat-lon");
+    private static Iri MeasuringInstrument = iri("http://www.theworldavatar.com/ontology/ontocape/chemical_process_system/CPS_realization/process_control_system.owl#MeasuringInstrument");
+    private static Iri ScalarValue = p_system.iri("ScalarValue");
+    static Iri OutsideAirCloudCover = p_ontosensor.iri("OutsideAirCloudCover");
+    static Iri OutsideAirPrecipitation = p_ontosensor.iri("OutsideAirPrecipitation");
+    static Iri OutsideAirPressure = p_ontosensor.iri("OutsideAirPressure");
+    static Iri OutsideAirTemperature = p_ontosensor.iri("OutsideAirTemperature");
+    static Iri OutsideAirRelativeHumidity = p_ontosensor.iri("OutsideAirRelativeHumidity");
+    static Iri OutsideWindSpeed = p_ontosensor.iri("OutsideWindSpeed");
+    static Iri OutsideWindDirection = p_ontosensor.iri("OutsideWindDirection");
+    
+    // properties
+    private static Iri hasCoordinates = p_station.iri("hasCoordinates");
+    private static Iri hasSubsystem = p_system.iri("hasSubsytem");
+    private static Iri observes = p_ontosensor.iri("observes");
+    private static Iri hasValue = p_system.iri("hasValue");
+    private static Iri hasUnitOfMeasure = p_system.iri("hasUnitOfMeasure");
+    private static Iri hasTime = p_time.iri("hasTime");
+	private static Iri numericPosition = p_time.iri("numericPosition");
+	private static Iri inTimePosition = p_time.iri("inTimePosition");
+    
+    // IRI of units used
+    private static Iri unit_mm = p_derived_SI_unit.iri("mm");
+    private static Iri unit_degree = p_derived_SI_unit.iri("degree");
+    private static Iri unit_mbar = p_derived_SI_unit.iri("mBar");
+    private static Iri unit_celcius = p_derived_SI_unit.iri("Celsius");
+    private static Iri unit_ms = p_derived_SI_unit.iri("m_per_s");
+    private static Iri unit_fraction = p_derived_SI_unit.iri("fraction"); // made up, does not exist in ontology
+    private static Iri unit_percentage = p_derived_SI_unit.iri("percentage"); // made up, does not exist in ontology
+    
+    // measured properties
+    private static List<Iri> weatherClasses = Arrays.asList(OutsideAirCloudCover, OutsideAirPrecipitation,
+    		OutsideAirPressure, OutsideAirTemperature, OutsideAirRelativeHumidity, OutsideWindSpeed, OutsideWindDirection);
+    
+    // fixed units for each measured property
+    @SuppressWarnings("serial")
+	static Map<Iri, Iri> unitMap = new HashMap<Iri , Iri>() {{
+    	put(OutsideAirCloudCover, unit_percentage);
+    	put(OutsideAirPrecipitation, unit_mm);
+    	put(OutsideAirPressure, unit_mbar);
+    	put(OutsideAirTemperature, unit_celcius);
+    	put(OutsideAirRelativeHumidity, unit_fraction);
+    	put(OutsideWindSpeed, unit_ms);
+    	put(OutsideWindDirection, unit_degree);
+    }};
+    
+    StoreClientInterface storeClient;
+    TimeSeriesClient<Long> tsClient;
+	
+    WeatherQueryClient(StoreClientInterface storeClient, TimeSeriesClient<Long> tsClient) {
+    	this.storeClient = storeClient;
+    	this.tsClient = tsClient;
+    }
+    
+    /**
+     * create a new station at the given coordinates, must be EPSG:4326
+     * @param x
+     * @param y
+     * @return
+     */
+    String createStation(double x, double y) {
+    	ModifyQuery modify = Queries.MODIFY();
+    	String station_name = "weatherstation_" + UUID.randomUUID();
+    	Iri station = p_station.iri(station_name);
+    	
+    	// coordinates
+    	StringLiteral coordinatesLiteral = Rdf.literalOfType(String.valueOf(y)+"#"+String.valueOf(y), latlon);
+    	modify.insert(station.isA(WeatherStation).andHas(hasCoordinates,coordinatesLiteral));
+    	
+    	List<String> datalist_for_timeseries = new ArrayList<>();
+    	List<Class<?>> classlist_for_timeseries = new ArrayList<>();
+    	
+    	// add 1 sensor per property
+    	for (Iri weatherClass : weatherClasses) {
+    		Iri sensor = p_station.iri("sensor_" + UUID.randomUUID());
+    		Iri data = p_station.iri("data_" + UUID.randomUUID());
+    		String datavalue = ontostation + "datavalue_" + UUID.randomUUID();
+    		Iri datavalue_iri = iri(datavalue);
+    		
+    		// to create time series table later
+    		datalist_for_timeseries.add(datavalue);
+    		classlist_for_timeseries.add(Double.class);
+    		
+    		// triples to insert
+    		modify.insert(station.has(hasSubsystem,sensor));
+    		modify.insert(sensor.isA(MeasuringInstrument).andHas(observes,data));
+    		modify.insert(data.isA(weatherClass).andHas(hasValue,datavalue_iri));
+    		modify.insert(datavalue_iri.isA(ScalarValue).andHas(hasUnitOfMeasure,unitMap.get(weatherClass)));
+    	}
+    	
+    	modify.prefix(p_geoliteral,p_station,p_system,p_ontosensor,p_derived_SI_unit);
+    	
+    	// insert triples in the triple-store
+    	storeClient.executeUpdate(modify.getQueryString());
+    	
+    	// then create a table for this weather station
+    	tsClient.initTimeSeries(datalist_for_timeseries, classlist_for_timeseries, null);
+    	
+    	// add timestamp to the station to use this as an input to a derivation
+    	DerivationClient dClient = new DerivationClient(storeClient);
+    	dClient.addTimeInstance(ontostation+station_name);
+    	
+    	// populate with current weather data
+    	updateStation(ontostation+station_name);
+    	
+    	return ontostation+station_name;
+    }
+    
+    /**
+     * deletes the given station, including its associated time series
+     * @param station_iri
+     */
+    void deleteStation(String station_iri) {
+    	Iri station = iri(station_iri);
+    	
+    	SelectQuery query = Queries.SELECT();
+    	
+    	Variable coordinates = query.var();
+    	Variable sensor = query.var();
+    	Variable data = query.var();
+    	Variable weatherClass = query.var();
+    	Variable datavalue = query.var();
+    	Variable units = query.var();
+    	
+    	TriplePattern[] queryPattern = {station.isA(WeatherStation).andHas(hasCoordinates,coordinates),
+    			station.has(hasSubsystem,sensor),
+    			sensor.isA(MeasuringInstrument).andHas(observes,data),
+    			data.isA(weatherClass).andHas(hasValue,datavalue),
+    			datavalue.isA(ScalarValue).andHas(hasUnitOfMeasure,units)};
+    	
+    	// first we record the data value IRIs, required to delete in RDB
+    	query.prefix(p_station,p_system,p_ontosensor).where(queryPattern).select(datavalue);
+    	
+    	// this is the list of iris with a corresponding time series table
+		@SuppressWarnings("unchecked")
+		// substring(1) to get rid of "?"
+		List<String> datalist = storeClient.executeQuery(query.getQueryString()).toList().stream()
+				.map(datavalueiri -> ((HashMap<String,String>) datavalueiri).get(datavalue.getQueryString().substring(1))).collect(Collectors.toList());
+    	
+		// this deletes the station instance, but not the time series triples
+    	ModifyQuery modify = Queries.MODIFY();
+    	modify.prefix(p_station,p_system,p_ontosensor).delete(queryPattern).where(queryPattern);
+    	storeClient.executeUpdate(modify.getQueryString());
+    	
+    	// use time series client to delete time series related data
+    	// get time series IRI for this data set, any data IRI will give the same time series
+    	TimeSeriesSparql tsSparql = new TimeSeriesSparql(storeClient);
+    	String timeseriesIRI = tsSparql.getTimeSeries(datalist.get(0));
+
+    	// then delete all time series data in one go
+    	tsClient.deleteTimeSeries(timeseriesIRI);
+    }
+    
+    /**
+     * updates weather station with the latest data if last updated more than 1 hour ago
+     * @param station_iri
+     */
+    void updateStation(String station_iri) {
+    	// first check the last updated time stamp (stored using w3c standard)
+    	// construct query
+    	SelectQuery query = Queries.SELECT();
+    	Variable timestamp = query.var();
+    	Iri[] time_predicates = {hasTime,inTimePosition,numericPosition};
+    	GraphPattern queryPattern = getQueryGraphPattern(query,time_predicates,iri(station_iri),timestamp);
+    	query.select(timestamp).where(queryPattern).prefix(p_time);
+    	
+    	// send query to triple-store
+    	long lastupdate = storeClient.executeQuery(query.getQueryString()).getJSONObject(0).getLong(timestamp.getQueryString().substring(1));
+    	
+    	// compare with current time, only make API call if data is 1 hour old
+    	long currenttime = Instant.now().getEpochSecond();
+    	if ((currenttime-lastupdate) > 3600) {
+    		// now get the latlon coordinates of this station
+    		
+    		// build coordinate query
+    		SelectQuery query2 = Queries.SELECT();
+    		Variable coord = query2.var();
+    		query2.select(coord).where(iri(station_iri).has(hasCoordinates,coord)).prefix(p_station);
+    		
+    		// submit coordinate query
+    		String coordinates = storeClient.executeQuery(query2.getQueryString()).getJSONObject(0).getString(coord.getQueryString().substring(1));
+    		String[] latlon = coordinates.split("#");
+    		
+    		// the key for this map is the weather class, value is corresponding value
+    		Map<Iri,Double> newWeatherData = WeatherAPIConnector.getWeatherDataFromOpenWeather(Double.parseDouble(latlon[0]), Double.parseDouble(latlon[1]));
+    		Iterator<Iri> weatherClasses = newWeatherData.keySet().iterator();
+    		newWeatherData.keySet().stream();
+    	    
+    		// to construct time series object
+    		List<String> datavalue_list = new ArrayList<>();
+    		List<List<?>> value_list = new ArrayList<>();
+    		
+    		// we need to find the data value for the corresponding weather class
+    		while (weatherClasses.hasNext()) {
+    			Iri weatherClass = weatherClasses.next();
+    			SelectQuery query3 = Queries.SELECT();
+    			Variable data = query3.var();
+    			Variable datavalue = query3.var();
+    			
+    			query3.select(datavalue).where(data.isA(weatherClass).andHas(hasValue,datavalue)).prefix(p_ontosensor,p_system);
+    			
+    			datavalue_list.add(storeClient.executeQuery(query3.getQueryString()).getJSONObject(0).getString(datavalue.getQueryString().substring(1)));
+    			double numericalValue = newWeatherData.get(weatherClass);
+    			value_list.add(Arrays.asList(numericalValue));
+    		}
+    		
+    		// append new values to time series table
+    		TimeSeries<Long> ts = new TimeSeries<Long>(Arrays.asList(currenttime), datavalue_list, value_list);
+    		tsClient.addTimeSeriesData(ts);
+    		
+    		// update last updated timestamp
+    		DerivationClient dClient = new DerivationClient(storeClient);
+    		dClient.updateTimestamp(station_iri);
+    	}
+    }
+
+    /**
+     * get latest weather data
+     * @param station_iri
+     */
+	TimeSeries<Long> getLatestWeatherData(String station_iri) {
+    	// first query the data IRIs
+    	SelectQuery query = Queries.SELECT();
+    	
+    	Iri station = iri(station_iri);
+    	Variable datavalue = query.var();
+    	
+    	Iri[] predicates = {hasSubsystem,observes,hasValue};
+    	GraphPattern queryPattern = getQueryGraphPattern(query,predicates,station,datavalue);
+    	
+    	query.select(datavalue).where(queryPattern).prefix(p_station,p_system,p_ontosensor);
+    	
+    	List<String> datalist = storeClient.executeQuery(query.getQueryString()).toList().stream()
+				.map(datavalueiri -> ((HashMap<String,String>) datavalueiri).get(datavalue.getQueryString().substring(1))).collect(Collectors.toList());
+    	
+    	long latestTime = tsClient.getMaxTime(datalist.get(0));
+    	TimeSeries<Long> ts = tsClient.getTimeSeriesWithinBounds(datalist, latestTime, latestTime);
+    	
+    	return ts;
+    }
+    
+	/**
+	 * returns the entire historical weather data
+	 * @param station_iri
+	 * @return
+	 */
+	TimeSeries<Long> getAllWeatherData(String station_iri) {
+		// first query the data IRIs
+    	SelectQuery query = Queries.SELECT();
+    	
+    	Iri station = iri(station_iri);
+    	Variable datavalue = query.var();
+    	
+    	Iri[] predicates = {hasSubsystem,observes,hasValue};
+    	GraphPattern queryPattern = getQueryGraphPattern(query,predicates,station,datavalue);
+    	
+    	query.select(datavalue).where(queryPattern).prefix(p_station,p_system,p_ontosensor);
+    	
+    	List<String> datalist = storeClient.executeQuery(query.getQueryString()).toList().stream()
+				.map(datavalueiri -> ((HashMap<String,String>) datavalueiri).get(datavalue.getQueryString().substring(1))).collect(Collectors.toList());
+    	
+    	TimeSeries<Long> ts = tsClient.getTimeSeries(datalist);
+    	
+    	return ts;
+	}
+	
+    /**
+     * constructs a query pattern given the list of predicates in between the given FirstNode and LastNode
+     * @param Query
+     * @param Predicates
+     * @param FirstNode
+     * @param LastNode
+     * @return
+     */
+    private GraphPattern getQueryGraphPattern (SelectQuery Query, Iri[] Predicates, Iri FirstNode, Variable LastNode) {
+        GraphPattern CombinedGP = null;
+    	
+    	Variable[] Variables = new Variable[Predicates.length];
+    	
+    	// initialise intermediate nodes
+    	for (int i=0; i < Variables.length-1; i++) {
+    		Variables[i] = Query.var();
+    	}
+    	Variables[Variables.length-1] = LastNode;
+    	
+    	// first triple
+    	GraphPattern firstTriple = FirstNode.has(Predicates[0],Variables[0]);
+    	CombinedGP = GraphPatterns.and(firstTriple);
+    	
+    	// the remaining
+    	for (int i=0; i < Variables.length-1; i++) {
+    		GraphPattern triple = Variables[i].has(Predicates[i+1],Variables[i+1]);
+    		CombinedGP.and(triple);
+    	}
+    	
+    	return CombinedGP;
+    }
+}
