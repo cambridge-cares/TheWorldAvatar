b2da11346d6c008b42f02c615c98d1c02dbea8ba
Laura Pascazio <lp521@cam.ac.uk>
2021-10-01 18:18:41 +0800

    changes of query templates

--- Agents/PESFittingAgent/pesfit/kgoperations/getkgdata.py
+++ Agents/PESFittingAgent/pesfit/kgoperations/getkgdata.py
@@ -2,7 +2,25 @@ from pesfit.kgoperations.queryendpoints import SPARQL_ENDPOINTS
 from pesfit.kgoperations.querykg import querykg
 from pesfit.kgoperations.querytemplates import ontopesscan_data_query
 
+def get_kg_data(opesIRI):
+    pesdata = get_ontopesscan_data(opesIRI) 
+    # loop on oocIRI
+    #   get_ontocompchem_data(oocIRI)
+    #   postprocess the data from json to format I want
+    #   xyz files and csv
+
 def get_ontopesscan_data(opesIRI):
+    
+    pesscan_sparqltemplate = ontopesscan_data_query(opesIRI)
+    sparqlendpoint = SPARQL_ENDPOINTS['ontopesscan']
+    data = querykg(sparqlEndPoint=sparqlendpoint, queryStr=pesscan_sparqltemplate)
+    if data:
+        data = data[0]
+        
+    return data
+
+def get_ontocompchem_data(oocIRI):
+    
     pesscan_sparqltemplate = ontopesscan_data_query(opesIRI)
     sparqlendpoint = SPARQL_ENDPOINTS['ontopesscan']
     data = querykg(sparqlEndPoint=sparqlendpoint, queryStr=pesscan_sparqltemplate)
--- Agents/PESFittingAgent/pesfit/kgoperations/querytemplates.py
+++ Agents/PESFittingAgent/pesfit/kgoperations/querytemplates.py
@@ -1,59 +1,57 @@
-from stdc.kgoperations.queryendpoints import SPARQL_ENDPOINTS
-from stdc.kgoperations.querykg import querykg
-import json
-
-def ontocompchem_data_query(ocIRI, osIRI):
+def ontocompchem_data_query(ocIRI):
     query = """PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
         PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX oc:  <http://www.theworldavatar.com/ontology/ontocompchem/ontocompchem.owl#>
-        PREFIX os:  <http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#>
         PREFIX gc:  <http://purl.org/gc/>
 
-        SELECT ?spin_mult ?frequencies ?rot_constants ?sym_number
+        SELECT ?elem ?XCoordValue ?YCoordValue ?ZCoordValue ?ScfElecValue ?ScfElecUnit
         WHERE {
-            <#ocIRI#> oc:hasUniqueSpecies <#osIRI#> ;
+            <#ocIRI#> a oc:G09;
                 gc:isCalculationOn ?geomOpt ;
-                gc:isCalculationOn ?geomType ;
-                oc:hasInitialization ?init .
-
-            ?init a oc:InitializationModule ;
-                gc:hasParameter ?method ;
-                gc:hasParameter ?basisSet .
-
-            ?geomOpt a gc:GeometryOptimization ;
-                       gc:hasMolecule ?mol .
-            ?mol oc:hasSpinMultiplicity ?spin_mult .
-
-            ?geomType a oc:GeometryType .
-            ?geomType oc:hasGeometryType ?geomTypeValue .
-
-            OPTIONAL {
-                <#ocIRI#> gc:isCalculationOn ?vibAnal ;
-                    gc:isCalculationOn ?rotConsts ;
-                    gc:isCalculationOn ?rotSym .
-
-                ?vibAnal a gc:VibrationalAnalysis ;
-                        gc:hasResult ?freqResult .
-
-                ?freqResult oc:hasFrequencies ?frequencies .
-
-                ?rotConsts a oc:RotationalConstants ;
-                        oc:hasRotationalConstants ?rot_constants .
-                ?rotSym a oc:RotationalSymmetry ;
-                        oc:hasRotationalSymmetryNumber ?sym_number .
-            }
-        }""".replace('#ocIRI#',ocIRI).replace('#osIRI#',osIRI)
+                gc:isCalculationOn ?ScfEnergy .
+            
+  
+            ?ScfEnergy a oc:ScfEnergy ;
+                gc:hasElectronicEnergy ?ScfElecEn .
+  
+            ?ScfElecEn gc:hasValue ?ScfElecValue ;
+                 gc:hasUnit ?ScfElecUnit .
+                 
+  
+  	        ?geomOpt a gc:GeometryOptimization ;
+                 gc:hasMolecule ?mol .
+  
+  	        ?mol gc:hasAtom ?atom .
+            ?atom gc:isElement ?elem;
+                gc:hasAtomCoordinateX ?coordX;
+                gc:hasAtomCoordinateY ?coordY;
+                gc:hasAtomCoordinateZ ?coordZ.
+  
+  	        ?coordX gc:hasValue ?XCoordValue.
+  	        ?coordY gc:hasValue ?YCoordValue.
+  	        ?coordZ gc:hasValue ?ZCoordValue.
+        }""".replace('#ocIRI#',ocIRI)
     return query
 
 def ontopesscan_data_query(opesIRI):
     query = """PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
         PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX os: <http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#>
+        PREFIX ops: <http://www.theworldavatar.com/ontology/ontopesscan/OntoPESScan.owl#>
+        PREFIX gc:  <http://purl.org/gc/>
 
-        SELECT ?y ?z
+        SELECT ?oc_IRIs ?scan_coord_value ?scan_coord_unit
         WHERE {
-            <#opesIRI#> ?y ?z
+            <#opesIRI#> a ops:PotentialEnergySurfaceScan ;
+                ops:hasScanPoint ?ScanPoints .
+        
+            ?ScanPoints a ops:ScanPoint ;
+                ops:hasCalculation ?oc_IRIs ;
+                ops:hasScanCoordinateValue ?Scan_Value .
+        
+            ?Scan_Value a ops:ScanCoordinateValue ;
+                gc:hasValue ?scan_coord_value ;
+                gc:hasUnit ?scan_coord_unit .                                                                                                                       
         }""".replace('#opesIRI#',opesIRI)
     return query
