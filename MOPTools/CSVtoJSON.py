c5301ca8626b518253b7a47544e3b1abb5ea2ef8
Aleksandar Kondinski <ak2332@cam.ac.uk>
2021-10-04 21:16:52 +0100

    mopsconverter: removed uuid generators and functions creating transitional files.

--- MOPTools/CSVtoJSON.py
+++ MOPTools/CSVtoJSON.py
@@ -1,59 +1,47 @@
-import rdkit
-from rdkit import Chem
-from rdkit.Chem import Draw
 import csv 
 import json
-import uuid
-import pandas as pd 
 import re
-#comment
 
 doc = """This is a converter of CSV files containing CBUs and MOPs.
 Each individual entry is assigned a UUID. In the case of CBUs, parsing 
 of xyz files takes place. The output are JSON files."""
 
-def cbuCSVtoJSON(cbuEditedCSVFilePath, cbuJSONFilePath):
-    '''Each row in the CBU.csv file is convered into separate JSON file.'''
-     
-    #read csv file
-    with open(cbuEditedCSVFilePath, "r", encoding='utf-8') as csvf: 
-        #load csv file data using csv library's dictionary reader
-        csvReader = csv.DictReader(csvf) 
-        for row in csvReader:
-            fileNaming = row['CBUCode']
-            out = json.dumps(row, indent=4)
-            jsonoutput = open(cbuJSONFilePath+fileNaming+'.json', 'w') 
-            jsonoutput.write(out) 
-
-def cbuCSVtoJSONComplete(cbuEditedCSVFilePath, cbuCompleteJSONPath):
+def cbuCSVtoJSON(cbuCSVFilePath, speciesJSONFilePath, cbuJSONFilePath):
     '''Each row in the CBU.csv file is convered into separate JSON file.
     An XYZ file corresponding to the CBU is parsed and merged in the final JSON.'''
     #read csv file
     data = {}
-    with open(cbuEditedCSVFilePath, "r", encoding='utf-8') as csvCompletef: 
+    rowoutSpecies = {}
+    rowoutCBUs = {}
+    with open(cbuCSVFilePath, "r", encoding='utf-8-sig') as csvCompletef: 
         #load csv file data using csv library's dictionary reader
         csvReaderComplete = csv.DictReader(csvCompletef) 
         for row in csvReaderComplete :
             cbu = row['CBUCode']
-            xyzCBU = 'C:\\Converter_CSV_JSON\\inputXYZ\\'+cbu+'.xyz'
-            #xyzCBU = 'C:\\Users\\ak2332\\Documents\\Programs_CambridgeHome\\Converter_CSV_JSON\\inputXYZ\\'+cbu+'.xyz'
+            #xyzCBU = 'C:\\Converter_CSV_JSON\\inputXYZ\\'+cbu+'.xyz'
+            xyzCBU = 'C:\\Users\\ak2332\\Documents\\Converter_CSV_JSON\\01_CBU_IO\\inputXYZ\\'+cbu+'.xyz'
             data = parseXYZ(xyzCBU)
             row.update(data)
-            out = json.dumps(row, indent=4)
-            jsonoutput = open(cbuCompleteJSONPath+cbu+'.json', 'w') 
-            jsonoutput.write(out) 
+            rowoutSpecies.update({"Empirical formula": row['Empirical formula'], "InChi": row['InChi'], "Smiles": row['Smiles'], "MolecularWeight": row['MolecularWeight'], "Formal charge": row['Formal charge'], "Spin multiplicity": row['Spin multiplicity'],"Atom types": row['Atom types'], "Geometry": row['Geometry']})
+            outspecies = json.dumps(rowoutSpecies, indent=4)
+            jsonoutput = open(speciesJSONFilePath+cbu+'.qc.json', 'w') 
+            jsonoutput.write(outspecies)
+            rowoutCBUs.update({"Empirical formula": row['Empirical formula'], "InChi": row['InChi'], "Smiles": row['Smiles'], "MolecularWeight": row['MolecularWeight'], "Formal charge": row['Formal charge'], "Spin multiplicity": row['Spin multiplicity'],"Planarity": row['Planarity'],"Modularity": row['Modularity'],"CentralUnit": row['CentralUnit'],"Spacer": row['Spacer'],"Substituent": row['Substituent'], "BindingUnit": row['BindingUnit']})
+            outCBUs = json.dumps(rowoutCBUs, indent=4)
+            jsonoutput = open(cbuJSONFilePath+cbu+'.qc.json', 'w') 
+            jsonoutput.write(outCBUs)
 
 def parseXYZ(xyzCBU):
     '''This function parses XYZ files and returs back a dictionarry corresponding to involved atom types and their coordinates.'''
     data = {}
     atoms = []
     atoms_coordinates = [] 
-    with open(xyzCBU, "r", encoding='utf-8') as xyzf: 
+    with open(xyzCBU, "r", encoding='utf-8-sig') as xyzf: 
         for row in xyzf:  
             rowCoordinates = re.split('\s+', row)
             atom = str(rowCoordinates[0])
             atoms.append(atom)
-            atomTypes = {"Atom_types":atoms}
+            atomTypes = {"Atom types":atoms}
             atom_coordinates = (rowCoordinates[1], rowCoordinates[2], rowCoordinates[3])
             atoms_coordinates.append(atom_coordinates)
             Geometry = {"Geometry":atoms_coordinates}
@@ -61,97 +49,55 @@ def parseXYZ(xyzCBU):
             data.update(Geometry)
         return data
 
-def addUUIDtoCBUCSV(cbuCSVFilePath, cbuEditedCSVFilePath):
-    '''Adds a column with generated UUIDs to the CBUs.csv file.
-    In the process it creates an edited temporary file.'''
-    csv_input = pd.read_csv(cbuCSVFilePath)
-    csv_input['UUID'] = [uuid.uuid4() for x in range(len(csv_input))]
-    csv_input.to_csv(cbuEditedCSVFilePath, index=True)
-
-def addUUIDtoMOPCSV(mopCSVFilePath, mopEditedCSVFilePath):
-    '''Adds a column with generated UUIDs to the MOPs.csv file.
-    In the process it creates an edited temporary file.'''
-    csv_input = pd.read_csv(mopCSVFilePath)
-    csv_input['UUID'] = [uuid.uuid4() for x in range(len(csv_input))]
-    csv_input.to_csv(mopEditedCSVFilePath, index=True)
-
-def mopCSVtoJSON(mopEditedCSVFilePath, mopJSONFilePath):
+def mopCSVtoJSON(mopCSVFilePath, cbuJSONFilePath, mopJSONFilePath):
     '''Converts the CBU.csv file to separate JSON files.'''
-    #read csv file
-    with open(mopEditedCSVFilePath, "r", encoding='utf-8') as csvf: 
-        #load csv file data using csv library's dictionary reader
+    
+    with open(mopCSVFilePath, "r", encoding='utf-8-sig') as csvf: 
         csvReader = csv.DictReader(csvf) 
         lcount = 1
-        for row in csvReader:
-            out = json.dumps(row, indent=4)
-            cbu1json = []
-            cbu2json = []
+        for row in csvReader:     
             cbu1 = row['CBU1']
             cbu2 = row['CBU2']
-            cbuPath1 = f'C:\\Converter_CSV_JSON\\CBU_JSON_Output\\CBU_'+cbu1+'.json'
-            cbuPath2 = f'C:\\Converter_CSV_JSON\\CBU_JSON_Output\\CBU_'+cbu2+'.json'     
-            #cbuPath1 = f'C:\\Users\\ak2332\\Documents\\Programs_CambridgeHome\\Converter_CSV_JSON\\CBU_JSON_Output\\CBU_'+cbu1+'.json'
-            #cbuPath2 = f'C:\\Users\\ak2332\\Documents\\Programs_CambridgeHome\\Converter_CSV_JSON\\CBU_JSON_Output\\CBU_'+cbu2+'.json'     
+            cbuPath1 = cbuJSONFilePath+cbu1+'.qc.json'
+            cbuPath2 = cbuJSONFilePath +cbu2+'.qc.json'
+            
             with open(cbuPath1, 'r') as cbu1read:
                 cbu1data = json.load(cbu1read)
-                cbu1json.append(cbu1data)
-                outcbu1 = json.dumps(cbu1json, indent=8)
+                cbu1data['GenericUnitNumber'] = row['Number_CBU1']
             with open(cbuPath2, 'r') as cbu2read:
                 cbu2data = json.load(cbu2read)
-                cbu2json.append(cbu2data)
-                outcbu2 = json.dumps(cbu2json, indent=8)
-            jsonoutput = open(mopJSONFilePath+str(lcount)+'.json', 'w') 
+                cbu2data['GenericUnitNumber'] = row['Number_CBU2']
+            row["Mops_Chemical_Building_Units"] = [cbu1data, cbu2data]
+
+            outMOPs = {
+            "Mops_Formula": row['Mops_Formula'],
+            "Mops_Charge": row['Mops_Charge'],
+            "Mops_Polyhedral_Shape": row['Mops_Polyhedral_Shape'],
+            "Mops_Polyhedral_Shape_Symbol": row['Mops_Polyhedral_Shape_Symbol'],
+            "Mops_Charge_Unit": row['Mops_Charge_Unit'],
+            "Mops_Molecular_Weight": row['Mops_Molecular_Weight'],
+            "Mops_Molecular_Weight_Unit": row['Mops_Molecular_Weight_Unit'],
+            "Mops_CCDC_Number": row['Mops_CCDC_Number'],
+            "Mops_CavityVolume": row['Mops_CavityVolume'],
+            "Mops_Chemical_Building_Units": row['Mops_Chemical_Building_Units']
+            }
+            out = json.dumps(outMOPs, indent=4)
+            jsonoutput = open(mopJSONFilePath+str(lcount)+'.qc.json', 'w') 
             jsonoutput.write(out)
-            jsonoutput.write(outcbu1)
-            jsonoutput.write(outcbu2)
             lcount+=1
-
-def cbuCSVtoJSON(cbuEditedCSVFilePath, cbuJSONFilePath):
-    '''Each row in the CBU.csv file is convered into separate JSON file.'''
-     
-    #read csv file
-    with open(cbuEditedCSVFilePath, "r", encoding='utf-8') as csvf: 
-        #load csv file data using csv library's dictionary reader
-        csvReader = csv.DictReader(csvf) 
-        for row in csvReader:
-            fileNaming = row['CBUCode']
-            out = json.dumps(row, indent=4)
-            jsonoutput = open(cbuJSONFilePath+fileNaming+'.json', 'w') 
-            jsonoutput.write(out) 
-
-def cbuCSVtoDraw(cbuSMILESCSVFilePath):
-    '''Each row in the CBU.csv file is convered into separate JSON file.
-    An XYZ file corresponding to the CBU is parsed and merged in the final JSON.'''
-    with open(cbuSMILESCSVFilePath, "r", encoding='utf-8') as csvCompletef: 
-        #load csv file data using csv library's dictionary reader
-        csvReaderComplete = csv.DictReader(csvCompletef) 
-        for row in csvReaderComplete :
-            cbu = row['SMILES']
-            cbuCode = row['CBUCode']
-            cbuMol = Chem.MolFromSmiles(cbu)
-            #InChiEntry = rdkit.Chem.inchi.MolToInchi(cbuMol)
-            imagefile = 'C:\\Converter_CSV_JSON\\drawCBU\\'+cbuCode+'.png'
-            rdkit.Chem.Draw.MolToFile(cbuMol, imagefile)
-            #xyzCBU = 'C:\\Users\\ak2332\\Documents\\Programs_CambridgeHome\\Converter_CSV_JSON\\inputXYZ\\'+cbu+'.xyz'
 
 
 #List of Paths. In case of change, they need to be updated. read csv file
-#operationsFolder = 'C:\\Users\\ak2332\\Documents\\Programs_CambridgeHome\\Converter_CSV_JSON\\'
-operationsFolder = 'C:\\Converter_CSV_JSON\\'
-cbuCSVFilePath = operationsFolder+'CBU_CSV_Input\\CBUs.csv'
-cbuEditedCSVFilePath = operationsFolder+'CBU_CSV_Input\\CBUs_Edited.csv'
-cbuJSONFilePath = operationsFolder+'CBU_JSON_Output\\CBU_'
-cbuCompleteJSONPath = operationsFolder+'CBU_JSON_CompleteOutput\\CBU_'
-mopCSVFilePath = operationsFolder+'MOP_CSV_Input\\MOPs.csv'
-mopEditedCSVFilePath = operationsFolder+'MOP_CSV_Input\\MOPs_Edited.csv'
-mopJSONFilePath =  operationsFolder+'MOP_JSON_Output\\MOP_'
+operationsFolder = 'C:\\Users\\ak2332\\Documents\\Converter_CSV_JSON\\01_CBU_IO\\'
+cbuCSVFilePath = operationsFolder+'CBU_Input\\CBUs_InChi.csv'
+cbuJSONFilePath = operationsFolder+'CBU_Output\\CBU_'
+speciesJSONFilePath = operationsFolder+'Species_Output\\Species_'
 xyzPath = operationsFolder+'inputXYZ\\'
-cbuSMILESCSVFilePath = operationsFolder+'CBU_CSV_Input\\CBUs_InChiEdited.csv'
+xyzMOPsPath = operationsFolder+'inputXYZ\\'
+mopCSVFilePath = operationsFolder+'MOP_Input\\MOPs.csv'
+mopJSONFilePath =  operationsFolder+'MOP_Output\\MOP_'
+
 
 #List of Running Functions. Addition of UUIDs preceeds the generation of the JSON Files.
-addUUIDtoCBUCSV(cbuCSVFilePath, cbuEditedCSVFilePath)
-addUUIDtoMOPCSV(mopCSVFilePath, mopEditedCSVFilePath)
-cbuCSVtoJSON(cbuEditedCSVFilePath, cbuJSONFilePath)
-mopCSVtoJSON(mopEditedCSVFilePath, mopJSONFilePath)
-cbuCSVtoJSONComplete(cbuEditedCSVFilePath, cbuCompleteJSONPath)
-cbuCSVtoDraw(cbuSMILESCSVFilePath)
+cbuCSVtoJSON(cbuCSVFilePath, speciesJSONFilePath, cbuJSONFilePath)
+mopCSVtoJSON(mopCSVFilePath, cbuJSONFilePath, mopJSONFilePath)
