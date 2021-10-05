d27dd406203a4f3f46a707749c565174e37ce3c5
Demetris Demetriou <ddemetriou@cmclinnovations.com>
2021-10-05 13:35:23 +0100

    Improved the JUnit test for the agent to compare the files produced by the agent with the original files.

--- Agents/MoDSDataDrivenAgent/src/test/java/uk/ac/cam/cares/jps/agent/mechanism/datadriven/test/AgentTest.java
+++ Agents/MoDSDataDrivenAgent/src/test/java/uk/ac/cam/cares/jps/agent/mechanism/datadriven/test/AgentTest.java
@@ -2,23 +2,54 @@ package uk.ac.cam.cares.jps.agent.mechanism.datadriven.test;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 
 import uk.ac.cam.cares.jps.agent.mechanism.datadriven.MoDSDataDrivenAgent;
+import uk.ac.cam.cares.jps.agent.mechanism.datadriven.MoDSDataDrivenAgentException;
+import uk.ac.cam.cares.jps.base.slurm.job.SlurmJobException;
 
 public class AgentTest {
 
 	@Test
-	public void test() {
+	public void test() throws JSONException, IOException, MoDSDataDrivenAgentException, SlurmJobException, URISyntaxException {
 		
-		MoDSDataDrivenAgent moDSDataDrivenAgent = new MoDSDataDrivenAgent();
+		MoDSDataDrivenAgent agent = new MoDSDataDrivenAgent();
 		
 		String string = "{\"acceptHeaders\":\"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\",\"method\":\"GET\",\"requestUrl\":\"http://localhost:8080/MoDSDataDrivenAgent/job/request\",\"json\":{\"mods\":{\"calibrationAlg\":{\"initPoints\":\"1\"},\"sensana\":{\"relPerturbation\":\"1e-3\",\"maxORavg\":\"max\",\"topN\":\"10\"},\"samplingAlg\":{\"outputInterval\":\"1000\",\"sobolPoints\":\"10000\"},\"ignDelayOption\":{\"method\":\"1\",\"species\":\"AR\"},\"flameSpeedOption\":{\"tranModel\":\"mix-average\"},\"executable\":{\"path\":\"/home/hpcdeme1/codes/mods-backend/outputs/Release/bin/MoDS_mpi\"}},\"cantera\":{\"environment\":\"cantera_env\"},\"ontochemexpIRI\":{\"ignitionDelay\":[\"https://como.ceb.cam.ac.uk/kb/ontochemexp/x00001700.owl#Experiment_404313416274000\",\"https://como.ceb.cam.ac.uk/kb/ontochemexp/x00001701.owl#Experiment_404313804188800\",\"https://como.ceb.cam.ac.uk/kb/ontochemexp/x00001702.owl#Experiment_404313946760600\"],\"flameSpeed\":[\"https://como.ceb.cam.ac.uk/kb/ontochemexp/x00001703.owl#Experiment_2748799135285400\"]},\"ontokinIRI\":{\"mechanism\":\"http://www.theworldavatar.com/kb/ontokin/mechanism_6651393202518600.owl#ReactionMechanism_6651394316481501\"},\"kinetics\":{\"numerical\":{\"simEnd\":\"500\"}}}}";  
 		JSONObject json = new JSONObject(string);  
 		
-		moDSDataDrivenAgent.processRequestParameters(json, "/job/request");
-		fail("Not yet implemented");
+		//moDSDataDrivenAgent.processRequestParameters(json, "/job/request");
+		
+		String message = agent.setUpJob(string).get("jobFolderPath").toString();
+        //   String message = setUpJobOnAgentMachine(jsonString);
+        Path outputPath = agent.jobSubmission.getWorkspaceDirectory().toPath().resolve(message);
+        Map<String, String> fileMap = new HashMap<>();
+        String resultZipFilename = agent.jobSubmission.getHpcAddress() + outputPath.getFileName().toString().substring(outputPath.getFileName().toString().lastIndexOf("_")) + ".zip";
+        fileMap.put(resultZipFilename, "input.zip");
+        Files.walk(outputPath)
+                .filter(resultPath -> resultPath.toFile().isFile())
+                .forEach(resultPath -> {
+                    try {
+                        String relativeResultPath = outputPath.relativize(resultPath).toString();
+                        final URL baseURL = AgentTest.class.getResource(fileMap.getOrDefault(relativeResultPath, relativeResultPath));
+                        if (null != baseURL) {
+                            Path basePath = Path.of(baseURL.toURI());
+                            TestUtils.compareFiles(basePath, resultPath);
+                        }
+                    } catch (URISyntaxException ex) {
+                        throw new RuntimeException(resultPath.toString(), ex);
+                    }
+                });
 	}
 
 }
