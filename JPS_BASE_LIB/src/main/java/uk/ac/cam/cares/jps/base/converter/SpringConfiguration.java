ecc488b9617c2888d358eecec5592c81b55480fc
Feroz Farazi <msff2@cam.ac.uk>
2021-10-08 13:11:00 +0100

    1227-merge-tbox-tool-into-jps-base-lib: Moved the TBoxManagement features to JPS_BASE_LIB and extended its code to read TBoxIRIs, imported TBoxes and version no from the corresponding CSV template.

---
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/converter/SpringConfiguration.java
@@ -0,0 +1,16 @@
+package uk.ac.cam.cares.jps.base.converter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@ComponentScan
+public class SpringConfiguration {
+
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
+}
