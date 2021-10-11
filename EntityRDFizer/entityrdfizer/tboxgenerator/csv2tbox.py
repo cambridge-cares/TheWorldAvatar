16d997c15ebff8aaea9ca896dc08796fa1f135db
dln22 <danieln@cmclinnovations.com>
2021-10-11 12:11:58 +0100

    1227-merge-tbox-tool-into-jps-base-lib: Finished the csv2tbox function.

--- EntityRDFizer/entityrdfizer/tboxgenerator/csv2tbox.py
+++ EntityRDFizer/entityrdfizer/tboxgenerator/csv2tbox.py
@@ -1,5 +1,7 @@
 from entityrdfizer.javagateway.gateway import jpsBaseLibGW
-from py4j import Py4JJavaError, Py4JError
+from py4j.java_gateway import Py4JJavaError, Py4JError
+import textwrap
+import pathlib
 import glob
 import os
 
@@ -14,8 +16,15 @@ def run_tbox_generator(
 ):
 
     if os.path.isfile(csvFileOrDirPath):
+        if outDir is None: outDir = os.path.dirname(csvFileOrDirPath)
+        os.makedirs(outDir, exist_ok=True)
+        outDir =os.path.abspath(outDir)
+
         csv2tbox(csvFileOrDirPath, outDir)
     elif os.path.isdir(csvFileOrDirPath):
+        if outDir is None: outDir = csvFileOrDirPath
+        os.makedirs(outDir, exist_ok=True)
+
         csv_files = glob.glob(os.path.join(csvFileOrDirPath, "*.csv"))
         for csv_file in csv_files:
             csv2tbox(csv_file, outDir)
@@ -23,11 +32,15 @@ def run_tbox_generator(
         print(f"File or directory {csvFileOrDirPath} does not exists")
 
 def csv2tbox(csvFile, outDir):
-    print(f"Converting tbox {csvFile} into owl format.")
+    print(f"Converting tbox {csvFile} into rdf format.")
+    outFile = os.path.join(outDir,pathlib.Path(csvFile).stem+'.owl')
     TBoxGenerator = jpsBaseLibGW_view.TBoxGeneration()
     try:
-        TBoxGenerator.generateTBox(csvFile)
-    except Py4JJavaError as e:
-        raise e(f"Tbox generation failed with the following error {e.java_exception}")
-    except Py4JError as e:
-        raise e(f"Tbox generation failed with the following error {e.java_exception}")
+        # the file name is added to the outDir, as the java code expects output file path
+        TBoxGenerator.generateTBox(csvFile, outFile)
+    except (Py4JJavaError, Py4JError) as e:
+        print(textwrap.dedent("""
+            Error: Tbox generation failed. Add 'redirect_stderr' parameter to the launch
+                   gateway method to see a more detailed error log."""))
+        raise e(f"Tbox generation failed with the following error {e.java_exception}. ")
+    print(f"Conversion complete. Tbox created at {outFile}")
