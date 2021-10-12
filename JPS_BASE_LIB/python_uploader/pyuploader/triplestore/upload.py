67d581a03b8ac2c6d90e92d88356f9fe3855f5b2
dln22 <danieln@cmclinnovations.com>
2021-10-12 16:53:23 +0100

    dev-pyuploader: Used the uploadRDFFile method to upload rdf files. Separated the rdf file upload code into its own function.

--- JPS_BASE_LIB/python_uploader/pyuploader/triplestore/upload.py
+++ JPS_BASE_LIB/python_uploader/pyuploader/triplestore/upload.py
@@ -1,3 +1,5 @@
+from py4j.protocol import Py4JError, Py4JJavaError
+import textwrap
 import pyuploader.triplestore.base as tsbase
 import pyuploader.common.utils as utils
 from pyuploader.common.gateways import jpsBaseLibGW
@@ -7,31 +9,36 @@ import pathlib
 logger = logging.getLogger(__name__)
 
 jpsBaseLib_view = jpsBaseLibGW.createModuleView()
-jpsBaseLibGW.importPackages(jpsBaseLib_view,"uk.ac.cam.cares.jps.base.upload.*")
+jpsBaseLibGW.importPackages(jpsBaseLib_view,"uk.ac.cam.cares.jps.base.query.*")
 
 def upload_to_triple_store(
         file_or_dir,
         url='',
         auth='',
-        namespace='base',
         file_ext='.owl',
         dry_run= False):
 
     if not url: url = tsbase.get_striple_store_url()
     if not auth: auth = tsbase.get_user_credentials()
+    if file_ext != '.owl': raise NotImplementedError('Only owl files are currently supported.')
     files = utils.get_files_by_extensions(file_or_dir,file_ext)
 
     logger.info(f"---------------------------------------------------------------------------")
     logger.info(f"TRIPLE STORE UPLOAD")
     logger.info(f"---------------------------------------------------------------------------")
     if files:
-        logger.info(f"Uploading files to the triple store: {url} at namespace: {namespace}.")
+        logger.info(f"Uploading files to the triple store: {url}.")
         for f in files:
             basenf = pathlib.Path(f).name
             logger.info(f"Uploading file: {basenf}")
             if not dry_run:
-                KnowledgeRepository = jpsBaseLib_view.RemoteStoreClient(url, url, auth[0], auth[1], namespace,f,"")
-                KnowledgeRepository.uploadOntology()
+                upload_rdf_file_to_triple_store(url, auth, f)
     else:
         logger.info('No files to upload')
     logger.info(f"---------------------------------------------------------------------------")
+
+
+def upload_rdf_file_to_triple_store(url, auth, f):
+    client = jpsBaseLib_view.RemoteStoreClient(url, url, auth[0], auth[1])
+    rdfFile = jpsBaseLib_view.java.io.File(f)
+    client.uploadRDFFile(rdfFile)
