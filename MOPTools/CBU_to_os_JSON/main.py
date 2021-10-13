ce866a9f5c80bcaa6f5abd8e54c24df0948f9d5e
Aleksandar Kondinski <ak2332@cam.ac.uk>
2021-10-13 20:01:21 +0100

    moptools: quering and writing of CBU iri is coupled with decision and printing of om_json files.

--- MOPTools/CBU_to_os_JSON/main.py
+++ MOPTools/CBU_to_os_JSON/main.py
@@ -1,8 +1,8 @@
 from cbuCSVtoJSON.osJSONwrapper import cbuOperations
 
-doc = """oswriter
-Usage: oswriter <path> 
-"""
+doc = """This program generates os_json files.
+The input files of for this program are single CBU.csv files and XYZ cbu files.
+The files are then converted to owl files with the help of A-box writers."""
 
 def start():
     cbuCSVFilePath = input("Path to the csv file:")
---
+++ MOPTools/CBU_to_os_JSON/mopcsv_operations/query_functions.py
@@ -0,0 +1,22 @@
+from kgoperations.querykg import querykg
+from kgoperations.queryendpoints import SPARQL_ENDPOINTS
+from kgoperations.querytemplate import inchi_query, mop_query
+
+def run_iri_query(inchi):
+    result  = querykg(SPARQL_ENDPOINTS['ontospecies'], inchi_query(inchi)) #query_endpoint(endpoint, spec_inchi_query(inchi))
+    if result:
+        if 'speciesIRI' in result[0].keys():
+            result =  result[0]['speciesIRI']
+    return result
+
+def printJSON(mop_weight, mop_shape, mop_charge):
+    result  = querykg(SPARQL_ENDPOINTS['ontomops'], mop_query(mop_weight, mop_shape, mop_charge))
+    decision = False
+    if result:
+        if 'MOP_Weight' in result[0].keys():
+            if result[0]['MOP_Weight'] is not None: 
+                pass
+    else:
+        decision = True
+    return decision
+
