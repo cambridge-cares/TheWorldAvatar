a159faf8ef7cf67c0c4091b83d937d748f5f6fb3
Jiaru Bai <jb2197@cam.ac.uk>
2021-10-14 21:34:41 +0100

    133-dev-design-of-experiment: parameterised the SPARQL query and update endpoint, also the periodical timescale of the scheduler of DoE Agent.

---
+++ Agents/DoEAgent/summit_agent/resources/doeagent_properties.py
@@ -0,0 +1,8 @@
+###--- Properties for Python script ---###
+
+# Full SPARQL Query and Update endpoints of OntoRxn triple store
+SPARQL_QUERY_ENDPOINT = 'http://theworldavatar.com/blazegraph/namespace/textontorxn/sparql'
+SPARQL_UPDATE_ENDPOINT = 'http://theworldavatar.com/blazegraph/namespace/textontorxn/sparql'
+
+# Periodic timescale for monitoring Derivation (mins)
+PERIODIC_TIMESCALE = 5
