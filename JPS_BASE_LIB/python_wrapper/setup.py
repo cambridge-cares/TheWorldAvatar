0101d52913f71aa37ad0bad4a3c37c873352fef5
dln22 <danieln@cmclinnovations.com>
2021-10-18 13:33:37 +0100

    924-python-java-wrapper: Bumped the wrapper version again.

--- JPS_BASE_LIB/python_wrapper/setup.py
+++ JPS_BASE_LIB/python_wrapper/setup.py
@@ -3,7 +3,7 @@ import os.path
 
 setup(
     name='py4jps',
-    version='1.0.5',
+    version='1.0.6',
     author='Daniel Nurkowski',
     author_email='danieln@cmclinnovations.com',
     license='MIT',
