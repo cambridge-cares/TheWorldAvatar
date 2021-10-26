85a5ce1b4d712c7b8ca44ca9e5fd6a5d409af326
Owen Parry <oparry@cmclinnovations.com>
2021-10-26 18:31:54 +0100

    dev-DAFNI_stack: Added a script to generate user-specific ports in the dafni stack's .env file.

---
+++ Deploy/stacks/dafni/generate_env_file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Read user ID from input
+read -p "Enter the last two numbers in your dafni user ID, or '10' if you're running this elsewhere: " user_id
+
+if [ ${#user_id} -ne 2 ]; then
+  echo "Input had length ${#user_id}; expected length 2, exiting..."
+  exit 1
+fi
+
+if [[ -n ${user_id//[0-9]/} ]]; then
+  echo "Input contained non-numeric characters, exiting..."
+  exit 2
+fi
+
+sed -e "s/USER_PORT_BASE/1$user_id/g" < .env_template > .env
