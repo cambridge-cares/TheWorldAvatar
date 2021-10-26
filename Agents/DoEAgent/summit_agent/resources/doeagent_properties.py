fe04fe8fe058a48f21f9ce2fb0c650de504d85ac
jb2197 <jb2197@cam.ac.uk>
2021-10-27 00:46:43 +0100

    133-dev-design-of-experiment: changed the testing triple store to default version of blazegraph.

--- Agents/DoEAgent/summit_agent/resources/doeagent_properties.py
+++ Agents/DoEAgent/summit_agent/resources/doeagent_properties.py
@@ -1,9 +1,9 @@
 ###--- Properties for Python script ---###
 
 # Full SPARQL Query and Update endpoints of OntoRxn triple store
-SPARQL_QUERY_ENDPOINT = 'http://kg.cmclinnovations.com:81/blazegraph_geo/namespace/testontorxn/sparql'
-SPARQL_UPDATE_ENDPOINT = 'http://kg.cmclinnovations.com:81/blazegraph_geo/namespace/testontorxn/sparql'
-TRIPLE_STORE_UPLOAD_SERVER = 'http://kg.cmclinnovations.com:81/blazegraph_geo'
+SPARQL_QUERY_ENDPOINT = 'http://kg.cmclinnovations.com:81/blazegraph/namespace/testontorxn/sparql'
+SPARQL_UPDATE_ENDPOINT = 'http://kg.cmclinnovations.com:81/blazegraph/namespace/testontorxn/sparql'
+TRIPLE_STORE_UPLOAD_SERVER = 'http://kg.cmclinnovations.com:81/blazegraph'
 TRIPLE_STORE_UPLOAD_REPOSITORY = 'testontorxn'
 
 # Periodic timescale for monitoring Derivation (seconds)
