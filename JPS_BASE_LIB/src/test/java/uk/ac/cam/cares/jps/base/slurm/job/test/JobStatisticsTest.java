2b4a744b864932d496440feb80f73c24552d573f
LerhFeng <lerhfeng.low@cares.cam.ac.uk>
2021-10-28 12:22:47 +0800

    dev-update-Job-Statistics-Test-to-have-separate-method-to-write-file
    The original code had two unit tests, both of which had their own code to generate and write the same test file. This update moves that code to its own method, and both unit tests now simply call generatefile() to create the test file.

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/slurm/job/test/JobStatisticsTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/slurm/job/test/JobStatisticsTest.java
@@ -12,15 +12,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JobStatisticsTest {
 
-    @Test
-    @DisplayName("Testing calculateStatistics")
-    void calculateStatistics() throws IOException, URISyntaxException {
+    public static void generatefile() throws IOException {
         String thisdir = System.getProperty("user.dir"); //Returns JPS_BASE_LIB
         String fileloc = thisdir + "\\src\\test\\resources\\JobStatisticsTestInputstatus.txt"; //Navigate to the resources folder define the name of the test file. Not strictly necessary given that the status file is temporary (i.e. it can go anywhere), but historically this is where we put the test file
-
-        //We make two files: one for creating JobStatisticsTestInputstatus.txt, and the other to call JobStatistics (since JobStatistics takes the "job workspace folder" as input)
         File tempInputStatus = new File(fileloc); //Create JobstatisticsTestInputstatus.txt. This file will be deleted at the end of the unit test.
-        File input = new File(thisdir); //To be used for calling JobStatistics
         FileWriter writer = new FileWriter(tempInputStatus);
 
         //Fill JobStatisticsTestInputstatus.txt with text. We have one JobStatus: completed, two JobStatus: completing, etc.
@@ -74,6 +69,19 @@ class JobStatisticsTest {
         }
 
         writer.close();
+    }
+
+    @Test
+    @DisplayName("Testing calculateStatistics")
+    void calculateStatistics() throws IOException, URISyntaxException {
+        generatefile(); //Creates JobStatisticsTestInputstatus.txt in the \\src\\test\\resources folder
+
+        String thisdir = System.getProperty("user.dir"); //Returns JPS_BASE_LIB
+        String fileloc = thisdir + "\\src\\test\\resources\\JobStatisticsTestInputstatus.txt"; //Navigate to the resources folder, which is where generatefile puts the test file
+
+        //We define two Files: one for deleting JobStatisticsTestInputstatus.txt at the end of the test, and the other to call JobStatistics (since JobStatistics takes the "job workspace folder" as input)
+        File tempInputStatus = new File(fileloc); //Call JobstatisticsTestInputstatus.txt, which was created by generatefile(). This file will be deleted at the end of the unit test.
+        File input = new File(thisdir); //To be used for calling JobStatistics
 
         JobStatistics result = new JobStatistics(input); //Create new object
         result.calculateStatistics(fileloc); //And feed JobStatisticsTestInputstatus.txt as the input. The file has 1 job completed, 2 jobs completing, etc, hence all of the assertEquals must be true for the test to be successful
@@ -95,69 +103,18 @@ class JobStatisticsTest {
     @Test
     @DisplayName("Testing JobStatistics")
     void JobStatistics() throws IOException {
+        generatefile(); //Creates JobStatisticsTestInputstatus.txt in the \\src\\test\\resources folder
+
         String thisdir = System.getProperty("user.dir"); //Returns the working directory, which is JPS_BASE_LIB
         String newdir = thisdir + "\\src\\test"; //Now go to JPS_BASE_LIB\src\test, because in JPS_BASE_LIB\src\test\resources is where the test input file is, and the JobStatistics method in JobStatistics.java searches two folders deep
-        String fileloc = thisdir + "\\src\\test\\resources\\JobStatisticsTestInputstatus.txt"; //Navigate to the resources folder define the name of the test file. Not strictly necessary given that the status file is temporary (i.e. it can go anywhere), but historically this is where we put the test file
+        String fileloc = thisdir + "\\src\\test\\resources\\JobStatisticsTestInputstatus.txt"; //Navigate to the resources folder, which is where generatefile puts the test file
                                                                                                 //Notably the test file must end in status.txt or it will not be detected by calculateStatistics (line 71 in Jobstatistics.java, endsWith(Status.STATUS_FILE.getName()).)
 
-        //We make two Files: one for creating JobStatisticsTestInputstatus.txt, and the other to call JobStatistics (since JobStatistics takes the "job workspace folder" as input)
+        //We make two Files: one to delete JobStatisticsTestInputstatus.txt at the end of the test, and the other to call JobStatistics (since JobStatistics takes the "job workspace folder" as input)
         File tempInputStatus = new File(fileloc); //Create JobstatisticsTestInputstatus.txt. This file will be deleted at the end of the unit test.
         File input = new File(newdir); //Use the new path as input
-        FileWriter writer = new FileWriter(tempInputStatus);
 
-        //Fill JobStatisticsTestInputstatus.txt with text. We have one JobStatus: completed, two JobStatus: completing, etc.
-        for (int i = 0; i < 1; i++) {
-            writer.write("JobStatus: completed");
-        }
-
-        for (int i = 0; i < 2; i++) {
-            writer.write(System.getProperty( "line.separator" )); //This puts the next JobStatus on a new line
-            writer.write("JobStatus: completing");
-        }
-
-        for (int i = 0; i < 3; i++) {
-            writer.write(System.getProperty( "line.separator" ));
-            writer.write("JobStatus: failed");
-        }
-
-        for (int i = 0; i < 4; i++) {
-            writer.write(System.getProperty( "line.separator" ));
-            writer.write("JobStatus: pending");
-        }
-
-        for (int i = 0; i < 5; i++) {
-            writer.write(System.getProperty( "line.separator" ));
-            writer.write("JobStatus: preempted");
-        }
-
-        for (int i = 0; i < 6; i++) {
-            writer.write(System.getProperty( "line.separator" ));
-            writer.write("JobStatus: running");
-        }
-
-        for (int i = 0; i < 7; i++) {
-            writer.write(System.getProperty( "line.separator" ));
-            writer.write("JobStatus: suspended");
-        }
-
-        for (int i = 0; i < 8; i++) {
-            writer.write(System.getProperty( "line.separator" ));
-            writer.write("JobStatus: stopped");
-        }
-
-        for (int i = 0; i < 9; i++) {
-            writer.write(System.getProperty( "line.separator" ));
-            writer.write("JobStatus: error termination");
-        }
-
-        for (int i = 0; i < 10; i++) {
-            writer.write(System.getProperty( "line.separator" ));
-            writer.write("JobStatus: not started");
-        }
-
-        writer.close();
-
-        JobStatistics result = new JobStatistics(input); //Create new object
+        JobStatistics result = new JobStatistics(input);
 
         assertEquals(55, result.getJobsSubmitted()); //There are 55 jobs total (= 1 + 2 + 3 + 4 + ... + 10). 1 completed, 2 completing, 3 failed, 4 pending, etc. getJobsSubmitted should therefore return 55.
 
