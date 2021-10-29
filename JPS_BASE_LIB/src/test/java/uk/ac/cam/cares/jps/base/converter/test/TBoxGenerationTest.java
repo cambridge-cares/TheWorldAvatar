d1fc42b5cac8d2dac0654f0025c5de451597d1f6
Feroz Farazi <msff2@cam.ac.uk>
2021-10-29 12:35:20 +0100

    1204-add-new-features-to-tbox-generation-code: Created the method signature for all required methods to test the TBoxManager component.

---
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/converter/test/TBoxGenerationTest.java
@@ -0,0 +1,41 @@
+package uk.ac.cam.cares.jps.base.converter.test;
+
+import org.junit.jupiter.api.Test;
+
+public class TBoxGenerationTest {
+	
+	@Test
+	public void testClassHierachy(){
+		
+	}
+	
+	@Test
+	public void testObjectPropertyHierachy(){
+		
+	}
+
+	@Test
+	public void testObjectPropertyInverseOfRelation(){
+		
+	}
+
+	@Test
+	public void testObjectPropertyCharacteristics(){
+		
+	}
+	
+	@Test
+	public void testDataPropertyHierachy(){
+		
+	}
+
+	@Test
+	public void testDataPropertyCharacteristics(){
+		
+	}
+
+	@Test
+	public void testEquivalentRelation(){
+		
+	}
+}
