88b174b3e89bda8aefdfbc41dde3084f91675925
George Brownbridge <gbrownbridge@cmclinnovations.com>
2021-10-29 17:53:40 +0100

    dev-DAFNI_stack: Modified the "upload_to_pgsql.sh" file so that it is POSIX shell compliant.

--- Deploy/stacks/dafni/pgsql-uploader/upload_to_pgsql.sh
+++ Deploy/stacks/dafni/pgsql-uploader/upload_to_pgsql.sh
@@ -9,7 +9,7 @@ create_postgres_database() {
   # Create the db
   echo "SELECT 'CREATE DATABASE ${db}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${db}')\gexec" | psql -h $POSTGRES_HOST -U $POSTGRES_USER -w
 
-  if [[ "${type:0:7}" == "postgis" ]] ; then
+  if [ "${type:0:7}" = "postgis" ] ; then
     # Load PostGIS into database
     echo "Loading base PostGIS extensions into $db"
     psql -h $POSTGRES_HOST -U $POSTGRES_USER -w --dbname="${db}" <<-'EOSQL'
@@ -20,7 +20,7 @@ create_postgres_database() {
 EOSQL
   fi
   
-  if [[ "$type" = "postgis_raster" ]] ; then
+  if [ "$type" = "postgis_raster" ] ; then
     # Load PostGIS into database
     echo "Loading PostGIS raster extension into $db"
     psql -h $POSTGRES_HOST -U $POSTGRES_USER -w --dbname="${db}" <<-'EOSQL'
@@ -46,68 +46,75 @@ for c in ${CONFIG_DIR}/*${c}.conf; do
   echo $c
   . $c
 
-  if [ $g_CSV_ENABLED -eq 1 ] && [ $CSV_ENABLED -eq 1 ]; then
-    echo loading csv files
+  if [ "$g_CSV_ENABLED" = "1" ]; then
+    if [ "$CSV_ENABLED" = "1" ]; then
+      echo loading csv files
     
-#    create_postgres_database $CSV_DB 
+#      create_postgres_database $CSV_DB 
 
-    if [ -z "$CSV_SQL_STATEMENT_TRANSFORM" ]; then
-      CSV_SQL_STATEMENT_TRANSFORM=cat
+      if [ -z "$CSV_SQL_STATEMENT_TRANSFORM" ] ; then
+        CSV_SQL_STATEMENT_TRANSFORM=cat
+      else
+        CSV_SQL_STATEMENT_TRANSFORM=${CONFIG_DIR}/$CSV_SQL_STATEMENT_TRANSFORM
+      fi
+
+      for xls in ${DATA_DIR}/${DATASET_DIR}/*.{xls,xlsx,XLS,XLSX}; do
+        echo $xls
+        # https://csvkit.readthedocs.io/en/1.0.6/scripts/in2csv.html
+#        in2csv $xls
+      done
+
+      for csv in ${DATA_DIR}/${DATASET_DIR}/*.csv; do
+        echo $csv
+
+        # https://csvkit.readthedocs.io/en/1.0.6/scripts/csvsql.html
+        csvsql --overwrite --insert --db-schema $CSV_SCHEMA --db "postgresql://${POSTGRES_USER}:$(cat ${POSTGRES_PASSWORD_FILE})@${POSTGRES_HOST}/${CSV_DB}" $csv
+      done
     else
-      CSV_SQL_STATEMENT_TRANSFORM=${CONFIG_DIR}/$CSV_SQL_STATEMENT_TRANSFORM
+      echo Skipping loading of csv files
     fi
-
-    for xls in ${DATA_DIR}/${DATASET_DIR}/*.{xls,xlsx,XLS,XLSX}; do
-      echo $xls
-      # https://csvkit.readthedocs.io/en/1.0.6/scripts/in2csv.html
- #     in2csv $xls
-    done
-
-    #
-    for csv in ${DATA_DIR}/${DATASET_DIR}/*.csv; do
-      echo $csv
-      # https://csvkit.readthedocs.io/en/1.0.6/scripts/csvsql.html
-      csvsql --overwrite --insert --db-schema $CSV_SCHEMA --db "postgresql://${POSTGRES_USER}:$(cat ${POSTGRES_PASSWORD_FILE})@${POSTGRES_HOST}/${CSV_DB}" $csv
-    done
   fi
 
-  if [ $g_SHP_ENABLED -eq 1 ] && [ $SHP_ENABLED -eq 1 ]; then
-    echo loading shp files
-    
-    create_postgres_database $SHP_DB postgis
-    
-    if [ -z "$SHP_SQL_STATEMENT_TRANSFORM" ]; then
-      SHP_SQL_STATEMENT_TRANSFORM=cat
+  if [ "$g_SHP_ENABLED" = "1" ] ; then
+    if [ "$SHP_ENABLED" = "1" ] ; then
+      echo loading shp files
+      
+      create_postgres_database $SHP_DB postgis
+      
+      if [ -z "$SHP_SQL_STATEMENT_TRANSFORM" ]; then
+        SHP_SQL_STATEMENT_TRANSFORM=cat
+      else
+        SHP_SQL_STATEMENT_TRANSFORM=${CONFIG_DIR}/$SHP_SQL_STATEMENT_TRANSFORM
+      fi
+      
+      # Add shape files to .sql file
+      mode=d # For the first shp file drop any existing table with the name defined by "${SHP_SCHEMA}.${SHP_TABLE}"
+      for shp in ${DATA_DIR}/${DATASET_DIR}/*.shp; do
+        echo $shp
+        
+        shp2pgsql -${mode} -s ${SHP_EPSG_FROM} -D -I $shp ${SHP_SCHEMA}.${SHP_TABLE} |
+        ${SHP_SQL_STATEMENT_TRANSFORM} |
+        psql -h $POSTGRES_HOST -d $SHP_DB -U $POSTGRES_USER -w
+        
+        mode=a # Just append the data from subsequent shp files
+      done
     else
-      SHP_SQL_STATEMENT_TRANSFORM=${CONFIG_DIR}/$SHP_SQL_STATEMENT_TRANSFORM
+      echo Skipping loading of shp files
     fi
-    
-    # Add shape files to .sql file
-    mode=d # For the first shp file drop any existing table with the name defined by "${SHP_SCHEMA}.${SHP_TABLE}"
-    for shp in ${DATA_DIR}/${DATASET_DIR}/*.shp; do
-      echo $shp
-      
-      shp2pgsql -${mode} -s ${SHP_EPSG_FROM} -D -I $shp ${SHP_SCHEMA}.${SHP_TABLE} |
-      ${SHP_SQL_STATEMENT_TRANSFORM} |
-      psql -h $POSTGRES_HOST -d $SHP_DB -U $POSTGRES_USER -w
-      
-      mode=a # Just append the data from subsequent shp files
-    done
-
-  else
-    echo not loading shp files
   fi
 
-  if [ $g_TIF_ENABLED -eq 1 ] && [ $TIF_ENABLED -eq 1 ]; then
-    echo loading tifs
-    
-    create_postgres_database $TIF_DB postgis_raster
+  if [ "$g_TIF_ENABLED" = "1" ] ; then
+    if [ "$TIF_ENABLED" = "1" ] ; then
+      echo loading geotiff files
+      
+      create_postgres_database $TIF_DB postgis_raster
 
-    # Add geotiff files to database
-    mode=d
-    raster2pgsql -C -I -F -Y -M -R -${mode} -s ${TIF_EPSG_FROM} -t auto ${DATA_DIR}/${DATASET_DIR}/*.tif ${TIF_SCHEMA}.${TIF_TABLE} | psql -h $POSTGRES_HOST -d $DB -U $POSTGRES_USER -w
+      # Add geotiff files to database
+      mode=d
+      raster2pgsql -C -I -F -Y -M -R -${mode} -s ${TIF_EPSG_FROM} -t auto ${DATA_DIR}/${DATASET_DIR}/*.tif ${TIF_SCHEMA}.${TIF_TABLE} | psql -h $POSTGRES_HOST -d $DB -U $POSTGRES_USER -w
 
-  else
-    echo not loading tifs
+   else
+      echo Skipping loading of geotiff files
+    fi
   fi
 done
