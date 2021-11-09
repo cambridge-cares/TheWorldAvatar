ef98ad3e7eed06469616f24e3c3327ca03ae406e
Markus Hofmeister <mh807@cam.ac.uk>
2021-11-09 16:42:09 +0100

    1242-dev-instantiate-interoperable-kings-lynn: included example dtvf visualisation and updated API key

---
+++ Agents/KingsLynnAgent/example-dt-vis/local_icons.js
@@ -0,0 +1,9 @@
+
+// A list of icons files to load for this specific visualisation.
+// The location of icons files should be stored per-visualisation
+// (as they'll change for each implementation) rather than within
+// the framework.
+const EXAMPLE_ICONS = [
+    "img/icons/warning.png",
+    "img/icons/error.png"
+];
