183f1ae65e11543bdc7f2e3ad5b520fada833e58
Markus Hofmeister <mh807@cam.ac.uk>
2021-11-23 14:37:38 +0100

    1246-dev-create-minimum-working-time-series-example: Updated index.html and removed API key

--- Agents/TimeSeriesExample/dtvf_visualisation_old/docker/start-up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script should run whenever the Docker container is started.
-echo "Running start-up.sh script..."
-
-# Start the Apache web server
-echo "Starting Apache web server..."
-httpd -D FOREGROUND
--- Agents/TimeSeriesExample/dtvf_visualisation_old/local_icons.js
+++
@@ -1,9 +0,0 @@
-
-// A list of icons files to load for this specific visualisation.
-// The location of icons files should be stored per-visualisation
-// (as they'll change for each implementation) rather than within
-// the framework.
-const EXAMPLE_ICONS = [
-    "img/icons/warning.png",
-    "img/icons/error.png"
-];
