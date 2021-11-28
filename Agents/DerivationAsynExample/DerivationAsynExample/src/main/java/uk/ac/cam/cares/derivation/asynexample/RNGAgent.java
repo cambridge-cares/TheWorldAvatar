b8f7a838426694f887552e5d40c8ed6df94bc2cb
Jiaru Bai <jb2197@cam.ac.uk>
2021-11-28 21:03:48 +0000

    139-dev-minimum-working-example-of-asyn-agent: fixed error of reading MaxValue instance where UpperLimit should be read; added checking if upper limit is greater than lower limit in provided agent input.

--- Agents/DerivationAsynExample/DerivationAsynExample/src/main/java/uk/ac/cam/cares/derivation/asynexample/RNGAgent.java
+++ Agents/DerivationAsynExample/DerivationAsynExample/src/main/java/uk/ac/cam/cares/derivation/asynexample/RNGAgent.java
@@ -12,6 +12,7 @@ import javax.servlet.ServletException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
 
 import uk.ac.cam.cares.jps.base.agent.AsynAgent;
 import uk.ac.cam.cares.jps.base.derivation.DerivationClient;
@@ -19,6 +20,12 @@ import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.interfaces.StoreClientInterface;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
 
+/**
+ * This random number generator (RNG) agent generates a list of random points given inputs.
+ * @author Jiaru Bai (jb2197@cam.ac.uk)
+ *
+ */
+@Controller
 public class RNGAgent extends AsynAgent {
 	
 	private static final Logger LOGGER = LogManager.getLogger(RNGAgent.class);
@@ -37,22 +44,25 @@ public class RNGAgent extends AsynAgent {
 		List<String> createdInstances = new ArrayList<String>();
 		
 		// get the input from the KG
-		String upperLimitIRI = requestParams.getJSONObject(DerivationClient.AGENT_INPUT_KEY).getString(SparqlClient.MaxValue.getQueryString().replaceAll(SparqlClient.prefix+":", SparqlClient.namespace));
+		String upperLimitIRI = requestParams.getJSONObject(DerivationClient.AGENT_INPUT_KEY).getString(SparqlClient.UpperLimit.getQueryString().replaceAll(SparqlClient.prefix+":", SparqlClient.namespace));
 		Integer upperLimit = sparqlClient.getValue(upperLimitIRI);
 		String lowerLimitIRI = requestParams.getJSONObject(DerivationClient.AGENT_INPUT_KEY).getString(SparqlClient.LowerLimit.getQueryString().replaceAll(SparqlClient.prefix+":", SparqlClient.namespace));
 		Integer lowerLimit = sparqlClient.getValue(lowerLimitIRI);
 		String numberOfPointsIRI = requestParams.getJSONObject(DerivationClient.AGENT_INPUT_KEY).getString(SparqlClient.NumberOfPoints.getQueryString().replaceAll(SparqlClient.prefix+":", SparqlClient.namespace));
 		Integer numberOfPoints = sparqlClient.getValue(numberOfPointsIRI);
 		
-		// generate a list of random points
-		List<Integer> listOfRandomPoints = randomNumberGeneration(upperLimit, lowerLimit, numberOfPoints);
-		
-		// write the generated list of random points to the KG
-		String listOfRandomPoints_iri = sparqlClient.createListOfRandomPoints(listOfRandomPoints);
-		
-		// respond with the created IRI
-		createdInstances.add(listOfRandomPoints_iri);
-		return createdInstances;
+		if (upperLimit >= lowerLimit) {
+			// generate a list of random points
+			List<Integer> listOfRandomPoints = randomNumberGeneration(upperLimit, lowerLimit, numberOfPoints);
+			// write the generated list of random points to the KG
+			String listOfRandomPoints_iri = sparqlClient.createListOfRandomPoints(listOfRandomPoints);
+			
+			// respond with the created IRI
+			createdInstances.add(listOfRandomPoints_iri);
+			return createdInstances;
+		} else {
+			return null;
+		}
 	}
 	
 	private List<Integer> randomNumberGeneration(int upperLimit, int lowerLimit, int numberOfPoints) {
