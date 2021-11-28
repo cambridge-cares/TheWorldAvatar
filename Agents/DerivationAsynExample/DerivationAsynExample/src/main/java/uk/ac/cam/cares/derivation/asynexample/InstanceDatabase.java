c6c7cc38b6513601dd8adce8240cd14febd13f87
Jiaru Bai <jb2197@cam.ac.uk>
2021-11-28 21:04:45 +0000

    139-dev-minimum-working-example-of-asyn-agent: created InstanceDatabase to store created derivation instances.

---
+++ Agents/DerivationAsynExample/DerivationAsynExample/src/main/java/uk/ac/cam/cares/derivation/asynexample/InstanceDatabase.java
@@ -0,0 +1,10 @@
+package uk.ac.cam.cares.derivation.asynexample;
+
+/**
+ * This class stores the instances in the KG.
+ * @author Jiaru Bai (jb2197@cam.ac.uk)
+ *
+ */
+public class InstanceDatabase {
+    public static String DerivedDifference = null;
+}
