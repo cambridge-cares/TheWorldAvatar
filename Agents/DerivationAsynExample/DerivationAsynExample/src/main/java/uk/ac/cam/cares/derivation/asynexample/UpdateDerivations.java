deb21b6614a074e161504338504afcb85e60cc80
Jiaru Bai <jb2197@cam.ac.uk>
2021-11-28 21:10:17 +0000

    139-dev-minimum-working-example-of-asyn-agent: added UpdateDerivations class to update the chain of derivations.

---
+++ Agents/DerivationAsynExample/DerivationAsynExample/src/main/java/uk/ac/cam/cares/derivation/asynexample/UpdateDerivations.java
@@ -0,0 +1,45 @@
+package uk.ac.cam.cares.derivation.asynexample;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+import uk.ac.cam.cares.jps.base.agent.JPSAgent;
+import uk.ac.cam.cares.jps.base.derivation.DerivationClient;
+import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
+
+/**
+ * This is the entry point of the asynchronous derivation example.
+ * @author Jiaru Bai (jb2197@cam.ac.uk)
+ *
+ */
+@WebServlet(urlPatterns = {"/UpdateDerivations"})
+public class UpdateDerivations extends JPSAgent {
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger LOGGER = LogManager.getLogger(UpdateDerivations.class);
+	
+	@Override
+	public JSONObject processRequestParameters(JSONObject requestParams) {
+		Config.initProperties();
+		RemoteStoreClient storeClient = new RemoteStoreClient(Config.sparqlEndpointQuery, Config.sparqlEndpointUpdate);
+		SparqlClient sparqlClient = new SparqlClient(storeClient);
+		DerivationClient devClient = new DerivationClient(storeClient);
+		
+		if (InstanceDatabase.DerivedDifference == null) {
+			String difference = sparqlClient.getDifferenceIRI();
+			InstanceDatabase.DerivedDifference = devClient.getDerivationOf(difference);
+		}
+		
+		devClient.updateDerivationAsyn(InstanceDatabase.DerivedDifference);
+		
+		String res_msg = "Checked derivation of difference <" + InstanceDatabase.DerivedDifference + ">, the update should be done in a few minutes";
+		LOGGER.info(res_msg);
+		
+		JSONObject response = new JSONObject();
+		response.put("status", res_msg);
+		return response;
+	}
+}
