dff36fbdfb18b595db3c1b412ec7d2f99b0869ce
Feroz Farazi <msff2@cam.ac.uk>
2021-12-01 21:22:47 +0000

    1161-dev-gas-grid-input-agent: Extended code to automatically create a namespace in Blazegraph and a database in PostgreSQL.

--- Agents/GasGridAgent/src/gasgridagent/kg_utils.py
+++ Agents/GasGridAgent/src/gasgridagent/kg_utils.py
@@ -4,6 +4,8 @@
 
 import os
 import json
+import psycopg2
+import requests
 from pathlib import Path
 from configobj import ConfigObj
 
@@ -16,6 +18,8 @@ PROPERTIES_FILE = os.path.abspath(os.path.join(Path(__file__).parent, "..", "res
 
 # Initialise global variables to be read from properties file
 global QUERY_ENDPOINT, UPDATE_ENDPOINT, OUTPUT_DIR
+global MAPBOX_APIKEY
+global DB_URL, DB_USER, DB_PASSWORD
 
 # Define format of time series time entries: Year-Month-Day T hour:minute:second Z
 FORMAT = '%Y-%m-%dT%H:%M:%SZ'
@@ -41,12 +45,12 @@ def read_properties_file(filepath):
     """
 
     # Define global scope for global variables
-    global OUTPUT_DIR, QUERY_ENDPOINT, UPDATE_ENDPOINT
-
+    global OUTPUT_DIR, QUERY_ENDPOINT, UPDATE_ENDPOINT, MAPBOX_APIKEY, DB_URL, DB_USER, DB_PASSWORD
+	
     # Read properties file
     props = ConfigObj(filepath)
 
-    # Extract output directory for JSON file containing retrieved time series data from KG
+    # Read output directory for JSON file containing retrieved time series data from KG
     try:
         OUTPUT_DIR = props['output.directory']
     except KeyError:
@@ -54,7 +58,7 @@ def read_properties_file(filepath):
     if OUTPUT_DIR == '':
         raise KeyError('No "output.directory" value has been provided in properties file: ' + filepath)
 
-    # Extract SPARQL Query endpoint of KG
+    # Read SPARQL Query endpoint of KG
     try:
         QUERY_ENDPOINT = props['sparql.query.endpoint']
     except KeyError:
@@ -62,7 +66,7 @@ def read_properties_file(filepath):
     if QUERY_ENDPOINT == '':
         raise KeyError('No "sparql.query.endpoint" value has been provided in properties file: ' + filepath)
 
-    # Extract SPARQL Update endpoint of KG
+    # Read SPARQL Update endpoint of KG
     try:
         UPDATE_ENDPOINT = props['sparql.update.endpoint']
     except KeyError:
@@ -70,6 +74,37 @@ def read_properties_file(filepath):
     if UPDATE_ENDPOINT == '':
         raise KeyError('No "sparql.update.endpoint" value has been provided in properties file: ' + filepath)
 
+    # Read Mapbox API key (required for visualisation using DTVF)
+    try:
+        MAPBOX_APIKEY = props['mapbox.apiKey']
+    except KeyError:
+        raise KeyError('Key "mapbox.apiKey" is missing in properties file: ' + filepath)
+    if MAPBOX_APIKEY == '':
+        raise KeyError('No "mapbox.apiKey" value has been provided in properties file: ' + filepath)
+
+    # Read PostgreSQL database URL
+    try:
+        DB_URL = props['db.url']
+    except KeyError:
+        raise KeyError('Key "db.url" is missing in properties file: ' + filepath)
+    if DB_URL == '':
+        raise KeyError('No "db.url" value has been provided in properties file: ' + filepath)
+
+    # Read PostgreSQL database username
+    try:
+        DB_USER = props['db.user']
+    except KeyError:
+        raise KeyError('Key "db.user" is missing in properties file: ' + filepath)
+    if DB_USER == '':
+        raise KeyError('No "db.user" value has been provided in properties file: ' + filepath)
+
+    # Read PostgreSQL database password
+    try:
+        DB_PASSWORD = props['db.password']
+    except KeyError:
+        raise KeyError('Key "db.password" is missing in properties file: '+ filepath)
+    if DB_PASSWORD == '':
+        raise KeyError('No "db.user" value has been provided in properties file: ' + filepath)
 
 def create_sparql_prefix(abbreviation):
     """
@@ -99,6 +134,101 @@ def create_sparql_prefix(abbreviation):
 
     return 'PREFIX ' + abbreviation + ': ' + iri + ' '
 
+def set_mapbox_apikey():
+    """
+        Populates Mapbox API key field in 'index.html' file within the dtvf_visualisation folder
+        with API key provided in properties file
+    """
+
+    # Define global scope for global variables
+    global MAPBOX_APIKEY, OUTPUT_DIR
+
+    # Get filepath to "index.html" file in DTVF folder
+    fp = Path.joinpath(Path(OUTPUT_DIR).parent, 'index.html')
+
+    # Read current "index.html" file
+    with open(fp, 'r') as f:
+        old_html = f.read()
+
+    # Find old API key
+    match = re.search(r'mapboxAPI\s*=\s*".*"', old_html)
+    old_key = match.group()
+    # Replace old/default API key with actual one
+    new_key = 'mapboxAPI = "{}"'.format(MAPBOX_APIKEY)
+    new_html = old_html.replace(old_key, new_key)
+
+    # Write updated "index.html" file
+    with open(fp, 'w') as f:
+        f.write(new_html)
+
+def create_postgres_db():
+    """
+        Creates PostgreSQL database with name as specified in db.url field in the properties file
+        Please note: The PostgreSQL server is assumed to be available at DEFAULT HOST (i.e. localhost)
+        and PORT (i.e. 5432)
+    """
+
+    # Extract database name from DB URL provided in properties file
+    # (for details see: https://www.postgresql.org/docs/7.4/jdbc-use.html)
+    db_name = DB_URL.split(':')[-1]
+
+    # Create PostgreSQL database with extracted name
+    # (for details see: https://www.psycopg.org/docs/module.html)
+    conn = None
+    try:
+        # Connect to PostgreSQL server (via DEFAULT host and port)
+        conn = psycopg2.connect(user=DB_USER, password=DB_PASSWORD, host='localhost')
+        conn.autocommit = True
+        # Create cursor object
+        cur = conn.cursor()
+        # Create db table
+        cur.execute('CREATE DATABASE ' + db_name)
+        # Close communication with the PostgreSQL database server
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def create_blazegraph_namespace():
+    """
+        Creates Blazegraph namespace with name as specified in SPARQL endpoints
+    """
+
+    # Extract Blazegraph REST API url from SPARQL endpoint
+    url = UPDATE_ENDPOINT[:UPDATE_ENDPOINT.find('namespace') + len('namespace')]
+
+    # Extract name for new namespace from SPARQL endpoint
+    ns = UPDATE_ENDPOINT[UPDATE_ENDPOINT.find('namespace') + len('namespace') + 1:]
+    ns = ns[:ns.find('/')]
+
+    # Define POST request header and payload
+    header = {'Content-type': 'text/plain'}
+
+    payload = 'com.bigdata.rdf.store.AbstractTripleStore.textIndex=false\r\n' \
+              'com.bigdata.rdf.store.AbstractTripleStore.axiomsClass=com.bigdata.rdf.axioms.NoAxioms\r\n' \
+              'com.bigdata.rdf.sail.isolatableIndices=false\r\n' \
+              'com.bigdata.rdf.sail.truthMaintenance=false\r\n' \
+              'com.bigdata.rdf.store.AbstractTripleStore.justify=false\r\n' \
+              'com.bigdata.namespace.{}.spo.com.bigdata.btree.BTree.branchingFactor=1024\r\n' \
+              'com.bigdata.rdf.sail.namespace={}\r\n' \
+              'com.bigdata.rdf.store.AbstractTripleStore.quads=false\r\n' \
+              'com.bigdata.namespace.{}.lex.com.bigdata.btree.BTree.branchingFactor=400\r\n' \
+              'com.bigdata.rdf.store.AbstractTripleStore.geoSpatial=true\r\n' \
+              'com.bigdata.rdf.store.AbstractTripleStore.statementIdentifiers=false'.format(ns, ns, ns)
+
+    # Post the request
+    response = requests.post(url, payload, headers=header)
+
+    if response.status_code == 201:
+        print('New namespace \"{}\" successfully created.\n'.format(ns))
+    elif response.status_code == 409:
+        print('Namespace \"{}\" already exists\n'.format(ns))
+    else:
+        print('Request status code: {}\n'.format(response.status_code))
+
 
 def get_instantiated_terminals(endpoint):
     """
@@ -118,7 +248,7 @@ def get_instantiated_terminals(endpoint):
     # Initialise remote KG client with only query endpoint specified
     print("Getting instantiated terminals from SPARQL endpoint:", endpoint)
     KGClient = jpsBaseLibView.RemoteStoreClient(endpoint)
-	
+
     # Perform SPARQL query (see StoreRouter in jps-base-lib for further details)
     query = create_sparql_prefix('comp') + \
             create_sparql_prefix('rdf') + \
