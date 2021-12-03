414ab958096797599422977e59655d8529d7a15d
szhang012 <szhang012@e.ntu.edu.sg>
2021-12-03 14:46:25 +0800

    httpcaller
    Add module for send HTTP request to other agents.

---
+++ Agents/OntoMatchAgent/ontomatch/httpCaller/httpcaller.py
@@ -0,0 +1,27 @@
+'''
+module for http requesting other agents
+'''
+
+from ontomatch.httpCaller.yellowpage import AGENT_IRIS
+import requests
+class caller():
+    def __init__(self):
+        pass
+
+    def callAgent(self, agentName, params, method = "POST"):
+        if agentName not in AGENT_IRIS:
+            raise KeyError("Agent not found")
+        IRI = AGENT_IRIS[agentName]
+        if method == "POST":
+            r = requests.post(IRI,params=params)
+        elif method == "GET":
+            r = requests.post(IRI,params=params)
+        else:
+            raise Exception("Invalid request. Invalid http method")
+        if r.status_code != 200:
+            raise ConnectionError()
+        data = r.json
+        #{"result":{'': ,...}}
+        return data['result']
+
+
---
+++ Agents/OntoMatchAgent/tests/test_http_calling.py
@@ -0,0 +1,43 @@
+import logging
+import unittest
+import json
+import ontomatch.blocking
+import ontomatch.knowledge.enrichment
+import tests.utils_for_testing
+import ontomatch.utils.util
+class TestGeocoding(unittest.TestCase):
+
+    def test_enrichement_nohttp(self):
+        addr = "../data/power_plant_DEU/kwl.ttl"
+        add_knowledge = "ontomatch.knowledge.geocoding"
+        enriched, handle = ontomatch.knowledge.enrichment.Agent().start(addr, add_knowledge, http=False)
+        print(enriched)
+        print(handle)
+
+    def test_blackboard_write_nohttp(self):
+        addr = './conf/conf_power_plant_DEU_auto.json'
+        params = ontomatch.utils.util.read_json_from_path(addr)
+        params_str = json.dumps(params)
+        config_handle = ontomatch.utils.util.call_agent_blackboard_for_writing(addr, params_str, http=False)
+        print(config_handle)
+        assert "_conf_conf_power_plant_DEU_auto.json" in config_handle
+
+    def test_blackboard_read_nohttp(self):
+        handle = '._conf_conf_power_plant_DEU_auto.json_1638437022.391991'
+        object = ontomatch.utils.util.call_agent_blackboard_for_reading(handle, http=False)
+        print("object")
+        #TODO: assert content
+
+    def test_blackboard_write_http(self):
+        #TODO: bug, writing directory wrong when calling from flask
+        addr = './conf/conf_power_plant_DEU_auto.json'
+        params = ontomatch.utils.util.read_json_from_path(addr)
+        params_str = json.dumps(params)
+        config_handle = ontomatch.utils.util.call_agent_blackboard_for_writing(addr, params_str, http=True)
+        assert "_conf_conf_power_plant_DEU_auto.json" in config_handle
+
+    def test_blackboard_read_http(self):
+        handle = '._conf_conf_power_plant_DEU_auto.json_1638437022.391991'
+        object = ontomatch.utils.util.call_agent_blackboard_for_reading(handle, http=True)
+        print("object")
+        #TODO: assert content
