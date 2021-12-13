54db5799bc2551bd7b5d18b6aae33360580ae7c2
Andreas <andreas.eibeck@t-online.de>
2021-12-13 10:54:54 +0100

    adapted paths in test_http_calling.py

--- Agents/OntoMatchAgent/tests/test_http_calling.py
+++ Agents/OntoMatchAgent/tests/test_http_calling.py
@@ -5,10 +5,10 @@ import ontomatch.blocking
 import ontomatch.knowledge.enrichment
 import tests.utils_for_testing
 import ontomatch.utils.util
-class TestGeocoding(unittest.TestCase):
+class TestGeocoding(tests.utils_for_testing.TestCaseOntoMatch):
 
     def test_enrichement_nohttp(self):
-        addr = "../data/power_plant_DEU/kwl.ttl"
+        addr = "./data/power_plant_DEU/kwl.ttl"
         add_knowledge = "ontomatch.knowledge.geocoding"
         enriched, handle = ontomatch.knowledge.enrichment.Agent().start(addr, add_knowledge, http=False)
         print(enriched)
@@ -17,7 +17,7 @@ class TestGeocoding(unittest.TestCase):
         assert handle is not None
 
     def test_blackboard_write_read_nohttp(self):
-        addr = './conf/conf_power_plant_DEU_auto.json'
+        addr = './tests/conf/conf_power_plant_DEU_auto.json'
         params = ontomatch.utils.util.read_json_from_path(addr)
         params_str = json.dumps(params)
         config_handle = ontomatch.utils.util.call_agent_blackboard_for_writing(addr, params_str, http=False)
