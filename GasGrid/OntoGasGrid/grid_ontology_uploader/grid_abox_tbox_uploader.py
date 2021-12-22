4bfbf061ec09c51c4d2a18cf1890ec0eccc33cac
Feroz Farazi <msff2@cam.ac.uk>
2021-12-22 20:30:04 +0000

    1161-dev-gas-grid-input-agent: Programmed to call the uploadOntology() method to upload grid components and pipelines to the knowledge graph.

--- GasGrid/OntoGasGrid/grid_ontology_uploader/grid_abox_tbox_uploader.py
+++ GasGrid/OntoGasGrid/grid_ontology_uploader/grid_abox_tbox_uploader.py
@@ -4,6 +4,7 @@
 ##########################################
 
 import property_reader as pr
+from jpsSingletons import jpsBaseLibView
 
 """This module is designed to upload ABox and TBox files to the knowledge graph."""
 
@@ -11,14 +12,18 @@ def upload_ontology(triple_store_url, namespace, ontology_file_path):
     """
     This function uploads an ontology to a triple store.
     """
-    print('test function')
+    # Initialise remote KG client with only query endpoint specified
+    KGClient = jpsBaseLibView.KnowledgeRepository()
+    print('Uploading ', ontology_file_path)
+    KGClient.uploadOntology(triple_store_url, namespace, ontology_file_path)
+    print('Successfully uploaded:', ontology_file_path)    
 
 def upload_gas_grid_ontologies():
     """
     This function uploads all ontologies required for the Gas Grid Agent to the knowledge graph.
     """
-    upload_ontology(pr.getTripleStoreURL, pr.getTripleStoreNamespace, pr.getGridComponentABoxFilePath)
-    upload_ontology(pr.getTripleStoreURL, pr.getTripleStoreNamespace, pr.getPipelineABoxFilePath)
+    upload_ontology(pr.getTripleStoreURL(), pr.getTripleStoreNamespace(), pr.getGridComponentABoxFilePath())
+    upload_ontology(pr.getTripleStoreURL(), pr.getTripleStoreNamespace(), pr.getPipelineABoxFilePath())
 
 if __name__ == '__main__':
     """Calls the RDF conversion function"""
