8cc1f6e32c3625b4ae080818dab4c04b2c5b56cb
jb2197 <jb2197@cam.ac.uk>
2022-01-20 23:22:32 +0000

    142-dev-python-async-agent: modified setup.py file for pyasyncagent package.

--- JPS_BASE_LIB/python_async_agent/setup.py
+++ JPS_BASE_LIB/python_async_agent/setup.py
@@ -2,20 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyasyncagent',
-    version='0.0.2',
+    version='0.0.3',
     author='Jiaru Bai',
     author_email='jb2197@cam.ac.uk',
     license='MIT',
     python_requires='>=3.5',
     description="pyasyncagent is a python wrapper for asynchronous agents as part of The World Avatar project.",
-    # py_modules=['AsyncAgent'], #
-    # package_dir={'': 'pyasyncagent'}, #
+    url="https://github.com/cambridge-cares/TheWorldAvatar/tree/142-dev-python-async-agent/JPS_BASE_LIB/python_async_agent",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=('tests')),
-    # url="https://github.com/cambridge-cares/TheWorldAvatar/tree/142-dev-python-async-agent/JPS_BASE_LIB/python_async_agent",
-    # long_description=open('README.md').read(),
-    # long_description_content_type="text/markdown",
-    # packages=find_packages(exclude=('tests')),
-    # install_requires= ['py4jps>=1.0.6, <=1.0.14'],
-    # include_package_data= True,
-    # entry_points={}
+    install_requires=['py4jps>=1.0.15', 'flask==1.1.2', 'gunicorn==20.0.4', 'Flask-APScheduler', 'rdflib'],
+    dependency_links=['git+https://github.com/cambridge-cares/TheWorldAvatar@develop#subdirectory=Agents/utils/python-utils'],
+    extras_require={
+        "dev": [
+            "testcontainers>=3.4.2",
+            "pytest>=6.2.3",
+        ],
+    },
+    include_package_data= True
 )
