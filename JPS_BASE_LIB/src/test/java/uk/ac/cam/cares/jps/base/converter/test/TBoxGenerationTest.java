7c3ee26971f5ac60362b096cb6f4b788a1c0e3a6
Feroz Farazi <msff2@cam.ac.uk>
2022-01-20 00:53:56 +0000

    1204-add-new-features-to-tbox-generation-code: Defined a method for generating an OWL ontology from a sample TBox CSV template file by calling TBoxGenerator methods. Defined another method to read a reference ontology from the file system.

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/converter/test/TBoxGenerationTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/converter/test/TBoxGenerationTest.java
@@ -1,10 +1,24 @@
 package uk.ac.cam.cares.jps.base.converter.test;
 
-import org.junit.jupiter.api.Test;
+import java.net.URL;
 
+import org.junit.jupiter.api.Test;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+import uk.ac.cam.cares.jps.base.converter.ITBoxGeneration;
+import uk.ac.cam.cares.jps.base.converter.TBoxGeneration;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 
 public class TBoxGenerationTest {
+	
+	private final String FILE_EXTENSION_CSV = ".csv";
+	private final String FILE_EXTENSION_OWL = ".owl";
+	
+	static ITBoxGeneration iTBoxGeneration = new TBoxGeneration();
 	
 	@Test
 	public void testClassHierachy(){
@@ -39,6 +53,47 @@ public class TBoxGenerationTest {
 	@Test
 	public void testEquivalentRelation(){
 		
+	}
+
+	/**
+	 * Reads an ontology from a file located in the file system.
+	 * 
+	 * @param referenceOntologyFilePath the absolute path of the ontology file.
+	 * @return
+	 */
+	private OWLOntology readReferenceOntology(String referenceOntologyFile){
+		URL referenceOWLFile = getClass().getClassLoader().getResource(referenceOntologyFile);
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology referenceOntology = null;
+		try {
+			referenceOntology = manager.loadOntology(IRI.create(addFileProtocol(referenceOWLFile.getPath())));
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		} catch (JPSRuntimeException e) {
+			e.printStackTrace();
+		}
+		return referenceOntology;
+	}
+
+	/**
+	 * Converts a TBox CSV Template formatted file into OWL. 
+	 * 
+	 * @param tBoxCSVTemplateFilePath the absolute path of the TBox CSV file.
+	 * @return
+	 */
+	private OWLOntology generateOntology(String tBoxCSVTemplateFilePath){
+		URL tBoxCSVTemplateFileURL = getClass().getClassLoader().getResource(tBoxCSVTemplateFilePath);
+		iTBoxGeneration.generateTBox(tBoxCSVTemplateFileURL.getPath());
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology generatedOntology = null;
+		try {
+			generatedOntology = manager.loadOntology(IRI.create(addFileProtocol(tBoxCSVTemplateFileURL.getPath().replace(FILE_EXTENSION_CSV, FILE_EXTENSION_OWL))));
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		} catch (JPSRuntimeException e) {
+			e.printStackTrace();
+		}
+		return generatedOntology;
 	}
 	
 	/**
