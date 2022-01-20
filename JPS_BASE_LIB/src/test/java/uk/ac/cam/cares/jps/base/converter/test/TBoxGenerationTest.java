4b52e91257a8a69997b5e1188b48454d7e122ed7
Feroz Farazi <msff2@cam.ac.uk>
2022-01-20 00:47:08 +0000

    1204-add-new-features-to-tbox-generation-code: Defined a method to add file protocol (file:) to a file path.

--- JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/converter/test/TBoxGenerationTest.java
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/converter/test/TBoxGenerationTest.java
@@ -2,6 +2,8 @@ package uk.ac.cam.cares.jps.base.converter.test;
 
 import org.junit.jupiter.api.Test;
 
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+
 public class TBoxGenerationTest {
 	
 	@Test
@@ -37,5 +39,25 @@ public class TBoxGenerationTest {
 	@Test
 	public void testEquivalentRelation(){
 		
+	}
+	
+	/**
+	 * Adds the protocol 'file:' at the beginning of a file path
+	 * to form a URL that can be used in an OWL file as a URL.
+	 * 
+	 * @param path an absolute file path that needs to be converted
+	 * to a URL that can be used in an OWL file.
+	 * @return an OWL file formatted URL.
+	 * @throws JPSRuntimeException a specialised exception designed to deal with
+	 * errors at runtime in JPS libraries.
+	 */
+	public static String addFileProtocol(String path) throws JPSRuntimeException{
+		if(path==null){
+			throw new JPSRuntimeException("A null input path has been provided.");
+		}
+		if(!path.contains("file:")){
+			path = "file:"+path;
+		}
+		return path;
 	}
 }
