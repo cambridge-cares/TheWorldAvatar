38572356ea72b2c6e85a15a523450fe9a3dfcc6e
jb2197 <jb2197@cam.ac.uk>
2022-01-20 22:54:04 +0000

    142-dev-python-async-agent: modified random_agent to use agentInputs passed into setupJob method according to the updated design in the commit 9e8427962b36e0881f0043c6721157eadde586e4.

--- JPS_BASE_LIB/python_async_agent/tests/random_agent.py
+++ JPS_BASE_LIB/python_async_agent/tests/random_agent.py
@@ -1,22 +1,22 @@
 import random
 
 from pyasyncagent import AsyncAgent
-from tests.sparql_client_for_test import PySparqlClientForTest
+from tests.sparql_client_for_test import PySparqlClientForTest, RANDOM_EXAMPLE_NUMOFPOINTS, RANDOM_EXAMPLE_UPPERLIMIT, RANDOM_EXAMPLE_LOWERLIMIT
 
 class RandomAgent(AsyncAgent):
     def setupJob(self, agentInputs) -> list:
         sparql_client = PySparqlClientForTest(self.kgUrl, self.kgUrl)
 
         # Get the number of points to be generated
-        numofpoints_iri = sparql_client.getNumOfPoints()
+        numofpoints_iri = agentInputs[RANDOM_EXAMPLE_NUMOFPOINTS]
         numofpoints = sparql_client.getValue(numofpoints_iri)
 
         # Get the upper limit
-        upperlimit_iri = sparql_client.getUpperLimit()
+        upperlimit_iri = agentInputs[RANDOM_EXAMPLE_UPPERLIMIT]
         upperlimit = sparql_client.getValue(upperlimit_iri)
 
         # Get the lower limit
-        lowerlimit_iri = sparql_client.getLowerLimit()
+        lowerlimit_iri = agentInputs[RANDOM_EXAMPLE_LOWERLIMIT]
         lowerlimit = sparql_client.getValue(lowerlimit_iri)
 
         new_points = []
@@ -24,4 +24,5 @@ class RandomAgent(AsyncAgent):
             new_points.append(random.randint(lowerlimit, upperlimit))
 
         new_created_iri = sparql_client.createListOfPoints(new_points)
+
         return [new_created_iri]
