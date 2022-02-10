cecfe7a85a725e8d0a779bce21a28f1df3820d02
jb2197 <jb2197@cam.ac.uk>
2022-02-10 14:46:43 +0000

    134-dev-lab-equipment-digital-twin: [chemistry-and-robots util] modified BaseOntology init method to throw better exception when namespace_for_init or/and clz is/are not provided when creating an instance, also added unit test for initialising instances of BaseOntology.

--- Agents/utils/chemistry-and-robots/chemistry_and_robots/data_model/base_ontology.py
+++ Agents/utils/chemistry-and-robots/chemistry_and_robots/data_model/base_ontology.py
@@ -5,8 +5,13 @@ from pyasyncagent.data_model.iris import *
 from pyasyncagent.data_model.utils import *
 
 class InstanceIRIInitialisationError(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
+    clz_missing = 'IRI of clz is missing when initialising an instance of: '
+    namespace_for_init_missing = 'IRI of namespace_for_init is missing when initialising an instance of: '
+    both_clz_and_namespace_missing = 'Both IRI of clz and namespace_for_init are missing when initialising an instance of: '
+
+    def __init__(self, value: str, message: str) -> None:
+        full_message = message + value
+        super().__init__(full_message)
 
 class BaseOntology(pydantic.BaseModel):
     instance_iri: str = None
@@ -15,11 +20,15 @@ class BaseOntology(pydantic.BaseModel):
 
     def __init__(__pydantic_self__, **data: Any) -> None:
         if data['instance_iri'] == INSTANCE_IRI_TO_BE_INITIALISED:
-            if data['namespace_for_init'] is None:
-                raise InstanceIRIInitialisationError(f"A namespace should be provided for initialising a/an {data['clz'] if 'clz' in data else __pydantic_self__.__class__.__fields__['clz'].default} instance.")
+            _clz = data['clz'] if 'clz' in data else __pydantic_self__.__class__.__fields__['clz'].default
+            _namespace_for_init = data['namespace_for_init'] if 'namespace_for_init' in data else None
+            if _clz is None and _namespace_for_init is None:
+                raise InstanceIRIInitialisationError(value=str(__pydantic_self__.__class__), message=InstanceIRIInitialisationError.both_clz_and_namespace_missing)
+            elif _clz is None:
+                raise InstanceIRIInitialisationError(value=str(__pydantic_self__.__class__), message=InstanceIRIInitialisationError.clz_missing)
+            elif _namespace_for_init is None:
+                raise InstanceIRIInitialisationError(value=_clz, message=InstanceIRIInitialisationError.namespace_for_init_missing)
             else:
-                if 'clz' not in data:
-                    data['instance_iri'] = initialiseInstanceIRI(data['namespace_for_init'], __pydantic_self__.__class__.__fields__['clz'].default)
-                else:
-                    data['instance_iri'] = initialiseInstanceIRI(data['namespace_for_init'], data['clz'])
+                data['instance_iri'] = initialiseInstanceIRI(data['namespace_for_init'], _clz)
+
         super().__init__(**data)
