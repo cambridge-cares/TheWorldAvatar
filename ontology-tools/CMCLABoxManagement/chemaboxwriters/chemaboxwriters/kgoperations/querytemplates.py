ab4b741d1c291c17e3ca8f7ef4a34b7a332bc0a8
dln22 <danieln@cmclinnovations.com>
2022-02-17 08:29:13 +0000

    Fixed species iri query based on inchi.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/kgoperations/querytemplates.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/kgoperations/querytemplates.py
@@ -1,38 +1,57 @@
 from chemaboxwriters.kgoperations.queryendpoints import SPARQL_ENDPOINTS
 from chemaboxwriters.kgoperations.querykg import querykg
-from py4j.java_gateway import Py4JJavaError
+#from py4j.java_gateway import Py4JJavaError
 import logging
+import re
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 logger = logging.getLogger(__name__)
 
 
 def spec_inchi_query(inchi_string):
+
+    inchi_string_standard = re.sub("^InChI=1(?!S)","InChI=1S", inchi_string)
+    inchi_string_non_standard = re.sub("^InChI=1S","InChI=1", inchi_string)
+
     query = """
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
     PREFIX OntoSpecies: <http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    SELECT ?speciesIRI ?Inchi
+    SELECT ?speciesIRI
     WHERE
     {
-    ?speciesIRI rdf:type OntoSpecies:Species .
-    ?speciesIRI OntoSpecies:inChI ?Inchi .
-    FILTER REGEX(str(?Inchi), REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(""" + '"' + inchi_string + '"' + """, "InChI=1S", "InChI=1"), "/t.+", ""), "/b.+", ""), "\\\\(", "\\\\\\\\("), "\\\\)", "\\\\\\\\)"), "i")
+    OPTIONAL {
+           ?speciesIRI rdf:type OntoSpecies:Species .
+           ?speciesIRI OntoSpecies:inChI "#inchi_string_standard#"^^xsd:string .}
+    OPTIONAL {
+           ?speciesIRI rdf:type OntoSpecies:Species .
+           ?speciesIRI OntoSpecies:inChI "#inchi_string_non_standard#"^^xsd:string .}
     }
     """
-    #print(query)
+    query = query.replace('#inchi_string_standard#', inchi_string_standard)\
+                 .replace('#inchi_string_non_standard#', inchi_string_non_standard)
     return query
 
 def get_species_iri(inchi):
     #Query OntoSpecies to find Species IRI that corresponds to a given InChI.
-    target = None
+    speciesIRI = None
     results = []
     try:
-        results  = querykg(SPARQL_ENDPOINTS['ontospecies'], spec_inchi_query(inchi)) #query_endpoint(endpoint, spec_inchi_query(inchi))
-    except Py4JJavaError:        
+        #results  = querykg(SPARQL_ENDPOINTS['ontospecies'], spec_inchi_query(inchi)) #query_endpoint(endpoint, spec_inchi_query(inchi))
+        #TODO, find a way to fix this query on the java side so that it works as expected
+        sparql = SPARQLWrapper(SPARQL_ENDPOINTS['ontospecies'])
+        sparql.setReturnFormat(JSON)
+        query = spec_inchi_query(inchi)
+        sparql.setQuery(query)
+        results = sparql.queryAndConvert()
+    except Exception:
         logger.warning("Warning: get_species_iri query failed.")
     if results:
-        if 'speciesIRI' in results[0].keys():
-            target = results[0]['speciesIRI']
-    return target
+        bindings = results['results']['bindings'][0] # type: ignore
+        target = bindings.get('speciesIRI')
+        if target is not None:
+            speciesIRI = target.get('value')
+    return speciesIRI
 
 def get_assemblyModel(modularity, planarity, gbu_number, symmetry):
     #queries the assembly model of a particular MOP.
