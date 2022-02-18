4902e3b1d4e918b48640aa56580fc821bdb25fdd
dln22 <danieln@cmclinnovations.com>
2022-02-18 15:54:11 +0000

    Redefined generic QC_LOG and CSV handlers.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/common/handlers.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/common/handlers.py
@@ -1,90 +1,95 @@
-from chemaboxwriters.common.base import get_handler, StageHandler
-from compchemparser.helpers.utils import jsonStringToFile
-from chemaboxwriters.common.globals import QUANTUM_CALC_TAG
+from chemaboxwriters.common.handler import IHandler
 from compchemparser.app import parseLog
-from chemaboxwriters.common.utilsfunc import csv2rdf_wrapper
-import chemutils.ioutils.ioutils as ioutils
-from typing import Callable
+import chemaboxwriters.common.utilsfunc as utilsfunc
+import entityrdfizer.aboxgenerator.ABoxTemplateCSVFileToRDF as entityrdfizer
+from typing import List
+from enum import Enum
+from chemaboxwriters.common.globals import aboxStages
+from dataclasses import dataclass, field
+import json
 
-def get_qc_log_to_qc_json_handler(
-    *handler_args,
-    **handler_kwargs
-    )->StageHandler:
 
-    inStage = f"{QUANTUM_CALC_TAG}_LOG"
-    outStage = f"{QUANTUM_CALC_TAG}_JSON"
+@dataclass
+class QC_LOG_TO_QC_JSON_Handler(IHandler):
+    """Gaussian quantum claculations log files handler.
+    Inputs: List of gaussian log file paths
+    Outputs: List of parsed json file paths
+    """
 
-    handler = get_handler(
-                    inStages=[inStage],
-                    outStage=outStage,
-                    handlerFunc=parseLog,
-                    fileWriter=jsonStringToFile,
-                    *handler_args,
-                    **handler_kwargs
-            )
-    return handler
+    name: str = field(default="QC_LOG_QC_JSON")
+    in_stages: List[Enum] = field(default_factory=lambda: [aboxStages.QC_LOG])
+    out_stage: Enum = field(default=aboxStages.QC_JSON)
 
-def get_json_to_json_handler(
-    inStageTag: str,
-    outStageTag: str,
-    handlerFunc: Callable,
-    *handler_args,
-    **handler_kwargs
-    )->StageHandler:
+    def handle_input(
+        self,
+        inputs: List[str],
+        out_dir: str,
+        input_type: Enum,
+        dry_run: bool,
+        **handler_kwargs,
+    ) -> List[str]:
 
-    inStage = f"{inStageTag}_JSON"
-    outStage = f"{outStageTag}_JSON"
+        outputs: List[str] = []
+        for cclog_file_path in inputs:
+            cclog_parsed_jobs = parseLog(cclog_file_path)
 
-    handler = get_handler(
-                    inStages=[inStage],
-                    outStage=outStage,
-                    handlerFunc=handlerFunc,
-                    fileWriter=jsonStringToFile,
-                    *handler_args,
-                    **handler_kwargs
-            )
-    return handler
-
-def get_json_to_csv_handler(
-    inStageTag: str,
-    outStageTag: str,
-    handlerFunc: Callable,
-    *handler_args,
-    **handler_kwargs
-    )->StageHandler:
-
-    inStage = f"{inStageTag}_JSON"
-    outStage = f"{outStageTag}_CSV"
-
-    handler = get_handler(
-                    inStages=[inStage],
-                    outStage=outStage,
-                    handlerFunc=handlerFunc,
-                    fileWriter=ioutils.writeFile,
-                    fileWriterKwargs={'newline':''},
-                    *handler_args,
-                    **handler_kwargs
+            if len(cclog_parsed_jobs) == 1:
+                out_file_path = utilsfunc.get_out_file_path(
+                    input_file_path=cclog_file_path,
+                    file_extension=self.out_stage.name.lower(),
+                    out_dir=out_dir,
+                    replace_last_ext=False,
                 )
-    return handler
-
-def get_csv_to_owl_handler(
-    inStageTag: str,
-    outStageTag: str,
-    *handler_args,
-    **handler_kwargs
-    )->StageHandler:
-
-    inStage = f"{inStageTag}_CSV"
-    outStage = f"{outStageTag}_OWL"
-
-    handler = get_handler(
-                    inStages=[inStage],
-                    outStage=outStage,
-                    handlerFunc=csv2rdf_wrapper,
-                    fileWriter=ioutils.writeFile,
-                    fileWriterKwargs={'newline':''},
-                    *handler_args,
-                    **handler_kwargs
+                utilsfunc.write_dict_to_file(
+                    dict_data=json.loads(cclog_parsed_jobs[0]), dest_path=out_file_path
                 )
-    return handler
+                outputs.append(out_file_path)
+            else:
+                for i, cclog_parsed_job in enumerate(cclog_parsed_jobs):
+                    out_file_path = utilsfunc.get_out_file_path(
+                        input_file_path=f"{cclog_file_path}_{i+1}",
+                        file_extension=self.out_stage.name.lower(),
+                        out_dir=out_dir,
+                        replace_last_ext=False,
+                    )
+                    utilsfunc.write_dict_to_file(
+                        dict_data=json.loads(cclog_parsed_job),
+                        dest_path=out_file_path,
+                    )
+                    outputs.append(out_file_path)
+        return outputs
 
+
+class CSV_TO_OWL_Handler(IHandler):
+    """Handler converting csv files to owl.
+    Inputs: List of csv file paths
+    Outputs: List of owl file paths
+    """
+
+    name: str = field(default="CSV_TO_OWL")
+
+    def handle_input(
+        self,
+        inputs: List[str],
+        out_dir: str,
+        input_type: Enum,
+        dry_run: bool,
+        **handler_kwargs,
+    ) -> List[str]:
+
+        outputs: List[str] = []
+        for csv_file_path in inputs:
+            out_file_path = utilsfunc.get_out_file_path(
+                input_file_path=csv_file_path,
+                file_extension=self.out_stage.name.lower(),
+                out_dir=out_dir,
+            )
+            with open(csv_file_path, "r") as csvfile:
+                owl_string = entityrdfizer.convert_csv_string_into_rdf(
+                    csv_string=csvfile.read()
+                )
+            with open(out_file_path, "w") as owlfile:
+                owlfile.write(owl_string)
+
+            outputs.append(out_file_path)
+        return outputs
