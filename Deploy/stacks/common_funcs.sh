8203adb724d5f5bb287ae98e8d80af28de6d6923
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-02-24 13:00:23 +0000

    dev-credo-geoserver: Fixed bug in the "write_env_file" function in the Docker stack "common_funcs.sh" script. Also renamed the Docker compose env file to the default name of ".env".

--- Deploy/stacks/common_funcs.sh
+++ Deploy/stacks/common_funcs.sh
@@ -168,9 +168,10 @@ write_env_file()
 
   if [ -e "$env_filename" ]; then
     rm "$env_filename"
-    if [ -e "${env_filename}.extra" ]; then
-      cp "${env_filename}.extra" "$env_filename"
-    fi
+  fi
+
+  if [ -e "${env_filename}.extra" ]; then
+    cp "${env_filename}.extra" "$env_filename"
   fi
 
   # Determine network name
