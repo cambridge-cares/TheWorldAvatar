bdd9438a72e38d48e03d6a9c828b808f6a84ca4b
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-03-07 14:07:39 +0000

    dev-ontop+geoserver: Improved the "generate_env_file.sh" script, including adding default settings.

--- Deploy/stacks/ontop+geoserver/scripts/generate_env_file.sh
+++ Deploy/stacks/ontop+geoserver/scripts/generate_env_file.sh
@@ -2,17 +2,37 @@
 
 script_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Read user ID from input
-read -p "Enter the last two numbers in your dafni user ID, or '10' if you're running this elsewhere: " user_id
+default_project_id="the_world_avatar"
 
-if [ ${#user_id} -ne 2 ]; then
-  echo "Input had length ${#user_id}; expected length 2, exiting..."
-  exit 1
+# Read user ID from input
+read -p "Enter a project identifier [${default_project_id}]: " project_id
+
+if [[ -z "${project_id}" ]]; then
+  project_id="${default_project_id}"
+else
+  if [[ -n ${project_id//[0-9a-zA-z_-]/} ]]; then
+    echo "Input contained characters that are not alphanumeric, '-' or '_', exiting..."
+    exit 2
+  fi
 fi
 
-if [[ -n ${user_id//[0-9]/} ]]; then
-  echo "Input contained non-numeric characters, exiting..."
-  exit 2
+default_port_prefix=231
+
+# Read user ID from input
+read -p "Enter three digits for port number prefix [${default_port_prefix}]: " port_prefix
+
+if [[ -z "${port_prefix}" ]]; then
+  port_prefix="${default_port_prefix}"
+else
+  if [ ${#port_prefix} -ne 3 ]; then
+    echo "Input had length ${#port_prefix}; expected length 3, exiting..."
+    exit 1
+  fi
+
+  if [[ -n ${port_prefix//[0-9]/} ]]; then
+    echo "Input contained non-numeric characters, exiting..."
+    exit 2
+  fi
 fi
 
 if command -v podman-compose &> /dev/null
@@ -23,7 +43,8 @@ else
 fi
 
 sed \
-    -e "s/USER_PORT_BASE/1$user_id/g" \
-    -e "s/CONTAINER_HOST/$container_host/" \
+    -e "s/PROJECT_ID/${project_id}/" \
+    -e "s/USER_PORT_BASE/${port_prefix}/" \
+    -e "s/CONTAINER_HOST/${container_host}/" \
     < "$script_dir/../.env_template" \
     > "$script_dir/../.env"
