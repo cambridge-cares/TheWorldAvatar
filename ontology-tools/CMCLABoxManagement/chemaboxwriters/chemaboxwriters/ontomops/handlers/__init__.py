c455bdde2af3524b392e0bf86d2fbb41640f9be8
dln22 <danieln@cmclinnovations.com>
2022-03-08 17:02:00 +0000

    Style changes in the init file.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontomops/handlers/__init__.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontomops/handlers/__init__.py
@@ -1,4 +1,6 @@
 from chemaboxwriters.ontomops.handlers.ominp_json_handler import (
     OMINP_JSON_TO_OM_JSON_Handler,
 )
-from chemaboxwriters.ontomops.handlers.om_json_handler import OM_JSON_TO_OM_CSV_Handler
+from chemaboxwriters.ontomops.handlers.om_json_handler import (
+    OM_JSON_TO_OM_CSV_Handler,
+)
