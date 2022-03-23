a3f020a38c4b6b4acc728703af87e96ef81f86e9
dln22 <danieln@cmclinnovations.com>
2022-03-23 15:50:58 +0000

    dev-chemaboxwriters: Simplified the aboxconfig module.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/common/aboxconfig.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/common/aboxconfig.py
@@ -1,4 +1,4 @@
-from typing import Dict, Tuple
+from typing import Dict, List
 import yaml
 import logging
 
@@ -7,87 +7,47 @@ logger = logging.getLogger(__name__)
 FILE_SERVER = "file server"
 TRIPLE_STORE = "triple store"
 
-UPLOAD_SETTINGS_KEY = "upload_settings"
-QUERY_SETTINGS_KEY = "query_settings"
+FS_UPLOAD_SETTINGS_KEY = "file_server_upload_settings"
+TS_UPLOAD_SETTINGS_KEY = "triple_store_upload_settings"
+QUERY_SETTINGS_KEY = "kg_query_endpoints"
 WRITERS_PREFIXES_KEY = "prefixes"
 HANDLER_KWARGS = "handler_kwargs"
 
-TRIPLE_STORE_SPARQL_ENDPOINT_KEY = "triple_store_sparql_endpoint"
-TRIPLE_STORE_SECRETS_FILE_KEY = "triple_store_secrets_file"
-TRIPLE_STORE_NO_AUTH_KEY = "triple_store_no_auth"
-FILE_SERVER_UPLOAD_ENDPOINT_KEY = "file_server_upload_endpoint"
-FILE_SERVER_SECRETS_FILE_KEY = "file_server_secrets_file"
-FILE_SERVER_SUBDIR_KEY = "file_server_subdir"
-FILE_SERVER_NO_AUTH_KEY = "file_server_no_auth"
-OSPECIES_QUERY_ENDPOINT_KEY = "ospecies_query_endpoint"
-OMOPS_QUERY_ENDPOINT_KEY = "omops_query_endpoint"
-OPSSCAN_QUERY_ENDPOINT_KEY = "opsscan_query_endpoint"
-OCOMPCHEM_QUERY_ENDPOINT_KEY = "ocompchem_query_endpoint"
-UPLOAD_TO_FILE_SERVER_KEY = "upload_to_file_server"
-UPLOAD_TO_TRIPLE_STORE_KEY = "upload_to_triple_store"
+
+URL_ENDPOINT_KEY = "url"
+AUTH_FILE_KEY = "auth_file"
+NO_AUTH_KEY = "no_auth"
+FS_SUBDIRS_KEY = "subdirs"
+UPL_FILE_TYPES = "upload_file_types"
 
 DEFAULT_CONFIG_KEYS = [
-    TRIPLE_STORE_SPARQL_ENDPOINT_KEY,
-    TRIPLE_STORE_SECRETS_FILE_KEY,
-    TRIPLE_STORE_NO_AUTH_KEY,
-    FILE_SERVER_UPLOAD_ENDPOINT_KEY,
-    FILE_SERVER_SECRETS_FILE_KEY,
-    FILE_SERVER_SUBDIR_KEY,
-    FILE_SERVER_NO_AUTH_KEY,
-    OCOMPCHEM_QUERY_ENDPOINT_KEY,
-    OSPECIES_QUERY_ENDPOINT_KEY,
-    OMOPS_QUERY_ENDPOINT_KEY,
-    OPSSCAN_QUERY_ENDPOINT_KEY,
-    UPLOAD_TO_FILE_SERVER_KEY,
-    UPLOAD_TO_TRIPLE_STORE_KEY,
+    URL_ENDPOINT_KEY,
+    AUTH_FILE_KEY,
+    NO_AUTH_KEY,
 ]
-HANDLERS_CONFIG_KEY = "handlers"
-
 
 CONFIG_GROUPS = [
-    UPLOAD_SETTINGS_KEY,
+    FS_UPLOAD_SETTINGS_KEY,
+    TS_UPLOAD_SETTINGS_KEY,
     QUERY_SETTINGS_KEY,
     WRITERS_PREFIXES_KEY,
     HANDLER_KWARGS,
 ]
 
 
-def get_pipeline_handler_configs(pipeline_name: str, config: Dict) -> Tuple[Dict, Dict]:
-
-    if HANDLERS_CONFIG_KEY in config:
-        logger.warning(
-            (
-                f"Found '{HANDLERS_CONFIG_KEY}' key in the default config section. "
-                "This will be omitted."
-            )
-        )
-        config.pop(HANDLERS_CONFIG_KEY)
-
-    pipeline_configs = config.get(pipeline_name, {})
-
-    _merge_config_groups(
-        merge_from=config,
-        merge_to=pipeline_configs,
-    )
-
-    handlers_config = pipeline_configs.pop(HANDLERS_CONFIG_KEY, {})
-    handlers = handlers_config.keys()
-    for handler in handlers:
-        _merge_config_groups(
-            merge_from=pipeline_configs, merge_to=handlers_config[handler]
-        )
-
-    return pipeline_configs, handlers_config
-
-
-def _merge_config_groups(merge_from: Dict, merge_to: Dict) -> None:
-    for key in CONFIG_GROUPS:
+def _merge_config_groups(
+    merge_from: Dict, merge_to: Dict, merge_fields: List[str]
+) -> None:
+    for key in merge_fields:
         _merge_config_field(merge_from=merge_from, merge_to=merge_to, merge_on=key)
 
 
 def _merge_config_field(merge_from: Dict, merge_to: Dict, merge_on: str) -> None:
-    merge_from_configs = merge_from.get(merge_on, {})
+    merge_from_configs = merge_from.get(merge_on)
     merge_to_configs = merge_to.get(merge_on, {})
+
+    if merge_from_configs is None:
+        return
 
     merge_to_configs = _merge_configs(
         merge_into=merge_to_configs,
@@ -108,3 +68,15 @@ def read_config_file(config_file: str) -> Dict:
             config_dict = yaml.safe_load(stream)
 
     return config_dict
+
+
+def cascade_configs(configs: Dict, cascade_fields: List[str] = CONFIG_GROUPS) -> None:
+    subconfigs = [name for name in configs.keys() if name not in cascade_fields]
+
+    for configs_group in subconfigs:
+        _merge_config_groups(
+            merge_from=configs,
+            merge_to=configs[configs_group],
+            merge_fields=cascade_fields,
+        )
+        cascade_configs(configs=configs[configs_group], cascade_fields=cascade_fields)
