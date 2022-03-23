99220a2661c4b3dbe16a6e71c0f9142faf189624
dln22 <danieln@cmclinnovations.com>
2022-03-23 15:52:11 +0000

    dev-chemaboxwriters: Modified assemble_pipeline module adter endpoints proxy removal.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/common/assemble_pipeline.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/common/assemble_pipeline.py
@@ -4,7 +4,6 @@ from chemaboxwriters.ontocompchem.pipeline import assemble_oc_pipeline, OC_PIPEL
 from chemaboxwriters.ontospecies.pipeline import assemble_os_pipeline, OS_PIPELINE
 from chemaboxwriters.ontomops.pipeline import assemble_omops_pipeline, OMOPS_PIPELINE
 from chemaboxwriters.ontopesscan.pipeline import assemble_ops_pipeline, OPS_PIPELINE
-import chemaboxwriters.common.endpoints_proxy as abconf
 from typing import Optional, Dict
 
 
@@ -12,28 +11,21 @@ def assemble_pipeline(
     pipeline_type: str,
     config_file: Optional[str] = None,
     config_dict: Optional[Dict] = None,
-    endpoints_proxy: Optional[abconf.Endpoints_proxy] = None,
 ) -> Pipeline:
 
-    if endpoints_proxy is None:
-        endpoints_proxy = abconf.get_endpoints_proxy()
-
     if pipeline_type.upper() == OC_PIPELINE.upper():
-
-        pipeline = assemble_oc_pipeline(endpoints_proxy=endpoints_proxy)
+        pipeline = assemble_oc_pipeline()
     elif pipeline_type.upper() == OS_PIPELINE.upper():
-        pipeline = assemble_os_pipeline(endpoints_proxy=endpoints_proxy)
+        pipeline = assemble_os_pipeline()
     elif pipeline_type.upper() == OMOPS_PIPELINE.upper():
-        pipeline = assemble_omops_pipeline(
-            endpoints_proxy=endpoints_proxy,
-        )
+        pipeline = assemble_omops_pipeline()
     elif pipeline_type.upper() == OPS_PIPELINE.upper():
-        pipeline = assemble_ops_pipeline(endpoints_proxy=endpoints_proxy)
+        pipeline = assemble_ops_pipeline()
     else:
         raise app_exc.UnsupportedPipeline("Selected pipeline type is not supported")
 
     if config_dict is not None:
-        pipeline.configure_from_dict(config=config_dict)
+        pipeline.configure_from_dict(config_dict=config_dict)
     elif config_file is not None:
         pipeline.configure_from_file(config_file=config_file)
     return pipeline
