2d71409dc26930934c557f128a37a963a3b78b3e
dln22 <danieln@cmclinnovations.com>
2022-03-23 15:58:11 +0000

    dev-chemaboxwriters: A few query template fixes.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/kgoperations/querytemplates.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/kgoperations/querytemplates.py
@@ -1,9 +1,8 @@
-from chemaboxwriters.kgoperations.querykg import querykg
+import chemaboxwriters.kgoperations.remotestore_client as rsc
 
 # from py4j.java_gateway import Py4JJavaError
 import logging
 import re
-from SPARQLWrapper import SPARQLWrapper, JSON
 from typing import Optional
 
 logger = logging.getLogger(__name__)
@@ -35,18 +34,17 @@ def spec_inchi_query(inchi_string):
     return query
 
 
-def get_species_iri(inchi: str, query_endpoint: str) -> Optional[str]:
+def get_species_iri(
+    inchi: str,
+    store_client: rsc.RemoteStoreClient,
+) -> Optional[str]:
     # Query OntoSpecies to find Species IRI that corresponds to a given InChI.
     speciesIRI = None
     results = []
     try:
-        # results  = querykg(SPARQL_ENDPOINTS['ontospecies'], spec_inchi_query(inchi)) #query_endpoint(endpoint, spec_inchi_query(inchi))
-        # TODO, find a way to fix this query on the java side so that it works as expected
-        sparql = SPARQLWrapper(query_endpoint)
-        sparql.setReturnFormat(JSON)
-        query = spec_inchi_query(inchi)
-        sparql.setQuery(query)
-        results = sparql.queryAndConvert()
+        results = store_client.execute_query(
+            query_str=spec_inchi_query(inchi),
+        )
     except Exception:
         logger.warning("Warning: get_species_iri query failed.")
     if results:
@@ -61,10 +59,10 @@ def get_assemblyModel(modularity, planarity, gbu_number, symmetry):
     # queries the assembly model of a particular MOP.
     queryStr = """
     PREFIX OntoMOPs: <http://www.theworldavatar.com/ontology/ontomops/OntoMOPs.owl#>
-	PREFIX OntoSpecies: <http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#>
-	PREFIX Measure: <http://www.ontology-of-units-of-measure.org/resource/om-2/>
+    PREFIX OntoSpecies: <http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#>
+    PREFIX Measure: <http://www.ontology-of-units-of-measure.org/resource/om-2/>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-	PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
     SELECT ?AssemblyModel ?NumberValue ?Planarity ?Modularity ?Symmetry
     WHERE
     {
@@ -77,7 +75,7 @@ def get_assemblyModel(modularity, planarity, gbu_number, symmetry):
     ?GBUNumber OntoMOPs:isNumberOf ?GBU .
     ?GBU OntoMOPs:hasPlanarity ?Planarity .
     ?GBU OntoMOPs:hasModularity ?Modularity .
-	?GBUNumber OntoSpecies:value ?NumberValue .
+    ?GBUNumber OntoSpecies:value ?NumberValue .
     FILTER ((?Modularity) = "#MODULARITY") .
     FILTER ((?Planarity) = "#PLANARITY") .
     FILTER ((?NumberValue) = "#NUMBER") .
@@ -90,15 +88,19 @@ def get_assemblyModel(modularity, planarity, gbu_number, symmetry):
     return queryStr
 
 
-def get_assembly_iri(query_endpoint, modularity, planarity, gbu_number, symmetry):
-    # Query OntoSpecies to find Species IRI that corresponds to a given InChI.
-    target = None
-    results = querykg(
-        query_endpoint,
-        get_assemblyModel(modularity, planarity, gbu_number, symmetry),
+def get_assembly_iri(
+    modularity: str,
+    planarity: str,
+    gbu_number: str,
+    symmetry: str,
+    store_client: rsc.RemoteStoreClient,
+):
+
+    target = []
+    results = store_client.execute_query(
+        query_str=get_assemblyModel(modularity, planarity, gbu_number, symmetry)
     )
     if results:
-        target = []
         for k in range(len(results)):
             if "AssemblyModel" in results[k].keys():
                 target.append(results[k]["AssemblyModel"])
