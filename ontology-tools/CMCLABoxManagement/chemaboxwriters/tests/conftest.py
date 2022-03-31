f8a0ca25ed7fe91a05210273bad8cb3c617b1883
dln22 <danieln@cmclinnovations.com>
2022-03-31 15:20:52 +0100

    dev-chemaboxwriters: Disabled the upload tests by defualt. Added flag to enable them.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/tests/conftest.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/tests/conftest.py
@@ -1,9 +1,25 @@
 import pytest
+import pathlib
+
 
 def pytest_addoption(parser):
     parser.addoption("--clean-tests", action="store", default=True)
+    parser.addoption(
+        "--upload-tests", action="store", default=False, help="Run uploads tests"
+    )
 
 
 @pytest.fixture
 def clean_tests(request):
     return request.config.getoption("--clean-tests")
+
+
+def pytest_configure(config):
+    upload_tests = config.getoption("--upload-tests")
+    if not upload_tests:
+        print("Upload tests skipped by default. Enable via --upload-tests flag.")
+        config.args = [
+            p
+            for p in pathlib.Path().rglob("test_abox_*")
+            if p.is_dir() and "uploads" not in str(p)
+        ]
