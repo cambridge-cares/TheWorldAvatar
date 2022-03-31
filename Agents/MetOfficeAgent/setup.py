f807a591738183a67826532f5cf354c1e347b53e
Markus Hofmeister <mh807@cam.ac.uk>
2022-03-31 11:49:04 +0800

    dev-MetOfficeAgent: Include setup.py

---
+++ Agents/MetOfficeAgent/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='metoffice',
+    version='1.0.0',
+    author='Markus Hofmeister',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=("tests")),
+    url="",
+    python_requires='>=3.5',
+    include_package_data=True,
+    install_requires= [
+        'docopt~=0.6',
+        'py4jps==1.0.17', 
+        'flask~=2.1',
+        'requests~=2.27',
+        'metoffer @ git+https://github.com/sludgedesk/metoffer#egg=MetOffer'],
+    entry_points={
+        'console_scripts': [
+            'metoffice=metoffice.driver:main',
+        ],
+    }
+)
