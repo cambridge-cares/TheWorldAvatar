0c5af9fd3ab5662c9590937ba24e3340bc0f87c7
Michael Hillman <mdhillman@cmclinnovations.com>
2022-04-07 17:08:44 +0100

    dev-dtvf: Added Grunt configuration to run JS linting.

--- web/digital-twin-vis-framework/library/grunt/GruntFile.js
+++ web/digital-twin-vis-framework/library/grunt/GruntFile.js
@@ -33,6 +33,23 @@ module.exports = function (grunt) {
                 src: "../src/css/dtvf.css",
                 dest: "../output/dtvf.min.css"
             }
+        },
+        jshint: {
+            files: [
+                "../src/js/control_handler.js",
+                "../src/js/data_registry.js",
+                "../src/js/icon_handler.js",
+                "../src/js/interaction_handler.js",
+                "../src/js/json_tree.js",
+                "../src/js/layer_handler.js",
+                "../src/js/manager.js",
+                "../src/js/panel_handler.js",
+                "../src/js/source_handler.js",
+                "../src/js/timeseries_handler.js"
+            ],
+            options: {
+                reporterOutput: "../output/jshint.log"
+            }
         }
     });
 
@@ -40,7 +57,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     // Register Tasks
     grunt.registerTask('package', ['concat', 'uglify', 'cssmin']);
+    grunt.registerTask('lint', ['jshint']);
 }
