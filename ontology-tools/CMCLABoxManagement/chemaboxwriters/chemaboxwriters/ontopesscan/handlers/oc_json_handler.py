6dc5ab108305b51b0bcf360b4cc5acd5db0e84df
dln22 <danieln@cmclinnovations.com>
2022-04-08 21:46:34 +0100

    dev-chemaboxwriters: Cleaned a bit oc json handler in the opsscan pipeline.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontopesscan/handlers/oc_json_handler.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontopesscan/handlers/oc_json_handler.py
@@ -7,10 +7,15 @@ import chemaboxwriters.common.utilsfunc as utilsfunc
 import json
 import numpy as np
 import chemaboxwriters.common.params as params
-from compchemparser.parsers.ccgaussian_parser import GEOM
 from chemaboxwriters.common.handler import Handler
 from chemaboxwriters.ontopesscan.abox_stages import OPS_ABOX_STAGES
 from chemaboxwriters.ontopesscan import OPS_SCHEMA
+import chemaboxwriters.app_exceptions.app_exceptions as app_exceptions
+from chemaboxwriters.ontocompchem.handlers.qc_json_handler import (
+    COORD_X,
+    COORD_Y,
+    COORD_Z,
+)
 from typing import List
 
 
@@ -69,14 +74,8 @@ class OC_JSON_TO_OPS_JSON_Handler(Handler):
         os_atoms_iris = self.get_parameter_value(name="os_atoms_iris")
         oc_atoms_pos = self.get_parameter_value(name="oc_atoms_pos")
 
-        if os_iris is None:
-            os_iris = ""
-
-        if os_atoms_iris is None:
-            os_atoms_iris = ""
-
-        if oc_atoms_pos is None:
-            oc_atoms_pos = ""
+        if os_iris is None or os_atoms_iris is None or oc_atoms_pos is None:
+            raise app_exceptions.MissingRequiredInput()
 
         data_out = {}
         data_out[params.SPECIES_IRI] = os_iris.split(",")
@@ -85,17 +84,6 @@ class OC_JSON_TO_OPS_JSON_Handler(Handler):
         ]
         data_out[SCAN_ATOM_IDS] = " ".join(oc_atoms_pos.split(",")[:])
         oc_atoms_pos_ids = [int(at_pos) - 1 for at_pos in oc_atoms_pos.split(",")]
-
-        ndegrees = len(os_atoms_iris.split(","))
-        if ndegrees == 2:
-            data_out[SCAN_COORDINATE_TYPE] = "DistanceCoordinate"
-            data_out[SCAN_COORDINATE_UNIT] = "Angstrom"
-        else:
-            data_out[SCAN_COORDINATE_UNIT] = "DegreeAngle"
-            if ndegrees == 3:
-                data_out[SCAN_COORDINATE_TYPE] = "AngleCoordinate"
-            else:
-                data_out[SCAN_COORDINATE_TYPE] = "DihedralAngleCoordinate"
 
         if not random_id:
             random_id = utilsfunc.get_random_id()
@@ -109,6 +97,7 @@ class OC_JSON_TO_OPS_JSON_Handler(Handler):
 
         scanCoordinateValue = []
         ontoCompChemJobs = []
+        ndegrees = len(os_atoms_iris.split(","))
 
         for file_path in file_paths:
 
@@ -116,26 +105,27 @@ class OC_JSON_TO_OPS_JSON_Handler(Handler):
                 data_item = json.load(file_handle)
 
             ontoCompChemJobs.append(data_item[params.ENTRY_IRI])
-            xyz = np.array(data_item[GEOM])
+
+            xyz = np.array(
+                list(zip(data_item[COORD_X], data_item[COORD_Y], data_item[COORD_Z]))
+            )
+            scanAtomsPos = xyz[oc_atoms_pos_ids]
 
             if ndegrees == 2:
-                scanAtomsPos = xyz[oc_atoms_pos_ids]
-                atom_dist = getXYZPointsDistance(scanAtomsPos[0], scanAtomsPos[1])
+                atom_dist = getXYZPointsDistance(*scanAtomsPos)
                 scanCoordinateValue.append(round(atom_dist, 3))
+                data_out[SCAN_COORDINATE_TYPE] = "DistanceCoordinate"
+                data_out[SCAN_COORDINATE_UNIT] = "Angstrom"
             elif ndegrees == 3:
-                scanAtomsPos = xyz[oc_atoms_pos_ids]
-                angle = getPlaneAngle(scanAtomsPos[0], scanAtomsPos[1], scanAtomsPos[2])
+                angle = getPlaneAngle(*scanAtomsPos)
                 scanCoordinateValue.append(round(angle, 3))
-
+                data_out[SCAN_COORDINATE_UNIT] = "DegreeAngle"
+                data_out[SCAN_COORDINATE_TYPE] = "AngleCoordinate"
             elif ndegrees == 4:
-                scanAtomsPos = xyz[oc_atoms_pos_ids]
-                dangle = getDihedralAngle(
-                    scanAtomsPos[0],
-                    scanAtomsPos[1],
-                    scanAtomsPos[2],
-                    scanAtomsPos[3],
-                )
+                dangle = getDihedralAngle(*scanAtomsPos)
                 scanCoordinateValue.append(round(dangle, 3))
+                data_out[SCAN_COORDINATE_UNIT] = "DegreeAngle"
+                data_out[SCAN_COORDINATE_TYPE] = "DihedralAngleCoordinate"
 
         scanCoordinateValue, ontoCompChemJobs = zip(
             *sorted(zip(scanCoordinateValue, ontoCompChemJobs))
