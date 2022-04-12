5da1e06af23b8a44d8e492a4a108d36c00eada0f
dln22 <danieln@cmclinnovations.com>
2022-04-12 21:56:07 +0100

    dev-chemaboxwriters: Standardised the ominp and om json keys. Cleaned up the ominp json handler and regenerated affected tests.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontomops/handlers/ominp_json_handler.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontomops/handlers/ominp_json_handler.py
@@ -1,19 +1,18 @@
 import json
 from chemaboxwriters.common.handler import Handler
 import chemaboxwriters.common.utilsfunc as utilsfunc
-import chemaboxwriters.common.params as params
+import chemaboxwriters.ontomops.handlers.om_json_keys as om_keys
+import chemaboxwriters.ontomops.handlers.ominp_json_keys as ominp_keys
 from chemaboxwriters.ontomops.abox_stages import OM_ABOX_STAGES
 import chemaboxwriters.kgoperations.querytemplates as qtmpl
 import os
 from chemaboxwriters.ontomops import OM_SCHEMA
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 
 HANDLER_PARAMETERS = {
     "random_id": {"required": False},
 }
-
-MOPS_XYZ_GEOMETRY_FILE_URL = "Mops_xyz_geometry_file_url"
 
 
 class OMINP_JSON_TO_OM_JSON_Handler(Handler):
@@ -58,12 +57,49 @@ class OMINP_JSON_TO_OM_JSON_Handler(Handler):
         self, file_path: str, output_file_path: str, dry_run: bool
     ) -> None:
 
-        random_id = self.get_parameter_value(name="random_id")
+        random_id = self.get_parameter_value(
+            name="random_id", default=utilsfunc.get_random_id()
+        )
 
+        # read in the incoming ominp json data file
         with open(file_path, "r") as file_handle:
-            data = json.load(file_handle)
+            ominp_data_in = json.load(file_handle)
 
-        xyz_file = data.get("Mops_XYZ_coordinates_file")
+        # create an empty om json out dict
+        data_out = dict.fromkeys(om_keys.OM_JSON_KEYS, None)
+
+        # populate the selected om json entries with the ominp json data
+        # apply any defined post processing steps
+        # --------------------------------------------------
+        for om_key, ominp_om_map in om_keys.OM_JSON_TO_OMINP_JSON_KEYS_MAP.items():
+            ominp_values = [ominp_data_in.get(key) for key in ominp_om_map["cckeys"]]
+            data_out[om_key] = ominp_om_map["postproc_func"](ominp_values)
+
+        data_out[om_keys.MOPS_XYZ_GEOMETRY_FILE_URL] = self._handle_xyz_file_data(
+            xyz_file=ominp_data_in.get("Mops_XYZ_coordinates_file"), dry_run=dry_run
+        )
+
+        main_inst_pref = utilsfunc.read_main_pref_from_schema(
+            schema_file=OM_SCHEMA, main_pref_name="main_inst_pref"
+        )
+
+        data_out[om_keys.ENTRY_ID] = random_id
+        data_out[om_keys.ENTRY_IRI] = f"{main_inst_pref}{random_id}"
+
+        assemblymodel = self.get_assembly_model_iri(om_data=data_out)
+
+        if assemblymodel is not None:
+            assemblymodel = assemblymodel.split("_")[-1]
+        else:
+            assemblymodel = random_id
+        data_out[om_keys.ASSEMBLY_MODEL_ID] = assemblymodel
+
+        utilsfunc.write_dict_to_file(dict_data=data_out, dest_path=output_file_path)
+
+    def _handle_xyz_file_data(
+        self, xyz_file: Optional[str], dry_run: bool
+    ) -> Optional[str]:
+
         if xyz_file is not None:
             xyz_file = os.path.abspath(xyz_file)
             self.do_fs_uploads(
@@ -71,60 +107,41 @@ class OMINP_JSON_TO_OM_JSON_Handler(Handler):
                 input_type="ominp_xyz",
                 dry_run=dry_run,
             )
-            xyz_file_loc = self.get_fs_upload_location(upload_file=xyz_file)
-            if xyz_file_loc is not None:
-                data[MOPS_XYZ_GEOMETRY_FILE_URL] = xyz_file_loc
+            return self.get_fs_upload_location(upload_file=xyz_file)
 
-        if random_id is None:
-            random_id = utilsfunc.get_random_id()
+    def get_assembly_model_iri(self, om_data: Dict) -> Optional[str]:
 
-        main_inst_pref = utilsfunc.read_main_pref_from_schema(
-            schema_file=OM_SCHEMA, main_pref_name="main_inst_pref"
-        )
+        mops_sym_point_group = om_data.get(ominp_keys.MOPS_SYMMETRY_POINT_GROUP)
+        mops_gbu_mods = om_data.get(om_keys.GBU_MODULARITIES)
+        mops_gbu_plan = om_data.get(om_keys.GBU_PLANARITIES)
+        mops_gbu_nums = om_data.get(om_keys.GBU_NUMBERS)
 
-        data[params.ENTRY_UUID] = random_id
-        data[params.ENTRY_IRI] = f"{main_inst_pref}{random_id}"
+        if (
+            mops_sym_point_group is None
+            or mops_gbu_mods is None
+            or mops_gbu_plan is None
+            or mops_gbu_nums is None
+        ):
+            return
 
-        assemblymodel = self.get_assembly_model_iri(data=data)
-
-        if assemblymodel is not None:
-            data["AssemblyModel_ID"] = assemblymodel.split("_")[-1]
-        else:
-            data["AssemblyModel_ID"] = data[params.ENTRY_UUID]
-
-        # flatten the Mops_Chemical_Building_Units entries
-        Mops_CBUs = data.pop("Mops_Chemical_Building_Units")
-        for cbu_data in Mops_CBUs:
-            for key, value in cbu_data.items():
-                if key not in data:
-                    data[key] = [value]
-                else:
-                    tmp = data[key]
-                    tmp.append(value)
-                    data[key] = tmp
-
-        utilsfunc.write_dict_to_file(dict_data=data, dest_path=output_file_path)
-
-    def get_assembly_model_iri(self, data) -> Optional[str]:
         assemblymodel = None
-
-        symmetry = data["Mops_Symmetry_Point_Group"]
         gbu_properties = []
-        for cbu in data["Mops_Chemical_Building_Units"]:
+        for i in range(len(mops_gbu_mods)):
             gbu_properties.append(
                 {
-                    "modularity": cbu["GenericUnitModularity"],
-                    "planarity": cbu["GenericUnitPlanarity"],
-                    "gbu_number": cbu["GenericUnitNumber"],
+                    "modularity": mops_gbu_mods[i],
+                    "planarity": mops_gbu_plan[i],
+                    "gbu_number": mops_gbu_nums[i],
                 },
             )
 
         response = self.do_remote_store_query(
             endpoint_prefix="omops",
             query_str=qtmpl.get_assemblyModel(
-                gbu_properties=gbu_properties, mops_symmetry=symmetry
+                gbu_properties=gbu_properties, mops_symmetry=mops_sym_point_group
             ),
         )
         if response:
             assemblymodel = response[0]["AssemblyModel"]
+
         return assemblymodel
