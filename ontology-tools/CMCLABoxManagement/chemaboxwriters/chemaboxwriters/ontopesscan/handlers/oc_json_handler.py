00756ad8628bf88e37f350b78ada759bb8b9aa63
dln22 <danieln@cmclinnovations.com>
2022-04-12 13:16:31 +0100

    dev-chemaboxwriters: Standardised ops json file keys. Cleaned up a bit the oc json handler and regenerated opsscan tests.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontopesscan/handlers/oc_json_handler.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/chemaboxwriters/ontopesscan/handlers/oc_json_handler.py
@@ -1,22 +1,13 @@
-from chemutils.mathutils.linalg import (
-    getXYZPointsDistance,
-    getPlaneAngle,
-    getDihedralAngle,
-)
+import chemaboxwriters.ontopesscan.handlers.ops_json_keys as ops_keys
+import chemaboxwriters.ontocompchem.handlers.oc_json_keys as oc_keys
+import chemutils.mathutils.linalg as linalg
 import chemaboxwriters.common.utilsfunc as utilsfunc
 import json
 import numpy as np
-import chemaboxwriters.common.params as params
 from chemaboxwriters.common.handler import Handler
 from chemaboxwriters.ontopesscan.abox_stages import OPS_ABOX_STAGES
 from chemaboxwriters.ontopesscan import OPS_SCHEMA
-import chemaboxwriters.app_exceptions.app_exceptions as app_exceptions
-from chemaboxwriters.ontocompchem.handlers.oc_json_keys import (
-    X_ATOMS_COORDINATES,
-    Y_ATOMS_COORDINATES,
-    Z_ATOMS_COORDINATES,
-)
-from typing import List
+from typing import List, Tuple
 
 
 SCAN_COORDINATE_ATOMS_IRIS = "ScanCoordinateAtomsIRIs"
@@ -63,83 +54,103 @@ class OC_JSON_TO_OPS_JSON_Handler(Handler):
             file_extension=self._out_stage,
             out_dir=out_dir,
         )
-        self._ops_jsonwriter(file_paths=inputs, output_file_path=out_file_path)
+        self._ops_jsonwriter(oc_json_files=inputs, output_file_path=out_file_path)
         outputs.append(out_file_path)
         return outputs
 
-    def _ops_jsonwriter(self, file_paths: List[str], output_file_path: str):
+    def _ops_jsonwriter(self, oc_json_files: List[str], output_file_path: str):
 
-        random_id = self.get_parameter_value(name="random_id")
-        os_iris = self.get_parameter_value(name="os_iris")
-        os_atoms_iris = self.get_parameter_value(name="os_atoms_iris")
-        oc_atoms_pos = self.get_parameter_value(name="oc_atoms_pos")
+        # read handler params
+        random_id = self.get_parameter_value(
+            name="random_id", default=utilsfunc.get_random_id()
+        )
+        os_iris: str = self.get_parameter_value(
+            name="os_iris", default=""
+        )  # type: ignore
+        os_atoms_iris: str = self.get_parameter_value(
+            name="os_atoms_iris", default=""
+        )  # type: ignore
+        oc_atoms_pos: str = self.get_parameter_value(
+            name="oc_atoms_pos", default=""
+        )  # type: ignore
 
-        if os_iris is None or os_atoms_iris is None or oc_atoms_pos is None:
-            raise app_exceptions.MissingRequiredInput()
+        # create an empty oc json out dict
+        data_out = dict.fromkeys(ops_keys.OPS_JSON_KEYS, None)
 
-        data_out = {}
-        data_out[params.SPECIES_IRI] = os_iris.split(",")
-        data_out[SCAN_COORDINATE_ATOMS_IRIS] = [
+        # assign values from the passed params
+        data_out[ops_keys.SPECIES_IRI] = os_iris.split(",")
+        data_out[ops_keys.SCAN_ATOMS_IRIS] = [
             iri.strip() for iri in os_atoms_iris.split(",")
         ]
-        data_out[SCAN_ATOM_IDS] = " ".join(oc_atoms_pos.split(",")[:])
-        oc_atoms_pos_ids = [int(at_pos) - 1 for at_pos in oc_atoms_pos.split(",")]
-
-        if not random_id:
-            random_id = utilsfunc.get_random_id()
-        data_out[params.ENTRY_UUID] = random_id
-
+        data_out[ops_keys.SCAN_ATOMS_CCJOB_POSITIONS] = " ".join(
+            oc_atoms_pos.split(",")
+        )
+        data_out[ops_keys.ENTRY_ID] = random_id
+        # assign the main ops entry IRI
         main_inst_pref = utilsfunc.read_main_pref_from_schema(
             schema_file=OPS_SCHEMA, main_pref_name="main_inst_pref"
         )
+        data_out[ops_keys.ENTRY_IRI] = f"{main_inst_pref}{random_id}"
 
-        data_out[params.ENTRY_IRI] = f"{main_inst_pref}{random_id}"
+        # now read oc json files and assign values
+        scan_coord_values = []
+        oc_jobs = []
+        atom_pos_int = list(map(lambda x: int(x) - 1, oc_atoms_pos.split(",")))
 
-        scanCoordinateValue = []
-        ontoCompChemJobs = []
-        ndegrees = len(os_atoms_iris.split(","))
+        for oc_file in oc_json_files:
+            with open(oc_file, "r") as file_handle:
+                oc_data_in = json.load(file_handle)
 
-        for file_path in file_paths:
+            oc_jobs.append(oc_data_in[oc_keys.ENTRY_IRI])
 
-            with open(file_path, "r") as file_handle:
-                data_item = json.load(file_handle)
-
-            ontoCompChemJobs.append(data_item[params.ENTRY_IRI])
-
-            xyz = np.array(
-                list(
-                    zip(
-                        data_item[X_ATOMS_COORDINATES],
-                        data_item[Y_ATOMS_COORDINATES],
-                        data_item[Z_ATOMS_COORDINATES],
-                    )
-                )
+            coord_value, scan_type, coord_unit = self._get_scan_coord_data(
+                atom_pos=atom_pos_int,
+                x_coord=oc_data_in[oc_keys.X_ATOMS_COORDINATES],
+                y_coord=oc_data_in[oc_keys.Y_ATOMS_COORDINATES],
+                z_coord=oc_data_in[oc_keys.Z_ATOMS_COORDINATES],
             )
-            scanAtomsPos = xyz[oc_atoms_pos_ids]
+            scan_coord_values.append(coord_value)
 
-            if ndegrees == 2:
-                atom_dist = getXYZPointsDistance(*scanAtomsPos)
-                scanCoordinateValue.append(round(atom_dist, 3))
-                data_out[SCAN_COORDINATE_TYPE] = "DistanceCoordinate"
-                data_out[SCAN_COORDINATE_UNIT] = "Angstrom"
-            elif ndegrees == 3:
-                angle = getPlaneAngle(*scanAtomsPos)
-                scanCoordinateValue.append(round(angle, 3))
-                data_out[SCAN_COORDINATE_UNIT] = "DegreeAngle"
-                data_out[SCAN_COORDINATE_TYPE] = "AngleCoordinate"
-            elif ndegrees == 4:
-                dangle = getDihedralAngle(*scanAtomsPos)
-                scanCoordinateValue.append(round(dangle, 3))
-                data_out[SCAN_COORDINATE_UNIT] = "DegreeAngle"
-                data_out[SCAN_COORDINATE_TYPE] = "DihedralAngleCoordinate"
+            data_out[ops_keys.SCAN_COORDINATE_UNIT] = coord_unit
+            data_out[ops_keys.SCAN_COORDINATE_TYPE] = scan_type
 
-        scanCoordinateValue, ontoCompChemJobs = zip(
-            *sorted(zip(scanCoordinateValue, ontoCompChemJobs))
-        )
-        scanCoordinateValue = list(scanCoordinateValue)
-        ontoCompChemJobs = list(ontoCompChemJobs)
+        scan_coord_values, oc_jobs = zip(*sorted(zip(scan_coord_values, oc_jobs)))
+        scan_coord_values = list(scan_coord_values)
+        oc_jobs = list(oc_jobs)
 
-        data_out[SCAN_COORDINATE_VALUE] = scanCoordinateValue
-        data_out[SCAN_POINTS_JOBS] = ontoCompChemJobs
+        data_out[ops_keys.SCAN_COORDINATE_VALUES] = scan_coord_values
+        data_out[ops_keys.SCAN_POINT_JOBS_IRIS] = oc_jobs
 
         utilsfunc.write_dict_to_file(dict_data=data_out, dest_path=output_file_path)
+
+    def _get_scan_coord_data(
+        self,
+        atom_pos: List[int],
+        x_coord: List[float],
+        y_coord: List[float],
+        z_coord: List[float],
+    ) -> Tuple[float, str, str]:
+        xyz = np.array(
+            list(
+                zip(
+                    x_coord,
+                    y_coord,
+                    z_coord,
+                )
+            )
+        )
+        scanAtomsPos = xyz[atom_pos]
+
+        if len(atom_pos) == 2:
+            value = round(linalg.getXYZPointsDistance(*scanAtomsPos), 3)
+            scan_type = "DistanceCoordinate"
+            coord_unit = "Angstrom"
+        elif len(atom_pos) == 3:
+            value = round(linalg.getPlaneAngle(*scanAtomsPos), 3)
+            scan_type = "AngleCoordinate"
+            coord_unit = "DegreeAngle"
+        else:
+            value = round(linalg.getDihedralAngle(*scanAtomsPos), 3)
+            scan_type = "DihedralAngleCoordinate"
+            coord_unit = "DegreeAngle"
+        return round(value, 3), scan_type, coord_unit
