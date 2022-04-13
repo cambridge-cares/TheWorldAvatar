afac28e7351a4f9965608a7f8709a88126a0c10b
Daniel Nurkowski <danieln@cmclinnovations.com>
2022-04-13 16:00:13 +0100

    dev-chemaboxwriters: Found a better option to disable upload tests by default that does not mess with the vs code test runner.

--- ontology-tools/CMCLABoxManagement/chemaboxwriters/tests/conftest.py
+++ ontology-tools/CMCLABoxManagement/chemaboxwriters/tests/conftest.py
@@ -6,9 +6,6 @@ from typing import List, Callable
 
 def pytest_addoption(parser):
     parser.addoption("--clean-tests", action="store", default=True)
-    parser.addoption(
-        "--upload-tests", action="store", default=True, help="Run uploads tests"
-    )
 
 
 @pytest.fixture
@@ -23,14 +20,3 @@ def cleanup_test_data() -> Callable[[List[str]], None]:
             os.remove(file)
 
     return _cleanup_test_data
-
-
-def pytest_configure(config):
-    upload_tests = config.getoption("--upload-tests")
-    if not upload_tests:
-        print("Upload tests skipped by default. Enable via --upload-tests flag.")
-        config.args = [
-            p
-            for p in pathlib.Path().rglob("test_abox_*")
-            if p.is_dir() and "uploads" not in str(p)
-        ]
