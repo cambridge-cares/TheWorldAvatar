036d926fb23d8ff6b2afef569e5a0934617af1e4
Casper Lindberg <casper.lindberg@cares.cam.ac.uk>
2022-04-21 14:27:45 +0800

    1318-dev-agent-routing: add comments to CacheInterface

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/interfaces/CacheInterface.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/interfaces/CacheInterface.java
@@ -9,10 +9,42 @@ package uk.ac.cam.cares.jps.base.interfaces;
  */
 public interface CacheInterface<K, V> {
 
+	/**
+	 * Shall implement logic to put key-value pair into the cache
+	 * @param key
+	 * @param value
+	 * @return
+	 */
 	V put(K key, V value);
+	
+	/**
+	 * Shall implement logic to get value mapped to key from the cache
+	 * @param key
+	 * @return
+	 */
     V get(K key);
+    
+    /**
+     * Shall implement logic to return true if the key is present in the cache
+     * @param key
+     * @return
+     */
     boolean contains(K key);
+    
+    /**
+     * Shall implement logic to return true if the cache is empty
+     * @return
+     */
     boolean isEmpty();
+    
+    /**
+     * Shall implement logic to clear cache
+     */
     void clear();
+    
+    /**
+     * Shall implement logic to return the max cache capacity
+     * @return
+     */
     int capacity();
 }
