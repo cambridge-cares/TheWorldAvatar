838056605e8440106d83fd67df56d4c9777cb3b2
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-04-25 11:45:48 +0100

    dev-simple-mods-agent: Added an outputs Data field to the Request class and added JsonInclude annotations to all of the optional fields so that they don't get serialised as "null", as this causes issues during deserialisation.

--- Agents/MoDSSimpleAgent/mods-simple-agent/src/main/java/com/cmclinnovations/mods/modssimpleagent/datamodels/Request.java
+++ Agents/MoDSSimpleAgent/mods-simple-agent/src/main/java/com/cmclinnovations/mods/modssimpleagent/datamodels/Request.java
@@ -2,17 +2,25 @@ package com.cmclinnovations.mods.modssimpleagent.datamodels;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Request {
 
+    @JsonInclude(Include.NON_NULL)
     String jobID;
     @JsonProperty("SimulationType")
     private String simulationType;
     @JsonProperty("Algorithms")
+    @JsonInclude(Include.NON_NULL)
     private List<Algorithm> algorithms;
     @JsonProperty("Inputs")
+    @JsonInclude(Include.NON_NULL)
     private Data inputs;
+    @JsonProperty("Outputs")
+    @JsonInclude(Include.NON_NULL)
+    private Data outputs;
 
     public String getJobID() {
         return jobID;
@@ -46,4 +54,11 @@ public class Request {
         this.inputs = inputs;
     }
 
+    public Data getOutputs() {
+        return outputs;
+    }
+
+    public void setOutputs(Data outputs) {
+        this.outputs = outputs;
+    }
 }
