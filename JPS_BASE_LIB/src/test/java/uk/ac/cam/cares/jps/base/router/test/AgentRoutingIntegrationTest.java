dedf8f9ac7f73da1087eb504dd47a2d9b59d04d0
Casper Lindberg <casper.lindberg@cares.cam.ac.uk>
2022-05-06 17:24:52 +0800

    1318-dev-agent-routing: add AgentRoutingIntegrationTest

---
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/router/test/AgentRoutingIntegrationTest.java
@@ -0,0 +1,25 @@
+package uk.ac.cam.cares.jps.base.router.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * This class is intended to test the integration of the 
+ * {@link uk.ac.cam.cares.jps.base.agent.AgentCaller AgentCaller} with
+ * the {@link uk.ac.cam.cares.jps.base.router.AgentRouter AgentRouter}.
+ * @author csl37
+ *
+ */
+@Disabled("This integration test requires Docker to run test containers.")
+@Testcontainers
+class AgentRoutingIntegrationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
