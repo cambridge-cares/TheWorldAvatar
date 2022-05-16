ff7b8acab0f92400eaae25ff61a4b470a455221a
jb2197 <jb2197@cam.ac.uk>
2022-05-17 00:04:24 +0100

    152-dev-pyderivationagent: provided method in DerivationOutputs python wrapper to add triple with customer data type; also method to add whole rdflib.Graph to derivation output triples.

--- JPS_BASE_LIB/python_derivation_agent/pyderivationagent/data_model/derivation.py
+++ JPS_BASE_LIB/python_derivation_agent/pyderivationagent/data_model/derivation.py
@@ -1,3 +1,5 @@
+from rdflib import Graph, RDF
+
 class DerivationInputs():
     """This is a warpper class for uk.ac.cam.cares.jps.base.derivation.DerivationInputs.java.
     All methods provided here are wrapped around the corresponding methods in the java class,
@@ -14,7 +16,7 @@ class DerivationInputs():
 
 class DerivationOutputs():
     """This is a warpper class for uk.ac.cam.cares.jps.base.derivation.DerivationOutputs.java.
-    All methods provided here are wrapped around the corresponding methods in the java class,
+    All methods provided here are wrapped around the relevant methods in the java class,
     as references for developers to handle derivations within python derivation agents."""
 
     def __init__(self, derivationOutputs) -> None:
@@ -26,3 +28,15 @@ class DerivationOutputs():
     def addTriple(self, s, p, o):
         """Only one addTriple is provided here as the correct method to use will be decided by java automatically."""
         self.derivation_outputs.addTriple(s, p, o)
+
+    def addTripleWithDataType(self, s, p, o, dataType):
+        self.derivation_outputs.addTriple(s, p, o, dataType)
+
+    def addGraph(self, g: Graph):
+        """Add a whole rdflib.Graph to derivation outputs."""
+        for s, p, o in g:
+            if p.toPython() == RDF.type.toPython():
+                self.createNewEntity(s.toPython(), o.toPython())
+            else:
+                # .toPython() works out what's the most suitable python class and cast to it
+                self.addTriple(s.toPython(), p.toPython(), o.toPython())
