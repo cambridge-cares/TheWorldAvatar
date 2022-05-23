5c552856b555e0c1c1744efeac9f5f2c720669b7
jb2197 <jb2197@cam.ac.uk>
2022-05-23 16:30:13 +0100

    159-release-py4jps-and-pyderivationagent: made necessary changes to enable release of pyderivationagent package 1.0.0, which is now accessible on both Test-PyPI and PyPI.

--- JPS_BASE_LIB/python_derivation_agent/pyderivationagent/__init__.py
+++ JPS_BASE_LIB/python_derivation_agent/pyderivationagent/__init__.py
@@ -2,4 +2,4 @@ from .agent import DerivationAgent, FlaskConfig
 from .conf import AgentConfig, config_derivation_agent
 from .data_model import DerivationInputs, DerivationOutputs
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
