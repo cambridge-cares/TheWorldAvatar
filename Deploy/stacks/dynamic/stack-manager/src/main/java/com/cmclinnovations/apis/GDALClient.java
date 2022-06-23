213421831c5b2c2e8c43f3b489647f95f95f2642
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-06-23 16:39:50 +0100

    dev-stack-manager: Update the GDAL client so that it can handle large String objects as well as files and URLs.

--- Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/apis/GDALClient.java
+++ Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/apis/GDALClient.java
@@ -1,13 +1,16 @@
 package com.cmclinnovations.apis;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
 
 public class GDALClient extends ContainerClient {
 
     private final PostGISEndpointConfig postgreSQLEndpoint;
 
     public GDALClient() {
-        super();
         postgreSQLEndpoint = readEndpointConfig("postgis", PostGISEndpointConfig.class);
     }
 
@@ -17,7 +20,51 @@ public class GDALClient extends ContainerClient {
                 + " password=" + postgreSQLEndpoint.getPassword();
     }
 
-    public void uploadGeoJsonToPostGIS(String database, String layername, String json,
+    public void uploadVectorStringToPostGIS(String database, String layername, String fileContents,
+            Ogr2OgrOptions options) {
+        String containerId = getDockerClient().getContainerId("gdal");
+        String tmpDir = getDockerClient().makeTempDir(containerId);
+        boolean exceptionThrown = false;
+        try {
+            getDockerClient().sendFiles(containerId, Map.of(layername, fileContents.getBytes()), tmpDir);
+
+            uploadVectorToPostGIS(database, layername, tmpDir + "/" + layername, null, options);
+        } catch (IOException ex) {
+            exceptionThrown = true;
+            throw new RuntimeException(
+                    "Failed to send file content for '" + layername + "' to database '" + database + "'.", ex);
+        } catch (Throwable ex) {
+            exceptionThrown = true;
+            throw ex;
+        } finally {
+            try {
+                getDockerClient().deleteDirectory(containerId, tmpDir);
+            } catch (Exception ex2) {
+                if (exceptionThrown) {
+                    // Don't worry about this exception as any previously thrown exception is more
+                    // important.
+                } else {
+                    throw ex2;
+                }
+            }
+        }
+    }
+
+    public void uploadVectorFileToPostGIS(String database, String layername, String filePath, Ogr2OgrOptions options) {
+        String fileContents;
+        try {
+            fileContents = Files.readString(Path.of(filePath));
+        } catch (IOException ex) {
+            throw new RuntimeException("Failed to read file '" + filePath + "'.", ex);
+        }
+        uploadVectorStringToPostGIS(database, layername, fileContents, options);
+    }
+
+    public void uploadVectorURLToPostGIS(String database, String layername, String url, Ogr2OgrOptions options) {
+        uploadVectorToPostGIS(database, layername, url, null, options);
+    }
+
+    private void uploadVectorToPostGIS(String database, String layername, String filePath, String fileContents,
             Ogr2OgrOptions options) {
 
         String containerId = getDockerClient().getContainerId("gdal");
@@ -28,9 +75,9 @@ public class GDALClient extends ContainerClient {
         getDockerClient().createComplexCommand(containerId, options.appendToArgs("ogr2ogr", "-overwrite",
                 "-f", "PostgreSQL",
                 computePGSQLSourceString(database),
-                "/vsistdin/",
+                filePath,
                 "-nln", layername))
-                .withHereDocument(json)
+                .withHereDocument(fileContents)
                 .withOutputStream(outputStream)
                 .withErrorStream(errorStream)
                 .withEnvVar("PG_USE_COPY", "YES")
