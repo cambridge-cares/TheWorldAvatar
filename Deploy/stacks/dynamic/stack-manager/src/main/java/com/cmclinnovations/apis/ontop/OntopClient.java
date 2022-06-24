a74e18ce6629e4d425fa2c45f9d3d736e1dd444e
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-06-24 15:23:20 +0100

    dev-stack-manager: Moved soe of the code out of the OntoService class and into a new OntopClient class. Also improved the code to add new OBDA mappings so that any previous mappings are retained.

---
+++ Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/apis/ontop/OntopClient.java
@@ -0,0 +1,75 @@
+package com.cmclinnovations.apis.ontop;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+import com.cmclinnovations.apis.ContainerClient;
+
+public class OntopClient extends ContainerClient {
+
+    public static final String ONTOP_MAPPING_FILE = "ONTOP_MAPPING_FILE";
+
+    public void updateOBDA(Path newMappingFilePath) {
+        String containerId = getDockerClient().getContainerId("ontop");
+        Path ontopMappingFilePath = getDockerClient().getEnvironmentVariable(containerId, ONTOP_MAPPING_FILE)
+                .map(Path::of)
+                .orElseThrow(() -> new RuntimeException("Environment variable '" + ONTOP_MAPPING_FILE
+                        + " not set through Docker for '" + "ontop" + "' container."));
+        Path localTempOntopMappingFilePath = null;
+        boolean exceptionThrown = false;
+        try {
+            SQLPPMappingImplementation mapping = new SQLPPMappingImplementation();
+
+            localTempOntopMappingFilePath = SQLPPMappingImplementation.createTempOBDAFile(ontopMappingFilePath);
+
+            if (getDockerClient().fileExists(containerId, ontopMappingFilePath.toString())) {
+                if (null == newMappingFilePath) {
+                    // A mapping file already exists and no new one has been passed to be added.
+                    return;
+                }
+                try (OutputStream outputStream = Files.newOutputStream(localTempOntopMappingFilePath)) {
+                    outputStream.write(getDockerClient().retrieveFile(containerId, ontopMappingFilePath.toString()));
+                    mapping.addMappings(localTempOntopMappingFilePath);
+                }
+            }
+
+            if (null != newMappingFilePath) {
+                mapping.addMappings(newMappingFilePath);
+            }
+            localTempOntopMappingFilePath = SQLPPMappingImplementation.createTempOBDAFile(ontopMappingFilePath);
+            mapping.serialize(localTempOntopMappingFilePath);
+
+            try {
+                Map<String, byte[]> files = Map.of(ontopMappingFilePath.getFileName().toString(),
+                        Files.readAllBytes(localTempOntopMappingFilePath));
+                getDockerClient().sendFiles(containerId, files, ontopMappingFilePath.getParent().toString());
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } catch (IOException ex) {
+            exceptionThrown = true;
+            throw new RuntimeException(
+                    "Failed to write out combined Ontop mapping file '" + ontopMappingFilePath + "'.", ex);
+        } catch (Throwable ex) {
+            exceptionThrown = true;
+            throw ex;
+        } finally {
+            if (null != localTempOntopMappingFilePath && Files.exists(localTempOntopMappingFilePath)) {
+                try {
+                    Files.delete(localTempOntopMappingFilePath);
+                } catch (Exception ex2) {
+                    if (exceptionThrown) {
+                        // Don't worry about this exception as any previously thrown exception is more
+                        // important.
+                    } else {
+                        throw new RuntimeException(ex2);
+                    }
+                }
+            }
+        }
+    }
+}
--- Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/services/OntopService.java
+++ Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/services/OntopService.java
@@ -1,115 +1,18 @@
 package com.cmclinnovations.services;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-import com.cmclinnovations.FileUtils;
 import com.cmclinnovations.apis.PostGISEndpointConfig;
+import com.cmclinnovations.apis.ontop.OntopClient;
 import com.cmclinnovations.services.config.ServiceConfig;
 import com.github.dockerjava.api.model.ContainerSpec;
 import com.github.dockerjava.api.model.ContainerSpecConfig;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-
-import it.unibz.inf.ontop.exception.MappingException;
-import it.unibz.inf.ontop.injection.OntopMappingConfiguration;
-import it.unibz.inf.ontop.injection.OntopSQLOWLAPIConfiguration;
-import it.unibz.inf.ontop.spec.mapping.PrefixManager;
-import it.unibz.inf.ontop.spec.mapping.pp.PreProcessedMapping;
-import it.unibz.inf.ontop.spec.mapping.pp.SQLPPMapping;
-import it.unibz.inf.ontop.spec.mapping.pp.SQLPPTriplesMap;
-import it.unibz.inf.ontop.spec.mapping.serializer.impl.OntopNativeMappingSerializer;
 
 public final class OntopService extends ContainerService {
-
-    private static final class SQLPPMappingImplementation implements SQLPPMapping {
-
-        private Map<String, String> prefixMap = new HashMap<>();
-        private List<SQLPPTriplesMap> triplesMap = new ArrayList<>();
-
-        public void addMappings(Path ontopMappingFilePath) {
-            try {
-                Path tempFilePath = reformatMappingFile(ontopMappingFilePath);
-
-                PreProcessedMapping<SQLPPTriplesMap> extraMapings = generateConfiguration(tempFilePath)
-                        .loadProvidedPPMapping();
-
-                prefixMap.putAll(extraMapings.getPrefixManager().getPrefixMap());
-                triplesMap.addAll(extraMapings.getTripleMaps());
-            } catch (MappingException ex) {
-                // TODO Auto-generated catch block
-                ex.printStackTrace();
-            } catch (IOException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-        }
-
-        private static Path reformatMappingFile(Path ontopMappingFilePath) throws IOException {
-            Path tempFilePath = createTempOBDAFile(ontopMappingFilePath);
-            try (BufferedReader bufferedReader = Files.newBufferedReader(ontopMappingFilePath);
-                    BufferedWriter bufferedWriter = Files.newBufferedWriter(tempFilePath)) {
-                bufferedReader.lines()
-                        .map(line -> line.replaceFirst("^ *#.*$", ""))
-                        .map(line -> line.replaceFirst("(, *|; *|\\. *)", "\1\n"))
-                        .forEachOrdered(string -> {
-                            try {
-                                bufferedWriter.write(string, 0, string.length());
-                            } catch (IOException e) {
-                                // TODO Auto-generated catch block
-                                e.printStackTrace();
-                            }
-                        });
-            }
-            return tempFilePath;
-        }
-
-        private static Path createTempOBDAFile(Path ontopMappingFilePath) throws IOException {
-            return File.createTempFile(FileUtils.getFileNameWithoutExtension(ontopMappingFilePath), ".obda").toPath();
-        }
-
-        private static OntopSQLOWLAPIConfiguration generateConfiguration(Path ontopMappingFilePath) {
-            return OntopSQLOWLAPIConfiguration
-                    .defaultBuilder()
-                    .nativeOntopMappingFile(ontopMappingFilePath.toFile())
-                    .jdbcDriver("dummy")
-                    .jdbcUrl("dummy")
-                    .jdbcUser("")
-                    .jdbcPassword("")
-                    .build();
-        }
-
-        @Override
-        public PrefixManager getPrefixManager() {
-            return OntopMappingConfiguration.defaultBuilder().build()
-                    .getSpecificationFactory()
-                    .createPrefixManager(ImmutableMap.copyOf(prefixMap));
-        }
-
-        @Override
-        public ImmutableList<SQLPPTriplesMap> getTripleMaps() {
-            return ImmutableList.copyOf(triplesMap);
-        }
-
-        public void serialize(Path ontopMappingFilePath) throws IOException {
-
-            OntopNativeMappingSerializer writer = new OntopNativeMappingSerializer();
-
-            writer.write(ontopMappingFilePath.toFile(), this);
-
-        }
-    }
 
     public static final String TYPE = "ontop";
 
@@ -118,7 +21,6 @@ public final class OntopService extends ContainerService {
     private static final String ONTOP_DB_DRIVER_URL = "ONTOP_DB_DRIVER_URL";
     private static final String ONTOP_DB_USER = "ONTOP_DB_USER";
     private static final String ONTOP_DB_PASSWORD_FILE = "ONTOP_DB_PASSWORD_FILE";
-    private static final String ONTOP_MAPPING_FILE = "ONTOP_MAPPING_FILE";
 
     public OntopService(String stackName, ServiceManager serviceManager, ServiceConfig config) {
         super(stackName, serviceManager, config);
@@ -149,7 +51,7 @@ public final class OntopService extends ContainerService {
         setEnvironmentVariableIfAbsent("ONTOP_LAZY_INIT", "false");
         setEnvironmentVariableIfAbsent("ONTOP_CORS_ALLOWED_ORIGINS", "*");
         setEnvironmentVariableIfAbsent("ONTOP_DEBUG", "false");
-        checkEnvironmentVariableNonNull(ONTOP_MAPPING_FILE);
+        checkEnvironmentVariableNonNull(OntopClient.ONTOP_MAPPING_FILE);
 
         containerSpec
                 .withCommand(List.of("/bin/sh", "-c", "wget -P /opt/ontop/jdbc "
@@ -159,7 +61,7 @@ public final class OntopService extends ContainerService {
 
     @Override
     public void doPostStartUpConfiguration() {
-        transformOBDA();
+        new OntopClient().updateOBDA(null);
     }
 
     @Deprecated
@@ -172,29 +74,6 @@ public final class OntopService extends ContainerService {
         } catch (MalformedURLException ex) {
             throw new IllegalArgumentException("Value provided for the environment variable '" + ONTOP_DB_DRIVER_URL
                     + "' for contianer '" + getName() + "' is not a valid URL.", ex);
-        }
-    }
-
-    private void transformOBDA() {
-        Path ontopMappingFilePath = Path.of(getEnvironmentVariable(ONTOP_MAPPING_FILE));
-
-        try {
-            SQLPPMappingImplementation mapping = new SQLPPMappingImplementation();
-            Path localTempOntopMappingFilePath = SQLPPMappingImplementation.createTempOBDAFile(ontopMappingFilePath);
-            mapping.serialize(localTempOntopMappingFilePath);
-
-            try {
-                Map<String, byte[]> files = Map.of(ontopMappingFilePath.getFileName().toString(),
-                        Files.readAllBytes(localTempOntopMappingFilePath));
-
-                sendFiles(files, ontopMappingFilePath.getParent().toString());
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        } catch (IOException ex) {
-            throw new RuntimeException(
-                    "Failed to write out combined Ontop mapping file '" + ontopMappingFilePath + "'.", ex);
         }
     }
 
