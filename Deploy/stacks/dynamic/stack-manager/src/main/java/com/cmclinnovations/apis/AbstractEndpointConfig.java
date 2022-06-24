4b57314a49c6dbdcbd3efe10c2ef4fe9840dee18
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-06-24 15:24:14 +0100

    dev-stack-manager: Removed some redundant import statements.

--- Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/apis/AbstractEndpointConfig.java
+++ Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/apis/AbstractEndpointConfig.java
@@ -1,11 +1,5 @@
 package com.cmclinnovations.apis;
 
-import java.io.BufferedReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 public abstract class AbstractEndpointConfig {
 
     private final String name;
