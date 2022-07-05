b964c7292e288750f5b6ca7424c57f4561920985
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-07-05 15:53:06 +0100

    dev-stack-manager: Added code to the PostGISService class to generate a ".pgpass" file so that the "psql" executable can be run inside the PostGIS container without needing to specify the password.

--- Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/stack/services/PostGISService.java
+++ Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/stack/services/PostGISService.java
@@ -1,11 +1,17 @@
 package com.cmclinnovations.stack.services;
 
 import com.cmclinnovations.stack.services.config.ServiceConfig;
+
+import java.nio.file.Path;
+import java.util.Map;
+
 import com.cmclinnovations.stack.clients.postgis.PostGISEndpointConfig;
 
 public final class PostGISService extends ContainerService {
 
     public static final String TYPE = "postgres";
+
+    private static final Path PGPASS_FILE = Path.of("/root", ".pgpass");
 
     private static final String DEFAULT_USERNAME = "postgres";
     private static final String DEFAULT_PORT = "5432";
@@ -18,6 +24,8 @@ public final class PostGISService extends ContainerService {
 
         setEnvironmentVariableIfAbsent("POSTGRES_USER", DEFAULT_USERNAME);
         setEnvironmentVariableIfAbsent("POSTGRES_PASSWORD_FILE", DEFAULT_PASSWORD_FILE);
+        setEnvironmentVariableIfAbsent("PGPASSFILE", PGPASS_FILE.toString());
+
         endpointConfig = new PostGISEndpointConfig("postgis", getContainerName(), DEFAULT_PORT,
                 getEnvironmentVariable("POSTGRES_USER"), getEnvironmentVariable("POSTGRES_PASSWORD_FILE"));
     }
@@ -25,6 +33,18 @@ public final class PostGISService extends ContainerService {
     @Override
     public void doPostStartUpConfiguration() {
         writeEndpointConfig(endpointConfig);
+
+        sendFiles(Map.of(
+                PGPASS_FILE.getFileName().toString(),
+                ("localhost:"
+                        + endpointConfig.getPort() + ":"
+                        + "*:"
+                        + endpointConfig.getUsername() + ":"
+                        + endpointConfig.getPassword())
+                        .getBytes()),
+                PGPASS_FILE.getParent().toString());
+
+        executeCommand("chmod", "0600", PGPASS_FILE.toString());
     }
 
 }
