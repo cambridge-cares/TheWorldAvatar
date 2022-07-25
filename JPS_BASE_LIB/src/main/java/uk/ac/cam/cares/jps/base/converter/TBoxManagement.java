6263e9c0145a456d7684ace1264502c694fb4f8c
Feroz Farazi <msff2@cam.ac.uk>
2022-07-25 18:37:17 +0100

    dev-add-restriction-n-dom-rang-together: Modified business logic to add domain and range even if quantifier is provided in TBox CSV template.

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/converter/TBoxManagement.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/converter/TBoxManagement.java
@@ -276,7 +276,7 @@ public class TBoxManagement extends TBoxGeneration implements ITBoxManagement{
 	}
 	
 	/**
-	 * Adds the definedBy annotation property to the current object property.
+	 * Adds a logical formula to the current object property.
 	 * 
 	 * @param property
 	 * @param quantifier
@@ -353,7 +353,7 @@ public class TBoxManagement extends TBoxGeneration implements ITBoxManagement{
 		} else if(quantifier !=null && !quantifier.isEmpty()
 				&& quantifier.trim().equalsIgnoreCase("maximum 1")){
 			addMaximumOneQuantification(objectProperty, domainClass, rangeClass);
-		} 
+		}
 	}
 	
 	/**
@@ -425,9 +425,10 @@ public class TBoxManagement extends TBoxGeneration implements ITBoxManagement{
 	 * @param relation
 	 * @param domain
 	 * @param range
+	 * @param quantifier
 	 * @throws JPSRuntimeException
 	 */
-	public void createOWLDataProperty(String propertyName, String type, String targetName, String relation, String domain, String range) throws JPSRuntimeException {
+	public void createOWLDataProperty(String propertyName, String type, String targetName, String relation, String domain, String range, String quantifier) throws JPSRuntimeException {
 		checkPropertyName(propertyName);
 			OWLDataProperty dataProperty = createDataProperty(propertyName);
 			
@@ -558,7 +559,7 @@ public class TBoxManagement extends TBoxGeneration implements ITBoxManagement{
 			addUnionOfDomain(objectProperty, domain.split("UNION"));
 		} else if(domain.contains("INTERSECTION")){
 			addIntersectionOfDomain(objectProperty, domain.split("INTERSECTION"));
-		} else if(quantifier==null || quantifier.isEmpty()){
+		} else if(domain!=null || !domain.isEmpty()){
 			addSingleClassDomain(objectProperty, domain);
 		}
 	}
@@ -598,7 +599,7 @@ public class TBoxManagement extends TBoxGeneration implements ITBoxManagement{
 			addUnionOfRange(objectProperty, range.split("UNION"));
 		} else if(range.contains("INTERSECTION") && (quantifier==null || quantifier.isEmpty())){
 			addIntersectionOfRange(objectProperty, range.split("INTERSECTION"));
-		}else if(quantifier==null || quantifier.isEmpty()){
+		}else if(range!=null || !range.isEmpty()){
 			addSingleClassRange(objectProperty, range);
 		}
 	}
