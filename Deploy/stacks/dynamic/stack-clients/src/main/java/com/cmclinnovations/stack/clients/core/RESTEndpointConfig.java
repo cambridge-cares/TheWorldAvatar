59e466111ed82236cde9b3832121c46f478c6046
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-07-26 10:15:35 +0100

    dev-stack-manager: Removed seemingly unnecessary uses of the URL/URIDeserialiser classes as Jackson seems to be able to deserialise them fine.

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/RESTEndpointConfig.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/RESTEndpointConfig.java
@@ -2,12 +2,8 @@ package com.cmclinnovations.stack.clients.core;
 
 import java.net.URL;
 
-import com.cmclinnovations.stack.jackson.deserialisers.URLDeserialiser;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
 public class RESTEndpointConfig extends PasswordEndpointConfig {
 
-    @JsonDeserialize(using = URLDeserialiser.class)
     private final URL url;
 
     private final String userName;
