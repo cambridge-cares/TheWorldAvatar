175bad71d11ea2093fbf4c520b5b1a8cc41a80b2
Casper Lindberg <casper.lindberg@cares.cam.ac.uk>
2022-07-26 12:37:33 +0800

    dev-improve-cloning-tool: add CloningToolTest

---
+++ JPS_BASE_LIB/src/test/java/uk/ac/cam/cares/jps/base/tools/test/CloningToolTest.java
@@ -0,0 +1,16 @@
+package uk.ac.cam.cares.jps.base.tools.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import uk.ac.cam.cares.jps.base.tools.CloningTool;
+
+class CloningToolTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
