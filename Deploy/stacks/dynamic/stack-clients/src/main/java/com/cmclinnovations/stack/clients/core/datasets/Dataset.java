de4ff095452c36f1e310544bd0524c3f369f8cc9
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-07-28 13:17:31 +0100

    dev-stack-manager: Updated the Dataset class so that it can also process Ontop OBDA mappings.

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/Dataset.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/Dataset.java
@@ -8,6 +8,7 @@ import java.util.function.Predicate;
 import com.cmclinnovations.stack.clients.gdal.GDALClient;
 import com.cmclinnovations.stack.clients.geoserver.GeoServerClient;
 import com.cmclinnovations.stack.clients.geoserver.GeoServerStyle;
+import com.cmclinnovations.stack.clients.ontop.OntopClient;
 import com.cmclinnovations.stack.clients.postgis.PostGISClient;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -24,19 +25,23 @@ public class Dataset {
 
     private List<GeoServerStyle> geoserverStyles;
 
+    private List<String> ontopMappings;
+
     @JsonCreator
     public Dataset(@JsonProperty(value = "name") String name,
             @JsonProperty(value = "datasetDirectory") Path datasetDirectory,
             @JsonProperty(value = "workspace") String workspaceName,
             @JsonProperty(value = "database") String database,
             @JsonProperty(value = "dataSubsets") List<DataSubset> dataSubsets,
-            @JsonProperty(value = "styles") List<GeoServerStyle> geoserverStyles) {
+            @JsonProperty(value = "styles") List<GeoServerStyle> geoserverStyles,
+            @JsonProperty(value = "mappings") List<String> ontopMappings) {
         this.name = name;
         this.datasetDirectory = (null != datasetDirectory) ? datasetDirectory : Path.of(name);
         this.workspaceName = (null != workspaceName) ? workspaceName : name;
         this.database = database;
         this.dataSubsets = (null != dataSubsets) ? dataSubsets : new ArrayList<>();
         this.geoserverStyles = (null != geoserverStyles) ? geoserverStyles : new ArrayList<>();
+        this.ontopMappings = (null != ontopMappings) ? ontopMappings : new ArrayList<>();
     }
 
     public String getName() {
@@ -63,6 +68,10 @@ public class Dataset {
         PostGISClient postGISClient = new PostGISClient();
         GDALClient gdalClient = new GDALClient();
         GeoServerClient geoServerClient = new GeoServerClient();
+        OntopClient ontopClient = new OntopClient();
+
+        Path fullDatasetDir = Path.of("/inputs", "data").resolve(datasetDirectory);
+        String fullDatasetDirStr = fullDatasetDir.toString();
 
         postGISClient.createDatabase(database);
         geoServerClient.createWorkspace(workspaceName);
@@ -71,10 +80,11 @@ public class Dataset {
 
         dataSubsets.stream().filter(Predicate.not(DataSubset::getSkip)).forEach(
                 subset -> {
-                    String dataSubsetDir = Path.of("/inputs", "data").resolve(getDatasetDirectory()).toString();
-                    subset.loadData(gdalClient, dataSubsetDir, database);
-                    subset.createLayer(geoServerClient, dataSubsetDir, workspaceName, database);
+                    subset.loadData(gdalClient, fullDatasetDirStr, database);
+                    subset.createLayer(geoServerClient, fullDatasetDirStr, workspaceName, database);
                 });
+
+        ontopMappings.forEach(mapping -> ontopClient.updateOBDA(fullDatasetDir.resolve(mapping)));
     }
 
 }
