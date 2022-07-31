aaaab6bafc3c8ebf3d2c29e5498fa987334ee70c
jb2197 <jb2197@cam.ac.uk>
2022-07-31 03:27:31 +0100

    161-dev-pyderivationagent-modifications: made development release for pyderivationagent 1.1.0b.

--- JPS_BASE_LIB/python_derivation_agent/pyderivationagent/__init__.py
+++ JPS_BASE_LIB/python_derivation_agent/pyderivationagent/__init__.py
@@ -2,4 +2,4 @@ from .agent import DerivationAgent, FlaskConfig
 from .conf import AgentConfig, config_derivation_agent
 from .data_model import DerivationInputs, DerivationOutputs
 
-__version__ = "1.0.0"
+__version__ = "1.1.0b"
--- JPS_BASE_LIB/python_derivation_agent/setup.py
+++ JPS_BASE_LIB/python_derivation_agent/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyderivationagent',
-    version='1.1.0a',
+    version='1.1.0b',
     author='Jiaru Bai',
     author_email='jb2197@cam.ac.uk',
     license='MIT',
