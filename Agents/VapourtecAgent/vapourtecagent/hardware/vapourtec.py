a94baa4c4d521cacbdda43c7a55442ff43bcd590
jb2197 <jb2197@cam.ac.uk>
2022-08-01 17:23:35 +0100

    134-dev-lab-equipment-digital-twin: [VapourtecAgent] added additional possible state of vapourtec hardware identified during test deployment.

--- Agents/VapourtecAgent/vapourtecagent/hardware/vapourtec.py
+++ Agents/VapourtecAgent/vapourtecagent/hardware/vapourtec.py
@@ -26,9 +26,12 @@ FC_MANUAL_COLLECT = "Manual Collect"
 FC_MANUAL_DIVERT = "Manual Divert"
 
 # Mapping int value returned by FlowCommander.GetState(fc) to the actual state
-# TODO expand to host all possible state
+# TODO double check if any other possible state
 MAPPING_VAPOURTEC_STATE = {
     0: ONTOVAPOURTEC_IDLE,
+    1: ONTOVAPOURTEC_INITIALISING,
+    2: ONTOVAPOURTEC_RUNNINGREACTION,
+    4: ONTOVAPOURTEC_FINALCLEANING,
 }
 
 # Mapping decimal place constraint with settings title
