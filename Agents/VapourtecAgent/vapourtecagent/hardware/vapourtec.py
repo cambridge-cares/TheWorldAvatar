9173e7a1b212bbbae275407dc376a3d7071f8626
jb2197 <jb2197@cam.ac.uk>
2022-08-02 18:29:57 +0100

    134-dev-lab-equipment-digital-twin: [chemistry-and-robots util] added new vapourtec state to list of valid state.

--- Agents/VapourtecAgent/vapourtecagent/hardware/vapourtec.py
+++ Agents/VapourtecAgent/vapourtecagent/hardware/vapourtec.py
@@ -27,6 +27,8 @@ FC_MANUAL_DIVERT = "Manual Divert"
 
 # Mapping int value returned by FlowCommander.GetState(fc) to the actual state
 # TODO double check if any other possible state
+# NOTE when adding new mappings, remember to update the list in:
+# chemistry_and_robots.data_model.ontovapourtec.LIST_ONTOVAPOURTEC_VALIDSTATE
 MAPPING_VAPOURTEC_STATE = {
     0: ONTOVAPOURTEC_IDLE,
     1: ONTOVAPOURTEC_INITIALISING,
