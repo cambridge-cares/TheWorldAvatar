d5fbc4a824db1ba98eeb66cca73ab597b2726b9a
jb2197 <jb2197@cam.ac.uk>
2022-08-07 19:27:39 +0100

    134-dev-lab-equipment-digital-twin: [chemistry-and-robots util] improved README.md; added release scripts to PyPI; made necessary changes in setup.py script.

--- Agents/utils/chemistry-and-robots/setup.py
+++ Agents/utils/chemistry-and-robots/setup.py
@@ -2,21 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name='chemistry_and_robots',
-    version='0.0.1',
+    version='0.0.1a',
     author='Jiaru Bai',
     author_email='jb2197@cam.ac.uk',
     license='MIT',
     python_requires='>=3.7',
     description="chemistryandrobots contains dataclasses and sparql queries for concepts related to chemistry-and-robots as part of The World Avatar project.",
-    # url="https://github.com/cambridge-cares/TheWorldAvatar/tree/134-dev-lab-equipment-digital-twin/Agents/utils/chemistry-and-robots",
-    # long_description=open('README.md').read(),
-    # long_description_content_type="text/markdown",
+    url="https://github.com/cambridge-cares/TheWorldAvatar/tree/main/Agents/utils/chemistry-and-robots",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=['tests','tests.*']),
-    install_requires=['pyderivationagent>=1.1.0', 'pydantic==1.9.0', 'xlrd', 'xlwt'],
+    install_requires=['pyderivationagent>=1.1.0', 'pydantic==1.9.0', 'pandas', 'xlrd', 'xlwt'],
     extras_require={
         "dev": [
             "testcontainers>=3.4.2",
             "pytest>=6.2.3",
+            "pytest-docker-compose>=3.2.1",
+            "pytest-rerunfailures>=10.2"
         ],
     },
     include_package_data= True
