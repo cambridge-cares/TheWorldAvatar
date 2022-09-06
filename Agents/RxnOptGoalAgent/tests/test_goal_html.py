e7edcbf54b0942412587e68a0ba86f643133ea84
Jiaru Bai <jb2197@cam.ac.uk>
2022-09-06 22:17:26 +0100

    160-dev-rxn-opt-goal-agent: [RxnOptGoalAgent] updated test_goal_html.py::test_handle_rxn_opt_goal_request to use the dynamically created test container.

--- Agents/RxnOptGoalAgent/tests/test_goal_html.py
+++ Agents/RxnOptGoalAgent/tests/test_goal_html.py
@@ -1,36 +1,39 @@
-# this file tests the function RxnOptGoalAgent::handle_rxn_opt_goal_request
-
 from datetime import datetime
 import json
+import uuid
 
 import tests.conftest as cf
 
-sample_goal_request = {
-    "chem_rxn": "https://www.example.com/triplestore/ontorxn/ChemRxn_1/ChemRxn_1",
-    "cycleAllowance": 6,
-    "deadline": "2022-09-12T17:05",
-    "first_goal_clz": "https://raw.githubusercontent.com/cambridge-cares/TheWorldAvatar/main/JPS_Ontology/ontology/ontoreaction/OntoReaction.owl#Conversion",
-    "first_goal_desires": "https://raw.githubusercontent.com/cambridge-cares/TheWorldAvatar/main/JPS_Ontology/ontology/ontogoal/OntoGoal.owl#desiresGreaterThan",
-    "first_goal_num_val": 20,
-    "first_goal_unit": "http://www.ontology-of-units-of-measure.org/resource/om-2/percent",
-    "rxn_opt_goal_plan": "http://www.theworldavatar.com/resource/plans/RxnOpt/rxnoptplan",
-    "second_goal_clz": "https://raw.githubusercontent.com/cambridge-cares/TheWorldAvatar/main/JPS_Ontology/ontology/ontoreaction/OntoReaction.owl#Yield",
-    "second_goal_desires": "https://raw.githubusercontent.com/cambridge-cares/TheWorldAvatar/main/JPS_Ontology/ontology/ontogoal/OntoGoal.owl#desiresLessThan",
-    "second_goal_num_val": 30,
-    "second_goal_unit": "http://www.ontology-of-units-of-measure.org/resource/om-2/percent"
-}
 
-def test_handle_rxn_opt_goal_request(create_rog_agent):
-    # upon receiving a goal request, the rog agent should:
+# ----------------------------------------------------------------------------------
+# Test cases for the function RxnOptGoalAgent::handle_rxn_opt_goal_request
+# upon receiving a goal request, the rog agent should:
+# 1. correctly create the rogi derivation
+# TODO 2. start the goal iteration monitoring job
+# ----------------------------------------------------------------------------------
+
+def test_handle_rxn_opt_goal_request(
+    initialise_test_triples,
+    create_rog_agent
+):
+    endpoint = initialise_test_triples
+
+    rog_agent = create_rog_agent(
+        goal_iter_agent_iri=f'http://{str(uuid.uuid4())}',
+        kg_url=endpoint,
+        kg_update_url=endpoint,
+    )
+
     # 1. correctly create the rogi derivation
-    # 2. start the goal iteration monitoring job
-
-    rog_agent = create_rog_agent()
+    sample_goal_request = cf.sample_goal_request
     with rog_agent.app.test_client() as test_client:
         r = test_client.post(rog_agent.goal_agent_endpoint, data=sample_goal_request)
         j = json.loads(r.text)
         rogi_derivation = j[rog_agent.GOAL_SPECS_RESPONSE_KEY]
         assert rogi_derivation is not None
+        assert rog_agent.sparql_client.check_if_triple_exist(
+            rogi_derivation, cf.ONTODERIVATION_ISDERIVEDUSING, rog_agent.goal_iter_agent_iri
+        )
 
         # Check that the experiment beliefs are correct
         rxn_exp_beliefs_res = rog_agent.sparql_client.performQuery(
@@ -42,11 +45,11 @@ def test_handle_rxn_opt_goal_request(create_rog_agent):
         )
         rxn_exp_beliefs = [x['rxn_exp'] for x in rxn_exp_beliefs_res]
         assert cf.dal.check_if_two_lists_equal(rxn_exp_beliefs, [
-            cf.rogi_IRIs.EXP_1_BASE_IRI.value,
-            cf.rogi_IRIs.EXP_2_BASE_IRI.value,
-            cf.rogi_IRIs.EXP_3_BASE_IRI.value,
-            cf.rogi_IRIs.EXP_4_BASE_IRI.value,
-            cf.rogi_IRIs.EXP_5_BASE_IRI.value,
+            cf.rogi_cf.IRIs.EXP_1_BASE_IRI.value,
+            cf.rogi_cf.IRIs.EXP_2_BASE_IRI.value,
+            cf.rogi_cf.IRIs.EXP_3_BASE_IRI.value,
+            cf.rogi_cf.IRIs.EXP_4_BASE_IRI.value,
+            cf.rogi_cf.IRIs.EXP_5_BASE_IRI.value,
         ])
 
         # Check that the GoalSet is valid
@@ -89,9 +92,9 @@ def test_handle_rxn_opt_goal_request(create_rog_agent):
 
         # plan
         assert len(goal_1.hasPlan) == 1
-        assert goal_1.hasPlan[0].instance_iri == cf.rogi_IRIs.RXN_OPT_PLAN.value
+        assert goal_1.hasPlan[0].instance_iri == cf.rogi_cf.IRIs.RXN_OPT_PLAN.value
         assert len(goal_2.hasPlan) == 1
-        assert goal_2.hasPlan[0].instance_iri == cf.rogi_IRIs.RXN_OPT_PLAN.value
+        assert goal_2.hasPlan[0].instance_iri == cf.rogi_cf.IRIs.RXN_OPT_PLAN.value
 
         # step
         for goal in [goal_1, goal_2]:
@@ -99,35 +102,36 @@ def test_handle_rxn_opt_goal_request(create_rog_agent):
 
             # step 1 - doe
             doe_step = goal.hasPlan[0].get_step(cf.ONTOGOAL_DESIGNOFEXPERIMENT)
-            assert doe_step.instance_iri == cf.rogi_IRIs.STEP_DOE.value
+            assert doe_step.instance_iri == cf.rogi_cf.IRIs.STEP_DOE.value
             assert len(doe_step.canBePerformedBy) == 1
-            assert doe_step.canBePerformedBy[0] == cf.rogi_IRIs.STEP_DOE_AGENT.value
+            assert doe_step.canBePerformedBy[0] == cf.rogi_cf.IRIs.STEP_DOE_AGENT.value
             assert len(doe_step.hasNextStep) == 1
-            assert doe_step.hasNextStep[0].instance_iri == cf.rogi_IRIs.STEP_EXE.value
+            assert doe_step.hasNextStep[0].instance_iri == cf.rogi_cf.IRIs.STEP_EXE.value
             assert len(doe_step.hasNextStep[0].canBePerformedBy) == 1
-            assert doe_step.hasNextStep[0].canBePerformedBy[0] == cf.rogi_IRIs.STEP_EXE_AGENT.value
+            assert doe_step.hasNextStep[0].canBePerformedBy[0] == cf.rogi_cf.IRIs.STEP_EXE_AGENT.value
             assert len(doe_step.hasNextStep[0].hasNextStep) == 1
-            assert doe_step.hasNextStep[0].hasNextStep[0].instance_iri == cf.rogi_IRIs.STEP_POSTPRO.value
+            assert doe_step.hasNextStep[0].hasNextStep[0].instance_iri == cf.rogi_cf.IRIs.STEP_POSTPRO.value
             assert len(doe_step.hasNextStep[0].hasNextStep[0].canBePerformedBy) == 1
-            assert doe_step.hasNextStep[0].hasNextStep[0].canBePerformedBy[0] == cf.rogi_IRIs.STEP_POSTPRO_AGENT.value
+            assert doe_step.hasNextStep[0].hasNextStep[0].canBePerformedBy[0] == cf.rogi_cf.IRIs.STEP_POSTPRO_AGENT.value
             assert doe_step.hasNextStep[0].hasNextStep[0].hasNextStep is None
 
             # step 2 - exe
             exe_step = goal.hasPlan[0].get_step(cf.ONTOGOAL_RXNEXPEXECUTION)
-            assert exe_step.instance_iri == cf.rogi_IRIs.STEP_EXE.value
+            assert exe_step.instance_iri == cf.rogi_cf.IRIs.STEP_EXE.value
             assert len(exe_step.canBePerformedBy) == 1
-            assert exe_step.canBePerformedBy[0] == cf.rogi_IRIs.STEP_EXE_AGENT.value
+            assert exe_step.canBePerformedBy[0] == cf.rogi_cf.IRIs.STEP_EXE_AGENT.value
             assert len(exe_step.hasNextStep) == 1
-            assert exe_step.hasNextStep[0].instance_iri == cf.rogi_IRIs.STEP_POSTPRO.value
+            assert exe_step.hasNextStep[0].instance_iri == cf.rogi_cf.IRIs.STEP_POSTPRO.value
             assert len(exe_step.hasNextStep[0].canBePerformedBy) == 1
-            assert exe_step.hasNextStep[0].canBePerformedBy[0] == cf.rogi_IRIs.STEP_POSTPRO_AGENT.value
+            assert exe_step.hasNextStep[0].canBePerformedBy[0] == cf.rogi_cf.IRIs.STEP_POSTPRO_AGENT.value
             assert exe_step.hasNextStep[0].hasNextStep is None
 
             # step 3 - postpro
             postpro_step = goal.hasPlan[0].get_step(cf.ONTOGOAL_POSTPROCESSING)
-            assert postpro_step.instance_iri == cf.rogi_IRIs.STEP_POSTPRO.value
+            assert postpro_step.instance_iri == cf.rogi_cf.IRIs.STEP_POSTPRO.value
             assert len(postpro_step.canBePerformedBy) == 1
-            assert postpro_step.canBePerformedBy[0] == cf.rogi_IRIs.STEP_POSTPRO_AGENT.value
+            assert postpro_step.canBePerformedBy[0] == cf.rogi_cf.IRIs.STEP_POSTPRO_AGENT.value
             assert postpro_step.hasNextStep is None
 
+        # TODO 2. start the goal iteration monitoring job
         # TODO test if the periodical goal monitoring job is started
