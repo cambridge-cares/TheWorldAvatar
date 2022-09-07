5fa8e8c413416e326aaa27040077f9006c7f1d49
Markus Hofmeister <mh807@cam.ac.uk>
2022-09-07 17:59:21 +0800

    dev-MetOfficeAgent-withinStack: All connection properties now read from Stack Clients; First MetOffice stations being instantiated in Blazegraph

--- Agents/MetOfficeAgent/metoffice/utils/properties.py
+++ Agents/MetOfficeAgent/metoffice/utils/properties.py
@@ -1,7 +1,7 @@
-###############################################
-# Authors: Markus Hofmeister (mh807cam.ac.uk) #    
-# Date: 31 Mar 2022                           #
-###############################################
+################################################
+# Authors: Markus Hofmeister (mh807@cam.ac.uk) #    
+# Date: 31 Mar 2022                            #
+################################################
 
 # The purpose of this module is to read the properties file with relevant
 # settings (i.e. for the Time Series Client) and the MetOffice API key
@@ -12,13 +12,12 @@ import os
 from configobj import ConfigObj
 from pathlib import Path
 
+from metoffice.kgutils.javagateway import stackClientsGw
+
 # Initialise logger
 #logger = agentlogging.get_logger("prod")
 
 
-# Define location of properties file
-PROPERTIES_FILE = os.path.abspath(os.path.join(Path(__file__).parent.parent.parent, 
-                  "resources", "metoffice.properties"))
 
 # Initialise global variables to be read from properties file
 global DATAPOINT_API_KEY
@@ -26,18 +25,14 @@ global DB_URL, DB_USER, DB_PASSWORD
 global QUERY_ENDPOINT, UPDATE_ENDPOINT
 
 
-def read_properties_file(filepath):
+def retrieve_settings():
     """
-        Reads settings from properties file (as global variables).
-        Arguments:
-            filepath - absolute file path to properties file.
+        Reads settings from environment variables and Stack clients 
+        (as global variables).
     """
 
     # Define global scope for global variables
     global DATAPOINT_API_KEY, DB_URL, DB_USER, DB_PASSWORD, QUERY_ENDPOINT, UPDATE_ENDPOINT
-
-    # Read properties file
-    props = ConfigObj(filepath)
 
     # Extract MetOffice API key
     DATAPOINT_API_KEY = os.getenv('API_KEY')    
@@ -48,56 +43,75 @@ def read_properties_file(filepath):
         #logger.error('No "API_KEY" value has been provided in environment variables')
         raise ValueError('No "API_KEY" value has been provided in environment variables')
     
-        # Extract PostgreSQL database URL
-    try:
-        DB_URL = props['db.url']
-    except KeyError:
-        #logger.error('Key "db.url" is missing in properties file: ' + filepath)
-        raise KeyError('Key "db.url" is missing in properties file: ' + filepath)
-    if DB_URL == '':
-        #logger.error('No "db.url" value has been provided in properties file: ' + filepath)
-        raise KeyError('No "db.url" value has been provided in properties file: ' + filepath)
+
+    # Create module views to relevant Stack clients
+    stackClientsView = stackClientsGw.createModuleView()
+    stackClientsGw.importPackages(stackClientsView, "com.cmclinnovations.stack.clients.docker.ContainerClient")
+    stackClientsGw.importPackages(stackClientsView, "com.cmclinnovations.stack.clients.blazegraph.BlazegraphEndpointConfig")
+    stackClientsGw.importPackages(stackClientsView, "com.cmclinnovations.stack.clients.postgis.PostGISEndpointConfig")
+
+    # Retrieve endpoint configurations from Stack clients
+    containerClient = stackClientsView.ContainerClient()
+    bg = stackClientsView.BlazegraphEndpointConfig("","","","","")
+    bg_conf = containerClient.readEndpointConfig("blazegraph", bg.getClass())
+
+    pg = stackClientsView.PostGISEndpointConfig("","","","","")
+    pg_conf = containerClient.readEndpointConfig("postgis", pg.getClass())
+
+
+    # Extract PostgreSQL database URL
+    db_name = os.getenv('DATABASE')
+    #TODO: elaborate
+    # if db_name is None:
+    #     #logger.error('API_KEY is missing in environment variables')
+    #     raise ValueError('API_KEY is missing in environment variables')
+    # if db_name != '':
+    #     #logger.error('No "API_KEY" value has been provided in environment variables')
+    #     raise ValueError('No "API_KEY" value has been provided in environment variables')
+    DB_URL = pg_conf.getJdbcURL(db_name)
+    # except KeyError:
+    #     #logger.error('Key "db.url" is missing in properties file: ' + filepath)
+    #     raise KeyError('Key "db.url" is missing in properties file: ' + filepath)
+    # if DB_URL == '':
+    #     #logger.error('No "db.url" value has been provided in properties file: ' + filepath)
+    #     raise KeyError('No "db.url" value has been provided in properties file: ' + filepath)
 
     # Extract PostgreSQL database username
-    try:
-        DB_USER = props['db.user']
-    except KeyError:
-        #logger.error('Key "db.user" is missing in properties file: ' + filepath)
-        raise KeyError('Key "db.user" is missing in properties file: ' + filepath)
-    if DB_USER == '':
-        #logger.error('No "db.user" value has been provided in properties file: ' + filepath)
-        raise KeyError('No "db.user" value has been provided in properties file: ' + filepath)
+    DB_USER = pg_conf.getUsername()
+    # except KeyError:
+    #     #logger.error('Key "db.user" is missing in properties file: ' + filepath)
+    #     raise KeyError('Key "db.user" is missing in properties file: ' + filepath)
+    # if DB_USER == '':
+    #     #logger.error('No "db.user" value has been provided in properties file: ' + filepath)
+    #     raise KeyError('No "db.user" value has been provided in properties file: ' + filepath)
 
     # Extract PostgreSQL database password
-    try:
-        DB_PASSWORD = props['db.password']
-    except KeyError:
-        #logger.error('Key "db.password" is missing in properties file: ' + filepath)
-        raise KeyError('Key "db.password" is missing in properties file: ' + filepath)
-    if DB_PASSWORD == '':
-        #logger.error('No "db.password" value has been provided in properties file: ' + filepath)
-        raise KeyError('No "db.password" value has been provided in properties file: ' + filepath)
+    DB_PASSWORD = pg_conf.getPassword()
+    # except KeyError:
+    #     #logger.error('Key "db.password" is missing in properties file: ' + filepath)
+    #     raise KeyError('Key "db.password" is missing in properties file: ' + filepath)
+    # if DB_PASSWORD == '':
+    #     #logger.error('No "db.password" value has been provided in properties file: ' + filepath)
+    #     raise KeyError('No "db.password" value has been provided in properties file: ' + filepath)
 
     # Extract SPARQL Query endpoint of KG
-    try:
-        QUERY_ENDPOINT = props['sparql.query.endpoint']
-    except KeyError:
-        #logger.error('Key "sparql.query.endpoint" is missing in properties file: ' + filepath)
-        raise KeyError('Key "sparql.query.endpoint" is missing in properties file: ' + filepath)
-    if QUERY_ENDPOINT == '':
-        #logger.error('No "sparql.query.endpoint" value has been provided in properties file: ' + filepath)
-        raise KeyError('No "sparql.query.endpoint" value has been provided in properties file: ' + filepath)
+    QUERY_ENDPOINT = bg_conf.getUrl("kb")
+    # except KeyError:
+    #     #logger.error('Key "sparql.query.endpoint" is missing in properties file: ' + filepath)
+    #     raise KeyError('Key "sparql.query.endpoint" is missing in properties file: ' + filepath)
+    # if QUERY_ENDPOINT == '':
+    #     #logger.error('No "sparql.query.endpoint" value has been provided in properties file: ' + filepath)
+    #     raise KeyError('No "sparql.query.endpoint" value has been provided in properties file: ' + filepath)
 
     # Extract SPARQL Update endpoint of KG
-    try:
-        UPDATE_ENDPOINT = props['sparql.update.endpoint']
-    except KeyError:
-        #logger.error('Key "sparql.update.endpoint" is missing in properties file: ' + filepath)
-        raise KeyError('Key "sparql.update.endpoint" is missing in properties file: ' + filepath)
-    if UPDATE_ENDPOINT == '':
-        #logger.error('No "sparql.update.endpoint" value has been provided in properties file: ' + filepath)
-        raise KeyError('No "sparql.update.endpoint" value has been provided in properties file: ' + filepath)
+    UPDATE_ENDPOINT = bg_conf.getUrl("kb")
+    # except KeyError:
+    #     #logger.error('Key "sparql.update.endpoint" is missing in properties file: ' + filepath)
+    #     raise KeyError('Key "sparql.update.endpoint" is missing in properties file: ' + filepath)
+    # if UPDATE_ENDPOINT == '':
+    #     #logger.error('No "sparql.update.endpoint" value has been provided in properties file: ' + filepath)
+    #     raise KeyError('No "sparql.update.endpoint" value has been provided in properties file: ' + filepath)
 
 
 # Run when module is imported
-read_properties_file(PROPERTIES_FILE)
+retrieve_settings()
