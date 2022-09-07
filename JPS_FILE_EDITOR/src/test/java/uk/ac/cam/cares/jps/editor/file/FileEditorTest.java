18b3f1748462f9f07af51420729107a10f65f125
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-09-07 12:07:04 +0100

    dev-1285-upgrade-from-java-8: Simplified and improved the "FileEditorTest" so that better error messages are produced if the test fails.

--- JPS_FILE_EDITOR/src/test/java/uk/ac/cam/cares/jps/editor/file/FileEditorTest.java
+++ JPS_FILE_EDITOR/src/test/java/uk/ac/cam/cares/jps/editor/file/FileEditorTest.java
@@ -1,16 +1,16 @@
 package uk.ac.cam.cares.jps.editor.file;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 /**
  * A test created for assessing the correctness of the file editing tool.
@@ -23,29 +23,24 @@ public class FileEditorTest {
 	 * Verifies if the FileEditor can edit a list of strings in a target file.
 	 * 
 	 * @throws SQLException
+	 * @throws IOException
 	 */
 	@Test
-	public void editStringsTest() throws SQLException{
+	public void editStringsTest() throws SQLException, IOException {
 		File dataDir = new File("data/owl");
-		if (dataDir == null) {
-		      System.out.println("Folder not found.");
-		  } else {
-			  IFileEditor iFileEditor = new FileEditor();
-				List<String> fileExtensions = Arrays.asList(".owl");
-				List<String> oldStrings = Arrays.asList("http://theworldavatar.com/");
-				List<String> newStrings = Arrays.asList("http://www.theworldavatar.com/");
-				iFileEditor.replace(dataDir.getAbsolutePath(), fileExtensions, oldStrings, newStrings);
-				File expectedOutputFile = new File("data/output/0cee09a7-235b-4d73-adaf-09aeda8f50cc_34.owl");
-				File editedFile = new File("data/owl-edited/0cee09a7-235b-4d73-adaf-09aeda8f50cc_34.owl");
-				try {
-					assertNotNull(editedFile);
-					boolean isTwoEqual = FileUtils.contentEqualsIgnoreEOL(expectedOutputFile, editedFile, null);
-					assertTrue(isTwoEqual);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		  }
+
+		assertNotNull("Folder not found.", dataDir);
+
+		IFileEditor iFileEditor = new FileEditor();
+		List<String> fileExtensions = Arrays.asList(".owl");
+		List<String> oldStrings = Arrays.asList("http://theworldavatar.com/");
+		List<String> newStrings = Arrays.asList("http://www.theworldavatar.com/");
+		iFileEditor.replace(dataDir.getAbsolutePath(), fileExtensions, oldStrings, newStrings);
+		File expectedOutputFile = new File("data/output/0cee09a7-235b-4d73-adaf-09aeda8f50cc_34.owl");
+		File editedFile = new File("data/owl-edited/0cee09a7-235b-4d73-adaf-09aeda8f50cc_34.owl");
+
+		// This method will output any difference between the two files rather than just 'true' or 'false'
+		assertEquals(Files.readAllLines(expectedOutputFile.toPath()), Files.readAllLines(editedFile.toPath()));
 	}
 
 }
