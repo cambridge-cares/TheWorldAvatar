447662596b513f48888f56a4694ea38fc4cc239c
Mehal Agarwal <99166247+mehalaga@users.noreply.github.com>
2022-09-08 15:06:19 +0800

    1386-investigate-remotestore-client-for-rdb-access: added executeQuery and executeUpdate methods

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/query/RDBStoreClient.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/query/RDBStoreClient.java
@@ -1,14 +1,12 @@
 package uk.ac.cam.cares.jps.base.query;
 
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class RDBStoreClient {
 
     private Connection conn;
+    Statement stmt;
     // URL and credentials for the relational database
     private String rdbURL;
     private String rdbUser;
@@ -24,10 +22,32 @@ public class RDBStoreClient {
         try {
             Class.forName("org.postgresql.Driver");
             conn = DriverManager.getConnection(this.rdbURL, this.rdbUser, this.rdbPassword);
+            stmt = conn.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY, java.sql.ResultSet.CONCUR_READ_ONLY);
         } catch (SQLException | ClassNotFoundException e) {
             throw new JPSRuntimeException(e.getMessage(), e);
         }
         return conn;
+    }
+
+    public ResultSet executeQuery(String query) {
+        ResultSet rs;
+        try {
+            if(this.conn == null || stmt==null){
+                this.conn = getConnection();
+            }
+            rs = this.stmt.executeQuery(query);
+        } catch (SQLException e) {
+            throw new JPSRuntimeException(e.getMessage(), e);
+        }
+        return rs;
+    }
+
+    public int executeUpdate(String update) {
+        try {
+            return this.stmt.executeUpdate(update);
+        } catch (SQLException e) {
+            throw new JPSRuntimeException(e.getMessage(), e);
+        }
     }
 
     /**
