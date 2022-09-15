2b5351aebbbd201a1f55adfb479152039e6c16f9
Mehal Agarwal <99166247+mehalaga@users.noreply.github.com>
2022-09-15 15:37:55 +0800

    1386-investigate-remotestore-client-for-rdb-access: added comments

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/query/RemoteStoreClient.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/query/RemoteStoreClient.java
@@ -533,6 +533,9 @@ public class RemoteStoreClient implements TripleStoreClientInterface {
         return builder.build();
     }
 
+    /**
+     * Establishes connection to triple store and sets the Statement Object
+     */
     protected void connect(){
         try {
             if (this.conn == null || this.conn.isClosed()) {
