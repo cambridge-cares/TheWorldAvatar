3b0bdc16039587e71c7fc718527220ab1e123ae9
Kok Foong Lee <kflee@cmclinnovations.com>
2022-09-16 16:48:44 +0100

    dev-virtual-sensor-update: remove stack manager start up from script

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # starts up all required components of virtual sensor
-(cd ../Deploy/stacks/dynamic/stack-manager/ && ./stack.sh start ship-stack)
 (cd ../Agents/WeatherAgent/ && ./stack.sh start ship-stack)
 (cd ShipInputAgent/ && ./stack.sh start ship-stack)
 (cd EmissionsAgent/ && ./stack.sh start ship-stack)
