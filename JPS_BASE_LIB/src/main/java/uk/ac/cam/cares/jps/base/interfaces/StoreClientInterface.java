407226eafe37df825f7026b71056b2fb2cbbbb0d
Mehal Agarwal <99166247+mehalaga@users.noreply.github.com>
2022-09-20 16:09:47 +0800

    1416-dev-split-storeclient-interface: StoreClientInterface generalized to triple stores and relational databases

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/interfaces/StoreClientInterface.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/interfaces/StoreClientInterface.java
@@ -1,38 +1,18 @@
 package uk.ac.cam.cares.jps.base.interfaces;
 
-import org.apache.jena.query.Query;
-import org.apache.jena.rdf.model.Model;
 import org.apache.jena.update.UpdateRequest;
 import org.json.JSONArray;
 
-import uk.ac.cam.cares.jps.base.tools.CloningTool;
-
-
 /**
  * This interface is to be implemented by store clients 
- * that establish a connection and perform SPARQL queries and updates
- * on remote triple stores, owl files etc. 
+ * that establish a connection and perform queries and updates
+ * on remote triple stores, relational databases and owl files etc.
  *  
  * @author Casper Lindberg
  */
 public interface StoreClientInterface {
 
-	/**
-	 * Get rdf content from store.
-	 * @param graphName (if any)
-	 * @param accept
-	 * @return String
-	 */
-	String get(String graphName, String accept);
-	
-	/**
-	 * Insert rdf content into store. 
-	 * @param graphName (if any)
-	 * @param content
-	 * @param contentType
-	 */
-	void insert(String graphName, String content, String contentType);
-	
+
 	// SPARQL Query methods
 	
 	/**
@@ -64,21 +44,7 @@ public interface StoreClientInterface {
 	 */
 	String execute(String query);
 	
-	// SPARQL Construct query methods
-	
-	/**
-	 * Execute sparql construct query.
-	 * @param sparql
-	 * @return
-	 */
-	Model executeConstruct(Query sparql);
-	
-	/**
-	 * Execute sparql construct query.
-	 * @param sparql
-	 * @return
-	 */
-	Model executeConstruct(String sparql);
+
 	
 	// SPARQL update methods
 	
@@ -114,27 +80,7 @@ public interface StoreClientInterface {
 	 */
 	String getQuery();
 	
-	/**
-	 * Can return the URL of the query EndPoint if available.  
-	 */
-	String getQueryEndpoint();
-	
-	/**
-	 * Sets the URL of the query EndPoint. 
-	 * @param queryEndpoint
-	 */
-	String setQueryEndpoint(String queryEndpoint);
-	
-	/**
-	 * Returns the URL of the update EndPoint if available.
-	 */
-	String getUpdateEndpoint();
-	
-	/**
-	 * Set the URL of the update EndPoint.
-	 * @param updateEndpoint
-	 */
-	String setUpdateEndpoint(String updateEndpoint);
+
 
 	// Authentication
 	String getUser();
@@ -145,41 +91,5 @@ public interface StoreClientInterface {
 
 	void setPassword(String password);
 
-	/**
-	 * Counts the total number of triples in the repository.
-	 * NOTE: this can be slow (of order of minutes) for large repositories.
-	 * @return
-	 */
-	default Integer getTotalNumberOfTriples() {
-		String query = "SELECT (COUNT(*) AS ?triples) WHERE { ?s ?p ?o . }";
-    	JSONArray results = executeQuery(query);
-    	int triples = Integer.parseInt(results.getJSONObject(0).get("triples").toString());
-    	return triples;
-	}
-	
-    /**
-     * Clone contents of <i>this</i> store to the target store.<br>
-     * <b>Note:</b> cloning a large store is a slow process 
-     * and the target store must be empty. <br>Before initiating a clone see 
-     * {@link uk.ac.cam.cares.jps.base.tools.CloningTool CloningTool} 
-     * for more details.
-     * @param targetStoreClient
-     */
-    default void cloneTo(StoreClientInterface targetStoreClient) {
-    	CloningTool cloningTool = new CloningTool();
-    	cloningTool.clone(this, targetStoreClient);
-    }
-    
-    /**
-     * Clone contents of the source store to <i>this</i> store.<br>
-     * <b>Note:</b> cloning a large store is a slow process 
-     * and <i>this</i> store must be empty. <br>Before initiating a clone see 
-     * {@link uk.ac.cam.cares.jps.base.tools.CloningTool CloningTool} 
-     * for more details.
-     * @param sourceStoreClient
-     */
-    default void cloneFrom(StoreClientInterface sourceStoreClient) {
-    	CloningTool cloningTool = new CloningTool();
-    	cloningTool.clone(sourceStoreClient, this);
-    }
+
 }
