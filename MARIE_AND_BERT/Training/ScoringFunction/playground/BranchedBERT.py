2b07ed46c5066208499133919407b56fbe6576b7
xiaochizhou <xz378@cam.ac.uk>
2022-09-20 13:43:12 +0800

    dev: removed inferior models

--- MARIE_AND_BERT/Training/ScoringFunction/playground/BranchedBERT.py
+++
@@ -1,9 +0,0 @@
-# This script serves as an experiment for testing branched BERT model
-
-# from Training.Util import
-from keras import Model, Input
-
-input_A = Input(shape=(None, 32), name='input_a')
-b
-
-model_nlp_branch = Model(inputs=[])
