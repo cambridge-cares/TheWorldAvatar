85604cfb0d8928b389064fa66c94ff20ddc027b5
Jiaru Bai <jb2197@cam.ac.uk>
2022-10-05 12:28:16 +0100

    169-dev-py4jps-and-pyderivationagent-new-release: updated py4jps version number to 1.0.24 in setup.py of pyderivationagent.

--- JPS_BASE_LIB/python_derivation_agent/setup.py
+++ JPS_BASE_LIB/python_derivation_agent/setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=['tests','tests.*']),
-    install_requires=['py4jps>=1.0.23', 'flask==2.1.0', 'gunicorn==20.0.4', 'Flask-APScheduler', 'rdflib', 'python-dotenv', 'yagmail'
+    install_requires=['py4jps>=1.0.24', 'flask==2.1.0', 'gunicorn==20.0.4', 'Flask-APScheduler', 'rdflib', 'python-dotenv', 'yagmail'
     # 'agentlogging @ git+https://github.com/cambridge-cares/TheWorldAvatar@develop#subdirectory=Agents/utils/python-utils'
     ],
     extras_require={
