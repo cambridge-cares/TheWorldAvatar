13f484414b6530f84c594b8e286b53157dfd2517
Jiaru Bai <jb2197@cam.ac.uk>
2022-10-11 22:39:22 +0100

    173-hotfix-pyderivationagent: released pyderivationagent 1.2.2 to both PyPI and TestPyPI.

--- JPS_BASE_LIB/python_derivation_agent/setup.py
+++ JPS_BASE_LIB/python_derivation_agent/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyderivationagent',
-    version='1.2.1',
+    version='1.2.2',
     author='Jiaru Bai',
     author_email='jb2197@cam.ac.uk',
     license='MIT',
