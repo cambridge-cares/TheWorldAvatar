e02b724e19fb1bd242db82037b6ffd417ba6e1f8
cmclscourtney <scourtney@cmclinnovations.com>
2022-10-13 17:16:05 +0100

    dev-mods-agent-evaluating-surrogates: template and class for Evaluate

---
+++ Agents/MoDSSimpleAgent/mods-simple-agent/src/main/java/com/cmclinnovations/mods/modssimpleagent/simulations/Evaluate.java
@@ -0,0 +1,30 @@
+package com.cmclinnovations.mods.modssimpleagent.simulations;
+
+import java.io.IOException;
+
+import com.cmclinnovations.mods.modssimpleagent.BackendInputFile;
+import com.cmclinnovations.mods.modssimpleagent.FileGenerator.FileGenerationException;
+import com.cmclinnovations.mods.modssimpleagent.MoDSBackend;
+import com.cmclinnovations.mods.modssimpleagent.datamodels.Algorithm;
+import com.cmclinnovations.mods.modssimpleagent.datamodels.InputMetaData;
+import com.cmclinnovations.mods.modssimpleagent.datamodels.Request;
+
+class Evaluate extends Simulation {
+
+        public Evaluate(Request request, BackendInputFile inputFile, MoDSBackend modsBackend, InputMetaData inputMetaData)
+                        throws IOException {
+                super(request, inputFile, modsBackend, inputMetaData);
+        }
+
+        @Override
+        protected Algorithm getPrimaryAlgorithm() {
+                return getAlgorithmOfType("SamplingAlg");
+        }
+
+        @Override
+        protected void generateFiles() throws FileGenerationException {
+                generateInitialFileFromMetaData();
+                super.generateFiles();
+        }
+
+}
