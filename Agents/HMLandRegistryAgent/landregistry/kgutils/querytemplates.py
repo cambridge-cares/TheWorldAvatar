b4bc8d92e39d3ad9b2eb255235f882deab610ebc
Markus Hofmeister <mh807@cam.ac.uk>
2022-10-16 12:08:35 +0800

    dev-PropertySalesInstantiationAgent: Updated address data model and query template

--- Agents/HMLandRegistryAgent/landregistry/kgutils/querytemplates.py
+++ Agents/HMLandRegistryAgent/landregistry/kgutils/querytemplates.py
@@ -76,13 +76,15 @@ def get_instantiated_properties_with_location_info(property_iris: list = None) -
                ?postcode ?street ?house_number
         WHERE {{
         {values_statement}
-        ?property_iri <{RDF_TYPE}>/<{RDFS_SUBCLASS}>* <{OBE_PROPERTY}> ;
-                      <{OBE_LOCATEDIN}> ?district_iri ;
-                      <{OBE_HAS_ADDRESS}> ?address_iri .
-        ?address_iri <{OBE_HAS_PROPERTYNUMBER}> ?house_number ;
-                     <{ICONTACT_HAS_STREET}> ?street ;
-                     <{OBE_HAS_POSTALCODE}> ?postcode_iri . 
-        ?postcode_iri <{RDFS_LABEL}> ?postcode .
+        ?property_iri <{RDF_TYPE}>/<{RDFS_SUBCLASS}>* <{OBE_PROPERTY}> ; 
+                      <{OBE_LOCATEDIN}> ?district_iri ; 
+                      <{OBE_HAS_ADDRESS}> ?address_iri . 
+        ?address_iri <{OBE_HAS_POSTALCODE}> ?postcode_iri . 
+        ?postcode_iri <{RDFS_LABEL}> ?postcode . 
+        OPTIONAL {{ ?address_iri <{ICONTACT_HAS_STREET}> ?street }}
+        OPTIONAL {{ ?address_iri <{ICONTACT_HAS_STREET_NUMBER}> ?number }}
+        OPTIONAL {{ ?address_iri <{ICONTACT_HAS_BUILDING}> ?bldg_name }}
+        OPTIONAL {{ ?address_iri <{OBE_HAS_UNIT_NAME}> ?unit_name }}        
         }}
     """
 
