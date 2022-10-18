9281312172fbb496a1026912cbea2df83182f988
Kok Foong Lee <kflee@cmclinnovations.com>
2022-10-18 16:49:00 +0100

    dev-virtual-sensor-update: initial commit of aermod agent

---
+++ JPS_VIRTUALSENSOR/Aermod/src/main/java/com/cmclinnovations/aermod/AermodAgent.java
@@ -0,0 +1,13 @@
+package com.cmclinnovations.aermod;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import uk.ac.cam.cares.jps.base.agent.DerivationAgent;
+
+@WebServlet(urlPatterns = {"/"})
+public class AermodAgent extends DerivationAgent {
+    private static final Logger LOGGER = LogManager.getLogger(AermodAgent.class);
+}
