4c7e8c455a31581ae11df42252cefe87b9b82706
szhang012 <81546703+szhang012@users.noreply.github.com>
2022-10-18 18:16:59 +0800

    Update: Modification for latest version of BMS ontolog

--- Agents/BMSBacnetAgent/deviceMap/bms_bacnet_dictionary.py
+++ Agents/BMSBacnetAgent/deviceMap/bms_bacnet_dictionary.py
@@ -1,11 +1,16 @@
+import csv
 from config import config
+#bms_IRI, bacnet_prop_name
 class bmsBacnetDict():
     def __init__(self):
-        file_address = config.DEVICE_NAME_MAPPING
-        self.nameDict = self.build()
+        self.file_address = config.DEVICE_NAME_MAPPING
 
     def build(self):
         namedict = {}
-        #TODO
+        with open(self.file_address) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            idx = 0
+            for row in reader:
+                namedict[idx] = row[2].strip()
+                idx = idx+1
         return namedict
-
