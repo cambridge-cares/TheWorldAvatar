01b0d2c441f5667e0eb82872e0b8583bffba0b5f
Markus Hofmeister <mh807@cam.ac.uk>
2022-10-18 16:33:58 +0000

    dev-PropertySalesInstantiationAgent: Added recurring task

--- Agents/HMLandRegistryAgent/landregistry/flaskapp/wsgi.py
+++ Agents/HMLandRegistryAgent/landregistry/flaskapp/wsgi.py
@@ -1,23 +1,22 @@
 ################################################
 # Authors: Markus Hofmeister (mh807@cam.ac.uk) #    
-# Date: 12 Oct 2022                            #
+# Date: 18 Oct 2022                            #
 ################################################
 
 # Start Web Server Gateway Interface (WSGI) as simple web server to forward
 # requests to actual application and initialise recurring tasks
 
 from landregistry.flaskapp import create_app
-#from landregistry.datainstantiation.epc_instantiation import instantiate_epc_data_for_all_postcodes
-#from apscheduler.schedulers.background import BackgroundScheduler
+from landregistry.datainstantiation.sales_instantiation import update_all_transaction_records
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
-# Add recurring background task to assimilate latest EPC data every 4 weeks
-# "The department will publish register data every four to six months for new EPCs
-# and DECs or where the status of the EPC or DEC has changed" 
-# (https://epc.opendatacommunities.org/docs/guidance#faq-updates)
-#sched = BackgroundScheduler(daemon=True)
-#sched.add_job(instantiate_epc_data_for_all_postcodes, trigger='interval', weeks=2)
-#sched.start()
+# Add recurring background task to assimilate latest HM Land Registry Price Paid Data
+# every 4 weeks ("HM Land Registry publish Price Paid Data for England and Wales on a monthly basis." 
+# (https://landregistry.data.gov.uk/app/root/doc/ppd)
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(update_all_transaction_records, trigger='interval', weeks=4)
+sched.start()
 
 app = create_app()
 
