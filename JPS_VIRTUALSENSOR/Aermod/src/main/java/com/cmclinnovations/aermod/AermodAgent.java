2e74ffca9bc15117a6ec70063c2a961b2a346f11
Kok Foong Lee <kflee@cmclinnovations.com>
2022-10-20 14:30:56 +0100

    dev-virtual-sensor-update: WIP to enable derivations

--- JPS_VIRTUALSENSOR/Aermod/src/main/java/com/cmclinnovations/aermod/AermodAgent.java
+++ JPS_VIRTUALSENSOR/Aermod/src/main/java/com/cmclinnovations/aermod/AermodAgent.java
@@ -1,13 +1,37 @@
 package com.cmclinnovations.aermod;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import uk.ac.cam.cares.jps.base.agent.DerivationAgent;
+import uk.ac.cam.cares.jps.base.derivation.DerivationClient;
+import uk.ac.cam.cares.jps.base.derivation.DerivationInputs;
+import uk.ac.cam.cares.jps.base.derivation.DerivationOutputs;
+import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
+import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
 
 @WebServlet(urlPatterns = {"/"})
 public class AermodAgent extends DerivationAgent {
     private static final Logger LOGGER = LogManager.getLogger(AermodAgent.class);
+
+    @Override
+	public void processRequestParameters(DerivationInputs derivationInputs, DerivationOutputs derivationOutputs) {   
+        String weatherStationIri = derivationInputs.getIris(RDFTypes.REPORTING_STATION).get(0);
+        String nxIri = derivationInputs.getIris(RDFTypes.NX).get(0);
+        String nyIri = derivationInputs.getIris(RDFTypes.NY).get(0);
+        String scopeIri = derivationInputs.getIris(RDFTypes.SCOPE).get(0);
+    }
+    
+    @Override
+    public void init() throws ServletException {
+        EndpointConfig endpointConfig = new EndpointConfig(); 
+        RemoteStoreClient storeClient = new RemoteStoreClient(endpointConfig.getKgurl(), endpointConfig.getKgurl());
+        RemoteRDBStoreClient rdbStoreClient = new RemoteRDBStoreClient(endpointConfig.getDburl(), endpointConfig.getDbuser(), endpointConfig.getDbpassword());
+        TimeSeriesClient<Long> tsClient = new TimeSeriesClient<>(storeClient, Long.class);
+        super.devClient = new DerivationClient(storeClient, QueryClient.PREFIX);
+    }
 }
