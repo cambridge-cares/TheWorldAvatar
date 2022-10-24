04fff00c53a3a23548ce64ba87aafbd039642ed2
Kok Foong Lee <kflee@cmclinnovations.com>
2022-10-24 17:56:54 +0100

    dev-virtual-sensor-update: query ships using time series client instead of ontop

--- JPS_VIRTUALSENSOR/AermodAgent/src/main/java/com/cmclinnovations/aermod/AermodAgent.java
+++ JPS_VIRTUALSENSOR/AermodAgent/src/main/java/com/cmclinnovations/aermod/AermodAgent.java
@@ -1,10 +1,17 @@
 package com.cmclinnovations.aermod;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.locationtech.jts.geom.Geometry;
 
 import uk.ac.cam.cares.jps.base.agent.DerivationAgent;
 import uk.ac.cam.cares.jps.base.derivation.DerivationClient;
@@ -12,14 +19,11 @@ import uk.ac.cam.cares.jps.base.derivation.DerivationInputs;
 import uk.ac.cam.cares.jps.base.derivation.DerivationOutputs;
 import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
-import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
 
 @WebServlet(urlPatterns = {"/"})
 public class AermodAgent extends DerivationAgent {
     private static final Logger LOGGER = LogManager.getLogger(AermodAgent.class);
-    private RemoteRDBStoreClient rdbStoreClient;
     private QueryClient queryClient;
-    private TimeSeriesClient<Long> tsClient;
 
     @Override
 	public void processRequestParameters(DerivationInputs derivationInputs, DerivationOutputs derivationOutputs) {   
@@ -28,25 +32,57 @@ public class AermodAgent extends DerivationAgent {
         String nyIri = derivationInputs.getIris(Constants.NY).get(0);
         String scopeIri = derivationInputs.getIris(Constants.SCOPE).get(0);
         String simulationTimeIri = derivationInputs.getIris(Constants.SIMULATION_TIME).get(0);
+        
+        long simulationTime = queryClient.getMeasureValueAsLong(simulationTimeIri);
+
+        if (simulationTime == 0) {
+            LOGGER.info("Simulation time = 0, this is from calling createSyncDerivationForNewInfo the first time");
+            return;
+        }
 
         int nx = queryClient.getMeasureValueAsInt(nxIri);
         int ny = queryClient.getMeasureValueAsInt(nyIri);
-        long simulationTime = queryClient.getMeasureValueAsLong(simulationTimeIri);
 
-        // get ship within a scope
+        // get ships within a scope and time
+        Geometry scope = queryClient.getScopeFromOntop(scopeIri);
+        List<String> ships = queryClient.getShipsWithinTimeAndScopeViaTsClient(simulationTime, scope);
+
+        // update derivation of ships (on demand)
+        List<String> derivationsToUpdate = queryClient.getDerivationsOfShips(ships);
+        updateDerivations(derivationsToUpdate);
     }
     
+    void updateDerivations(List<String> derivationsToUpdate) {
+        CompletableFuture<String> getAsync = null;
+        for (String derivation : derivationsToUpdate) {
+            getAsync = CompletableFuture.supplyAsync(() -> {
+                try {
+                    devClient.updatePureSyncDerivation(derivation);
+                    return null; // need to return something, could not get it to work with CompletableFuture<Void>
+                } catch (Exception e) {
+                    LOGGER.error(e.getMessage());
+                    LOGGER.error("Error occured while updating ship derivations");
+                    return null;
+                }
+            });   
+        }
+        if (getAsync != null) {
+            getAsync.join();
+        }
+    }
+
     void create144File() {
 
     }
-
     @Override
     public void init() throws ServletException {
         EndpointConfig endpointConfig = new EndpointConfig(); 
+
         RemoteStoreClient storeClient = new RemoteStoreClient(endpointConfig.getKgurl(), endpointConfig.getKgurl());
-        rdbStoreClient = new RemoteRDBStoreClient(endpointConfig.getDburl(), endpointConfig.getDbuser(), endpointConfig.getDbpassword());
-        tsClient = new TimeSeriesClient<>(storeClient, Long.class);
-        queryClient = new QueryClient(storeClient);
+        RemoteStoreClient ontopStoreClient = new RemoteStoreClient(endpointConfig.getOntopurl());
+        RemoteRDBStoreClient rdbStoreClient = new RemoteRDBStoreClient(endpointConfig.getDburl(), endpointConfig.getDbuser(), endpointConfig.getDbpassword());
+
+        queryClient = new QueryClient(storeClient, ontopStoreClient, rdbStoreClient);
         super.devClient = new DerivationClient(storeClient, Constants.PREFIX_DISP);
     }
 }
--- JPS_VIRTUALSENSOR/AermodAgent/src/main/java/com/cmclinnovations/aermod/QueryClient.java
+++ JPS_VIRTUALSENSOR/AermodAgent/src/main/java/com/cmclinnovations/aermod/QueryClient.java
@@ -1,5 +1,7 @@
 package com.cmclinnovations.aermod;
 
+import org.eclipse.rdf4j.model.vocabulary.GEOF;
+import org.eclipse.rdf4j.sparqlbuilder.constraint.Expressions;
 import org.eclipse.rdf4j.sparqlbuilder.core.Prefix;
 import org.eclipse.rdf4j.sparqlbuilder.core.PropertyPaths;
 import org.eclipse.rdf4j.sparqlbuilder.core.SparqlBuilder;
@@ -7,24 +9,67 @@ import org.eclipse.rdf4j.sparqlbuilder.core.Variable;
 import org.eclipse.rdf4j.sparqlbuilder.core.query.Queries;
 import org.eclipse.rdf4j.sparqlbuilder.core.query.SelectQuery;
 import org.eclipse.rdf4j.sparqlbuilder.graphpattern.GraphPattern;
+import org.eclipse.rdf4j.sparqlbuilder.graphpattern.GraphPatterns;
 import org.eclipse.rdf4j.sparqlbuilder.rdf.Iri;
+import org.eclipse.rdf4j.sparqlbuilder.rdf.Rdf;
 import org.json.JSONArray;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.io.ParseException;
+import org.postgis.Point;
+import org.apache.jena.geosparql.implementation.parsers.wkt.WKTReader;
 
+import com.cmclinnovations.aermod.sparqlbuilder.GeoSPARQL;
+import com.cmclinnovations.aermod.sparqlbuilder.ValuesPattern;
+
+import it.unibz.inf.ontop.model.vocabulary.GEO;
+import uk.ac.cam.cares.jps.base.derivation.DerivationSparql;
+import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeries;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
+
 import static org.eclipse.rdf4j.sparqlbuilder.rdf.Rdf.iri;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class QueryClient {
+    private static final Logger LOGGER = LogManager.getLogger(QueryClient.class);
+
+    private RemoteStoreClient storeClient;
+    private RemoteStoreClient ontopStoreClient;
+    private RemoteRDBStoreClient rdbStoreClient;
+    private TimeSeriesClient<Long> tsClient;
+
     // prefixes
     private static final Prefix P_OM = SparqlBuilder.prefix("om",iri("http://www.ontology-of-units-of-measure.org/resource/om-2/"));
+    private static final Prefix P_DISP = SparqlBuilder.prefix("disp", iri(Constants.PREFIX_DISP));
+    private static final Prefix P_GEO = SparqlBuilder.prefix("geo", iri(GEO.PREFIX));
+    private static final Prefix P_GEOF = SparqlBuilder.prefix("geof", iri(GEOF.NAMESPACE));
     
+    // classes
+    private static final Iri SHIP = P_DISP.iri("Ship");
+
     // properties
     private static final Iri HAS_VALUE = P_OM.iri("hasValue");
     private static final Iri HAS_NUMERICALVALUE = P_OM.iri("hasNumericalValue");
+    private static final Iri HAS_LOCATION = P_DISP.iri("hasLocation");
+    private static final Iri HAS_GEOMETRY = P_GEO.iri("hasGeometry");
+    private static final Iri AS_WKT = P_GEO.iri("asWKT");
 
-    private RemoteStoreClient storeClient;
-
-    public QueryClient(RemoteStoreClient storeClient) {
+    public QueryClient(RemoteStoreClient storeClient, RemoteStoreClient ontopStoreClient, RemoteRDBStoreClient rdbStoreClient) {
         this.storeClient = storeClient;
+        this.ontopStoreClient = ontopStoreClient;
+        this.tsClient = new TimeSeriesClient<>(storeClient, Long.class);
+        this.rdbStoreClient = rdbStoreClient;
     }
 
     int getMeasureValueAsInt(String instance) {
@@ -43,5 +88,217 @@ public class QueryClient {
         query.where(gp);
         JSONArray queryResult = storeClient.executeQuery(query.getQueryString());
         return Long.parseLong(queryResult.getJSONObject(0).getString(value.getQueryString().substring(1)));
+    }
+
+    List<String> getShipsWithinTimeAndScopeViaTsClient(long simulationTime, Geometry scope) {
+        long simTimeUpperBound = simulationTime + 1800; // +30 minutes
+        long simTimeLowerBound = simulationTime - 1800; // -30 minutes
+
+        Map<String,String> measureToShipMap = getMeasureToShipMap();
+        List<String> measures = new ArrayList<>(measureToShipMap.keySet());
+
+        List<String> ships = new ArrayList<>();
+        try (Connection conn = rdbStoreClient.getConnection()) {
+            measures.stream().forEach(measure -> {
+                TimeSeries<Long> ts = tsClient.getTimeSeriesWithinBounds(List.of(measure), simTimeLowerBound, simTimeUpperBound, conn);
+                if (ts.getValuesAsPoint(measure).size() > 1) {
+                    LOGGER.warn("More than 1 point within this time inverval");
+                } else if (ts.getValuesAsPoint(measure).isEmpty()) {
+                    return;
+                }
+
+                try {
+                    Point postgisPoint = ts.getValuesAsPoint(measure).get(0);
+                    String wktLiteral = postgisPoint.getTypeString() + postgisPoint.getValue();
+
+                    Geometry point = new org.locationtech.jts.io.WKTReader().read(wktLiteral);
+                    
+                    if (scope.contains(point)) {
+                        ships.add(measureToShipMap.get(measure));
+                    }
+                } catch (ParseException e) {
+                    LOGGER.error("Failed to parse WKT literal of point");
+                    LOGGER.error(e.getMessage());
+                }
+                
+            });
+        } catch (SQLException e) {
+            LOGGER.error("Probably failed at closing connection");
+            LOGGER.error(e.getMessage());
+        }
+
+        return ships;
+    }
+
+    /**
+     * the result is the geo:wktLiteral type with IRI of SRID in front
+     * @param scopeIri
+     * @return
+     */
+    Geometry getScopeFromOntop(String scopeIri) {
+        SelectQuery query = Queries.SELECT();
+        Variable scope = query.var();
+        
+        query.prefix(P_GEO).where(iri(scopeIri).has(PropertyPaths.path(HAS_GEOMETRY, AS_WKT), scope));
+
+        JSONArray queryResult = ontopStoreClient.executeQuery(query.getQueryString());
+        String wktLiteral = queryResult.getJSONObject(0).getString(scope.getQueryString().substring(1));
+        return WKTReader.extract(wktLiteral).getGeometry();
+    }
+
+    Map<String,String> getMeasureToShipMap() {
+        SelectQuery query = Queries.SELECT();
+
+        Variable ship = query.var();
+        Variable locationMeasure = query.var();
+        GraphPattern gp = ship.isA(SHIP).andHas(PropertyPaths.path(HAS_LOCATION,HAS_VALUE), locationMeasure);
+
+        query.where(gp).prefix(P_OM,P_DISP);
+
+        JSONArray queryResult = storeClient.executeQuery(query.getQueryString());
+
+        Map<String,String> locationMeasureToShipMap = new HashMap<>();
+        for (int i = 0; i < queryResult.length(); i++) {
+            String locationMeasureIri = queryResult.getJSONObject(i).getString(locationMeasure.getQueryString().substring(1));
+            String shipIri = queryResult.getJSONObject(i).getString(ship.getQueryString().substring(1));
+            locationMeasureToShipMap.put(locationMeasureIri, shipIri);
+        }
+
+        return locationMeasureToShipMap;
+    }
+
+    @Deprecated
+    List<String> getShipsWithinTimeAndScopeViaKG(long simulationTime, String scopeIri) {
+        long simTimeUpperBound = simulationTime + 1800; // +30 minutes
+        long simTimeLowerBound = simulationTime - 1800; // -30 minutes
+
+        Map<String,String> measureToShipMap = getMeasureToShipMap();
+        List<String> locationMeasures = new ArrayList<>(measureToShipMap.keySet());
+
+        Map<String, String> geometryToMeasureMap = getGeometryToMeasureMap(locationMeasures);
+        List<String> geometryIrisWithinTimeBounds = getGeometriesWithinTimeBounds(new ArrayList<>(geometryToMeasureMap.keySet()), simTimeLowerBound, simTimeUpperBound);
+        String scopeGeometry = getScopeGeometry(scopeIri);
+        List<String> geometriesWithinScopeAndTime = getGeometriesWithinScope(scopeGeometry, geometryIrisWithinTimeBounds);
+
+        List<String> shipsWithinTimeAndScope = new ArrayList<>();
+        geometriesWithinScopeAndTime.forEach(g -> 
+            shipsWithinTimeAndScope.add(measureToShipMap.get(geometryToMeasureMap.get(g)))
+        );
+
+        return shipsWithinTimeAndScope;
+    }
+
+    /**
+     * to ontop
+     * @param scopeIri
+     */
+    @Deprecated
+    String getScopeGeometry(String scopeIri) {
+        SelectQuery query = Queries.SELECT();
+        Variable scopeGeometry = query.var();
+        query.prefix(P_GEO).where(iri(scopeIri).has(HAS_GEOMETRY, scopeGeometry));
+
+        JSONArray queryResult = ontopStoreClient.executeQuery(query.getQueryString());
+        return queryResult.getJSONObject(0).getString(scopeGeometry.getQueryString().substring(1));
+    }
+
+    /**
+     * from ontop
+     * @param locationMeasures
+     * @return
+     */
+    @Deprecated
+    Map<String, String> getGeometryToMeasureMap(List<String> locationMeasures) {
+        SelectQuery query = Queries.SELECT();
+        Variable locationMeasure = query.var();
+        Variable geometry = query.var();
+
+        ValuesPattern<Iri> vp = new ValuesPattern<>(locationMeasure, locationMeasures.stream().map(Rdf::iri).collect(Collectors.toList()), Iri.class);
+        GraphPattern gp = locationMeasure.has(HAS_GEOMETRY,geometry);
+
+        query.prefix(P_GEO).where(vp,gp);
+
+        JSONArray queryResult = ontopStoreClient.executeQuery(query.getQueryString());
+
+        Map<String, String> geometryToMeasureMap = new HashMap<>();
+        for (int i = 0; i < queryResult.length(); i++) {
+            String geometryIri = queryResult.getJSONObject(i).getString(geometry.getQueryString().substring(1));
+            String measureIri = queryResult.getJSONObject(i).getString(locationMeasure.getQueryString().substring(1));
+            geometryToMeasureMap.put(geometryIri, measureIri);
+        }
+
+        return geometryToMeasureMap;
+    }
+
+    /**
+     * from ontop
+     * @param geometryIris
+     * @param simTimeLowerBound
+     * @param simTimeUpperBound
+     */
+    @Deprecated
+    List<String> getGeometriesWithinTimeBounds(List<String> geometryIris, long simTimeLowerBound, long simTimeUpperBound) {
+        SelectQuery query = Queries.SELECT();
+
+        Variable shipTime = query.var();
+        Variable geometry = query.var();
+
+        ValuesPattern<Iri> vp = new ValuesPattern<>(geometry, geometryIris.stream().map(Rdf::iri).collect(Collectors.toList()), Iri.class);
+        GraphPattern gp = GraphPatterns.and(vp,geometry.has(P_DISP.iri("hasTime"), shipTime));
+
+        query.prefix(P_GEO,P_GEOF,P_OM,P_DISP).
+        where(gp.filter
+        (Expressions.and(Expressions.gt(shipTime, simTimeLowerBound), Expressions.lt(shipTime, simTimeUpperBound))));
+
+        JSONArray queryResult = ontopStoreClient.executeQuery(query.getQueryString());
+        List<String> geometriesWithinTimeBounds = new ArrayList<>();
+
+        for (int i = 0; i < queryResult.length(); i++) {
+            geometriesWithinTimeBounds.add(queryResult.getJSONObject(i).getString(geometry.getQueryString().substring(1)));
+        }
+
+        return geometriesWithinTimeBounds;
+    }
+
+    List<String> getDerivationsOfShips(List<String> ships) {
+        SelectQuery query = Queries.SELECT();
+
+        Variable derivation = query.var();
+        Variable ship = query.var();
+        Iri isDerivedFrom = iri(DerivationSparql.derivednamespace + "isDerivedFrom");
+        ValuesPattern<Iri> vp = new ValuesPattern<>(ship, ships.stream().map(Rdf::iri).collect(Collectors.toList()), Iri.class);
+        GraphPattern gp = derivation.has(isDerivedFrom, ship);
+
+        query.where(gp,vp);
+
+        JSONArray queryResult = storeClient.executeQuery(query.getQueryString());
+        
+        List<String> derivations = new ArrayList<>();
+        for (int i = 0; i < queryResult.length(); i++) {
+            derivations.add(queryResult.getJSONObject(i).getString(derivation.getQueryString().substring(1)));
+        }
+
+        return derivations;
+    }
+
+    @Deprecated
+    List<String> getGeometriesWithinScope(String scopeGeometry, List<String> shipGeometriesWithinTimeBounds) {
+        SelectQuery query = Queries.SELECT();
+        Variable shipGeometry = query.var();
+        Variable scopeWkt = query.var();
+        Variable shipWkt = query.var();
+
+        ValuesPattern<Iri> vp = new ValuesPattern<>(shipGeometry, shipGeometriesWithinTimeBounds.stream().map(Rdf::iri).collect(Collectors.toList()), Iri.class);
+
+        GraphPattern gp = GraphPatterns.and(vp, shipGeometry.has(AS_WKT, shipWkt), iri(scopeGeometry).has(AS_WKT, scopeWkt));
+
+        query.where(gp.filter(Expressions.and(GeoSPARQL.sfIntersects(shipWkt, scopeWkt)))).prefix(P_GEO,P_GEOF);
+
+        JSONArray queryResult = ontopStoreClient.executeQuery(query.getQueryString());
+        List<String> geometriesWithinScope = new ArrayList<>();
+        for (int i = 0; i < queryResult.length(); i++) {
+            geometriesWithinScope.add(queryResult.getJSONObject(i).getString(shipGeometry.getQueryString().substring(1)));
+        }
+        return geometriesWithinScope;
     }
 }
