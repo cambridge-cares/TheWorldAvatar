99-UK-digital-twin-powsys: corrected the errors in looping calculation.

UK_Digital_Twin/SMRSitePreSelection/SitePreSelection_pymoo.py
@@ -1,6 +1,6 @@
 ##########################################
 # Author: Wanni Xie (wx243@cam.ac.uk)    #
-# Last Update Date: 13 Oct 2022          #
+# Last Update Date: 31 Oct 2022          #
 ##########################################
 
 """
@@ -30,7 +30,6 @@ class siteSelector(Problem):
             ProbabilityOfReactorFailure:float,
             SMRCapability:float,
             bankRate:float,
-            carbonTax:float,
             maxmumSMRUnitAtOneSite:int,
             SMRIntergratedDiscount:float, 
             startTime_of_EnergyConsumption:str,
@@ -49,15 +48,12 @@ class siteSelector(Problem):
         self.FP = ProbabilityOfReactorFailure
         self.geospatialQueryEndpointLabel = geospatialQueryEndpointLabel
         self.i = bankRate
-        self.carbonTax = carbonTax
         self.N = maxmumSMRUnitAtOneSite
         self.ir = SMRIntergratedDiscount
         self.population_list = population_list
         self.weightedDemandingDistance_list = weightedDemandingDistance_list
         self.varSets = locals()  
-
         self.startTime_of_EnergyConsumption = str(startTime_of_EnergyConsumption)
-        
         siteSelectionBinaryVariableNumber = len(self.generatorToBeReplacedList) * self.N
 
         super().__init__(
@@ -107,28 +103,12 @@ class siteSelector(Problem):
         ## Objective 1: the total cost of SMR retrofitting: SMR investment and risk cost ## 
         ## Objective 2: demanding weighter of each potential site ## 
         for i in range(len(self.generatorToBeReplacedList)):
-            # latlon = self.generatorToBeReplacedList[i]["LatLon"]
-            # sumUpOfWeightedDemanding = 0
-            # for demand in dclist.demandingAndCentroid[self.startTime_of_EnergyConsumption]:
-            #     LA_code  = demand["Area_LACode"]
-            #     if LA_code in ["K03000001", "K02000001", "W92000004","S92000003", "E12000001", "E12000002", "E12000003", "E12000004", "E12000005", 
-            #                     "E12000006", "E12000007", "E12000008", "E12000009", "E13000001", "E13000002"]:
-            #         continue
-            #     distance = DistanceBasedOnGPSLocation(latlon + demand['Geo_InfoList'])
-            #     weightedDemanding = distance * float(demand['v_TotalELecConsumption'])  
-            #     sumUpOfWeightedDemanding += weightedDemanding
-
             for n in range(self.N):
-                # print(i, genIRI)
-                # rs =  (self.r0/1000) * ((n + 1) * (self.Cap_SMR**(0.5))) ## where n+1 is the number of SMR units
-                # population = populationDensityCalculator(latlon, rs, self.geospatialQueryEndpointLabel)
                 numOfBV = int(i * self.N + n)
                 population = self.population_list[i][n]
                 totalLifeMonetaryCost += x[:, numOfBV] * population * self.FP * self.Hu * self.D / (1 - ((1 + self.D)**(-1 * self.L)))  
                 totalSMRCapitalCost += SMRIntegratedCostForDifferentInterationNumberList[n] * x[:, numOfBV] 
-
                 f2 += x[:, numOfBV] * self.weightedDemandingDistance_list[i]
-                ## f2 += x[:, numOfBV] * sumUpOfWeightedDemanding
         f1 = totalLifeMonetaryCost + totalSMRCapitalCost
         out["F"]  = np.column_stack([f1, f2])
 