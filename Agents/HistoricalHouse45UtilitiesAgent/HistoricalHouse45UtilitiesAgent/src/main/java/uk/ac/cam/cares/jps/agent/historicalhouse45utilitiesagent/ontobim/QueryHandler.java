d4b13a11c17b55156d487f8338f30af71913196b
qhouyee <quekhouyee@gmail.com>
2022-11-08 09:48:01 +0800

    dev-8-historical-house45-utilities-agent: Fixed typo in namespace.

--- Agents/HistoricalHouse45UtilitiesAgent/HistoricalHouse45UtilitiesAgent/src/main/java/uk/ac/cam/cares/jps/agent/historicalhouse45utilitiesagent/ontobim/QueryHandler.java
+++ Agents/HistoricalHouse45UtilitiesAgent/HistoricalHouse45UtilitiesAgent/src/main/java/uk/ac/cam/cares/jps/agent/historicalhouse45utilitiesagent/ontobim/QueryHandler.java
@@ -16,7 +16,7 @@ class QueryHandler {
     private static final String RDFS_URI = "http://www.w3.org/2000/01/rdf-schema#";
     private static final String OM_URI = "http://www.ontology-of-units-of-measure.org/resource/om-2/";
     private static final String SKOS_URI = "http://www.w3.org/2004/02/skos/core#";
-    private static final String QUDT_URI = "http://qudt.org/schema/qudt";
+    private static final String QUDT_URI = "http://qudt.org/schema/qudt/";
     private static final String TIMESERIES_URI = "https://github.com/cambridge-cares/TheWorldAvatar/blob/main/JPS_Ontology/ontology/ontotimeseries/OntoTimeSeries.owl#";
     private static final String UBEMMP_URI = "https://www.theworldavatar.com/kg/ontoubemmp/";
     private static final String ONTOBUILTENV_URI = "http://www.theworldavatar.com/ontology/ontobuiltenv/OntoBuiltEnv.owl#";
