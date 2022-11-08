b84129598ac4a5e84b1b4cfb084fd13436d80515
Jiaru Bai <jb2197@cam.ac.uk>
2022-11-08 12:03:18 +0000

    160-dev-rxn-opt-goal-agent: [RxnOptGoalAgent] updated following pyderivationagent 1.3.0.

--- Agents/RxnOptGoalAgent/setup.py
+++ Agents/RxnOptGoalAgent/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='rxnoptgoalagent',
@@ -6,13 +6,13 @@ setup(
     author='Jiaru Bai',
     author_email='jb2197@cam.ac.uk',
     license='MIT',
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     description="rxnoptgoalagent is capable of instantiating and managing iterations of pursuring the reaction optimisation goal as part of The World Avatar project.",
     url="https://github.com/cambridge-cares/TheWorldAvatar/tree/main/Agents/RxnOptGoalAgent",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=['tests','tests.*']),
-    install_requires=['pyderivationagent>=1.2.2', 'pandas==1.3.5', 'pydantic==1.9.0', 'chemistry_and_robots>=1.1.2', 'matplotlib', 'yagmail'
+    packages=find_namespace_packages(exclude=['tests','tests.*']),
+    install_requires=['pyderivationagent>=1.3.0', 'pandas==1.3.5', 'pydantic==1.9.0', 'chemistry_and_robots>=1.1.2', 'matplotlib', 'yagmail'
     # 'agentlogging @ git+https://github.com/cambridge-cares/TheWorldAvatar@main#subdirectory=Agents/utils/python-utils'
     ],
     extras_require={
@@ -23,5 +23,5 @@ setup(
             "pytest-rerunfailures>=10.2"
         ],
     },
-    include_package_data= True
+    include_package_data=True
 )
