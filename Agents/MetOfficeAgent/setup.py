a83b7ad657e4491d1977d500f53fd506c3e14700
Markus Hofmeister <mh807@cam.ac.uk>
2022-11-08 07:30:16 +0000

    dev-MetOfficeAgent-withinStack: Fixed version numbers in setup py and updated entrypoint.py

--- Agents/MetOfficeAgent/setup.py
+++ Agents/MetOfficeAgent/setup.py
@@ -14,21 +14,22 @@ setup(
     python_requires='>=3.7',
     include_package_data=True,
     install_requires= [
-        'apscheduler~=3.9',
-        'docopt~=0.6',
-        'flask~=2.1',
-        'pandas~=1.3',
-        'JayDeBeApi~=1.2',
-        'py4jps>=1.0.27', 
-        'requests~=2.27',
+        'APScheduler==3.9.1',
+        'docopt==0.6.2',
+        'Flask==2.2.2',
+        'pandas==1.3.5',
+        'JayDeBeApi==1.2.3',
+        'py4jps==1.0.27', 
+        'requests==2.28.1',
+        'gunicorn==20.1.0',
         'metoffer @ git+https://github.com/sludgedesk/metoffer#egg=MetOffer'
     ],
     extras_require={
         "dev": [
-            "pytest>=6.2.3",
-            "pytest-mock>=3.7.0",
-            "testcontainers>=3.4.2",
-            "psycopg2~=2.9.5"
+            "pytest==6.2.3",
+            "pytest-mock==3.7.0",
+            "testcontainers==3.4.2",
+            "psycopg2==2.9.5"
         ]
     }
 )
