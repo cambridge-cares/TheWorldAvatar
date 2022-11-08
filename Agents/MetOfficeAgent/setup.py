5ed408bedc300d83366ed672890f222adf41c46a
Markus Hofmeister <mh807@cam.ac.uk>
2022-11-08 11:15:32 +0800

    dev-MetOfficeAgent-withinStack: Update py4jps to 1.0.27

--- Agents/MetOfficeAgent/setup.py
+++ Agents/MetOfficeAgent/setup.py
@@ -19,7 +19,7 @@ setup(
         'flask~=2.1',
         'pandas~=1.3',
         'JayDeBeApi~=1.2',
-        'py4jps>=1.0.26', 
+        'py4jps>=1.0.27', 
         'requests~=2.27',
         'metoffer @ git+https://github.com/sludgedesk/metoffer#egg=MetOffer'
     ],
