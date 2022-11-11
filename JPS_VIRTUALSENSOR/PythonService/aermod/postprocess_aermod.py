6de9e6a1ef41d48a227cebda5d7b1eeebd84c7ec
Kok Foong Lee <kflee@cmclinnovations.com>
2022-11-11 16:56:39 +0000

    dev-virtual-sensor-update: convert coordinates to 4326 for geojson

--- JPS_VIRTUALSENSOR/PythonService/aermod/postprocess_aermod.py
+++ JPS_VIRTUALSENSOR/PythonService/aermod/postprocess_aermod.py
@@ -7,6 +7,7 @@ from matplotlib.figure import Figure
 import geojsoncontour
 from io import StringIO
 from flask import Blueprint, request, jsonify
+from pyproj import Transformer
 import requests
 import agentlogging
 import json
@@ -20,14 +21,15 @@ logger = agentlogging.get_logger("dev")
 def api():
     logger.info("Received request to process AERMOD dispersion matrix")
     aermod_output_url = request.args["dispersionMatrix"]
+    srid = request.args["srid"]
 
     dispersion_file = requests.get(aermod_output_url, auth=requests.auth.HTTPBasicAuth('fs_user','fs_pass'))
 
     # download file from url
-    return get_aermod_geojson(dispersion_file.text)
+    return get_aermod_geojson(dispersion_file.text, srid)
 
 # This script is only valid for a 1 hour simulation because this file shows the maximum concentration at each receptor
-def get_aermod_geojson(aermod_output):
+def get_aermod_geojson(aermod_output, srid):
     aermod_output_buffer = StringIO(aermod_output)
     data = pd.read_csv(aermod_output_buffer, delim_whitespace=True, skiprows=range(0,8), header=None, names=['X','Y','AVERAGE CONC', 'ZELEV', 'ZHILL','ZFLAG','AVE','GRP','RANK','NET ID','DATE(CONC)'])
     x_all = data['X']
@@ -39,12 +41,15 @@ def get_aermod_geojson(aermod_output):
     x_matrix = np.empty((len(x_set), len(y_set)))
     y_matrix = np.empty((len(x_set), len(y_set)))
 
+    transformer = Transformer.from_crs("epsg:" + srid, "epsg:4326")
+
     for i in range(len(x_set)):
         for j in range (len(y_set)):
             x_index = x_set.index(x_set[i])
             y_index = y_set.index(y_set[j])
-            x_matrix[x_index,y_index] = x_set[i]
-            y_matrix[x_index,y_index] = y_set[j]
+            lat,lon = transformer.transform(x_set[i], y_set[j])
+            x_matrix[x_index,y_index] = lon
+            y_matrix[x_index,y_index] = lat
 
     conc_list = data['AVERAGE CONC']
     conc_matrix = np.empty((len(x_set), len(y_set)))
