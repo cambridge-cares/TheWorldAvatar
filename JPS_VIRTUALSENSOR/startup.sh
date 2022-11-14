cf13526dfde3066c08be1f40f472043b01b82f5d
Kok Foong Lee <kflee@cmclinnovations.com>
2022-11-14 17:28:14 +0000

    dev-virtual-sensor-update: add vis in startup script

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -6,3 +6,5 @@
 (cd PythonService && ./stack.sh start ship-stack)
 (cd DispersionInteractor/ && ./stack.sh start ship-stack)
 (cd AermodAgent && ./stack.sh start ship-stack)
+(cd DispersionVis && ./stack.sh start ship-stack)
+(cd ../Deploy/stacks/db/fileserver/ && ./stack.sh start ship-stack)
