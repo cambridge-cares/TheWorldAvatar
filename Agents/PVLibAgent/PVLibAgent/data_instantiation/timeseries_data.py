cb42fbc064c6e0bce03129c1ff22d7a838a73ddb
Wilson Ang <wilson1010x@gmail.com>
2022-11-21 12:00:10 +0800

    43-dev-pvlib-agent: Modify agent to check whether timeseries IRIs have been initialized, create IRIs for DC and AC Power and initialize these IRIs and their corresponding timeseries relations in the knowledge graph

---
+++ Agents/PVLibAgent/PVLibAgent/data_instantiation/timeseries_data.py
@@ -0,0 +1,45 @@
+from PVLibAgent.kg_utils.kgClient import KGClient
+from PVLibAgent.kg_utils.utils import QUERY_ENDPOINT, UPDATE_ENDPOINT
+from PVLibAgent.kg_utils.tsClientForQuery import TSClientForQuery
+from PVLibAgent.error_handling.exceptions import TSException
+from PVLibAgent.kg_utils.utils import DB_UPDATE_URL, DB_UPDATE_USER, DB_UPDATE_PASSWORD
+
+import agentlogging
+
+
+# Initialise logger
+logger = agentlogging.get_logger("prod")
+
+class timeseries_data:
+    def add_timeseries_data(timeseries):
+        kg_client = KGClient(query_endpoint=QUERY_ENDPOINT, update_endpoint=UPDATE_ENDPOINT)
+        ts_client = TSClientForQuery(kg_client=kg_client, rdb_url=DB_UPDATE_URL, rdb_user=DB_UPDATE_USER, rdb_password=DB_UPDATE_PASSWORD)
+        with ts_client.connect() as conn:
+            try:
+                (ts_client.tsclient.addTimeSeriesData(timeseries, conn))
+            except Exception as ex:
+                logger.error("Adding of timeseries data to knowledge graph was not successful.")
+                raise TSException("Adding of timeseries data to knowledge graph was not successful.") from ex
+
+    def init_timeseries(dataIRIs: list, dataClass: list, timeUnit):
+        kg_client = KGClient(query_endpoint=QUERY_ENDPOINT, update_endpoint=UPDATE_ENDPOINT)
+        ts_client = TSClientForQuery(kg_client=kg_client, rdb_url=DB_UPDATE_URL, rdb_user=DB_UPDATE_USER, rdb_password=DB_UPDATE_PASSWORD)
+        with ts_client.connect() as conn:
+            try:
+                (ts_client.tsclient.initTimeSeries(dataIRIs, dataClass, timeUnit, conn))
+            except Exception as ex:
+                logger.error("Unable to initialise timeseries.")
+                raise TSException("Unable to initialise timeseries.") from ex
+
+    def check_data_has_timeseries(dataIRIs: list):
+        kg_client = KGClient(query_endpoint=QUERY_ENDPOINT, update_endpoint=UPDATE_ENDPOINT)
+        ts_client = TSClientForQuery(kg_client=kg_client, rdb_url=DB_UPDATE_URL, rdb_user=DB_UPDATE_USER, rdb_password=DB_UPDATE_PASSWORD)
+        with ts_client.connect() as conn:
+            try:
+                for iri in dataIRIs:
+                    response = (ts_client.tsclient.checkDataHasTimeSeries(iri, conn))
+            except Exception as ex:
+                logger.error("Unable to check whether " + iri + " has timeseries.")
+                raise TSException("Unable to check whether " + iri + " has timeseries.") from ex
+
+            return response
--- Agents/PVLibAgent/PVLibAgent/data_retrieval/query_timeseries.py
+++ Agents/PVLibAgent/PVLibAgent/data_retrieval/query_timeseries.py
@@ -1,7 +1,7 @@
 from PVLibAgent.kg_utils.kgClient import KGClient
 from PVLibAgent.kg_utils.utils import QUERY_ENDPOINT, UPDATE_ENDPOINT
-from PVLibAgent.kg_utils.tsClient import TSClient
-from PVLibAgent.kg_utils.utils import DB_URL, DB_USER, DB_PASSWORD
+from PVLibAgent.kg_utils.tsClientForQuery import TSClientForQuery
+from PVLibAgent.kg_utils.utils import DB_QUERY_URL, DB_QUERY_USER, DB_QUERY_PASSWORD
 
 import agentlogging
 
@@ -13,8 +13,8 @@ logger = agentlogging.get_logger("prod")
 def query_latest_timeseries(iri):
     # Initialise KG and TimeSeries Clients
     kg_client = KGClient(query_endpoint=QUERY_ENDPOINT, update_endpoint=UPDATE_ENDPOINT)
-    ts_client = TSClient(kg_client=kg_client, rdb_url=DB_URL, rdb_user=DB_USER, rdb_password=DB_PASSWORD)
+    ts_client = TSClientForQuery(kg_client=kg_client, rdb_url=DB_QUERY_URL, rdb_user=DB_QUERY_USER, rdb_password=DB_QUERY_PASSWORD)
     with ts_client.connect() as conn:
-        latest_value = (ts_client.tsclient.getLatestData(iri, conn))
+        value = (ts_client.tsclient.getLatestData(iri, conn))
 
-        return latest_value
+        return value
