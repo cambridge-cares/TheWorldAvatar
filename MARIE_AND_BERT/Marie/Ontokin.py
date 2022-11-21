5a5a39903c47c17da1874037026cfab3e7220248
xiaochizhou <xz378@cam.ac.uk>
2022-11-21 13:07:21 +0800

    dev: used QAEngine class as super for OntoKin QA Engine

--- MARIE_AND_BERT/Marie/Ontokin.py
+++ MARIE_AND_BERT/Marie/Ontokin.py
@@ -1,21 +1,16 @@
 from KGToolbox.NHopExtractor import HopExtractor
 from Marie.EntityLinking.ChemicalNEL import ChemicalNEL
+from Marie.QAEngine import QAEngine
 from Marie.Util.Models.TransEScoreModel import TransEScoreModel
 
 
-class OntoKinQAEngine:
+class OntoKinQAEngine(QAEngine):
 
-    def __init__(self):
-        self.model_name = "bert_ontokin"
-        self.dataset_dir = "CrossGraph/ontokin"
-        self.score_model = TransEScoreModel(device="cpu",
-                                            model_name=self.model_name,
-                                            dataset_dir=self.dataset_dir,
-                                            dim=80)
-
-        self.subgraph_extractor = HopExtractor(dataset_dir=self.dataset_dir, dataset_name='ontokin')
-        self.chemical_nel = ChemicalNEL(dataset_dir="ontokin")
+    def __init__(self, dataset_dir="CrossGraph/ontokin", dataset_name="ontokin"):
+        super().__init__(dataset_dir, dataset_name)
 
 
 if __name__ == "__main__":
-    my_engine = OntoKinQAEngine()
+    my_engine = OntoKinQAEngine(dataset_dir="CrossGraph/ontokin", dataset_name="ontokin")
+    rst = my_engine.run("what is the jone well depth of CO2")
+    print(rst)
