8752228055c77dce33e7e1bd1d93587698a69bfc
magmueller <mamagnus00@gmail.com>
2022-11-22 19:03:08 +0800

    dev-forecasting-agent: Update setup.py with configobj

--- Agents/ForecastingAgent/setup.py
+++ Agents/ForecastingAgent/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages 
 
 setup(
     name='forecasting',
@@ -7,15 +7,16 @@ setup(
     license='MIT',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=("tests")),
+    packages=find_namespace_packages( exclude=("tests")),
     url="",
     python_requires='>=3.7',
     include_package_data=True,
     install_requires= [
-        'flask>=2.2',
+        'flask>=2.2.2',
         'pandas>=1.3',
         'py4jps>=1.0.26', 
         'requests>=2.28',
         'darts~=0.21.0',
+        'configobj>=5.0.6'
     ]
 )
