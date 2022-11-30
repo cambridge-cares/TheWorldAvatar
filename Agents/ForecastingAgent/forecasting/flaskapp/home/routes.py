18eda5a25966d7ab0d8244dfc84eb69faf9eaaa7
magmueller <mamagnus00@gmail.com>
2022-11-30 18:05:28 +0800

    dev-forecasting-agent:  Home route not as json return

--- Agents/ForecastingAgent/forecasting/flaskapp/home/routes.py
+++ Agents/ForecastingAgent/forecasting/flaskapp/home/routes.py
@@ -17,13 +17,12 @@ def default():
     msg = 'Forecasting agent: \n'
     msg += 'The forecasting agent can be used to forecast a time series. \n'
     msg += 'The following parameters are required: \n'
-    msg += 'iri: the IRI of the time series to be forecasted \n'
-    msg += 'horizon: the number of steps to forecast \n'
+    msg += 'iri: (the IRI of the time series to be forecasted), \n'
+    msg += 'horizon: (the number of steps to forecast) \n'
     msg += 'available routes: \n'
     msg += '/api/forecastingAgent/forecast \n'
 
-    msg += 'Checkout the README.md for more information.'
+    msg += 'Checkout the https://github.com/cambridge-cares/TheWorldAvatar/tree/main/Agents/ForecastingAgent/ for more information.'
 
 
-    res = {'status': '200', 'msg': msg}
-    return jsonify(res)
+    return msg
