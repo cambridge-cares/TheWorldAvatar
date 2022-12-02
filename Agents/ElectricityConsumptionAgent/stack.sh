f9280d34ae0ab9d0813e3bc4489159b02aff6dc5
jx309 <jx309@cam.ac.uk>
2022-12-02 10:45:26 +0000

    A few changes to accommodate the bash file

--- Agents/ElectricityConsumptionAgent/stack.sh
+++ Agents/ElectricityConsumptionAgent/stack.sh
@@ -3,7 +3,7 @@
 COMMAND=$1
 shift
 
-SCRIPTS_DIR="$(git rev-parse --show-toplevel)/Deploy/stacks/dynamic/common-scripts"
+SCRIPTS_DIR="$(git rev-parse --show-toplevel)../../Deploy/stacks/dynamic/common-scripts"
 export SCRIPTS_DIR
 
 "${SCRIPTS_DIR}/${COMMAND}.sh" "$@"
