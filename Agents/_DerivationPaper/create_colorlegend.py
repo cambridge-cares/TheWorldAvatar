77b4be1bea0cc9c26879dd3b651c55d8b6f44019
Markus Hofmeister <mh807@cam.ac.uk>
2022-12-05 11:25:45 +0800

    194-dev-derivation-mvp-markup: Update creation of colorbar legend based on values from geojson files

--- Agents/_DerivationPaper/create_colorlegend.py
+++ Agents/_DerivationPaper/create_colorlegend.py
@@ -1,21 +1,52 @@
 # This module creates colorbar legend for DTVF
 
-from pathlib import Path
 import os
+import json
+import pandas as pd
+from pathlib import Path
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
+
+#
+#  1) Extract min and max values for colorbar
+# 
+# Quantile cutoff value for values to be included in colorbar
+q = 0.05
+files = ['affected_buildings_first_round.geojson',
+         'affected_buildings_second_round.geojson']
+cb_min = []
+cb_max = []
+for f in files:
+    fpath = os.path.join(Path(__file__).parent, 'visualisation','data', f)
+    # To ensure correct formatting of GBP symbol suppress ascii encoding and use UTF-8
+    with open(fpath, encoding='utf-8') as fp:
+        data = json.load(fp)
+    # Extract values from geojson
+    values = [d['properties']['Property market value (£)'] for d in data['features']]
+    v = pd.Series(values)
+    cb_min.append(v.quantile(q))
+    cb_max.append(v.quantile(1-q))
+
+# Extract min and max values and round to nearest 10k value
+cb_min = int(round(min(cb_min)/10000,0)*10000)
+cb_max = int(round(max(cb_max)/10000,0)*10000)
+
+#
+# 2) Create colorbar legend
+# 
 # Specify filepath to store colorbar legend
 f = 'colorbar.png'
 fp = os.path.join(Path(__file__).parent, 'visualisation','data', f)
 
 # Specify colors for colorbar (must match settings in data.json)
 colors = ["#F7BA56", "#AD0709"]
-values = [100000, 400000]
-ticks = range(values[0], values[1]+1, 100000)
+values = [cb_min, cb_max]
+ticks = range(values[0], values[1]+1, 20000)
 labels = [f'£ {t:,}' for t in ticks]
+labels = [labels[i] if i%2==0 else None for i in range(len(labels))]
 
 # Create figure and colorbar location
 fig = plt.figure( figsize=(1,4) )
@@ -31,4 +62,5 @@ cb = mpl.colorbar.ColorbarBase(ax, cmap=cmap, norm=norm, orientation='vertical')
 cb.set_ticks(ticks)
 cb.ax.set_yticklabels(labels)
 
+#plt.show
 plt.savefig(fp, dpi=300, bbox_inches='tight')
