289ecac7c53cb34afad9e774ba351fd935bc4c5d
Markus Hofmeister <mh807@cam.ac.uk>
2022-12-08 18:44:55 +0800

    dev-dockerise-forecasting-agent: Include __init__.py for tests

--- Agents/ForecastingAgent/forecasting/utils/env_configs.py
+++ Agents/ForecastingAgent/forecasting/utils/env_configs.py
@@ -84,14 +84,5 @@ def retrieve_settings():
         retrieve_stack_settings(database=DATABASE,namespace=NAMESPACE)
 
 
-#TODO: Implement properly, ideally as dockerised version
-# Set environment variables for standalone testing (non-dockerised deployment)
-os.environ["STACK_NAME"] = ""
-os.environ["DB_URL"] = "jdbc:postgresql://localhost:9432/districtheating"
-os.environ["DB_USER"] = "postgres"
-os.environ["DB_PASSWORD"] = "postgres"
-os.environ["QUERY_ENDPOINT"] = "http://localhost:9999/blazegraph/namespace/districtheating/sparql"
-os.environ["UPDATE_ENDPOINT"] = "http://localhost:9999/blazegraph/namespace/districtheating/sparql"
-
 # Run when module is imported
 retrieve_settings()
