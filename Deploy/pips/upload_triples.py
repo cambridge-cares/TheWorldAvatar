ab44a5336b1ee4fd51c6252b7b031854348a56d6
Jiaru Bai <jb2197@cam.ac.uk>
2022-12-09 14:12:50 +0000

    200-dev-pips-deploy: updated design for script that uploads triples.

--- Deploy/pips/upload_triples.py
+++ Deploy/pips/upload_triples.py
@@ -3,35 +3,33 @@ from rdflib import Graph
 import os
 
 from pyderivationagent import PySparqlClient
-from pyderivationagent import config_derivation_agent
+from pyderivationagent import config_generic
+from pyderivationagent import Config
 
+
+class ConfigKG(Config):
+    SPARQL_QUERY_ENDPOINT: str
+    SPARQL_UPDATE_ENDPOINT: str
+    KG_USERNAME: str
+    KG_PASSWORD: str
 
 # Folder paths
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 ENV_FILES_DIR = os.path.join(THIS_DIR, "env_files")
-SECRETS_DIR = os.path.join(THIS_DIR, "secrets")
 TRIPLES_DIR = os.path.join(THIS_DIR, "triples")
 
-# Credentials
-BLAZEGRAPH_USERNAME_FILE = os.path.join(SECRETS_DIR, "blazegraph_username.txt")
-BLAZEGRAPH_PASSWORD_FILE = os.path.join(SECRETS_DIR, "blazegraph_password.txt")
-
 # Config for kg endpoint
-ROGI_ENV = os.path.join(ENV_FILES_DIR, "agent.goal.iter.env.deploy")
-config = config_derivation_agent(ROGI_ENV)
+AGENT_CREDENTIALS_ENV_DEPLOY = os.path.join(ENV_FILES_DIR, "agent.credentials.env.deploy")
+config = config_generic(ConfigKG, AGENT_CREDENTIALS_ENV_DEPLOY)
 
 
 if __name__ == '__main__':
     # Initialise the sparql_client
-    with open(BLAZEGRAPH_USERNAME_FILE, 'r') as file:
-        kg_username = file.read().rstrip()
-    with open(BLAZEGRAPH_PASSWORD_FILE, 'r') as file:
-        kg_password = file.read().rstrip()
     sparql_client = PySparqlClient(
         query_endpoint=config.SPARQL_QUERY_ENDPOINT,
         update_endpoint=config.SPARQL_UPDATE_ENDPOINT,
-        kg_user=kg_username,
-        kg_password=kg_password,
+        kg_user=config.KG_USERNAME,
+        kg_password=config.KG_PASSWORD,
     )
 
     # Upload the example triples for testing
