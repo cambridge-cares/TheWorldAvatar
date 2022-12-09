6d6f221045c5747ae8a61acde133b95a8f891d23
Markus Hofmeister <mh807@cam.ac.uk>
2022-12-09 15:27:17 +0800

    dev-dockerise-forecasting-agent: Update startup message

--- Agents/ForecastingAgent/forecasting/flaskapp/home/routes.py
+++ Agents/ForecastingAgent/forecasting/flaskapp/home/routes.py
@@ -4,8 +4,9 @@
 ################################################
 
 # The purpose of this module is to print available HTTP requests (i.e. routes)
+# at agent stratup
 
-from flask import Blueprint, jsonify
+from flask import Blueprint
 
 # Blueprint Configuration
 home_bp = Blueprint(
@@ -15,15 +16,17 @@ home_bp = Blueprint(
 # Show an instructional message at the app root
 @home_bp.route('/', methods=['GET'])
 def default():
-    msg = 'Forecasting agent: <BR>'
-    msg += 'The forecasting agent can forecast time series and instantiate the new series in the KG. <BR>'
+    msg = '<B>Forecasting agent</B>:<BR><BR>'
+    msg += 'The Forecasting Agent can predict instantiated time series and instantiate the forecasted series in the KG.<BR><BR>'
     msg += 'The following parameters are required: <BR><BR>'
-    msg += 'iri: (the IRI of the time series to be forecasted), <BR>'
-    msg += 'horizon: (the number of steps to forecast) <BR><BR>'
-    msg += 'available routes: <BR>'
-    msg += '/api/forecastingAgent/forecast <BR><BR>'
-
-    msg += 'Checkout https://github.com/cambridge-cares/TheWorldAvatar/tree/main/Agents/ForecastingAgent/ for more information.'
-
+    msg += 'iri: the dataIRI of the time series to be forecasted<BR>'
+    msg += 'horizon: the number of steps to forecast <BR><BR>'
+    msg += "The Forecasting Agent offers the following functions via the specified API endpoints:<BR>"
+    msg += "<BR>"
+    msg += "Request to forecast the provided dataIRI (POST request):<BR>"
+    msg += "(this IRI will also receive the `hasForecastedValue` relationship to the predicted TimeSeries)<BR>"
+    msg += '&nbsp&nbsp /api/forecastingAgent/forecast'
+    msg += '<BR><BR>'
+    msg += 'Checkout the <a href="https://github.com/cambridge-cares/TheWorldAvatar/tree/main/Agents/ForecastingAgent/">Forecasting Agent README</a> for more information.'
 
     return msg
