84175e270ded0d5f5fe485dcb9d7c022a1c3cfde
George Brownbridge <gbrownbridge@cmclinnovations.com>
2022-12-12 18:24:08 +0000

    510-add-a-username-and-password-to-blazegraph-container: Updated the Blazegraph image so that if the environment variables ```BLAZEGRAPH_PASSWORD_FILE``` and ```BLAZEGRAPH_USER``` are specified at runtime, along with a secret with the name ```blazegraph_password```, then the Blazegraph container will be password protected.

--- Deploy/stacks/db/blazegraph/docker-entrypoint.sh
+++ Deploy/stacks/db/blazegraph/docker-entrypoint.sh
@@ -4,13 +4,19 @@
 
 
 # Update the contents of WEB-INF in the blazegraph war file
-pushd /tmp/blazegraph
+pushd /tmp/blazegraph || exit
+
+if [[ -n $BLAZEGRAPH_USER && -n $BLAZEGRAPH_PASSWORD_FILE && -f $BLAZEGRAPH_PASSWORD_FILE ]]; then
+    # Set username and password in the tomcat-users.xml using docker secret values
+    sed -i -e "s/BLAZEGRAPH_USER/$BLAZEGRAPH_USER/" -e "s/BLAZEGRAPH_PASSWORD/$(cat $BLAZEGRAPH_PASSWORD_FILE)/" /usr/local/tomcat/conf/tomcat-users.xml
+else
+    # Remove "security-constraint" related blocks
+    sed -i '6,24d' WEB-INF/web.xml
+fi
+
 #zip /usr/local/tomcat/webapps/blazegraph.war WEB-INF/web.xml
 zip -r /usr/local/tomcat/webapps/blazegraph.war WEB-INF/*
-popd
-
-# Set password in the tomcat-users.xml using docker secret values
-sed -i "s/BLAZEGRAPH_PASSWORD/$(cat $BLAZEGRAPH_PASSWORD_FILE)/" /usr/local/tomcat/conf/tomcat-users.xml
+popd || exit
 
 # Start tomcat (deploys modified war file)
 catalina.sh run
