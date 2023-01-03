6c1c6b3be01ec30de34e0cc76d869205e1daacf4
Kok Foong Lee <kflee@cmclinnovations.com>
2023-01-03 15:08:00 +0000

    dev-virtual-sensor-update: removed episode postprocess codes

--- JPS_VIRTUALSENSOR/PythonService/app.py
+++ JPS_VIRTUALSENSOR/PythonService/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-import episode.getGeoJSON as getGeoJSON
+
 import speedLoadMap.getEmissions as getEmissions
 import aermod.postprocess_aermod as postprocess_aermod
 
 app = Flask(__name__)
-app.register_blueprint(getGeoJSON.getGeoJSON_bp)
 app.register_blueprint(getEmissions.getEmissions_bp)
 app.register_blueprint(postprocess_aermod.get_aermod_geojson_bp)
 
