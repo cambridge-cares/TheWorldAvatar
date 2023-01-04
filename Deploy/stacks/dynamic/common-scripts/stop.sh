889497a27688dd583808e720a180f8fb6c450e1f
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-01-04 11:37:33 +0000

    537-stack-doesnt-start-due-to-absence-of-blazegraph-credentials: Updated some of the common stack scripts. Added checks before trying to pull docker images or remove services before restarting them to reduce false error messages, this fixes issue #532.

--- Deploy/stacks/dynamic/common-scripts/stop.sh
+++ Deploy/stacks/dynamic/common-scripts/stop.sh
@@ -6,11 +6,9 @@
 # This is added to prevent warning messages, this variable should not be used during the stop process
 export EXTERNAL_PORT="EXTERNAL_PORT should only be required at runtime!"
 
-set +e
-
 # Remove existing services started from this directory
 for SERVICE in $(${COMPOSE_EXECUTABLE} -f docker-compose-stack.yml -f docker-compose.yml convert --services); do
-	${EXECUTABLE} service rm "${STACK_NAME}_${SERVICE}"
+	if [ -n "$(docker service ls -q -f "name=${STACK_NAME}_${SERVICE}")" ]; then
+		${EXECUTABLE} service rm "${STACK_NAME}_${SERVICE}" > /dev/null
+	fi
 done
-
-set -e
