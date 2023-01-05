649e67ad6447b703c057d31501019c25f5fe9677
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-01-05 14:50:52 +0000

    272-ability-to-upload-triples-through-the-stack-data-uploader: Updated the BlazegraphClient::createNamespace method to work when authentication has been enabled.

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/blazegraph/BlazegraphClient.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/blazegraph/BlazegraphClient.java
@@ -1,6 +1,8 @@
 package com.cmclinnovations.stack.clients.blazegraph;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
@@ -9,7 +11,10 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import org.apache.commons.io.FilenameUtils;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.util.BasicAuthentication;
 
+import com.bigdata.rdf.sail.webapp.client.HttpClientConfigurator;
 import com.bigdata.rdf.sail.webapp.client.HttpException;
 import com.bigdata.rdf.sail.webapp.client.RemoteRepositoryManager;
 import com.cmclinnovations.stack.clients.core.ClientWithEndpoint;
@@ -50,15 +55,68 @@ public class BlazegraphClient extends ContainerClient implements ClientWithEndpo
 
     public void createNamespace(String namespace, Properties properties) {
         String serviceUrl = blazegraphEndpoint.getServiceUrl();
-        try (RemoteRepositoryManager manager = new RemoteRepositoryManager(serviceUrl)) {
+
+        RuntimeException firstException = null;
+        HttpClient httpClient = null;
+        if (!blazegraphEndpoint.getPassword().isEmpty()) {
+            // If authentication is enabled then create an HttpClient explicitly and add the
+            // authentication details to its AuthenticationStore.
+            try {
+                httpClient = HttpClientConfigurator.getInstance().newInstance();
+                httpClient.getAuthenticationStore()
+                        .addAuthentication(new BasicAuthentication(new URI(serviceUrl), "Blazegraph",
+                                blazegraphEndpoint.getUsername(), blazegraphEndpoint.getPassword()));
+            } catch (URISyntaxException | RuntimeException ex) {
+                if (ex instanceof URISyntaxException) {
+                    firstException = new RuntimeException(
+                        "The Blazegraph service URL '" + serviceUrl + "' is not a valid URI.", ex);
+                } else if (ex instanceof RuntimeException) {
+                    firstException = (RuntimeException) ex;
+                }
+
+                closeHttpClient(httpClient, firstException,
+                        "Failed to stop httpClient after failing to set authentication before creating new namespace '"
+                                + namespace + "' at endpoint '" + serviceUrl + "'.");
+            }
+        }
+
+        try (RemoteRepositoryManager manager = new RemoteRepositoryManager(serviceUrl, httpClient, null)) {
             manager.createRepository(namespace, properties);
         } catch (Exception ex) {
             if ((ex instanceof HttpException) && (409 == ((HttpException) ex).getStatusCode())) {
                 // Namespace already exists error
             } else {
-                throw new RuntimeException(
+                firstException = new RuntimeException(
                         "Failed to create new namespace '" + namespace + "' at endpoint '" + serviceUrl + "'.", ex);
             }
+        } finally {
+            closeHttpClient(httpClient, firstException,
+                    "Failed to stop httpClient after trying to create new namespace '" + namespace
+                            + "' at endpoint '" + serviceUrl + "'.");
+        }
+    }
+
+    /**
+     * If the HttpClient needs to be close then close it.
+     * 
+     * @param httpClient     HttpClient to close
+     * @param firstException any existing RuntimeException that has already been
+     *                       thrown
+     * @param failureMessage
+     */
+    private void closeHttpClient(HttpClient httpClient, RuntimeException firstException, String failureMessage) {
+        try {
+            if (null != httpClient && !httpClient.isStopping() && !httpClient.isStopped()) {
+                httpClient.stop();
+            }
+        } catch (Exception ex) {
+            if (null != firstException) {
+                ex.addSuppressed(firstException);
+            }
+            throw new RuntimeException(failureMessage, ex);
+        }
+        if (null != firstException) {
+            throw firstException;
         }
     }
 
