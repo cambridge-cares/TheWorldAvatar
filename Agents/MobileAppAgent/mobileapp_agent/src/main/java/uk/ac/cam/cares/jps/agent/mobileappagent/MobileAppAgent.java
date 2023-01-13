639df0e7c1dbd07d231652c9e815d5f9860a1b19
Shin Zert <szp20@cam.ac.uk>
2023-01-13 10:35:07 +0800

    dev-1457-develop-mobileappagent: Edited BASEURI format, defined variable type in private, added @Controller and @WebServlet

--- Agents/MobileAppAgent/mobileapp_agent/src/main/java/uk/ac/cam/cares/jps/agent/mobileappagent/MobileAppAgent.java
+++ Agents/MobileAppAgent/mobileapp_agent/src/main/java/uk/ac/cam/cares/jps/agent/mobileappagent/MobileAppAgent.java
@@ -2,20 +2,28 @@ package uk.ac.cam.cares.jps.agent.mobileappagent;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
 import uk.ac.cam.cares.jps.base.timeseries.TimeSeries;
 import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
+import uk.ac.cam.cares.jps.base.util.JSONKeyToIRIMapper;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BadRequestException;
 import java.sql.Connection;
 import java.time.OffsetDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
+
+
+@Controller
+@WebServlet(urlPatterns = {"/performdata"})
 public class MobileAppAgent extends JPSAgent {
 
     //Declare tables
@@ -55,14 +63,15 @@ public class MobileAppAgent extends JPSAgent {
     public static List<List<String>> tableHeaderList= Arrays.asList(accelerometerHeader,gravityHeader,lightHeader,locationHeader,magnetometerHeader,microphoneHeader);
     public static List<String> tableList = Arrays.asList(accelerometer, gravity,light, location,magnetometer,microphone);
 
-    String dbURL = "jdbc:postgresql://localhost:5432/sensor";
-    String user = "postgres";
-    String password = "postgres";
-    RemoteRDBStoreClient rdbStoreClient = new RemoteRDBStoreClient(dbURL, user, password);
-    RemoteStoreClient storeClient = new RemoteStoreClient("http://127.0.0.1:9999/blazegraph/namespace/sensor/sparql", "http://127.0.0.1:9999/blazegraph/namespace/sensor/sparql");
-    TimeSeriesClient tsClient = new TimeSeriesClient(storeClient, OffsetDateTime.class);
-    JSONArray dataArray;
-    String Query;
+    private static final String dbURL = "jdbc:postgresql://localhost:5432/sensor";
+    private static final String user = "postgres";
+    private static final String password = "postgres";
+    private static RemoteRDBStoreClient rdbStoreClient = new RemoteRDBStoreClient(dbURL, user, password);
+    private static RemoteStoreClient storeClient = new RemoteStoreClient("http://127.0.0.1:9999/blazegraph/namespace/sensor/sparql", "http://127.0.0.1:9999/blazegraph/namespace/sensor/sparql");
+    private static TimeSeriesClient tsClient = new TimeSeriesClient(storeClient, OffsetDateTime.class);
+    private JSONArray dataArray;
+    private String Query;
+    private static final String BASEURI = "https://www.theworldavatar.com/kg/measure_";
 
 
     /**
@@ -91,6 +100,9 @@ public class MobileAppAgent extends JPSAgent {
 
                 dataArray = rdbStoreClient.executeQuery(Query);
 
+                //Check if timeseries exists
+
+                //If not exists then create timeseries
                 //Create Timeseries
                 List<String> dataIRIList = createTimeSeries(i);
 
@@ -172,7 +184,7 @@ public class MobileAppAgent extends JPSAgent {
 
         //Create dataIRI for each variable
         for (int sensorVariable = 1; sensorVariable < tableHeader.size() ;sensorVariable++){
-            String dataIRIName ="https://www.theworldavatar.com/kg/ontotimeseries/MobileAppAgent_"+ tableList.get(tableNumber)+"_"+ tableHeader.get(sensorVariable)+ "_"+ UUID.randomUUID();
+            String dataIRIName =BASEURI+ tableHeader.get(sensorVariable)+ "_"+ UUID.randomUUID();
 
             dataIRIList.add(dataIRIName);
         }
@@ -207,5 +219,96 @@ public class MobileAppAgent extends JPSAgent {
         query = query + " FROM public." + tableList.get(i);
 
         return query;
+    }
+
+
+    /**
+     * Initializes all time series maintained by the agent (represented by the key to IRI mappings) if they do no exist
+     * using the time series client.
+     */
+
+//    public void initializeTimeSeriesIfNotExist() {
+//            // The IRIs used by the current mapping
+//            List<String> iris = dataArray.getAllIRIs();
+//            // Check whether IRIs have a time series linked and if not initialize the corresponding time series
+//            if(!timeSeriesExist(iris)) {
+//                // Get the classes (datatype) corresponding to each JSON key needed for initialization
+//                List<Class<?>> classes = iris.stream().map(this::getClassFromJSONKey).collect(Collectors.toList());
+//                // Initialize the time series
+//                try {
+//                    tsClient.initTimeSeries(iris, classes, timeUnit, TimeSeriesClient.Type.INSTANTANEOUS, null, null);
+//                    LOGGER.info(String.format("Initialized time series with the following IRIs: %s", String.join(", ", iris)));
+//                } catch (Exception e) {
+//                    throw new JPSRuntimeException("Could not initialize timeseries!");
+//                }
+//            }
+//
+//    }
+//
+//
+//    /**
+//     * Checks whether a time series exists by checking whether any of the IRIs that should be attached to
+//     * the time series is not initialised in the central RDB lookup table using the time series client.
+//     * @param iris The IRIs that should be attached to the same time series provided as list of strings.
+//     * @return True if all IRIs have a time series attached, false otherwise.
+//     */
+//    private boolean timeSeriesExist(List<String> iris) {
+//        // If any of the IRIs does not have a time series the time series does not exist
+//        for(String iri: iris) {
+//            try {
+//                if (!tsClient.checkDataHasTimeSeries(iri)) {
+//                    return false;
+//                }
+//                // If central RDB lookup table ("dbTable") has not been initialised, the time series does not exist
+//            } catch (DataAccessException e) {
+//                if (e.getMessage().contains("ERROR: relation \"dbTable\" does not exist")) {
+//                    return false;
+//                }
+//                else {
+//                    throw e;
+//                }
+//            }
+//        }
+//        return true;
+//    }
+
+
+
+    /**
+     * Prunes a times series so that all timestamps and corresponding values start after the threshold.
+     * @param timeSeries The times series tp prune
+     * @param timeThreshold The threshold before which no data should occur
+     * @return The resulting datetime object.
+     */
+    private TimeSeries<OffsetDateTime> pruneTimeSeries(TimeSeries<OffsetDateTime> timeSeries, OffsetDateTime timeThreshold) {
+        // Find the index from which to start
+        List<OffsetDateTime> times = timeSeries.getTimes();
+        int index = 0;
+        while(index < times.size()) {
+            if (times.get(index).isAfter(timeThreshold)) {
+                break;
+            }
+            index++;
+        }
+        // Prune timestamps
+        List<OffsetDateTime> newTimes = new ArrayList<>();
+        // There are timestamps above the threshold
+        if (index != times.size()) {
+            // Prune the times
+            newTimes = new ArrayList<>(times.subList(index, times.size()));
+        }
+        // Prune data
+        List<List<?>> newValues = new ArrayList<>();
+        // Prune the values
+        for (String iri: timeSeries.getDataIRIs()) {
+            // There are timestamps above the threshold
+            if (index != times.size()) {
+                newValues.add(timeSeries.getValues(iri).subList(index, times.size()));
+            }
+            else {
+                newValues.add(new ArrayList<>());
+            }
+        }
+        return new TimeSeries<>(newTimes, timeSeries.getDataIRIs(), newValues);
     }
 }
