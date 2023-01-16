8329d1efdfa9928add04f61230835994e269c48d
Wilson Ang <wilson1010x@gmail.com>
2023-01-16 18:15:36 +0800

    1469-dev-rfid-query-agent: Upload first draft of RFID Query Agent

---
+++ Agents/RFIDQueryAgent/rfid-query-agent-code/src/main/java/uk/ac/cam/cares/jps/agent/rfidquery/RFIDQueryAgent.java
@@ -0,0 +1,194 @@
+package uk.ac.cam.cares.jps.agent.rfidquery;
+
+import uk.ac.cam.cares.jps.base.email.EmailSender;
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeries;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
+import java.io.IOException;
+import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.*;
+import java.util.TimeZone;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+
+/**
+ * Class to query for the latest RFID tag status from the knowledge graph and subsequently the chemical species information.
+ * @author  */
+public class RFIDQueryAgent{
+
+	/**
+     * Logger for reporting info/errors.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(RFIDQueryAgentLauncher.class);
+
+    /**
+     * The time series client to interact with the knowledge graph and data storage
+     */
+    private TimeSeriesClient<OffsetDateTime> tsClient;
+
+    ArrayList<String> dataIRIList = new ArrayList<String>();
+
+    /**
+     * Log messages
+     */
+    private static final String GETLATESTDATA_ERROR_MSG = "Unable to query for latest data!" ;
+
+    /**
+     * List to store timeseries string values
+     */
+    private List<String> dataValuesAsString;
+
+    /**
+     * Number of hours
+     */
+    private long numOfHours ;
+
+    /**
+     * timeSeries Object
+     */
+    TimeSeries<OffsetDateTime> timeseries;
+    
+    /**
+     * RDB Client Object
+     */
+    private RemoteRDBStoreClient RDBClient;
+
+    /**
+     * The Zone offset of the timestamp (https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/time/ZoneOffset.html)
+     */
+    public static final ZoneOffset ZONE_OFFSET = ZoneOffset.UTC;
+
+    /**
+     * Standard constructor which receives and parses DataIRIs from the AgentLauncher class.
+     * @param dataIRIs The data IRIs to query latest data for
+     */
+    public RFIDQueryAgent(String dataIRIs, String Hours) throws IOException {
+        for (int i = 0; i <= dataIRIs.split(",").length - 1; i++) {
+            this.dataIRIList.add(i, dataIRIs.split(",")[i]);
+        }
+
+        this.numOfHours = Long.valueOf(Hours);
+    }
+
+    /**
+     * Setter for the time series client.
+     * @param tsClient The time series client to use.
+     */
+    public void setTsClient(TimeSeriesClient<OffsetDateTime> tsClient) {
+        this.tsClient = tsClient;
+    }
+
+    /**
+     * Setter for the remote rdb store client.
+     * @param RDBClient The remote rdb store client to use.
+     */
+    public void setRDBClient(RemoteRDBStoreClient RDBClient) {
+        this.RDBClient = RDBClient;
+    }
+
+    /**
+     * Query the database for the latest RFID tag status and timestamp.
+     * @param dataIRI the data IRI to query values from
+     */
+    public TimeSeries<OffsetDateTime> queryLatestRFIDStatus(String dataIRI)throws IllegalArgumentException {
+        try (Connection conn = RDBClient.getConnection()){
+            timeseries = tsClient.getLatestData(dataIRI, conn);
+        } catch (Exception e) {
+            throw new JPSRuntimeException(GETLATESTDATA_ERROR_MSG, e);
+        }
+        return timeseries;
+    }
+
+    /**
+     * Queries for latest data and check whether latest timestamps exceed threshold set by user
+     * @return resuTts A Json Object with the following format {iri_1: {exceedThreshold: true/false, timestamp: timestamp value, dataIRI: data IRI 1}, 
+     *                                                          iri_2: {exceedThreshold: true/false, timestamp: timestamp value, dataIRI: data IRI 2} }
+     */
+    public JSONObject queriesStatusAndCheckTimeStamps() {
+        JSONObject results = new JSONObject();
+        for (int i = 0; i < dataIRIList.size() - 1; i++) {
+            JSONObject values = new JSONObject();
+            TimeSeries<OffsetDateTime> LatestTimeSeries = queryLatestRFIDStatus(dataIRIList.get(i));
+            values = checkRFIDStatusThreshold(LatestTimeSeries, dataIRIList.get(i), numOfHours);
+            results.put("iri_"+i, values);
+        }
+
+        return results;
+    }
+
+    /**
+     * 
+     * @param timeSeriesObject The timeseries containing the latest timeseries data
+     * @param dataIRI The data IRI to retrieve data for
+     * @param hours Number of hours
+     * @return values A JSONObject with the following format: {exceedThreshold: true/false, timestamp: timestamp value, dataIRI: data IRI 1}
+     */
+    public JSONObject checkRFIDStatusThreshold (TimeSeries<OffsetDateTime> timeSeriesObject, String dataIRI, Long hours) {
+    	Boolean exceedThreshold = false ;
+        //process timeseries object and convert to a suitable form, retrieve values only
+    	dataValuesAsString = timeSeriesObject.getValuesAsString(dataIRI);
+    	String latestTimeSeriesValue = dataValuesAsString.get(dataValuesAsString.size() - 1);
+    	OffsetDateTime latestTimeStamp = timeSeriesObject.getTimes().get(timeSeriesObject.getTimes().size() - 1);
+    	if (latestTimeSeriesValue.contains("In")) {
+    	LOGGER.info("The latest RFID status is " + latestTimeSeriesValue);
+    	
+    	LOGGER.info("The latest timestamp is "+ latestTimeStamp);
+
+        exceedThreshold = false;
+
+    	} else if (latestTimeSeriesValue.contains("Out")) {
+            // if latest status is Out and timestamp is "2022-10-27 18:20:02+08"
+			// take timestamp and add the number of hours in which a bottle is allowed to be outside of the cabinet for 
+			// e.g. "2022-10-27 18:20:02+08 + 4 hours = 2022-10-27 22:20:02+08"
+            OffsetDateTime thresholdTimeStamp = latestTimeStamp.plusHours(hours);
+            long timestamp = System.currentTimeMillis();
+            Date date = new java.util.Date(timestamp);
+            SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Object ts = sdf.format(date);
+            LocalDateTime localTime = LocalDateTime.parse(ts.toString());
+            // Then add the zone id
+            OffsetDateTime currentDateTime = OffsetDateTime.of(localTime, ZoneOffset.UTC);
+
+            // if 2022-10-27 22:20:02+08 is before the current date time, it means that the bottle has been outside of the cabinet for a time
+            // period longer than the allowed duration
+            if (thresholdTimeStamp.isBefore(currentDateTime)) {
+                exceedThreshold = true;
+            }
+            else {
+                exceedThreshold = false;
+            }
+        }
+        JSONObject values = new JSONObject();
+        values.put("exceedThreshold", exceedThreshold);
+        values.put("timestamp", latestTimeStamp.toString());
+        values.put("dataIRI", dataIRI);
+        return values;
+    }
+
+    /**
+     * @param tagID RFID tag ID number that is usually append at the end of the data IRI
+     * @param chemicalSpeciesName Name of the chemical species that is stored in the tagged bottle
+     * @param latestTimeStamp latest timestamp value
+     */
+    public void sendEmail(String tagID, String chemicalSpeciesName, String latestTimeStamp) {
+        try {
+            EmailSender sender = new EmailSender();
+            String emailMessages;
+            emailMessages = "The bottle with the following information has been removed since " + latestTimeStamp.toString() + 
+            "The bottle has the following tag ID " + tagID + " and it is storing " + chemicalSpeciesName + ". This chemical species has the following information: ";
+            sender.sendEmail("Alert!", emailMessages);
+        
+        } catch (Exception e) {
+            throw new JPSRuntimeException("Unable to send out alert email!");
+        }
+    }
+}
+
---
+++ Agents/RFIDQueryAgent/rfid-query-agent-code/src/main/java/uk/ac/cam/cares/jps/agent/rfidquery/RFIDQueryAgentLauncher.java
@@ -0,0 +1,249 @@
+package uk.ac.cam.cares.jps.agent.rfidquery;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeries;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
+import uk.ac.cam.cares.jps.base.agent.JPSAgent;
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
+import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.*;
+import java.util.*;
+import javax.servlet.annotation.WebServlet;
+import javax.ws.rs.BadRequestException;
+
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@WebServlet(urlPatterns = {"/"})
+public class RFIDQueryAgentLauncher extends JPSAgent{
+	/**
+     * Logger for reporting info/errors.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(RFIDQueryAgentLauncher.class);
+
+    /**
+     * The time series client to interact with the knowledge graph and data storage
+     */
+    private TimeSeriesClient<OffsetDateTime> tsClient;
+    
+    //timeSeries Object
+    TimeSeries<OffsetDateTime> timeseries;
+    
+    /**
+     * Parameters to set rdf client and rdb client
+     */
+	String dbUrl;
+	String dbUsername;
+	String dbPassword;
+	String sparqlQueryEndpoint;
+	String sparqlUpdateEndpoint;
+    
+    //latest timeseries value
+    double latestTimeSeriesValue;
+    
+    //keys in the request's JSON Object
+    public static final String KEY_TIMESERIES_CLIENTPROPERTIES = "timeSeriesClientProperties";
+    public static final String KEY_DATAIRIS = "dataIRIs";
+	public static final String KEY_NUMOFHOURS = "hours";
+    
+    //data values
+    private List<String> dataValuesAsString;
+    
+    //timeseries IRI
+    private String timeseriesIRI;
+    
+    //set kbClient
+    RemoteStoreClient kbClient = new RemoteStoreClient();
+    
+    //JSONObjects
+    JSONObject message;
+    
+    /**
+     * Error messages
+     */
+    private static final String TSCLIENT_ERROR_MSG = "Could not construct the time series client needed by the input agent!";
+    private static final String LOADCONFIGS_ERROR_MSG = "Could not load RDB and SPARQL configs from the properties file!";
+    private static final String GETLATESTSTATUSANDCHECKTHRESHOLD_ERROR_MSG = "Unable to query for latest data and/or check RFID Status Threshold!";
+    private static final String ARGUMENT_MISMATCH_MSG = "Need two properties files in the following order:1) time series client for timeseries data 2)list of data IRIs";
+    private static final String AGENT_ERROR_MSG = "The RFID Query Agent could not be constructed!";
+    @Override
+    public JSONObject processRequestParameters(JSONObject requestParams) {
+    	JSONObject jsonMessage = new JSONObject();
+      if (validateInput(requestParams)) {
+        	LOGGER.info("Passing request to Agent..");
+        	String timeseriesDataClientProperties = System.getenv(requestParams.getString(KEY_TIMESERIES_CLIENTPROPERTIES));
+        	String DataIRIs = requestParams.getString(KEY_DATAIRIS);
+			String numOfHours = requestParams.getString(KEY_NUMOFHOURS);
+            String[] args = new String[] {timeseriesDataClientProperties, DataIRIs, numOfHours};
+            jsonMessage = initializeAgent(args);
+            jsonMessage.accumulate("message","POST request has been sent successfully.");
+            requestParams = jsonMessage;
+            }
+      else {
+    	  jsonMessage.accumulate("message","Unable to validate request sent to the agent.");
+    	  requestParams = jsonMessage;
+      }
+	return requestParams;
+}
+    
+    @Override
+    public boolean validateInput(JSONObject requestParams) throws BadRequestException {
+      boolean validate = true;
+
+      String timeseriesClientProperties;
+      String dataIRIs;
+      
+      if (requestParams.isEmpty()) {
+    	  validate = false;
+    	  LOGGER.info("Empty Request!");
+    	  
+      }
+      else {
+ 		 validate = requestParams.has(KEY_TIMESERIES_CLIENTPROPERTIES);
+ 		 if (validate == true) {
+ 	 		 validate = requestParams.has(KEY_DATAIRIS);
+ 	 		 }
+ 	 		 if (validate == true) {
+ 	 		 timeseriesClientProperties =  (requestParams.getString(KEY_TIMESERIES_CLIENTPROPERTIES));
+ 	 		if (System.getenv(timeseriesClientProperties) == null) {
+ 	 			validate = false;
+ 	 			LOGGER.info("Environment variable is not assigned to the timeseries client properties file!");
+ 	 		 }
+ 		 }
+ 	 }
+	return validate;
+    }
+    
+    public JSONObject initializeAgent(String[] args) {
+    	
+    	 // Ensure that there are three two arguments provided
+        if (args.length != 2) {
+            LOGGER.error(ARGUMENT_MISMATCH_MSG);
+            throw new JPSRuntimeException(ARGUMENT_MISMATCH_MSG);
+        }
+        LOGGER.debug("Launcher called with the following files: " + String.join(" ", args));
+    	
+         // Create the agent
+		 RFIDQueryAgent agent;
+		 try {
+			 agent = new RFIDQueryAgent(args[1], args[2]);
+		 } catch (IOException e) {
+			 LOGGER.error(AGENT_ERROR_MSG, e);
+			 throw new JPSRuntimeException(AGENT_ERROR_MSG, e);
+		 }
+		 LOGGER.info("Input agent object initialized.");
+		 JSONObject jsonMessage = new JSONObject();
+		 jsonMessage.accumulate("Result", "Input agent object initialized.");
+		 
+		 try {
+			 loadTSClientConfigs(args[0]);
+		 } catch (IOException e) {
+			 throw new JPSRuntimeException(LOADCONFIGS_ERROR_MSG, e);
+		 }
+		 
+		 RemoteStoreClient kbClient = new RemoteStoreClient();
+		 kbClient.setQueryEndpoint(sparqlQueryEndpoint);
+		 kbClient.setUpdateEndpoint(sparqlUpdateEndpoint);
+		 TimeSeriesClient<OffsetDateTime> tsClient;
+		 
+		 RemoteRDBStoreClient rdbStoreClient = new RemoteRDBStoreClient(dbUrl, dbUsername, dbPassword);
+		 agent.setRDBClient(rdbStoreClient);
+		 
+		 // Create and set the time series client
+		 try {
+			 tsClient = new TimeSeriesClient<>(kbClient ,OffsetDateTime.class);
+			 agent.setTsClient(tsClient);
+		 } catch (JPSRuntimeException e) {
+			 LOGGER.error(TSCLIENT_ERROR_MSG, e);
+			 throw new JPSRuntimeException(TSCLIENT_ERROR_MSG, e);
+		 }
+		 LOGGER.info("Time series client object initialized.");
+		 jsonMessage.accumulate("Result", "Time series client object initialized.");
+
+		 JSONObject overallResults = new JSONObject();
+		 try {
+			overallResults = agent.queriesStatusAndCheckTimeStamps();
+			LOGGER.info("Queried for latest RFID tag status and checked timestamp threshold.");
+			jsonMessage.accumulate("Result", "Queried for latest RFID tag status and checked timestamp threshold.");
+		}
+		catch (Exception e) {
+			LOGGER.error(GETLATESTSTATUSANDCHECKTHRESHOLD_ERROR_MSG, e);
+			throw new JPSRuntimeException(GETLATESTSTATUSANDCHECKTHRESHOLD_ERROR_MSG ,e);
+		}
+		/**
+		 * loop through results to get tag ID, exceedThreshold
+		 * If exceedThreshold is true, query for chemical species info
+		 * Run sendEmail with tag ID, chemical species info
+		 */
+		for (int i = 0; i < overallResults.length() - 1; i++){
+			JSONObject a = overallResults.getJSONObject("iri_"+i);
+			String timestamp = a.getString("timestamp");
+			Boolean exceedThreshold = a.getBoolean("exceedThreshold");
+			String dataIRI = a.getString("dataIRI");
+
+			if (exceedThreshold == true){
+				agent.sendEmail(dataIRI, "Potassium Nitrate", timestamp);
+				LOGGER.info("Alert Email sent for " + dataIRI);
+				jsonMessage.accumulate("Result", "Alert Email sent for " + dataIRI);
+			}
+		}
+		 
+		 return jsonMessage;
+	 }
+
+
+	/**
+     * Reads the username, password, sparql endpoint from a properties file and saves it in fields.
+     * @param filepath Path to the properties file from which to read the username, password, sparql endpoints
+     */
+    public void loadTSClientConfigs(String filepath) throws IOException {
+        // Check whether properties file exists at specified location
+        File file = new File(filepath);
+        if (!file.exists()) {
+            throw new FileNotFoundException("No properties file found at specified filepath: " + filepath);
+        }
+        try (InputStream input = new FileInputStream(file)) {
+
+            // Load properties file from specified path
+            Properties prop = new Properties();
+            prop.load(input);
+
+            if (prop.containsKey("db.user")) {
+                this.dbUsername = prop.getProperty("db.user");
+            } else {
+                throw new IOException("Properties file is missing \"db.user=<db_user>\"");
+            }
+            if (prop.containsKey("db.password")) {
+                this.dbPassword = prop.getProperty("db.password");
+            } else {
+                throw new IOException("Properties file is missing \"db.password=<db_password>\"");
+            }
+            if (prop.containsKey("sparql.query.endpoint")) {
+	        	kbClient.setQueryEndpoint(prop.getProperty("sparql.query.endpoint"));
+	        } else {
+	        	throw new IOException("Properties file is missing \"sparql.query.endpoint=<sparql_endpoint>\" ");
+	        }
+	        if (prop.containsKey("sparql.update.endpoint")) {
+	        	kbClient.setUpdateEndpoint(prop.getProperty("sparql.update.endpoint"));
+	        } else {
+	        	throw new IOException("Properties file is missing \"sparql.update.endpoint=<sparql_endpoint>\" ");
+	        }
+        }
+        }
+    }
+
