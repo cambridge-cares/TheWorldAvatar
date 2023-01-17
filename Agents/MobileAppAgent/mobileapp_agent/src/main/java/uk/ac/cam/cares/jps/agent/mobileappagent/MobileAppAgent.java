3572a78b93b394e2e43eb4b23a23993646ecb35d
Shin Zert <szp20@cam.ac.uk>
2023-01-17 16:03:10 +0800

    dev-1457-develop-mobileappagent: Added lines to parseIRIList and connect to dbTable

--- Agents/MobileAppAgent/mobileapp_agent/src/main/java/uk/ac/cam/cares/jps/agent/mobileappagent/MobileAppAgent.java
+++ Agents/MobileAppAgent/mobileapp_agent/src/main/java/uk/ac/cam/cares/jps/agent/mobileappagent/MobileAppAgent.java
@@ -1,5 +1,8 @@
 package uk.ac.cam.cares.jps.agent.mobileappagent;
 
+import org.apache.commons.logging.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jooq.exception.DataAccessException;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -15,6 +18,7 @@ import uk.ac.cam.cares.jps.base.util.JSONKeyToIRIMapper;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BadRequestException;
+import java.sql.Array;
 import java.sql.Connection;
 import java.time.OffsetDateTime;
 import java.util.*;
@@ -73,6 +77,8 @@ public class MobileAppAgent extends JPSAgent {
     private String Query;
     private static final String BASEURI = "https://www.theworldavatar.com/kg/measure_";
 
+    private static final Logger LOGGER = LogManager.getLogger(MobileAppAgent.class);
+
 
     /**
      * Processes HTTP requests with originating details.
@@ -100,22 +106,6 @@ public class MobileAppAgent extends JPSAgent {
 
                 dataArray = rdbStoreClient.executeQuery(Query);
 
-
-//                try {
-//                    if (!tsClient.checkDataHasTimeSeries(iri)) {
-//                        return false;
-//                    }
-//                    // If central RDB lookup table ("dbTable") has not been initialised, the time series does not exist
-//                } catch (DataAccessException e) {
-//                    if (e.getMessage().contains("ERROR: relation \"dbTable\" does not exist")) {
-//                        return false;
-//                    } else {
-//                        throw e;
-//                    }
-//                }
-
-
-
                 try//Check if dbTable exists
                 {
 
@@ -127,8 +117,11 @@ public class MobileAppAgent extends JPSAgent {
                     dataIRIArray = rdbStoreClient.executeQuery(Query);
 
                     //Get the newest timeseries
-                    TimeSeries getTimeSeries = parseDataToLists(i, dataArray, dataIRIArray);
-                    pruneTimeSeries (getTimeSeries,timeThreshold);
+                    TimeSeries getTimeSeries = parseDataToLists(i, dataArray, parseJSONArrayToList(dataIRIArray));
+
+
+                    updateData(getTimeSeries);
+
 
                 }
                 finally //When time series does not exist create timeseries
@@ -153,6 +146,52 @@ public class MobileAppAgent extends JPSAgent {
 
         }
         return requestParams;
+    }
+
+
+    public void Test(){
+        for (int i = 0; i < tableList.size(); i++) {
+            Query = getQueryString(i);
+
+            dataArray = rdbStoreClient.executeQuery(Query);
+
+            try//Check if dbTable exists
+            {
+                String IRIQuery;
+
+                JSONArray dataIRIArray;
+                OffsetDateTime timeThreshold;
+
+                //If dbTable exists
+                IRIQuery = getDataIRIFromDBTable(i);
+                dataIRIArray = rdbStoreClient.executeQuery(IRIQuery);
+
+                //Get the newest timeseries
+                TimeSeries getTimeSeries = parseDataToLists(i, dataArray, parseJSONArrayToList(dataIRIArray));
+
+                updateData(getTimeSeries);
+
+            }
+            finally //When time series does not exist create timeseries
+            {
+
+//                //Create Timeseries
+//                List<String> dataIRIList = createTimeSeries(i);
+//
+//                //GetTimeSeries
+//                TimeSeries getTimeSeries = parseDataToLists(i, dataArray, dataIRIList);
+//
+//                //Add timeseries data with tsList
+//                try (Connection conn = rdbStoreClient.getConnection()) {
+//                    tsClient.addTimeSeriesData(getTimeSeries, conn);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                    throw new JPSRuntimeException(e);
+//                }
+            }
+
+        }
+
     }
 
         /**
@@ -265,56 +304,60 @@ public class MobileAppAgent extends JPSAgent {
         return query;
     }
 
+    private static ArrayList<String> parseJSONArrayToList (JSONArray jArray){
+        ArrayList<String> listdata = new ArrayList<String>();
+        if (jArray != null) {
+            for (int i=0;i<jArray.length();i++){
+                JSONObject row = jArray.getJSONObject(i);
 
-    /**
-     * Initializes all time series maintained by the agent (represented by the key to IRI mappings) if they do no exist
-     * using the time series client.
-     */
-
-    public void initializeTimeSeriesIfNotExist() {
-            // The IRIs used by the current mapping
-            List<String> iris = dataArray.getAllIRIs();
-            // Check whether IRIs have a time series linked and if not initialize the corresponding time series
-            if(!timeSeriesExist(iris)) {
-                // Get the classes (datatype) corresponding to each JSON key needed for initialization
-                List<Class<?>> classes = iris.stream().map(this::getClassFromJSONKey).collect(Collectors.toList());
-                // Initialize the time series
-                try {
-                    tsClient.initTimeSeries(iris, classes, timeUnit, TimeSeriesClient.Type.INSTANTANEOUS, null, null);
-                    LOGGER.info(String.format("Initialized time series with the following IRIs: %s", String.join(", ", iris)));
-                } catch (Exception e) {
-                    throw new JPSRuntimeException("Could not initialize timeseries!");
-                }
-            }
-
-    }
-
-
-    /**
-     * Checks whether a time series exists by checking whether any of the IRIs that should be attached to
-     * the time series is not initialised in the central RDB lookup table using the time series client.
-     * @param iris The IRIs that should be attached to the same time series provided as list of strings.
-     * @return True if all IRIs have a time series attached, false otherwise.
-     */
-    private boolean timeSeriesExist(List<String> iris) {
-        // If any of the IRIs does not have a time series the time series does not exist
-        for(String iri: iris) {
-            try {
-                if (!tsClient.checkDataHasTimeSeries(iri)) {
-                    return false;
-                }
-                // If central RDB lookup table ("dbTable") has not been initialised, the time series does not exist
-            } catch (DataAccessException e) {
-                if (e.getMessage().contains("ERROR: relation \"dbTable\" does not exist")) {
-                    return false;
-                }
-                else {
-                    throw e;
-                }
+                listdata.add(row.get("dataIRI").toString());
             }
         }
-        return true;
+        return listdata;
     }
+//value = dataArray.getJSONObject(row).getDouble((tableHeader.get(column)).toString())
+//.get("dataIRI")
+    /**
+     * @param ts
+     * @throws IllegalArgumentException
+     */
+    public void updateData(TimeSeries <OffsetDateTime> ts) throws IllegalArgumentException {
+        // Update each time series
+//        for (TimeSeries<OffsetDateTime> ts : timeSeries) {
+            // Retrieve current maximum time to avoid duplicate entries (can be null if no data is in the database yet)
+            OffsetDateTime endDataTime;
+            try (Connection conn = rdbStoreClient.getConnection()) {
+                TimeSeriesClient tsClient = new TimeSeriesClient(storeClient, OffsetDateTime.class);
+
+                try {
+                    endDataTime = (OffsetDateTime) tsClient.getMaxTime(ts.getDataIRIs().get(0),conn);
+                } catch (Exception e) {
+                    throw new JPSRuntimeException("Could not get max time!");
+                }
+                OffsetDateTime startCurrentTime = ts.getTimes().get(0);
+                // If there is already a maximum time
+                if (endDataTime != null) {
+                    // If the new data overlaps with existing timestamps, prune the new ones
+                    if (startCurrentTime.isBefore(endDataTime)) {
+                        ts = pruneTimeSeries(ts, endDataTime);
+                    }
+                }
+                // Only update if there actually is data
+                if (!ts.getTimes().isEmpty()) {
+                    try {
+                        tsClient.addTimeSeriesData(ts);
+                        LOGGER.debug(String.format("Time series updated for following IRIs: %s", String.join(", ", ts.getDataIRIs())));
+                    } catch (Exception e) {
+                        throw new JPSRuntimeException("Could not add timeseries!");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new JPSRuntimeException(e);
+            }
+
+        }
+//    }
 
 
 
@@ -355,4 +398,5 @@ public class MobileAppAgent extends JPSAgent {
         }
         return new TimeSeries<>(newTimes, timeSeries.getDataIRIs(), newValues);
     }
+
 }
