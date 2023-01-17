2def3edffd5854183e0d78ad1568c3f209f33480
cmclscourtney <scourtney@cmclinnovations.com>
2023-01-17 15:17:26 +0000

    add-superset-to-stack: configuring main location block for superset

--- Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/stack/services/SupersetService.java
+++ Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/stack/services/SupersetService.java
@@ -1,6 +1,14 @@
 package com.cmclinnovations.stack.services;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.net.URI;
+
+import com.cmclinnovations.stack.services.config.Connection;
 import com.cmclinnovations.stack.services.config.ServiceConfig;
+import com.cmclinnovations.stack.clients.utils.FileUtils;
+import com.github.odiszapc.nginxparser.NgxBlock;
+import com.github.odiszapc.nginxparser.NgxParam;
 
 public class SupersetService extends ContainerService {
 
@@ -10,4 +18,24 @@ public class SupersetService extends ContainerService {
         super(stackName, serviceManager, config);
     }
 
+    @Override
+    public void addServerSpecificNginxSettingsToLocationBlock(NgxBlock locationBlock,
+            Map<String, String> upstreams, Entry<String, Connection> endpoint) {
+        Connection connection = endpoint.getValue();
+        URI externalPath = connection.getExternalPath();
+
+        // adding "proxy_redirect off;"
+        NgxParam proxySetHeaderParam = new NgxParam();
+        proxySetHeaderParam.addValue("proxy_redirect");
+        proxySetHeaderParam.addValue("off");
+        locationBlock.addEntry(proxySetHeaderParam);
+
+        // adding "proxy_set_header X-Script-Name /dashboard;" or whatever is specified
+        // as the externalPath
+        NgxParam proxyRedirectParam = new NgxParam();
+        proxyRedirectParam.addValue("proxy_set_header");
+        proxyRedirectParam.addValue("X-Script-Name");
+        proxyRedirectParam.addValue(FileUtils.fixSlashs(externalPath.getPath(), true, true));
+        locationBlock.addEntry(proxyRedirectParam);
+    }
 }
