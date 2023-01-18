a04d921e8abaee3fd084cdb296a23f718f2dee2f
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-01-18 13:19:06 +0000

    546-update-stack-tools-to-work-with-podman: Updated the ListPodsCmdExec::execute method to use Gson for the object mapping as that is what the Kubernetes api data model classes are anotated for.

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/github/dockerjava/core/exec/ListPodsCmdExec.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/github/dockerjava/core/exec/ListPodsCmdExec.java
@@ -1,6 +1,7 @@
 package com.github.dockerjava.core.exec;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,11 +12,15 @@ import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.MediaType;
 import com.github.dockerjava.core.WebTarget;
 import com.github.dockerjava.core.util.FiltersEncoder;
+import com.google.gson.Gson;
 
 import io.kubernetes.client.openapi.models.V1Pod;
+import jakarta.json.JsonObject;
 
 public class ListPodsCmdExec extends AbstrSyncDockerCmdExec<ListPodsCmd, List<V1Pod>> implements
         ListPodsCmd.Exec {
+
+    private Gson gson = new Gson();
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ListPodsCmdExec.class);
 
@@ -34,9 +39,13 @@ public class ListPodsCmdExec extends AbstrSyncDockerCmdExec<ListPodsCmd, List<V1
 
         LOGGER.trace("GET: {}", webTarget);
 
-        List<V1Pod> pods = webTarget.request().accept(MediaType.APPLICATION_JSON)
-                .get(new TypeReference<List<V1Pod>>() {
+        List<JsonObject> podsJson = webTarget.request().accept(MediaType.APPLICATION_JSON)
+                .get(new TypeReference<List<JsonObject>>() {
                 });
+
+        List<V1Pod> pods = podsJson.stream()
+                .map(object -> gson.fromJson(object.toString(), V1Pod.class))
+                .collect(Collectors.toList());
 
         LOGGER.trace("Response: {}", pods);
 
