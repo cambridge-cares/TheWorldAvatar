1f4e9910a0dbdae5cf06fd07ebe91dee1580ce7b
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-01-20 12:21:59 +0000

    546-update-stack-tools-to-work-with-podman: Add implementation of the ```PodmanService::addStackConfigs``` that just adds them as secrets.

--- Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/stack/services/PodmanService.java
+++ Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/stack/services/PodmanService.java
@@ -1,8 +1,16 @@
 package com.cmclinnovations.stack.services;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.cmclinnovations.stack.clients.docker.PodmanClient;
 import com.cmclinnovations.stack.services.config.ServiceConfig;
@@ -42,6 +50,30 @@ public class PodmanService extends DockerService {
 
     @Override
     protected void addStackConfigs() {
+
+        try {
+            Files.walkFileTree(Path.of("/inputs/config"), new SimpleFileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    try {
+                        if (Files.isReadable(file) && !file.getFileName().toString().startsWith(".git")) {
+                            String configName = file.getFileName().toString();
+
+                            try (Stream<String> lines = Files.lines(file)) {
+                                String data = lines.collect(Collectors.joining("\n"));
+                                dockerClient.addSecret(configName, data);
+                            }
+                        }
+                        return FileVisitResult.CONTINUE;
+                    } catch (IOException ex) {
+                        throw new IOException("Failed to load config file '" + file + "'.", ex);
+                    }
+                }
+            });
+        } catch (IOException ex) {
+            throw new RuntimeException("Failed to load configs.", ex);
+        }
     }
 
     @Override
