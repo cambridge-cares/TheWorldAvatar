db2b442c0312d31529a852229071e0c9a33d7e8c
cmclscourtney <scourtney@cmclinnovations.com>
2023-01-24 14:19:33 +0000

    566-add-dimensions-to-geoserver-in-stack-data-uploader: added dimenstions to GeoServerVectorSettings

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/geoserver/GeoServerVectorSettings.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/geoserver/GeoServerVectorSettings.java
@@ -1,5 +1,7 @@
 package com.cmclinnovations.stack.clients.geoserver;
 
+import java.util.Map;
+
 import it.geosolutions.geoserver.rest.encoder.GSLayerEncoder;
 import it.geosolutions.geoserver.rest.encoder.metadata.virtualtable.GSVirtualTableEncoder;
 
@@ -13,5 +15,15 @@ public class GeoServerVectorSettings extends GSLayerEncoder {
 
     public void setVirtualTable(UpdatedGSVirtualTableEncoder virtualTable) {
         this.virtualTable = virtualTable;
+    }
+
+    private Map<String, UpdatedGSFeatureDimensionInfoEncoder> dimensions;
+
+    public Map<String, UpdatedGSFeatureDimensionInfoEncoder> getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(Map<String, UpdatedGSFeatureDimensionInfoEncoder> dimensions) {
+        this.dimensions = dimensions;
     }
 }
