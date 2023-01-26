61e09eb2720edabde8043c7a7e141d01f8ec5672
cmclscourtney <scourtney@cmclinnovations.com>
2023-01-26 14:30:05 +0000

    573-stack-data-uploader-config-add-ability-to-have-geoserver-sql-queries-in-seperate-referenced-file: added file to string to virtual table query

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/Vector.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/Vector.java
@@ -24,6 +24,7 @@ public class Vector extends GeoServerDataSubset {
 
     @Override
     public void createLayer(String workspaceName, String database) {
+        geoServerSettings.getVirtualTable().setSql(sqlFileToString(geoServerSettings.getVirtualTable().getSql()));
         GeoServerClient.getInstance()
                 .createPostGISLayer(workspaceName, database, getName(), geoServerSettings);
     }
