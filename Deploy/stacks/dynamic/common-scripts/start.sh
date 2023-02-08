64d2e1fc0d4973005bc8dd600e17587d36d709ba
cmclscourtney <scourtney@cmclinnovations.com>
2023-02-08 16:52:19 +0000

    546-update-stack-tools-to-work-with-podman: Added Podman/Docker specific compose files for specifying the configs for the stack-data-uploader.

--- Deploy/stacks/dynamic/common-scripts/start.sh
+++ Deploy/stacks/dynamic/common-scripts/start.sh
@@ -18,10 +18,10 @@ done
 # Remove existing services started from this directory
 "${SCRIPTS_DIR}/stop.sh" "${STACK_NAME}"
 
-while (( $# >= 1 )); do 
+while (( $# >= 1 )); do
     case $1 in
-    --debug-port) DEBUG_PORT=$2; shift;;
-    *) export EXTERNAL_PORT=${1};;
+        --debug-port) DEBUG_PORT=$2; shift;;
+        *) export EXTERNAL_PORT=${1};;
     esac;
     shift
 done
@@ -31,15 +31,23 @@ if [ "${EXECUTABLE}" == "docker" ]; then
         export DEBUG_PORT
         DEBUG_COMPOSE_FILE="--compose-file=docker-compose-debug.yml"
     fi
-
+    
+    if [ -f "docker-compose-docker.yml" ]; then
+        EXECUTABLE_SPECIFIC_COMPOSE_FILE="--compose-file=docker-compose-docker.yml"
+    fi
+    
     # Redeploy services
-    ${EXECUTABLE} stack deploy --compose-file=docker-compose-stack.yml --compose-file=docker-compose.yml $DEBUG_COMPOSE_FILE --with-registry-auth "${STACK_NAME}"
+    ${EXECUTABLE} stack deploy --compose-file=docker-compose-stack.yml --compose-file=docker-compose.yml $EXECUTABLE_SPECIFIC_COMPOSE_FILE $DEBUG_COMPOSE_FILE --with-registry-auth "${STACK_NAME}"
 else
     if [[ -n "${DEBUG_PORT}" ]]; then
         export DEBUG_PORT
         DEBUG_COMPOSE_FILE="-f docker-compose-debug.yml"
     fi
-
+    
+    if [ -f "docker-compose-podman.yml" ]; then
+        EXECUTABLE_SPECIFIC_COMPOSE_FILE="-f docker-compose-podman.yml"
+    fi
+    
     # Redeploy services
-    ${COMPOSE_EXECUTABLE} -f docker-compose-stack.yml -f docker-compose.yml $DEBUG_COMPOSE_FILE -p "${STACK_NAME}" up -d
+    ${COMPOSE_EXECUTABLE} -f docker-compose-stack.yml -f docker-compose.yml $EXECUTABLE_SPECIFIC_COMPOSE_FILE $DEBUG_COMPOSE_FILE -p "${STACK_NAME}" up -d
 fi
