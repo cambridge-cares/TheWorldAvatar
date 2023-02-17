360130406d05bebc415391c4f88262a0a3468a23
Mehal Agarwal <99166247+mehalaga@users.noreply.github.com>
2023-02-17 10:46:58 +0800

    dev-marie-and-bert: added OntoAgentReader.py

---
+++ MARIE_AND_BERT/KGToolbox/OntoAgentReader.py
@@ -0,0 +1,214 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+import pandas as pd
+import os
+import sys
+
+sys.path.append("")
+from Marie.CandidateSelection.location import DATA_DIR
+
+class OntoAgentReader:
+
+    def __init__(self, agent, qualifier_exists, namespace):
+        self.agent = agent
+        self.full_dataset_dir = os.path.join(DATA_DIR, "CrossGraph", self.agent)
+        self.qualifier = qualifier_exists
+        self.namespace=namespace
+
+    def query_blazegraph(self, query):
+        sparql = SPARQLWrapper("http://127.0.0.1:9999/blazegraph/namespace/" + self.namespace + "/sparql")
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        return results
+
+    def get_operation_output(self):
+        triples = []
+
+        query = """
+        PREFIX msm: <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#>
+        select DISTINCT ?operation ?msg_part 
+        where { 
+                ?operation rdf:type <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#Operation>.
+                ?operation msm:hasOutput ?msg_content.
+                ?msg_content msm:hasMandatoryPart ?msg_part.
+        }
+        """
+
+        result = self.query_blazegraph(query)["results"]["bindings"]
+        
+        for binding in result:
+            operation = binding["operation"]["value"].split("#")[-1]
+            message_part = binding["msg_part"]["value"].split("#")[-1]
+            row = (operation, "hasOutput", message_part)
+            triples.append(row)
+        
+        return triples
+
+    def get_operation_input(self):
+        triples = []
+
+        query = """
+        PREFIX msm: <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#>
+        select DISTINCT ?operation ?msg_part 
+        where { 
+                ?operation rdf:type <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#Operation>.
+                ?operation msm:hasInput ?msg_content.
+                ?msg_content msm:hasMandatoryPart ?msg_part.
+        }
+        """
+
+        result = self.query_blazegraph(query)["results"]["bindings"]
+        
+        for binding in result:
+            operation = binding["operation"]["value"].split("#")[-1]
+            message_part = binding["msg_part"]["value"].split("#")[-1]
+            row = (operation, "hasInput", message_part)
+            triples.append(row)
+        
+        return triples
+
+    def get_resource_properties(self):
+        triples = []
+
+        query = """
+        PREFIX msm: <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#>
+        select DISTINCT ?msg_part ?class ?name ?label ?isArray
+         where { 
+            ?msg_part rdf:type <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#MessagePart>;
+   	   		msm:hasType ?class;
+    		msm:hasName ?name;
+    		msm:hasNerLabel ?label;
+    		msm:isArray ?isArray.
+        }
+        """
+
+        result = self.query_blazegraph(query)["results"]["bindings"]
+        
+        for binding in result:
+            message_part = binding["msg_part"]["value"].split("#")[-1]
+            resource_class = binding["class"]["value"].split("#")[-1]
+            name = binding["name"]["value"]
+            ner_label = binding["label"]["value"]
+            isArray = binding["isArray"]["value"]
+            class_row = (message_part, "hasType", resource_class)
+            name_row = (message_part, "hasName", name)
+            ner_label_row = (message_part, "hasNerLabel", ner_label)
+            isArray_row = (message_part, "isArray", isArray)
+            
+            # Append each tuple to the list
+            triples.append(class_row)
+            triples.append(name_row)
+            triples.append(ner_label_row)
+            triples.append(isArray_row)
+
+        return triples
+    
+    def get_qualifier_for_output(self):
+        triples = []
+
+        query = """
+        PREFIX msm: <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#>
+        select DISTINCT ?msg_part ?qualifier
+        where { 
+            ?msg_part rdf:type <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#MessagePart>;
+   			msm:hasQualifier ?qualifier;
+        }
+        """
+
+        result = self.query_blazegraph(query)["results"]["bindings"]
+        
+        for binding in result:
+            message_part = binding["msg_part"]["value"].split("#")[-1]
+            qualifier = binding["qualifier"]["value"].split("#")[-1]
+            row = (message_part, "hasQualifier", qualifier)
+            triples.append(row)
+        
+        return triples
+
+
+    def get_operation_question_templates(self):
+        triples = []
+
+        query = """
+        PREFIX msm: <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#>
+        select DISTINCT ?operation ?ques
+        where { 
+            ?operation rdf:type <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#Operation>.
+            ?operation msm:hasQuestionTemplates ?ques
+        }
+        """
+
+        result = self.query_blazegraph(query)["results"]["bindings"]
+        
+        for binding in result:
+            operation = binding["operation"]["value"].split("#")[-1]
+            ques_template = binding["ques"]["value"]
+            row = (operation, "hasQuestionTemplates", ques_template)
+            triples.append(row)
+        
+        return triples
+
+    def get_http_url(self):
+        triples = []
+
+        query = """
+        PREFIX msm: <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#>
+        select DISTINCT ?operation ?url 
+        where { 
+            ?operation rdf:type <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#Operation>.
+            ?operation msm:hasHttpUrl ?url.
+        }
+        """
+
+        result = self.query_blazegraph(query)["results"]["bindings"]
+        
+        for binding in result:
+            operation = binding["operation"]["value"].split("#")[-1]
+            url = binding["url"]["value"]
+            row = (operation, "hasHttpUrl", url)
+            triples.append(row)
+        
+        return triples
+    
+    def get_service(self):
+        triples = []
+
+        query = """
+        PREFIX msm: <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#>
+        select DISTINCT ?service ?operation
+        where { 
+            ?operation rdf:type <http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#Operation>.
+            ?service msm:hasOperation ?operation
+        }
+        """
+
+        result = self.query_blazegraph(query)["results"]["bindings"]
+        
+        for binding in result:
+            service = binding["service"]["value"].split("#")[-1]
+            operation = binding["operation"]["value"].split("#")[-1]
+            row = (service, "hasOperation", operation)
+            triples.append(row)
+        
+        return triples    
+    
+    def run(self):
+        triples=self.get_operation_output()+\
+        self.get_operation_input()+\
+        self.get_resource_properties()+\
+        self.get_service()+\
+        self.get_http_url()+\
+        self.get_operation_question_templates()
+
+        if self.qualifier:
+            triples += self.get_qualifier_for_output()
+
+        df = pd.DataFrame(triples)
+        df.to_csv(os.path.join(self.full_dataset_dir, f"{self.agent}-train.txt"),
+                        sep="\t", header=False, index=False)
+        
+if __name__ == "__main__":
+    reader1 = OntoAgentReader('ontopceagent', False, namespace='ontopceagent')
+    reader2 = OntoAgentReader('ontothermoagent', True, 'ontothermoagent')
+    reader1.run()
+    reader2.run()
