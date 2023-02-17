2d7c0e47c217593363d88d14a7deae09e335ad35
Markus Hofmeister <79832739+markushofmeister@users.noreply.github.com>
2023-02-17 13:47:11 +0800

    Update setup.py
    dev-EPCInstantiationAgent-2: Align version number in setup.py and Docker image

--- Agents/EnergyPerformanceCertificateAgent/setup.py
+++ Agents/EnergyPerformanceCertificateAgent/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='epcdata',
-    version='0.1.0',
+    version='1.0.0',
     author='Markus Hofmeister, Srishti Ganguly',
     author_email='mh807@cam.ac.uk',
     license='MIT',
