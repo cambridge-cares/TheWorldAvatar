9597c0de1b04280923759e66f34c54af07d74e6b
szhang012 <81546703+szhang012@users.noreply.github.com>
2023-02-27 16:25:06 +0800

    Docker: move script.sh to uwsgi predefined prestart.sh

---
+++ MARIE_AND_BERT/prestart.sh
@@ -0,0 +1,22 @@
+mkdir /app/DATA
+
+
+#wget http://159.223.42.53:8080/CrossGraph.zip && unzip CrossGraph.zip -d /app/DATA/ && rm CrossGraph.zip
+#wget http://159.223.42.53:8080/Evaluation.zip && unzip Evaluation.zip -d /app/DATA/ && rm Evaluation.zip
+#wget http://159.223.42.53:8080/EntityLinking.zip && unzip EntityLinking.zip -d /app/DATA/ && rm EntityLinking.zip
+#wget http://159.223.42.53:8080/bert_pretrained.zip && unzip bert_pretrained.zip -d /app/ && rm bert_pretrained.zip
+
+#wget http://159.223.42.53:8080/Dictionaries.zip && unzip Dictionaries.zip -d /app/DATA/ && rm Dictionaries.zip
+#wget http://159.223.42.53:8080/cde_models.zip
+
+wget http://www.theworldavatar.com/MARIE_DEPLOYMENT/CrossGraph.zip && unzip CrossGraph.zip -d /app/DATA/ && rm CrossGraph.zip
+wget http://www.theworldavatar.com/MARIE_DEPLOYMENT/Evaluation.zip && unzip Evaluation.zip -d /app/DATA/ && rm Evaluation.zip
+wget http://www.theworldavatar.com/MARIE_DEPLOYMENT/EntityLinking.zip && unzip EntityLinking.zip -d /app/DATA/ && rm EntityLinking.zip
+wget http://www.theworldavatar.com/MARIE_DEPLOYMENT/bert_pretrained.zip && unzip bert_pretrained.zip -d /app/ && rm bert_pretrained.zip
+wget http://www.theworldavatar.com/MARIE_DEPLOYMENT/Dictionaries.zip && unzip Dictionaries.zip -d /app/DATA/ && rm Dictionaries.zip
+wget http://www.theworldavatar.com/MARIE_DEPLOYMENT/cde_models.zip
+
+mkdir -p /root/.local/share/ChemDataExtractor/
+unzip cde_models.zip -d /root/.local/share/ChemDataExtractor/
+rm cde_models.zip
+pip --default-timeout=10000 install --no-cache-dir -r requirements_linux.txt
