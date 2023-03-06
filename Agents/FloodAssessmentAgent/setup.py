dcaa7b0862cedec51f9953b823d43bfb28cb02bf
Markus Hofmeister <mh807@cam.ac.uk>
2023-03-06 08:56:10 +0800

    dev-update-flood-assessment-agent: Update py4jps and pyderivationagent versions

--- Agents/FloodAssessmentAgent/setup.py
+++ Agents/FloodAssessmentAgent/setup.py
@@ -17,9 +17,9 @@ setup(
         'flask~=2.1.0',
         'JayDeBeApi~=1.2.3',
         'pandas~=1.5.1',
-        'py4jps~=1.0.30',
+        'py4jps~=1.0.34',
         'requests~=2.28.1',
-        'pyderivationagent~=1.4.1'
+        'pyderivationagent~=1.4.4'
     ],
     extras_require={
         "dev": [
