51d12a76fcde03fd43e2a1c993e04e7361e65d3d
cmclscourtney <scourtney@cmclinnovations.com>
2023-03-07 14:34:57 +0000

    646-add-externally-accessible-service-type-to-stack-manager: extracted nginx settings in feature info agent to method in nginx service So can be usable by other services

--- Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/stack/services/FeatureInfoAgentService.java
+++ Deploy/stacks/dynamic/stack-manager/src/main/java/com/cmclinnovations/stack/services/FeatureInfoAgentService.java
@@ -6,8 +6,6 @@ import java.util.Map.Entry;
 import com.cmclinnovations.stack.services.config.Connection;
 import com.cmclinnovations.stack.services.config.ServiceConfig;
 import com.github.odiszapc.nginxparser.NgxBlock;
-import com.github.odiszapc.nginxparser.NgxComment;
-import com.github.odiszapc.nginxparser.NgxParam;
 
 public class FeatureInfoAgentService extends ContainerService {
 
@@ -20,21 +18,6 @@ public class FeatureInfoAgentService extends ContainerService {
     @Override
     public void addServerSpecificNginxSettingsToLocationBlock(NgxBlock locationBlock, Map<String, String> upstreams,
             Entry<String, Connection> endpoint) {
-
-        NgxComment corsFixComment = new NgxComment("# Necessary to fix CORS problem");
-        locationBlock.addEntry(corsFixComment);
-
-        NgxParam allowOriginHeaderParam = new NgxParam();
-        allowOriginHeaderParam.addValue("add_header");
-        allowOriginHeaderParam.addValue("'Access-Control-Allow-Origin'");
-        allowOriginHeaderParam.addValue("'*'");
-        allowOriginHeaderParam.addValue("always");
-        locationBlock.addEntry(allowOriginHeaderParam);
-
-        NgxParam allowMethodsHeaderParam = new NgxParam();
-        allowMethodsHeaderParam.addValue("add_header");
-        allowMethodsHeaderParam.addValue("'Access-Control-Allow-Methods'");
-        allowMethodsHeaderParam.addValue("'GET'");
-        locationBlock.addEntry(allowMethodsHeaderParam);
+        NginxService.addOpenCORSForGetNginxSettings(locationBlock);
     }
 }
