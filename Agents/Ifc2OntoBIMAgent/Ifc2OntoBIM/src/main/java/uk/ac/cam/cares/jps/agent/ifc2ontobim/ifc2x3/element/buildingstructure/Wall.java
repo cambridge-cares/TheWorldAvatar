273907cb6d689ff5212cf159d8a5390c59bab5be
qhouyee <quekhouyee@gmail.com>
2023-03-09 17:10:31 +0800

    dev-update-Ifc2OntoBIM-agent: Edited comments for the new classes

--- Agents/Ifc2OntoBIMAgent/Ifc2OntoBIM/src/main/java/uk/ac/cam/cares/jps/agent/ifc2ontobim/ifc2x3/element/buildingstructure/Wall.java
+++ Agents/Ifc2OntoBIMAgent/Ifc2OntoBIM/src/main/java/uk/ac/cam/cares/jps/agent/ifc2ontobim/ifc2x3/element/buildingstructure/Wall.java
@@ -25,7 +25,9 @@ public class Wall extends IfcModelRepresentation {
      * @param iri          The instance IRI to be created.
      * @param name         The name of this IFC object.
      * @param uid          The IFC uid generated for this object.
-     * @param placementIri The local placement IRI for the zone's position.
+     * @param placementIri The local placement IRI for the element's position.
+     * @param hostZoneIri  The IRI of the zone containing this element .
+     * @param geomRepIri   The IRI of the element's geometry representation.
      */
     public Wall(String iri, String name, String uid, String placementIri, String hostZoneIri, String geomRepIri) {
         super(iri, name, uid, placementIri);
@@ -42,7 +44,7 @@ public class Wall extends IfcModelRepresentation {
     @Override
     public void constructStatements(LinkedHashSet<Statement> statementSet) {
         super.addIfcModelRepresentationStatements(statementSet);
-        StatementHandler.addStatement(statementSet, this.hostZoneIRI, OntoBimConstant.BOT_CONTAINS_ELEMENT,  this.elementIRI);
+        StatementHandler.addStatement(statementSet, this.hostZoneIRI, OntoBimConstant.BOT_CONTAINS_ELEMENT, this.elementIRI);
         StatementHandler.addStatement(statementSet, this.elementIRI, OntoBimConstant.RDF_TYPE, OntoBimConstant.BIM_WALL_CLASS);
         StatementHandler.addStatement(statementSet, this.elementIRI, OntoBimConstant.BIM_HAS_IFC_REPRESENTATION, this.getIfcRepIri());
         StatementHandler.addStatement(statementSet, this.elementIRI, OntoBimConstant.BIM_HAS_GEOM_REP, this.geomRepIRI);
