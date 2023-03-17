e9071ebe6ba49a7f9cdcf8a424a63569ae10758f
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-03-17 15:36:30 +0000

    582-add-dcat-concepts-in-data-uploader: Removed duplicated stack name from Ontop endpoint config names.

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/OntopService.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/OntopService.java
@@ -44,9 +44,8 @@ public final class OntopService extends ContainerService {
     public OntopService(String stackName, ServiceManager serviceManager, ServiceConfig config) {
         super(stackName, serviceManager, config);
 
-        endpointConfig = new OntopEndpointConfig(
-                config.getName(), getHostName(), DEFAULT_PORT,
-                "", null);
+        endpointConfig = new OntopEndpointConfig(StackClient.removeStackName(config.getName()),
+                getHostName(), DEFAULT_PORT, "", null);
     }
 
     @Override
