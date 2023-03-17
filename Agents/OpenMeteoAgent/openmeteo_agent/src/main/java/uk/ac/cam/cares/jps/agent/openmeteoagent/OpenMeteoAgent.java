19870aec59f0c447e59cf35c7712912fcc9362ee
Yi-Kai Tsai <yikai.tsai@cares.cam.ac.uk>
2023-03-17 17:11:05 +0800

    1515-dev-openmeteoagent: add validation of incoming request parameters

--- Agents/OpenMeteoAgent/openmeteo_agent/src/main/java/uk/ac/cam/cares/jps/agent/openmeteoagent/OpenMeteoAgent.java
+++ Agents/OpenMeteoAgent/openmeteo_agent/src/main/java/uk/ac/cam/cares/jps/agent/openmeteoagent/OpenMeteoAgent.java
@@ -2,6 +2,8 @@ package uk.ac.cam.cares.jps.agent.openmeteoagent;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.BadRequestException;
+
 import org.json.JSONObject;
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
@@ -12,7 +14,11 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @WebServlet(
@@ -55,12 +61,78 @@ public class OpenMeteoAgent extends JPSAgent {
 
     @Override
     public JSONObject processRequestParameters(JSONObject requestParams) {
-        latitude = requestParams.getString(KEY_LAT);
-        longitude = requestParams.getString(KEY_LONG);
-        getWeatherData(latitude, longitude, requestParams.getString(KEY_START), requestParams.getString(KEY_END));
+        if (validateInput(requestParams)) {
+            latitude = requestParams.getString(KEY_LAT);
+            longitude = requestParams.getString(KEY_LONG);
+            JSONObject weatherData = getWeatherData(latitude, longitude, requestParams.getString(KEY_START), requestParams.getString(KEY_END));
+        }
         return requestParams;
     }
 
+    /**
+     * Checks validity of incoming request
+     * @param requestParams Request parameters as JSONObject
+     * @return Validity of request
+     */
+    @Override
+    public boolean validateInput(JSONObject requestParams) {
+        boolean validate;
+
+        try{
+            // check latitude and longitude are provided, and are numbers
+            validate = !requestParams.get(KEY_LAT).equals(null) && ! requestParams.get(KEY_LONG).equals(null)
+                    && !(requestParams.get(KEY_LAT) instanceof String) && !(requestParams.get(KEY_LONG) instanceof String);
+
+            if (requestParams.getString("requestUrl").contains(URI_INSTANTIATE)){
+                validate = !requestParams.getString(KEY_START).isEmpty() && !requestParams.getString(KEY_END).isEmpty()
+                        && validateDate(requestParams.getString(KEY_START))
+                        && validateDate(requestParams.getString(KEY_END));
+
+                SimpleDateFormat ymd = new SimpleDateFormat("yyyy-MM-dd");
+                Date start = ymd.parse(requestParams.getString(KEY_START));
+                Date end = ymd.parse(requestParams.getString(KEY_END));
+                Date now = new Date();
+
+                // start date cannot be later than end date, and end date cannot be later than the date at the time of the incoming request
+                validate = validate && start.before(end)
+                        && (end.before(now) || end.equals(now));
+            }
+
+        }
+        catch (Exception e){
+            throw new BadRequestException();
+        }
+
+        if (!validate){
+            throw new BadRequestException();
+        }
+
+        return true;
+    }
+
+    /**
+     * Validate whether the string is in the yyyy-mm-dd date format
+     * @param date The date as a string
+     * @return Validity of the format of date
+     */
+    public boolean validateDate(String date){
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            Date dateCheck = df.parse(date);
+            return df.format(dateCheck).equals(date);
+        } catch (ParseException e) {
+            throw new BadRequestException();
+        }
+    }
+
+    /**
+     * Calls the Open-Meteo Historical Weather API to retrieve the hourly weather data from start_date to end_date with weather parameter in API_PARAMETERS for weather station located at (latitude, longitude)
+     * @param latitude Geographical WGS84 coordinate of the weather station
+     * @param longitude Geographical WGS84 coordinate of the weather station
+     * @param startDate Start date of the time interval, in yyyy-mm-dd format
+     * @param endDate End date of the time interval, in yyyy-mm-dd format
+     * @return Weather data as a JSONObject
+     */
     public JSONObject getWeatherData(String latitude, String longitude, String startDate, String endDate) {
         String query = KEY_LAT + "=" + latitude + "&";
         query = query + KEY_LONG + "=" + longitude + "&";
