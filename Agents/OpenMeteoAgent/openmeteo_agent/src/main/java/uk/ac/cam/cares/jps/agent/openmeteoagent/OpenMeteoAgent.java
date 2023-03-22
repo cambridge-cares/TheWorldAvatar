ad2d256713d01a7ecfd9d1f45fb3bfdc929a896d
Yi-Kai Tsai <yikai.tsai@cares.cam.ac.uk>
2023-03-22 17:55:59 +0800

    1515-dev-openmeteoagent: add methods to parse API response

--- Agents/OpenMeteoAgent/openmeteo_agent/src/main/java/uk/ac/cam/cares/jps/agent/openmeteoagent/OpenMeteoAgent.java
+++ Agents/OpenMeteoAgent/openmeteo_agent/src/main/java/uk/ac/cam/cares/jps/agent/openmeteoagent/OpenMeteoAgent.java
@@ -4,6 +4,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BadRequestException;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
@@ -12,14 +15,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @WebServlet(
         urlPatterns = {
@@ -31,23 +35,43 @@ public class OpenMeteoAgent extends JPSAgent {
     private static final String KEY_LONG = "longitude";
     private static final String KEY_START = "start_date";
     private static final String KEY_END = "end_date";
-    private static final String KEY_HOURLY = "hourly";
 
     private static final String API_URL = "https://archive-api.open-meteo.com/v1/archive";
+    private static final String API_HOURLY = "hourly";
+    private static final String API_HOURLY_UNITS = "hourly_units";
     private static final String API_PARAMETER_TEMP = "temperature_2m";
+    private static final String ONTOEMS_TEMP = "AirTemperature";
     private static final String API_PARAMETER_HUMIDITY = "relativehumidity_2m";
+    private static final String ONTOEMS_HUMIDITY = "RelativeHumidity";
     private static final String API_PARAMETER_DEWPOINT = "dewpoint_2m";
+    private static final String ONTOEMS_DEWPOINT = "DewPoint";
     private static final String API_PARAMETER_PRESSURE = "surface_pressure";
+    private static final String ONTOEMS_PRESSURE = "AtmosphericPressure";
     private static final String API_PARAMETER_RAIN = "rain";
+    private static final String ONTOEMS_RAIN = "Rainfall";
     private static final String API_PARAMETER_SNOW = "snowfall";
+    private static final String ONTOEMS_SNOW = "Snowfall";
     private static final String API_PARAMETER_CLOUD = "cloudcover";
+    private static final String ONTOEMS_CLOUD = "CloudCover";
     private static final String API_PARAMETER_DNI = "direct_normal_irradiance";
+    private static final String ONTOEMS_DNI = "DirectNormalIrradiance";
     private static final String API_PARAMETER_DHI = "diffuse_radiation";
+    private static final String ONTOEMS_DHI = "DiffuseHorizontalIrradiance";
     private static final String API_PARAMETER_WINDSPEED = "windspeed_10m";
+    private static final String ONTOEMS_WINDSPEED = "WindSpeed";
     private static final String API_PARAMETER_WINDDIRECTION = "winddirection_10m";
-
+    private static final String ONTOEMS_WINDDIRECTION = "WindDirection";
+    private static final String API_WINDSPEED_UNIT = "windspeed_unit=ms";
+    private static final String OM_C = "degreeCelsius";
+    private static final String OM_PA = "pascal";
+    private static final String OM_MM = "millimetre";
+    private static final String OM_CM = "centimetre";
+    private static final String OM_MS = "metrePerSecond-Time";
+    private static final String OM_DEGREE = "degree";
     private List<String> API_PARAMETERS = Arrays.asList(API_PARAMETER_TEMP, API_PARAMETER_HUMIDITY, API_PARAMETER_DEWPOINT, API_PARAMETER_PRESSURE, API_PARAMETER_RAIN, API_PARAMETER_SNOW, API_PARAMETER_CLOUD, API_PARAMETER_DNI, API_PARAMETER_DHI, API_PARAMETER_WINDSPEED, API_PARAMETER_WINDDIRECTION);
-
+    private List<String> ONTOEMS_CONCEPTS = Arrays.asList(ONTOEMS_TEMP, ONTOEMS_HUMIDITY, ONTOEMS_DEWPOINT, ONTOEMS_PRESSURE, ONTOEMS_RAIN, ONTOEMS_SNOW, ONTOEMS_CLOUD, ONTOEMS_DNI, ONTOEMS_DHI, ONTOEMS_WINDSPEED, ONTOEMS_WINDDIRECTION);
+    private Map<String, String> API_ONTOEMS= IntStream.range(0, API_PARAMETERS.size()).boxed()
+            .collect(Collectors.toMap(API_PARAMETERS::get, ONTOEMS_CONCEPTS::get));
     private Double latitude;
     private Double longitude;
 
@@ -64,7 +88,12 @@ public class OpenMeteoAgent extends JPSAgent {
         if (validateInput(requestParams)) {
             latitude = requestParams.getDouble(KEY_LAT);
             longitude = requestParams.getDouble(KEY_LONG);
-            JSONObject weatherData = getWeatherData(latitude, longitude, requestParams.getString(KEY_START), requestParams.getString(KEY_END));
+            JSONObject response = getWeatherData(latitude, longitude, requestParams.getString(KEY_START), requestParams.getString(KEY_END));
+
+            JSONObject weatherData = response.getJSONObject(API_HOURLY);
+            JSONObject weatherUnit = response.getJSONObject(API_HOURLY_UNITS);
+
+            Map<String, List<Object>> parsedData = parseWeatherData(weatherData, weatherUnit);
         }
         return requestParams;
     }
@@ -137,7 +166,7 @@ public class OpenMeteoAgent extends JPSAgent {
         String query = KEY_LAT + "=" + latitude + "&";
         query = query + KEY_LONG + "=" + longitude + "&";
         query = query + KEY_START + "=" + startDate + "&";
-        query = query + KEY_END + "=" + endDate + "&" + KEY_HOURLY + "=";
+        query = query + KEY_END + "=" + endDate + "&" + API_WINDSPEED_UNIT +"&" + API_HOURLY + "=";
 
         for (String parameter: API_PARAMETERS){
             query = query + parameter + ",";
@@ -164,5 +193,49 @@ public class OpenMeteoAgent extends JPSAgent {
         }
     }
 
+    /**
+     * Parses weatherData into list
+     * @param weatherData JSONObject containing weather data retrieved by the API
+     * @param weatherUnit JSONObject containing the units of the weather data retrieved by the API
+     * @return a Map object with the key being the API weather parameters, and the values being the corresponding units ontology and the corresponding data parsed to a list
+     */
+    public  Map<String, List<Object>> parseWeatherData(JSONObject weatherData, JSONObject weatherUnit){
+        Map<String, List<Object>> data = new HashMap<>();
+        List<Object> temp;
+        JSONArray tempJSONArray;
+        Type tempType;
+        for (String parameter: API_PARAMETERS){
+            temp = new ArrayList<>();
+            temp.add(getUnitOntology(weatherUnit.getString(parameter)));
+            tempJSONArray = weatherData.getJSONArray(parameter);
+            tempType = new TypeToken<List<Double>>() {}.getType();
+            temp.add(new Gson().fromJson(tempJSONArray.toString(), tempType));
+            data.put(parameter, temp);
+        }
 
+        return data;
+    }
+
+    /**
+     * Converts the unit text string to its representative ontology concept name
+     * @param unit the unit text string
+     * @return the unit ontology concept name
+     */
+    public String getUnitOntology(String unit){
+        if (unit.contains("°C")){return OM_C;}
+        if (unit.contains("°")){return OM_DEGREE;}
+
+        switch (unit){
+            case("mm"):
+                return OM_MM;
+            case("hPa"):
+                return OM_PA;
+            case("cm"):
+                return OM_CM;
+            case("m/s"):
+                return OM_MS;
+            default:
+                return "";
+        }
+    }
 }
