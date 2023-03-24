2667dccd233d3f189c5d1f1c4df929682a85356b
ctlao <ctlao@cmclinnovations.com>
2023-03-24 14:42:03 +0000

    Revert "dev-virtual-sernsor-generic-vis-container: use generic visualisation container instead of specific visualistion container for virtual sensor use case."
    This reverts commit e2cdaa7cfb3ac6c12464c147ea43f730aaf258d8.
    
    # Conflicts:
    #	JPS_VIRTUALSENSOR/AermodAgent/docker-compose.yml

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -6,5 +6,5 @@
 (cd PythonService && ./stack.sh start ship-stack)
 (cd DispersionInteractor/ && ./stack.sh start ship-stack)
 (cd AermodAgent && ./stack.sh start ship-stack)
+(cd DispersionVis && ./stack.sh start ship-stack)
 (cd ../Deploy/stacks/db/fileserver/ && ./stack.sh start ship-stack)
-(cd DispersionVis && ./copy_vis_file.sh)
