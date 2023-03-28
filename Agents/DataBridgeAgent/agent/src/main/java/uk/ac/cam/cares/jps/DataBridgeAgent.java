009c397caffda7890679b76dd1cc4d4c367721ba
qhouyee <quekhouyee@gmail.com>
2023-03-28 10:22:05 +0800

    dev-data-bridge-agent: Added default agent code and configuration

---
+++ Agents/DataBridgeAgent/agent/src/main/java/uk/ac/cam/cares/jps/DataBridgeAgent.java
@@ -0,0 +1,111 @@
+package uk.ac.cam.cares.jps;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+import uk.ac.cam.cares.jps.base.agent.JPSAgent;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * This class acts as the entry point of the agent that accepts parameter requests to specific routes and achieve its task.
+ *
+ * @author qhouyee
+ */
+@WebServlet(urlPatterns = {"/transfer", "/status"})
+public class DataBridgeAgent extends JPSAgent {
+    private static final Logger LOGGER = LogManager.getLogger(DataBridgeAgent.class);
+    // Agent starts off in valid state, and will be invalid when running into exceptions
+    private static boolean VALID = true;
+    private static final String INVALID_ROUTE_ERROR_MSG = "Invalid request type! Route ";
+
+    /**
+     * Perform required setup.
+     */
+    @Override
+    public synchronized void init() {
+        try {
+            super.init();
+            // Ensure logging are properly working
+            LOGGER.debug("This is a test DEBUG message");
+            LOGGER.info("This is a test INFO message");
+            LOGGER.warn("This is a test WARN message");
+            LOGGER.error("This is a test ERROR message");
+            LOGGER.fatal("This is a test FATAL message");
+        } catch (Exception exception) {
+            DataBridgeAgent.VALID = false;
+            LOGGER.error("Could not initialise an agent instance!", exception);
+        }
+    }
+
+    /**
+     * An overloaded method to process all the different HTTP (GET/POST/PULL..) requests.
+     * Do note all requests to JPS agents are processed similarly and will only return response objects.
+     *
+     * @return A response to the request called as a JSON Object.
+     */
+    @Override
+    public JSONObject processRequestParameters(JSONObject requestParams, HttpServletRequest request) {
+        return processRequestParameters(requestParams);
+    }
+
+    /**
+     * A method that process all the different HTTP (GET/POST/PULL..) requests.
+     * This will validate the incoming request type and parameters against their route options.
+     *
+     * @return A response to the request called as a JSON Object.
+     */
+    @Override
+    public JSONObject processRequestParameters(JSONObject requestParams) {
+        JSONObject jsonMessage = new JSONObject();
+        // Retrieve the request type and route
+        String requestType = requestParams.get("method").toString();
+        String route = requestParams.get("requestUrl").toString();
+        // Retrieve the route name
+        route = route.substring(route.lastIndexOf("/") + 1);
+        LOGGER.info("Passing request to the DataBridge Agent...");
+        // Run logic based on request path
+        switch (route) {
+            case "transfer":
+                LOGGER.fatal("Route is under construction.");
+                jsonMessage.put("Result", "Route is under construction.");
+                break;
+            case "status":
+                if (requestType.equals("GET")) {
+                    jsonMessage = statusRoute();
+                } else {
+                    LOGGER.fatal(INVALID_ROUTE_ERROR_MSG + route + " can only accept GET request.");
+                    jsonMessage.put("Result", INVALID_ROUTE_ERROR_MSG + route + " can only accept GET request.");
+                }
+                break;
+        }
+        return jsonMessage;
+    }
+
+    /**
+     * Validates the request parameter.
+     *
+     * @return true or false depending on valid parameter status.
+     */
+    @Override
+    public boolean validateInput(JSONObject requestParams) {
+        return true;
+    }
+
+    /**
+     * Run logic for the "/status" route that indicates the agent's current status.
+     *
+     * @return A response to the request called as a JSON Object.
+     */
+    protected JSONObject statusRoute() {
+        JSONObject response = new JSONObject();
+        LOGGER.info("Detected request to get agent status...");
+        if (DataBridgeAgent.VALID) {
+            response.put("Result", "Agent is ready to receive requests.");
+        } else {
+            response.put("Result", "Agent could not be initialise!");
+        }
+        return response;
+    }
+}
