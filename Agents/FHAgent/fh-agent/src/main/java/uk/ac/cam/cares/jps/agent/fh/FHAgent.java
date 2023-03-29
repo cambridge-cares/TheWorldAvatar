dfda09df7179daa606e6c06c1b10197490965a75
Michael-TeguhLaksana <mtl41@cam.ac.uk>
2023-03-29 17:04:59 +0800

    dev-1505-proximity-sensor-for-lab_FHAgent: Added logger

--- Agents/FHAgent/fh-agent/src/main/java/uk/ac/cam/cares/jps/agent/fh/FHAgent.java
+++ Agents/FHAgent/fh-agent/src/main/java/uk/ac/cam/cares/jps/agent/fh/FHAgent.java
@@ -1,7 +1,21 @@
 package uk.ac.cam.cares.jps.agent.fh;
 
 
-public class FHAgent {
 
+
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/*
+ * Class for hndling distance timeseries and determine FH occupancy
+ * Determine occupiedState, instantiate it in the KB, and toggle the FH 
+ * 
+ * @author Michael Teguh Laksana
+ */
+
+public class FHAgent {
+    private static final Logger LOGGER = LogManager.getLogger(FHAgentLauncher.class);
     
 }
--- Agents/FHAgent/fh-agent/src/main/java/uk/ac/cam/cares/jps/agent/fh/FHAgentAPIConnector.java
+++ Agents/FHAgent/fh-agent/src/main/java/uk/ac/cam/cares/jps/agent/fh/FHAgentAPIConnector.java
@@ -1,5 +1,13 @@
 package uk.ac.cam.cares.jps.agent.fh;
 
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class FHAgentAPIConnector {
-    
+    /**
+     * Logger for reporting info/errors.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(FHAgentLauncher.class);
 }
