0f2ec4c782c9f78dcf6ebdc5649f1078278ab395
ctlao <ctlao@cmclinnovations.com>
2023-03-31 12:22:17 +0100

    dev-stack-manager-for-virtual-sensor: start python service with stack manager.

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -3,6 +3,5 @@
 (cd ../Agents/WeatherAgent/ && ./stack.sh start ship-stack)
 (cd ShipInputAgent/ && ./stack.sh start ship-stack)
 (cd EmissionsAgent/ && ./stack.sh start ship-stack)
-(cd PythonService && ./stack.sh start ship-stack)
 (cd ../Deploy/stacks/db/fileserver/ && ./stack.sh start ship-stack)
 (cd DispersionVis && ./copy_vis_file.sh)
