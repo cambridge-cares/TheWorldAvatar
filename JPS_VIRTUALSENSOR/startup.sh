ea790c30d3fe52c00634980adec646625d598f38
ctlao <ctlao@cmclinnovations.com>
2023-03-31 15:10:30 +0100

    dev-stack-manager-for-virtual-sensor: all containers for virtual sensor are now started by the stack manager.

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # starts up all required components of virtual sensor
-(cd ../Agents/WeatherAgent/ && ./stack.sh start ship-stack)
-(cd ../Deploy/stacks/db/fileserver/ && ./stack.sh start ship-stack)
 (cd DispersionVis && ./copy_vis_file.sh)
 (cd ShipInputAgent && ./copy_ship_file.sh)
