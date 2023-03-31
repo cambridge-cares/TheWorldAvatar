395cd3e65e66e83fe08432fd2b3acf002ba0d2d7
ctlao <ctlao@cmclinnovations.com>
2023-03-31 10:21:03 +0100

    dev-stack-manager-for-virtual-sensor: temporary update of dock-compose.yml. Expect to remove them later once everything is managed via the stack manager.

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -4,8 +4,5 @@
 (cd ShipInputAgent/ && ./stack.sh start ship-stack)
 (cd EmissionsAgent/ && ./stack.sh start ship-stack)
 (cd PythonService && ./stack.sh start ship-stack)
-(cd DispersionInteractor/ && ./stack.sh start ship-stack)
-(cd AermodAgent && ./stack.sh start ship-stack)
-(cd DispersionVis && ./stack.sh start ship-stack)
 (cd ../Deploy/stacks/db/fileserver/ && ./stack.sh start ship-stack)
 (cd DispersionVis && ./copy_vis_file.sh)
