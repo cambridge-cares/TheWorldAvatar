797bb2a13a39c97c6f866d43da94f542a729070a
sandradeng20 <xsd20@cam.ac.uk>
2023-04-06 17:03:05 +0800

    1517-dev-android-status-agent: add test cases

---
+++ Agents/AndroidStatusAgent/AndroidStatusAgent/src/test/java/uk/ac/cam/cares/jps/agent/androidstatus/AndroidStatusAgentTest.java
@@ -0,0 +1,82 @@
+package uk.ac.cam.cares.jps.agent.androidstatus;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static org.junit.Assert.assertEquals;
+
+public class AndroidStatusAgentTest {
+
+    private static final Logger LOGGER = LogManager.getLogger(AndroidStatusAgentTest.class);
+    private AndroidStatusAgent agent;
+
+    @Before
+    public void setup() {
+        agent = new AndroidStatusAgent();
+    }
+
+    @Test
+    public void testDoGet_EmptyOutput() throws IOException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        when(request.getRequestURI()).thenReturn("http://localhost:3838/android-status-agent/get");
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+
+        agent.doGet(request, response);
+        JSONObject result = new JSONObject(stringWriter.toString());
+        assertEquals("", result.getString("equipmentIRI"));
+    }
+
+    @Test
+    public void testDoPost_DoGet_NormalOutput() throws IOException, ServletException {
+        HttpServletRequest setRequest = mock(HttpServletRequest.class);
+        HttpServletResponse setResponse = mock(HttpServletResponse.class);
+        when(setRequest.getRequestURI()).thenReturn("http://localhost:3838/android-status-agent/set?equipmentIRI=http://www.example.com/test-element");
+        when(setRequest.getParameter("equipmentIRI")).thenReturn("http://www.example.com/test-element");
+
+        agent.doPost(setRequest, setResponse);
+
+        HttpServletRequest getRequest = mock(HttpServletRequest.class);
+        HttpServletResponse getResponse = mock(HttpServletResponse.class);
+        when(getRequest.getRequestURI()).thenReturn("http://localhost:3838/android-status-agent/get");
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(getResponse.getWriter()).thenReturn(writer);
+
+        agent.doGet(getRequest, getResponse);
+        JSONObject result = new JSONObject(stringWriter.toString());
+        assertEquals("http://www.example.com/test-element", result.getString("equipmentIRI"));
+    }
+
+    @Test
+    public void testDoPost_EmptyValue() throws ServletException, IOException {
+        HttpServletRequest setRequest = mock(HttpServletRequest.class);
+        HttpServletResponse setResponse = mock(HttpServletResponse.class);
+        when(setRequest.getRequestURI()).thenReturn("http://localhost:3838/android-status-agent/set?equipmentIRI=");
+        when(setRequest.getParameter("equipmentIRI")).thenReturn("");
+
+        try {
+            agent.doPost(setRequest, setResponse);
+        } catch (JPSRuntimeException e) {
+            assertEquals("Unable to validate request sent to the agent.", e.getMessage());
+        }
+
+    }
+}
