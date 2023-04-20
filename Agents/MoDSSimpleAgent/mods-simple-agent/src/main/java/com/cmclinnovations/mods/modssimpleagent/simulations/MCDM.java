823445b59f584b9e5c080f9f185a1aef36872c7a
Seán Courtney <scourtney@cmclinnovations.com>
2023-04-20 14:56:09 +0100

    711-modssimpleagent-add-mcdm-only-functionality: generate files for mcdm

--- Agents/MoDSSimpleAgent/mods-simple-agent/src/main/java/com/cmclinnovations/mods/modssimpleagent/simulations/MCDM.java
+++ Agents/MoDSSimpleAgent/mods-simple-agent/src/main/java/com/cmclinnovations/mods/modssimpleagent/simulations/MCDM.java
@@ -1,12 +1,14 @@
 package com.cmclinnovations.mods.modssimpleagent.simulations;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.cmclinnovations.mods.modssimpleagent.BackendInputFile;
 import com.cmclinnovations.mods.modssimpleagent.FileGenerator.FileGenerationException;
 import com.cmclinnovations.mods.modssimpleagent.MoDSBackend;
 import com.cmclinnovations.mods.modssimpleagent.datamodels.InputMetaData;
 import com.cmclinnovations.mods.modssimpleagent.datamodels.Request;
+import com.cmclinnovations.mods.modssimpleagent.datamodels.Variable;
 
 public class MCDM extends Simulation {
 
@@ -22,8 +24,16 @@ public class MCDM extends Simulation {
     }
 
     @Override
+    protected void populateAlgorithmNodes(List<Variable> variables) {
+        populateMOOAlgorithmNode(Simulation.DEFAULT_MOO_ALGORITHM_NAME, getPrimaryAlgorithm().getName(),
+                variables);
+        super.populateAlgorithmNodes(variables);
+    }
+
+    @Override
     protected void generateFiles() throws FileGenerationException {
-        generateDataAlgFiles();
+        generateMOOAlgDataFiles();
+        generateInitialFileFromInputs();
         super.generateFiles();
     }
 
