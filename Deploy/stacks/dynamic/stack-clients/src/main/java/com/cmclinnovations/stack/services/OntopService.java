e53725fd36ba91d7eabe492fc522f89142a5912a
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-04-27 12:44:02 +0100

    dev-ontop-per-db: Added a JDBC driver volume that providers can copy their driver into an then it can be mounted in the consuming containers. This is a change from the Ontop Service doing this via a temporary copy of the driver.
    (cherry picked from commit 06e2f2d6887c55e190b2be8c06cce0f295b7c250)

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/OntopService.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/OntopService.java
@@ -1,15 +1,10 @@
 package com.cmclinnovations.stack.services;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 import com.cmclinnovations.stack.clients.core.StackClient;
 import com.cmclinnovations.stack.clients.ontop.OntopClient;
@@ -31,7 +26,6 @@ public final class OntopService extends ContainerService {
 
     private static final String ONTOP_DB_URL = "ONTOP_DB_URL";
     private static final String ONTOP_DB_DRIVER = "ONTOP_DB_DRIVER";
-    private static final String ONTOP_DB_DRIVER_URL = "ONTOP_DB_DRIVER_URL";
     private static final String ONTOP_DB_USER = "ONTOP_DB_USER";
     private static final String ONTOP_DB_PASSWORD_FILE = "ONTOP_DB_PASSWORD_FILE";
 
@@ -51,24 +45,6 @@ public final class OntopService extends ContainerService {
     @Override
     public void doPreStartUpConfiguration() {
 
-        // Copy the PostgreSQL driver library from the stack-manager into the scratch
-        // volume so that the Ontop container can copy it
-        Pattern postgresqlDriverPattern = Pattern.compile(".*/postgresql-[0-9.]*\\.jar");
-        try (Stream<Path> postgresqlDrivers = Files.find(Path.of("/app/lib/org/postgresql/postgresql/"), 2, (path,
-                attributes) -> postgresqlDriverPattern.matcher(path.toString()).matches())) {
-            Optional<Path> possiblePostgresqlDriver = postgresqlDrivers.findFirst();
-            if (possiblePostgresqlDriver.isPresent()) {
-                Path postgresqlDriver = possiblePostgresqlDriver.get();
-                postgresqlDriverScratchPath = Path.of(StackClient.SCRATCH_DIR)
-                        .resolve(postgresqlDriver.getFileName());
-                Files.copy(postgresqlDriver, postgresqlDriverScratchPath, StandardCopyOption.REPLACE_EXISTING);
-                setEnvironmentVariableIfAbsent(ONTOP_DB_DRIVER_URL, postgresqlDriverScratchPath.toString());
-            }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
         ContainerSpec containerSpec = getContainerSpec();
 
         Optional<ContainerSpecConfig> dbConfigRef = containerSpec.getConfigs().stream().findFirst();
@@ -78,13 +54,11 @@ public final class OntopService extends ContainerService {
             String databaseName = getEnvironmentVariable(ONTOP_DB_NAME);
             setEnvironmentVariableIfAbsent(ONTOP_DB_URL, postgreSQLEndpoint.getJdbcURL(databaseName));
             setEnvironmentVariableIfAbsent(ONTOP_DB_DRIVER, postgreSQLEndpoint.getJdbcDriver());
-            setEnvironmentVariableIfAbsent(ONTOP_DB_DRIVER_URL, postgreSQLEndpoint.getJdbcDriverURL());
             setEnvironmentVariableIfAbsent(ONTOP_DB_USER, postgreSQLEndpoint.getUsername());
             setEnvironmentVariableIfAbsent(ONTOP_DB_PASSWORD_FILE, postgreSQLEndpoint.getPasswordFile());
         } else {
             checkEnvironmentVariableNonNull(ONTOP_DB_URL);
             checkEnvironmentVariableNonNull(ONTOP_DB_DRIVER);
-            checkEnvironmentVariableNonNull(ONTOP_DB_DRIVER_URL);
             checkEnvironmentVariableNonNull(ONTOP_DB_USER);
             checkEnvironmentVariableNonNull(ONTOP_DB_PASSWORD_FILE);
         }
@@ -94,15 +68,6 @@ public final class OntopService extends ContainerService {
         setEnvironmentVariableIfAbsent("ONTOP_DEBUG", "false");
         checkEnvironmentVariableNonNull(OntopClient.ONTOP_MAPPING_FILE);
 
-        // Choose a preprocess command depending on where the PostgreSQL driver file is
-        // coming from
-        String driverURL = getEnvironmentVariable(ONTOP_DB_DRIVER_URL);
-        String getDriverCommand = driverURL.startsWith("/")
-                ? "mkdir -p /opt/ontop/jdbc && cp " + driverURL + " /opt/ontop/jdbc/"
-                : "wget -P /opt/ontop/jdbc " + driverURL;
-        containerSpec
-                .withCommand(List.of("/bin/sh", "-c", getDriverCommand
-                        + " && ./entrypoint.sh"));
     }
 
     @Override
@@ -111,15 +76,6 @@ public final class OntopService extends ContainerService {
 
         writeEndpointConfig(endpointConfig);
 
-        // Remove the PostgreSQL driver file from the scratch volume
-        if (null != postgresqlDriverScratchPath) {
-            try {
-                Files.deleteIfExists(postgresqlDriverScratchPath);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
     }
 
     @Override
