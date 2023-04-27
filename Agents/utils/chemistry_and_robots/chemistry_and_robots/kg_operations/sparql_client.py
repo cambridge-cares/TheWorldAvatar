cambridge-cares/TheWorldAvatar

commit 0f37685f48b26c24b0a738f758500fb2eb93ca96

200-dev-pips-deploy: modified codes/triples to remove appearances of isContainedIn.

Agents/utils/chemistry_and_robots/chemistry_and_robots/kg_operations/sparql_client.py
@@ -668,7 +668,7 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
     def construct_query_for_autosampler(self, given_autosampler_iri: str = None) -> str:
         # TODO this query will return nothing if the autosampler is an empty rack on line "?site <{ONTOVAPOURTEC_HOLDS}> ?vial; <{ONTOVAPOURTEC_LOCATIONID}> ?loc."
         query = f"""{PREFIX_RDF}
-                SELECT ?autosampler ?autosampler_manufacturer ?laboratory ?autosampler_power_supply
+                SELECT ?autosampler ?autosampler_manufacturer ?autosampler_power_supply
                 ?sample_loop_volume ?sample_loop_volume_value ?sample_loop_volume_unit ?sample_loop_volume_num_val
                 ?site ?loc ?vial ?fill_level ?fill_level_om_value ?fill_level_unit ?fill_level_num_val
                 ?warn_level ?warn_level_om_value ?warn_level_unit ?warn_level_num_val
@@ -677,7 +677,6 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
                     {'VALUES ?autosampler { <%s> }' % trimIRI(given_autosampler_iri) if given_autosampler_iri is not None else ""}
                     ?autosampler rdf:type <{ONTOVAPOURTEC_AUTOSAMPLER}>;
                                  <{DBPEDIA_MANUFACTURER}> ?autosampler_manufacturer;
-                                 <{ONTOLAB_ISCONTAINEDIN}> ?laboratory;
                                  <{ONTOLAB_HASPOWERSUPPLY}> ?autosampler_power_supply;
                                  <{ONTOVAPOURTEC_SAMPLELOOPVOLUME}> ?sample_loop_volume.
                     ?sample_loop_volume <{OM_HASVALUE}> ?sample_loop_volume_value.
@@ -727,12 +726,6 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
                 unique_specific_autosampler_manufacturer_iri = dal.get_the_unique_value_in_list_of_dict(info_of_specific_autosampler, 'autosampler_manufacturer')
             except Exception as e:
                 logger.error(f"dbpedia:manufacturer is not correctly defined for {specific_autosampler}", exc_info=True)
-                raise e
-
-            try:
-                unique_specific_autosampler_laboratory_iri = dal.get_the_unique_value_in_list_of_dict(info_of_specific_autosampler, 'laboratory')
-            except Exception as e:
-                logger.error(f"OntoLab:isContainedIn is not correctly defined for {specific_autosampler}", exc_info=True)
                 raise e
 
             try:
@@ -828,7 +821,6 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
             autosampler = AutoSampler(
                 instance_iri=specific_autosampler,
                 manufacturer=unique_specific_autosampler_manufacturer_iri,
-                isContainedIn=unique_specific_autosampler_laboratory_iri,
                 hasPowerSupply=unique_specific_autosampler_power_supply_iri,
                 hasSite=list_autosampler_site,
                 sampleLoopVolume=OM_Volume(
@@ -1132,15 +1124,14 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
     ) -> List[VapourtecRS400]:
         list_vapourtec_rs400_iri = trimIRI([list_vapourtec_rs400_iri] if not isinstance(list_vapourtec_rs400_iri, list) else list_vapourtec_rs400_iri) if list_vapourtec_rs400_iri is not None else None
         list_of_labs_as_constraint = trimIRI(list_of_labs_as_constraint) if list_of_labs_as_constraint is not None else None
-        query = f"""SELECT ?rs400 ?rs400_manufacturer ?laboratory ?rs400_power_supply ?state
+        query = f"""SELECT ?rs400 ?rs400_manufacturer ?rs400_power_supply ?state
                            ?state_type ?last_update ?autosampler ?is_managed_by
                            ?collection_method ?collection_method_type ?waste_receptacle
                            ?recommended_reaction_scale ?recommended_reaction_scale_measure ?recommended_reaction_scale_unit ?recommended_reaction_scale_val
                 WHERE {{
                     {"VALUES ?rs400 { <%s> } ." % '> <'.join(list_vapourtec_rs400_iri) if list_vapourtec_rs400_iri is not None else ""}
                     {"VALUES ?laboratory { <%s> } ." % '> <'.join(list_of_labs_as_constraint) if list_of_labs_as_constraint is not None else ""}
-                    ?rs400 <{ONTOLAB_ISCONTAINEDIN}> ?laboratory;
-                           <{SAREF_CONSISTSOF}> ?autosampler;
+                    ?rs400 <{SAREF_CONSISTSOF}> ?autosampler;
                            <{DBPEDIA_MANUFACTURER}> ?rs400_manufacturer;
                            <{ONTOLAB_HASPOWERSUPPLY}> ?rs400_power_supply;
                            <{SAREF_HASSTATE}> ?state.
@@ -1179,7 +1170,6 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
             vapourtec_rs400 = VapourtecRS400(
                 instance_iri=rs400_iri,
                 manufacturer=res['rs400_manufacturer'],
-                isContainedIn=res['laboratory'],
                 hasPowerSupply=res['rs400_power_supply'],
                 isManagedBy=res['is_managed_by'] if 'is_managed_by' in res else None,
                 consistsOf=list_vapourtec_reactor_and_pump,
@@ -1204,14 +1194,13 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
 
     def get_vapourtec_rs400_given_autosampler(self, autosampler: AutoSampler) -> VapourtecRS400:
         query = f"""{PREFIX_RDF}
-                SELECT ?rs400 ?rs400_manufacturer ?laboratory ?rs400_power_supply ?state ?state_type ?last_update ?is_managed_by
+                SELECT ?rs400 ?rs400_manufacturer ?rs400_power_supply ?state ?state_type ?last_update ?is_managed_by
                 ?collection_method ?collection_method_type ?waste_receptacle
                 ?recommended_reaction_scale ?recommended_reaction_scale_measure ?recommended_reaction_scale_unit ?recommended_reaction_scale_val
                 WHERE {{
                     ?rs400 <{SAREF_CONSISTSOF}> <{autosampler.instance_iri}>;
                             rdf:type <{ONTOVAPOURTEC_VAPOURTECRS400}>;
                             <{DBPEDIA_MANUFACTURER}> ?rs400_manufacturer;
-                            <{ONTOLAB_ISCONTAINEDIN}> ?laboratory;
                             <{ONTOLAB_HASPOWERSUPPLY}> ?rs400_power_supply;
                             <{SAREF_HASSTATE}> ?state.
                     ?state a ?state_type; <{ONTOLAB_STATELASTUPDATEDAT}> ?last_update.
@@ -1244,7 +1233,6 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
         vapourtec_rs400 = VapourtecRS400(
             instance_iri=res['rs400'],
             manufacturer=res['rs400_manufacturer'],
-            isContainedIn=res['laboratory'],
             hasPowerSupply=res['rs400_power_supply'],
             isManagedBy=res['is_managed_by'] if 'is_managed_by' in res else None,
             consistsOf=list_vapourtec_reactor_and_pump,
@@ -1305,7 +1293,7 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
         vapourtec_rs400_iri = trimIRI(vapourtec_rs400_iri)
         query = PREFIX_RDF + \
                 """
-                SELECT ?r4_reactor ?r4_reactor_manufacturer ?laboratory ?r4_reactor_power_supply ?loc ?r4_reactor_material
+                SELECT ?r4_reactor ?r4_reactor_manufacturer ?r4_reactor_power_supply ?loc ?r4_reactor_material
                 ?r4_reactor_internal_diameter ?r4_reactor_internal_diameter_measure ?r4_reactor_internal_diameter_unit ?r4_reactor_internal_diameter_val
                 ?r4_reactor_length ?r4_reactor_length_measure ?r4_reactor_length_unit ?r4_reactor_length_val
                 ?r4_reactor_volume ?r4_reactor_volume_measure ?r4_reactor_volume_unit ?r4_reactor_volume_val
@@ -1313,7 +1301,7 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
                 ?r4_reactor_temp_upper ?r4_reactor_temp_upper_measure ?r4_reactor_temp_upper_unit ?r4_reactor_temp_upper_val
                 WHERE {
                     <%s> <%s> ?r4_reactor .
-                    ?r4_reactor rdf:type <%s>; <%s> ?r4_reactor_manufacturer; <%s> ?laboratory; <%s> ?r4_reactor_power_supply; <%s> ?loc;
+                    ?r4_reactor rdf:type <%s>; <%s> ?r4_reactor_manufacturer; <%s> ?r4_reactor_power_supply; <%s> ?loc;
                     <%s> ?r4_reactor_material; <%s> ?r4_reactor_internal_diameter; <%s> ?r4_reactor_length;
                     <%s> ?r4_reactor_volume; <%s> ?r4_reactor_temp_lower; <%s> ?r4_reactor_temp_upper.
                     ?r4_reactor_internal_diameter <%s> ?r4_reactor_internal_diameter_measure. ?r4_reactor_internal_diameter_measure <%s> ?r4_reactor_internal_diameter_unit; <%s> ?r4_reactor_internal_diameter_val.
@@ -1323,7 +1311,7 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
                     ?r4_reactor_temp_upper <%s> ?r4_reactor_temp_upper_measure. ?r4_reactor_temp_upper_measure <%s> ?r4_reactor_temp_upper_unit; <%s> ?r4_reactor_temp_upper_val.
                 }
                 """ % (
-                    vapourtec_rs400_iri, SAREF_CONSISTSOF, ONTOVAPOURTEC_VAPOURTECR4REACTOR, DBPEDIA_MANUFACTURER, ONTOLAB_ISCONTAINEDIN, ONTOLAB_HASPOWERSUPPLY, ONTOVAPOURTEC_LOCATIONID,
+                    vapourtec_rs400_iri, SAREF_CONSISTSOF, ONTOVAPOURTEC_VAPOURTECR4REACTOR, DBPEDIA_MANUFACTURER, ONTOLAB_HASPOWERSUPPLY, ONTOVAPOURTEC_LOCATIONID,
                     ONTOVAPOURTEC_HASREACTORMATERIAL, ONTOVAPOURTEC_HASINTERNALDIAMETER, ONTOVAPOURTEC_HASREACTORLENGTH,
                     ONTOVAPOURTEC_HASREACTORVOLUME, ONTOVAPOURTEC_HASREACTORTEMPERATURELOWERLIMIT, ONTOVAPOURTEC_HASREACTORTEMPERATUREUPPERLIMIT,
                     OM_HASVALUE, OM_HASUNIT, OM_HASNUMERICALVALUE,
@@ -1349,7 +1337,6 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
             r4_reactor = VapourtecR4Reactor(
                 instance_iri=info_['r4_reactor'],
                 manufacturer=info_['r4_reactor_manufacturer'],
-                isContainedIn=info_['laboratory'],
                 hasPowerSupply=info_['r4_reactor_power_supply'],
                 locationID=info_['loc'],
                 hasReactorMaterial=info_['r4_reactor_material'],
@@ -1380,12 +1367,11 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
 
     def get_r2_pump_given_vapourtec_rs400(self, vapourtec_rs400_iri: str) -> List[VapourtecR2Pump]:
         vapourtec_rs400_iri = trimIRI(vapourtec_rs400_iri)
-        query = f"""SELECT ?r2_pump ?r2_pump_manufacturer ?laboratory ?r2_pump_power_supply ?loc ?reagent_bottle
+        query = f"""SELECT ?r2_pump ?r2_pump_manufacturer ?r2_pump_power_supply ?loc ?reagent_bottle
                 WHERE {{
                     <{vapourtec_rs400_iri}> <{SAREF_CONSISTSOF}> ?r2_pump .
                     ?r2_pump a <{ONTOVAPOURTEC_VAPOURTECR2PUMP}>;
                              <{DBPEDIA_MANUFACTURER}> ?r2_pump_manufacturer;
-                             <{ONTOLAB_ISCONTAINEDIN}> ?laboratory;
                              <{ONTOLAB_HASPOWERSUPPLY}> ?r2_pump_power_supply;
                              <{ONTOVAPOURTEC_LOCATIONID}> ?loc.
                     OPTIONAL{{?r2_pump <{ONTOVAPOURTEC_HASREAGENTSOURCE}> ?reagent_bottle.}}
@@ -1408,7 +1394,6 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
             r2_pump = VapourtecR2Pump(
                 instance_iri=info_['r2_pump'],
                 manufacturer=info_['r2_pump_manufacturer'],
-                isContainedIn=info_['laboratory'],
                 hasPowerSupply=info_['r2_pump_power_supply'],
                 locationID=info_['loc'],
                 hasReagentSource=self.get_reagent_bottle(info_['reagent_bottle']) if 'reagent_bottle' in info_ else None,
@@ -2375,9 +2360,9 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
     def get_hplc_given_vapourtec_rs400(self, vapourtec_rs400_iri: str) -> HPLC:
         vapourtec_rs400_iri = trimIRI(vapourtec_rs400_iri)
         query = """SELECT ?hplc ?hplc_manufacturer ?lab ?hplc_power_supply ?is_managed_by ?report_extension
-                WHERE {<%s> ^<%s>/<%s> ?hplc. ?hplc a <%s>; <%s> ?hplc_manufacturer; <%s> ?lab; <%s> ?hplc_power_supply; <%s> ?report_extension.
+                WHERE {<%s> ^<%s>/<%s> ?hplc. ?hplc a <%s>; <%s> ?hplc_manufacturer; <%s> ?hplc_power_supply; <%s> ?report_extension.
                 OPTIONAL{?hplc <%s> ?is_managed_by}}""" % (
-            vapourtec_rs400_iri, SAREF_CONSISTSOF, SAREF_CONSISTSOF, ONTOHPLC_HIGHPERFORMANCELIQUIDCHROMATOGRAPHY, DBPEDIA_MANUFACTURER, ONTOLAB_ISCONTAINEDIN, ONTOLAB_HASPOWERSUPPLY,
+            vapourtec_rs400_iri, SAREF_CONSISTSOF, SAREF_CONSISTSOF, ONTOHPLC_HIGHPERFORMANCELIQUIDCHROMATOGRAPHY, DBPEDIA_MANUFACTURER, ONTOLAB_HASPOWERSUPPLY,
             ONTOHPLC_REPORTEXTENSION, ONTOLAB_ISMANAGEDBY
         )
         response = self.performQuery(query)
@@ -2392,7 +2377,6 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
         hplc = HPLC(
             instance_iri=res['hplc'],
             manufacturer=res['hplc_manufacturer'],
-            isContainedIn=res['lab'],
             hasPowerSupply=res['hplc_power_supply'],
             isManagedBy=res['is_managed_by'] if 'is_managed_by' in res else None,
             reportExtension=res['report_extension'],

Agents/RxnOptGoalAgent/tests/conftest.py
@@ -882,7 +882,7 @@ def assert_rxn_iterations(
         assert lst_hplc_report_iri == vapourtec_schedule_derivation_outputs[ONTOHPLC_HPLCREPORT]
         # (3) The assigned reactor should be contained in the lab which the Vapourtec Schedule Derivation isDerivedFrom
         assigned_lab_iri = get_lab_as_constriant_of_vapourtec_schedule_derivation(vapourtec_schedule_derivation, sparql_client)
-        assert sparql_client.check_if_triple_exist(lst_done_rxn_exp_instance[0].isAssignedTo, ONTOLAB_ISCONTAINEDIN, assigned_lab_iri)
+        assert sparql_client.check_if_triple_exist(assigned_lab_iri, ONTOLAB_CONTAINS, lst_done_rxn_exp_instance[0].isAssignedTo)
         print(f"Vapourtec Schedule Derivation checked successfully for reaction experiment {rxn_exp_iri}")
 
         ##################################

Agents/RxnOptGoalAgent/tests/test_triples/lab1.ttl
@@ -54,7 +54,6 @@
 ##################################################
 :AutomatedRxnPlatform_Dummy
     a OntoLab:LabEquipment;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     saref:consistsOf :VapourtecRS400_Dummy;
     saref:consistsOf :HPLC_Dummy;
     saref:consistsOf :BPR_Dummy;
@@ -66,7 +65,6 @@
 :HPLC_Dummy # TODO
     a OntoHPLC:HighPerformanceLiquidChromatography;
     dbo:manufacturer :HPLC_Manufacturer;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :HPLC_Dummy_PowerSupply;
     OntoHPLC:localReportDirectory "/app/CHEM32/"^^xsd:string; # NOTE "\**\" or "/**/" depends on whether it is windows or linux machine
     OntoHPLC:reportExtension <http://dbpedia.org/resource/Text_file>;
@@ -213,7 +211,6 @@
 :BPR_Dummy # TODO
     a OntoBPR:BackPressureRegulator;
     dbo:manufacturer :BPR_Manufacturer;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :BPR_Dummy_PowerSupply;
 .
 
@@ -222,7 +219,6 @@
 #############################
 :VapourtecRS400_Dummy
     a OntoVapourtec:VapourtecRS400;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     # Basic information
     OntoLab:hasPrice :VapRS400_Price;
     OntoLab:hasWeight :VapRS400_Weight; # TODO
@@ -345,7 +341,6 @@
 ##################################
 :VapourtecR4_Dummy
     a OntoVapourtec:VapourtecR4Reactor;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR4_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "4"^^xsd:string;
@@ -417,7 +412,6 @@
 #################################
 :VapourtecR2_1_Dummy
     a OntoVapourtec:VapourtecR2Pump;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR2_1_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "A"^^xsd:string;
@@ -429,7 +423,6 @@
 #################################
 :VapourtecR2_2_Dummy
     a OntoVapourtec:VapourtecR2Pump;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR2_2_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "B"^^xsd:string;
@@ -441,7 +434,6 @@
 #################################
 :VapourtecR2_3_Dummy
     a OntoVapourtec:VapourtecR2Pump;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR2_3_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "C"^^xsd:string;
@@ -453,7 +445,6 @@
 #########################
 :AutoSampler_Dummy
     a OntoVapourtec:AutoSampler;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :AutoSampler_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:sampleLoopVolume :AutoSampler_Dummy_SampleLoopVolume;

Agents/RxnOptGoalAgent/tests/test_triples/lab2.ttl
@@ -54,7 +54,6 @@
 ##################################################
 :AutomatedRxnPlatform_Dummy
     a OntoLab:LabEquipment;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     saref:consistsOf :VapourtecRS400_Dummy;
     saref:consistsOf :HPLC_Dummy;
     saref:consistsOf :BPR_Dummy;
@@ -66,7 +65,6 @@
 :HPLC_Dummy # TODO
     a OntoHPLC:HighPerformanceLiquidChromatography;
     dbo:manufacturer :HPLC_Manufacturer;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :HPLC_Dummy_PowerSupply;
     OntoHPLC:localReportDirectory "/app/CHEM32/"^^xsd:string; # NOTE "\**\" or "/**/" depends on whether it is windows or linux machine
     OntoHPLC:reportExtension <http://dbpedia.org/resource/Microsoft_Excel>;
@@ -200,7 +198,6 @@
 :BPR_Dummy # TODO
     a OntoBPR:BackPressureRegulator;
     dbo:manufacturer :BPR_Manufacturer;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :BPR_Dummy_PowerSupply;
 .
 
@@ -209,7 +206,6 @@
 #############################
 :VapourtecRS400_Dummy
     a OntoVapourtec:VapourtecRS400;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     # Basic information
     OntoLab:hasPrice :VapRS400_Price;
     OntoLab:hasWeight :VapRS400_Weight; # TODO
@@ -332,7 +328,6 @@
 ##################################
 :VapourtecR4_Dummy
     a OntoVapourtec:VapourtecR4Reactor;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR4_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "4"^^xsd:string;
@@ -404,7 +399,6 @@
 #################################
 :VapourtecR2_1_Dummy
     a OntoVapourtec:VapourtecR2Pump;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR2_1_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "A"^^xsd:string;
@@ -457,7 +451,6 @@
 #################################
 :VapourtecR2_2_Dummy
     a OntoVapourtec:VapourtecR2Pump;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR2_2_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "B"^^xsd:string;
@@ -510,7 +503,6 @@
 #################################
 :VapourtecR2_3_Dummy
     a OntoVapourtec:VapourtecR2Pump;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR2_3_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "C"^^xsd:string;
@@ -563,7 +555,6 @@
 #########################
 :AutoSampler_Dummy
     a OntoVapourtec:AutoSampler;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :AutoSampler_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:sampleLoopVolume :AutoSampler_Dummy_SampleLoopVolume;

Agents/utils/chemistry_and_robots/chemistry_and_robots/data_model/iris.py
@@ -251,7 +251,6 @@ ONTOLAB_HASQUANTITY = ONTOLAB + 'hasQuantity'
 ONTOLAB_HASSETTING = ONTOLAB + 'hasSetting'
 ONTOLAB_HASWEIGHT = ONTOLAB + 'hasWeight'
 ONTOLAB_HASWIDTH = ONTOLAB + 'hasWidth'
-ONTOLAB_ISCONTAINEDIN = ONTOLAB + 'isContainedIn'
 ONTOLAB_ISPREPAREDBY = ONTOLAB + 'isPreparedBy'
 ONTOLAB_SPECIFIES = ONTOLAB + 'specifies'
 ONTOLAB_CHEMICALAMOUNT = ONTOLAB + 'ChemicalAmount'

Agents/utils/chemistry_and_robots/chemistry_and_robots/data_model/ontolab.py
@@ -98,7 +98,6 @@ class Saref_State(BaseOntology):
 class LabEquipment(Saref_Device):
     clz: str = ONTOLAB_LABEQUIPMENT
     manufacturer: str # it should be pointing to an instance of https://dbpedia.org/ontology/Organisation, but we simplified here
-    isContainedIn: Union[str, Laboratory] # NOTE here str is provided as an optional as it seems impossible to circular reference at instance level
     hasPowerSupply: Union[str, PowerSupply] # NOTE TODO [future work] here str is provided as an optional to simplify the implementation
     consistsOf: Optional[List[LabEquipment]] = None
     # TODO [future work] add support for hasHeight, hasLength, hasPrice, hasWeight, and hasWidth

Agents/utils/chemistry_and_robots/chemistry_and_robots/resources/sample_data/dummy_lab.ttl
@@ -59,7 +59,6 @@ exp5:RxnExp_1 ontorxn:isAssignedTo :VapourtecR4_Another_Dummy.
 ##################################################
 :AutomatedRxnPlatform_Dummy 
     a OntoLab:LabEquipment;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     saref:consistsOf :VapourtecRS400_Dummy;
     saref:consistsOf :HPLC_Dummy;
     saref:consistsOf :BPR_Dummy;
@@ -71,7 +70,6 @@ exp5:RxnExp_1 ontorxn:isAssignedTo :VapourtecR4_Another_Dummy.
 :HPLC_Dummy
     a ontohplc:HighPerformanceLiquidChromatography;
     dbo:manufacturer :HPLC_Manufacturer;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :HPLC_Dummy_PowerSupply;
     ontohplc:localReportDirectory "/home/user/CHEM32/**/"^^xsd:string; # NOTE "\**\" or "/**/" depends on whether its windows or linux machine
     ontohplc:reportExtension <http://dbpedia.org/resource/Microsoft_Excel>;
@@ -676,7 +674,6 @@ SinglePhase:PhaseComponent_InternalStandard_Property_1_ScalarValue_1
 #############################
 :VapourtecRS400_Dummy
     a OntoVapourtec:VapourtecRS400;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     # Basic information
     OntoLab:hasPrice :VapRS400_Price;
     OntoLab:hasWeight :VapRS400_Weight; # TODO
@@ -788,7 +785,6 @@ SinglePhase:PhaseComponent_InternalStandard_Property_1_ScalarValue_1
 ##################################
 :VapourtecR4_Dummy
     a OntoVapourtec:VapourtecR4Reactor;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR4_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "4"^^xsd:string;
@@ -860,7 +856,6 @@ SinglePhase:PhaseComponent_InternalStandard_Property_1_ScalarValue_1
 ##########################################
 :VapourtecR4_Another_Dummy
     a OntoVapourtec:VapourtecR4Reactor;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR4_Another_DummyPowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "1"^^xsd:string; # NOTE this is different from the locationID of :VapourtecR4_Dummy
@@ -932,7 +927,6 @@ SinglePhase:PhaseComponent_InternalStandard_Property_1_ScalarValue_1
 #################################
 :VapourtecR2_1_Dummy
     a OntoVapourtec:VapourtecR2Pump;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR2_1_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "A"^^xsd:string;
@@ -986,7 +980,6 @@ SinglePhase:PhaseComponent_InternalStandard_Property_1_ScalarValue_1
 #################################
 :VapourtecR2_2_Dummy
     a OntoVapourtec:VapourtecR2Pump;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR2_2_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "B"^^xsd:string;
@@ -997,7 +990,6 @@ SinglePhase:PhaseComponent_InternalStandard_Property_1_ScalarValue_1
 #################################
 :VapourtecR2_3_Dummy
     a OntoVapourtec:VapourtecR2Pump;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR2_3_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "C"^^xsd:string;
@@ -1008,7 +1000,6 @@ SinglePhase:PhaseComponent_InternalStandard_Property_1_ScalarValue_1
 #################################
 :VapourtecR2_4_Dummy
     a OntoVapourtec:VapourtecR2Pump;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :VapR2_4_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:locationID "D"^^xsd:string;
@@ -1019,7 +1010,6 @@ SinglePhase:PhaseComponent_InternalStandard_Property_1_ScalarValue_1
 #########################
 :AutoSampler_Dummy
     a OntoVapourtec:AutoSampler;
-    OntoLab:isContainedIn :Laboratory_Dummy;
     OntoLab:hasPowerSupply :AutoSampler_PowerSupply; # TODO
     dbo:manufacturer :VapourtecLtd;
     OntoVapourtec:sampleLoopVolume :AutoSampler_Dummy_SampleLoopVolume;

Agents/utils/chemistry_and_robots/chemistry_and_robots/tests/test_sparql_client.py
@@ -819,7 +819,6 @@ def test_get_vapourtec_rs400(initialise_triples):
     vapourtec_rs400 = vapourtec_rs400_list[0]
     assert vapourtec_rs400.instance_iri == TargetIRIs.VAPOURTECRS400_DUMMY_IRI.value
     assert vapourtec_rs400.manufacturer == TargetIRIs.VAPOURTEC_LTD.value
-    assert vapourtec_rs400.isContainedIn == TargetIRIs.DUMMY_LAB_IRI.value
     assert vapourtec_rs400.isManagedBy is None
     assert vapourtec_rs400.consistsOf is not None
     assert dal.check_if_two_lists_equal(TargetIRIs.VAPOURTECRS400_DUMMY_CONSISTS_OF_LIST.value,
@@ -928,7 +927,6 @@ def test_get_autosampler(initialise_triples):
     sparql_client = initialise_triples
     autosampler = sparql_client.get_autosampler(TargetIRIs.AUTOSAMPLER_DUMMY_IRI.value)
     assert autosampler.instance_iri == TargetIRIs.AUTOSAMPLER_DUMMY_IRI.value
-    assert autosampler.isContainedIn == TargetIRIs.DUMMY_LAB_IRI.value
 
     # Check autosampler sampleLoopVolume is correctly parsed
     assert autosampler.sampleLoopVolume.instance_iri == TargetIRIs.AUTOSAMPLER_SAMPLE_LOOP_VOLUME_IRI.value
@@ -1069,7 +1067,6 @@ def test_get_hplc_given_vapourtec_rs400(initialise_triples):
     hplc = sparql_client.get_hplc_given_vapourtec_rs400(vapourtec_rs400_iri=TargetIRIs.VAPOURTECRS400_DUMMY_IRI.value)
     assert hplc.instance_iri == TargetIRIs.HPLC_DUMMY_IRI.value
     assert hplc.manufacturer == TargetIRIs.HPLC_DUMMY_MANUFACTURER_IRI.value
-    assert hplc.isContainedIn == TargetIRIs.DUMMY_LAB_IRI.value
     assert hplc.isManagedBy is None
 
     # Register agent with hplc

commit 49866e39938d486e92271a1a46641f3f0a072b49

210-dev-catalyst-selection: fixed bug introduced in https://github.com/cambridge-cares/TheWorldAvatar/pull/677/commits/0f37685f48b26c24b0a738f758500fb2eb93ca96 which accidently removed the constraint to only check for the reactor located in the given laboratory - this caused non-deterministic behaviour as any reactor can be returned.

Agents/utils/chemistry_and_robots/chemistry_and_robots/kg_operations/sparql_client.py
@@ -1231,6 +1231,7 @@ class ChemistryAndRobotsSparqlClient(PySparqlClient):
                 WHERE {{
                     {"VALUES ?rs400 { <%s> } ." % '> <'.join(list_vapourtec_rs400_iri) if list_vapourtec_rs400_iri is not None else ""}
                     {"VALUES ?laboratory { <%s> } ." % '> <'.join(list_of_labs_as_constraint) if list_of_labs_as_constraint is not None else ""}
+                    ?laboratory <{ONTOLAB_CONTAINS}> ?rs400.
                     ?rs400 <{SAREF_CONSISTSOF}> ?autosampler;
                            <{DBPEDIA_MANUFACTURER}> ?rs400_manufacturer;
                            <{ONTOLAB_HASPOWERSUPPLY}> ?rs400_power_supply;