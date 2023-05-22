1fda2c2ccdaa507a5d88cf70bd6f2cbd7fa74704
xiaochizhou <xz378@cam.ac.uk>
2023-05-22 15:50:24 +0800

    Update eval.py
    updated entity linking script

--- MARIE_AND_BERT/Marie/EntityLinking/eval.py
+++ MARIE_AND_BERT/Marie/EntityLinking/eval.py
@@ -1,24 +1,31 @@
 import json
+import os
+
+from Marie.EntityLinking.IRILookup import IRILookup
+from Marie.Util.location import DATA_DIR
 
 '''
 accu metrics
 
 '''
+
+
 def metrics(result, questions):
-    correct= 0
+    correct = 0
     assert len(result) == len(questions)
     for idx, item in enumerate(result):
         if item is None:
             continue
         cid_str = item[1]
         cid = int(cid_str.replace('CID', ''))
-        #bounds = item[2]
-        #pre_entity_label = item[3]
+        # bounds = item[2]
+        # pre_entity_label = item[3]
         GT_cid = questions[idx]['label_id'][0]
         if cid == GT_cid:
-            correct = correct +1
+            correct = correct + 1
 
-    print('acc: {}'.format(correct/len(questions)))
+    print('acc: {}'.format(correct / len(questions)))
+
 
 def load_testfile(path):
     questions = []
@@ -31,10 +38,11 @@ def load_testfile(path):
 
 if __name__ == '__main__':
     from ChemicalNEL import ChemicalNEL
-    cn = ChemicalNEL(dataset_name="pubchem")
-    questions = load_testfile('D:\work\Marie\BLINK-main\data\pubchemELQ5000alt\\test_all.jsonl')
+    cn = ChemicalNEL()
+    my_iri = IRILookup(dataset_name="pubchem")
+    questions = load_testfile(os.path.join(DATA_DIR, 'Evaluation', 'entitylinking', 'test.jsonl'))
     result = []
     for q in questions:
-        rst = cn.find_cid(q['text'])
+        rst = my_iri.find_cid(q['text'])
         result.append(rst)
     metrics(result, questions)
