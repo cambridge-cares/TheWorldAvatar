56bf124d98691fd63724be7977e6ff2410b7e36a
Michael-TeguhLaksana <mtl41@cam.ac.uk>
2023-05-29 10:03:18 +0800

    dev-1505-proximity-sensor-for-lab_FHAgent: Bugfix on unit tests

--- Agents/FHAgent/FHAgent/src/test/java/uk/ac/cam/cares/jps/agent/fh/FHAgentDerivationTest.java
+++ Agents/FHAgent/FHAgent/src/test/java/uk/ac/cam/cares/jps/agent/fh/FHAgentDerivationTest.java
@@ -7,7 +7,7 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-
+import org.apache.jena.sparql.function.library.print;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.sparqlbuilder.core.Prefix;
 import org.eclipse.rdf4j.sparqlbuilder.core.SparqlBuilder;
@@ -80,19 +80,22 @@ public class FHAgentDerivationTest {
     // Default list of JSON keys
     private final String[] mockDerivVarKey = {"occupiedState1"};
     
-    private static final String ONTODERIV = "https://www.theworldavatar.com/kg/ontoderivation/";
-    private static final String ONTOAGE = "https://www.theworldavatar.com/kg/ontoagent/";
+    private static final String ONTODERIV = "https://raw.githubusercontent.com/cambridge-cares/TheWorldAvatar/main/JPS_Ontology/ontology/ontoderivation/OntoDerivation.owl#";
+    private static final String ONTOAGE = "http://www.theworldavatar.com/ontology/ontoagent/MSM.owl#";
     private static final String ONTOTS = "https://www.theworldavatar.com/kg/ontotimeseries/";
-    private static final String W3 = "http://www.w3.org/2006/";
+    private static final String W3TIME = "http://www.w3.org/2006/time#";
 
     private static final Prefix P_AGENT = SparqlBuilder.prefix("ontoagent",iri(ONTOAGE));
     private static final Prefix P_TS = SparqlBuilder.prefix("ontotimeseries", iri(ONTOTS));
-    private static final Prefix P_W3 = SparqlBuilder.prefix("w3", iri(W3));
+    private static final Prefix P_W3TIME = SparqlBuilder.prefix("w3", iri(W3TIME));
+    private static final Prefix P_DERIV = SparqlBuilder.prefix("ontoderivation", iri(ONTODERIV));
 
-    private static final Iri Service = P_AGENT.iri("MSM.owl#Service");
+    private static final Iri Service = P_AGENT.iri("Service");
     private static final Iri hasTimeSeries = P_TS.iri("hasTimeSeries");
-    private static final Iri hasHttpUrl = P_AGENT.iri("MSM.owl#hasHttpUrl");
-    private static final Iri hasTime = P_W3.iri("time#hasTime");
+    private static final Iri hasHttpUrl = P_AGENT.iri("hasHttpUrl");
+    private static final Iri hasTime = P_W3TIME.iri("hasTime");
+    private static final Iri isDerivedFrom = P_DERIV.iri("isDerivedFrom");
+    private static final Iri belongsTo = P_DERIV.iri("belongsTo");
     //Default list of timestamps
 
     //Mock set of keys from Thingsboard
@@ -185,7 +188,7 @@ public class FHAgentDerivationTest {
         
         writePropertyFile(agentPropFile, Arrays.asList(new String[]{"thingsboard.mappingfolder=TEST_MAPPINGS", "derivation.mapping=avgDist1:occupiedState1", "threshold.tally = 170.", "data_bridge.url = https://example.com", "use_stack = false", "derivation.baseurl = http://derivationexample.com/triplestore/repository/"}));
         
-        String[] clientPropParam = {"db_url="+jdbcURL, "db.user="+postgresUsername, "db.password="+postgresPassword, "sparql.query.endpoint="+sparql_endpoint};
+        String[] clientPropParam = {"db_url="+jdbcURL, "db.user="+postgresUsername, "db.password="+postgresPassword,"sparql.query.endpoint="+sparql_endpoint, "sparql.update.endpoint="+sparql_endpoint};
         writePropertyFile(clientPropFile, Arrays.asList(clientPropParam));
         // To create testAgent without an exception being thrown, SystemLambda is used to mock an environment variable
         // To mock the environment variable, a try catch need to be used
@@ -232,7 +235,7 @@ public class FHAgentDerivationTest {
             writePropertyFile(propertiesFile, new ArrayList<>());
 
             String clientPropFile = Paths.get(folder.getRoot().toString(), "client.properties").toString();
-            String[] clientPropParam = {"db_url="+jdbcURL, "db.user="+postgresUsername, "db.password="+postgresPassword, "sparql.query.endpoint="+sparql_endpoint};
+            String[] clientPropParam = {"db_url="+jdbcURL, "db.user="+postgresUsername, "db.password="+postgresPassword, "sparql.query.endpoint="+sparql_endpoint, "sparql.update.endpoint="+sparql_endpoint};
             writePropertyFile(clientPropFile, Arrays.asList(clientPropParam));
             String iriMapperFile = Paths.get(folder.getRoot().toString(), "iriMapping.txt").toString();
         
@@ -334,33 +337,44 @@ public class FHAgentDerivationTest {
         //Test with dummy URL
         testDerivator.instantiateAgent("http://localhost:1010/fh-agent/instantiate");
         SelectQuery queryURL = Queries.SELECT();
-        queryURL.where(queryURL.var().has(hasHttpUrl, queryURL.var()));
+        queryURL.where(queryURL.var().has(hasHttpUrl, queryURL.var())).prefix(P_AGENT);
 
-        JSONArray queryResult = storeClient.executeQuery(queryURL.getQueryString());
+        JSONArray queryUrlResult = storeClient.executeQuery(queryURL.getQueryString());
+        //System.out.println(queryResult);
         //Should only have 1 FHAgent instance; 1 column for iri, 1 for url
-        Assert.assertTrue(queryResult.length() == 2);
-        JSONObject column = queryResult.getJSONObject(1);
-        String agentIRI = column.getString(column.keys().next());
+        Assert.assertEquals(1, queryUrlResult.length());
+        JSONObject columns = queryUrlResult.getJSONObject(0);
+        String agentIRI = columns.getString("x1");
         Assert.assertEquals("http://localhost:1010/fh-agent/instantiate", agentIRI);
 
         //Check if agent is instantiated
         SelectQuery query = Queries.SELECT();
-        query.where(query.var().has(RDF.TYPE, Service));
-        Assert.assertTrue(storeClient.executeQuery(query.getQueryString()).length() > 1);
+        query.where(query.var().has(RDF.TYPE, Service)).prefix(P_AGENT);
+        JSONArray queryResult = storeClient.executeQuery(query.getQueryString());
+        Assert.assertTrue(queryResult.length() >= 1);
+
+        //DEBUG show all triple for debugging
+        SelectQuery queryAllTriple = Queries.SELECT();
+        queryAllTriple.where(queryAllTriple.var().has(queryAllTriple.var(), queryAllTriple.var()));
+        System.out.println(storeClient.execute(queryAllTriple.getQueryString()));
 
         //Check raw and derived var instances
-        //Derived var has timeseries
+        //Derived var has belongsTo derivationWithTimeseries
         SelectQuery queryDeriv = Queries.SELECT();
-        queryDeriv.where(queryDeriv.var().has(hasTimeSeries, queryDeriv.var()));
+        queryDeriv.where(queryDeriv.var().has(belongsTo, queryDeriv.var())).prefix(P_DERIV);
+        //System.out.println(queryDeriv.getQueryString());
+        JSONArray queryResultDeriv = storeClient.executeQuery(queryDeriv.getQueryString());
+        //System.out.println(queryResultDeriv);
         //Must be equal to the number of derived variables
-        Assert.assertTrue(storeClient.executeQuery(queryDeriv.getQueryString()).length() == testAgent.getNumberOfTimeSeries());
+        Assert.assertTrue( queryResultDeriv.getJSONObject(0).getString("x0").equals("example:prefix/api_occupiedState1"));
 
-        //Raw var has timestamp
+        //Raw var has isDerivedFrom
         SelectQuery queryRaw = Queries.SELECT();
-        queryRaw.where(queryRaw.var().has(hasTime, queryRaw.var()));
+        queryRaw.where(queryRaw.var().has(isDerivedFrom, queryRaw.var())).prefix(P_DERIV);
         //IRI Map contains both derived and raw var
         //The number of raw var instances must be equal to irimap size - # of derived var
-        Assert.assertTrue(storeClient.executeQuery(queryRaw.getQueryString()).length() == testDerivator.iriMap.size()-testAgent.getNumberOfTimeSeries());
+        System.out.println(storeClient.executeQuery(queryRaw.getQueryString()));
+        Assert.assertEquals(testDerivator.iriMap.size()-testAgent.getNumberOfTimeSeries(), storeClient.executeQuery(queryRaw.getQueryString()).length());
     }
 
 
