74fcee5309fbb879d76074dad1366344bf9ad9ac
Kok Foong Lee <kfl31@cam.ac.uk>
2023-06-06 18:04:36 +0800

    dev-aermod-agent-cleanup: standardise version numbers in virtual sensor

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # pull docker images from docker.cmclinnovations.com because stack manager can't pull these
-docker pull ghcr.io/cambridge-cares/dispersion-interactor:1.1.1-dev-aermod-agent-cleanup-SNAPSHOT
+docker pull ghcr.io/cambridge-cares/dispersion-interactor:1.1.5-dev-aermod-agent-cleanup-SNAPSHOT
 docker pull ghcr.io/cambridge-cares/aermod-agent:1.1.5-dev-aermod-agent-cleanup-SNAPSHOT
 docker pull ghcr.io/cambridge-cares/dispersion-vis:1.1.1-SNAPSHOT
 docker pull ghcr.io/cambridge-cares/python-service:1.1.1-SNAPSHOT
-docker pull ghcr.io/cambridge-cares/emissions-agent:1.0.1-SNAPSHOT
-docker pull docker.cmclinnovations.com/ship-input-agent:1.0.0
+docker pull ghcr.io/cambridge-cares/emissions-agent:1.1.5-dev-aermod-agent-cleanup-SNAPSHOT
+docker pull docker.cmclinnovations.com/ship-input-agent:1.1.5-dev-aermod-agent-cleanup-SNAPSHOT
 docker pull docker.cmclinnovations.com/weatheragent:1.1.1
 docker pull docker.cmclinnovations.com/file-server:1.0.0
 docker pull ghcr.io/cambridge-cares/feature-info-agent:2.0.1
