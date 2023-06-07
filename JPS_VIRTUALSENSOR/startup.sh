3794c23950d51cf6a23ba9661ab62ef091241a3c
Kok Foong Lee <kfl31@cam.ac.uk>
2023-06-07 17:46:20 +0800

    dev-aermod-agent-cleanup: correct ship input agent image path

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -5,7 +5,7 @@ docker pull ghcr.io/cambridge-cares/aermod-agent:1.1.5-dev-aermod-agent-cleanup-
 docker pull ghcr.io/cambridge-cares/dispersion-vis:1.1.1-SNAPSHOT
 docker pull ghcr.io/cambridge-cares/python-service:1.1.1-SNAPSHOT
 docker pull ghcr.io/cambridge-cares/emissions-agent:1.1.5-dev-aermod-agent-cleanup-SNAPSHOT
-docker pull docker.cmclinnovations.com/ship-input-agent:1.1.5-dev-aermod-agent-cleanup-SNAPSHOT
+docker pull ghcr.io/cambridge-cares/ship-input-agent:1.1.5-dev-aermod-agent-cleanup-SNAPSHOT
 docker pull docker.cmclinnovations.com/weatheragent:1.1.1
 docker pull docker.cmclinnovations.com/file-server:1.0.0
 docker pull ghcr.io/cambridge-cares/feature-info-agent:2.0.1
