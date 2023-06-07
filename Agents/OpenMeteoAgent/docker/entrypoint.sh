fb43f583e5ac21893b8c651699f459b3a180490e
nagarajankarthik <nkarthik3092@gmail.com>
2023-06-07 15:05:12 +0800

    dev-aermod-agent-aermap: Remove OpenMeteoAgent

--- Agents/OpenMeteoAgent/docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ ${DEBUG} == ON ]]; then
-  catalina.sh jpda run
-else
-  catalina.sh run
-fi
--- Agents/OpenMeteoAgent/stack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-COMMAND=$1
-shift
-
-SCRIPTS_DIR="$(git rev-parse --show-toplevel)/Deploy/stacks/dynamic/common-scripts"
-export SCRIPTS_DIR
-
-"${SCRIPTS_DIR}/${COMMAND}.sh" "$@"
