00aacbc225acc39aeae4c9c47169d948f617798f
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-06-16 14:03:11 +0100

    542-t_srs-does-not-work-for-rasters: Explicitly disabled the copying or creation of mask bands when reprojecting rasters.

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/gdal/GDALTranslateOptions.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/gdal/GDALTranslateOptions.java
@@ -28,7 +28,7 @@ public class GDALTranslateOptions extends CommonOptions<GDALTranslateOptions> {
             allArgs.add("-a_srs");
             allArgs.add(sridIn);
         }
-        
+
         String sridOut = getSridOut();
         if (null != sridOut) {
             // This is a COG file format specific option.
@@ -39,6 +39,10 @@ public class GDALTranslateOptions extends CommonOptions<GDALTranslateOptions> {
             // to rasters that don't have a nodata value, which causes issues. This also
             // brings the output more inline with just using gdalswarp.
             creationOptions.put("ADD_ALPHA", "NO");
+            // Masks seem not to be supported properly by PostGIS or GeoServer so disabling
+            // them seem sensible.
+            allArgs.add("-mask");
+            allArgs.add("none");
         }
     }
 }
