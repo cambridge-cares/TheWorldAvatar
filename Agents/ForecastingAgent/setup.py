c486fb9b1517ec6e9770a0f2a5d59e45a773bb0f
Markus Hofmeister <mh807@cam.ac.uk>
2023-06-22 08:12:52 +0800

    hotfix-forecasting-agent: Updated version numbers in setup.py and stack-manager-input config file

--- Agents/ForecastingAgent/setup.py
+++ Agents/ForecastingAgent/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='forecasting',
-    version='1.1.0',
+    version='1.1.1',
     author='Markus Hofmeister, Magnus Mueller',
     author_email='mh807@cam.ac.uk',
     license='MIT',
