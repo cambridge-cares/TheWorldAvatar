46270e5f0f20df11b654dc39d4a6646bc696c7ab
szhang012 <81546703+szhang012@users.noreply.github.com>
2023-06-27 18:33:10 +0800

    Update: readme

--- MARIE_AND_BERT/Training/EntityLinking/my_scripts/train_ner.sh
+++ MARIE_AND_BERT/Training/EntityLinking/my_scripts/train_ner.sh
@@ -1,4 +1,5 @@
 valid_file_name=$1
 train_file_name=$2
-command="python translator/train.py --valid_file_name ${valid_file_name} --train_file_name ${train_file_name}"
+output_path=$3
+command="python translator/train.py --valid_file_name ${valid_file_name} --train_file_name ${train_file_name} --model_name ${output_path}/SMILES_NER.bin"
 ${command}
