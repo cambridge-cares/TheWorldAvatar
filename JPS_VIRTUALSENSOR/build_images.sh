680c2677c22f1217373e26e7d376adfde558b798
Kok Foong Lee <kfl31@cam.ac.uk>
2023-06-30 10:18:31 +0800

    dev-aermod-agent-elevation: undo accident commit to build_images.sh

--- JPS_VIRTUALSENSOR/build_images.sh
+++ JPS_VIRTUALSENSOR/build_images.sh
@@ -2,3 +2,6 @@
 (cd AermodAgent && ./stack.sh build)
 (cd DispersionInteractor && ./stack.sh build)
 (cd EmissionsAgent && ./stack.sh build)
+(cd JurongIslandInputAgent && ./stack.sh build)
+(cd PythonService && ./stack.sh build)
+(cd ShipInputAgent && ./stack.sh build)
