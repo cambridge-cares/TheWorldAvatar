41e8a26c85b60c0bc84ec17a64e01a05f57d8cf0
mingchuantian812 <mt933@cam.ac.uk>
2023-07-05 17:40:51 +0800

    1604-develop-geosegmentagent: Stackerised GeoSegmentAgent

--- Agents/GeoSegmentAgent/GeoSegmentAgent/app.py
+++ Agents/GeoSegmentAgent/GeoSegmentAgent/app.py
@@ -1,17 +1,25 @@
-from flask import Flask, jsonify, request, json
+from flask import Flask, jsonify, request, json, send_file
 import logging
-
-import pkg_resources
-installed_packages = pkg_resources.working_set
-installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
-print(installed_packages_list)
 
 from samgeo import tms_to_geotiff
 from samgeo.text_sam import LangSAM
 
+SAM = None
 
 # Create the Flask app object
 app = Flask(__name__)
+
+from .stack_utils.stack_configs import QUERY_ENDPOINT_STACK, UPDATE_ENDPOINT_STACK
+from .stack_utils.stack_configs import DB_UPDATE_URL_STACK, DB_UPDATE_USER_STACK, DB_UPDATE_PASSWORD_STACK
+from .stack_utils.stack_configs import DB_QUERY_URL_STACK, DB_QUERY_USER_STACK, DB_QUERY_PASSWORD_STACK
+
+@app.route('/setup')
+def setup():
+    global SAM
+    if SAM is not None:
+        return 'The SAM model has already been initialized'
+    SAM = LangSAM(model_type='vit_b')
+    return 'The SAM model has been initialized'
 
 @app.route('/')
 def default():
@@ -19,17 +27,18 @@ def default():
     if not check_request_parameters(request.args):
         return 'Wrong request parameters'
 
+    if SAM is None:
+        return 'The SAM model has not been initialized'
+
     bbox = [-118.4932, 34.0404, -118.4903, 34.0417]
 
     tms_to_geotiff(output="Image.tif", bbox=bbox, zoom=19, source="Satellite", overwrite=True)
 
-    #Initialize LangSAM class
-    sam = LangSAM()
     text_prompt = "building rooftops"
 
-    sam.predict("Image.tif", "houses", box_threshold=0.3, text_threshold=0.3)
-
-    sam.show_anns(
+    SAM.predict("Image.tif", "houses", box_threshold=0.3, text_threshold=0.3)
+    logging.info('predicted')
+    SAM.show_anns(
         cmap='Greys_r',
         add_boxes=False,
         alpha=1,
@@ -38,10 +47,12 @@ def default():
         output='segmented_houses.tif',
     )
 
-    sam.raster_to_vector("segmented_houses.tif", "segmented_houses.shp")
+    SAM.raster_to_vector("segmented_houses.tif", "segmented_houses.shp")
     logging.info('returning polygons')
 
     return send_file("/segmented_houses.tif", attachment_filename="segmented_houses.tif")
+
+
 
 
 def check_request_parameters(request_arg):
