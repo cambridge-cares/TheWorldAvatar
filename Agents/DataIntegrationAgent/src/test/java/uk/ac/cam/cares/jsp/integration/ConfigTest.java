323fdcd98253776f010e5f61d284efcfc4d2ddfd
JYAN01 <jy470@cam.ac.uk>
2023-07-11 11:20:07 +0800

    unit test of config

--- Agents/DataIntegrationAgent/src/test/java/uk/ac/cam/cares/jsp/integration/ConfigTest.java
+++ Agents/DataIntegrationAgent/src/test/java/uk/ac/cam/cares/jsp/integration/ConfigTest.java
@@ -1,11 +1,14 @@
 package uk.ac.cam.cares.jsp.integration;
 
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 public class ConfigTest {
     @Test
-    void testRetrieveSQLConfig() {
-
+    void testRetrieveSQLConfig() throws IOException {
+        String[] configInfo = Config.retrieveSQLConfig();
+        System.out.println(configInfo);
+        
     }
 
 }
