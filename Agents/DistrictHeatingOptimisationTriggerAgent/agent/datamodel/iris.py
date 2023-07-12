abc5c4e76a4438ffb4727ba66147672d7bf1c768
Markus Hofmeister <mh807@cam.ac.uk>
2023-07-12 08:40:13 +0800

    dev-district-heating-trigger-agent: Updated data model

---
+++ Agents/DistrictHeatingOptimisationTriggerAgent/agent/datamodel/iris.py
@@ -0,0 +1,31 @@
+# Provide Constants/IRIs for KG queries/updates
+
+ # Namespaces
+RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+XSD = "http://www.w3.org/2001/XMLSchema#"
+TIME = "http://www.w3.org/2006/time#"
+OD = 'https://www.theworldavatar.com/kg/ontodispersion/'
+
+# Data types
+RDF_TYPE = RDF + 'type'
+XSD_DECIMAL = XSD + 'decimal'
+
+# OntoDispersion
+OD_SIMULATION_TIME = OD + 'SimulationTime'
+
+### Time ontology ###
+TIME_INTERVAL = TIME + 'Interval'
+TIME_HASBEGINNING = TIME + 'hasBeginning'
+TIME_HASEND = TIME + 'hasEnd'
+TIME_INSTANT = TIME + 'Instant'
+TIME_INTIMEPOSITION = TIME + 'inTimePosition'
+TIME_TIMEPOSITION = TIME + 'TimePosition'
+TIME_HASTRS = TIME + 'hasTRS'
+TIME_NUMERICPOSITION = TIME + 'numericPosition'
+UNIX_TIME = "http://dbpedia.org/resource/Unix_time"
+TIME_DURATION = TIME + 'Duration'
+# Time units
+TIME_DAY = TIME + 'unitDay'
+TIME_HOUR = TIME + 'unitHour'
+TIME_MINUTE = TIME + 'unitMinute'
+TIME_SECOND = TIME + 'unitSecond'
