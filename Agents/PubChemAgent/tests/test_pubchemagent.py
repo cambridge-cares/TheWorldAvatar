15747ce539aa2dbdeaa97e695bee06db3fb0cf3b
Andrea <ao559@cam.ac.uk>
2023-07-14 11:37:15 +0100

    dev-TestAditionToPubChem: It checks property of molecular weight.(l 17-21)

--- Agents/PubChemAgent/tests/test_pubchemagent.py
+++ Agents/PubChemAgent/tests/test_pubchemagent.py
@@ -4,18 +4,21 @@ import pytest
 import os
 import json
 
+
 def test_pug_request():
     pug_access = pug_api()
     data = pug_access.pug_request('InChI','InChI=1S/C24H10/c1-5-15-13-16(6-2)20-11-12-22-18(8-4)14-17(7-3)21-10-9-19(15)23(20)24(21)22/h1-4,9-14H')
-    assert pug_access.get_cid(data)['cid'] == 11630783
-    assert pug_access.get_props(data)['Molecular Formula'] == 'C24H10'
-    # Test the functionality of the request with SMILES
-    smiles =  pug_access.get_props(data)['Canonical SMILES']
-    print('Before the call ', smiles)
-    data = pug_access.pug_request('SMILES',smiles)
-    print('After the call ', pug_access.get_props(data)['Canonical SMILES'])
-    assert pug_access.get_cid(data)['cid'] == 11630783
-    assert pug_access.get_props(data)['Molecular Formula'] == 'C24H10'
+    cid, comp_props, identifiers = pug_access.get_props(data)
+    assert cid == 11630783
+    for i in identifiers:
+        if identifiers[i]['key'] == 'MolecularFormula':
+            MF = identifiers[i]['value'] 
+    assert MF == 'C24H10'
+    for i in comp_props:
+        if comp_props[i]['key'] == 'MolecularWeight':
+            MF = comp_props[i]['value'] 
+            MW = MF['value']
+    assert MW == 298.3
 
 def test_from_textfile():
     inchi = 'inchi string'
