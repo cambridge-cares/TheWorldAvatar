e40229375004b42b4d6171b4d29f0da26c426979
Markus Hofmeister <mh807@cam.ac.uk>
2023-07-17 10:24:08 +0800

    dev-district-heating-trigger-agent: Update datamodel

--- Agents/DistrictHeatingOptimisationTriggerAgent/agent/datamodel/iris.py
+++ Agents/DistrictHeatingOptimisationTriggerAgent/agent/datamodel/iris.py
@@ -5,6 +5,7 @@ RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
 XSD = "http://www.w3.org/2001/XMLSchema#"
 TIME = "http://www.w3.org/2006/time#"
 OD = 'https://www.theworldavatar.com/kg/ontodispersion/'
+TS = 'https://www.theworldavatar.com/kg/ontotimeseries/'
 KB = 'https://www.theworldavatar.com/kg/pirmasens/'
 DERIVATION_INSTANCE_BASE_URL = 'https://www.theworldavatar.com/kg/derivation/'
 
@@ -14,6 +15,9 @@ XSD_DECIMAL = XSD + 'decimal'
 
 # OntoDispersion
 OD_SIMULATION_TIME = OD + 'SimulationTime'
+
+# OntoTimeSeries
+TS_FREQUENCY = TS + 'Frequency'
 
 ### Time ontology ###
 TIME_INTERVAL = TIME + 'Interval'
@@ -26,3 +30,9 @@ TIME_HASTRS = TIME + 'hasTRS'
 TIME_NUMERICPOSITION = TIME + 'numericPosition'
 UNIX_TIME = "http://dbpedia.org/resource/Unix_time"
 TIME_DURATION = TIME + 'Duration'
+TIME_NUMERICDURATION = TIME + 'numericDuration'
+TIME_UNIT_TYPE = TIME + 'unitType'
+TIME_UNIT_DAY = TIME + 'unitDay'
+TIME_UNIT_HOUR = TIME + 'unitHour'
+TIME_UNIT_MINUTE = TIME + 'unitMinute'
+TIME_UNIT_SECOND = TIME + 'unitSecond'
