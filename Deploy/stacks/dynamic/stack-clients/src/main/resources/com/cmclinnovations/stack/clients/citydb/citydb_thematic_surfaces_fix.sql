24c15194ee630e04895f225e230b6a85fdcdc5ba
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-07-17 18:25:40 +0100

    dev-3DCityDB-in-stack: Added extra filter to the citydb_thematic_surfaces_fix query to prevent the adding of thematic surfaces without geometries.

--- Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/citydb/citydb_thematic_surfaces_fix.sql
+++ Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/citydb/citydb_thematic_surfaces_fix.sql
@@ -1,4 +1,4 @@
 INSERT INTO citydb.thematic_surface(id, objectclass_id, building_id, lod2_multi_surface_id, lod3_multi_surface_id, lod4_multi_surface_id)
 SELECT co.id, b.objectclass_id, co.id AS building_id, COALESCE(b.lod2_multi_surface_id, b.lod3_multi_surface_id) AS lod2_multi_surface_id, b.lod3_multi_surface_id, b.lod4_multi_surface_id
 FROM citydb.cityobject co JOIN citydb.building b USING (id, objectclass_id)
-WHERE NOT EXISTS(SELECT 1 FROM citydb.thematic_surface ts WHERE co.id = ts.id);
+WHERE NOT EXISTS(SELECT 1 FROM citydb.thematic_surface ts WHERE co.id = ts.id) AND COALESCE(b.lod2_multi_surface_id, b.lod3_multi_surface_id) IS NOT NULL;
