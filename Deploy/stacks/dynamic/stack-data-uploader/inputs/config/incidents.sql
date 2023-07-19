6e2b452a2cd9e05554e381fb5fe455b4160cbedd
Ushcode <opeppard@cmcl.io>
2023-07-19 11:00:15 +0100

    pub-and-road-mapping: move project specific files to examples

--- Deploy/stacks/dynamic/stack-data-uploader/inputs/config/incidents.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    ogc_fid,
-    date as name,
-    geom,
-    'http://www.w3.org/2012/7/ra3.owl#RoadAccident/' || ogc_fid as iri
-    
-FROM
-    road_incidents
--- Deploy/stacks/dynamic/stack-data-uploader/inputs/config/pubs.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    ogc_fid,
-    name,
-    geom,
-    'http://theworldavatar.com/ontology/pubontology.owl#PublicHouse/' || ogc_fid as iri,
-    'http://traffic-blazegraph:8080/blazegraph/namespace/tboxcsv/sparql' AS endpoint
-FROM
-    open_pubs
