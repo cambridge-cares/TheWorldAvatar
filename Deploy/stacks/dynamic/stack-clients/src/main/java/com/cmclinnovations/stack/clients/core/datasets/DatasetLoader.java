bdc406c3a7862c23676254f94a3d99837a618904
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-07-20 14:46:18 +0100

    dev-ontop-per-db: Updated the ```DatasetLoader::loadData``` method so that it creates a new Ontop container for each Postgres database that has an OBDA file.
    (cherry picked from commit 8df514448889902092d811ac76da3e26955c8681)
    
    # Conflicts:
    #	Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/DatasetLoader.java

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/DatasetLoader.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/DatasetLoader.java
@@ -1,6 +1,7 @@
 package com.cmclinnovations.stack.clients.core.datasets;
 
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -12,11 +13,16 @@ import org.jgrapht.graph.DirectedAcyclicGraph;
 
 import com.cmclinnovations.stack.clients.blazegraph.BlazegraphClient;
 import com.cmclinnovations.stack.clients.blazegraph.Namespace;
+import com.cmclinnovations.stack.clients.core.EndpointNames;
 import com.cmclinnovations.stack.clients.core.StackClient;
 import com.cmclinnovations.stack.clients.geoserver.GeoServerClient;
 import com.cmclinnovations.stack.clients.ontop.OntopClient;
 import com.cmclinnovations.stack.clients.postgis.PostGISClient;
 import com.cmclinnovations.stack.clients.utils.FileUtils;
+import com.cmclinnovations.stack.services.OntopService;
+import com.cmclinnovations.stack.services.ServiceManager;
+import com.cmclinnovations.stack.services.config.Connection;
+import com.cmclinnovations.stack.services.config.ServiceConfig;
 import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -24,6 +30,8 @@ public class DatasetLoader {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
     private static final Path configPath = Path.of("/inputs/config");
+
+    private static final ServiceManager serviceManager = new ServiceManager(false);
 
     private DatasetLoader() {
     }
@@ -127,8 +135,27 @@ public class DatasetLoader {
 
             dataSubsets.forEach(subset -> subset.load(dataset));
 
-            OntopClient ontopClient = OntopClient.getInstance();
-            dataset.getOntopMappings().forEach(mapping -> ontopClient.updateOBDA(directory.resolve(mapping)));
+            List<String> ontopMappings = dataset.getOntopMappings();
+            if (!ontopMappings.isEmpty()) {
+
+                String newOntopServiceName = EndpointNames.ONTOP + "-" + dataset.getName();
+
+                ServiceConfig newOntopServiceConfig = serviceManager.duplicateServiceConfig(EndpointNames.ONTOP,
+                        newOntopServiceName);
+
+                newOntopServiceConfig.setEnvironmentVariable(OntopService.ONTOP_DB_NAME, dataset.getDatabase());
+                newOntopServiceConfig.getEndpoints()
+                        .replaceAll((endpointName, connection) -> new Connection(
+                                connection.getUrl(),
+                                connection.getUri(),
+                                URI.create(connection.getExternalPath().toString()
+                                        .replace(EndpointNames.ONTOP, newOntopServiceName))));
+
+                serviceManager.initialiseService(StackClient.getStackName(), newOntopServiceName);
+
+                OntopClient ontopClient = OntopClient.getInstance(newOntopServiceName);
+                ontopMappings.forEach(mapping -> ontopClient.updateOBDA(directory.resolve(mapping)));
+            }
         }
     }
 
