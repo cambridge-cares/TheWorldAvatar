eceb225f9771bf6978da03fb8b3514f89d2fb059
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-07-25 10:43:50 +0100

    dev-3DCityDB-in-stack: Added more options to the citydb_fudge_thematic_surfaces query.

--- Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/citydb/citydb_fudge_thematic_surfaces.sql
+++ Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/citydb/citydb_fudge_thematic_surfaces.sql
@@ -1,5 +1,36 @@
-INSERT INTO citydb.thematic_surface(id, objectclass_id, building_id, lod2_multi_surface_id, lod3_multi_surface_id, lod4_multi_surface_id)
-SELECT co.id, b.objectclass_id, co.id AS building_id, COALESCE(b.lod2_multi_surface_id, b.lod3_multi_surface_id) AS lod2_multi_surface_id, NULL, NULL
-FROM citydb.cityobject co JOIN citydb.building b USING (id, objectclass_id)
-WHERE NOT EXISTS(SELECT 1 FROM citydb.thematic_surface ts WHERE co.id = ts.id) AND COALESCE(b.lod2_multi_surface_id, b.lod3_multi_surface_id) IS NOT NULL
+INSERT INTO citydb.thematic_surface(
+        id,
+        objectclass_id,
+        building_id,
+        lod2_multi_surface_id
+    )
+SELECT b.id,
+    b.objectclass_id,
+    b.id AS building_id,
+    COALESCE(
+        b.lod2_multi_surface_id,
+        b.lod3_multi_surface_id,
+        b.lod4_multi_surface_id,
+        b.lod1_multi_surface_id,
+        b.lod2_solid_id,
+        b.lod3_solid_id,
+        b.lod4_solid_id,
+        b.lod1_solid_id
+    ) AS lod2_multi_surface_id
+FROM citydb.building b
+WHERE NOT EXISTS(
+        SELECT 1
+        FROM citydb.thematic_surface ts
+        WHERE b.id = ts.id
+    )
+    AND COALESCE(
+        b.lod2_multi_surface_id,
+        b.lod3_multi_surface_id,
+        b.lod4_multi_surface_id,
+        b.lod1_multi_surface_id,
+        b.lod2_solid_id,
+        b.lod3_solid_id,
+        b.lod4_solid_id,
+        b.lod1_solid_id
+    ) IS NOT NULL
 RETURNING id;
