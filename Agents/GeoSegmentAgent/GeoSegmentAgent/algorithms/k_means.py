2a846717fa963d4380cd90aef10e047e0e3aedbf
mingchuantian812 <mt933@cam.ac.uk>
2023-08-01 17:35:15 +0800

    1604-develop-geosegmentagent: Add functions to process and instantiate band labels for raster data. Updated the OBDA Mapping.

--- Agents/GeoSegmentAgent/GeoSegmentAgent/algorithms/k_means.py
+++ Agents/GeoSegmentAgent/GeoSegmentAgent/algorithms/k_means.py
@@ -3,7 +3,7 @@ from sklearn.cluster import KMeans
 import rasterio
 from rasterio.features import shapes
 import geopandas as gpd
-import logging, os, uuid
+import logging, os, uuid, json
 
 def raster_to_clusters(input_file, n_clusters):
     with rasterio.open("./GeoSegmentAgent/raster_input/" + input_file) as src:
@@ -32,7 +32,7 @@ def raster_to_clusters(input_file, n_clusters):
 
         return averages, labels_reshaped
 
-def raster_to_geojson(input_file_labels, averages, output_file):
+def raster_to_geojson(input_file_labels, averages, output_file, value_to_object_dict):
     with rasterio.open(input_file_labels) as src_labels:
         # read the raster data
         data_labels = src_labels.read(1)
@@ -45,13 +45,27 @@ def raster_to_geojson(input_file_labels, averages, output_file):
                             'Feature': 'http://www.opengis.net/ont/geosparql#Feature/' + str(int(v)) + str(uuid.uuid4()),
                             'Segmentation':'https://www.theworldavatar.com/kg/Segmentation/' + str(int(v)) + str(uuid.uuid4()),
                             'SegmentationModel':'KMeansClusteringModel',
+                            'representsRealWorldObject': 'placeholder',
                             }, 'geometry': s}
             for (s, v), _
             in zip(shapes(data_labels, mask=None, transform=src_labels.transform), np.ndindex(data_labels.shape)))
 
+        if value_to_object_dict != None:
+            # Converts the input parameter string to a list of dict
+            # E.g., '[{"10":"tree"}, {"20":"building"}, {"30":"road"}]' to a list of dict
+            results = add_real_world_object(list(results), value_to_object_dict)
+
         # write the shapes to a geojson file
+        logging.error(results)  # <--- check here what is in the results
         geoms = list(results)
         gpd_polygonized_raster = gpd.GeoDataFrame.from_features(geoms, crs=src_labels.crs)
         gpd_polygonized_raster.to_file(output_file, driver='GeoJSON')
 
+
+def add_real_world_object(results, value_to_object_dict):
+    for result in results:
+        mean_of_cluster = result['properties']['meanOfCluster']
+        closest_key = min(value_to_object_dict.keys(), key=lambda k: abs(k - mean_of_cluster))
+        result['properties']['representsRealWorldObject'] = value_to_object_dict[closest_key]
+    return results
 
