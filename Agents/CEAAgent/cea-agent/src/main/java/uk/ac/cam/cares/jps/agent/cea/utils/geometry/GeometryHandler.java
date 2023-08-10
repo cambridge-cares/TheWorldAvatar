c8d97ca4e67da0e1f3cc8912e64932451460c90a
Yi-Kai Tsai <yikai.tsai@cares.cam.ac.uk>
2023-08-10 11:01:55 +0800

    1628-dev-cea-agent-to-work-with-building-geometry-in-postgis: update extractFootprint to work with geometry strings stored as wktLiteral and remove getGroundGeometry"

--- Agents/CEAAgent/cea-agent/src/main/java/uk/ac/cam/cares/jps/agent/cea/utils/geometry/GeometryHandler.java
+++ Agents/CEAAgent/cea-agent/src/main/java/uk/ac/cam/cares/jps/agent/cea/utils/geometry/GeometryHandler.java
@@ -24,77 +24,6 @@ import java.util.Set;
 
 public class GeometryHandler {
     /**
-     * Finds footprint of building from array of building surfaces by searching for constant minimum z in geometries
-     * NB. On data TSDA has run on, the thematic surface is labelled with a ground surface id so this is not required
-     * @param surfaceArray array of building surfaces
-     * @return ground geometry in a JSONArray
-     */
-    public static JSONArray getGroundGeometry(JSONArray surfaceArray) {
-        ArrayList<Integer> ind = new ArrayList<>();
-        ArrayList<Double> z;
-        Double minZ = Double.MAX_VALUE;
-        double eps = 0.5;
-        boolean flag;
-        String geom;
-        String[] split;
-
-        for (int i = 0; i < surfaceArray.length(); i++) {
-            geom = surfaceArray.getJSONObject(i).get("geometry").toString();
-            split = geom.split("#");
-
-            z = new ArrayList<>();
-            z.add(Double.parseDouble(split[2]));
-
-            flag = true;
-
-            for (int j = 5; j < split.length; j += 3) {
-                for (int k = 0;  k < z.size(); k++) {
-                    if (Math.abs(Double.parseDouble(split[j]) - z.get(k)) > eps) {
-                        flag = false;
-                        break;
-                    }
-                }
-
-                if (flag){
-                    z.add(Double.parseDouble(split[j]));
-                }
-                else{
-                    break;
-                }
-            }
-
-            if (!flag){
-                ind.add(i);
-            }
-            else{
-                if (z.get(0) < minZ){minZ = z.get(0);}
-            }
-        }
-
-        // gets rid of geometry without constant z (up to eps tolerance)
-        for (int i = ind.size() - 1; i >= 0; i--) {
-            surfaceArray.remove(ind.get(i));
-        }
-
-        int i = 0;
-
-        // gets rid of geometry without minimum z value (up to eps tolerance)
-        while (i < surfaceArray.length()){
-            geom = surfaceArray.getJSONObject(i).get("geometry").toString();
-            split = geom.split("#");
-
-            if (Double.parseDouble(split[2]) > minZ + eps) {
-                surfaceArray.remove(i);
-            }
-            else {
-                i++;
-            }
-        }
-
-        return surfaceArray;
-    }
-
-    /**
      * Extracts the footprint of the building from its ground surface geometries
      * @param surfaceArray JSONArray of the query results for ground surface geometries
      * @return footprint as a string
@@ -116,9 +45,9 @@ public class GeometryHandler {
             if (datatype.contains("wktLiteral")) {
                 return surfaceArray.getJSONObject(0).get("geometry").toString();
             }
-            stringToGeometries(surfaceArray.getJSONObject(0).get("geometry").toString(), surfaceArray.getJSONObject(0).get("datatype").toString(), exteriors, holes);
-            exteriorPolygon = exteriors.get(0);
-            exteriorRing = exteriorPolygon.getExteriorRing();
+            else{
+                throw new JPSRuntimeException("Geometry is not stored as geosparql:wktLiteral");
+            }
         }
         else {
             for (int i = 0; i < surfaceArray.length(); i++) {
@@ -129,14 +58,12 @@ public class GeometryHandler {
                         for (int j = 0; j < polygon.getNumInteriorRing(); j++) {
                             holes.add(polygon.getInteriorRingN(j));
                         }
-
                     }
                     catch (ParseException e) {
                         e.printStackTrace();
                         throw new JPSRuntimeException(e);
                     }
                 }
-                stringToGeometries(surfaceArray.getJSONObject(i).get("geometry").toString(), surfaceArray.getJSONObject(i).get("datatype").toString(), exteriors, holes);
             }
 
             GeometryCollection geoCol = (GeometryCollection) geometryFactory.buildGeometry(exteriors);
@@ -177,84 +104,6 @@ public class GeometryHandler {
             LinearRing[] holeRings = holes.toArray(new LinearRing[holes.size()]);
             return geometryFactory.createPolygon(exteriorRing, holeRings).toString();
         }
-    }
-
-    /**
-     * Creates a polygon with the given points
-     * @param points points of the polygon as a string
-     * @return a polygon
-     */
-    public static Geometry toPolygon(String points) {
-        int ind = 0;
-        GeometryFactory geometryFactory = new GeometryFactory();
-
-        String[] arr = points.split("#");
-
-        Coordinate[] coordinates = new Coordinate[(arr.length) / 3];
-
-        for (int i = 0; i < arr.length; i += 3){
-            coordinates[ind] = new Coordinate(Double.valueOf(arr[i]), Double.valueOf(arr[i+1]), Double.valueOf(arr[i+2]));
-            ind++;
-        }
-
-        return geometryFactory.createPolygon(coordinates);
-    }
-
-    /**
-     * Converts geometry strings to geometry objects
-     * @param geometry geometry string
-     * @param polygonType string describing the polygon type of geometry, in the format of POLYGON-3-15
-     * @param exteriors list to store the exterior geometry
-     * @param holes list to store the hole geometries
-     */
-    public static void stringToGeometries(String geometry, String polygonType, List<Polygon> exteriors, List<LinearRing> holes) {
-        GeometryFactory geometryFactory = new GeometryFactory();
-        int ind = 0;
-        int k = 0;
-
-        String[] points = geometry.split("#");
-        String[] split = polygonType.split("-");
-
-        int num = Integer.parseInt(split[1]);
-
-        // exterior ring
-        Coordinate[] exterior = new Coordinate[Integer.parseInt(split[2])/num];
-
-        while (ind < Integer.parseInt(split[2])) {
-            exterior[k] = new Coordinate(Double.parseDouble(points[ind]), Double.parseDouble(points[ind+1]));
-            ind += 3;
-            k++;
-        }
-        exteriors.add(geometryFactory.createPolygon(exterior));
-
-        int sum = Integer.parseInt(split[2]);
-        // holes
-        for (int i = 3; i < split.length; i++) {
-            Coordinate[] hole = new Coordinate[Integer.parseInt(split[i])/num];
-            k = 0;
-            sum += Integer.parseInt(split[i]);
-            while (ind < sum) {
-                hole[k] = new Coordinate(Double.parseDouble(points[ind]), Double.parseDouble(points[ind+1]));
-                ind += 3;
-                k++;
-            }
-            holes.add(geometryFactory.createLinearRing(hole));
-        }
-    }
-
-    /**
-     * Converts an array of coordinates into a string
-     * @param coordinates array of footprint coordinates
-     * @return coordinates as a string
-     */
-    public static String coordinatesToString(Coordinate[] coordinates) {
-        String output = "";
-
-        for (int i = 0; i < coordinates.length; i++){
-            output = output + "#" + Double.toString(coordinates[i].getX()) + "#" + Double.toString(coordinates[i].getY()) + "#" + Double.toString(coordinates[i].getZ());
-        }
-
-        return output.substring(1, output.length());
     }
 
     /**
