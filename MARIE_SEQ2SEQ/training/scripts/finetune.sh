a2a2bb422829bbcf0d0d100fa03d7514ab7546b9
Dan Tran <nmdt2@cam.ac.uk>
2023-08-11 11:00:11 +0800

    dev-marie-plm: Update finetune script template to do eval and load best model at end

--- MARIE_SEQ2SEQ/training/scripts/finetune.sh
+++ MARIE_SEQ2SEQ/training/scripts/finetune.sh
@@ -1,11 +1,18 @@
 python finetune.py \
     --model_path google/flan-t5-base \
-    --data_path ./data/train.json \
+    --train_data_path ./data/train.json \
+    --eval_data_path ./data/dev.json \
     --output_dir /rds/user/nmdt2/hpc-work/outputs \
     --do_train \
+    --do_eval \
     --per_device_train_batch_size 16 \
     --num_train_epochs 3 \
     --optim paged_adamw_32bit \
     --learning_rate 0.0002 \
     --logging_steps 1 \
-    --report_to wandb
+    --evaluation_strategy steps \
+    --eval_steps 10 \
+    --load_best_model_at_end \
+    --save_total_limit 2 \
+    --save_strategy no \
+    --report_to wandb 
--- MARIE_SEQ2SEQ/training/scripts/inference.sh
+++ MARIE_SEQ2SEQ/training/scripts/inference.sh
@@ -1,4 +1,4 @@
 python inference.py \
     --model_path /rds/user/nmdt2/hpc-work/outputs/model \
-    --data_path ./data/test.json \
+    --eval_data_path ./data/test.json \
     --out_file ./predictions.json
