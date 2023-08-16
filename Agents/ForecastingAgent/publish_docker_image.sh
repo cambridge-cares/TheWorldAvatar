7d8098b55bc182d98c50330795b2702720b756a4
markushofmeister <mh807@cam.ac.uk>
2023-08-16 01:04:23 +0000

    dev-forecast-derivation-agent: Updated docker-compose to be tailored for standalone deployment

--- Agents/ForecastingAgent/publish_docker_image.sh
+++ Agents/ForecastingAgent/publish_docker_image.sh
@@ -6,9 +6,9 @@
 #
 AUTHOR="Jiaru Bai <jb2197@cam.ac.uk>"
 SPATH="$( cd  "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-AGENT_NAME='forecasting_agent_debug'
+AGENT_NAME='forecasting_agent'
 CARES_GITHUB='ghcr.io/cambridge-cares/'
-DOCKER_COMPOSE_FILE='docker-compose.debug.yml'
+DOCKER_COMPOSE_FILE='docker-compose.yml'
 STEP_NR=1
 NEXT_VERSION=''
 IMAGE_TAG=''
