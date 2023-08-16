49cb886f288228ccad98219a9f2bbe634114a7b7
qhouyee <quekhouyee@gmail.com>
2023-08-16 09:53:58 +0800

    dev-dashboard-agent: Refactor addSpaceBetweenCapitalWords method to ensure that abbreviations are excluded from the process

--- Agents/DashboardAgent/agent/src/main/java/uk/ac/cam/cares/jps/agent/dashboard/utils/StringHelper.java
+++ Agents/DashboardAgent/agent/src/main/java/uk/ac/cam/cares/jps/agent/dashboard/utils/StringHelper.java
@@ -13,6 +13,15 @@ public class StringHelper {
 
     /**
      * Add space between each word, which is defined by having the first letter be capital. For eg, MyTestCase will return My Test Case.
+     * Note that abbreviations will not be separated.
      */
-    public static String addSpaceBetweenCapitalWords(String name) {return name.replaceAll("(.)([A-Z])", "$1 $2");}
+    public static String addSpaceBetweenCapitalWords(String input) {
+        // Add a space before an uppercase letter followed by a lowercase letter
+        String result = input.replaceAll("([a-z])([A-Z])", "$1 $2");
+        // Add a space only if capital letters are followed by lower case.
+        // Else keep the capital letters together as they are abbreviations: Example ExhaustVAV = Exhaust VAV
+        // See test cases for more information
+        result = result.replaceAll("([A-Z])([A-Z][a-z])", "$1 $2");
+        return result;
+    }
 }
