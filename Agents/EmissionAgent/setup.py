2ce7b18feac13cd16d754762d539fb8698d0c06c
Markus Hofmeister <mh807@cam.ac.uk>
2023-08-21 13:27:58 +0800

    dev-create-EmissionAgent: Initial commit of setup and requirements file

---
+++ Agents/EmissionAgent/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_namespace_packages 
+
+setup(
+    name='emissionagent',
+    version='1.0.0',
+    author='Markus Hofmeister',
+    author_email='mh807@cam.ac.uk',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_namespace_packages(exclude=['tests','tests.*']),
+    description="The `Emission Agent` converts consumed gas and generated heat amounts into associated emission values.",
+    url="https://github.com/cambridge-cares/TheWorldAvatar/tree/main/Agents/EmissionAgent/",
+    python_requires='>=3.7',
+    include_package_data=True,
+    install_requires= [
+        'flask~=2.1.0',
+        'py4jps~=1.0.37', 
+        'pyderivationagent~=1.5.0'
+    ],
+    extras_require={
+        "dev": [
+            "testcontainers==3.7.0",
+            "pytest==7.2.0",
+            "pytest-docker-compose==3.2.1",
+            "pytest-rerunfailures==10.2",
+            "psycopg2==2.9.5"
+        ],
+    }
+)
