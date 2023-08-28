637f5b447ff7edc3c08ffe79cab6be1234560745
Kok Foong Lee <kokfoong.lee@cares.cam.ac.uk>
2023-08-28 18:17:50 +0800

    dev-aermod-vis: copy to container directly instead to volume

--- JPS_VIRTUALSENSOR/DispersionVis/copy_vis_file.sh
+++ JPS_VIRTUALSENSOR/DispersionVis/copy_vis_file.sh
@@ -5,8 +5,8 @@ copy_done=false
 while [ "$copy_done" = false ];
 do
 	timeout 10 sleep 10
-	for container_name in $(docker ps --format "{{.Names}}" --filter name="aermod-agent"); do
-		docker cp . "$container_name":/vis_data
+	for container_name in $(docker ps --format "{{.Names}}" --filter name="dispersion-vis"); do
+		docker cp . "$container_name":/var/www/html
 		copy_done=true
 		echo "$container_name"
 	done
