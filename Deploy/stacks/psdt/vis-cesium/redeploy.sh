d8e4c5e510c2028296f82fbdf42e7c746ce37a83
Sebastian Mosbach <sm453@cam.ac.uk>
2023-09-01 08:33:49 +0000

    dev-psdt: Removed deprecated hyphen from docker compose commands in visualisation redeploy script.

--- Deploy/stacks/psdt/vis-cesium/redeploy.sh
+++ Deploy/stacks/psdt/vis-cesium/redeploy.sh
@@ -2,5 +2,5 @@
 
 # Removes the visualisation container, rebuilds the image, and deploys the container.
 
-docker-compose -p psdt-vis-cesium -f ./docker/docker-compose.yml build --force-rm
-docker-compose -p psdt-vis-cesium -f ./docker/docker-compose.yml up -d --force-recreate
+docker compose -p psdt-vis-cesium -f ./docker/docker-compose.yml build --force-rm
+docker compose -p psdt-vis-cesium -f ./docker/docker-compose.yml up -d --force-recreate
