603bc5659667fa97daccd27ce76774435a239bca
Dan Tran <nmdt2@cam.ac.uk>
2023-09-06 11:15:07 +0800

    dev-marie-plm: Add minimal home webpage

--- MARIE_SEQ2SEQ/deployment/flask/README.md
+++ MARIE_SEQ2SEQ/deployment/flask/README.md
@@ -16,6 +16,11 @@ Start the development server at `localhost:5000`.
 python app.py
 ```
 
+To enter debug mode (app is automatically reloaded upon code changes)
+```
+flask --app app.py --debug run
+```
+
 ## Deployment
 
 See [Deployment for Marie](../README.md#deployment-for-marie).
--- MARIE_SEQ2SEQ/deployment/flask/app.py
+++ MARIE_SEQ2SEQ/deployment/flask/app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return "Hello World!"
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
