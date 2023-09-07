1ce7110a52bfb5e7a81d0556179c6c2a80d95a02
Yi-Kai Tsai <yikai.tsai@cares.cam.ac.uk>
2023-09-07 11:13:55 +0800

    1629-dev-osmagent: update updateLandUse to match all buildings without OSM usage and updateUsageShare to work with a separate usage table

--- Agents/OSMAgent/osmagent/src/main/java/uk/ac/cam/cares/jps/agent/osmagent/usage/UsageShareCalculator.java
+++ Agents/OSMAgent/osmagent/src/main/java/uk/ac/cam/cares/jps/agent/osmagent/usage/UsageShareCalculator.java
@@ -3,6 +3,7 @@ package uk.ac.cam.cares.jps.agent.osmagent.usage;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import uk.ac.cam.cares.jps.agent.osmagent.OSMAgent;
+import uk.ac.cam.cares.jps.agent.osmagent.geometry.object.GeoObject;
 import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 
 import java.io.IOException;
@@ -27,107 +28,42 @@ public class UsageShareCalculator {
          * @param user
          * @param password
          */
-        public static void updateUsageShare(String database, String user, String password, String pointTable,
-                        String polygonTable) {
+        public static void updateUsageShare(String database, String user, String password, String usageTable) {
                 RemoteRDBStoreClient rdbStoreClient = new RemoteRDBStoreClient(database, user, password);
-
-                String points = pointTable;
-                String polygons = polygonTable;
 
                 String add_uuid_ossp_Extension = "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";";
 
                 String assignUsageShare =
                                 // SQL to update polygons table
-                                "UPDATE " + polygons + " AS p\n" +
+                                "UPDATE " + usageTable + " AS p\n" +
                                                 "SET UsageShare = c.instances / c.total_instances::float,\n" +
-                                                "    propertyusage_iri = 'https://www.theworldavatar.com/kg/' || c.OntoBuilt || '_' || uuid_generate_v4()::text\n"
+                                                "    propertyusage_iri = 'https://www.theworldavatar.com/kg/' || c.ontobuilt || '_' || uuid_generate_v4()::text\n"
                                                 +
                                                 "FROM (\n" +
                                                 "    SELECT building_iri,\n" +
-                                                "           OntoBuilt,\n" +
+                                                "           ontobuilt,\n" +
                                                 "           COUNT(*) AS instances,\n" +
                                                 "           SUM(COUNT(*)) OVER (PARTITION BY building_iri) AS total_instances\n"
                                                 +
-                                                "    FROM (\n" +
-                                                "        SELECT building_iri, OntoBuilt FROM " + polygons + "\n" +
-                                                "        WHERE OntoBuilt IS NOT NULL AND building_iri IS NOT NULL\n" +
-                                                "        UNION ALL\n" +
-                                                "        SELECT building_iri, OntoBuilt FROM " + points + "\n" +
-                                                "        WHERE OntoBuilt IS NOT NULL AND building_iri IS NOT NULL\n" +
-                                                "    ) AS combined_data\n" +
-                                                "    GROUP BY building_iri, OntoBuilt\n" +
+                                                "    FROM " + usageTable + " \n" +
+                                                "    GROUP BY building_iri, ontobuilt\n" +
                                                 ") AS c\n" +
                                                 "WHERE p.building_iri = c.building_iri\n" +
-                                                "  AND p.OntoBuilt = c.OntoBuilt;\n" +
-
-                                                // SQL to update points table
-                                                "UPDATE " + points + " AS pt\n" +
-                                                "SET UsageShare = c.instances / c.total_instances::float,\n" +
-                                                "    propertyusage_iri = 'https://www.theworldavatar.com/kg/' || c.OntoBuilt || '_' || uuid_generate_v4()::text\n"
-                                                +
-                                                "FROM (\n" +
-                                                "    SELECT building_iri,\n" +
-                                                "           OntoBuilt,\n" +
-                                                "           COUNT(*) AS instances,\n" +
-                                                "           SUM(COUNT(*)) OVER (PARTITION BY building_iri) AS total_instances\n"
-                                                +
-                                                "    FROM (\n" +
-                                                "        SELECT building_iri, OntoBuilt FROM " + polygons + "\n" +
-                                                "        WHERE OntoBuilt IS NOT NULL AND building_iri IS NOT NULL\n" +
-                                                "        UNION ALL\n" +
-                                                "        SELECT building_iri, OntoBuilt FROM " + points + "\n" +
-                                                "        WHERE OntoBuilt IS NOT NULL AND building_iri IS NOT NULL\n" +
-                                                "    ) AS combined_data\n" +
-                                                "    GROUP BY building_iri, OntoBuilt\n" +
-                                                ") AS c\n" +
-                                                "WHERE pt.building_iri = c.building_iri\n" +
-                                                "  AND pt.OntoBuilt = c.OntoBuilt;";
+                                                "  AND p.ontobuilt = c.ontobuilt;";
 
                 String updatePropertyUsageStatement =
                                 // SQL to update polygons table
-                                "UPDATE " + polygons + " AS p\n" +
+                                "UPDATE " + usageTable + " AS p\n" +
                                                 "SET propertyusage_iri = subquery.min_propertyusage_iri\n" +
                                                 "FROM (\n" +
                                                 "    SELECT p.building_iri, p.ontobuilt, MIN(propertyusage_iri) AS min_propertyusage_iri\n"
                                                 +
-                                                "    FROM " + polygons + " AS p\n" +
+                                                "    FROM " + usageTable + " AS p\n" +
                                                 "    INNER JOIN (\n" +
                                                 "        SELECT building_iri, ontobuilt\n" +
-                                                "        FROM (\n" +
-                                                "            SELECT building_iri, OntoBuilt FROM " + polygons + "\n" +
-                                                "            WHERE OntoBuilt IS NOT NULL AND building_iri IS NOT NULL\n"
+                                                "        FROM " + usageTable + " \n" +
+                                                "            WHERE ontobuilt IS NOT NULL AND building_iri IS NOT NULL\n"
                                                 +
-                                                "            UNION ALL\n" +
-                                                "            SELECT building_iri, OntoBuilt FROM " + points + "\n" +
-                                                "            WHERE OntoBuilt IS NOT NULL AND building_iri IS NOT NULL\n"
-                                                +
-                                                "        ) AS combined_data\n" +
-                                                "    ) AS cd ON p.building_iri = cd.building_iri AND p.ontobuilt = cd.ontobuilt\n"
-                                                +
-                                                "    GROUP BY p.building_iri, p.ontobuilt\n" +
-                                                "    HAVING COUNT(*) > 1\n" +
-                                                ") AS subquery\n" +
-                                                "WHERE p.building_iri = subquery.building_iri\n" +
-                                                "    AND p.ontobuilt = subquery.ontobuilt;\n" +
-
-                                                // SQL to update points table
-                                                "UPDATE " + points + " AS p\n" +
-                                                "SET propertyusage_iri = subquery.min_propertyusage_iri\n" +
-                                                "FROM (\n" +
-                                                "    SELECT p.building_iri, p.ontobuilt, MIN(propertyusage_iri) AS min_propertyusage_iri\n"
-                                                +
-                                                "    FROM " + points + " AS p\n" +
-                                                "    INNER JOIN (\n" +
-                                                "        SELECT building_iri, ontobuilt\n" +
-                                                "        FROM (\n" +
-                                                "            SELECT building_iri, OntoBuilt FROM " + polygons + "\n" +
-                                                "            WHERE OntoBuilt IS NOT NULL AND building_iri IS NOT NULL\n"
-                                                +
-                                                "            UNION ALL\n" +
-                                                "            SELECT building_iri, OntoBuilt FROM " + points + "\n" +
-                                                "            WHERE OntoBuilt IS NOT NULL AND building_iri IS NOT NULL\n"
-                                                +
-                                                "        ) AS combined_data\n" +
                                                 "    ) AS cd ON p.building_iri = cd.building_iri AND p.ontobuilt = cd.ontobuilt\n"
                                                 +
                                                 "    GROUP BY p.building_iri, p.ontobuilt\n" +
@@ -145,13 +81,8 @@ public class UsageShareCalculator {
 
         }
 
-        public static void updateLandUse(String database, String user, String password, String pointTable,
-                        String polygonTable, String landUseTable) throws IOException {
-
+        public static void updateLandUse(String database, String user, String password, String usageTable, String landUseTable) throws IOException {
                 RemoteRDBStoreClient rdbStoreClient = new RemoteRDBStoreClient(database, user, password);
-
-                String points = pointTable;
-                String polygons = polygonTable;
 
                 InputStreamReader inputStreamReader = new InputStreamReader(
                                 UsageMatcher.class.getResourceAsStream("/dlm_landuse.csv"));
@@ -163,66 +94,23 @@ public class UsageShareCalculator {
                         String key = line[0];
                         String value = line[1];
 
-                        String updateLandusePoints = "UPDATE " + points + " AS p\n" +
-                                        "SET ontobuilt = '" + ontobuilt + "',\n" +
-                                        "propertyusage_iri = 'https://www.theworldavatar.com/kg/'||'" + ontobuilt
-                                        + "'||'_' || uuid_generate_v4()::text,\n" +
-                                        "usageShare =1\n" +
-                                        "\n" +
-                                        "FROM " + landUseTable + " AS d\n" +
-                                        "WHERE p.building_iri IS NOT NULL\n" +
-                                        "  AND p.ontobuilt IS NULL\n" +
-                                        "  AND NOT EXISTS (\n" +
-                                        "    SELECT 1\n" +
-                                        "    FROM " + points + " AS p2\n" +
-                                        "    WHERE p2.building_iri = p.building_iri\n" +
-                                        "      AND p2.ontobuilt IS NOT NULL\n" +
-                                        "  )" +
-                                        "  AND NOT EXISTS (\n" +
-                                        "    SELECT 1\n" +
-                                        "    FROM " + polygons + " AS p3  \n" +
-                                        "    WHERE p3.building_iri = p.building_iri\n" +
-                                        "      AND p3.ontobuilt IS NOT NULL\n" +
-                                        "  )" +
-                                        "  AND ST_Intersects(p.\"geometryProperty\", \n" +
-                                        "      ST_Transform((SELECT ST_Collect(wkb_geometry) \n" +
-                                        "                    FROM " + landUseTable + " \n" +
-                                        "                    WHERE \"" + key + "\" = '" + value + "'), 4326)\n" +
-                                        "      );";
+                        String updateLandUse = "INSERT INTO " + usageTable + " (building_iri, ontobuilt) \n" +
+                                "SELECT q2.iri, \'" + ontobuilt + "\' FROM \n" +
+                                "(SELECT q.urival AS iri, q.geometry AS geo, q.srid AS srid FROM \n" +
+                                "(" + GeoObject.getQuery() + ") AS q \n" +
+                                "LEFT JOIN " + usageTable + " u ON q.urival = u.building_iri \n" +
+                                "WHERE u.building_iri IS NULL) AS q2 \n" +
+                                "WHERE ST_Intersects(q2.geo, ST_Transform(\n" +
+                                "(SELECT ST_Collect(wkb_geometry) AS g FROM " + landUseTable + " \n" +
+                                "WHERE \"" + key + "\" = \'" + value +"\'), q2.srid))";
 
-                        String updateLandusePolygons = "UPDATE " + polygons + " AS p\n" +
-                                        "SET ontobuilt = '" + ontobuilt + "',\n" +
-                                        "propertyusage_iri = 'https://www.theworldavatar.com/kg/'||'" + ontobuilt
-                                        + "'||'_' || uuid_generate_v4()::text,\n" +
-                                        "usageShare =1\n" +
-                                        "\n" +
-                                        "FROM " + landUseTable + " AS d\n" +
-                                        "WHERE p.building_iri IS NOT NULL\n" +
-                                        "  AND p.ontobuilt IS NULL\n" +
-                                        "  AND NOT EXISTS (\n" +
-                                        "    SELECT 1\n" +
-                                        "    FROM " + polygons + " AS p2\n" +
-                                        "    WHERE p2.building_iri = p.building_iri\n" +
-                                        "      AND p2.ontobuilt IS NOT NULL\n" +
-                                        "  )" +
-                                        "  AND NOT EXISTS (\n" +
-                                        "    SELECT 1\n" +
-                                        "    FROM " + points + " AS p3  \n" +
-                                        "    WHERE p3.building_iri = p.building_iri\n" +
-                                        "      AND p3.ontobuilt IS NOT NULL\n" +
-                                        "  )" +
-                                        "  AND ST_Intersects(p.\"geometryProperty\", \n" +
-                                        "      ST_Transform((SELECT ST_Collect(wkb_geometry) \n" +
-                                        "                    FROM " + landUseTable + " \n" +
-                                        "                    WHERE \"" + key + "\" = '" + value + "'), 4326)\n" +
-                                        "      );";
-
-                        rdbStoreClient.executeUpdate(updateLandusePoints);
-                        rdbStoreClient.executeUpdate(updateLandusePolygons);
+                        rdbStoreClient.executeUpdate(updateLandUse);
                         System.out.println(
                                         "Untagged buildings with building_iri are assigned for " + key + " with value:"
                                                         + value + " under the ontobuiltenv:" + ontobuilt
                                                         + " category.");
+
+
                 }
 
                 System.out.println(
