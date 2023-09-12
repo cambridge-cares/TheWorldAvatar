cc9edd396531508e19fac9c671d33b81c1bfc494
Dan Tran <nmdt2@cam.ac.uk>
2023-09-12 17:13:30 +0800

    dev-marie-plm: Add more labels from OntoSpecies paper

--- MARIE_SEQ2SEQ/data_generation/data_generation/constants.py
+++ MARIE_SEQ2SEQ/data_generation/data_generation/constants.py
@@ -26,33 +26,73 @@ PROPERTIES = [
     if not isinstance(p, ValueLabelsEntry)
     else p
     for p in [
-        "AtomChiralCount",
-        "AtomChiralDefCount",
-        "AtomChiralUndefCount",
-        "BondChiralCount",
-        "BondChiralDefCount",
-        "BondChiralUndefCount",
+        ValueLabelsEntry(
+            value="AtomChiralCount",
+            labels=["atom chiral count", "atom stereocenter count"],
+        ),
+        ValueLabelsEntry(
+            value="AtomChiralDefCount",
+            labels=["atom chiral def count", "defined atom stereocenter count"],
+        ),
+        ValueLabelsEntry(
+            value="AtomChiralUndefCount",
+            labels=["atom chiral undef count", "undefined atom stereocenter count"],
+        ),
+        ValueLabelsEntry(
+            value="BondChiralCount",
+            labels=["bond chiral count", "bond stereocenter count"],
+        ),
+        ValueLabelsEntry(
+            value="BondChiralDefCount",
+            labels=["bond chiral def count", "defined bond stereocenter count"],
+        ),
+        ValueLabelsEntry(
+            value="BondChiralUndefCount",
+            labels=["bond chiral undef count", "undefined bond stereocenter count"],
+        ),
         "CanonicalizedCompound",
         "Charge",
         "CompoundComplexity",
-        "CovalentUnitCount",
+        ValueLabelsEntry(
+            value="CovalentUnitCount",
+            labels=["covalent unit count", "number of covalent units"],
+        ),
         "ExactMass",
-        "HeavyAtomCount",
-        "HydrogenBondAcceptorCount",
-        "HydrogenBondDonorCount",
+        ValueLabelsEntry(
+            value="HeavyAtomCount",
+            labels=["heavy atom count", "number of non-hydrogen atoms"],
+        ),
+        ValueLabelsEntry(
+            value="HydrogenBondAcceptorCount",
+            labels=[
+                "hydrogen bond acceptor count",
+                "number of hydrogen bond acceptors",
+            ],
+        ),
+        ValueLabelsEntry(
+            value="HydrogenBondDonorCount",
+            labels=["hydrogen bond donor count", "number of hydrogen bond donors"],
+        ),
         "IsotopeAtomCount",
-        "MolecularWeight",
-        "MonoIsotopicWeight",
+        ValueLabelsEntry(
+            value="MolecularWeight", labels=["molecular weight", "molecular mass"]
+        ),
+        ValueLabelsEntry(
+            value="MonoIsotopicWeight",
+            labels=["mono isotopic weight", "monoisotopic weight", "monoisotopic mass"],
+        ),
         "RotatableBondCount",
         "SubStructureKeysFingerprint",
-        "TautomerCount",
+        ValueLabelsEntry(
+            value="TautomerCount", labels=["tautomer count", "number of tautomers"]
+        ),
         ValueLabelsEntry(value="XLogP3", labels=["XLogP3", "XLOGP3"]),
         "AutoignitionTemperature",
-        ValueLabelsEntry(value="Caco2Permeability", labels=["CaCO2 permeability", "caco2 permeability"]),
+        ValueLabelsEntry(value="Caco2Permeability", labels=["Caco-2 permeability"]),
         "CollisionCrossSection",
         "Hydrophobicity",
         "IonizationPotential",
-        "IsoelectricPoint",
+        ValueLabelsEntry(value="IsoelectricPoint", labels=["isoelectric point", "IEP"]),
         ValueLabelsEntry(value="LogP", labels=["LogP", "logP", "Log P", "log P"]),
         ValueLabelsEntry(value="LogS", labels=["LogS", "logS", "Log S", "log S"]),
         "PolarSurfaceArea",
@@ -64,7 +104,7 @@ PROPERTIES = [
         "StandardEnthalpyOfFormation",
         "HeatOfCombustion",
         "HeatOfVaporization",
-        "HenrysLawConstant",
+        ValueLabelsEntry(value="HenrysLawConstant", labels=["Henry's law constant"]),
         "MeltingPoint",
         "OpticalRotation",
         "Solubility",
@@ -85,7 +125,12 @@ IDENTIFIERS = [
     ),
     ValueLabelsEntry(value="EmpiricalFormula", labels=["empirical formula"]),
     ValueLabelsEntry(
-        value="InChI", labels=["InChI", "International Chemical Identifier"]
+        value="InChI",
+        labels=[
+            "InChI",
+            "International Chemical Identifier",
+            "IUPAC International Chemical Identifier",
+        ],
     ),
     ValueLabelsEntry(value="InChIKey", labels=["InChIKey", "hashed InChI"]),
     ValueLabelsEntry(value="IUPACName", labels=["IUPAC name"]),
