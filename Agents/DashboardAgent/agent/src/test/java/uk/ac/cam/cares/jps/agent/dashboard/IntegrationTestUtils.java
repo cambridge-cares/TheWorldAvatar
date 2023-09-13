3af4602c4ca5d3038f55e023bfd9e1b0cfbbc52f
qhouyee <quekhouyee@gmail.com>
2023-09-13 15:59:26 +0800

    dev-dashboard-agent: Added integration test for creating and verifying data sources

--- Agents/DashboardAgent/agent/src/test/java/uk/ac/cam/cares/jps/agent/dashboard/IntegrationTestUtils.java
+++ Agents/DashboardAgent/agent/src/test/java/uk/ac/cam/cares/jps/agent/dashboard/IntegrationTestUtils.java
@@ -1,6 +1,6 @@
 package uk.ac.cam.cares.jps.agent.dashboard;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 
 import java.io.IOException;
@@ -8,14 +8,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.time.Duration;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class IntegrationTestUtils {
-    public static final String TEST_DASHBOARD_URL = "http://172.27.0.2:3000";
+    public static final String TEST_DASHBOARD_URL = "http://172.27.0.3:3000";
+    public static final String TEST_POSTGIS_JDBC = "jdbc:postgresql://172.27.0.2:5432/";
+    public static final String TEST_POSTGIS_URL = "http://172.27.0.2:5432/";
+    public static final String TEST_POSTGIS_USER = "user";
+    public static final String TEST_POSTGIS_PASSWORD = "pg123";
     public static final String NAME_KEY = "name";
     public static final String SERVICE_ACCOUNT_NAME = "grafana";
     private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
@@ -23,41 +27,88 @@ public class IntegrationTestUtils {
     private static final String SERVICE_ACCOUNT_SEARCH_SUB_ROUTE = "/search";
     private static final String DATA_SOURCE_ROUTE = "/api/datasources";
     private static final String ID_KEY = "id";
+    public static final String DASHBOARD_ACCOUNT_USER = "admin";
+    public static final String DASHBOARD_ACCOUNT_PASS = "admin";
 
-    public static void deleteServiceAccounts(String user, String password) {
+    public static Object retrieveServiceAccounts(String user, String password) {
         String route = TEST_DASHBOARD_URL + SERVICE_ACCOUNT_ROUTE;
-        List<Map<String, Object>> accountInfo = (List<Map<String, Object>>) retrieveServiceAccounts(user, password);
+        // Send a get request to target API for a response
+        HttpResponse response = sendGetRequest(route + SERVICE_ACCOUNT_SEARCH_SUB_ROUTE, user, password);
+        // Will always be a hash map
+        Map<String, Object> responseMap = (Map<String, Object>) retrieveResponseBody(response);
+        // Response will always contain service accounts key, even if there is no account
+        return responseMap.get("serviceAccounts");
+    }
+
+    public static JsonArray retrieveDataSources(String user, String password) {
+        String route = TEST_DASHBOARD_URL + DATA_SOURCE_ROUTE;
+        // Send a get request to target API for a response
+        HttpResponse response = sendGetRequest(route, user, password);
+        JsonArray responseMap = (JsonArray) retrieveResponseBody(response);
+        return responseMap;
+    }
+
+    public static void deleteServiceAccounts() {
+        String route = TEST_DASHBOARD_URL + SERVICE_ACCOUNT_ROUTE;
+        List<Map<String, Object>> accountInfo = (List<Map<String, Object>>) retrieveServiceAccounts(DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
         int accountId = -1;
         if (accountInfo.size() > 0 && accountInfo.get(0).get(NAME_KEY).equals(SERVICE_ACCOUNT_NAME)) {
             Double idDoubleFormat = (Double) accountInfo.get(0).get(ID_KEY);
             accountId = idDoubleFormat.intValue();
         }
         if (accountId != -1) {
-            sendDeleteRequest(route + "/" + accountId, user, password);
+            sendDeleteRequest(route + "/" + accountId, DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
         }
     }
 
-    public static Object retrieveServiceAccounts(String user, String password) {
-        String route = TEST_DASHBOARD_URL + SERVICE_ACCOUNT_ROUTE;
-        // Send a get request to target API for a response
-        HttpResponse response = sendGetRequest(route + SERVICE_ACCOUNT_SEARCH_SUB_ROUTE, user, password);
-        Map<String, Object> responseMap = retrieveResponseBodyAsMap(response);
-        // Response will always contain service accounts key, even if there is no account
-        return responseMap.get("serviceAccounts");
+    public static void deleteDataSources() {
+        List<Map<String, Object>> accountInfo = (List<Map<String, Object>>) retrieveServiceAccounts(DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
+        int accountId = -1;
+        if (accountInfo.size() > 0 && accountInfo.get(0).get(NAME_KEY).equals(SERVICE_ACCOUNT_NAME)) {
+            Double idDoubleFormat = (Double) accountInfo.get(0).get(ID_KEY);
+            accountId = idDoubleFormat.intValue();
+        }
+        if (accountId != -1) {
+            String route = TEST_DASHBOARD_URL + DATA_SOURCE_ROUTE;
+            HttpResponse response = sendGetRequest(route, DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
+            JsonArray dataSources = (JsonArray) retrieveResponseBody(response);
+            for (JsonElement dataSource : dataSources) {
+                if (dataSource.isJsonObject()) {
+                    JsonObject jsonObject = dataSource.getAsJsonObject();
+                    String id = jsonObject.get("uid").getAsString();
+                    sendDeleteRequest(route + "/uid/" + id, DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
+                }
+            }
+        }
+    }
+    
+    public static Connection connectDatabase(String jdbc) {
+        try {
+            return DriverManager.getConnection(jdbc, TEST_POSTGIS_USER, TEST_POSTGIS_PASSWORD);
+        } catch (Exception e) {
+            throw new JPSRuntimeException("Unable to connect to database: " + e.getMessage());
+        }
     }
 
-    public static Object retrieveDataSources(String user, String password) {
-        String route = TEST_DASHBOARD_URL + DATA_SOURCE_ROUTE;
-        // Send a get request to target API for a response
-        HttpResponse response = sendGetRequest(route, user, password);
-        Map<String, Object> responseMap = retrieveResponseBodyAsMap(response);
-        return responseMap;
+    public static void updateDatabase(Connection connection, String query) {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        } catch (Exception e) {
+            throw new JPSRuntimeException("Unable to execute updates: " + e.getMessage());
+        }
     }
 
-    public static Map<String, Object> retrieveResponseBodyAsMap(HttpResponse response) {
-        Gson gson = new Gson();
-        // Although the method returns the result as a HashMap, it can be abstracted to its parent Map object
-        return gson.fromJson(response.body().toString(), HashMap.class);
+    public static Object retrieveResponseBody(HttpResponse response) {
+        JsonElement jsonResponse = JsonParser.parseString(response.body().toString());
+        // When the response is a JSON Object
+        if (jsonResponse.isJsonObject()) {
+            Gson gson = new Gson();
+            // Return a hashmap parsed from it
+            return gson.fromJson(response.body().toString(), HashMap.class);
+        } else {
+            // When the response is a JSON array, return a JSON array
+            return jsonResponse.getAsJsonArray();
+        }
     }
 
     public static HttpResponse sendGetRequest(String url, String userName, String password) {
--- Agents/DashboardAgent/agent/src/test/java/uk/ac/cam/cares/jps/agent/dashboard/json/DashboardClientIntegrationTest.java
+++ Agents/DashboardAgent/agent/src/test/java/uk/ac/cam/cares/jps/agent/dashboard/json/DashboardClientIntegrationTest.java
@@ -1,24 +1,48 @@
 package uk.ac.cam.cares.jps.agent.dashboard.json;
 
+import com.google.gson.JsonArray;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
 import uk.ac.cam.cares.jps.agent.dashboard.IntegrationTestUtils;
 import uk.ac.cam.cares.jps.agent.dashboard.stack.StackClient;
 
+import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DashboardClientIntegrationTest {
-    private static final String DASHBOARD_ACCOUNT_USER = "admin";
-    private static final String DASHBOARD_ACCOUNT_PASS = "admin";
+    private static final String SAMPLE_SQL_DATABASE = "test";
+
+    @BeforeAll
+    static void setup() {
+        try (Connection conn = IntegrationTestUtils.connectDatabase(IntegrationTestUtils.TEST_POSTGIS_JDBC)) {
+            String dbCreationQuery = "CREATE DATABASE " + SAMPLE_SQL_DATABASE;
+            IntegrationTestUtils.updateDatabase(conn, dbCreationQuery);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to set up test databases: " + e.getMessage());
+        }
+    }
 
     @AfterEach
     void resetDashboard() {
-        IntegrationTestUtils.deleteServiceAccounts(DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
+        IntegrationTestUtils.deleteDataSources();
+        IntegrationTestUtils.deleteServiceAccounts();
+    }
+
+    @AfterAll
+    static void cleanUp() {
+        try (Connection conn = IntegrationTestUtils.connectDatabase(IntegrationTestUtils.TEST_POSTGIS_JDBC)) {
+            String dbCreationQuery = "DROP DATABASE IF EXISTS " + SAMPLE_SQL_DATABASE;
+            IntegrationTestUtils.updateDatabase(conn, dbCreationQuery);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to clean up databases: " + e.getMessage());
+        }
     }
 
     @Test
@@ -29,7 +53,7 @@ class DashboardClientIntegrationTest {
         })) {
             StackClient mockStackClient = new StackClient();
             // Execute method
-            assertNotNull(new DashboardClient(mockStackClient, DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS));
+            assertNotNull(new DashboardClient(mockStackClient, IntegrationTestUtils.DASHBOARD_ACCOUNT_USER, IntegrationTestUtils.DASHBOARD_ACCOUNT_PASS));
         }
     }
 
@@ -42,17 +66,17 @@ class DashboardClientIntegrationTest {
             Mockito.when(mock.getAllSpatialZones()).thenReturn(new String[]{});
         })) {
             StackClient mockStackClient = new StackClient();
-            DashboardClient client = new DashboardClient(mockStackClient, DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
+            DashboardClient client = new DashboardClient(mockStackClient, IntegrationTestUtils.DASHBOARD_ACCOUNT_USER, IntegrationTestUtils.DASHBOARD_ACCOUNT_PASS);
             // Execute method
             client.initDashboard();
             // Verify if an account has been created
-            List<Map<String, Object>> accountInfo = (List<Map<String, Object>>) IntegrationTestUtils.retrieveServiceAccounts(DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
+            List<Map<String, Object>> accountInfo = (List<Map<String, Object>>) IntegrationTestUtils.retrieveServiceAccounts(IntegrationTestUtils.DASHBOARD_ACCOUNT_USER, IntegrationTestUtils.DASHBOARD_ACCOUNT_PASS);
             assertEquals(1, accountInfo.size());
             assertEquals(1.0, accountInfo.get(0).get("tokens")); // Only one token should be generated
             assertEquals(IntegrationTestUtils.SERVICE_ACCOUNT_NAME, accountInfo.get(0).get(IntegrationTestUtils.NAME_KEY));
             // Verify that no data sources has been created
-            Map<String, Object> dataSources = (Map<String, Object>) IntegrationTestUtils.retrieveDataSources(DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
-            assertEquals(0, dataSources.size());
+            JsonArray dataSources = IntegrationTestUtils.retrieveDataSources(IntegrationTestUtils.DASHBOARD_ACCOUNT_USER, IntegrationTestUtils.DASHBOARD_ACCOUNT_PASS);
+            assertTrue(dataSources.isEmpty());
         }
     }
 
@@ -65,18 +89,44 @@ class DashboardClientIntegrationTest {
             Mockito.when(mock.getAllSpatialZones()).thenReturn(new String[]{});
         })) {
             StackClient mockStackClient = new StackClient();
-            DashboardClient client = new DashboardClient(mockStackClient, DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
+            DashboardClient client = new DashboardClient(mockStackClient, IntegrationTestUtils.DASHBOARD_ACCOUNT_USER, IntegrationTestUtils.DASHBOARD_ACCOUNT_PASS);
             // Execute method twice to replicate the service account being created when the agent is running for the second time onwards
             client.initDashboard();
             client.initDashboard();
             // Verify the accounts are created as expected
-            List<Map<String, Object>> accountInfo = (List<Map<String, Object>>) IntegrationTestUtils.retrieveServiceAccounts(DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
+            List<Map<String, Object>> accountInfo = (List<Map<String, Object>>) IntegrationTestUtils.retrieveServiceAccounts(IntegrationTestUtils.DASHBOARD_ACCOUNT_USER, IntegrationTestUtils.DASHBOARD_ACCOUNT_PASS);
             assertEquals(1, accountInfo.size()); // Only one account should be created
             assertEquals(IntegrationTestUtils.SERVICE_ACCOUNT_NAME, accountInfo.get(0).get(IntegrationTestUtils.NAME_KEY));
             assertEquals(2.0, accountInfo.get(0).get("tokens")); // There should be two tokens
             // Verify that no data sources has been created
-            Map<String, Object> dataSources = (Map<String, Object>) IntegrationTestUtils.retrieveDataSources(DASHBOARD_ACCOUNT_USER, DASHBOARD_ACCOUNT_PASS);
-            assertEquals(0, dataSources.size());
+            JsonArray dataSources = IntegrationTestUtils.retrieveDataSources(IntegrationTestUtils.DASHBOARD_ACCOUNT_USER, IntegrationTestUtils.DASHBOARD_ACCOUNT_PASS);
+            assertTrue(dataSources.isEmpty());
+        }
+    }
+
+    @Test
+    void testInitDashboardWithOneDatabaseConnection() {
+        try (MockedConstruction<StackClient> mockClient = Mockito.mockConstruction(StackClient.class, (mock, context) -> {
+            // Ensure all mocks return the test dashboard url and to allow the program to continue
+            Mockito.when(mock.getDashboardUrl()).thenReturn(IntegrationTestUtils.TEST_DASHBOARD_URL);
+            // Mock that this returns an empty string array as this test is not for creating dashboards
+            Mockito.when(mock.getAllSpatialZones()).thenReturn(new String[]{});
+            Mockito.when(mock.getDatabaseNames()).thenReturn(List.of(new String[]{SAMPLE_SQL_DATABASE}));
+            Mockito.when(mock.getPostGisCredentials()).thenReturn(new String[]{IntegrationTestUtils.TEST_POSTGIS_URL,
+                    IntegrationTestUtils.TEST_POSTGIS_USER, IntegrationTestUtils.TEST_POSTGIS_PASSWORD});
+        })) {
+            StackClient mockStackClient = new StackClient();
+            DashboardClient client = new DashboardClient(mockStackClient, IntegrationTestUtils.DASHBOARD_ACCOUNT_USER, IntegrationTestUtils.DASHBOARD_ACCOUNT_PASS);
+            // Execute method
+            client.initDashboard();
+            // Verify if an account has been created
+            List<Map<String, Object>> accountInfo = (List<Map<String, Object>>) IntegrationTestUtils.retrieveServiceAccounts(IntegrationTestUtils.DASHBOARD_ACCOUNT_USER, IntegrationTestUtils.DASHBOARD_ACCOUNT_PASS);
+            assertEquals(1, accountInfo.size());
+            assertEquals(1.0, accountInfo.get(0).get("tokens")); // Only one token should be generated
+            assertEquals(IntegrationTestUtils.SERVICE_ACCOUNT_NAME, accountInfo.get(0).get(IntegrationTestUtils.NAME_KEY));
+            // Verify that only one data source has been created
+            JsonArray dataSources = IntegrationTestUtils.retrieveDataSources(IntegrationTestUtils.DASHBOARD_ACCOUNT_USER, IntegrationTestUtils.DASHBOARD_ACCOUNT_PASS);
+            assertEquals(1, dataSources.size());
         }
     }
 }
