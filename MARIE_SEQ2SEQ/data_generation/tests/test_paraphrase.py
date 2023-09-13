8f30e0a7a80f3c64e9abdfea67298e1a943ef0e4
Dan Tran <nmdt2@cam.ac.uk>
2023-09-13 15:32:39 +0800

    dev-marie-plm: Update pararaphrasing call

--- MARIE_SEQ2SEQ/data_generation/tests/test_paraphrase.py
+++ MARIE_SEQ2SEQ/data_generation/tests/test_paraphrase.py
@@ -31,64 +31,39 @@ class TestParaphrase:
         "text, expected",
         [
             (
-                "1. How do individuals utilize (3-amino-2-oxopropyl) dihydrogen phosphate?\n2. In what manner do people employ (3-amino-2-oxopropyl) dihydrogen phosphate?\n3. three\n4. four\n5. five\n6. six\n7. seven\n8. eight\n9. nine\n10. ten",
+                "1. How do individuals utilize (3-amino-2-oxopropyl) dihydrogen phosphate?\n2. In what manner do people employ (3-amino-2-oxopropyl) dihydrogen phosphate?",
                 [
                     "How do individuals utilize (3-amino-2-oxopropyl) dihydrogen phosphate?",
                     "In what manner do people employ (3-amino-2-oxopropyl) dihydrogen phosphate?",
-                    "three",
-                    "four",
-                    "five",
-                    "six",
-                    "seven",
-                    "eight",
-                    "nine",
-                    "ten",
                 ],
             ),
             (
-                '1. "How many covalent units do chemical compounds in the aminoacridine category have?"\n2. "Could you tell me the covalent unit count of chemical entities within the aminoacridine group?"\n3. "three"\n4. "four"\n5. "five"\n6. "six"\n7. "seven"\n8. "eight"\n9. "nine"\n10. "ten"',
+                '1. "How many covalent units do chemical compounds in the aminoacridine category have?"\n2. "Could you tell me the covalent unit count of chemical entities within the aminoacridine group?"',
                 [
                     "How many covalent units do chemical compounds in the aminoacridine category have?",
                     "Could you tell me the covalent unit count of chemical entities within the aminoacridine group?",
-                    "three",
-                    "four",
-                    "five",
-                    "six",
-                    "seven",
-                    "eight",
-                    "nine",
-                    "ten",
                 ],
             ),
             (
-                'Certainly, here are 10 paraphrased versions of the statement:\n\n1. "How many covalent units are found in chemical entities associated with aminoacridines?"\n2. "Could you tell me the covalent unit tally for chemical substances within the aminoacridine group?"\n3. "three"\n4. "four"\n5. "five"\n6. "six"\n7. "seven"\n8. "eight"\n9. "nine"\n10. "ten"',
+                'Certainly, here are 10 paraphrased versions of the statement:\n\n1. "How many covalent units are found in chemical entities associated with aminoacridines?"\n2. "Could you tell me the covalent unit tally for chemical substances within the aminoacridine group?"',
                 [
                     "How many covalent units are found in chemical entities associated with aminoacridines?",
                     "Could you tell me the covalent unit tally for chemical substances within the aminoacridine group?",
-                    "three",
-                    "four",
-                    "five",
-                    "six",
-                    "seven",
-                    "eight",
-                    "nine",
-                    "ten",
                 ],
             ),
             (
-                "Can you provide information on the optical rotation and vapor pressure of CC(C(=O)C1=CC(=CC=C1)Cl)NC(C)(C)C?\nI'm curious about the optical rotation and vapor pressure of CC(C(=O)C1=CC(=CC=C1)Cl)NC(C)(C)C. Are you able to share any information?\nthree\nfour\nfive\nsix\nseven\neight\nnine\nten",
+                "Can you provide information on the optical rotation and vapor pressure of CC(C(=O)C1=CC(=CC=C1)Cl)NC(C)(C)C?\nI'm curious about the optical rotation and vapor pressure of CC(C(=O)C1=CC(=CC=C1)Cl)NC(C)(C)C. Are you able to share any information?",
                 [
                     "Can you provide information on the optical rotation and vapor pressure of CC(C(=O)C1=CC(=CC=C1)Cl)NC(C)(C)C?",
                     "I'm curious about the optical rotation and vapor pressure of CC(C(=O)C1=CC(=CC=C1)Cl)NC(C)(C)C. Are you able to share any information?",
-                    "three",
-                    "four",
-                    "five",
-                    "six",
-                    "seven",
-                    "eight",
-                    "nine",
-                    "ten",
                 ],
+            ),
+            (
+                "1. Provide a list of species with a sublimation enthalpy exceeding 284, renowned for their hormone production.\n2. Compile a catalog including species that possess a hormone and exhibit sublimation enthalpy values higher than 284.\n\nNote: The original statement appears to have some inconsistencies or unusual combination of criteria, as sublimation enthalpy is a property related to a substance's phase transition from solid to gas, while hormones are chemical compounds produced by living organisms.",
+                [
+                    "Provide a list of species with a sublimation enthalpy exceeding 284, renowned for their hormone production.",
+                    "Compile a catalog including species that possess a hormone and exhibit sublimation enthalpy values higher than 284."
+                ]
             ),
         ],
     )
