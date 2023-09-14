b99411486a152cff88a03d6b829ce733606ba5ff
ChungTingLao <chung.ting.lao.34@gmail.com>
2023-09-14 10:52:13 +0100

    dev-stack-building-thematic-vis: modified the footprint script to avoid adding footprints to buildings that already have footprints.

--- Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/citydb/citydb_add_footprint.sql
+++ Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/citydb/citydb_add_footprint.sql
@@ -19,13 +19,15 @@ WITH "fp_parent" AS (
                 NULL,
                 "building"."id"
             FROM "building" JOIN "cityobject" ON "building"."id" = "cityobject"."id" WHERE "building"."lod0_footprint_id" IS NULL)
-    RETURNING "id" AS "footprint_id", "cityobject_id" AS "bid"
-)
-UPDATE "citydb"."building"
-SET "lod0_footprint_id" = "fp_parent"."footprint_id" FROM "fp_parent" WHERE "building"."id" = "fp_parent"."bid" AND "building"."lod0_footprint_id" IS NULL;
--- make parents self-aware
-UPDATE "surface_geometry"
-SET "root_id" = "id" WHERE "root_id" IS NULL;
+    RETURNING "id" AS "footprint_id", "cityobject_id" AS "bid","gmlid"
+),
+"anon" AS (UPDATE "citydb"."building"
+SET "lod0_footprint_id" = "fp_parent"."footprint_id" FROM "fp_parent" WHERE "building"."id" = "fp_parent"."bid" AND "building"."lod0_footprint_id" IS NULL),
+"fg" AS (SELECT "bid","footprint_id","gmlid",("dumpling").geom AS "geometry"
+FROM (SELECT "building_id",public.ST_Dump(public.ST_Union("geometry")) AS "dumpling" FROM "surface_geometry"
+        JOIN "thematic_surface" ON "surface_geometry"."root_id" = "thematic_surface"."lod2_multi_surface_id"
+        WHERE "geometry" IS NOT NULL AND "objectclass_id" = 35
+        GROUP BY "building_id") AS "t1" JOIN "fp_parent" ON "fp_parent"."bid" = "t1"."building_id")
 -- add footprint geometry
 INSERT INTO "citydb"."surface_geometry" (
         "gmlid",
@@ -40,18 +42,16 @@ INSERT INTO "citydb"."surface_geometry" (
         "cityobject_id"
     ) (
         SELECT "fg"."gmlid" || '_child',
-            "fg"."lod0_footprint_id",
-            "fg"."lod0_footprint_id",
+            "fg"."footprint_id",
+            "fg"."footprint_id",
             0,
             0,
             0,
             0,
             0,
             "fg"."geometry",
-            "fg"."building_id"
-        FROM (SELECT "building_id","lod0_footprint_id", "surface_geometry"."gmlid", ("dumpling").geom AS "geometry"
-        FROM (SELECT "building_id",public.ST_Dump(public.ST_Union("geometry")) AS "dumpling" FROM "surface_geometry"
-        JOIN "thematic_surface" ON "surface_geometry"."root_id" = "thematic_surface"."lod2_multi_surface_id"
-        WHERE "geometry" IS NOT NULL AND "objectclass_id" = 35
-        GROUP BY "building_id") AS "t1" JOIN "building" ON "building"."id" = "t1"."building_id"
-        JOIN "surface_geometry" ON "surface_geometry"."id" = "building"."lod0_footprint_id") AS "fg");
+            "fg"."bid"
+        FROM "fg");
+-- make parents self-aware
+UPDATE "surface_geometry"
+SET "root_id" = "id" WHERE "root_id" IS NULL;
