73b95c73fb1962edc728dacd9af55173904584b9
Dan Tran <nmdt2@cam.ac.uk>
2023-09-15 09:53:09 +0800

    dev-marie-plm: Add identifier to ExampleMakerHead2Tail

--- MARIE_SEQ2SEQ/data_generation/data_generation/create_training_data_from_kg/make_example_head2tail.py
+++ MARIE_SEQ2SEQ/data_generation/data_generation/create_training_data_from_kg/make_example_head2tail.py
@@ -5,6 +5,24 @@ from data_generation.utils import add_space_and_lower
 
 
 class ExampleMakerHead2Tail:
+    IDENTIFIER_LABELS = {
+        "ChebiID": ["ChEBI ID", "Chemical Entities of Biological Interest ID"],
+        "CID": ["CID", "PubChem CID", "PubChem ID", "PubChem Compound ID"],
+        "EmpiricalFormula": ["empirical formula"],
+        "InChI": [
+            "InChI",
+            "International Chemical Identifier",
+            "IUPAC International Chemical Identifier",
+        ],
+        "InChIKey": ["InChIKey", "hashed InChI"],
+        "IUPACName": ["IUPAC name"],
+        "MolecularFormula": ["molecular formula"],
+        "SMILES": [
+            "SMILES",
+            "SMILES string",
+        ],
+    }
+
     def make_example(self, subgraph: Dict[str, Union[dict, List[dict]]]):
         select_variables = []
 
@@ -16,6 +34,7 @@ class ExampleMakerHead2Tail:
         tails: List[dict] = list(subgraph["tails"])
         random.shuffle(tails)
         property_num = 0
+        identifier_num = 0
         for tail in tails:
             if tail["type"] == "property":
                 property_num += 1
@@ -23,8 +42,20 @@ class ExampleMakerHead2Tail:
 
                 select_variables.append(f"?{PropertyName}Value")
                 where_clause_blocks.append(self.make_where_property(PropertyName))
-                where_clause_compact_blocks.append(self.make_where_property_compact(PropertyName))
+                where_clause_compact_blocks.append(
+                    self.make_where_property_compact(PropertyName)
+                )
                 ask_items.append(add_space_and_lower(PropertyName))
+            elif tail["type"] == "identifier":
+                identifier_num += 1
+                IdentifierName = tail["IdentifierName"]
+
+                select_variables.append(f"?{IdentifierName}Value")
+                where_clause_blocks.append(self.make_where_identifier(IdentifierName))
+                where_clause_compact_blocks.append(
+                    self.make_where_identifier_compact(IdentifierName)
+                )
+                ask_items.append(random.choice(self.IDENTIFIER_LABELS[IdentifierName]))
             elif tail["type"] == "use":
                 select_variables.append("?UseValue")
                 where_clause_blocks.append(self.make_where_use())
@@ -33,71 +64,82 @@ class ExampleMakerHead2Tail:
             elif tail["type"] == "chemicalclass":
                 select_variables.append("?ChemicalClassValue")
                 where_clause_blocks.append(self.make_where_chemicalclass())
-                where_clause_compact_blocks.append(self.make_where_chemicalclass_compact())
+                where_clause_compact_blocks.append(
+                    self.make_where_chemicalclass_compact()
+                )
                 ask_items.append("chemical class")
             else:
                 raise ValueError("Unexpected tail type: " + tail["type"])
 
         sparql_query = f"""SELECT {" ".join(["?label"] + select_variables)} 
-    WHERE {{{where_clause_blocks}
-    }}"""
+WHERE {{{where_clause_blocks}
+}}"""
         sparql_query_compact = f"""SELECT {" ".join(select_variables)} 
-    WHERE {{{where_clause_compact_blocks}
-    }}"""
+WHERE {{{where_clause_compact_blocks}
+}}"""
 
         return dict(
             canonical_question=self.make_canonical_question(ask_items),
-            values=dict(species=subgraph["head"]["IdentifierValue"]),
+            bindings=dict(species=subgraph["head"]["IdentifierValue"]),
             sparql_query=sparql_query,
             sparql_query_compact=sparql_query_compact,
         )
-    
+
     def make_where_species(self, species: str):
         return f"""
-        ?SpeciesIRI rdf:type os:Species ; rdfs:label ?label ; ?hasIdentifier ?IdentifierIRI .
-        ?IdentifierIRI rdf:type ?Identifier ; os:value ?species .
-        ?Identifier rdfs:subClassOf os:Identifier .
-        FILTER ( ?species = "{species}" )"""
+    ?SpeciesIRI rdf:type os:Species ; rdfs:label ?label ; ?hasIdentifier ?IdentifierIRI .
+    ?IdentifierIRI rdf:type ?Identifier ; os:value ?species .
+    ?Identifier rdfs:subClassOf os:Identifier .
+    FILTER ( ?species = "{species}" )"""
 
     def make_where_species_compact(self, species: str):
         return f"""
-        ?SpeciesIRI ?hasIdentifier ?species .
-        FILTER ( ?species = "{species}" )"""
+    ?SpeciesIRI ?hasIdentifier ?species .
+    FILTER ( ?species = "{species}" )"""
 
     def make_where_property(self, PropertyName: str):
         return f"""
-        ?SpeciesIRI os:has{PropertyName} ?{PropertyName}IRI .
-        ?{PropertyName}IRI os:value ?{PropertyName}Value ; os:unit ?{PropertyName}UnitIRI ; os:hasProvenance ?{PropertyName}ProvenanceIRI . 
-        ?{PropertyName}UnitIRI rdfs:label ?{PropertyName}UnitValue .
-        OPTIONAL{{
-            ?{PropertyName}IRI os:hasReferenceState ?{PropertyName}ReferenceStateIRI .
-            ?{PropertyName}ReferenceStateIRI os:value ?{PropertyName}ReferenceStateValue ; os:unit ?{PropertyName}ReferenceStateUnitIRI .
-            ?{PropertyName}ReferenceStateUnitIRI rdfs:label ?{PropertyName}ReferenceStateUnitValue .
-        }}"""
+    ?SpeciesIRI os:has{PropertyName} ?{PropertyName}IRI .
+    ?{PropertyName}IRI os:value ?{PropertyName}Value ; os:unit ?{PropertyName}UnitIRI ; os:hasProvenance ?{PropertyName}ProvenanceIRI . 
+    ?{PropertyName}UnitIRI rdfs:label ?{PropertyName}UnitValue .
+    OPTIONAL{{
+        ?{PropertyName}IRI os:hasReferenceState ?{PropertyName}ReferenceStateIRI .
+        ?{PropertyName}ReferenceStateIRI os:value ?{PropertyName}ReferenceStateValue ; os:unit ?{PropertyName}ReferenceStateUnitIRI .
+        ?{PropertyName}ReferenceStateUnitIRI rdfs:label ?{PropertyName}ReferenceStateUnitValue .
+    }}"""
+    
+    def make_where_property_compact(self, PropertyName: str):
+        return f"""
+    ?SpeciesIRI os:has{PropertyName} ?{PropertyName}Value ."""
+    
+    def make_where_identifier(self, IdentifierName: str):
+        return f"""
+    ?SpeciesIRI os:has{IdentifierName} ?{IdentifierName}IRI .
+    ?{IdentifierName}IRI os:value ?{IdentifierName}Value ."""
+
+    def make_where_identifier_compact(self, IdentifierName: str):
+        return f"""
+    ?SpeciesIRI os:has{IdentifierName} ?{IdentifierName}Value ."""
 
     def make_where_use(self):
         return f"""
-        ?SpeciesIRI os:hasUse ?UseIRI .
-        ?UseIRI rdfs:label ?UseValue ."""
+    ?SpeciesIRI os:hasUse ?UseIRI .
+    ?UseIRI rdfs:label ?UseValue ."""
 
     def make_where_use_compact(self):
         return f"""
-        ?SpeciesIRI os:hasUse ?UseValue ."""
-
-    def make_where_property_compact(self, PropertyName: str):
-        return f"""
-        ?SpeciesIRI os:has{PropertyName} ?{PropertyName}Value ."""
+    ?SpeciesIRI os:hasUse ?UseValue ."""
 
     def make_where_chemicalclass(self):
         return f"""
-        ?SpeciesIRI os:hasChemicalClass* ?x .
-        ?x ?y ?z .
-        ?z rdfs:subClassOf* ?ChemicalClassIRI .
-        ?ChemicalClassIRI rdf:type os:ChemicalClass ; rdfs:label ?ChemicalClassValue ."""
+    ?SpeciesIRI os:hasChemicalClass* ?x .
+    ?x ?y ?z .
+    ?z rdfs:subClassOf* ?ChemicalClassIRI .
+    ?ChemicalClassIRI rdf:type os:ChemicalClass ; rdfs:label ?ChemicalClassValue ."""
 
     def make_where_chemicalclass_compact(self):
         return f"""
-        ?SpeciesIRI os:hasChemicalClass ?ChemicalClassValue ."""
+    ?SpeciesIRI os:hasChemicalClass ?ChemicalClassValue ."""
 
     def make_canonical_question(self, ask_items: List[str]):
         canonical_question_tokens = ["What "]
