51ce9f84ec81743daa762dafd09beb484a0979bc
Dan Tran <nmdt2@cam.ac.uk>
2023-09-18 14:05:38 +0800

    dev-marie-plm: Refactor SubgraphRetriever's method convert_kg_results_to_subgraph into a class

--- MARIE_SEQ2SEQ/data_generation/data_generation/create_training_data_from_kg/retrieve_subgraph/construct_query.py
+++ MARIE_SEQ2SEQ/data_generation/data_generation/create_training_data_from_kg/retrieve_subgraph/construct_query.py
@@ -3,15 +3,10 @@ import itertools
 import random
 from typing import Dict, List, Optional
 
-from SPARQLWrapper import SPARQLWrapper, JSON, POST
-
-from data_generation.constants import (
-    IDENTIFIER_NAMES,
-    PROPERTY_NAMES,
-)
+from data_generation.constants import IDENTIFIER_NAMES, PROPERTY_NAMES
 
 
-class SubgraphRetriever:
+class QueryConstructor:
     PREFIXES = """PREFIX os: <http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"""
@@ -22,75 +17,7 @@ PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"""
         "SMILES",
     ]
 
-    def __init__(
-        self,
-        kg_endpoint: str = "http://theworldavatar.com/blazegraph/namespace/copy_ontospecies_pubchem/sparql",
-    ):
-        sparql_client = SPARQLWrapper(kg_endpoint)
-        sparql_client.setReturnFormat(JSON)
-        sparql_client.setMethod(POST)
-        self.sparql_client = sparql_client
-
-    def get_subgraph(
-        self,
-        tail_nums: Optional[Dict[str, int]] = None,
-        bindings: Optional[Dict[str, str]] = None,
-    ):
-        results = []
-
-        try_num = 0
-        try_limit = 5 if not bindings else 1
-        while len(results) == 0 and try_num < try_limit:
-            if tail_nums is None:
-                tail_nums = self.get_rand_tail_nums()
-            sparql_query = self.get_subgraph_query(tail_nums, bindings)
-            print(sparql_query)
-
-            self.sparql_client.setQuery(sparql_query)
-            results = self.sparql_client.queryAndConvert()["results"]["bindings"]
-            try_num += 1
-
-        if len(results) == 0:
-            return None
-
-        return self.convert_kg_results_to_subgraph(results[0])
-
-    def get_rand_tail_nums(self):
-        property_num = random.randint(0, 3)
-        identifier_num = random.randint(property_num, 3) - property_num
-
-        id_prop_num = identifier_num + property_num
-        if id_prop_num == 0:
-            use_num, chemicalclass_num = random.choice([(0, 1), (1, 0)])
-        elif id_prop_num == 1:
-            use_num, chemicalclass_num = random.choice([(0, 0), (0, 1), (1, 0), (1, 1)])
-        elif id_prop_num == 2:
-            use_num, chemicalclass_num = random.choice([(0, 0), (0, 1), (1, 0)])
-        else:
-            use_num, chemicalclass_num = 0, 0
-
-        return dict(
-            property_num=property_num,
-            identifier_num=identifier_num,
-            use_num=use_num,
-            chemicalclass_num=chemicalclass_num,
-        )
-
-    def get_rand_tail_nums_non_identifier(self):
-        property_num = random.randint(0, 3)
-        use_num = random.randint(property_num, 3) - property_num
-        chemicalclass_num = (
-            random.randint(property_num + use_num, 3) - property_num - use_num
-        )
-        return dict(
-            property_num=property_num,
-            use_num=use_num,
-            chemicalclass_num=chemicalclass_num,
-        )
-
-    def get_subgraph_query(
-        self, tail_nums: Dict[str, int], bindings: Optional[dict] = None
-    ):
+    def construct(self, tail_nums: Dict[str, int], bindings: Optional[dict] = None):
         if bindings is None:
             bindings = dict()
 
@@ -127,69 +54,6 @@ SELECT {" ".join(select_variables)}
 WHERE {{{"".join(where_clause_blocks).rstrip()}
 }}
 LIMIT 1"""
-
-    def convert_kg_results_to_subgraph(self, valueset: Dict[str, Dict[str, str]]):
-        values = {k: v["value"] for k, v in valueset.items()}
-
-        head = dict(
-            SpeciesIRI=values["SpeciesIRI"],
-            IdentifierIRI=values["IdentifierIRI"],
-            IdentifierValue=values["IdentifierValue"],
-        )
-
-        tails = []
-
-        for i in range(1, 4):
-            if f"PropertyIRI{i}" not in values:
-                break
-            tails.append(
-                dict(
-                    type="property",
-                    PropertyName=values[f"hasProperty{i}"].split("#", maxsplit=1)[-1][
-                        len("has") :
-                    ],
-                    PropertyIRI=values[f"PropertyIRI{i}"],
-                    PropertyValue=values[f"PropertyValue{i}"],
-                )
-            )
-
-        for i in range(1, 4):
-            if f"IdentifierIRI{i}" not in values:
-                break
-            tails.append(
-                dict(
-                    type="identifier",
-                    IdentifierName=values[f"hasIdentifier{i}"].split("#", maxsplit=1)[
-                        -1
-                    ][len("has") :],
-                    IdentifierIRI=values[f"IdentifierIRI{i}"],
-                    IdentifierValue=values[f"IdentifierValue{i}"],
-                )
-            )
-
-        for i in range(1, 4):
-            if f"UseIRI{i}" not in values:
-                break
-            tails.append(
-                dict(
-                    type="use",
-                    UseIRI=values[f"UseIRI{i}"],
-                    UseValue=values[f"UseValue{i}"],
-                )
-            )
-
-        for i in range(1, 4):
-            if f"ChemicalClassIRI{i}" not in values:
-                break
-            tails.append(
-                dict(
-                    type="chemicalclass",
-                    ChemicalClassIRI=values[f"ChemicalClassIRI{i}"],
-                    ChemicalClassValue=values[f"ChemicalClassValue{i}"],
-                )
-            )
-
-        return dict(head=head, tails=tails)
 
 
 class QueryConstructionHelper(ABC):
