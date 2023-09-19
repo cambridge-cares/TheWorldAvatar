3806d57f25805d104f89bde5d3fcac67988f89f6
ChungTingLao <chung.ting.lao.34@gmail.com>
2023-09-19 16:43:56 +0100

    dev-stack-building-thematic-vis: calculate building heights from associated geometries instead of the envelope.

--- Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/citydb/citydb_add_building_height.sql
+++ Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/citydb/citydb_add_building_height.sql
@@ -1,5 +1,8 @@
 SET search_path TO public,citydb;
 UPDATE "building"
-SET "measured_height" = ST_ZMax(Box3D("cityobject"."envelope")) - ST_ZMin(Box3D("cityobject"."envelope"))
-FROM "cityobject" WHERE
-"building"."id" = "cityobject"."id" AND "building"."measured_height" IS NULL;
+SET "measured_height" = "h" FROM
+(SELECT COALESCE("building"."id", "thematic_surface"."building_id") AS "bid",MAX(public.ST_ZMax("geometry"))-MIN(public.ST_ZMin("geometry")) AS "h" FROM "surface_geometry"
+FULL JOIN "building" ON "surface_geometry"."root_id" = COALESCE("building"."lod4_multi_surface_id","building"."lod3_multi_surface_id","building"."lod2_multi_surface_id","building"."lod1_multi_surface_id","building"."lod4_solid_id","building"."lod3_solid_id","building"."lod2_solid_id","building"."lod1_solid_id")
+FULL JOIN "thematic_surface" ON "surface_geometry"."root_id" = COALESCE("thematic_surface"."lod4_multi_surface_id","thematic_surface"."lod3_multi_surface_id","thematic_surface"."lod2_multi_surface_id")
+WHERE "geometry" IS NOT NULL GROUP BY "building"."id", "thematic_surface"."building_id"
+) AS "x" WHERE "building"."measured_height" IS NULL AND "x"."bid"="building"."id";
