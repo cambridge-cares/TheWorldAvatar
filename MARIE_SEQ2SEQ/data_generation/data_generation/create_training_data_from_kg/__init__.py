01e5a12b81d5ee3c441f474d13ab8e3092ff781b
Dan Tran <nmdt2@cam.ac.uk>
2023-09-20 13:38:21 +0800

    dev-marie-plm: Refactor DatasetFromKgMaker

--- MARIE_SEQ2SEQ/data_generation/data_generation/create_training_data_from_kg/__init__.py
+++ MARIE_SEQ2SEQ/data_generation/data_generation/create_training_data_from_kg/__init__.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 
+from data_generation.constants import IDENTIFIER_NAMES
+
 from .make_examples_by_query_path import ExampleMakerByQueryPath
 
 
@@ -13,72 +15,95 @@ class DatasetFromKgMaker:
 
         for _ in range(repeats):
             print("Generating examples with one head and one tail...")
-            for query_path in ["h2t", "t2h"]:
-                examples.extend(
-                    self.example_maker.make_examples_1h_1t(
-                        query_path=query_path,
-                        tail_class="property",
-                        sampling_size=-1,
-                    )
-                )
-                examples.extend(
-                    self.example_maker.make_examples_1h_1t(
-                        query_path=query_path,
-                        tail_class="identifier",
-                        sampling_size=-1,
-                    )
-                )
-                sampling_size = 3
-                examples.extend(
-                    self.example_maker.make_examples_1h_1t(
-                        query_path=query_path,
-                        tail_class="use",
-                        sampling_size=sampling_size,
-                    )
-                )
-                examples.extend(
-                    self.example_maker.make_examples_1h_1t(
-                        query_path=query_path,
-                        tail_class="chemicalclass",
-                        sampling_size=sampling_size,
-                    )
-                )
+            examples.extend(self.make_examples_1h_1t())
 
             print("Generating examples with one head and two tails...")
-            for query_path in ["h2t", "t2h"]:
-                examples.extend(
-                    self.example_maker.make_examples_1h_2t(
-                        query_path=query_path,
-                        tail_class="property",
-                        sampling_size=-1,
-                    )
-                )
-            examples.extend(
-                self.example_maker.make_examples_1h_2t(
-                    query_path="h2t",
-                    tail_class="identifier",
-                    sampling_size=-1,
-                )
-            )
-            for query_path in ["h2t", "t2h", "t2t"]:
-                examples.extend(
-                    self.example_maker.make_examples_1h_2t(
-                        query_path=query_path, sampling_size=5
-                    )
-                )
+            examples.extend(self.make_examples_1h_2t())
 
             print("Generating examples with one head and three tails...")
-            for query_path in ["h2t", "t2h", "t2t"]:
-                examples.extend(
-                    self.example_maker.make_examples_1h_3t(
-                        query_path=query_path, sampling_size=5
-                    )
-                )
+            examples.extend(self.make_examples_1h_3t())
 
         if len(self.missing_entries) > 0:
             print("Missing entries: ")
             for k, v in self.missing_entries.items():
                 print(k, ": ", v)
+
+        return examples
+
+    def make_examples_1h_1t(self):
+        examples = []
+
+        for query_path in ["h2t", "t2h"]:
+            examples.extend(
+                self.example_maker.make_examples_1h_1t(
+                    query_path=query_path,
+                    tail_class="property",
+                    sampling_size=-1,
+                )
+            )
+            examples.extend(
+                self.example_maker.make_examples_1h_1t(
+                    query_path=query_path,
+                    tail_class="identifier",
+                    sampling_size=-1,
+                )
+            )
+            sampling_size = 3
+            examples.extend(
+                self.example_maker.make_examples_1h_1t(
+                    query_path=query_path,
+                    tail_class="use",
+                    sampling_size=sampling_size,
+                )
+            )
+            examples.extend(
+                self.example_maker.make_examples_1h_1t(
+                    query_path=query_path,
+                    tail_class="chemicalclass",
+                    sampling_size=sampling_size,
+                )
+            )
+
+        return examples
+
+    def make_examples_1h_2t(self):
+        examples = []
+
+        for query_path in ["h2t", "t2h"]:
+            examples.extend(
+                self.example_maker.make_examples_1h_2t(
+                    query_path=query_path,
+                    tail_class="property",
+                    sampling_size=-1,
+                )
+            )
+        examples.extend(
+            self.example_maker.make_examples_1h_2t(
+                query_path="h2t",
+                tail_class="identifier",
+                sampling_size=-1,
+            )
+        )
+        for query_path in ["h2t", "t2h", "t2t"]:
+            examples.extend(
+                self.example_maker.make_examples_1h_2t(
+                    query_path=query_path,
+                    sampling_size=len(IDENTIFIER_NAMES) if query_path == "t2t" else 5,
+                )
+            )
+
+        return examples
+
+    def make_examples_1h_3t(self):
+        examples = []
+
+        for query_path in ["h2t", "t2h", "t2t"]:
+            examples.extend(
+                self.example_maker.make_examples_1h_3t(
+                    query_path=query_path,
+                    sampling_size=len(IDENTIFIER_NAMES) if query_path == "t2t" else 5,
+                )
+            )
 
         return examples
 
