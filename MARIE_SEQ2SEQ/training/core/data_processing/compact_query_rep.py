a1bfb022315aa79e4b2811fb981fd11fdaef1fbe
Dan Tran <nmdt2@cam.ac.uk>
2023-09-21 11:32:10 +0800

    dev-marie-plm: Fix bug in parsing VALUES clauses

--- MARIE_SEQ2SEQ/training/core/data_processing/compact_query_rep.py
+++ MARIE_SEQ2SEQ/training/core/data_processing/compact_query_rep.py
@@ -200,7 +200,7 @@ class CompactQueryRep:
                     select_variables = select_variables.replace(
                         f"?{name}Value",
                         f"?{name}Value ?{name}UnitValue ?{name}ReferenceStateValue ?{name}ReferenceStateUnitValue",
-                        1
+                        1,
                     )
                     where_clauses_verbose.append(PROPERTY_PATTERNS_VERBOSE(name))
                 elif name in IDENTIFIER_NAMES:
@@ -226,7 +226,7 @@ class CompactQueryRep:
                 select_variables = select_variables.replace(
                     "?PropertyNameValue",
                     "?PropertyLabel ?PropertyNameValue ?PropertyNameUnitValue ?PropertyNameReferenceStateValue ?PropertyNameReferenceStateUnitValue",
-                    1
+                    1,
                 )
                 where_clauses_verbose.extend(ALL_PROPERTIES_PATTERNS_VERBOSE)
 
@@ -238,7 +238,7 @@ class CompactQueryRep:
 
             else:
                 raise Exception("Unexpected compact clause: " + pattern)
-        
+
         return f"""SELECT DISTINCT ?label {select_variables}
 WHERE {{{"".join(where_clauses_verbose).rstrip()}
 }}"""
@@ -319,12 +319,39 @@ WHERE {{{"".join(where_clauses_verbose).rstrip()}
                         "Open curly bracket is missing from VALUES clause: " + query
                     )
                 ptr += 1
-                ptr = advance_ptr_to_kw(query, "}")
-                if ptr >= len(query):
-                    raise ValueError(
-                        "Close curly bracket is missing from VALUES clause: " + query
-                    )
-                ptr += 1
+                while True:
+                    ptr = advance_ptr_thru_space(query, ptr)
+                    if ptr >= len(query):
+                        raise ValueError(
+                            "Close curly bracket is missing from VALUES clause: "
+                            + query
+                        )
+                    if query[ptr] == "}":
+                        ptr += 1
+                        break
+                    elif query[ptr] == "(":
+                        ptr = advance_ptr_to_kw(query, '"', ptr)
+                        if ptr >= len(query):
+                            raise ValueError(
+                                "Value string is missing from VALUES clause: " + query
+                            )
+                        ptr += 1
+                        ptr = advance_ptr_to_kw(query, '"', ptr)
+                        if ptr >= len(query):
+                            raise ValueError(
+                                "Close quotation mark is missing from value string in VALUES clause: "
+                                + query
+                            )
+                        ptr += 1
+                        ptr = advance_ptr_to_kw(query, ")", ptr)
+                        if ptr >= len(query):
+                            raise ValueError(
+                                "Close brack is missing from value string in VALUES clause: "
+                                + query
+                            )
+                        ptr += 1
+                    else:
+                        raise ValueError("Unexpected VALUES clause: " + query)
 
             else:  # assume it's the triple pattern
                 ptr = advance_ptr_to_kw(query, ".", ptr)
