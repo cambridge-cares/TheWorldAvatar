66ce692030c6e153800d15c0885c81a99edbfab7
Yi-Kai Tsai <yikai.tsai@cares.cam.ac.uk>
2023-09-26 11:44:20 +0800

    1628-dev-cea-agent-to-work-with-building-geometry-in-postgis: fix CEABuildingData

--- Agents/CEAAgent/cea-agent/src/main/java/uk/ac/cam/cares/jps/agent/cea/data/CEABuildingData.java
+++ Agents/CEAAgent/cea-agent/src/main/java/uk/ac/cam/cares/jps/agent/cea/data/CEABuildingData.java
@@ -1,29 +1,17 @@
 package uk.ac.cam.cares.jps.agent.cea.data;
 
-import org.locationtech.jts.geom.Geometry;
-import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
-public class CEAInputData {
-    public CEAGeometry geometry;
+public class CEABuildingData {
+    public CEAGeometryData geometry;
     public Map<String, Double> usage;
-    public ArrayList<CEAInputData> surrounding;
-    public List<OffsetDateTime> weatherTimes;
-    public Map<String, List<Double>> weather;
-    public List<Double> weatherMetaData;
 
-    public CEAInputData(CEAGeometry geometry_value, Map<String, Double> usage_value, ArrayList<CEAInputData> surrounding_value, List<OffsetDateTime> weatherTimes_value, Map<String, List<Double>> weather_value, List<Double> weatherMetaData_value) {
+    public CEABuildingData(CEAGeometryData geometry_value, Map<String, Double> usage_value) {
         this.geometry = geometry_value;
         this.usage = usage_value;
-        this.surrounding = surrounding_value;
-        this.weatherTimes = weatherTimes_value;
-        this.weather = weather_value;
-        this.weatherMetaData = weatherMetaData_value;
     }
 
-    public CEAGeometry getGeometry() {
+    public CEAGeometryData getGeometry() {
         return this.geometry;
     }
 
@@ -31,31 +19,11 @@ public class CEAInputData {
         return this.usage;
     }
 
-    public ArrayList<CEAInputData> getSurrounding() {
-        return this.surrounding;
-    }
-
-    public List<OffsetDateTime> getWeatherTimes() {return this.weatherTimes;}
-
-    public Map<String, List<Double>> getWeather() {return this.weather;}
-
-    public List<Double> getWeatherMetaData() {return this.weatherMetaData;}
-
-    public void setGeometry(CEAGeometry geometry_value) {
+    public void setGeometry(CEAGeometryData geometry_value) {
         this.geometry = geometry_value;
     }
 
     public void setUsage(Map<String, Double> usage_value) {
         this.usage = usage_value;
     }
-
-    public void setSurrounding(ArrayList<CEAInputData> surrounding_value) {
-        this.surrounding = surrounding_value;
-    }
-
-    public void setWeatherTimes(List<OffsetDateTime> weatherTimes_value) {this.weatherTimes = weatherTimes_value;}
-
-    public void setWeather(Map<String, List<Double>> weather_value) {this.weather = weather_value;}
-
-    public void setWeatherMetaData(List<Double> weatherMetaData_value) {this.weatherMetaData = weatherMetaData_value;}
 }
