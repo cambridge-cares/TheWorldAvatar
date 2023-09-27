e88ae238c69205d3f6e77035d79764f931c5c6af
NPhamDinh <49727803+NPhamDinh@users.noreply.github.com>
2023-09-27 15:11:39 +0800

    dev-forecasting-prophet-covariates: Update integration tests

--- Agents/ForecastingAgent/tests/test_integration.py
+++ Agents/ForecastingAgent/tests/test_integration.py
@@ -12,6 +12,7 @@ import pytest
 import requests
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from pathlib import Path
 from rdflib import Graph
 from rdflib import RDF
@@ -519,18 +520,14 @@ def test_create_tft_forecast(
     print("All check passed.")
 
 
-#pytest.mark.skip(reason="")
+#@pytest.mark.skip(reason="")
 @pytest.mark.parametrize(
     "derivation_input_set, dataIRI, input_chunk_length, with_unit, overwrite_forecast, ts_times, covariates, case",
     [
         (cf.DERIVATION_INPUTS_7, cf.IRI_TO_FORECAST_1, cf.DURATION_2, False, True, cf.TIMES, cf.COVARIATES_2, cf.TEST_CASE_13),
-        (cf.DERIVATION_INPUTS_7, cf.IRI_TO_FORECAST_1, cf.DURATION_2, False, False, cf.TIMES, cf.COVARIATES_2, cf.TEST_CASE_14),
-        (cf.DERIVATION_INPUTS_8, cf.IRI_TO_FORECAST_2, cf.DURATION_2, True, True, cf.TIMES, cf.COVARIATES_2, cf.TEST_CASE_15),
-        (cf.DERIVATION_INPUTS_8, cf.IRI_TO_FORECAST_2, cf.DURATION_2, True, False, cf.TIMES, cf.COVARIATES_2, cf.TEST_CASE_16),
-        (cf.DERIVATION_INPUTS_9, cf.IRI_TO_FORECAST_1, cf.DURATION_2, False, True, cf.TIMES, cf.COVARIATES_2, cf.TEST_CASE_17),
-        (cf.DERIVATION_INPUTS_9, cf.IRI_TO_FORECAST_1, cf.DURATION_2, False, False, cf.TIMES, cf.COVARIATES_2, cf.TEST_CASE_18),
-        (cf.DERIVATION_INPUTS_10, cf.IRI_TO_FORECAST_2, cf.DURATION_2, True, True, cf.TIMES, cf.COVARIATES_2, cf.TEST_CASE_19),
-        (cf.DERIVATION_INPUTS_10, cf.IRI_TO_FORECAST_2, cf.DURATION_2, True, False, cf.TIMES, cf.COVARIATES_2, cf.TEST_CASE_20)
+        (cf.DERIVATION_INPUTS_8, cf.IRI_TO_FORECAST_1, cf.DURATION_2, False, True, cf.TIMES, cf.COVARIATES_2, cf.TEST_CASE_14),
+        (cf.DERIVATION_INPUTS_9, cf.IRI_TO_FORECAST_1, cf.DURATION_2, False, True, cf.TIMES, cf.COVARIATES_3, cf.TEST_CASE_15),
+         (cf.DERIVATION_INPUTS_10, cf.IRI_TO_FORECAST_1, cf.DURATION_2, False, True, cf.TIMES, cf.COVARIATES_3, cf.TEST_CASE_16),
     ],
 )
 def test_create_prophet_covariates_forecast(
@@ -538,14 +535,15 @@ def test_create_prophet_covariates_forecast(
     with_unit, overwrite_forecast, ts_times, covariates, case
 ):
     """
-    Test if forecasting agent performs derivation update as expected for 
-    pre-trained temporal fusion transformer model 
-    (as required for district heating optimisation use case)
-
-    NOTE: historical_data_length (marked up Duration instance) and input_chunk_length
-          are not equivalent for pre-trained neural method:
-          - historical_data_length: used to scale input data (and covariates)
-          - input_chunk_length: length of historical data used to generate (next) forecast
+    Test if Forecasting Agent performs derivation update as expected (using 
+    default Prophet model with covariates)
+        - forecasts are created using Prophet
+        - historical data length (same as input_chunk_length for non-neural method):
+            336/8760h (HIST_DURATION_1, DURATION_1 or HIST_DURATION_2, DURATION_2)
+        - initial interval: OptimisationInterval_1
+                            Jan 01 2020 00:00:00 UTC - Jan 02 2020 00:00:00 UTC
+        - updated interval: OptimisationInterval_2
+                            Jan 02 2020 00:00:00 UTC - Jan 03 2020 00:00:00 UTC
     """
 
     # Get forecast agent IRI for current test case
@@ -568,12 +566,9 @@ def test_create_prophet_covariates_forecast(
     sales = [sale + (20 * promo) for sale, promo in zip(sales, covariate1)]
     sales = [float(sale + (50 * event)) for sale, event in zip(sales, covariate2)]
 
-    test_data = {
-        dataIRI: sales,
-        # promotion, special event
-        covariates[0]: covariate1,
-        covariates[1]: covariate2
-    }
+    # Create test data dictionary depending on number of covariates
+    test_data = {dataIRI: sales, covariates[0]: covariate1} if len(covariates) == 1 else {dataIRI: sales, covariates[0]: covariate1, covariates[1]: covariate2}
+
 
     # Get required clients from fixture
     sparql_client, ts_client, derivation_client, rdb_url = initialise_clients
@@ -686,5 +681,171 @@ def test_create_prophet_covariates_forecast(
     print(f'Forecast errors for case: {case}_updated')
     for k,v in errors.items():
         print(f'{k}: {round(v,5)}')
+
+    print("All check passed.")
+
+
+#pytest.mark.skip(reason="")
+@pytest.mark.parametrize(
+    "derivation_input_set1, derivation_input_set2, fcmodelIRI1, fcmodelIRI2, dataIRI, ts_times, covariates, case1, case2",
+    [
+        (cf.DERIVATION_INPUTS_3, cf.DERIVATION_INPUTS_7, cf.FORECASTING_MODEL_1, cf.FORECASTING_MODEL_3, cf.IRI_TO_FORECAST_1,  cf.TIMES,
+          cf.COVARIATES_2, cf.TEST_CASE_5+'_covariates_comparison', cf.TEST_CASE_13+'_covariates_comparison'),
+    ],
+)
+def test_significance_covariates_forecast(
+    initialise_clients, create_example_agent, derivation_input_set1, derivation_input_set2, fcmodelIRI1, fcmodelIRI2, dataIRI, 
+    ts_times, covariates, case1, case2
+):
+    """
+    Test if Forecasting Agent performs better with covariates (using 
+    default Prophet model with covariates)
+    Forecasting with covariates is case-specific, so this test is for
+    an example constructed to be forecasted more accurately with covariates
+        - forecasts are created using Prophet
+        - historical data length (same as input_chunk_length for non-neural method):
+            336/8760h (HIST_DURATION_1, DURATION_1 or HIST_DURATION_2, DURATION_2)
+        - initial interval: OptimisationInterval_1
+                            Jan 01 2020 00:00:00 UTC - Jan 02 2020 00:00:00 UTC
+        - updated interval: OptimisationInterval_2
+                            Jan 02 2020 00:00:00 UTC - Jan 03 2020 00:00:00 UTC
+    derivation_input_set1, fcmodelIRI1 and case1 are for the model without covariates
+    derivation_input_set2, fcmodelIRI2 and case2 are for the model with covariates
+    """
+
+    # Get forecast agent IRI for current test case
+
+    #agent iri
+    agent_iri = cf.AGENT_w_OVERWRITING_IRI
+    agent_url = cf.AGENT_w_OVERWRITING_URL
+
+    # Generate synthetic test time series data (incl. required covariates)
+
+    # Promotion on every 6th and 7th time unit in a cycle of 7
+    covariate1 = [float(1) if i % 7 == 6 or i % 7 == 5 else float(0) for i in range(len(ts_times))]
+    # Special Event on every 9th and 24th time unit in a cycle of 25
+    covariate2 = [float(1) if i % 25 == 9 or i % 25 == 24 else float(0) for i in range(len(ts_times))]
+    # Baseline sales with a linear trend
+    sales = [50 + i for i in range(len(ts_times))]
+    # Increase sales for promotion and special event
+    sales = [sale + (20 * promo) for sale, promo in zip(sales, covariate1)]
+    sales = [float(sale + (50 * event)) for sale, event in zip(sales, covariate2)]
+
+    # Create test data dictionary
+    test_data = {dataIRI: sales, covariates[0]: covariate1, covariates[1]: covariate2}
+
+    # Get required clients from fixture
+    sparql_client, ts_client, derivation_client, rdb_url = initialise_clients
+
+    # Initialise all triples in test_triples repository
+    cf.initialise_triples(sparql_client)
+    cf.clear_database(rdb_url)
+
+    # Verify that model1 and model2 are witout and with covariates
+    model1 = sparql_client.get_fcmodel_details(fcmodelIRI1)
+    model2 = sparql_client.get_fcmodel_details(fcmodelIRI2)
+    assert not "covariates" in model1.keys()
+    assert len(model2['covariates']) > 0
+    
+    # Initialise time series in KG and RDB
+    for k, v in test_data.items():
+        ts_client.init_timeseries(dataIRI=k, times=ts_times, values=v,
+                                  ts_type=DOUBLE, time_format=TIME_FORMAT)
+
+    #1) Get forecast of agent without covariates
+
+    # Register derivation agents in KG
+    create_example_agent(ontoagent_service_iri=agent_iri, ontoagent_http_url=agent_url) 
+
+    # Create derivation instances for new information (incl. timestamps for pure inputs)
+    derivation = derivation_client.createSyncDerivationForNewInfo(agent_iri, derivation_input_set1,
+                                                                  dm.ONTODERIVATION_DERIVATIONWITHTIMESERIES)
+    derivation_iri = derivation.getIri()
+    print(f"Initialised successfully, created synchronous derivation instance: {derivation_iri}")
+
+    # Query input & output of the derivation instance
+    _, derivation_outputs = cf.get_derivation_inputs_outputs(derivation_iri, sparql_client)
+    print(f"Generated derivation outputs that belongsTo the derivation instance: {', '.join(derivation_outputs)}")
+
+    # Retrieve instantiated forecast and verify its details
+    fcIRI = list(derivation_outputs[dm.TS_FORECAST])[0]
+
+    # Assess initial forecast error and create plot for visual inspection
+    errors1 = cf.assess_forecast_error(dataIRI, fcIRI, sparql_client, ts_client, 
+                                      agent_url=agent_url, name=case1)
+    
+    ts1 = ts_client.retrieve_timeseries(fcIRI)
+    
+    print(f'Forecast errors for case: {case1}')
+    for k,v in errors1.items():
+        print(f'{k}: {round(v,5)}')
+
+
+    #2) Get forecast of agent with covariates
+
+    # Initialise all triples in test_triples repository
+    cf.initialise_triples(sparql_client)
+    cf.clear_database(rdb_url)
+
+    # Initialise time series in KG and RDB
+    for k, v in test_data.items():
+        ts_client.init_timeseries(dataIRI=k, times=ts_times, values=v,
+                                  ts_type=DOUBLE, time_format=TIME_FORMAT)
+
+    #Register derivation agents in KG
+    create_example_agent(ontoagent_service_iri=agent_iri, ontoagent_http_url=agent_url) 
+
+    # Create derivation instances for new information (incl. timestamps for pure inputs)
+    derivation = derivation_client.createSyncDerivationForNewInfo(agent_iri, derivation_input_set2,
+                                                                  dm.ONTODERIVATION_DERIVATIONWITHTIMESERIES)
+    derivation_iri = derivation.getIri()
+    print(f"Initialised successfully, created synchronous derivation instance: {derivation_iri}")
+
+    # Query input & output of the derivation instance
+    _, derivation_outputs = cf.get_derivation_inputs_outputs(derivation_iri, sparql_client)
+    print(f"Generated derivation outputs that belongsTo the derivation instance: {', '.join(derivation_outputs)}")
+
+    # Retrieve instantiated forecast and verify its details
+    fcIRI = list(derivation_outputs[dm.TS_FORECAST])[0]
+
+    # Assess initial forecast error and create plot for visual inspection
+    errors2 = cf.assess_forecast_error(dataIRI, fcIRI, sparql_client, ts_client, 
+                                      agent_url=agent_url, name=case2)
+    
+    ts2 = ts_client.retrieve_timeseries(fcIRI)
+
+    print(f'Forecast errors for case: {case2}')
+    for k,v in errors2.items():
+        print(f'{k}: {round(v,5)}')    
+        # Check if forecast with covariates is better than without
+        assert round(errors2[k], 5) <= round(errors1[k], 5)
+
+    # Plot the two forecasts for visual comparison
+    target = ts_client.retrieve_timeseries(dataIRI)
+    df_target = pd.DataFrame({'timestamp': target[0], 'actual data': target[1]})
+    df1 = pd.DataFrame({'timestamp': ts1[0], 'without covariates': ts1[1]})
+    df2 = pd.DataFrame({'timestamp': ts2[0], 'with covariates': ts2[1]})
+    # Convert 'timestamp' column to a datetime data type
+    df_target['timestamp'] = pd.to_datetime(df_target['timestamp'])
+    df_target.set_index('timestamp', inplace=True)
+    df1['timestamp'] = pd.to_datetime(df1['timestamp'])
+    df1.set_index('timestamp', inplace=True)
+    df2['timestamp'] = pd.to_datetime(df2['timestamp'])
+    df2.set_index('timestamp', inplace=True)
+    # Merge DataFrames (while keeping all historical data) and slice to relevant period
+    df = pd.merge(df_target, pd.merge(df1, df2, on='timestamp', how='outer'), on='timestamp', how='outer')
+    offset1 = pd.DateOffset(days=3)
+    offset2 = pd.DateOffset(days=1)
+    valid_indices = (df.index - offset2 <= df2.index.max()) & (df.index + offset1 >= df2.index.min())
+    valid_entries = df[valid_indices]
+
+    # Create new figure, plot and save to volume
+    ax = valid_entries.plot()
+    ax.set_xlabel('Timestamp')
+    ax.set_ylabel('Values')
+    ax.set_title("Prophet Forecasting with and without Covariates")
+    ax.grid(which='minor', axis='both')
+    fp = '/app/tests/test_plots/' + 'Prophet_comparison_with_and_without_covariates' + '.png'
+    plt.savefig(fp)
 
     print("All check passed.")
