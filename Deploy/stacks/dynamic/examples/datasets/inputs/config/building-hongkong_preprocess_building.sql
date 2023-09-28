84fa5d9ce998378152fa3528ff0ed84ae4026c98
ChungTingLao <chung.ting.lao.34@gmail.com>
2023-09-28 14:15:11 +0100

    dev-stack-building-thematic-vis: updated building data examples.

--- Deploy/stacks/dynamic/examples/datasets/inputs/config/building-hongkong_preprocess_building.sql
+++ Deploy/stacks/dynamic/examples/datasets/inputs/config/building-hongkong_preprocess_building.sql
@@ -1,45 +1,63 @@
-SET search_path TO public,
+SET
+    search_path TO public,
     citydb;
+
 --UPDATE "public"."raw_gdal" SET height = '0.1' WHERE height IS NULL;
 CREATE TABLE "public"."raw_data" AS (
-    SELECT parent,
+    SELECT
+        parent,
         average_height,
-        50.AS elevation,
+        50. AS elevation,
         ST_MakeValid(geometry) AS geometry
-    FROM (
-            SELECT parent,
+    FROM
+        (
+            SELECT
+                parent,
                 avg(height) AS average_height,
                 ST_Union(wkb_geometry) AS geometry
-            FROM (
-                    SELECT id,
+            FROM
+                (
+                    SELECT
+                        id,
                         TO_NUMBER(height, '99G999D9S') AS height,
-                        CASE
-                            WHEN rel IS NOT NULL THEN rel
-                            ELSE id
-                        END AS parent,
+case
+                            when rel IS NOT NULL then rel
+                            else id
+                        end AS parent,
                         ST_FlipCoordinates(wkb_geometry) AS wkb_geometry
-                    FROM (
-                            SELECT split_part(id, '/', 2) AS id,
+                    FROM
+                        (
+                            SELECT
+                                split_part(id, '/', 2) AS id,
                                 height,
-                                CAST(raw_gdal."@relations"->0->'rel' AS TEXT) AS rel,
+                                CAST(raw_gdal."@relations" -> 0 -> 'rel' AS TEXT) as rel,
                                 wkb_geometry
-                            FROM "public"."raw_gdal"
-                            WHERE GeometryType(wkb_geometry) = 'POLYGON'
-                                OR GeometryType(wkb_geometry) = 'MULTIPOLYGON'
+                            FROM
+                                "public"."raw_gdal"
+                            WHERE
+                                GeometryType(wkb_geometry) = 'POLYGON'
+                                or GeometryType(wkb_geometry) = 'MULTIPOLYGON'
                         ) AS tmp
-                    WHERE rel IS NOT NULL
-                        OR height IS NOT NULL
-                        OR id IN (
-                            SELECT CAST(raw_gdal."@relations"->0->'rel' AS TEXT)
-                            FROM raw_gdal
+                    WHERE
+                        rel IS NOT NULL
+                        or height IS NOT NULL
+                        or id IN (
+                            SELECT
+                                CAST(raw_gdal."@relations" -> 0 -> 'rel' AS TEXT)
+                            FROM
+                                raw_gdal
                         )
                 ) AS tmp2
-            GROUP BY parent
+            GROUP BY
+                parent
         ) AS tmp3
-    WHERE average_height IS NOT NULL
+    WHERE
+        average_height IS NOT NULL
 );
-CREATE TABLE "public"."raw_building" AS (
-    SELECT "parent",
+
+create table "public"."raw_building_XtoCityDB" AS (
+    SELECT
+        "parent",
         'building_' || gen_random_uuid() AS "gmlid",
         ST_Translate(
             ST_Extrude("geometry", 0, 0, "average_height"),
@@ -48,16 +66,22 @@ CREATE TABLE "public"."raw_building" AS (
             "elevation"
         ) AS "geom",
         "average_height" AS "mh"
-    FROM "public"."raw_data"
-    WHERE "average_height" IS NOT NULL
+    FROM
+        "public"."raw_data"
+    WHERE
+        "average_height" IS NOT NULL
 );
-CREATE TABLE "public"."raw_surface" AS (
-    SELECT "building_gmlid",
+
+create table "public"."raw_surface_XtoCityDB" AS (
+    SELECT
+        "building_gmlid",
         'surface_' || gen_random_uuid() AS "gmlid",
         "class",
         ST_Reverse("geom") AS geom
-    FROM (
-            SELECT "building_gmlid",
+    FROM
+        (
+            SELECT
+                "building_gmlid",
                 "geom",
                 CASE
                     WHEN ST_Zmin("geom") = ST_Zmax("geom") THEN CASE
@@ -66,11 +90,14 @@ CREATE TABLE "public"."raw_surface" AS (
                     END
                     ELSE 34
                 END AS "class"
-            FROM (
-                    SELECT "gmlid" AS "building_gmlid",
+            FROM
+                (
+                    SELECT
+                        "gmlid" AS "building_gmlid",
                         ST_Zmin("geom") AS "bzl",
                         (ST_Dump(ST_CollectionExtract("geom"))).geom AS "geom"
-                    FROM "public"."raw_building"
+                    FROM
+                        "public"."raw_building_XtoCityDB"
                 ) AS "table1"
         ) AS "table2"
 );
