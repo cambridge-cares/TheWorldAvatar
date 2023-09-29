5a089973504e066dfdd08d2ab77476d81bb71f08
markushofmeister <mh807@cam.ac.uk>
2023-09-29 05:44:28 +0000

    dev-create-district-heating-optimisation-agent: Removed some outdated files

---
+++ Agents/DistrictHeatingOptimisationAgent/dhoptimisation/agent/config.py
@@ -0,0 +1,11 @@
+################################################
+# Authors: Markus Hofmeister (mh807@cam.ac.uk) #    
+# Date: 29 Sep 2023                            #
+################################################
+
+# The purpose of this module is to provide some overarching configurations and
+# settings for the agent
+
+
+# Define time format for timeseries
+TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
