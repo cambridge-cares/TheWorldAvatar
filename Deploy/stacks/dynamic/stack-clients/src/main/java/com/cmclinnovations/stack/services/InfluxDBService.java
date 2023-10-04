f2e1aa38b829348e4a85a578e4c162e3ad65d89e
Jiawei Lai <jlai@cmclinnovations.com>
2023-10-04 15:38:23 +0100

    dev_InfluxDB_Stack: Created the influxDB service in the stack

---
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/InfluxDBService.java
@@ -0,0 +1,13 @@
+package com.cmclinnovations.stack.services;
+
+import com.cmclinnovations.stack.services.config.ServiceConfig;
+
+import java.nio.file.Path;
+import java.util.Map;
+
+import com.cmclinnovations.stack.clients.core.EndpointNames;
+import com.cmclinnovations.stack.clients.postgis.PostGISEndpointConfig;
+
+public final class InfluxDBService extends ContainerService {
+    
+}
