c7f963ad2cbd93af3d08c57b8bedea00e6a1daa4
Jiawei Lai <jlai@cmclinnovations.com>
2023-10-04 16:20:40 +0100

    dev-InfluxDB_Stack: Added default variables

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/InfluxDBService.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/InfluxDBService.java
@@ -9,5 +9,15 @@ import com.cmclinnovations.stack.clients.core.EndpointNames;
 import com.cmclinnovations.stack.clients.postgis.PostGISEndpointConfig;
 
 public final class InfluxDBService extends ContainerService {
-    
+
+    private static final String INFLUXDB_USER_KEY = "DOCKER_INFLUXDB_INIT_USERNAME";
+
+    public static final String TYPE = "influxdb";
+    private static final Path influxdb_FILE = Path.of("/root", ".influxdb");
+    private static final String DEFAULT_USERNAME = "admin";
+    private static final String DEFAULT_PORT = "8086";
+    private static final String DEFAULT_PASSWORD_FILE = "/run/secrets/influxdb_password";
+
+    private final PostGISEndpointConfig endpointConfig;
+
 }
