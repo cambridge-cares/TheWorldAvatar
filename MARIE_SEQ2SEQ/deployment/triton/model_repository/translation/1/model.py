efee7f1cc86de09b1e575bdf645887a03a1de72e
Dan Tran <nmdt2@cam.ac.uk>
2023-10-05 14:43:18 +0800

    dev-marie-plm: Update triton server

--- MARIE_SEQ2SEQ/deployment/triton/model_repository/translation/1/model.py
+++ MARIE_SEQ2SEQ/deployment/triton/model_repository/translation/1/model.py
@@ -1,27 +1,18 @@
+import os
 from typing import List
+
+
 import torch
 import numpy as np
 import triton_python_backend_utils as pb_utils
-
-from optimum.onnxruntime import ORTModelForSeq2SeqLM
 from transformers import AutoTokenizer
+from transformers.generation.configuration_utils import GenerationConfig
+from optimum.onnxruntime import ORTModelForSeq2SeqLM
+from optimum.utils.save_utils import maybe_load_preprocessors
 
-
-T5_INPUT_PREFIX = "translate to SPARQL: "
-T5_INPUT_ENCODINGS = {
-    "<": "ls_th",
-    ">": "gt_th",
-}
-T5_INPUT_DECODINGS = {v: k for k, v in T5_INPUT_ENCODINGS.items()}
-
-T5_OUTPUT_ENCODINGS = {
-    "{": "op_br",
-    "}": "cl_br",
-    "?": "var_",
-    "<": "ls_th",
-    ">": "gr_th",
-}
-T5_OUTPUT_DECODINGS = {v: k for k, v in T5_OUTPUT_ENCODINGS.items()}
+from compact_query.compact_query_rep import CompactQueryRep
+from compact_query.correct_relations import RelationCorrector
+from compact_query.correct_spans import SpanCorrector
 
 
 def replace_multi(text: str, mapper: dict):
@@ -31,18 +22,68 @@ def replace_multi(text: str, mapper: dict):
 
 
 class TritonPythonModel:
+    MODEL_PATH = "/models/translation/1/model"
+    T5_INPUT_PREFIX = "translate to SPARQL: "
+    T5_INPUT_ENCODINGS = {
+        "<": "&lt;",
+        ">": "&gt;",
+    }
+    T5_OUTPUT_DECODINGS = {
+        "var_": "?",
+        "&lcub;": "{",
+        "&rcub;": "}",
+        "&lt;": "<",
+        "&gt;": ">"
+    }
+
+    def _load_model(self):
+        encoder_path = os.path.join(self.MODEL_PATH, "encoder_model_quantized.onnx")
+        decoder_path = os.path.join(self.MODEL_PATH, "decoder_model_quantized.onnx")
+        decoder_wp_path = os.path.join(self.MODEL_PATH, "decoder_with_past_model_quantized.onnx")
+
+        (
+            encoder_session,
+            decoder_session,
+            decoder_wp_session,
+        ) = ORTModelForSeq2SeqLM.load_model(
+            encoder_path=encoder_path,
+            decoder_path=decoder_path,
+            decoder_with_past_path=decoder_wp_path,
+        )
+        config = ORTModelForSeq2SeqLM._load_config(self.MODEL_PATH)
+        generation_config = GenerationConfig.from_pretrained(self.MODEL_PATH)
+        preprocessors = maybe_load_preprocessors(self.MODEL_PATH)
+
+        return ORTModelForSeq2SeqLM(
+            encoder_session=encoder_session,
+            decoder_session=decoder_session,
+            config=config,
+            onnx_paths=[
+                encoder_path,
+                decoder_path,
+                decoder_wp_path,
+            ],
+            decoder_with_past_session=decoder_wp_session,
+            model_save_dir=self.MODEL_PATH,
+            preprocessors=preprocessors,
+            generation_config=generation_config,
+        )
+
     def initialize(self, args):
-        self.tokenizer = AutoTokenizer.from_pretrained("/models/translation/1/model")
-        self.model = ORTModelForSeq2SeqLM.from_pretrained("/models/translation/1/model")
+        self.tokenizer = AutoTokenizer.from_pretrained(self.MODEL_PATH)
+        self.model = self._load_model()
         self.max_new_tokens = 256
 
+        self.span_corrector = SpanCorrector()
+        self.relation_corrector = RelationCorrector()
+
     def preprocess(self, text):
-        text = replace_multi(text, T5_INPUT_ENCODINGS)
-        text = T5_INPUT_PREFIX + text
+        text = replace_multi(text, self.T5_INPUT_ENCODINGS)
+        text = self.T5_INPUT_PREFIX + text
         return text
     
     def postprocess(self, text):
-        text = replace_multi(text, T5_OUTPUT_DECODINGS)
+        text = replace_multi(text, self.T5_OUTPUT_DECODINGS)
         return text
 
     def translate(self, text):
@@ -68,7 +109,10 @@ class TritonPythonModel:
             output_text = self.translate(input_text)
 
             try:
-                verbose_output = AbstractQueryRep.from_string(output_text).compact2verbose().to_query_string()
+                query = CompactQueryRep.from_string(output_text)
+                query = self.span_corrector.correct(query, nlq=input_text)
+                query = self.relation_corrector.correct(query)
+                verbose_output = query.to_verbose()
             except:
                 verbose_output = ""
             
@@ -81,268 +125,3 @@ class TritonPythonModel:
             responses.append(inference_response)
 
         return responses
-
-
-RESULT_CLAUSE_KWS = ["SELECT"]
-
-SPECIES_FROM_IDENTIFIER_PATTERN_COMPACT_PREFIX = "?SpeciesIRI ?hasIdentifier ?species"
-PROPERTY_PATTERN_COMPACT_PREFIX = "?SpeciesIRI os:hasProperty"
-IDENTIFIER_PATTERN_COMPACT_PREFIX = "?SpeciesIRI os:hasIdentifier"
-CHEMCLASS_PATTERN_COMPACT_PREFIX = "?SpeciesIRI os:hasChemicalClass ?ChemicalClassValue"
-USE_PATTERN_COMPACT_PREFIX = "?SpeciesIRI os:hasUse ?UseValue"
-ALL_PROPERTIES_PATTERN_COMPACT_PREFIX = (
-    "?SpeciesIRI ?hasPropertyName ?PropertyNameValue"
-)
-ALL_IDENTIFIERS_PATTERN_COMPACT_PREFIX = (
-    "?SpeciesIRI ?hasIdentifierName ?IdentifierNameValue"
-)
-
-SPECIES_FROM_QUALIFIERS_PATTERN_COMPACT_PREFIX = (
-    "?SpeciesIRI os:hasIUPACName ?IUPACNameValue"
-)
-
-SPECIES_FROM_IDENTIFIER_PATTERNS_VERBOSE = [
-    "?SpeciesIRI rdf:type os:Species ; rdfs:label ?label .",
-    "?SpeciesIRI ?hasIdentifier ?IdentifierIRI .",
-    "?IdentifierIRI rdf:type ?Identifier ; os:value ?species .",
-    "?Identifier rdfs:subClassOf os:Identifier .",
-]
-PROPERTY_PATTERNS_VERBOSE = lambda property_name: [
-    f"?SpeciesIRI os:has{property_name} ?{property_name}IRI .",
-    f"?{property_name}IRI os:value ?{property_name}Value ; os:unit ?{property_name}UnitIRI ; os:hasProvenance ?{property_name}ProvenanceIRI .",
-    f"?{property_name}UnitIRI rdfs:label ?{property_name}UnitValue .",
-    (
-        f"OPTIONAL{{?{property_name}IRI os:hasReferenceState ?{property_name}ReferenceStateIRI .\n"
-        f"?{property_name}ReferenceStateIRI os:value ?{property_name}ReferenceStateValue ; os:unit ?{property_name}ReferenceStateUnitIRI .\n"
-        f"?{property_name}ReferenceStateUnitIRI rdfs:label ?{property_name}ReferenceStateUnitValue .}}"
-    ),
-]
-IDENTIFIER_PATTERNS_VERBOSE = lambda identifier_name: [
-    f"?SpeciesIRI os:has{identifier_name} ?{identifier_name}IRI .",
-    f"?{identifier_name}IRI os:value ?{identifier_name}Value .",
-]
-CHEMCLASS_PATTERNS_VERBOSE = [
-    "?SpeciesIRI os:hasChemicalClass* ?x .",
-    "?x ?y ?z .",
-    "?z rdfs:subClassOf* ?ChemicalClassIRI .",
-    "?ChemicalClassIRI rdf:type os:ChemicalClass  ; rdfs:label ?ChemicalClassValue .",
-]
-USE_PATTERNS_VERBOSE = [
-    "?SpeciesIRI os:hasUse ?UseIRI .",
-    "?UseIRI rdfs:label ?UseValue .",
-]
-ALL_PROPERTIES_PATTERNS_VERBOSE = [
-    "?SpeciesIRI ?hasPropertyName ?PropertyNameIRI .",
-    "?PropertyNameIRI  rdf:type ?PropertyName .",
-    "?PropertyName rdfs:subClassOf os:Property .",
-    "?PropertyNameIRI os:value ?PropertyNameValue ; os:unit ?PropertyNameUnitIRI ; os:hasProvenance ?PropertyNameProvenanceIRI .",
-    "?PropertyNameUnitIRI rdfs:label ?PropertyNameUnitValue .",
-    (
-        "OPTIONAL{?PropertyNameIRI os:hasReferenceState ?PropertyNameReferenceStateIRI .\n"
-        "?PropertyNameReferenceStateIRI os:value ?PropertyNameReferenceStateValue ; os:unit ?PropertyNameReferenceStateUnitIRI .\n"
-        "?PropertyNameReferenceStateUnitIRI rdfs:label ?PropertyNameReferenceStateUnitValue .}\n"
-    ),
-    "BIND(strafter(str(?PropertyName),'#') AS ?PropertyLabel)",
-]
-ALL_IDENTIFIERS_PATTERNS_VERBOSE = [
-    "?SpeciesIRI ?hasIdentifierName ?IdentifierNameIRI .",
-    "?IdentifierNameIRI  rdf:type ?IdentifierName .",
-    "?IdentifierName rdfs:subClassOf os:Identifier .",
-    "?IdentifierNameIRI os:value ?IdentifierNameValue .",
-    "BIND(strafter(str(?IdentifierName),'#') AS ?IdentifierLabel)",
-]
-
-SPECIES_FROM_QUALIFIERS_PATTERNS_VERBOSE = [
-    "?SpeciesIRI rdf:type os:Species ; rdfs:label ?label .",
-    "?SpeciesIRI os:hasIUPACName ?IUPACNameIRI .",
-    "?IUPACNameIRI os:value ?IUPACNameValue .",
-]
-
-
-class AbstractQueryRep:
-    def __init__(
-        self,
-        result_clause: str,
-        where_clause: List[str],
-    ):
-        self.result_clause = result_clause
-        self.where_clause = where_clause
-
-    def __eq__(self, other):
-        return (
-            isinstance(other, AbstractQueryRep)
-            and self.result_clause == other.result_clause
-            and self.where_clause == other.where_clause
-        )
-
-    def __repr__(self):
-        return repr(vars(self))
-
-    def to_query_string(self):
-        return "{result_clause}\nWHERE {{\n{where_clause}\n}}".format(
-            result_clause=self.result_clause, where_clause="\n".join(self.where_clause)
-        )
-
-    def compact2verbose(self):
-        if not self.result_clause.startswith("SELECT DISTINCT"):
-            raise Exception("Unexpected result clause: " + self.result_clause)
-
-        result_clause_tokens = ["SELECT DISTINCT ?label"] + self.result_clause[
-            len("SELECT DISTINCT") :
-        ].strip().split()
-
-        where_clause = []
-        for pattern in self.where_clause:
-            if pattern.startswith("FILTER"):
-                where_clause.append(pattern)
-
-            # assume it's triple pattern, species -> properties/identifiers/chemclass
-            elif pattern.startswith(SPECIES_FROM_IDENTIFIER_PATTERN_COMPACT_PREFIX):
-                where_clause.extend(SPECIES_FROM_IDENTIFIER_PATTERNS_VERBOSE)
-            elif pattern.startswith(PROPERTY_PATTERN_COMPACT_PREFIX):
-                property_name = pattern[
-                    len(PROPERTY_PATTERN_COMPACT_PREFIX) : advance_ptr_to_space(
-                        pattern, len(PROPERTY_PATTERN_COMPACT_PREFIX)
-                    )
-                ].strip()
-
-                replace_list_element(
-                    result_clause_tokens,
-                    f"?{property_name}Value",
-                    f"?{property_name}Value ?{property_name}UnitValue ?{property_name}ReferenceStateValue ?{property_name}ReferenceStateUnitValue",
-                )
-
-                where_clause.extend(PROPERTY_PATTERNS_VERBOSE(property_name))
-            elif pattern.startswith(IDENTIFIER_PATTERN_COMPACT_PREFIX):
-                identifier_name = pattern[
-                    len(IDENTIFIER_PATTERN_COMPACT_PREFIX) : advance_ptr_to_space(
-                        pattern, len(PROPERTY_PATTERN_COMPACT_PREFIX)
-                    )
-                ].strip()
-                where_clause.extend(IDENTIFIER_PATTERNS_VERBOSE(identifier_name))
-            elif pattern.startswith(CHEMCLASS_PATTERN_COMPACT_PREFIX):
-                where_clause.extend(CHEMCLASS_PATTERNS_VERBOSE)
-            elif pattern.startswith(USE_PATTERN_COMPACT_PREFIX):
-                where_clause.extend(USE_PATTERNS_VERBOSE)
-            elif pattern.startswith(ALL_PROPERTIES_PATTERN_COMPACT_PREFIX):
-                replace_list_element(
-                    result_clause_tokens,
-                    old="?PropertyNameValue",
-                    new="?PropertyLabel ?PropertyNameValue ?PropertyNameUnitValue ?PropertyNameReferenceStateValue ?PropertyNameReferenceStateUnitValue",
-                )
-
-                where_clause.extend(ALL_PROPERTIES_PATTERNS_VERBOSE)
-            elif pattern.startswith(ALL_IDENTIFIERS_PATTERN_COMPACT_PREFIX):
-                replace_list_element(
-                    result_clause_tokens,
-                    old="?IdentifierNameValue",
-                    new="?IdentifierLabel ?IdentifierNameValue",
-                )
-
-                where_clause.extend(ALL_IDENTIFIERS_PATTERNS_VERBOSE)
-
-            # qualifiers -> species
-            elif pattern.startswith(SPECIES_FROM_QUALIFIERS_PATTERN_COMPACT_PREFIX):
-                where_clause.extend(SPECIES_FROM_QUALIFIERS_PATTERNS_VERBOSE)
-            else:
-                raise Exception("Unexpected compact clause: " + pattern)
-
-        return AbstractQueryRep(" ".join(result_clause_tokens), where_clause)
-
-    @classmethod
-    def _does_startwith_result_clause_kw(cls, query: str, ptr: int):
-        return any(query.startswith(kw, ptr) for kw in RESULT_CLAUSE_KWS)
-
-    @classmethod
-    def from_string(cls, query: str):
-        ptr = 0
-        while not cls._does_startwith_result_clause_kw(query, ptr) and ptr < len(query):
-            ptr += 1
-        if ptr >= len(query):
-            raise ValueError("Result clause is missing from the query: ", query)
-        result_clause_idx = ptr
-
-        ptr = advance_ptr_to_kw(query, "WHERE", ptr)
-        if ptr >= len(query):
-            raise ValueError("WHERE clause is missing from the query: ", query)
-        where_clause_idx = ptr
-
-        result_clause = query[result_clause_idx:where_clause_idx].strip()
-
-        ptr += len("WHERE")
-        ptr = advance_ptr_thru_space(query, ptr)
-        if query[ptr] != "{":
-            raise ValueError("Missing open bracket after WHERE keyword: ", query)
-
-        ptr += 1
-        # assume that WHERE clause contains only basic triple patterns and FILTER clauses
-        where_clause = []
-        while True:
-            ptr = advance_ptr_thru_space(query, ptr)
-
-            if query[ptr] == "}":
-                break
-            if ptr >= len(query):
-                raise ValueError(
-                    "Close curly bracket is missing from the WHERE clause: " + query
-                )
-
-            start_idx = ptr
-            if query.startswith("FILTER", ptr):
-                ptr += len("FILTER")
-                ptr = advance_ptr_thru_space(query, ptr)
-                if query[ptr] != "(":
-                    raise ValueError(
-                        "Open bracket is missing from FITLER clause: "
-                        + query[start_idx:]
-                    )
-
-                open_brac_num = 1
-                while open_brac_num > 0:
-                    ptr += 1
-                    if query[ptr] == "(":
-                        open_brac_num += 1
-                    elif query[ptr] == ")":
-                        open_brac_num -= 1
-
-                ptr += 1
-            else:  # assume it's the triple pattern
-                ptr = advance_ptr_to_kw(query, ".", ptr)
-                if ptr >= len(query):
-                    raise ValueError(
-                        "Full-stop is missing from triple pattern: " + query[start_idx:]
-                    )
-                ptr += 1
-
-            end_idx = ptr
-            where_clause.append(query[start_idx:end_idx].strip())
-
-        return cls(result_clause, where_clause)
-
-
-def advance_ptr_to_kw(text: str, kw: str, ptr: int=0):
-    while ptr < len(text) and not text.startswith(kw, ptr):
-        ptr += 1
-    return ptr
-
-
-def advance_ptr_thru_space(text: str, ptr: int=0):
-    while ptr < len(text) and text[ptr].isspace():
-        ptr += 1
-    return ptr
-
-
-def advance_ptr_to_space(text: str, ptr: int=0):
-    while ptr < len(text) and not text[ptr].isspace():
-        ptr += 1
-    return ptr
-
-
-def replace_list_element(lst: list, old, new):
-    found = False
-    for i, val in enumerate(lst):
-        if val == old:
-            lst[i] = new
-            found = True
-        if found:
-            break
