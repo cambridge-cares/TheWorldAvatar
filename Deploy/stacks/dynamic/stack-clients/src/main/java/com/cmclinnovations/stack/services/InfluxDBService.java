48850e53da2a2adbda0e94bbfd6eaa30b9341464
Jiawei Lai <jlai@cmclinnovations.com>
2023-10-11 11:47:36 +0100

    dev-InfluxDB_Stack： Modify the InfluxDBService file

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/InfluxDBService.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/InfluxDBService.java
@@ -1,66 +1,78 @@
 package com.cmclinnovations.stack.services;
 
-import com.cmclinnovations.stack.clients.core.EndpointNames;
 import com.cmclinnovations.stack.services.config.ServiceConfig;
+import com.cmclinnovations.stack.clients.influxdb.InfluxDBEndpointConfig;
 import java.nio.file.Path;
 import java.util.Map;
 
-import java.util.logging.Logger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 public final class InfluxDBService extends ContainerService {
 
-    private static final String TYPE = "influxdb";
-    private static final Path SECRETS_FILE = Path.of("/stacks/dynamic/stack-manager/inputs/secrets");
-
-    // Default environment variables for InfluxDB
+    // Constants for environment variable keys and default values
     private static final String DEFAULT_INIT_MODE = "setup";
     private static final String DEFAULT_USERNAME = "admin";
-    private static final String DEFAULT_PASSWORD = "admin123";
     private static final String DEFAULT_ORG = "TestOrg";
     private static final String DEFAULT_BUCKET = "TestBucket";
     private static final String DEFAULT_RETENTION = "1w";
-    private static final String DEFAULT_ADMIN_TOKEN = "your_default_admin_token_here";
 
+    // Secrets files address
     private static final String DEFAULT_PASSWORD_FILE = "/run/secrets/influxdb_password";
+    private static final String DEFAULT_TOKEN_FILE = "/run/secrets/influxdb_token";
 
-    private static final Logger logger = Logger.getLogger(InfluxDBService.class.getName());
+    // Configuration file path for InfluxDB inside the container
+    private static final Path INFLUXDB_CONFIG_PATH = Path.of("/etc/influxdb2/influxdb.conf");
+
+    // Endpoint configuration object for InfluxDB
+    private final InfluxDBEndpointConfig endpointConfig;
 
     public InfluxDBService(String stackName, ServiceConfig config) {
         super(stackName, config);
 
-        logger.info("Starting post-startup configuration...");
-        // Use the methods from ContainerService to set environment variables
+        // Set environment variables if they are not already set
         setEnvironmentVariableIfAbsent("DOCKER_INFLUXDB_INIT_MODE", DEFAULT_INIT_MODE);
         setEnvironmentVariableIfAbsent("DOCKER_INFLUXDB_INIT_USERNAME", DEFAULT_USERNAME);
-        setEnvironmentVariableIfAbsent("DOCKER_INFLUXDB_INIT_PASSWORD", DEFAULT_PASSWORD);
         setEnvironmentVariableIfAbsent("DOCKER_INFLUXDB_INIT_ORG", DEFAULT_ORG);
         setEnvironmentVariableIfAbsent("DOCKER_INFLUXDB_INIT_BUCKET", DEFAULT_BUCKET);
         setEnvironmentVariableIfAbsent("DOCKER_INFLUXDB_INIT_RETENTION", DEFAULT_RETENTION);
-        setEnvironmentVariableIfAbsent("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN", DEFAULT_ADMIN_TOKEN);
+        setEnvironmentVariableIfAbsent("INFLUXDB_PASSWORD_FILE", DEFAULT_PASSWORD_FILE);
+        setEnvironmentVariableIfAbsent("INFLUXDB_TOKEN_FILE", DEFAULT_TOKEN_FILE);
+
+        // Initialize the endpoint configuration
+        endpointConfig = new InfluxDBEndpointConfig(
+            "InfluxDB", 
+            getHostName(), 
+            "8086", 
+            getEnvironmentVariable("DOCKER_INFLUXDB_INIT_USERNAME"), 
+            getEnvironmentVariable("INFLUXDB_PASSWORD_FILE"),
+            getEnvironmentVariable("DOCKER_INFLUXDB_INIT_ORG"),
+            getEnvironmentVariable("DOCKER_INFLUXDB_INIT_BUCKET")
+        );
     }
 
     @Override
     public void doPostStartUpConfiguration() {
-        // Write the environment variables to the secrets file
-        writeSecretsFile(SECRETS_FILE);
-
-        // Any other post-startup configuration can be done here
-    }
-
-    private void writeSecretsFile(Path filePath) {
-        // Create a map of secret key-value pairs to be written to the secrets file
-        Map<String, byte[]> secretData = Map.of(
-            "DOCKER_INFLUXDB_INIT_MODE", getEnvironmentVariable("DOCKER_INFLUXDB_INIT_MODE").getBytes(),
-            "DOCKER_INFLUXDB_INIT_USERNAME", getEnvironmentVariable("DOCKER_INFLUXDB_INIT_USERNAME").getBytes(),
-            "DOCKER_INFLUXDB_INIT_PASSWORD", getEnvironmentVariable("DOCKER_INFLUXDB_INIT_PASSWORD").getBytes(),
-            "DOCKER_INFLUXDB_INIT_ORG", getEnvironmentVariable("DOCKER_INFLUXDB_INIT_ORG").getBytes(),
-            "DOCKER_INFLUXDB_INIT_BUCKET", getEnvironmentVariable("DOCKER_INFLUXDB_INIT_BUCKET").getBytes(),
-            "DOCKER_INFLUXDB_INIT_RETENTION", getEnvironmentVariable("DOCKER_INFLUXDB_INIT_RETENTION").getBytes(),
-            "DOCKER_INFLUXDB_INIT_ADMIN_TOKEN", getEnvironmentVariable("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN").getBytes()
-        );
-        
-        // Use the sendFiles method from ContainerService to send the secrets to the specified file path
-        sendFiles(secretData, filePath.toString());
-    }
+        // Write endpoint configuration to some storage (file, database, etc.)
+        writeEndpointConfig(endpointConfig);
     
+        // Send the influxdb.conf file to the container
+        sendFiles(Map.of(
+                INFLUXDB_CONFIG_PATH.getFileName().toString(),
+                getConfigFileContent()),
+                INFLUXDB_CONFIG_PATH.getParent().toString());
+    }
+
+    // Helper method to get the content of the config file as a byte array
+    private byte[] getConfigFileContent() {
+        // Replace with actual logic for reading the file
+        // For example, read the file located at the standard path inside the container
+        try {
+            return Files.readAllBytes(INFLUXDB_CONFIG_PATH);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read InfluxDB configuration file", e);
+        }
+    }
 }
