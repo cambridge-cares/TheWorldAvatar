435d9211f09f00afb308a4093b8b0ad365893ff7
Kok Foong Lee <kokfoong.lee@cares.cam.ac.uk>
2023-10-20 16:01:16 +0800

    dev-aermod-vis: version number updates

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 # pull docker images from docker.cmclinnovations.com because stack manager can't pull these
-docker pull ghcr.io/cambridge-cares/virtual-sensor-agent:1.3-dev-aermod-vis-SNAPSHOT
-docker pull ghcr.io/cambridge-cares/dispersion-interactor:1.3-dev-aermod-vis-SNAPSHOT
-docker pull ghcr.io/cambridge-cares/aermod-agent:1.3-dev-aermod-vis-SNAPSHOT
-docker pull ghcr.io/cambridge-cares/python-service:1.3-dev-aermod-vis-SNAPSHOT
-docker pull ghcr.io/cambridge-cares/emissions-agent:1.3-dev-aermod-vis-SNAPSHOT
-docker pull ghcr.io/cambridge-cares/ship-input-agent:1.3-dev-aermod-vis-SNAPSHOT
 docker pull docker.cmclinnovations.com/weatheragent:1.1.1
 docker pull docker.cmclinnovations.com/file-server:1.0.0
-docker pull ghcr.io/cambridge-cares/feature-info-agent:2.0.1
 # starts up all required components of virtual sensor
 (cd stack-manager && ./stack.sh start ship-stack)
 # copy required files into containers
