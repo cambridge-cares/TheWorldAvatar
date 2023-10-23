e06ebc3d4cbcc95da8091df3168f685de4939df6
ChungTingLao <chung.ting.lao.34@gmail.com>
2023-10-23 16:47:50 +0100

    hotfix-raster-postgis: update search path by pattern matching instead of relying on fixed index.

--- Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/postgis/postgis_reset_schema.sql
+++ Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/postgis/postgis_reset_schema.sql
@@ -6,16 +6,32 @@ WITH "sdb" AS (
         JOIN "pg_catalog"."pg_database" ON "pg_catalog"."pg_db_role_setting"."setdatabase" = "pg_catalog"."pg_database"."oid"
     WHERE
         "pg_catalog"."pg_database"."datname" = '{database}'
+),
+"rr" AS (
+    SELECT
+        (
+            regexp_replace(
+                unnest("setconfig"),
+                '^(search_path=)(.*?)(?:(?:, *public)|(?:public *,))(.*)$',
+                '\1public,\2\3'
+            )
+        ) AS x
+    FROM
+        "pg_catalog"."pg_db_role_setting"
+        JOIN "sdb" ON "pg_catalog"."pg_db_role_setting"."setdatabase" = "sdb"."id"
+),
+"new_value" AS (
+    SELECT
+        array_agg("x") AS "y"
+    FROM
+        "rr"
 )
 UPDATE
     "pg_catalog"."pg_db_role_setting"
 SET
-    "setconfig"[1] = replace(
-            replace(setconfig [1], 'public,', ''),
-            'search_path=',
-            'search_path=public,'
-        )
+    "setconfig" = "new_value"."y"
 FROM
-    "sdb"
+    "sdb",
+    "new_value"
 WHERE
     "pg_catalog"."pg_db_role_setting"."setdatabase" = "sdb"."id"
