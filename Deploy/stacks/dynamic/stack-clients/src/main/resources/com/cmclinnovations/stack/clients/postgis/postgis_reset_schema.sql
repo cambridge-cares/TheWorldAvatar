e572a25de53d2c7328d741167dba01461512f10a
George Brownbridge <gbrownbridge@cmclinnovations.com>
2023-10-24 17:01:37 +0100

    Simplified query in postgis_reset_schema.sql
    Used implicit lateral joins to simplify the SQL query.

--- Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/postgis/postgis_reset_schema.sql
+++ Deploy/stacks/dynamic/stack-clients/src/main/resources/com/cmclinnovations/stack/clients/postgis/postgis_reset_schema.sql
@@ -1,37 +1,18 @@
-WITH "sdb" AS (
-    SELECT
-        "pg_catalog"."pg_db_role_setting"."setdatabase" AS "id"
-    FROM
-        "pg_catalog"."pg_db_role_setting"
-        JOIN "pg_catalog"."pg_database" ON "pg_catalog"."pg_db_role_setting"."setdatabase" = "pg_catalog"."pg_database"."oid"
-    WHERE
-        "pg_catalog"."pg_database"."datname" = '{database}'
-),
-"rr" AS (
-    SELECT
-        (
+UPDATE pg_catalog.pg_db_role_setting
+SET setconfig = (
+        SELECT array_agg(new)
+        FROM
+            unnest(setconfig) AS existing,
             regexp_replace(
-                unnest("setconfig"),
+                existing,
                 '^(search_path=)(.*?)(?:(?:, *public)|(?:public *,))(.*)$',
                 '\1public,\2\3'
-            )
-        ) AS x
-    FROM
-        "pg_catalog"."pg_db_role_setting"
-        JOIN "sdb" ON "pg_catalog"."pg_db_role_setting"."setdatabase" = "sdb"."id"
-),
-"new_value" AS (
-    SELECT
-        array_agg("x") AS "y"
-    FROM
-        "rr"
-)
-UPDATE
-    "pg_catalog"."pg_db_role_setting"
-SET
-    "setconfig" = "new_value"."y"
-FROM
-    "sdb",
-    "new_value"
-WHERE
-    "pg_catalog"."pg_db_role_setting"."setdatabase" = "sdb"."id"
+            ) AS new
+        GROUP BY setdatabase
+    )
+WHERE (
+        SELECT TRUE
+        FROM pg_catalog.pg_database
+        WHERE datname = '{database}'
+            AND setdatabase = oid
+    )
