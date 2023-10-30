074191b10e7166500918959f8a857e0ebc8491e1
Seán Courtney <scourtney@cmclinnovations.com>
2023-10-30 11:07:24 +0000

    963-filter-agent: remove unneeded import

--- Agents/FilterAgent/filter-agent/src/main/java/com/cmclinnovations/filteragent/FilterAgentApplication.java
+++ Agents/FilterAgent/filter-agent/src/main/java/com/cmclinnovations/filteragent/FilterAgentApplication.java
@@ -3,7 +3,6 @@ package com.cmclinnovations.filteragent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
