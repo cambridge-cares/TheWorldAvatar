9e21c96cbfe5b521a53ff856262b1a3d94b8615f
Jiawei Lai <jlai@cmclinnovations.com>
2023-11-01 15:39:24 +0000

    dev-Vision: Reorganise the entry_point.py.

--- Agents/VisionAgent/visionagent/entry_point.py
+++ Agents/VisionAgent/visionagent/entry_point.py
@@ -14,22 +14,32 @@ app = Flask(__name__)
 # Create an instance of the VisionAgent class
 agent = VisionAgent()
 
+def process_request(image_source_keyword):
+
+    if image_source_keyword == "yes":
+        image_source = "visionagent/resources/test01.jpg"
+    else:
+        image_source = None
+    
+    agent = VisionAgent(image_source=image_source)
+    
+    try:
+        frame, outputs, height, width = agent.run_one_frame()
+        detected_objects, _ = agent.draw_boxes(frame, outputs, height, width)
+        return detected_objects, frame
+    except Exception as e:
+        logger.error(f"An error occurred during Vision Agent processing: {e}")
+        return None, None
+
 @app.route('/detect', methods = ['GET'])
 def detect_route():
 
     image_source = request.args.get('image_source', None)
-
-    if image_source == "yes":
-        image_source = "visionagent/resources/test01.jpg"
-
-    agent = VisionAgent(image_source=image_source)
-
-    try:
-        frame, outputs, height, width = agent.run_one_frame()
-        detected_objects, _ = agent.draw_boxes(frame, outputs, height, width)
-
+    detected_objects, frame = process_request(image_source)
+    
+    if detected_objects is not None:
         # Encode image
-        _, buffer = cv2.imencode('.jpg', detected_objects)
+        _, buffer = cv2.imencode('.jpg', frame)
         
         # Create a file object
         buffer_file = io.BytesIO(buffer.tobytes())
@@ -39,29 +49,19 @@ def detect_route():
             mimetype='image/jpeg',
             headers={'Content-Disposition': 'inline; filename=image.jpg'}
         )
-
-    except Exception as e:
-        logger.error(f"An error occurred during Vision Agent detection: {e}")
-        return jsonify({'Error': str(e)}), 500
+    else:
+        return jsonify({'Error': 'Failed to process image'}), 500
 
 @app.route('/update', methods=['GET'])
 def update_route():
 
     image_source = request.args.get('image_source', None)
-
-    if image_source == "yes":
-        image_source = "visionagent/resources/test01.jpg"
-
-    agent = VisionAgent(image_source=image_source)
-
-    try:
-        frame, outputs, height, width = agent.run_one_frame()
-        _, detected_objects = agent.draw_boxes(frame, outputs, height, width)
+    detected_objects, _ = process_request(image_source)
+    
+    if detected_objects is not None:
         return jsonify({'object_history': detected_objects})
-
-    except Exception as e:
-        logger.error(f"An error occurred during Vision Agent update: {e}")
-        return jsonify({'Error': str(e)}), 500
+    else:
+        return jsonify({'Error': 'Failed to process image'}), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9048)
