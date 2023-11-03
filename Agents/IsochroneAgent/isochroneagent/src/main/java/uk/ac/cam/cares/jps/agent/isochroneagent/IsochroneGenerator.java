6f00154a7915aa48b9acc156a4737ad4ef48381d
Shin Zert <szp20@cam.ac.uk>
2023-11-03 13:41:52 +0800

    1684-dev-isochroneagent-multiple-runs: Query for poi that has not been run before

--- Agents/IsochroneAgent/isochroneagent/src/main/java/uk/ac/cam/cares/jps/agent/isochroneagent/IsochroneGenerator.java
+++ Agents/IsochroneAgent/isochroneagent/src/main/java/uk/ac/cam/cares/jps/agent/isochroneagent/IsochroneGenerator.java
@@ -286,7 +286,7 @@ public class IsochroneGenerator {
      * @throws SQLException
      */
     public List<String> getPoiTypes (RemoteRDBStoreClient remoteRDBStoreClient) throws SQLException {
-        String getNearestNode_sql = "SELECT DISTINCT pnn.poi_type FROM poi_nearest_node AS pnn JOIN isochrone_aggregated AS ia ON pnn.poi_type = ia.poi_type WHERE ia.poi_type IS NULL;";
+        String getNearestNode_sql = "SELECT DISTINCT pnn.poi_type FROM poi_nearest_node AS pnn LEFT JOIN isochrone_aggregated AS ia ON pnn.poi_type = ia.poi_type WHERE ia.poi_type IS NULL";
     
         List<String> poiTypes = new ArrayList<>();
     
