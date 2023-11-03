d142cda3c54f436c261d8e2c997f96fb9c4ba90f
Jiawei Lai <jlai@cmclinnovations.com>
2023-11-03 13:44:50 +0000

    dev-Vision： Modified the entry_point so that it takes into HTTP request for source and path

--- Agents/VisionAgent/visionagent/entry_point.py
+++ Agents/VisionAgent/visionagent/entry_point.py
@@ -12,15 +12,15 @@ logger = logging.getLogger('vision-agent')
 # Initialise Flask app
 app = Flask(__name__)
 
-def process_request():
+def process_request(source,path):
     # Use the pre-configured global 'agent' instance
     # Load properties
     properties = read_properties_file("visionagent/resources/visionagent.properties")
 
     # Construct the VisionAgent
     agent = VisionAgent(
-        video_source=0 if properties['input.source'] == 'video' else None,
-        image_source=f"visionagent/resources/{properties['image.file_name']}" if properties['input.source'] == 'image' else None,
+        video_source=0 if source == 'video' else None,
+        image_source=f"visionagent/resources/{path}.jpg" if source == 'image' else None,
         weights_path=f"visionagent/resources/{properties['cv.model.weights']}",
         cfg_path=f"visionagent/resources/{properties['cv.model.config']}",
         names_path=f"visionagent/resources/{properties['cv.class.names']}"
@@ -36,7 +36,9 @@ def process_request():
 
 @app.route('/detect', methods=['GET'])
 def detect_route():
-    detected_objects, frame_with_boxes, _ = process_request()
+    source = request.args.get('source')
+    path = request.args.get('path')
+    detected_objects, frame_with_boxes, _ = process_request(source,path)
     if detected_objects is not None:
         # Encode image with detection boxes
         _, buffer = cv2.imencode('.jpg', frame_with_boxes)
@@ -52,7 +54,10 @@ def detect_route():
 
 @app.route('/update', methods=['GET'])
 def update_route():
-    _, _, detected_objects = process_request()
+    source = request.args.get('source')
+    path = request.args.get('path')
+
+    _, _, detected_objects = process_request(source,path)
 
     if detected_objects is not None:
         return jsonify({'object_history': detected_objects})
