0521d1a6d275f5ae916c866e8f1ca26bfffa7fe0
Dan Tran <nmdt2@cam.ac.uk>
2023-11-08 10:16:21 +0800

    dev-marie-combustion-symposium: Rewrite combine_then_split.py to take in arguments

--- combine_then_split.py
+++ combine_then_split.py
@@ -1,11 +1,12 @@
+import argparse
 import ast
 import json
+import os
 import random
 import time
 from typing import List
 
 import pandas as pd
-
 
 
 def sanitize(texts: List[str]):
@@ -14,26 +15,36 @@ def sanitize(texts: List[str]):
         while ptr < len(text):
             if text.startswith("<entity>", ptr):
                 ptr_end = text.find("</entity>", ptr)
-                entity = text[ptr + len("<entity>"): ptr_end]
-                text = text[:ptr] + entity + text[ptr_end + len("</entity>"):]
+                entity = text[ptr + len("<entity>") : ptr_end]
+                text = text[:ptr] + entity + text[ptr_end + len("</entity>") :]
                 ptr = ptr_end
             elif text[ptr] == "[":
                 ptr_end = text.find("]", ptr)
-                literal = text[ptr + 1: ptr_end]
-                text = text[:ptr] + literal + text[ptr_end + 1:]
+                literal = text[ptr + 1 : ptr_end]
+                text = text[:ptr] + literal + text[ptr_end + 1 :]
                 ptr = ptr_end
             else:
                 ptr += 1
         return text
+
     return [_sanitize(text) for text in texts]
 
+
 def main():
-    df = pd.read_csv("data/examples_2000_paraphrases.csv", index_col="id")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filepath-examples", type=str, required=True)
+    parser.add_argument("--filepath-paraphrases", type=str, required=True)
+    parser.add_argument("--dirpath-out", type=str, required=True)
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.filepath_paraphrases, index_col="id")
     df["paraphrases"] = df["paraphrases"].apply(ast.literal_eval)
-    df["sampling_pool"] = df.apply(lambda row: [row["verbalization"]] + row["paraphrases"], axis=1)
+    df["sampling_pool"] = df.apply(
+        lambda row: [row["verbalization"]] + row["paraphrases"], axis=1
+    )
     df["sampling_pool"] = df["sampling_pool"].apply(sanitize)
 
-    with open("data/examples_2000.json", "r") as f:
+    with open(args.filepath_examples, "r") as f:
         data = json.load(f)
 
     data_out = []
@@ -48,22 +59,29 @@ def main():
         except Exception as e:
             print(datum)
             raise e
-    
+
     ids = df.sample(frac=1).index.to_list()
     test_size = len(ids) // 10
     dev_size = len(ids) // 10
-    
+
     ids = {
         "test": ids[:test_size],
         "dev": ids[test_size : test_size + dev_size],
-        "train": ids[test_size + dev_size:]
+        "train": ids[test_size + dev_size :],
     }
 
+    os.makedirs(args.dirpath_out, exist_ok=True)
+    
     time_label = time.strftime("%Y-%m-%d_%H:%M:%S")
     for split, _ids in ids.items():
         split_data = [x for x in data_out if x["id"] in _ids]
-        with open(f"data/{time_label}_{split}.json", "w") as f:
+
+        filename = f"{time_label}_{split}.json"
+        filepath = os.path.join(args.dirpath_out, filename)
+
+        with open(filepath, "w") as f:
             json.dump(split_data, f, indent=4)
+
 
 if __name__ == "__main__":
     main()
