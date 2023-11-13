bf89810b77656947dc38d90aefaa86e597b03fab
Seán Courtney <scourtney@cmclinnovations.com>
2023-11-13 23:52:52 +0000

    1012-fia-caches-enforcedendpoint: Allow for extra properties in the input json

--- Agents/FeatureInfoAgent/code/src/main/java/com/cmclinnovations/featureinfo/objects/Request.java
+++ Agents/FeatureInfoAgent/code/src/main/java/com/cmclinnovations/featureinfo/objects/Request.java
@@ -3,38 +3,33 @@ package com.cmclinnovations.featureinfo.objects;
 import java.util.Optional;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Request // (@JsonProperty("iri") @JsonAlias("IRI") String iri, @JsonProperty("endpoint")
-                     // @JsonAlias("ENDPOINT") Optional<String> endpoint)
-{
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Request {
     @JsonProperty("iri")
     @JsonAlias("IRI")
     private String iri;
 
     @JsonAlias("ENDPOINT")
     @JsonProperty("endpoint")
-    private Optional<String> endpoint;
+    private String endpoint;
+
+    public Request() {
+    }
 
     public Request(String iri, String endpoint) {
         this.iri = iri;
-        this.endpoint = Optional.ofNullable(endpoint);
+        this.endpoint = endpoint;
     }
 
     public String getIri() {
         return iri;
     }
 
-    public void setIri(String iri) {
-        this.iri = iri;
-    }
-
     public Optional<String> getEndpoint() {
-        return endpoint;
-    }
-
-    public void setEndpoint(Optional<String> endpoint) {
-        this.endpoint = endpoint;
+        return Optional.ofNullable(endpoint);
     }
 
 }
