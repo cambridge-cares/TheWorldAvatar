263ddc0c1ef101f8583f66911285bcbf4607ff56
Dan Tran <nmdt2@cam.ac.uk>
2023-11-16 15:46:11 +0800

    dev-marie-combustion-symposium: Update convert_model.py

--- convert_model.py
+++ convert_model.py
@@ -1,15 +1,20 @@
 import argparse
 
+from transformers import AutoTokenizer
 from optimum.onnxruntime import ORTModelForSeq2SeqLM
 
 
 def convert():
     parser = argparse.ArgumentParser()
-    parser.add_argument("model_dir", type=str)
+    parser.add_argument("input_dir", type=str)
+    parser.add_argument("output_dir", type=str)
     args = parser.parse_args()
 
-    model = ORTModelForSeq2SeqLM.from_pretrained(args.model_dir, export=True)
-    model.save_pretrained(args.model_dir)
+    model = ORTModelForSeq2SeqLM.from_pretrained(args.input_dir, export=True)
+    tokenizer = AutoTokenizer.from_pretrained(args.input_dir)
+
+    model.save_pretrained(args.output_dir)
+    tokenizer.save_pretrained(args.output_dir)
 
 if __name__ == "__main__":
     convert()
