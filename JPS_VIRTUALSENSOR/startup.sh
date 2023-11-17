54516037a151c0cb114b6da4a916dc9c0b87c52b
Kok Foong Lee <kokfoong.lee@cares.cam.ac.uk>
2023-11-17 18:50:17 +0800

    dev-aermod-vis: removed snapshot qualifiers and changed fileserver to ghcr version

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# pull docker images from docker.cmclinnovations.com because stack manager can't pull these
-docker pull docker.cmclinnovations.com/file-server:1.0.0
 # starts up all required components of virtual sensor
 (cd stack-manager && ./stack.sh start ship-stack)
 # copy required files into containers
