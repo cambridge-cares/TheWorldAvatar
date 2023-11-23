6c4fced806a5f95ec1f4ae57628629a6f5f6e952
qhouyee <quekhouyee@gmail.com>
2023-11-23 14:39:43 +0800

    dev-grafana-service-bug-fix-for-invalid-config: Refactor the grafana service and config to meet JSON serialisation requirements and update their workflow

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/GrafanaService.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/services/GrafanaService.java
@@ -1,31 +1,27 @@
 package com.cmclinnovations.stack.services;
 
+import com.cmclinnovations.stack.clients.core.EndpointNames;
 import com.cmclinnovations.stack.clients.grafana.GrafanaEndpointConfig;
 import com.cmclinnovations.stack.services.config.ServiceConfig;
 
 public class GrafanaService extends ContainerService {
-    private final GrafanaEndpointConfig ENDPOINT_CONFIG;
     public static final String TYPE = "grafana";
     private static final String PASSWORD_FILE_KEY = "GRAFANA_PASSWORD_FILE";
     private static final String PASSWORD_SECRET_NAME = "grafana_password";
     private static final String DEFAULT_PORT = "3000";
+    private static final String DEFAULT_USER = "admin";
     private static final String DEFAULT_PASSWORD_FILE = "/run/secrets/" + PASSWORD_SECRET_NAME;
-    private static final String GRAFANA_USER_CONFIG = "GF_SECURITY_ADMIN_USER";
-    private static final String GRAFANA_PASSWORD_CONFIG = "GF_SECURITY_ADMIN_PASSWORD__FILE";
 
     public GrafanaService(String stackName, ServiceConfig config) {
         super(stackName, config);
-        // Set up an endpoint config for Grafana with a default username of admin
-        setEnvironmentVariableIfAbsent(PASSWORD_FILE_KEY, DEFAULT_PASSWORD_FILE);
-        this.ENDPOINT_CONFIG = new GrafanaEndpointConfig(
-                TYPE, getHostName(), DEFAULT_PORT, getEnvironmentVariable(PASSWORD_FILE_KEY));
     }
 
     @Override
     protected void doPreStartUpConfigurationImpl() {
+        setEnvironmentVariableIfAbsent(PASSWORD_FILE_KEY, DEFAULT_PASSWORD_FILE);
         // Before running the container, set the following grafana configuration parameters
-        setEnvironmentVariable(GRAFANA_USER_CONFIG, this.ENDPOINT_CONFIG.getUsername()); // Set default username
-        setEnvironmentVariable(GRAFANA_PASSWORD_CONFIG, DEFAULT_PASSWORD_FILE); // Set default password
+        setEnvironmentVariable("GF_SECURITY_ADMIN_USER", DEFAULT_USER); // Set default username
+        setEnvironmentVariable("GF_SECURITY_ADMIN_PASSWORD__FILE", getEnvironmentVariable(PASSWORD_FILE_KEY)); // Set default password
         // Configure the container's url
         setEnvironmentVariable("GF_SERVER_ROOT_URL", "%(protocol)s://%(domain)s:%(http_port)s/analytics/");
         setEnvironmentVariable("GF_SERVER_SERVE_FROM_SUB_PATH", "false");
@@ -36,8 +32,11 @@ public class GrafanaService extends ContainerService {
     }
 
     @Override
-    public void doPostStartUpConfiguration() {
-        // After the container is running, write and expose the config within the stack for other agents to employ
-        writeEndpointConfig(this.ENDPOINT_CONFIG);
+    public void createEndpoints() {
+        // Write and expose the config within the stack for other agents to employ
+        // Set up an endpoint config for Grafana with a default username of admin
+        GrafanaEndpointConfig endpointConfig = new GrafanaEndpointConfig(
+                EndpointNames.GRAFANA, getHostName(), DEFAULT_PORT, DEFAULT_USER, getEnvironmentVariable(PASSWORD_FILE_KEY));
+        writeEndpointConfig(endpointConfig);
     }
 }
