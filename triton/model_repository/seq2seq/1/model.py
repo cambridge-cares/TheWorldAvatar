924ef22b0f7f0e0b7f07a55e02edf852c4083954
Dan Tran <nmdt2@cam.ac.uk>
2023-11-27 14:13:38 +0800

    dev-marie-combustion-symposium: Update model load

--- triton/model_repository/seq2seq/1/model.py
+++ triton/model_repository/seq2seq/1/model.py
@@ -3,52 +3,16 @@ import os
 import numpy as np
 import triton_python_backend_utils as pb_utils
 from transformers import AutoTokenizer
-from transformers.generation.configuration_utils import GenerationConfig
-from transformers.utils.hub import cached_file
 from optimum.onnxruntime import ORTModelForSeq2SeqLM
-from optimum.utils.save_utils import maybe_load_preprocessors
 
 
 class TritonPythonModel:
-    def _load_model(self, model_path: str):
-        encoder_path = cached_file(model_path, "encoder_model_quantized.onnx")
-        decoder_path = cached_file(model_path, "decoder_model_quantized.onnx")
-        decoder_wp_path = cached_file(model_path, "decoder_with_past_model_quantized.onnx")
-
-        (
-            encoder_session,
-            decoder_session,
-            decoder_wp_session,
-        ) = ORTModelForSeq2SeqLM.load_model(
-            encoder_path=encoder_path,
-            decoder_path=decoder_path,
-            decoder_with_past_path=decoder_wp_path,
-        )
-        config = ORTModelForSeq2SeqLM._load_config(model_path)
-        generation_config = GenerationConfig.from_pretrained(model_path)
-        preprocessors = maybe_load_preprocessors(model_path)
-
-        return ORTModelForSeq2SeqLM(
-            encoder_session=encoder_session,
-            decoder_session=decoder_session,
-            config=config,
-            onnx_paths=[
-                encoder_path,
-                decoder_path,
-                decoder_wp_path,
-            ],
-            decoder_with_past_session=decoder_wp_session,
-            model_save_dir=model_path,
-            preprocessors=preprocessors,
-            generation_config=generation_config,
-        )
-
     def initialize(self, args):
         model_path = os.getenv("SEQ2SEQ_MODEL_PATH", "picas9dan/20231115_6_onnx_8bit")
         print("Loading weights for seq2seq model from: " + model_path, flush=True)
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-        self.model = self._load_model(model_path)
+        self.model = ORTModelForSeq2SeqLM.from_pretrained(model_path)
         self.max_new_tokens = 512
 
     def forward(self, text: str):
