bf9b894abf532209887cafa54dacf62729893e6d
Sebastian Mosbach <sm453@cam.ac.uk>
2023-11-28 16:46:21 +0000

    dev-1505-proximity-sensor-for-lab_DevInstAgent-2: Added bulk instantiation script. (Re-added this commit as it got removed as part of a clean-up operation.)

---
+++ Agents/DevInstAgent/bulk_inst.py
@@ -0,0 +1,20 @@
+import requests
+import os
+import json
+import sys
+
+def get_args(name='default', url='', folder=""):
+    return url, folder
+
+
+if "__name__" == "__main__":
+    url, folder = get_args(*sys.argv)
+    if url == "" or folder == "":
+        raise Exception("Please uinput the URL and path to folder in the parameter")
+
+    for filename in os.listdir(folder):
+        with open(filename) as f:
+            req = requests.post(url, data = json.load(filename))
+
+    print("Command success!")
+
