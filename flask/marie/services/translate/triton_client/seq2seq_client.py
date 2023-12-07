4b3fdfacf17f439490cb9e97081ca36f04020d82
Dan Tran <nmdt2@cam.ac.uk>
2023-12-07 10:49:30 +0800

    dev-marie-combustion-symposium: Update error message to reflect failure to connect to triton server

--- flask/marie/services/translate/triton_client/seq2seq_client.py
+++ flask/marie/services/translate/triton_client/seq2seq_client.py
@@ -18,10 +18,13 @@ class Seq2SeqClient:
         input_tensors = [httpclient.InferInput("INPUT", text.shape, datatype="BYTES")]
         input_tensors[0].set_data_from_numpy(text)
 
-        # Get Predictions
-        pred_response = self.client.infer(
-            model_name="seq2seq", inputs=input_tensors
-        )
+        try:
+            # Get Predictions
+            pred_response = self.client.infer(
+                model_name="seq2seq", inputs=input_tensors
+            )
+        except ConnectionRefusedError:
+            raise ConnectionRefusedError("Unable to connect to triton server at the endpoint: " + self.client._parsed_url)
 
         # Output
         return pred_response.as_numpy("OUTPUT").astype(str)[0]
