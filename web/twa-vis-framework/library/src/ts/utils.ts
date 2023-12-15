052ffbfee5f30d7226d1d10a91bf14bbf37a8158
Ushcode <opeppard@cmcl.io>
2023-12-15 14:30:10 +0000

    dev-upload-timeseries-heat: revert web changes from this branch

--- web/twa-vis-framework/library/src/ts/utils.ts
+++ web/twa-vis-framework/library/src/ts/utils.ts
@@ -214,7 +214,7 @@ function buildDashboardButton() {
         var doc = parser.parseFromString(html, 'text/html');
 
         let container = document.getElementById("controlContainer");
-        let searchButton = document.getElementById("helpandsearchandlogout");
+        let searchButton = document.getElementById("helpandsearch");
         container.insertBefore(doc.body.firstChild, searchButton);
     }
 }
