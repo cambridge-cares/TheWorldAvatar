d9a4bbebf4fa3ca777bd8cd9922bd27a54e25b11
Yi-Kai Tsai <yikai.tsai@cares.cam.ac.uk>
2023-12-26 11:48:36 +0800

    1716-dev-ceavisualisationagent: change CEAVisualisationConstants to enum class

--- Agents/CEAVisualisationAgent/ceavisualisation-agent/src/main/java/uk/ac/cam/cares/jps/agent/ceavisualisation/CEAVisualisationConstants.java
+++ Agents/CEAVisualisationAgent/ceavisualisation-agent/src/main/java/uk/ac/cam/cares/jps/agent/ceavisualisation/CEAVisualisationConstants.java
@@ -4,52 +4,60 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public final class CEAVisualisationConstants {
-    public static final String GRID = "grid";
-    public static final String ELECTRICITY = "electricity";
-    public static final String HEATING = "heating";
-    public static final String COOLING = "cooling";
-    public static final String GFA = "gfa";
-    public static final String ROOF_AREA = "roof_area";
-    public static final String NORTH_AREA = "north_area";
-    public static final String SOUTH_AREA = "south_area";
-    public static final String WEST_AREA = "west_area";
-    public static final String EAST_AREA = "east_area";
-    public static final String PV_ROOF = "pv_roof";
-    public static final String PV_NORTH = "pv_north";
-    public static final String PV_SOUTH = "pv_south";
-    public static final String PV_WEST = "pv_west";
-    public static final String PV_EAST = "pv_east";
-    public static final String PVT_PLATE_Q_ROOF = "pvt_plate_q_roof";
-    public static final String PVT_PLATE_Q_NORTH = "pvt_plate_q_north";
-    public static final String PVT_PLATE_Q_SOUTH = "pvt_plate_q_south";
-    public static final String PVT_PLATE_Q_WEST = "pvt_plate_q_west";
-    public static final String PVT_PLATE_Q_EAST = "pvt_plate_q_east";
-    public static final String PVT_PLATE_E_ROOF = "pvt_plate_e_roof";
-    public static final String PVT_PLATE_E_NORTH = "pvt_plate_e_north";
-    public static final String PVT_PLATE_E_SOUTH = "pvt_plate_e_south";
-    public static final String PVT_PLATE_E_WEST = "pvt_plate_e_west";
-    public static final String PVT_PLATE_E_EAST = "pvt_plate_e_east";
-    public static final String PVT_TUBE_Q_ROOF = "pvt_tube_q_roof";
-    public static final String PVT_TUBE_Q_NORTH = "pvt_tube_q_north";
-    public static final String PVT_TUBE_Q_SOUTH = "pvt_tube_q_south";
-    public static final String PVT_TUBE_Q_WEST = "pvt_tube_q_west";
-    public static final String PVT_TUBE_Q_EAST = "pvt_tube_q_east";
-    public static final String PVT_TUBE_E_ROOF = "pvt_tube_e_roof";
-    public static final String PVT_TUBE_E_NORTH = "pvt_tube_e_north";
-    public static final String PVT_TUBE_E_SOUTH = "pvt_tube_e_south";
-    public static final String PVT_TUBE_E_WEST = "pvt_tube_e_west";
-    public static final String PVT_TUBE_E_EAST = "pvt_tube_e_east";
-    public static final String SC_PLATE_ROOF = "sc_plate_roof";
-    public static final String SC_PLATE_NORTH = "sc_plate_north";
-    public static final String SC_PLATE_SOUTH = "sc_plate_south";
-    public static final String SC_PLATE_WEST = "sc_plate_west";
-    public static final String SC_PLATE_EAST = "sc_plate_east";
-    public static final String SC_TUBE_ROOF = "sc_tube_roof";
-    public static final String SC_TUBE_NORTH = "sc_tube_north";
-    public static final String SC_TUBE_SOUTH = "sc_tube_south";
-    public static final String SC_TUBE_WEST = "sc_tube_west";
-    public static final String SC_TUBE_EAST = "sc_tube_east";
+public enum CEAVisualisationConstants {
+    GRID("grid"),
+    ELECTRICITY("electricity"),
+    HEATING("heating"),
+    COOLING("cooling"),
+    GFA("gfa"),
+    ROOF_AREA("roof_area"),
+    NORTH_AREA("north_area"),
+    SOUTH_AREA("south_area"),
+    WEST_AREA("west_area"),
+    EAST_AREA("east_area"),
+    PV_ROOF("pv_roof"),
+    PV_NORTH("pv_north"),
+    PV_SOUTH("pv_south"),
+    PV_WEST("pv_west"),
+    PV_EAST("pv_east"),
+    PVT_PLATE_Q_ROOF("pvt_plate_q_roof"),
+    PVT_PLATE_Q_NORTH("pvt_plate_q_north"),
+    PVT_PLATE_Q_SOUTH("pvt_plate_q_south"),
+    PVT_PLATE_Q_WEST("pvt_plate_q_west"),
+    PVT_PLATE_Q_EAST("pvt_plate_q_east"),
+    PVT_PLATE_E_ROOF("pvt_plate_e_roof"),
+    PVT_PLATE_E_NORTH("pvt_plate_e_north"),
+    PVT_PLATE_E_SOUTH("pvt_plate_e_south"),
+    PVT_PLATE_E_WEST("pvt_plate_e_west"),
+    PVT_PLATE_E_EAST("pvt_plate_e_east"),
+    PVT_TUBE_Q_ROOF("pvt_tube_q_roof"),
+    PVT_TUBE_Q_NORTH("pvt_tube_q_north"),
+    PVT_TUBE_Q_SOUTH("pvt_tube_q_south"),
+    PVT_TUBE_Q_WEST("pvt_tube_q_west"),
+    PVT_TUBE_Q_EAST("pvt_tube_q_east"),
+    PVT_TUBE_E_ROOF("pvt_tube_e_roof"),
+    PVT_TUBE_E_NORTH("pvt_tube_e_north"),
+    PVT_TUBE_E_SOUTH("pvt_tube_e_south"),
+    PVT_TUBE_E_WEST("pvt_tube_e_west"),
+    PVT_TUBE_E_EAST("pvt_tube_e_east"),
+    SC_PLATE_ROOF("sc_plate_roof"),
+    SC_PLATE_NORTH("sc_plate_north"),
+    SC_PLATE_SOUTH("sc_plate_south"),
+    SC_PLATE_WEST("sc_plate_west"),
+    SC_PLATE_EAST("sc_plate_east"),
+    SC_TUBE_ROOF("sc_tube_roof"),
+    SC_TUBE_NORTH("sc_tube_north"),
+    SC_TUBE_SOUTH("sc_tube_south"),
+    SC_TUBE_WEST("sc_tube_west"),
+    SC_TUBE_EAST("sc_tube_east");
+
+    private final String value;
+
+    CEAVisualisationConstants(String value) {
+        this.value = value;
+    }
     
-    public static final List<String> columns = Arrays.asList("");
+    public String getValue() {
+        return value;
+    }
 }
