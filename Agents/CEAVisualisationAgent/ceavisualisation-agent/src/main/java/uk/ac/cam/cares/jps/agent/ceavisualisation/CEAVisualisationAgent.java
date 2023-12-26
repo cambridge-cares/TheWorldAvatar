48b69c6a5ac89269140ce9fc434971671f523157
Yi-Kai Tsai <yikai.tsai@cares.cam.ac.uk>
2023-12-26 11:52:37 +0800

    1716-dev-ceavisualisationagent: add method to update table with CEA values for visualisation

--- Agents/CEAVisualisationAgent/ceavisualisation-agent/src/main/java/uk/ac/cam/cares/jps/agent/ceavisualisation/CEAVisualisationAgent.java
+++ Agents/CEAVisualisationAgent/ceavisualisation-agent/src/main/java/uk/ac/cam/cares/jps/agent/ceavisualisation/CEAVisualisationAgent.java
@@ -4,6 +4,7 @@ import uk.ac.cam.cares.jps.base.agent.JPSAgent;
 import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 
 import javax.servlet.annotation.WebServlet;
+import java.util.Map;
 
 @WebServlet(
         urlPatterns = {
@@ -17,6 +18,7 @@ public class CEAVisualisationAgent extends JPSAgent {
     public RemoteRDBStoreClient rdbStoreClient;
     public static final String SCHEMA = "ceavis";
     public static final String TABLE = "cea";
+    public static final String IRI = "iri";
 
     public CEAVisualisationAgent() {
         EndpointConfig endpointConfig = new EndpointConfig();
@@ -39,12 +41,40 @@ public class CEAVisualisationAgent extends JPSAgent {
         String createTable = "CREATE TABLE IF NO EXISTS " + SCHEMA + "." + TABLE + "("
                 + "building_iri VARCHAR(4000),\n";
 
-        for (String column : CEAVisualisationConstants.columns) {
-            createTable += column + "DOUBLE PRECISION,";
+        for (CEAVisualisationConstants column : CEAVisualisationConstants.values()) {
+            createTable += column.getValue() + " DOUBLE PRECISION,";
         }
 
         createTable = createTable.substring(0, createTable.length()-1) + ")";
 
         rdbStoreClient.executeUpdate(createTable);
+    }
+
+
+    /**
+     * Update table with CEA values used for visualisation
+     * @param ceaValues map storing CEA values
+     * @param iri building iri associated with ceaValues
+     */
+    public void updateTable(Map<String, Double> ceaValues, String iri) {
+        String insert = "INSERT INTO " + SCHEMA + "." + TABLE + " (" + IRI + ",";
+        String values = "VALUES (" + iri + ",";
+        String conflict = "ON CONFLICT (" + IRI + ")";
+        String update = "DO UPDATE SET";
+        String set = "";
+
+        for (Map.Entry<String, Double> entry : ceaValues.entrySet()) {
+            insert += entry.getKey() + ",";
+            values += entry.getValue() + ",";
+            set += entry.getKey() + "=" + entry.getValue() + ",";
+        }
+
+        insert = insert.substring(0, insert.length()-1) + ")";
+        values = values.substring(0, values.length()-1) + ")";
+        set = set.substring(0, set.length()-1) + ")";
+
+        String sql = insert + "\n" + values + "\n" + conflict + "\n" + update + "\n" + set;
+
+        rdbStoreClient.executeUpdate(sql);
     }
 }
