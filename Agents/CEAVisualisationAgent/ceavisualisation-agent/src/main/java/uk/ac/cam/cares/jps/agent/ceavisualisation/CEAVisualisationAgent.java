28060a98cb6b0f73ec485d6b1086ebc548af67c4
Yi-Kai Tsai <yikai.tsai@cares.cam.ac.uk>
2023-12-26 17:01:12 +0800

    1716-dev-ceavisualisationagent: add method to process CEA values to the values for the visualisation table

--- Agents/CEAVisualisationAgent/ceavisualisation-agent/src/main/java/uk/ac/cam/cares/jps/agent/ceavisualisation/CEAVisualisationAgent.java
+++ Agents/CEAVisualisationAgent/ceavisualisation-agent/src/main/java/uk/ac/cam/cares/jps/agent/ceavisualisation/CEAVisualisationAgent.java
@@ -4,6 +4,7 @@ import uk.ac.cam.cares.jps.base.agent.JPSAgent;
 import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 
 import javax.servlet.annotation.WebServlet;
+import java.util.HashMap;
 import java.util.Map;
 
 @WebServlet(
@@ -18,7 +19,7 @@ public class CEAVisualisationAgent extends JPSAgent {
     public RemoteRDBStoreClient rdbStoreClient;
     public static final String SCHEMA = "ceavis";
     public static final String TABLE = "cea";
-    public static final String IRI = "iri";
+    public static final String IRI = "building_iri";
 
     public CEAVisualisationAgent() {
         EndpointConfig endpointConfig = new EndpointConfig();
@@ -39,14 +40,14 @@ public class CEAVisualisationAgent extends JPSAgent {
 
         // create table
         String createTable = "CREATE TABLE IF NO EXISTS " + SCHEMA + "." + TABLE + "("
-                + "building_iri VARCHAR(4000),\n";
+                + IRI + " VARCHAR(4000),\n";
 
-        for (Column ceaFields : Column.values()) {
-            for (Annual annual : ceaFields.getAnnuals()) {
+        for (Area area : Area.values()) {
+            for (Annual annual : Column.getAnnuals(area)) {
                 createTable += annual.getAnnual() + " DOUBLE PRECISION,";
                 createTable += annual.getAnnualPerArea() + " DOUBLE PRECISION,";
             }
-            createTable += ceaFields.getArea() + " DOUBLE PRECISION,";
+            createTable += area.getValue() + " DOUBLE PRECISION,";
         }
 
         createTable = createTable.substring(0, createTable.length()-1) + ")";
@@ -80,5 +81,27 @@ public class CEAVisualisationAgent extends JPSAgent {
         String sql = insert + "\n" + values + "\n" + conflict + "\n" + update + "\n" + set;
 
         rdbStoreClient.executeUpdate(sql);
+    }
+
+    /**
+     * Calculates the annual per area values, and return a map of all the CEA values, i.e. area values, annual values, and annual per area values
+     * @param areas map of area values
+     * @param annuals map of annual values
+     * @return map of CEA values for updating the table
+     */
+    public Map<String, Double> visValues(Map<String, Double> areas, Map<String, Double> annuals) {
+        Map<String, Double> ceaValues = new HashMap<>();
+
+        for (Map.Entry<String, Double> area : areas.entrySet()) {
+            Double areaValue = area.getValue();
+            ceaValues.put(area.getKey(), areaValue);
+            for (Annual annual : Column.getAnnuals(Area.valueOf(area.getKey()))) {
+                Double annualValue = annuals.get(annual.getAnnual());
+                ceaValues.put(annual.getAnnual(), annualValue);
+                ceaValues.put(annual.getAnnualPerArea(), annualValue / areaValue);
+            }
+        }
+
+        return ceaValues;
     }
 }
