a142b2a16a414ba3928add4e598de63806d454a9
Yi-Kai Tsai <yikai.tsai@cares.cam.ac.uk>
2023-12-26 10:45:37 +0800

    1716-dev-ceavisualisationagent: add method to intialise table in Postgres

--- Agents/CEAVisualisationAgent/ceavisualisation-agent/src/main/java/uk/ac/cam/cares/jps/agent/ceavisualisation/CEAVisualisationAgent.java
+++ Agents/CEAVisualisationAgent/ceavisualisation-agent/src/main/java/uk/ac/cam/cares/jps/agent/ceavisualisation/CEAVisualisationAgent.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.cares.jps.agent.ceavisualisation;
 
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
+import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 
 import javax.servlet.annotation.WebServlet;
 
@@ -10,5 +11,40 @@ import javax.servlet.annotation.WebServlet;
 public class CEAVisualisationAgent extends JPSAgent {
     public static final String URI_RUN = "/run";
 
+    public final String DB_NAME;
+    public final String DB_USER;
+    public final String DB_PASSWORD;
+    public RemoteRDBStoreClient rdbStoreClient;
+    public static final String SCHEMA = "ceavis";
+    public static final String TABLE = "cea";
 
+    public CEAVisualisationAgent() {
+        EndpointConfig endpointConfig = new EndpointConfig();
+        DB_USER = endpointConfig.getDbUser();
+        DB_PASSWORD = endpointConfig.getDbPassword();
+        rdbStoreClient = new RemoteRDBStoreClient(endpointConfig.getDbUrl(DB_NAME), DB_USER, DB_PASSWORD);
+        initialiseTable();
+    }
+
+    /***
+     * Intialise table to be used for visualisation in TWA-VF
+     */
+    public void initialiseTable() {
+        // create schema
+        String createSchema = "CREATE SCHEMA IF NOT EXISTS " + SCHEMA;
+
+        rdbStoreClient.executeUpdate(createSchema);
+
+        // create table
+        String createTable = "CREATE TABLE IF NO EXISTS " + SCHEMA + "." + TABLE + "("
+                + "building_iri VARCHAR(4000),\n";
+
+        for (String column : CEAVisualisationConstants.columns) {
+            createTable += column + "DOUBLE PRECISION,";
+        }
+
+        createTable = createTable.substring(0, createTable.length()-1) + ")";
+
+        rdbStoreClient.executeUpdate(createTable);
+    }
 }
