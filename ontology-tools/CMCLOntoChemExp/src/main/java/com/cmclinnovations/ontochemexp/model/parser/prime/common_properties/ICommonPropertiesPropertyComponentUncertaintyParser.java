ac7841001297bca482e0b9d785d8d0383f4e0560
Feroz Farazi <msff2@cam.ac.uk>
2024-01-16 12:26:27 +0000

    dev-composite-materials-ontology: Renamed the file.

--- ontology-tools/CMCLOntoChemExp/src/main/java/com/cmclinnovations/ontochemexp/model/parser/prime/common_properties/ICommonPropertiesPropertyComponentUncertaintyParser.java
+++
@@ -1,7 +0,0 @@
-package com.cmclinnovations.ontochemexp.model.parser.prime.common_properties;
-
-import org.xml.sax.Attributes;
-
-public interface ICommonPropertiesPropertyComponentUncertaintyParser {
-	public void parse(String qName, Attributes attributes);
-}
