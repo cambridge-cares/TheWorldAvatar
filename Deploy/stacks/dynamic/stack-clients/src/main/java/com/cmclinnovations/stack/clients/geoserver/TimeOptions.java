df90ae963e1a9a025e7ae936859678ee3bb9a46a
Ushcode <opeppard@cmcl.io>
2024-01-17 12:49:33 +0000

    add multidim settings for raster config

---
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/geoserver/TimeOptions.java
@@ -0,0 +1,21 @@
+package com.cmclinnovations.stack.clients.geoserver;
+
+public class TimeOptions {
+
+    private String arrayName;
+    private String format;
+    private String regex;
+
+    public String getArrayName() {
+        return arrayName;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+}
