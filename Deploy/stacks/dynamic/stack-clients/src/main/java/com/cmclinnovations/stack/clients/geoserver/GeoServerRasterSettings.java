51b7a1ed9cf9ac967fd6b324183207cd490d1e71
Ushcode <opeppard@cmcl.io>
2024-01-24 16:34:01 +0000

    vector and raster settings options as well as super interface

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/geoserver/GeoServerRasterSettings.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/geoserver/GeoServerRasterSettings.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import it.geosolutions.geoserver.rest.encoder.GSLayerEncoder;
 import it.geosolutions.geoserver.rest.encoder.coverage.GSImageMosaicEncoder;
 
-public class GeoServerRasterSettings {
+public class GeoServerRasterSettings implements GeoServerDimensionSettings {
 
     private final GSImageMosaicEncoder dataStoreSettings = new GSImageMosaicEncoder();
     private final GSLayerEncoder layerSettings = new GSLayerEncoder();
@@ -20,10 +20,12 @@ public class GeoServerRasterSettings {
 
     private Map<String, UpdatedGSFeatureDimensionInfoEncoder> dimensions;
 
+    @Override
     public Map<String, UpdatedGSFeatureDimensionInfoEncoder> getDimensions() {
         return dimensions;
     }
 
+    @Override
     public void setDimensions(Map<String, UpdatedGSFeatureDimensionInfoEncoder> dimensions) {
         this.dimensions = dimensions;
     }
