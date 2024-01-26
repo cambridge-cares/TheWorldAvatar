3d999ca0da3108cfd7ceb9dbd4c4df0f5a7318d9
Jiawei Lai <jlai@cmclinnovations.com>
2024-01-26 10:38:20 +0000

    dev-vis-platform: Change the map object for the type of map being used

--- web/twa-vis-platform/code/src/index.d.ts
+++ web/twa-vis-platform/code/src/index.d.ts
@@ -4,11 +4,16 @@ export {};
 
 declare global {
     interface Window {
-
-        // Map object from provider library
+        /**
+         * Map object from the map provider library (Mapbox or Cesium).
+         * This global object allows for easy access to the map instance throughout the application.
+         */
         map: Map;
 
-        // "mapbox" or "cesium"
-        type: string;
+        /**
+         * The type of map being used, either "mapbox" or "cesium".
+         * This property helps in making decisions based on the map provider being used.
+         */
+        type: 'mapbox' | 'cesium';
     }
 }
