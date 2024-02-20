fdd2cffaeb0f34a98411299aa60cfc0061415010
Wilson Ang <wilson1010x@gmail.com>
2024-02-20 10:02:47 +0800

    1762-dev-hawker-centre-agent: Modify APIConnector

--- Agents/HawkerCentreAgent/HawkerCentreAPIInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/HawkerCentre/APIConnector.java
+++ Agents/HawkerCentreAgent/HawkerCentreAPIInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/HawkerCentre/APIConnector.java
@@ -18,8 +18,8 @@ import java.util.Properties;
 
 public class APIConnector
 {
-    private String hawkerURL = "https://data.gov.sg/api/action/datastore_search?resource_id=8f6bba57-19fc-4f36-8dcf-c0bda382364d&limit=107";
-    private String stallsURL ="https://data.gov.sg/api/action/datastore_search?resource_id=34f86c3e-a90c-4de9-a69f-afc86b7f31b6&limit=36687";
+    private String hawkerURL;
+    private String stallsURL;
     
 
     private static final String ERRORMSG = "Hawker centre data could not be retrieved";
