a337b3cf9d7e22b465f184c76ea8778b394f5cee
Seán Courtney <scourtney@cmclinnovations.com>
2024-02-22 17:25:54 +0000

    1120-geoserver-custom-dimensions-not-setting-default-values-and-not-able-to-set-start-and-end-values: added custom starts and ends to dimensions

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/geoserver/UpdatedGSFeatureDimensionInfoEncoder.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/geoserver/UpdatedGSFeatureDimensionInfoEncoder.java
@@ -9,24 +9,25 @@ import it.geosolutions.geoserver.rest.encoder.metadata.GSFeatureDimensionInfoEnc
 public class UpdatedGSFeatureDimensionInfoEncoder extends GSFeatureDimensionInfoEncoder {
 
     private static final String NEAREST_MATCH_ENABLED = "nearestMatchEnabled";
-
     private static final String RAW_NEAREST_MATCH_ENABLED = "rawNearestMatchEnabled";
+    private static final String START_VALUE = "startValue";
+    private static final String END_VALUE = "endValue";
 
     public UpdatedGSFeatureDimensionInfoEncoder() {
         super("");
     }
 
     private Map<String, String> defaultValue;
-
     private Boolean nearestMatchEnabled;
-
     private Boolean rawNearestMatchEnabled;
+    private String startValue;
+    private String endValue;
 
     public Map<String, String> getDefaultValue() {
         return defaultValue;
     }
 
-    public void setDefaultValue(Map<String, String> defaultValue) { 
+    public void setDefaultValue(Map<String, String> defaultValue) {
         Element defaultValueNode = new Element("defaultValue");
         defaultValue.entrySet()
                 .forEach(entry -> defaultValueNode.addContent(new Element(entry.getKey()).setText(entry.getValue())));
@@ -39,7 +40,7 @@ public class UpdatedGSFeatureDimensionInfoEncoder extends GSFeatureDimensionInfo
 
     public void setNearestMatchEnabled(Boolean nearestMatchEnabled) {
         this.nearestMatchEnabled = nearestMatchEnabled;
-        set(NEAREST_MATCH_ENABLED, nearestMatchEnabled!=null?nearestMatchEnabled.toString():"false");
+        set(NEAREST_MATCH_ENABLED, nearestMatchEnabled != null ? nearestMatchEnabled.toString() : "false");
 
     }
 
@@ -49,11 +50,28 @@ public class UpdatedGSFeatureDimensionInfoEncoder extends GSFeatureDimensionInfo
 
     public void setRawNearestMatchEnabled(Boolean rawNearestMatchEnabled) {
         this.rawNearestMatchEnabled = rawNearestMatchEnabled;
-        set(RAW_NEAREST_MATCH_ENABLED, rawNearestMatchEnabled!=null?rawNearestMatchEnabled.toString():"false");
+        set(RAW_NEAREST_MATCH_ENABLED, rawNearestMatchEnabled != null ? rawNearestMatchEnabled.toString() : "false");
     }
 
     public void setUnits(String units) {
         setUnit(units);
     }
 
+    public String getStartValue() {
+        return startValue;
+    }
+
+    public void setStartValue(String startValue) {
+        this.startValue = startValue;
+        set(START_VALUE, startValue != null ? startValue : "");
+    }
+
+    public String getEndValue() {
+        return endValue;
+    }
+
+    public void setEndValue(String endValue) {
+        this.endValue = endValue;
+        set(END_VALUE, endValue != null ? endValue : "");
+    }
 }
