1e89bf017c94c6a79d37e07feaeef3c41192d937
Wilson Ang <wilson1010x@gmail.com>
2024-02-22 17:51:53 +0800

    1561-dev-carpark-agent: Modify CarparkGeospatialClient

--- Agents/CarparkAgent/CarparkAPIInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/carpark/CarparkGeospatialClient.java
+++ Agents/CarparkAgent/CarparkAPIInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/carpark/CarparkGeospatialClient.java
@@ -20,50 +20,34 @@ public class CarparkGeospatialClient {
      * Logger for reporting info/errors.
      */
 
-	private static final Logger LOGGER = LogManager.getLogger(CarparkAgent.class);
+        private static final Logger LOGGER = LogManager.getLogger(CarparkAgent.class);
 
-	public void createGeospatialInformation(double lat, double lon, String name, String carparkIRI, String agency, int ID,  
-    String carLotsIRI, String carLotsTimeSeriesIRI, String motorcyclesLotsIRI, String motorcyclesLotsTimeSeriesIRI, 
-    String heavyVehicleLotsIRI, String heavyVehicleTimeSeriesLotsIRI, String buildingIRI, String building_uuid) {
-		String geomUuid = "geometry_" + UUID.randomUUID();
+	public void createGeospatialInformation(double lat, double lon, String carparkName, String carparkIRI) {
+	        String geomUuid = "geometry_" + UUID.randomUUID();
 
-        // create geojson object for PostGIS
-        JSONObject geojson = new JSONObject();
-        JSONObject geometry = new JSONObject();
-        JSONObject properties = new JSONObject();
-        geometry.put("type", "Point");
-        geometry.put("coordinates", new JSONArray().put(lon).put(lat));
-        properties.put("carpark_iri", carparkIRI);
-        properties.put("carpark_uuid", carparkIRI.split("/ontocarpark/")[1]);
-        properties.put("agency", agency);
-        properties.put("id", ID);
-        properties.put("car_lots_iri", carLotsIRI);
-        properties.put("car_lots_time_series_iri", carLotsTimeSeriesIRI);
-        properties.put("motorcycle_lots_iri", motorcyclesLotsIRI);
-        properties.put("motorcycle_lots_time_series_iri", motorcyclesLotsTimeSeriesIRI);
-        properties.put("heavy_vehicle_lots_iri", heavyVehicleLotsIRI);
-        properties.put("heavy_vehicle_lots_time_series_iri", heavyVehicleTimeSeriesLotsIRI);
-        properties.put("geom_uuid", geomUuid);
-        properties.put("type", "carpark");
-        properties.put("building_iri", buildingIRI);
-        properties.put("building_uuid", building_uuid);
-        if (name != null) {
-            properties.put("name", name);
-        } else {
-            properties.put("name", String.format("Carpark at (%f, %f)", lat, lon));
-        }
+                // create geojson object for PostGIS
+                JSONObject geojson = new JSONObject();
+                JSONObject geometry = new JSONObject();
+                JSONObject properties = new JSONObject();
+                geometry.put("type", "Point");
+                geometry.put("coordinates", new JSONArray().put(lon).put(lat));
+                properties.put("carpark_iri", carparkIRI);
+                properties.put("carpark_uuid", carparkIRI.split("/ontocarpark/")[1]);
+                properties.put("carpark_name", carparkName);
+                properties.put("geom_uuid", geomUuid);
+                properties.put("type", "carpark");
 
-        geojson.put("type", "Feature").put("properties", properties).put("geometry", geometry);
+                geojson.put("type", "Feature").put("properties", properties).put("geometry", geometry);
 
-        LOGGER.info("Uploading GeoJSON to PostGIS");
-        GDALClient gdalclient = GDALClient.getInstance();
-        gdalclient.uploadVectorStringToPostGIS(CarparkAgent.DATABASE, CarparkAgent.LAYERNAME, geojson.toString(),
+                LOGGER.info("Uploading GeoJSON to PostGIS");
+                GDALClient gdalclient = GDALClient.getInstance();
+                gdalclient.uploadVectorStringToPostGIS(CarparkAgent.DATABASE, CarparkAgent.LAYERNAME, geojson.toString(),
                 new Ogr2OgrOptions(), true);
 
-        LOGGER.info("Creating layer in Geoserver");
-        GeoServerClient geoserverclient = new GeoServerClient(null, null, null);
-        geoserverclient.createWorkspace(CarparkAgent.GEOSERVER_WORKSPACE);
-        geoserverclient.createPostGISLayer(CarparkAgent.GEOSERVER_WORKSPACE, CarparkAgent.DATABASE, CarparkAgent.LAYERNAME,
+                LOGGER.info("Creating layer in Geoserver");
+                GeoServerClient geoserverclient = new GeoServerClient(null, null, null);
+                geoserverclient.createWorkspace(CarparkAgent.GEOSERVER_WORKSPACE);
+                geoserverclient.createPostGISLayer(CarparkAgent.GEOSERVER_WORKSPACE, CarparkAgent.DATABASE, CarparkAgent.LAYERNAME,
                 new GeoServerVectorSettings());
 	}
 
