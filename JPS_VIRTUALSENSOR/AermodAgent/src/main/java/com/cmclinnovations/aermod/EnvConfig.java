e9cd8b02888caabd3460adb4aa184c0ba7eb084c
Kok Foong Lee <kokfoong.lee@cares.cam.ac.uk>
2024-02-22 13:26:34 +0800

    dev-live-ship-data: make agent URLs in configs more agnostic to stack name

--- JPS_VIRTUALSENSOR/AermodAgent/src/main/java/com/cmclinnovations/aermod/EnvConfig.java
+++ JPS_VIRTUALSENSOR/AermodAgent/src/main/java/com/cmclinnovations/aermod/EnvConfig.java
@@ -1,15 +1,17 @@
 package com.cmclinnovations.aermod;
 
 public class EnvConfig {
-    public static final String PYTHON_SERVICE_URL = System.getenv("PYTHON_SERVICE_URL");
+    private static final String STACK_NAME = System.getenv("STACK_NAME");
+    public static final String PYTHON_SERVICE_URL = System.getenv("PYTHON_SERVICE_URL").replace("${STACK_NAME}",
+            STACK_NAME);
     public static final String AERMAP_EXE = System.getenv("AERMAP_EXE");
     public static final String AERMET_EXE = System.getenv("AERMET_EXE");
     public static final String BPIPPRM_EXE = System.getenv("BPIPPRM_EXE");
     public static final String AERMOD_EXE = System.getenv("AERMOD_EXE");
     public static final String DATABASE = System.getenv("DATABASE");
-    public static final String WEATHER_AGENT = System.getenv("WEATHER_AGENT");
+    public static final String WEATHER_AGENT = System.getenv("WEATHER_AGENT").replace("${STACK_NAME}", STACK_NAME);
     public static final String SIMULATION_DIR = System.getenv("SIMULATION_DIR");
-    public static final String FILE_SERVER = System.getenv("FILE_SERVER_URL");
+    public static final String FILE_SERVER = System.getenv("FILE_SERVER_URL").replace("${STACK_NAME}", STACK_NAME);
     public static final String GEOSERVER_WORKSPACE = System.getenv("GEOSERVER_WORKSPACE");
     public static final String ELEVATION_TABLE = System.getenv("ELEVATION_TABLE");
     public static final String DISPERSION_RASTER_TABLE = System.getenv("DISPERSION_RASTER_TABLE");
--- JPS_VIRTUALSENSOR/EmissionsAgent/src/main/java/com/cmclinnovations/emissions/EnvConfig.java
+++ JPS_VIRTUALSENSOR/EmissionsAgent/src/main/java/com/cmclinnovations/emissions/EnvConfig.java
@@ -1,7 +1,9 @@
 package com.cmclinnovations.emissions;
 
 public class EnvConfig {
-    public static final String PYTHON_SERVICE_URL = System.getenv("PYTHON_SERVICE_URL");
+    private static final String STACK_NAME = System.getenv("STACK_NAME");
+    public static final String PYTHON_SERVICE_URL = System.getenv("PYTHON_SERVICE_URL").replace("${STACK_NAME}",
+            STACK_NAME);
     public static final String DATABASE = System.getenv("DATABASE");
 
     private EnvConfig() {
