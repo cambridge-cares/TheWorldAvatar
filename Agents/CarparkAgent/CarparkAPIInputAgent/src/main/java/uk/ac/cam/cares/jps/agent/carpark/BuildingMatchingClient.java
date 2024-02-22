058e0ff3c5dd79998bfa999345df0d55acf694ec
Wilson Ang <wilson1010x@gmail.com>
2024-02-22 17:52:23 +0800

    1561-dev-carpark-agent: Modify BuildingMatchingClient to utilize postgis route instead

--- Agents/CarparkAgent/CarparkAPIInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/carpark/BuildingMatchingClient.java
+++ Agents/CarparkAgent/CarparkAPIInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/carpark/BuildingMatchingClient.java
@@ -13,25 +13,20 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import uk.ac.cam.cares.jps.agent.carpark.file.ConfigReader;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 
 public class BuildingMatchingClient {
-    private static final String BUILDING_MATCHING_ERROR = "Unable to execute building identification/matching for the following latitude, longitude values: " ;
+    private static final String BUILDING_MATCHING_ERROR = "Unable to execute building identification/matching for the carparks!" ;
     private static final Logger LOGGER = LogManager.getLogger(BuildingMatchingClient.class);
 
-    //latitude and longitude of carpark to match
-    private String carparkLatitude;
-    private String carparkLongitude;
     private String buildingIdentificationAgentEndpoint;
-    private String buildingPrefix;
+    private static final String CARPARK_TABLE = "public." + CarparkAgent.LAYERNAME;
+    private static final String CARPARK_GEOM_COLUMN = "wkb_geometry";
 
-    public BuildingMatchingClient(String latitude, String longitude, String filePath) throws IOException {
-        this.carparkLatitude = latitude;
-        this.carparkLongitude = longitude;
+    public BuildingMatchingClient(String filePath) throws IOException {
         readBuildingMatchingProperties(filePath);
     }
 
@@ -40,20 +35,14 @@ public class BuildingMatchingClient {
     private void readBuildingMatchingProperties(String filePath) throws IOException {
         Queue<String> buildingConfigs = ConfigReader.retrieveBuildingMatchingConfig(filePath);
         this.buildingIdentificationAgentEndpoint =  buildingConfigs.poll();
-        this.buildingPrefix =  buildingConfigs.poll();
     }
 
-    public String matchCarparkToBuilding() {
+    public void matchCarparkToBuilding() {
         JSONObject requestBody = new JSONObject();
-        JSONArray coordinateArray = new JSONArray();
-        JSONArray subArray = new JSONArray();
-        subArray.put(carparkLongitude);
-        subArray.put(carparkLatitude);
-        coordinateArray.put(subArray);
-        requestBody.put("coordinates", coordinateArray);
+        requestBody.put("table", CARPARK_TABLE);
+        requestBody.put("column", CARPARK_GEOM_COLUMN);
         JSONObject buildingMatchingInformation = sendPostRequest(requestBody.toString());
-        String buildingIRI = constructBuildingIRI(buildingMatchingInformation);
-        return buildingIRI;
+        LOGGER.info("The number of matches are " +  buildingMatchingInformation.getInt("number_matched"));
     }
 
     private JSONObject sendPostRequest(String jsonObjectString) {
@@ -66,21 +55,12 @@ public class BuildingMatchingClient {
                 if (status == 200) {
                     return new JSONObject(EntityUtils.toString(response.getEntity()));
                 } else {
-                    JSONObject jsonObject = new JSONObject(jsonObjectString);
-                    LOGGER.fatal(BUILDING_MATCHING_ERROR + jsonObject.getJSONArray("coordinates").toString());
-                    throw new HttpResponseException(status, BUILDING_MATCHING_ERROR + jsonObject.getJSONArray("coordinates").toString());
+                    LOGGER.fatal(BUILDING_MATCHING_ERROR);
+                    throw new HttpResponseException(status, BUILDING_MATCHING_ERROR);
                 }
             }
         } catch (Exception e) {
             throw new JPSRuntimeException(BUILDING_MATCHING_ERROR, e);
         }
-    }
-
-    //return building IRI
-    private String constructBuildingIRI(JSONObject buildingMatchingInformation) {
-        JSONArray building_uuid_array = buildingMatchingInformation.getJSONArray("building_iri");
-        String building_uuid = building_uuid_array.getString(0);
-        String building_iri = buildingPrefix + building_uuid;
-        return building_iri;
     }
 }
