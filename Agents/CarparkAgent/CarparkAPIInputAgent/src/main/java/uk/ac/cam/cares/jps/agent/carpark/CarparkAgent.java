271e18ec1b6a07c48e276044d92859d0190f2215
Wilson Ang <wilson1010x@gmail.com>
2024-02-22 17:55:44 +0800

    1561-dev-carpark-agent: Add create route

--- Agents/CarparkAgent/CarparkAPIInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/carpark/CarparkAgent.java
+++ Agents/CarparkAgent/CarparkAPIInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/carpark/CarparkAgent.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.cares.jps.agent.carpark;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import uk.ac.cam.cares.jps.agent.carpark.file.ConfigReader;
@@ -13,6 +14,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.time.OffsetDateTime;
 import javax.servlet.ServletException;
@@ -27,6 +29,11 @@ import uk.ac.cam.cares.jps.base.util.JSONKeyToIRIMapper;
 @WebServlet(urlPatterns = {"/retrieve", "/status"})
 
 public class CarparkAgent extends JPSAgent {
+    public static String GEOSERVER_WORKSPACE = System.getenv("GEOSERVER_WORKSPACE");
+	public static String DATABASE = System.getenv("DATABASE");
+	public static String LAYERNAME = System.getenv("LAYERNAME");
+    public static final String TIMESERIES_IRI_PREFIX = TimeSeriesSparql.TIMESERIES_NAMESPACE + "carpark";
+
     // Agent starts off in invalid state, and will become valid when initialised without exceptions
     private boolean valid = false;
     private static final Logger LOGGER = LogManager.getLogger(CarparkAgent.class);
@@ -42,7 +49,7 @@ public class CarparkAgent extends JPSAgent {
     private static final String CARPARK_AGENT_PROPERTIES_KEY = "CARPARK_AGENTPROPERTIES";
     private static final String CARPARK_CLIENT_PROPERTIES_KEY = "CARPARK_CLIENTPROPERTIES";
     private static final String CARPARK_API_PROPERTIES_KEY = "CARPARK_APIPROPERTIES";
-    private static final String TIMESERIES_IRI_PREFIX = TimeSeriesSparql.TIMESERIES_NAMESPACE + "carpark";
+    
 
     /**
      * Servlet init.
@@ -97,6 +104,11 @@ public class CarparkAgent extends JPSAgent {
                 LOGGER.info("Executing retrieve route ...");
                 setSchedulerForRetrievedRoute(args, delay, interval, timeunit);
                 break;
+            case "create":
+                args = new String[]{CARPARK_AGENT_PROPERTIES_KEY, CARPARK_CLIENT_PROPERTIES_KEY, CARPARK_API_PROPERTIES_KEY};
+                LOGGER.info("Executing create route ...");
+                createRoute(args);
+                break;
             default:
                 LOGGER.fatal("{}{}", UNDEFINED_ROUTE_ERROR_MSG, route);
                 msg.put(JSON_ERROR_KEY, UNDEFINED_ROUTE_ERROR_MSG + route);
@@ -147,6 +159,120 @@ public class CarparkAgent extends JPSAgent {
             response.put(JSON_ERROR_KEY, "Agent could not be initialised! Please check logs for more information.");
         }
         return response;
+    }
+
+    /**
+     * Handle POST /create route and return result message
+     * @param args contains the environment variables that indicates the location of each config file
+     */
+    private void createRoute(String[] args) {
+        if (args.length != 3) {
+            LOGGER.error(ARGUMENT_MISMATCH_MSG);
+            throw new JPSRuntimeException(ARGUMENT_MISMATCH_MSG);
+        }
+        LOGGER.info("Attempting to retrieve mappings...");
+        List<JSONKeyToIRIMapper> mappings;
+        JSONObject jsonMessage = new JSONObject();
+        try {
+            mappings = ConfigReader.retrieveKeyToIriMappings(System.getenv(args[0]), TIMESERIES_IRI_PREFIX);
+        } catch (IOException e) {
+            LOGGER.fatal("Failed to retrieve mappings: ", e);
+            throw new JPSRuntimeException("Failed to retrieve mappings: ", e);
+        }
+
+        LOGGER.info("Setting up the API connector...");
+        CarparkAPIConnector connector;
+        try {
+            connector = new CarparkAPIConnector(System.getenv(args[2]));
+        } catch (IOException e) {
+            LOGGER.error(CONNECTOR_ERROR_MSG, e);
+            throw new JPSRuntimeException(CONNECTOR_ERROR_MSG, e);
+        }
+
+        LOGGER.info("Retrieving available carpark lot data from the API...");
+        JSONObject carparkReadings;
+        try {
+            carparkReadings = connector.getAvailableLots();
+        } catch (Exception e) {
+            throw new JPSRuntimeException(GET_READINGS_ERROR_MSG, e);
+        }
+
+        
+        LOGGER.info("Retrieving carpark rates from the API...");
+        JSONObject ratesReadings;
+        try {
+            ratesReadings = connector.getCarparkRates();
+        } catch (Exception e) {
+            LOGGER.error(GET_READINGS_ERROR_MSG, e);
+            throw new JPSRuntimeException(GET_READINGS_ERROR_MSG, e);
+        }
+        LOGGER.info("Retrieved carpark rates for {} carparks", ratesReadings.length());
+        jsonMessage.accumulate(JSON_RESULT_KEY, " Retrieved carpark rates for " + ratesReadings.getJSONObject("result").getJSONArray("records").length() + " carparks.");
+
+        
+        LOGGER.info("Retrieved available lot readings for {} carparks", carparkReadings.length());
+        jsonMessage.put(JSON_RESULT_KEY, "Retrieved available lot readings for " + carparkReadings.getJSONArray("value").length() + " carparks.");
+
+        LOGGER.info("Setting up the time series and remote store clients...");
+        TimeSeriesClient<OffsetDateTime> tsclient;
+        RemoteStoreClient kbClient;
+        try {
+            // Retrieve sparql configurations
+            Queue<String> sparqlConfigs = ConfigReader.retrieveSparqlConfig(System.getenv(args[1]));
+            kbClient = new RemoteStoreClient();
+            // First two configuration should be query and update endpoint
+            kbClient.setQueryEndpoint(sparqlConfigs.poll());
+            kbClient.setUpdateEndpoint(sparqlConfigs.poll());
+            // If there are more configuration, then it should be user and password
+            if (!sparqlConfigs.isEmpty()) {
+                kbClient.setUser(sparqlConfigs.poll());
+                kbClient.setPassword(sparqlConfigs.poll());
+            }
+            // Retrieve RDB configs and populate it accordingly in sequence of url, user, password
+            Queue<String> rdbConfigs = ConfigReader.retrieveRDBConfig(System.getenv(args[1]));
+            tsclient = new TimeSeriesClient<>(kbClient, OffsetDateTime.class, rdbConfigs.poll(), rdbConfigs.poll(), rdbConfigs.poll());
+
+        } catch (Exception e) {
+            LOGGER.error(TSCLIENT_ERROR_MSG, e);
+            throw new JPSRuntimeException(TSCLIENT_ERROR_MSG, e);
+        }
+
+        LOGGER.info("Setting up the time series for the instantiation process...");
+        TimeSeriesHandler tsHandler;
+        try {
+            tsHandler = new TimeSeriesHandler(mappings);
+            tsHandler.setTsClient(tsclient);
+        } catch (IOException e) {
+            LOGGER.error(AGENT_ERROR_MSG, e);
+            throw new JPSRuntimeException(AGENT_ERROR_MSG, e);
+        }
+
+        try {
+            tsHandler.initializeTimeSeriesIfNotExist();
+        } catch (JPSRuntimeException e) {
+            LOGGER.error(INITIALIZE_ERROR_MSG, e);
+            throw new JPSRuntimeException(INITIALIZE_ERROR_MSG, e);
+        }
+        
+        LOGGER.info("Setting up the sparql handler...");
+        SparqlHandler sparqlHandler;
+        try {
+            sparqlHandler = new SparqlHandler(mappings, kbClient, System.getenv(args[0]));
+        } catch (Exception e) {
+            LOGGER.error("Could not build the SparqlHandler ", e);
+            throw new JPSRuntimeException("Could not successfully initialise the SparqlHandler Object", e);
+        }
+        sparqlHandler.instantiateIfNotInstantiated(carparkReadings, ratesReadings);
+        BuildingMatchingClient buildingMatchingClient;
+        try {
+            buildingMatchingClient = new BuildingMatchingClient(System.getenv(args[0]));
+        } catch (IOException e) {
+            throw new JPSRuntimeException("Unable to create building matching client!", e);
+        }
+        buildingMatchingClient.matchCarparkToBuilding();
+
+        jsonMessage.accumulate(JSON_RESULT_KEY, " Data has been successfully instantiated!");
+        LOGGER.info(jsonMessage.toString());
     }
 
     /**
@@ -232,28 +358,5 @@ public class CarparkAgent extends JPSAgent {
             LOGGER.info("Updating data with new API Readings");
             tsHandler.updateData(carparkReadings);
         }
-
-        LOGGER.info("Retrieving carpark rates from the API...");
-        JSONObject ratesReadings;
-        try {
-            ratesReadings = connector.getCarparkRates();
-        } catch (Exception e) {
-            LOGGER.error(GET_READINGS_ERROR_MSG, e);
-            throw new JPSRuntimeException(GET_READINGS_ERROR_MSG, e);
-        }
-        LOGGER.info("Retrieved carpark rates for {} carparks", ratesReadings.length());
-        jsonMessage.accumulate(JSON_RESULT_KEY, " Retrieved carpark rates for " + ratesReadings.getJSONObject("result").getJSONArray("records").length() + " carparks.");
-
-        LOGGER.info("Setting up the sparql handler...");
-        SparqlHandler sparqlHandler;
-        try {
-            sparqlHandler = new SparqlHandler(mappings, kbClient, System.getenv(args[0]));
-        } catch (Exception e) {
-            LOGGER.error("Could not build the QueryBuilder ", e);
-            throw new JPSRuntimeException("Could not successfully initialise the QueryBuilder Object", e);
-        }
-        sparqlHandler.instantiateIfNotInstantiated(carparkReadings, ratesReadings);
-        jsonMessage.accumulate(JSON_RESULT_KEY, " Data has been successfully instantiated!");
-        LOGGER.info(jsonMessage.toString());
     }
 }
