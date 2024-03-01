512bddabb92e208a6cac10d1ab6ff2d9c878d3ad
sandradeng20 <xsd20@cam.ac.uk>
2024-03-01 16:14:59 +0800

    1768-dev-trajectoryqueryagent: create database and update obda when init agent

--- Agents/TrajectoryQueryAgent/trajectoryqueryagent/src/main/java/uk/ac/cam/cares/jps/agent/trajectoryqueryagent/TrajectoryQueryAgent.java
+++ Agents/TrajectoryQueryAgent/trajectoryqueryagent/src/main/java/uk/ac/cam/cares/jps/agent/trajectoryqueryagent/TrajectoryQueryAgent.java
@@ -1,7 +1,10 @@
 package uk.ac.cam.cares.jps.agent.trajectoryqueryagent;
 
+import com.cmclinnovations.stack.clients.ontop.OntopClient;
 import org.apache.jena.graph.Node;
+import org.json.JSONArray;
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
+import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
 
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Path;
 import java.time.OffsetDateTime;
 
 import org.apache.jena.graph.NodeFactory;
@@ -24,11 +28,41 @@ import com.cmclinnovations.stack.clients.geoserver.UpdatedGSVirtualTableEncoder;
 public class TrajectoryQueryAgent extends JPSAgent {
     private KGQueryClient kgQueryClient;
     private RemoteStoreClient storeClient;
+    private RemoteRDBStoreClient timelineRdbClient;
+
+    private static final Path obdaFile = Path.of("/inputs/user.obda");
 
     public void init() {
         EndpointConfig endpointConfig = new EndpointConfig();
         storeClient = new RemoteStoreClient(endpointConfig.getKgurl(), endpointConfig.getKgurl());
         kgQueryClient = new KGQueryClient(storeClient);
+
+        initTimelineDatabase(endpointConfig);
+        timelineRdbClient = new RemoteRDBStoreClient(endpointConfig.getDburl("timeline"), endpointConfig.getDbuser(), endpointConfig.getDbpassword());
+        initPhoneTable();
+
+        try {
+            OntopClient ontopClient = OntopClient.getInstance();
+            ontopClient.updateOBDA(obdaFile);
+        } catch (Exception e) {
+            System.out.println("Could not retrieve user.obda file.");
+        }
+    }
+
+    private void initTimelineDatabase(EndpointConfig endpointConfig) {
+        RemoteRDBStoreClient postgresRdbClient = new RemoteRDBStoreClient(endpointConfig.getDburl(), endpointConfig.getDbuser(), endpointConfig.getDbpassword());
+        JSONArray result = postgresRdbClient.executeQuery("SELECT datname FROM pg_database WHERE datname = 'timeline';");
+        if (!result.isEmpty()) {
+            return;
+        }
+
+        postgresRdbClient.executeUpdate("CREATE DATABASE timeline;");
+    }
+
+    private void initPhoneTable() {
+        timelineRdbClient.executeUpdate("CREATE TABLE IF NOT EXISTS 'timeline'.'smartPhone' (" +
+                "phone_id CHAR(36) PRIMARY KEY," +
+                "user_id CHAR(36)");
     }
     
     @Override
