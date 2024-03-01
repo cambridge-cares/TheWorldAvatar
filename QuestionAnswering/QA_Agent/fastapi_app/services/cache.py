fc68da0a62f0b30c506340b191ec2d6b711e4654
Dan Tran <nmdt2@cam.ac.uk>
2024-03-01 13:16:35 +0800

    dev-marie-experimental: Refactor nearest neighbor retriever

---
+++ QuestionAnswering/QA_Agent/fastapi_app/services/cache.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Generic, TypeVar
+
+from redis import Redis
+
+K = TypeVar("K")
+V = TypeVar("V")
+
+class ICache(ABC, Generic[K, V]):
+    @abstractmethod
+    def set(self, key: K, value: V) -> None:
+        pass
+
+    @abstractmethod
+    def get(self, key: K) -> V:
+        pass
+
+    @abstractmethod
+    def exists(self, key: V) -> bool:
+        pass
+
+
+class DictCache(ICache[K, V]):
+    def __init__(self):
+        self.cache: Dict[K, V] = dict()
+
+    def set(self, key: K, value: V):
+        self.cache[key] = value
+
+    def get(self, key: K):
+        return self.cache.get(key)
+
+    def exists(self, key: K):
+        return key in self.cache
+
+
+class RedisCache(ICache[K, V]):
+    def __init__(self):
+        self.client = Redis(host="localhost", port=6379, decode_responses=True)
+
+    def set(self, key: K, value: V):
+        self.client.set(key, value)
+
+    def get(self, key: K) -> V:
+        # TODO: perform type-checks
+        return self.client.get(key)
+
+    def exists(self, key: K):
+        return self.client.exists(key) > 0
--- QuestionAnswering/QA_Agent/fastapi_app/services/nearest_neighbor.py
+++ QuestionAnswering/QA_Agent/fastapi_app/services/nearest_neighbor.py
@@ -1,39 +1,39 @@
-from typing import Dict, List
+from typing import List
 
 import numpy as np
 
-from .openai_client import openai_client
+from services.embed import IEmbedder
+from services.cache import ICache
 
 
 def cos_sim(a: np.ndarray, b: np.ndarray):
-    return a @ b.T / (np.linalg.norm(a) * np.linalg.norm(b))
+    a = a / np.linalg.norm(a, axis=1)[:, None]
+    b = b / np.linalg.norm(b, axis=1)[:, None]
+    return a @ b.T
 
 
 class NNRetriever:
-    def __init__(self, model: str = "text-embedding-3-small"):
-        self.model = model
-        self.doc_cache: Dict[str, np.ndarray] = dict()
-
-    def _compute_embeddings(self, documents: List[str]):
-        return [
-            x.embedding
-            for x in openai_client.embeddings.create(
-                input=documents, model=self.model
-            ).data
-        ]
+    def __init__(
+        self,
+        embedder: IEmbedder,
+        cache: ICache[str, List[float]],
+    ):
+        self.embeder = embedder
+        self.cache = cache
 
     def _retrieve_embeddings(self, documents: List[str]):
-        new_docs = [x for x in documents if x not in self.doc_cache]
-        new_embeds = self._compute_embeddings(new_docs)
+        new_docs = [doc for doc in documents if not self.cache.exists(doc)]
+        new_embeds = self.embeder(new_docs)
         for doc, embed in zip(new_docs, new_embeds):
-            self.doc_cache[doc] = embed
+            self.cache.set(doc, embed)
 
-        return np.array([self.doc_cache[doc] for doc in documents])
+        return [self.cache.get(doc) for doc in documents]
 
     def retrieve(self, documents: List[str], queries: List[str]):
-        doc_embeds = self._retrieve_embeddings(documents)
-        query_embeds = np.array(self._compute_embeddings(queries))
+        doc_embeds = np.array(self._retrieve_embeddings(documents))
+        query_embeds = np.array(self._retrieve_embeddings(queries))
 
         cosine_scores = cos_sim(doc_embeds, query_embeds)
+
         closest_idxes: List[int] = cosine_scores.argmax(axis=0)
         return [documents[idx] for idx in closest_idxes]
