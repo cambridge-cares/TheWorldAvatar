87403a9ed7f3c552a923959378a2740361e1a51a
Shin Zert <szp20@cam.ac.uk>
2024-03-04 15:31:01 +0800

    1768-dev-trajectoryqueryagent: Automated creation of Geoserver layer

--- Agents/TrajectoryQueryAgent/trajectoryqueryagent/src/main/java/uk/ac/cam/cares/jps/agent/trajectoryqueryagent/TrajectoryQueryAgent.java
+++ Agents/TrajectoryQueryAgent/trajectoryqueryagent/src/main/java/uk/ac/cam/cares/jps/agent/trajectoryqueryagent/TrajectoryQueryAgent.java
@@ -3,23 +3,19 @@ package uk.ac.cam.cares.jps.agent.trajectoryqueryagent;
 import org.apache.jena.graph.Node;
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.OffsetDateTime;
-
 import org.apache.jena.graph.NodeFactory;
-
 import com.cmclinnovations.stack.clients.geoserver.GeoServerClient;
 import com.cmclinnovations.stack.clients.geoserver.GeoServerVectorSettings;
 import com.cmclinnovations.stack.clients.geoserver.UpdatedGSVirtualTableEncoder;
 
 
-@WebServlet(urlPatterns = "/update")
+@WebServlet(urlPatterns = "/createlayer")
 
 public class TrajectoryQueryAgent extends JPSAgent {
     private KGQueryClient kgQueryClient;
@@ -31,56 +27,62 @@ public class TrajectoryQueryAgent extends JPSAgent {
         kgQueryClient = new KGQueryClient(storeClient);
     }
     
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse response)
-            throws IOException {
 
+    //Receive userID
+    //Query for pointIRI based on userID
+    //Create geoserver layer
+    //Return pointIRI to application 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse response) throws IOException {
+
+        //Process HTTP Request
         response.setStatus(HttpServletResponse.SC_OK);
         PrintWriter out = response.getWriter();
         out.println("HTTP POST request processed.");
 
-        // EWKT literal for the scope to create
-        String deviceID = req.getParameter("deviceID");
-        OffsetDateTime date = OffsetDateTime.parse(req.getParameter("date"));
+        //Retrieve params
+        String deviceID = req.getParameter("userID");
 
-        Node smartphoneIRI = NodeFactory.createURI(kgQueryClient.getIRIfromJSONarray(kgQueryClient.getSmartPhoneIRI(deviceID)));
+        //Find pointIRI
+        //Node smartphoneIRI = NodeFactory.createURI(kgQueryClient.getIRIfromJSONarray(kgQueryClient.getSmartPhoneIRI(deviceID)));
+        //String pointIRI = kgQueryClient.getIRIfromJSONarray(kgQueryClient.getPointIRIArray(smartphoneIRI));
 
+        //Create Geoserver layer
+        createGeoserver();
 
-        String pointIRI = kgQueryClient.getIRIfromJSONarray(kgQueryClient.getPointIRIArray(smartphoneIRI));
-
-        createGeoserver(pointIRI);
-
-        // Next thing is to pass to pointIRI, date
-        // SQL view to return (GPS trajectory, date)
+        //Return pointIRI to application
 
     }
 
-    private void createGeoserver(String pointIRI){
-
+    private void createGeoserver(){
         //Create geoserver layer
         GeoServerClient geoServerClient = GeoServerClient.getInstance();
         String workspaceName= "twa";
         String schema = "public";
         String dbName="postgres";
         geoServerClient.createWorkspace(workspaceName);
-
         UpdatedGSVirtualTableEncoder virtualTable = new UpdatedGSVirtualTableEncoder();
         GeoServerVectorSettings geoServerVectorSettings = new GeoServerVectorSettings();
-        virtualTable.setSql("SELECT get_geometry_table(c.\"tableName\", c.\"columnName\")\n" +
-                            "FROM (\n" +
-                            "    SELECT \"tableName\", \"columnName\" \n" +
-                            "    FROM \"dbTable\" \n" +
-                            "    WHERE \"dataIRI\" = '"+pointIRI+"'\n" +
-                            ") AS c");
+        virtualTable.setSql("SELECT timeseries.time AS time,\n" +
+                "                    timeseries.value AS geom\n" +
+                "                FROM \"dbTable\",\n" +
+                "                    public.get_geometry_table(\"tableName\", \"columnName\") AS timeseries,\n" +
+                "                    information_schema.columns c\n" +
+                "                WHERE \"dbTable\".\"dataIRI\" = '%pointiri%'\n" +
+                "                    AND c.table_schema = 'public'\n" +
+                "                    AND c.table_name = \"tableName\"\n" +
+                "                    AND c.column_name = \"columnName\"\n" +
+                "                    AND c.udt_name = 'geometry'\n");
         virtualTable.setEscapeSql(true);
         virtualTable.setName("trajectoryVirtualTable");
-        virtualTable.addVirtualTableGeometry("geometry", "Geometry", "4326"); // geom needs to match the sql query
+        virtualTable.addVirtualTableGeometry("value", "Geometry", "4326"); // geom needs to match the sql query
+        virtualTable.addVirtualTableParameter("pointiri","",".*");
         geoServerVectorSettings.setVirtualTable(virtualTable);
         geoServerClient.createPostGISDataStore(workspaceName,"trajectory" , dbName, schema);
         geoServerClient.createPostGISLayer(workspaceName, dbName,"trajectory" ,geoServerVectorSettings);
 
-
-
+        //Sample requests to retrieve Geojson
+        //http://localhost:3838/geoserver/twa/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=twa%3Atrajectory&outputFormat=application%2Fjson&viewparams=pointiri:https://www.theworldavatar.com/kg/sensorloggerapp/point_3bfa75a3-5b2c-45d3-b05a-63879a2e7b94
     }
 
 }
