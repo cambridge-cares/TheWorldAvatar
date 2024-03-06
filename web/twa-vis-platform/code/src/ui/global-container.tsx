d60e97698f2410a7fdee40ca3c4ff44aa5aadb52
Jiawei Lai <jlai@cmclinnovations.com>
2024-03-06 17:17:05 +0000

    dev-vis-platform: Implement custom context menu with dynamic positioning on right-click

--- web/twa-vis-platform/code/src/ui/global-container.tsx
+++ web/twa-vis-platform/code/src/ui/global-container.tsx
@@ -12,65 +12,82 @@ import { SettingsInterface } from 'io/config/ui-settings';
 
 // Incoming properties for global container
 type GlobalContainerProps = {
-    children?: React.ReactNode,
-    settings: SettingsInterface
-}
+  children?: React.ReactNode;
+  settings: SettingsInterface;
+};
 
 // Internal state for global container
 type GlobalContainerState = {
-    popup: boolean
-}
+  popup: boolean;
+  contextMenuVisible: boolean;
+  contextMenuPosition: { x: number; y: number };
+};
 
 /**
  * Component representing a common global page container for all content.
  */
-export default class GlobalContainer extends React.Component<GlobalContainerProps, GlobalContainerState> {
+export default class GlobalContainer extends React.Component<
+  GlobalContainerProps,
+  GlobalContainerState
+> {
+  state = {
+    popup: false,
+    contextMenuVisible: false,
+    contextMenuPosition: { x: 0, y: 0 },
+  };
 
-    // Initial state
-    state = {
-        popup: false
-    }
+  setPopup = () => {
+    this.setState((prevState) => ({
+      popup: !prevState.popup,
+    }));
+  };
 
-    // Update popup state
-    setPopup = () => {
-        this.setState(prevState => ({
-            popup: !prevState.popup
-        }));
-    }
+  // New method to handle right-click and show the context menu
+  handleContextMenu = (e: React.MouseEvent) => {
+    e.preventDefault();
+    this.setState({
+      contextMenuVisible: true,
+      contextMenuPosition: { x: e.pageX, y: e.pageY },
+    });
+  };
 
-    // Return displayable element
-    render() {
-        const { modules, branding } = this.props.settings;
+  // New method to close the context menu when it is no longer needed
+  closeContextMenu = () => {
+    this.setState({
+      contextMenuVisible: false,
+    });
+  };
 
-        return (
-            <Provider store={reduxStore}>
-                <div
-                    id="globalContainer"
-                    onContextMenu={(e) => {
-                        e.preventDefault()
-                    }}> 
+  render() {
+    const { modules, branding } = this.props.settings;
 
-                    {/* Right click menu */}
-                    <ContextMenu />
+    return (
+      <Provider store={reduxStore}>
+        <div
+          id="globalContainer"
+          onContextMenu={this.handleContextMenu}
+          onClick={this.closeContextMenu} // Close context menu when clicking elsewhere
+        >
+          {/* Conditionally render the ContextMenu component based on contextMenuVisible */}
+          {this.state.contextMenuVisible && (
+            <ContextMenu
+              x={this.state.contextMenuPosition.x}
+              y={this.state.contextMenuPosition.y}
+              onClose={this.closeContextMenu} // Assuming ContextMenu can accept an onClose prop
+            />
+          )}
 
-                    {/* Slim toolbar component */}
-                    <Toolbar
-                        showLanding={modules.landing}
-                        toolbarLogo={branding.toolbarLogo.toString()}
-                    />
+          <Toolbar
+            showLanding={modules.landing}
+            toolbarLogo={branding.toolbarLogo.toString()}
+          />
 
-                    {/* Main content container */}
-                    <div id="contentContainer">
-                        {this.props.children}
-                    </div>
-                    
-                    <Konami action={this.setPopup} timeout={6000} resetDelay={1000}/>
-                    {this.state.popup &&
-                        <Trex callback={this.setPopup}/>
-                    }
-                </div>
-            </Provider>
-        );
-    }
+          <div id="contentContainer">{this.props.children}</div>
 
+          <Konami action={this.setPopup} timeout={6000} resetDelay={1000} />
+          {this.state.popup && <Trex callback={this.setPopup} />}
+        </div>
+      </Provider>
+    );
+  }
 }
