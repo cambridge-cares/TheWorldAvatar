6632b8e93e4c471d36992ed233fd9a7f09f15f71
simonrihm <s.rihm@gmx.de>
2024-03-06 18:11:41 +0000

    Added methods to create sparql queries to get/modify/create/delete bookings

--- Agents/EquipmentBookingAgent/equipmentbookingagent/data_model/iris.py
+++ Agents/EquipmentBookingAgent/equipmentbookingagent/data_model/iris.py
@@ -23,42 +23,20 @@ ONTOBIM = 'https://www.theworldavatar.com/kg/ontobim/'
 ###--- Some IRIs from ONTOASSETMANAGEMENT ---###
 OAM_BOOKINGSYSTEM = ONTOASSETMANAGEMENT + 'BookingSystem'
 OAM_HASBOOKINGSYSTEM = ONTOASSETMANAGEMENT + 'hasBookingSystem'
-OAM_BOOKING = ONTOASSETMANAGEMENT + 'BOOKING'
-OAM_HASBOOKING = ONTOASSETMANAGEMENT + 'hasBOOKING'
+OAM_BOOKING = ONTOASSETMANAGEMENT + 'Booking'
+OAM_HASBOOKING = ONTOASSETMANAGEMENT + 'hasBooking'
+OAM_HASBOOKER = ONTOASSETMANAGEMENT + 'hasBooker'
+OAM_HASBOOKINGPERIOD = ONTOASSETMANAGEMENT + 'hasBookingPeriod'
 
-###--- Some IRIs from DBPEDIA ---###
-DBPEDIA_XLSFILE = 'http://dbpedia.org/resource/Microsoft_Excel' # NOTE: <DBPEDIA_XLSFILE> <rdf:type> <DBPEDIA_WIKICATFILENAMEEXTENSIONS>.
-DBPEDIA_CSVFILE = 'http://dbpedia.org/resource/Comma-separated_values' # NOTE: <DBPEDIA_CSVFILE> <rdf:type> <DBPEDIA_WIKICATFILENAMEEXTENSIONS>.
-DBPEDIA_TXTFILE = 'http://dbpedia.org/resource/Text_file' # NOTE: <DBPEDIA_TXTFILE> <rdf:type> <DBPEDIA_WIKICATFILENAMEEXTENSIONS>.
-DBPEDIA_WIKICATFILENAMEEXTENSIONS = 'http://dbpedia.org/class/yago/WikicatFilenameExtensions'
-DBPEDIA_MANUFACTURER = DBPEDIA + 'manufacturer'
-DBPEDIA_ORGANISATION = DBPEDIA + 'Organisation'
+###--- Some IRIs from FIBO ---###
+FIBO_PERSON = FIBO + 'Person'
 
+###--- Some IRIs from TIME ---###
+TIME_INSTANT = W3C_TIME + 'Instant'
+TIME_INTERVAL = W3C_TIME + 'Interval'
+TIME_TIMEPOSITION = W3C_TIME + 'TimePosition'
+TIME_HASBEGINNING = W3C_TIME + 'hasBeginning'
+TIME_HASEND = W3C_TIME + 'hasEnd'
 
-###--- Some IRIs from SAREF ---###
-SAREF_CONSISTSOF = SAREF + 'consistsOf'
-SAREF_DEVICE = SAREF + 'Device'
-SAREF_COMMAND = SAREF + 'Command'
-SAREF_ACCOMPLISHES = SAREF + 'accomplishes'
-SAREF_ACTSUPON = SAREF + 'actsUpon'
-SAREF_HASCOMMAND = SAREF + 'hasCommand'
-SAREF_HASFUNCTION = SAREF + 'hasFunction'
-SAREF_HASSTATE = SAREF + 'hasState'
-SAREF_ISACCOMPLISHEDBY = SAREF + 'isAccomplishedBy'
-SAREF_ISCOMMANDOF = SAREF + 'isCommandOf'
-SAREF_ISOFFEREDBY = SAREF + 'isOfferedBy'
-SAREF_OFFERS = SAREF + 'offers'
-SAREF_REPRESENTS = SAREF + 'represents'
-SAREF_FUNCTION = SAREF + 'Function'
-SAREF_TASK = SAREF + 'Task'
-SAREF_SERVICE = SAREF + 'Service'
-SAREF_STATE = SAREF + 'State'
-
-###--- Some IRIs from OntoSpecies ---###
-ONTOSPECIES_HASUNIQUESPECIES = ONTOSPECIES + 'hasUniqueSpecies'
-ONTOSPECIES_HASMOLECULARWEIGHT = ONTOSPECIES + 'hasMolecularWeight'
-ONTOSPECIES_UNITS = ONTOSPECIES + 'units'
-ONTOSPECIES_VALUE = ONTOSPECIES + 'value'
-ONTOSPECIES_SPECIES = ONTOSPECIES + 'Species'
-ONTOSPECIES_GRAMPERMOLUNIT = 'http://www.theworldavatar.com/kg/ontospecies/Unit_a501ff0e-7cbc-4e3c-ba2e-211da22cac2b'
-ONTOSPECIES_KILOGRAMPERMOLUNIT = 'http://www.theworldavatar.com/kg/ontospecies/Unit_671a1cb7-9962-4f9d-a0f7-ba5857383106'
+###--- Some IRIs from DBPEDIA ---#
+DBPEDIA_UNIX = 'http://dbpedia.org/resource/Unix_time'
--- Agents/EquipmentBookingAgent/equipmentbookingagent/kg_operations/sparql_client.py
+++ Agents/EquipmentBookingAgent/equipmentbookingagent/kg_operations/sparql_client.py
@@ -3,7 +3,8 @@
 #============================================================
 from datetime import datetime
 from typing import Tuple
-from rdflib import Graph
+from rdflib import Graph, Literal, URIRef
+from rdflib.namespace import RDF
 from rdflib import XSD
 from pathlib import Path
 import collections
@@ -15,6 +16,7 @@ import os
 
 from pyderivationagent.kg_operations import PySparqlClient
 from pyderivationagent.data_model import *
+from equipmentbookingagent.data_model.iris import *
 
 from py4jps import agentlogging
 logger = agentlogging.get_logger('dev')
@@ -22,22 +24,105 @@ logger = agentlogging.get_logger('dev')
 
 class EquipmentBookingSparqlClient(PySparqlClient):
 
-    def get_all_chemical_reaction_iri(self):
-        query = f"""SELECT ?chem_rxn WHERE {{ ?chem_rxn a <{ONTOREACTION_CHEMICALREACTION}>. }}"""
+    def get_all_bookable_equipment_iri(self):
+        query = f"""SELECT ?eq WHERE {{ ?eq a <{OAM_HASBOOKINGSYSTEM}> ?bs . }}"""
+        response = self.performQuery(query)
+        return [list(res.values())[0] for res in response]
+    
+    def get_all_users_iri(self):
+        query = f"""SELECT ?user WHERE {{ ?user a <{FIBO_PERSON}>. }}"""
+        response = self.performQuery(query)
+        return [list(res.values())[0] for res in response]
+
+    def get_all_bookings_in_system(self, booking_system_iri: str):
+        booking_system_iri = trimIRI(booking_system_iri)
+        query = f"""SELECT ?booking WHERE {{ <{booking_system_iri}> <{OAM_HASBOOKING}>  ?booking . }}"""
         response = self.performQuery(query)
         return [list(res.values())[0] for res in response]
 
 
-    def create_chemical_amount_for_outlet_to_waste(self, waste_bottle_iri: str, amount_of_chemical_amount: float):
+    def create_booking_within_system(self, booking_system_iri: str, booker_person_iri: str, booking_start: datetime, booking_end: datetime):
         g = Graph()
-        waste_bottle_iri = trimIRI(waste_bottle_iri)
-        chemical_amount_iri = initialiseInstanceIRI(getNameSpace(waste_bottle_iri), ONTOLAB_CHEMICALAMOUNT)
-        g.add((URIRef(chemical_amount_iri), RDF.type, URIRef(ONTOLAB_CHEMICALAMOUNT)))
+        booking_system_iri = trimIRI(booking_system_iri)
+        booker_person_iri = trimIRI(booker_person_iri)
+        namespace_iri = getNameSpace(booking_system_iri)
+
+        booking_iri = initialiseInstanceIRI(namespace_iri, OAM_BOOKING)
+        g.add((URIRef(booking_iri), RDF.type, URIRef(OAM_BOOKING)))
+
+        start_pos_iri = initialiseInstanceIRI(namespace_iri, TIME_TIMEPOSITION)
+        g.add((URIRef(start_pos_iri), RDF.type, URIRef(TIME_TIMEPOSITION)))
+        start_iri = initialiseInstanceIRI(namespace_iri, TIME_INSTANT)
+        g.add((URIRef(start_iri), RDF.type, URIRef(TIME_INSTANT)))
+
+        end_pos_iri = initialiseInstanceIRI(namespace_iri, TIME_TIMEPOSITION)
+        g.add((URIRef(end_pos_iri), RDF.type, URIRef(TIME_TIMEPOSITION)))
+        end_iri = initialiseInstanceIRI(namespace_iri, TIME_INSTANT)
+        g.add((URIRef(end_iri), RDF.type, URIRef(TIME_INSTANT)))
+
+        period_iri = initialiseInstanceIRI(namespace_iri, TIME_INTERVAL)
+        g.add((URIRef(period_iri), RDF.type, URIRef(TIME_INTERVAL)))
+
+        # TODO figure out time zone conversion
         update = f"""{PREFIX_RDF} INSERT DATA {{
-            <{chemical_amount_iri}> rdf:type <{ONTOLAB_CHEMICALAMOUNT}>.
-            <{waste_bottle_iri}> <{ONTOLAB_ISFILLEDWITH}> <{chemical_amount_iri}>.}}"""
+            <{start_pos_iri}> rdf:type <{TIME_TIMEPOSITION}> ;
+                <{TIME_HASTRS}> {DBPEDIA_UNIX} ;
+                <{TIME_NUMERICPOSITION}> {booking_start.timestamp()} .
+            <{start_iri}> rdf:type <{TIME_INSTANT}> ;
+                {TIME_INTIMEPOSITION} <{start_pos_iri}> .
+
+            <{end_pos_iri}> rdf:type <{TIME_TIMEPOSITION}> ;
+                <{TIME_HASTRS}> {DBPEDIA_UNIX} ;
+                <{TIME_NUMERICPOSITION}> {booking_end.timestamp()} .
+            <{end_iri}> rdf:type <{TIME_INSTANT}> ;
+                {TIME_INTIMEPOSITION} <{end_pos_iri}> .
+
+            <{period_iri}> rdf:type <{TIME_INTERVAL}> ;
+                <{TIME_HASBEGINNING}> <{start_iri}> ;
+                <{TIME_HASEND}> <{end_iri}> .
+
+            <{booking_iri}> rdf:type <{OAM_BOOKING}> ;
+                <{OAM_HASBOOKER}> <{booker_person_iri}> ;
+                <{OAM_HASBOOKINGPERIOD}> <{period_iri}> .
+            <{booking_system_iri} <{OAM_HASBOOKING}> <{booking_iri}> .}}"""
         self.performUpdate(update)
-        self.update_waste_bottle_liquid_level_millilitre(
-            {waste_bottle_iri:amount_of_chemical_amount}, for_consumption=False
-        )
         return g
+    
+
+    def remove_booking_from_system (self, booking_iri: str):
+        booking_iri = trimIRI(booking_iri)
+        update = f"""{PREFIX_RDF} DELETE WHERE{{
+            ?bookingsystem <{OAM_HASBOOKING}> <{booking_iri}> .
+            <{booking_iri}> ?bookingpred ?bookingobj ;
+                <{OAM_HASBOOKINGPERIOD}> ?period .
+            ?period ?periodpred ?periodobj ;
+                rdf:type <{TIME_INTERVAL}> .
+            ?periodobj <{TIME_INTIMEPOSITION}> ?timepos ;
+                rdf:type <{TIME_INSTANT}> .
+            ?timepos ?timepospred ?timeposobj .
+        }}"""
+        self.performUpdate(update)
+        logger.info(f"Booking {booking_iri} was removed.")
+
+
+    def modify_booking_in_system (self, booking_iri: str, booker_iri: str, booker_person_iri: str, booking_start: datetime, booking_end: datetime):
+        booking_iri = trimIRI(booking_iri)
+        booker_iri = trimIRI(booker_iri)
+
+        update = f""" DELETE {{
+            <{booking_iri}> <{OAM_HASBOOKER}> ?booker .
+            ?startpos <{TIME_NUMERICPOSITION}> ?starttime .
+            ?endpos <{TIME_NUMERICPOSITION}> ?endtime .
+        }} INSERT {{
+            <{booking_iri}> <{OAM_HASBOOKER}> <{booker_iri}> .
+            ?startpos <{TIME_NUMERICPOSITION}> {booking_start.timestamp()} .
+            ?endpos <{TIME_NUMERICPOSITION}> {booking_end.timestamp()} .
+        }} WHERE {{
+            <{booking_iri}> <{OAM_HASBOOKER}> ?booker ;
+                <{OAM_HASBOOKINGPERIOD}>/<{TIME_HASBEGINNING}>/<{TIME_INTIMEPOSITION}> ?startpos ;
+                <{OAM_HASBOOKINGPERIOD}>/<{TIME_HASEND}>/<{TIME_INTIMEPOSITION}> ?startpos .
+            ?startpos <{TIME_NUMERICPOSITION}> ?starttime .
+            ?endpos <{TIME_NUMERICPOSITION}> ?endtime .
+        }}"""
+        self.performUpdate(update)
+        logger.info(f"Booking {booking_iri} was updated.")
