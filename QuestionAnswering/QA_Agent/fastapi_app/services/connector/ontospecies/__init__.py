ecd4d117a89ce99d2a52e0190bc6af62c31b3089
Dan Tran <nmdt2@cam.ac.uk>
2024-03-07 09:30:18 +0800

    dev-marie-experimental: Refactor ontospecies agent

--- QuestionAnswering/QA_Agent/fastapi_app/services/connector/ontospecies/__init__.py
+++ QuestionAnswering/QA_Agent/fastapi_app/services/connector/ontospecies/__init__.py
@@ -3,7 +3,6 @@ import time
 from typing import List, Type
 
 import unit_parse
-from .find_species import SpeciesFinder
 
 from model.qa import QAData, QAStep
 from services.func_call import get_func_caller
@@ -19,8 +18,8 @@ from .constants import (
     SpeciesPropertyAttrKey,
 )
 from .kg_client import get_ontospecies_kg_client
-from .retrieve_attributes import SpeciesAttributeRetriever
 from .store import get_ontospecies_literal_store
+from .agent import OntoSpeciesAgent
 
 logger = logging.getLogger(__name__)
 
@@ -90,8 +89,7 @@ class OntoSpeciesAgentConnector(IAgentConnector):
     def __init__(self, nn_retriever: NNRetriever):
         self.kg_client = get_ontospecies_kg_client()
         self.literal_store = get_ontospecies_literal_store()
-        self.species_attr_retriever = SpeciesAttributeRetriever()
-        self.species_finder = SpeciesFinder()
+        self.agent = OntoSpeciesAgent()
         self.nn_retriever = nn_retriever
         self.constraint_parser = ConstraintParser(
             schema_parser=SchemaParser(func_call_predictor=get_func_caller())
@@ -107,11 +105,7 @@ class OntoSpeciesAgentConnector(IAgentConnector):
             "find_chemicalSpecies": self.find_chemicalSpecies,
         }
 
-    def lookup_chemicalSpecies_attributes(self, species: str, attributes: List[str]):
-        steps: List[QAStep] = []
-
-        logger.info("Aligning attribute keys: " + str(attributes))
-        timestamp = time.time()
+    def _align_attribute_keys(self, attributes: List[str]):
         attr_keys: List[SpeciesAttrKey] = []
         for key in self.nn_retriever.retrieve(
             documents=self._SPECIES_ATTR_KEYS, queries=attributes
@@ -122,67 +116,50 @@ class OntoSpeciesAgentConnector(IAgentConnector):
                     attr_keys.append(attr_key)
                 except ValueError:
                     pass
+        return attr_keys
+
+    def lookup_chemicalSpecies_attributes(self, species: str, attributes: List[str]):
+        steps: List[QAStep] = []
+
+        logger.info("Aligning attribute keys: " + str(attributes))
+        timestamp = time.time()
+        attr_keys = self._align_attribute_keys(attributes)
         latency = time.time() - timestamp
         logger.info("Aligned attribute keys: " + str(attr_keys))
+        steps.append(
+            QAStep(action="align_attribute_keys", arguments=attributes, latency=latency)
+        )
 
         timestamp = time.time()
-        species_iris = self.species_finder.find(species)
+        bindings = self.agent.lookup_chemicalSpecies_attributes(species, attr_keys)
         latency = time.time() - timestamp
         steps.append(
             QAStep(
                 latency=latency,
-                action="find_species_iri",
-                arguments=[dict(species=species)],
+                action="lookup_attributes",
+                arguments=dict(
+                    species=species, attributes=[key.value for key in attr_keys]
+                ),
             )
         )
 
-        timestamp = time.time()
-        bindings: List[dict] = []
-        for iri in species_iris:
-            datum = dict(IRI=iri)
-            for key in attr_keys:
-                datum[key.value] = self.species_attr_retriever.retrieve(iri, key)
-            bindings.append(datum)
-        latency = time.time() - timestamp
-        limit = 10
-        attr_keys_unpacked = [key.value for key in attr_keys]
-        arguments = [
-            dict(IRI=iri, attributes=attr_keys_unpacked) for iri in species_iris[:limit]
-        ]
-        if len(species_iris) > limit:
-            arguments.append("...")
-        steps.append(
-            QAStep(latency=latency, action="lookup_attributes", arguments=arguments)
+        return steps, QAData(vars=list(bindings[0].keys()), bindings=bindings)
+
+    def _align_chemical_classes(self, chemical_classes: List[str]):
+        # TODO: instead of align each chemical_class to its closest neighbour, retrieve top-k
+        # and match to any one of these top-k labels
+        abox_chemical_classes = self.literal_store.get_chemical_classes()
+        return self.nn_retriever.retrieve(
+            abox_chemical_classes, chemical_classes
         )
-        return steps, QAData(vars=["IRI"] + attr_keys_unpacked, bindings=bindings)
+    
+    def _align_uses(self, uses: List[str]):
+        # TODO: instead of align each use to its closest neighbour, retrieve top-k
+        # and match to any one of these top-k labels
+        abox_uses = self.literal_store.get_uses()
+        return self.nn_retriever.retrieve(abox_uses, uses)
 
-    def find_chemicalSpecies(
-        self,
-        chemical_classes: List[str] = [],
-        uses: List[str] = [],
-        properties: List[str] = [],
-    ):
-        steps = []
-
-        if chemical_classes:
-            # TODO: instead of align each chemical_class to its closest neighbour, retrieve top-k
-            # and match to any one of these top-k labels
-            logger.info("Aligning chemical class labels...")
-            abox_chemical_classes = self.literal_store.get_chemical_classes()
-            chemical_classes = self.nn_retriever.retrieve(
-                abox_chemical_classes, chemical_classes
-            )
-            logger.info("Aligned chemical classes: " + str(chemical_classes))
-
-        if uses:
-            # TODO: instead of align each use to its closest neighbour, retrieve top-k
-            # and match to any one of these top-k labels
-            logger.info("Aligning use labels...")
-            abox_uses = self.literal_store.get_uses()
-            uses = self.nn_retriever.retrieve(abox_uses, uses)
-            logger.info("Aligned uses: " + str(uses))
-
-        logger.info("Parsing property constraints...")
+    def _parse_property_constraints(self, properties: List[str]):
         property_constraints = [self.constraint_parser.parse(x) for x in properties]
 
         property_constraints_unit_converted = []
@@ -226,17 +203,47 @@ class OntoSpeciesAgentConnector(IAgentConnector):
             )
             for key, _ in property_constraints
         ]
-        aligned_property_constraints = [
+        return [
             (aligned_key, constraint)
             for aligned_key, constraint in zip(
                 aligned_keys, property_constraints_unit_converted
             )
         ]
-        logger.info("Parsed property constraints: " + str(aligned_property_constraints))
+
+    def find_chemicalSpecies(
+        self,
+        chemical_classes: List[str] = [],
+        uses: List[str] = [],
+        properties: List[str] = [],
+    ):
+        steps = []
+
+        if chemical_classes:
+            logger.info("Aligning chemical class labels...")
+            timestamp = time.time()
+            chemical_classes = self._align_chemical_classes(chemical_classes)
+            latency = time.time() - timestamp
+            logger.info("Aligned chemical classes: " + str(chemical_classes))
+            steps.append(QAStep(action="align_chemical_classes", arguments=chemical_classes, latency=latency))
+
+        if uses:
+            logger.info("Aligning use labels...")
+            timestamp = time.time()
+            uses = self._align_uses(uses)
+            latency = time.time() - timestamp
+            logger.info("Aligned uses: " + str(uses))
+            steps.append(QAStep(action="align_uses", arguments=uses, latency=latency))
+
+        if properties:
+            logger.info("Parsing property constraints...")
+            property_constraints = self._parse_property_constraints(properties)
+            logger.info("Parsed property constraints: " + str(property_constraints))
+        else:
+            property_constraints = []
 
         timestamp = time.time()
-        species_iris = self._find_chemicalSpecies(
-            chemical_classes, uses, aligned_property_constraints
+        species_iris = self.agent.find_chemicalSpecies(
+            chemical_classes, uses, property_constraints
         )
         bindings = [
             dict(IRI=iri, label=self.literal_store.get_label(iri))
@@ -248,10 +255,10 @@ class OntoSpeciesAgentConnector(IAgentConnector):
             arguments["chemical_classes"] = chemical_classes
         if uses:
             arguments["uses"] = uses
-        if aligned_property_constraints:
+        if property_constraints:
             arguments["properties"] = [
                 dict(property=key.value, constraint=str(constraint))
-                for key, constraint in aligned_property_constraints
+                for key, constraint in property_constraints
             ]
         steps.append(
             QAStep(
