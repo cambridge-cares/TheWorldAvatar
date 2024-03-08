a929fce08b702f032ed50246ab5708d99389f536
sandradeng20 <xsd20@cam.ac.uk>
2024-03-08 16:04:05 +0800

    1773-timeline-app: timeline basic ui

--- Apps/TimelineApp/core/network/src/test/java/uk/ac/cam/cares/jps/network/ExampleUnitTest.java
+++ Apps/TimelineApp/core/network/src/test/java/uk/ac/cam/cares/jps/network/ExampleUnitTest.java
@@ -1,12 +1,6 @@
 package uk.ac.cam.cares.jps.network;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-import uk.ac.cam.cares.jps.network.di.NetworkModule;
-import uk.ac.cam.cares.jps.network.poi.ToiletNetworkSource;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
--- Apps/TimelineApp/feature/timeline/src/main/java/uk/ac/cam/cares/jps/timeline/TimelineFragment.java
+++ Apps/TimelineApp/feature/timeline/src/main/java/uk/ac/cam/cares/jps/timeline/TimelineFragment.java
@@ -1,6 +1,27 @@
 package uk.ac.cam.cares.jps.timeline;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import uk.ac.cam.cares.jps.timeline.databinding.FragmentTimelineBinding;
+
 public class TimelineFragment extends Fragment {
+    private FragmentTimelineBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentTimelineBinding.inflate(inflater);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
