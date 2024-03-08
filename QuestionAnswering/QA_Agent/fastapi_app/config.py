2319c01c37197a96b52cbcbe60d3c874f218a2bd
Dan Tran <nmdt2@cam.ac.uk>
2024-03-08 12:59:44 +0800

    dev-qa-icl: Update default value for settings

--- QuestionAnswering/QA_Agent/fastapi_app/config.py
+++ QuestionAnswering/QA_Agent/fastapi_app/config.py
@@ -6,12 +6,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     embedding_service: Literal["openai", "triton"]
-    function_calling_service: Literal["openai"] = "openai"
+    function_calling_service: Literal["openai"]
 
 
 @cache
 def get_settings():
     return Settings(
         embedding_service=os.getenv("EMBEDDING_SERVICE"),
-        function_calling_service=os.getenv("FUNCTION_CALLING_SERVICE"),
+        function_calling_service=os.getenv("FUNCTION_CALLING_SERVICE", "openai"),
     )
