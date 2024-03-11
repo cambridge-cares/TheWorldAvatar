8410eadf4721aae5a19dd35678c6075ffc7d1cc3
Dan Tran <nmdt2@cam.ac.uk>
2024-03-11 16:02:49 +0800

    dev-qa-icl: Update query templates

--- QuestionAnswering/QA_Agent/fastapi_app/services/connector/ontospecies/agent/__init__.py
+++ QuestionAnswering/QA_Agent/fastapi_app/services/connector/ontospecies/agent/__init__.py
@@ -55,29 +55,25 @@ SELECT DISTINCT ?ChemicalClass WHERE {{
         elif isinstance(attr_key, SpeciesIdentifierAttrKey):
             template = """PREFIX os: <http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#>
 
-SELECT DISTINCT ?{key} WHERE {{{{
-    OPTIONAL {{{{
-        <{{IRI}}> os:has{key}/os:value ?{key} .
-    }}}}
-}}}}""".format(
-                key=attr_key.value
-            )
+SELECT DISTINCT ?{key} WHERE {{
+    OPTIONAL {{
+        <{IRI}> os:has{key}/os:value ?{key} .
+    }}
+}}"""
         else:
             template = """PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX os: <http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#>
 
-SELECT DISTINCT ?Value ?Unit ?ReferenceStateValue ?ReferenceStateUnit WHERE {{{{
-    OPTIONAL {{{{
-        <{{IRI}}> os:has{key} ?{key} .
+SELECT DISTINCT ?Value ?Unit ?ReferenceStateValue ?ReferenceStateUnit WHERE {{
+    OPTIONAL {{
+        <{IRI}> os:has{key} ?{key} .
         ?{key} os:value ?Value ; os:unit/rdfs:label ?Unit .
-        OPTIONAL {{{{
+        OPTIONAL {{
             ?{key} os:hasReferenceState [ os:value ?ReferenceStateValue ; os:unit/rdfs:label ?ReferenceStateUnit ] .
-        }}}}
-    }}}}
-}}}}""".format(
-                key=attr_key.value
-            )
-        query = template.format(IRI=species_iri)
+        }}
+    }}
+}}"""
+        query = template.format(key=attr_key.value, IRI=species_iri)
         return [
             {k: v["value"] for k, v in x.items()}
             for x in self.kg_client.query(query)["results"]["bindings"]
