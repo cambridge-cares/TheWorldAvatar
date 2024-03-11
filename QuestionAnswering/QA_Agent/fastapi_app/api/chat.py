a1afd0d3fdc0640bfbc0719949a4de01c78a9571
Dan Tran <nmdt2@cam.ac.uk>
2024-03-11 10:33:43 +0800

    dev-qa-icl: Add chat service

---
+++ QuestionAnswering/QA_Agent/fastapi_app/api/chat.py
@@ -0,0 +1,55 @@
+import json
+import logging
+import time
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+
+from services.chatbot import ChatbotClient
+
+
+class ChatRequest(BaseModel):
+    question: str
+    data: str
+
+
+logger = logging.getLogger(__name__)
+
+router = APIRouter()
+
+
+def get_chatbot_client():
+    return ChatbotClient()
+
+
+@router.post("")
+async def chat(
+    req: ChatRequest,
+    chatbot_client: Annotated[ChatbotClient, Depends(get_chatbot_client)],
+):
+    logger.info("Request received to chat endpoint with the following request body")
+    logger.info(req)
+
+    def generate():
+        start = time.time()
+        for chunk in chatbot_client.request_stream(req.question, req.data):
+            content = chunk.choices[0].delta.content
+            if content is not None:
+                yield "data: {data}\n\n".format(
+                    data=json.dumps(
+                        {"content": content, "latency": time.time() - start}
+                    )
+                )
+
+    return StreamingResponse(
+        generate(),
+        media_type="text/event-stream",
+        headers={
+            "Content-Type": "text/event-stream",
+            "Connection": "keep-alive",
+            "Cache-Control": "no-cache",
+            "X-Accel-Buffering": "no",  # needed to enable SSE over HTTPS
+        },
+    )
