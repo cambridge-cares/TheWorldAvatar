07d8e9cbcece49f93ae0d500cca1f1ec7b64433d
Dan Tran <nmdt2@cam.ac.uk>
2024-03-12 13:32:54 +0800

    dev-zaha-experimental: Implement function to query aggregate attribute results for sg land plots

--- QuestionAnswering/QA_Agent/fastapi_app/tests/integration/services/singapore/test_agent.py
+++ QuestionAnswering/QA_Agent/fastapi_app/tests/integration/services/singapore/test_agent.py
@@ -1,5 +1,8 @@
 import os
 
+import pytest
+from model.aggregate import AggregateOperator
+
 from model.constraint import (
     AtomicNumericalConstraint,
     CompoundNumericalConstraint,
@@ -11,14 +14,15 @@ from services.connector.singapore.constants import LandUseType, PlotAttrKey
 from services.kg_client import KgClient
 from services.connector.singapore.agent import PlotArgs, SingporeLandLotAgent
 
+@pytest.fixture
+def agent():
+    ontop_client = KgClient(os.getenv("KG_ENDPOINT_SINGAPORE_ONTOP"))
+    bg_endpoint = os.getenv("KG_ENDPOINT_SINGAPORE")
+    yield SingporeLandLotAgent(ontop_client=ontop_client, bg_endpoint=bg_endpoint)
 
 class TestSingporeLandLotAgent:
-    def test_findPlotIri(self):
+    def test_findPlotIri(self, agent: SingporeLandLotAgent):
         # Arrange
-        ontop_client = KgClient(os.getenv("KG_ENDPOINT_SINGAPORE_ONTOP"))
-        bg_endpoint = os.getenv("KG_ENDPOINT_SINGAPORE")
-        agent = SingporeLandLotAgent(ontop_client=ontop_client, bg_endpoint=bg_endpoint)
-
         plot_args = PlotArgs(
             land_use_type=LandUseType.BUSINESS1,
             gross_plot_ratio=CompoundNumericalConstraint(
@@ -54,12 +58,8 @@ class TestSingporeLandLotAgent:
         # Assert
         assert "https://www.theworldavatar.com/kg/landplot/94739" in iris
 
-    def test_findPlotIri_greatestPlotArea(self):
+    def test_findPlotIri_greatestPlotArea(self, agent: SingporeLandLotAgent):
         # Arrange
-        ontop_client = KgClient(os.getenv("KG_ENDPOINT_SINGAPORE_ONTOP"))
-        bg_endpoint = os.getenv("KG_ENDPOINT_SINGAPORE")
-        agent = SingporeLandLotAgent(ontop_client=ontop_client, bg_endpoint=bg_endpoint)
-
         plot_args = PlotArgs(
             land_use_type=LandUseType.BUSINESS1,
             plot_area=ExtremeValueConstraint.MAX,
@@ -73,12 +73,8 @@ class TestSingporeLandLotAgent:
         assert len(iris) == 2
         print(iris)
 
-    def test_lookupPlotAttributes(self):
+    def test_lookupPlotAttributes(self, agent: SingporeLandLotAgent):
         # Arrange
-        ontop_client = KgClient(os.getenv("KG_ENDPOINT_SINGAPORE_ONTOP"))
-        bg_endpoint = os.getenv("KG_ENDPOINT_SINGAPORE")
-        agent = SingporeLandLotAgent(ontop_client=ontop_client, bg_endpoint=bg_endpoint)
-
         plot_args = PlotArgs(
             land_use_type=LandUseType.BUSINESS1,
             gross_floor_area=CompoundNumericalConstraint(
@@ -96,3 +92,17 @@ class TestSingporeLandLotAgent:
         # Assert
         assert data.vars == ["IRI", "PlotArea"]
         assert all("IRI" in binding and "PlotArea" in binding for binding in data.bindings)
+
+    def test_computeAggregatePlotAttributes(self, agent: SingporeLandLotAgent):
+        # Arrange
+        plot_args = PlotArgs(
+            land_use_type=LandUseType.BUSINESS1
+        )
+        attr_aggs = [(PlotAttrKey.PLOT_AREA, AggregateOperator.AVG), (PlotAttrKey.GROSS_PLOT_RATIO, AggregateOperator.MIN)]
+        
+        # Act
+        data = agent.compute_aggregate_plot_attributes(plot_args=plot_args, attr_aggs=attr_aggs)
+
+        # Assert
+        assert data.vars == ["PlotAreaNumericalValueAVG", "GrossPlotRatioNumericalValueMIN"]
+        assert all("PlotAreaNumericalValueAVG" in binding and "GrossPlotRatioNumericalValueMIN" in binding for binding in data.bindings)
