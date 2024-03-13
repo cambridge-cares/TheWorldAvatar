117f0261ef37c9f0e6d2a3196b57e3606e3e0b51
Dan Tran <nmdt2@cam.ac.uk>
2024-03-13 10:32:44 +0800

    dev-zaha-experimental: Update UI for zaha

--- QuestionAnswering/QA_Agent/fastapi_app/api/html.py
+++ QuestionAnswering/QA_Agent/fastapi_app/api/html.py
@@ -1,31 +1,54 @@
+from functools import cache
 from importlib.resources import files
 import json
+import os
+from typing import Annotated, Dict
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 
 templates = Jinja2Templates(directory="templates")
 
-METADATA = json.loads(
-    files("resources.chemistry").joinpath("metadata.json").read_text()
-)
-SAMPLE_QUESTIONS = json.loads(
-    files("resources.chemistry").joinpath("sample_questions.json").read_text()
-)
+
+@cache
+def get_superdomain():
+    return os.getenv("QA_SUPERDOMAIN", "chemistry")
+
+
+@cache
+def get_metadata(superdomain: Annotated[str, Depends(get_superdomain)]):
+    return json.loads(
+        files("resources." + superdomain).joinpath("metadata.json").read_text()
+    )
+
+
+@cache
+def get_sample_questions(superdomain: Annotated[str, Depends(get_superdomain)]):
+    return json.loads(
+        files("resources." + superdomain).joinpath("sample_questions.json").read_text()
+    )
+
 
 router = APIRouter()
 
 
 @router.get("/", response_class=HTMLResponse)
-async def home(request: Request):
+async def home(
+    request: Request,
+    superdomain: Annotated[str, Depends(get_superdomain)],
+    metadata: Annotated[Dict[str, str], Depends(get_metadata)],
+    sample_questions: Annotated[list, Depends(get_sample_questions)],
+):
+
     return templates.TemplateResponse(
         "qa.html",
         dict(
             request=request,
-            title=METADATA["title"],
-            subtitle_paras=METADATA["subtitle"].split("\n"),
-            sample_questions=SAMPLE_QUESTIONS,
+            superdomain=superdomain,
+            title=metadata["title"],
+            subtitle_paras=metadata["subtitle"].split("\n"),
+            sample_questions=sample_questions,
         ),
     )
