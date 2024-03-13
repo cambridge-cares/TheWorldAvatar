42e869590ddd1b606e57ccf152d1e2c5af38e2f2
Jiaru Bai <jb2197@cam.ac.uk>
2024-03-13 12:23:35 +0000

    dev-pydantic-rdflib: cleaned up a bit.

--- JPS_BASE_LIB/python_wrapper/py4jps/data_model/base_ontology.py
+++ JPS_BASE_LIB/python_wrapper/py4jps/data_model/base_ontology.py
@@ -65,22 +65,22 @@ def reveal_object_property_range(t: Set[Union[T, str]]) -> T:
 
 
 class BaseProperty(BaseModel, validate_assignment=True):
-    # validate_assignment=True is to make sure the validation is triggered when range is updated
+    # NOTE validate_assignment=True is to make sure the validation is triggered when range is updated
+
     base_prefix: str = Field(default=TWA_BASE_PREFIX, frozen=True)
     namespace: str = Field(default=None, frozen=True)
     predicate_iri: str = Field(default=None)
     # setting default_factory to set is safe here, i.e. it won't be shared between instances
     # see https://docs.pydantic.dev/latest/concepts/models/#fields-with-non-hashable-default-values
     range: Set = Field(default_factory=set)
-    # _range_cache: Set = PrivateAttr(default_factory=set)
-    # TODO vanilla set operations don't trigger the validation as of pydantic 2.6.1
+
+    # TODO [future] vanilla set operations don't trigger the validation as of pydantic 2.6.1
     # it also seems this will not be supported in the near future
     # see https://github.com/pydantic/pydantic/issues/496
     # for a workaround, see https://github.com/pydantic/pydantic/issues/8575
     # and https://gist.github.com/geospackle/8f317fc19469b1e216edee3cc0f1c898
 
     def __init__(self, **data) -> None:
-        # TODO validate range is either str or specific type
         # below code is to make sure range is always a set even if it's a single value
         if 'range' in data:
             if not isinstance(data['range'], set):
@@ -118,43 +118,36 @@ class BaseProperty(BaseModel, validate_assignment=True):
 
 
 class BaseOntology(BaseModel, validate_assignment=True):
-    # validate_assignment=True is to make sure the validation is triggered when range is updated
-    # TODO think about how to make it easier to instantiate an instance by hand, especially the object properties (or should it be fully automated?)
-    # two directions:
-    # 1. firstly, from instance to triples
-    #    - [done] store all object properties as pydantic objects
-    #    - [done] store all data properties as pydantic objects
-    #    - [done] updating existing instance in kg, i.e., consider cache
-    # 2. from kg triples to instance
-    #    - [done] pull single instance (node) from kg given iri
-    #    - [done] arbitary recursive depth when pull triples from kg?
-    #    - [done] pull all instances of a class from kg
-    # TODO when pulling from kg before push, just update the cache
-    """The initialisation and validator sequence:
-        (I) start to run BaseOntology.__init__(__pydantic_self__, **data) with **data as the raw input arguments;
-        (II) run until super().__init__(**data), note data is updated within BaseOntology before sending to super().init(**data);
-        (III) now within BaseModel __init__:
-            (i) run root_validator (for those pre=True), in order of how the root_validators are listed in codes;
-            (ii) in order of how the fields are listed in codes:
-                (1) run validator (for those pre=True) in order of how the validators (for the same field) are listed in codes;
-                (2) run validator (for those pre=False) in order of how the validators (for the same field) are listed in codes;
-            (iii) (if we are instantiating a child class of BaseOntology) load default values in the child class (if they are provided)
-                  and run root_validator (for those pre=False) in order of how the root_validators are listed in codes,
-                  e.g. clz='clz provided in the child class' will be added to 'values' of the input argument of root_validator;
-        (IV) end BaseModel __init__;
-        (V) end BaseOntology __init__
-
+    """
     Example:
     class MyClass(BaseOntology):
         myObjectProperty: MyObjectProperty
         myDataProperty: MyDataProperty
     """
+
+    # TODO [future] think about how to make it easier to instantiate an instance by hand, especially the object properties (or should it be fully automated?)
+
+    # NOTE validate_assignment=True is to make sure the validation is triggered when range is updated
+
+    # The initialisation and validator sequence:
+    # (I) start to run BaseOntology.__init__(__pydantic_self__, **data) with **data as the raw input arguments;
+    # (II) run until super().__init__(**data), note data is updated within BaseOntology before sending to super().init(**data);
+    # (III) now within BaseModel __init__:
+    #     (i) run root_validator (for those pre=True), in order of how the root_validators are listed in codes;
+    #     (ii) in order of how the fields are listed in codes:
+    #         (1) run validator (for those pre=True) in order of how the validators (for the same field) are listed in codes;
+    #         (2) run validator (for those pre=False) in order of how the validators (for the same field) are listed in codes;
+    #     (iii) (if we are instantiating a child class of BaseOntology) load default values in the child class (if they are provided)
+    #             and run root_validator (for those pre=False) in order of how the root_validators are listed in codes,
+    #             e.g. clz='clz provided in the child class' will be added to 'values' of the input argument of root_validator;
+    # (IV) end BaseModel __init__;
+    # (V) end BaseOntology __init__
+
     base_prefix: str = Field(default=TWA_BASE_PREFIX, frozen=True)
     namespace: str = Field(default=None, frozen=True)
     rdfs_comment: str = Field(default=None)
     rdf_type: str = Field(default=None)
     instance_iri: str = Field(default=None)
-    # is_pulled_from_kg: bool = Field(default=False, frozen=True) # TODO delete?
     _timestamp_of_latest_cache: float = PrivateAttr(default_factory=time.time)
     # format of the cache for all properties: {property_name: property_object}
     _latest_cache: Dict[str, Any] = PrivateAttr(default_factory=dict)
@@ -173,21 +166,6 @@ class BaseOntology(BaseModel, validate_assignment=True):
     @classmethod
     def get_rdf_type(cls) -> str:
         return construct_rdf_type(cls.model_fields['base_prefix'].default, cls.model_fields['namespace'].default, cls.__name__)
-
-    # def push_to_kg(self, sparql_client: PySparqlClient, recursive_depth: int = 0):
-    #     """This method is for pushing new instances to the KG, or updating existing instance."""
-    #     g = Graph()
-    #     self.create_triples_for_kg(g)
-    #     sparql_client.upload_graph(g)
-
-    # def update_cache_from_kg(self, sparql_client: PySparqlClient, recursive_depth: int = 0):
-    #     """This method is for pulling instances from the KG."""
-    #     # self._latest_cache =
-    #     cached_object = self.__class__.pull_from_kg(self.instance_iri, sparql_client, recursive_depth)[0]
-    #     self._latest_cache = copy.deepcopy(cached_object._latest_cache)
-    #     if bool(self._latest_cache):
-    #         self._exist_in_kg = True
-    #         # self.is_pulled_from_kg = True # TODO the design should be updated here
 
     @classmethod
     def pull_from_kg(cls, iris: List[str], sparql_client: PySparqlClient, recursive_depth: int = 0) -> List[BaseOntology]:
@@ -209,20 +187,6 @@ class BaseOntology(BaseModel, validate_assignment=True):
             inst = get_ontology_object_from_lookup(iri)
             # handle object properties (where the recursion happens)
             # TODO need to consider what to do when two instances pointing to each other
-            # object_dict = {}
-            # for op_iri, op_dct in ops.items():
-            #     o_list_given_op = set(inst.get_object_property_range_iris(op_dct['field'])).union(set(props.get(op_iri, [])))
-            #     if flag_pull:
-            #         obj_list = reveal_object_property_range(
-            #             op_dct['type'].model_fields['range'].annotation
-            #         ).pull_from_kg(list(o_list_given_op), sparql_client, recursive_depth)
-            #         for o in obj_list:
-            #             object__dict[o.instance_iri] = o
-            #     else:
-            #         obj_list = list(o_list_given_op)
-            #         for o in obj_list:
-            #             object__dict[o] = o
-
             object_properties_dict = {
                 op_dct['field']: op_dct['type'](
                     range=set() if op_iri not in props else reveal_object_property_range(
@@ -251,21 +215,18 @@ class BaseOntology(BaseModel, validate_assignment=True):
             else:
                 inst = cls(
                     instance_iri=iri,
-                    # is_pulled_from_kg=True, # TODO remove this?
                     **object_properties_dict,
                     **data_properties_dict,
                 )
                 inst._latest_cache = {f: create_cache(getattr(inst, f)) for f in inst.model_fields}
                 inst._timestamp_of_latest_cache = time.time()
 
-            inst._exist_in_kg = True # TODO remove this?
+            inst._exist_in_kg = True
             # update cache here
             instance_lst.append(inst)
             # set new instance to the global look up table, so that we can avoid creating the same instance multiple times
             # also, existing objects will be skipped as the modification should already be done to the objects themselves
             set_new_ontology_object(iri, inst)
-            # TODO here need to add the cache for the instance - need to also provide another member function to pull from KG
-            # TODO or this method can be changed to a member function so that BaseOntology().pull_from_kg(sparql_client)?
         # TODO add check for rdf_type
         return instance_lst
 
@@ -302,52 +263,13 @@ class BaseOntology(BaseModel, validate_assignment=True):
         raise NotImplementedError
 
     def push_to_kg(self, sparql_client: PySparqlClient, recursive_depth: int = 0):
-        # TODO figure out recursive update
         # type of changes: remove old triples, add new triples
         g_to_remove = Graph()
         g_to_add = Graph()
         g_to_remove, g_to_add = self.collect_diff_to_graph(g_to_remove, g_to_add, recursive_depth)
+        # TODO [future] what happens when KG changed during processing in the python side? race conditions...
         sparql_client.delete_and_insert_graphs(g_to_remove, g_to_add)
         return g_to_remove, g_to_add
-        # TODO need to update cache at the end of the method?
-        # # TODO remove the below lines
-        # for f, field_info in self.model_fields.items():
-        #     # (1) data property
-        #     if issubclass(field_info.annotation, DataProperty):
-        #         dp = getattr(self, f)
-        #         # (1.1) remove existing data property
-        #         g_to_remove = dp.collect_range_diff_to_graph(self.instance_iri, g_to_remove, to_remove_range=True)
-        #         # (1.2) add new data property
-        #         g_to_add = dp.collect_range_diff_to_graph(self.instance_iri, g_to_add, to_add_range=True)
-        #         # clear the changes so that it does not get carried over to the next push
-        #         dp.clear_changes()
-        #     # (2) object property
-        #     elif issubclass(field_info.annotation, ObjectProperty):
-        #         op = getattr(self, f)
-        #         # (2.1) remove existing object property
-        #         # (2.1.1) break the link between two instances
-        #         # (2.1.2) TODO remove the instance [should this be handled here?]
-        #         g_to_remove = op.collect_range_diff_to_graph(self.instance_iri, g_to_remove, to_remove_range=True)
-        #         # (2.2) add new object property
-        #         # (2.2.2) add the link between two instances only (instances already exist in KG)
-        #         # (2.2.1) TODO add new object property also the triples for new instance [should this be handled here?]
-        #         g_to_add = op.collect_range_diff_to_graph(self.instance_iri, g_to_add, to_add_range=True)
-        #         # clear the changes so that it does not get carried over to the next push
-        #         op.clear_changes()
-        #     else:
-        #         # TODO process extra fields
-        #         continue
-        # # TODO merge this with push_to_kg
-        # if self.is_pulled_from_kg:
-
-        #     g_to_remove = Graph()
-        #     g_to_add = Graph()
-
-        #     # push the changes to KG
-        #     sparql_client.delete_and_insert_graphs(g_to_remove, g_to_add)
-        #     # TODO what happens when KG changed during processing in the python side? do one pull and push again?
-        # else:
-        #     self.push_to_kg(sparql_client)
 
     def collect_diff_to_graph(self, g_to_remove: Graph, g_to_add: Graph, recursive_depth: int = 0) -> Tuple[Graph, Graph]:
         for f, field_info in self.model_fields.items():
@@ -358,7 +280,7 @@ class BaseOntology(BaseModel, validate_assignment=True):
             elif f == 'rdf_type' and not self._exist_in_kg and not bool(self._latest_cache.get(f)):
                 g_to_add.add((URIRef(self.instance_iri), RDF.type, URIRef(self.rdf_type)))
                 # assume that the instance is in KG once the triples are added
-                # TODO or need to a better way to represent this?
+                # TODO [future] or need to a better way to represent this?
                 self._exist_in_kg = True
             elif f == 'rdfs_comment':
                 if self._latest_cache.get(f) != self.rdfs_comment:
@@ -368,30 +290,6 @@ class BaseOntology(BaseModel, validate_assignment=True):
                         g_to_add.add((URIRef(self.instance_iri), RDFS.comment, Literal(self.rdfs_comment)))
         return g_to_remove, g_to_add
 
-    def collect_triples_to_add(self, g: Graph) -> Graph:
-        pass
-
-    # def create_triples_for_kg(self, g: Graph) -> Graph:
-    #     """This method is for creating triples as rdflib.Graph() for the knowledge graph.
-    #     By default, it will create triples for the instance itself.
-    #     One can overwrite this method and provide additional triples should they wish.
-    #     In which case, please remember to call the super.create_triples_for_kg(g),
-    #     unless one wants to provide completely different triples."""
-    #     g.add((URIRef(self.instance_iri), RDF.type, URIRef(self.rdf_type)))
-    #     if self.rdfs_comment:
-    #         g.add((URIRef(self.instance_iri), RDFS.comment,
-    #               Literal(self.rdfs_comment)))
-    #     for f, prop in iter(self):
-    #         # TODO optimise below condition
-    #         if f not in ['base_prefix', 'namespace', 'rdfs_comment', 'instance_iri', 'rdf_type'] and bool(prop):
-    #             if isinstance(prop, BaseProperty) and bool(prop.range):
-    #                 g = prop.collect_range_diff_to_graph(self.instance_iri, g)
-    #                 # TODO consider what to do when the range is already part of the KG, or it's only str
-    #             else:
-    #                 raise TypeError(
-    #                     f"Type of {prop} is not supported for field {f} when creating KG triples for instance {self.dict()}.")
-    #     return g
-
     def _exclude_keys_for_compare_(self, *keys_to_exclude):
         list_keys_to_exclude = list(keys_to_exclude) if not isinstance(
             keys_to_exclude, list) else keys_to_exclude
@@ -399,7 +297,6 @@ class BaseOntology(BaseModel, validate_assignment=True):
         list_keys_to_exclude.append('rdfs_comment')
         list_keys_to_exclude.append('base_prefix')
         list_keys_to_exclude.append('namespace')
-        # list_keys_to_exclude.append('is_pulled_from_kg') # TODO delete?
         return set(tuple(list_keys_to_exclude))
 
     def __eq__(self, other: Any) -> bool:
@@ -408,7 +305,7 @@ class BaseOntology(BaseModel, validate_assignment=True):
     def __hash__(self):
         # using instance_iri for hash so that iri and object itself are treated the same in set operations
         return self.instance_iri.__hash__()
-        # TODO do we want to provide the method to compare if the content of two instances are the same?
+        # TODO [future] do we want to provide the method to compare if the content of two instances are the same?
         # a use case would be to compare if the chemicals in the two bottles are the same concentration
         # return self._make_hash_sha256_(self.dict(exclude=self._exclude_keys_for_compare_()))
 
@@ -427,7 +324,7 @@ class BaseOntology(BaseModel, validate_assignment=True):
             return tuple(sorted((self._make_hashable_(e) for e in o)))
 
         if isinstance(o, dict):
-            # TODO below is a shortcut for the implementation, the specific _exclude_keys_for_compare_ of nested classes are not called
+            # TODO [future] below is a shortcut for the implementation, the specific _exclude_keys_for_compare_ of nested classes are not called
             # but for OntoCAPE_SinglePhase this is sufficient for the comparison (as 'instance_iri' and 'namespace_for_init' are excluded by default)
             # to do it properly, we might need recursion that calls all _exclude_keys_for_compare_ while iterate the nested classes
             for key in self._exclude_keys_for_compare_():
@@ -507,34 +404,9 @@ class ObjectProperty(BaseProperty):
 
         return g_to_remove, g_to_add
 
-    # # TODO optimise the code for the range_to_add and range_to_remove
-    # def collect_range_diff_to_graph(self, subject: str, g_to_remove: Graph, g_to_add: Graph, recursive_depth: int = 0) -> Graph:
-    #     # NOTE the range can be a set of BaseOntology or str
-    #     # behaviour of recursive_depth: 0 means no recursion, -1 means infinite recursion, n means n-level recursion
-    #     flag_collection = abs(recursive_depth) > 0
-    #     recursive_depth = max(recursive_depth - 1, 0) if recursive_depth > -1 else max(recursive_depth - 1, -1)
-    #     # compare the range and its cache to find out what to remove and what to add
-    #     diff_to_remove = self._range_cache - self.range
-    #     diff_to_add = self.range - self._range_cache
-    #     # iterate the differences and add them to the graph
-    #     for o in diff_to_add:
-    #         if isinstance(o, BaseOntology):
-    #             g.add((URIRef(subject), URIRef(self.predicate_iri), URIRef(o.instance_iri)))
-    #             # TODO consider recursion for the object property, should be controlled by a flag
-    #             if flag_collection:
-    #                 pass
-    #             # g = o.collect_triples_to_remove(g)
-    #         elif isinstance(o, str):
-    #             g.add((URIRef(subject), URIRef(self.predicate_iri), URIRef(o)))
-    #         else:
-    #             raise TypeError(f"Type of {o} is not supported.")
-    #     # TODO besides the differences between the range and its cache, also need to consider the intersection of the range and its cache when recursive
-    #     return g
-
 
 class DataProperty(BaseProperty):
 
-    # TODO optimise the code for the range_to_add and range_to_remove
     def collect_range_diff_to_graph(
         self,
         subject: str,
@@ -543,7 +415,6 @@ class DataProperty(BaseProperty):
         g_to_add: Graph,
         recursive_depth: int = 0,
     ) -> Tuple[Graph, Graph]:
-        # TODO optimise the code for the range_to_add and range_to_remove
         # compare the range and its cache to find out what to remove and what to add
         diff_to_remove = cache.range - self.range
         for d in diff_to_remove:
