919cb6b0c79694bbe7c2638cfdcf30365a7d97c8
Dan Tran <nmdt2@cam.ac.uk>
2024-03-14 11:33:06 +0800

    dev-zaha-experimental: Send query to a particular domain

--- QuestionAnswering/QA_Agent/fastapi_app/api/qa.py
+++ QuestionAnswering/QA_Agent/fastapi_app/api/qa.py
@@ -1,16 +1,21 @@
 import logging
 import time
-from typing import Annotated, List, Literal
+from typing import Annotated, List, Optional
 
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
 from model.qa import QAData, QAMode, QAStep
-from services.connector import AgentConnectorMediator, get_agent_connector_mediator
+from services.connector import (
+    AgentConnectorMediator,
+    get_chemistry_mediator,
+    get_cities_mediator,
+)
 
 
 class QARequest(BaseModel):
     question: str
+    domain: Optional[str] = None
 
 
 class QAResponseMetadata(BaseModel):
@@ -26,21 +31,39 @@ class QAResponse(BaseModel):
 
 logger = logging.getLogger(__name__)
 
-router = APIRouter()
+chemistry_router = APIRouter()
+cities_router = APIRouter()
 
 
-@router.post("")
-def qa(
+@chemistry_router.post("/")
+def qa_chemistry(
     req: QARequest,
-    agent_connector: Annotated[AgentConnectorMediator, Depends(get_agent_connector_mediator)],
+    mediator: Annotated[AgentConnectorMediator, Depends(get_chemistry_mediator)],
 ):
     logger.info("Received request to QA endpoint with the following request body")
     logger.info(req)
 
     timestamp = time.time()
-    qa_mode, steps, data = agent_connector.query(req.question)
+    qa_mode, steps, data = mediator.query(req.question)
     latency = time.time() - timestamp
 
-    return QAResponse(
-        metadata=QAResponseMetadata(mode=qa_mode, latency=latency, steps=steps), data=data
-    )
+    metadata = QAResponseMetadata(mode=qa_mode, latency=latency, steps=steps)
+
+    return QAResponse(metadata=metadata, data=data)
+
+
+@cities_router.post("/")
+def qa_cities(
+    req: QARequest,
+    mediator: Annotated[AgentConnectorMediator, Depends(get_cities_mediator)],
+):
+    logger.info("Received request to QA endpoint with the following request body")
+    logger.info(req)
+
+    timestamp = time.time()
+    qa_mode, steps, data = mediator.query(query=req.question, domain=req.domain)
+    latency = time.time() - timestamp
+
+    metadata = QAResponseMetadata(mode=qa_mode, latency=latency, steps=steps)
+
+    return QAResponse(metadata=metadata, data=data)
