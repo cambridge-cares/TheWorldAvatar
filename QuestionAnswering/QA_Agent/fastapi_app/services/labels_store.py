e79bd85433959f3c355f654afa4b4995ac005a2c
Dan Tran <nmdt2@cam.ac.uk>
2024-03-18 09:21:58 +0800

    dev-marie-experimental: Add test for LabelsStore

--- QuestionAnswering/QA_Agent/fastapi_app/services/labels_store.py
+++ QuestionAnswering/QA_Agent/fastapi_app/services/labels_store.py
@@ -38,48 +38,49 @@ class LabelsStore:
         self.bindings = bindings
 
     def _create_index(self):
-        pipeline = self.redis_client.json().pipeline()
+        pipeline = self.redis_client.pipeline()
         for i, binding in enumerate(self.bindings):
             name = self.key_prefix + str(i)
             pipeline.json().set(name, "$", asdict(binding))
         pipeline.execute()
 
         definition = IndexDefinition(
-            prefix=[self.key_prefix], index_type=IndexType.HASH
+            prefix=[self.key_prefix], index_type=IndexType.JSON
         )
         self.redis_client.ft(self.index_name).create_index(
             fields=self.SCHEMA, definition=definition
         )
 
-    @cache
     def _all_labels(self):
         if not does_index_exist(self.redis_client, self.index_name):
             self._create_index()
 
-        labels = [
-            label
-            for doc in self.redis_client.ft(self.index_name)
+        docs = (
+            self.redis_client.ft(self.index_name)
             .search(Query("*").return_field("$.labels", as_field="labels_serialized"))
             .docs
-            for label in json.loads(doc.labels_serialized)
-        ]
+        )
+        labels = [label for doc in docs for label in json.loads(doc.labels_serialized)]
         return list(set(labels))
 
     def _lookup_iri(self, label: str):
+        print("label to lookup:", label)
         if not does_index_exist(self.redis_client, self.index_name):
             self._create_index()
 
-        iris = [
-            doc.IRI
-            for doc in self.redis_client.ft(self.index_name)
-            .search(Query("@labels:{{label}}".format(label=label)).return_field("IRI"))
+        docs = (
+            self.redis_client.ft(self.index_name)
+            .search(
+                Query("@labels:{{{label}}}".format(label=label)).return_field("IRI")
+            )
             .docs
-        ]
+        )
+        iris = [doc.IRI for doc in docs]
         return list(set(iris))
 
     def link_entity(self, name: str):
         choices = self._all_labels()
-        closest_label, _ = rapidfuzz.process.extractOne(
+        closest_label, _, _ = rapidfuzz.process.extractOne(
             name,
             choices,
             scorer=rapidfuzz.fuzz.WRatio,
--- QuestionAnswering/QA_Agent/fastapi_app/tests/integration/services/test_labels_store.py
+++ QuestionAnswering/QA_Agent/fastapi_app/tests/integration/services/test_labels_store.py
@@ -1,6 +1,42 @@
-class TestLabelsStore:
-    def test_linkEntity(self):
-        pass
+from services.labels_store import IRIWithLabels, LabelsStore
 
-    def test_lookupLabels(self):
-        pass
+
+class TestLabelsStore:
+    SAMPLE_BINDINGS = [
+        IRIWithLabels(IRI="1", labels=["one", "One"]),
+        IRIWithLabels(IRI="2", labels=["two", "tWo"]),
+        IRIWithLabels(IRI="3", labels=["thRee", "3333"]),
+        IRIWithLabels(IRI="3bis", labels=["thRee"])
+    ]
+
+    def test_linkEntity(self, redis_client):
+        # Arrange
+        labels_store = LabelsStore(
+            redis_client=redis_client,
+            key_prefix="test:",
+            index_name="idx:test",
+            bindings=self.SAMPLE_BINDINGS,
+        )
+
+        # Act
+        actual = labels_store.link_entity("three")
+
+        # Assert
+        expected = ["3", "3bis"]
+        assert set(actual) == set(expected)
+
+    def test_lookupLabels(self, redis_client):
+        # Arrange
+        labels_store = LabelsStore(
+            redis_client=redis_client,
+            key_prefix="test:",
+            index_name="idx:test",
+            bindings=self.SAMPLE_BINDINGS,
+        )
+
+        # Act
+        actual = labels_store.lookup_labels("2")
+
+        # Assert
+        expected = ["two", "tWo"]
+        actual == expected
