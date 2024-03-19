8cb2c7cc6486f1c65a9ec0f561111c30d15a763e
Dan Tran <nmdt2@cam.ac.uk>
2024-03-19 18:02:34 +0800

    dev-qa-icl: Use separate blazegraph server for automated tests

--- QuestionAnswering/QA_Agent/fastapi_app/tests/integration/services/connectors/sg_factories/test_agent.py
+++ QuestionAnswering/QA_Agent/fastapi_app/tests/integration/services/connectors/sg_factories/test_agent.py
@@ -1,8 +1,11 @@
 import os
+from urllib.parse import urljoin
 import pytest
 
 from redis import Redis
+from yarl import URL
 
+from tests.integration.services.utils import TriplesManager
 from model.aggregate import AggregateOperator
 from services.connectors.sg_factories.constants import FactoryAttrKey, FactoryConcept
 from services.connectors.sg_factories.agent.labels_store import (
@@ -13,11 +16,43 @@ from services.kg_client import KgClient
 from services.connectors.sg_factories.agent import SGFactoriesAgent
 
 
-# TODO: Use a single dedicated blazegraph instance to run tests
-# TODO: Insert sample data within this scope
 @pytest.fixture(scope="module")
-def sg_factories_ontop_client():
-    yield KgClient(os.getenv("KG_ENDPOINT_SG_FACTORIES_ONTOP"))
+def sg_factories_ontop_client(blazegraph_base_url: URL):
+    triples_manager = TriplesManager(
+        base_url=blazegraph_base_url,
+        namespace="sg_factories_ontop",
+        prefixes="""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX om: <http://www.ontology-of-units-of-measure.org/resource/om-2/>
+PREFIX ontocompany: <http://www.theworldavatar.com/kg/ontocompany/>
+PREFIX ontochemplant: <http://www.theworldavatar.com/kg/ontochemplant/>""",
+    )
+
+    triples_manager.insert(
+        [
+            """<http://test.com/1> 
+    rdf:type ontochemplant:ChemicalPlant ;
+    rdfs:label "NORMET SINGAPORE PTE. LTD." ;
+    ontocompany:hasGeneratedHeat [ om:hasValue [ om:hasNumericalValue "5.610722679"^^xsd:float ; om:hasUnit [ skos:notation "MW" ] ]  ] ;
+    ontocompany:hasThermalEfficiency [ om:hasValue [ om:hasNumericalValue "0.52"^^xsd:float ] ] .""",
+            """<http://test.com/2>
+    rdf:type ontochemplant:ChemicalPlant ;
+    rdfs:label "AICA SINGAPORE PTE. LTD." ;
+    ontocompany:hasGeneratedHeat [ om:hasValue [ om:hasNumericalValue "12.97975266"^^xsd:float ; om:hasUnit [ skos:notation "MW" ] ] ] ;
+    ontocompany:hasThermalEfficiency [ om:hasValue [ om:hasNumericalValue "0.52"^^xsd:float ] ] .""",
+            """<http://test.com/3>
+    rdf:type ontocompany:FoodPlant ;
+    rdfs:label "NORTHERN LUCK PTE LTD" ;
+    ontocompany:hasGeneratedHeat [ om:hasValue [ om:hasNumericalValue "6.427321157"^^xsd:float ; om:hasUnit [ skos:notation "MW" ] ] ]  ;
+    ontocompany:hasThermalEfficiency [ om:hasValue [ om:hasNumericalValue "0.43"^^xsd:float ] ] .""",
+        ]
+    )
+
+    endpoint = str(blazegraph_base_url / "blazegraph/namespace/sg_factories_ontop/sparql")
+    yield KgClient(endpoint)
+
+    triples_manager.delete_all()
 
 
 @pytest.fixture(scope="module")
@@ -46,22 +81,18 @@ def sg_factories_agent(
 
 class TestSGFactoriesAgent:
     def test_lookupFactoryAttribute(self, sg_factories_agent: SGFactoriesAgent):
-        # Arrange
-        plant_name = "KENSETSU INTERNATIONAL (S) PTE. LTD."
-
         # Act
         data = sg_factories_agent.lookup_factory_attribute(
-            plant_name=plant_name, attr_key=FactoryAttrKey.GENERATED_HEAT
+            plant_name="Normet Singapore", attr_key=FactoryAttrKey.THERMAL_EFFICIENCY
         )
 
         # Assert
-        assert data.vars == ["IRI", "GeneratedHeatNumericalValue", "GeneratedHeatUnit"]
+        assert data.vars == ["IRI", "ThermalEfficiencyNumericalValue"]
         assert len(data.bindings) == 1
         assert (
             data.bindings[0].items()
             >= {
-                "GeneratedHeatNumericalValue": "3.625656925",
-                "GeneratedHeatUnit": "MW",
+                "ThermalEfficiencyNumericalValue": "0.52",
             }.items()
         )
 
@@ -73,7 +104,7 @@ class TestSGFactoriesAgent:
 
         # Assert
         assert data.vars == ["Count"]
-        assert data.bindings == [{"Count": "96"}]
+        assert data.bindings == [{"Count": "2"}]
 
     def test_countFactories_groupby(self, sg_factories_agent: SGFactoriesAgent):
         # Act
@@ -84,16 +115,12 @@ class TestSGFactoriesAgent:
         # TODO: compare by set
         assert data.bindings == [
             {
-                "Count": "96",
+                "Count": "2",
                 "Type": "http://www.theworldavatar.com/kg/ontochemplant/ChemicalPlant",
             },
             {
-                "Count": "201",
+                "Count": "1",
                 "Type": "http://www.theworldavatar.com/kg/ontocompany/FoodPlant",
-            },
-            {
-                "Count": "11",
-                "Type": "http://www.theworldavatar.com/kg/ontocompany/SemiconductorPlant",
             },
         ]
 
@@ -102,14 +129,17 @@ class TestSGFactoriesAgent:
     ):
         # Act
         data = sg_factories_agent.compute_aggregate_factory_attribute(
-            factory_type=FactoryConcept.FOOD_PLANT,
-            attr_agg=(FactoryAttrKey.DESIGN_CAPACITY, AggregateOperator.AVG),
+            factory_type=FactoryConcept.CHEMICAL_PLANT,
+            attr_agg=(FactoryAttrKey.GENERATED_HEAT, AggregateOperator.AVG),
         )
 
         # Assert
-        assert data.vars == ["DesignCapacityNumericalValueAVG"]
+        assert data.vars == ["GeneratedHeatNumericalValueAVG", "GeneratedHeatUnit"]
         assert data.bindings == [
-            {"DesignCapacityNumericalValueAVG": "0.07682154649751231"}
+            {
+                "GeneratedHeatNumericalValueAVG": "5.2952376695",
+                "GeneratedHeatUnit": "MW",
+            }
         ]
 
     def test_computeAggregateFactoryAttribute_groupby(
@@ -127,14 +157,10 @@ class TestSGFactoriesAgent:
         assert data.bindings == [
             {
                 "Type": "http://www.theworldavatar.com/kg/ontochemplant/ChemicalPlant",
-                "GeneratedHeatNumericalValueSUM": "648.8399716939999",
+                "GeneratedHeatNumericalValueSUM": "18.590475",
             },
             {
-                "GeneratedHeatNumericalValueSUM": "149.17109734199994",
+                "GeneratedHeatNumericalValueSUM": "6.427321",
                 "Type": "http://www.theworldavatar.com/kg/ontocompany/FoodPlant",
-            },
-            {
-                "GeneratedHeatNumericalValueSUM": "124.29142511900002",
-                "Type": "http://www.theworldavatar.com/kg/ontocompany/SemiconductorPlant",
             },
         ]
