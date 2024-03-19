911cc429ae997ac899cf65185d59ca4dfaba02bc
Jiaru Bai <jb2197@cam.ac.uk>
2024-03-19 23:54:30 +0000

    dev-pydantic-rdflib: modified requirements in setup.py.

--- JPS_BASE_LIB/python_wrapper/setup.py
+++ JPS_BASE_LIB/python_wrapper/setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=find_namespace_packages(exclude=['tests', 'tests.*']),
-    install_requires=['py4j==0.10.9.1','docopt','concurrent_log_handler', 'pydantic', 'rdflib'],
+    install_requires=['py4j==0.10.9.1','docopt','concurrent_log_handler', 'pydantic', 'rdflib==7.0.0', 'requests'],
     include_package_data= True,
     entry_points={
         'console_scripts': [
