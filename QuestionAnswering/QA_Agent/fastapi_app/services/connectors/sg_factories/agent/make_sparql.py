3b6cbbbda3055bb9fcbfd6fc2c9794fb97037c4d
Dan Tran <nmdt2@cam.ac.uk>
2024-03-22 16:43:10 +0800

    dev-qa-agent: Update SG factories SPARQL maker

--- QuestionAnswering/QA_Agent/fastapi_app/services/connectors/sg_factories/agent/make_sparql.py
+++ QuestionAnswering/QA_Agent/fastapi_app/services/connectors/sg_factories/agent/make_sparql.py
@@ -1,14 +1,14 @@
 from functools import cache
-from typing import Annotated, List, Literal, Optional, Tuple
+from typing import Annotated, Dict, List, Optional, Tuple
 
 from fastapi import Depends
 
 from model.aggregate import AggregateOperator
 from model.constraint import ExtremeValueConstraint
 from ..model import (
-    FACTORYATTR2UNIT,
     FactoryAttrKey,
-    FactoryConstraints,
+    FactoryIndustryKey,
+    FactoryNumAttrKey,
     Industry,
 )
 from .labels_store import get_factory_subclasses
@@ -30,25 +30,33 @@ class SGFactoriesSPARQLMaker:
         return list(self.factory_subclass_patterns)
 
     def lookup_factory_attribute(self, iris: List[str], attr_key: FactoryAttrKey):
-        vars = ["?IRI"]
+        select_vars = ["?IRI"]
+        patterns = [
+            "VALUES ?IRI {{ {iris} }}".format(
+                iris=" ".join(["<{iri}>".format(iri=iri) for iri in iris])
+            )
+        ]
 
-        if attr_key is FactoryAttrKey.INDUSTRY:
-            vars.append("?Industry")
-            pattern = "?IRI ontocompany:belongsToIndustry/rdf:type ?Industry ."
-        elif attr_key is FactoryAttrKey.THERMAL_EFFICIENCY:
-            vars.append("?{key}NumericalValue".format(key=attr_key.value))
-            pattern = "?IRI ontocompany:has{key}/om:hasValue/om:hasNumericalValue ?{key}NumericalValue .".format(
-                key=attr_key.value
-            )
+        if isinstance(attr_key, FactoryIndustryKey):
+            select_vars.append("?Industry")
+            patterns.append("?IRI ontocompany:belongsToIndustry/rdf:type ?Industry .")
         else:
-            vars.extend(
+            value_var = "?{key}Value".format(key=attr_key.value)
+            unit_var = "?{key}Unit".format(key=attr_key.value)
+
+            select_vars.extend([value_var, unit_var])
+            patterns.extend(
                 [
-                    "?{key}NumericalValue".format(key=attr_key.value),
-                    "?{key}Unit".format(key=attr_key.value),
+                    "?IRI ontocompany:has{key}/om:hasValue ?{key} .".format(
+                        key=attr_key.value
+                    ),
+                    "?{key} om:hasNumericalValue {value_node} .".format(
+                        key=attr_key.value, value_node=value_var
+                    ),
+                    "OPTIONAL {{ ?{key} om:hasUnit/skos:notation {unit_node} . }}".format(
+                        key=attr_key.value, unit_node=unit_var
+                    ),
                 ]
-            )
-            pattern = "?IRI ontocompany:has{key}/om:hasValue [ om:hasNumericalValue ?{key}NumericalValue ; om:hasUnit/skos:notation ?{key}Unit ] .".format(
-                key=attr_key.value
             )
 
         return """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -58,122 +66,80 @@ PREFIX om: <http://www.ontology-of-units-of-measure.org/resource/om-2/>
 PREFIX ontocompany: <http://www.theworldavatar.com/kg/ontocompany#>
 
 SELECT DISTINCT {vars} WHERE {{
-VALUES ?IRI {{ {iris} }}
-{pattern}
+{patterns}
 }}""".format(
-            iris=" ".join(["<{iri}>".format(iri=iri) for iri in iris]),
-            vars=" ".join(vars),
-            pattern=pattern,
+            vars=" ".join(select_vars),
+            patterns="\n".join(patterns),
         )
 
     def _make_clauses_for_constraint(
         self,
-        key: Literal[
-            FactoryAttrKey.DESIGN_CAPACITY,
-            FactoryAttrKey.GENERATED_HEAT,
-            FactoryAttrKey.SPECIFIC_ENERGY_CONSUMPTION,
-            FactoryAttrKey.THERMAL_EFFICIENCY,
-        ],
+        key: FactoryNumAttrKey,
         constraint: ExtremeValueConstraint,
     ):
-        unit = FACTORYATTR2UNIT[key]
-        value_var = "?{key}NumericalValue".format(key=key.value)
+        value_var = "?{key}Value".format(key=key.value)
+        unit_var = "?{key}Unit".format(key=key.value)
 
-        if unit:
-            where_pattern = '?IRI ontocompany:has{key}/om:hasValue [ om:hasNumericalValue {value_node} ; om:hasUnit/skos:notation "{unit}" ] .'.format(
-                key=key.value, value_node=value_var, unit=unit
-            )
-        else:
-            where_pattern = "?IRI ontocompany:has{key}/om:hasValue/om:hasNumericalValue {value_node} .".format(
+        where_patterns = [
+            "?IRI ontocompany:has{key}/om:hasValue ?{key} .".format(key=key.value),
+            "?{key} om:hasNumericalValue {value_node} .".format(
                 key=key.value, value_node=value_var
-            )
+            ),
+            "OPTIONAL {{ ?{key} om:hasUnit/skos:notation {unit_node} . }}".format(
+                key=key.value, unit_node=unit_var
+            ),
+        ]
 
         if constraint is ExtremeValueConstraint.MAX:
-            orderby = "DESC({var})".format(var=value_var)
+            value_var_orderby = "DESC({var})".format(var=value_var)
         else:
-            orderby = value_var
+            value_var_orderby = value_var
 
-        return value_var, where_pattern, orderby
+        return value_var, value_var_orderby, unit_var, where_patterns
+
+    def _make_industry_pattern(self, industry: Industry):
+        return "?IRI ontocompany:belongsToIndustry/rdf:type ontocompany:{industry} .".format(
+            industry=industry.value
+        )
 
     def find_factories(
         self,
-        constraints: Optional[FactoryConstraints] = None,
+        industry: Industry,
+        numattr_constraints: Dict[FactoryNumAttrKey, ExtremeValueConstraint] = dict(),
         limit: Optional[int] = None,
     ):
-        vars = ["?IRI"]
+        select_vars = ["?IRI"]
         ontop_patterns = self._make_factory_subclass_patterns()
+        ontop_patterns.append(self._make_industry_pattern(industry))
         orderby_vars = []
 
-        if constraints:
-            if constraints.industry:
-                ontop_patterns.append(
-                    "?IRI ontocompany:belongsToIndustry/rdf:type ontocompany:{industry} .".format(
-                        industry=constraints.industry.value
-                    )
-                )
-            for field, key in (
-                ("generated_heat", FactoryAttrKey.GENERATED_HEAT),
-                ("design_capacity", FactoryAttrKey.DESIGN_CAPACITY),
-                (
-                    "specific_energy_consumption",
-                    FactoryAttrKey.SPECIFIC_ENERGY_CONSUMPTION,
-                ),
-                ("thermal_efficiency", FactoryAttrKey.THERMAL_EFFICIENCY),
-            ):
-                constraint = getattr(constraints, field)
-                if constraint:
-                    value_var, where_pattern, orderby = (
-                        self._make_clauses_for_constraint(
-                            key=key, constraint=constraint
-                        )
-                    )
-                    vars.append(value_var)
-                    ontop_patterns.append(where_pattern)
-                    orderby_vars.append(orderby)
+        for key, constraint in numattr_constraints.items():
+            value_var, value_var_orderby, unit_var, where_patterns = (
+                self._make_clauses_for_constraint(key=key, constraint=constraint)
+            )
+            select_vars.extend([value_var, unit_var])
+            ontop_patterns.extend(where_patterns)
+            orderby_vars.append(value_var_orderby)
 
         return """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX om: <http://www.ontology-of-units-of-measure.org/resource/om-2/>
 PREFIX ontocompany: <http://www.theworldavatar.com/kg/ontocompany#>
 
 SELECT DISTINCT {vars} WHERE {{
 {patterns}
 }}{orderby}{limit}""".format(
-            vars=" ".join(vars),
+            vars=" ".join(select_vars),
             patterns="\n".join(ontop_patterns),
             orderby="\nORDER BY " + " ".join(orderby_vars),
             limit="\nLIMIT " + str(limit) if limit else "",
         )
 
-    def _init_clauses_for_agg(
-        self, industry: Optional[Industry] = None, groupby_industry: bool = False
-    ):
+    def count_factories(self, industry: Industry):
         select_vars = []
         ontop_patterns = self._make_factory_subclass_patterns()
-        groupby_vars = []
-
-        if industry:
-            ontop_patterns.append(
-                "VALUES ?Industry {{ ontocompany:{industry} }}".format(
-                    industry=industry.value
-                )
-            )
-
-        if industry or groupby_industry:
-            select_vars.append("?Industry")
-            ontop_patterns.append(
-                "?IRI ontocompany:belongsToIndustry/rdf:type ?Industry ."
-            )
-            groupby_vars.append("?Industry")
-
-        return select_vars, ontop_patterns, groupby_vars
-
-    def count_factories(
-        self, industry: Optional[Industry] = None, groupby_industry: bool = False
-    ):
-        select_vars, ontop_patterns, groupby_vars = self._init_clauses_for_agg(
-            industry=industry, groupby_industry=groupby_industry
-        )
+        ontop_patterns.append(self._make_industry_pattern(industry))
         select_vars.append("(COUNT(?IRI) AS ?Count)")
 
         return """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -182,40 +148,42 @@ PREFIX ontocompany: <http://www.theworldavatar.com/kg/ontocompany#>
 
 SELECT {select_vars} WHERE {{
 {patterns}
-}}{groupby}""".format(
-            select_vars=" ".join(select_vars),
-            patterns="\n".join(ontop_patterns),
-            groupby="\nGROUP BY " + " ".join(groupby_vars) if groupby_vars else "",
+}}""".format(
+            select_vars=" ".join(select_vars), patterns="\n".join(ontop_patterns)
         )
 
     def compute_aggregate_factory_attribute(
         self,
-        attr_agg: Tuple[FactoryAttrKey, AggregateOperator],
-        industry: Optional[Industry] = None,
-        groupby_industry: bool = False,
+        industry: Industry,
+        attr_agg: Tuple[FactoryNumAttrKey, AggregateOperator],
     ):
-        select_vars, ontop_patterns, groupby_vars = self._init_clauses_for_agg(
-            industry=industry, groupby_industry=groupby_industry
-        )
+        select_vars = []
+        ontop_patterns = self._make_factory_subclass_patterns()
+        ontop_patterns.append(self._make_industry_pattern(industry))
+        groupby_vars = []
 
         attr_key, agg_op = attr_agg
-        unit = None
-        if attr_key is FactoryAttrKey.INDUSTRY:
-            pass
-        else:
-            agg_var = "?{key}NumericalValue".format(key=attr_key.value)
-            unit = FACTORYATTR2UNIT[attr_key]
-            if unit:
-                pattern = '?IRI ontocompany:has{key}/om:hasValue [ om:hasNumericalValue ?{key}NumericalValue ; om:hasUnit/skos:notation "{unit}" ] .'.format(
-                    key=attr_key.value, unit=unit
-                )
-            else:
-                pattern = "?IRI ontocompany:has{key}/om:hasValue/om:hasNumericalValue ?{key}NumericalValue .".format(
+
+        unit_var = "?{key}Unit".format(key=attr_key.value)
+        agg_var = "?{key}Value".format(key=attr_key.value)
+        ontop_patterns.extend(
+            [
+                "?IRI ontocompany:has{key}/om:hasValue ?{key} .".format(
                     key=attr_key.value
-                )
-            ontop_patterns.append(pattern)
-        select_vars.append(
-            "({func}({var}) AS {var}{func})".format(func=agg_op.value, var=agg_var)
+                ),
+                "?{key} om:hasNumericalValue ?{key}Value .".format(key=attr_key.value),
+                "OPTIONAL {{ ?{key} om:hasUnit/skos:notation {unit_node} . }}".format(
+                    key=attr_key.value, unit_node=unit_var
+                ),
+            ]
+        )
+        groupby_vars.append(unit_var)
+
+        select_vars.extend(
+            [
+                "({func}({var}) AS {var}{func})".format(func=agg_op.value, var=agg_var),
+                unit_var,
+            ]
         )
 
         return """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
