945b56a6900b2419239c48c31fb3d37e4b48ef58
Dan Tran <nmdt2@cam.ac.uk>
2024-03-27 17:56:50 +0800

    dev-qa-icl: Update tests for DocsRetriever

--- QuestionAnswering/QA_Agent/fastapi_app/tests/integration/services/core/test_retrieve_docs.py
+++ QuestionAnswering/QA_Agent/fastapi_app/tests/integration/services/core/test_retrieve_docs.py
@@ -1,19 +1,45 @@
+import pytest
 from services.core.retrieve_docs import DocsRetriever
 
 
+@pytest.fixture(scope="class")
+def docs_retriever(redis_client, embedder):
+    docs = [
+        "happy",
+        "sad",
+        "surprised",
+        "melancholic",
+        "joyful",
+    ]
+
+    yield DocsRetriever(
+        embedder=embedder, redis_client=redis_client, key="test", docs=iter(docs)
+    )
+
+
+@pytest.fixture(scope="class")
+def docs_retriever_with_tags(redis_client, embedder):
+    docs = [
+        "happy",
+        "sad",
+        "surprised",
+        "joyful",
+        "melancholic",
+    ]
+    tags = ["a", "b", "a", "b", "a"]
+
+    yield DocsRetriever(
+        embedder=embedder,
+        redis_client=redis_client,
+        key="test_with_keys",
+        docs=iter(docs),
+        tags=iter(tags),
+    )
+
+
 class TestDocsRetriever:
-    def test_retrieve(self, redis_client, embedder):
+    def test_retrieve(self, docs_retriever: DocsRetriever[str]):
         # Arrange
-        docs = [
-            "happy",
-            "sad",
-            "surprised",
-            "melancholic",
-            "joyful",
-        ]
-        docs_retriever = DocsRetriever(
-            embedder=embedder, redis_client=redis_client, key="test", docs=iter(docs)
-        )
         queries = ["depressed", "contented"]
 
         # Act
@@ -26,3 +52,19 @@ class TestDocsRetriever:
         actual = [[doc for doc, _ in lst] for lst in retrieved]
         assert set(actual[0]) == set(expected[0])
         assert set(actual[1]) == set(expected[1])
+
+    def test_match(self, docs_retriever: DocsRetriever[str]):
+        # Act
+        match = docs_retriever.match("happiness")
+
+        # Assert
+        assert match == "happy"
+
+    def test_retrieve_withTag(self, docs_retriever_with_tags: DocsRetriever[str]):
+        # Act
+        match, _ = docs_retriever_with_tags.retrieve(
+            queries=["happiness"], k=1, tag="b"
+        )[0][0]
+
+        # Assert
+        assert match == "joyful"
