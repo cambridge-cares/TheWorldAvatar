7ba6c1b91768e52fcb1babe2ecf5fd52f110be80
Kok Foong Lee <kokfoong.lee@cares.cam.ac.uk>
2024-03-27 13:13:25 +0800

    dev-singapore-stack: modified jurong_island_city_furniture.sql to not use a manually created table

--- Deploy/stacks/Singapore/stack-data-uploader/inputs/config/jurong_island_city_furniture.sql
+++ Deploy/stacks/Singapore/stack-data-uploader/inputs/config/jurong_island_city_furniture.sql
@@ -1,12 +1,19 @@
 alter table jurong_island_city_furniture drop column if exists "city_furniture_uuid";
 alter table jurong_island_city_furniture add column "city_furniture_uuid" character varying (10000) ;
+
+WITH cityfurniture_footprint AS (
+    SELECT cityobject_genericattrib.cityobject_id,
+    cityobject_genericattrib.geomval
+    FROM citydb.cityobject_genericattrib
+    WHERE citydb.cityobject_genericattrib.attrname = 'footprint'
+)
 Update jurong_island_city_furniture set "city_furniture_uuid" = match.uuid from (
 SELECT ogc_fid, uuid, dist FROM
 (SELECT ogc_fid, public.ST_TRANSFORM(wkb_geometry, 24500) AS geometry FROM jurong_island_city_furniture) r1
 LEFT JOIN LATERAL (
-Select strval AS uuid, r1.geometry <-> citydb.cityfurniture_footprint.geomval AS dist
-from citydb.cityfurniture_footprint, citydb.cityobject_genericattrib
-where citydb.cityobject_genericattrib.cityobject_id = citydb.cityfurniture_footprint.cityobject_id
+Select strval AS uuid, r1.geometry <-> cityfurniture_footprint.geomval AS dist
+from cityfurniture_footprint, citydb.cityobject_genericattrib
+where citydb.cityobject_genericattrib.cityobject_id = cityfurniture_footprint.cityobject_id
 and citydb.cityobject_genericattrib.attrname = 'uuid'
 order by dist
 limit 1
