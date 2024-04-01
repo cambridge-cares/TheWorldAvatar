9d0695294fabe2aee0d3c7c0a5c7b14236f1e81a
Dan Tran <nmdt2@cam.ac.uk>
2024-04-01 12:44:04 +0800

    dev-qa-agent: Refactor land lots agent

--- QuestionAnswering/QA_Agent/fastapi_app/services/connectors/sg_land_lots/agent/make_sparql.py
+++ QuestionAnswering/QA_Agent/fastapi_app/services/connectors/sg_land_lots/agent/make_sparql.py
@@ -1,69 +1,15 @@
 from functools import cache
-import logging
-from typing import Annotated, Optional, Tuple
+from typing import Optional, Tuple
 
-from fastapi import Depends
-
-from services.utils.rdf import flatten_sparql_response
 from model.aggregate import AggregateOperator
-from model.constraint import (
-    CompoundComparativeConstraint,
-    NumericalConstraint,
-    ExtremeValueConstraint,
-)
-from model.qa import QAData
 from services.core.kg import KgClient
-from services.connectors.sg import get_sg_ontopClient
-from .constants import LAND_USE_TYPE_PREDICATE, PLOT_NUM_ATTR_PREDICATES
-from .model import PlotAttrKey, PlotCatAttrKey, PlotNumAttrKey
-from .kg import get_sgLandLots_bgClient
-
-logger = logging.getLogger(__name__)
+from ..model import PlotAttrKey, PlotCatAttrKey, PlotNumAttrKey
+from ..constants import LAND_USE_TYPE_PREDICATE, PLOT_NUM_ATTR_PREDICATES
 
 
-class SGLandLotsAgent:
-    def __init__(
-        self,
-        bg_client: KgClient,
-        ontop_client: KgClient,
-    ):
+class SGLandLotsSPARQLMaker:
+    def __init__(self, bg_client: KgClient):
         self.bg_client = bg_client
-        # self.ontop_client = ontop_client
-        self.ontop_client = KgClient("http://174.138.23.221:3838/ontop/ui/sparql")
-
-    def _make_clauses_for_constraint(
-        self, key: PlotNumAttrKey, constraint: NumericalConstraint
-    ):
-        where_patterns = []
-        orderby = None
-
-        valuenode = "?{key}NumericalValue".format(key=key.value)
-        where_patterns.append(
-            "?IRI {pred}/om:hasNumericalValue {valuenode} .".format(
-                pred=PLOT_NUM_ATTR_PREDICATES[key], valuenode=valuenode
-            )
-        )
-
-        if isinstance(constraint, CompoundComparativeConstraint):
-            atomic_constraints = [
-                "{valuenode} {operator} {operand}".format(
-                    valuenode=valuenode, operator=x.operator.value, operand=x.operand
-                )
-                for x in constraint.constraints
-            ]
-            if constraint.logical_operator:
-                delimiter = constraint.logical_operator.value
-            else:
-                delimiter = "&&"
-            exprn = delimiter.join(atomic_constraints)
-            filter_pattern = "FILTER ( {exprn} )".format(exprn=exprn)
-            where_patterns.append(filter_pattern)
-        elif constraint is ExtremeValueConstraint.MAX:
-            orderby = "DESC({var})".format(var=valuenode)
-        else:
-            orderby = valuenode
-
-        return where_patterns, orderby
 
     @cache
     def _landUse_clsname2iri(self, land_use_type: str):
@@ -74,7 +20,7 @@ SELECT ?IRI WHERE {{
 }}""".format(
             land_use=land_use_type
         )
-        print(query)
+
         return [
             binding["IRI"]["value"]
             for binding in self.bg_client.query(query)["results"]["bindings"]
@@ -99,9 +45,7 @@ SELECT ?IRI WHERE {{
         return ontop_patterns
 
     def lookup_plot_attribute(
-        self,
-        attr_key: PlotAttrKey,
-        land_use_type: Optional[str] = None,
+        self, attr_key: PlotAttrKey, land_use_type: Optional[str] = None
     ):
         ontop_patterns = self._make_clauses_to_locate_plots(land_use_type)
         vars = ["?IRI"]
@@ -109,18 +53,19 @@ SELECT ?IRI WHERE {{
             ontop_patterns.append("?IRI ontozoning:hasLandUseType ?LandUseType .")
             vars.append("?LandUseType")
         elif attr_key is PlotNumAttrKey.GROSS_PLOT_RATIO:
-            ontop_patterns.append(
-                """OPTIONAL {{
-    ?IRI {pred} ?gpr . 
-}}
-OPTIONAL {{
-    ?IRI opr:isAwaitingDetailedGPREvaluation ?awaiting_detailed_evaluation .
-}}
-BIND(IF(BOUND(?gpr), ?gpr, IF(?awaiting_detailed_evaluation = true, "Awaiting detailed evaluation", "")) AS ?{key})""".format(
-                    pred=PLOT_NUM_ATTR_PREDICATES[attr_key], key=attr_key.value
-                )
+            varnode = "?" + attr_key.value
+            ontop_patterns.extend(
+                [
+                    "OPTIONAL {{ ?IRI {pred} ?gpr . }}".format(
+                        pred=PLOT_NUM_ATTR_PREDICATES[attr_key]
+                    ),
+                    "OPTIONAL { ?IRI opr:isAwaitingDetailedGPREvaluation ?awaiting_detailed_evaluation . }",
+                    'BIND(IF(BOUND(?gpr), ?gpr, IF(?awaiting_detailed_evaluation = true, "Awaiting detailed evaluation", "")) AS {varnode})'.format(
+                        varnode=varnode
+                    ),
+                ]
             )
-            vars.append("?" + attr_key.value)
+            vars.append(varnode)
         else:
             ontop_patterns.append(
                 "?IRI {pred} [ om:hasNumericalValue ?{key} ; om:hasUnit ?{key}Unit ] .".format(
@@ -129,7 +74,7 @@ BIND(IF(BOUND(?gpr), ?gpr, IF(?awaiting_detailed_evaluation = true, "Awaiting de
             )
             vars.append("?" + attr_key.value)
 
-        query = """PREFIX ontoplot:<https://www.theworldavatar.com/kg/ontoplot/>
+        return """PREFIX ontoplot:<https://www.theworldavatar.com/kg/ontoplot/>
 PREFIX opr: <https://www.theworldavatar.com/kg/ontoplanningregulation/>
 PREFIX ontozoning: <https://www.theworldavatar.com/kg/ontozoning/>
 PREFIX om:<http://www.ontology-of-units-of-measure.org/resource/om-2/>
@@ -142,16 +87,11 @@ SELECT {vars} WHERE {{
             vars=" ".join(vars), patterns="\n".join(ontop_patterns)
         )
 
-        res = self.ontop_client.query(query)
-        vars, bindings = flatten_sparql_response(res)
-
-        return QAData(vars=vars, bindings=bindings)
-
-    def count_plots(self, land_use_type: Optional[str] = None):
+    def count_plots(self, land_use_type: Optional[str]):
         ontop_patterns = self._make_clauses_to_locate_plots(land_use_type)
         vars = ["(COUNT(?IRI) as ?Count)"]
 
-        query = """PREFIX ontoplot:<https://www.theworldavatar.com/kg/ontoplot/>
+        return """PREFIX ontoplot:<https://www.theworldavatar.com/kg/ontoplot/>
 PREFIX opr: <https://www.theworldavatar.com/kg/ontoplanningregulation/>
 PREFIX ontozoning:<https://www.theworldavatar.com/kg/ontozoning/>
 PREFIX om:<http://www.ontology-of-units-of-measure.org/resource/om-2/>
@@ -163,10 +103,6 @@ SELECT {vars} WHERE {{
 }}""".format(
             vars=" ".join(vars), patterns="\n".join(ontop_patterns)
         )
-        res = self.ontop_client.query(query)
-        vars, bindings = flatten_sparql_response(res)
-
-        return QAData(vars=vars, bindings=bindings)
 
     def compute_aggregate_plot_attribute(
         self,
@@ -191,7 +127,7 @@ SELECT {vars} WHERE {{
             )
         )
 
-        query = """PREFIX ontoplot:<https://www.theworldavatar.com/kg/ontoplot/>
+        return """PREFIX ontoplot:<https://www.theworldavatar.com/kg/ontoplot/>
 PREFIX opr: <https://www.theworldavatar.com/kg/ontoplanningregulation/>
 PREFIX ontozoning:<https://www.theworldavatar.com/kg/ontozoning/>
 PREFIX om:<http://www.ontology-of-units-of-measure.org/resource/om-2/>
@@ -203,17 +139,3 @@ SELECT {vars} WHERE {{
 }}""".format(
             vars=" ".join(vars), patterns="\n".join(ontop_patterns)
         )
-        res = self.ontop_client.query(query)
-        vars, bindings = flatten_sparql_response(res)
-
-        return QAData(vars=vars, bindings=bindings)
-
-
-def get_sgLandLots_agent(
-    bg_client: Annotated[KgClient, Depends(get_sgLandLots_bgClient)],
-    ontop_client: Annotated[KgClient, Depends(get_sg_ontopClient)],
-):
-    return SGLandLotsAgent(
-        bg_client=bg_client,
-        ontop_client=ontop_client,
-    )
