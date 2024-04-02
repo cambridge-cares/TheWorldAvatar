1ca24bdd5c0515c717effb4d7332b4d34a39da5d
Kok Foong Lee <kokfoong.lee@cares.cam.ac.uk>
2024-04-02 14:36:49 +0800

    dev-singapore-stack: added new configs for new companies data

--- Deploy/stacks/Singapore/stack-data-uploader/inputs/config/data_centres.sql
+++ Deploy/stacks/Singapore/stack-data-uploader/inputs/config/data_centres.sql
@@ -1,10 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 alter table data_centres 
 drop column if exists company_uuid,
+drop column if exists industry_uuid,
 drop column if exists facility_uuid;
 alter table data_centres 
 add column company_uuid UUID,
+add column industry_uuid UUID,
 add column facility_uuid UUID;
 UPDATE data_centres
 SET company_uuid = uuid_generate_v5('6ba7b810-9dad-11d1-80b4-00c04fd430c8', company),
+   industry_uuid = uuid_generate_v5('9a791b56-6c62-4ebf-87bd-e4e87cbedd61', infrastructure_type),
    facility_uuid = uuid_generate_v5('0f849316-1751-461a-8462-e3b0c5df3b85',ogc_fid::VARCHAR);
 
--- Deploy/stacks/Singapore/stack-data-uploader/inputs/config/factories.sql
+++ Deploy/stacks/Singapore/stack-data-uploader/inputs/config/factories.sql
@@ -1,9 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 alter table factories 
 drop column if exists company_uuid,
+drop column if exists industry_uuid,
 drop column if exists facility_uuid;
 alter table factories 
 add column company_uuid UUID,
+add column industry_uuid UUID,
 add column facility_uuid UUID;
 UPDATE factories
 SET company_uuid = uuid_generate_v5('19a7288a-dbfd-48a4-8726-de1b6e30fce4', company),
+   industry_uuid = uuid_generate_v5('ee95784f-22cc-4389-82a5-244736859dc3', infrastructure_type),
    facility_uuid = uuid_generate_v5('1e3f6693-9d9d-402c-be1f-30a3ef3ea138',ogc_fid::VARCHAR);
