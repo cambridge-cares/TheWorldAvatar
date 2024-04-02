f18503564d963f10bc3acfb5418f767667012eeb
ChungTingLao <chung.ting.lao.34@gmail.com>
2024-04-02 14:44:03 +0100

    dev-ship-demo: set default port number if not supplied as an argument.

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+DEFAULT_PORT=4242
+
+PORT="${1:-$DEFAULT_PORT}"
+
 # starts up all required components of virtual sensor
-(cd stack-manager && ./stack.sh start ship-stack)
+(cd stack-manager && ./stack.sh start ship-stack "$PORT")
 # copy required files into containers
 (cd ShipInputAgent && ./copy_ship_file.sh)
 ./copy_tbox.sh
