61bade03825866a9a7f161bf636ab2b66a7c6a8d
JYAN01 <jy470@cam.ac.uk>
2024-04-04 11:43:01 +0800

    calculation of building cost

--- Agents/GFAAgent/src/main/java/uk/ac/cam/cares/jps/agent/gfaagent/CostCalculation.java
+++ Agents/GFAAgent/src/main/java/uk/ac/cam/cares/jps/agent/gfaagent/CostCalculation.java
@@ -26,23 +26,14 @@ public class CostCalculation {
     private BuildingInfo buildingInfo;
     private static final String MATCHING_PATH = "/resources/cost_landuse.csv";
 
-    private static final String costSQLInsert = "INSERT INTO cityobject_genericattrib (attrname, realval, cityobject_id)\n" +
-    "SELECT DISTINCT ON (attrname, cityobject_id) *\n" +
-        "FROM  (\n" +
-        "SELECT 'cost',\n" +
-        "(CASE WHEN building.storeys_above_ground IS NOT NULL THEN (public.ST_Area(surface_geometry.geometry)*building.storeys_above_ground)\n" +
-            "ELSE (building.measured_height/3.2*public.ST_Area(surface_geometry.geometry)) END) AS realval, building.id\n" +
-        "FROM building, surface_geometry\n" +
-        "WHERE building.lod0_footprint_id = surface_geometry.parent_id\n" +
-        ") AS cg(attrname, realval, cityobject_id)\n" +
-    "ON CONFLICT (attrname, cityobject_id) DO UPDATE SET realval= cityobject_genericattrib.realval;";
     
-    private static final String buildingTypeQuery = "SELECT pl.\"LU_DESC\", cb.id\r\n" + //
+    private static final String buildingTypeQuery = "SELECT pl.\"LU_DESC\", cb.id, mb.building_iri, cb.storeys_above_ground\n" + //
                 "FROM citydb.building cb, public.landplot pl, citydb.cityobject_genericattrib ccg, public.matched_buildings mb\r\n" + //
                 "WHERE cb.id = ccg.cityobject_id \r\n" + //
                 "AND ccg.attrname = 'uuid' \r\n" + //
                 "AND ccg.strval = mb.building_iri\r\n" + //
                 "AND mb.public_landplot_ogc_fid = pl.ogc_fid";
+
 
     public CostCalculation (String postgisDb, String postgisUser, String postgisPassword){
         this.dbUrl = postgisDb;
@@ -59,33 +50,51 @@ public class CostCalculation {
                 .build()
                 .parse();
 
-        ResultSet buildingType = getBuildingType();
-        try {
-            while (buildingType.next()) {
-                String typeLand = buildingType.getString("LU_DESC");
-                String typeCost = null;
-                String keyCost = null;
-                for (int i = 0; i < matchingType.size(); i++){
-                    if(typeLand == matchingType.get(i).getLUType()){
-                        typeCost = matchingType.get(i).getType();
-                        keyCost = matchingType.get(i).getKey();
-                    }
+        try (Connection srcConn = postgisClient.getConnection()) {
+            try (Statement stmt = srcConn.createStatement()) {
+                ResultSet buildingType = stmt.executeQuery(buildingTypeQuery);
+                while (buildingType.next()) {
+                    String typeLand = buildingType.getString("LU_DESC");
+                    String typeCost = null;
+                    String keyCost = null;
+                    String buildingIri = buildingType.getString("building_iri");
+                    int buildingId = buildingType.getInt("id");
+                    int floors = buildingType.getInt("storeys_above_ground");
+                    for (int i = 0; i < matchingType.size(); i++){
+                        if(typeLand.equals(matchingType.get(i).getLUType()) ){
+                            typeCost = matchingType.get(i).getType();
+                            keyCost = matchingType.get(i).getKey();
+                            break;
+                        }
+                    } 
+                    // if(floors == null){
+                    //     costQuery = "SELECT MIN(cost) AS cost FROM cost WHERE cost.\"" + keyCost + "\" = " + typeCost + " AND floorscat IS NULL";
+                    // }else{
+                        
+                    // }
+                    String costQuery ="SELECT cost FROM cost WHERE cost.\"Type\" = '" + typeCost + "' AND floorscat > " + floors + 
+                                        " union all select MIN(cost) from cost " +//
+                                        "where cost.\"" + keyCost + "\" = '" + typeCost + "' AND floorscat IS NULL " + //
+                                        "LIMIT 1";
+
+                    String gfaQuery = "SELECT realval AS gfa, cityobject_id\r\n" + //
+                                        "FROM citydb.cityobject_genericattrib\r\n" + //
+                                        "WHERE attrname = 'GFA' AND cityobject_id= " + buildingId;
+
+                    String costCal = "with cost_table as (" + costQuery + "), gfa_table as ( " + gfaQuery + ")\n" + //
+                                "INSERT INTO citydb.cityobject_genericattrib (cityobject_id, attrname, realval)\n" +
+                                "SELECT DISTINCT ON (cityobject_id, attrname) * FROM(\n" +
+                                "SELECT gfa_table.cityobject_id,'cost', gfa_table.gfa * cost_table.cost\n" +
+                                "FROM gfa_table, cost_table) AS cg(cityobject_id, attrname, realval) " +
+                                "ON CONFLICT (attrname, cityobject_id) DO UPDATE SET realval= cityobject_genericattrib.realval;";
+                    
+                    Statement stmtupdate = srcConn.createStatement();
+                    stmtupdate.executeUpdate(costCal);
                 }
-                
             }
         }catch (SQLException e) {
             throw new JPSRuntimeException("Error connecting to source database: " + e);
         }
         
-    }
-
-    public ResultSet getBuildingType(){
-        try (Connection srcConn = postgisClient.getConnection()) {
-            try (Statement stmt = srcConn.createStatement()) {
-                 return stmt.executeQuery(buildingTypeQuery);
-            }
-        }catch (SQLException e) {
-            throw new JPSRuntimeException("Error connecting to source database: " + e);
-        }  
     }
 }
