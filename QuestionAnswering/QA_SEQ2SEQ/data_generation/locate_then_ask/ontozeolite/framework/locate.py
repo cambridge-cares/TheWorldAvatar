7f3e39da87ef1c5269c939de3d70f812f4041db6
Dan Tran <nmdt2@cam.ac.uk>
2024-04-05 11:22:49 +0800

    dev-marie-webpage: Refactor zeolite framework locator

--- QuestionAnswering/QA_SEQ2SEQ/data_generation/locate_then_ask/ontozeolite/framework/locate.py
+++ QuestionAnswering/QA_SEQ2SEQ/data_generation/locate_then_ask/ontozeolite/framework/locate.py
@@ -1,22 +1,22 @@
 from collections import defaultdict
+from decimal import Decimal
 import random
+from typing import Dict
 
 from constants.functions import BASIC_NUM_OPS
 from constants.ontozeolite import (
-    CRYSTAL_SCALAR_KEYS,
     ZEOTOPO_ATTR_LABELS,
-    ZEOTOPO_SCALAR_KEYS,
     OZCrystalInfoAttrKey,
     OZFrameworkAttrKey,
     OZZeoTopoAttrKey,
 )
+from locate_then_ask.ontozeolite.model import OZCrystalInfo
 from utils.numerical import make_operand_and_verbn
 from locate_then_ask.query_graph import QueryGraph
 from locate_then_ask.ontozeolite.entity_store import OZEntityStore
 
 
 class OZFrameworkLocator:
-
     def __init__(self, store: OZEntityStore):
         self.store = store
 
@@ -37,72 +37,37 @@ class OZFrameworkLocator:
 
         return query_graph, verbn
 
-    def locate_concept_and_literal_multi(self, entity_iri: str, cond_num: int):
-        query_graph, concept = self.locate_concept_name(entity_iri)
-
-        entity = self.store.get_framework(entity_iri)
-
-        crystal_info_frame = list(CRYSTAL_SCALAR_KEYS)
-        if entity.crystal_info.tile_code is None:
-            crystal_info_frame.remove(OZCrystalInfoAttrKey.UNIT_CELL)
-
-        attr_key_weights = {
-            OZFrameworkAttrKey.CRYSTAL_INFO: len(crystal_info_frame),
-            OZFrameworkAttrKey.TOPO_ATTR: 10,
-        }
-        attr2freq: defaultdict[OZFrameworkAttrKey, int] = defaultdict(lambda: 0)
-        for k in random.sample(
-            tuple(attr_key_weights.keys()),
-            k=min(cond_num, len(attr_key_weights)),
-            counts=tuple(attr_key_weights.values()),
-        ):
-            attr2freq[k] += 1
-
-        crystalinfo_keys = random.sample(
-            crystal_info_frame,
-            k=attr2freq[OZFrameworkAttrKey.CRYSTAL_INFO],
-        )
-        toposcalar_keys = random.sample(
-            ZEOTOPO_SCALAR_KEYS, k=attr2freq[OZFrameworkAttrKey.TOPO_ATTR]
-        )
-        keys = crystalinfo_keys + toposcalar_keys
-        random.shuffle(keys)
-
+    def _locate_crystal_info(
+        self, query_graph: QueryGraph, crystal_info: OZCrystalInfo, freq: int
+    ):
+        keys = random.sample(tuple(OZCrystalInfoAttrKey), k=freq)
         conds = []
         for k in keys:
-            if k is OZCrystalInfoAttrKey.UNIT_CELL or isinstance(k, OZZeoTopoAttrKey):
-                if k is OZCrystalInfoAttrKey.UNIT_CELL:
-                    attr_key = "UnitCellVolume"
-                    pred = "ocr:hasCrystalInformation/ocr:hasUnitCell/ocr:hasUnitCellVolume/om:hasNumericalValue"
-                    value = entity.crystal_info.unit_cell_volume
-                    attr_verbn = "unit cell volume"
-                else:
-                    attr_key = k.value
-                    pred = "zeo:hasZeoliteTopology/zeo:has{key}/om:hasNumericalValue".format(
-                        key=attr_key
-                    )
-                    value = entity.topo_scalar[k]
-                    attr_verbn = random.choice(ZEOTOPO_ATTR_LABELS[k])
-                val_node = attr_key + "NumericalValue"
+            if k is OZCrystalInfoAttrKey.UNIT_CELL:
+                val_node = "UnitCellVolumeNumericalValue"
 
                 query_graph.add_literal_node(val_node)
-                query_graph.add_triple("Framework", pred, val_node, key=k)
+                query_graph.add_triple(
+                    "Framework",
+                    "ocr:hasCrystalInformation/ocr:hasUnitCell/ocr:hasUnitCellVolume/om:hasNumericalValue",
+                    val_node,
+                    key=k,
+                )
 
                 operator = random.choice(BASIC_NUM_OPS)
                 operand, op_verbn = make_operand_and_verbn(
                     operator,
-                    value=value,
+                    value=crystal_info.unit_cell_volume,
                     to_int=random.getrandbits(1),
                 )
                 query_graph.add_func(
                     target_node=val_node, operator=operator, operand=operand
                 )
 
-                cond = "whose {attr} is {op}".format(attr=attr_verbn, op=op_verbn)
+                cond = "whose unit cell volume is {op}".format(op=op_verbn)
             elif k is OZCrystalInfoAttrKey.TILED_STRUCTURE:
-                literal_node = query_graph.make_literal_node(
-                    entity.crystal_info.tile_code
-                )
+                assert crystal_info.tile_code is not None
+                literal_node = query_graph.make_literal_node(crystal_info.tile_code)
                 query_graph.add_triple(
                     "Framework",
                     "ocr:hasCrystalInformation/ocr:hasTiledStructure/ocr:hasTile/ocr:hasTileCode",
@@ -110,11 +75,83 @@ class OZFrameworkLocator:
                     key=k,
                 )
                 cond = "whose tile code is [{label}]".format(
-                    label=entity.crystal_info.tile_code
+                    label=crystal_info.tile_code
                 )
-            else:
-                raise Exception("Unexpected k: " + str(k))
             conds.append(cond)
+
+        return conds
+
+    def _locate_topo_attr(
+        query_graph: QueryGraph, topo_scalar: Dict[OZZeoTopoAttrKey, Decimal], freq: int
+    ):
+        conds = []
+
+        for k, value in random.sample(topo_scalar.items(), k=freq):
+            attr_key = k.value
+            pred = "zeo:hasZeoliticProperties/zeo:has{key}/om:hasNumericalValue".format(
+                key=attr_key
+            )
+            attr_verbn = random.choice(ZEOTOPO_ATTR_LABELS[k])
+            val_node = attr_key + "NumericalValue"
+
+            query_graph.add_literal_node(val_node)
+            query_graph.add_triple("Framework", pred, val_node, key=k)
+
+            operator = random.choice(BASIC_NUM_OPS)
+            operand, op_verbn = make_operand_and_verbn(
+                operator,
+                value=value,
+                to_int=random.getrandbits(1),
+            )
+            query_graph.add_func(
+                target_node=val_node, operator=operator, operand=operand
+            )
+
+            cond = "whose {attr} is {op}".format(attr=attr_verbn, op=op_verbn)
+            conds.append(cond)
+
+        return conds
+
+    def locate_concept_and_literal_multi(self, entity_iri: str, cond_num: int):
+        query_graph, concept = self.locate_concept_name(entity_iri)
+
+        entity = self.store.get_framework(entity_iri)
+
+        attr_key_counts = {
+            OZFrameworkAttrKey.FRAMEWORK_COMPONENTS: 2,
+            OZFrameworkAttrKey.CRYSTAL_INFO: 2 if entity.crystal_info.tile_code else 1,
+            OZFrameworkAttrKey.TOPO_ATTR: 10,
+            OZFrameworkAttrKey.MATERIALS: 1,
+            OZFrameworkAttrKey.GUEST_SPECIES: 5,
+            OZFrameworkAttrKey.GUEST_FORMULA: 2,
+        }
+        attr2freq: defaultdict[OZFrameworkAttrKey, int] = defaultdict(lambda: 0)
+        for k in random.sample(
+            tuple(attr_key_counts.keys()),
+            k=min(cond_num, len(attr_key_counts)),
+            counts=tuple(attr_key_counts.values()),
+        ):
+            attr2freq[k] += 1
+
+        conds = []
+        for attr, freq in attr2freq.items():
+            if attr is OZFrameworkAttrKey.FRAMEWORK_COMPONENTS:
+                pass
+            elif attr is OZFrameworkAttrKey.CRYSTAL_INFO:
+                _conds = self._locate_crystal_info(
+                    query_graph, entity.crystal_info, freq
+                )
+            elif attr is OZFrameworkAttrKey.TOPO_ATTR:
+                _conds = self._locate_topo_attr(query_graph, entity.topo_scalar, freq)
+            elif attr is OZFrameworkAttrKey.MATERIALS:
+                pass
+            elif attr is OZFrameworkAttrKey.GUEST_SPECIES:
+                pass
+            elif attr is OZFrameworkAttrKey.GUEST_FORMULA:
+                pass
+            else:
+                raise Exception("Unexpected attr: " + str(attr))
+            conds.extend(_conds)
 
         return query_graph, "{concept}, {attrs}".format(
             concept=concept, attrs=", and ".join(conds)
