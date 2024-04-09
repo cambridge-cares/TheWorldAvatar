dd30c31c0d5d9c414bc51038ec571b2e856704e7
Jiaru Bai <jb2197@cam.ac.uk>
2024-04-09 19:33:20 +0100

    dev-pydantic-rdflib: exposed additional classes to twa __init__ file.

--- JPS_BASE_LIB/python_wrapper/twa/__init__.py
+++ JPS_BASE_LIB/python_wrapper/twa/__init__.py
@@ -1,3 +1,13 @@
 from twa.JPSGateway import JPSGateway
+from twa.data_model.base_ontology import (
+    KnowledgeGraph,
+    BaseOntology,
+    BaseClass,
+    ObjectProperty,
+    DataProperty,
+)
+from twa.kg_operations import (
+    PySparqlClient,
+)
 
 __version__ = "0.0.2a0"
