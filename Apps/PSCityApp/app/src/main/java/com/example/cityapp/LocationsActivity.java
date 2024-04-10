fc6737833931a41b60e1482a63350bba424bea5f
qhouyee <quekhouyee@gmail.com>
2024-04-10 15:56:44 +0100

    dev-ps-city-app: Replace png with icons to reduce file sizes. Added some quality of life settings

--- Apps/PSCityApp/app/src/main/java/com/example/cityapp/LocationsActivity.java
+++ Apps/PSCityApp/app/src/main/java/com/example/cityapp/LocationsActivity.java
@@ -2,33 +2,32 @@ package com.example.cityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class LocationsActivity extends AppCompatActivity {
-    private ImageButton button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locations);
 
-        button1 = (ImageButton) findViewById(R.id.button1);
-        ImageButton button2 = (ImageButton) findViewById(R.id.button2);
-        ImageButton button3 = (ImageButton) findViewById(R.id.button3);
-        ImageButton button4 = (ImageButton) findViewById(R.id.button4);
+        Button parkingButton = findViewById(R.id.parking);
+        Button toiletButton = findViewById(R.id.toilet);
+        Button attractionButton = findViewById(R.id.attraction);
+        Button recyclingButton = findViewById(R.id.recycling);
+        Button waterButton = findViewById(R.id.water);
+        Button shoppingButton = findViewById(R.id.shopping);
+        Button entertainmentButton = findViewById(R.id.entertainment);
 
-        View.OnClickListener onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(view.getContext(), "This function has not implemented yet!", Toast.LENGTH_SHORT).show();
-            }
-        };
-        button1.setOnClickListener(onClickListener);
-        button2.setOnClickListener(onClickListener);
-        button3.setOnClickListener(onClickListener);
-        button4.setOnClickListener(onClickListener);
+        View.OnClickListener onClickListener = view -> Toast.makeText(view.getContext(), "This function has not been implemented yet!", Toast.LENGTH_SHORT).show();
+        parkingButton.setOnClickListener(onClickListener);
+        toiletButton.setOnClickListener(onClickListener);
+        attractionButton.setOnClickListener(onClickListener);
+        recyclingButton.setOnClickListener(onClickListener);
+        waterButton.setOnClickListener(onClickListener);
+        shoppingButton.setOnClickListener(onClickListener);
+        entertainmentButton.setOnClickListener(onClickListener);
     }
 }
--- Apps/PSCityApp/app/src/main/java/com/example/cityapp/MainActivity.java
+++ Apps/PSCityApp/app/src/main/java/com/example/cityapp/MainActivity.java
@@ -4,31 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageButton locations_btn;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        locations_btn = (ImageButton) findViewById(R.id.locations_btn);
+        Button locations_btn = findViewById(R.id.amenity);
 
-        locations_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,LocationsActivity.class);
-
-                startActivity(intent);
-            }
+        locations_btn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LocationsActivity.class);
+            startActivity(intent);
         });
     }
 
