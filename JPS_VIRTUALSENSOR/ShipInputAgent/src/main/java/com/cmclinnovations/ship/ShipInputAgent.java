a524df8fa81159701083e12b5a464ec507443149
Kok Foong Lee <kokfoong.lee@cares.cam.ac.uk>
2024-04-11 15:00:59 +0800

    dev-cleanup-dispersion-data: added a route to delete old ship data

--- JPS_VIRTUALSENSOR/ShipInputAgent/src/main/java/com/cmclinnovations/ship/ShipInputAgent.java
+++ JPS_VIRTUALSENSOR/ShipInputAgent/src/main/java/com/cmclinnovations/ship/ShipInputAgent.java
@@ -22,13 +22,15 @@ import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
 import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
 
-@WebServlet(urlPatterns = { ShipInputAgent.UPDATE_PATH, ShipInputAgent.LIVE_SERVER_PATH }, loadOnStartup = 1)
+@WebServlet(urlPatterns = { ShipInputAgent.UPDATE_PATH, ShipInputAgent.LIVE_SERVER_PATH,
+        ShipInputAgent.CLEAR_OLD_DATA_PATH }, loadOnStartup = 1)
 public class ShipInputAgent extends HttpServlet {
     private static final Logger LOGGER = LogManager.getLogger(ShipInputAgent.class);
     private QueryClient queryClient;
     private AisStreamWebsocketClient client;
     public static final String UPDATE_PATH = "/update";
     public static final String LIVE_SERVER_PATH = "/live-server";
+    public static final String CLEAR_OLD_DATA_PATH = "/clear-old-data";
     private static final String URI_STRING = "wss://stream.aisstream.io/v0/stream";
 
     @Override
@@ -99,6 +101,16 @@ public class ShipInputAgent extends HttpServlet {
                 client.close();
             } else {
                 LOGGER.info("No ongoing live updates to stop");
+            }
+        } else if (req.getServletPath().contentEquals(CLEAR_OLD_DATA_PATH)) {
+            // number of days before current time to keep
+            String daysBefore = req.getParameter("daysBefore");
+            if (daysBefore != null) {
+                List<String> shipsToDelete = queryClient.cleanUpTimeSeries(Long.parseLong(daysBefore));
+                queryClient.deleteShipDerivation(shipsToDelete);
+                queryClient.deleteShips(shipsToDelete);
+            } else {
+                throw new RuntimeException("daysBefore value is needed");
             }
         } else {
             LOGGER.warn("Unsupported path for DELETE request");
