6ae2f0e31fef31db86646e5992d6299c5b8b766d
Dan Tran <nmdt2@cam.ac.uk>
2024-04-18 09:06:14 +0800

    dev-qa-agent: Reorganise directory

--- QuestionAnswering/QA_Agent/deployment/fastapi_app/services/connectors/sg_land_lots/constants.py
+++ QuestionAnswering/QA_Agent/deployment/fastapi_app/services/connectors/sg_land_lots/constants.py
@@ -5,5 +5,5 @@ LAND_USE_TYPE_PREDICATE = "ontozoning:hasLandUseType"
 PLOT_NUM_ATTR_PREDICATES = {
     PlotNumAttrKey.GROSS_PLOT_RATIO: "^opr:appliesTo/opr:allowsGrossPlotRatio/om:hasValue",
     PlotNumAttrKey.PLOT_AREA: "ontoplot:hasPlotArea/om:hasValue",
-    PlotNumAttrKey.GROSS_FLOOR_AREA: "ontoplot:hasMaximumPermittedGPR/om:hasValue",
+    PlotNumAttrKey.GROSS_FLOOR_AREA: "ontoplot:hasMaximumPermittedGFA/om:hasValue",
 }
