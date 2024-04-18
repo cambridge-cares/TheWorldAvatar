186779e2f665e19f125990a0223a5290fb53c4a9
JYAN01 <jy470@cam.ac.uk>
2024-04-18 17:39:10 +0800

    add upload odba

--- Agents/GFAAgent/src/main/java/uk/ac/cam/cares/jps/agent/gfaagent/GFAAgent.java
+++ Agents/GFAAgent/src/main/java/uk/ac/cam/cares/jps/agent/gfaagent/GFAAgent.java
@@ -2,12 +2,14 @@ package uk.ac.cam.cares.jps.agent.gfaagent;
 
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import com.cmclinnovations.stack.clients.ontop.OntopClient;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.nio.file.Path;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,10 +17,9 @@ import javax.servlet.http.HttpServletRequest;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-@WebServlet(urlPatterns = {"/calculation", "/cost"})
+@WebServlet(urlPatterns = {"/calculation", "/calculationwithodba", "/cost", "/costwithodba"})
 public class GFAAgent extends JPSAgent{
-    
-    
+       
     private EndpointConfig endpointConfig = new EndpointConfig();
 
     private String dbName;
@@ -51,12 +52,21 @@ public class GFAAgent extends JPSAgent{
 
         try {
             GFACalculation gfaCalculation = new GFACalculation(dbUrl, dbUser, dbPassword);
-            if(requestParams.getString("requestUrl").contains("/calculation")){            
+            String paremString = requestParams.getString("requestUrl");
+            if(paremString.contains("/calculation")){            
                 //calculate GFA 1. query footpring 2. query height (if no height, estimate 3.2m/floor) 3. calculate 4. store
                 gfaCalculation.calculationGFA();
-            }else if (requestParams.getString("requestUrl").contains("/cost")){
+                if (paremString.contains("withodba")){
+                    Path obdaFile = Path.of("/resources/gfa.obda");
+                    uploadODBA(obdaFile);
+                }
+            }else if (paremString.contains("/cost")){
                 CostCalculation costCalculation = new CostCalculation(dbUrl, dbUser, dbPassword);
                 costCalculation.calculationCost();
+                if (paremString.contains("withodba")){
+                    Path obdaFile = Path.of("/resources/cost.obda");
+                    uploadODBA(obdaFile);
+                }
             }
 
 
@@ -66,5 +76,15 @@ public class GFAAgent extends JPSAgent{
         }
 
         return requestParams;
+    }
+
+    public void uploadODBA (Path obdaFile) {
+        try {
+            OntopClient ontopClient = OntopClient.getInstance();
+            ontopClient.updateOBDA(obdaFile);
+        } catch (Exception e) {
+            System.out.println("Could not retrieve isochrone .obda file.");
+        }
+
     }
 }
