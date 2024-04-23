2d396f09fb65b3ae5c48577503a8419c2fd62c4a
sandradeng20 <xsd20@cam.ac.uk>
2024-04-23 11:55:04 +0800

    1786-android-documentation: add comments

--- Apps/SampleApp/core/model/src/main/java/uk/ac/cam/cares/jps/model/Todo.java
+++ Apps/SampleApp/core/model/src/main/java/uk/ac/cam/cares/jps/model/Todo.java
@@ -1,6 +1,15 @@
 package uk.ac.cam.cares.jps.model;
 
-
+/**
+ * This is a model class which structure follows the result JSON from <a href="https://jsonplaceholder.typicode.com/todos/">...</a>{id}
+ * And here is a sample result of <a href="https://jsonplaceholder.typicode.com/todos/2">...</a>
+ * {
+ *     "userId": 1,
+ *     "id": 2,
+ *     "title": "quis ut nam facilis et officia qui",
+ *     "completed": false
+ * }
+ */
 public class Todo {
     private String userId;
     private String id;
