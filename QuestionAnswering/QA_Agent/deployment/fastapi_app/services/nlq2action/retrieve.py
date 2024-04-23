5d34f4245fa4246d76a0daba59886332c095ba2a
Dan Tran <nmdt2@cam.ac.uk>
2024-04-23 14:58:09 +0800

    dev-qa-icl: Implement example retriever

---
+++ QuestionAnswering/QA_Agent/deployment/fastapi_app/services/nlq2action/retrieve.py
@@ -0,0 +1,106 @@
+import json
+from typing import Iterable
+
+import numpy as np
+from redis import Redis
+from redis.commands.search.field import VectorField
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+from redis.commands.search.query import Query
+from pydantic.dataclasses import dataclass
+
+from services.utils.itertools_recipes import batched
+from services.core.embed import IEmbedder
+from services.core.redis import does_index_exist
+
+
+@dataclass
+class Nlq2ActionExample:
+    nlq: str
+    action: dict
+
+
+class Nlq2ActionRetriever:
+    EXAMPLES_KEY_PREFIX = "nlq2actionExamples:"
+    EXAMPLES_INDEX_NAME = "idx:nlq2actionExamples_vss"
+
+    @classmethod
+    def _insert_examples_and_create_index(
+        cls,
+        redis_client: Redis,
+        embedder: IEmbedder,
+        examples: Iterable[Nlq2ActionExample],
+    ):
+        offset = 0
+        vector_dim = None
+        for chunk in batched(examples, 10):
+            chunk = list(chunk)
+            embeddings = (
+                embedder([example.nlq for example in chunk]).astype(np.float32).tolist()
+            )
+            if vector_dim is None:
+                vector_dim = len(embeddings[0])
+
+            pipeline = redis_client.pipeline()
+            for i, (example, embedding) in enumerate(zip(chunk, embeddings)):
+                redis_key = cls.EXAMPLES_KEY_PREFIX + str(offset + i)
+                doc = dict(
+                    nlq=example.nlq,
+                    action=json.dumps(example.action),
+                    nlq_embedding=embedding,
+                )
+                pipeline.json().set(redis_key, "$", doc)
+            pipeline.execute()
+
+            offset += len(chunk)
+
+        if vector_dim is None:
+            raise ValueError(
+                "Index for natural language question-to-action examples is not found and must be created. Therefore, `examples` must not be None."
+            )
+
+        schema = (
+            VectorField(
+                "$.nlq_embedding",
+                "FLAT",
+                {"TYPE": "FLOAT32", "DIM": vector_dim, "DISTANCE_METRIC": "IP"},
+                as_name="vector",
+            ),
+        )
+        definition = IndexDefinition(
+            prefix=[cls.EXAMPLES_KEY_PREFIX], index_type=IndexType.JSON
+        )
+        redis_client.ft(cls.EXAMPLES_INDEX_NAME).create_index(
+            fields=schema, definition=definition
+        )
+
+    def __init__(
+        self,
+        redis_client: Redis,
+        embedder: IEmbedder,
+        examples: Iterable[Nlq2ActionExample],
+    ):
+        if not does_index_exist(redis_client, self.EXAMPLES_INDEX_NAME):
+            self._insert_examples_and_create_index(
+                redis_client=redis_client, embedder=embedder, examples=examples
+            )
+
+        self.redis_client = redis_client
+        self.embedder = embedder
+
+    def retrieve_examples(self, nlq: str, k: int = 5):
+        encoded_nlq = self.embedder([nlq])[0].astype(np.float32)
+        knn_query = (
+            Query("(*)=>[KNN {k} @vector $query_vector AS vector_score]".format(k=k))
+            .sort_by("vector_score")
+            .return_field("$.nlq", "nlq")
+            .return_field("$.action", "action")
+            .dialect(2)
+        )
+        res = self.redis_client.ft(self.EXAMPLES_INDEX_NAME).search(
+            knn_query, {"query_vector": encoded_nlq.tobytes()}
+        )
+
+        return [
+            Nlq2ActionExample(nlq=doc.nlq, action=json.loads(doc.action))
+            for doc in res.docs
+        ]
---
+++ QuestionAnswering/QA_Agent/deployment/fastapi_app/tests/integration/services/nlq2action/test_retrieve.py
@@ -0,0 +1,63 @@
+import pytest
+
+from services.nlq2action.retrieve import Nlq2ActionExample, Nlq2ActionRetriever
+from services.core.retrieve_docs import DocsRetriever
+
+
+@pytest.fixture(scope="class")
+def nlq2action_retriever(redis_client, embedder):
+    examples = [
+        Nlq2ActionExample(
+            nlq="Find the plots with a maximum permitted GFA greater than 500 square meters.",
+            action={
+                "sparql": {
+                    "namespace": "ontop",
+                    "query": "SELECT ?Plot ?GFAValue ?GFAUnit WHERE {\n  ?Plot rdf:type ontoplot:Plot .\n  ?Plot ontoplot:hasMaximumPermittedGFA/om:hasValue [ om:hasNumericalValue  ?GFAValue ; om:hasUnit ?GFAUnit ] .\n  FILTER (?GFAValue > 500)\n}",
+                }
+            },
+        ),
+        Nlq2ActionExample(
+            nlq="Identify residential plots with a maximum permitted GFA less than 1000 square meters.",
+            action={
+                "sparql": {
+                    "namespace": "ontop",
+                    "query": 'SELECT ?Plot ?GFAValue ?GFAUnit WHERE {\n  ?Plot rdf:type ontoplot:Plot .\n  VALUES ?LandUseType { <LandUseType:"residential"> }\n  ?Plot ontozoning:hasLandUseType ?LandUseType .\n  ?Plot ontoplot:hasMaximumPermittedGFA/om:hasValue [ om:hasNumericalValue  ?GFAValue ; om:hasUnit ?GFAUnit ] .\n  FILTER (?GFAValue > 500)\n}',
+                }
+            },
+        ),
+        Nlq2ActionExample(
+            nlq="Retrieve the top 5 plots with the largest plot area.",
+            action={
+                "sparql": {
+                    "namespace": "ontop",
+                    "query": "SELECT ?Plot ?PlotAreaValue ?PlotAreaUnit WHERE {\n    ?Plot rdf:type ontoplot:Plot .\n    ?Plot ontoplot:hasPlotArea/om:value [ om:hasNumericalValue ?PlotAreaValue ; om:hasUnit ?PlotAreaUnit ] .\n}\nORDER BY DESC(?PlotAreaValue)\nLIMIT 5",
+                }
+            },
+        ),
+    ]
+
+    yield Nlq2ActionRetriever(
+        redis_client=redis_client, embedder=embedder, examples=iter(examples)
+    )
+
+
+class TestNlq2ActionRetriever:
+    def test_retrieveExamples(self, nlq2action_retriever: Nlq2ActionRetriever):
+        # Arrange
+        nlq = "Identify residential plots with a maximum permitted GFA more than 700 square meters."
+
+        # Act
+        actual = nlq2action_retriever.retrieve_examples(nlq, k=1)
+
+        # Assert
+        actual == [
+            Nlq2ActionExample(
+                nlq="Identify residential plots with a maximum permitted GFA less than 1000 square meters.",
+                action={
+                    "sparql": {
+                        "namespace": "ontop",
+                        "query": 'SELECT ?Plot ?GFAValue ?GFAUnit WHERE {\n  ?Plot rdf:type ontoplot:Plot .\n  VALUES ?LandUseType { <LandUseType:"residential"> }\n  ?Plot ontozoning:hasLandUseType ?LandUseType .\n  ?Plot ontoplot:hasMaximumPermittedGFA/om:hasValue [ om:hasNumericalValue  ?GFAValue ; om:hasUnit ?GFAUnit ] .\n  FILTER (?GFAValue > 500)\n}',
+                    }
+                },
+            )
+        ]
