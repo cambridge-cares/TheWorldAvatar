f697b6308d8579064b69a712c7102f2dec4b545d
Oisín <opeppard@cmcl.io>
2024-04-23 15:20:49 +0100

    update snippets

--- web/twa-vis-framework/library/src/snippets/plotData.js
+++ web/twa-vis-framework/library/src/snippets/plotData.js
@@ -1,17 +1,15 @@
+// Once the underlying style has loaded...
 MapHandler.MAP.on("style.load", function () {
     // Load registered images and linked files
     manager.loadImagesAndLinks().then(() => {
+        buildLegend();
+        buildTimeControls();
+
         // Check if the settings contains a scenarios endpoint
         let scenariosEnabled = manager.checkForScenarios();
+
         if (scenariosEnabled) {
             manager.showScenarioSelector();
-            // Poll until selectScenarioResolved becomes true
-            const intervalId = setInterval(() => {
-                if (manager.scenarioHandler.selectScenarioResolved) {
-                    clearInterval(intervalId); // Stop polling
-                    populateSlider(manager.scenarioHandler.agentBaseURL, manager.scenarioHandler.selectedScenario);
-                }
-            }, 100);
         } else {
             // Plot the default visible data
             manager.plotData();
