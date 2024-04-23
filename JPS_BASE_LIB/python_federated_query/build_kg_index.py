ff9d80212189c13d657cf38b74237593a030b3df
Md Hanif Seddiqui <mhs62@cam.ac.uk>
2024-04-23 17:29:22 +0100

    Create build_kg_index.py
    inverted index: class to files

---
+++ JPS_BASE_LIB/python_federated_query/build_kg_index.py
@@ -0,0 +1,89 @@
+from rdflib import Graph
+import os
+import json
+
+class BuildKGIndex:
+  
+  def __init__(self, directory):
+    self.filecount=0
+    self.triplecount=0
+    self.class_index = {}
+    self.directory=directory
+  
+  def start_processing(self):
+    for root, dirs, files in os.walk(self.directory):
+      for filename in files:
+          if filename.endswith(".owl"):  # Assuming files are owl files
+              file_path = os.path.join(root, filename)
+              self.filecount+=1
+              print(f"{self.filecount}. Processing: {file_path}")
+              classes=self.extract_concepts_from_owl_file(file_path)
+              self.build_inverted_index(classes,file_path)
+    return self.class_index
+  
+  def extract_concepts_from_owl_file(self,owl_file):
+    graph = Graph()
+    graph.parse(owl_file, format="xml")  # Adjust format if needed
+    rdf_type = '#type'
+    triple_counter = 0
+    classes = set()
+    # Extract classes (concepts)
+    for subject, predicate, obj in graph: #triples in graph 
+        if predicate.endswith(rdf_type) and not obj.endswith(rdf_type):
+            classes.add(obj)
+            triple_counter+=1
+    print(f"Processed {triple_counter} triples from file {owl_file}")
+    self.triplecount+=triple_counter
+    return classes
+  
+  def build_inverted_index(self,classes,filename):
+    # Iterate over each file in the directory
+    for class_name in classes:
+        if class_name not in self.class_index:
+            self.class_index[class_name] = []
+        self.class_index[class_name].append(filename)
+  
+  def save_inverted_index(self, inverted_index, file_path):
+    with open(file_path, 'w') as f:
+        json.dump(inverted_index, f, indent=4)
+        
+  def get_triplecount(self):
+    return self.triplecount
+    
+# usage
+if __name__ == "__main__":
+  directory = "C:/Users/printer_admin/Downloads/KGs/"
+  kgs = BuildKGIndex(directory)
+  kg_index=kgs.start_processing()
+  kgs.save_inverted_index(kg_index, os.path.join(directory, 'inverted_index.json'))
+  total_triple_processed=kgs.get_triplecount()
+  print(f"Completed. Total Number of Processed Triples: {total_triple_processed}")
+
+  # for class_name, related_files in kg_index.items():
+  #   print(f"Class: {class_name}")
+  #   print(f"Related Files: {related_files}")
+  #   print()
+    
+  # concepts=kg_index.extract_concepts_from_kg()
+  # concept_counter = 0
+  # for concept_uri in concepts:
+  #   print(concept_uri)
+  #   concept_counter+=1
+  # print(f"Total Number of Concepts {concept_counter}")
+    
+  # # Complex SPARQL example (replace with your actual query)
+  # sparql_query = """
+  # SELECT ?subject 
+  # WHERE {
+  #   ?subject rdf:type <http://purl.org/gc/Atom> .
+  # }
+  # """
+
+  # # Get results in list format
+  # results = kg.get_results(sparql_query)
+  
+  # if results:
+  #   for result in results:
+  #     print(result)
+  # else:
+  #   print("No results found for the SPARQL query.")
