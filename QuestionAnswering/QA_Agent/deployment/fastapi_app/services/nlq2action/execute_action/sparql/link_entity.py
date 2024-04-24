59f57f7d879418319f8016dbb3f5c1853381eb81
Dan Tran <nmdt2@cam.ac.uk>
2024-04-24 14:34:38 +0800

    dev-qa-icl: Handle whitespace in entity linking procedure

---
+++ QuestionAnswering/QA_Agent/deployment/fastapi_app/services/nlq2action/execute_action/sparql/link_entity.py
@@ -0,0 +1,44 @@
+from functools import cache
+import logging
+from typing import Annotated
+
+from fastapi import Depends
+
+from services.nlq2action.link_entity import ELMediator, get_elMediator
+
+
+logger = logging.getLogger(__name__)
+
+
+class SparqlEntityLinker:
+    def __init__(self, el_mediator: ELMediator):
+        self.el_mediator = el_mediator
+
+    def link(self, token: str):
+        # '<LandUseType:\"residential\">' -> ['<https://example.org/LandUseType_1>', '<https://example.org/LandUseType_2>']
+        if not token.startswith("<") or not token.endswith(">"):
+            return [token]
+
+        try:
+            entity_type, surface_form = token[1:-1].split(":", maxsplit=1)
+        except:
+            return [token]
+
+        if not surface_form.startswith('"') or not surface_form.endswith('"'):
+            return [token]
+        surface_form = surface_form[1:-1]
+
+        try:
+            iris = self.el_mediator.link(entity_type, surface_form)
+        except Exception as e:
+            logger.error("Error during entity linking: " + str(e))
+            return [token]
+
+        return ["<{iri}>".format(iri=iri) for iri in iris]
+
+
+@cache
+def get_sparql_entityLinker(
+    el_mediator: Annotated[ELMediator, Depends(get_elMediator)]
+):
+    return SparqlEntityLinker(el_mediator)
