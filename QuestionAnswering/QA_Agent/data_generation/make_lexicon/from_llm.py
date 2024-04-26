3caecb8a7e335d8510e463f7174a92a18bfadd8a
Dan Tran <nmdt2@cam.ac.uk>
2024-04-26 18:00:31 +0800

    dev-qa-icl: Update logic to retrieve literal data and LLM generation of surface forms

--- QuestionAnswering/QA_Agent/data_generation/make_lexicon/from_llm.py
+++ QuestionAnswering/QA_Agent/data_generation/make_lexicon/from_llm.py
@@ -5,15 +5,18 @@ import os
 from openai import OpenAI
 
 
-PROMPT_TEMPLATE = """Below are some entity and relationship data:
-{data}
+PROMPT_TEMPLATE = """Here are information on entity classes:
+{class_data}
 
-Your task is to generate {k} surface forms for each entity and relationship. Do not provide any explanation and please formulate your response as a list of objects with keys "iri" and "surface_forms", in the JSON format exactly."""
+Below are some entity instances:
+{instance_data}
+
+Your task is to generate {k} surface forms for each entity instance to train an entity linking model. Do not provide any explanation and please formulate your response as a list of objects with keys "iri" and "surface_forms", in the JSON format exactly."""
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
-        "filename", help="Path to JSON file containing IRI and any associated data"
+        "filename", help="Path to JSON file containing class and instance data"
     )
     parser.add_argument(
         "--chunk_size",
@@ -35,6 +38,9 @@ if __name__ == "__main__":
     with open(args.filename, "r") as f:
         data = json.load(f)
 
+    class_data = data["classes"]
+    instance_data = data["instances"]
+
     # If output JSONL file exists, filter input for those that have not been processed
     out_filename = args.out.rsplit(".", maxsplit=1)[0]
     out_jsonl = out_filename + ".jsonl"
@@ -42,17 +48,21 @@ if __name__ == "__main__":
         with open(out_jsonl, "r") as f:
             iris = [json.loads(line)["iri"] for line in f.read().splitlines()]
         iris = set(iris)
-        data = [datum for datum in data if datum["iri"] not in iris]
+        instance_data = [datum for datum in instance_data if datum["iri"] not in iris]
 
-    if data:
-        iri2label = {datum["iri"]: datum["data"]["label"] for datum in data}
+    if instance_data:
+        iri2label = {
+            datum["iri"]: datum["attributes"]["label"] for datum in instance_data
+        }
         openai_client = OpenAI(base_url=args.openai_base_url)
 
-        with open(args.out, "a+") as f:
+        with open(out_jsonl, "a+") as f:
             for i in range(0, len(data), args.chunk_size):
-                chunk = data[i : i + args.chunk_size]
+                chunk = instance_data[i : i + args.chunk_size]
                 prompt = PROMPT_TEMPLATE.format(
-                    data=json.dumps(chunk, indent=4), k=args.surface_form_num
+                    class_data=json.dumps(class_data, indent=4),
+                    instance_data=json.dumps(chunk, indent=4),
+                    k=args.surface_form_num,
                 )
                 res = openai_client.chat.completions.create(
                     model=args.model, messages=[{"role": "user", "content": prompt}]
@@ -70,6 +80,6 @@ if __name__ == "__main__":
 
     # Convert JSONL to JSON
     with open(out_jsonl, "r") as f:
-        data = [json.loads(line) for line in f.read().splitlines()]
+        instance_data = [json.loads(line) for line in f.read().splitlines()]
     with open(args.out, "w") as f:
-        json.dump(data, f, indent=4)
+        json.dump(instance_data, f, indent=4)
