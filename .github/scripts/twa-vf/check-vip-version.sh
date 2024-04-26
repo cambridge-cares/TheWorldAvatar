f38f7661f5632b696906ff3e1e71e37ca3c86d1a
qhouyee <quekhouyee@gmail.com>
2024-04-26 11:30:50 +0100

    dev-vis-ux-enhancements: Split check version workflows

--- .github/scripts/twa-vf/check-vip-version.sh
+++ .github/scripts/twa-vf/check-vip-version.sh
@@ -8,25 +8,25 @@
 
 
 # Read (what should be) the correct version
-VERSION=$(cat -s "./web/twa-vis-framework/library/VERSION" 2>/dev/null)
+VERSION=$(cat -s "./web/twa-vis-platform/resources/VERSION" 2>/dev/null)
 if [ "$VERSION" == "" ]; then
-	echo "The VERSION file of the TWA-VF has no content!"
+	echo "The VERSION file of the TWA-ViP has no content!"
 	exit 1
 fi
-echo "TWA-VF defined in file as: $VERSION"
+echo "TWA-ViP defined in file as: $VERSION"
 
 # Get the VERSION file from the main branch of the repo, check that this new version is updated ie does not match
-MAIN_VERSION=$(curl -s "https://raw.githubusercontent.com/cambridge-cares/TheWorldAvatar/main/web/twa-vis-framework/library/VERSION")
+MAIN_VERSION=$(curl -s "https://raw.githubusercontent.com/cambridge-cares/TheWorldAvatar/main/web/twa-vis-platform/resources/VERSION")
 if [ "$VERSION" == "$MAIN_VERSION" ]; then
-	echo "The TWA-VF VERSION file on this branch match that on the main branch! Update the VERSION file before merging."
+	echo "The TWA-ViP VERSION file on this branch match that on the main branch! Update the VERSION file before merging."
 	exit 1
 fi
-echo "The updated version of the TWA-VF is: $MAIN_VERSION"
+echo "The updated version of the TWA-ViP is: $MAIN_VERSION"
 
 # Check that there's no -SNAPSHOT qualifier
 TOKEN="-SNAPSHOT"
 if [[ "$VERSION" == *"$TOKEN"* ]]; then
-  echo "Remove the -SNAPSHOT qualifier in TWA-VF version file! "
+  echo "Remove the -SNAPSHOT qualifier in TWA-ViP version file! "
   exit 1
 fi
 
@@ -38,6 +38,13 @@ if ! grep -q "$TOKEN" "$CHANGELOG"; then
 	exit 1
 fi
 
-echo "CHANGELOG is updated for the VF."
+CHANGELOG="./web/twa-vis-platform/resources/CHANGELOG.md"
+TOKEN="# $VERSION"
+if ! grep -q "$TOKEN" "$CHANGELOG"; then
+	echo "Could not find corresponding node for the updated TWA-ViP in CHANGELOG.md file!"
+	exit 1
+fi
+
+echo "CHANGELOG is updated for the ViP."
 
 exit 0
