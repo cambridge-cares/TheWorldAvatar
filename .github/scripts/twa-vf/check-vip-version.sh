1828e0754964cd0011466b7f4ed7c5ecc134d6d2
qhouyee <quekhouyee@gmail.com>
2024-04-29 13:44:14 +0100

    dev-vis-ux-enhancements: Update workflow and script

--- .github/scripts/twa-vf/check-vip-version.sh
+++ .github/scripts/twa-vf/check-vip-version.sh
@@ -10,7 +10,7 @@
 # Read (what should be) the correct version
 VERSION=$(cat -s "./web/twa-vis-platform/resources/VERSION" 2>/dev/null)
 if [ "$VERSION" == "" ]; then
-	echo "The VERSION file of the TWA-ViP has no content!"
+	echo "Error: The VERSION file of the TWA-ViP is empty. Please ensure the correct version number has been added."
 	exit 1
 fi
 echo "TWA-ViP defined in file as: $VERSION"
@@ -18,7 +18,7 @@ echo "TWA-ViP defined in file as: $VERSION"
 # Get the VERSION file from the main branch of the repo, check that this new version is updated ie does not match
 MAIN_VERSION=$(curl -s "https://raw.githubusercontent.com/cambridge-cares/TheWorldAvatar/main/web/twa-vis-platform/resources/VERSION")
 if [ "$VERSION" == "$MAIN_VERSION" ]; then
-	echo "The TWA-ViP VERSION file on this branch match that on the main branch! Update the VERSION file before merging."
+	echo "Error: The TWA-ViP VERSION file on this branch matches that on the main branch! Update the VERSION file before merging."
 	exit 1
 fi
 echo "The updated version of the TWA-ViP is: $MAIN_VERSION"
@@ -26,25 +26,18 @@ echo "The updated version of the TWA-ViP is: $MAIN_VERSION"
 # Check that there's no -SNAPSHOT qualifier
 TOKEN="-SNAPSHOT"
 if [[ "$VERSION" == *"$TOKEN"* ]]; then
-  echo "Remove the -SNAPSHOT qualifier in TWA-ViP version file! "
+  echo "Error: Remove the -SNAPSHOT qualifier in TWA-ViP version file! "
   exit 1
 fi
 
 # Check that the change log contains an entry for the updated versions
-CHANGELOG="./web/twa-vis-framework/library/CHANGELOG.md"
-TOKEN="# $VERSION"
-if ! grep -q "$TOKEN" "$CHANGELOG"; then
-	echo "Could not find corresponding node for the updated TWA-VF in CHANGELOG.md file!"
-	exit 1
-fi
-
 CHANGELOG="./web/twa-vis-platform/resources/CHANGELOG.md"
 TOKEN="# $VERSION"
 if ! grep -q "$TOKEN" "$CHANGELOG"; then
-	echo "Could not find corresponding node for the updated TWA-ViP in CHANGELOG.md file!"
+	echo "Error: Could not find corresponding node for the updated TWA-ViP in CHANGELOG.md file!"
 	exit 1
 fi
 
-echo "CHANGELOG is updated for the ViP."
+echo "All checks have passed successfully. The ViP has been updated correctly."
 
 exit 0
