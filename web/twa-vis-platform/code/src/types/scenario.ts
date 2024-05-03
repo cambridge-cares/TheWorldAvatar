ee08208af53809e9d13a437362dff1dbd1e8f804
Oisín <opeppard@cmcl.io>
2024-05-03 09:42:36 +0100

    time series types

--- web/twa-vis-platform/code/src/types/scenario.ts
+++ web/twa-vis-platform/code/src/types/scenario.ts
@@ -1,7 +1,10 @@
+import { ScenarioDimensionsData } from "./timeseries";
+
 export type ScenarioDefinition = {
   id: string;
   name: string;
   description: string;
   url?: string;
   dataset?: string;
+  dimensions: ScenarioDimensionsData
 };
