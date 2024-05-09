0d28c395459384a94c438cb5c3f7def24be9fe3d
jc2341 <jc2341@cam.ac.uk>
2024-05-09 17:06:02 +0000

    dev-FenlandTrajectoryAgent: An exception is now thrown if import fails in wsgi.py.

--- Agents/FenlandTrajectoryAgent/agent/flaskapp/wsgi.py
+++ Agents/FenlandTrajectoryAgent/agent/flaskapp/wsgi.py
@@ -5,16 +5,17 @@
 
 # Start Web Server Gateway Interface (WSGI) as a simple web server to forward
 # requests to the actual application and initialise recurring tasks
+try:
+    import logging
+    logging.getLogger("py4j").setLevel(logging.INFO)
 
-import logging
-logging.getLogger("py4j").setLevel(logging.INFO)
-
-from pytz import utc
-from apscheduler.schedulers.background import BackgroundScheduler
-from agent.flaskapp import create_app
-# Adjust the import path based on your project structure
-from agent.datainstantiation.GPS_data_instantiation import main as instantiate_gps_data
-
+    from pytz import utc
+    from apscheduler.schedulers.background import BackgroundScheduler
+    from agent.flaskapp import create_app
+    # Adjust the import path based on your project structure
+    from agent.datainstantiation.GPS_data_instantiation import main as instantiate_gps_data
+except:
+    raise Exception("Import error in wsgi.py.")
 # Initialise background scheduler and add a recurring background task
 # Here, you can define how often you want your data instantiation process to run
 # For example, setting it to run daily at midnight
