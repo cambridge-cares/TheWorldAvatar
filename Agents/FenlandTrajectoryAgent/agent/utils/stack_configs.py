9c0c316f55805779b906ee9b35dfed38dd856b18
jc2341 <jc2341@cam.ac.uk>
2024-05-09 17:04:46 +0000

    dev-FenlandTrajectoryAgent: An exception is now thrown if retrieving parameters from stack clients failed.

--- Agents/FenlandTrajectoryAgent/agent/utils/stack_configs.py
+++ Agents/FenlandTrajectoryAgent/agent/utils/stack_configs.py
@@ -24,34 +24,35 @@ def retrieve_stack_settings():
     # Import stack gateway module only when needed to avoid import issues/
     # potentially unnecessary installation of py4jps StackClients resource
     from agent.utils.stack_gateway import stackClientsGw
+    try:
+        # Create module views to relevant Stack clients
+        stackClientsView = stackClientsGw.createModuleView()
+        stackClientsGw.importPackages(stackClientsView, "com.cmclinnovations.stack.clients.docker.ContainerClient")
+        stackClientsGw.importPackages(stackClientsView, "com.cmclinnovations.stack.clients.blazegraph.BlazegraphEndpointConfig")
+        stackClientsGw.importPackages(stackClientsView, "com.cmclinnovations.stack.clients.postgis.PostGISEndpointConfig")
 
-    # Create module views to relevant Stack clients
-    stackClientsView = stackClientsGw.createModuleView()
-    stackClientsGw.importPackages(stackClientsView, "com.cmclinnovations.stack.clients.docker.ContainerClient")
-    stackClientsGw.importPackages(stackClientsView, "com.cmclinnovations.stack.clients.blazegraph.BlazegraphEndpointConfig")
-    stackClientsGw.importPackages(stackClientsView, "com.cmclinnovations.stack.clients.postgis.PostGISEndpointConfig")
+        # Retrieve endpoint configurations from Stack clients
+        containerClient = stackClientsView.ContainerClient()
+        # Blazegraph
+        bg = stackClientsView.BlazegraphEndpointConfig("","","","","")
+        bg_conf = containerClient.readEndpointConfig("blazegraph", bg.getClass())
+        # PostgreSQL/PostGIS
+        pg = stackClientsView.PostGISEndpointConfig("","","","","")
+        pg_conf = containerClient.readEndpointConfig("postgis", pg.getClass())
 
-    # Retrieve endpoint configurations from Stack clients
-    containerClient = stackClientsView.ContainerClient()
-    # Blazegraph
-    bg = stackClientsView.BlazegraphEndpointConfig("","","","","")
-    bg_conf = containerClient.readEndpointConfig("blazegraph", bg.getClass())
-    # PostgreSQL/PostGIS
-    pg = stackClientsView.PostGISEndpointConfig("","","","","")
-    pg_conf = containerClient.readEndpointConfig("postgis", pg.getClass())
+        # Extract PostgreSQL database URL
+        DB_URL = pg_conf.getJdbcURL(DATABASE_NAME)
+        # Extract PostgreSQL database username and password
+        DB_USER = pg_conf.getUsername()
+        DB_PASSWORD = pg_conf.getPassword()
 
-    # Extract PostgreSQL database URL
-    DB_URL = pg_conf.getJdbcURL(DATABASE_NAME)
-    # Extract PostgreSQL database username and password
-    DB_USER = pg_conf.getUsername()
-    DB_PASSWORD = pg_conf.getPassword()
+        # Extract SPARQL endpoints of KG
+        # (i.e. Query and Update endpoints are equivalent for Blazegraph)
+        SPARQL_QUERY_ENDPOINT = bg_conf.getUrl(NAMESPACE)
+        SPARQL_UPDATE_ENDPOINT = SPARQL_QUERY_ENDPOINT
 
-    # Extract SPARQL endpoints of KG 
-    # (i.e. Query and Update endpoints are equivalent for Blazegraph)
-    SPARQL_QUERY_ENDPOINT = bg_conf.getUrl(NAMESPACE)
-    SPARQL_UPDATE_ENDPOINT = SPARQL_QUERY_ENDPOINT
-
-    return DB_URL, DB_USER, DB_PASSWORD, SPARQL_QUERY_ENDPOINT, SPARQL_UPDATE_ENDPOINT
-
+        return DB_URL, DB_USER, DB_PASSWORD, SPARQL_QUERY_ENDPOINT, SPARQL_UPDATE_ENDPOINT
+    except:
+        raise Exception("Stack client parameter extraction error.")
 
 DB_URL, DB_USER, DB_PASSWORD, SPARQL_QUERY_ENDPOINT, SPARQL_UPDATE_ENDPOINT = retrieve_stack_settings()
