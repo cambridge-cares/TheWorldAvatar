a8476287f3d387434ccfd24c6e624d4a641b1170
Dan Tran <nmdt2@cam.ac.uk>
2024-05-10 15:55:45 +0800

    dev-marie-webpage: Update postprocessing step for chemcal class linkng

--- QuestionAnswering/QA_SEQ2SEQ/deployment/fastapi_app/services/translate/data_processing/ontospecies/compact2verbose.py
+++ QuestionAnswering/QA_SEQ2SEQ/deployment/fastapi_app/services/translate/data_processing/ontospecies/compact2verbose.py
@@ -270,11 +270,11 @@ class OSSparqlCompact2VerboseConverter:
             return None
 
         """
-        ?Species (a|!a)+ [ rdf:type os:ChemicalClass ; rdfs:label {label} ] .
+        ?Species (a|!a)*/(a|!a)/(a|!a)* [ rdf:type os:ChemicalClass ; rdfs:label {label} ] .
         """
         return TriplePattern.from_triple(
             "?Species",
-            "(a|!a)+",
+            "(a|!a)*/(a|!a)/(a|!a)*",
             "[ rdf:type os:ChemicalClass ; rdfs:label {label} ]".format(label=obj),
         )
 
