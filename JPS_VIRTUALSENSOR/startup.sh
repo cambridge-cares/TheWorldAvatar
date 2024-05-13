5c7c910d5bf38c266923084baae62aadf8d6e79a
ChungTingLao <chung.ting.lao.34@gmail.com>
2024-05-13 15:19:24 +0100

    dev-ship-demo: increase nginx timeout limit for ship stack.

--- JPS_VIRTUALSENSOR/startup.sh
+++ JPS_VIRTUALSENSOR/startup.sh
@@ -2,7 +2,11 @@
 
 DEFAULT_PORT=4242
 
+DEFAULT_TIMEOUT="900s"
+
 PORT="${1:-$DEFAULT_PORT}"
+
+TIMEOUT="${2:-$DEFAULT_TIMEOUT}"
 
 file_path="stack-manager/inputs/data/visualisation/data.json"
 
@@ -16,7 +20,17 @@ sed -i "s|$search_string|$replace_string|g" "$file_path"
 
 # starts up all required components of virtual sensor
 (cd stack-manager && ./stack.sh start ship-stack "$PORT")
+
 # copy required files into containers
 (cd ShipInputAgent && ./copy_ship_file.sh)
+
+# overwrite nginx timeout
+
+for container_name in $(docker ps --format "{{.Names}}" --filter name="ship-stack-nginx"); do
+	docker exec $container_name sed -i "s/300s/${TIMEOUT}/g" /etc/nginx/conf.d/default.conf
+	docker exec $container_name nginx -t 
+	docker exec $container_name nginx -s reload
+done
+
 ./copy_tbox.sh
 (cd stack-data-uploader && ./stack.sh start ship-stack)
