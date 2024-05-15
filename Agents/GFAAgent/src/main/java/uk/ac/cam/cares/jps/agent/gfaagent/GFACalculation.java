a0d4f8b56f1c0284c445e074719a440db78a0e73
JYAN01 <jy470@cam.ac.uk>
2024-05-15 17:17:12 +0800

    fix calculation GFA sql bug

--- Agents/GFAAgent/src/main/java/uk/ac/cam/cares/jps/agent/gfaagent/GFACalculation.java
+++ Agents/GFAAgent/src/main/java/uk/ac/cam/cares/jps/agent/gfaagent/GFACalculation.java
@@ -30,13 +30,13 @@ public class GFACalculation {
     /**********************************************/ 
 
     public void calculationGFA(){
-        String gfaSQL = "INSERT INTO cityobject_genericattrib (attrname, realval, cityobject_id)\n" +
+        String gfaSQL = "INSERT INTO citydb.cityobject_genericattrib (attrname, realval, cityobject_id)\n" +
         "SELECT DISTINCT ON (attrname, cityobject_id) *\n" +
             "FROM  (\n" +
             "SELECT 'GFA',\n" +
             "(CASE WHEN building.storeys_above_ground IS NOT NULL THEN (public.ST_Area(surface_geometry.geometry)*building.storeys_above_ground)\n" +
                 "ELSE (building.measured_height/3.2*public.ST_Area(surface_geometry.geometry)) END) AS realval, building.id\n" +
-            "FROM building, surface_geometry\n" +
+            "FROM citydb.building, citydb.surface_geometry\n" +
             "WHERE building.lod0_footprint_id = surface_geometry.parent_id\n" +
             ") AS cg(attrname, realval, cityobject_id)\n" +
         "ON CONFLICT (attrname, cityobject_id) DO UPDATE SET realval= cityobject_genericattrib.realval;";
