176688785bc60aa4446a6f1059745eee985f0d09
Dan Tran <nmdt2@cam.ac.uk>
2024-05-20 15:35:28 +0800

    dev-qa-icl: Update lexicon for ship

--- QuestionAnswering/QA_Agent/data_generation/csv2json.py
+++ QuestionAnswering/QA_Agent/data_generation/csv2json.py
@@ -7,11 +7,16 @@ import pandas as pd
 def csvrow2jsonobj(row: pd.Series):
     output = dict()
     if row["target"] == "sparql":
-        bindings = (
-            json.loads(row["sparql_bindings"])
-            if not pd.isna(row["sparql_bindings"])
-            else None
-        )
+        try:
+            bindings = (
+                json.loads(row["sparql_bindings"])
+                if not pd.isna(row["sparql_bindings"])
+                else None
+            )
+        except Exception as e:
+            print(row["sparql_bindings"])
+            raise e
+
         output["sparql"] = {
             k: v
             for k, v in {
@@ -22,7 +27,10 @@ def csvrow2jsonobj(row: pd.Series):
             if v
         }
     elif row["target"] == "func":
-        output["func"] = {"name": row["func_name"], "args": json.loads(row["func_args"])}
+        output["func"] = {
+            "name": row["func_name"],
+            "args": json.loads(row["func_args"]),
+        }
     else:
         raise ValueError(f'Unexpected target: {row["target"]}')
     return dict(input=row["question"], output=output)
@@ -33,7 +41,7 @@ if __name__ == "__main__":
     parser.add_argument("filename", help="CSV file containing examples")
     args = parser.parse_args()
 
-    df = pd.read_csv(args.filename)
+    df = pd.read_csv(args.filename, encoding="ISO-8859-1")
     examples = df.apply(csvrow2jsonobj, axis=1).to_list()
     with open(args.filename.rsplit(".", maxsplit=1)[0] + ".json", "w") as f:
         json.dump(examples, f, indent=4)
