bbf52632cbf8c35ebf1a29a1e6b8da97bfad069d
Dan Tran <nmdt2@cam.ac.uk>
2024-05-21 13:36:33 +0800

    dev-qa-icl: Add species' IUPAC name to SPARQL response

--- QuestionAnswering/QA_Agent/deployment/fastapi_app/controllers/qa/execute_action/sparql/process_response.py
+++ QuestionAnswering/QA_Agent/deployment/fastapi_app/controllers/qa/execute_action/sparql/process_response.py
@@ -4,6 +4,7 @@ from typing import Annotated, Dict, List, Union
 from fastapi import Depends
 
 from services.entity_store import EntityStore, get_entity_store
+from services.entity_store.species import SpeciesStore, get_species_store
 from services.wkt import CRS84_URI, WKTTextSRS
 from controllers.qa.model import DataItem, TableDataItem, WktCrs84DataItem
 
@@ -12,43 +13,13 @@ class SparqlResponseProcessor:
     TWA_PREFIXES = [
         "http://www.theworldavatar.com/kg/",
         "https://www.theworldavatar.com/kg/",
-        "http://www.theworldavatar.com/kb/"
+        "http://www.theworldavatar.com/kb/",
     ]
     WKT_LITERAL_PREFIX = "<http://www.opengis.net/def/crs/OGC/1.3/CRS84> "
 
-    def __init__(self, entity_store: EntityStore):
+    def __init__(self, entity_store: EntityStore, species_store: SpeciesStore):
         self.entity_store = entity_store
-
-    def add_labels(self, items: List[DataItem]):
-        for item in items:
-            if not isinstance(item, TableDataItem):
-                continue
-
-            vars_set = set(item.vars)
-
-            for binding in item.bindings:
-                new_kvs = dict()
-                for k, v in binding.items():
-                    if not isinstance(v, str) or not any(
-                        v.startswith(prefix) for prefix in self.TWA_PREFIXES
-                    ):
-                        continue
-
-                    label_key = k + "Label"
-                    if label_key in binding.keys():
-                        continue
-
-                    label = self.entity_store.lookup_label(v)
-                    if not label:
-                        continue
-
-                    if label_key not in vars_set:
-                        idx = item.vars.index(k)
-                        item.vars.insert(idx + 1, label_key)
-                        vars_set.add(label_key)
-                    new_kvs[label_key] = label
-
-                binding.update(new_kvs)
+        self.species_store = species_store
 
     def extract_wkt(self, vars: List[str], bindings: List[Dict[str, Dict[str, str]]]):
         wkt_vars = [
@@ -95,14 +66,78 @@ class SparqlResponseProcessor:
 
         return items
 
+    def add_labels(self, item: TableDataItem):
+        vars_set = set(item.vars)
+
+        for binding in item.bindings:
+            new_kvs = dict()
+            for k, v in binding.items():
+                if not isinstance(v, str) or not any(
+                    v.startswith(prefix) for prefix in self.TWA_PREFIXES
+                ):
+                    continue
+
+                label_key = k + "Label"
+                if label_key in binding.keys():
+                    continue
+
+                label = self.entity_store.lookup_label(v)
+                if not label:
+                    continue
+
+                if label_key not in vars_set:
+                    idx = item.vars.index(k)
+                    item.vars.insert(idx + 1, label_key)
+                    vars_set.add(label_key)
+                new_kvs[label_key] = label
+
+            binding.update(new_kvs)
+
+    def add_iupac_names(self, item: TableDataItem):
+        vars_set = set(item.vars)
+
+        for binding in item.bindings:
+            new_kvs = dict()
+            for k, v in binding.items():
+                if (
+                    not isinstance(v, str)
+                    or not any(v.startswith(prefix) for prefix in self.TWA_PREFIXES)
+                    or self.entity_store.lookup_clsname(v) != "Species"
+                ):
+                    continue
+
+                iupac_key = k + "IUPACName"
+                if iupac_key in binding.keys():
+                    continue
+
+                name = self.species_store.lookup_iupac_name(v)
+                if not name:
+                    continue
+
+                if iupac_key not in vars_set:
+                    idx = item.vars.index(k)
+                    item.vars.insert(idx + 1, iupac_key)
+                    vars_set.add(iupac_key)
+                new_kvs[iupac_key] = name
+
+            binding.update(new_kvs)
+
     def process(self, vars: List[str], bindings: List[Dict[str, Dict[str, str]]]):
         items = self.extract_wkt(vars, bindings)
-        self.add_labels(items)
+
+        for item in items:
+            if isinstance(item, DataItem):
+                self.add_labels(item)
+                self.add_iupac_names(item)
+
         return items
 
 
 @cache
 def get_sparqlRes_processor(
-    entity_store: Annotated[EntityStore, Depends(get_entity_store)]
+    entity_store: Annotated[EntityStore, Depends(get_entity_store)],
+    species_store: Annotated[SpeciesStore, Depends(get_species_store)],
 ):
-    return SparqlResponseProcessor(entity_store=entity_store)
+    return SparqlResponseProcessor(
+        entity_store=entity_store, species_store=species_store
+    )
