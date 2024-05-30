b1959e7b1919574c602c5a98043844145177f3a1
Dan Tran <nmdt2@cam.ac.uk>
2024-05-30 18:06:21 +0800

    dev-qa-icl: Display data table

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/lib/model.ts
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/lib/model.ts
@@ -1,29 +1,35 @@
+export interface TableDataItemHeader {
+  value: string
+  label: string
+  dtype: "string" | "number" | "object"
+}
+
 export interface TableDataItem {
-  type: "table",
-  vars: string[],
-  bindings: object[]
+  type: "table"
+  headers: TableDataItemHeader[]
+  data: { [key: string]: string | number | object }
 }
 
 export interface TypedSeries {
-  data: any[],
+  data: any[]
   type: string
 }
 
 export interface ScatterPlotTrace {
-  name?: string,
-  x: TypedSeries,
+  name?: string
+  x: TypedSeries
   y: TypedSeries
 }
 
 export interface ScatterPlotDataItem {
-  type: "scatter_plot",
-  title: string,
+  type: "scatter_plot"
+  title: string
   traces: ScatterPlotTrace[]
 }
 
 export interface MapDataItem {
-  type: "map",
-  title?: string,
+  type: "map"
+  title?: string
   wkt_crs84: string
 }
 
