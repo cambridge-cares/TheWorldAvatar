e9645fce0d14c4596ebbc463a7826c79ec49392e
jc2341 <jc2341@cam.ac.uk>
2024-05-31 11:18:32 +0100

    dev-FenlandTrajectoryAgent, correct the url for http post

--- Agents/FenlandTrajectoryAgent/trigger_service/curl/gps_load_and_preprocess.sh
+++ Agents/FenlandTrajectoryAgent/trigger_service/curl/gps_load_and_preprocess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Copy all the following commands once, paste them into the terminal together, and then press Enter
-curl -X POST http://localhost:3840/gpstasks/fenlandtrajectoryagent/process_and_instantiate \
+curl -X POST http://localhost:3840/fenland-trajectory-agent/gpstasks/fenlandtrajectoryagent/load_and_preprocess \
      -H "Content-Type: application/json" \
      -d '{"file_path":"/app/agent/raw_data/gps_target_folder"}'
