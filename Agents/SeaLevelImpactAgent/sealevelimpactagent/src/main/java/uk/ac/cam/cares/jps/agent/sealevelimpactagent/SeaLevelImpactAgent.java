6e75baaaefc98c3f377865841dd3dc1f43b0e6d6
ShinZert <szp20@cam.ac.uk>
2024-06-02 15:16:05 +0800

    1808-dev-sealevelimpactagent: Modify README.md, Input params

--- Agents/SeaLevelImpactAgent/sealevelimpactagent/src/main/java/uk/ac/cam/cares/jps/agent/sealevelimpactagent/SeaLevelImpactAgent.java
+++ Agents/SeaLevelImpactAgent/sealevelimpactagent/src/main/java/uk/ac/cam/cares/jps/agent/sealevelimpactagent/SeaLevelImpactAgent.java
@@ -7,27 +7,26 @@ import org.apache.logging.log4j.Logger;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
-import java.util.Map;
 import java.util.Properties;
-import org.json.JSONArray;
+
 import org.json.JSONObject;
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
-import com.cmclinnovations.stack.clients.geoserver.GeoServerClient;
-import com.cmclinnovations.stack.clients.geoserver.GeoServerVectorSettings;
-import com.cmclinnovations.stack.clients.geoserver.UpdatedGSVirtualTableEncoder;
 
-@WebServlet(urlPatterns = "/runtsp")
+@WebServlet(urlPatterns = "/slrimpact")
 
 public class SeaLevelImpactAgent extends JPSAgent {
     
-    private static String tspFunction = null; 
+    private static String sspScenario = null;
+    private static String confidence = null;
+    private static Double quantile = null;
 
     private static final String PROPETIES_PATH = "/inputs/config.properties";
-    private final String FUNCTION_KEY = "function";
+    private final String SSP_SCENARIO_KEY = "ssp";
+    private final String CONFIDENCE_KEY = "confidence";
+    private final String QUANTILE_KEY = "quantile";
     private static final Logger LOGGER = LogManager.getLogger(SeaLevelImpactAgent.class);
     private EndpointConfig endpointConfig = new EndpointConfig();
     private String dbName;
@@ -90,63 +89,20 @@ public class SeaLevelImpactAgent extends JPSAgent {
             throw new JPSRuntimeException("Unable to validate request sent to the agent.");
         }
 
-        this.tspFunction = requestParams.getString(FUNCTION_KEY);
-
-        LOGGER.info("Successfully set tspFunction to " + tspFunction);
+        this.sspScenario = requestParams.getString(SSP_SCENARIO_KEY);
+        this.confidence = requestParams.getString(CONFIDENCE_KEY);
+        this.quantile = requestParams.getDouble(QUANTILE_KEY);
+        LOGGER.info("Successfully set SSP Scenario to " + sspScenario);
+        LOGGER.info("Successfully set Confidence level to " + confidence);
+        LOGGER.info("Successfully set Quantile to " + quantile);
 
         JSONObject response = new JSONObject();
-        response.put("message", "Successfully set tspFunction to " + tspFunction);
-
-
-        Path POI_PATH = Path.of("/inputs/"+tspFunction+"/POIqueries");
-        Path EDGESTABLESQL_PATH = Path.of("/inputs/"+tspFunction+"/edgesSQLTable");
-
+        response.put("message", "Successfully set SSP Scenario to " + sspScenario);
+        response.put("message", "Successfully set Confidence level to " + confidence);
+        response.put("message", "Successfully set Quantile to " + quantile);
 
         try {
             init();
-            // Read SPARQL and SQL files.
-            Map<String, String> POImap = FileReader.readPOIsparql(POI_PATH);
-            Map<String, String> EdgesTableSQLMap = FileReader.readEdgesTableSQL(EDGESTABLESQL_PATH);
-
-            // Iterate through the SPARQL entries, execute the SPARQL queries and add POIs to the cumulative array
-            JSONArray cumulativePOI = FileReader.getPOILocation(storeClient, POImap);
-
-
-            //Create geoserver layer
-            GeoServerClient geoServerClient = GeoServerClient.getInstance();
-            String workspaceName= "twa";
-            String schema = "public";
-            geoServerClient.createWorkspace(workspaceName);
-
-            UpdatedGSVirtualTableEncoder virtualTable = new UpdatedGSVirtualTableEncoder();
-            GeoServerVectorSettings geoServerVectorSettings = new GeoServerVectorSettings();
-            virtualTable.setSql("SELECT CONCAT('https://www.theworldavatar.com/kg/',poi_tsp_iri) as iri, poi_tsp_type, nearest_node, geom FROM poi_tsp_nearest_node");
-            virtualTable.setEscapeSql(true);
-            virtualTable.setName("poi_tsp_nearest_node");
-            virtualTable.addVirtualTableGeometry("geom", "Geometry", "4326"); // geom needs to match the sql query
-            geoServerVectorSettings.setVirtualTable(virtualTable);
-            geoServerClient.createPostGISDataStore(workspaceName,"poi_tsp_nearest_node" , dbName, schema);
-            geoServerClient.createPostGISLayer(workspaceName, dbName,"poi_tsp_nearest_node" ,geoServerVectorSettings);
-
-
-            /**
-             *  Loop through the edgeTable SQL and generate two geoserver layer for each edgeTableSQL
-             *  - TSP_route:  Gives the geometry of the shortest path for all TSP points
-             *  - TSP_seq:  Gives the sequence of order to visit all the TSP points
-             */
-            if (tspFunction.equals("UR")){
-                TSPRouteGenerator tspRouteGenerator = new TSPRouteGenerator();
-                for (Map.Entry<String, String> entry : EdgesTableSQLMap.entrySet()) {
-                    String layerName = "TSP_"+entry.getKey();
-                    String sql = entry.getValue();
-                    tspRouteGenerator.generateTSPLayer(geoServerClient, workspaceName, schema, dbName, layerName, sql);
-                    tspRouteGenerator.generateSequenceLayer(geoServerClient, workspaceName, schema, dbName, layerName, sql);
-                }
-            }
-
-
-
-
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -163,7 +119,7 @@ public class SeaLevelImpactAgent extends JPSAgent {
      */
     @Override
     public boolean validateInput(JSONObject requestParams) throws BadRequestException {
-        if (!requestParams.has(FUNCTION_KEY)) {
+        if (!requestParams.has(SSP_SCENARIO_KEY)) {
             LOGGER.error("Function is missing.");
             return false;
         }
