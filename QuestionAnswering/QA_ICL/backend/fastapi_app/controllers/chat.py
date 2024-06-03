9a678aed53d4dc4f8ca5433e69614db4863e7834
Dan Tran <nmdt2@cam.ac.uk>
2024-06-03 22:00:02 +0800

    dev-qa-icl: Formulate human-like response from QA request artifact

--- QuestionAnswering/QA_ICL/backend/fastapi_app/controllers/chat.py
+++ QuestionAnswering/QA_ICL/backend/fastapi_app/controllers/chat.py
@@ -1,6 +1,6 @@
 from functools import cache
+import json
 import logging
-import os
 from typing import Annotated, Callable, Optional
 
 from fastapi import Depends
@@ -9,6 +9,8 @@ from openai.types.chat.chat_completion_chunk import ChatCompletionChunk
 import tiktoken
 
 from config import AppSettings, get_app_settings
+from services.exceptions import QARequestArtifactNotFound
+from services.qa_artifact_store import QARequestArtifactStore, get_qaReq_artifactStore
 
 logger = logging.getLogger(__name__)
 
@@ -37,52 +39,78 @@ def binary_search(low: int, high: int, fn: Callable[[int], int], target: int):
     return low
 
 
-class ChatbotClient:
-    PROMPT_TEMPLATE = """Context information is below.
----------------------
-{context_str}
----------------------
-Given the context information and not prior knowledge, answer the query.
-Query: {query_str}
-Answer: """
+class ChatController:
+    PROMPT_TEMPLATE = """### Input question:
+{question}
+
+### Semantically parsed query:
+{data_req}
+
+### Query execution results:
+{data}
+
+Given the context information, please answer the query."""
 
     def __init__(
         self,
+        qa_req_artifact_store: QARequestArtifactStore,
         openai_base_url: Optional[str],
         openai_api_key: Optional[str],
         openai_model: str,
-        user_input_tokens_limit: int = 16000,
+        token_limit: int = 16000,
     ):
+        self.qa_req_artifact_store = qa_req_artifact_store
         self.openai_client = OpenAI(base_url=openai_base_url, api_key=openai_api_key)
         self.model = openai_model
         self.tokenizer = tiktoken.get_encoding("cl100k_base")
-        self.user_input_tokens_limit = user_input_tokens_limit
+        self.token_limit = token_limit
 
     def _count_tokens(self, text: str):
         return len(self.tokenizer.encode(text))
 
-    def request_stream(self, question: str, data: str) -> Stream[ChatCompletionChunk]:
-        content = self.PROMPT_TEMPLATE.format(context_str=data, query_str=question)
+    def request_stream(self, qa_request_id: str) -> Stream[ChatCompletionChunk]:
+        artifact = self.qa_req_artifact_store.load(qa_request_id)
+        if not artifact:
+            raise QARequestArtifactNotFound()
 
-        if self._count_tokens(content) > self.user_input_tokens_limit:
+        msg = self.PROMPT_TEMPLATE.format(
+            question=artifact.nlq, data_req=artifact.data_req, data=artifact.data
+        )
+
+        if self._count_tokens(msg) > self.token_limit:
+            if (
+                self._count_tokens(
+                    self.PROMPT_TEMPLATE.format(
+                        question=artifact.nlq, data_req=artifact.data_req, data=""
+                    )
+                )
+                > self.token_limit
+            ):
+                raise Exception("Token limit is too low!!")
+
             logger.info(
                 "The supplied data exceeds the token limit of {num} tokens. The data will be truncated.".format(
-                    num=self.user_input_tokens_limit
+                    num=self.token_limit
                 )
             )
+            serialized_data = json.dumps(artifact.data)
 
             truncate_idx = binary_search(
                 low=0,
-                high=len(content),
+                high=len(msg),
                 fn=lambda idx: self._count_tokens(
                     self.PROMPT_TEMPLATE.format(
-                        context_str=data[:idx], query_str=question
+                        question=artifact.nlq,
+                        data_req=artifact.data_req,
+                        data=serialized_data[:idx],
                     )
                 ),
-                target=self.user_input_tokens_limit,
+                target=self.token_limit,
             )
-            content = self.PROMPT_TEMPLATE.format(
-                context_str=data[:truncate_idx], query_str=question
+            msg = self.PROMPT_TEMPLATE.format(
+                question=artifact.nlq,
+                data_req=artifact.data_req,
+                data=serialized_data[:truncate_idx],
             )
 
         return self.openai_client.chat.completions.create(
@@ -90,7 +118,7 @@ Answer: """
             messages=[
                 {
                     "role": "user",
-                    "content": content,
+                    "content": msg,
                 },
             ],
             stream=True,
@@ -98,8 +126,14 @@ Answer: """
 
 
 @cache
-def get_chatbot_client(settings: Annotated[AppSettings, Depends(get_app_settings)]):
-    return ChatbotClient(
+def get_chatbot_client(
+    settings: Annotated[AppSettings, Depends(get_app_settings)],
+    qa_req_artifact_store: Annotated[
+        QARequestArtifactStore, Depends(get_qaReq_artifactStore)
+    ],
+):
+    return ChatController(
+        qa_req_artifact_store=qa_req_artifact_store,
         openai_base_url=settings.chat.base_url,
         openai_api_key=settings.chat.api_key,
         openai_model=settings.chat.model,
