56d3329bc4b1d2f49bf5352b8a778b87a200e085
Dan Tran <nmdt2@cam.ac.uk>
2024-06-03 11:51:16 +0800

    dev-qa-icl: Process table data to not include any IRI info

--- QuestionAnswering/QA_ICL/backend/fastapi_app/utils/rdf.py
+++ QuestionAnswering/QA_ICL/backend/fastapi_app/utils/rdf.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from constants.prefixes import PREFIX_NAME2URI
 from services.kg import SparqlSelectResponse
 
@@ -27,3 +25,30 @@ def flatten_sparql_select_response(res: SparqlSelectResponse):
         {k: v.value for k, v in binding.items()} for binding in res.results.bindings
     ]
     return vars, bindings
+
+
+def filter_remove_iris(
+    x: dict | list | tuple | str | float, iri_prefixes: list[str] | tuple[str]
+):
+    if isinstance(x, dict):
+        return filter_remove_iris_from_dict(x, iri_prefixes=iri_prefixes)
+    elif isinstance(x, list) or isinstance(x, tuple):
+        return filter_remove_iris_from_list(x, iri_prefixes=iri_prefixes)
+    else:
+        return x
+
+
+def filter_remove_iris_from_list(
+    lst: list | tuple, iri_prefixes: list[str] | tuple[str]
+):
+    return [filter_remove_iris(x, iri_prefixes=iri_prefixes) for x in lst]
+
+
+def filter_remove_iris_from_dict(doc: dict, iri_prefixes: list[str] | tuple[str]):
+    return {
+        k: filter_remove_iris(v, iri_prefixes=iri_prefixes)
+        for k, v in doc.items()
+        if not (
+            isinstance(v, str) and any(v.startswith(prefix) for prefix in iri_prefixes)
+        )
+    }
