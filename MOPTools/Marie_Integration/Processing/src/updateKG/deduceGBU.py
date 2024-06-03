f2a964dbe0aeb4b49dbea660f636e0b825059c0d
saluzf <saluzf@ethz.ch>
2024-06-03 11:06:08 +0100

    dev-MOPs-Marie-integration: Remove unnecessary methods and variables.

--- MOPTools/Marie_Integration/Processing/src/updateKG/deduceGBU.py
+++ MOPTools/Marie_Integration/Processing/src/updateKG/deduceGBU.py
@@ -3,10 +3,9 @@ import re
 import pyderivationagent
 
 class MOPGBU:
-    OM      = 'http://www.theworldavatar.com/ontology/ontomops/OntoMOPs.owl#'
-    RDF     = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
-    RDFS    = 'http://www.w3.org/2000/01/rdf-schema#'
-    
+    OMPrefix      = f"""PREFIX om:      <http://www.theworldavatar.com/ontology/ontomops/OntoMOPs.owl#>
+                        PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#> """
+
     def __init__(self, query_endpoint, update_endpoint, kg_user, kg_password, file_directory):
         self.sparql_client      = pyderivationagent.kg_operations.PySparqlClient(
             query_endpoint      = query_endpoint,
@@ -17,22 +16,20 @@ class MOPGBU:
         self.file_directory     = file_directory
         gbus                    = []
 
-    def query_Geometry(self):
+    def query_geometry(self):
         """Query for newly instantiated Mops and return the Geometry value to deduce the GBUs"""
         query = f"""
-        PREFIX om:      <{self.OM}>
-        PREFIX rdfs:    <{self.RDFS}>
-
+        {self.OMPrefix}
         SELECT ?Geometry
         WHERE {{
         ?MOPID          om:hasAssemblyModel     ?AMID       .
         ?AMID           om:value                ?Geometry   .
         }}
-        LIMIT 10
+        LIMIT 2
         """
         return self.sparql_client.performQuery(query)
 
-    def deduceGBU(self, Geometry):
+    def deduce_gbu(self, Geometry):
         """Deduce GBU by splitting the Geometry value string into the two parts, 
         e.g. (4-planar)x12(2-bent)x24 => GBU1 = 4-planar, GBU 2 = 2-bent"""
         matches = re.findall(r'\(([^)]+)\)', Geometry)
@@ -41,51 +38,19 @@ class MOPGBU:
         else:
             raise ValueError("The input string does not contain exactly two bracketed parts.")
     
-    def checkGBU(self, gbu):
+    def check_gbu(self, gbu):
         """Query the old MOPs for the GBU and check if it exists. Error if there is one wrongly identified."""
         query = f"""
-        PREFIX om:      <{self.OM}>
-        PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX rdfs:    <{self.RDFS}>
-
+        {self.OMPrefix}
         ASK WHERE {{
             ?MOPID om:hasGeneralBuildingUnit "{gbu}" .
         }}
         """
         return self.sparql_client.performQuery(query)
 
-            
-
-    def delete_geometry(self, response):
-        for res in response:
-            mop_iri         = res['MOPIRI']
-            ccdc_num        = res['CCDCNum']
-            delete_query    = f"""
-            PREFIX om: <{self.OM}>
-
-            DELETE DATA {{
-                <{mop_iri}> om:hasGeometry "{ccdc_num}" .
-            }}
-            """
-            self.sparql_client.performUpdate(delete_query)
-            print(f"Triple deleted: <{mop_iri}> om:hasGeometry \"{ccdc_num}\"")
-
-    def process_results(self, response):
-        for res in response:
-            mop_iri     = res['MOPIRI']
-            ccdc_num    = res['CCDCNum']
-            file_path   = os.path.join(self.file_directory, f"{ccdc_num}.xyz")
-            if os.path.isfile(file_path):
-                print(f"File found for CCDC number {ccdc_num}: {file_path}")
-                self.update_geometry(mop_iri, ccdc_num)
-            else:
-                print(f"File not found for CCDC number {ccdc_num}: {file_path}")
-
 def main():
     SPARQL_QUERY_ENDPOINT       = 'http://localhost:7578/blazegraph/namespace/OntoMOPs'
     SPARQL_UPDATE_ENDPOINT      = 'http://localhost:7578/blazegraph/namespace/OntoMOPs'
-    # SPARQL_QUERY_ENDPOINT       = 'http://68.183.227.15:3838/blazegraph/namespace/OntoMOPs'
-    # SPARQL_UPDATE_ENDPOINT      = 'http://68.183.227.15:3838/blazegraph/namespace/OntoMOPs'
     KG_USERNAME                 = 'bg_user'
     KG_PASSWORD                 = 'admin'
     FILE_DIRECTORY              = "../data/MOPGeometryData"
@@ -97,16 +62,16 @@ def main():
         kg_password             = KG_PASSWORD,
         file_directory          = FILE_DIRECTORY
     )
-    response                    = updater.query_Geometry()
-    
+    response                    = updater.query_geometry()
+
     for i, res in enumerate(response):
         geometry        = res['Geometry']
-        gbus            = updater.deduceGBU(geometry)
+        gbus            = updater.deduce_gbu(geometry)
         print(f"MOP {i} geometry: {geometry}")
         print(f"GBU1: {gbus[0]}, GBU2: {gbus[1]}")
         # check if CBUs exist:
         for gbu in gbus:
-            if updater.checkGBU(gbu):
+            if updater.check_gbu(gbu):
                 print(f"Successfully identified gbu: {gbu}!")
             else:
                 print(f"Gbu: {gbu} is not yet in the database check if there is an error!\n")
