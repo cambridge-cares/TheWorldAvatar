9787ed7f49de21ff3e41688c91ca6514f3e48523
Dan Tran <nmdt2@cam.ac.uk>
2024-06-03 16:44:33 +0800

    dev-qa-icl: Enable scrolling for data tables

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/components/ui/data-table.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/components/ui/data-table.tsx
@@ -21,23 +21,25 @@ import {
 } from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
 import { CaretSortIcon } from "@radix-ui/react-icons"
-import { TableDataBase, TableDataRow } from "@/lib/model"
+import { TableDataBase, TableDataRow, TableDataValue } from "@/lib/model"
+import { ScrollArea, ScrollBar } from "./scroll-area"
 
 
 export interface DataTableProps extends TableDataBase {
-  paginated: boolean
-  isInner: boolean
+  paginated?: boolean
+  bordered?: boolean
+  scrollable: boolean
 }
 
 
 function DataTableBase({
   columns,
   data,
-  paginated,
-  isInner,
+  scrollable,
   ...props
 }: DataTableProps) {
-  console.log("drawing table")
+  const { paginated = false, bordered = false, ...otherProps } = props
+
   const cols = columns.map(h => ({
     id: h,
     accessorFn: (row: TableDataRow) => row[h],
@@ -53,11 +55,13 @@ function DataTableBase({
       )
     },
     cell: ({ row }: { row: Row<TableDataRow> }) => {
-      const val = row.getValue(h) as TableDataBase;
+      const val = row.getValue(h) as TableDataValue;
       if (!(val)) {
         return ""
+      } else if (Array.isArray(val)) {
+        return (<ul>{val.map((elem, idx) => <li key={idx}>elem</li>)}</ul>)
       } else if (typeof val === "object") {
-        return (<DataTableBase columns={val.columns} data={val.data} paginated={false} isInner={true} />)
+        return (<DataTableBase columns={val.columns} data={val.data} scrollable={false} />)
       } else {
         return val;
       }
@@ -82,52 +86,65 @@ function DataTableBase({
   }
   const table = useReactTable(tableOptions)
 
-  return (
-    <div {...props}>
-      <div className={isInner ? "" : "rounded-md border"}>
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={cols.length} className="h-24 text-center">
-                  No results.
+  // const scrollableClassName = scrollable ? "h-fit max-h-80 w-fit max-w-full overflow-y-auto overflow-x-auto relative" : null
+  const borderClassName = bordered ? "rounded-md border" : ""
+  const tableComponent = (
+    <Table>
+      <TableHeader className={scrollable ? "sticky top-0 bg-secondary" : ""}>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              return (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                </TableHead>
+              )
+            })}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell colSpan={cols.length} className="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  )
+
+  return (
+    <div {...otherProps}>
+      {
+        scrollable ? (
+          <ScrollArea className={["h-96 w-full", borderClassName].filter(x => x).join(" ")}>
+            {tableComponent}
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
+        ) : (
+          <div className={borderClassName}>{tableComponent}</div>
+        )
+      }
       {paginated && (
         <div className="flex items-center justify-end space-x-2 py-4">
           <Button
@@ -157,4 +174,4 @@ export const DataTable = ({
   columns,
   data,
   ...props
-}: TableDataBase) => DataTableBase({ columns, data, paginated: true, isInner: false, ...props })
+}: TableDataBase) => (<DataTableBase columns={columns} data={data} paginated bordered scrollable {...props} />)
