9697021cf7e3d8fe6cfc1699da2248838c6acfac
Oisín <opeppard@cmcl.io>
2024-06-04 16:37:41 +0100

    start keycloak integration

--- web/twa-vis-platform/code/server.js
+++ web/twa-vis-platform/code/server.js
@@ -15,6 +15,12 @@
 const express = require("express");
 const next = require("next");
 
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
+
+const memoryStore = new session.MemoryStore();
+const keycloak = new Keycloak({ store: memoryStore });
+
 // Configure the server port; default to 3000 if not specified in environment variables
 if (process.env.PORT) {console.log('port specified in .env file: ', process.env.PORT);}
 const port = process.env.PORT || 3000;
