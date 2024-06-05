7654541ae85a9dcf729d900218a6935da9ddda2a
Dan Tran <nmdt2@cam.ac.uk>
2024-06-05 10:31:23 +0800

    dev-qa-icl: Make example questions collapsible

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/page.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/page.tsx
@@ -18,22 +18,28 @@ export default async function Home() {
   return (
     <main className="p-4 min-h-screen flex flex-col justify-center items-center">
       <IntroSection imgSrc='/images/marie-thumbnail.jpg' imgAlt="Marie's thumbnail" introTextMd={introText} />
-      <QAFragment />
-      <div className="max-w-2xl w-full">
-        <h2>Example Questions</h2>
-        <Accordion type="single" collapsible className="w-full">
-          {exampleQuestionGroups.map((grp, idx) => (
-            <AccordionItem key={idx} value={idx.toString()}>
-              <AccordionTrigger>{grp.heading}</AccordionTrigger>
-              <AccordionContent>
-                <ul className='list-disc list-inside'>
-                  {grp.questions.map((qn, i) => (<li key={i} >{qn}</li>))}
-                </ul>
-              </AccordionContent>
-            </AccordionItem>
-          ))}
+      <section className="max-w-3xl w-full">
+        <Accordion type="single" collapsible className='w-full mb-8'>
+          <AccordionItem value="0">
+            <AccordionTrigger>Example Questions</AccordionTrigger>
+            <AccordionContent className='px-4'>
+              <Accordion type="single" collapsible className="w-full">
+                {exampleQuestionGroups.map((grp, idx) => (
+                  <AccordionItem key={idx} value={idx.toString()}>
+                    <AccordionTrigger>{grp.heading}</AccordionTrigger>
+                    <AccordionContent className='px-8'>
+                      <ul className='list-disc list-outside'>
+                        {grp.questions.map((qn, i) => (<li key={i} >{qn}</li>))}
+                      </ul>
+                    </AccordionContent>
+                  </AccordionItem>
+                ))}
+              </Accordion>
+            </AccordionContent>
+          </AccordionItem>
         </Accordion>
-      </div>
+      </section>
+      <QAFragment />
     </main>
   );
 }
