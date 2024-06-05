09ef4fd168b428f131cb8a10774032a1230505f2
Jiaru Bai <jb2197@cam.ac.uk>
2024-06-05 15:54:35 +0100

    dev-pydantic-rdflib: stylish modification to __init__.py to ease the import.

--- JPS_BASE_LIB/python_wrapper/twa/__init__.py
+++ JPS_BASE_LIB/python_wrapper/twa/__init__.py
@@ -5,9 +5,12 @@ from twa.data_model.base_ontology import (
     BaseClass,
     ObjectProperty,
     DataProperty,
+    TransitiveProperty,
+    as_range,
 )
 from twa.kg_operations import (
     PySparqlClient,
 )
+from twa.data_model.iris import TWA_BASE_URL
 
 __version__ = "0.0.2a0"
