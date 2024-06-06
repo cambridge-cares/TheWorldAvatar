95dabc6cf55cce792394b01f4c3cda618ba85acf
George Brownbridge <gbrownbridge@cmcl.io>
2024-06-06 12:20:16 +0100

    dev-podman-improvements: Updated the "remove.sh" script to be more robust (not removing things from stacks that have the name of the specified stack in their name) and to remove networks and secrets/configs when using Podman.

--- Deploy/stacks/dynamic/common-scripts/remove.sh
+++ Deploy/stacks/dynamic/common-scripts/remove.sh
@@ -6,8 +6,8 @@ remove_stack_volumes() {
     i=0
     while
         # Wait a max of 30 seconds
-        (( i < 30 )) \
-        && [[ -n $(${EXECUTABLE} ps -a --filter "name=$STACK_NAME" --format "{{.ID}}") ]]
+        ((i < 30)) &&
+            [[ -n $(${EXECUTABLE} ps -a --filter "name=$STACK_NAME" --format "{{.ID}}") ]]
     do
         echo "Waiting for containers to be removed"
         sleep 1
@@ -16,18 +16,17 @@ remove_stack_volumes() {
 
     sleep 2
 
-    ${EXECUTABLE} volume ls --filter "name=$STACK_NAME" --format "{{.Name}}" | xargs -r ${EXECUTABLE} volume rm -f
+    ${EXECUTABLE} volume ls --filter "name=^${STACK_NAME}_" --format "{{.Name}}" | xargs -r "${EXECUTABLE}" volume rm -f
 }
-
 
 # Load common functions
 . "${SCRIPTS_DIR}/common_functions.sh"
 
-while (( $# >= 1 )); do 
+while (($# >= 1)); do
     case $1 in
-        --volumes|-v) REMOVE_VOLUMES=TRUE;;
-        *) SERVICE_NAME=$1;;
-    esac;
+    --volumes | -v) REMOVE_VOLUMES=TRUE ;;
+    *) SERVICE_NAME=$1 ;;
+    esac
     shift
 done
 
@@ -40,9 +39,17 @@ if [[ -n "$SERVICE_NAME" ]]; then
     exit
 fi
 
+if [[ -n "$REMOVE_VOLUMES" ]]; then
+    # If we are removing the volumes then there is no state to loose
+    timeout=0
+else
+    # If we are keeping the volumes then give the containers a chance to save their state
+    timeout=10
+fi
+
 case "$STACK_NAME" in
-    "")
-        echo "Usages:
+"" | "--help" | "-h")
+    echo "Usages:
     ./stack.sh remove all [OPTIONS]             Remove all stacks (docker)
     ./stack.sh remove all [OPTIONS]             Remove all containers and pods (podman)
     ./stack.sh remove STACK_NAME [OPTIONS]      Remove stack \"STACK_NAME\"
@@ -51,50 +58,60 @@ case "$STACK_NAME" in
 Options:
     -v, --volumes       Also remove named volumes
 "
-exit;;
-    "all")
-        if [ "$EXECUTABLE" == "docker" ]; then
-            ALL_STACKS=$(${EXECUTABLE} stack ls --format="{{.Name}}")
-            # Remove all stacks
-            ${EXECUTABLE} swarm leave --force
-
-            if [[ -n "$REMOVE_VOLUMES" ]]; then
-                # Remove named volumes
-                for STACK_NAME in $ALL_STACKS; do
-                    remove_stack_volumes "${STACK_NAME}"
-                done
-            fi
-        else
-            # NB Removing containers before pods is faster if they don't respond to kill signals.
-            ${EXECUTABLE} rm --force --all
-            ${EXECUTABLE} pod rm --force --all
-            if [[ -n "$REMOVE_VOLUMES" ]]; then
-                ${EXECUTABLE} volume rm --force --all
-            fi
-        fi
     ;;
-    *)
-        if [ "$EXECUTABLE" == "docker" ]; then
-            # Don't exit if stack has already been removed
-            set -e
-            # Remove named stack
-            ${EXECUTABLE} stack rm "${STACK_NAME}"
-            set +e
-        else
-            # If pod is running remove it
-            for POD_ID in $(${EXECUTABLE} pod ls -q -f "name=${STACK_NAME}") ; do
-                ${EXECUTABLE} pod rm --force "${POD_ID}" > /dev/null
-            done
-            # If container is running outside a pod remove it
-            for CONTAINER_ID in $(${EXECUTABLE} ps -a -q -f "name=${STACK_NAME}"); do
-                ${EXECUTABLE} rm --force "${CONTAINER_ID}" > /dev/null
-            done
-        fi
+"all")
+    if [ "$EXECUTABLE" == "docker" ]; then
+        ALL_STACKS=$(${EXECUTABLE} stack ls --format="{{.Name}}")
+        # Remove all stacks
+        ${EXECUTABLE} swarm leave --force
 
         if [[ -n "$REMOVE_VOLUMES" ]]; then
             # Remove named volumes
-            remove_stack_volumes "${STACK_NAME}"
+            for STACK_NAME in $ALL_STACKS; do
+                remove_stack_volumes "${STACK_NAME}"
+            done
         fi
+    else
+        # Removing networks removes containers that are in them
+        # NB Removing containers before pods is faster if they don't respond to kill signals.
+        # Regex in filiter required as podman doesn't like it if you try to remove the default "podman" network
+        ${EXECUTABLE} network ls -q -f "name=^(?!podman$).*$" | xargs -r -I{} "${EXECUTABLE}" network rm -ft "$timeout" "{}"
+        ${EXECUTABLE} rm --force --all -t "$timeout"
+        ${EXECUTABLE} pod rm --force --all -t "$timeout"
+        ${EXECUTABLE} secret rm --all
+        if [[ -n "$REMOVE_VOLUMES" ]]; then
+            ${EXECUTABLE} volume rm --force --all
+        fi
+    fi
+    ;;
+*)
+    if [ "$EXECUTABLE" == "docker" ]; then
+        # Don't exit if stack has already been removed
+        set -e
+        # Remove named stack
+        ${EXECUTABLE} stack rm "${STACK_NAME}"
+        set +e
+    else
+        # Remove stack network (this also removes the containers within it)
+        ${EXECUTABLE} network rm -ft "$timeout" "${STACK_NAME}" >/dev/null
+
+        # Regex in "name=^${STACK_NAME}_" filters is an attempt to prevent "stack-10" being removed with "stack-1"
+
+        # If pod is running remove it (don't need to force as relevant containers should have been removed with the network)
+        ${EXECUTABLE} pod ls -q -f "name=^${STACK_NAME}_" | xargs -r -I{} "${EXECUTABLE}" pod rm "{}" >/dev/null
+
+        # If a container is running outside a pod remove it
+        ${EXECUTABLE} ps -a -q -f "name=^${STACK_NAME}_" | xargs -r -I{} "${EXECUTABLE}" rm -ft "$timeout" "{}" >/dev/null
+
+        # Remove any secrets and configs
+        ${EXECUTABLE} secret ls -q --filter "name=^${STACK_NAME}_" | xargs -r -I{} "${EXECUTABLE}" secret rm "{}" >/dev/null
+
+    fi
+
+    if [[ -n "$REMOVE_VOLUMES" ]]; then
+        # Remove named volumes
+        remove_stack_volumes "${STACK_NAME}"
+    fi
     ;;
 
 esac
