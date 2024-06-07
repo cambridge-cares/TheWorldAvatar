1087bfa507fc25d9110b4462654f2ad9360e78cf
jx309 <jx309@cam.ac.uk>
2024-06-07 09:58:46 +0100

    dev-heat-pump-migration-to-stack-3:deleted toy agent since it is not really needed

--- Agents/ToyAgent_age/toyagent/errorhandling/exceptions.py
+++
@@ -1,6 +0,0 @@
-class KGException(Exception):
-    """Raise in case of exception when executing SPARQL queries/updates."""
-
-
-class StackException(Exception):
-    """Raise in case of exception when interacting with the Docker Stack."""
