a3d60f8fd2f6eedcff38d9a649fc373e211f7c42
Dan Tran <nmdt2@cam.ac.uk>
2024-06-07 13:59:15 +0800

    dev-qa-icl: Link reaction mechanism

--- QuestionAnswering/QA_ICL/backend/fastapi_app/services/stores/entity_store/ontokin.py
+++ QuestionAnswering/QA_ICL/backend/fastapi_app/services/stores/entity_store/ontokin.py
@@ -7,6 +7,33 @@ from services.kg import KgClient, get_ontokin_bgClient
 from services.stores.entity_store.base import IEntityLinker
 
 
+class MechanismLinker(IEntityLinker):
+    def __init__(self, bg_client: KgClient):
+        self.bg_client = bg_client
+
+    def link(self, text: str | None, **kwargs):
+        if "provenance" not in kwargs:
+            return []
+
+        query = """PREFIX okin: <http://www.theworldavatar.com/ontology/ontokin/OntoKin.owl#>
+PREFIX op: <http://www.theworldavatar.com/ontology/ontoprovenance/OntoProvenance.owl#>
+
+SELECT DISTINCT *
+WHERE {{
+    ?Mechanism okin:hasProvenance/(op:hasDOI|op:hasURL) "{provenance}"
+}}""".format(
+            provenance=kwargs["provenance"]
+        )
+
+        _, bindings = self.bg_client.querySelectThenFlatten(query)
+        return [binding["Mechanism"] for binding in bindings]
+
+
+@cache
+def get_mechanism_linker(bg_client: Annotated[KgClient, Depends(get_ontokin_bgClient)]):
+    return MechanismLinker(bg_client=bg_client)
+
+
 class ReactionLinker(IEntityLinker):
     def __init__(self, bg_client: KgClient):
         self.bg_client = bg_client
@@ -17,13 +44,14 @@ class ReactionLinker(IEntityLinker):
 
         query = """PREFIX okin: <http://www.theworldavatar.com/ontology/ontokin/OntoKin.owl#>
         
-SELECT * WHERE {{
+SELECT DISTINCT *
+WHERE {{
     ?Reaction okin:hasEquation "{equation}"
 }}""".format(
             equation=kwargs["equation"]
         )
 
-        _, bindings  = self.bg_client.querySelectThenFlatten(query)
+        _, bindings = self.bg_client.querySelectThenFlatten(query)
         return [binding["Reaction"] for binding in bindings]
 
 
