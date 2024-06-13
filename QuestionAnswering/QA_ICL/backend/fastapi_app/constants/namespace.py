36f5786708a3878cb3c41e280b31894f280a075b
Dan Tran <nmdt2@cam.ac.uk>
2024-06-13 15:50:40 +0800

    dev-qa-icl: Define classes for ontospecies and ontokin

---
+++ QuestionAnswering/QA_ICL/backend/fastapi_app/constants/namespace.py
@@ -0,0 +1,8 @@
+from rdflib import Namespace
+
+
+ONTOSPECIES = Namespace(
+    "http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#"
+)
+ONTOKIN = Namespace("http://www.theworldavatar.com/ontology/ontokin/OntoKin.owl#")
+ONTOPROVENANCE = Namespace("http://www.theworldavatar.com/ontology/ontoprovenance/OntoProvenance.owl#")
