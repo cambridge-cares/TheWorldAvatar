6ff68b9f97d0891406f1b82bfeba1e9eff5bd710
Md Hanif Seddiqui <mhs62@cam.ac.uk>
2024-06-14 18:13:15 +0100

    Update analyse_sparql.py
    It can analyse a SPARQL and find necessary endpoints to run the SPARQL

--- JPS_BASE_LIB/python_federated_query/analyse_sparql.py
+++ JPS_BASE_LIB/python_federated_query/analyse_sparql.py
@@ -2,14 +2,31 @@ from rdflib.plugins.sparql.parser import parseQuery
 from rdflib.plugins.sparql.algebra import translateQuery
 from rdflib import URIRef, BNode
 import rdflib.plugins.sparql.algebra as algebra
+import json
 
 class AnalyseSparql:
   
     def __init__(self,sparql_query):
         self.query_object = translateQuery(parseQuery(sparql_query))
+        self.class_index_file_path=""
+        self.property_index_file_path=""
+        self.cp_index_file_path=""
         self.classes = set()
         self.properties = set()
+        self.class_index = {}
+        self.property_index = {}
+        self.cp_index = {}
     
+    def set_index_location(self,index_dir):
+      if(index_dir.strip()[-1]=="/"):
+          index_dir=index_dir.strip()
+      else:
+          index_dir=index_dir.strip()+"/"
+          
+      self.class_index_file_path= index_dir+"cinv.indx"
+      self.property_index_file_path=index_dir+"pinv.indx"
+      self.cp_index_file_path=index_dir+"cpinv.indx"
+      
     def extract_classes_and_properties(self):
         """Analyzes a SPARQL query to extract classes and properties.
 
@@ -45,38 +62,98 @@ class AnalyseSparql:
                         self.traverse_query_tree(item)
                 else:
                     self.traverse_query_tree(child)
+    
+    #Load index from file
+    def load_indices(self):
+        self.load_class_index()
+        self.load_property_index()
+        self.load_concept2property_index()
+            
+    def load_class_index(self):
+        try:
+            with open(self.class_index_file_path, 'r') as file:
+                self.class_index = json.load(file)
+            print(f"Class Index loaded from {self.class_index_file_path}")
+        except FileNotFoundError:
+            print(f"File '{self.class_index_file_path}' not found.")
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON data from '{self.class_index_file_path}'.")
+
+    def load_property_index(self):
+        try:
+            with open(self.property_index_file_path, 'r') as file:
+                self.property_index = json.load(file)
+            print(f"Property Index loaded from {self.property_index_file_path}")
+        except FileNotFoundError:
+            print(f"File '{self.property_index_file_path}' not found.")
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON data from '{self.property_index_file_path}'.")
+    
+    def load_concept2property_index(self):
+        try:
+            # Open the file for reading
+            with open(self.cp_index_file_path, 'r') as file:
+                # Load the JSON data from the file into the index variable
+                self.cp_index = json.load(file)
+            print("Class-Property multilevel inverted index loaded successfully.")
+        except FileNotFoundError:
+            print(f"File '{self.cp_index_file_path}' not found.")
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON data from '{self.cp_index_file_path}'.")
+          
+    def get_endpoints(self):
+        endpoints = set()
+        classes, properties = ana.extract_classes_and_properties()
+        
+        print("Analysing Classes:")
+        for class_uriref in classes:
+            class_uri=str(class_uriref)
+            
+            if class_uri in self.class_index:
+                print("Found class alignment: " + class_uri)
+                for endpoint in self.class_index[class_uri]:
+                    endpoints.add(endpoint)
+            else: print("Un-aligned class: "+class_uri)
+            
+        print("Analysing Properties:")        
+        for property_uriref in properties:
+            property_uri=str(property_uriref)
+            if property_uri in self.property_index:
+                print("Found property alignment:" + property_uri)
+                for endpoint in self.property_index[property_uri]:
+                    endpoints.add(endpoint)
+            else: print("Un-aligned property: "+property_uri)
+        
+        print("The Final Endpoints: ")
+        print(endpoints)
+        
 # Example usage
 sparql_query = """
-PREFIX ex: <http://example.org/>
-SELECT ?subject ?type ?property
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX pt: <http://www.daml.org/2003/01/periodictable/PeriodicTable.owl#>
+PREFIX OntoKin: <http://www.theworldavatar.com/ontology/ontokin/OntoKin.owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+SELECT ?identifier ?atomicMass ?atomicMassUnits
 WHERE {
-  ?subject rdf:type ex:Class1 .
-  ?subject ex:property1 ?value1 .
-  OPTIONAL { ?subject ex:property2 ?value2 . }
-  FILTER(?value1 > 10)
-  { ?subject ex:property3 ?value3 . }
-  UNION
-  { ?subject ex:property4 ?value4 . }
-  GRAPH ?g {
-    ?s ?p ?o .
-  }
-  VALUES (?a ?b) {
-    (1 2)
-    (3 4)
-  }
-  BIND(?value1 + 1 AS ?newValue)
-  {
-    SELECT ?sub ?prop WHERE {
-      ?sub ex:prop ?prop .
-    }
-  }
+    ?element1 rdf:type pt:Element .
+    BIND(STRAFTER(STR(?element1), "#") AS ?identifier)
+    ?element2 rdf:type OntoKin:Element .
+    ?element2 rdfs:label ?identifier1 .
+    ?element2 OntoKin:hasAtomicMass ?atomicMass .
+    ?element2 OntoKin:hasAtomicMassUnits ?atomicMassUnits .
+    FILTER(?identifier = ?identifier1)
 }
 """
 
 # usage
 if __name__ == "__main__":
+    index_location='C:/Users/printer_admin/Downloads/KGs/'
+    
     ana = AnalyseSparql(sparql_query)
-    classes, properties = ana.extract_classes_and_properties()
-  
-    print("Classes:", classes)
-    print("Properties:", properties)
+    ana.set_index_location(index_location)
+    ana.load_indices()
+    ana.get_endpoints()
+        
+    
