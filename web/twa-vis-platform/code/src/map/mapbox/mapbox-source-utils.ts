39a0c79dc35b6b3ff9381480aea2afab5dedbf10
Oisín <opeppard@cmcl.io>
2024-06-14 15:47:37 +0100

    dev-credo-vis: tidy add source code

--- web/twa-vis-platform/code/src/map/mapbox/mapbox-source-utils.ts
+++ web/twa-vis-platform/code/src/map/mapbox/mapbox-source-utils.ts
@@ -25,54 +25,54 @@ export function addAllSources(map: Map, dataStore: DataStore) {
  */
 export function addSource(map: Map, source: LayerSource) {
     const collision = map?.getSource(source.id);
-    if(collision != null) {
-        console.warn("Attempting to add a source that's already on map: '" + source.id + "'.");
+    if (collision) {
+        console.warn(`Attempting to add a source that's already on map: '${source.id}'.`);
         return;
     }
 
     // Clone the original source definition
-    const options: JsonObject = {...source.definition};
+    const options: JsonObject = { ...source.definition };
     // Remove properties not expected by Mapbox
-    if(options["id"] != null) delete options["id"];
-    if(options["metaFiles"] != null) delete options["metaFiles"];
-    if(options["timeseriesFiles"] != null) delete options["timeseriesFiles"];
+    delete options["id"];
+    delete options["metaFiles"];
+    delete options["timeseriesFiles"];
 
     // Ensure data have been formatted for the app
     if (options["data"]) {
-        const dataUrl: string = options["data"] as string;
-        options["data"] = formatAppUrl(dataUrl);
+        options["data"] = formatAppUrl(options["data"] as string);
     }
-
     // Add attributions if missing
-    if (!options["attribution"]) {
-        options["attribution"] = "CMCL";
-    }
-    options["attribution"] = 'Powered by <a style="text-color:rgba(0,0,0,.5)" href="https://theworldavatar.io/" target = "_blank" title = "TWA" aria - label="TheWorldAvatar">The World Avatar&#8482;</a> | <a href="https://cmcl.io/" target = "_blank" title = "CMCL" aria - label="CMCL">'  + (options["attribution"] + " </a>" as string);
+    options["attribution"] = options["attribution"] ?? "CMCL";
+    options["attribution"] = `Powered by <a style="text-color:rgba(0,0,0,.5)" href="https://theworldavatar.io/" target = "_blank" title = "TWA" aria - label="TheWorldAvatar">The World Avatar&#8482;</a> | <a href="https://cmcl.io/" target = "_blank" title = "CMCL" aria - label="CMCL">${options["attribution"]} </a>`;
 
     // Have to cast to type specific object to meet Mapbox's API
+
+    
     let mapboxObj: AnySourceData;
-    switch(source.type) {
+    switch (source.type) {
         case "canvas":
             mapboxObj = ((options as unknown) as CanvasSourceRaw);
-        break;
+            break;
         case "geojson":
             mapboxObj = ((options as unknown) as GeoJSONSourceRaw);
-        break;
+            break;
         case "image":
             mapboxObj = ((options as unknown) as ImageSourceRaw);
-        break;
+            break;
         case "raster":
             mapboxObj = ((options as unknown) as RasterSource);
-        break;
+            break;
         case "vector":
             mapboxObj = ((options as unknown) as VectorSource);
-        break;
+            break;
         case "video":
             mapboxObj = ((options as unknown) as VideoSourceRaw);
-        break;
+            break;
+        default:
+            throw new Error(`Unsupported source type: ${source.type}`);
     }
 
     // Add to the map
     map?.addSource(source.id, mapboxObj);
-    console.info("Pushed data source to map '" + source.id + "'.");
+    console.info(`Pushed data source: ${source.id} to map`);
 }
