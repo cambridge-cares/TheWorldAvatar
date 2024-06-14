a980fe9fdedddd201a608ed99f011c43fa142d4c
Md Hanif Seddiqui <mhs62@cam.ac.uk>
2024-06-14 18:19:34 +0100

    moved to backup_extra
    experimented earlier. not necessary now.

--- JPS_BASE_LIB/python_federated_query/backup_extra/build_c2endpoints_indx.py
+++ JPS_BASE_LIB/python_federated_query/backup_extra/build_c2endpoints_indx.py
@@ -1,4 +1,4 @@
-from rdflib import Graph, Namespace
+from rdflib import Graph, Namespace, URIRef
 from rdflib.plugins.stores.sparqlstore import SPARQLStore
 import os
 import json
@@ -59,10 +59,10 @@ class BuildKGIndex:
                 
                 # Check if class_uri already exists in cp_index
                 if class_uri not in self.class_index:
-                    self.class_index[class_uri] = []
+                    self.class_index[class_uri] = set()
                 
                 if endpoint_url not in self.class_index[class_uri]:
-                  self.class_index[class_uri].append(endpoint_url)
+                  self.class_index[class_uri].add(endpoint_url)
                   
                 # self.build_inverted_index(classes,file_path)
                 # if endpoint_url not in self.class_index[class_uri]:
@@ -86,21 +86,77 @@ class BuildKGIndex:
         return None
     
     return self.class_index
-  
-  def get_sparql_result(self, sparql_query):
-    return self.graph.query(sparql_query)
               
-  def save_concept2endpoint_invindex(self, file_path):
-    with open(file_path, 'w') as f:
-        json.dump(self.class_index, f, indent=4)
+  # def save_concept2endpoint_invindex(self, file_path):
+  #   with open(file_path, 'w') as f:
+  #       json.dump(self.class_index, f, indent=4)
+  def get_index(self):
+    return self.class_index
+       
+  def add_triples_and_update_index(self, new_triples, endpoint_url):
+        modified_classes = set()
         
+        for s, p, o in new_triples:
+            self.graph.add((s, p, o))
+            
+            if p == URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"):
+                class_uri = str(o)
+                
+                if class_uri not in self.class_index:
+                    self.class_index[class_uri] = set()
+                
+                self.class_index[class_uri].add(endpoint_url)
+                modified_classes.add(class_uri)
+                
+        self.triplecount += len(new_triples)
+        print(f"Added {len(new_triples)} triples and updated the index.")
+        
+        return modified_classes
+    
+  def append_index(self, file_path, modified_classes):
+      if os.path.exists(file_path):
+          with open(file_path, 'r') as file:
+              current_index = json.load(file)
+      else:
+          current_index = {}
+      
+      for class_uri in modified_classes:
+          if class_uri not in current_index:
+              current_index[class_uri] = list(self.class_index[class_uri])
+          else:
+              current_index[class_uri] = list(set(current_index[class_uri]) | self.class_index[class_uri])
+      
+      with open(file_path, 'w') as file:
+          json.dump(current_index, file)
+      
+      print(f"Index appended to {file_path}")
+      
+  def save_index(self, file_path):
+    current_index = {}
+    
+    for class_uri in self.class_index:
+        if class_uri not in current_index:
+            current_index[class_uri] = list(self.class_index[class_uri])
+        else:
+            current_index[class_uri] = list(set(current_index[class_uri]) | self.class_index[class_uri])
+    
+    with open(file_path, 'w') as file:
+        json.dump(current_index, file)
+        
+  def load_index(self, file_path):
+      if os.path.exists(file_path):
+          with open(file_path, 'r') as file:
+              self.class_index = {k: set(v) for k, v in json.load(file).items()}
+          print(f"Index loaded from {file_path}")
+      else:
+          print(f"No index file found at {file_path}")
 # usage
 if __name__ == "__main__":
   # Define the Blazegraph base URL
   blazegraph_base_url = "http://localhost:8080/blazegraph"
 
   # Define the namespace
-  namespaces = ["namespace_1","namespace_2"]
+  namespaces = ["namespace_kin","namespace_compchem"]
   
   kgs = BuildKGIndex()
   
@@ -109,7 +165,8 @@ if __name__ == "__main__":
     endpoint_url = f"{blazegraph_base_url}/namespace/{namespaces[i]}/sparql"
     print(f"Start processing endpoint: {endpoint_url}")
     kgs.start_processing(endpoint_url)
-  
-  saved_datafile='C:/Users/printer_admin/Downloads/KGs/c2ep_mix_invindex.json'
-  kgs.save_concept2endpoint_invindex(saved_datafile)
+    
+  # current_index=kgs.get_index()
+  saved_datafile='C:/Users/printer_admin/Downloads/KGs/c2ep_invindex.json'
+  kgs.save_index(saved_datafile)
   print(f"Completed and saved in {saved_datafile}.")
