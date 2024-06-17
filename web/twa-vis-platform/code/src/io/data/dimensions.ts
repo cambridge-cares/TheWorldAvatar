cc9486ee63df23d30ff09ccdad213fd0294094a2
Oisín <opeppard@cmcl.io>
2024-06-17 15:49:37 +0100

    dev-credo-vis: remove redundant dimension handling

--- web/twa-vis-platform/code/src/io/data/dimensions.ts
+++
@@ -1,9 +0,0 @@
-// public async fetchScenarioDimensions(): Promise < ScenarioDimensionsData > {
-//     try {
-//         const response = await fetch(`${this.agentBaseURL}/getScenarioTimes/${this.selectedScenario}`);
-//         const data: ScenarioDimensionsData = await response.json();
-//         return data;
-//     } catch(error) {
-//         console.error('Error fetching times from CentralStackAgent/getScenarioTimes:', error);
-//     }
-// }
