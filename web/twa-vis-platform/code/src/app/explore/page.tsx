a394946913d27a7fd5d3b93f6d400fe608956c27
Oisín <opeppard@cmcl.io>
2024-06-19 18:44:58 +0100

    dev-credo-vis: move getScenarios to an API utils file

--- web/twa-vis-platform/code/src/app/explore/page.tsx
+++ web/twa-vis-platform/code/src/app/explore/page.tsx
@@ -1,41 +1,26 @@
-import React from 'react';
 
 import SettingsStore from 'io/config/settings';
 import MapContainer from 'map/map-container';
 import { ScenarioDefinition } from 'types/scenario';
 import { DefaultSettings } from 'types/settings';
+import { getScenarios } from '../../utils/getScenarios';
 
 export const dynamic = 'force-dynamic'
 
-async function getScenarios(scenarioUrl: string): Promise<ScenarioDefinition[]> {
-  const url: string = scenarioUrl + "/getScenarios";
-  let response;
-  let data: ScenarioDefinition[];
-  try {
-    response = await fetch(url, { cache: 'no-store' });
-    data = await response.json();
-    console.log(`Fetching scenarios from URL specified in 'ui-settings': ${scenarioUrl}`)
-    console.log(`Responded with scenarios: `, data)
-  } catch (error) {
-    console.error(`Failed to fetch scenarios from URL specified in 'ui-settings': ${scenarioUrl}\n`, error);
-    return;
-  }
-  return data;
-}
 
 /**
- * A server component that handles the explore  route (i.e. "/explore") to display the map container and its components.
+ * A server component that handles the explore  route (i.e. images/defaultsexplore") to display the map container and its components.
  * 
  */
 export default async function VisualisationPage() {
 
-  
+
   const uiSettings: DefaultSettings = JSON.parse(SettingsStore.getDefaultSettings());
   let scenarios: ScenarioDefinition[];
   // When scenarios are available, retrieve their definitions on the server side
   if (uiSettings.resources?.scenario) {
     try {
-      scenarios = await getScenarios(uiSettings.resources.scenario.url);
+      scenarios = await getScenarios(uiSettings.resources?.scenario?.url);
       scenarios = scenarios.map((scenario) => ({
         ...scenario,
         url: uiSettings.resources.scenario.url,
@@ -51,6 +36,7 @@ export default async function VisualisationPage() {
 
   return (
     <MapContainer
+      scenarioURL={SettingsStore.getDefaultSettings()}
       settings={SettingsStore.getMapSettings()}
       data={SettingsStore.getMapDataSettings()}
       scenarios={scenarios}
