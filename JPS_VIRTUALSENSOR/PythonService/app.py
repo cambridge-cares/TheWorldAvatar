43c36be513fcfb4293f223028ec051bf8f2bd0ce
ChungTingLao <chung.ting.lao.34@gmail.com>
2024-06-20 12:22:13 +0100

    Revert "dev-ship-DOME: call for AERMOD simulation through python service."
    This reverts commit e1066b5f1c08f172c554f8a8af3b4ffde8949353.

--- JPS_VIRTUALSENSOR/PythonService/app.py
+++ JPS_VIRTUALSENSOR/PythonService/app.py
@@ -3,13 +3,11 @@ from flask import Flask
 import speedLoadMap.getEmissions as getEmissions
 import aermod.postprocess_aermod as postprocess_aermod
 import aermod.elevation as elevation
-import dome.newSimulation as newSimulation
 
 app = Flask(__name__)
 app.register_blueprint(getEmissions.getEmissions_bp)
 app.register_blueprint(postprocess_aermod.get_aermod_geojson_bp)
 app.register_blueprint(elevation.get_elevation_geojson_bp)
-app.register_blueprint(newSimulation.get_simulation_geojson_bp)
 
 if __name__ == "__main__":
     app.run(threaded=True)
