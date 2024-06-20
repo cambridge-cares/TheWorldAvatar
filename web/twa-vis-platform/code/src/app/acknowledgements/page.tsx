51521ccf217eea700755c462d43c9a7be4d321af
Oisín <opeppard@cmcl.io>
2024-06-20 15:49:48 +0100

    dev-credo-vis: route to acknowledements graphic

--- web/twa-vis-platform/code/src/app/acknowledgements/page.tsx
+++ web/twa-vis-platform/code/src/app/acknowledgements/page.tsx
@@ -7,7 +7,7 @@ const AcknowledgementsPage: React.FC = () => {
     return (
         <div className={styles.acknowledgementsContainer}>
             <Image
-                src="/images/credo-misc/acknowledgements.svg"
+                src="./images/credo-misc/acknowledgements.svg"
                 width={1750}
                 height={1050}
                 alt="Acknowledgements"
