cdf8f133bf394fdc4cf55ea7e1e2dd497a6cf75e
Dan Tran <nmdt2@cam.ac.uk>
2024-06-28 21:08:40 +0800

    dev-qa-icl: Move tbox info into a separate page

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/_components/additional-info-section.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/_components/additional-info-section.tsx
@@ -15,41 +15,17 @@ export type TBoxInfoTabsProps = React.ComponentPropsWithoutRef<typeof Tabs> & {
   data: { heading: string; mdContent: string }[]
 }
 
-export const TBoxInfoTabs = ({ data, ...props }: TBoxInfoTabsProps) => (
-  <Tabs defaultValue='0' {...props}>
-    <TabsList className='inline-flex mb-2'>
-      {data.map(({ heading }, i) => (
-        <TabsTrigger key={i} value={i.toString()}>
-          {heading}
-        </TabsTrigger>
-      ))}
-    </TabsList>
-    {data.map(({ mdContent }, i) => (
-      <TabsContent key={i} value={i.toString()}>
-        <Markdown className='prose prose-sm max-w-none'>{mdContent}</Markdown>
-      </TabsContent>
-    ))}
-  </Tabs>
-)
 
 export interface AdditionalInfoProps extends React.HTMLAttributes<HTMLElement> {
-  tboxInfoData: { heading: string; mdContent: string }[]
   historyInfoMdContent: string
 }
 
 export const AdditionalInfoSection = ({
-  tboxInfoData,
   historyInfoMdContent,
   ...props
 }: AdditionalInfoProps) => (
   <section {...props}>
     <Accordion type='single' collapsible>
-      <AccordionItem value='tbox_info'>
-        <AccordionTrigger>Information on Chemistry Ontologies</AccordionTrigger>
-        <AccordionContent className='px-4'>
-          <TBoxInfoTabs data={tboxInfoData} />
-        </AccordionContent>
-      </AccordionItem>
       <AccordionItem value='history_info'>
         <AccordionTrigger>History of Marie</AccordionTrigger>
         <AccordionContent className='px-6'>
---
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/ontology-info/page.tsx
@@ -0,0 +1,59 @@
+import path from "path";
+import { promises as fs } from 'fs'
+
+import Markdown from "react-markdown";
+import matter from 'gray-matter'
+
+import { PATH_TO_RESOURCES } from "@/lib/fs";
+
+
+const PATH_TO_TBOX_INFO = path.join(PATH_TO_RESOURCES, 'tbox-info')
+
+export default async function OntologyInfo() {
+  const data = await fs
+    .readdir(PATH_TO_TBOX_INFO, {
+      withFileTypes: true,
+    })
+    .then(dirents =>
+      Promise.all(
+        dirents
+          .filter(dirent => dirent.isFile() && dirent.name.endsWith('.md'))
+          .map(f =>
+            fs
+              .readFile(path.join(PATH_TO_TBOX_INFO, f.name), 'utf-8')
+              .then(content => matter(content))
+              .then(({ data, content }) => ({
+                sortKey: data.sortKey as number,
+                id: data.id as string,
+                heading: data.title as string,
+                mdContent: content,
+              }))
+          )
+      )
+    )
+    .then(data =>
+      data.sort(({ sortKey: keyLeft }, { sortKey: keyRight }) => keyLeft - keyRight)
+    )
+
+  return (
+    <main className='flex flex-col items-center'>
+      <div className="w-full md:max-w-screen-md lg:max-w-screen-lg pt-8 mb-12">
+        <h1 className="mb-8">Information on Chemistry Ontologies</h1>
+        <section className="mb-8">
+          <h2 className="mb-2">Table of Contents</h2>
+          <ol className="list-decimal list-inside">
+            {data.map(({ id, heading }, i) => (
+              <li key={i} className="text-xl"><a href={`#${id}`} className="hover:underline">{heading}</a></li>
+            ))}
+          </ol>
+        </section>
+        {data.map(({ id, heading, mdContent }, i) => (
+          <section key={i} className="mb-12">
+            <h2 id={id} className="mb-2"><a href={`#${id}`} className="hover:underline">{heading}</a></h2>
+            <Markdown className='prose prose-xl max-w-none'>{mdContent}</Markdown>
+          </section>
+        ))}
+      </div>
+    </main>
+  )
+}
