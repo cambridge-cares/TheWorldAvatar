96615b266a861dcca599d713a27b34dcb8ee28f4
Dan Tran <nmdt2@cam.ac.uk>
2024-06-30 19:56:58 +0800

    dev-qa-icl: Allow query species by identifier

--- QuestionAnswering/QA_ICL/backend/fastapi_app/model/web/ontospecies.py
+++ QuestionAnswering/QA_ICL/backend/fastapi_app/model/web/ontospecies.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
 
 from model.web.comp_op import ComparisonOperator
-from model.kg.ontospecies import SpeciesPropertyKey
+from model.kg.ontospecies import SpeciesIdentifierKey, SpeciesPropertyKey
 
 
 class SpeciesRequest(BaseModel):
     chemical_class: list[str]
     use: list[str]
+    identifier: dict[SpeciesIdentifierKey, str]
     property: dict[SpeciesPropertyKey, list[tuple[ComparisonOperator, float]]]
