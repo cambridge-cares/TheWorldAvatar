93ce657967dd3c20927088cb696edf13a30d6345
Dan Tran <nmdt2@cam.ac.uk>
2024-06-30 15:04:03 +0800

    dev-qa-icl: Refactor combobox

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/species/_components/species-form.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/species/_components/species-form.tsx
@@ -1,18 +1,19 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import * as React from 'react'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
-import { ChemicalClass, Use } from "@/lib/model/ontospecies"
-import { CheckIcon, ChevronDownIcon } from "@radix-ui/react-icons"
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
-
+import { ChemicalClass, Use } from '@/lib/model/ontospecies'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form'
+import { Combobox } from '@/components/ui/combobox'
 
 export interface SpeciesFormProps {
   allChemicalClasses: ChemicalClass[]
@@ -20,92 +21,45 @@ export interface SpeciesFormProps {
 }
 
 const formSchema = z.object({
-  chemicalClass: z.union([z.string(), z.undefined()])
+  chemicalClass: z.union([z.string(), z.undefined()]),
 })
 
 export function SpeciesForm({ allChemicalClasses, allUses }: SpeciesFormProps) {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      chemicalClass: undefined
-    }
+      chemicalClass: undefined,
+    },
   })
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
   }
 
-  const [open, setOpen] = React.useState(false)
-  const chemicalClass_iri2label = React.useMemo(() => Object.fromEntries(allChemicalClasses.map(({IRI, label}) => [IRI, label])), [allChemicalClasses])
-  const chemicalClass_iri2labelLower = React.useMemo(() => Object.fromEntries(Object.entries(chemicalClass_iri2label).map(([IRI, label]) => [IRI, label.toLowerCase()])), [chemicalClass_iri2label])
-
-  console.log("Form")
-
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
         <FormField
           control={form.control}
-          name="chemicalClass"
+          name='chemicalClass'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Chemical class</FormLabel>
               <FormControl>
-                <Popover open={open} onOpenChange={setOpen}>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      aria-expanded={open}
-                      className="w-[200px] justify-between"
-                    >
-                      {field.value ? chemicalClass_iri2label[field.value] : "Select chemical class..."}
-                      <ChevronDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[200px] p-0">
-                    <Command
-                      filter={(value, search) => {
-                        return chemicalClass_iri2labelLower[value].includes(search.toLowerCase()) ? 1 : 0
-                      }}
-                    >
-                      <CommandInput placeholder="Search framework..." className="h-9" />
-                      <CommandList>
-                        <CommandEmpty>No chemical class found.</CommandEmpty>
-                        <CommandGroup>
-                          {allChemicalClasses.map(({ IRI, label }, i) => (
-                            <CommandItem
-                              key={i}
-                              value={IRI}
-                              onSelect={(val) => {
-                                if (field.value === val) {
-                                  field.onChange(undefined)
-                                } else {
-                                  field.onChange(val)
-                                }
-                                setOpen(false)
-                              }}
-                            >
-                              {label}
-                              <CheckIcon
-                                className={cn(
-                                  "ml-auto h-4 w-4",
-                                  field.value === IRI ? "opacity-100" : "opacity-0"
-                                )}
-                              />
-                            </CommandItem>
-                          ))}
-                        </CommandGroup>
-                      </CommandList>
-                    </Command>
-                  </PopoverContent>
-                </Popover>
+                <Combobox
+                  itemCls='chemical class'
+                  value={field.value}
+                  setValue={field.onChange}
+                  items={allChemicalClasses.map(({ IRI, label }) => ({
+                    value: IRI,
+                    label,
+                  }))}
+                />
               </FormControl>
             </FormItem>
           )}
-        >
-        </FormField>
+        ></FormField>
       </form>
     </Form>
   )
-} 
+}
---
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/components/ui/combobox.tsx
@@ -0,0 +1,95 @@
+'use client'
+
+import * as React from 'react'
+import { CheckIcon, ChevronDownIcon } from '@radix-ui/react-icons'
+
+import { cn } from '@/lib/utils'
+import { Button } from './button'
+import { Popover, PopoverContent, PopoverTrigger } from './popover'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from './command'
+
+export interface ComboboxProps {
+  itemCls: string
+  value: string | undefined
+  setValue: (value: string | undefined) => void
+  items: { value: string; label: string }[]
+}
+
+export function Combobox({ itemCls, value, setValue, items }: ComboboxProps) {
+  const [open, setOpen] = React.useState(false)
+  const value2label = React.useMemo(
+    () => Object.fromEntries(items.map(({ value, label }) => [value, label])),
+    [items]
+  )
+  const value2labelLower = React.useMemo(
+    () =>
+      Object.fromEntries(
+        Object.entries(value2label).map(([value, label]) => [
+          value,
+          label.toLowerCase(),
+        ])
+      ),
+    [value2label]
+  )
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant='outline'
+          role='combobox'
+          aria-expanded={open}
+          className='w-[200px] justify-between'
+        >
+          {value ? value2label[value] : `Select ${itemCls}...`}
+          <ChevronDownIcon className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='w-[200px] p-0'>
+        <Command
+          filter={(itemValue, search) => {
+            return value2labelLower[itemValue].includes(search.toLowerCase())
+              ? 1
+              : 0
+          }}
+        >
+          <CommandInput placeholder={`Search ${itemCls}`} className='h-9' />
+          <CommandList>
+            <CommandEmpty>{`No ${itemCls} found`}</CommandEmpty>
+            <CommandGroup>
+              {items.map(({ value: itemValue, label }, i) => (
+                <CommandItem
+                  key={i}
+                  value={itemValue}
+                  onSelect={itemValue => {
+                    if (itemValue === value) {
+                      setValue(undefined)
+                    } else {
+                      setValue(itemValue)
+                    }
+                    setOpen(false)
+                  }}
+                >
+                  {label}
+                  <CheckIcon
+                    className={cn(
+                      'ml-auto h-4 w-4',
+                      value === itemValue ? 'opacity-100' : 'opacity-0'
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
