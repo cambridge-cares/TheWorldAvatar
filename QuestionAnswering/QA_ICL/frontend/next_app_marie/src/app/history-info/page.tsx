df9fce625f01abc806560e0f1e033bdf2d60e189
Dan Tran <nmdt2@cam.ac.uk>
2024-06-30 21:37:56 +0800

    dev-qa-icl: Refactor md page

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/history-info/page.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/history-info/page.tsx
@@ -1,10 +1,10 @@
 import path from 'path'
 import { promises as fs } from 'fs'
 
-import Markdown from 'react-markdown'
 
 import { PATH_TO_RESOURCES } from '@/lib/fs'
 import { Main } from '@/components/ui/main'
+import { MarkdownStyled } from '@/components/ui/markdown'
 
 const PATH_TO_HISTORY_INFO = path.join(PATH_TO_RESOURCES, 'history.md')
 export default async function HistoryInfo() {
@@ -14,24 +14,9 @@ export default async function HistoryInfo() {
     <Main className='flex flex-col items-center'>
       <div className='w-full md:max-w-screen-md lg:max-w-screen-lg mb-12 px-4'>
         <h1 className='pt-8 mb-8'>History of Marie</h1>
-        <Markdown
-          components={{
-            h2(props) {
-              const { children, ...rest } = props
-              const id = children?.toString().toLowerCase()
-              return (
-                <h2 {...rest} id={id}>
-                  <a href={`#${id}`} className='hover:underline'>
-                    {children}
-                  </a>
-                </h2>
-              )
-            },
-          }}
-          className='prose-lg max-w-none prose-ol:list-decimal'
-        >
+        <MarkdownStyled>
           {mdContent}
-        </Markdown>
+        </MarkdownStyled>
       </div>
     </Main>
   )
