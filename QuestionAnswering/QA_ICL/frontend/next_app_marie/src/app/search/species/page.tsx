9e0a5457e150adfa9bac4e1c96eb06fa0997d72b
Dan Tran <nmdt2@cam.ac.uk>
2024-07-02 11:27:20 +0800

    dev-qa-icl: Add suspense boundaries

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/search/species/page.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/search/species/page.tsx
@@ -2,6 +2,7 @@ import { getChemicalClasses, getUses } from '@/lib/api/ontospecies'
 import { SpeciesForm } from './_components/form'
 import { Main } from '@/components/ui/main'
 import { SpeciesSearchResults } from './_components/results'
+import { Suspense } from 'react'
 
 export default async function SpeciesPage() {
   const [chemicalClasses, uses] = await Promise.all([
@@ -13,12 +14,16 @@ export default async function SpeciesPage() {
     <Main className='flex flex-col items-center'>
       <div className='w-full mt-8 px-4 md:max-w-screen-md lg:max-w-screen-lg'>
         <h1 className='mb-4'>Species search</h1>
-        <SpeciesForm
-          allChemicalClasses={chemicalClasses}
-          allUses={uses}
-          className='mb-12'
-        />
-        <SpeciesSearchResults />
+        <Suspense>
+          <SpeciesForm
+            allChemicalClasses={chemicalClasses}
+            allUses={uses}
+            className='mb-12'
+          />
+        </Suspense>
+        <Suspense>
+          <SpeciesSearchResults />
+        </Suspense>
       </div>
     </Main>
   )
