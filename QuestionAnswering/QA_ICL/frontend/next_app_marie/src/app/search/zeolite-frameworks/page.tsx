697c5f7bc5ebda2034c8ba45e30e27a4466f94d0
Dan Tran <nmdt2@cam.ac.uk>
2024-07-02 11:27:20 +0800

    dev-qa-icl: Add suspense boundaries

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/search/zeolite-frameworks/page.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/search/zeolite-frameworks/page.tsx
@@ -2,6 +2,7 @@ import { ZeoliteFrameworkForm } from './_components/form'
 import { Main } from '@/components/ui/main'
 import { getCBUs, getSBUs } from '@/lib/api/ontozeolite'
 import { ZeoliteFrameworkResults } from './_components/results'
+import { Suspense } from 'react'
 
 export default async function ZeoliteFrameworksPage() {
   const [allCBUs, allSBUs] = await Promise.all([getCBUs(), getSBUs()])
@@ -10,12 +11,16 @@ export default async function ZeoliteFrameworksPage() {
     <Main className='flex flex-col items-center'>
       <div className='w-full mt-8 px-4 md:max-w-screen-md lg:max-w-screen-lg'>
         <h1 className='mb-4'>Zeolite framework search</h1>
-        <ZeoliteFrameworkForm
-          allCBUs={allCBUs}
-          allSBUs={allSBUs}
-          className='mb-12'
-        />
-        <ZeoliteFrameworkResults />
+        <Suspense>
+          <ZeoliteFrameworkForm
+            allCBUs={allCBUs}
+            allSBUs={allSBUs}
+            className='mb-12'
+          />
+        </Suspense>
+        <Suspense>
+          <ZeoliteFrameworkResults />
+        </Suspense>
       </div>
     </Main>
   )
