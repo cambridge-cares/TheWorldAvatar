b3a5d84978d1009d4e4308157227129cc7f6fe65
Kok Foong Lee <kfl31@cam.ac.uk>
2024-07-04 14:54:22 +0800

    1818-dev-sealevelimpactagent-fix: add a label for >25 for cost colour bar

--- Deploy/stacks/Singapore-sea-level-rise/colorbar_generator/colourbar.py
+++ Deploy/stacks/Singapore-sea-level-rise/colorbar_generator/colourbar.py
@@ -7,11 +7,11 @@ import json
 
 def create_colors():
     start = 0
-    end = 2.5 * 10 ** 7
-    num_points = 11
+    end = 3.0 * 10 ** 7
+    num_points = 12
+    ticks = [0, 6/1.1, 12/1.1, 18/1.1, 24/1.1, 30/1.1, 30]
 
     array_for_mapbox_expression = np.linspace(start, end, num_points).tolist()
-    array_for_mapbox_expression.append(1.1 * 10**10)
     array_for_colorbar = [x / (10.0 ** 6) for x in array_for_mapbox_expression]
 
     fig, ax = plt.subplots(figsize=(6, 1))
@@ -28,9 +28,10 @@ def create_colors():
     norm = mpl.colors.BoundaryNorm(array_for_colorbar, cmap.N)
 
     cb2 = mpl.colorbar.ColorbarBase(ax, cmap=cmap,
-                                    norm=norm,
+                                    norm=norm, ticks=ticks,
                                     orientation='horizontal')
     cb2.set_label('Estimated construction cost (million SGD)')
+    cb2.ax.set_xticklabels(['0', '5', '10', '15', '20', '25', '>25'])
     plt.savefig("cost.png", bbox_inches='tight',
                 transparent=True, dpi=300)
 
@@ -45,7 +46,7 @@ def generate_mapbox_expressions(input_dict):
             value
         ])
     expression.append("#8D99AE")  # default colour
-    f = open("mapbox_expression.txt", "w")
+    f = open("mapbox_expression_cost.txt", "w")
     f.write(json.dumps(expression))
     f.close()
 
