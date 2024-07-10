b828df8aee4586f31e89b645c1eb4cd8c7be61d7
Wilson Ang <wilson1010x@gmail.com>
2024-07-10 17:21:26 +0800

    1807-dev-pips-agent: Modify PIPSTSAgentAPIConnector to include option for including client certificate in request

--- Agents/PIPSRequestAgent/PIPSRequestAgent/src/main/java/uk/ac/cam/cares/jps/agent/pips/PIPSTSAgentAPIConnector.java
+++ Agents/PIPSRequestAgent/PIPSRequestAgent/src/main/java/uk/ac/cam/cares/jps/agent/pips/PIPSTSAgentAPIConnector.java
@@ -12,21 +12,45 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.google.gson.JsonObject;
+
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+
 import java.io.*;
+
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
 public class PIPSTSAgentAPIConnector {
     private static final Logger LOGGER = LogManager.getLogger(PIPSTSAgentAPIConnector.class);
 
     // variables
     private String pipsAgentTimeSeriesPath;
+    private static final String CERT_FILE_ENV = "CLIENT_CERT";
+    private static final String CERT_PASSWORD_ENV = "CLIENT_CERT_PASSWORD";
 
     // error messages
-    private static final String dataRetrievalError = "Could not retrieve timeseries data!";
+    private static final String RETRIEVE_CLIENT_CERT_ERROR_MSG = "Could not retrieve client certificate. ";
+    private static final String SEND_CLIENT_CERT_ERROR_MSG = "Could not send request loaded with client certificate. ";
+
+    // error variables
+    private static final String JSON_ERROR_KEY = "Error";
 
     /**
      * Constructor for PIPSTSAgentAPIConnector
+     * @throws IOException 
+     * @throws FileNotFoundException 
+     * @throws KeyStoreException 
+     * @throws CertificateException 
+     * @throws NoSuchAlgorithmException 
      */
-    public PIPSTSAgentAPIConnector() {
+    public PIPSTSAgentAPIConnector(){
         pipsAgentTimeSeriesPath = System.getenv("PIPS_AGENT_TIMESERIES_PATH");
     }
 
@@ -36,8 +60,8 @@ public class PIPSTSAgentAPIConnector {
      * @throws JSONException
      * @throws IOException
      */
-    public JSONObject getTimeSeries(String accessToken) throws JSONException, IOException {
-        return retrieveTimeSeriesData(accessToken);
+    public JSONObject getTimeSeries(String accessToken, String source, int num, Boolean include_client_cert) throws JSONException, IOException {
+        return retrieveTimeSeriesData(accessToken, source, num, include_client_cert);
     }
 
     /**
@@ -46,20 +70,62 @@ public class PIPSTSAgentAPIConnector {
      * @return response from pips-timeseries-agent
      * @throws IOException
      */
-    private JSONObject retrieveTimeSeriesData(String accessToken) throws IOException {
-        LOGGER.info("Sending request to pips-timeseries-agent...");
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpGet readingRequest = new HttpGet(pipsAgentTimeSeriesPath);
-            setTokenAuthorization(readingRequest, accessToken);
+    private JSONObject retrieveTimeSeriesData(String accessToken, String source, int num, Boolean include_client_cert) throws IOException {
+        pipsAgentTimeSeriesPath = pipsAgentTimeSeriesPath + "?source=" + source + "&num=" + num;
+        if (!include_client_cert) {
+            LOGGER.info("Attempting to send request without client certificates...");
+            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+                HttpGet readingRequest = new HttpGet(pipsAgentTimeSeriesPath);
+                setTokenAuthorization(readingRequest, accessToken);
+    
+                try (CloseableHttpResponse response = httpClient.execute(readingRequest)) {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status == 200) {
+                        return new JSONObject(EntityUtils.toString(response.getEntity()));
+                    }
+                    else {
+                        throw new HttpResponseException(status, response.getEntity().toString());
+                    }
+                }
+            }
+        } else {
+            // Load the client certificate
+            KeyStore keyStore;
+            SSLContextBuilder sslContextBuilder = SSLContextBuilder.create();
+            try {
+                keyStore = KeyStore.getInstance("PKCS12");
+                try (FileInputStream keyStoreStream = new FileInputStream(System.getenv(CERT_FILE_ENV))) {
+                    keyStore.load(keyStoreStream, readFromFile(System.getenv(CERT_PASSWORD_ENV)).toCharArray());
+                }
+                // Create SSLContext with the client certificate
+                sslContextBuilder.loadKeyMaterial(keyStore, readFromFile(System.getenv(CERT_PASSWORD_ENV)).toCharArray());
+            } catch (Exception e) {
+                throw new JPSRuntimeException(RETRIEVE_CLIENT_CERT_ERROR_MSG, e);
+            }
 
-            try (CloseableHttpResponse response = httpclient.execute(readingRequest)) {
-                int status = response.getStatusLine().getStatusCode();
-                if (status == 200) {
-                    return new JSONObject(EntityUtils.toString(response.getEntity()));
+            // Create HttpClient with the SSLContext
+            try {
+                LOGGER.info("Attempting to send request with client certificates...");
+                CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLContext(sslContextBuilder.build())
+                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                .build();
+                HttpGet readingRequest = new HttpGet(pipsAgentTimeSeriesPath);
+                setTokenAuthorization(readingRequest, accessToken);
+    
+                try (CloseableHttpResponse response = httpClient.execute(readingRequest)) {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status == 200) {
+                        return new JSONObject(EntityUtils.toString(response.getEntity()));
+                    }
+                    else {
+                        JSONObject error_response = new JSONObject();
+                        error_response.put(JSON_ERROR_KEY, response.getStatusLine().getReasonPhrase());
+                        return error_response;
+                    }
                 }
-                else {
-                    throw new HttpResponseException(status, response.getEntity().toString());
-                }
+            } catch (Exception e) {
+                throw new JPSRuntimeException(SEND_CLIENT_CERT_ERROR_MSG, e);
             }
         }
     }
@@ -71,6 +137,25 @@ public class PIPSTSAgentAPIConnector {
     private void setTokenAuthorization(HttpRequest request, String accessToken) {
         String authHeader = "Bearer " + accessToken;
         request.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
+    }
+
+    /**
+     * read content of files
+     * @param filePath file path
+     * @return content of the file
+     * @throws IOException
+     */
+    private String readFromFile(String filePath) throws IOException {
+        String content = null;
+        try (DataInputStream reader = new DataInputStream(new FileInputStream(filePath))) {
+            int nBytesToRead = reader.available();
+            if(nBytesToRead > 0) {
+                byte[] bytes = new byte[nBytesToRead];
+                reader.read(bytes);
+                content = new String(bytes);
+            }
+        }
+        return content;
     }
 
 }
