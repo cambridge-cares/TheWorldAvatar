ef2715ac9f4c0a2639c9214eab2a96e3e34e9023
Oisín <opeppard@cmcl.io>
2024-07-10 17:55:08 +0100

    dev-credo-vis: add scenario types to ScenarioDefinition type

--- web/twa-vis-platform/code/src/types/scenario.ts
+++ web/twa-vis-platform/code/src/types/scenario.ts
@@ -1,10 +1,11 @@
 import { ScenarioDimensionsData } from "./timeseries";
 
 export type ScenarioDefinition = {
-  id: string;
   name: string;
   description: string;
+  id: string;
   url?: string;
   dataset?: string;
+  type: 'Heat Event' | 'Flood Event' | 'Probabilistic Heat Event';
   dimensions: ScenarioDimensionsData
 };
