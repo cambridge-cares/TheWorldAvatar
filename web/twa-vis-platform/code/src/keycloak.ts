3981f78a4592dd22b4109c69e66e7f419e31390f
Oisín <opeppard@cmcl.io>
2024-07-11 17:47:12 +0100

    rm keycloak front channel stuff

--- web/twa-vis-platform/code/src/keycloak.ts
+++
@@ -1,9 +0,0 @@
-import Keycloak from 'keycloak-js'
-
-const keycloak = new Keycloak({
-    url: "https://idm-credo.hartree.app",
-    realm: "MFATest",
-    clientId: "credo-app",
-});
-
-export default keycloak
