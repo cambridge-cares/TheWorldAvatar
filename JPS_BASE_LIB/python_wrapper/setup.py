1dd846d16f0909e3d641dd5b06165a37e794f0d3
Jiaru Bai <jb2197@cam.ac.uk>
2024-07-11 14:27:15 +0100

    dev-derivation-ogm: made stable release twa==0.0.3 to PyPI.

--- JPS_BASE_LIB/python_wrapper/setup.py
+++ JPS_BASE_LIB/python_wrapper/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='twa',
-    version='0.0.2',
+    version='0.0.3',
     author='Jiaru Bai; Daniel Nurkowski',
     author_email='jb2197@cam.ac.uk; danieln@cmclinnovations.com',
     license='MIT',
