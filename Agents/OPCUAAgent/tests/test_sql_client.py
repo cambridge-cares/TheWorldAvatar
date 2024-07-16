e30576ab6677d370c830416ead37067df922d14b
Wilson Ang <wilson1010x@gmail.com>
2024-07-16 18:02:50 +0800

    1821-dev-ample-client: add additional tests to test_sql_client.py

--- Agents/OPCUAAgent/tests/test_sql_client.py
+++ Agents/OPCUAAgent/tests/test_sql_client.py
@@ -18,11 +18,21 @@ class Test_sql_client:
                                               'host=postgres_agent_test\n' +
                                               'port=5432\n')
         
-    def setUp_fail(self):
+    def setUp_fail_non_existent_db(self):
         self._temp_dir = tempfile.TemporaryDirectory()
         self.temp_path = pathlib.Path(self._temp_dir.name)
         self._create_temporary_file_with_data(self.temp_path / 'test_conf.properties',
                                               'dbname=non_existent_db\n' +
+                                              'user=postgres\n' +
+                                              'password=postgres\n' +
+                                              'host=postgres_agent_test\n' +
+                                              'port=5432\n')
+        
+    def setUp_new_db(self):
+        self._temp_dir = tempfile.TemporaryDirectory()
+        self.temp_path = pathlib.Path(self._temp_dir.name)
+        self._create_temporary_file_with_data(self.temp_path / 'test_conf.properties',
+                                              'dbname=test\n' +
                                               'user=postgres\n' +
                                               'password=postgres\n' +
                                               'host=postgres_agent_test\n' +
@@ -34,9 +44,29 @@ class Test_sql_client:
 
     def tearDown(self):
         self._temp_dir.cleanup()
+        
+    def test_create_database_if_not_exist_fail(self):
+        self.setUp_new_db()
+        #sleep test to wait for postgresql container to spin up
+        time.sleep(5)
+        with pytest.raises(Exception) as excinfo:
+            sql_client.create_database_if_not_exist()
+        assert "Error while connecting to PostgreSQL, do check the environment variable and properties file..." in str(excinfo.value)
+        self.tearDown()
+            
+        
+    def test_create_database_if_not_exist_success(self):
+        self.setUp_new_db()
+        #sleep test to wait for postgresql container to spin up
+        time.sleep(5)
+        with patch.dict(os.environ, {"POSTGRES_CONF":str(self.temp_path / 'test_conf.properties')}, clear=True):
+            sql_client.create_database_if_not_exist()
+            connection = sql_client.connect_to_database()
+        assert connection != None
+        assert "test" == connection.info.dbname
           
     def test_connect_to_database_fail(self):
-        self.setUp_fail()
+        self.setUp_fail_non_existent_db()
         #sleep test to wait for postgresql container to spin up
         time.sleep(5)
         with patch.dict(os.environ, {"POSTGRES_CONF":str(self.temp_path / 'test_conf.properties')}, clear=True):
@@ -44,3 +74,14 @@ class Test_sql_client:
                 sql_client.connect_to_database()
             assert "FATAL:  database \"non_existent_db\" does not exist" in str(excinfo.value)
         self.tearDown()
+        
+    def test_connect_to_database_success(self):
+        self.setUp()
+        #sleep test to wait for postgresql container to spin up
+        time.sleep(5)
+        with patch.dict(os.environ, {"POSTGRES_CONF":str(self.temp_path / 'test_conf.properties')}, clear=True):
+            connection = sql_client.connect_to_database()
+        assert connection != None
+        assert "postgres" == connection.info.dbname
+        self.tearDown()
+    
