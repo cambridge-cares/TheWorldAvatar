5f1bb70613f9a3921a7d380c6f0ff8d783331f56
Dan Tran <nmdt2@cam.ac.uk>
2024-07-17 13:06:41 +0800

    dev-qa-icl: Style intro text

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/_components/intro-section.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/_components/intro-section.tsx
@@ -30,7 +30,7 @@ export const IntroSection = async ({
     </div>
     <div className='w-full content-center space-y-2 md:col-span-2 lg:col-span-3'>
       <h1>{heading}</h1>
-      <Markdown className='prose max-w-none prose-sm prose-slate'>
+      <Markdown className='prose max-w-none prose-slate prose-p:leading-6 prose-li:leading-6'>
         {mdContent}
       </Markdown>
     </div>
