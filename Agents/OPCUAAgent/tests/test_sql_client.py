64296a6ed7857f4de1b2d70c9fc2718e862ae4ae
Wilson Ang <wilson1010x@gmail.com>
2024-07-17 14:18:36 +0800

    1821-dev-ample-client: Add test to test_sql_client.py and modify sleep period for tests

--- Agents/OPCUAAgent/tests/test_sql_client.py
+++ Agents/OPCUAAgent/tests/test_sql_client.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timezone
+import datetime
 import pytest
 import tempfile
 import os
 import pathlib
 from unittest.mock import patch
 import time
+from psycopg2 import sql
 
 from OPCUAAgent import sql_client
 
@@ -48,17 +51,16 @@ class Test_sql_client:
     def test_create_database_if_not_exist_fail(self):
         self.setUp_new_db()
         #sleep test to wait for postgresql container to spin up
-        time.sleep(5)
+        time.sleep(3)
         with pytest.raises(Exception) as excinfo:
             sql_client.create_database_if_not_exist()
         assert "Error while connecting to PostgreSQL, do check the environment variable and properties file..." in str(excinfo.value)
         self.tearDown()
             
-        
     def test_create_database_if_not_exist_success(self):
         self.setUp_new_db()
         #sleep test to wait for postgresql container to spin up
-        time.sleep(5)
+        time.sleep(3)
         with patch.dict(os.environ, {"POSTGRES_CONF":str(self.temp_path / 'test_conf.properties')}, clear=True):
             sql_client.create_database_if_not_exist()
             connection = sql_client.connect_to_database()
@@ -68,7 +70,7 @@ class Test_sql_client:
     def test_connect_to_database_fail(self):
         self.setUp_fail_non_existent_db()
         #sleep test to wait for postgresql container to spin up
-        time.sleep(5)
+        time.sleep(3)
         with patch.dict(os.environ, {"POSTGRES_CONF":str(self.temp_path / 'test_conf.properties')}, clear=True):
             with pytest.raises(Exception) as excinfo:
                 sql_client.connect_to_database()
@@ -78,10 +80,28 @@ class Test_sql_client:
     def test_connect_to_database_success(self):
         self.setUp()
         #sleep test to wait for postgresql container to spin up
-        time.sleep(5)
+        time.sleep(3)
         with patch.dict(os.environ, {"POSTGRES_CONF":str(self.temp_path / 'test_conf.properties')}, clear=True):
             connection = sql_client.connect_to_database()
         assert connection != None
         assert "postgres" == connection.info.dbname
+        self.tearDown()
+        
+    def test_create_if_not_exist_and_insert(self):
+        self.setUp_new_db()
+        time.sleep(3)
+        with patch.dict(os.environ, {"POSTGRES_CONF":str(self.temp_path / 'test_conf.properties')}, clear=True):
+            sql_client.create_database_if_not_exist()
+            connection = sql_client.connect_to_database()
+            timestamp = datetime.datetime.now(timezone.utc).isoformat(timespec='seconds')
+            values_dict = {"testing":{"tag_01":{"timestamp":timestamp, "value":123, "data_type":"Float"}, "tag_02":{"timestamp":timestamp, "value":12, "data_type":"Float"}}}
+            sql_client.create_if_not_exist_and_insert(connection, values_dict)
+        
+        cursor = connection.cursor()
+        cursor.execute(
+                    sql.SQL("SELECT * FROM {}.{}").format(
+                    sql.Identifier("opcua_pips"), sql.Identifier("testing")))
+        existing_record = cursor.fetchall()
+        assert 1 == len(existing_record)
         self.tearDown()
     
