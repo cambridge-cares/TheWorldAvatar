5957fa942c55d3f9a41e657ee533551fbce83755
Dan Tran <nmdt2@cam.ac.uk>
2024-07-17 13:00:07 +0800

    dev-qa-icl: Rename "MarkdownStyled" to "Prose"

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/history-info/page.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/history-info/page.tsx
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs'
 
 import { PATH_TO_RESOURCES } from '@/lib/fs'
 import { Main } from '@/components/ui/main'
-import { MarkdownStyled } from '@/components/ui/markdown'
+import { Prose } from '@/components/ui/prose'
 
 const PATH_TO_HISTORY_INFO = path.join(PATH_TO_RESOURCES, 'history.md')
 export default async function HistoryInfo() {
@@ -13,7 +13,7 @@ export default async function HistoryInfo() {
     <Main className='flex flex-col items-center'>
       <div className='w-full md:max-w-screen-md lg:max-w-screen-lg mb-12 px-4'>
         <h1 className='pt-8 mb-8'>History of Marie</h1>
-        <MarkdownStyled>{mdContent}</MarkdownStyled>
+        <Prose>{mdContent}</Prose>
       </div>
     </Main>
   )
--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/components/ui/prose.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/components/ui/prose.tsx
@@ -5,14 +5,14 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 import { cn } from '@/lib/utils'
 
-export const MarkdownStyled = ({
+export const Prose = ({
   className,
   ...props
 }: React.ComponentProps<typeof Markdown>) => (
   <Markdown
     rehypePlugins={[rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]]}
     className={cn(
-      'prose max-w-none prose-xl prose-h2:text-2xl prose-h3:text-xl prose-ol:list-decimal prose-a:no-underline hover:prose-a:underline',
+      'prose max-w-none prose-lg prose-h2:text-2xl prose-h3:text-xl prose-ol:list-decimal prose-a:no-underline hover:prose-a:underline',
       className
     )}
     {...props}
