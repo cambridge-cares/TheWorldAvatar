98a96f619fa25b2a74845f3b0314f37d75fd6bf6
Dan Tran <nmdt2@cam.ac.uk>
2024-07-19 09:02:57 +0800

    dev-qa-icl: Plot XRD peaks

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/components/ui/mol-viewer.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/components/ui/mol-viewer.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { cn } from '@/lib/utils'
 import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+
 export interface MolViewerProps extends React.HTMLAttributes<HTMLDivElement> {
   type: 'xyz' | 'cif'
   data: string
