c5fdda9e1d1884c26de9351f3bd0758cbea7edac
Dan Tran <nmdt2@cam.ac.uk>
2024-07-19 10:08:44 +0800

    dev-qa-icl: Link zeolite framework page to search results

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/search/zeolite-frameworks/_components/results.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/search/zeolite-frameworks/_components/results.tsx
@@ -7,10 +7,24 @@ import { createColumnHelper } from '@tanstack/react-table'
 import { ZeoliteFrameworkBase } from '@/lib/model/ontozeolite'
 import { getZeoliteFrameworks } from '@/lib/api/ontozeolite'
 import { DataTable } from '@/components/ui/data-table'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 
 const COL_HELPER = createColumnHelper<ZeoliteFrameworkBase>()
 const COLS = [
-  COL_HELPER.accessor('IRI', { header: 'IRI' }),
+  COL_HELPER.accessor('IRI', {
+    header: '', cell: cell => {
+      const val = cell.getValue()
+      return (
+        <Link
+          href={`/zeolite-frameworks/${encodeURIComponent(val)}`}
+          className='hover:underline'
+        >
+          <Button variant='secondary'>View</Button>
+        </Link>
+      )
+    }
+  }),
   COL_HELPER.accessor('code', { header: 'Code' }),
 ]
 
