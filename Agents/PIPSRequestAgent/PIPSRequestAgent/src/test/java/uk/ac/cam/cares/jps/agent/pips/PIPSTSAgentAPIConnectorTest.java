8294e1aed9f4998ce03bcfc03c2689cc96ab266a
Wilson Ang <wilson1010x@gmail.com>
2024-07-23 17:42:16 +0800

    1807-dev-pips-agent: Modify PIPSTSAgentAPIConnectorTest based on changes to Docker deployment files

--- Agents/PIPSRequestAgent/PIPSRequestAgent/src/test/java/uk/ac/cam/cares/jps/agent/pips/PIPSTSAgentAPIConnectorTest.java
+++ Agents/PIPSRequestAgent/PIPSRequestAgent/src/test/java/uk/ac/cam/cares/jps/agent/pips/PIPSTSAgentAPIConnectorTest.java
@@ -6,44 +6,31 @@ import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.model.Parameters;
-import org.testcontainers.containers.MockServerContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 import static uk.org.webcompere.systemstubs.SystemStubs.withEnvironmentVariable;
 
-@Testcontainers
 public class PIPSTSAgentAPIConnectorTest {
 
-    public static final DockerImageName MOCKSERVER_IMAGE = DockerImageName
-            .parse("mockserver/mockserver")
-            .withTag("mockserver-" + MockServerClient.class.getPackage().getImplementationVersion());
+    private static final String MOCKSERVER_HOST = "test_pips_request_agent-mockserver-1";
+    private static final int MOCKSERVER_PORT = 1080;
+
+    //public static final DockerImageName MOCKSERVER_IMAGE = DockerImageName
+            //.parse("mockserver/mockserver")
+            //.withTag("mockserver-" + MockServerClient.class.getPackage().getImplementationVersion());
 
     @ClassRule
     public static TemporaryFolder folder = new TemporaryFolder();
-    public static MockServerContainer mockWacnetServer = new MockServerContainer(MOCKSERVER_IMAGE);
+    //public static MockServerContainer mockWacnetServer = new MockServerContainer(MOCKSERVER_IMAGE);
     private static PIPSTSAgentAPIConnector pipsTsAgentAPIConnector;
 
-    @Before
-    public void setUp() throws IOException {
-        try {
-            mockWacnetServer.start();
-        } catch (Exception e) {
-            throw new AssertionError("IntegrationTest: Docker container startup failed. Please try running tests again");
-        }
-    }
 
-    @After
-    public void tearDown() throws InterruptedException {
-        mockWacnetServer.close();
-    }
 
     private void setupMockPIPSTSAgent(MockServerClient agent) {
         Map<String, List<String>> entries = new HashMap<>();
@@ -55,10 +42,15 @@ public class PIPSTSAgentAPIConnectorTest {
         entries.put("num", num);
         agent.when(request().withMethod("GET").withPath("/pips-timeseries-agent/timeseries").withQueryStringParameters(new Parameters().withEntries(entries)).withHeader(HttpHeaders.AUTHORIZATION, "Bearer test_token"))
         .respond(response().withStatusCode(200).withBody("{\"test_module\":[{\"temperature_A\":10.0, \"timestamp\": \"2024-07-09T16:41:53+08:00\"}]}"));
+    }
 
+    private void setupMockPIPSTSAgentForFailed(MockServerClient agent) throws InterruptedException {
         Map<String, List<String>> incorrectEntries = new HashMap<>();
         ArrayList<String> incorrectSource = new ArrayList<String>();
+        ArrayList<String> source = new ArrayList<String>();
         source.add("wrong_source");
+        ArrayList<String> num = new ArrayList<String>();
+        num.add("1");
         incorrectEntries.put("source", incorrectSource);
         incorrectEntries.put("num", num);
 
@@ -66,11 +58,13 @@ public class PIPSTSAgentAPIConnectorTest {
         .respond(response().withStatusCode(200).withBody("{\"Result\":\"No data retrieved...\"}"));
     }
 
+
     @Test
     public void testGetTimeSeriesSuccess() throws Exception {
-        try (MockServerClient pipsTsAgent = new MockServerClient(mockWacnetServer.getHost(), mockWacnetServer.getServerPort())) {
+        TimeUnit.SECONDS.sleep(10);
+        try (MockServerClient pipsTsAgent = new MockServerClient(MOCKSERVER_HOST, MOCKSERVER_PORT)) {
             setupMockPIPSTSAgent(pipsTsAgent);
-            withEnvironmentVariable("PIPS_AGENT_TIMESERIES_PATH", "http://" + mockWacnetServer.getHost() + ":" + mockWacnetServer.getServerPort() + "/pips-timeseries-agent/timeseries")
+            withEnvironmentVariable("PIPS_AGENT_TIMESERIES_PATH", "http://" + MOCKSERVER_HOST + ":" + String.valueOf(MOCKSERVER_PORT) + "/pips-timeseries-agent/timeseries")
             .execute(() -> {
                 JSONObject result = new JSONObject();
                 pipsTsAgentAPIConnector = new PIPSTSAgentAPIConnector();
@@ -83,9 +77,10 @@ public class PIPSTSAgentAPIConnectorTest {
 
     @Test
     public void testGetTimeSeriesFail() throws Exception {
-        try (MockServerClient pipsTsAgent = new MockServerClient(mockWacnetServer.getHost(), mockWacnetServer.getServerPort())) {
-            setupMockPIPSTSAgent(pipsTsAgent);
-            withEnvironmentVariable("PIPS_AGENT_TIMESERIES_PATH", "http://" + mockWacnetServer.getHost() + ":" + mockWacnetServer.getServerPort() + "/pips-timeseries-agent/timeseries")
+        TimeUnit.SECONDS.sleep(10);
+        try (MockServerClient pipsTsAgent = new MockServerClient(MOCKSERVER_HOST, MOCKSERVER_PORT)) {
+            setupMockPIPSTSAgentForFailed(pipsTsAgent);
+            withEnvironmentVariable("PIPS_AGENT_TIMESERIES_PATH", "http://" + MOCKSERVER_HOST + ":" + String.valueOf(MOCKSERVER_PORT) + "/pips-timeseries-agent/timeseries")
             .execute(() -> {
                 JSONObject result = new JSONObject();
                 pipsTsAgentAPIConnector = new PIPSTSAgentAPIConnector();
