6c1519ed6229df0a842a291974ffd8709a8febe9
sandradeng20 <xsd20@cam.ac.uk>
2024-07-29 16:36:17 +0800

    1768-dev-trajectoryqueryagent: able to handle multile phone id on a user

--- Agents/TrajectoryQueryAgent/trajectoryqueryagent/src/main/java/uk/ac/cam/cares/jps/agent/trajectoryqueryagent/TrajectoryQueryAgent.java
+++ Agents/TrajectoryQueryAgent/trajectoryqueryagent/src/main/java/uk/ac/cam/cares/jps/agent/trajectoryqueryagent/TrajectoryQueryAgent.java
@@ -15,13 +15,13 @@ import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
 
-
 import javax.servlet.annotation.WebServlet;
 import javax.ws.rs.BadRequestException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @WebServlet(urlPatterns = "/createlayer")
@@ -45,9 +45,10 @@ public class TrajectoryQueryAgent extends JPSAgent {
 
     /**
      * 1) Receive userID
-     * 2) SPARQL query for pointIRI based on userID 
+     * 2) SPARQL query for pointIRI based on userID
      * 3) Create geoserver layer
      * 4) Return pointIRI to application as response
+     *
      * @param requestParams
      * @return
      */
@@ -71,92 +72,118 @@ public class TrajectoryQueryAgent extends JPSAgent {
             return response;
         }
 
-        String phoneIri = phoneIdResponse.getJSONArray("PhoneIds").getString(0);     // todo: assume each user only has 1 phone id for now
+        List<String> pointIRIs = new ArrayList<>();
+        List<String> altitudeIRIs = new ArrayList<>();
+        List<String> speedIRIs = new ArrayList<>();
+        List<String> bearingIRIs = new ArrayList<>();
 
-        //SPARQL query for pointIRI based on userID - Note currently userID is deviceID, needs to be changed
-        Node smartphoneIRI = NodeFactory.createURI(phoneIri);
-        String pointIRI = kgQueryClient.getIRIfromJSONarray(kgQueryClient.getPointIRIArray(smartphoneIRI));
-        String altitudeIRI = kgQueryClient.getIRIfromJSONarray(kgQueryClient.getAltitudeIRIArray(smartphoneIRI));
-        String speedIRI = kgQueryClient.getIRIfromJSONarray(kgQueryClient.getSpeedIRIArray(smartphoneIRI));
-        String bearingIRI = kgQueryClient.getIRIfromJSONarray(kgQueryClient.getBearingIRIArray(smartphoneIRI));
+        for (int i = 0; i < phoneIdResponse.getJSONArray("PhoneIds").length(); i ++) {
+            String phoneIri = phoneIdResponse.getJSONArray("PhoneIds").getString(i);
 
-        if (pointIRI == null || altitudeIRI == null || speedIRI == null || bearingIRI == null) {
+            //SPARQL query for pointIRI based on userID - Note currently userID is deviceID, needs to be changed
+            Node smartphoneIRI = NodeFactory.createURI(phoneIri);
+            String pointIRI = kgQueryClient.getIRIfromJSONarray(kgQueryClient.getPointIRIArray(smartphoneIRI));
+            String altitudeIRI = kgQueryClient.getIRIfromJSONarray(kgQueryClient.getAltitudeIRIArray(smartphoneIRI));
+            String speedIRI = kgQueryClient.getIRIfromJSONarray(kgQueryClient.getSpeedIRIArray(smartphoneIRI));
+            String bearingIRI = kgQueryClient.getIRIfromJSONarray(kgQueryClient.getBearingIRIArray(smartphoneIRI));
+
+            if (pointIRI == null || altitudeIRI == null || speedIRI == null || bearingIRI == null) {
+                continue;
+            }
+
+            pointIRIs.add(pointIRI);
+            altitudeIRIs.add(altitudeIRI);
+            speedIRIs.add(speedIRI);
+            bearingIRIs.add(bearingIRI);
+        }
+
+        if (pointIRIs.isEmpty() || altitudeIRIs.isEmpty() || speedIRIs.isEmpty() || bearingIRIs.isEmpty()) {
             JSONObject response = new JSONObject();
             response.put("message", "Measurement IRI is missing");
             return response;
         }
 
         //Create Geoserver layer
-        createGeoserver(pointIRI,altitudeIRI,speedIRI,bearingIRI );
+        createGeoserver(pointIRIs.get(0), altitudeIRIs.get(0), speedIRIs.get(0), bearingIRIs.get(0));
 
         //Return pointIRI, altitudeIRI, speedIRI, bearingIRI to app as response
         JSONObject response = new JSONObject();
         response.put("message", "Layer created");
-        response.put("pointIRI", pointIRI);
-        response.put("altitudeIRI", altitudeIRI);
-        response.put("speedIRI", speedIRI);
-        response.put("bearingIRI", bearingIRI);
+        response.put("pointIRI", String.join(",", pointIRIs));
+        response.put("altitudeIRI", String.join(",", altitudeIRIs));
+        response.put("speedIRI", String.join(",", speedIRIs));
+        response.put("bearingIRI", String.join(",", bearingIRIs));
         return response;
     }
 
 
-    private void createGeoserver(String pointIRI, String altitudeIRI, String speedIRI, String bearingIRI){
+    private void createGeoserver(String pointIRI, String altitudeIRI, String speedIRI, String bearingIRI) {
         //Function retrieves column_name from dbTable given IRI
-         String getColumnNameFunction = "CREATE OR REPLACE FUNCTION getColumnName(iri VARCHAR)\n" +
-                 "RETURNS VARCHAR AS\n" +
-                 "$$\n" +
-                 "DECLARE\n" +
-                 "    column_name VARCHAR;\n" +
-                 "BEGIN\n" +
-                 "    SELECT \"columnName\" INTO column_name FROM \"dbTable\" WHERE \"dataIRI\" = iri;\n" +
-                 "    RETURN column_name;\n" +
-                 "END;\n" +
-                 "$$\n" +
-                 "LANGUAGE plpgsql;";
+        String getColumnNameFunction = "CREATE OR REPLACE FUNCTION getColumnName(iri VARCHAR)\n" +
+                "RETURNS VARCHAR AS\n" +
+                "$$\n" +
+                "DECLARE\n" +
+                "    column_name VARCHAR;\n" +
+                "BEGIN\n" +
+                "    SELECT \"columnName\" INTO column_name FROM \"dbTable\" WHERE \"dataIRI\" = iri;\n" +
+                "    RETURN column_name;\n" +
+                "END;\n" +
+                "$$\n" +
+                "LANGUAGE plpgsql;";
 
         //Function retrieves table_name from dbTable given IRI
-         String getTableNameFunction= "CREATE OR REPLACE FUNCTION getTableName(iri VARCHAR)\n" +
-                 "RETURNS VARCHAR AS\n" +
-                 "$$\n" +
-                 "DECLARE\n" +
-                 "    table_name VARCHAR;\n" +
-                 "BEGIN\n" +
-                 "    SELECT \"tableName\" INTO table_name FROM \"dbTable\" WHERE \"dataIRI\" = iri;\n" +
-                 "    RETURN table_name;\n" +
-                 "END;\n" +
-                 "$$\n" +
-                 "LANGUAGE plpgsql;";
+        String getTableNameFunction = "CREATE OR REPLACE FUNCTION getTableName(iri VARCHAR)\n" +
+                "RETURNS VARCHAR AS\n" +
+                "$$\n" +
+                "DECLARE\n" +
+                "    table_name VARCHAR;\n" +
+                "BEGIN\n" +
+                "    SELECT \"tableName\" INTO table_name FROM \"dbTable\" WHERE \"dataIRI\" = iri;\n" +
+                "    RETURN table_name;\n" +
+                "END;\n" +
+                "$$\n" +
+                "LANGUAGE plpgsql;";
 
-        //Function retrieves locationTable given pointiri, speediri, altitudeiri, bearingiri
-         String getLocationTableFunction="CREATE OR REPLACE FUNCTION getLocationTable(pointiri VARCHAR, speediri VARCHAR, altitudeiri VARCHAR, bearingiri VARCHAR)\n" +
-                 "RETURNS TABLE (\"time\" timestamptz, \"geom\" geometry, \"speed\" double precision, \"altitude\" double precision, \"bearing\" double precision)\n" +
-                 "AS $$\n" +
-                 "DECLARE\n" +
-                 "    tableName TEXT;\n" +
-                 "BEGIN\n" +
-                 "    tableName := getTableName(pointiri);\n" +
-                 "    RETURN QUERY EXECUTE \n" +
-                 "        format('SELECT time, %I AS geom, %I AS speed, %I AS altitude, %I AS bearing FROM %I', \n" +
-                 "               getColumnName(pointiri), getColumnName(speediri), getColumnName(altitudeiri), getColumnName(bearingiri), tableName);\n" +
-                 "END\n" +
-                 "$$ LANGUAGE plpgsql;";
+        //Function retrieves the combined locationTable given pointiris, speediris, altitudeiris, bearingiris. Each iris is a string of iri separated by ','
+        //example of pointiris: 'https://www.theworldavatar.com/kg/sensorloggerapp/point_1,https://www.theworldavatar.com/kg/sensorloggerapp/point_2'
+        String getLocationTableFunction = "CREATE OR REPLACE FUNCTION getLocationTable(pointiri TEXT, speediri TEXT, altitudeiri TEXT, bearingiri TEXT)\n" +
+                "RETURNS TABLE (\"time\" timestamptz, \"geom\" geometry, \"speed\" double precision, \"altitude\" double precision, \"bearing\" double precision) AS $$\n" +
+                "DECLARE\n" +
+                "pointIriArray TEXT[];\n" +
+                "altitudeIriArray TEXT[];\n" +
+                "speedIriArray TEXT[];\n" +
+                "bearingIriArray TEXT[];\n" +
+                "query TEXT := '';\n" +
+                "BEGIN\n" +
+                "pointIriArray := string_to_array(pointiri, ',');\n" +
+                "altitudeIriArray := string_to_array(altitudeiri, ',');\n" +
+                "speedIriArray := string_to_array(speediri, ',');\n" +
+                "bearingIriArray := string_to_array(bearingiri, ',');\n" +
+                "\n" +
+                "FOR i IN 1..array_length(pointIriArray, 1) LOOP\n" +
+                "    IF i > 1 THEN\n" +
+                "            query := query || ' UNION ALL ';\n" +
+                "    END IF;\n" +
+                "    query := query || format('SELECT time, %I AS geom, %I AS speed, %I AS altitude, %I AS bearing FROM %I', getColumnName(pointIriArray[i]), getColumnName(speedirIArray[i]), getColumnName(altitudeIriArray[i]), getColumnName(bearingIriArray[i]), getTableName(pointIriArray[i]));\n" +
+                "END LOOP;\n" +
+                "RETURN QUERY EXECUTE query;\n" +
+                "END $$ LANGUAGE plpgsql;";
 
         try (Connection connection = remoteRDBStoreClient.getConnection()) {
             executeSql(connection, getColumnNameFunction);
             executeSql(connection, getTableNameFunction);
             executeSql(connection, getLocationTableFunction);
             System.out.println("Created get_geometry_table function.");
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             throw new JPSRuntimeException(e);
         }
 
         //Create geoserver point layer
         GeoServerClient geoServerClient = GeoServerClient.getInstance();
-        String workspaceName= "twa";
+        String workspaceName = "twa";
         String schema = "public";
-        String dbName="postgres";
+        String dbName = "postgres";
         geoServerClient.createWorkspace(workspaceName);
         UpdatedGSVirtualTableEncoder virtualTable = new UpdatedGSVirtualTableEncoder();
         GeoServerVectorSettings geoServerVectorSettings = new GeoServerVectorSettings();
@@ -171,51 +198,36 @@ public class TrajectoryQueryAgent extends JPSAgent {
         virtualTable.setEscapeSql(true);
         virtualTable.setName("trajectoryPointVirtualTable");
         virtualTable.addVirtualTableGeometry("geom", "Geometry", "4326"); // geom needs to match the sql query
-        virtualTable.addVirtualTableParameter("pointiri",pointIRI,".*");
-        virtualTable.addVirtualTableParameter("speediri",speedIRI,".*");
-        virtualTable.addVirtualTableParameter("altitudeiri",altitudeIRI,".*");
-        virtualTable.addVirtualTableParameter("bearingiri",bearingIRI,".*");
+        virtualTable.addVirtualTableParameter("pointiri", pointIRI, ".*");
+        virtualTable.addVirtualTableParameter("speediri", speedIRI, ".*");
+        virtualTable.addVirtualTableParameter("altitudeiri", altitudeIRI, ".*");
+        virtualTable.addVirtualTableParameter("bearingiri", bearingIRI, ".*");
         virtualTable.addVirtualTableParameter("date", "0001-01-01 00:00:00.000+00", "^\\d{4,}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}.\\d{2,}\\+\\d{2}$");
         geoServerVectorSettings.setVirtualTable(virtualTable);
-        geoServerClient.createPostGISDataStore(workspaceName,"trajectoryPoint" , dbName, schema);
-        geoServerClient.createPostGISLayer(workspaceName, dbName,"trajectoryPoint" ,geoServerVectorSettings);
-        
+        geoServerClient.createPostGISDataStore(workspaceName, "trajectoryPoint", dbName, schema);
+        geoServerClient.createPostGISLayer(workspaceName, dbName, "trajectoryPoint", geoServerVectorSettings);
+
         //Create line layer
         UpdatedGSVirtualTableEncoder virtualTableLine = new UpdatedGSVirtualTableEncoder();
         GeoServerVectorSettings geoServerVectorLineSettings = new GeoServerVectorSettings();
-        virtualTableLine.setSql("WITH numbered_points AS (\n" +
-                "    SELECT\n" +
-                "        time,\n" +
-                "        speed,\n" +
-                "        altitude,\n" +
-                "        geom,\n" +
-                "        bearing,\n" +
-                "        LEAD(geom) OVER (ORDER BY time) AS next_geom\n" +
-                "    FROM\n" +
-                "        public.getLocationTable('%pointiri%','%speediri%','%altitudeiri%','%bearingiri%') as timeseries\n" +
-                ")\n" +
+        virtualTableLine.setSql(
                 "SELECT\n" +
-                "    ST_MakeLine(geom, next_geom) AS geom,\n" +
-                "    AVG(speed) AS speed,\n" +
-                "    AVG(altitude) AS altitude,\n" +
-                "    AVG(bearing) AS bearing\n" +
-                "FROM\n" +
-                "    numbered_points\n" +
-                "WHERE\n" +
-                "time > '%date%'::TIMESTAMPTZ AND time < '%date%'::TIMESTAMPTZ + INTERVAL '23 hours 59 minutes 59 seconds 999 milliseconds'\n" +
-                "GROUP BY\n" +
-                "    geom, next_geom");
+                "    ST_MakeLine(geom) AS geom\n" +
+                "    FROM\n" +
+                "        public.getLocationTable('%pointiri%','%speediri%','%altitudeiri%','%bearingiri%')\n" +
+                "    WHERE\n" +
+                "        time > '%date%'::TIMESTAMPTZ AND time < '%date%'::TIMESTAMPTZ + INTERVAL '23 hours 59 minutes 59 seconds 999 milliseconds'\n");
         virtualTableLine.setEscapeSql(true);
         virtualTableLine.setName("trajectoryLineVirtualTable");
         virtualTableLine.addVirtualTableGeometry("geom", "Geometry", "4326"); // geom needs to match the sql query
-        virtualTableLine.addVirtualTableParameter("pointiri",pointIRI,".*");
-        virtualTableLine.addVirtualTableParameter("speediri",speedIRI,".*");
-        virtualTableLine.addVirtualTableParameter("altitudeiri",altitudeIRI,".*");
-        virtualTableLine.addVirtualTableParameter("bearingiri",bearingIRI,".*");
+        virtualTableLine.addVirtualTableParameter("pointiri", pointIRI, ".*");
+        virtualTableLine.addVirtualTableParameter("speediri", speedIRI, ".*");
+        virtualTableLine.addVirtualTableParameter("altitudeiri", altitudeIRI, ".*");
+        virtualTableLine.addVirtualTableParameter("bearingiri", bearingIRI, ".*");
         virtualTableLine.addVirtualTableParameter("date", "0001-01-01 00:00:00.000+00", "^\\d{4,}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}.\\d{2,}\\+\\d{2}$");
         geoServerVectorLineSettings.setVirtualTable(virtualTableLine);
-        geoServerClient.createPostGISDataStore(workspaceName,"trajectoryLine" , dbName, schema);
-        geoServerClient.createPostGISLayer(workspaceName, dbName,"trajectoryLine" ,geoServerVectorLineSettings);
+        geoServerClient.createPostGISDataStore(workspaceName, "trajectoryLine", dbName, schema);
+        geoServerClient.createPostGISLayer(workspaceName, dbName, "trajectoryLine", geoServerVectorLineSettings);
 
 
         //Sample point request - specify pointIRI, speedIRI, altitudeIRI, bearingIRI
@@ -227,8 +239,9 @@ public class TrajectoryQueryAgent extends JPSAgent {
 
     /**
      * Create connection to remoteStoreClient and execute SQL statement
+     *
      * @param connection PostgreSQL connection object
-     * @param sql SQl statement to execute
+     * @param sql        SQl statement to execute
      */
     private void executeSql(Connection connection, String sql) throws SQLException {
         try (Statement statement = connection.createStatement()) {
@@ -238,6 +251,7 @@ public class TrajectoryQueryAgent extends JPSAgent {
 
     /**
      * Check if the JSONObject in the processRequestParameters inputs are correct or missing.
+     *
      * @param requestParams
      * @return
      * @throws BadRequestException
