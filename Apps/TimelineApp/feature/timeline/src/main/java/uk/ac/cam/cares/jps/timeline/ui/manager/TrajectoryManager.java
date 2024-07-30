3e9d932818744d4a2e24198f6ac6a066e62fb99a
sandradeng20 <xsd20@cam.ac.uk>
2024-07-30 15:28:50 +0800

    1773-timeline-app: refactor trajectory painting

--- Apps/TimelineApp/feature/timeline/src/main/java/uk/ac/cam/cares/jps/timeline/ui/manager/TrajectoryManager.java
+++ Apps/TimelineApp/feature/timeline/src/main/java/uk/ac/cam/cares/jps/timeline/ui/manager/TrajectoryManager.java
@@ -1,5 +1,9 @@
 package uk.ac.cam.cares.jps.timeline.ui.manager;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -10,6 +14,7 @@ import com.mapbox.geojson.Point;
 import com.mapbox.maps.CameraOptions;
 import com.mapbox.maps.LayerPosition;
 import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
 import com.mapbox.maps.plugin.animation.MapAnimationOptions;
 
 import org.apache.log4j.Logger;
@@ -28,46 +33,23 @@ public class TrajectoryManager {
     public TrajectoryManager(Fragment fragment, MapView mapView) {
         trajectoryViewModel = new ViewModelProvider(fragment).get(TrajectoryViewModel.class);
 
-        trajectoryViewModel.trajectory.observe(fragment.getViewLifecycleOwner(), trajectory -> {
+        trajectoryViewModel.trajectory.observe(fragment.getViewLifecycleOwner(), trajectoryStr -> {
             mapView.getMapboxMap().getStyle(style -> {
                 Expected<String, None> removeLayerSuccess = style.removeStyleLayer("trajectory_layer");
                 Expected<String, None> removeSourceSuccess = style.removeStyleSource("trajectory");
                 LOGGER.debug("trajectory: layer and source removed result " + (removeSourceSuccess.isError() && removeLayerSuccess.isError() ? removeSourceSuccess.getError() + "\n" + removeLayerSuccess.getError() : "success"));
 
-                if (trajectory.isEmpty()) {
+                if (trajectoryStr.isEmpty()) {
                     return;
                 }
 
-                JSONObject sourceJson = new JSONObject();
-                try {
-                    sourceJson.put("type", "geojson");
-                    sourceJson.put("data", trajectory);
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
-                Expected<String, None> success = style.addStyleSource("trajectory", Objects.requireNonNull(Value.fromJson(sourceJson.toString()).getValue()));
-                LOGGER.debug("trajectory: source created " + (success.isError() ? success.getError() : "success"));
+                String colorCode = String.format("#%06X", (0xFFFFFF & getColor(fragment.requireContext())));
+                paintTrajectory(style, trajectoryStr, colorCode, "trajectory_layer");
 
-                JSONObject layerJson = new JSONObject();
-                try {
-                    layerJson.put("id", "trajectory_layer");
-                    layerJson.put("type", "line");
-                    layerJson.put("source", "trajectory");
-                    layerJson.put("line-join", "bevel");
-
-                    JSONObject paint = new JSONObject();
-                    paint.put("line-color", "#00687C");
-                    paint.put("line-width", 8);
-                    layerJson.put("paint", paint);
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
-                Expected<String, None> layerSuccess = style.addStyleLayer(Objects.requireNonNull(Value.fromJson(layerJson.toString()).getValue()), new LayerPosition(null, null, null));
-                LOGGER.debug("trajectory: layer created " + (layerSuccess.isError() ? layerSuccess.getError() : "success"));
             });
 
             try {
-                JSONObject jsonObject = new JSONObject(trajectory);
+                JSONObject jsonObject = new JSONObject(trajectoryStr);
                 JSONArray bbox = jsonObject.getJSONArray("bbox");
                 mapView.getMapboxMap().cameraAnimationsPlugin(plugin -> {
                     Point newCenter = getBBoxCenter(bbox);
@@ -87,6 +69,46 @@ public class TrajectoryManager {
             }
 
         });
+    }
+
+    private int getColor(Context context) {
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(new int[]{com.google.android.material.R.attr.colorPrimary});
+        int colorPrimary = typedArray.getColor(0, Color.BLACK);
+        typedArray.recycle();
+        return colorPrimary;
+    }
+
+    private void paintTrajectory(Style style, String trajectory, String colorCode, String mode) {
+        Expected<String, None> removeLayerSuccess = style.removeStyleLayer("trajectory_layer_" + mode);
+        Expected<String, None> removeSourceSuccess = style.removeStyleSource("trajectory_" + mode);
+        LOGGER.debug("trajectory: layer and source removed result " + (removeSourceSuccess.isError() && removeLayerSuccess.isError() ? removeSourceSuccess.getError() + "\n" + removeLayerSuccess.getError() : "success"));
+
+        JSONObject sourceJson = new JSONObject();
+        try {
+            sourceJson.put("type", "geojson");
+            sourceJson.put("data", trajectory);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        Expected<String, None> success = style.addStyleSource("trajectory_" + mode, Objects.requireNonNull(Value.fromJson(sourceJson.toString()).getValue()));
+        LOGGER.debug("trajectory: source created " + (success.isError() ? success.getError() : "success"));
+
+        JSONObject layerJson = new JSONObject();
+        try {
+            layerJson.put("id", "trajectory_layer_" + mode);
+            layerJson.put("type", "line");
+            layerJson.put("source", "trajectory_" + mode);
+            layerJson.put("line-join", "bevel");
+
+            JSONObject paint = new JSONObject();
+            paint.put("line-color", colorCode);
+            paint.put("line-width", 8);
+            layerJson.put("paint", paint);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        Expected<String, None> layerSuccess = style.addStyleLayer(Objects.requireNonNull(Value.fromJson(layerJson.toString()).getValue()), new LayerPosition(null, null, null));
+        LOGGER.debug("trajectory: layer created " + (layerSuccess.isError() ? layerSuccess.getError() : "success"));
     }
 
     private Point getBBoxCenter(JSONArray bbox) {
