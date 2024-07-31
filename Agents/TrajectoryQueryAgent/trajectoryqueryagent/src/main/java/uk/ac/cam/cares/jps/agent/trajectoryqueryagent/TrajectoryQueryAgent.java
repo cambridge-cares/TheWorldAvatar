27e55c03265a3a7a9d2cec65315359149db516df
sandradeng20 <xsd20@cam.ac.uk>
2024-07-31 14:35:59 +0800

    1768-dev-trajectoryqueryagent: retrieve dates with trajectory

--- Agents/TrajectoryQueryAgent/trajectoryqueryagent/src/main/java/uk/ac/cam/cares/jps/agent/trajectoryqueryagent/TrajectoryQueryAgent.java
+++ Agents/TrajectoryQueryAgent/trajectoryqueryagent/src/main/java/uk/ac/cam/cares/jps/agent/trajectoryqueryagent/TrajectoryQueryAgent.java
@@ -7,6 +7,11 @@ import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jooq.DatePart;
+import org.jooq.Field;
+import org.jooq.Query;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
@@ -16,22 +21,28 @@ import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BadRequestException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static org.jooq.impl.DSL.*;
+import static org.jooq.impl.SQLDataType.TIMESTAMP;
 
-@WebServlet(urlPatterns = "/createlayer")
+@WebServlet(urlPatterns = {"/createlayer", "/getDatesWithData"})
 public class TrajectoryQueryAgent extends JPSAgent {
     private KGQueryClient kgQueryClient;
     private RemoteStoreClient storeClient;
     private RemoteRDBStoreClient remoteRDBStoreClient;
     private final String USER_ID = "userID";
+    private final String TIMEZONE = "timezone";
     private static final Logger LOGGER = LogManager.getLogger(TrajectoryQueryAgent.class);
-    private String userID;
     private String userAgentUrl;
 
 
@@ -43,33 +54,34 @@ public class TrajectoryQueryAgent extends JPSAgent {
         userAgentUrl = endpointConfig.getUserAgentUrl();
     }
 
-    /**
-     * 1) Receive userID
-     * 2) SPARQL query for pointIRI based on userID
-     * 3) Create geoserver layer
-     * 4) Return pointIRI to application as response
-     *
-     * @param requestParams
-     * @return
-     */
     @Override
-    public JSONObject processRequestParameters(JSONObject requestParams) {
-
-        if (!validateInput(requestParams)) {
+    public JSONObject processRequestParameters(JSONObject requestParams, HttpServletRequest request) {
+        String url = request.getRequestURI();
+        if (!validateInput(requestParams, url)) {
             throw new JPSRuntimeException("Unable to validate request sent to the agent.");
         }
 
+        if (url.contains("getDatesWithData")) {
+            return getDatesWithData(requestParams);
+        } else if (url.contains("createlayer")) {
+            return createLayer(requestParams);
+        }
+
+        JSONObject response = new JSONObject();
+        response.put("message", "Path invalid, no operation performed");
+        return response;
+    }
+
+    public Map<String, List<String>> getMeasurementIris (JSONObject requestParams) {
         //Retrieve params (Although currently receiver deviceID)
-        this.userID = requestParams.getString(USER_ID);
+        String userID = requestParams.getString(USER_ID);
 
         //Retrieve phoneId from User Agent with userId
         String getPhoneIds = userAgentUrl + "getPhoneIds";
         JSONObject phoneIdResponse = new JSONObject(AgentCaller.executeGet(getPhoneIds, "userId", userID));
         JSONArray phoneIds = phoneIdResponse.getJSONArray("PhoneIds");
         if (phoneIds.isEmpty()) {
-            JSONObject response = new JSONObject();
-            response.put("message", "No phone id on this user.");
-            return response;
+            throw new JPSRuntimeException("No phone id on this user.");
         }
 
         List<String> pointIRIs = new ArrayList<>();
@@ -98,26 +110,101 @@ public class TrajectoryQueryAgent extends JPSAgent {
         }
 
         if (pointIRIs.isEmpty() || altitudeIRIs.isEmpty() || speedIRIs.isEmpty() || bearingIRIs.isEmpty()) {
-            JSONObject response = new JSONObject();
-            response.put("message", "Measurement IRI is missing");
-            return response;
+            throw new JPSRuntimeException("Measurement IRI is missing");
         }
 
-        //Create Geoserver layer
-        createGeoserver(pointIRIs.get(0), altitudeIRIs.get(0), speedIRIs.get(0), bearingIRIs.get(0));
+        Map<String, List<String>> iris = new HashMap<>();
+        iris.put("pointIRIs", pointIRIs);
+        iris.put("altitudeIRIs", altitudeIRIs);
+        iris.put("speedIRIs", speedIRIs);
+        iris.put("bearingIRIs", bearingIRIs);
+        return iris;
+    }
 
-        //Return pointIRI, altitudeIRI, speedIRI, bearingIRI to app as response
+    /**
+     * 1) Receive userID
+     * 2) SPARQL query for pointIRI based on userID
+     * 3) Create geoserver layer
+     * 4) Return pointIRI to application as response
+     *
+     * @param requestParams
+     * @return
+     */
+    public JSONObject createLayer(JSONObject requestParams) {
+        try {
+            Map<String, List<String>> iriMap = getMeasurementIris(requestParams);
+            List<String> pointIRIs = iriMap.get("pointIRIs");
+            List<String> altitudeIRIs = iriMap.get("altitudeIRIs");
+            List<String> speedIRIs = iriMap.get("speedIRIs");
+            List<String> bearingIRIs = iriMap.get("bearingIRIs");
+
+            //Create Geoserver layer
+            createGeoserver(pointIRIs.get(0), altitudeIRIs.get(0), speedIRIs.get(0), bearingIRIs.get(0));
+
+            //Return pointIRI, altitudeIRI, speedIRI, bearingIRI to app as response
+            JSONObject response = new JSONObject();
+            response.put("message", "Layer created");
+            response.put("pointIRI", String.join(",", pointIRIs));
+            response.put("altitudeIRI", String.join(",", altitudeIRIs));
+            response.put("speedIRI", String.join(",", speedIRIs));
+            response.put("bearingIRI", String.join(",", bearingIRIs));
+            return response;
+        } catch (JPSRuntimeException e) {
+            if (e.getMessage().contains("No phone id on this user.")) {
+                JSONObject response = new JSONObject();
+                response.put("message", "No phone id on this user.");
+                return response;
+            } else if (e.getMessage().contains("Measurement IRI is missing")) {
+                JSONObject response = new JSONObject();
+                response.put("message", "Measurement IRI is missing");
+                return response;
+            } else {
+                throw new JPSRuntimeException("error occur");
+            }
+        }
+    }
+
+    public JSONObject getDatesWithData(JSONObject requestParams) {
+        createPostgresFunctions();
+
+        Map<String, List<String>> iriMap = getMeasurementIris(requestParams);
+        String pointIRIs = String.join(",", iriMap.get("pointIRIs"));
+        String altitudeIRIs = String.join(",", iriMap.get("altitudeIRIs"));
+        String speedIRIs = String.join(",", iriMap.get("speedIRIs"));
+        String bearingIRIs = String.join(",", iriMap.get("bearingIRIs"));
+
+        Field<Timestamp> f = function("public.getLocationTable",
+                TIMESTAMP,
+                inline(pointIRIs),
+                inline(speedIRIs),
+                inline(altitudeIRIs),
+                inline(bearingIRIs));
+
+        Field<Timestamp> time = field(name("time"), TIMESTAMP);
+
+        String timezone = requestParams.getString(TIMEZONE);
+        Field<Timestamp> shiftedTime = DSL.field("{0} AT TIME ZONE '+00' AT TIME ZONE {1}", TIMESTAMP, time, inline(timezone));
+
+        Field<Integer> year = extract(shiftedTime, DatePart.YEAR).as("year");
+        Field<Integer> month = extract(shiftedTime, DatePart.MONTH).as("month");
+        Field<Integer> day = extract(shiftedTime, DatePart.DAY);
+
+        Field<Integer[]> days = arrayAggDistinct(day).orderBy(day).as("days");
+
+        Query query = using(SQLDialect.POSTGRES).select(year, month, days)
+                .from(table(f.toString()))
+                .groupBy(year, month)
+                .orderBy(year, month);
+
+        JSONArray result = remoteRDBStoreClient.executeQuery(query.getSQL());
+
         JSONObject response = new JSONObject();
-        response.put("message", "Layer created");
-        response.put("pointIRI", String.join(",", pointIRIs));
-        response.put("altitudeIRI", String.join(",", altitudeIRIs));
-        response.put("speedIRI", String.join(",", speedIRIs));
-        response.put("bearingIRI", String.join(",", bearingIRIs));
+        response.put("message", "Succeed");
+        response.put("result", result);
         return response;
     }
 
-
-    private void createGeoserver(String pointIRI, String altitudeIRI, String speedIRI, String bearingIRI) {
+    private void createPostgresFunctions() {
         //Function retrieves column_name from dbTable given IRI
         String getColumnNameFunction = "CREATE OR REPLACE FUNCTION getColumnName(iri VARCHAR)\n" +
                 "RETURNS VARCHAR AS\n" +
@@ -178,6 +265,10 @@ public class TrajectoryQueryAgent extends JPSAgent {
             e.printStackTrace();
             throw new JPSRuntimeException(e);
         }
+    }
+
+    private void createGeoserver(String pointIRI, String altitudeIRI, String speedIRI, String bearingIRI) {
+        createPostgresFunctions();
 
         //Create geoserver point layer
         GeoServerClient geoServerClient = GeoServerClient.getInstance();
@@ -250,17 +341,22 @@ public class TrajectoryQueryAgent extends JPSAgent {
     }
 
     /**
-     * Check if the JSONObject in the processRequestParameters inputs are correct or missing.
-     *
+     * Check if the JSONObject in the processRequestParameters inputs are correct or missing based on the input path.
+     * All path retrieving trajectory related data will need user id
      * @param requestParams
      * @return
      * @throws BadRequestException
      */
-    @Override
-    public boolean validateInput(JSONObject requestParams) throws BadRequestException {
+    private boolean validateInput(JSONObject requestParams, String url) {
         if (!requestParams.has(USER_ID)) {
             LOGGER.error("userID is missing.");
             return false;
+        }
+        if (url.contains("getDatesWithData")) {
+            if (!requestParams.has(TIMEZONE)) {
+                LOGGER.error("timezone is missing.");
+                return false;
+            }
         }
         return true;
     }
