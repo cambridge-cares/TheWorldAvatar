2c5f9bcef4dcbb6f04e328ce243f902a676f303e
Dan Tran <nmdt2@cam.ac.uk>
2024-07-31 17:21:20 +0800

    dev-qa-icl: Refactor the display of search results

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/search/zeolite-frameworks/_components/results.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/search/zeolite-frameworks/_components/results.tsx
@@ -1,71 +1,21 @@
 'use client'
 
 import * as React from 'react'
-import { useSearchParams } from 'next/navigation'
 import { createColumnHelper } from '@tanstack/react-table'
 
 import { ZeoliteFrameworkBase } from '@/lib/model/ontozeolite'
 import { getZeoliteFrameworksMany } from '@/lib/api/ontozeolite'
-import { DataTable } from '@/components/ui/data-table'
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
+import { LinkButtonToIRIPage, SearchResults } from '@/components/search/search-result'
 
 const COL_HELPER = createColumnHelper<ZeoliteFrameworkBase>()
 const COLS = [
   COL_HELPER.accessor('IRI', {
     header: '',
-    cell: cell => {
-      const val = cell.getValue()
-      return (
-        <Link
-          href={`/zeolite-frameworks/${encodeURIComponent(val)}`}
-          className='hover:underline'
-        >
-          <Button variant='secondary'>View</Button>
-        </Link>
-      )
-    },
+    cell: cell => <LinkButtonToIRIPage IRI={cell.getValue()} prefixPath='/zeolite-frameworks' />,
   }),
   COL_HELPER.accessor('code', { header: 'Code' }),
 ]
 
-export function ZeoliteFrameworkResults() {
-  const searchParams = useSearchParams()
-
-  const [isLoading, setIsLoading] = React.useState(false)
-  const [data, setData] = React.useState<ZeoliteFrameworkBase[] | undefined>(
-    undefined
-  )
-
-  React.useEffect(() => {
-    async function retrieveData() {
-      setIsLoading(true)
-      try {
-        setData(await getZeoliteFrameworksMany(searchParams))
-      } catch {
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
-    setData(undefined)
-    if (searchParams.size > 0) {
-      retrieveData()
-    }
-  }, [searchParams])
-
-  return data !== undefined ? (
-    <DataTable
-      columns={COLS}
-      data={data}
-      numbered
-      paginated
-      bordered
-      scrollable
-    />
-  ) : isLoading ? (
-    <div>Getting search results...</div>
-  ) : (
-    <></>
-  )
-}
+export const ZeoliteFrameworkResults = () => (
+  <SearchResults dataGetter={getZeoliteFrameworksMany} columns={COLS} />
+)
--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/search/zeolitic-materials/_components/results.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/search/zeolitic-materials/_components/results.tsx
@@ -1,73 +1,23 @@
 'use client'
 
 import * as React from 'react'
-import Link from 'next/link'
-import { useSearchParams } from 'next/navigation'
 import { createColumnHelper } from '@tanstack/react-table'
 
-import { Button } from '@/components/ui/button'
 import { getZeoliticMaterialsMany } from '@/lib/api/ontozeolite'
 import { ZeoliticMaterialBase } from '@/lib/model/ontozeolite'
-import { DataTable } from '@/components/ui/data-table'
+import { LinkButtonToIRIPage, SearchResults } from '@/components/search/search-result'
 
 const COL_HELPER = createColumnHelper<ZeoliticMaterialBase>()
 const COLS = [
   COL_HELPER.accessor('IRI', {
     header: '',
-    cell: cell => {
-      const val = cell.getValue()
-      return (
-        <Link
-          href={`/zeolitic-materials/${encodeURIComponent(val)}`}
-          className='hover:underline'
-        >
-          <Button variant='secondary'>View</Button>
-        </Link>
-      )
-    },
+    cell: cell => <LinkButtonToIRIPage IRI={cell.getValue()} prefixPath='/zeolitic-materials' />,
   }),
   COL_HELPER.accessor('ChemicalFormula', {
     header: 'Formula'
   })
 ]
 
-export function ZeoliticMaterialResults() {
-  const searchParams = useSearchParams()
-
-  const [isLoading, setIsLoading] = React.useState(false)
-  const [data, setData] = React.useState<ZeoliticMaterialBase[] | undefined>(
-    undefined
-  )
-
-  React.useEffect(() => {
-    async function retrieveData() {
-      setIsLoading(true)
-      try {
-        setData(await getZeoliticMaterialsMany(searchParams))
-      } catch {
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
-    setData(undefined)
-    if (searchParams.size > 0) {
-      retrieveData()
-    }
-  }, [searchParams])
-
-  return data !== undefined ? (
-    <DataTable
-      columns={COLS}
-      data={data}
-      numbered
-      paginated
-      bordered
-      scrollable
-    />
-  ) : isLoading ? (
-    <div>Getting search results...</div>
-  ) : (
-    <></>
-  )
-}
+export const ZeoliticMaterialResults = () => (
+  <SearchResults dataGetter={getZeoliticMaterialsMany} columns={COLS} />
+)
