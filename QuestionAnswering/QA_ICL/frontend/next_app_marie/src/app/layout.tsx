245185d74cfcb7fe0d3b454f7f62eea463bce446
Dan Tran <nmdt2@cam.ac.uk>
2024-07-31 10:10:35 +0800

    dev-qa-icl: Not display molviewer for species if XYZ is not found

--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/layout.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Sidebar from '@/components/sidebar'
 import { cn } from '@/lib/utils'
+import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={cn('flex', inter.className)}>
         <Sidebar />
         {children}
+        <Toaster />
       </body>
     </html>
   )
--- QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/species/[iriEncoded]/page.tsx
+++ QuestionAnswering/QA_ICL/frontend/next_app_marie/src/app/species/[iriEncoded]/page.tsx
@@ -5,6 +5,7 @@ import { capitalize } from '@/lib/utils'
 import { Main } from '@/components/ui/main'
 import { MolViewer } from '@/components/ui/mol-viewer'
 import { PropertyTable } from './_components/property-table'
+import { NotFoundError } from '@/lib/api'
 
 interface SpeciesPageInterface {
   params: { iriEncoded: string }
@@ -13,7 +14,11 @@ interface SpeciesPageInterface {
 export default async function SpeciesPage({ params }: SpeciesPageInterface) {
   const [data, xyz] = await Promise.all([
     getSpeciesOne(params.iriEncoded),
-    getSpeciesXYZ(params.iriEncoded),
+    getSpeciesXYZ(params.iriEncoded).catch(err =>
+      err instanceof NotFoundError
+        ? Promise.resolve(undefined)
+        : Promise.reject(err)
+    ),
   ])
 
   const identifiers = [
@@ -35,9 +40,13 @@ export default async function SpeciesPage({ params }: SpeciesPageInterface) {
     <Main className='flex flex-col items-center'>
       <div className='w-full mt-8 px-4 flex flex-col space-y-4 md:max-w-screen-md lg:max-w-screen-lg'>
         <h1>{data.IUPACName ? capitalize(data.IUPACName) : data.InChI}</h1>
-        <section>
-          <MolViewer type='xyz' data={xyz} />
-        </section>
+        {xyz ? (
+          <section>
+            <MolViewer type='xyz' data={xyz} />
+          </section>
+        ) : (
+          <></>
+        )}
         <section>
           <h2 id='identifiers'>
             <a href='#identifiers' className='hover:underline'>
@@ -54,6 +63,27 @@ export default async function SpeciesPage({ params }: SpeciesPageInterface) {
               </ul>
             </div>
           ))}
+        </section>
+        <section>
+          <h2 id='alt-labels'>
+            <a href='#alt-labels' className='hover:underline'>
+              Alternative labels
+            </a>
+          </h2>
+          <ul className='list-disc list-inside grid md:grid-cols-2 md:grid-flow-row-dense'>
+            {data.altLabel.map((node, i) => (
+              <li
+                key={i}
+                className={
+                  i <= (data.altLabel.length - 1) / 2
+                    ? 'md:col-start-1'
+                    : 'md:col-start-2'
+                }
+              >
+                {node}
+              </li>
+            ))}
+          </ul>
         </section>
         <section>
           <h2 id='chemical-classes'>
