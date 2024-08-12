d1fa5c4165dff996a886a7e44dc156a45baffc13
Oisín <opeppard@cmcl.io>
2024-08-12 10:42:50 +0100

    dev-op-isochroneagent: tidy isochroneagentTest

--- Agents/IsochroneAgent/isochroneagent/src/test/java/uk/ac/cam/cares/jps/agent/isochroneagent/IsochroneAgentTest.java
+++ Agents/IsochroneAgent/isochroneagent/src/test/java/uk/ac/cam/cares/jps/agent/isochroneagent/IsochroneAgentTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -15,26 +14,23 @@ import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
+import java.nio.file.Path;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.nio.file.Path;
-import java.util.Properties;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BadRequestException;
-
 import org.json.JSONObject;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
-
 import com.cmclinnovations.stack.clients.geoserver.GeoServerClient;
 import com.cmclinnovations.stack.clients.geoserver.GeoServerVectorSettings;
 import com.cmclinnovations.stack.clients.ontop.OntopClient;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
 import uk.ac.cam.cares.jps.base.query.RemoteRDBStoreClient;
 import uk.ac.cam.cares.jps.base.query.RemoteStoreClient;
 
@@ -44,13 +40,12 @@ public class IsochroneAgentTest {
     private RemoteStoreClient remoteStoreClient;
 
     @Mock
-    private RemoteRDBStoreClient RemoteRDBStoreClient;
+    private RemoteRDBStoreClient remoteRDBStoreClient;
 
     private IsochroneAgent agent;
 
     @Test
-    @Disabled
-    public void testProcessRequestParameters_EmptyIRIInput() {
+    public void testProcessRequestParametersEmptyIRIInput() {
 
         agent = new IsochroneAgent();
         HttpServletRequest setRequest = mock(HttpServletRequest.class);
@@ -72,43 +67,30 @@ public class IsochroneAgentTest {
     @Test
     public void testReadConfig() {
 
-        String PROPETIES_PATH = "/resources/configTest.properties";
+        final String PROPERTIES_PATH = "/resources/configTest.properties";
 
-        try (InputStream input = FileReader.getStream(PROPETIES_PATH)) {
+        try (InputStream input = FileReader.getStream(PROPERTIES_PATH)) {
             Properties prop = new Properties();
             prop.load(input);
             assertEquals("test", prop.getProperty("db.name"));
             assertEquals(0.0002, prop.getProperty("segmentization_length"));
             assertEquals("", prop.getProperty("kgEndpoint"));
-        } catch (Exception e) {
         }
-
     }
 
-    @SuppressWarnings("resource")
     @Test
-    public void testProcessRequestParameters_ValidInput() {
-        // Mock response
-        MockedConstruction<EndpointConfig> endpointConfigMock = mockConstruction(EndpointConfig.class,
-                (mock, context) -> {
-                    doReturn("test").when(mock).getDbUrl(anyString());
-                    doReturn("test").when(mock).getDbUser();
-                    doReturn("test").when(mock).getDbPassword();
-                });
-
+    public void testProcessRequestParametersValidInput() {
         agent = new IsochroneAgent();
         String content = "db.name=test\nsegmentization_length=0.0002 \nkgEndpoint=\npopulationTables=population, population_test, population_women";
 
         // Create Mockclasses
         InputStream mockInputStream = new ByteArrayInputStream(content.getBytes());
         MockedStatic<FileReader> fileReaderMock = mockStatic(FileReader.class);
-
         fileReaderMock.when(() -> FileReader.getStream(anyString())).thenReturn(mockInputStream);
         MockedConstruction<RouteSegmentization> routeSegmentizationMock = mockConstruction(RouteSegmentization.class,
-                (mock, context) -> {
-                    // Table does not exists
-                    doReturn(false).when(mock).doesTableExist(any(RemoteRDBStoreClient.class));
-                });
+                (mock, context) ->
+                // Table does not exists
+                doReturn(false).when(mock).doesTableExist(any(RemoteRDBStoreClient.class)));
         MockedConstruction<IsochroneGenerator> isochroneGeneratorMock = mockConstruction(IsochroneGenerator.class);
         MockedConstruction<PopulationMapper> populationMapperMock = mockConstruction(PopulationMapper.class);
         MockedConstruction<GeoServerClient> geoserverClientMock = mockConstruction(GeoServerClient.class);
@@ -121,11 +103,10 @@ public class IsochroneAgentTest {
                 populationMapperMock;
                 geoserverClientMock;
                 ontopClientMock;) {
-
-            JSONObject input = new JSONObject();
-            input.put("function", "15MSC");
-            input.put("timethreshold", "15");
-            input.put("timeinterval", "2");
+            JSONObject input = new JSONObject()
+                    .put("function", "15MSC")
+                    .put("timethreshold", "15")
+                    .put("timeinterval", "2");
             agent.processRequestParameters(input);
             verify(endpointConfigMock.constructed().get(0), times(1)).getDbUrl(anyString());
             verify(endpointConfigMock.constructed().get(0), times(1)).getDbUser();
@@ -133,11 +114,13 @@ public class IsochroneAgentTest {
             verify(routeSegmentizationMock.constructed().get(0), times(1)).segmentize(any(RemoteRDBStoreClient.class),
                     anyDouble());
             verify(isochroneGeneratorMock.constructed().get(0), times(1))
-                    .generateIsochrone(any(RemoteRDBStoreClient.class), anyInt(), anyInt(), anyMap());
+                    .generateIsochrone(any(RemoteRDBStoreClient.class), anyInt(), anyInt(), any(Map.class));
             verify(isochroneGeneratorMock.constructed().get(0), times(1))
                     .createIsochroneBuilding(any(RemoteRDBStoreClient.class));
             verify(populationMapperMock.constructed().get(0), times(1))
-                    .checkAndAddColumns(any(RemoteRDBStoreClient.class), any());
+                    .checkAndAddColumns(any(RemoteRDBStoreClient.class), any(ArrayList.class));
+            verify(populationMapperMock.constructed().get(0), times(1)).mapPopulation(any(RemoteRDBStoreClient.class),
+                    any(ArrayList.class));
             verify(geoserverClientMock.constructed().get(0), times(1)).createWorkspace(anyString());
             verify(geoserverClientMock.constructed().get(0), times(1)).createPostGISDataStore(anyString(), anyString(),
                     anyString(), anyString());
@@ -145,6 +128,7 @@ public class IsochroneAgentTest {
                     anyString(), any(GeoServerVectorSettings.class));
             verify(ontopClientMock.constructed().get(0), times(1)).updateOBDA(any(Path.class));
         }
+
     }
 
 }
