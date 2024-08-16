2af8962e32160bc3af22c73401c4d1c4d686821d
George Brownbridge <gbrownbridge@cmcl.io>
2024-08-16 17:06:33 +0100

    dev-geoserverclient-tests: Added missing basic `GeoServerClient` tests.

--- Deploy/stacks/dynamic/stack-clients/src/test/java/com/cmclinnovations/stack/clients/geoserver/GeoServerClientTest.java
+++ Deploy/stacks/dynamic/stack-clients/src/test/java/com/cmclinnovations/stack/clients/geoserver/GeoServerClientTest.java
@@ -5,9 +5,14 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockserver.model.HttpResponse.response;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Assertions;
@@ -23,14 +28,19 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import com.cmclinnovations.stack.clients.mocks.MockGeoServer;
 import com.cmclinnovations.stack.clients.mocks.MockPostGIS;
 import com.cmclinnovations.stack.clients.mocks.MockServiceWithFileSystem;
+import com.cmclinnovations.stack.clients.utils.JsonHelper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 @Testcontainers
 public class GeoServerClientTest {
 
+    private static final String SCHEMA_NAME = "public";
+    private static final String DATABASE_NAME = "postgres";
     private static final String EXISTING_WORKSPACE = "existingWorkspace";
     private static final String NEW_WORKSPACE = "newWorkspace";
     private static final String EXISTING_COVERAGE_STORE = "existingCoverageStore";
     private static final String NEW_COVERAGE_STORE = "newCoverageStore";
+    private static final String NEW_POSTGIS_STORE = "newPostgresStore";
 
     @AutoClose("stop")
     public static MockGeoServer mockGeoServer;
@@ -75,24 +85,23 @@ public class GeoServerClientTest {
     }
 
     @Test
-    void testCreateExistingGeoTiffLayer() {
+    void testCreateGeoTiffLayerExisting() {
         HttpRequest coverageStoreCheck = HttpRequest
                 .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/coveragestores/" + EXISTING_COVERAGE_STORE
                         + ".xml")
                 .withMethod("GET");
         mockGeoServer.when(coverageStoreCheck).respond(response().withStatusCode(200));
 
-        geoServerClient.createGeoTiffLayer(EXISTING_WORKSPACE, EXISTING_COVERAGE_STORE, "postgres", "public",
+        geoServerClient.createGeoTiffLayer(EXISTING_WORKSPACE, EXISTING_COVERAGE_STORE, DATABASE_NAME, SCHEMA_NAME,
                 new GeoServerRasterSettings(), new MultidimSettings());
 
         mockGeoServer.verifyCalls(coverageStoreCheck);
     }
 
     @Test
-    void testCreateNewGeoTiffLayer() {
+    void testCreateGeoTiffLayerNew() {
         HttpRequest coverageStoreCheck = HttpRequest
-                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/coveragestores/" + NEW_COVERAGE_STORE
-                        + ".xml")
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/coveragestores/" + NEW_COVERAGE_STORE + ".xml")
                 .withMethod("GET");
         mockGeoServer.when(coverageStoreCheck).respond(response().withStatusCode(404));
 
@@ -115,7 +124,7 @@ public class GeoServerClientTest {
         mockGeoServer.when(addRasterLayer).respond(response().withStatusCode(200));
 
         try {
-            geoServerClient.createGeoTiffLayer(EXISTING_WORKSPACE, NEW_COVERAGE_STORE, "postgres", "public",
+            geoServerClient.createGeoTiffLayer(EXISTING_WORKSPACE, NEW_COVERAGE_STORE, DATABASE_NAME, SCHEMA_NAME,
                     new GeoServerRasterSettings(), new MultidimSettings());
         } catch (RuntimeException ex) {
         }
@@ -124,19 +133,119 @@ public class GeoServerClientTest {
     }
 
     @Test
-    @Disabled("Needs to be implemented.")
-    void testCreatePostGISDataStore() {
+    void testCreatePostGISDataStoreExisting() {
+        HttpRequest postgresStoreCheck = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/datastores/" + NEW_POSTGIS_STORE + ".xml")
+                .withMethod("GET");
+        mockGeoServer.when(postgresStoreCheck).respond(response().withStatusCode(200));
 
+        geoServerClient.createPostGISDataStore(EXISTING_WORKSPACE, NEW_POSTGIS_STORE, DATABASE_NAME, SCHEMA_NAME);
+
+        mockGeoServer.verifyCalls(postgresStoreCheck);
     }
 
     @Test
-    @Disabled("Needs to be implemented.")
-    void testCreatePostGISLayer() {
+    void testCreatePostGISDataStoreNew() {
 
+        HttpRequest postgresStoreCheck = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/datastores/" + NEW_POSTGIS_STORE + ".xml")
+                .withMethod("GET");
+        mockGeoServer.when(postgresStoreCheck).respond(response().withStatusCode(404));
+
+        HttpRequest postgresStoreCreate = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/datastores.xml")
+                .withMethod("POST");
+        mockGeoServer.when(postgresStoreCreate).respond(response().withStatusCode(200));
+
+        geoServerClient.createPostGISDataStore(EXISTING_WORKSPACE, NEW_POSTGIS_STORE, DATABASE_NAME, SCHEMA_NAME);
+
+        mockGeoServer.verifyCalls(postgresStoreCheck, postgresStoreCreate);
     }
 
     @Test
-    void testCreateExistingWorkspace() {
+    void testCreatePostGISLayerExisting() {
+        HttpRequest postgresStoreCheck = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/datastores/" + DATABASE_NAME + ".xml")
+                .withMethod("GET");
+        mockGeoServer.when(postgresStoreCheck).respond(response().withStatusCode(200));
+
+        String layerName = "layerName";
+        HttpRequest postgresLayerCheck = HttpRequest
+                .request("/rest/layers/" + EXISTING_WORKSPACE + ":" + layerName + ".xml")
+                .withMethod("GET");
+        mockGeoServer.when(postgresLayerCheck).respond(response().withStatusCode(200));
+
+        geoServerClient.createPostGISLayer(EXISTING_WORKSPACE, DATABASE_NAME, layerName,
+                new GeoServerVectorSettings());
+
+        mockGeoServer.verifyCalls(postgresStoreCheck, postgresLayerCheck);
+    }
+
+    @Test
+    void testCreatePostGISLayerExistingStore() {
+        HttpRequest postgresStoreCheck = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/datastores/" + DATABASE_NAME + ".xml")
+                .withMethod("GET");
+        mockGeoServer.when(postgresStoreCheck).respond(response().withStatusCode(200));
+
+        String layerName = "layerName";
+        HttpRequest postgresLayerCheck = HttpRequest
+                .request("/rest/layers/" + EXISTING_WORKSPACE + ":" + layerName + ".xml")
+                .withMethod("GET");
+        mockGeoServer.when(postgresLayerCheck).respond(response().withStatusCode(404));
+
+        HttpRequest postgresLayerCreate = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/datastores/" + DATABASE_NAME + "/featuretypes")
+                .withMethod("POST");
+        mockGeoServer.when(postgresLayerCreate).respond(response().withStatusCode(200));
+
+        HttpRequest postgresLayerConfigure = HttpRequest.request("/rest/layers/" + EXISTING_WORKSPACE + ":" + layerName)
+                .withMethod("PUT");
+        mockGeoServer.when(postgresLayerConfigure).respond(response().withStatusCode(200));
+
+        geoServerClient.createPostGISLayer(EXISTING_WORKSPACE, DATABASE_NAME, layerName,
+                new GeoServerVectorSettings());
+
+        mockGeoServer.verifyCalls(postgresStoreCheck, postgresLayerCheck, postgresLayerCreate,
+                postgresLayerConfigure);
+    }
+
+    @Test
+    void testCreatePostGISLayerNew() {
+        HttpRequest postgresStoreCheck = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/datastores/" + DATABASE_NAME + ".xml")
+                .withMethod("GET");
+        mockGeoServer.when(postgresStoreCheck).respond(response().withStatusCode(404));
+
+        HttpRequest postgresStoreCreate = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/datastores.xml")
+                .withMethod("POST");
+        mockGeoServer.when(postgresStoreCreate).respond(response().withStatusCode(200));
+
+        String layerName = "layerName";
+        HttpRequest postgresLayerCheck = HttpRequest
+                .request("/rest/layers/" + EXISTING_WORKSPACE + ":" + layerName + ".xml")
+                .withMethod("GET");
+        mockGeoServer.when(postgresLayerCheck).respond(response().withStatusCode(404));
+
+        HttpRequest postgresLayerCreate = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/datastores/" + DATABASE_NAME + "/featuretypes")
+                .withMethod("POST");
+        mockGeoServer.when(postgresLayerCreate).respond(response().withStatusCode(200));
+
+        HttpRequest postgresLayerConfigure = HttpRequest.request("/rest/layers/" + EXISTING_WORKSPACE + ":" + layerName)
+                .withMethod("PUT");
+        mockGeoServer.when(postgresLayerConfigure).respond(response().withStatusCode(200));
+
+        geoServerClient.createPostGISLayer(EXISTING_WORKSPACE, DATABASE_NAME, layerName,
+                new GeoServerVectorSettings());
+
+        mockGeoServer.verifyCalls(postgresStoreCheck, postgresStoreCreate, postgresLayerCheck, postgresLayerCreate,
+                postgresLayerConfigure);
+    }
+
+    @Test
+    void testCreateWorkspaceExisting() {
         HttpRequest workspaceCheck = HttpRequest.request("/rest/workspaces/" + EXISTING_WORKSPACE + ".xml")
                 .withMethod("GET");
         mockGeoServer.when(workspaceCheck).respond(response().withStatusCode(200));
@@ -147,7 +256,7 @@ public class GeoServerClientTest {
     }
 
     @Test
-    void testCreateNewWorkspace() {
+    void testCreateWorkspaceNew() {
         HttpRequest workspaceCheck = HttpRequest.request("/rest/workspaces/" + NEW_WORKSPACE + ".xml")
                 .withMethod("GET");
         mockGeoServer.when(workspaceCheck).respond(response().withStatusCode(404));
@@ -174,21 +283,100 @@ public class GeoServerClientTest {
     }
 
     @Test
-    @Disabled("Needs to be implemented.")
     void testLoadIcons() {
+        Path iconPath = Assertions
+                .assertDoesNotThrow(() -> Path.of(GeoServerClientTest.class.getResource("icons/icon1.gif").toURI()));
+        Path baseDirectory = iconPath.getParent().getParent();
 
+        geoServerClient.loadIcons(baseDirectory, "icons");
+
+        byte[] allBytes = Assertions.assertDoesNotThrow(() -> Files.readAllBytes(iconPath));
+        dummyGeoServerContainer.assertFileContent(allBytes, "/opt/geoserver_data/www/icons/icon1.gif");
     }
 
     @Test
-    @Disabled("Needs to be implemented.")
-    void testLoadOtherFiles() {
+    @Disabled("Disabled until fixed in PR #1300")
+    void testLoadOtherFilesGood() throws IOException {
 
+        try (InputStream is = GeoServerClientTest.class.getResourceAsStream("otherFiles.json")) {
+            Path baseDirectory = Assertions
+                    .assertDoesNotThrow(() -> Path.of(GeoServerClientTest.class.getResource("otherFiles").toURI()));
+            List<GeoserverOtherStaticFile> otherFiles = Assertions
+                    .assertDoesNotThrow(() -> JsonHelper.getMapper().readValue(is,
+                            new TypeReference<List<GeoserverOtherStaticFile>>() {
+                            }));
+
+            geoServerClient.loadOtherFiles(baseDirectory, otherFiles);
+
+            String staticDir = "/opt/geoserver_data/www/static_data/";
+            Map<String, String> expected = Map.of(
+                    "a2.txt", "a1",
+                    "a3/a4.txt", "a1",
+                    "d1/c1.txt", "c1",
+                    "d2/c1.txt", "c1",
+                    "d1/d1.txt", "d1",
+                    "d2/d1.txt", "d1",
+                    "d3", "d1",
+                    "d4/d5.txt", "d1");
+
+            dummyGeoServerContainer.assertDirContent(expected, staticDir);
+        }
     }
 
     @Test
-    @Disabled("Needs to be implemented.")
-    void testLoadStyle() {
+    void testLoadStyleExisting() throws IOException {
+        String styleName = "styleName";
+        HttpRequest styleCheck = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/styles/" + styleName + ".xml")
+                .withMethod("GET");
+        mockGeoServer.when(styleCheck).respond(response().withStatusCode(200));
 
+        String fileName = "styleFile.sld";
+        Assertions.assertDoesNotThrow(() -> geoServerClient
+                .loadStyle(JsonHelper.getMapper().readValue(
+                        "{\"name\":\"" + styleName + "\", \"file\":\"" + fileName + "\" }",
+                        GeoServerStyle.class), EXISTING_WORKSPACE));
+
+        mockGeoServer.verifyCalls(styleCheck);
+    }
+
+    @Test
+    void testLoadStyleNew() throws IOException {
+        String styleName = "styleName";
+        HttpRequest styleCheck = HttpRequest
+                .request("/rest/workspaces/" + EXISTING_WORKSPACE + "/styles/" + styleName + ".xml")
+                .withMethod("GET");
+        mockGeoServer.when(styleCheck).respond(response().withStatusCode(404));
+
+        HttpRequest postStyle = HttpRequest.request("/rest/workspaces/" + EXISTING_WORKSPACE + "/styles")
+                // .withPathParameter("name", styleName)
+                .withMethod("POST");
+        mockGeoServer.when(postStyle).respond(response().withStatusCode(200));
+
+        Path configDir = null;
+        Path stylePath = null;
+        try (InputStream styleIn = GeoServerClientTest.class.getResourceAsStream("point_simplepoint.sld")) {
+            configDir = Assertions
+                    .assertDoesNotThrow(() -> Files.createDirectories(Path.of("/inputs/config")));
+
+            stylePath = configDir.resolve("styleFile.sld");
+            Files.copy(styleIn, stylePath);
+
+            Path fileName = stylePath.getFileName();
+            Assertions.assertDoesNotThrow(() -> geoServerClient
+                    .loadStyle(JsonHelper.getMapper().readValue(
+                            "{\"name\":\"" + styleName + "\", \"file\":\"" + fileName + "\" }",
+                            GeoServerStyle.class), EXISTING_WORKSPACE));
+        } finally {
+            if (null != stylePath) {
+                Files.deleteIfExists(stylePath);
+            }
+            if (null != configDir) {
+                Files.deleteIfExists(configDir);
+                Files.deleteIfExists(configDir.getParent());
+            }
+        }
+        mockGeoServer.verifyCalls(styleCheck, postStyle);
     }
 
     @Test
@@ -203,7 +391,7 @@ public class GeoServerClientTest {
     }
 
     @Test
-    void testRemoveNonExistingWorkspace() {
+    void testRemoveWorkspaceNonExisting() {
         HttpRequest workspaceCheck = HttpRequest.request("/rest/workspaces/" + NEW_WORKSPACE + ".xml")
                 .withMethod("GET");
         mockGeoServer.when(workspaceCheck).respond(response().withStatusCode(404));
@@ -214,7 +402,7 @@ public class GeoServerClientTest {
     }
 
     @Test
-    void testRemoveExistingWorkspace() {
+    void testRemoveWorkspaceExisting() {
         HttpRequest workspaceCheck = HttpRequest.request("/rest/workspaces/" + EXISTING_WORKSPACE + ".xml")
                 .withMethod("GET");
         mockGeoServer.when(workspaceCheck).respond(response().withStatusCode(200));
@@ -233,7 +421,7 @@ public class GeoServerClientTest {
     }
 
     @Test
-    void testRemoveExistingWorkspaceWithStyles() {
+    void testRemoveWorkspaceExistingWithStyles() {
         HttpRequest workspaceCheck = HttpRequest.request("/rest/workspaces/" + EXISTING_WORKSPACE + ".xml")
                 .withMethod("GET");
         mockGeoServer.when(workspaceCheck).respond(response().withStatusCode(200));
