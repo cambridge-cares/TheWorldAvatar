64f774b15645e123aad7417736b754d347dea3b6
Jiaru Bai <jb2197@cam.ac.uk>
2024-08-27 17:42:31 +0100

    dev-twa-mops: changed the SPARQL endpoint to loadhost ones as the previously stated public endpoint is no longer working.

--- MOPTools/MOP_Discovery/query_operations/queryendpoint.py
+++ MOPTools/MOP_Discovery/query_operations/queryendpoint.py
@@ -1,4 +1,4 @@
 SPARQL_ENDPOINTS = {
-    'ontospecies': 'http://theworldavatar.com/blazegraph/namespace/ontospecies/sparql',
-    'ontomops': 'https://kg.cmclinnovations.com/blazegraph_geo/namespace/ontomops/sparql'
+    'ontospecies': 'http://localhost:48082/blazegraph/namespace/ontomops/sparql',
+    'ontomops': 'http://localhost:48082/blazegraph/namespace/ontomops/sparql'
 }
