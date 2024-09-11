b0e6a68564c684e6caa2bdb76270df26b7cdd460
qhouyee <quekhouyee@gmail.com>
2024-09-11 12:05:13 +0100

    dev-vip-backend-agent: Improve exception message returned at api

--- Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/exception/AgentExceptionHandler.java
+++ Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/exception/AgentExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.cmclinnovations.agent.exception;
 
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,8 +14,10 @@ import org.springframework.web.context.request.WebRequest;
 public class AgentExceptionHandler {
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> globalExceptionHandling(Exception exception, WebRequest request) {
+    LocalDateTime currentTime = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     return new ResponseEntity<>(
-        exception.getMessage() + String.valueOf(System.currentTimeMillis()),
+        MessageFormat.format("Error encountered at {0}: {1}", currentTime.format(formatter), exception.getMessage()),
         HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
