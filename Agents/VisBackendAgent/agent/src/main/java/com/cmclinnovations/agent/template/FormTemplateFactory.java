edc43c6212bbe4f45278980c6d17c68cda199f39
qhouyee <quekhouyee@gmail.com>
2024-09-11 16:14:24 +0100

    dev-vip-backend-agent: Refactor form template factory constants into a separate interface

--- Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/template/FormTemplateFactory.java
+++ Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/template/FormTemplateFactory.java
@@ -17,7 +17,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
-public class FormTemplateFactory {
+public class FormTemplateFactory implements ShaclTemplateFactory{
   // Data stores
   private Queue<JsonNode> properties;
   private Map<String, Object> form;
@@ -26,31 +26,6 @@ public class FormTemplateFactory {
 
   private final ObjectMapper objectMapper;
   private static final Logger LOGGER = LogManager.getLogger(FormTemplateFactory.class);
-  // JSON LD keys
-  public static final String ID_KEY = "@id";
-  public static final String TYPE_KEY = "@type";
-  public static final String VAL_KEY = "@value";
-  // Namespaces
-  public static final String BASE_PREFIX = "https://www.theworldavatar.com/kg/";
-  public static final String RDFS_PREFIX = "http://www.w3.org/2000/01/rdf-schema#";
-  public static final String SHACL_PREFIX = "http://www.w3.org/ns/shacl#";
-  public static final String XSD_PREFIX = "http://www.w3.org/2001/XMLSchema#";
-  // SHACL classes
-  public static final String PROPERTY_GROUP = "PropertyGroup";
-  public static final String PROPERTY_SHAPE = "PropertyShape";
-  // SHACL properties
-  public static final String COMMENT_PROPERTY = "comment";
-  public static final String LABEL_PROPERTY = "label";
-  public static final String NAME_PROPERTY = "name";
-  public static final String DESCRIPTION_PROPERTY = "description";
-  public static final String ORDER_PROPERTY = "order";
-  public static final String GROUP_PROPERTY = "group";
-  public static final String PROPERTY_PROPERTY = "property";
-  public static final String DEFAULT_VAL_PROPERTY = "defaultValue";
-  public static final String CLASS_PROPERTY = "class";
-  public static final String DATA_TYPE_PROPERTY = "datatype";
-  public static final String IN_PROPERTY = "in";
-  public static final String QUALIFIED_VAL_SHAPE_PROPERTY = "qualifiedValueShape";
 
   /**
    * Constructs a new form template factory.
---
+++ Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/template/ShaclTemplateFactory.java
@@ -0,0 +1,29 @@
+package com.cmclinnovations.agent.template;
+
+public interface ShaclTemplateFactory {
+  // JSON LD keys
+  public static final String ID_KEY = "@id";
+  public static final String TYPE_KEY = "@type";
+  public static final String VAL_KEY = "@value";
+  // Namespaces
+  public static final String BASE_PREFIX = "https://www.theworldavatar.com/kg/";
+  public static final String RDFS_PREFIX = "http://www.w3.org/2000/01/rdf-schema#";
+  public static final String SHACL_PREFIX = "http://www.w3.org/ns/shacl#";
+  public static final String XSD_PREFIX = "http://www.w3.org/2001/XMLSchema#";
+  // SHACL classes
+  public static final String PROPERTY_GROUP = "PropertyGroup";
+  public static final String PROPERTY_SHAPE = "PropertyShape";
+  // SHACL properties
+  public static final String COMMENT_PROPERTY = "comment";
+  public static final String LABEL_PROPERTY = "label";
+  public static final String NAME_PROPERTY = "name";
+  public static final String DESCRIPTION_PROPERTY = "description";
+  public static final String ORDER_PROPERTY = "order";
+  public static final String GROUP_PROPERTY = "group";
+  public static final String PROPERTY_PROPERTY = "property";
+  public static final String DEFAULT_VAL_PROPERTY = "defaultValue";
+  public static final String CLASS_PROPERTY = "class";
+  public static final String DATA_TYPE_PROPERTY = "datatype";
+  public static final String IN_PROPERTY = "in";
+  public static final String QUALIFIED_VAL_SHAPE_PROPERTY = "qualifiedValueShape";
+}
