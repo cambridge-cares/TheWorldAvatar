fad7ae909dbbf7f57fd053249c206ef3679169a7
qhouyee <quekhouyee@gmail.com>
2024-09-12 12:38:16 +0100

    dev-vip-backend-agent: Remove unused imports

--- Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/service/KGService.java
+++ Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/service/KGService.java
@@ -9,16 +9,13 @@ import java.util.stream.StreamSupport;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import com.cmclinnovations.agent.model.SparqlBinding;
 import com.cmclinnovations.agent.template.FormTemplateFactory;
 import com.cmclinnovations.agent.template.QueryTemplateFactory;
-import com.cmclinnovations.agent.utils.StringResource;
 import com.cmclinnovations.stack.clients.blazegraph.BlazegraphClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
