7827a43322e16d95dc39f6c87cd178c14d7d2928
qhouyee <quekhouyee@gmail.com>
2024-09-20 09:55:50 +0100

    dev-vip-backend-agent: Log exceptions when encountered instead of returning just the error message in the request

--- Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/exception/AgentExceptionHandler.java
+++ Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/exception/AgentExceptionHandler.java
@@ -4,6 +4,8 @@ import java.text.MessageFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,10 +14,13 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class AgentExceptionHandler {
+  private static final Logger LOGGER = LogManager.getLogger(AgentExceptionHandler.class);
+
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> globalExceptionHandling(Exception exception, WebRequest request) {
     LocalDateTime currentTime = LocalDateTime.now();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    LOGGER.error("Error encountered:", exception);
     return new ResponseEntity<>(
         MessageFormat.format("Error encountered at {0}: {1}", currentTime.format(formatter), exception.getMessage()),
         HttpStatus.INTERNAL_SERVER_ERROR);
