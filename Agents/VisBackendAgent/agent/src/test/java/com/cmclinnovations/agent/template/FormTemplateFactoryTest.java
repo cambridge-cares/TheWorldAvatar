c0a14fd9ce8bc8d55cb725acd29fa07bc26d5bee
qhouyee <quekhouyee@gmail.com>
2024-09-20 10:02:21 +0100

    dev-vip-backend-agent: Fix unit test

--- Agents/VisBackendAgent/agent/src/test/java/com/cmclinnovations/agent/template/FormTemplateFactoryTest.java
+++ Agents/VisBackendAgent/agent/src/test/java/com/cmclinnovations/agent/template/FormTemplateFactoryTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.cmclinnovations.agent.utils.ShaclResource;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -59,7 +60,7 @@ class FormTemplateFactoryTest {
     ArrayNode sample = objectMapper.createArrayNode();
     // Mocking an invalid JSON object that does not have a valid type
     ObjectNode invalidShape = genPropertyShape("testInvalidShape", "invalidType", "invalid field",
-        "This shape should fail", FormTemplateFactory.XSD_PREFIX + XSD_STRING_TYPE, "");
+        "This shape should fail", ShaclResource.XSD_PREFIX + XSD_STRING_TYPE, "");
     sample.add(invalidShape);
     // Execute & assert
     IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
@@ -73,12 +74,12 @@ class FormTemplateFactoryTest {
     // Set up
     ArrayNode sample = objectMapper.createArrayNode();
     ObjectNode firstShape = genPropertyShape(STRING_TEST_CASE_ID,
-        FormTemplateFactory.SHACL_PREFIX + FormTemplateFactory.PROPERTY_SHAPE, STRING_TEST_CASE_NAME,
-        STRING_TEST_CASE_DESCRIPTION, FormTemplateFactory.XSD_PREFIX + XSD_STRING_TYPE, STRING_TEST_CASE_ORDER);
+        ShaclResource.SHACL_PREFIX + ShaclResource.PROPERTY_SHAPE, STRING_TEST_CASE_NAME,
+        STRING_TEST_CASE_DESCRIPTION, ShaclResource.XSD_PREFIX + XSD_STRING_TYPE, STRING_TEST_CASE_ORDER);
     sample.add(firstShape);
     ObjectNode secShape = genPropertyShape(NUMBER_TEST_CASE_ID,
-        FormTemplateFactory.SHACL_PREFIX + FormTemplateFactory.PROPERTY_SHAPE, NUMBER_TEST_CASE_NAME,
-        NUMBER_TEST_CASE_DESCRIPTION, FormTemplateFactory.XSD_PREFIX + XSD_INTEGER_TYPE, NUMBER_TEST_CASE_ORDER);
+        ShaclResource.SHACL_PREFIX + ShaclResource.PROPERTY_SHAPE, NUMBER_TEST_CASE_NAME,
+        NUMBER_TEST_CASE_DESCRIPTION, ShaclResource.XSD_PREFIX + XSD_INTEGER_TYPE, NUMBER_TEST_CASE_ORDER);
     sample.add(secShape);
     // Execute
     Map<String, Object> result = this.formTemplateFactory.genTemplate(sample, new HashMap<>());
@@ -106,31 +107,31 @@ class FormTemplateFactoryTest {
     // Init empty JSON object
     ObjectNode propertyShape = objectMapper.createObjectNode();
     // Add ID
-    propertyShape.put(FormTemplateFactory.ID_KEY, id);
+    propertyShape.put(ShaclResource.ID_KEY, id);
     // Add type as "@type:[class]"
     ArrayNode typeValueNode = objectMapper.createArrayNode()
         .add(typeClass);
-    propertyShape.set(FormTemplateFactory.TYPE_KEY, typeValueNode);
+    propertyShape.set(ShaclResource.TYPE_KEY, typeValueNode);
     // Add name as`"sh:name":[{"@value" : "name"}]`
     ObjectNode nameValueNode = objectMapper.createObjectNode()
-        .put(FormTemplateFactory.VAL_KEY, name);
-    propertyShape.set(FormTemplateFactory.SHACL_PREFIX + FormTemplateFactory.NAME_PROPERTY,
+        .put(ShaclResource.VAL_KEY, name);
+    propertyShape.set(ShaclResource.SHACL_PREFIX + ShaclResource.NAME_PROPERTY,
         objectMapper.createArrayNode().add(nameValueNode));
     // Add description as `"sh:description":[{"@value" : "description"}]`
     ObjectNode descriptionValueNode = objectMapper.createObjectNode()
-        .put(FormTemplateFactory.VAL_KEY, description);
-    propertyShape.set(FormTemplateFactory.SHACL_PREFIX + FormTemplateFactory.DESCRIPTION_PROPERTY,
+        .put(ShaclResource.VAL_KEY, description);
+    propertyShape.set(ShaclResource.SHACL_PREFIX + ShaclResource.DESCRIPTION_PROPERTY,
         objectMapper.createArrayNode().add(descriptionValueNode));
     // Add datatype as `"sh:datatype":[{"@id" : "data type"}]`
     ObjectNode dataTypeValueNode = objectMapper.createObjectNode()
-        .put(FormTemplateFactory.ID_KEY, dataType);
-    propertyShape.set(FormTemplateFactory.SHACL_PREFIX + FormTemplateFactory.DATA_TYPE_PROPERTY,
+        .put(ShaclResource.ID_KEY, dataType);
+    propertyShape.set(ShaclResource.SHACL_PREFIX + ShaclResource.DATA_TYPE_PROPERTY,
         objectMapper.createArrayNode().add(dataTypeValueNode));
     // Add order as`"sh:order":[{"@type": "xsd:type", "@value" : "order"}]`
     ObjectNode orderValueNode = objectMapper.createObjectNode()
-        .put(FormTemplateFactory.TYPE_KEY, FormTemplateFactory.XSD_PREFIX + XSD_INTEGER_TYPE)
-        .put(FormTemplateFactory.VAL_KEY, order);
-    propertyShape.set(FormTemplateFactory.SHACL_PREFIX + FormTemplateFactory.ORDER_PROPERTY,
+        .put(ShaclResource.TYPE_KEY, ShaclResource.XSD_PREFIX + XSD_INTEGER_TYPE)
+        .put(ShaclResource.VAL_KEY, order);
+    propertyShape.set(ShaclResource.SHACL_PREFIX + ShaclResource.ORDER_PROPERTY,
         objectMapper.createArrayNode().add(orderValueNode));
     // Return object
     return propertyShape;
@@ -150,21 +151,21 @@ class FormTemplateFactoryTest {
   public static void validatePropertyShape(Map<String, Object> result, String id, String name, String description,
       String dataType, String order) {
     List<Map<String, Object>> propertyShapesList = (List<Map<String, Object>>) result
-        .get(FormTemplateFactory.PROPERTY_PROPERTY);
+        .get(ShaclResource.PROPERTY_PROPERTY);
     Optional<Map<String, Object>> outputProperty = propertyShapesList.stream()
-        .filter(current -> id.equals(current.get(FormTemplateFactory.ID_KEY)))
+        .filter(current -> id.equals(current.get(ShaclResource.ID_KEY)))
         .findFirst();
     outputProperty.ifPresentOrElse(
         property -> {
           assertEquals(name,
-              ((Map<String, Object>) property.get(FormTemplateFactory.NAME_PROPERTY)).get(FormTemplateFactory.VAL_KEY),
+              ((Map<String, Object>) property.get(ShaclResource.NAME_PROPERTY)).get(ShaclResource.VAL_KEY),
               "Name should match");
           assertEquals(description,
-              ((Map<String, Object>) property.get(FormTemplateFactory.DESCRIPTION_PROPERTY))
-                  .get(FormTemplateFactory.VAL_KEY),
+              ((Map<String, Object>) property.get(ShaclResource.DESCRIPTION_PROPERTY))
+                  .get(ShaclResource.VAL_KEY),
               "Description should match");
-          assertEquals(dataType, property.get(FormTemplateFactory.DATA_TYPE_PROPERTY), "Data type should match");
-          assertEquals(order, property.get(FormTemplateFactory.ORDER_PROPERTY).toString(), "Order should match");
+          assertEquals(dataType, property.get(ShaclResource.DATA_TYPE_PROPERTY), "Data type should match");
+          assertEquals(order, property.get(ShaclResource.ORDER_PROPERTY).toString(), "Order should match");
         },
         () -> assertTrue(false, "No matching property is found for the id"));
   }
