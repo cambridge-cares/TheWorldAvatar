baae2a9bd4318af116db810940b30dc0e64c953e
George Brownbridge <gbrownbridge@cmcl.io>
2024-09-24 11:39:02 +0100

    add-ontology-to-ontop: Simplified the `OntopSettings` class.

--- Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/OntopSettings.java
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/OntopSettings.java
@@ -4,22 +4,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class OntopSettings {
 
     @JsonProperty
-    private final Optional<List<String>> rules;
-
-    @JsonCreator
-    OntopSettings() {
-        this.rules = Optional.empty();
-    }
-
-    OntopSettings(Optional<List<String>> rules) {
-        this.rules = rules;
-    }
+    private final Optional<List<String>> rules = Optional.empty();
 
     public List<String> getRules() {
         return rules.orElse(Collections.emptyList());
