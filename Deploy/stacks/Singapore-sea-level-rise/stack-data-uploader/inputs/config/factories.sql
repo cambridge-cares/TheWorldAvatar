4dea986513f58c2220c350df1e1e3137db450242
Kok Foong Lee <kfl31@cam.ac.uk>
2024-09-26 16:28:06 +0800

    Reapply "dev-integrate-sg-sea-level: changed UUID columns to TEXT columns"
    This reverts commit ae2acbc323cbfa71d1dd3ccb6506a0e7a2343aa9.

--- Deploy/stacks/Singapore-sea-level-rise/stack-data-uploader/inputs/config/factories.sql
+++ Deploy/stacks/Singapore-sea-level-rise/stack-data-uploader/inputs/config/factories.sql
@@ -4,9 +4,9 @@ drop column if exists company_uuid,
 drop column if exists industry_uuid,
 drop column if exists facility_uuid;
 alter table factories 
-add column company_uuid UUID,
-add column industry_uuid UUID,
-add column facility_uuid UUID;
+add column company_uuid TEXT,
+add column industry_uuid TEXT,
+add column facility_uuid TEXT;
 UPDATE factories
 SET company_uuid = uuid_generate_v5('19a7288a-dbfd-48a4-8726-de1b6e30fce4', company),
    industry_uuid = uuid_generate_v5('ee95784f-22cc-4389-82a5-244736859dc3', infrastructure_type),
