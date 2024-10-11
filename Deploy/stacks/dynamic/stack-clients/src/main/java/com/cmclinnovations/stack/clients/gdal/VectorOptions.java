51db3474ea5beda7bba16af1c08285896cb3fba8
Oisín <oisinpeppard@hotmail.com>
2024-10-11 09:50:37 +0100

    create abstract vector options class to superset polygonize options and ogr2ogr

---
+++ Deploy/stacks/dynamic/stack-clients/src/main/java/com/cmclinnovations/stack/clients/gdal/VectorOptions.java
@@ -0,0 +1,94 @@
+package com.cmclinnovations.stack.clients.gdal;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public abstract class VectorOptions<T extends VectorOptions<T>> extends CommonOptions<T> {
+
+    @JsonProperty
+    private final Map<String, String> datasetCreationOptions = new HashMap<>();
+    @JsonProperty
+    private final Map<String, String> layerCreationOptions = new HashMap<>();
+    @JsonProperty
+    private final Map<String, String> outputDatasetOpenOptions = new HashMap<>();
+
+    public VectorOptions(String commamnd) {
+        super(commamnd);
+    }
+
+    @SuppressWarnings("unchecked")
+    public T addDatasetCreationOption(String name, String value) {
+        datasetCreationOptions.put(name, value);
+        return (T) this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T addLayerCreationOption(String name, String value) {
+        layerCreationOptions.put(name, value);
+        return (T) this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T addOutputDatasetOpenOption(String name, String value) {
+        outputDatasetOpenOptions.put(name, value);
+        return (T) this;
+    }
+
+    private void processDatasetCreationOption(List<String> args, String name, String value) {
+        processKeyValuePair(args, "-dsco", name, value);
+    }
+
+    private void processLayerCreationOption(List<String> args, String name, String value) {
+        processKeyValuePair(args, "-lco", name, value);
+    }
+
+    private void processOutputDatasetOpenOption(List<String> args, String name, String value) {
+        processKeyValuePair(args, "-doo", name, value);
+    }
+
+    public String[] generateCommand(String layerName, boolean append, String source, String destination,
+            String... extraArgs) {
+
+        List<String> args = new ArrayList<>(2 * extraArgs.length);
+
+        if (null != layerName) {
+            processLayerNameOption(layerName, args);
+        }
+
+        processConfigOption(args, "OGR_TRUNCATE", append ? "NO" : "YES");
+
+        return generateCommandInternal(args, source, destination, extraArgs);
+    }
+
+    protected void processLayerNameOption(String layerName, List<String> args) {
+        processOtherOption(args, "-nln", layerName);
+    }
+
+    @Override
+    protected void processArgs(List<String> args) {
+        super.processArgs(args);
+
+        processOtherOption(args, "-f", "PostgreSQL");
+
+        processConfigOption(args, "PG_USE_COPY", "YES");
+
+        // Setting this option prevents GDAL from "cleaning" the table and column
+        // names for Postgres, as described here:
+        // https://gdal.org/drivers/vector/pg.html#layer-creation-options
+        processLayerCreationOption(args, "LAUNDER", "NO");
+
+        datasetCreationOptions.forEach((name, value) -> processDatasetCreationOption(args, name, value));
+        layerCreationOptions.forEach((name, value) -> processLayerCreationOption(args, name, value));
+        outputDatasetOpenOptions.forEach((name, value) -> processOutputDatasetOpenOption(args, name, value));
+    }
+
+    public void setSchema(String schema) {
+        if (null != schema) {
+            layerCreationOptions.put("SCHEMA", schema);
+        }
+    }
+}
