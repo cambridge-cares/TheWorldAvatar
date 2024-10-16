74e197083a7c99b6fdd4abfba1b0c6e12fada885
Md Hanif Seddiqui <mhs62@cam.ac.uk>
2024-10-16 11:26:48 +0100

    Update ProcessQuery.java

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/query/fedq/ProcessQuery.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/query/fedq/ProcessQuery.java
@@ -1,6 +1,8 @@
 package uk.ac.cam.cares.jps.base.query.fedq;
 
 import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.sparql.algebra.Op;
 import org.apache.jena.sparql.algebra.OpWalker;
@@ -22,6 +24,10 @@ import java.lang.reflect.Type;
 
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.Triple;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
 import java.io.*;
 import java.util.*;
 
@@ -189,6 +195,7 @@ public class ProcessQuery {
         // ?MechanismIRI rdfs:label ?MechanismName .
         // }
         // """;
+
         String sparqlQuery = """
                 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                 PREFIX owl: <http://www.w3.org/2002/07/owl#>
@@ -208,42 +215,127 @@ public class ProcessQuery {
                 }
                 """;
 
+        // String sparqlQuery = """
+        // PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        // PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        // PREFIX pt: <http://www.daml.org/2003/01/periodictable/PeriodicTable.owl#>
+        // PREFIX OntoKin: <http://www.theworldavatar.com/ontology/ontokin/OntoKin.owl#>
+
+        // SELECT DISTINCT ?identifier ?atomicMass ?atomicMassUnits
+        // WHERE {
+        // SERVICE
+        // <http://localhost:8080/blazegraph/namespace/namespace_compchem/sparql> {
+        // ?element1 rdf:type pt:Element .
+        // BIND(STRAFTER(STR(?element1), "#") AS ?identifier)
+        // }
+        // SERVICE <http://localhost:8080/blazegraph/namespace/namespace_kin/sparql> {
+        // ?element2 rdf:type OntoKin:Element .
+        // ?element2 rdfs:label ?identifier1 .
+        // ?element2 OntoKin:hasAtomicMass ?atomicMass .
+        // ?element2 OntoKin:hasAtomicMassUnits ?atomicMassUnits .
+        // FILTER(?identifier = ?identifier1)
+        // }
+        // }
+        // """;
+
         ProcessQuery ana = new ProcessQuery();
         String indexLocation = "C:/Users/printer_admin/Downloads/KGs/tests/";
         ana.loadIndices(indexLocation);
 
-        long totatl_duration = 0;
-        int iteration = 100;
-        for (int i = 0; i < iteration; i++) {
-            long startTime = System.nanoTime();
+        int iteration = 600;
+        int lower_index = 32;
+        int upper_index = 40;
 
-            Set<String> eps = new HashSet<>();
+        String endpoints[] = { "http://localhost:8080/blazegraph/namespace/namespace_kin/sparql",
+                "http://localhost:8080/blazegraph/namespace/namespace_compchem/sparql",
+                "http://localhost:8080/blazegraph/namespace/namespace_automotive/sparql",
+                "http://localhost:8080/blazegraph/namespace/namespace_species/sparql",
+                "http://localhost:8080/blazegraph/namespace/namespace_uken/sparql",
+                "http://localhost:8080/blazegraph/namespace/uken_1m/sparql",
+                "http://localhost:8080/blazegraph/namespace/uken_10m/sparql",
+                "http://localhost:8080/blazegraph/namespace/uken_200m/sparql"
+        };
+        String experiments[] = { "0", "01", "012", "0123", "01234", "012345", "0123456", "01234567",
+                "1", "12", "123", "1234", "12345", "123456", "1234567", "12345670",
+                "2", "23", "234", "2345", "23456", "234567", "2345670", "23456701",
+                "3", "34", "345", "3456", "34567", "345670", "3456701", "34567012",
+                "4", "45", "456", "4567", "45670", "456701", "4567012", "45670123",
+                "5", "56", "567", "5670", "56701", "567012", "5670123", "56701234",
+                "6", "67", "670", "6701", "67012", "670123", "6701234", "67012345",
+                "7", "70", "701", "7012", "70123", "701234", "7012345", "70123456" };
 
-            // run against extracted endpoints
-            // ana.extractClassesAndProperties(sparqlQuery);
-            // eps = ana.getEndpoints();
-
-            // run against manual endpoints
-            eps.add("http://localhost:8080/blazegraph/namespace/namespace_all/sparql");
-            eps.add("http://localhost:8080/blazegraph/namespace/namespace_uken/sparql");
-            eps.add("http://localhost:8080/blazegraph/namespace/namespace_kin/sparql");
-            eps.add("http://localhost:8080/blazegraph/namespace/namespace_compchem/sparql");
-            eps.add("http://localhost:8080/blazegraph/namespace/namespace_species/sparql");
-
-            Set<BindingSet> result = ana.executeQuery(eps, sparqlQuery);
-            long endTime = System.nanoTime();
-            long duration = (endTime - startTime) / 1000000;
-            if (i == 0) {
-                System.out.println("First execution time: " + duration + " millisecond.");
-                for (BindingSet element : result) {
-                    System.out.println(element);
-                }
+        double[][] results = new double[64][3];
+        for (int expt = lower_index; expt < upper_index; expt++) {
+            // init result
+            for (int k = 0; k < 3; k++) {
+                results[expt][k] = 0;
             }
-            totatl_duration += duration;
 
+            long totatl_duration = 0;
+            long first_duration = 0;
+
+            for (int i = 0; i < iteration; i++) {
+                long startTime = System.nanoTime();
+
+                Set<String> eps = new HashSet<>();
+
+                // run against extracted endpoints
+                // ana.extractClassesAndProperties(sparqlQuery);
+                // eps = ana.getEndpoints();
+
+                // run against manual endpoints
+                // eps.add("http://localhost:8080/blazegraph/namespace/namespace_all/sparql");
+                // eps.add("http://localhost:8080/blazegraph/namespace/namespace_automotive/sparql");
+                // eps.add("http://localhost:8080/blazegraph/namespace/namespace_compchem/sparql");
+                // eps.add("http://localhost:8080/blazegraph/namespace/namespace_kin/sparql");
+                // eps.add("http://localhost:8080/blazegraph/namespace/namespace_species/sparql");
+                // eps.add("http://localhost:8080/blazegraph/namespace/namespace_uken/sparql");
+                // eps.add("http://localhost:8080/blazegraph/namespace/uken_10m/sparql");
+                // eps.add("http://localhost:8080/blazegraph/namespace/uken_1m/sparql");
+                // eps.add("http://localhost:8080/blazegraph/namespace/uken_200m/sparql");
+                // select endpoints
+                int nos = experiments[expt].length();
+                for (int index = 0; index < nos; index++) {
+                    char charDigit = experiments[expt].charAt(index);
+                    int ep_index = Character.getNumericValue(charDigit);
+                    eps.add(endpoints[ep_index]);
+                }
+
+                Set<BindingSet> result = ana.executeQuery(eps, sparqlQuery);
+                long endTime = System.nanoTime();
+                long duration = (endTime - startTime) / 1000000;
+                if (i == 0) {
+                    first_duration = duration;
+                    for (BindingSet element : result) {
+                        System.out.println(element);
+                    }
+                    System.out.println("Against " + eps.size() + " endpoints");
+                }
+                totatl_duration += duration;
+                // System.out.print(i + "\t");
+            }
+
+            results[expt][0] = (double) first_duration;
+            results[expt][1] = totatl_duration / (double) iteration;
+            results[expt][2] = (totatl_duration - first_duration) / ((double) iteration - 1.0);
+
+            System.out.println("Experiment " + (expt + 1) + " finished: [" + results[expt][0] + ", " + results[expt][1]
+                    + ", " + results[expt][2] + "]");
+            // System.out.println("First execution time: " + first_duration + "
+            // millisecond.");
+            // System.out
+            // .println("Processing Time including first iteration: " + totatl_duration /
+            // iteration + " millisecond");
+            // System.out.println("Processing Time without first iteration: "
+            // + (totatl_duration - first_duration) / (iteration - 1) + " millisecond");
         }
 
-        System.out.println("Processing Time: " + totatl_duration / iteration + " millisecond");
-
+        for (int expt = lower_index; expt < upper_index; expt++) {
+            for (int k = 0; k < 3; k++) {
+                System.out.print(results[expt][k] + "\t");
+            }
+            System.out.println();
+        }
     }
+
 }
