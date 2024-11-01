c8810a30638964d7f7678e50b9f044bfb927c54a
ChungTingLao <chung.ting.lao.34@gmail.com>
2024-11-01 14:37:39 +0000

    dev-time-series-bulk: use parallelStream to speed up time series initialisation in KG.

--- JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/timeseries/TimeSeriesSparql.java
+++ JPS_BASE_LIB/src/main/java/uk/ac/cam/cares/jps/base/timeseries/TimeSeriesSparql.java
@@ -482,7 +482,7 @@ public class TimeSeriesSparql {
 
         Map<CustomDuration, String> durationMap = createDurationIRIMapping();
 
-        timeSeriesKgMetadataList.stream().forEach(timeSeriesKgMetadata -> {
+        timeSeriesKgMetadataList.parallelStream().forEach(timeSeriesKgMetadata -> {
             Iri tsIRI = iri(timeSeriesKgMetadata.getTimeSeriesIri());
 
             modify.and(tsIRI.isA(timeSeriesKgMetadata.getTimeSeriesType()));
