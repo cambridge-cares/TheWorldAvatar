7628e566bfcf2b85d1205cbeff088a95da6ad6d5
ChungTingLao <chung.ting.lao.34@gmail.com>
2024-11-07 14:41:18 +0000

    dev-flood-routing-consolidation: separate geoserver layer creation in its own function in TSP agent.

--- Agents/TravellingSalesmanAgent/travellingsalesmanagent/src/main/java/uk/ac/cam/cares/jps/agent/travellingsalesmanagent/TSPRouteGenerator.java
+++ Agents/TravellingSalesmanAgent/travellingsalesmanagent/src/main/java/uk/ac/cam/cares/jps/agent/travellingsalesmanagent/TSPRouteGenerator.java
@@ -1,38 +1,39 @@
 package uk.ac.cam.cares.jps.agent.travellingsalesmanagent;
 
-
 import com.cmclinnovations.stack.clients.geoserver.GeoServerClient;
 import com.cmclinnovations.stack.clients.geoserver.GeoServerVectorSettings;
 import com.cmclinnovations.stack.clients.geoserver.UpdatedGSVirtualTableEncoder;
 
 public class TSPRouteGenerator {
 
-
     /**
      * Generate TSP route layer
+     * 
      * @param geoServerClient
      * @param workspaceName
      * @param schema
      * @param dbName
-     * @param LayerName
-     * @param cost_table
+     * @param layerName
+     * @param costTable
      */
-    public void generateTSPLayer(GeoServerClient geoServerClient, String workspaceName, String schema, String dbName, String LayerName, String cost_table){
+    public void generateTSPLayer(GeoServerClient geoServerClient, String workspaceName, String schema, String dbName,
+            String layerName, String costTable) {
 
         String tspLayer = "WITH tsp AS (\n" +
                 "    SELECT *\n" +
                 "    FROM pgr_TSP(\n" +
                 "        $$SELECT * FROM pgr_dijkstraCostMatrix(\n" +
-                "            '"+cost_table+"',\n" +
+                "            '" + costTable + "',\n" +
                 "            (\n" +
                 "                SELECT ARRAY_APPEND(array_agg(id), (%target%))\n" +
                 "                FROM routing_ways_vertices_pgr\n" +
-                "                WHERE id IN (SELECT DISTINCT nearest_node FROM poi_tsp_nearest_node, flood_polygon_single_10cm WHERE ST_Intersects(poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom) OR ST_DISTANCE (poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom) < 0.005)\n" +
+                "                WHERE id IN (SELECT DISTINCT nearest_node FROM poi_tsp_nearest_node, flood_polygon_single_10cm WHERE ST_Intersects(poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom) OR ST_DISTANCE (poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom) < 0.005)\n"
+                +
                 "            ),\n" +
                 "            false\n" +
                 "        )$$, \n" +
-                "        (%target%),"+
-                "        (%target%)"+
+                "        (%target%)," +
+                "        (%target%)" +
                 "    )\n" +
                 "),\n" +
                 "tsp_route AS (\n" +
@@ -46,7 +47,7 @@ public class TSPRouteGenerator {
                 "    FROM tsp\n" +
                 ") n2 ON n1.seq + 1 = n2.seq\n" +
                 "JOIN pgr_dijkstra(\n" +
-                "    '"+cost_table+"',\n" +
+                "    '" + costTable + "',\n" +
                 "    n1.node,\n" +
                 "    n2.node, false\n" +
                 ") AS di ON true\n" +
@@ -56,65 +57,65 @@ public class TSPRouteGenerator {
                 "SELECT tsp_route.the_geom as geom\n" +
                 "FROM tsp_route ";
 
-        // Find nearest TSP_node which is the nearest grid
-        UpdatedGSVirtualTableEncoder virtualTableTSPRoute = new UpdatedGSVirtualTableEncoder();
-        GeoServerVectorSettings geoServerVectorSettingsTSPRoute = new GeoServerVectorSettings();
-        virtualTableTSPRoute.setSql(tspLayer);
-        virtualTableTSPRoute.setEscapeSql(true);
-        virtualTableTSPRoute.setName(LayerName);
-        virtualTableTSPRoute.addVirtualTableParameter("target","4121","^[\\d]+$");
-        virtualTableTSPRoute.addVirtualTableGeometry("geom", "Geometry", "4326"); // geom needs to match the sql query
-        geoServerVectorSettingsTSPRoute.setVirtualTable(virtualTableTSPRoute);
-        geoServerClient.createPostGISDataStore(workspaceName,LayerName, dbName, schema);
-        geoServerClient.createPostGISLayer(workspaceName, dbName,"public", LayerName ,geoServerVectorSettingsTSPRoute);
+        // Add TSP route as geoserver layer
+        addTSPGeoserverLayer(geoServerClient, workspaceName, schema, dbName, layerName, tspLayer);
     }
 
     /**
      * Generate TSP sequence layer
+     * 
      * @param geoServerClient
      * @param workspaceName
      * @param schema
      * @param dbName
-     * @param LayerName
-     * @param cost_table
+     * @param layerName
+     * @param costTable
      */
-    public void generateSequenceLayer(GeoServerClient geoServerClient, String workspaceName, String schema, String dbName, String LayerName, String cost_table){
+    public void generateSequenceLayer(GeoServerClient geoServerClient, String workspaceName, String schema,
+            String dbName, String layerName, String costTable) {
 
-        LayerName= LayerName+"_seq";
-
+        layerName = layerName + "_seq";
 
         String tspLayer = "WITH tsp AS (\n" +
                 "    SELECT *\n" +
                 "    FROM pgr_TSP(\n" +
                 "        $$SELECT * FROM pgr_dijkstraCostMatrix(\n" +
-                "            '"+cost_table+"',\n" +
+                "            '" + costTable + "',\n" +
                 "            (\n" +
                 "                SELECT ARRAY_APPEND(array_agg(id), (%target%))\n" +
                 "                FROM routing_ways_vertices_pgr\n" +
-                "                WHERE id IN (SELECT DISTINCT nearest_node FROM poi_tsp_nearest_node, flood_polygon_single_10cm WHERE ST_Intersects(poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom) OR ST_DISTANCE (poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom) < 0.005)\n" +
+                "                WHERE id IN (SELECT DISTINCT nearest_node FROM poi_tsp_nearest_node, flood_polygon_single_10cm WHERE ST_Intersects(poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom) OR ST_DISTANCE (poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom) < 0.005)\n"
+                +
                 "            ),\n" +
                 "            false\n" +
                 "        )$$, \n" +
-                "        (%target%),"+
-                "        (%target%)"+
+                "        (%target%)," +
+                "        (%target%)" +
                 "    )\n" +
                 "), tsp_seq AS (    SELECT nearest_node as id, poi_tsp_nearest_node.geom\n" +
                 "    FROM poi_tsp_nearest_node, flood_polygon_single_10cm\n" +
                 "                WHERE ST_Intersects(poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom)\n" +
-                "                    OR ST_DISTANCE(poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom) < 0.005)\n" +
+                "                    OR ST_DISTANCE(poi_tsp_nearest_node.geom, flood_polygon_single_10cm.geom) < 0.005)\n"
+                +
                 "SELECT DISTINCT tsp.seq -1 as seq, tsp.node, tsp.cost, tsp.agg_cost, tsp_seq.geom FROM tsp\n" +
                 "FULL JOIN tsp_seq ON tsp.node = tsp_seq.id ORDER BY seq ASC";
 
-        // Find nearest TSP_node which is the nearest grid
+        // Add TSP sequence as geoserver layer
+        addTSPGeoserverLayer(geoServerClient, workspaceName, schema, dbName, layerName, tspLayer);
+
+    }
+
+    void addTSPGeoserverLayer(GeoServerClient geoServerClient, String workspaceName, String schema,
+            String dbName, String layerName, String tspLayer) {
         UpdatedGSVirtualTableEncoder virtualTableTSPRoute = new UpdatedGSVirtualTableEncoder();
         GeoServerVectorSettings geoServerVectorSettingsTSPRoute = new GeoServerVectorSettings();
         virtualTableTSPRoute.setSql(tspLayer);
         virtualTableTSPRoute.setEscapeSql(true);
-        virtualTableTSPRoute.setName(LayerName);
-        virtualTableTSPRoute.addVirtualTableParameter("target","4121","^[\\d]+$");
+        virtualTableTSPRoute.setName(layerName);
+        virtualTableTSPRoute.addVirtualTableParameter("target", "4121", "^[\\d]+$");
         virtualTableTSPRoute.addVirtualTableGeometry("geom", "Geometry", "4326"); // geom needs to match the sql query
         geoServerVectorSettingsTSPRoute.setVirtualTable(virtualTableTSPRoute);
-        geoServerClient.createPostGISDataStore(workspaceName,LayerName, dbName, schema);
-        geoServerClient.createPostGISLayer(workspaceName, dbName, "public", LayerName ,geoServerVectorSettingsTSPRoute);
+        geoServerClient.createPostGISDataStore(workspaceName, layerName, dbName, schema);
+        geoServerClient.createPostGISLayer(workspaceName, dbName, schema, layerName, geoServerVectorSettingsTSPRoute);
     }
 }
