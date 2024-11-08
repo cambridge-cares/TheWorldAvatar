aef885fcd4fc08e0b69920a3df565c657f7c9cf7
ChungTingLao <chung.ting.lao.34@gmail.com>
2024-11-08 10:45:51 +0000

    dev-flood-routing-consolidation: refactor CTE construction in TSP agent.

--- Agents/TravellingSalesmanAgent/travellingsalesmanagent/src/main/java/uk/ac/cam/cares/jps/agent/travellingsalesmanagent/TSPRouteGenerator.java
+++ Agents/TravellingSalesmanAgent/travellingsalesmanagent/src/main/java/uk/ac/cam/cares/jps/agent/travellingsalesmanagent/TSPRouteGenerator.java
@@ -1,15 +1,14 @@
 package uk.ac.cam.cares.jps.agent.travellingsalesmanagent;
 
 import com.cmclinnovations.stack.clients.geoserver.GeoServerClient;
-import com.cmclinnovations.stack.clients.geoserver.GeoServerVectorSettings;
-import com.cmclinnovations.stack.clients.geoserver.UpdatedGSVirtualTableEncoder;
 
 public class TSPRouteGenerator {
 
     String poiTableName; // table containing POI and nearest node information
     String floodTableName; // table defining flooded region
     String routeTablePrefix; // prefix of OSM routing tables
-    Double floodCutOff = 0.0; // Cut-off distance in meters between POI and flood; if distance is below this value, POI is consider flooded
+    Double floodCutOff = 0.0; // Cut-off distance in meters between POI and flood; if distance is below this
+                              // value, POI is consider flooded
 
     public TSPRouteGenerator(String poiTableName, String floodTableName, String routeTablePrefix, Double floodCutOff) {
         this.poiTableName = poiTableName;
@@ -31,27 +30,9 @@ public class TSPRouteGenerator {
     public void generateTSPLayer(GeoServerClient geoServerClient, String workspaceName, String schema, String dbName,
             String layerName, String costTable) {
 
-        String tspLayer = "WITH tsp AS (\n" +
-                "    SELECT *\n" +
-                "    FROM pgr_TSP(\n" +
-                "        $$SELECT * FROM pgr_dijkstraCostMatrix(\n" +
-                "            '" + costTable + "',\n" +
-                "            (\n" +
-                "                SELECT ARRAY_APPEND(array_agg(id), (%target%))\n" +
-                "                FROM "+routeTablePrefix+"ways_vertices_pgr\n" +
-                "                WHERE id IN (SELECT DISTINCT nearest_node FROM " + poiTableName
-                + ", "+floodTableName+" WHERE ST_DISTANCE (" + poiTableName
-                + ".geom::geography, "+floodTableName+".geom::geography) <= "+Double.toString(floodCutOff)+")\n"
-                +
-                "            ),\n" +
-                "            false\n" +
-                "        )$$, \n" +
-                "        (%target%)," +
-                "        (%target%)" +
-                "    )\n" +
-                "),\n" +
+        String tspLayer = generateTSPCTE(costTable) +
                 "tsp_route AS (\n" +
-                "SELECT "+routeTablePrefix+"ways.the_geom\n" +
+                "SELECT " + routeTablePrefix + "ways.the_geom\n" +
                 "FROM (\n" +
                 "    SELECT ROW_NUMBER() OVER() as seq, tsp.node\n" +
                 "    FROM tsp\n" +
@@ -65,7 +46,7 @@ public class TSPRouteGenerator {
                 "    n1.node,\n" +
                 "    n2.node, false\n" +
                 ") AS di ON true\n" +
-                "JOIN "+routeTablePrefix+"ways ON di.edge = "+routeTablePrefix+"ways.gid\n" +
+                "JOIN " + routeTablePrefix + "ways ON di.edge = " + routeTablePrefix + "ways.gid\n" +
                 "ORDER BY n1.seq\n" +
                 ")\n" +
                 "SELECT tsp_route.the_geom as geom\n" +
@@ -73,6 +54,18 @@ public class TSPRouteGenerator {
 
         // Add TSP route as geoserver layer
         GeoServerInteractor.addTSPGeoserverLayer(geoServerClient, workspaceName, schema, dbName, layerName, tspLayer);
+    }
+
+    String generateTSPCTE(String costTable) {
+        return "WITH tsp AS ( SELECT * FROM pgr_TSP(\n" +
+                "$$SELECT * FROM pgr_dijkstraCostMatrix('" + costTable + "',\n" +
+                "(SELECT ARRAY_APPEND(array_agg(id), (%target%))\n" +
+                "FROM " + routeTablePrefix + "ways_vertices_pgr WHERE id IN (\n" +
+                "SELECT DISTINCT nearest_node FROM " + poiTableName + ", " + floodTableName +
+                " WHERE ST_DISTANCE (" + poiTableName + ".geom::geography, " + floodTableName + ".geom::geography) <= "
+                +
+                Double.toString(floodCutOff) + ")\n" +
+                "), false )$$, (%target%), (%target%) ) ),\n";
     }
 
     /**
@@ -90,28 +83,11 @@ public class TSPRouteGenerator {
 
         layerName = layerName + "_seq";
 
-        String tspLayer = "WITH tsp AS (\n" +
-                "    SELECT *\n" +
-                "    FROM pgr_TSP(\n" +
-                "        $$SELECT * FROM pgr_dijkstraCostMatrix(\n" +
-                "            '" + costTable + "',\n" +
-                "            (\n" +
-                "                SELECT ARRAY_APPEND(array_agg(id), (%target%))\n" +
-                "                FROM "+routeTablePrefix+"ways_vertices_pgr\n" +
-                "                WHERE id IN (SELECT DISTINCT nearest_node FROM " + poiTableName
-                + ", "+floodTableName+" WHERE ST_DISTANCE (" + poiTableName
-                + ".geom::geography, "+floodTableName+".geom::geography) <= "+Double.toString(floodCutOff)+")\n"
-                +
-                "            ),\n" +
-                "            false\n" +
-                "        )$$, \n" +
-                "        (%target%)," +
-                "        (%target%)" +
-                "    )\n" +
-                "), tsp_seq AS (    SELECT nearest_node as id, " + poiTableName + ".geom\n" +
-                "    FROM "+poiTableName+", "+floodTableName+"\n" +
+        String tspLayer = generateTSPCTE(costTable) +
+                "tsp_seq AS (    SELECT nearest_node as id, " + poiTableName + ".geom\n" +
+                "    FROM " + poiTableName + ", " + floodTableName + "\n" +
                 "                WHERE ST_DISTANCE(" + poiTableName
-                + ".geom::geography, "+floodTableName+".geom::geography) <= "+Double.toString(floodCutOff)+")\n"
+                + ".geom::geography, " + floodTableName + ".geom::geography) <= " + Double.toString(floodCutOff) + ")\n"
                 +
                 "SELECT DISTINCT tsp.seq -1 as seq, tsp.node, tsp.cost, tsp.agg_cost, tsp_seq.geom FROM tsp\n" +
                 "FULL JOIN tsp_seq ON tsp.node = tsp_seq.id ORDER BY seq ASC";
