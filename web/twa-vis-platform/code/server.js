e272a82d2f0e300ca2ff61e9430a70d57b94b747
ushcode <opeppard@cmcl.io>
2024-11-11 17:23:43 +0000

    remove redundant logout path

--- web/twa-vis-platform/code/server.js
+++ web/twa-vis-platform/code/server.js
@@ -96,15 +96,6 @@ app.prepare().then(() => {
       res.json({ userName, firstName, lastName, fullName, roles, clientRoles });
     });
 
-    // server.get('/logout', (req, res) => {
-    //   console.log('Gremlin!')
-    //   req.logout(); // Keycloak adapter logout
-    //   req.session.destroy(() => { // This destroys the session
-    //     res.clearCookie('connect.sid', { path: '/' }); // Clear the session cookie
-    //     console.log('Gremlinagain!')
-    //   });
-    // });
-
     const protectedPages = process.env.PROTECTED_PAGES.split(',');
     protectedPages.forEach(page => {
       server.get(page, keycloak.protect());
