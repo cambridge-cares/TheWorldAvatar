14e08cf30291efde22a077ba204e8a35cc1e1b71
ChungTingLao <chung.ting.lao.34@gmail.com>
2024-11-12 15:59:06 +0000

    dev-flood-routing-consolidation: parameterised setting variables for network analysis agent.

--- Agents/NetworkAnalysisAgent/networkanalysisagent/src/main/java/uk/ac/cam/cares/jps/agent/networkanalysisagent/TripCentralityCalculator.java
+++ Agents/NetworkAnalysisAgent/networkanalysisagent/src/main/java/uk/ac/cam/cares/jps/agent/networkanalysisagent/TripCentralityCalculator.java
@@ -1,6 +1,5 @@
 package uk.ac.cam.cares.jps.agent.networkanalysisagent;
 
-
 import com.cmclinnovations.stack.clients.geoserver.GeoServerClient;
 import com.cmclinnovations.stack.clients.geoserver.GeoServerVectorSettings;
 import com.cmclinnovations.stack.clients.geoserver.UpdatedGSVirtualTableEncoder;
@@ -14,16 +13,22 @@ import java.sql.*;
 
 public class TripCentralityCalculator {
 
+    String routeTablePrefix; // prefix of OSM routing tables
+
+    public TripCentralityCalculator(String routeTablePrefix) {
+        this.routeTablePrefix = routeTablePrefix;
+    }
 
     /**
      * Get the nearest node of POI and create the tripcentrality table
+     * 
      * @param remoteRDBStoreClient
-     * @param jsonArray JSONArray of POI
-     * @param tableName Table name of trip centrality table 
-     * @param sql EdgeTableSQL 
+     * @param jsonArray            JSONArray of POI
+     * @param tableName            Table name of trip centrality table
+     * @param sql                  EdgeTableSQL
      */
-    public void calculateTripCentrality(RemoteRDBStoreClient remoteRDBStoreClient, JSONArray jsonArray,String tableName, String sql ) {
-
+    public void calculateTripCentrality(RemoteRDBStoreClient remoteRDBStoreClient, JSONArray jsonArray,
+            String tableName, String sql) {
 
         try (Connection connection = remoteRDBStoreClient.getConnection()) {
 
@@ -33,64 +38,57 @@ public class TripCentralityCalculator {
                 PGgeometry pgGeometry = new PGgeometry(geometry);
                 pgGeometry.getGeometry().setSrid(4326);
 
-                int nearest_node = Integer.parseInt(findNearestNode(connection, geometry));
-                generateTripCentralityTable(connection,nearest_node, tableName,sql);
-                System.out.println("Table generated for " +tableName);
+                int nearestNode = Integer.parseInt(findNearestNode(connection, geometry));
+                generateTripCentralityTable(connection, nearestNode, tableName, sql);
+                System.out.println("Table generated for " + tableName);
 
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             throw new JPSRuntimeException(e);
         }
 
     }
 
-    /** Create trip centrality 
+    /**
+     * Create trip centrality
+     * 
      * @param connection
-     * @param node POI node id
-     * @param tableName Table name of trip centrality table
-     * @param costTable Edge table SQL
+     * @param node       POI node id
+     * @param tableName  Table name of trip centrality table
+     * @param costTable  Edge table SQL
      * @throws SQLException
      */
-    private void generateTripCentralityTable(Connection connection, int node, String tableName, String costTable) throws SQLException {
+    private void generateTripCentralityTable(Connection connection, int node, String tableName, String costTable)
+            throws SQLException {
 
-        String tripCentrality_sql = "CREATE TABLE IF NOT EXISTS tc_"+tableName+" AS (\n" +
-                "    SELECT\n" +
-                "        b.gid,\n" +
-                "        b.the_geom AS geom,\n" +
-                "        COUNT(b.gid) AS count\n" +
-                "    FROM\n" +
-                "        pgr_dijkstra(\n" +
-                "            '"+costTable+"', \n" +
-                "             "+node+", (SELECT ARRAY_AGG(id) FROM routing_ways_vertices_pgr),\n" +
-                "             directed := FALSE\n" +
-                "        ) AS j\n" +
-                "    JOIN routing_ways AS b ON j.edge = b.gid \n" +
-                "    GROUP BY b.gid, b.the_geom \n" +
-                "ORDER BY count DESC\n" +
-                ");";
-        executeSql(connection,tripCentrality_sql);
+        String tripCentralitySQL = "CREATE TABLE IF NOT EXISTS tc_" + tableName + " AS (\n" +
+                "SELECT b.gid, b.the_geom AS geom, COUNT(b.gid) AS count FROM " +
+                "pgr_dijkstra('" + costTable + "', " + node + ", (SELECT ARRAY_AGG(id) FROM " + routeTablePrefix
+                + "ways_vertices_pgr), directed := FALSE) AS j JOIN " + routeTablePrefix
+                + "ways AS b ON j.edge = b.gid GROUP BY b.gid, b.the_geom ORDER BY count DESC);";
+        executeSql(connection, tripCentralitySQL);
     }
 
-
     /**
-     * Generate trip centrality layer that compares the two centrality table by finding the difference of it.
+     * Generate trip centrality layer that compares the two centrality table by
+     * finding the difference of it.
+     * 
      * @param geoServerClient
-     * @param workspaceName 
-     * @param schema Schema name - default to public
-     * @param dbName Datbase name
-     * @param LayerName Geoserver layer name
-     * @param normalTableName First trip centrality table 
-     * @param floodTableName Second trip centrality table to be compared
+     * @param workspaceName
+     * @param schema          Schema name - default to public
+     * @param dbName          Datbase name
+     * @param layerName       Geoserver layer name
+     * @param normalTableName First trip centrality table
+     * @param floodTableName  Second trip centrality table to be compared
      */
-    public void generateTCLayer(GeoServerClient geoServerClient, String workspaceName, String schema, String dbName, String LayerName, String normalTableName, String floodTableName){
-
+    public void generateTCLayer(GeoServerClient geoServerClient, String workspaceName, String schema, String dbName,
+            String layerName, String normalTableName, String floodTableName) {
 
         String tcLayer = "WITH maxcount AS (\n" +
                 "    SELECT MAX(ABS(COALESCE(f.count, 0) - n.count)) AS max\n" +
-                "    FROM tc_"+normalTableName+" n\n" +
-                "    LEFT JOIN tc_"+ floodTableName +" f ON n.gid = f.gid AND n.geom = f.geom\n" +
+                "    FROM tc_" + normalTableName + " n\n" +
+                "    LEFT JOIN tc_" + floodTableName + " f ON n.gid = f.gid AND n.geom = f.geom\n" +
                 ")\n" +
                 "\n" +
                 "SELECT\n" +
@@ -105,11 +103,11 @@ public class TripCentralityCalculator {
                 "    (COALESCE(f.count, 0) - n.count)::decimal / maxcount.max AS count_difference_percentage,\n" +
                 "maxcount.max as max_countdifference\n" +
                 "FROM\n" +
-                "    tc_"+normalTableName+" n\n" +
+                "    tc_" + normalTableName + " n\n" +
                 "LEFT JOIN\n" +
-                "    tc_"+ floodTableName +" f ON n.gid = f.gid AND n.geom = f.geom\n" +
+                "    tc_" + floodTableName + " f ON n.gid = f.gid AND n.geom = f.geom\n" +
                 "JOIN\n" +
-                "    routing_ways r ON n.gid = r.gid\n" +
+                "    " + routeTablePrefix + "ways r ON n.gid = r.gid\n" +
                 "CROSS JOIN\n" +
                 "    maxcount -- Include the CTE in the FROM clause\n" +
                 "ORDER BY \n" +
@@ -119,33 +117,31 @@ public class TripCentralityCalculator {
         GeoServerVectorSettings geoServerVectorSettingsTC = new GeoServerVectorSettings();
         virtualTableTC.setSql(tcLayer);
         virtualTableTC.setEscapeSql(true);
-        virtualTableTC.setName(LayerName);
+        virtualTableTC.setName(layerName);
         virtualTableTC.addVirtualTableGeometry("geom", "Geometry", "4326"); // geom needs to match the sql query
         geoServerVectorSettingsTC.setVirtualTable(virtualTableTC);
-        geoServerClient.createPostGISDataStore(workspaceName,LayerName, dbName, schema);
-        geoServerClient.createPostGISLayer(workspaceName, dbName, "public", LayerName ,geoServerVectorSettingsTC);
+        geoServerClient.createPostGISDataStore(workspaceName, layerName, dbName, schema);
+        geoServerClient.createPostGISLayer(workspaceName, dbName, schema, layerName, geoServerVectorSettingsTC);
     }
-
 
     /**
      * Find the nearest node
+     * 
      * @param connection
-     * @param geom geom wkt from poi
+     * @param geom       geom wkt from poi
      * @return String of id
      * @throws SQLException
      */
     private String findNearestNode(Connection connection, String geom) throws SQLException {
 
-        String geomConvert= "ST_GeometryFromText('"+geom+"', 4326)";
+        String geomConvert = "ST_GeometryFromText('" + geom + "', 4326)";
 
-
-        String findNearestNode_sql = "SELECT id, ST_Distance(the_geom, " + geomConvert + ") AS distance\n" +
-                "FROM routing_ways_vertices_pgr\n" +
-                "ORDER BY the_geom <-> " + geomConvert + "\n" +
-                "LIMIT 1;\n";
+        String findNearestNodeSQL = "SELECT id, ST_Distance(the_geom, " + geomConvert + ") AS distance\n" +
+                "FROM " + routeTablePrefix + "ways_vertices_pgr\n" + "ORDER BY the_geom <-> " + geomConvert
+                + " LIMIT 1;";
 
         try (Statement statement = connection.createStatement()) {
-            try (ResultSet resultSet = statement.executeQuery(findNearestNode_sql)) {
+            try (ResultSet resultSet = statement.executeQuery(findNearestNodeSQL)) {
                 if (resultSet.next()) {
                     // Assuming 'id' and 'distance' are columns in your query result
                     int id = resultSet.getInt("id");
@@ -161,8 +157,9 @@ public class TripCentralityCalculator {
 
     /**
      * Create connection to remoteStoreClient and execute SQL statement
+     * 
      * @param connection PostgreSQL connection object
-     * @param sql SQl statement to execute
+     * @param sql        SQl statement to execute
      */
     private void executeSql(Connection connection, String sql) throws SQLException {
         try (Statement statement = connection.createStatement()) {
