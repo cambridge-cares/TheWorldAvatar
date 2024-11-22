d4e827ae01dd8f454efaacbf2518d9296887cf03
qhouyee <quekhouyee@gmail.com>
2024-11-22 17:04:15 +0000

    dev-vip-registry-lifecycle-stages: reset modal open state on clicking return button

--- web/twa-vis-platform/code/src/ui/navigation/return/return.tsx
+++ web/twa-vis-platform/code/src/ui/navigation/return/return.tsx
@@ -2,8 +2,10 @@ import iconStyles from 'ui/graphic/icon/icon-button.module.css';
 import styles from './return.module.css';
 
 import { useRouter } from 'next/navigation';
+import { useDispatch } from 'react-redux';
 
 import MaterialIconButton from 'ui/graphic/icon/icon-button';
+import { setIsOpen } from 'state/modal-slice';
 
 interface ReturnButtonProps {
   styles?: string;
@@ -17,9 +19,15 @@ interface ReturnButtonProps {
  */
 export default function ReturnButton(props: Readonly<ReturnButtonProps>) {
   const router = useRouter();
+  const dispatch = useDispatch();
+
+  const onReturn = () => {
+    dispatch(setIsOpen(false));
+    router.back();
+  };
 
   return (
-    <button type="button" className={`${styles.button} ${props.styles}`} onClick={() => router.back()}>
+    <button type="button" className={`${styles.button} ${props.styles}`} onClick={onReturn}>
       <MaterialIconButton
         iconName="arrow_circle_left"
         iconStyles={[iconStyles["large-icon"]]}
