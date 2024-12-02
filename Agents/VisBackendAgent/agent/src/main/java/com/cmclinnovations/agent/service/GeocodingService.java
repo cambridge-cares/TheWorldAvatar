ca720ead7fb0252fcebc1cae24854337b4723812
qhouyee <quekhouyee@gmail.com>
2024-12-02 15:13:58 +0000

    dev-vis-backend-lifecycle: extended regex for point parsing for wkt literals

--- Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/service/GeocodingService.java
+++ Agents/VisBackendAgent/agent/src/main/java/com/cmclinnovations/agent/service/GeocodingService.java
@@ -298,7 +298,7 @@ public class GeocodingService {
    */
   private double[] parseCoordinates(String geoPoint) {
     // REGEX for `POINT(Longitude Latitude)` format
-    Pattern pattern = Pattern.compile("POINT\\((\\d+\\.\\d+) (\\d+\\.\\d+)\\)");
+    Pattern pattern = Pattern.compile("POINT\\((\\d+\\.\\d+),? (\\d+\\.\\d+)\\)");
     Matcher matcher = pattern.matcher(geoPoint);
 
     if (matcher.matches()) {
