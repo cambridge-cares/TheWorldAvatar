5a1e1d2517c932d851b53c2b0287ef302ed86fb0
Kok Foong Lee <kfl31@cam.ac.uk>
2024-12-02 11:44:34 +0800

    dev-add-session-id: updated user ID based layer to not connect between different login sessions

--- Agents/TrajectoryQueryAgent/trajectoryqueryagent/src/main/resources/line_layer_user_id.sql
+++ Agents/TrajectoryQueryAgent/trajectoryqueryagent/src/main/resources/line_layer_user_id.sql
@@ -18,20 +18,6 @@ timeseries AS (
     FROM
         public.get_location_table((SELECT device_list FROM distinct_devices)) AS timeseries
     ORDER BY time
-),
-
-line AS (
-    SELECT 
-        ts.time as time,
-        LAG(ts.geom) OVER (PARTITION BY user_id, session_id ORDER BY ts.time) AS prev_geom,
-        ST_MakeLine(LAG(ts.geom) OVER (PARTITION BY user_id, session_id ORDER BY ts.time), ts.geom) AS geom,
-        ts.speed AS speed,
-        ts.altitude AS altitude,
-        ts.bearing AS bearing,
-        ts.user_id AS user_id,
-        CONCAT('https://w3id.org/MON/person.owl#person_', ts.user_id) AS iri
-    FROM 
-        timeseries ts
 )
 
 SELECT 
@@ -43,3 +29,5 @@ WHERE
     ('%user_id%' = '' OR user_id = '%user_id%')
     AND (%lowerbound% = 0 OR time > %lowerbound%)
     AND (%upperbound% = 0 OR time < %upperbound%)
+GROUP BY
+    ts.session_id
