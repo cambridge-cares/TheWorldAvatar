dbca7b3b36e178d81a104cbea7de012a533bf472
ChungTingLao <chung.ting.lao.34@gmail.com>
2024-12-04 12:48:42 +0000

    1011-add-dkl-to-modssimpleagnet: updated example python script.

--- Agents/MoDSSimpleAgent/run_example.py
+++ Agents/MoDSSimpleAgent/run_example.py
@@ -1,10 +1,11 @@
 import requests
 import urllib.parse
+import time
 
 # Input parameter
 
-BASE_URL = "" #"http://localhost:58085/request"
-QUERY_FILE = "" #"./examples/HDMR.json"
+BASE_URL = ""#"http://localhost:58085/"
+QUERY_FILE = ""#"./examples/HDMR.json"
 
 if len(BASE_URL)>0 and len(QUERY_FILE)>0:
 
@@ -13,12 +14,18 @@ if len(BASE_URL)>0 and len(QUERY_FILE)>0:
     with open(QUERY_FILE,'r') as file:
         data = file.read()
     data = data.replace('\r','').replace('\n','').replace('\t','').replace(' ','')
-    QUERY_URL=f"{BASE_URL}?query={urllib.parse.quote(data)}"
+    QUERY_URL=f"{BASE_URL}request?query={urllib.parse.quote(data)}"
 
     try:
         response = requests.get(QUERY_URL)
         print(f"Response status code: {response.status_code}")
         print(f"Response content: {response.text}")
+        time.sleep(10)
+        # try to get result
+        RESULT_URL=f"{BASE_URL}output/request?query={urllib.parse.quote(response.text)}"
+        resp = requests.get(RESULT_URL)
+        print(f"Response status code: {resp.status_code}")
+        print(f"Response content: {resp.text}")
     except:
         print("Something went wrong.")
 
