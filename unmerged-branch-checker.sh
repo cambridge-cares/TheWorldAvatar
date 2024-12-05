d1c972c1fdb11c639b1c2d0103360c6ad030a27c
Oisín <oisinpeppard@hotmail.com>
2024-12-05 18:44:02 +0000

    add unmerged branch checker script

---
+++ unmerged-branch-checker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script will check all unmerged branches to see if they contain unmerged commits to files within the specified directory
+
+# Check if the path argument is provided
+if [ -z "$1" ]; then
+    echo -e "Usage: \e[32m $0 <path-to-folder>\e[37m Use this script to iterate over unmerged branches and see if any commits contain changes to file in the directory supplied as the argument"
+    exit 1
+fi
+
+FOLDER_PATH=$1
+BRANCH_COUNT=0
+
+# Iterate over all remote branches and check for changes in the specified folder
+for branch in $(git branch -r | grep -v '\->' | grep -v 'origin/main'); do
+    BRANCH_COUNT=$((BRANCH_COUNT + 1))
+    echo -e "Checking branch \e[32m#$BRANCH_COUNT\e[0m: $branch"
+    git log --oneline --name-status "$branch" --not origin/main -- "$FOLDER_PATH"
+done
+
+echo -e "Total number of branches checked: \e[33m$BRANCH_COUNT\e[0m"
