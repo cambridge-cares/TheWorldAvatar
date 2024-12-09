d64b6733bde44ddcb64d4c31fe1c00bff5f9c89a
George Brownbridge <gbrownbridge@cmcl.io>
2024-12-09 11:23:14 +0000

    repo-moving-tools: Fixed issue with the unmerged-branch-checker.sh when there are multiple remote repos and/or are not called `origin`.

--- unmerged-branch-checker.sh
+++ unmerged-branch-checker.sh
@@ -11,11 +11,14 @@ fi
 FOLDER_PATH=$1
 BRANCH_COUNT=0
 
+IFS=" " read -r -a mainBranches <<<"$(git branch -r | grep -v '\->' | grep '/main')"
+echo "Main branch =" "${mainBranches[@]}"
+
 # Iterate over all remote branches and check for changes in the specified folder
-for branch in $(git branch -r | grep -v '\->' | grep -v 'origin/main'); do
+for branch in $(git branch -r | grep -v '\->' | grep -v '/main'); do
     BRANCH_COUNT=$((BRANCH_COUNT + 1))
     echo -e "Checking branch \e[32m#$BRANCH_COUNT\e[0m: $branch"
-    git log --oneline --name-status "$branch" --not origin/main -- "$FOLDER_PATH" | sed 's/^/\x1b[31m/;s/$/\x1b[0m/'
+    git log --oneline --name-status "$branch" "${mainBranches[@]/#/^}" -- "$FOLDER_PATH" | sed 's/^/\x1b[31m/;s/$/\x1b[0m/'
 done
 
 echo -e "Total number of branches checked: \e[33m$BRANCH_COUNT\e[0m"
