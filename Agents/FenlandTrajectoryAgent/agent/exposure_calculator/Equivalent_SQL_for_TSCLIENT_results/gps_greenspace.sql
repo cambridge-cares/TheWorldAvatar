f04e4050029f822df251930d59f5d4b9df12fbcf
jc2341 <jc2341@cam.ac.uk>
2025-01-20 17:26:25 +0000

    dev-Environmental-Exposure: tested equivalent sql for greenspace and instantiated gps trajectory table, response time 0.057 seconds

--- Agents/FenlandTrajectoryAgent/agent/exposure_calculator/Equivalent_SQL_for_TSCLIENT_results/gps_greenspace.sql
+++ Agents/FenlandTrajectoryAgent/agent/exposure_calculator/Equivalent_SQL_for_TSCLIENT_results/gps_greenspace.sql
@@ -6,7 +6,7 @@ WITH Trajectory AS (
         )::geometry, 
         27700
     ) AS buffered_trajectory
-    FROM "aca2947a-8d41-4edd-a9dd-4ffa8ed616d0" --placehodler, replace with table name from dbtable
+    FROM "35cb3043-9bfc-4cc7-b995-fe6290d8c86b" --placehodler, replace with table name from dbtable
 )
 SELECT g.ogc_fid, g.function, g."distName1", g."distName2", g.wkb_geometry
 FROM public."GB_GreenspaceSite" g, Trajectory t
