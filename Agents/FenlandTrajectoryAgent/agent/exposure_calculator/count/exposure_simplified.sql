ddba2a343515c04edae27224f78ccf54759072d3
Jiying Chen <jc2341@cam.ac.uk>
2025-02-03 01:33:47 +0000

    dev-Environmental-Exposure: remove test case, use sql template for better understanding

--- Agents/FenlandTrajectoryAgent/agent/exposure_calculator/count/exposure_simplified.sql
+++ Agents/FenlandTrajectoryAgent/agent/exposure_calculator/count/exposure_simplified.sql
@@ -2,10 +2,10 @@ WITH BufferedLine AS (
     SELECT 
         ST_Buffer(
             ST_MakeLine(gps."column7"::geometry ORDER BY gps."time")::geography, 
-            100
-        ) AS buffered_geom -- create static buffer
+            {exposure_radius}
+        ) AS buffered_geom
     FROM 
-        "35cb3043-9bfc-4cc7-b995-fe6290d8c86b" AS gps --This table name is the placehodler for test purpose, this table name will be dynamically populated by the agent during actual usage
+        "{table_name}" AS gps
 ),
 combined_frs AS (
     SELECT "Name", "Address", geom
@@ -17,7 +17,7 @@ combined_frs AS (
 SELECT 
     frs."Name" AS entity_name, 
     frs."Address" AS address,
-	ST_AsText(frs.geom) AS entity_geom,
+    ST_AsText(frs.geom) AS entity_geom,
     COUNT(frs."Name") AS no_of_entities
 FROM 
     BufferedLine bl
