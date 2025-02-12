402c5ffcf4c9a8590c278790f93d904d6a88f993
Jiying Chen <jc2341@cam.ac.uk>
2025-02-12 00:04:18 +0000

    dev-Environmental-Exposure-TripDetection: correct route url, create high-level detection function inside the route and test parameter transfer

--- Agents/FenlandTrajectoryAgent/agent/flaskapp/trip_detection/route.py
+++ Agents/FenlandTrajectoryAgent/agent/flaskapp/trip_detection/route.py
@@ -1,4 +1,12 @@
 from flask import Blueprint, request, jsonify
 from agent.exposure_calculator.exposure_utilities import ExposureUtils
 
-trip_detection_bp = Blueprint('exposure_bp', __name__)
+trip_detection_bp = Blueprint('trip_detection_bp', __name__)
+exposure_util = ExposureUtils()
+
+@trip_detection_bp('/trip_detection', methods=['POST'])
+def trip_detect_for_TSCLIENT():
+    
+    return jsonify(final_results)
+
+
