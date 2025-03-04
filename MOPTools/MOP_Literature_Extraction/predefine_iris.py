c27e4b58ff18e77611369518f4bcb7e1a101e619
saluzf <saluzf@ethz.ch>
2025-03-04 09:04:28 +0100

    dev-MOPs-Synthesis-Extraction: Add comments for predefined_iris.py

--- MOPTools/MOP_Literature_Extraction/predefine_iris.py
+++ MOPTools/MOP_Literature_Extraction/predefine_iris.py
@@ -1,15 +1,23 @@
-# CharacteristicPeak
+# -----------------------------------------------
+# Characteristic Peaks (NMR & IR Spectroscopy)
+# -----------------------------------------------
 IR_NA               = "http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#CharacteristicPeak_f6cce625-9d69-4491-bd9d-b096114db7af"
 NMR_NA              = "http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#CharacteristicPeak_920795e4-f412-4ad6-807b-8da69519a332"
-#Species
+# -----------------------------------------------
+# Chemical Species
+# -----------------------------------------------
 KBR                 = "http://www.theworldavatar.com/kb/ontospecies/Species_3b6489f0-a36c-4734-962b-521c8fab639b"
-# Element
+# -----------------------------------------------
+# Chemical Elements
+# -----------------------------------------------
 HYDROGEN            = "http://www.daml.org/2003/01/periodictable/PeriodicTable#Element_43cfac3b-81db-4338-bfb1-b0b3386f7473"
 CARBON              = "http://www.daml.org/2003/01/periodictable/PeriodicTable#Element_f1a5025b-7e20-4a6a-821e-a7b6c0473b8c"
 OXYGEN              = "http://www.daml.org/2003/01/periodictable/PeriodicTable#Element_6a6be1ce-2021-4634-aed4-6a77488765df"
 NITROGEN            = "http://www.daml.org/2003/01/periodictable/PeriodicTable#Element_a9a7806c-f077-4eb2-b5b0-099d51033b7b"
 UNKNOWN_ELEMENT     = "http://www.daml.org/2003/01/periodictable/PeriodicTable#Element_e253b6ca-c169-4e60-b6be-46b95e045a85"
-# vessel type
+# -----------------------------------------------
+# Vessel Types
+# -----------------------------------------------
 VESSEL_SS_TEFLON    = "https://www.theworldavatar.com/kg/OntoSyn/VesselType_eb0f5942-d36b-47b1-86f0-725c1549fa2e"
 GLASS_VIAL          = "https://www.theworldavatar.com/kg/OntoSyn/VesselType_90589d23-44e8-4698-acdf-bee3e44df96f"
 QUARTZ_TUBE         = "https://www.theworldavatar.com/kg/OntoSyn/VesselType_06304c23-7926-45d2-841d-690b5de16ed0"
@@ -18,7 +26,9 @@ GLASS_SCINTILATION_VIAL     = "https://www.theworldavatar.com/kg/OntoSyn/VesselT
 PYREX_TUBE          = "https://www.theworldavatar.com/kg/OntoSyn/VesselType_080ad74b-950d-4651-a87c-5aa96d5ffb52"
 SCHLENK             = "https://www.theworldavatar.com/kg/OntoSyn/VesselType_080zd54b-230c-4341-e87g-5ta46d2fgh91"
 UNDEFINED_VESSEL    = "https://www.theworldavatar.com/kg/OntoSyn/VesselType_183ad74b-950d-4631-a47c-5aa91d5ffb12"
-# unit of measure
+# -----------------------------------------------
+# Units of Measurement
+# -----------------------------------------------
 DEGREE_CELSIUS      = "http://www.ontology-of-units-of-measure.org/resource/om-2/degreeCelsius"
 KELVIN              = "http://www.ontology-of-units-of-measure.org/resource/om-2/kelvin"
 DEGREE_CELSIUS_HOUR = "http://www.ontology-of-units-of-measure.org/resource/om-2/degreeCelsiusPerHour"
@@ -38,17 +48,23 @@ MLITRE              = "http://www.ontology-of-units-of-measure.org/resource/om-2
 REVOLUTIONS_PER_MINUTE  = "http://www.ontology-of-units-of-measure.org/resource/om-2/revolutionPerMinute-Time"
 DROP                = "http://www.ontology-of-units-of-measure.org/resource/om-2/drop"
 UNKNOWN_UNIT        = "http://www.ontology-of-units-of-measure.org/resource/om-2/unknown"
-# Vessel Environment
+# -----------------------------------------------
+# Vessel Environments
+# -----------------------------------------------
 N2_ATMO             = "https://www.theworldavatar.com/kg/OntoSyn/VesselEnvironment_434aa6e1-3ac6-4a08-a208-fbc23e78a758"
 AR_ATMO             = "https://www.theworldavatar.com/kg/OntoSyn/VesselEnvironment_65b5af5d-349d-467c-bd14-b239d4e94376"
 AIR_ATMO            = "https://www.theworldavatar.com/kg/OntoSyn/VesselEnvironment_bd2ef29a-1c5c-40eb-a9b2-84f1a3fda734"
 UNKNOWN_ATMO        = "https://www.theworldavatar.com/kg/OntoSyn/VesselEnvironment_1f70dc2c-5a37-491a-89ec-0897f9dcb7b8"
-# GENERAL UNKNOWNS 
+# -----------------------------------------------
+# General Unknowns
+# -----------------------------------------------
 YIELD_VALUE         = "https://www.theworldavatar.com/kg/OntoSyn/YieldValue_3ed5e18b-5206-405d-ada0-382071f73f74"
 YIELD_INSTANCE      = "https://www.theworldavatar.com/kg/OntoSyn/Yield_3ed5e18b-5206-405d-ada0-382071f73f74"
 CHEMICAL_INPUT      = "https://www.theworldavatar.com/kg/OntoSyn/ChemicalInput_3ed5e18b-5206-405d-ada0-382071f73f74"
 UNKNWON_MOP         = "https://www.theworldavatar.com/kg/ontomops/MetalOrganicPolyhedra_1d981ba2-4072-47ef-9ecd-b9f5cc06a50a"
-# SEPARRATION TYPES
+# -----------------------------------------------
+# Separation Techniques
+# -----------------------------------------------
 CENTRIFUGE          = "https://www.theworldavatar.com/kg/OntoSyn/SeparationType_5aa57330-613e-437d-a22b-dc10833a50b8"
 COLUMN              = "https://www.theworldavatar.com/kg/OntoSyn/SeparationType_aea2a49f-067f-4818-8abc-544dd8696ba8"
 WASH                = "https://www.theworldavatar.com/kg/OntoSyn/SeparationType_61233c76-a0e5-4cb0-8c5c-ab8347955ea6"
@@ -56,11 +72,5 @@ EXTRACTION          = "https://www.theworldavatar.com/kg/OntoSyn/SeparationType_
 UNKNOWN_SEP         = "https://www.theworldavatar.com/kg/OntoSyn/SeparationType_9ff2a8f7-3c9c-4419-9f3a-76b34d8629c0"
 UNKNOWN_STEP        = "https://www.theworldavatar.com/kg/OntoSyn/SynthesisStep_ddb7ceda-13d2-461a-a63e-9e7df3116882"
 PERCENTAGE          = "http://www.ontology-of-units-of-measure.org/resource/om-2/percent"
-
-
-
-
-
-
 
 
