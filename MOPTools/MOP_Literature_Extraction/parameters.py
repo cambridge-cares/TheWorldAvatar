2a100e60dbf31196a1b84b868f35f6bd1ce1f274
saluzf <saluzf@ethz.ch>
2025-03-05 21:09:43 +0100

    dev-MOPs-Synthesis-Extraction: Remove file path from parameters.py

--- MOPTools/MOP_Literature_Extraction/parameters.py
+++ MOPTools/MOP_Literature_Extraction/parameters.py
@@ -5,6 +5,6 @@ BATCH_NAME          = "exampleBatchName"
 # Name of the language model used for processing
 MODEL_NAME          = "gpt-4o-2024-08-06"
 # Path to the directory where data files are stored
-DATA_FOLDER         = "/Users/fabiosaluz/Documents/newTWA/TheWorldAvatar/MOPTools/MOP_Literature_Extraction/Data"
+DATA_FOLDER         = "PathToTWALocation/TheWorldAvatar/MOPTools/MOP_Literature_Extraction/Data"
 # Debug mode flag (set to True to enable debug logging and verbose output)
 DEBUGER_MODE        = Boolean
