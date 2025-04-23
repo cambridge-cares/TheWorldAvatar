cc37ecfdd1a1a0aa9014f71b4e7acf971a55e14b
sandradeng20 <xsd20@cam.ac.uk>
2025-04-23 14:38:47 +0800

    1854-environment-exposure-calculation: update boundary code for new sample point table design

--- Agents/EnvironmentExposureCalculationAgent/agent/boundary/buffer.py
+++ Agents/EnvironmentExposureCalculationAgent/agent/boundary/buffer.py
@@ -11,4 +11,4 @@ def create_buffer_around_points(table_name_helper: TableNameHelper, buffer_radiu
     script_dir = os.path.dirname(os.path.abspath(__file__))
     buffer_sql_path = os.path.join(script_dir, "script", "buffer.sql")
     with open(buffer_sql_path, "r") as file:
-        postgis_client.execute_update(file.read(), {"point_table_name": table_name_helper.get_points_table_name()}, {"buffer_radius": buffer_radius})
+        postgis_client.execute_update(file.read(), None, {"buffer_radius": buffer_radius, "query_id": table_name_helper.get_table_name()})
--- Agents/EnvironmentExposureCalculationAgent/agent/boundary/script/buffer.sql
+++ Agents/EnvironmentExposureCalculationAgent/agent/boundary/script/buffer.sql
@@ -1,13 +1,12 @@
-ALTER TABLE "env_exposure"."%(point_table_name)s" DROP COLUMN IF EXISTS buffer_geom;
-ALTER TABLE "env_exposure"."%(point_table_name)s" ADD COLUMN buffer_geom Geometry;
-UPDATE "env_exposure"."%(point_table_name)s"
+UPDATE "env_exposure"."point_table"
 SET buffer_geom = ST_Transform(
     ST_Buffer(
         ST_Transform(
-            ST_SetSRID(ST_MakePoint(lon, lat), 4326),
+            ST_SetSRID(ST_GeomFromWKB(wkb_geometry), 4326),
             3857
         ),
         %(buffer_radius)s
     ),
     4326
-);
+)
+WHERE query_id = %(query_id)s;
