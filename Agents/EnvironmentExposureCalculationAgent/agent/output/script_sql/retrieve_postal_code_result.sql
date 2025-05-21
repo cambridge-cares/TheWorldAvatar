ae15be3831cb27a13bf0b01662f30603b7367a9c
sandradeng20 <xsd20@cam.ac.uk>
2025-05-21 17:27:02 +0800

    1854-environment-exposure-calculation: fill null value with zero in output

--- Agents/EnvironmentExposureCalculationAgent/agent/output/script_sql/retrieve_postal_code_result.sql
+++ Agents/EnvironmentExposureCalculationAgent/agent/output/script_sql/retrieve_postal_code_result.sql
@@ -6,7 +6,7 @@ WITH lat_lng AS (
     ) AS subquery
 )
 
-SELECT pc.postal_code, ll.lng, ll.lat, po.greenspace_area, po.greenspace
+SELECT pc.postal_code, ll.lng, ll.lat, COALESCE(po.greenspace_area, 0) AS greenspace_area, COALESCE(po.greenspace, 0) AS greenspace
 FROM "env_exposure"."point_table" as po
 JOIN "public"."sgpostcode" as pc ON pc.ogc_fid = po.ogc_fid
 JOIN lat_lng as ll ON ll.id = po.id
