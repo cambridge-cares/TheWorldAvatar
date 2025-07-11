0b771bf9c5910ac36b569f6bc8e858c575878944
JESDANIELLA <127171884+JESDANIELLA@users.noreply.github.com>
2025-07-11 15:06:47 +0800

    timeline-app-add-recording-button : added the logout logic to user menu logout button

--- Apps/TimelineApp/feature/user/src/main/java/uk/ac/cam/cares/jps/user/UserDialogFragment.java
+++ Apps/TimelineApp/feature/user/src/main/java/uk/ac/cam/cares/jps/user/UserDialogFragment.java
@@ -5,24 +5,26 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavDeepLinkRequest;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.fragment.app.FragmentManager;
 
 import dagger.hilt.android.AndroidEntryPoint;
+import kotlin.Pair;
 import uk.ac.cam.cares.jps.user.databinding.FragmentUserDialogBinding;
 import uk.ac.cam.cares.jps.user.viewmodel.AccountViewModel;
+import uk.ac.cam.cares.jps.user.viewmodel.SensorViewModel;
 import uk.ac.cam.cares.jps.ui.UiUtils;
 
 @AndroidEntryPoint
@@ -30,6 +32,7 @@ public class UserDialogFragment extends DialogFragment {
 
     private FragmentUserDialogBinding binding;
     private AccountViewModel accountViewModel;
+    private SensorViewModel sensorViewModel;
 
     public static void show(@NonNull FragmentManager fragmentManager) {
         UserDialogFragment dialog = new UserDialogFragment();
@@ -41,6 +44,8 @@ public class UserDialogFragment extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentUserDialogBinding.inflate(inflater, container, false);
         accountViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
+        sensorViewModel = new ViewModelProvider(requireActivity()).get(SensorViewModel.class);
+        accountViewModel.registerForLogoutResult(this);
         binding.setLifecycleOwner(getViewLifecycleOwner());
         binding.setAccountViewModel(accountViewModel);
         return binding.getRoot();
@@ -53,6 +58,24 @@ public class UserDialogFragment extends DialogFragment {
         if (accountViewModel.name.getValue() == null || accountViewModel.name.getValue().isEmpty()) {
             accountViewModel.getUserInfo();
         }
+
+        accountViewModel.logoutStatus.observe(getViewLifecycleOwner(), status -> {
+            if (status != null && Boolean.TRUE.equals(status.getFirst())) {
+
+                sensorViewModel.clearManagers(status.getSecond());
+
+                dismiss();
+                navigate(R.string.onboarding_fragment_link);
+            } else {
+                Toast.makeText(requireContext(), uk.ac.cam.cares.jps.loginmodule.R.string.cancel_logout, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        accountViewModel.shouldShowSessionExpired.observe(getViewLifecycleOwner(), expired -> {
+            if (Boolean.TRUE.equals(expired)) {
+                accountViewModel.getSessionExpiredDialog(this).show();
+            }
+        });
 
         binding.accountSetting.setOnClickListener(v -> {
             dismiss();
@@ -77,7 +100,7 @@ public class UserDialogFragment extends DialogFragment {
         binding.privacySetting.setOnClickListener(v -> UiUtils.showNotImplementedDialog(requireContext()));
         binding.healthReport.setOnClickListener(v -> UiUtils.showNotImplementedDialog(requireContext()));
         binding.locationHistory.setOnClickListener(v -> UiUtils.showNotImplementedDialog(requireContext()));
-        binding.logOut.setOnClickListener(v -> UiUtils.showNotImplementedDialog(requireContext()));
+        binding.logOut.setOnClickListener(v -> accountViewModel.logout());
     }
 
     private void navigate(int uriResId) {
