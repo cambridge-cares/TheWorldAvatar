43eb4b5d76178c511c289c94f746bab8e4947a8c
JESDANIELLA <127171884+JESDANIELLA@users.noreply.github.com>
2025-07-11 12:47:01 +0800

    timeline-app-add-recording-button : re-added the help pages

--- Apps/TimelineApp/feature/user/src/main/java/uk/ac/cam/cares/jps/user/HelpSensorFragment.java
+++ Apps/TimelineApp/feature/user/src/main/java/uk/ac/cam/cares/jps/user/HelpSensorFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +12,6 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import dagger.hilt.android.AndroidEntryPoint;
-import uk.ac.cam.cares.jps.user.R;
 import uk.ac.cam.cares.jps.user.databinding.HelpSensorDocumentationBinding;
 
 @AndroidEntryPoint
@@ -32,6 +32,25 @@ public class HelpSensorFragment extends Fragment {
         binding.topAppbar.setNavigationOnClickListener(v ->
                 NavHostFragment.findNavController(this).navigateUp());
 
+        setupExpandable(binding.contentAccelerometer, binding.iconExpandAccelerometer);
+        setupExpandable(binding.contentActivity, binding.iconExpandActivity);
+        setupExpandable(binding.contentGravity, binding.iconExpandGravity);
+        setupExpandable(binding.contentGyroscope, binding.iconExpandGyroscope);
+        setupExpandable(binding.contentLocation, binding.iconExpandLocation);
+        setupExpandable(binding.contentMagnetometer, binding.iconExpandMagnetometer);
+        setupExpandable(binding.contentLight, binding.iconExpandLight);
+        setupExpandable(binding.contentHumidity, binding.iconExpandHumidity);
+        setupExpandable(binding.contentPressure, binding.iconExpandPressure);
+
         return binding.getRoot();
+    }
+
+    private void setupExpandable(View content, ImageView icon) {
+        View header = (View) icon.getParent();
+        header.setOnClickListener(v -> {
+            boolean isVisible = content.getVisibility() == View.VISIBLE;
+            content.setVisibility(isVisible ? View.GONE : View.VISIBLE);
+            icon.setRotation(isVisible ? 0 : 180);
+        });
     }
 }
