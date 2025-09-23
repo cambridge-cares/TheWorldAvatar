9c4a881b215b26bb2fe05c024229b2daf30f1b8e
Sebastian Mosbach <sm453@cam.ac.uk>
2025-09-23 10:47:01 +0100

    dev-marie-pka: Added docker compose project name in backend deploy script.

--- QuestionAnswering/QA_ICL/backend/deploy.sh
+++ QuestionAnswering/QA_ICL/backend/deploy.sh
@@ -1,2 +1,2 @@
-docker compose -f docker-compose.yaml up -d --build
+docker compose -f docker-compose.yaml -p marie up -d --build
 docker exec -it fastapi_app python -m ingest_data --redis_host redis-stack --text_embedding_backend triton --text_embedding_url "triton:8001"
